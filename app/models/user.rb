515a01eb62ca95fd7b60f599300e4cdb766c8d67
Dan Thomas <dan.thomas@dc.gov>
2015-01-04 12:14:41 -0500

    Add GlueDB extension code

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,14 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  field :authentication_token
+  field :role, :type => String, :default => "user"
+
+  ROLES = %w[user edi_ops admin]
+
   # Enable polymorphic associations
   belongs_to :profile, polymorphic: true
+
 
   ## Confirmable
   # field :confirmation_token,   type: String
@@ -38,4 +44,56 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+
+  before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super # Use whatever other message
+    end
+  end
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
+
+  def self.find_all_by_approved(appvd_status)
+    where(approved: appvd_status)
+  end
+
+  def self.find_by_authentication_token(token)
+    where(authentication_token: token).first
+  end
+
+protected
+  def send_admin_mail
+    AdminMailer.new_user_waiting_for_approval(self).deliver
+  end
+
+private
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
+  
 end
