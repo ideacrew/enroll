e8da56fedc7fec9c5ba3aad1f45bce881d3c9c89
Dan Thomas <dan.thomas@dc.gov>
2015-01-04 12:36:14 -0500

    fix model name

--- spec/models/hbx_staff_spec.rb
+++ spec/models/hbx_staff_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe HbxStaffProfile, :type => :model do
+RSpec.describe HbxStaff, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
