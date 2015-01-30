0d3591c9599d519c3735d36a69072822e85304e6
user <jakemunta@gmail.com>
2015-01-30 08:27:42 -0500

    Added enrollment code in enrollment factory

--- app/models/person.rb
+++ app/models/person.rb
@@ -174,37 +174,6 @@ class Person
   # def employees=()
   # end
 
-  def add_employee_role(new_employer, new_ssn, new_dob, new_gender, new_date_of_hire)
-
-    # Return instance if this role already exists
-
-    # Verify/assign required additional local attributes
-    ssn = new_ssn
-    dob = new_dob
-    gender = new_gender
-
-    # Assign employee-specifc attributes
-    employee_role = self.build_employee(employer: new_employer, date_of_hire: new_date_of_hire)
-
-    # Add 'self' to personal relationship
-    self.personal_relationships << PersonRelationhip.new()
-
-    # Instantiate new family model
-    family = self.families.build()
-
-    # Persist results?
-    self.save
-    employee_role.save
-    family.save
-
-    # Return new instance
-    employee_role
-  end
-
-  def add_consumer_role
-  end
-
-
   def subscriber
     abs_subscriber = nil
     case self.subscriber_type
