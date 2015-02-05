3cdf8a392131d1d53229be43955f37ef3f59b12d
Trey Evans <lewis.r.evans@gmail.com>
2015-02-05 13:43:30 -0500

    Updating deployment settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,36 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'enroll'
+set :repo_url, 'git@github.com:dchbx/enroll.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/deployments/enroll'
 
 # Default value for :scm is :git
 # set :scm, :git
+# set :scm, :gitcopy
 
 # Default value for :format is :pretty
 # set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
+set :bundle_binstubs, false
+set :bundle_flags, "--quiet"
+set :bundle_path, nil
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, (fetch(:linked_files, []) | ['config/database.rb'])
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'pids')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,14 +39,19 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 20 do
+        sudo "service unicorn_enroll restart"
+      end
     end
-  end
+
+    after :publishing, :restart
+
+
+    after :restart, :clear_cache do
+      on roles(:web), in: :groups, limit: 3, wait: 10 do
+      end
+    end
 
 end
