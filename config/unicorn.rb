5ef9cfe184a0153b56e542b2b90bffef92ae0f00
Trey Evans <lewis.r.evans@gmail.com>
2015-02-05 13:03:43 -0500

    Working on deployment settings.

---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+root_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+shared_path = File.expand_path(File.join(root_path, "..", "shared"))
+
+working_directory root_path
+pid shared_path + "/pids/unicorn.pid"
+stderr_path shared_path + "/log/unicorn.log"
+stdout_path shared_path + "/log/unicorn.log"
+
+listen "/tmp/unicorn_enroll.ap.sock"
+worker_processes 20
+timeout 30
