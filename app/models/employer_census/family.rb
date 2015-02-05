4b123f33f63c869867c46384091b1e8e05d7f184
Dan Thomas <dan.thomas@dc.gov>
2015-02-04 22:41:00 -0500

    Add clone model and specs

--- app/models/employer_census/family.rb
+++ app/models/employer_census/family.rb
@@ -3,12 +3,15 @@ class EmployerCensus::Family
 
   embedded_in :employer
 
+
+  field :matched_at, type: DateTime
   field :is_active, type: Boolean, default: true
 
   embeds_one :employee, 
     class_name: "EmployerCensus::Employee",
     cascade_callbacks: true,
     validate: true
+
   embeds_many :dependents, 
     class_name: "EmployerCensus::Dependent",
     cascade_callbacks: true,
@@ -17,6 +20,16 @@ class EmployerCensus::Family
   validates_presence_of :employee
 
   scope :active, ->{ where(:is_active => true) }
+
+  # Create a copy of this instance for rehires into same organization
+  def clone
+    copy = self.dup
+    copy.employee.date_of_hire = nil
+    copy.employee.date_of_termination = nil
+    copy.matched_at = nil
+    copy.is_active = true
+    copy
+  end
 
   def is_active?
     self.is_active
