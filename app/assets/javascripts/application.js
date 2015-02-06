67b93f93522bb4a3a515de062fd91a5e25a6bec3
Brian Russel Davis <brian@nurdymuny.com>
2015-02-06 14:37:12 +0545

    Fixing Search Employer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,20 +23,43 @@ $(document).ready(function () {
   $('.floatlabel').floatlabel({
       slideInput: false
   });
+
+  $('.autofill_yes').click(function(){
+    $('.autofill-initial').addClass('hidden');
+    $('.autofill-failed').removeClass('hidden');
+    common_body_style();
+    side_bar_link_style();
+
+    $.ajax({
+        type: "POST",
+        url: "/people/match_person.json",
+        data: $('#new_person').serialize(),
+        success: function (result) {
+          alert("find your details.Please select employer");
+          getAllEmployers();
+        }
+    });
+  });
+  
+  $('.autofill_no').click(function(){
+    $('.autofill-cloud').addClass('hidden');
+    side_bar_link_style();
+  });
   
   $("#person_ssn").on("blur", function(){
-    confirm_flag = confirm("We may be able to auto-fill your information with data from our records");
-    if(confirm_flag){
-        $.ajax({
-      type: "POST",
-      url: "/people/match_person.json",   
-      data: $('#new_person').serialize(),
-      success: function (result) {
-        alert("find your details.Please select employer");
-        getAllEmployers();
-      }
- });
-    }
+    $('.autofill-cloud.autofill-initial').removeClass('hidden');
+    // confirm_flag = confirm("We may be able to auto-fill your information with data from our records");
+    // if(confirm_flag){
+    //     $.ajax({
+    //       type: "POST",
+    //       url: "/people/match_person.json",
+    //       data: $('#new_person').serialize(),
+    //       success: function (result) {
+    //         alert("find your details.Please select employer");
+    //         getAllEmployers();
+    //       }
+    //  });
+    // }
     
   });
   
@@ -45,7 +68,104 @@ $(document).ready(function () {
     $.ajax({
       type: "GET",
       url: "/people/get_employer.js"
- });
-}
+    });
+  }
+
+
+
+  // People/new Page
+  $('.focus_effect').click(function(e){
+    var check = check_personal_info_exists();
+    active_div_id = $(this).attr('id');
+    if( check.length==0 && (!$('.autofill-failed').hasClass('hidden') || $('.autofill-cloud').hasClass('hidden'))) {
+      $('.focus_effect').removeClass('personaol-info-top-row');
+      $('.focus_effect').removeClass('personaol-info-row');
+      $('.focus_effect').addClass('personaol-info-row');
+      $(this).addClass('personaol-info-top-row');
+      $('.sidebar a').removeClass('style_s_link');
+      $('.sidebar a.'+active_div_id).addClass('style_s_link');
+      $(this).removeClass('personaol-info-row');
+    }
+    if(active_div_id!='personal_info') {
+      if(check.length!=0) {
+        $('#personal_info .col-md-10').addClass('require-field');
+      }
+    }
+  });
+
+  $('#continue').click(function() {
+    $("#overlay").css("display", "none");
+    $(".welcome-msg").css("display", "none");
+    $(".focus_effect").css("opacity", "1");
+    $(".information").css("opacity", "1");
+    $("a.name").css("padding-top", "65px");
+    $(".disable-btn").css("display", "inline-block");
+    $('.focus_effect:first').addClass('personaol-info-top-row');
+    $('.focus_effect:first').removeClass('personaol-info-row');
+    $('.sidebar a:first').addClass('style_s_link');
+    $(".key").css("display", "block");
+    var check = check_personal_info_exists();
+    if(check.length==0) {
+      $('.autofill-cloud.autofill-initial').removeClass('hidden');
+    }
+  });
   
+  $('#personal_info input:text').focusout(function(){
+    var check = check_personal_info_exists();
+    if(check.length==0) {
+      $('.autofill-cloud.autofill-initial').removeClass('hidden');
+    }
+  })
+  
+  $('#personal_info.focus_effect').focusout(function(){
+    var tag_id = $(this).attr('id');
+    var has_class = $(this).hasClass('personaol-info-top-row');
+    var check = check_personal_info_exists();
+    if(check.length!=0 && !has_class) {
+      $('#personal_info .col-md-10').addClass('require-field');
+    }
+    else {
+      $('#personal_info .col-md-10').removeClass('require-field');
+    }
+  })
+  
+  $('span.close').click(function(){
+    $('.autofill-cloud').addClass('hidden');
+    common_body_style();
+    side_bar_link_style();
+  });
+  
+  function side_bar_link_style()
+  {
+    $('.sidebar a').removeClass('style_s_link');
+    $('.sidebar a.address_info').addClass('style_s_link');
+  }
+  
+  function common_body_style()
+  {
+    $('#personal_info').addClass('personaol-info-row');
+    $('.focus_effect').removeClass('personaol-info-top-row');
+    $('#address_info').addClass('personaol-info-top-row');
+    $('#address_info').removeClass('personaol-info-row');
+  }
+  
+  function check_personal_info_exists()
+  {
+    var check = $('#personal_info input:text').filter(function() { return this.value == ""; });
+    return check;
+  }
+
+  $(".adderess-select-box").focusin(function() {
+    $(".bg-color").css({
+      "background-color": "rgba(220, 234, 241, 1)", 
+      "height": "46px", 
+    });
+  });
+  
+  $(".adderess-select-box").focusout(function() {
+    $(".bg-color").css({
+      "background-color": "rgba(255, 255, 255, 1)", 
+      "height": "46px", 
+    });
+  });
 });
