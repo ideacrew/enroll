621897060da913f8d2c8686079c26c0a6bb7b139
Trey Evans <lewis.r.evans@gmail.com>
2015-02-08 22:04:38 -0500

    More work on employee spec.

--- spec/models/requests/employee_request_spec.rb
+++ spec/models/requests/employee_request_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
 
 describe Requests::EmployeeRequest do
-  let(:valid_params) { { 
-    :name_first => "A first name",
-    :name_last => "A last name",
-    :ssn => "123456789"
-  } }
+  let(:params) { { } }
 
-  subject { Requests::EmployeeRequest.new(valid_params) }
+  subject { Requests::EmployeeRequest.new(params) }
 
-  it "should be valid" do
-    expect(subject).to be_valid
+  describe "with validations" do
+    before(:each) { subject.valid? }
+
+    it "should have errors on first_name" do
+      expect(subject.errors).to include(:name_first)
+    end
+
+    it "should have errors on last_name" do
+      expect(subject.errors).to include(:name_last)
+    end
   end
+
+  describe "with valid parameters" do
+    let(:params) { { 
+      :name_first => "A first name",
+      :name_last => "A last name",
+      :name_middle => "A middle name",
+      :ssn => "123456789",
+      :dob => "10/3/1950",
+      :gender => "female"
+    } }
+
+
+    it "should be valid" do
+      expect(subject).to be_valid
+    end
+  end
+
 end
