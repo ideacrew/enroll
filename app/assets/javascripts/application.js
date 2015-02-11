dfbeed381cffe611195ba55524324675d6c51c32
Kevin Wei <kevin.tw.wei@gmail.com>
2015-02-11 14:24:17 -0500

    Adding month and year options to date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,19 +22,15 @@
 $('input.floatlabel').floatlabel();
 
 $(document).ready(function () {
-  
-  $(".date-picker, .date_picker").datepicker();
+
+  $(".date-picker, .date_picker").datepicker({
+    changeMonth: true,
+    changeYear: true
+    });
   $('.floatlabel').floatlabel({
       slideInput: false
   });
-  
-  $(".address-li").on('click',function(){
-    $(".address-span").html($(this).data("address-text"));
-    $(".address-row").hide();
-    divtoshow = $(this).data("value") + "-div";
-    $("."+divtoshow).show();
-  })
-  
+
   $(".phone-mask").inputmask("(999) 999-9999");
 
   $('.autofill_yes').click(function(){
@@ -51,17 +47,17 @@ $(document).ready(function () {
     $('.searching_span').text('Searching');
     $('.search_alert_msg').addClass('hidden');
     getAllEmployers();
-    
+
     //Sidebar Switch - Search Active
     $('#personal_sidebar').addClass('hidden');
     $('#search_sidebar').removeClass('hidden');
   });
-  
+
   $('.autofill_no').click(function(){
     $('.autofill-cloud').addClass('hidden');
     side_bar_link_style();
   });
-  
+
   $("#person_ssn").on("blur", function(){
     $('.autofill-failed').addClass('hidden');
     $('.autofill-cloud.autofill-initial').removeClass('hidden');
@@ -71,7 +67,7 @@ $(document).ready(function () {
         data: $('#new_person').serialize(),
         success: function (result) {
          // result.person gives person info to populate
-        
+
         if(result.matched == true)
         {
           $(".div-cloud-image").show();
@@ -91,7 +87,7 @@ $(document).ready(function () {
     //  });
     // }
   });
-  
+
   function getAllEmployers()
   {
     $.ajax({
@@ -135,33 +131,28 @@ $(document).ready(function () {
 
   $('#continue').click(function() {
     $("#overlay").css("display", "none");
-    $(".emp-welcome-msg").css("display", "none");
+    $(".welcome-msg").css("display", "none");
     $(".focus_effect").css("opacity", "1");
     $(".information").css("opacity", "1");
     $("a.name").css("padding-top", "65px");
     $(".disable-btn").css("display", "inline-block");
-    $(".welcome-msg").css("display", "none");
-    $("a.welcome_msg").css("display", "none");
-    $("a.credential_info, a.name_info, a.tax_info").css("display", "block");
-    $("#tax_info .btn-continue").css("display", "inline-block");
     $('.focus_effect:first').addClass('personaol-info-top-row');
     $('.focus_effect:first').removeClass('personaol-info-row');
     $('.sidebar a:first').addClass('style_s_link');
-    $('.sidebar a.credential_info').addClass('style_s_link');
     $(".key").css("display", "block");
     var check = check_personal_info_exists();
     if(check.length==0) {
       $('.autofill-cloud.autofill-initial').removeClass('hidden');
     }
   });
-  
+
   $('#personal_info input:text').focusout(function(){
     var check = check_personal_info_exists();
     if(check.length==0) {
       $('.autofill-cloud.autofill-initial').removeClass('hidden');
     }
   })
-  
+
   $('#personal_info.focus_effect').focusout(function(){
     var tag_id = $(this).attr('id');
     var has_class = $(this).hasClass('personaol-info-top-row');
@@ -173,19 +164,19 @@ $(document).ready(function () {
       $('#personal_info .col-md-10').removeClass('require-field');
     }
   })
-  
+
   $('span.close').click(function(){
     $('.autofill-cloud').addClass('hidden');
     common_body_style();
     side_bar_link_style();
   });
-  
+
   function side_bar_link_style()
   {
     $('.sidebar a').removeClass('style_s_link');
     $('.sidebar a.address_info').addClass('style_s_link');
   }
-  
+
   function common_body_style()
   {
 
@@ -194,32 +185,24 @@ $(document).ready(function () {
     $('#address_info').addClass('personaol-info-top-row');
     $('#address_info').removeClass('personaol-info-row');
   }
-  
-  function change_address(kind)
-  {
-    $(".address-row").hide();
-    $(kind+"-div").show();
-    
-    //address-li
-  }
-    
+
   function check_personal_info_exists()
   {
-    var check = $('#personal_info input[required1]').filter(function() { return this.value == ""; });
+    var check = $('#personal_info input[required]').filter(function() { return this.value == ""; });
     return check;
   }
 
-  $(".qqqadderess-select-box").focusin(function() {
+  $(".adderess-select-box").focusin(function() {
     $(".bg-color").css({
-      "background-color": "rgba(220, 234, 241, 1)", 
-      "height": "46px", 
+      "background-color": "rgba(220, 234, 241, 1)",
+      "height": "46px",
     });
   });
-  
-  $(".qqadderess-select-box").focusout(function() {
+
+  $(".adderess-select-box").focusout(function() {
     $(".bg-color").css({
-      "background-color": "rgba(255, 255, 255, 1)", 
-      "height": "46px", 
+      "background-color": "rgba(255, 255, 255, 1)",
+      "height": "46px",
     });
   });
 });
