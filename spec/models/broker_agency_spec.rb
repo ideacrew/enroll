5d95e26f3551ae3d04c90e4bea0b1a73f3a3caf2
Dan Thomas <dan.thomas@dc.gov>
2015-02-17 11:02:00 -0500

    Add model instantiation specs

--- spec/models/broker_agency_spec.rb
+++ spec/models/broker_agency_spec.rb
@@ -1,7 +1,57 @@
 require 'rails_helper'
 
-describe BrokerAgency, ".new", type: :model do
+describe BrokerAgency, type: :model do
 
   it { should validate_presence_of :name }
+  it { should validate_presence_of :market_kind }
   it { should validate_presence_of :primary_broker_id }
+
+
+  let(:name) {"Acme Brokers, Inc"}
+  let(:market_kind) {"Individual"}
+  let(:primary_broker) {FactoryGirl.create(:broker)}
+
+  describe ".new" do
+    let(:valid_params) do
+      { name: name,
+        market_kind: market_kind,
+        primary_broker: primary_broker
+      }
+    end
+
+    context "with no arguments" do
+      let(:params) {{}}
+
+      it "should not save" do
+        expect(BrokerAgency.new(**params).save).to be_false
+      end
+    end
+
+    context "with no name" do
+      let(:params) {valid_params.except(:name)}
+
+      it "should fail validation" do
+        expect(BrokerAgency.create(**params).errors[:name].any?).to be_true
+      end
+    end
+
+    context "with no market_kind" do
+      let(:params) {valid_params.except(:market_kind)}
+
+      it "should fail validation" do
+        expect(BrokerAgency.create(**params).errors[:market_kind].any?).to be_true
+      end
+    end
+
+    context "with no primary_broker" do
+      let(:params) {valid_params.except(:primary_broker)}
+
+      it "should fail validation" do
+        expect(BrokerAgency.create(**params).errors[:primary_broker_id].any?).to be_true
+      end
+    end
+
+
+  end
+
 end
