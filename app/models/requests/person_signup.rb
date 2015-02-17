456aa93fb121528e250bdaf468f23a0d7cd5d7a3
Dan Thomas <dan.thomas@dc.gov>
2015-02-17 12:00:13 -0500

    Move validations back into models

--- app/models/requests/person_signup.rb
+++
@@ -1,9 +0,0 @@
-module Requests
-  class PersonSignup
-    include ActiveModel::Model
-
-    attr_accessor :date_of_birth
-
-    include Validations::USDate.on(:date_of_birth)
-  end
-end
--- spec/models/requests/person_signup_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-describe Requests::PersonSignup do
-  it "should have error on dob" do
-    subject.valid?
-    expect(subject.errors).to include(:date_of_birth)
-  end
-end
