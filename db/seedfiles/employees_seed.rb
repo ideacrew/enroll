82f1e6ac5d1e84b2dde5d0413f1b82ef1d841bc8
Sean Carley <milythael.mourning@gmail.com>
2015-02-23 12:14:45 -0600

    user EnrollmentFactory.add_employee_role in seed data

--- db/seedfiles/employees_seed.rb
+++ db/seedfiles/employees_seed.rb
@@ -1,21 +1,25 @@
+require 'factories/enrollment_factory'
+
 puts "*"*80
 puts "::: Creating Employee Roles:::"
 
-er = Employer.first
-ee = er.employee_families.first.employee
-
-psn = Person.create!(
-    first_name: ee.first_name, 
-    last_name: ee.last_name,
-    addresses: [ee.address],
-    gender: ee.gender,
-    ssn: ee.ssn,
-    dob: ee.dob
-  )
-
-e0 = psn.employees.build
-e0.employer = er
-e0.hired_on = ee.hired_on
-e0.save!
+Employer.each do |employer|
+  employer.employee_families.each do |census_employee_family|
+    census_employee = census_employee_family.employee
+    person = Person.create!(
+        first_name: census_employee.first_name,
+        last_name: census_employee.last_name,
+        addresses: [census_employee.address],
+    )
+    employee = EnrollmentFactory.add_employee_role(
+      person: person,
+      employer: employer,
+      gender: census_employee.gender,
+      ssn: census_employee.ssn,
+      dob: census_employee.dob,
+      hired_on: census_employee.hired_on
+    )
+  end
+end
 
 puts "::: Employees Complete :::"
