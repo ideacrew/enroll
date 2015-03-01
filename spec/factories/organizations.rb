b15ac2ce76c9019b63b224fa0054750fbce37cfe
Dan Thomas <dan.thomas@dc.gov>
2015-03-01 17:42:18 -0500

    Fix office_location generation

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :organization do
+
     legal_name  "Turner Agency, Inc"
-    dba         "Turner Brokerage"
+    dba         "Turner Brokers"
     fein        "675762234"
     home_page   "http://www.example.com"
-    office_locations  FactoryGirl.build_list(:office_location, 3)
-
+    office_locations  { FactoryGirl.build_list(:office_location, 3) }
   end
-
 end
