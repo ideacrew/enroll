0b9ea0160a8ea0cc67ec83d01692f427338ad43d
Dan Thomas <dan.thomas@dc.gov>
2015-03-15 18:10:19 -0400

    Fix failing spec Add formatter methods

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -15,6 +15,7 @@ class Phone
   field :extension, type: String, default: ""
   field :primary, type: Boolean
   field :full_phone_number, type: String, default: ""
+
   before_validation :save_phone_components
 
   validates :area_code,
@@ -32,27 +33,40 @@ class Phone
     allow_blank: false
 
   def save_phone_components
-    phone_number = filter_non_numbers(self.full_phone_number).to_s
-    if phone_number && phone_number.length == 10
-      self.area_code = phone_number[0,3]
-      self.number = phone_number[3,7]
+    return unless full_phone_number && (full_phone_number.length == 10)
+    self.area_code = full_phone_number[0,3]
+    self.number = full_phone_number[3,7]
+    self.to_s
+  end
+
+  def full_phone_number=(new_full_phone_number)
+   super filter_non_numeric(new_full_phone_number)
+   save_phone_components
+  end
+
+  def area_code=(new_area_code)
+   super filter_non_numeric(new_area_code)
+  end
+
+  def number=(new_number)
+   super filter_non_numeric(new_number)
+  end
+
+  def extension=(new_extension)
+   super filter_non_numeric(new_extension)
+  end
+
+  def to_s
+    full_number = (self.area_code + self.number).to_i
+    if self.extension.present?
+      full_number.to_s(:phone, area_code: true, extension: self.extension)
+    else
+      full_number.to_s(:phone, area_code: true)
     end
   end
 
-  def area_code=(value)
-   super filter_non_numbers(value)
-  end
-
-  def number=(value)
-   super filter_non_numbers(value)
-  end
-
-  def extension=(value)
-   super filter_non_numbers(value)
-  end
-
 private
-  def filter_non_numbers(str)
+  def filter_non_numeric(str)
     str.to_s.gsub(/\D/,'') if str.present? 
   end
 end
