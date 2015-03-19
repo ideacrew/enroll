0e7377ead92f1bf4470e3635fb2f0194848228d4
Kevin Wei <kevin.tw.wei@gmail.com>
2015-03-19 15:44:22 -0400

    Fix factory spec call error

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -87,14 +87,14 @@ describe Family do
     end
     let(:brady_ph) {FactoryGirl.build(:phone, kind: "home", area_code: "202", number: "7620799", extension: nil)}
     let(:last_name) {"Brady"}
-    let(:mike)   {FactoryGirl.create(:male, first_name: "Mike",   last_name: last_name, dob: 40.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:carol)  {FactoryGirl.create(:male, first_name: "Carol",  last_name: last_name, dob: 35.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:greg)   {FactoryGirl.create(:male, first_name: "Greg",   last_name: last_name, dob: 17.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:marcia) {FactoryGirl.create(:male, first_name: "Marcia", last_name: last_name, dob: 16.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:peter)  {FactoryGirl.create(:male, first_name: "Peter",  last_name: last_name, dob: 14.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:jan)    {FactoryGirl.create(:male, first_name: "Jan",    last_name: last_name, dob: 12.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:bobby)  {FactoryGirl.create(:male, first_name: "Bobby",  last_name: last_name, dob: 8.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:cindy)  {FactoryGirl.create(:male, first_name: "Cindy",  last_name: last_name, dob: 6.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:mike)   {FactoryGirl.create(:person, first_name: "Mike",   last_name: last_name, dob: 40.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:carol)  {FactoryGirl.create(:person, first_name: "Carol",  last_name: last_name, dob: 35.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:greg)   {FactoryGirl.create(:person, first_name: "Greg",   last_name: last_name, dob: 17.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:marcia) {FactoryGirl.create(:person, first_name: "Marcia", last_name: last_name, dob: 16.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:peter)  {FactoryGirl.create(:person, first_name: "Peter",  last_name: last_name, dob: 14.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:jan)    {FactoryGirl.create(:person, first_name: "Jan",    last_name: last_name, dob: 12.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:bobby)  {FactoryGirl.create(:person, first_name: "Bobby",  last_name: last_name, dob: 8.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
+    let(:cindy)  {FactoryGirl.create(:person, first_name: "Cindy",  last_name: last_name, dob: 6.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
     let(:bradys) {[mike, carol, greg, marcia, peter, jan, bobby, cindy]}
     let!(:mikes_family) do
       family = FactoryGirl.create(:family)
