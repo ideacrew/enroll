6b42bfb9543898c95ae9bcf4052254db44aa9b60
Sean Carley <milythael.mourning@gmail.com>
2015-03-19 15:43:45 -0500

    use static DC area code for default in factory

--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :phone do
     kind 'home'
-    sequence(:area_code, WrappingSequence.new(100, 999)) { |n| "#{n}"}
+    # sequence(:area_code, WrappingSequence.new(100, 999)) { |n| "#{n}"}
+    area_code 202
     sequence(:number, 1111111) { |n| "#{n}"}
     sequence(:extension) { |n| "#{n}"}
 
