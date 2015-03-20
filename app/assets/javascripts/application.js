37d05b70924f85aac563c9dd83d05cb70cbb38cc
Brian Russel Davis <brian@nurdymuny.com>
2015-03-20 17:52:17 +0530

    Changed SS# input limit to 9 digits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -363,13 +363,13 @@ $(document).ready(function () {
   });
 
   $("#dependent_ul .floatlabel").focusin(function() {
-  	$('.house').css("opacity","0.5");
-  	$(this).closest('.house').addClass('employee-info');
+    $('.house').css("opacity","0.5");
+    $(this).closest('.house').addClass('employee-info');
     $("a.dependent_ul").css("color","#98cbff");
     $(this).closest('.house').css("opacity","1");
   });
   $("#dependent_ul .floatlabel").blur(function() {
-  	$(this).closest('.house').css("opacity","0.5");
+    $(this).closest('.house').css("opacity","0.5");
       // $("#dependent_ul div.first").css("opacity","0.5");
   });
   
@@ -420,15 +420,15 @@ $(document).ready(function () {
   // Email validation after 1 seconds of stopping typing
   var timeout;
   $('#email_info input').keyup(function() {
-  	var email = $(this).val();
+    var email = $(this).val();
     if(timeout) {
         clearTimeout(timeout);
         timeout = null;
     }
 
     timeout = setTimeout(function() {
-    	check_email(email);
-    	}, 1000);
+      check_email(email);
+      }, 1000);
   });
   
   function check_email(email) { 
@@ -444,7 +444,7 @@ $(document).ready(function () {
   
   // Add new address
   $('.btn-new-address').click(function(e){
-  	e.preventDefault();
+    e.preventDefault();
     $(".new-address-flow").css("display", "block");
   });
   
@@ -478,22 +478,22 @@ $(document).ready(function () {
       var current_element, message;
 
       $('.close-2').on('click', function() {
-      	current_element = $(this).closest("div.house");
-      	message = 'Remove ' + current_element.find('#employer_census_dependent_first_name').val() + ' ' + current_element.find('#employer_census_dependent_middle_name').val() + ' ' + current_element.find('#employer_census_dependent_last_name').val();
-      	
-      	$('.house').css("opacity","0.5");
-      	$(this).closest("div.house").css('border', '1px solid red');      	
-      	$(this).closest('div.house').css("opacity","1.0");
-      	$(this).closest("div.house").find("#remove_confirm")
-      		.html('<div>' + message + '?</div><a href="javascript:void(0);" class="btn remove_dependent cancel">' + (link.data('cancel')) + '</a> <a class="btn remove_dependent confirm" href="javascript:void(0);">' + (link.data('ok')) + '</a>')
-      		.removeClass('hidden'); 
+        current_element = $(this).closest("div.house");
+        message = 'Remove ' + current_element.find('#employer_census_dependent_first_name').val() + ' ' + current_element.find('#employer_census_dependent_middle_name').val() + ' ' + current_element.find('#employer_census_dependent_last_name').val();
+        
+        $('.house').css("opacity","0.5");
+        $(this).closest("div.house").css('border', '1px solid red');        
+        $(this).closest('div.house').css("opacity","1.0");
+        $(this).closest("div.house").find("#remove_confirm")
+          .html('<div>' + message + '?</div><a href="javascript:void(0);" class="btn remove_dependent cancel">' + (link.data('cancel')) + '</a> <a class="btn remove_dependent confirm" href="javascript:void(0);">' + (link.data('ok')) + '</a>')
+          .removeClass('hidden'); 
       });
       
       $('.remove_dependent').on('click', function() {
-      	$(this).closest("div.house").css('border-color', '#007bc3');
-      	$(this).closest("#remove_confirm")
-      		.addClass('hidden')
-      		.html('');
+        $(this).closest("div.house").css('border-color', '#007bc3');
+        $(this).closest("#remove_confirm")
+          .addClass('hidden')
+          .html('');
       });
       
       return $('#remove_confirm .confirm').on('click', function() {
@@ -505,6 +505,6 @@ $(document).ready(function () {
   // Input Masks
   $(".phone_number").mask("(999) 999-9999");
   $(".zip").mask("99999");
-  $("#person_ssn").mask("99999999");
+  $("#person_ssn").mask("999999999");
   
 });
