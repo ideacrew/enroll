dbd22db35731d1f231193e4404176384efa4e185
Dan Thomas <dan.thomas@dc.gov>
2015-03-21 09:45:15 -0400

    Remove gender as required attribute

--- spec/models/employer_census/member_spec.rb
+++ spec/models/employer_census/member_spec.rb
@@ -4,6 +4,4 @@ RSpec.describe EmployerCensus::Member, type: :model do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :dob }
-  it { should validate_presence_of :gender }
-
 end
