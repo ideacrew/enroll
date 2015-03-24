a9802d365499486633240efe6903c09b8725abe9
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-03-24 15:43:26 -0400

    specs for hbx_enrollment creations

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -11,7 +11,6 @@ describe Family do
   family_member.person=p1;
   family_member }
 
-=begin
   describe "instantiates object." do
     it "sets and gets all basic model fields" do
       now = DateTime.now.utc
@@ -57,16 +56,16 @@ describe Family do
   end
 
   describe "one family exists" do
-    let!(:primary_person) {FactoryGirl.create(:person)}
-    let!(:dependent_person) {FactoryGirl.create(:person)}
-    let!(:first_family) {FactoryGirl.create(:family)}
-    let!(:first_primary_member) {FactoryGirl.create(:family_member, :primary, family: first_family, person: primary_person)}
-    let!(:first_dependent_member) {FactoryGirl.create(:family_member, family: first_family, person: dependent_person)}
+    let!(:primary_person) { FactoryGirl.create(:person) }
+    let!(:dependent_person) { FactoryGirl.create(:person) }
+    let!(:first_family) { FactoryGirl.create(:family) }
+    let!(:first_primary_member) { FactoryGirl.create(:family_member, :primary, family: first_family, person: primary_person) }
+    let!(:first_dependent_member) { FactoryGirl.create(:family_member, family: first_family, person: dependent_person) }
 
     context "and a second family is built" do
-      let!(:second_family) {FactoryGirl.build(:family)}
-      let!(:second_primary_member) {FactoryGirl.build(:family_member, :primary, family: second_family, person: primary_person)}
-      let!(:second_dependent_member) {FactoryGirl.build(:family_member, family: second_family, person: dependent_person)}
+      let!(:second_family) { FactoryGirl.build(:family) }
+      let!(:second_primary_member) { FactoryGirl.build(:family_member, :primary, family: second_family, person: primary_person) }
+      let!(:second_dependent_member) { FactoryGirl.build(:family_member, family: second_family, person: dependent_person) }
 
       it "second family should be valid" do
         expect(second_family.valid?).to be
@@ -77,26 +76,26 @@ describe Family do
   describe "large family with multiple employees - The Brady Bunch" do
     let(:brady_addr) do
       FactoryGirl.build(:address,
-        kind: "home",
-        address_1:
-        "4222 Clinton Way",
-        address_2: nil,
-        city: "Washington",
-        state: "DC",
-        zip: "20011"
+                        kind: "home",
+                        address_1:
+                            "4222 Clinton Way",
+                        address_2: nil,
+                        city: "Washington",
+                        state: "DC",
+                        zip: "20011"
       )
     end
-    let(:brady_ph) {FactoryGirl.build(:phone, kind: "home", area_code: "202", number: "7620799", extension: nil)}
-    let(:last_name) {"Brady"}
-    let(:mike)   {FactoryGirl.create(:male,   first_name: "Mike",   last_name: last_name, dob: 40.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:carol)  {FactoryGirl.create(:female, first_name: "Carol",  last_name: last_name, dob: 35.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:greg)   {FactoryGirl.create(:male,   first_name: "Greg",   last_name: last_name, dob: 17.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:marcia) {FactoryGirl.create(:female, first_name: "Marcia", last_name: last_name, dob: 16.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:peter)  {FactoryGirl.create(:male,   first_name: "Peter",  last_name: last_name, dob: 14.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:jan)    {FactoryGirl.create(:female, first_name: "Jan",    last_name: last_name, dob: 12.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:bobby)  {FactoryGirl.create(:male,   first_name: "Bobby",  last_name: last_name, dob: 8.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:cindy)  {FactoryGirl.create(:female, first_name: "Cindy",  last_name: last_name, dob: 6.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:bradys) {[mike, carol, greg, marcia, peter, jan, bobby, cindy]}
+    let(:brady_ph) { FactoryGirl.build(:phone, kind: "home", area_code: "202", number: "7620799", extension: nil) }
+    let(:last_name) { "Brady" }
+    let(:mike) { FactoryGirl.create(:male, first_name: "Mike", last_name: last_name, dob: 40.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:carol) { FactoryGirl.create(:female, first_name: "Carol", last_name: last_name, dob: 35.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:greg) { FactoryGirl.create(:male, first_name: "Greg", last_name: last_name, dob: 17.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:marcia) { FactoryGirl.create(:female, first_name: "Marcia", last_name: last_name, dob: 16.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:peter) { FactoryGirl.create(:male, first_name: "Peter", last_name: last_name, dob: 14.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:jan) { FactoryGirl.create(:female, first_name: "Jan", last_name: last_name, dob: 12.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:bobby) { FactoryGirl.create(:male, first_name: "Bobby", last_name: last_name, dob: 8.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:cindy) { FactoryGirl.create(:female, first_name: "Cindy", last_name: last_name, dob: 6.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup]) }
+    let(:bradys) { [mike, carol, greg, marcia, peter, jan, bobby, cindy] }
     let!(:mikes_family) do
       family = FactoryGirl.create(:family)
       family.family_members << FactoryGirl.build(:family_member, :primary, family: family, person: mike)
@@ -118,14 +117,14 @@ describe Family do
 
     context "Family.find_by_primary_applicant" do
       context "on Mike" do
-        let(:find) {Family.find_by_primary_applicant(mike)}
+        let(:find) { Family.find_by_primary_applicant(mike) }
         it "should find Mike's family" do
           expect(find.id.to_s).to eq mikes_family.id.to_s
         end
       end
 
       context "on Carol" do
-        let(:find) {Family.find_by_primary_applicant(carol)}
+        let(:find) { Family.find_by_primary_applicant(carol) }
         it "should find Carol's family" do
           expect(find.id.to_s).to eq carols_family.id.to_s
         end
@@ -134,7 +133,7 @@ describe Family do
 
     context "Family.find_by_person" do
       context "on Mike" do
-        let(:find) {Family.find_all_by_person(mike).collect(&:id)}
+        let(:find) { Family.find_all_by_person(mike).collect(&:id) }
         it "should find two families" do
           expect(find.count).to be 2
         end
@@ -147,7 +146,7 @@ describe Family do
       end
 
       context "on Carol" do
-        let(:find) {Family.find_all_by_person(carol).collect(&:id)}
+        let(:find) { Family.find_all_by_person(carol).collect(&:id) }
         it "should find two families" do
           expect(find.count).to be 2
         end
@@ -160,7 +159,7 @@ describe Family do
       end
 
       context "on Greg" do
-        let(:find) {Family.find_all_by_person(greg).collect(&:id)}
+        let(:find) { Family.find_all_by_person(greg).collect(&:id) }
         it "should find two families" do
           expect(find.count).to be 2
         end
@@ -173,15 +172,15 @@ describe Family do
       end
     end
   end
-=end
+
   describe "update_household callback" do
-    let!(:primary_person) { FactoryGirl.create(:person) }
-    let!(:second_person) {FactoryGirl.create(:person)}
+    let!(:primary_person) { FactoryGirl.create(:person, :male) }
+    let!(:second_person) { FactoryGirl.create(:person) }
 
     context "family is saved" do
       let!(:new_family) { FactoryGirl.create(:family) }
       let!(:first_primary_member) { FactoryGirl.create(:family_member, :primary, family: new_family, person: primary_person) }
-=begin
+
       it "should create a household" do
         new_family.valid?
         expect(new_family.save).to be_truthy
@@ -199,15 +198,26 @@ describe Family do
         expect(new_family.save).to be_truthy
         expect(new_family.active_household.coverage_households.first.coverage_household_members.length).to eq(1)
       end
-=end
-      describe " " do
+
+      context "family has multiple family members" do
         let!(:second_member) { FactoryGirl.create(:family_member, family: new_family, person: second_person) }
         it "should create coverage_household with 2 members (self and spouse)" do
-          new_family.family_members.first.person.person_relationships << PersonRelationship.new({kind:'spouse', relative_id: second_person.id})
+          new_family.family_members.first.person.person_relationships << PersonRelationship.new({kind: 'spouse', relative_id: second_person.id})
           new_family.valid?
           expect(new_family.save).to be_truthy
           expect(new_family.active_household.coverage_households.first.coverage_household_members.length).to eq(2)
         end
+      end
+
+      it "should create a hbx_enrollment" do
+        directory_premium = FactoryGirl.create(:directory_premium)
+        plan = FactoryGirl.create(:plan)
+        enrollee = Enrollee.new({person: primary_person, coverage_start_on: Date.today})
+        policy = FactoryGirl.create(:policy, plan: plan, enrollees: [enrollee])
+
+        new_family.valid?
+        expect(new_family.save).to be_truthy
+        expect(new_family.active_household.hbx_enrollments.length).to eq(1)
       end
     end
   end
