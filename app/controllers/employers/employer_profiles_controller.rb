175adc36620ff3d2c09e80108ee4f2ed6f9bba2e
Dan Thomas <dan.thomas@dc.gov>
2015-03-31 22:19:16 -0400

    Add broker_agency and employer_profile routes Add employer_file index and show page view content

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -4,4 +4,15 @@ class Employers::EmployerProfilesController < ApplicationController
     @employer_profiles = EmployerProfile.all.to_a
   end
 
+  # My Account page
+  def show
+    @employer_profile = EmployerProfile.find(params[:id])
+    @current_plan_year = @employer_profile.plan_years.last
+    @benefit_groups = @current_plan_year.benefit_groups
+  end
+
+  def new
+    @employer_profile = EmployerProfile.new
+  end
+
 end
