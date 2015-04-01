4308ac35d75a6111dc276a28150da4760641d80d
Sean Carley <milythael.mourning@gmail.com>
2015-04-01 03:49:29 -0500

    move Brady Bunch into shared context

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -74,47 +74,8 @@ describe Family do
   end
 
   describe "large family with multiple employees - The Brady Bunch" do
-    let(:brady_addr) do
-      FactoryGirl.build(:address,
-        kind: "home",
-        address_1:
-        "4222 Clinton Way",
-        address_2: nil,
-        city: "Washington",
-        state: "DC",
-        zip: "20011"
-      )
-    end
-    let(:brady_ph) {FactoryGirl.build(:phone, kind: "home", area_code: "202", number: "7620799", extension: nil)}
-    let(:last_name) {"Brady"}
-    let(:mike)   {FactoryGirl.create(:male,   first_name: "Mike",   last_name: last_name, dob: 40.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:carol)  {FactoryGirl.create(:female, first_name: "Carol",  last_name: last_name, dob: 35.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:greg)   {FactoryGirl.create(:male,   first_name: "Greg",   last_name: last_name, dob: 17.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:marcia) {FactoryGirl.create(:female, first_name: "Marcia", last_name: last_name, dob: 16.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:peter)  {FactoryGirl.create(:male,   first_name: "Peter",  last_name: last_name, dob: 14.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:jan)    {FactoryGirl.create(:female, first_name: "Jan",    last_name: last_name, dob: 12.years.ago, addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:bobby)  {FactoryGirl.create(:male,   first_name: "Bobby",  last_name: last_name, dob: 8.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:cindy)  {FactoryGirl.create(:female, first_name: "Cindy",  last_name: last_name, dob: 6.years.ago,  addresses: [brady_addr.dup], phones: [brady_ph.dup])}
-    let(:bradys) {[mike, carol, greg, marcia, peter, jan, bobby, cindy]}
-    let!(:mikes_family) do
-      family = FactoryGirl.create(:family)
-      family.family_members << FactoryGirl.build(:family_member, :primary, family: family, person: mike)
-      (bradys - [mike]).each do |brady|
-        family.family_members << FactoryGirl.build(:family_member, family: family, person: brady)
-      end
-      family.save
-      family
-    end
-    let!(:carols_family) do
-      family = FactoryGirl.create(:family)
-      family.family_members << FactoryGirl.build(:family_member, :primary, family: family, person: carol)
-      (bradys - [carol]).each do |brady|
-        family.family_members << FactoryGirl.build(:family_member, family: family, person: brady)
-      end
-      family.save
-      family
-    end
-
+    include_context "BradyBunch"
+    
     context "Family.find_by_primary_applicant" do
       context "on Mike" do
         let(:find) {Family.find_by_primary_applicant(mike)}
