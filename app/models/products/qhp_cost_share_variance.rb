3a303928271583e3e4c14e69937f70bf3678b48f
vinner <vineelsonnathi@users.noreply.github.com>
2015-04-02 18:05:08 -0400

    Plan Comparison table, change datatypes in qhp models as necessary. remove dashes in tin while importing.

--- app/models/products/qhp_cost_share_variance.rb
+++ app/models/products/qhp_cost_share_variance.rb
@@ -10,29 +10,29 @@ class Products::QhpCostShareVariance
   field :metal_level, type: String
   field :csr_variation_type, type: String
 
-  field :issuer_actuarial_value, type: Float
-  field :av_calculator_output_number, type: Float
+  field :issuer_actuarial_value, type: String
+  field :av_calculator_output_number, type: String
 
-  field :medical_and_drug_deductibles_integrated, type: Boolean
-  field :medical_and_drug_max_out_of_pocket_integrated, type: Boolean
-  field :multiple_provider_tiers, type: Boolean
-  field :first_tier_utilization, type: Float
-  field :second_tier_utilization, type: Float
+  field :medical_and_drug_deductibles_integrated, type: String
+  field :medical_and_drug_max_out_of_pocket_integrated, type: String
+  field :multiple_provider_tiers, type: String
+  field :first_tier_utilization, type: String
+  field :second_tier_utilization, type: String
 
-  field :default_copay_in_network, type: Money
-  field :default_copay_out_of_network, type: Money
-  field :default_co_insurance_in_network, type: Money
-  field :default_co_insurance_out_of_network, type: Money
+  field :default_copay_in_network, type: String
+  field :default_copay_out_of_network, type: String
+  field :default_co_insurance_in_network, type: String
+  field :default_co_insurance_out_of_network, type: String
 
   ## SBC Scenario
-  field :having_baby_deductible, type: Money
-  field :having_baby_co_payment, type: Money
-  field :having_baby_co_insurance, type: Money
-  field :having_baby_limit, type: Money
-  field :having_diabetes_deductible, type: Money
-  field :having_diabetes_copay, type: Money
-  field :having_diabetes_co_insurance, type: Money
-  field :having_diabetes_limit, type: Money
+  field :having_baby_deductible, type: String
+  field :having_baby_co_payment, type: String
+  field :having_baby_co_insurance, type: String
+  field :having_baby_limit, type: String
+  field :having_diabetes_deductible, type: String
+  field :having_diabetes_copay, type: String
+  field :having_diabetes_co_insurance, type: String
+  field :having_diabetes_limit, type: String
 
   embeds_one :qhp_deductable,
     class_name: "Products::QhpDeductable",
--- app/models/products/qhp_deductable.rb
+++ app/models/products/qhp_deductable.rb
@@ -6,20 +6,20 @@ class Products::QhpDeductable
 
   # Plan deductable list
   field :deductible_type, type: String   
-  field :in_network_tier_1_individual, type: Money
-  field :in_network_tier_1_family, type: Money
-  field :coinsurance_in_network_tier_1, type: Money
+  field :in_network_tier_1_individual, type: String
+  field :in_network_tier_1_family, type: String
+  field :coinsurance_in_network_tier_1, type: String
 
-  field :in_network_tier_two_individual, type: Money
-  field :in_network_tier_two_family, type: Money
-  field :coinsurance_in_network_tier_2, type: Money
+  field :in_network_tier_two_individual, type: String
+  field :in_network_tier_two_family, type: String
+  field :coinsurance_in_network_tier_2, type: String
 
-  field :out_of_network_individual, type: Money
-  field :out_of_network_family, type: Money
-  field :coinsurance_out_of_network, type: Money
+  field :out_of_network_individual, type: String
+  field :out_of_network_family, type: String
+  field :coinsurance_out_of_network, type: String
 
-  field :combined_in_or_out_network_individual, type: Money
-  field :combined_in_or_out_network_family, type: Money
-  field :combined_in_out_network_tier_2, type: Money
+  field :combined_in_or_out_network_individual, type: String
+  field :combined_in_or_out_network_family, type: String
+  field :combined_in_out_network_tier_2, type: String
 
 end
