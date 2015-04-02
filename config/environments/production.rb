d0eddc74034987dc8e23e0cc51df95d17fc869b7
Trey Evans <lewis.r.evans@gmail.com>
2015-04-02 12:33:56 -0400

    Fixing gem list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
 #  config.active_record.dump_schema_after_migration = false
+  config.acapi.publish_amqp_events = true
 end
