945f321b2934ad6e6eec73c750ea7a556e614e0f
Ken Lu <ken@seeice.com>
2015-04-04 16:58:11 -0400

    added spec and sample format for detail plan

--- spec/models/shop_premium_matrix_spec.rb
+++ spec/models/shop_premium_matrix_spec.rb
@@ -32,12 +32,41 @@ RSpec.describe ShopPremiumMatrix, :type => :model do
     }
   end
 
+  let(:detail_plan) do
+    {"plan_b" => [
+      {"hbx_enrollment_member_id"=>1, 
+       "relationship"=>"employee",
+       "age_on_effective_date"=>50,
+       "employer_max_contribution"=>370.43,
+       "select_plan_id"=>"plan_b",
+       "plan_premium_total"=>550.0,
+       "employee_responsible_amount"=>179.57}, 
+      
+       {"hbx_enrollment_member_id"=>1,
+       "relationship"=>"spouse", 
+       "age_on_effective_date"=>22, 
+       "employer_max_contribution"=>215.73, 
+       "select_plan_id"=>"plan_b", 
+       "plan_premium_total"=>197.13,
+       "employee_responsible_amount"=>0.0}]
+    }
+  end
+
   before do 
     [premium_matrix, premium_matrix2].each{|pm| ShopPremiumMatrix.new(pm)}
     ShopPremiumMatrix.cache_sum_family(1, 'plan_b')
+    ShopPremiumMatrix.cache_family_detail(1, 'plan_b')
+  end
+
+  after do
+    class ShopPremiumMatrix; @@premium_table = nil; end
   end
 
   it 'able to cache and fetch family total cost' do
     expect(ShopPremiumMatrix.fetch_cost(1, ['plan_b'], 'family-total')).to eq sum_plan
+  end
+
+  it 'able to cache and fetch family total cost' do
+    expect(ShopPremiumMatrix.fetch_cost(1, ['plan_b'], 'family-detail')).to eq detail_plan
   end
 end
