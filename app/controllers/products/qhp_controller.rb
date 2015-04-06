771212af987e6911974157f904b4f387d9c1fc25
Trey Evans <lewis.r.evans@gmail.com>
2015-04-06 04:59:54 -0400

    Fixing display and speed issues.

--- app/controllers/products/qhp_controller.rb
+++ app/controllers/products/qhp_controller.rb
@@ -3,7 +3,7 @@ class Products::QhpController < ApplicationController
   def comparison
     params.permit("standard_component_ids")
     found_params = params["standard_component_ids"].map { |str| str[0..13] }
-    @qhps = Products::Qhp.where(:standard_component_id.in => found_params)
+    @qhps = Products::Qhp.where(:standard_component_id.in => found_params).to_a
     respond_to do |format|
       format.html
       format.js
