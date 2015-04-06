972fe02c94eae38efb7e7710cc339aa2d0b4ed1b
Trey Evans <lewis.r.evans@gmail.com>
2015-04-05 23:04:42 -0400

    Now have plan details modal.

--- app/controllers/products/qhp_controller.rb
+++ app/controllers/products/qhp_controller.rb
@@ -6,7 +6,11 @@ class Products::QhpController < ApplicationController
   end
 
   def summary
-    sc_id = params[:standard_component_id][0..13]
+    sc_id = params.permit(:standard_component_id)[:standard_component_id][0..13]
     @qhp = Products::Qhp.where(standard_component_id: sc_id).to_a.first
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
