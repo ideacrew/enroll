488ab25c9a207f6b90b61b114efa0afcf5c7df8a
Sean Carley <milythael.mourning@gmail.com>
2015-04-12 10:37:17 -0500

    add a little more structure to specs for example purposes

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,14 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
-
   describe "GET index" do
-    it "returns http success" do
-      sign_in nil
+    shared_examples "welcome" do
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
 
-      get :index
-      expect(response).to have_http_status(:success)
+      it "renders welcome index" do
+        expect(response).to render_template("index")
+      end
+    end
+
+    context "when not signed in" do
+      before do
+        sign_in nil
+        get :index
+      end
+
+      include_examples "welcome"
+    end
+
+    context "when signed in" do
+      before do
+        sign_in
+        get :index
+      end
+
+      include_examples "welcome"
     end
   end
-
 end
