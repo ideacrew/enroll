e20b7bf5d033ee9349c615060cc0f70c5c343fb5
Brian Russel Davis <brian@nurdymuny.com>
2015-04-14 00:55:58 +0545

    Fixed remove family double click issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -570,7 +570,20 @@ $(document).ready(function () {
   // });
   
   // Customize Dependent Family Member Delete Confirmation
-  $(function() {
+  $('#family .remove').click(function() {
+    var current_element = $(this).closest("#family .family_members_list");
+    var confirm_buttons = 'single_row';
+    override_confirmation( $(this), current_element, confirm_buttons, '#family .family_members_list' );    
+  });
+
+  $('#dependent_ul .close-2').click(function() {
+    var current_element = $(this).closest("div.house");
+    var confirm_buttons = 'double_row';
+    override_confirmation( $(this), current_element, confirm_buttons, 'div.house' );
+  });
+
+  // Overrides javascript default confirm modal
+  function override_confirmation(thisObj, element, confirm_buttons, closest_element) {
     $.rails.allowAction = function(link) {
       if (!link.attr('data-confirm')) {
         return true;
@@ -578,49 +591,36 @@ $(document).ready(function () {
       $.rails.showConfirmDialog(link);
       return false;
     };
+
     $.rails.confirmed = function(link) {
       link.removeAttr('data-confirm');
       return link.trigger('click.rails');
     };
+
     return $.rails.showConfirmDialog = function(link) {
-      var current_element, message;
+      var html;
+      if(confirm_buttons == 'single_row') {
+        html = '<div>' + link.data('confirm') + '<a href="javascript:;" class="btn remove_dependent cancel">' + (link.data('cancel')) + '</a> <a class="btn remove_dependent confirm" href="javascript:void(0);">' + (link.data('ok')) + '</a></div>';
+      } else {
+        html = '<div>' + link.data('confirm') + '</div><a href="javascript:void(0);" class="btn remove_dependent cancel">' + (link.data('cancel')) + '</a> <a class="btn remove_dependent confirm" href="javascript:void(0);">' + (link.data('ok')) + '</a>';
+      }
+  
+      thisObj.closest(closest_element).find("#remove_confirm")
+        .html(html)
+        .removeClass('hidden');
 
-      $('.close-2').on('click', function() {
-        current_element = $(this).closest("div.house");
-        message = $(this).attr('data-confirm');
-        if(message.trim() == 'Remove') {
-          $('#cancel_member').click();
-        } else {
-          // $('.house').css("opacity","0.5");
-          $(this).closest("div.house").css('border', '1px solid red');        
-          $(this).closest('div.house').css("opacity","1.0");
-          $(this).closest("div.house").find("#remove_confirm")
-            .html('<div>' + message + '?</div><a href="javascript:void(0);" class="btn remove_dependent cancel">' + (link.data('cancel')) + '</a> <a class="btn remove_dependent confirm" href="javascript:void(0);">' + (link.data('ok')) + '</a>')
-            .removeClass('hidden');
-        }
-      });
-
-      $('#family .remove').click(function() {
-        current_element = $(this).closest("#family .family_members_list");
-        message = 'Remove ' + current_element.find('#family_member_first_name').val() + ' ' + current_element.find('#family_member_middle_name').val() + ' ' + current_element.find('#family_member_last_name').val();
-
-        $(this).closest("#family .family_members_list").find("#remove_confirm")
-          .html(message + '? <a href="javascript:void(0);" class="btn remove_dependent cancel">' + (link.data('cancel')) + '</a> <a class="btn remove_dependent confirm" href="javascript:void(0);">' + (link.data('ok')) + '</a>')
-          .removeClass('hidden');
-      });
-      
       $('.remove_dependent').on('click', function() {
         $(this).closest("div.house").css('border-color', '#007bc3');
         $(this).closest("#remove_confirm")
           .addClass('hidden')
           .html('');
       });
-      
+
       return $('#remove_confirm .confirm').on('click', function() {
         return $.rails.confirmed(link);
       });
     };
-  });
+  }
 
   // Change Dropdown Address Text
   $('.address-li').on('click', function(){
@@ -629,11 +629,6 @@ $(document).ready(function () {
   });
   
   // Select Plan Page
-  // $('#select-plan-container .personal_info').hide();
-  // $('#select-plan-container #coverage-back').show();
-  // $('#select-plan-container .coverage-options').removeClass('hidden');
-  // $('#select-plan-container .coverage-options .arrow-right').show();
-  
   $('#select-plan-btn1').click(function() {
     $(".select-plan p.detail").hide();
     $(this).hide();
