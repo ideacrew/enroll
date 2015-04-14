1b0adbd63d8ee0ab01a2d0faebf9f74dad9fc191
Ken Lu <ken@seeice.com>
2015-04-14 10:34:52 -0400

    updated spec for creating employer profile

--- spec/controllers/employers/employer_profiles_controller_spec.rb
+++ spec/controllers/employers/employer_profiles_controller_spec.rb
@@ -8,15 +8,26 @@ RSpec.describe Employers::EmployerProfilesController, :type => :controller do
     let(:employer_profile) { mikes_employer }
     let(:plan_year) { mikes_plan_year }
     let(:benefit_group) { mikes_benefit_group }
+    let(:relationship_benefit) { FactoryGirl.build(:relationship_benefit) }
+    let(:office_location) { mikes_office_location }
 
     it "create successful" do
-      benefit_group_params = benefit_group.attributes.to_hash.delete_if {|k, v| k=="_id"}
-      plan_year_params = plan_year.attributes.to_hash.delete_if {|k, v| k=="_id"}
+      relationship_benefit_params = relationship_benefit.attributes.to_hash
+      benefit_group_params = benefit_group.attributes.to_hash
+      benefit_group_params[:relationship_benefits_attributes] = {"0" => relationship_benefit_params}
+      plan_year_params = plan_year.attributes.to_hash
       plan_year_params[:benefit_groups_attributes] = {"0" => benefit_group_params}
-      employer_profile_params = employer_profile.attributes.to_hash.delete_if {|k, v| k=="_id"}
+      employer_profile_params = employer_profile.attributes.to_hash
       employer_profile_params[:plan_years_attributes] = {"0" => plan_year_params}
-      organization_params = organization.attributes.to_hash.delete_if {|k, v| k=="_id"}
+      employer_profile_params[:fein] = "123321456"
+      employer_profile_params[:dba] = "dba"
+      employer_profile_params[:legal_name] = "legal name"
+      organization_params = organization.attributes.to_hash
       organization_params[:employer_profile_attributes] = employer_profile_params
+      office_location_params = office_location.attributes.to_hash
+      office_location_params[:phone_attributes] = mikes_work_phone.attributes.to_hash
+      office_location_params[:address_attributes] = mikes_work_addr.attributes.to_hash
+      organization_params[:office_locations_attributes] = {"0" => office_location_params}
 
       organization.destroy
       org_count = Organization.count
