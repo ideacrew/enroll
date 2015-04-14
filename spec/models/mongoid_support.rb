9cc6a17128bce89b38ccdf8a769623c3f72a248c
Trey Evans <lewis.r.evans@gmail.com>
2015-04-14 14:17:35 -0400

    Spec was missing a case.

--- spec/models/mongoid_support.rb
+++ spec/models/mongoid_support.rb
@@ -12,11 +12,13 @@ describe MongoidSupport do
     end
 
     it "nilling the underlying attribute should nil the association" do
+      subject.person = person
       subject.person_id = nil
       expect(subject.person).to eq nil
     end
 
     it "nilling the association should nil the attribute" do
+      subject.person = person
       subject.person = nil
       expect(subject.person_id).to eq nil
     end
