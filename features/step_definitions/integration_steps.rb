93329d2e6b3b933741f0170880307b78aff2547c
Trey Evans <lewis.r.evans@gmail.com>
2015-04-14 17:14:25 -0400

    Bare-bones test.

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -32,13 +32,28 @@ Then(/^I should be logged in$/) do
 end
 
 When(/^I go to register as an employee$/) do
+  @browser.goto("http://localhost:3000/people/new")
+  sleep(3)
 end
 
 Then(/^I should see the employee search page$/) do
+  expect(@browser.a(:text => "Continue").visible?).to be_truthy
 end
 
 When(/^I enter the identifying info of my existing person$/) do
+  @browser.a(:text => "Continue").click
+  sleep(1)
+  expect(@browser.text_field(:name => "person[first_name]").visible?).to be_truthy
+  @browser.text_field(:name => "person[first_name]").set("Jonathan")
+  @browser.text_field(:name => "person[last_name]").set("Hall")
+  @browser.text_field(:name => "person[date_of_birth]").set("10/12/1983")
+  @browser.p(:text=> /Personal Information/).click
+  @browser.text_field(:name => "person[ssn]").set("722991234")
+  sleep(2)
+  @browser.a(:text => "Search").click
+  sleep(3)
 end
 
 Then(/^I should see the matched employee record form$/) do
+  expect(@browser.div(:text => /Employer : Acme Inc\./).visible?).to be_truthy
 end
