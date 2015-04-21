0a836b420e621e866e5535720c934233910905e2
Kevin Wei <kevin.tw.wei@gmail.com>
2015-04-21 14:32:46 -0400

    moving some config options to match cucumber best practices

--- features/support/env.rb
+++ features/support/env.rb
@@ -55,7 +55,3 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
-# Enable factory_girl
-World(FactoryGirl::Syntax::Methods)
-
