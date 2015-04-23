dc4e7f1f2f8dd8484392a0dbcbe407023a5d5f0e
Dan Thomas <dan.thomas@dc.gov>
2015-04-22 21:47:34 -0400

    Remove unused code from GDB migration

--- app/models/coverage_household.rb
+++ app/models/coverage_household.rb
@@ -18,16 +18,8 @@ class CoverageHousehold
   embeds_many :coverage_household_members
   accepts_nested_attributes_for :coverage_household_members
 
+  validates_inclusion_of :is_immediate_family
   validate :presence_of_coverage_household_members
-
-# HACK: Disabling for now until we fix Person#policies
-#  validate :integrity_of_coverage_household_members
-
-  def presence_of_coverage_household_members
-    if self.coverage_household_members.size == 0
-      self.errors.add(:base, "Should have at least one coverage_household_member")
-    end
-  end
 
   def family
     return nil unless household
@@ -38,25 +30,12 @@ class CoverageHousehold
     coverage_household_members.map(&:applicant_id)
   end
 
-  def integrity_of_coverage_household_members
-
-    return unless family
-
-    return if family.primary_applicant.person.policies.length == 0 #if no policies
-
-    people_in_coverage_household = self.family_members.flat_map(&:person) - [nil]
-
-    enrollees = family.primary_applicant.person.policies.flat_map(&:enrollees).uniq
-
-    people_in_policies = enrollees.map do |enrollee|
-      Person.find_by_member_id(enrollee.m_id)
-    end
-
-    same_people = people_in_coverage_household.map(&:id).uniq.sort == people_in_policies.map(&:id).uniq.sort
-
-    unless same_people
-      self.errors.add(:base, "Applicants in coverage household are not the same as enrollees covered in policies")
+private
+  def presence_of_coverage_household_members
+    if self.coverage_household_members.size == 0
+      self.errors.add(:base, "Should have at least one coverage_household_member")
     end
   end
+
 
 end
