59a2161cefc90cd262b044c7744e2fe34a13ddf6
Trey Evans <lewis.r.evans@gmail.com>
2015-04-28 14:44:11 -0400

    Beginning to move employee dependent functionality.

--- app/controllers/consumer/employee_dependents_controller.rb
+++ app/controllers/consumer/employee_dependents_controller.rb
@@ -1,5 +1,11 @@
 class Consumer::EmployeeDependentsController < ApplicationController
   def index
     @family = current_user.primary_family
+    @person = current_user.person
+  end
+
+  def new
+    @person = current_user.person
+    @dependent = Forms::EmployeeDependent.new(:family_id => params.require(:family_id))
   end
 end
--- spec/controllers/consumers/employee_dependents_controller_spec.rb
+++ spec/controllers/consumers/employee_dependents_controller_spec.rb
@@ -3,8 +3,9 @@ require 'factories/enrollment_factory'
 
 RSpec.describe Consumer::EmployeeDependentsController do
   describe "GET index" do
-    let(:user) { instance_double("User", :primary_family => family) }
-    let(:family) { double("family") }
+    let(:user) { instance_double("User", :primary_family => family, :person => person) }
+    let(:family) { double }
+    let(:person) { double }
 
     before(:each) do
       sign_in(user)
@@ -16,8 +17,35 @@ RSpec.describe Consumer::EmployeeDependentsController do
       expect(response).to render_template("index")
     end
 
-    it "sets the family" do
+    it "assigns the person" do
+      expect(assigns(:person)).to eq person
+    end
+
+    it "assigns the family" do
       expect(assigns(:family)).to eq family
+    end
+  end
+
+  describe "GET new" do
+    let(:user) { instance_double("User", :person => person) }
+    let(:family) { double }
+    let(:person) { double }
+    let(:family_id) { "addbedddedtotallyafamiyid" }
+    let(:dependent) { double }
+
+    before(:each) do
+      sign_in(user)
+      allow(Forms::EmployeeDependent).to receive(:new).with({:family_id => family_id}).and_return(dependent)
+      get :new, :family_id => family_id
+    end
+
+    it "renders the 'new' template" do
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template("new")
+    end
+
+    it "assigns the family" do
+      expect(assigns(:dependent)).to eq dependent
     end
   end
 end
