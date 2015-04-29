255d761d483f2e020fc21a218c76359a868c0e01
Sean Carley <milythael.mourning@gmail.com>
2015-04-29 13:04:44 +0200

    don't construct every user with same email address

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email 'example@example.com'
+    sequence(:email) {|n| "example\##{n}@example.com"}
     password '12345678'
     password_confirmation '12345678'
     approved true
