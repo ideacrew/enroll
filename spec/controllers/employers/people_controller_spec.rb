8428ff7b9f37a9e75516610e7e6164ec52fc3b78
Ken Lu <ken@seeice.com>
2015-05-07 09:26:21 -0400

    adding spec for employer workflow

---
+++ spec/controllers/employers/people_controller_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe Employers::PeopleController do
+  describe "GET search" do
+    it "renders the 'search' template" do
+      sign_in
+      get :search
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template("search")
+      expect(assigns(:person).class).to eq (Forms::EmployeeCandidate)
+    end
+  end
+
+  describe "POST match" do
+    let(:person_parameters) { { :first_name => "SOMDFINKETHING" } }
+    let(:found_person) { [] }
+    let(:mock_employee_candidate) { instance_double("Forms::EmployeeCandidate", :valid? => validation_result) }
+
+    before(:each) do
+      sign_in
+      allow(Forms::EmployeeCandidate).to receive(:new).with(person_parameters).and_return(mock_employee_candidate)
+      allow(mock_employee_candidate).to receive(:match_person).and_return(found_person)
+      post :match, :person => person_parameters
+    end
+
+    context "given invalid parameters" do
+      let(:validation_result) { false }
+
+      it "renders the 'search' template" do
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("search")
+        expect(assigns[:employee_candidate]).to eq mock_employee_candidate
+      end
+    end
+
+    context "given valid parameters render 'no_match' template" do
+      let(:validation_result) { true }
+
+      it "renders the 'no_match' template" do
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("no_match")
+        expect(assigns[:employee_candidate]).to eq mock_employee_candidate
+        expect(assigns(:person).class).to eq Person
+      end
+
+    end
+
+    context "given valid parameters render 'match' template" do
+      let(:validation_result) { true }
+      let(:found_person) { FactoryGirl.create(:person) }
+
+      it "renders the 'match' template" do
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("match")
+        expect(assigns[:employee_candidate]).to eq mock_employee_candidate
+      end
+    end
+  end
+
+  describe "POST update" do
+    let(:person_parameters) { { :first_name => "SOMDFINKETHING", :last_name => "SOME"} }
+    let(:person) { FactoryGirl.create(:person) }
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:each) do
+      sign_in(user)
+      allow(Person).to receive(:find).with(person.id).and_return(person)
+      allow(person).to receive(:update_attributes).and_return(save_result)
+      allow(controller).to receive(:sanitize_person_params).and_return(nil)
+      allow(controller).to receive(:make_new_person_params).with(person).and_return(nil)
+      put :update, :person => person_parameters, :id => person.id
+    end
+
+    context "given valid person parameters" do
+      let(:save_result) { true }
+
+      it "should render 'employer_form'" do
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("employer_form")
+      end
+    end
+  end
+end
