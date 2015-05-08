a60f413bb4f923d8e167b6e27fb9b32b4fc6c224
Trey Evans <lewis.r.evans@gmail.com>
2015-05-08 18:05:05 -0400

    Can now get back to profile page.

--- app/models/factories/enrollment_factory.rb
+++ app/models/factories/enrollment_factory.rb
@@ -191,7 +191,7 @@ module Factories
       family = person.primary_family
       family ||= Family.new
       applicant = family.primary_applicant
-      applicant = initialize_primary_applicant(family, person) if applicant.blank?
+      applicant ||= initialize_primary_applicant(family, person) 
       person.relatives.each do |related_person|
         family.add_family_member(related_person)
       end
@@ -228,7 +228,7 @@ module Factories
 
     def self.save_all_or_delete_new(*list)
       objects_to_save = list.reject {|o| !o.changed?}
-      num_saved = objects_to_save.count {|o| o.save}
+      num_saved = objects_to_save.count { |o| o.save }
       if num_saved < objects_to_save.count
         objects_to_save.each {|o| o.delete}
         false
