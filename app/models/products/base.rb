531928fa71fb4b14e4a5d431671f04e747b4d88b
Dan Thomas <dan.thomas@dc.gov>
2015-05-13 08:18:46 -0400

    initial commit

---
+++ app/models/products/base.rb
@@ -0,0 +1,10 @@
+class Products::Base
+  include Mongoid::Document
+
+  ## Attributes
+  # Subject to enrollment periods?
+  # Markets served
+
+  # Examples: qhp, standalone dental, disability insurance
+
+end
---
+++ spec/factories/products_bases.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :products_basis, :class => 'Products::Base' do
+    
+  end
+
+end
