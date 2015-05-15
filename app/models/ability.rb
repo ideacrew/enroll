db50090f13c003f9a92baa88cb4bf18900528a87
Dan Thomas <dan.thomas@dc.gov>
2015-05-15 00:04:33 -0400

    Fix role and profile name references

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,17 +8,17 @@ class Ability
 
     alias_action :update, :destroy, :to => :modify
 
-    if user.has_role? :employer
+    if user.has_role? :employer_staff
       can :read, :all
     elsif user.has_role? :employee
       can :read, :all
     elsif user.has_role? :broker
       can :read, :all
-    elsif user.has_role? :undocumented_consumer
+    elsif user.has_role? :consumer
       can :read, :all
-    elsif user.has_role? :qhp_consumer
+    elsif user.has_role? :resident
       can :read, :all
-    elsif user.has_role? :hbx_employee
+    elsif user.has_role? :hbx_staff
       can :read, :all
     elsif user.has_role? :system_service
       can :read, :all
