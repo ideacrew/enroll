935144fc3193e2aeca083f22be0dc9a0ccb1fa50
Brian Russel Davis <brian@nurdymuny.com>
2015-05-22 08:46:46 +0545

    Fixed font-awesome icons in employer pages

--- app/assets/javascripts/semantic_class.js
+++ app/assets/javascripts/semantic_class.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-	//This function adds semantic class to all input fields of type "text" eg. interaction-field-control-person-first-name
+	// Adds semantic class to all input fields eg. interaction-field-control-person-first-name
 	$('input[type="text"], input[type="email"], input[type="password"]').each(function() {
 		var element_id = $(this).attr('id');
-		$(this).addClass('interaction-field-control-' + element_id.replace(/_/gi, '-'));
+		if(element_id) {
+			$(this).addClass('interaction-field-control-' + element_id.replace(/_/gi, '-'));
+		}
 	});
+
+	// Adds semantic class to all a, submit
 });
