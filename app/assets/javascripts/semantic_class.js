011ede80993dac8b4090f59bb5246bb0b16b722f
Brian Russel Davis <brian@nurdymuny.com>
2015-05-22 19:29:50 +0545

    Add semantic class for radio & checkbox

--- app/assets/javascripts/semantic_class.js
+++ app/assets/javascripts/semantic_class.js
@@ -1,20 +1,32 @@
-// Adds semantic class to all input fields eg. interaction-field-control-person-first-name
 function semantic_class() {
-	$('input[type="text"], input[type="email"], input[type="password"], input[type="submit"]').each(function() {
-		var element_id = $(this).attr('id');
+	// Adds semantic class to all input fields eg. interaction-field-control-person-first-name
+	$('input[type="text"], input[type="email"], input[type="password"]').each(function() {
+		var element_id = $.trim($(this).attr('id'));
+
 		if(element_id) {
-			console.log($(this));
-			$(this).addClass('interaction-field-control-' + element_id.replace(/_/gi, '-'));
+			$(this).addClass('interaction-field-control-' + element_id.toLowerCase().replace(/_| /gi, '-'));
 		}
 	});
 
 	// Adds semantic class to all a, submit
-	$('a, button').each(function() {
-		var element_id = $.trim($(this).text());
+	$('a, button, input[type="submit"]').each(function() {
+
+		if($(this).is('input[type="submit"]')) {
+			var element_id = $.trim($(this).val());
+		} else {
+			var element_id = $.trim($(this).text());
+		}
 
 		if(element_id) {
-			// console.log($.trim(element_id));
 			$(this).addClass('interaction-click-control-' + element_id.toLowerCase().replace(/_| /gi, '-'));
+		}
+	});
+
+	$('input[type="radio"], input[type="checkbox"]').each(function() {
+		var element_id = $.trim($(this).attr('id'));
+
+		if(element_id) {
+			$(this).addClass('interaction-choice-control-value-' + element_id.toLowerCase().replace(/_| /gi, '-'));
 		}
 	});
 }
