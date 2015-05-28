d4956375cc4510fe145cb3d076fe0749fff614ed
Dan Thomas <dan.thomas@dc.gov>
2015-05-27 21:44:16 -0400

    Correct open_enrollment_start_on to pass new spec

--- spec/factories/plan_years.rb
+++ spec/factories/plan_years.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     employer_profile
     start_on { (Date.current - 60).beginning_of_month }
     end_on { start_on + 1.year - 1 }
-    open_enrollment_start_on { (start_on - 30).beginning_of_month }
+    open_enrollment_start_on { (start_on - 32).beginning_of_month }
     open_enrollment_end_on { open_enrollment_start_on + 2.weeks }
     fte_count { 5 }
   end
