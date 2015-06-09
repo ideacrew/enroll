69ca903d37b72070c44d6b8a12dd288876954cb7
Dan Thomas <dan.thomas@dc.gov>
2015-06-09 11:23:50 -0700

    Refactor to move financial activity/states into employer_profile_account

--- spec/factories/premium_payments.rb
+++ spec/factories/premium_payments.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
-  factory :premium_statement do
-    employer_profile  { FactoryGirl.build(:employer_profile) }
-    effective_on  Date.current
-    
+  factory :premium_payment do
+    paid_on  Date.current.beginning_of_month - 1.day
+    amount  7215.12
+    method_kind  "ach"
+    sequence(:reference_id)     { |n| "zzz777\##{n}" }
+    sequence(:document_uri)     { |n| "http://example.com/abc\##{n}" }
   end
 
 end
