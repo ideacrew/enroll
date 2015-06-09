2c0f43c0ca199118c2c82c9e0350b4c0339c9d8c
Dan Thomas <dan.thomas@dc.gov>
2015-06-09 10:40:24 -0700

    Specs passing

--- app/models/premium_payment.rb
+++ app/models/premium_payment.rb
@@ -1,128 +1,24 @@
-class PremiumStatement
+class PremiumPayment
   include Mongoid::Document
   include Mongoid::Timestamps
-  include AASM
 
-  embedded_in :employer_profile
+  embedded_in :employer_profile_account
+
+  MethodKinds = %w(ach credit_card check)
 
   # Payment status
-  field :effective_on, type: Date
-  field :last_premium_paid_on, type: Date
-  field :last_premium_amount, type: Money
-  field :next_premium_due_on, type: Date
-  field :next_premium_amount, type: Money
+  field :paid_on, type: Date
+  field :amount, type: Money
 
-  field :aasm_state, type: String
-  field :last_aasm_state, type: String
+  # Payment instrument
+  field :method_kind, type: String
 
-  validates_presence_of :effective_on
+  # Confirmation ID or similar 
+  field :reference_id, type: String
 
-  def notify_hbx
-  end
+  # Network reference to the payment document
+  field :document_uri, type: String
 
-  def persist_state
-    self.last_aasm_state = aasm.from_state
-  end
-
-  def revert_state
-    self.aasm_state = last_aasm_state unless last_aasm_state.blank?
-  end
-
-  def enroll_employer
-    employer_profile.enroll
-  end
-
-  def reinstate_employer
-    employer_profile.reinstate_coverage
-  end
-
-  def suspend_employer
-    employer_profile.suspend_coverage
-  end
-
-  def cancel_employer
-    employer_profile.cancel_coverage
-  end
-
-  def terminate_employer
-    employer_profile.terminate_coverage
-  end
-
-  ## TODO -- reconcile the need for history via embeds_many with the state machine functionality
-  aasm do
-    # Initial open enrollment period closed, first premium payment not received/processed
-    state :binder_pending, initial: true  
-    state :binder_paid, :after_enter => :enroll_employer
-
-    # Enrolled and premium payment up-to-date
-    state :current 
-
-    # Premium payment 1-29 days past due
-    state :overdue
-
-    # Premium payment 30-60 days past due - send notices to employees
-    state :late
-
-    # Premium payment 61-90 - transmit terms to carriers with retro date   
-    state :suspended,   :after_enter => :suspend_employer    
-
-    # Premium payment > 90 days past due (day 91) or Employer voluntarily terminates
-    state :terminated,  :after_enter => :terminate_employer  
-
-    # Coverage never took effect, either voluntarily withdrawal or not paying binder
-    state :canceled,    :after_enter => :cancel_employer
-
-    # Signal parent Employer Profile
-    event :allocate_binder_payment do
-      transitions from: :binder_pending, to: :binder_paid
-    end
-
-    # TODO Advance billing period on binder_pending in middle of month
-    event :advance_billing_period do
-      # transitions from: [:current, :overdue, :late, :suspended, :terminated], to: [:current, :overdue, :late, :suspended, :terminated]
-      transitions from: :binder_pending, to: :canceled
-      transitions from: :binder_paid, to: :overdue
-      transitions from: :current, to: :overdue
-      transitions from: :overdue, to: :late
-      transitions from: :late, to: :suspended
-      transitions from: :suspended, to: :terminated
-    end
-
-    # Premium payment credit received and allocated to account
-    event :advance_coverage_period do
-      transitions from: [:binder_paid, :current, :overdue, :late, :suspended], 
-                  to: :current, :after => [:persist_state, :reinstate_employer]
-    end
-
-    # Premium payment reversed and account debited
-    event :reverse_coverage_period, :after => :revert_state do
-      transitions from: :binder_paid, to: :canceled
-      transitions from: :current, to: :overdue
-      transitions from: :overdue, to: :late
-      transitions from: :late, to: :suspended
-    end
-
-    event :cancel_coverage do
-      transitions from: :binder_pending, to: :canceled
-    end
-
-    event :suspend_coverage do
-      transitions from: [:current, :late, :overdue], to: :suspended
-    end
-
-    event :reinstate_coverage do
-      transitions from: :suspended, to: :current
-    end
-
-    event :terminate_coverage do
-      transitions from: :suspended, to: :terminated
-    end
-
-    event :reapply do
-      transitions from: :terminated, to: :binder_pending
-    end
-
-  end
-
+  validates_presence_of :paid_on, :amount, :method_kind, :reference_id
 
 end
--- spec/models/premium_payment_spec.rb
+++ spec/models/premium_payment_spec.rb
@@ -1,207 +1,73 @@
 require 'rails_helper'
 
-describe PremiumStatement, dbclean: :after_each do
+describe PremiumPayment, type: :model do
 
-  let(:employer_profile)  { FactoryGirl.build(:employer_profile) }
+  let(:employer_profile_account)  { FactoryGirl.build(:employer_profile_account) }
+  let(:paid_on)                   { Date.current.beginning_of_month }
+  let(:amount)                    { 2345.07 }
+  let(:method_kind)               { "ach" }
+  let(:reference_id)              { "alphabetadelta" }
+  let(:document_uri)              { "uri:document_store:12345" }
+
   let(:valid_params) do
     {
-      effective_on: Date.current,
-      employer_profile: employer_profile,
-      last_premium_paid_on: Date.current - 25.days,
-      last_premium_amount: 2345.07,
-      next_premium_due_on: Date.current + 5.days,
-      next_premium_amount: 3155.86
+      employer_profile_account: employer_profile_account,
+      paid_on: paid_on,
+      amount: amount,
+      method_kind: method_kind,
+      reference_id: reference_id,
+      document_uri: document_uri
     }
   end
 
   context ".new" do
-    context "with only 'effective on' parameter" do
-      let(:params) {{effective_on: Date.current}}
+    context "with no employer profile account" do
+      let(:params) {valid_params.except(:employer_profile_account)}
 
       it "should raise" do
-        expect{PremiumStatement.create!(**params)}.to raise_error(Mongoid::Errors::NoParent)
+        expect{PremiumPayment.create!(**params)}.to raise_error(Mongoid::Errors::NoParent)
       end
     end
 
-    context "with no employer profile" do
-      let(:params) {valid_params.except(:employer_profile)}
+    context "with no paid on" do
+      let(:params) {valid_params.except(:paid_on)}
 
-      it "should raise" do
-        expect{PremiumStatement.create(**params)}.to raise_error(Mongoid::Errors::NoParent)
+      it "should fail validation" do
+        expect(PremiumPayment.create(**params).errors[:paid_on].any?).to be_truthy
       end
     end
 
-    context "with no effective on date" do
-      let(:params) {valid_params.except(:effective_on)}
+    context "with no amount" do
+      let(:params) {valid_params.except(:amount)}
 
-      it "should not save" do
-        expect(PremiumStatement.create(**params).errors[:effective_on].any?).to be_truthy
+      it "should fail validation" do
+        expect(PremiumPayment.create(**params).errors[:amount].any?).to be_truthy
       end
     end
 
-    context "with all parameters and open enrollment is closed" do
-      let(:premium_statement) {employer_profile.premium_statements.build(valid_params)}
+    context "with no method kind" do
+      let(:params) {valid_params.except(:method_kind)}
 
-      before do
-        employer_profile.aasm_state = "binder_pending"
+      it "should fail validation" do
+        expect(PremiumPayment.create(**params).errors[:method_kind].any?).to be_truthy
       end
+    end
 
-      it "should initialize with premium binder pending state" do
-        expect(premium_statement.binder_pending?).to be_truthy
+    context "with no reference_id" do
+      let(:params) {valid_params.except(:reference_id)}
+
+      it "should fail validation" do
+        expect(PremiumPayment.create(**params).errors[:reference_id].any?).to be_truthy
       end
+    end
 
-      it "should enable valid state transistions" do
-        expect(premium_statement.may_allocate_binder_payment?).to be_truthy
-        expect(premium_statement.may_cancel_coverage?).to be_truthy
-      end
+    context "with all valid parameters" do
+      let(:params) { valid_params }
 
-      it "should not enable invalid state transistions" do
-        expect(premium_statement.may_reverse_coverage_period?).to be_falsey
-        expect(premium_statement.may_suspend_coverage?).to be_falsey
-        expect(premium_statement.may_reinstate_coverage?).to be_falsey
-      end
-
-
-      it "should initialize with premium binder pending state" do
-        expect(premium_statement.binder_pending?).to be_truthy
-      end
-
-      it "should save" do
-        expect(premium_statement.save).to be_truthy
-      end
-
-      context "and it is saved" do
-        before do
-          premium_statement.save
-        end
-
-        it "should be findable" do
-          # expect(BrokerRole.find(broker_role.id).id.to_s).to eq broker_role.id.to_s
-        end
+      it "should pass validation" do
+        expect(PremiumPayment.new(**params).valid?).to be_truthy
       end
     end
   end
 
-  context "open enrollment has closed" do
-    let(:premium_statement) {employer_profile.premium_statements.build(valid_params)}
-
-    before do
-      employer_profile.aasm_state = "binder_pending"
-    end
-
-    context "for a new employer" do
-      it "should be waiting to receive binder payment" do
-        expect(premium_statement.binder_pending?).to be_truthy
-      end
-
-      context "doesn't pay the premium binder" do
-        before do
-          premium_statement.advance_billing_period
-        end
-
-        it "coverage should be canceled" do
-          expect(premium_statement.aasm_state).to eq "canceled"
-        end
-      end
-
-      context "pays the binder premium" do
-        before do
-          premium_statement.allocate_binder_payment
-        end
-
-        it "it should transition to binder paid status" do
-          expect(premium_statement.binder_paid?).to be_truthy
-        end
-
-        context "and binder premium payment is reversed" do
-          before do
-            premium_statement.reverse_coverage_period
-          end
-
-          it "coverage should be canceled" do
-            expect(premium_statement.aasm_state).to eq "canceled"
-            expect(employer_profile.aasm_state).to eq "canceled"
-          end          
-        end
-      end
-    end
-
-    context "for an enrolled employer" do
-      before do
-        employer_profile.aasm_state = "enrolled"
-        premium_statement.aasm_state = "current"
-      end
-
-      context "and the billing period advances without a premium payment" do
-        before do
-          premium_statement.advance_billing_period
-        end
-
-        it "should transition to overdue status" do
-          expect(premium_statement.overdue?).to be_truthy
-        end
-
-        context "and a second billing period advances without a premium payment" do
-          before do
-            premium_statement.advance_billing_period
-          end
-
-          it "should transition to late status" do
-            expect(premium_statement.late?).to be_truthy
-          end
-
-          context "and a third billing period advances without a premium payment" do
-            before do
-              premium_statement.advance_billing_period
-            end
-
-            it "should transition to suspended status and set parent employer to suspended" do
-              expect(premium_statement.suspended?).to be_truthy
-              expect(premium_statement.employer_profile.suspended?).to be_truthy
-            end
-
-            context "and a premium in arrears is paid-in-full" do
-              before do
-                premium_statement.advance_coverage_period
-              end
-
-              it "should transition to current status and set parent employer to enrolled" do
-                expect(premium_statement.current?).to be_truthy
-                expect(premium_statement.employer_profile.enrolled?).to be_truthy
-              end
-
-              context "but the premium payment NSFs" do
-                before do
-                  premium_statement.reverse_coverage_period
-                end
-
-                it "should revert to suspended status" do 
-                  expect(premium_statement.aasm_state).to eq "suspended"
-                end
-
-                it "and reset employer to suspended status" do
-                  expect(premium_statement.employer_profile.enrolled?).to be_truthy
-                end
-              end
-            end
-
-            context "and a fourth (final) billing period advances without a premium payment" do
-              before do
-                premium_statement.advance_billing_period
-              end
-
-              it "should transition self and employer to terminated status" do
-                expect(premium_statement.terminated?).to be_truthy
-                expect(premium_statement.terminated?).to be_truthy
-              end
-            end
-
-          end
-        end
-      end
-    end
-  end
-
-  context "an employer who is terminated" do
-  end
 end
