b8d2f8b7a24707b7915e5f6589c7e976debec0f1
vinner <vineelsonnathi@users.noreply.github.com>
2015-06-11 02:15:39 -0400

    1210: show total_employee_cost for the plan on plan detail page.

--- app/controllers/products/qhp_controller.rb
+++ app/controllers/products/qhp_controller.rb
@@ -11,8 +11,14 @@ class Products::QhpController < ApplicationController
   end
 
   def summary
-    sc_id = params.permit(:standard_component_id)[:standard_component_id][0..13]
+    new_params = params.permit(:standard_component_id, :hbx_enrollment_id)
+    hbx_enrollment_id = new_params[:hbx_enrollment_id]
+    sc_id = new_params[:standard_component_id][0..13]
+    @hbx_enrollment = HbxEnrollment.find(hbx_enrollment_id)
+    @benefit_group = @hbx_enrollment.benefit_group
+    @reference_plan = @benefit_group.reference_plan
     @qhp = Products::Qhp.where(standard_component_id: sc_id).to_a.first
+    @plan = PlanCostDecorator.new(@qhp.plan, @hbx_enrollment, @benefit_group, @reference_plan)
     respond_to do |format|
       format.html
       format.js
