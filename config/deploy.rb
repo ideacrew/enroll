aea334f4fb7507f891bbd8a3f911e502a3bf77ab
Trey Evans <lewis.r.evans@gmail.com>
2015-06-13 18:49:05 -0400

    Trying to fix deployment issues.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :rails_env, 'production'
 
 # Default value for :scm is :git
 # set :scm, :git
-# set :scm, :gitcopy
+set :scm, :gitcopy
 
 # Default value for :format is :pretty
 # set :format, :pretty
