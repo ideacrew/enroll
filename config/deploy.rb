5630fd6e8dcb396b0f11d8fa17e4abfb6e58faf0
Trey Evans <lewis.r.evans@gmail.com>
2015-06-13 19:02:37 -0400

    Working on fixing stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.3.5'
 
 set :application, 'enroll'
-set :repo_url, 'git@github.com:dchbx/enroll.git'
+set :repo_url, 'https://github.com/dchbx/enroll.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -13,7 +13,7 @@ set :rails_env, 'production'
 
 # Default value for :scm is :git
 # set :scm, :git
-set :scm, :gitcopy
+# set :scm, :gitcopy
 
 # Default value for :format is :pretty
 # set :format, :pretty
