62d9dc4a9f861a325575885665cdd12631008a37
Dan Thomas <dan.thomas@dc.gov>
2015-06-14 09:56:45 -0400

    Fix incorrect model reference. Persist person before building hbx_staff_role

--- app/models/hbx_staff_role.rb
+++ app/models/hbx_staff_role.rb
@@ -9,23 +9,25 @@ class HbxStaffRole
   field :department, type: String, default: ""
   field :is_active, type: Boolean, default: true
 
+  delegate :hbx_id, to: :person, allow_nil: true
+  delegate :dob, :dob=, to: :person, allow_nil: true
+
+  accepts_nested_attributes_for :person
+
   validates_presence_of :hbx_profile_id
+
+  alias_method :is_active?, :is_active
 
   # belongs_to Hbx
   def hbx_profile=(new_hbx_profile)
-    raise ArgumentError.new("expected EmployerProfile") unless new_hbx_profile.is_a? EmployerProfile
+    raise ArgumentError.new("expected HbxProfile") unless new_hbx_profile.is_a? HbxProfile
     self.hbx_profile_id = new_hbx_profile._id
     @hbx_profile = new_hbx_profile
   end
 
   def hbx_profile
     return @hbx_profile if defined? @hbx_profile
-    @hbx_profile = EmployerProfile.find(self.hbx_profile_id)
+    @hbx_profile = HbxProfile.find(self.hbx_profile_id)
   end
 
-
-
-  def is_active?
-    self.is_active
-  end
 end
--- db/seedfiles/admins_seed.rb
+++ db/seedfiles/admins_seed.rb
@@ -17,8 +17,8 @@ hbx = Organization.create(
     )
 
 admin_user    = User.create!(email: "admin@dc.gov", password: "password", password_confirmation: "password", roles: ["hbx_staff"])
-admin_person  = Person.new(first_name: "system", last_name: "admin", user: admin_user)
-admin_person.build_hbx_staff_role(hbx_profile_id: hbx._id, job_title: "grand poobah", department: "accountability")
+admin_person  = Person.new(first_name: "system", last_name: "admin", dob: "1976-07-04", user: admin_user)
 admin_person.save!
+admin_person.build_hbx_staff_role(hbx_profile_id: hbx._id, job_title: "grand poobah", department: "accountability")
 
 puts "::: HBX Admins Complete :::"
