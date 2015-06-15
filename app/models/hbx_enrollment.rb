5e3dc13dd658d7c78399d13fcf6ace38da5244be
Sean Carley <milythael.mourning@gmail.com>
2015-06-15 22:28:45 +0100

    set benefit group assignment when new hbx enrollment is created

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -150,6 +150,9 @@ class HbxEnrollment
     enrollment.effective_on = calculate_start_date_from(employee_role, coverage_household, benefit_group)
     # benefit_group.plan_year.start_on
     enrollment.benefit_group = benefit_group
+    census_employee = employee_role.census_employee
+    benefit_group_assignment = census_employee.benefit_group_assignments.by_benefit_group_id(benefit_group.id).first
+    enrollment.benefit_group_assignment_id = benefit_group_assignment.id
     coverage_household.coverage_household_members.each do |coverage_member|
       enrollment_member = HbxEnrollmentMember.new_from(coverage_household_member: coverage_member)
       enrollment_member.eligibility_date = enrollment.effective_on
