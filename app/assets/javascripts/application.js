cc5b6361efce5c8dfd75101d123f848bca89f066
jfinucane <jimway@gmail.com>
2015-06-16 11:02:39 -0400

    persistent contact information

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,6 +92,8 @@ $(document).ready(function () {
 
   // Progress Bar
   $(document).on('click', '#btn-continue', function() {
+    console.log('continue', $('#btn-search-employer').length, $('#btn_user_contact_info').length, $('#btn_household_continue').length,$('#btn_select_plan_continue').length)
+    
     if($('#btn-search-employer').length) $('#btn-search-employer').click();
     else if($('#btn_user_contact_info').length) $('#btn_user_contact_info').click();
     else if($('#btn_household_continue').length) window.location = $('#btn_household_continue').val();
