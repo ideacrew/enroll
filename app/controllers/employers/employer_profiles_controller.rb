edfe7d9cd090f907719ee0eee831683833767b6a
Trey Evans <lewis.r.evans@gmail.com>
2015-06-16 10:56:04 -0400

    Let's use the right id.

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -148,7 +148,7 @@ class Employers::EmployerProfilesController < ApplicationController
 
     def check_employer_staff_role
       if current_user.has_employer_staff_role?
-        redirect_to employers_employer_profile_path(current_user.person.employer_staff_roles.first)
+        redirect_to employers_employer_profile_path(:id => current_user.person.employer_staff_roles.first.employer_profile_id)
       end
     end
 
