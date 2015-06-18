929638ee277426c1adbf105094cc0a38ea84af21
Raghuram <raghuramg83@gmail.com>
2015-06-18 09:20:28 -0400

    Added hire on date for active broker page

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -85,7 +85,7 @@ class Employers::EmployerProfilesController < ApplicationController
     @page_alphabets = page_alphabets(census_employees, "last_name")
     page_no = cur_page_no(@page_alphabets.first)
     @census_employees = census_employees.where("last_name" => /^#{page_no}/i)
-    @broker_agency_profiles = @employer_profile.broker_agency_profile.to_a
+    @broker_agency_accounts = @employer_profile.broker_agency_accounts
   end
 
   def new
