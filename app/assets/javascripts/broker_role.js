a9cd2fd4a82da43bdf49019be1e68a07ff1bfa2b
Raghuram <raghuramg83@gmail.com>
2015-06-25 11:06:45 -0400

    reverted tab functionality to ajax requests

--- app/assets/javascripts/broker_role.js
+++ app/assets/javascripts/broker_role.js
@@ -1,22 +1,23 @@
-// $(function() {
-//   $('ul[name=broker_signup_primary_tabs] > ').children().each( function() { 
-//     $(this).change(function(){
-//       filter = $(this).val();
-//       $('#' + filter + '_panel').siblings().empty().hide();
-//       $('#' + filter + '_panel').show();
-//       $.ajax({
-//         url: '/broker_roles/new.js',
-//         type: "GET",
-//         data : { 'filter': filter }
-//       });
-//     })
-//   })
-// })
+$(function() {
+  $('div[name=broker_agency_tabs] > ').children().each( function() { 
+    $(this).change(function(){
+      filter = 'broker_role';
+      agency_type = $(this).val();
+      $('#' + agency_type + '_broker_agency_form').siblings().hide();
+      $('#' + agency_type + '_broker_agency_form').show();
+      $.ajax({
+        url: '/broker_roles/new.js',
+        type: "GET",
+        data : { 'filter': filter, 'agency_type': agency_type }
+      });
+    })
+  })
+})
 
 $(function() {
   $('ul[name=broker_signup_primary_tabs] > li > a').on('click', function() {
       filter = $(this).data('value');
-      $('#' + filter + '_panel').siblings().empty().hide();
+      $('#' + filter + '_panel').siblings().hide();
       $('#' + filter + '_panel').show();
       $.ajax({
         url: '/broker_roles/new.js',
