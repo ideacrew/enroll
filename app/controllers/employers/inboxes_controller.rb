23f5d14ea4f83f7e66d172606f467a266ab7c7ba
vinner <vineelsonnathi@users.noreply.github.com>
2015-06-25 19:51:18 -0400

    remove puts message.

--- app/controllers/employers/inboxes_controller.rb
+++ app/controllers/employers/inboxes_controller.rb
@@ -2,8 +2,7 @@ class Employers::InboxesController < InboxesController
 
   def new
     @inbox_to_name = params["to"]
-     @inbox_provider_name = 'HBXAdmin'
-    puts 'Params', params
+    @inbox_provider_name = 'HBXAdmin'
     super
   end
 
