ef6d2de65479f42643be908eef6fc1b1bf3642b3
Raghuram <raghuramg83@gmail.com>
2015-06-26 11:30:50 -0400

    Added certify/deceritfy functionality on show page with ability capture notes

--- app/controllers/exchanges/broker_applicants_controller.rb
+++ app/controllers/exchanges/broker_applicants_controller.rb
@@ -14,32 +14,48 @@ class Exchanges::BrokerApplicantsController < ApplicationController
     @broker_role = BrokerRole.find(BSON::ObjectId.from_string(params[:id]))
   end
 
-  def certify_broker
+  def update
     broker_role = BrokerRole.find(BSON::ObjectId.from_string(params[:id]))
-    password = SecureRandom.hex(5)
-    user = broker_role.person.user
-    if user.present?
-      user.set_random_password(password)
+    broker_role.comments = params[:broker_role][:comments]
+    broker_role.save!
+
+    if params[:decertify].present?
+      broker_role.decertify!
+      flash[:notice] = "Broker applicant decertified successfully."
     else
-      person = broker_role.person
-      user = User.new(:email => person.emails.first.address, :password => password, :password_confirmation => password)
-      user.roles << "broker"
-      user.save!
-      person.user = user
-      person.save!
+      broker_role.approve!
+      Invitation.invite_broker!(broker_role)
+      flash[:notice] = "Broker applicant certified successfully."
     end
-    broker_role.approve!
-    Invitation.invite_broker!(broker_role)
-    flash[:notice] = "Broker applicant certified successfully."
     redirect_to "/exchanges/hbx_profiles"
   end
 
-  def decertify_broker
-    broker_role = BrokerRole.find(BSON::ObjectId.from_string(params[:id]))
-    broker_role.decertify!
-    flash[:notice] = "Broker applicant decertified successfully."
-    redirect_to "/exchanges/hbx_profiles"
-  end
+  # def certify_broker
+  #   broker_role = BrokerRole.find(BSON::ObjectId.from_string(params[:id]))
+  #   password = SecureRandom.hex(5)
+  #   user = broker_role.person.user
+  #   if user.present?
+  #     user.set_random_password(password)
+  #   else
+  #     person = broker_role.person
+  #     user = User.new(:email => person.emails.first.address, :password => password, :password_confirmation => password)
+  #     user.roles << "broker"
+  #     user.save!
+  #     person.user = user
+  #     person.save!
+  #   end
+  #   broker_role.approve!
+  #   Invitation.invite_broker!(broker_role)
+  #   flash[:notice] = "Broker applicant certified successfully."
+  #   redirect_to "/exchanges/hbx_profiles"
+  # end
+  
+  # def decertify_broker
+  #   broker_role = BrokerRole.find(BSON::ObjectId.from_string(params[:id]))
+  #   broker_role.decertify!
+  #   flash[:notice] = "Broker applicant decertified successfully."
+  #   redirect_to "/exchanges/hbx_profiles"
+  # end
 
   private
 
