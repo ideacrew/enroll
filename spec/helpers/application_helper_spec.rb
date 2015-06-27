6b1eef5af74696dc69304d93d52f97f6e5b8088d
Brian Russel Davis <brian@nurdymuny.com>
2015-06-28 00:29:18 +0545

    Fixed portal_display_name rspec test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ApplicationHelper, :type => :helper do
   describe "#portal_display_name" do
     it "returns the portal display title" do
-    	expect(helper.portal_display_name('consumer_profiles')).to eq("<img src=\"/assets/icons/icon-employee.png\" alt=\"Icon employee\" /> &nbsp; I'm an Individual/Family")
+    	expect(helper.portal_display_name('consumer_profiles')).to eq("<img src=\"/assets/icons/icon-individual.png\" alt=\"Icon individual\" /> &nbsp; I'm an Individual/Family")
     	expect(helper.portal_display_name('employer_profiles')).to eq("<img src=\"/assets/icons/icon-business-owner.png\" alt=\"Icon business owner\" /> &nbsp; I'm an Employer")
     	expect(helper.portal_display_name('profiles')).to eq("<img src=\"/assets/icons/icon-expert.png\" alt=\"Icon expert\" /> &nbsp; I'm a Broker")
     	expect(helper.portal_display_name('broker_roles')).to eq("<img src=\"/assets/icons/icon-expert.png\" alt=\"Icon expert\" /> &nbsp; I'm a Broker")
