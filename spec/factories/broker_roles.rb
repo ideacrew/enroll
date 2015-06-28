21ae6f2322350f2627ba928260bed8a42074374b
Dan Thomas <dan.thomas@dc.gov>
2015-06-28 13:53:21 -0400

    Generate valid numeric NPNs

--- spec/factories/broker_roles.rb
+++ spec/factories/broker_roles.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :broker_role do
     person { FactoryGirl.create(:person) }
-    sequence(:npn) {|n| "abcxyz\##{n}" }
+    sequence(:npn) {|n| "2002345#{n}" }
     provider_kind {"broker"}
 
     trait :with_invalid_provider_kind do
