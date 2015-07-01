99294d7679ee56c854083f2a9acb331d4bac4861
Dan Thomas <dan.thomas@dc.gov>
2015-07-01 08:52:52 -0400

    add deprecation note

--- app/models/employee_role.rb
+++ app/models/employee_role.rb
@@ -8,7 +8,7 @@ class EmployeeRole
 
   field :employer_profile_id, type: BSON::ObjectId
   field :census_employee_id, type: BSON::ObjectId
-  field :benefit_group_id, type: BSON::ObjectId
+  field :benefit_group_id, type: BSON::ObjectId  # TODO: Deprecate
   field :employment_status, type: String
   field :hired_on, type: Date
   field :terminated_on, type: Date
