d9357beccd599a60ab6ce2444fb48392c041391a
Trey Evans <lewis.r.evans@gmail.com>
2015-07-08 13:59:10 -0400

    Fixing #1678.  Now has specs.
    Fixes #1678.

---
+++ spec/views/shared/summary.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "shared/_summary.html.erb" do
+  let(:mock_carrier_profile) { instance_double("CarrierProfile", :dba => "a carrier name") }
+  let(:mock_hbx_enrollment) { instance_double("HbxEnrollment", :hbx_enrollment_members => [], :id => "3241251524") }
+  let(:mock_plan) { instance_double("Plan",
+      :name => "A Plan Name",
+      :carrier_profile_id => "a carrier profile id",
+      :carrier_profile => mock_carrier_profile,
+      :metal_level => "Silver",
+      :plan_type => "A plan type",
+      :nationwide => true,
+      :deductible => 0,
+      :total_premium => 0,
+      :total_employer_contribution => 0,
+      :total_employee_cost => 0,
+      :id => "1234234234",
+      :sbc_file => "THE SBC FILE.PDF"
+      ) }
+  let(:mock_qhp) { instance_double("Products::Qhp", :qhp_benefits => []) }
+
+  before :each do
+    assign :plan, mock_plan
+    assign :hbx_enrollment, mock_hbx_enrollment
+    render "shared/summary", :qhp => mock_qhp
+  end
+
+  it "should have a link to download the sbc pdf" do
+    expect(rendered).to have_selector("a[href='#{root_path + "sbc/THE SBC FILE.PDF"}']")
+  end
+end
