0ad83712c3eaa319c374e2bb21d42369323077c6
Ken Lu <ken@seeice.com>
2015-07-11 08:48:27 -0400

    Refs #1685 - relationship benefit coverage for child over 26 premium contribution percent

--- app/helpers/consumer/employee_roles_helper.rb
+++ app/helpers/consumer/employee_roles_helper.rb
@@ -6,4 +6,19 @@ module Consumer::EmployeeRolesHelper
       { :url => consumer_employee_index_path, :method => :post }
     end
   end
+
+  def coverage_relationship_check(offered_relationship_benefits=[], family_member)
+    relationship = PlanCostDecorator.benefit_relationship(family_member.primary_relationship)
+
+    if relationship == "child_under_26" and calculate_age_by_dob(family_member.dob) > 26
+      relationship = "child_over_26"
+    end
+
+    offered_relationship_benefits.include? relationship
+  end
+
+  def calculate_age_by_dob(dob)
+    now = Date.today
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 end
