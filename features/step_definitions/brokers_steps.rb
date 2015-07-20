322c06b34e8736d569bdf2150e35bd7d5612c967
Raghuram <raghuramg83@gmail.com>
2015-07-20 18:19:15 -0400

    Added cucumber feature for broker registratino

--- features/step_definitions/brokers_steps.rb
+++ features/step_definitions/brokers_steps.rb
@@ -1,16 +1,23 @@
-When(/^I visit the HBX Broker Agency portal$/) do
+When(/^I visit the HBX Broker Registration form$/) do
   @browser.goto("http://localhost:3000/")
-  # screenshot("enroll_home_page")
   @browser.element(class: /interaction-click-control-broker-registration/).wait_until_present
   @browser.element(class: /interaction-click-control-broker-registration/).click
-  # screenshot("broker_agency_portal_click")
+end
+
+When(/^I click on New Broker Agency Tab$/) do 
   @browser.radio(class: /interaction-choice-control-value-new-broker-agency/).wait_until_present
   @browser.radio(class: /interaction-choice-control-value-new-broker-agency/).fire_event("onclick")
 end
 
-And(/^I should see an initial form to enter personal information$/) do
+When(/^I should see the New Broker Agency form$/) do
+  @browser.element(id: "broker_agency_form").wait_until_present
+  expect(@browser.element(id: "broker_agency_form").visible?).to be_truthy
+end
+
+When(/^I enter personal information$/) do
   @browser.element(class: /interaction-click-control-create-broker-agency/).wait_until_present
   @browser.text_field(class: /interaction-field-control-person-first-name/).wait_until_present
+
   @browser.text_field(class: /interaction-field-control-person-first-name/).set("Ricky")
   @browser.text_field(class: /interaction-field-control-person-last-name/).set("Martin")
   @browser.text_field(class: /interaction-field-control-person-dob/).set("10/10/1984")
@@ -19,28 +26,29 @@ And(/^I should see an initial form to enter personal information$/) do
   @browser.text_field(class: /interaction-field-control-person-npn/).set("109109109")
 end
 
-
-And(/^I should see a second fieldset to enter broker agency information$/) do
+And(/^I enter broker agency information$/) do
   @browser.text_field(class: /interaction-field-control-organization-legal-name/).set("Prometric Inc")
   @browser.text_field(class: /interaction-field-control-organization-dba/).set("Prometric Inc")
   @browser.text_field(class: /interaction-field-control-organization-fein/).set("890890891")
+  
   entity_kind = @browser.div(class: /interaction-choice-control-organization-entity-kind/)
   entity_kind.click
   entity_kind.li(text: /S Corporation/).click
-  #Address
+
   @browser.text_field(class: /interaction-field-control-broker-corporate-npn/).set("890890892")
   @browser.text_field(class: /interaction-field-control-broker-home-page/).set("www.prometric.example.com")
 
   practice_area = @browser.div(class: /selectric-interaction-choice-control-broker-agency-practice-area/)
   practice_area.click
   practice_area.li(text: /Small Business Marketplace ONLY/).click
+
   @browser.text_field(class: /interaction-field-control-broker-agency-languages-spoken/).set("English")
-  # @browser.checkbox(class: /interaction-choice-control-broker-agency-evening-weekend-hours/).set
-  @browser.checkboxes.first.set # evening/weekend hours
-  @browser.checkboxes.last.set # accept new clients
+  
+  # Select evening/weekend hours, accept new clients checkboxes
+  @browser.checkboxes.each {|checkbox| checkbox.set }
 end
 
-And(/^I should see a third fieldset to enter more office location information$/) do
+And(/^I enter office locations information$/) do
   enter_office_location({
     address1: "623a Spalding Ct",
     address2: "Suite 200",
@@ -53,15 +61,18 @@ And(/^I should see a third fieldset to enter more office location information$/)
     })
 end
 
-When(/^I click on create broker agency button$/) do
+
+And(/^I click on Create Broker Agency$/) do
   @browser.element(class: /interaction-click-control-create-broker-agency/).wait_until_present
   @browser.element(class: /interaction-click-control-create-broker-agency/).click
 end
 
-Then(/^I should see a successful broker registration message$/) do
+
+Then(/^I should see broker registration successful message$/) do
   @browser.element(text: /Your registration has been submitted. A response will be sent to the email address you provided once your application is reviewed./).wait_until_present
   expect(@browser.element(text: /Your registration has been submitted. A response will be sent to the email address you provided once your application is reviewed./).visible?).to be_truthy
 end
+
 
 When(/^I login as an Hbx Admin$/) do
   @browser.goto("http://localhost:3000/")
@@ -73,21 +84,31 @@ When(/^I login as an Hbx Admin$/) do
   @browser.element(class: /interaction-click-control-sign-in/).click
 end
 
+
 And(/^I click on brokers tab$/) do
   @browser.element(class: /interaction-click-control-brokers/).wait_until_present
   scroll_then_click(@browser.element(class: /interaction-click-control-brokers/))
 end
-Then(/^I click on show button for the broker$/) do
+
+And(/^I should see the list of broker applicants$/) do
+ 
+end
+
+Then(/^I click on the current broker applicant show button$/) do
   @browser.element(class: /interaction-click-control-broker-show/).wait_until_present
   scroll_then_click(@browser.element(class: /interaction-click-control-broker-show/))
 end
 
-And(/^I click on approve broker$/) do
+And(/^I should see the broker application$/) do
+ 
+end
+
+And(/^I click on approve broker button$/) do
   @browser.element(class: /interaction-click-control-broker-approve/).wait_until_present
   scroll_then_click(@browser.element(class: /interaction-click-control-broker-approve/))
 end
 
-Then(/^I should see a broker successful approve message$/) do
+Then(/^I should see the broker successfully approved message$/) do
   @browser.element(text: /Broker applicant approved successfully./).wait_until_present
   expect(@browser.element(text: /Broker applicant approved successfully./).visible?).to be_truthy
 end
