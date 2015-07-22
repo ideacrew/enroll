d5c684e27d3a564893282bca2226d07a4295385f
Brian Russel Davis <brian@nurdymuny.com>
2015-07-22 19:56:01 +0545

    Rspec for enrollment progress bar display

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe ApplicationHelper, :type => :helper do
       expect(helper.dob_in_words(0, "20/07/2014".to_date)).to eq time_ago_in_words("20/07/2014".to_date)
     end
   end
+
+  describe "#enrollment_progress_bar" do
+    let(:employer_profile){FactoryGirl.create(:employer_profile)}
+    let(:plan_year){FactoryGirl.create(:plan_year, employer_profile: employer_profile)}
+
+    it "display progress bar" do
+      expect(helper.enrollment_progress_bar(plan_year, 1, minimum: false)).to include('<div class="progress-wrapper">')
+    end
+  end
 end
