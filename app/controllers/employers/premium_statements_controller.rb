2fd66620e87398eca007cf7f88dd94fc42ef77b8
Ken Lu <ken@seeice.com>
2015-07-24 13:59:52 -0400

    Refs #1814 - added more attributes and fields correct the number to currency display

--- app/controllers/employers/premium_statements_controller.rb
+++ app/controllers/employers/premium_statements_controller.rb
@@ -20,9 +20,23 @@ private
   def csv_for(hbx_enrollments)
     (output = "").tap do
       CSV.generate(output) do |csv|
+        csv << ["Name", "SSN", "DOB", "Hired On", "Benefit Group", "Type", "Name", "Issuer", "Covered Ct", "Employer Contribution",
+        "Employee Premium", "Total Premium"]
         hbx_enrollments.each do |enrollment|
           ee = enrollment.subscriber.person.employee_roles.first.census_employee
-          csv << [ee.full_name, ee.ssn, ee.dob, ee.hired_on, ee.published_benefit_group.title, "Health", enrollment.plan.name]
+          csv << [  ee.full_name,
+                    ee.ssn,
+                    ee.dob,
+                    ee.hired_on,
+                    ee.published_benefit_group.title,
+                    "Health",
+                    enrollment.plan.name,
+                    enrollment.plan.carrier_profile.legal_name,
+                    enrollment.humanized_dependent_summary,
+                    view_context.number_to_currency(enrollment.total_employer_contribution),
+                    view_context.number_to_currency(enrollment.total_employee_cost),
+                    view_context.number_to_currency(enrollment.total_premium)
+                  ]
         end
       end
     end
