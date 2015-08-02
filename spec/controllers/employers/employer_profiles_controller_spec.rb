3953a5b04c25c15bdd5a66824459873e4881c1dd
sullivanm <sullivancoder@gmail.com>
2015-08-02 12:52:08 -0700

    FIXES #1480 spec

--- spec/controllers/employers/employer_profiles_controller_spec.rb
+++ spec/controllers/employers/employer_profiles_controller_spec.rb
@@ -356,9 +356,10 @@ RSpec.describe Employers::EmployerProfilesController do
       allow(person).to receive(:employer_staff_roles).and_return([EmployerStaffRole.new])
       sign_in(user)
       expect(Organization).to receive(:find)
+      expect(EmployerStaffRole).to receive(:create)
       expect(user).to receive(:roles)
       put :update, id: organization.id
-      expect(response).to render_template("edit")
+      expect(response).to be_redirect
     end
 
     context "given current user is invalid" do
