ed9a106972c636fa668b980e10ac1f04e0268431
sullivanm <sullivancoder@gmail.com>
2015-08-05 15:13:24 -0700

    Revert "FIXES #1887 	More user friendly error when publishing a plan"
    This reverts commit 18ae236f1b1498b5c29d0bcd7533830c39423039.

--- app/controllers/employers/plan_years_controller.rb
+++ app/controllers/employers/plan_years_controller.rb
@@ -144,11 +144,8 @@ class Employers::PlanYearsController < ApplicationController
         format.js
       end
     else
-      if (@plan_year.published? || @plan_year.enrolling?) 
-        flash[:notice] = "Plan Year successfully published."
-      else
-        flash[:error] = "Plan Year failed to publish. Every employee must be assigned to a benefit group defined for the published plan year."
-      end
+      flash[:notice] = (@plan_year.published? || @plan_year.enrolling?) ? "Plan Year successfully published."
+                           : "Plan Year failed to publish: #{@plan_year.application_errors}"
       render :js => "window.location = #{employers_employer_profile_path(@employer_profile).to_json}"
     end
   end
