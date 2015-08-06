48b3e71dbbfdd14bd34dd6c782e347d561617137
Raghuram <raghuramg83@gmail.com>
2015-08-06 15:04:49 -0400

    Refs #2033 Fixed premium_tables selection for plan year creation page

--- app/controllers/employers/plan_years_controller.rb
+++ app/controllers/employers/plan_years_controller.rb
@@ -59,20 +59,14 @@ class Employers::PlanYearsController < ApplicationController
 
   def search_reference_plan
     @location_id = params[:location_id]
-    return unless params[:reference_plan_id].present?
-
     @plan = Plan.find(params[:reference_plan_id])
-    @premium_tables = @plan.premium_tables.where(start_on: @plan.premium_tables.distinct(:start_on).max)
+    @premium_tables = @plan.premium_table_for(Date.parse(params[:start_on]))
   end
 
   def calc_employer_contributions
     @location_id = params[:location_id]
-    return unless params[:reference_plan_id].present?
-
-    params.merge!({ plan_year: {
-      start_on: params[:start_on] #PlanYear.calculate_start_on_dates.first.to_s(:db) 
-      }.merge(relationship_benefits) })
-
+    params.merge!({ plan_year: { start_on: params[:start_on] }.merge(relationship_benefits) })
+    
     @plan = Plan.find(params[:reference_plan_id])
     @plan_year = ::Forms::PlanYearForm.build(@employer_profile, plan_year_params)
     @plan_year.benefit_groups[0].reference_plan = @plan
