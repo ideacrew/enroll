d957b12f7d3f3da83763a0f543e564c3a827522f
Raghuram <raghuramg83@gmail.com>
2015-08-06 17:16:06 -0400

    moved helper menu_tab_class to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  
+  def menu_tab_class(a_tab, current_tab)
+    (a_tab == current_tab) ? raw(" class=\"active\"") : ""
+  end
 
   def datepicker_control(f, field_name, options = {}, value = "")
     sanitized_field_name = field_name.to_s.sub(/\?$/,"")
