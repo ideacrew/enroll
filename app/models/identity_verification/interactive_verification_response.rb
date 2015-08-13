8a09e5282605e259559b4c81cc02e6b642d1931e
Trey Evans <lewis.r.evans@gmail.com>
2015-08-13 13:23:38 -0400

    Refs #2216.  Parser for RIDP responses.
    Completed the parser for responses from the RIDP service.
    
    Refs #2216.

--- app/models/identity_verification/interactive_verification_response.rb
+++ app/models/identity_verification/interactive_verification_response.rb
@@ -8,17 +8,43 @@ module IdentityVerification
 
     element 'verification_result', "IdentityVerification::InteractiveVerificationResult"
 
+    element "session", "IdentityVerification::InteractiveVerificationSession"
+
     def failed?
-      return false if verification_result.nil?
-      verification_result.failed?
+      "urn:openhbx:terms:v1:interactive_identity_verification#FAILURE" == response_code
+    end
+
+    def response_code
+      either_child_property(:response_code)
     end
 
     def response_text
+      return nil if verification_result.nil?
       verification_result.response_text
     end
 
     def transaction_id
-      verification_result.transaction_id
+      either_child_property(:transaction_id)
+    end
+
+    def either_child_property(prop, default_prop_val = nil)
+      return(session.send(prop)) unless session.nil?
+      return(verification_result.send(prop)) unless verification_result.nil?
+      default_prop_val
+    end
+
+    def questions
+      return [] if session.nil?
+      session.questions
+    end
+
+    def session_id
+      return nil if session.nil?
+      session.session_id
+    end
+
+    def continue_session?
+      !session.nil?
     end
   end
 end
--- app/models/identity_verification/interactive_verification_session.rb
+++ app/models/identity_verification/interactive_verification_session.rb
@@ -1,14 +1,15 @@
 module IdentityVerification
-  class InteractiveVerificationResult
+  class InteractiveVerificationSession
     include HappyMapper
 
     register_namespace 'acapi', 'http://openhbx.org/api/terms/1.0'
-    tag 'verification_result'
+    tag 'session'
     namespace 'acapi'
 
     element 'response_code', String
-    element 'response_text', String
     element 'transaction_id', String
+    element 'session_id', String
 
+    has_many :questions, "IdentityVerification::InteractiveQuestion", :tag => "question"
   end
 end
