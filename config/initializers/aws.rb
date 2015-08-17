236eb0f44149c5c5f95c537d83062786f608c15f
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-17 18:16:35 -0400

    Refs #2160 Aws init changes

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,11 @@
 #This assumes you have stored the parameters in environment variables
+
 akid=ENV['AWS_ACCESS_KEY_ID']
 secret=ENV['AWS_SECRET_ACCESS_KEY']
-region=ENV['AWS_REGION']
 
 #ENV['AWS_ACCESS_KEY_ID']=akid
 #ENV['AWS_SECRET_ACCESS_KEY']=secret
 #ENV['AWS_REGION']=region
 
-Aws.config.update({region: region,
+Aws.config.update({region: 'us-east-1',
                    credentials: Aws::Credentials.new(akid, secret)})
