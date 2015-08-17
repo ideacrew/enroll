102d98f0f59585fcad14db04ba770f2246a41a2f
Trey Evans <lewis.r.evans@gmail.com>
2015-08-17 15:49:51 -0400

    Fixed issues with build and emails fields.
    Normally you want to exclude the ID fields on new records when doing
    nested_attributes_for with mongoid.
    
    If you don't:
    - Mongoid assigns an ID as soon as the object is created
    - nested_attributes_for, if you assign an object with an ID, will try to
      Mongoid.find that document
    - ???
    - Crash
    
    What we had previously been doing was constraining the parameters
    allowing things into the controller to prevent this.  However, there was
    a full whitelist being passed, which allowed the "id" attribute to be
    accepted.

--- app/controllers/consumer/consumer_roles_controller.rb
+++ app/controllers/consumer/consumer_roles_controller.rb
@@ -26,7 +26,7 @@ class Consumer::ConsumerRolesController < ApplicationController
     @person.addresses = []
     @person.phones = []
     @person.emails = []
-    if @person.update_attributes(params.require(:person).permit!)
+    if @person.update_attributes(params.require(:person).permit(*person_parameters_list))
       redirect_to new_insured_interactive_identity_verifications_path
     else
       build_nested_models
@@ -37,6 +37,29 @@ class Consumer::ConsumerRolesController < ApplicationController
   end
 
   private
+
+  def person_parameters_list
+    [
+      { :addresses_attributes => [:kind, :address_1, :address_2, :city, :state, :zip] },
+      { :phones_attributes => [:kind, :full_phone_number] },
+      { :email_attributes => [:kind, :address] },
+      :first_name,
+      :last_name,
+      :middle_name,
+      :name_pfx,
+      :name_sfx,
+      :date_of_birth,
+      :ssn,
+      :gender,
+      :language_code,
+      :is_incarcerated,
+      :race,
+      :is_tobacco_user,
+      :is_consumer_role,
+      :ethnicity
+    ]
+  end
+
   def build_nested_models
     Phone::KINDS.delete_if{|kind| kind == "work"}.each do |kind|
       @person.phones.build(kind: kind) if @person.phones.select{|phone| phone.kind == kind}.blank?
