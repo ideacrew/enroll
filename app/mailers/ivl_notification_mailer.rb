beb31bd52a16d7b3e95bc5d5d36c0b98b72a44d8
sullivanm <sullivancoder@gmail.com>
2015-08-17 08:28:55 -0700

    Refs #2200 modified IVL notification script and include the spec scripts

--- app/mailers/ivl_notification_mailer.rb
+++ app/mailers/ivl_notification_mailer.rb
@@ -1,31 +1,33 @@
 class IvlNotificationMailer < ApplicationMailer
-
+  include ActionView::Helpers::UrlHelper
+   # helper ActionView::Helpers::UrlHelper
+   
   def lawful_presence_verified(user)
-    notice = Notice.new(user.email, {:notice_data => {:user => user}, :template => "notices/sample_notice.html.erb"})
-    attachments["notice.pdf"] = notice.send_paper_notice
+    notice = Notice.new(user.email, {:notice_data => {:user => user}, :template => "notices/9cindividual.html.erb"})    
+    attachments["notice.pdf"] = File.read(notice.send_paper_notice)
     _link = link_to("click here", :controller => "ivl_notifications").to_html
-    
-    mail({to: user.email, subject: "DCHealthLink Notification"}) do |format|
+    mail({to: user.email.address, subject: "DCHealthLink Notification"}) do |format|
       format.html { render "ivl_notification", :locals => { :user => user, :link => _link}}
     end
   end
 
   def lawful_presence_unverified(user)
-    notice = Notice.new(user.email, {:notice_data => {:user => user}, :template => "notices/sample_notice.html.erb"})
-    attachments["notice.pdf"] = notice.send_paper_notice
+    notice = Notice.new(user.email, {:notice_data => {:user => user}, :template => "notices/9cindividual.html.erb"})
+    attachments["notice.pdf"] = File.read(notice.send_paper_notice)
     _link = link_to("click here", :controller => "ivl_notifications").to_html
-    mail({to: user.email, subject: "DCHealthLink Notification"}) do |format|
+    mail({to: user.email.address, subject: "DCHealthLink Notification"}) do |format|
       format.html { render "ivl_notification", :locals => { :user => user, :link => _link}}
     end
   end
 
   def lawfully_ineligible(user)
-    notice = Notice.new(user.email, {:notice_data => {:user => user}, :template => "notices/sample_notice.html.erb"})
-    attachments["notice.pdf"] = notice.send_paper_notice
+    notice = Notice.new(user.email, {:notice_data => {:user => user}, :template => "notices/9cindividual.html.erb"})
+    attachments["notice.pdf"] = File.read(notice.send_paper_notice)
     _link = link_to("click here", :controller => "ivl_notifications").to_html
-    mail({to: user.email, subject: "DCHealthLink Notification"}) do |format|
+    mail({to: user.email.address, subject: "DCHealthLink Notification"}) do |format|
       format.html { render "ivl_notification", :locals => { :user => user, :link => _link}}
     end
   end
   
 end 
+
