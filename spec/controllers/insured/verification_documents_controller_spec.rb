62a1b79accc67695f3dc8fd08682a41d531b7c60
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-18 10:07:40 -0400

    Refs #2160 Added spec. made controller testable

--- spec/controllers/insured/verification_documents_controller_spec.rb
+++ spec/controllers/insured/verification_documents_controller_spec.rb
@@ -1,21 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Insured::VerificationDocumentsController, :type => :controller do
-  let(:person) {FactoryGirl.create(:person)}
-  let(:consumer_role) {FactoryGirl.creat(:consumer_role)}
-  let(:consumer_wrapper) {Forms::ConsumerRole.new(consumer_role)}
+  let(:user) { FactoryGirl.create(:user) }
+  let(:person) { double }
+  let(:consumer_role) { {consumer_role: ''} }
+  let(:consumer_wrapper) { double }
 
 
-  context "POST upload" do
+  context "Fails to upload file" do
+    it "redirects" do
+      allow(Forms::ConsumerRole).to receive(:initialize).with(anything).and_return(consumer_wrapper)
+      allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:get_family)
+      allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:person_consumer_role)
+      sign_in user
+      post :upload, consumer_role: consumer_role
+      expect(flash[:error]).to be_present
+    end
+  end
 
-    context "AWS returns nil" do
+  context "Succeeds to save file" do
+
+    describe "" do
+      let(:file) { double }
+      let(:temp_file) { double }
+      let(:consumer_role) { {consumer_role: '', file: file} }
+      let(:doc_id) { 'erewrewrewr234214' }
+
       it "redirects" do
-        post :upload
-        expect(response).to have_http_status(:redirect)
+        allow(file).to receive(:tempfile).and_return(temp_file)
+        allow(temp_file).to receive(:path)
+        #allow(doc_id).to receive(:present?).and_return(true)
+        allow(Forms::ConsumerRole).to receive(:initialize).with(anything).and_return(consumer_wrapper)
+        allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:build_document).and_return(double)
+        allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:save_consumer_role).and_return(true)
+        allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:get_family)
+        allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:person_consumer_role)
+        allow_any_instance_of(Insured::VerificationDocumentsController).to receive(:file_path).and_return('')
+        allow(Aws::S3Storage).to receive(:save).with(anything, anything).and_return(doc_id)
+        sign_in user
+        post :upload, consumer_role: consumer_role
+        expect(flash[:notice]).to be_present
       end
     end
-
-    context "save success"
-
   end
 end
