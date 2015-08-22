dec5668244d76bc01764a2fc01d1796697ccf419
Ken Lu <ken@seeice.com>
2015-08-22 09:01:36 -0400

    Refs #2232 - format view details and my account page remote link

---
+++ spec/views/products/qhp/summary.js.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "products/qhp/summary.js.erb" do
+  before :each do
+    stub_template "shared/_summary.html.erb" => ''
+    render file: "products/qhp/summary.js.erb"
+  end
+
+  it "should call account-detail" do
+    expect(rendered).to match /account-detail/
+    expect(rendered).to match /all-plans/
+    expect(rendered).to match /plan-summary/
+  end
+end
