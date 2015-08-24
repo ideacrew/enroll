deddc0568d04e824544cf7d298c0237ee2b2fb8c
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-24 16:53:18 -0400

    removed puts()

--- spec/models/parsers/xml/cv/residency_verification_response_spec.rb
+++ spec/models/parsers/xml/cv/residency_verification_response_spec.rb
@@ -6,7 +6,6 @@ describe Parsers::Xml::Cv::ResidencyVerificationResponse do
   let(:subject) {Parsers::Xml::Cv::ResidencyVerificationResponse.new}
   it 'returns hash' do
     subject.parse(xml)
-    puts subject.to_hash
     expect(subject.to_hash).to eq({:residency_verification_response=>"ADDRESS_NOT_IN_AREA"})
   end
 end
