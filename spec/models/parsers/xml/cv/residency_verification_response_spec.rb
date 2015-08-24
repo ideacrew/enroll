d3e6ac285fc3e212eb737e501d5e2c56d659a665
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-24 16:09:36 -0400

    Fixes #2368 Local Residency response parser & spec

--- spec/models/parsers/xml/cv/residency_verification_response_spec.rb
+++ spec/models/parsers/xml/cv/residency_verification_response_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-describe Parsers::ResidencyVerificationResponse do
+describe Parsers::Xml::Cv::ResidencyVerificationResponse do
 
   let(:xml) {File.read(Rails.root.join("spec","test_data", "residency_verification_payloads", "response.xml"))}
-  let(:subject) {Parsers::ResidencyVerificationResponse.new}
+  let(:subject) {Parsers::Xml::Cv::ResidencyVerificationResponse.new}
   it 'returns hash' do
     subject.parse(xml)
+    puts subject.to_hash
     expect(subject.to_hash).to eq({:residency_verification_response=>"ADDRESS_NOT_IN_AREA"})
   end
 end
