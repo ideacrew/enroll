99c1a41e8da8e43b6c32de8ceff3f06dee47a9f7
Raghuram <raghuramg83@gmail.com>
2015-08-25 15:12:39 -0400

    code cleanup for notices

--- app/notices/eligibility_notice_builder.rb
+++ app/notices/eligibility_notice_builder.rb
@@ -16,30 +16,29 @@ class EligibilityNoticeBuilder < Notice
     hbx_enrollments = family.try(:latest_household).try(:hbx_enrollments).active || []
     @notice = PdfTemplates::EligibilityNotice.new
     @notice.primary_fullname = @consumer.full_name.titleize
-    append_address
+    append_address(@consumer.addresses[0])
     append_enrollments(hbx_enrollments)
   end
 
-  def append_address
-    primary_address = @consumer.addresses[0]
-    address = PdfTemplates::NoticeAddress.new
-    address.street_1 = primary_address.address_1.titleize
-    address.street_2 = primary_address.address_2.titleize
-    address.city = primary_address.city.titleize
-    address.state = primary_address.state
-    address.zip = primary_address.zip
-    @notice.primary_address = address
+  def append_address(primary_address)
+    @notice.primary_address = PdfTemplates::NoticeAddress.new({
+      street_1: primary_address.address_1.titleize,
+      street_2: primary_address.address_2.titleize,
+      city: primary_address.city.titleize,
+      state: primary_address.state,
+      zip: primary_address.zip
+      })
   end
 
   def append_enrollments(hbx_enrollments)
     hbx_enrollments.each do |hbx_enrollment|
-      enrollment = PdfTemplates::Enrollment.new 
-      enrollment.plan_name = hbx_enrollment.plan.name
-      enrollment.enrollees = hbx_enrollment.hbx_enrollment_members.map(&:person).inject([]) do |names, person| 
-        names << person.full_name.titleize
-      end
-      enrollment.monthly_premium_cost = hbx_enrollment.total_premium
-      @notice.enrollments << enrollment
-    end  
+      @notice.enrollments << PdfTemplates::Enrollment.new({
+        plan_name: hbx_enrollment.plan.name,
+        monthly_premium_cost: hbx_enrollment.total_premium,
+        enrollees: hbx_enrollment.hbx_enrollment_members.inject([]) do |names, member| 
+          names << member.person.full_name.titleize
+        end
+        }) 
+    end 
   end
 end
