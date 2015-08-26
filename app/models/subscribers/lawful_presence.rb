ae15a0830721ceb5a364dea6b05e3a161f02d2f8
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-26 16:20:52 -0400

    Fixes #2397 listener for lawful presence and spec

---
+++ app/models/subscribers/lawful_presence.rb
@@ -0,0 +1,50 @@
+module Subscribers
+  class LawfulPresence < ::Acapi::Subscription
+    def self.subscription_details
+      ["local.enroll.lawful_presence.lawful_presence_response"]
+    end
+
+    def call(event_name, e_start, e_end, msg_id, payload)
+      stringed_key_payload = payload.stringify_keys
+      process_response(stringed_key_payload['body'], stringed_key_payload['individual_id'])
+    end
+
+    private
+    def process_response(xml, person_hbx_id)
+      person = find_person(person_hbx_id)
+      return if person.nil? || person.consumer_role.nil?
+
+      consumer_role = person.consumer_role
+      xml_hash = xml_to_hash(xml)
+
+      update_consumer_role(consumer_role, xml_hash)
+    end
+
+    def update_consumer_role(consumer_role, xml_hash)
+      args = OpenStruct.new
+
+      if xml_hash[:lawful_presence_indeterminate].present?
+        args.determined_at = Time.now
+        args.vlp_authority = 'ssa'
+        consumer_role.deny_lawful_presence(args)
+      elsif xml_hash[:lawful_presence_determination].present?
+        args.determined_at = Time.now
+        args.vlp_authority = 'ssa'
+        args.citizen_status = get_citizen_status(xml_hash[:lawful_presence_determination][:legal_status])
+        consumer_role.authorize_lawful_presence(args)
+      end
+    end
+
+    def get_citizen_status(legal_status)
+      return "us_citizen" if legal_status.eql? "citizen"
+    end
+
+    def xml_to_hash(xml)
+      Parsers::Xml::Cv::LawfulPresenceResponseParser.parse(xml).to_hash
+    end
+
+    def find_person(person_hbx_id)
+      Person.where(hbx_id:person_hbx_id).first
+    end
+  end
+end
---
+++ spec/models/subscribers/lawful_presence_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+describe Subscribers::LawfulPresence do
+
+  it "should subscribe to the correct event" do
+    expect(Subscribers::LawfulPresence.subscription_details).to eq ["local.enroll.lawful_presence.lawful_presence_response"]
+  end
+
+  describe "given a ssa verification message to handle" do
+    let(:individual_id) { "121211" }
+    let(:xml) { double }
+    let(:xml_hash2) {{:case_number=>"12121", :lawful_presence_indeterminate=>{:response_code=>"invalid_information", :response_text=>"Complete information."}} }
+    let(:xml_hash) { {:case_number=>"12121", :lawful_presence_determination=>{:response_code=>"not_lawfully_present", :legal_status=>"lawful_permanent_resident", :employment_authorized=>"authorized", :document_results=>{:document_cert_of_naturalization=>{:case_number=>"2113123", :response_code=>"022", :response_description_text=>"Good", :tds_response_description_text=>nil, :entry_date=>nil, :admitted_to_date=>nil, :admitted_to_text=>nil, :country_birth_code=>nil, :country_citizen_code=>nil, :coa_code=>nil, :eads_expire_date=>nil, :elig_statement_code=>"011", :elig_statement_txt=>"ok", :iav_type_code=>nil, :iav_type_text=>nil, :grant_date=>nil, :grant_date_reason_code=>nil}, :document_foreign_passport=>{:case_number=>"12123123", :response_code=>"011", :response_description_text=>"Valid Passport", :tds_response_description_text=>nil, :entry_date=>nil, :admitted_to_date=>nil, :admitted_to_text=>nil, :country_birth_code=>nil, :country_citizen_code=>nil, :coa_code=>nil, :eads_expire_date=>nil, :elig_statement_code=>"01", :elig_statement_txt=>"1.1", :iav_type_code=>nil, :iav_type_text=>nil, :grant_date=>nil, :grant_date_reason_code=>nil}}}} }
+    let(:person) { person = FactoryGirl.build(:person);
+    consumer_role = person.build_consumer_role;
+    consumer_role = FactoryGirl.build(:consumer_role);
+    person.consumer_role = consumer_role;
+    person.consumer_role.aasm_state=:verifications_pending;
+    person
+    }
+
+    let(:payload) { {:individual_id => individual_id, :body => xml} }
+
+    context "lawful_presence_determination" do
+      it "should approve lawful presence" do
+        allow(subject).to receive(:xml_to_hash).with(xml).and_return(xml_hash)
+        allow(subject).to receive(:find_person).with(individual_id).and_return(person)
+        subject.call(nil, nil, nil, nil, payload)
+        expect(person.consumer_role.aasm_state).to eq('fully_verified')
+      end
+    end
+
+    context "lawful_presence_indeterminate" do
+      it "should deny lawful presence" do
+        allow(subject).to receive(:xml_to_hash).with(xml).and_return(xml_hash2)
+        allow(subject).to receive(:find_person).with(individual_id).and_return(person)
+        subject.call(nil, nil, nil, nil, payload)
+        expect(person.consumer_role.aasm_state).to eq('verifications_outstanding')
+      end
+    end
+
+  end
+end
