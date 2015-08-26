06dc009316daaa8375f9285586149e744adbd717
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-26 10:48:21 -0400

    Fixes #2399 Listener for local residency event

---
+++ app/models/subscribers/local_residency.rb
@@ -0,0 +1,35 @@
+module Subscribers
+  class LocalResidency < ::Acapi::Subscription
+    def self.subscription_details
+      ["acapi.info.events.residency_verification.vlp_verification_response"]
+    end
+
+    def call(event_name, e_start, e_end, msg_id, payload)
+      stringed_key_payload = payload.stringify_keys
+      process_response(stringed_key_payload['body'], stringed_key_payload['individual_id'])
+    end
+
+    private
+    def process_response(xml, person_hbx_id)
+      person = find_person(person_hbx_id)
+      return if person.nil? || person.consumer_role.nil?
+
+      consumer_role = person.consumer_role
+      residency_verification_hash = xml_to_hash(xml)
+
+      if residency_verification_hash[:residency_verification_response].eql? 'ADDRESS_NOT_IN_AREA'
+        consumer_role.deny_residency
+      else
+        consumer_role.authorize_residency
+      end
+    end
+
+    def xml_to_hash(xml)
+      Parsers::Xml::Cv::ResidencyVerificationResponse.parse(xml).to_hash
+    end
+
+    def find_person(person_hbx_id)
+      Person.where(hbx_id:person_hbx_id).first
+    end
+  end
+end
---
+++ spec/models/subscribers/local_residency_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe Subscribers::LocalResidency do
+
+  it "should subscribe to the correct event" do
+    expect(Subscribers::LocalResidency.subscription_details).to eq ["acapi.info.events.residency_verification.vlp_verification_response"]
+  end
+
+  describe "given a residency verification message to handle" do
+    let(:individual_id) { "121211" }
+    let(:xml) { File.read(Rails.root.join("spec", "test_data", "residency_verification_payloads", "response.xml")) }
+    let(:xml_hash) { {residency_verification_response: 'ADDRESS_NOT_IN_AREA'} }
+    let(:person) { person = FactoryGirl.build(:person);
+    consumer_role = person.build_consumer_role;
+    consumer_role = FactoryGirl.build(:consumer_role);
+    person.consumer_role = consumer_role;
+    person.consumer_role.aasm_state=:verifications_pending;
+    person
+    }
+
+    let(:payload) { {:individual_id => individual_id, :body => xml} }
+
+    it "should set local residency the timekeeper" do
+      allow(subject).to receive(:xml_to_hash).with(xml).and_return(xml_hash)
+      allow(subject).to receive(:find_person).with(individual_id).and_return(person)
+      subject.call(nil, nil, nil, nil, payload)
+      expect(person.consumer_role.aasm_state).to eq('verifications_pending')
+    end
+  end
+end
