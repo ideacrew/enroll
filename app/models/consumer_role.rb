5667a8d5e8555f59129628971927b8a5a96a3880
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-26 17:24:51 -0400

    added comment for usage of raw_event_responses

--- app/models/consumer_role.rb
+++ app/models/consumer_role.rb
@@ -96,7 +96,7 @@ class ConsumerRole
   field :marital_status, type: String
   field :is_active, type: Boolean, default: true
 
-  field :raw_event_responses, type: Array, default: []
+    field :raw_event_responses, type: Array, default: [] #e.g. [{:lawful_presence_response => payload}]
 
   delegate :hbx_id, :hbx_id=, to: :person, allow_nil: true
   delegate :ssn,    :ssn=,    to: :person, allow_nil: true
