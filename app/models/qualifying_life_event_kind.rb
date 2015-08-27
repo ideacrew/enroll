bf03f06862f398d7ffdd6b8b732842d6572e8321
Dan Thomas <dan.thomas@dc.gov>
2015-08-27 11:23:56 -0400

    Update model to support additional IVL QLE paramters

--- app/models/qualifying_life_event_kind.rb
+++ app/models/qualifying_life_event_kind.rb
@@ -2,9 +2,23 @@ class QualifyingLifeEventKind
   include Mongoid::Document
 	include Mongoid::Timestamps
 
-  Kinds = %w[add_member drop_member change_benefit terminate_benefit administrative]
+  # Model Changes to support IVL needs
+  ## effective_on_kinds -- type changed to Array to support multiple choices (view to provide choice when size > 1)
+  ### added enumerations to EffectiveOnKinds with following definitions:
+  ###   first_of_this_month: event_on date first day
+  ###   first_of_next_month: event_on date first day of following month
+  ## populated reason code in some instances -- use to call class method for business rules when present
+  ## ordinal_position -- set and use these values to determine sort order in view
+  ## added event_on -- property to store date on which QLE event occured
+  ## added event_kind_label -- use to populate label for collecting event_on date
+  ## renamed property: kind to action_kind (also renamed associated constant)
+
+
+  ACTION_KINDS = %w[add_benefit add_member drop_member change_benefit terminate_benefit administrative]
   MarketKinds = %w[shop individual]
-  EffectiveOnKinds = %w(date_of_event first_of_month)
+
+  # first_of_next_month: not subject to 15th of month effective date rule
+  EffectiveOnKinds = %w(date_of_event first_of_month first_of_this_month first_of_next_month)
 
   Reasons = [
     "initial_enrollment",
@@ -19,10 +33,11 @@ class QualifyingLifeEventKind
     "location_change",
     "termination_of_benefits",
     "termination_of_employment",
+    "employee_gaining_medicare",
     "immigration_status_change",
     "enrollment_error_or_misconduct_hbx",
     "enrollment_error_or_misconduct_issuer",
-    "enrollment_error_or_misconduct_non_hbx",
+    "enrollment_error_or_misconduct_non_hbx", 
     "contract_violation",
     "eligibility_change_medicaid_ineligible",
     "eligibility_change_assistance",
@@ -37,33 +52,37 @@ class QualifyingLifeEventKind
     "exceptional_circumstances"
   ]
 
+  field :event_kind_label, type: String
+  field :action_kind, type: String
+
   field :title, type: String
-  field :kind, type: String
-  field :effective_on_kind, type: String
+  field :action_kind, type: String
+  field :effective_on_kinds, type: Array, default: []
   field :reason, type: String
   field :edi_code, type: String
   field :market_kind, type: String
-  field :description, type: String
+  field :tool_tip, type: String
   field :pre_event_sep_in_days, type: Integer
   field :is_self_attested, type: Mongoid::Boolean
   field :post_event_sep_in_days, type: Integer
   field :ordinal_position, type: Integer
 
   field :is_active, type: Boolean, default: true
+  field :event_on, type: Date
+  field :coverage_effective_on, type: Date
   field :start_on, type: Date 
   field :end_on, type: Date
 
 
-  index({kind:  1})
-  index({market:  1})
-  index({sep_start_date:  1})
-  index({sep_end_date:  1})
+  index({action_kind: 1})
+  index({market: 1, ordinal_position: 1 })
+  index({start_on: 1, end_on: 1})
 
-  validates :effective_on_kind, 
-            presence: true,
-            allow_blank: false,
-            allow_nil:   false,
-            inclusion: {in: EffectiveOnKinds}
+  # validates :effective_on_kinds, 
+  #           presence: true,
+  #           allow_blank: false,
+  #           allow_nil:   false,
+  #           inclusion: {in: EffectiveOnKinds}
 
   validates :market_kind, 
             presence: true,
@@ -73,8 +92,48 @@ class QualifyingLifeEventKind
 
   # before_create :activate_household_sep
   # before_save :activate_household_sep
-  validates_presence_of :title, :market_kind, :effective_on_kind, :pre_event_sep_in_days,
+  validates_presence_of :title, :market_kind, :effective_on_kinds, :pre_event_sep_in_days,
                         :post_event_sep_in_days
+
+
+  # Business rules for EmployeeGainingMedicare
+  # If coverage ends on last day of month and plan selected before loss of coverage: 
+  #   effective date is first day of the month after other coverage will end
+  # If coverage ends on last day of month and plan selected after loss of other coverage: 
+  #   effective date is first day of the month following plan selection (not following 15th of month rule)
+  # If coverage ends on date other than last day of the month and plan selected during or after the month in which coverage ends: 
+  #   effective date is 1st of the month following plan selection
+  # If coverage ends on date other than last day of month and plan selected before the month in which coverage ends: 
+  #   effective date is consumer chooses between 1st of the month when coverage ends (allowing overlap - but no APTC for overlapping month) 
+  #   or the first of the month following the month when coverage ends
+  def employee_gaining_medicare(consumer_coverage_effective_on = nil)
+    coverage_end_last_day_of_month = Date.new(event_on.year, event_on.month, event_on.end_of_month)
+    if event_on == coverage_end_last_day_of_month
+      if TimeKeeper.date_of_record <= event_on
+        coverage_effective_on = coverage_end_last_day_of_month + 1.day
+      else
+        coverage_effective_on = TimeKeeper.date_of_record.end_of_month + 1.day
+      end
+    else
+      if TimeKeeper.date_of_record < (coverage_end_last_day_of_month - 1.month).end_of_month
+
+      else
+        coverage_effective_on = TimeKeeper.date_of_record.end_of_month + 1.day
+      end
+        if (consumer_coverage_effective_on >= event_on.first_of_month) && 
+           (consumer_coverage_effective_on <= (event_on.first_of_month + 1.month))
+          coverage_effective_on = consumer_coverage_effective_on
+        else
+          # raise invalid effective date error
+        end
+    end
+  end
+
+  # Business rules for MoveToState
+  ## The effective date should be the first day of the month following plan selection, but no earlier than the date of the move
+  def move_to_state(move_date)
+    # raise_error if move_date > TimeKeeper.date_of_record.end_of_month + 1.day
+  end
 
 
   class << self
--- db/seedfiles/qualifying_life_event_kinds_seed.rb
+++ db/seedfiles/qualifying_life_event_kinds_seed.rb
@@ -4,199 +4,199 @@ QualifyingLifeEventKind.delete_all
 
 QualifyingLifeEventKind.create!(
     title: "I've started a new job", 
-    kind: "add",
+    action_kind: "add_benefit",
     reason: " ",
     edi_code: "28-INITIAL ENROLLMENT", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 10,
-    description: "Enroll due to becoming newly eligibile"
+    tool_tip: "Enroll due to becoming newly eligibile"
   )
 
 
 QualifyingLifeEventKind.create!(
     title: "I've married", 
-    kind: "add",
+    action_kind: "add_benefit",
     reason: " ",
     edi_code: "32-MARRIAGE", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 15,
-    description: "Enroll or add a family member because of marriage"
+    tool_tip: "Enroll or add a family member because of marriage"
   )
 
 QualifyingLifeEventKind.create!(
     title: "I've entered into a legal domestic partnership", 
-    kind: "add",
+    action_kind: "add_benefit",
     reason: " ",
     edi_code: "33-ENTERING DOMESTIC PARTNERSHIP", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 20,
-    description: "Enroll or add a family member due to a new domestic partnership"
+    tool_tip: "Enroll or add a family member due to a new domestic partnership"
   )
 
 QualifyingLifeEventKind.create!(
     title: "I've had a baby", 
-    kind: "add",
+    action_kind: "add_benefit",
     reason: " ",
     edi_code: "02-BIRTH", 
     market_kind: "shop", 
-    effective_on_kind: "date_of_event",
+    effective_on_kinds: ["date_of_event"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 25,
-    description: "Enroll or add a family member due to birth"
+    tool_tip: "Enroll or add a family member due to birth"
   )
 
 QualifyingLifeEventKind.create!(
     title: "I've adopted a child", 
-    kind: "add",
+    action_kind: "add_benefit",
     reason: " ",
     edi_code: "05-ADOPTION", 
     market_kind: "shop", 
-    effective_on_kind: "date_of_event",
+    effective_on_kinds: ["date_of_event"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 30,
-    description: "Enroll or add a family member due to adoption"
+    tool_tip: "Enroll or add a family member due to adoption"
   )
 
 QualifyingLifeEventKind.create!(
     title: "Myself or a family member has lost other coverage", 
-    kind: "add",
+    action_kind: "add_benefit",
     reason: " ",
     edi_code: "33-LOST ACCESS TO MEC", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 35,
-    description: "Enroll or add a family member due to loss of eligibility for other coverage"
+    tool_tip: "Enroll or add a family member due to loss of eligibility for other coverage"
   )
 
 QualifyingLifeEventKind.create!(
     title: "I've divorced", 
-    kind: "drop",
+    action_kind: "drop_member",
     reason: " ",
     edi_code: "01-DIVORCE", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 40,
-    description: "Remove a family member due to divorce"
+    tool_tip: "Remove a family member due to divorce"
   )
 
 QualifyingLifeEventKind.create!(
     title: "A family member has died", 
-    kind: "drop",
+    action_kind: "drop_member",
     reason: " ",
     edi_code: "03-DEATH", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 45,
-    description: "Remove a family member due to death"
+    tool_tip: "Remove a family member due to death"
   )
 
 QualifyingLifeEventKind.create!(
     title: "My child has lost coverage due to age", 
-    kind: "drop",
+    action_kind: "drop_member",
     reason: "child_age_off",
     edi_code: "33-CHILD AGE OFF", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 50,
-    description: "Remove a child who is no longer eligible due to turning age 26"
+    tool_tip: "Remove a child who is no longer eligible due to turning age 26"
   )
 
 QualifyingLifeEventKind.create!(
     title: "drop_self_due_to_new_eligibility", 
-    kind: "drop",
+    action_kind: "drop_member",
     reason: "terminate_benefit",
     edi_code: "07-TERMINATION OF BENEFITS", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 55,
-    description: "Drop coverage for myself or family member due to new eligibility for other coverage"
+    tool_tip: "Drop coverage for myself or family member due to new eligibility for other coverage"
   )
 
 QualifyingLifeEventKind.create!(
     title: "drop_family_member_due_to_new_elgibility", 
-    kind: "drop",
+    action_kind: "drop_member",
     reason: "drop_family_member_due_to_new_elgibility",
     edi_code: "07-TERMINATION OF BENEFITS", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: true, 
     ordinal_position: 60,
-    description: "Drop coverage for a family member due to their new eligibility for other coverage"
+    tool_tip: "Drop coverage for a family member due to their new eligibility for other coverage"
   )
 
 QualifyingLifeEventKind.create!(
     title: "I've moved", 
-    kind: "administrative",
+    action_kind: "administrative",
     reason: "relocate",
     edi_code: "43-CHANGE OF LOCATION", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: false, 
     ordinal_position: 65,
-    description: "Drop coverage due to a permanent move outside of my current plan's service area"
+    tool_tip: "Drop coverage due to a permanent move outside of my current plan's service area"
   )
 
 QualifyingLifeEventKind.create!(
     title: "exceptional_circumstances", 
-    kind: "administrative",
+    action_kind: "administrative",
     reason: "exceptional_circumstances",
     edi_code: "EX-EXCEPTIONAL CIRCUMSTANCES", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: false, 
     ordinal_position: 70,
-    description: "Enroll due to an inadvertent or erroneous enrollment or another exceptional circumstance"
+    tool_tip: "Enroll due to an inadvertent or erroneous enrollment or another exceptional circumstance"
   )
 
 QualifyingLifeEventKind.create!(
     title: "contract_violation", 
-    kind: "administrative",
+    action_kind: "administrative",
     reason: "contract_violation",
     edi_code: "33-CONTRACT VIOLATION", 
     market_kind: "shop", 
-    effective_on_kind: "first_of_month",
+    effective_on_kinds: ["first_of_month"],
     pre_event_sep_in_days: 0,
     post_event_sep_in_days: 30, 
     is_self_attested: false, 
     ordinal_position: 75,
-    description: "Enroll due to contract violation"
+    tool_tip: "Enroll due to contract violation"
   )
 
 puts "::: QualifyingLifeEventKinds Complete :::"
