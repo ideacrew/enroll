86dc979e933a97a565cfacc5dbe237ebc9add2f7
Trey Evans <lewis.r.evans@gmail.com>
2015-08-27 16:17:00 -0400

    Cache translations.

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,2 +1,5 @@
 require 'mongo_i18n'
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(MongoI18n.store), I18n.backend)
+I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
+I18n::Backend::KeyValue.send(:include, I18n::Backend::Cache)
+I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
