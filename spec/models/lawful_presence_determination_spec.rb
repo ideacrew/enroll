a60140577554dc02eb37679f4b6282ddfc2c1c7d
Trey Evans <lewis.r.evans@gmail.com>
2015-08-31 13:11:41 -0400

    Fixes #2398. Fixed persisting of responses.
    Needed correction for name of attribute and proper embedded_in
    statements.
    
    Fixes #2398.

---
+++ spec/models/lawful_presence_determination_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe LawfulPresenceDetermination do
+  let(:consumer_role) {
+    FactoryGirl.create(:consumer_role_object)
+  }
+  let(:person_id) { consumer_role.person.id }
+  let(:payload) { "lsjdfioennnklsjdfe" }
+
+  describe "being given an ssa response which fails" do
+    it "should have the ssa response document" do
+      consumer_role.lawful_presence_determination.ssa_responses << EventResponse.new({received_at: Time.now, body: payload})
+      consumer_role.person.save!
+      found_person = Person.find(person_id)
+      ssa_response = found_person.consumer_role.lawful_presence_determination.ssa_responses.first
+      expect(ssa_response.body).to eq payload
+    end
+  end
+end
+
