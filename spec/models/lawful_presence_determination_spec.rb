cf290eddc83ee88d2a1fd10207804f5abe5c72fe
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-31 14:31:04 -0400

    describe() text and var name from ssn to vlp

--- spec/models/lawful_presence_determination_spec.rb
+++ spec/models/lawful_presence_determination_spec.rb
@@ -17,13 +17,13 @@ describe LawfulPresenceDetermination do
     end
   end
 
-  describe "being given an ssa response which fails" do
-    it "should have the ssa response document" do
+  describe "being given an vlp response which fails" do
+    it "should have the vlp response document" do
       consumer_role.lawful_presence_determination.vlp_responses << EventResponse.new({received_at: Time.now, body: payload})
       consumer_role.person.save!
       found_person = Person.find(person_id)
-      ssa_response = found_person.consumer_role.lawful_presence_determination.vlp_responses.first
-      expect(ssa_response.body).to eq payload
+      vlp_response = found_person.consumer_role.lawful_presence_determination.vlp_responses.first
+      expect(vlp_response.body).to eq payload
     end
   end
 end
