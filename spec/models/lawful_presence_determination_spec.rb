bfe6cfa49126227c814443843a7b2f5e7cb32039
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-08-31 14:27:54 -0400

    saving responses with << instead of .build() added specs

--- spec/models/lawful_presence_determination_spec.rb
+++ spec/models/lawful_presence_determination_spec.rb
@@ -16,5 +16,15 @@ describe LawfulPresenceDetermination do
       expect(ssa_response.body).to eq payload
     end
   end
+
+  describe "being given an ssa response which fails" do
+    it "should have the ssa response document" do
+      consumer_role.lawful_presence_determination.vlp_responses << EventResponse.new({received_at: Time.now, body: payload})
+      consumer_role.person.save!
+      found_person = Person.find(person_id)
+      ssa_response = found_person.consumer_role.lawful_presence_determination.vlp_responses.first
+      expect(ssa_response.body).to eq payload
+    end
+  end
 end
 
