146be60936fd1b141cdc783f56f4b4d9ac6af257
vinner <vineelsonnathi@users.noreply.github.com>
2015-08-30 22:58:36 -0400

    ridp

--- config/routes.rb
+++ config/routes.rb
@@ -221,6 +221,7 @@ Rails.application.routes.draw do
     resources :consumer_role, controller: 'consumer_roles' do
       get :search, on: :collection
       get :match, on: :collection
+      get :ridp_agreement, on: :collection
     end
   end
 
