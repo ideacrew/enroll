2f7401044a01f0ce23d12538cce93232cedbcedf
Trey Evans <lewis.r.evans@gmail.com>
2015-09-03 12:42:46 -0400

    Stubs for hbx_enrollment verification.
    There are now stub behaviours for hbx_enrollment verification.  Some work on the
    hbx_enrollment state machine is probably needed to correctly transition
    when verification events are received.

--- app/models/rule_set/hbx_enrollment/individual_market_verification.rb
+++ app/models/rule_set/hbx_enrollment/individual_market_verification.rb
@@ -16,9 +16,6 @@ module RuleSet
       end
 
       def determine_next_state
-        return(:move_to_pending!) if roles_for_determination.any?(&:verifications_pending?)
-        return(:move_to_contingent!) if roles_for_determination.any?(&:verifications_outstanding?)
-        :move_to_enrolled!
       end
     end
   end
