d80ba9de0f5a98a611bf8fc28f50211fd8d4444c
Ken Lu <ken@seeice.com>
2015-09-10 06:16:40 -0400

    Refs #2453 - change the error message of qle

--- spec/views/insured/families/_qle_detail.html.erb_spec.rb
+++ spec/views/insured/families/_qle_detail.html.erb_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe "insured/families/_qle_detail.html.erb" do
   it 'should have a hidden area' do
     expect(rendered).to have_selector('#qle-details.hidden')
   end
+
+  it "should have error message" do
+    expect(rendered).to have_content "Based on the information you entered, you may be eligible for a special enrollment period. Please call us at 1-855-532-5465 to give us more information so we can see if you qualify."
+  end
 end
