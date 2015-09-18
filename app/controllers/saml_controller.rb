8404d30834f4c6e018034976adb38808d96cb1ec
Kevin Wei <kevin.tw.wei@gmail.com>
2015-09-18 10:50:28 -0400

    Refs #316 kill devise session if user somehow gets to saml with one

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -10,6 +10,8 @@ class SamlController < ApplicationController
     response          = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
     response.settings = saml_settings
 
+    sign_out current_user if current_user.present?
+
     if response.is_valid?
       email = response.attributes['mail'].downcase
 
