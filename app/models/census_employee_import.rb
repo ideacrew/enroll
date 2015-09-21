7104d2c45de5020ff8da3d82c2458f136c0d9283
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-09-21 18:08:11 -0400

    Refs #2749 assigning benefit group to census_employees

--- app/models/census_employee_import.rb
+++ app/models/census_employee_import.rb
@@ -1,5 +1,5 @@
 class CensusEmployeeImport
-  extend  ActiveModel::Naming
+  extend ActiveModel::Naming
   include ActiveModel::Conversion
   include ActiveModel::Validations
 
@@ -27,6 +27,7 @@ class CensusEmployeeImport
       termination_date
       is_business_owner
       benefit_group
+      plan_year
     )
 
   CENSUS_MEMBER_RECORD_TITLES = [
@@ -43,8 +44,9 @@ class CensusEmployeeImport
       "Date of Hire",
       "Date of Termination(optional)",
       "Is Business Owner?",
-      "Benefit Group(optional)"
-    ]
+      "Benefit Group(optional)",
+      "Plan Year(Optional)"
+  ]
 
   def initialize(attributes = {})
     attributes.each { |name, value| send("#{name}=", value) }
@@ -70,12 +72,12 @@ class CensusEmployeeImport
 
     # To match spreadsheet convention, Roo gem uses 1-based (rather than 0-based) references 
     # First three rows are header content
-    sheet_header_row  = @sheet.row(1)
+    sheet_header_row = @sheet.row(1)
     column_header_row = @sheet.row(2)
     # label_header_row  = @sheet.row(3)
 
     unless header_valid?(sheet_header_row) && column_header_valid?(column_header_row)
-      raise "Unrecognized Employee Census spreadsheet format. Contact DC Health Link for current template." 
+      raise "Unrecognized Employee Census spreadsheet format. Contact DC Health Link for current template."
     end
 
     census_employees = []
@@ -84,7 +86,6 @@ class CensusEmployeeImport
       record = parse_row(row)
       break if record[:employer_assigned_family_id].nil?
       if record[:termination_date].present?
-        debugger
         census_employee = terminate_employee(record)
       else
         census_employee = add_or_update_census_member(record)
@@ -138,43 +139,58 @@ class CensusEmployeeImport
       member.is_business_owner = true
     end
     member.gender = record[:gender].to_s if record[:gender]
-    member.email = Email.new({address:record[:email].to_s, kind:"home"}) if record[:email]
+    member.email = Email.new({address: record[:email].to_s, kind: "home"}) if record[:email]
     member.employee_relationship = record[:employee_relationship].to_s if record[:employee_relationship]
     member.employer_profile = @employer_profile
+    assign_benefit_group(member, record[:benefit_group], record[:plan_year])
     member
+  end
+
+  def assign_benefit_group(member, benefit_group, plan_year)
+    plan_year_found = @employer_profile.plan_years.detect do |py|
+      py.start_on.year.to_s == plan_year
+    end
+    return if plan_year_found.nil?
+    benefit_group_found = plan_year_found.benefit_groups.detect do |bg|
+      bg.title == benefit_group
+    end
+    return if benefit_group_found.nil?
+    member.benefit_group_assignments << BenefitGroupAssignment.new({benefit_group_id: benefit_group_found.id , start_on: plan_year_found.start_on})
   end
 
   def parse_row(row)
     employer_assigned_family_id = parse_text(row["employer_assigned_family_id"])
     employee_relationship = parse_relationship(row["employee_relationship"])
-    last_name             = parse_text(row["last_name"])
-    first_name            = parse_text(row["first_name"])
-    middle_initial        = parse_text(row["middle_initial"])
-    name_sfx              = parse_text(row["name_sfx"])
-    email                 = parse_text(row["email"])
-    ssn                   = parse_ssn(row["ssn"])
-    dob                   = parse_date(row["dob"])
-    gender                = parse_text(row["gender"])
-    hire_date             = parse_date(row["hire_date"])
-    termination_date      = parse_date(row["termination_date"])
-    is_business_owner     = parse_boolean(row["is_business_owner"])
-    benefit_group         = parse_text(row["benefit_group"])
+    last_name = parse_text(row["last_name"])
+    first_name = parse_text(row["first_name"])
+    middle_initial = parse_text(row["middle_initial"])
+    name_sfx = parse_text(row["name_sfx"])
+    email = parse_text(row["email"])
+    ssn = parse_ssn(row["ssn"])
+    dob = parse_date(row["dob"])
+    gender = parse_text(row["gender"])
+    hire_date = parse_date(row["hire_date"])
+    termination_date = parse_date(row["termination_date"])
+    is_business_owner = parse_boolean(row["is_business_owner"])
+    benefit_group = parse_text(row["benefit_group"])
+    plan_year = parse_text(row["plan_year"])
 
-    { 
-      employer_assigned_family_id: employer_assigned_family_id,
-      employee_relationship: employee_relationship,
-      last_name: last_name,
-      first_name: first_name,
-      middle_initial: middle_initial,
-      name_sfx: name_sfx,
-      email: email,
-      ssn: ssn,
-      dob: dob,
-      gender: gender,
-      hire_date: hire_date,
-      termination_date: termination_date,
-      is_business_owner: is_business_owner,
-      benefit_group: benefit_group,
+    {
+        employer_assigned_family_id: employer_assigned_family_id,
+        employee_relationship: employee_relationship,
+        last_name: last_name,
+        first_name: first_name,
+        middle_initial: middle_initial,
+        name_sfx: name_sfx,
+        email: email,
+        ssn: ssn,
+        dob: dob,
+        gender: gender,
+        hire_date: hire_date,
+        termination_date: termination_date,
+        is_business_owner: is_business_owner,
+        benefit_group: benefit_group,
+        plan_year: plan_year
     }
   end
 
@@ -196,20 +212,20 @@ class CensusEmployeeImport
     # defined? @last_employer_assigned_family_id ? 
     return nil if cell.blank?
     field_map = case parse_text(cell).downcase
-      when "employee"
-        "self"
-      when "spouse"
-        "spouse"
-      when "domestic partner"
-       "domestic_partner"
-      when "child"
-        "child"
-      when "disabled child"
-        "disabled_child"
-      else
-        nil
-    end
-     
+                  when "employee"
+                    "self"
+                  when "spouse"
+                    "spouse"
+                  when "domestic partner"
+                    "domestic_partner"
+                  when "child"
+                    "child"
+                  when "disabled child"
+                    "disabled_child"
+                  else
+                    nil
+                end
+
     field_map
   end
 
@@ -262,10 +278,14 @@ class CensusEmployeeImport
 
   def open_spreadsheet
     case File.extname(file.original_filename)
-      when ".csv" then Csv.new(file.path, nil, :ignore)
-      when ".xls" then Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Excelx.new(file.path, nil, :ignore)
-      else raise "Unknown file type: #{file.original_filename}"
+      when ".csv" then
+        Csv.new(file.path, nil, :ignore)
+      when ".xls" then
+        Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then
+        Excelx.new(file.path, nil, :ignore)
+      else
+        raise "Unknown file type: #{file.original_filename}"
     end
   end
 
@@ -275,7 +295,7 @@ class CensusEmployeeImport
 
   alias_method :count, :length
 
-private
+  private
   def sanitize_value(value)
     value = value.to_s.split('.')[0] if value.is_a? Float
     value.gsub(/[[:cntrl:]]|^[\p{Space}]+|[\p{Space}]+$/, '')
@@ -283,6 +303,8 @@ private
 
 end
 
-class ImportErrorValue < Exception; end
-class ImportErrorDate < Exception; end
+class ImportErrorValue < Exception;
+end
+class ImportErrorDate < Exception;
+end
 
