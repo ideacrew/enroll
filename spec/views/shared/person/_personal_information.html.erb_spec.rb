e218a979f6e4e41ae1ab92a00aabf4b9b96fab49
sullivanm <sullivancoder@gmail.com>
2015-09-26 14:00:27 -0700

    Fixes #2800:Change "Birthdate" to "Date of Birth"

--- spec/views/shared/person/_personal_information.html.erb_spec.rb
+++ spec/views/shared/person/_personal_information.html.erb_spec.rb
@@ -22,7 +22,7 @@ describe "shared/person/_personal_information.html.erb" do
   it "should have required input fields with asterisk" do
     expect(rendered).to have_selector('input[placeholder="FIRST NAME *"]')
     expect(rendered).to have_selector('input[placeholder="LAST NAME *"]')
-    expect(rendered).to have_selector('input[placeholder="BIRTHDATE *"]')
+    expect(rendered).to have_selector('input[placeholder="Date of Birth *"]')
     expect(rendered).to have_selector('input[placeholder="SOCIAL SECURITY *"]')
   end
 end
