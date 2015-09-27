87e3cd1df8eae3cfcb3ec85291e511a964dfad60
Ken Lu <ken@seeice.com>
2015-09-27 05:23:21 -0400

    Refs #1904 - Ability to export Plan Compare Pop-Up Window in Plan Shopping

--- app/controllers/products/qhp_controller.rb
+++ app/controllers/products/qhp_controller.rb
@@ -4,6 +4,10 @@ class Products::QhpController < ApplicationController
   def comparison
     params.permit("standard_component_ids", :hbx_enrollment_id)
     found_params = params["standard_component_ids"].map { |str| str[0..13] }
+
+    @standard_component_ids = params[:standard_component_ids]
+    @hbx_enrollment_id = params[:hbx_enrollment_id]
+
     if @market_kind == 'employer_sponsored' and @coverage_kind == 'health'
       @benefit_group = @hbx_enrollment.benefit_group
       @reference_plan = @benefit_group.reference_plan
@@ -25,8 +29,12 @@ class Products::QhpController < ApplicationController
     respond_to do |format|
       format.html
       format.js
+      format.csv do
+        send_data(csv_for(@qhps), type: csv_content_type, filename: "comparsion_plans.csv")
+      end
     end
   end
+
 
   def summary
     sc_id = @new_params[:standard_component_id][0..13]
@@ -52,5 +60,63 @@ class Products::QhpController < ApplicationController
     params[:market_kind] = params[:market_kind] == "shop" ? @hbx_enrollment.kind : params[:market_kind]
     @market_kind = params[:market_kind].present? ? params[:market_kind] : 'employer_sponsored'
     @coverage_kind = params[:coverage_kind].present? ? params[:coverage_kind] : 'health'
+  end
+
+  def csv_for(qhps)
+    (output = "").tap do 
+      CSV.generate(output) do |csv|
+        csv_ary = []
+        csv_ary << ["Carrier", "Plan Name", "Your Cost", "Provider NetWork", "Plan Benefits"] + Products::Qhp::VISIT_TYPES
+        qhps.each do |qhp|
+          arry1 = [
+            qhp.plan.carrier_profile.organization.legal_name,
+            qhp.plan_marketing_name, 
+            "$#{qhp[:total_employee_cost].round(2)}", 
+            qhp.plan.nationwide ? "Nationwide" : "DC Area Network",
+            "In Network"
+          ]
+          arry2 = [
+            "","","","","Out of Network"
+          ]
+          Products::Qhp::VISIT_TYPES.each do |visit_type|
+            matching_benefit = qhp.qhp_benefits.detect { |qb| qb.benefit_type_code == visit_type } 
+            if matching_benefit 
+              deductible = matching_benefit.find_deductible 
+              arry1 << deductible.copay_in_network_tier_1 
+              arry2 << deductible.copay_out_of_network
+            end 
+          end
+          csv_ary << arry1
+          csv_ary << arry2
+        end
+
+        inversion = convert_csv(csv_ary)
+        inversion.each do |row|
+          csv << row
+        end
+      end
+    end
+  end
+
+  def convert_csv(arr)
+    row = arr.count
+    column = arr.first.count
+
+    inversion = Array.new(column){ Array.new(row, 0)}
+    row.times do |r|
+      column.times do |c|
+        inversion[c][r] = arr[r][c]
+      end
+    end
+    inversion
+  end
+
+  def csv_content_type
+    case request.user_agent
+      when /windows/i 
+        'application/vnd.ms-excel'
+      else
+        'text/csv'
+    end
   end
 end
