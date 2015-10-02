f6f74668e146a4b0f96be628105528b09e1044b2
Kevin Wei <kevin.tw.wei@gmail.com>
2015-10-02 16:08:34 -0400

    Refs #2568 deal with multiple copies of the same payload from family application completed

--- spec/models/subscribers/family_application_completed_spec.rb
+++ spec/models/subscribers/family_application_completed_spec.rb
@@ -134,9 +134,9 @@ describe Subscribers::FamilyApplicationCompleted do
 
       context "with the xml person built by EnrollmentFactory family logging no errors" do
         let(:message) { { "body" => xml } }
-        let(:xml) { File.read(Rails.root.join("spec", "test_data", "verified_family_payloads", "valid_verified_family_with_multiple_tax_filers_sample.xml")) }
+        let(:xml) { File.read(Rails.root.join("spec", "test_data", "verified_family_payloads", "valid_verified_family_single_dup_payload_sample.xml")) }
         let(:user) { FactoryGirl.create(:user) }
-        let(:parser) { Parsers::Xml::Cv::VerifiedFamilyParser.new.parse(File.read(Rails.root.join("spec", "test_data", "verified_family_payloads", "valid_verified_family_with_multiple_tax_filers_sample.xml"))).first }
+        let(:parser) { Parsers::Xml::Cv::VerifiedFamilyParser.new.parse(File.read(Rails.root.join("spec", "test_data", "verified_family_payloads", "valid_verified_family_single_dup_payload_sample.xml"))).first }
         let(:primary) { parser.family_members.detect{ |fm| fm.id == parser.primary_family_member_id } }
         let(:person) { consumer_role.person }
         let(:ua_params) do
@@ -162,20 +162,20 @@ describe Subscribers::FamilyApplicationCompleted do
         let(:tax_household) { person.primary_family.active_household.tax_households.first }
 
         before do
-          person.save!
-          person.primary_family.update_attribute(:e_case_id, "curam_landing_for#{person.id}")
-          expect(subject).not_to receive(:log)
           allow(HbxProfile).to receive(:current_hbx).and_return(hbx_profile_organization)
-          subject.call(nil, nil, nil, nil, message)
         end
 
         it "builds a default taxhousehold with primary person as primary applicant and updates consumer role to fully vlp verified" do
+          person.save!
+          person.primary_family.update_attribute(:e_case_id, "curam_landing_for#{person.id}")
+          expect(subject).not_to receive(:log)
+          subject.call(nil, nil, nil, nil, message)
           expect(tax_household).to be_truthy
           expect(tax_household).to eq person.primary_family.active_household.latest_active_tax_household
           expect(tax_household.primary_applicant.family_member.person).to eq person
           expect(tax_household.allocated_aptc).to eq 0
           expect(tax_household.is_eligibility_determined).to be_truthy
-          expect(tax_household.eligibility_determinations.last.max_aptc).to eq 0
+          expect(tax_household.eligibility_determinations.last.max_aptc).to eq 269
           consumer_role.reload
           expect(consumer_role.fully_verified?).to be_truthy
           expect(consumer_role.vlp_authority).to eq "curam"
@@ -186,8 +186,19 @@ describe Subscribers::FamilyApplicationCompleted do
           expect(consumer_role.is_incarcerated).to eq primary.person_demographics.is_incarcerated
           expect(person.addresses).to be_truthy
         end
-      end
 
+        it "can recieve duplicate payloads and simply creates a new taxhousehold and deactivating the old one" do
+          expect(subject).not_to receive(:log)
+          subject.call(nil, nil, nil, nil, message)
+          latest_active_tax_household = person.primary_family.active_household.latest_active_tax_household
+          expect(person.primary_family.active_household.tax_households.select{|th| th.effective_ending_on.present? }).to be_truthy
+          expect(latest_active_tax_household).to be_truthy
+          expect(latest_active_tax_household.primary_applicant.family_member.person).to eq person
+          expect(latest_active_tax_household.allocated_aptc).to eq 0
+          expect(latest_active_tax_household.is_eligibility_determined).to be_truthy
+          expect(latest_active_tax_household.eligibility_determinations.last.max_aptc).to eq 269
+        end
+      end
     end
   end
 end
