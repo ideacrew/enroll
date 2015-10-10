759b9d656df3c572449a8ff1289ba8ca96e3f456
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-09 19:19:54 -0500

    replace all references to dchl customer service link with class constant

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -473,7 +473,7 @@ module ApplicationHelper
   def incarceration_cannot_purchase(family_member)
     pronoun = family_member.try(:gender)=='male' ? ' he ':' she '
     name=family_member.try(:first_name) || ''
-    result = "Since " + name + " is currently incarcerated," + pronoun + "is not eligible to purchase a plan on DC Health Link.<br/> Other family members may still be eligible to enroll. <br/>Please call us at 1-855-532-5465 to learn about other health insurance options for " + name
+    result = "Since " + name + " is currently incarcerated," + pronoun + "is not eligible to purchase a plan on DC Health Link.<br/> Other family members may still be eligible to enroll. <br/>Please call us at <%= HbxProfile::CallCenterPhoneNumber %> to learn about other health insurance options for " + name
   end
 
   def generate_options_for_effective_on_kinds(effective_on_kinds, qle_date)
