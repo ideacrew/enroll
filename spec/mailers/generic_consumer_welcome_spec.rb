50b614fbc1b7f5101dd69d04a567c75efeb4a3db
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-09 20:40:35 -0500

    swap out all references to dchl customer services customer care and call center number with class constants

--- spec/mailers/generic_consumer_welcome_spec.rb
+++ spec/mailers/generic_consumer_welcome_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 RSpec.describe UserMailer do
   describe 'generic_consumer_welcome' do
     let(:hbx_id) { rand(10000 )}
     let(:email){UserMailer.generic_consumer_welcome('john', hbx_id, 'john@dc.gov')}
-   
+
     it 'should not allow a reply' do
     	expect(email.from).to match(["no-reply@individual.dchealthlink.com"])
     end
@@ -18,12 +18,10 @@ RSpec.describe UserMailer do
       expect(email.subject).to match(/DC HealthLink/)
     end
 
-    it 'should have body text' do 
+    it 'should have body text' do
       expect(email.body).to match(/DC Health Link is strongly committed/)
       expect(email.body).to match(/Your Account/)
       expect(email.body).to match(/Questions\?  Call DC Health Link Customer Service/)
     end
   end
 end
-
-
