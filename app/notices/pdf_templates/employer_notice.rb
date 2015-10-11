14191899113d70596268d926eea017e8fc3dc8d0
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-10-11 13:01:20 -0400

    Modifications to notices models for new shop notices

--- app/notices/pdf_templates/employer_notice.rb
+++ app/notices/pdf_templates/employer_notice.rb
@@ -12,6 +12,7 @@ module PdfTemplates
     attribute :hbe, PdfTemplates::Hbe
     attribute :open_enrollment_end_on, Date
     attribute :coverage_end_on, Date
+    attribute :coverage_start_on, Date
     attribute :to, String
     attribute :plan, PdfTemplates::Plan
 
