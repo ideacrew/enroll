d006b64fb25899a560bc82f5fdfbd652e2d3343d
Kevin Wei <kevin.tw.wei@gmail.com>
2015-10-11 00:23:38 -0400

    fix build

--- spec/views/insured/families/_shop_for_plans_widget.html.erb_spec.rb
+++ spec/views/insured/families/_shop_for_plans_widget.html.erb_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "insured/families/_shop_for_plans_widget.html.erb" do
   let(:employee_role) { double(id: '123') }
   let(:hbx_enrollments) {double}
   let!(:benefit_coverage_period) { FactoryGirl.create(:benefit_coverage_period, open_enrollment_start_on: TimeKeeper.date_of_record - 10.days, open_enrollment_end_on: TimeKeeper.date_of_record + 10.days) }
+  let(:user) { FactoryGirl.create(:user)}
 
 
   context "with hbx_enrollments" do
@@ -13,6 +14,7 @@ RSpec.describe "insured/families/_shop_for_plans_widget.html.erb" do
       assign :employee_role, employee_role
       assign :hbx_enrollments, hbx_enrollments
       render "insured/families/shop_for_plans_widget"
+      sign_in user
     end
 
     it 'should have title' do
@@ -48,24 +50,24 @@ RSpec.describe "insured/families/_shop_for_plans_widget.html.erb" do
     end
   end
 
-  context "action path" do 
+  context "action path" do
     before :each do
       assign :person, person
       assign :employee_role, employee_role
       assign :hbx_enrollments, []
     end
 
-    it "should action to new insured group selection path" do 
+    it "should action to new insured group selection path" do
       allow(employee_role).to receive(:is_under_open_enrollment?).and_return(true)
       allow(view).to receive(:is_under_open_enrollment?).and_return(true)
-      render "insured/families/shop_for_plans_widget" 
+      render "insured/families/shop_for_plans_widget"
       expect(rendered).to have_selector("form[action='/insured/group_selections/new']")
     end
 
     it "should action to find sep insured families path" do
       allow(employee_role).to receive(:is_under_open_enrollment?).and_return(false)
       allow(view).to receive(:is_under_open_enrollment?).and_return(false)
-      render "insured/families/shop_for_plans_widget" 
+      render "insured/families/shop_for_plans_widget"
 
       expect(rendered).to have_selector("form[action='/insured/families/find_sep']")
 
