4ff835407fd9d91b789e0b7b057de130b1d90578
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-12 11:38:14 -0500

    fix premium reports date range on employer dashboard

--- app/controllers/insured/families_controller.rb
+++ app/controllers/insured/families_controller.rb
@@ -5,7 +5,7 @@ class Insured::FamiliesController < FamiliesController
 
   def home
     set_bookmark_url
-    
+
     @hbx_enrollments = @family.enrolled_hbx_enrollments.active || []
     @waived = @family.coverage_waived?
     @employee_role = @person.employee_roles.try(:first)
