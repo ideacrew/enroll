051c76d20fa99ef43b8e80130f6f4f49bfd4a8dd
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-14 13:52:30 -0500

    restyle layout of benefit packages

--- app/assets/javascripts/benefits.js
+++ app/assets/javascripts/benefits.js
@@ -13,16 +13,26 @@ $.fn.toggleClick=function(){
 $('.benefit-package').mouseleave(function() {
   if ( $(this).find('.make-default').hasClass('active') ) {
   } else {
-    $(this).find('.make-default').hide();
+    // $(this).find('.make-default').hide();
   }
 });
 $('.benefit-package').mouseenter(function() {
   $(this).find('.make-default').show();
   $(this).find('.make-default').attr('data-toggle', 'tooltip');
-  $(this).find('.make-default').attr('data-original-title', 'Make this your default benefit group');
+  if ( $(this).find('.make-default').hasClass('active') ) {
+    $(this).find('.make-default').attr('data-original-title', 'This is your default benefit group');
+  } else {
+    $(this).find('.make-default').attr('data-original-title', 'Make this your default benefit group');
+  }
 
 
 });
+
+// make so only one star can be active
+$('.make-default').on('click', function() {
+  $('.make-default').removeClass('active');
+});
+
 // $('.benefit-package .make-default').mouseenter(function () {
 //   //replace code for css hover color change and use js
 // }
@@ -35,6 +45,19 @@ $('.benefit-package .make-default').toggleClick(function () {
   }, function () {
     $(this).removeClass('active');
   });
+
+
+  // toggle details in benefits only
+  $('.benefit-details').toggleClick(function () {
+    $(this).closest('.benefit-package').next().find('.plan-details').show();
+    $(this).html('Hide Details <i class="fa fa-chevron-up fa-lg"></i>');
+  }, function () {
+    $(this).closest('.benefit-package').next().find('.plan-details').hide();
+    $(this).html('View Details <i class="fa fa-chevron-down fa-lg"></i>');
+
+
+  });
+
 
 };
 
