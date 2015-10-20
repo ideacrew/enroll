450c2d31133b8cce917222c5fef0b07848f500b4
Trey Evans <lewis.r.evans@gmail.com>
2015-10-20 16:59:46 -0400

    Fixing sep detection for shop.

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -240,7 +240,7 @@ class HbxEnrollment
 
   def is_shop_sep?
     return false if consumer_role.present?
-    true
+    !("open_enrollment" == self.enrollment_kind)
   end
 
   def transmit_shop_enrollment!
