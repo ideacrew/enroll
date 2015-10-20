c213c631d35b183b8252dbda2343ed480fbad6de
Raghuram <raghuramg83@gmail.com>
2015-10-20 17:25:33 -0400

    Refs #3084 added scope to retreive enrollements

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -87,6 +87,8 @@ class HbxEnrollment
   scope :current_year, -> { where(:effective_on.gte => TimeKeeper.date_of_record.beginning_of_year, :effective_on.lte => TimeKeeper.date_of_record.end_of_year) }
   scope :enrolled, ->{ where(:aasm_state.in => ENROLLED_STATUSES ) }
 
+  scope :with_in, -> (time_limit){ where(:created_at.gte => time_limit) }
+
   embeds_many :hbx_enrollment_members
   accepts_nested_attributes_for :hbx_enrollment_members, reject_if: :all_blank, allow_destroy: true
 
