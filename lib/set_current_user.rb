2b87767869acb46684cb99a54c613ca080fcc8e4
Kevin Wei <kevin.tw.wei@gmail.com>
2015-10-20 14:13:36 -0400

    Hotfix #1718 specs

--- lib/set_current_user.rb
+++ lib/set_current_user.rb
@@ -5,7 +5,7 @@ module SetCurrentUser
   def save_user
     self.updated_by_id = SAVEUSER[:current_user_id]
   end
-  def self.included(klass) 
+  def self.included(klass)
     klass.before_save :save_user
     klass.before_update :update_user
     klass.field :updated_by_id, type: BSON::ObjectId
