b9a350ef820bdb1b113ea7c51778e7ca22459d92
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-23 10:25:41 -0500

    add button_continue to step 4 of ivl sep progress

--- spec/views/insured/families/_enrollment.html.erb_spec.rb
+++ spec/views/insured/families/_enrollment.html.erb_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "insured/families/_enrollment.html.erb" do
 
     it "should display the title" do
       expect(rendered).to match /#{plan.active_year} #{plan.coverage_kind} Coverage/
-      expect(rendered).to match /DCHL/
+      expect(rendered).to match /Individual & Family/
     end
 
     it "should display the link of view detail" do
