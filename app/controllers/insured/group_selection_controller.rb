8ef14991907610f46abcba1512993753352495c8
jfinucane <jimway@gmail.com>
2015-10-23 16:04:45 -0400

    Refs #3341 counts
    (cherry picked from commit c3a0bc2be3301b0fb6272f7587dc4abbdfbceb4b)

--- app/controllers/insured/group_selection_controller.rb
+++ app/controllers/insured/group_selection_controller.rb
@@ -50,6 +50,7 @@ class Insured::GroupSelectionController < ApplicationController
     end
 
     @family.hire_broker_agency(current_user.person.broker_role.try(:id))
+    
     hbx_enrollment.writing_agent_id = current_user.person.try(:broker_role).try(:id)
     hbx_enrollment.original_application_type = session[:original_application_type]
     broker_role = current_user.person.broker_role
