e75d3069bbefde7c624d63e47b560d7abe249269
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-25 15:17:10 -0500

    add new text for headers

--- app/helpers/portal_header_helper.rb
+++ app/helpers/portal_header_helper.rb
@@ -10,7 +10,7 @@ module PortalHeaderHelper
     elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
       agent_header
     elsif current_user.try(:has_employee_role?)
-        "#{image_tag 'icons/icon-individual.png'} &nbsp; I'm Shopping for Employee Health Insurance".html_safe
+        "#{image_tag 'icons/icon-individual.png'} &nbsp; I'm an Employee".html_safe
     elsif (controller_path.include?("insured") && current_user.try(:has_consumer_role?))
         "#{image_tag 'icons/icon-family.png'} &nbsp; Individual and Family".html_safe
     elsif current_user.try(:has_broker_agency_staff_role?)
@@ -31,33 +31,17 @@ module PortalHeaderHelper
     end
   end
   def agent_header
-    if current_user.person.assister_role
-    	agent = "I'm an In Person Assister"
-    elsif  current_user.person.csr_role.cac
-    	agent = "I'm a Certified Applicant Counselor"
-    else
-    	agent = "I'm a Customer Service Representative"
-    end
-    header_text = "<div style='display:inline-block'>&nbsp; #{agent}<br/>&nbsp; #{enrollment_name}</div>"
-    link_to "#{image_tag 'icons/icon-expert.png'}#{header_text}".html_safe,
-      home_exchanges_agents_path
+    header_text = "<div style='display:inline-block'>&nbsp; I'm a Trained Expert</div>"
+    link_to "#{image_tag 'icons/icon-expert.png'}#{header_text}".html_safe, home_exchanges_agents_path
   end
 
   def hbx_staff_header
-    if enrollment_name.present?
-    header_text = "<div style='display:inline-block; white-space: nowrap;'>&nbsp; I'm HBX Staff<br/>&nbsp; #{enrollment_name.try(:titleize)}</div>"
-    else
-    header_text = "<div style='display:inline-block; white-space: nowrap; position: relative; top: -5px;'>&nbsp; I'm HBX Staff</div>"
-    end
+    header_text = "<div style='display:inline-block; white-space: nowrap; position: relative; top: -5px;'>&nbsp; I'm an Admin</div>"
     link_to "#{image_tag 'icons/icon-exchange-admin.png', style: 'position: relative; top: -8px; left: 10px; margin-right: 5px;'} #{header_text}".html_safe, exchanges_hbx_profiles_root_path, style: 'padding: 16px 20px 0 0;'
   end
 
   def broker_header
-    if enrollment_name.present?
-    header_text = "<div style='display:inline-block white-space: nowrap;'>&nbsp; I'm a Broker<br/>&nbsp; #{enrollment_name.try(:titleize)}</div>"
-    else
-      header_text = "<div style='display:inline-block white-space: nowrap; position: relative; top: -5px;'>&nbsp; I'm a Broker<br/>&nbsp; #{enrollment_name.try(:titleize)}</div>"
-    end
+    header_text = "<div style='display:inline-block white-space: nowrap; position: relative; top: -5px;'>&nbsp; I'm a Broker</div>"
     link_to "#{image_tag 'icons/icon-expert.png', style: 'position: relative; top: -8px; left: 10px; margin-right: 5px;'} #{header_text}".html_safe, broker_agencies_profile_path(id: current_user.person.broker_role.broker_agency_profile_id), style: 'padding: 16px 20px 0 0;'
   end
 end
