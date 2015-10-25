b642209afec9e239c4791f62d4367a0f7da26f11
Dan Thomas <dan.thomas@dc.gov>
2015-10-25 18:52:11 -0400

    Add validation for csr_eligibility_kind Add spec to verify default values are set, eligibility kind changes when csr_percent changes

--- app/models/eligibility_determination.rb
+++ app/models/eligibility_determination.rb
@@ -2,18 +2,19 @@ class EligibilityDetermination
   include Mongoid::Document
   include SetCurrentUser
   include Mongoid::Timestamps
+  include HasFamilyMembers
 
   embedded_in :tax_household
 
-  # CSR subsidies reduce out-of-pocket expenses by raising the plan actuarial value 
-  #   (the average out-of-pocket costs an insurer pays on a plan) 
+  COST_SHARING_REDUCTION_KINDS = %w(csr_100 csr_94 csr_87 csr_73)
+
+  CSR_PERCENT_TO_PLAN_VARIANT_MAP = {
+      "csr_100": "01",
+      "csr_94":  "06",
+      "csr_87":  "05",
+      "csr_73":  "04",
   #   csr_0:   "02", # Native Americans
   #   limited: "03", # limited?
-  CSR_PERCENT_TO_PLAN_VARIANT_MAP = {
-      csr_100: "01",
-      csr_73:  "04",
-      csr_87:  "05",
-      csr_94:  "06"
     }
 
   field :e_pdc_id, type: String
@@ -22,16 +23,45 @@ class EligibilityDetermination
   # Premium tax credit assistance eligibility.
   # Available to household with income between 100% and 400% of the Federal Poverty Level (FPL)
   field :max_aptc, type: Money, default: 0.00
+  field :premium_credit_strategy_kind, type: String
 
-  # Cost-sharing reduction assistance eligibility for co-pays, etc.
+  # Cost-sharing reduction assistance subsidies reduce out-of-pocket expenses by raising 
+  #   the plan actuarial value (the average out-of-pocket costs an insurer pays on a plan) 
   # Available to households with income between 100-250% of FPL and enrolled in Silver plan.
   field :csr_percent_as_integer, type: Integer, default: 0  #values in DC: 0, 73, 87, 94
+  field :csr_eligibility_kind, type: String, default: "csr_100"
 
   field :determined_on, type: DateTime
 
   validates_presence_of :determined_on, :max_aptc, :csr_percent_as_integer
 
-  include HasFamilyMembers
+  validates :premium_credit_strategy_kind,
+    allow_blank: false,
+    inclusion: {
+      in: BenefitPackage::PREMIUM_CREDIT_STRATEGY_KINDS,
+      message: "%{value} is not a valid premium credit strategy kind"
+    }
+
+  validates :csr_eligibility_kind,
+    allow_blank: false,
+    inclusion: {
+      in: COST_SHARING_REDUCTION_KINDS,
+      message: "%{value} is not a valid cost sharing eligibility kind"
+    }
+
+  def csr_percent_as_integer=(new_csr_percent)
+    super
+    self.csr_eligibility_kind = case csr_percent_as_integer
+    when 73
+      "csr_73"
+    when 87
+      "csr_87"
+    when 94
+      "csr_94"
+    else
+      "csr_100"
+    end
+  end
 
   def family
     return nil unless tax_household
