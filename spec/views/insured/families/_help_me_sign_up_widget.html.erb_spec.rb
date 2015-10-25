8ecd2bbaa7265c49b2e8cec5ec3ab58e6563d306
Phillip Jacowski <phillipjacowski@gmail.com>
2015-10-25 12:54:30 -0500

    add header refinements for brokers and remove check for modal in help me sign up widget

--- spec/views/insured/families/_help_me_sign_up_widget.html.erb_spec.rb
+++ spec/views/insured/families/_help_me_sign_up_widget.html.erb_spec.rb
@@ -1,16 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "_help_me_sign_up_widget.html.erb_spec" do
+RSpec.describe "_help_signup.html.erb" do
   before :each do
-    render "insured/families/help_me_sign_up_widget.html.erb_spec"
+    render "insured/families/help_signup"
   end
   it "should have button with text 'Help Me Sign Up'" do
     expect(rendered).to have_content "Help Me Sign Up"
   end
-
-  it "should have hidden help with plan shoppping modal" do
-    expect(rendered).to have_selector("#help_with_plan_shopping")
-  end
-  debugger
 
 end
