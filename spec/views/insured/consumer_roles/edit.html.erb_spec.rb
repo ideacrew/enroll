6a3fbed1aa3eb191c65390c0b8f8a3e563f04e09
Tony Schaffert <antonio.schaffert@gmail.com>
2015-10-26 01:08:58 -0400

    Refs #3395 IVL Text Changes 1-5

--- spec/views/insured/consumer_roles/edit.html.erb_spec.rb
+++ spec/views/insured/consumer_roles/edit.html.erb_spec.rb
@@ -28,9 +28,9 @@ RSpec.describe "insured/consumer_roles/edit.html.erb" do
     expect(rendered).to match(/#{person.consumer_role.language_preference}/)
     expect(rendered).to match(/#{person.gender}/)
     expect(rendered).to match(/#{person.emails.last.address}/mi)
-    expect(rendered).to match(/Let’s begin by entering your personal information. This will take approximately 10 minutes. When you finish, select CONTINUE./)
-    expect(rendered).to have_selector('h3', text: 'Enroll - let\'s get you signed up for healthcare')
-    expect(rendered).to have_selector('h5', text: 'Please indicate preferred method to receive notices (OPTIONAL)')
+    expect(rendered).to match(/Please answer the following questions, provide your address/)
+    #expect(rendered).to have_selector('h3', text: 'Enroll - let\'s get you signed up for healthcare')
+    #expect(rendered).to have_selector('h5', text: 'Please indicate preferred method to receive notices (OPTIONAL)')
   end
 
   it "shouldn't display the docs fields" do
