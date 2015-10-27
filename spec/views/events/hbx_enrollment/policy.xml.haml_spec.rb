0ebc033fe3c0d3e214c633b697fb6ce5e492577f
Trey Evans <lewis.r.evans@gmail.com>
2015-10-27 17:00:42 -0400

    Correcting a number of policy resource errors.
    The policy resource was missing some tags which are needed by clients to
    properly process the data.

--- spec/views/events/hbx_enrollment/policy.xml.haml_spec.rb
+++ spec/views/events/hbx_enrollment/policy.xml.haml_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "events/hbx_enrollment/policy.haml.erb" do
 
-  let(:plan) { FactoryGirl.create(:plan) }
-  let(:employee_role) { FactoryGirl.create(:employee_role) }
+  let(:plan) { FactoryGirl.build(:plan) }
+  let(:census_employee) { FactoryGirl.build(:census_employee, :benefit_group_assignments => [FactoryGirl.build(:benefit_group_assignment)]) }
+  let(:employee_role) { FactoryGirl.build(:employee_role, census_employee: census_employee) }
+  let(:benefit_group_assignment) { employee_role.census_employee.benefit_group_assignments.first }
+  let(:benefit_group) { benefit_group_assignment.benefit_group }
   let(:hbx_enrollment) {  HbxEnrollment.new(plan:plan, employee_role: employee_role) }
+
+  before :each do
+    allow(hbx_enrollment).to receive(:benefit_group_assignment).and_return(benefit_group_assignment)
+    allow(hbx_enrollment).to receive(:benefit_group).and_return(benefit_group)
+  end
 
   it "generates a policy cv with policy, enrollees and plan elements" do
     render :template=>"events/hbx_enrollment/policy", :locals=>{hbx_enrollment: hbx_enrollment}
