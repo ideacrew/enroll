757723a2e80e8af25c20097a49354ff1ce2323a0
vinner <vineelsonnathi@users.noreply.github.com>
2015-10-27 22:52:51 -0400

    Fixes #3338: Fix cucumber spec

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -645,7 +645,7 @@ And(/I select three plans to compare/) do
     compare_options[2].click
     click_when_present(@browser.a(text: "COMPARE PLANS"))
     @browser.h3(text: /Choose Plan - Compare Selected Plans/).wait_until_present
-    expect(@browser.elements(:class => "sbc_link").size).to eq 3
+    expect(@browser.elements(:class => "plan_comparison").size).to eq 3
     @browser.button(text: 'Close').click
   end
 end
