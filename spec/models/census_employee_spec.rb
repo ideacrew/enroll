01fdcd67b9aa7df6096932a3056f71170b997dca
Dan Thomas <dan.thomas@dc.gov>
2015-10-28 06:32:41 -0400

    Add pending specs for migrated employees

--- spec/models/census_employee_spec.rb
+++ spec/models/census_employee_spec.rb
@@ -583,7 +583,26 @@ RSpec.describe CensusEmployee, type: :model, dbclean: :after_each do
   end
 
 
-  context "and " do
+  context "Employee is migrated into Enroll database without an EmployeeRole" do
+    let(:person)
+    let(:family)
+    let(:employer_profile)
+    let(:plan_year)
+    let(:hbx_enrollment)
+    let(:benefit_group_assignment)
+
+    context "and the employee links to roster" do
+      
+      it "should create an employee_role" do
+      end
+    end
+
+    context "and the employee is terminated" do
+
+      it "should create an employee_role" do
+      end
+    end
+
   end
 
   # context '.edit' do
