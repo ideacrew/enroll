2aed468eec20ed8926eba6a57f29a991029631d1
vinner <vineelsonnathi@users.noreply.github.com>
2015-10-30 00:33:56 -0400

    Fixes #3648 : Fix display count & list  of active family members on family/home.

---
+++ spec/views/insured/families/_navigation.html.erb_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe "insured/families/_navigation.html.erb" do
+
+  let(:user){ instance_double("User") }
+  let(:person){
+    instance_double(
+      "Person",
+      first_name: "my first name",
+      last_name: "my last name"
+      )
+  }
+  let(:employee_role){ instance_double("EmployeeRole") }
+  let(:employer_profile){ instance_double("EmployerProfile") }
+  let(:broker_agency_profile){ instance_double("BrokerAgencyProfile") }
+  let(:inbox){ instance_double("Inbox") }
+  def family_member
+    random_value = rand(999_999_999)
+    instance_double(
+      "FamilyMember#{random_value}",
+      first_name: "my real first name #{random_value}",
+      last_name: "my real last name #{random_value}"
+      )
+  end
+
+  let(:family_members){ [ family_member, family_member ] }
+
+  before :each do
+    allow(user).to receive(:person).and_return(person)
+    allow(person).to receive(:has_active_employee_role?).and_return(true)
+    allow(person).to receive(:inbox).and_return(inbox)
+    allow(inbox).to receive(:unread_messages).and_return(3)
+    allow(employee_role).to receive(:employer_profile).and_return(employer_profile)
+    allow(employer_profile).to receive(:broker_agency_profile).and_return(broker_agency_profile)
+    assign(:person, person)
+    assign(:family_members, family_members)
+    assign(:employee_role, employee_role)
+    render partial: "insured/families/navigation.html.erb"
+  end
+
+  it "should match the side bar tabs info on family home page" do
+    expect(rendered).to have_selector('a[href="/families/home?tab=home"]', text: 'My DC Health Link')
+    expect(rendered).to have_selector('a[href="/insured/families/brokers?tab=broker"]', text: 'My Broker')
+    expect(rendered).to have_selector('a[href="/insured/families/documents_index?tab=documents"]', text: 'Documents')
+    expect(rendered).to have_selector('a[href="/insured/families/inbox?tab=messages"]', text: 'Messages')
+  end
+
+  it "should display the info under my account in the side bar" do
+    expect(rendered).to have_selector('h4', 'My Account')
+    expect(rendered).to have_selector('span', "#{person.first_name} #{person.last_name}")
+    expect(rendered).to have_selector('span', "Your household of #{family_members.count}")
+  end
+
+  it "should display the list of active household" do
+    family_members.each do |family_member|
+      expect(rendered).to match(/#{family_member.first_name} #{family_member.last_name}/)
+    end
+  end
+
+  it "should have a link to manage family button" do
+    expect(rendered).to have_selector('a[href="/insured/families/manage_family?tab=family"]', text: 'Manage Family')
+  end
+
+end
