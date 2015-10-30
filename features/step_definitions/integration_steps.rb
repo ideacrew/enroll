fae61f3afe6f7c5bf7ca08eaa73ec3e876d5d67b
Raghuram <raghuramg83@gmail.com>
2015-10-30 12:58:43 -0400

    cucumber fix

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -551,7 +551,7 @@ Then(/^.+ should see the current plan year$/) do
 end
 
 And(/^.+ should see the premium billings report$/) do
-  expect(@browser.h4(text: /Premium Billing Report/i).visible?).to be_truthy
+  expect(@browser.h3(text: /Premium Billing Report/i).visible?).to be_truthy
 end
 
 When(/^.+ should see a published success message without employee$/) do
