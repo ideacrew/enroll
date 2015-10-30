7f9ebfff7fe69cf1f5d452af32e4cb5de49837df
Ken Lu <ken@seeice.com>
2015-10-30 08:49:30 -0400

    Refs #3305 - QLE heading for same-type consistent

--- spec/factories/qualifying_life_event_kinds.rb
+++ spec/factories/qualifying_life_event_kinds.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :qualifying_life_event_kind do
 
-    title "I've married"
+    title "Married"
     action_kind "add"
-    reason " "
     edi_code "32-MARRIAGE"
+    reason "marriage"
     market_kind "shop"
     effective_on_kinds ["first_of_month"]
     pre_event_sep_in_days 0
@@ -14,15 +14,16 @@ FactoryGirl.define do
     tool_tip "Enroll or add a family member because of marriage"
 
     trait :effective_on_event_date do
-      title "I've had a baby"
+      title "Had a baby"
       edi_code "02-BIRTH"
       effective_on_kinds ["date_of_event"]
       tool_tip "Enroll or add a family member due to birth"
     end
 
     trait :effective_on_first_of_month do
-      title "I've married"
+      title "Married"
       edi_code "32-MARRIAGE"
+      reason "marriage"
       effective_on_kinds ["first_of_month"]
       tool_tip "Enroll or add a family member because of marriage"
     end
