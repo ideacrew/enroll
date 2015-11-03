73e7621e2b7c1827628a8a5d9b62aabb150ef926
Ken Lu <ken@seeice.com>
2015-11-03 18:42:03 -0500

    Refs #3750 - 'Keep Existing Plan' button displays zero premium and no APTC

--- app/controllers/insured/group_selection_controller.rb
+++ app/controllers/insured/group_selection_controller.rb
@@ -137,5 +137,6 @@ class Insured::GroupSelectionController < ApplicationController
     @change_plan = params[:change_plan].present? ? params[:change_plan] : ''
     @coverage_kind = params[:coverage_kind].present? ? params[:coverage_kind] : 'health'
     @enrollment_kind = params[:enrollment_kind].present? ? params[:enrollment_kind] : ''
+    @shop_for_plans = params[:shop_for_plans].present? ? params{:shop_for_plans} : ''
   end
 end
