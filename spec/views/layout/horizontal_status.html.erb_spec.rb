915fe768531defb215701f25bf108173d5dbea6e
Phillip Jacowski <phillipjacowski@gmail.com>
2015-11-03 12:09:27 -0600

    confirm spec is working for horizontal status

--- spec/views/layout/horizontal_status.html.erb_spec.rb
+++ spec/views/layout/horizontal_status.html.erb_spec.rb
@@ -1,8 +1,9 @@
-# require 'rails_helper'
-#
-# RSpec.describe "insured/families/_horizontal_status.html.erb" do
-#   it "should display the horizontal status bar with step value 2" do
-#     render partial: "insured/families/horizontal_status", locals: {step: 2}
-#     expect(rendered).content_for(:horizontal_status).should include('Tell us about yourself')
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "insured/families/_horizontal_status.html.erb" do
+  it "should display the horizontal status bar with step value 2" do
+    render "insured/families/horizontal_status", step: 2
+    expect(rendered).to have_selector("strong", text: "Compare plans and choose")
+    expect(rendered).to have_selector("span", text: "Enroll")
+  end
+end
