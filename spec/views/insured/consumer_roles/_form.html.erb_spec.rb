967822b59ad0f902905eacd6b915e1423d1493e0
Ken Lu <ken@seeice.com>
2015-11-05 00:43:20 -0500

    Refs #2731 - Fixed issue that no dc address function is not working

--- spec/views/insured/consumer_roles/_form.html.erb_spec.rb
+++ spec/views/insured/consumer_roles/_form.html.erb_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 include ActionView::Context
 RSpec.describe "insured/consumer_roles/_form.html.erb" do
   let(:person) { Person.new }
-  let(:consumer_role) { double("ConsumerRole", id: "test")}
   let(:current_user) {FactoryGirl.create(:user)}
   before :each do
     helper = Object.new.extend ActionView::Helpers::FormHelper
     helper.extend ActionDispatch::Routing::PolymorphicRoutes
     helper.extend ActionView::Helpers::FormOptionsHelper
+    person.build_consumer_role if person.consumer_role.blank?
+    person.consumer_role.build_nested_models_for_person
     mock_form = ActionView::Helpers::FormBuilder.new(:person, person, helper, {})
     stub_template "shared/_consumer_fields.html.erb" => ''
     sign_in current_user
     render partial: "insured/consumer_roles/form", locals: {f: mock_form}
-
   end
 
   it "should have title" do
@@ -21,5 +21,9 @@ RSpec.describe "insured/consumer_roles/_form.html.erb" do
 
   it "should display hint for asterisks" do
     expect(rendered).to have_selector('p.memo', text: '* = required field')
+  end
+
+  it "should display only one no_dc_address" do
+    expect(rendered).to have_selector('input#no_dc_address', count: 1)
   end
 end
