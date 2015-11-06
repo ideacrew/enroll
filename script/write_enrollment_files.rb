e17d1c661be918e105a416bf72296c6910203fcd
Trey Evans <lewis.r.evans@gmail.com>
2015-11-06 16:16:39 -0500

    Write out enrollment files.

---
+++ script/write_enrollment_files.rb
@@ -0,0 +1,33 @@
+controller = Events::PoliciesController.new
+
+vs = VitalSign.new(
+  start_at: DateTime.new(2015,10,27,23,43,0,'-4'),
+  end_at: DateTime.new(2015,11,2,23,55,0,'-4')
+)
+
+PropertiesSlug = Struct.new(:reply_to, :headers)
+
+ConnectionSlug = Struct.new(:policy_id) do
+  def create_channel
+    self
+  end
+
+  def default_exchange
+    self
+  end
+
+  def close
+  end
+
+  def publish(payload, headers)
+    File.open(File.join("policy_cvs", "#{policy_id}.xml"), 'w') do |f|
+      f.puts payload
+    end
+  end
+end
+
+CSV.foreach("policy_transmission_list.csv", headers: false) do |row|
+   pid, *rest = row.to_a
+   properties_slug = PropertiesSlug.new("", {:policy_id => pid})
+   controller.resource(ConnectionSlug.new(pid), "", properties_slug, "")
+end
