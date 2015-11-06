cfd5c9ee10b6dc605ab551e1ae94d95c671b9adc
Dan Thomas <dan.thomas@dc.gov>
2015-11-06 00:42:53 -0500

    Refactor to better manage plans

--- app/models/factories/family_enrollment_renewal_factory.rb
+++ app/models/factories/family_enrollment_renewal_factory.rb
@@ -5,36 +5,78 @@ module Factories
     attr_accessor :family
 
     def renew
-      # Collect active enrollments -- 
-      # collect waived coverage
-      @family.enrollments.each do |active_enrollment|
-
+      renewal_builder = lambda do |active_enrollment|
         renewal_enrollment = @family.active_household.hbx_enrollments.new
         renewal_enrollment = assign_common_attributes(active_enrollment, renewal_enrollment)
-
-        if active_enrollment.kind == "employer_sponsored"
-          renewal_enrollment = clone_shop_enrollment(active_enrollment, renewal_enrollment)
-        else
-          renewal_enrollment = clone_ivl_enrollment(active_enrollment, renewal_enrollment)          
-        end
-
-        # enrollment_kind == "special_enrollment" || "open_enrollment"
-
         renewal_enrollment.renew_enrollment
-        if renewal_enrollment.save
-          renewal_enrollment
-        else
-          raise FamilyEnrollmentRenewalFactoryError, 
-            "For enrollment: #{renewal_enrollment.inspect}, \n" \
-            "Error(s): \n #{renewal_enrollment.errors.map{|k,v| "#{k} = #{v}"}.join(" & \n")} \n" \
-            "Unable to save renewal enrollment: #{renewal_enrollment.inspect}"
+        renewal_enrollment
+      end
+
+      shop_enrollments = @family.enrollments.shop_market + @family.active_household.hbx_enrollments.waived
+      if shop_enrollments.any?
+        active_enrollment = shop_enrollments.detect{|enrollment| enrollment.currently_active?}
+        @census_employee = get_census_employee(active_enrollment)
+
+        if @census_employee.present?
+          shop_enrollments.each do |active_enrollment|       
+            next unless active_enrollment.currently_active?
+
+            renewal_enrollment = renewal_builder.call(active_enrollment)
+            renewal_enrollment = clone_shop_enrollment(active_enrollment, renewal_enrollment)
+            save_renewal_enrollment(renewal_enrollment, active_enrollment)
+          end
         end
+      end
+
+
+      # @family.enrollments.individual_market do |active_enrollment|       
+      #   next unless active_enrollment.currently_active?
+
+      #   renewal_enrollment = renewal_builder.call(active_enrollment)
+      #   renewal_enrollment = clone_ivl_enrollment(active_enrollment, renewal_enrollment)
+      #   save_renewal_enrollment(renewal_enrollment, active_enrollment)        
+      # end
+
+      # enrollment_kind == "special_enrollment" || "open_enrollment"
+    end
+
+
+    def get_census_employee(active_enrollment)
+      employee = active_enrollment.household.family.primary_family_member
+      census_employee = CensusEmployee.by_ssn(employee.ssn).active.first
+      
+      if census_employee.blank?
+binding.pry
+        message = "Unable to find census_employee for "\
+          "primary family member: #{employee.full_name} "\
+          "id: #{employee.id} "\
+          "for hbx_enrollment: #{active_enrollment.id}"
+
+        Rails.logger.error { message }
+        raise FamilyEnrollmentRenewalFactoryError, message
+      else
+        census_employee
+      end
+    end
+
+
+    def save_renewal_enrollment(renewal_enrollment, active_enrollment)
+      if renewal_enrollment.save
+        renewal_enrollment
+      else
+binding.pry
+        message = "Enrollment: #{active_enrollment.id}, \n" \
+        "Unable to save renewal enrollment: #{renewal_enrollment.inspect}, \n" \
+          "Error(s): \n #{renewal_enrollment.errors.map{|k,v| "#{k} = #{v}"}.join(" & \n")} \n"
+
+        Rails.logger.error { message }
+        raise FamilyEnrollmentRenewalFactoryError, message
       end
     end
 
     def assign_common_attributes(active_enrollment, renewal_enrollment)
       common_attributes = %w(coverage_household_id coverage_kind changing broker_agency_profile_id 
-          writing_agent_id original_application_type kind
+          writing_agent_id original_application_type kind special_enrollment_period_id
         )
       common_attributes.each do |attr|
          renewal_enrollment.send("#{attr}=", active_enrollment.send(attr))
@@ -62,19 +104,18 @@ module Factories
       # Find and associate with new ER benefit group
 
       renewal_enrollment.employee_role_id = active_enrollment.employee_role_id
-
       renewal_enrollment.benefit_group_id = active_enrollment.benefit_group_id
 
-      employee = active_enrollment.household.family.primary_family_member
-
-      census_employee = CensusEmployee.matchable(employee.ssn, employee.dob).first
-      if census_employee.blank?
-        raise FamilyEnrollmentRenewalFactoryError, "Unable to find census_employee for primary family member: #{employee.inspect} for hbx_enrollment #{active_enrollment.id}"
-      end
-
-      benefit_group_assignment = census_employee.renewal_benefit_group_assignment
+      benefit_group_assignment = @census_employee.renewal_benefit_group_assignment
       if benefit_group_assignment.blank?
-        raise FamilyEnrollmentRenewalFactoryError, "Unable to find benefit_group_assignment for census_employee: #{census_employee.inspect} for hbx_enrollment #{active_enrollment.id}"
+binding.pry
+        message = "Unable to find benefit_group_assignment for census_employee: \n"\
+          "census_employee: #{@census_employee.full_name} "\
+          "id: #{@census_employee.id} "\
+          "for hbx_enrollment #{active_enrollment.id}"
+
+        Rails.logger.error { message }
+        raise FamilyEnrollmentRenewalFactoryError, message
       end
 
       renewal_enrollment.benefit_group_assignment_id = benefit_group_assignment.id
