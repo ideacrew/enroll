9f6b9e88cfc8c87a3eb09852450669e877783007
Lisyk <sergey.liskovoy@gmail.com>
2015-11-09 15:27:05 -0500

    Fixes #3797 add curam_user list check that means user has DC address

--- spec/models/curam_user_spec.rb
+++ spec/models/curam_user_spec.rb
@@ -6,10 +6,9 @@ describe CuramUser, "verification if user exists in Curam" do
   let(:dob_1982) { Date.new(1982, 1, 1) }
 
   before :each do
-    user1 = CuramUser.create(first_name: "Serg", last_name:"Lis", ssn: "123456789", dob: dob_1980)
-    user2 = CuramUser.create(first_name: "Ivan", last_name:"Umen", ssn: "987654321", dob: dob_1981)
-    user3 = CuramUser.create(first_name: "Potap", last_name:"Razumen", ssn: "345678912", dob: dob_1982)
+    user1 = FactoryGirl.create(:curam_user, ssn: "123456789")
   end
+
   it "returns true if SSN is matching any record" do
     expect(CuramUser.match_ssn(123456789)).to be true
   end
@@ -19,18 +18,19 @@ describe CuramUser, "verification if user exists in Curam" do
   end
 
   it "returns true if SSN and DOB exist and belongs to the same user" do
-    expect(CuramUser.match_ssn_dob(123456789, dob_1980).any?).to be true
-  end
-
-  it "returns false if SSN mathing but DOB doesn't" do
-    expect(CuramUser.match_ssn_dob(123456789, Date.new(1970, 1, 1)).any?).to be false
+    expect(CuramUser.match_ssn_dob(123456789, "01/01/1980").any?).to be true
   end
 
   it "returns false if DOB matching but SSN doesn't" do
-    expect(CuramUser.match_ssn_dob(1234567899, dob_1980).any?).to be false
+    expect(CuramUser.match_ssn_dob(123456782, "01/01/1980").any?).to be false
   end
 
   it "returns false if DOB mathing and SSN matching but belongs to diff users" do
-    expect(CuramUser.match_ssn_dob(123456789, dob_1981).any?).to be false
+    user2 = FactoryGirl.create(:curam_user, dob: Date.new(1981, 1, 1))
+    expect(CuramUser.match_ssn_dob(123456789, "01/01/1981").any?).to be false
+  end
+
+  it "returns true if firstname and lastname exists in list" do
+    expect(CuramUser.name_in_curam_list("Ivan", "Lisyk")).to be true
   end
 end
