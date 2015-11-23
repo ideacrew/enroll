a9e64c6e58f6b97dea586ee6ced5a210147dd311
Ken Lu <ken@seeice.com>
2015-11-23 02:17:32 -0500

    Refs #3993 - added employee_role after employer added and match to current individual

--- app/controllers/employers/census_employees_controller.rb
+++ app/controllers/employers/census_employees_controller.rb
@@ -27,6 +27,7 @@ class Employers::CensusEmployeesController < ApplicationController
     if @census_employee.save
       if benefit_group_id.present?
         @census_employee.send_invite!
+        @census_employee.construct_employee_role_for_match_person
         flash[:notice] = "Census Employee is successfully created."
       else
         flash[:notice] = "Your employee was successfully added to your roster."
