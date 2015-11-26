29c0c6bdd09d6c34e742505df0a4dd63655201b4
Phillip Jacowski <phillipjacowski@gmail.com>
2015-11-25 19:19:03 -0500

    Fixes #3340 add flash messenger to census employee failed to terminate

--- spec/controllers/employers/census_employees_controller_spec.rb
+++ spec/controllers/employers/census_employees_controller_spec.rb
@@ -238,7 +238,7 @@ RSpec.describe Employers::CensusEmployeesController do
 
     context "with termination date" do
       it "should terminate census employee" do
-        xhr :get, :terminate, :census_employee_id => census_employee.id, :employer_profile_id => employer_profile_id, termination_date: "05/01/2015", :format => :js
+        xhr :get, :terminate, :census_employee_id => census_employee.id, :employer_profile_id => employer_profile_id, termination_date: Date.today.to_s, :format => :js
         expect(response).to have_http_status(:success)
         expect(assigns[:fa]).to eq true
       end
