41bae32b9540a62a61219201f2ba5bb5afd4b73b
Tony Schaffert <antonio.schaffert@gmail.com>
2015-11-30 10:48:41 -0500

    Refs #4054 No Method Error on SAML Controller

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -13,7 +13,7 @@ class SamlController < ApplicationController
 
     sign_out current_user if current_user.present?
 
-    if response.is_valid?
+    if response.is_valid? && response.attributes['mail'].present?
       email = response.attributes['mail'].downcase
 
       user_with_email = User.where(email: email).first
