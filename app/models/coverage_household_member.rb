5c12590ce2934171c6b4a367963df9efc5541802
Raghuram <raghuramg83@gmail.com>
2015-12-05 19:40:24 +0530

    Refs #3839 updated specs

--- app/models/coverage_household_member.rb
+++ app/models/coverage_household_member.rb
@@ -8,9 +8,9 @@ class CoverageHouseholdMember
   field :is_subscriber, type: Boolean, default: false
 
 
-  def save_parent
-    coverage_household.save
-  end
+  # def save_parent
+  #   coverage_household.save
+  # end
 
   include BelongsToFamilyMember
 
