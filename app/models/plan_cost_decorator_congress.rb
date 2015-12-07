d9ac5aa038f00d5c4c57a7b03fad8f81f984e61a
Trey Evans <lewis.r.evans@gmail.com>
2015-12-07 17:40:05 -0500

    Correct rounding errors across all premium calcs.

--- app/models/plan_cost_decorator_congress.rb
+++ app/models/plan_cost_decorator_congress.rb
@@ -1,6 +1,8 @@
 class PlanCostDecoratorCongress < SimpleDelegator
   attr_reader :member_provider, :benefit_group, :reference_plan
 
+  include ShopPolicyCalculations
+
   def initialize(plan, member_provider, benefit_group, max_cont_cache = {})
     super(plan)
     @member_provider = member_provider
@@ -8,26 +10,8 @@ class PlanCostDecoratorCongress < SimpleDelegator
     @max_contribution_cache = max_cont_cache
   end
 
-  def members
-    case member_provider.class
-    when HbxEnrollment
-      member_provider.hbx_enrollment_members
-    when CensusEmployee
-      [member_provider] + member_provider.census_dependents
-    end
-  end
-
   def plan_year_start_on
     benefit_group.plan_year.start_on
-  end
-
-  def age_of(member)
-    case member.class
-    when HbxEnrollmentMember
-      member.age_on_effective_date
-    else
-      member.age_on(plan_year_start_on)
-    end
   end
 
   def child_index(member)
@@ -37,73 +21,6 @@ class PlanCostDecoratorCongress < SimpleDelegator
 
   def member_index(member)
     members.index(member)
-  end
-
-  def benefit_relationship(person_relationship)
-    PlanCostDecorator.benefit_relationship(person_relationship)
-  end
-
-  def self.benefit_relationship(person_relationship)
-    {
-      "head of household" => nil,
-      "spouse" => "spouse",
-      "ex-spouse" => "spouse",
-      "cousin" => nil,
-      "ward" => "child_under_26",
-      "trustee" => "child_under_26",
-      "annuitant" => nil,
-      "other relationship" => nil,
-      "other relative" => nil,
-      "self" => "employee",
-      "parent" => nil,
-      "grandparent" => nil,
-      "aunt_or_uncle" => nil,
-      "nephew_or_niece" => nil,
-      "father_or_mother_in_law" => nil,
-      "daughter_or_son_in_law" => nil,
-      "brother_or_sister_in_law" => nil,
-      "adopted_child" => "child_under_26",
-      "stepparent" => nil,
-      "foster_child" => "child_under_26",
-      "sibling" => nil,
-      "stepchild" => "child_under_26",
-      "sponsored_dependent" => "child_under_26",
-      "dependent_of_a_minor_dependent" => nil,
-      "guardian" => nil,
-      "court_appointed_guardian" => nil,
-      "collateral_dependent" => "child_under_26",
-      "life_partner" => "domestic_partner",
-      "child" => "child_under_26",
-      "grandchild" => nil,
-      "unrelated" => nil,
-      "great_grandparent" => nil,
-      "great_grandchild" => nil,
-    }[person_relationship]
-  end
-
-  def relationship_for(member)
-    case member.class
-    when HbxEnrollmentMember
-      if member.is_subscriber?
-        "employee"
-      else
-        benefit_relationship(member.primary_relationship)
-      end
-    else
-      member.employee_relationship
-    end
-  end
-
-  def large_family_factor(member)
-    if age_of(member) > 20
-      1.00
-    else
-      if child_index(member) > 2
-        0.00
-      else
-        1.00
-      end
-    end
   end
 
   def employer_contribution_percent
@@ -135,12 +52,6 @@ class PlanCostDecoratorCongress < SimpleDelegator
 
   def employee_cost_for(member)
     premium_for(member) - employer_contribution_for(member)
-  end
-
-  def total_premium
-    members.reduce(0.00) do |sum, member|
-      (sum + premium_for(member))
-    end.round(2)
   end
 
   def total_employer_contribution
