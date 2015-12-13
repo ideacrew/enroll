8be01717100e39b13806713d08bfe25a7fbe702b
Tony Schaffert <antonio.schaffert@gmail.com>
2015-12-13 10:04:25 -0500

    Refs #4045 #2987 rspecs

--- app/controllers/insured/families_controller.rb
+++ app/controllers/insured/families_controller.rb
@@ -11,31 +11,13 @@ class Insured::FamiliesController < FamiliesController
 
     @hbx_enrollments = @family.enrollments.order(submitted_at: :desc, effective_on: :desc, coverage_kind: :desc) || []
 
-    @enrollment_filter = @family.collection.aggregate([
+    @enrollment_filter = Family.collection.aggregate([
       {"$match" => {'_id' => @family._id}},
-      #{"$lookUp" => {
-      #  "from" => "plans",
-      #  "localField" => "plan_id",
-      #  "foreignField" => "_id",
-      #  "as" => "plan"
-      #  }},
       {"$unwind" => '$households'},
       {"$unwind" => '$households.hbx_enrollments'},
-      #{"$unwind" => '$households.hbx_enrollments.plan'},
       {"$match" => {"aasm_state" => {"$ne" => 'inactive'}}},
       {"$sort" => {"submitted_at" => -1 }},
       {"$group" => {'_id' => {'year' => { "$year" => '$households.hbx_enrollments.effective_on'}, 'provider_id' => '$households.hbx_enrollments.carrier_profile_id', 'state' => '$households.hbx_enrollments.aasm_state', 'kind' => '$households.hbx_enrollments.hbx_enrollments.kind', 'coverage_kind' => '$households.hbx_enrollments.coverage_kind'}, "hbx_enrollment" => { "$first" => '$households.hbx_enrollments'}}},
-      {"$project" => {'hbx_enrollment._id' => 1, '_id' => 1}}
-      ])
-
-
-    @fil = Family.collection.aggregate([
-      {"$match" => {'_id' => @family._id}},
-      {"$unwind" => '$households'},
-      {"$unwind" => '$households.hbx_enrollments'},
-      {"$match" => {"households.hbx_enrollments.aasm_state" => {"$ne" => 'inactive'}}},
-      {"$sort" => {"households.hbx_enrollments.submitted_at" => -1 }},
-      {"$group" => {'_id' => {'year' => { "$year" => '$households.hbx_enrollments.effective_on'},'state' => '$households.hbx_enrollments.aasm_state', 'kind' => '$households.hbx_enrollments.kind', 'coverage_kind' => '$households.hbx_enrollments.coverage_kind'}, "hbx_enrollment" => { "$first" => '$households.hbx_enrollments'}}},
       {"$project" => {'hbx_enrollment._id' => 1, '_id' => 1}}
       ])
 
@@ -62,7 +44,7 @@ class Insured::FamiliesController < FamiliesController
     #binding.pry
 
     # Filter out enrollments for display only
-    @hbx_enrollments = @hbx_enrollments.each.reject { |r| !valid_display_enrollments.include? r._id }
+    @hbx_enrollments = @hbx_enrollments.reject { |r| !valid_display_enrollments.include? r._id }
     @waived_hbx_enrollments = @waived_hbx_enrollments.each.reject { |r| !valid_display_waived_enrollments.include? r._id }
 
     unique_display_years = @hbx_enrollments.map{|t| t.effective_on.year if t.aasm_state == 'coverage_selected'}.compact
