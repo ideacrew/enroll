d6168fab2b61fd22b4a7b94fc315f5056562fb55
lisyk <sergey.liskovoy@gmail.com>
2015-12-16 15:36:42 -0500

    Fixes #4406 fix regex for curam_user match_email to match exactly the same address but only case-insensitive

--- spec/models/curam_user_spec.rb
+++ spec/models/curam_user_spec.rb
@@ -21,6 +21,10 @@ describe CuramUser, "verification if user exists in Curam" do
     it "should match when email has only one uppercase character" do
       expect(CuramUser.match_email("test@Example.COM").any?).to be true
     end
+
+    it "returns false for incomplete email address" do
+      expect(CuramUser.match_email("TEST@EXAMPLE").any?).to be false
+    end
   end
 
   it "returns true if SSN is matching any record" do
