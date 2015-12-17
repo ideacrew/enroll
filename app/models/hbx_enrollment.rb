0e70290f1371b15782f4df146685738c117c27c8
Raghuram <raghuramg83@gmail.com>
2015-12-17 14:30:19 -0500

    Refs #4060 Fixing the alert when new not eligible to enroll

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -518,13 +518,12 @@ class HbxEnrollment
     elsif employee_role.is_eligible_to_enroll_as_new_hire_on?(TimeKeeper.date_of_record)
       new_hire_effective_date = employee_role.coverage_effective_on
     else
-      if employee_role.is_under_open_enrollment?
-        open_enrollment_effective_date = employee_role.employer_profile.show_plan_year.start_on
-        if open_enrollment_effective_date < employee_role.coverage_effective_on
-          new_hire_enrollment_period = employee_role.benefit_group.new_hire_enrollment_period(employee_role.new_census_employee.hired_on)
-          raise "You're not yet eligible under your employer-sponsored benefits. Please return on #{new_hire_enrollment_period.first} to enroll for coverage."
-        end
-      else
+      open_enrollment_effective_date = employee_role.employer_profile.show_plan_year.start_on
+      
+      if open_enrollment_effective_date < employee_role.coverage_effective_on
+        new_hire_enrollment_period = employee_role.benefit_group.new_hire_enrollment_period(employee_role.new_census_employee.hired_on)
+        raise "You're not yet eligible under your employer-sponsored benefits. Please return on #{new_hire_enrollment_period.first} to enroll for coverage."
+      elsif !employee_role.is_under_open_enrollment?
         raise "You may not enroll until you're eligible under an enrollment period"
       end
     end
