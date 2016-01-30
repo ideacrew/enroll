072f484037b66dda18cd1869c7c61b3cea31da4e
Trey Evans <lewis.r.evans@gmail.com>
2016-01-30 18:15:29 -0500

    Refs #5004. Fixing nil case.
    Refs #5004.

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -380,6 +380,7 @@ class HbxEnrollment
     last_broker_before_purchase = broker_accounts.select do |baa|
       (baa.start_on < self.time_of_purchase)# &&
     end.sort_by(&:start_on).last
+    return nil if last_broker_before_purchase.nil?
     if  ((last_broker_before_purchase.end_on.blank?) || (last_broker_before_purchase.end_on >= self.time_of_purchase))
       last_broker_before_purchase
     else
