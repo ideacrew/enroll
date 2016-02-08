93e760bcd95e028a266ffa88397b9a3cd2c0a81d
Trey Evans <lewis.r.evans@gmail.com>
2016-02-08 12:05:17 -0500

    Refs #5125. Fix address types.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,4 +12,14 @@ module EventsHelper
   def vocab_relationship_map(rel)
     rel.gsub(" ", "_")
   end
+
+  def office_location_address_kind(kind)
+    if kind == "primary"
+      "work"
+    elsif kind == "branch"
+      "work"
+    else
+      kind
+    end
+  end
 end
