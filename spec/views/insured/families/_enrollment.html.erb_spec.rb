f73e8e730e956f4354376b4b3b35f4a8027eec7c
Tony Schaffert <antonio.schaffert@gmail.com>
2016-02-25 10:31:59 -0500

    Merge hotfix-4045-r1 with Master

--- spec/views/insured/families/_enrollment.html.erb_spec.rb
+++ spec/views/insured/families/_enrollment.html.erb_spec.rb
@@ -41,6 +41,7 @@ RSpec.describe "insured/families/_enrollment.html.erb" do
 
     before :each do
       allow(hbx_enrollment).to receive(:coverage_terminated?).and_return(false)
+      allow(hbx_enrollment).to receive(:coverage_canceled?).and_return(false)
       allow(hbx_enrollment).to receive(:coverage_year).and_return(plan.active_year)
       allow(hbx_enrollment).to receive(:created_at).and_return(plan.created_at)
       allow(hbx_enrollment).to receive(:hbx_id).and_return(true)
@@ -79,6 +80,7 @@ RSpec.describe "insured/families/_enrollment.html.erb" do
    let(:benefit_group) { FactoryGirl.create(:benefit_group) }
 
     before :each do
+      allow(hbx_enrollment).to receive(:coverage_canceled?).and_return(false)
       allow(hbx_enrollment).to receive(:coverage_year).and_return(plan.active_year)
       allow(hbx_enrollment).to receive(:created_at).and_return(plan.created_at)
       allow(hbx_enrollment).to receive(:hbx_id).and_return(true)
@@ -113,6 +115,7 @@ RSpec.describe "insured/families/_enrollment.html.erb" do
     let(:benefit_group) { FactoryGirl.create(:benefit_group) }
 
     before :each do
+      allow(hbx_enrollment).to receive(:coverage_canceled?).and_return(false)
       allow(hbx_enrollment).to receive(:coverage_year).and_return(plan.active_year)
       allow(hbx_enrollment).to receive(:created_at).and_return(plan.created_at)
       allow(hbx_enrollment).to receive(:hbx_id).and_return(true)
