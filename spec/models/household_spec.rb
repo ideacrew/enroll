66c4f27e8630a091bdf875456595c45634868c1c
Kevin Wei <kevin.tw.wei@gmail.com>
2016-03-16 19:18:25 -0400

    Refs #5133 fix intermittent spec failures caused by lack of db cleaning

--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Household, "given a coverage household with a dependent" do
+describe Household, "given a coverage household with a dependent", :dbclean => :after_each do
   let(:family_member) { FamilyMember.new }
   let(:coverage_household_member) { CoverageHouseholdMember.new(:family_member_id => family_member.id) }
   let(:coverage_household) { CoverageHousehold.new(:coverage_household_members => [coverage_household_member]) }
