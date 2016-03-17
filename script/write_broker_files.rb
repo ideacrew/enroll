fa954fd1dfaf2ea7e38fcd6d907ab146a77316cd
Trey Evans <lewis.r.evans@gmail.com>
2016-03-17 15:08:09 -0400

    Fixing missing office locations.

---
+++ script/write_broker_files.rb
@@ -0,0 +1,30 @@
+controller = Events::BrokersController.new
+PropertiesSlug = Struct.new(:reply_to, :headers)
+
+ConnectionSlug = Struct.new(:broker_npn) do
+  def create_channel
+    self
+  end
+
+  def default_exchange
+    self
+  end
+
+  def close
+  end
+
+  def publish(payload, headers)
+    File.open(File.join("broker_xmls", "#{broker_npn}.xml"), 'w') do |f|
+      f.puts payload
+    end
+  end
+end
+
+npn_list = Person.where("broker_role.aasm_state" => "active").map do |pers|
+  pers.broker_role.npn
+end
+
+npn_list.each do |npn|
+  properties_slug = PropertiesSlug.new("", {:broker_id => npn})
+  controller.resource(ConnectionSlug.new(npn), "", properties_slug, "")
+end
