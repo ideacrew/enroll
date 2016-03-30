4700c75044a70ae5fb67c0640130bc75412eb3b4
lisyk <sergey.liskovoy@gmail.com>
2016-03-30 13:46:14 -0400

    UseCase #5314 hbx admin verification process for enrollments index view, extending due date, fedhub request, individual verification, enrollment verification

--- app/helpers/verification_helper.rb
+++ app/helpers/verification_helper.rb
@@ -1,23 +1,5 @@
 module VerificationHelper
 
-  def admin_docs_filter(filter_param, title = nil, style = nil)
-    direction = filter_param == sort_filter && sort_direction == 'asc' ? 'desc' : 'asc'
-    style = direction if style == 'admin_docs'
-    link_to title, consumer_role_status_documents_path(:sort => filter_param, :direction => direction), remote: true, class: style
-  end
-
-  def docs_waiting_for_review
-    Person.unverified_persons.in('consumer_role.vlp_documents.status':['downloaded']).count
-  end
-
-  def missing_docs
-    Person.unverified_persons.where('consumer_role.vlp_documents.status': 'not submitted').count
-  end
-
-  def all_unverified
-    number_with_delimiter(@unverified_persons.count)
-  end
-
   # info popover list of the documents types that consumer can upload as vlp_document
   def info_pop_up(type)
     case type
@@ -56,19 +38,20 @@ module VerificationHelper
   end
 
   def unverified?(person)
-    true if person.consumer_role.aasm_state != "fully_verified"
+    person.consumer_role.aasm_state != "fully_verified"
   end
 
   def enrollment_group_verified?(person)
     person.primary_family.active_family_members.all? {|member| member.person.consumer_role.aasm_state == "fully_verified"} if person.has_consumer_role?
   end
 
-  def coverage_household_verification
-    "???????"
-  end
+  def verification_due_date(person)
+    if person.consumer_role.special_verification_period
+      person.consumer_role.special_verification_period.strftime("%m/%d/%Y")
+    else
+      person.consumer_role.lawful_presence_determination.latest_denial_date.try(:+, 90.days).try(:>, TimeKeeper.date_of_record) || (TimeKeeper.date_of_record + 90.days)
+    end
 
-  def verification_due_date
-    @person.consumer_role.lawful_presence_determination.latest_denial_date.try(:+, 90.days).try(:>, TimeKeeper.date_of_record) || (TimeKeeper.date_of_record + 90.days)
   end
 
   def member_has_uploaded_docs(member)
@@ -79,8 +62,13 @@ module VerificationHelper
     person.consumer_role.vlp_documents.select{|doc| doc.identifier}.count
   end
 
-  def due_date(person)
-    time = (Time.now + SecureRandom.random_number(10).days)
-    time.strftime("%m/%d/%Y")
+  def review_button_class(family)
+    if family.active_household.hbx_enrollments.first.review_status == "ready"
+      "success"
+    elsif family.active_household.hbx_enrollments.first.review_status == "in review"
+      "info"
+    else
+      "default"
+    end
   end
 end
--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -3,45 +3,116 @@ require 'rails_helper'
 RSpec.describe DocumentsController, :type => :controller do
   let(:user) { FactoryGirl.create(:user) }
   let(:person) { FactoryGirl.create(:person, :with_consumer_role) }
+  let(:consumer_role) {FactoryGirl.build(:consumer_role)}
+  let(:document) {FactoryGirl.build(:vlp_document)}
+  let(:family)  {FactoryGirl.create(:family, :with_primary_family_member)}
+  let(:lawful_presence_determination) { FactoryGirl.build(:lawful_presence_determination)}
 
+  before :each do
+    sign_in user
+  end
 
-  describe "GET consumer role status" do
+  describe "PUT update individual" do
+    before :each do
+      request.env["HTTP_REFERER"] = "http://test.com"
+    end
 
-    it "renders index_consumer_role_status partial" do
-      sign_in user
-      get :consumer_role_status
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(partial: "documents/_index_consumer_role_status")
+    it "redirect_to back" do
+      post :update_individual, person_id: person.id
+      expect(response).to redirect_to :back
+    end
+
+    it "transfers the current state" do
+      post :update_individual, person_id: person.id
+      person.reload
+      expect(person.consumer_role.aasm_state).to eq("fully_verified")
     end
   end
 
-  describe "GET index" do
-    it "renders index template" do
-      sign_in user
-      get :index, person_id: person.id
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template :index
+  describe "destroy" do
+    before :each do
+      person.consumer_role.vlp_documents = [document]
+      delete :destroy, person_id: person.id, id: document.id
+    end
+    it "redirects_to verification page" do
+      expect(response).to redirect_to verification_insured_families_path
     end
 
-    it "assigns @person with person_id" do
-      sign_in user
-      get :index, person_id: person.id
-      expect(assigns(:person)).to eq person
+    it "should delete document record" do
+      person.reload
+      expect(person.consumer_role.vlp_documents).to be_empty
     end
   end
 
-  describe "GET new comment" do
-    it "renders new_comment template" do
-      sign_in user
-      get :new_comment, person_id: person.id, :format => 'js'
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template :new_comment
+  describe "PUT update" do
+    context "rejecting with comments" do
+      before :each do
+        person.consumer_role.vlp_documents = [document]
+      end
+
+      it "should redirect to verification" do
+        put :update, person_id: person.id, id: document.id
+        expect(response).to redirect_to verification_insured_families_path
+      end
+
+      it "updates document status" do
+        put :update, person_id: person.id, id: document.id, :person=>{ :vlp_document=>{:comment=>"hghghg"}}, :comment => true, :status => "ready"
+        document.reload
+        expect(document.status).to eq("ready")
+      end
     end
 
-    it "assigns @person with person_id" do
-      sign_in user
-      get :new_comment, person_id: person.id, :format => 'js'
-      expect(assigns(:person)).to eq person
+    context "accepting without comments" do
+      before :each do
+        person.consumer_role.vlp_documents = [document]
+      end
+
+      it "should redirect to verification" do
+        put :update, person_id: person.id, id: document.id
+        expect(response).to redirect_to verification_insured_families_path
+      end
+
+      it "updates document status" do
+        put :update, person_id: person.id, id: document.id, :status => "accept"
+        document.reload
+        expect(document.status).to eq("accept")
+      end
+    end
+  end
+  describe "PUT extend due date" do
+    before :each do
+      request.env["HTTP_REFERER"] = "http://test.com"
+      put :extend_due_date, person_id: person.id
+    end
+
+    it "should redirect to back" do
+      expect(response).to redirect_to :back
+    end
+
+    context "with denied documents by FedHub response" do
+      it "creates special verification period" do
+        allow(lawful_presence_determination).to receive(:latest_denial_date).and_return(TimeKeeper.date_of_record)
+        person.consumer_role.lawful_presence_determination = lawful_presence_determination
+        person.reload
+        expect(person.consumer_role.special_verification_period).to eq(TimeKeeper.date_of_record + 120.days)
+      end
+    end
+
+    context "without FedHub response" do
+      it "creates special verification period with 30 days delay" do
+        person.reload
+        expect(person.consumer_role.special_verification_period).to eq(TimeKeeper.date_of_record + 120.days)
+      end
+    end
+
+    context "person has special verification period" do
+      it "extend existing special verification period to 30 days" do
+        person.consumer_role.special_verification_period = TimeKeeper.date_of_record
+        person.save
+        put :extend_due_date, person_id: person.id
+        person.reload
+        expect(person.consumer_role.special_verification_period).to eq(TimeKeeper.date_of_record + 30.days)
+      end
     end
   end
 end
