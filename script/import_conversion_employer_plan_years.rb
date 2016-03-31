fe8e5f7d81f61c208cd8a8fc358e83aca1cdec3c
Trey Evans <lewis.r.evans@gmail.com>
2016-03-31 12:17:42 -0400

    Refs #5722. Refs #5723. Create previous plan year.

--- script/import_conversion_employer_plan_years.rb
+++ script/import_conversion_employer_plan_years.rb
@@ -1,7 +1,7 @@
 def import_employer(in_file)
 #  begin
     result_file = File.open(File.join(Rails.root, "conversion_employer_plan_year_results", "RESULT_PLAN_YEARS_" + File.basename(in_file) + ".csv"), 'wb')
-    importer = Importers::ConversionEmployerPlanYearSet.new(in_file, result_file, Date.new(2016,7,1))
+    importer = Importers::ConversionEmployerPlanYearSet.new(in_file, result_file, Date.new(2015,7,1))
     importer.import!
     result_file.close
 #  rescue
