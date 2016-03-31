e1374018f25fee577308658422d22b061f97ef69
Dan Thomas <dan.thomas@dc.gov>
2016-03-30 22:53:25 -0400

    outline methods

--- app/models/event_for_notice_trigger_rule.rb
+++ app/models/event_for_notice_trigger_rule.rb
@@ -1,14 +1,40 @@
 class EventForNoticeTriggerRule
 
-  def initialize()
+  def initialize(resource_kind, event_id, resource_instance)
+    @resource_kind = resource_kind
+    @event_id = event_id
+    @resource_instance = resource_instance
   end
 
   def satisfied?
   end
 
-  def determination_results
-    @errors
+  def is_primary_recipient_criteria_satisfied?
+    # delivery method available?
+
   end
 
+  def is_secondary_recipient_criteria_satisfied?
+    # electronic delivery method available?
+
+  end
+
+  def determination_results
+    @errors
+
+    # log errors
+  end
+
+  def work_email
+  end
+
+  def home_email
+  end
+
+  def mailing_address
+  end
+
+  def primary_address
+  end
 
 end
