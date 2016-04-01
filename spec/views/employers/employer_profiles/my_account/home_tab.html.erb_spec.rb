bda5e6e6a68800488ea7e40da1aaf54352844ab6
Phillip Jacowski <phillipjacowski@gmail.com>
2016-04-01 14:21:03 -0400

    refs #6516 restore code in employer profiles home tab left behind on release-1.3

--- spec/views/employers/employer_profiles/my_account/home_tab.html.erb_spec.rb
+++ spec/views/employers/employer_profiles/my_account/home_tab.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "employers/employer_profiles/my_account/_home_tab.html.erb" do
-  context "employer profile dashboard" do
+  context "employer profile dashboard with current plan year" do
 
     let(:start_on){TimeKeeper.date_of_record.beginning_of_year}
     let(:end_on){TimeKeeper.date_of_record.end_of_year}
@@ -77,12 +77,9 @@ RSpec.describe "employers/employer_profiles/my_account/_home_tab.html.erb" do
       double(
         "Plan",
         name: "name_1",
-        active_year: 2016,
-        coverage_kind: "health",
         plan_type: "ppo",
         metal_level: "metal_level_1",
         carrier_profile: carrier_profile,
-        id: "12312312312323",
         )
     end
 
@@ -90,75 +87,45 @@ RSpec.describe "employers/employer_profiles/my_account/_home_tab.html.erb" do
       double(
         "Plan",
         name: "name_2",
-        active_year: 2016,
-        coverage_kind: "health",
         plan_type: "",
         metal_level: "metal_level_2",
         carrier_profile: carrier_profile,
-        id: "12312312312323",
-
         )
     end
 
     def benefit_group_1
       double(
         "BenefitGroup",
-        id: "someotherid",
         title: "title_1",
         effective_on_kind: "first_of_month",
         effective_on_offset: "30",
         plan_option_kind: "plan_option_kind_1",
         relationship_benefits: [relationship_benefits],
-        dental_relationship_benefits: [dental_relationship_benefits],
         reference_plan: reference_plan_1,
         reference_plan_id: double("id"),
         monthly_employer_contribution_amount: "monthly_employer_contribution_amount_1",
         monthly_min_employee_cost: "monthly_min_employee_cost_1",
         monthly_max_employee_cost: "monthly_max_employee_cost_1",
-        dental_reference_plan_id: double("id"),
-        dental_reference_plan: reference_plan_1,
-        dental_plan_option_kind: 'single_plan',
-        elected_dental_plan_ids: [:dental_reference_plan_id, :dental_reference_plan_id],
-        elected_dental_plans: [reference_plan_1]
-
-
         )
     end
 
     def benefit_group_2
       double(
         "BenefitGroup",
-        id: "someid",
         title: "title_2",
         effective_on_kind: "date_of_hire",
         effective_on_offset: "0",
         plan_option_kind: "plan_option_kind_2",
         relationship_benefits: [relationship_benefits],
-        dental_relationship_benefits: [dental_relationship_benefits],
         reference_plan: reference_plan_2,
         reference_plan_id: double("id"),
         monthly_employer_contribution_amount: "monthly_employer_contribution_amount_2",
         monthly_min_employee_cost: "monthly_min_employee_cost_2",
         monthly_max_employee_cost: "monthly_max_employee_cost_2",
-        dental_reference_plan_id: double("id"),
-        dental_reference_plan: reference_plan_2,
-        dental_plan_option_kind: 'single_plan',
-        elected_dental_plan_ids: [:dental_reference_plan_id, :dental_reference_plan_id],
-        elected_dental_plans: [reference_plan_2]
         )
     end
 
     def relationship_benefits
-      random_value = rand(999_999_999)
-      double(
-        "RelationshipBenefit",
-        offered: "offered;#{random_value}",
-        relationship: "relationship;#{random_value}",
-        premium_pct: "premium_pct;#{random_value}"
-        )
-    end
-
-    def dental_relationship_benefits
       random_value = rand(999_999_999)
       double(
         "RelationshipBenefit",
@@ -227,7 +194,6 @@ RSpec.describe "employers/employer_profiles/my_account/_home_tab.html.erb" do
     let(:benefit_groups){ [benefit_group_1, benefit_group_2] }
 
     before :each do
-      # allow(benefit_group_2).to receive(:elected_dental_plans).and_return(benefit_group_2.elected_dental_plan_ids)
       assign :employer_profile, employer_profile
       assign :hbx_enrollments, [hbx_enrollment]
       assign :current_plan_year, employer_profile.published_plan_year
@@ -247,9 +213,22 @@ RSpec.describe "employers/employer_profiles/my_account/_home_tab.html.erb" do
         expect(rendered).to match(/.*#{bg.reference_plan.plan_type}.*/mi)
       end
     end
+  end
 
-    it "should display a link to custom dental plans modal" do
-      expect(rendered).to have_selector("a", text: "View Plans")
+  context "employer profile without current plan year" do
+    let(:employer_profile){ FactoryGirl.create(:employer_profile) }
+
+    before :each do
+      assign :employer_profile, employer_profile
+      render partial: "employers/employer_profiles/my_account/home_tab"
+    end
+
+    it "should not display employee enrollment information" do
+      expect(rendered).to_not match(/Employee Enrollments and Waivers/i)
+    end
+
+    it "should display a link to download employer guidance pdf" do
+      expect(rendered).to have_selector(".icon-left-download", text: /Download Step-by-Step Instructions/i)
     end
 
   end
