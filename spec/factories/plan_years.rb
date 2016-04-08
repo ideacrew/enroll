ca9650b95698c106495b5f4281a86721a3811ba5
vinner <vineelsonnathi@users.noreply.github.com>
2016-04-07 22:08:37 -0400

    Refs #5306

--- spec/factories/plan_years.rb
+++ spec/factories/plan_years.rb
@@ -20,6 +20,16 @@ FactoryGirl.define do
     fte_count { 5 }
   end
 
+  factory :renewing_plan_year, class: PlanYear do
+    employer_profile
+    start_on { (TimeKeeper.date_of_record).next_month.beginning_of_month }
+    end_on { start_on + 1.year - 1.day }
+    open_enrollment_start_on { (start_on - 32).beginning_of_month }
+    open_enrollment_end_on { open_enrollment_start_on + 2.weeks }
+    aasm_state "renewing_enrolling"
+    fte_count { 5 }
+  end
+
   factory :plan_year_not_started, class: PlanYear do
     employer_profile
     start_on { (TimeKeeper.date_of_record + 90).beginning_of_month }
