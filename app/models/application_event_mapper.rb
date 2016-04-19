55f6a59a829a5b419c192183ed9cc32e9d429687
Raghuram <raghuramg83@gmail.com>
2016-04-19 14:43:45 -0400

    Added application event mapper with spec

--- app/models/application_event_mapper.rb
+++ app/models/application_event_mapper.rb
@@ -1,39 +1,62 @@
 class ApplicationEventMapper
+  Resource = Struct.new(:resource_name, :identifier_method, :identifier_key)
 
   EVENT_MAP = {
-      employer_profile: {
-          applicant: :created,
-# - address_changed
-# - contact_changed
-# - fein_corrected
-# - name_changed
-# - broker_added
-# - broker_terminated          
-        },
-      plan_year: {
-          published:  :benefit_coverage_initial_application_approved,
-          enrolling:  :benefit_coverage_initial_open_enrollment_began,
-          enrolled:   :benefit_coverage_initial_open_enrollment_ended,
-          draft:      :benefit_coverage_initial_application_reverted,
-          renewing_draft:       :benefit_coverage_renewal_application_period_begin,
-          renewing_published:   :benefit_coverage_renewal_application_approved,
-          renewing_enrolling:   :benefit_coverage_renewal_open_enrollment_began,
-          renewing_enrolled:    :benefit_coverage_renewal_open_enrollment_ended,
-# benefit_coverage_renewal_application_reverted
-          active: :benefit_coverage_period_begin,
-          suspended:  :benefit_coverage_period_suspended,
-          terminated: :benefit_coverage_period_terminated,
-          expired:    :benefit_coverage_period_expired,
+      employer: {
+          binder_paid: :benefit_coverage_initial_binder_paid,        
         }
     }
 
-  # Employer Profile :fein_corrected ->
-  # "acapi.info.events.employer.fein_corrected"
-  def publish_friendly_event(resource_name, current_event, from_state, to_state)
+  RESOURCE_MAP = {
+    employer_profile: Resource.new(:employer, :hbx_id, :employer_id)
+  }
 
+  class << self
+    def publish_friendly_event(resource_name, current_event, from_state, to_state)
+    end
+
+    def map_resource(resource_name)
+      RESOURCE_MAP[resource_name.to_s.underscore.to_sym]
+    end
+
+    def map_event_name(resource_mapping, transition_event_name)
+      event_name = transition_event_name.to_s.sub(/\!$/, '').to_sym
+      if EVENT_MAP[resource_mapping.resource_name][event_name].present?
+        event_name = EVENT_MAP[resource_mapping.resource_name][event_name]
+      end
+      "acapi.info.events.employer.#{event_name}"
+    end
   end
-
 end
+
+
+#   EVENT_MAP = {
+#       employer_profile: {
+#           applicant: :created,
+# # - address_changed
+# # - contact_changed
+# # - fein_corrected
+# # - name_changed
+# # - broker_added
+# # - broker_terminated          
+#         },
+#       plan_year: {
+#           published:  :benefit_coverage_initial_application_approved,
+#           enrolling:  :benefit_coverage_initial_open_enrollment_began,
+#           enrolled:   :benefit_coverage_initial_open_enrollment_ended,
+#           draft:      :benefit_coverage_initial_application_reverted,
+#           renewing_draft:       :benefit_coverage_renewal_application_period_begin,
+#           renewing_published:   :benefit_coverage_renewal_application_approved,
+#           renewing_enrolling:   :benefit_coverage_renewal_open_enrollment_began,
+#           renewing_enrolled:    :benefit_coverage_renewal_open_enrollment_ended,
+# # benefit_coverage_renewal_application_reverted
+#           active: :benefit_coverage_period_begin,
+#           suspended:  :benefit_coverage_period_suspended,
+#           terminated: :benefit_coverage_period_terminated,
+#           expired:    :benefit_coverage_period_expired,
+#         }
+#     }
+
 
       # <xs:enumeration value="urn:openhbx:events:v1:employer#created"/>
       # <xs:enumeration value="urn:openhbx:events:v1:employer#address_changed"/>
--- spec/models/application_event_mapper_spec.rb
+++ spec/models/application_event_mapper_spec.rb
@@ -1,5 +1,48 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe ApplicationEventMapper, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ApplicationEventMapper do
+  describe "providing a resource mapping for EmployerProfile" do
+    let(:resource_mapping) { ApplicationEventMapper.map_resource(EmployerProfile) }
+    subject { resource_mapping }
+
+    it "has the correct resource name" do
+      expect(subject.resource_name).to eq :employer
+    end
+
+    it "has the correct identifier method" do
+      expect(subject.identifier_method).to eq :hbx_id
+    end
+
+    it "has the correct identifier key" do
+      expect(subject.identifier_key).to eq :employer_id
+    end
+
+    describe "and it's #binder_paid event" do
+      let(:transition_event_name) { :binder_paid }
+      subject { ApplicationEventMapper.map_event_name(resource_mapping, transition_event_name) }
+
+      it "provides the mapping to #benefit_coverage_initial_binder_paid" do
+        expect(subject).to eq "acapi.info.events.employer.benefit_coverage_initial_binder_paid"
+      end
+    end
+
+    describe "and it's #binder_paid! event" do
+      let(:transition_event_name) { :binder_paid! }
+      subject { ApplicationEventMapper.map_event_name(resource_mapping, transition_event_name) }
+
+      it "provides the mapping to #benefit_coverage_initial_binder_paid" do
+      expect(subject).to eq "acapi.info.events.employer.benefit_coverage_initial_binder_paid"
+      end
+    end
+
+    describe "and an arbitrary event with no mapping" do
+      let(:transition_event_name) { :arglebargle_gsnug_shibuh! }
+      let(:expected_event_name) { "acapi.info.events.employer.arglebargle_gsnug_shibuh" }
+      subject { ApplicationEventMapper.map_event_name(resource_mapping, transition_event_name) }
+
+      it "provides the same mapping back, stripping any exclamation points" do
+        expect(subject).to eq expected_event_name
+      end
+    end
+  end
 end
