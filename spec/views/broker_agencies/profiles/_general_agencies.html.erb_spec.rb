b6a7d954cefdc2caf66051a0d9fff127ffbc4e04
Ken Lu <ken@seeice.com>
2016-04-21 08:40:33 -0400

    Refs 6796 - add test for GA Default Selection

--- spec/views/broker_agencies/profiles/_general_agencies.html.erb_spec.rb
+++ spec/views/broker_agencies/profiles/_general_agencies.html.erb_spec.rb
@@ -4,30 +4,63 @@ RSpec.describe "broker_agencies/profiles/_general_agencies.html.erb" do
   let(:general_agency_profile) { FactoryGirl.create(:general_agency_profile) }
   let(:broker_agency_profile) { FactoryGirl.create(:broker_agency_profile) }
 
-  before :each do
-    assign :general_agency_profiles, [general_agency_profile]
-    assign :broker_agency_profile, broker_agency_profile
-    render template: "broker_agencies/profiles/_general_agencies.html.erb" 
+  context "without default_general_agency_profile" do
+    before :each do
+      allow(broker_agency_profile).to receive(:default_general_agency_profile).and_return nil
+      assign :general_agency_profiles, [general_agency_profile]
+      assign :broker_agency_profile, broker_agency_profile
+      render template: "broker_agencies/profiles/_general_agencies.html.erb" 
+    end
+
+    it 'should have title' do
+      expect(rendered).to have_selector('h3', text: 'General Agencies')
+    end
+
+    it "should have button of clear default ga" do
+      expect(rendered).to have_selector('a', text: 'Clear Default GA')
+    end
+
+    it "should have general_agency_profile" do
+      expect(rendered).to have_selector('td', text: "#{general_agency_profile.dba}")
+      expect(rendered).to have_selector('td', text: "#{general_agency_profile.fein}")
+    end
+
+    it "should have link of set default ga" do
+      expect(rendered).to have_selector('a', text: 'Select Default GA')
+    end
+
+    it "should have hint msg for select default GA" do
+      expect(rendered).to have_content("You do not have default GA, to select your default GA click \"Select Default GA\" under your desired agency")
+    end
   end
 
-  it 'should have title' do
-    expect(rendered).to have_selector('h3', text: 'General Agencies')
-  end
+  context "with default_general_agency_profile" do
+    before :each do
+      allow(broker_agency_profile).to receive(:default_general_agency_profile).and_return general_agency_profile
+      assign :general_agency_profiles, [general_agency_profile]
+      assign :broker_agency_profile, broker_agency_profile
+      render template: "broker_agencies/profiles/_general_agencies.html.erb" 
+    end
 
-  it "should have button of clear default ga" do
-    expect(rendered).to have_selector('a', text: 'Clear Default GA')
-  end
+    it 'should have title' do
+      expect(rendered).to have_selector('h3', text: 'General Agencies')
+    end
 
-  it "should have general_agency_profile" do
-    expect(rendered).to have_selector('td', text: "#{general_agency_profile.dba}")
-    expect(rendered).to have_selector('td', text: "#{general_agency_profile.fein}")
-  end
+    it "should have button of clear default ga" do
+      expect(rendered).to have_selector('a', text: 'Clear Default GA')
+    end
 
-  it "should have link of set default ga" do
-    expect(rendered).to have_selector('a', text: 'Select Default GA')
-  end
+    it "should have general_agency_profile" do
+      expect(rendered).to have_selector('td', text: "#{general_agency_profile.dba}")
+      expect(rendered).to have_selector('td', text: "#{general_agency_profile.fein}")
+    end
 
-  it "should have hint msg for select default GA" do
-    expect(rendered).to have_content("your default GA click \"Select Default GA\" under your desired agency")
+    it "should show Default GA" do
+      expect(rendered).to have_selector('td', text: 'Default GA')
+    end
+
+    it "should have hint msg for select default GA" do
+      expect(rendered).to have_content("#{broker_agency_profile.legal_name} - this is your default GA, to change your default GA click \"Select Default GA\" under your desired agency")
+    end
   end
 end
