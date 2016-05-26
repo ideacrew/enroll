ae2ba59acb2bfd2d36ae37a1136b7ff67999bb30
Phillip Jacowski <phillipjacowski@gmail.com>
2016-05-26 12:20:23 -0400

    refs #6525 exclude by entire plan resource instead of aasm state in sort_plan_years method

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -244,10 +244,11 @@ class Employers::EmployerProfilesController < Employers::EmployersController
   private
 
   def sort_plan_years(plans)
+    debugger
     renewing_states = PlanYear::RENEWING_PUBLISHED_STATE + PlanYear::RENEWING
     renewing = plans.select { |plan_year| renewing_states.include? plan_year.aasm_state }
     ineligible_plans, active_plans = plans.partition { |plan_year| PlanYear::INELIGIBLE_FOR_EXPORT_STATES.include? plan_year.aasm_state }
-    ineligible_plans = ineligible_plans.select { |plan_year| renewing.exclude? plan_year.aasm_state }
+    ineligible_plans = ineligible_plans.select { |plan_year| renewing.exclude? plan_year }
     active_plans = active_plans.partition { |plan_year| PlanYear::PUBLISHED.include? plan_year.aasm_state }.flatten
     @plan_years = renewing + active_plans + ineligible_plans
   end
