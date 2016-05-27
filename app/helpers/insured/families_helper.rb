89a86788eb23f86b8a5ec46e2f60913d2ee14eb7
Phillip Jacowski <phillipjacowski@gmail.com>
2016-05-27 10:16:48 -0400

    refs #5300 add waived date to coverage waived widget, clean up coverage waived widget template and enrollment panel styles, add spec

--- app/helpers/insured/families_helper.rb
+++ app/helpers/insured/families_helper.rb
@@ -32,12 +32,24 @@ module Insured::FamiliesHelper
     policy.created_at.in_time_zone('Eastern Time (US & Canada)')
   end
 
+  def shift_waived_time(policy)
+    policy.submitted_at.in_time_zone('Eastern Time (US & Canada)')
+  end
+
   def format_policy_purchase_date(policy)
     format_date(shift_purchase_time(policy))
   end
 
   def format_policy_purchase_time(policy)
     shift_purchase_time(policy).strftime("%-I:%M%p")
+  end
+
+  def format_policy_waived_date(policy)
+    format_date(shift_waived_time(policy))
+  end
+
+  def format_policy_waived_time(policy)
+    shift_waived_time(policy).strftime("%-I:%M%p")
   end
 
   def render_plan_type_details(plan)
@@ -108,6 +120,6 @@ module Insured::FamiliesHelper
   def display_aasm_state?(aasm_state)
     if aasm_state == "coverage_selected" || aasm_state == "coverage_canceled" || aasm_state == "coverage_terminated"
      true
-    end  
-  end  
+    end
+  end
 end
