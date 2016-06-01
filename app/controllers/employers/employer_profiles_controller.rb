f9b091e91a555349ffaf8adec036792cf41fb71d
sarathpyerra <sarathpyerra@gmail.com>
2016-06-01 10:31:43 -0400

    POCs Need Confirmation Page After Submitting Application Page

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -157,8 +157,8 @@ class Employers::EmployerProfilesController < Employers::EmployersController
       @person = current_user.person
       create_sso_account(current_user, current_user.person, 15, "employer") do
         if pending
-          flash[:notice] = 'Your Employer Staff application is pending'
-          render action: 'new'
+          # flash[:notice] = 'Your Employer Staff application is pending'
+          render action: 'show_pending'
         else
           redirect_to employers_employer_profile_path(@organization.employer_profile, tab: 'home')
         end
@@ -166,6 +166,9 @@ class Employers::EmployerProfilesController < Employers::EmployersController
     else
       render action: "new"
     end
+  end
+
+  def show_pending
   end
 
   def update
