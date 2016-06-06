26729740035bc25dcc3015431bd9b312826f6b3e
Tony Schaffert <antonio.schaffert@gmail.com>
2016-06-06 19:54:12 -0400

    Fix scope of resulset

--- app/models/plan_year.rb
+++ app/models/plan_year.rb
@@ -10,8 +10,8 @@ class PlanYear
   RENEWING  = %w(renewing_draft renewing_published renewing_enrolling renewing_enrolled)
   RENEWING_PUBLISHED_STATE = %w(renewing_published renewing_enrolling renewing_enrolled)
 
-  INVOICE_VIEW_RENEWING = %w(renewing_published renewing_enrolling)
-  INVOICE_VIEW_INITIAL = %w(published enrolling)
+  INVOICE_VIEW_RENEWING = %w(renewing_published renewing_enrolling renewing_enrolled)
+  INVOICE_VIEW_INITIAL = %w(published enrolling enrolled active suspended)
 
   INELIGIBLE_FOR_EXPORT_STATES = %w(draft publish_pending eligibility_review published_invalid canceled renewing_draft suspended terminated ineligible expired)
 
