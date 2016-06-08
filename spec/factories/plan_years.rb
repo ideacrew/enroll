6d97d00941c1cf0d83471a2e5958535073bfd6fe
sarathpyerra <sarathpyerra@gmail.com>
2016-06-08 10:30:56 -0400

    fixed some rspec tests

--- spec/factories/plan_years.rb
+++ spec/factories/plan_years.rb
@@ -5,16 +5,16 @@ FactoryGirl.define do
     start_on { (TimeKeeper.date_of_record).beginning_of_month }
 
     end_on { start_on + 1.year - 1.day }
-    open_enrollment_start_on { (start_on - 32).beginning_of_month }
-    open_enrollment_end_on { open_enrollment_start_on + 2.weeks }
+    open_enrollment_start_on { (start_on - 30).beginning_of_month }
+    open_enrollment_end_on { open_enrollment_start_on + 1.weeks }
     fte_count { 5 }
   end
   factory :plan_year_not_started, class: PlanYear do
     employer_profile
     start_on { (TimeKeeper.date_of_record + 90).beginning_of_month }
     end_on { start_on + 1.year - 1.day }
-    open_enrollment_start_on { (start_on - 32).beginning_of_month }
-    open_enrollment_end_on { open_enrollment_start_on + 2.weeks }
+    open_enrollment_start_on { (start_on - 30).beginning_of_month }
+    open_enrollment_end_on { open_enrollment_start_on + 1.weeks }
     fte_count { 5 }
   end
 end
