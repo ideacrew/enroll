17ad783498c292ed58a0895d9e82f597a9f70c36
202chaz <chaz.haskins@gmail.com>
2016-06-09 13:54:11 -0400

    urls relative

--- script/remove_prod_urls_from_bookmarks.rb
+++ script/remove_prod_urls_from_bookmarks.rb
@@ -4,10 +4,14 @@ p.each do |c|
   if c.present?
     if c.consumer_role.present?
       if c.consumer_role.bookmark_url.present?
-        link = c.consumer_role.bookmark_url.gsub(/enroll.dchealthlink.com/, 'enroll-preprod.dchbx.org')
+        link = c.consumer_role.bookmark_url.gsub("https://enroll.dchealthlink.com", '')
         c.consumer_role.update_attributes!(bookmark_url:link)
         p c.consumer_role.bookmark_url
       end
     end
   end
 end
+
+
+
+
