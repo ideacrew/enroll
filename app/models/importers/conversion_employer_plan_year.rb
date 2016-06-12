692076821ffdb36fff94897b91008ddf14112edd
Trey Evans <lewis.r.evans@gmail.com>
2016-06-12 17:26:23 -0400

    More duplication removal.

--- app/models/importers/conversion_employer_plan_year.rb
+++ app/models/importers/conversion_employer_plan_year.rb
@@ -1,28 +1,5 @@
 module Importers
-  class ConversionEmployerPlanYear
-    NewHireCoveragePolicy = Struct.new(:kind, :offset)
-
-    include ActiveModel::Validations
-    include ActiveModel::Model
-
-    HIRE_COVERAGE_POLICIES = {
-      "date of hire equal to effective date" => NewHireCoveragePolicy.new("date_of_hire", 0),
-      "first of the month following 30 days" => NewHireCoveragePolicy.new("first_of_month", 30),
-      "first of the month following 60 days" => NewHireCoveragePolicy.new("first_of_month", 60),
-      "first of the month following date of hire" => NewHireCoveragePolicy.new("first_of_month", 0)
-    }
-
-    attr_reader :fein, :plan_selection, :carrier
-
-    attr_accessor :action,
-      :enrolled_employee_count,
-      :new_coverage_policy,
-      :default_plan_year_start,
-      :most_common_hios_id,
-      :single_plan_hios_id,
-      :reference_plan_hios_id,
-      :coverage_start
-
+  class ConversionEmployerPlanYear < ConversionEmployerPlanYearCommon
     validates_length_of :fein, is: 9
 
     validate :validate_fein
@@ -30,29 +7,6 @@ module Importers
 
     validates_presence_of :plan_selection, :allow_blank => false
     validates_numericality_of :enrolled_employee_count, :allow_blank => false
-
-    attr_reader :warnings
-
-    include ::Importers::ConversionEmployerCarrierValue
-
-    def initialize(opts = {})
-      super(opts)
-      @warnings = ActiveModel::Errors.new(self)
-    end
-
-    include ValueParsers::OptimisticSsnParser.on(:fein)
-
-    def new_coverage_policy=(val)
-      if val.blank?
-        @new_coverage_policy = nil
-        return val
-      end
-      @new_coverage_policy = HIRE_COVERAGE_POLICIES[val.strip.downcase]
-    end
-
-    def plan_selection=(val)
-      @plan_selection = (val.to_s =~ /single plan/i) ? "single_plan" : "single_carrier"
-    end
 
     def validate_fein
       return true if fein.blank?
@@ -162,72 +116,6 @@ module Importers
         end
       end
       return save_result
-    end
-
-    def find_and_update_plan_year
-      employer = find_employer
-      found_carrier = find_carrier
-
-      puts "Processing....#{employer.legal_name}...#{employer.fein}"
-
-      current_coverage_start = Date.strptime(coverage_start, "%m/%d/%y")
-      
-      available_plans = Plan.valid_shop_health_plans("carrier", found_carrier.id, current_coverage_start.year - 1)
-      reference_plan = select_reference_plan(available_plans)
-
-      if reference_plan.blank?
-        errors.add(:base, 'Unable to find a Reference plan with given Hios ID')
-      end
-
-      if single_plan_hios_id.blank? && most_common_hios_id.blank? && reference_plan_hios_id.blank?
-        errors.add(:base, 'Reference Plan Hios Id missing')
-      end
-
-      if plan_selection == 'single_plan' && single_plan_hios_id.blank?
-        errors.add(:base, 'Single Plan Hios Id missing')
-      end
-
-      plan_year = employer.plan_years.where(:start_on => current_coverage_start - 1.year).first
-      if plan_year.blank?
-        errors.add(:base, 'Plan year not imported')
-      end
-
-      if plan_year && plan_year.benefit_groups.size > 1
-        errors.add(:base, 'Employer offering more than 1 benefit package')
-      end
-
-      renewing_plan_year = employer.plan_years.where(:start_on => (current_coverage_start)).first
-      if renewing_plan_year.blank?
-        errors.add(:base, 'Renewing plan year not present')
-      end
-
-      if renewing_plan_year && PlanYear::RENEWING_PUBLISHED_STATE.include?(renewing_plan_year.aasm_state)
-        errors.add(:base, "Renewing plan year already published. Reference plan can't be updated")
-      end
-
-      return false if errors.present?
-      if plan_year.benefit_groups[0].reference_plan.hios_id != reference_plan.hios_id
-        update_reference_plan(plan_year, reference_plan)
-        renewal_reference_plan = Plan.find(reference_plan.renewal_plan_id)
-        update_reference_plan(renewing_plan_year, renewal_reference_plan)
-        return true
-      else
-        errors.add(:base, "Reference plan is same")
-        return false
-      end
-    end
-
-    def update_reference_plan(plan_year, reference_plan)
-      plan_year.benefit_groups.each do |benefit_group|
-        benefit_group.reference_plan= reference_plan
-        benefit_group.elected_plans= benefit_group.elected_plans_by_option_kind
-        benefit_group.save!
-      end
-    end
-
-    def update
-      return false unless valid?
-      find_and_update_plan_year
     end
 
     def map_employees_to_benefit_groups(employer, plan_year)
--- app/models/importers/conversion_employer_plan_year_update.rb
+++ app/models/importers/conversion_employer_plan_year_update.rb
@@ -1,28 +1,5 @@
 module Importers
-  class ConversionEmployerPlanYearUpdate
-    NewHireCoveragePolicy = Struct.new(:kind, :offset)
-
-    include ActiveModel::Validations
-    include ActiveModel::Model
-
-    HIRE_COVERAGE_POLICIES = {
-      "date of hire equal to effective date" => NewHireCoveragePolicy.new("date_of_hire", 0),
-      "first of the month following 30 days" => NewHireCoveragePolicy.new("first_of_month", 30),
-      "first of the month following 60 days" => NewHireCoveragePolicy.new("first_of_month", 60),
-      "first of the month following date of hire" => NewHireCoveragePolicy.new("first_of_month", 0)
-    }
-
-    attr_reader :fein, :plan_selection, :carrier
-
-    attr_accessor :action,
-      :enrolled_employee_count,
-      :new_coverage_policy,
-      :default_plan_year_start,
-      :most_common_hios_id,
-      :single_plan_hios_id,
-      :reference_plan_hios_id,
-      :coverage_start
-
+  class ConversionEmployerPlanYearUpdate < ConversionEmployerPlanYearCommon
     validates_length_of :fein, is: 9
 
     validate :validate_fein
@@ -30,29 +7,6 @@ module Importers
 
     validates_presence_of :plan_selection, :allow_blank => false
     validates_numericality_of :enrolled_employee_count, :allow_blank => false
-
-    attr_reader :warnings
-
-    include ::Importers::ConversionEmployerCarrierValue
-
-    def initialize(opts = {})
-      super(opts)
-      @warnings = ActiveModel::Errors.new(self)
-    end
-
-    include ValueParsers::OptimisticSsnParser.on(:fein)
-
-    def new_coverage_policy=(val)
-      if val.blank?
-        @new_coverage_policy = nil
-        return val
-      end
-      @new_coverage_policy = HIRE_COVERAGE_POLICIES[val.strip.downcase]
-    end
-
-    def plan_selection=(val)
-      @plan_selection = (val.to_s =~ /single plan/i) ? "single_plan" : "single_carrier"
-    end
 
     def validate_fein
       return true if fein.blank?
