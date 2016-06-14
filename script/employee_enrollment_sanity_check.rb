631a99f19fe918cddf6124482dc2ace038be731a
Raghuram <raghuramg83@gmail.com>
2016-06-14 14:59:03 -0400

    Added scripts to verify enrollments with employer elected plans

--- script/employee_enrollment_sanity_check.rb
+++ script/employee_enrollment_sanity_check.rb
@@ -7,30 +7,45 @@ orgs = Organization.where({:"employer_profile.plan_years" => {
 count = 0
 invlid_renewals = []
 orgs.each do |org|
-  puts "processing #{org.legal_name}"
-
+  # puts "processing #{org.legal_name}"
   renewal_plan_year = org.employer_profile.plan_years.renewing.first
   elected_plans = renewal_plan_year.benefit_groups.first.elected_plan_ids
   id_list = renewal_plan_year.benefit_groups.collect(&:_id).uniq
 
   families = Family.where(:"households.hbx_enrollments.benefit_group_id".in => id_list)
   enrollments = families.inject([]) do |enrollments, family|
-    enrollments += family.active_household.hbx_enrollments.where(:benefit_group_id.in => id_list).where({ 
+    policies = family.active_household.hbx_enrollments.where(:benefit_group_id.in => id_list).where({ 
       :aasm_state.in => (HbxEnrollment::ENROLLED_STATUSES + HbxEnrollment::RENEWAL_STATUSES)
       })
+    
+    coverage = policies.detect{|x| x.aasm_state == 'coverage_selected'}
+
+    if coverage.blank?
+      coverage = policies.detect{|x| x.aasm_state == 'auto_renewing'}
+    end
+    
+    enrollments << coverage
   end
 
-  puts "found #{enrollments.size} enrollments"
-  enrollments.each do |enrollment|
+  people = []
+
+  # puts "found #{enrollments.size} enrollments"
+  enrollments.compact.each do |enrollment|
     if !elected_plans.include?(enrollment.plan_id)
-      count += 1
       # puts "#{enrollment.plan_id.inspect} not offered by the employer #{elected_plans.inspect}"
-      puts enrollment.subscriber.person.full_name
-      invlid_renewals << org.legal_name + "---#{org.fein}"
+      people << enrollment.subscriber.person.full_name
+      # invlid_renewals << org.legal_name + "---#{org.fein}"
       # break
     end
+  end
+
+  if people.any?
+    puts "----#{org.legal_name}"
+    puts people.inspect
+    puts "----------------------"
+    count += 1
   end
 end
 
 puts count
-puts invlid_renewals.uniq
+# puts invlid_renewals.uniq
