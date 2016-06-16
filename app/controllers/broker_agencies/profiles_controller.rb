888a57b1d333c5657790fd07b9262a8780ae1517
Kevin Wei <kevin.tw.wei@gmail.com>
2016-06-16 12:32:27 -0400

    Refs #5388 fix specs, clean up controller

--- app/controllers/broker_agencies/profiles_controller.rb
+++ app/controllers/broker_agencies/profiles_controller.rb
@@ -142,11 +142,10 @@ class BrokerAgencies::ProfilesController < ApplicationController
       @orgs = Organization.by_broker_role(broker_role_id)
     end
 
-    @organizations = @orgs
     @page_alphabets = page_alphabets(@orgs, "legal_name")
     page_no = cur_page_no(@page_alphabets)
     @organizations = @orgs.where("legal_name" => /^#{page_no}/i) unless @orgs.blank?
-    @employer_profiles = @organizations.map {|o| o.employer_profile}
+    @employer_profiles = @organizations.map {|o| o.employer_profile} unless @orgs.blank?
 
     @broker_role = current_user.person.broker_role || nil
     @general_agency_profiles = GeneralAgencyProfile.all_by_broker_role(@broker_role, approved_only: true)
