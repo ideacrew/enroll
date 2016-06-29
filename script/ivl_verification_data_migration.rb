aa7ba36208d9856c0ef65f01373363db0e2df07a
lisyk <sergey.liskovoy@gmail.com>
2016-06-29 16:44:25 -0400

    Refs #7057 add one more case to datamigration script, short fixes

--- script/ivl_verification_data_migration.rb
+++ script/ivl_verification_data_migration.rb
@@ -22,7 +22,7 @@
 # case 4  |  "pending"     | "ssa"      |  "true"  | "true"    | "pending, retrigger"   | "pending"     | "pending"
 # case 5  |  "pending"     | "dhs"      |  "false" | "false"   | "pending, retrigger"   | "pending"     | "n/a"
 # case 6  |  "valid"       | "ssa"      |  "true"  | "true"    | "valid"                | "valid"       | "valid"
-### -- skip for now -- case 7  |  "_"           | "ssa"      |  "true"  | "false"   | "pending, retrigger"   | "pending"     | "pending"
+# case 7  |  "pending"     | "ssa"      |  "true"  | "false"   | "outstanding"          | "outstanding" | "outstanding"
 # case 8  |  "valid"       | "dhs"      |  "false" | "false"   | "valid"                | "valid"       | "n/a"
 # case 9  |  "_"           | "curam"    |  "_"     | "true"    | "valid"                | "valid"       | "valid"
 # case 10 |  "_"           | "curam"    |  "_"     | "false"   | "valid"                | "valid"       | "n/a"
@@ -31,87 +31,88 @@
 
 # outstanding, native, ssn  | count: 401
 def get_case_1_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_outstanding',
                'consumer_role.lawful_presence_determination.citizen_status' => 'us_citizen',
-               'encrypted_ssn' => {:$exists=>1})
+               'encrypted_ssn' => {'$exists' => true})
 
 end
 
 # outstanding, native, NO ssn  | count: 209
 def get_case_2_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_outstanding',
                'consumer_role.lawful_presence_determination.citizen_status' => 'us_citizen',
-               'encrypted_ssn' => {:$exists=>0})
+               'encrypted_ssn' => {'$exists' => false})
 end
 
 # outstanding, NON native, NO ssn  | count: 61
 def get_case_3_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_outstanding',
                'consumer_role.lawful_presence_determination.citizen_status' => {'$ne' => 'us_citizen'},
-               'encrypted_ssn' => {:$exists=>0})
+               'encrypted_ssn' => {'$exists' => false})
 end
 
 # pending, native, ssn  | count: 8785
 def get_case_4_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_pending',
                'consumer_role.lawful_presence_determination.citizen_status' => 'us_citizen',
-               'encrypted_ssn' => {:$exists=>1})
+               'encrypted_ssn' => {'$exists' => true})
 end
 
 # pending, NO native, NO ssn   | count: 1091
 def get_case_5_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_pending',
                'consumer_role.lawful_presence_determination.citizen_status' => {'$ne' => 'us_citizen'},
-               'encrypted_ssn' => {:$exists=>0})
+               'encrypted_ssn' => {'$exists' => false})
 end
 
 # verified, native, ssn    | count: 27891
 def get_case_6_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_successful',
                'consumer_role.lawful_presence_determination.citizen_status' => 'us_citizen',
-               'encrypted_ssn' => {:$exists=>1})
+               'encrypted_ssn' => {'$exists' => true})
 end
 
-# def get_case_7_people
-#   Person.where('consumer_role' => {'$exists' => 1},
-#                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_successful',
-#                'consumer_role.lawful_presence_determination.citizen_status' => 'us_citizen',
-#                'encrypted_ssn' => {:$exists=>1})
-# end
+# pending, native, NO ssn  | count: 1084
+def get_case_7_people
+  Person.where('consumer_role' => {'$exists' => true},
+               'consumer_role.lawful_presence_determination.aasm_state' => 'verification_pending',
+               'consumer_role.lawful_presence_determination.citizen_status' => 'us_citizen',
+               'encrypted_ssn' => {'$exists' => false}).count
+end
 
 # verified, NO native, NO ssn  | count: 441
 def get_case_8_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.aasm_state' => 'verification_successful',
                'consumer_role.lawful_presence_determination.citizen_status' => {'$ne' => 'us_citizen'},
-               'encrypted_ssn' => {:$exists=>0})
+               'encrypted_ssn' => {'$exists' => false})
 end
 
 #Curam, ssn   | count: 39338
 def get_case_9_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.vlp_authority' => 'curam',
-               'encrypted_ssn' => {:$exists=>1})
+               'encrypted_ssn' => {'$exists' => true})
 end
 
 #Curam, NO ssn   | count:  1575
 def get_case_10_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.vlp_authority' => 'curam',
-               'encrypted_ssn' => {:$exists=>0})
+               'encrypted_ssn' => {'$exists' => false})
 end
 
 #NON native, ssn   | count: 62955
 def get_case_11_people
-  Person.where('consumer_role' => {'$exists' => 1},
+  Person.where('consumer_role' => {'$exists' => true},
                'consumer_role.lawful_presence_determination.citizen_status' => {'$ne' => 'us_citizen'},
-               'encrypted_ssn' => {:$exists=>1})
+               'encrypted_ssn' => {'$exists' => true})
 end
 
 
@@ -122,7 +123,12 @@ def consumer_to_outstanding(person)
   person.consumer_role.aasm_state = "verification_outstanding"
 end
 
+def consumer_to_unverified(person)
+  person.consumer_role.aasm_state = "unverified"
+end
+
 def retrigger(person)
+  consumer_to_unverified(person)
   person.consumer_role.invoke_verification!
 end
 
@@ -302,6 +308,27 @@ def migration_6
   end
 end
 
+def migration_7
+  puts "Start runnung #{__method__}..."
+  people = get_case_7_people
+  puts "#{people.count} records will be fixed."
+  errors=0
+  people.each do |person|
+    begin
+      move_all_to_outstanding(person)
+      person.save!
+    rescue => e
+      errors+=1
+      puts "data migration error for person: #{person.id}. Error: #{e.message}"
+    end
+  end
+  if errors > 0
+    puts "You have #{errors} errors for #{__method__}"
+  else
+    puts "No errors for #{__method__}"
+  end
+end
+
 def migration_8
   puts "Start runnung #{__method__}..."
   people = get_case_8_people
@@ -399,6 +426,7 @@ def init_migration!
   migration_4
   migration_5
   migration_6
+  migration_7
   migration_8
   migration_9
   migration_10
