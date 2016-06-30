2b370c359a604ff22ebea5ed510b38e9aab31abe
Phillip Jacowski <phillipjacowski@gmail.com>
2016-06-30 12:03:37 -0500

    refs #7988 place iam_login_url in session expiration warning modal and in custom failure app

--- lib/custom_failure_app.rb
+++ lib/custom_failure_app.rb
@@ -2,7 +2,7 @@ class CustomFailureApp < Devise::FailureApp
   def redirect
     message = warden.message || warden_options[:message]
     if (message == :timeout)
-      redirect_to('https://www.dchealthlink.com/') and return
+      redirect_to("#{SamlInformation.iam_login_url}") and return
     else
       super
     end
