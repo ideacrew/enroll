a339717f76fadbcd81797313cefea4cf5a0f845d
Phillip Jacowski <phillipjacowski@gmail.com>
2016-07-07 12:56:58 -0400

    refs #7922 make my portal links dropdown toggle blue until dismissed

--- app/assets/javascripts/hints.js
+++ app/assets/javascripts/hints.js
@@ -1,8 +1,9 @@
 var Hints = ( function( window, undefined ) {
 
   function myPortalsLinkReminder() {
+    $('.my-portal-links .dropdown-toggle').css({'color': '#007bc4;'});
     $('.dropdown[data-toggle="popover"]').popover('show');
-    $('.popover-title').append('<button id="popovercloseid" type="button" class="close" style="position: absolute; top: 10px; right: 15px;" title="Close" data-toggle="tooltip">&times;</button>');
+    $('.popover-title').append('<button id="popovercloseid" type="button" class="close" style="position: absolute; top: 10px; right: 15px;" title="Don\'t Show Again" data-toggle="tooltip">&times;</button>');
     Freebies.tooltip();
     $('html').on('click', function(e) {
       if (typeof $(e.target).data('original-title') == 'undefined') {
