2239539533a981b75475ba7a716a23538eceb708
lisyk <sergey.liskovoy@gmail.com>
2016-07-07 21:39:42 -0400

    Refs #8185 datafix script for curam users, move all to verified states

--- app/data_migrations/correct_curam_vlp_status.rb
+++ app/data_migrations/correct_curam_vlp_status.rb
@@ -1,7 +1,31 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class CorrectCuramVlpStatus < MongoidMigrationTask
+  def get_people
+    Person.where('consumer_role' => {'$exists' => true},
+                 'consumer_role.lawful_presence_determination.vlp_authority' => 'curam')
+  end
+
+  def update_person(person)
+    begin
+      person.consumer_role.lawful_presence_determination.aasm_state = "verification_successful"
+      person.consumer_role.import!
+      person.save!
+      print "."
+    rescue => e
+      puts
+      puts "Error for Person id: #{person.id}. Error: #{e.message}"
+      puts
+    end
+  end
+
   def migrate
-    puts "MIGRATION"
+    people_to_fix = get_people
+    puts
+    puts "#{people_to_fix.count} records will be fixed."
+    puts
+    people_to_fix.each do |person|
+      update_person(person)
+    end
   end
 end
--- spec/data_migrations/correct_curam_vlp_status_spec.rb
+++ spec/data_migrations/correct_curam_vlp_status_spec.rb
@@ -13,12 +13,39 @@ describe CorrectCuramVlpStatus do
 
   describe "given a person who has a vlp authority of 'curam', in the 'pending' status" do
     subject { CorrectCuramVlpStatus.new("fix me task", double(:current_scope => nil)) }
+    let(:curam_user) { FactoryGirl.create(:person, :with_consumer_role)}
 
-    it "moves the person to 'fully verified'"
+    before :each do
+      curam_user.consumer_role.lawful_presence_determination.vlp_authority = "curam"
+      curam_user.consumer_role.lawful_presence_determination.aasm_state = "verification_pending"
+      curam_user.consumer_role.aasm_state = "verification_outstanding"
+      curam_user.save!
+      subject.migrate
+    end
+    it "moves the person to 'fully verified'" do
+      curam_user.reload
+      expect(curam_user.consumer_role.lawful_presence_determination.vlp_authority).to eq "curam"
+      expect(curam_user.consumer_role.lawful_presence_determination.aasm_state).to eq "verification_successful"
+      expect(curam_user.consumer_role.aasm_state).to eq "fully_verified"
+    end
   end
 
   describe "given a person who has a vlp authority of 'ssa', in the 'pending' status" do
     subject { CorrectCuramVlpStatus.new("fix me task", double(:current_scope => nil)) }
-    it "does not change the state of the person"
+    let(:user) { FactoryGirl.create(:person, :with_consumer_role)}
+
+    before :each do
+      user.consumer_role.lawful_presence_determination.vlp_authority = "ssa"
+      user.consumer_role.lawful_presence_determination.aasm_state = "verification_pending"
+      user.consumer_role.aasm_state = "verification_outstanding"
+      user.save!
+      subject.migrate
+    end
+    it "does not change the state of the person"do
+      user.reload
+      expect(user.consumer_role.lawful_presence_determination.vlp_authority).to eq "ssa"
+      expect(user.consumer_role.lawful_presence_determination.aasm_state).to eq "verification_pending"
+      expect(user.consumer_role.aasm_state).to eq "verification_outstanding"
+    end
   end
 end
