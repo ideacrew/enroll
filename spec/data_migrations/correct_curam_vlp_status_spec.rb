e0a8782809a3d965889564defe72c66db1eb0da5
lisyk <sergey.liskovoy@gmail.com>
2016-07-08 14:04:51 -0400

    Refs #8185 spec for curam with and with no ssn

--- spec/data_migrations/correct_curam_vlp_status_spec.rb
+++ spec/data_migrations/correct_curam_vlp_status_spec.rb
@@ -13,40 +13,83 @@ describe CorrectCuramVlpStatus do
 
   describe "given a person who has a vlp authority of 'curam', in the 'pending' status" do
     subject { CorrectCuramVlpStatus.new("fix me task", double(:current_scope => nil)) }
-    let(:curam_user) { FactoryGirl.create(:person, :with_consumer_role)}
-    before :each do
-      curam_user.consumer_role.lawful_presence_determination.vlp_authority = "curam"
-      curam_user.consumer_role.lawful_presence_determination.aasm_state = "verification_pending"
-      curam_user.consumer_role.aasm_state = "verification_outstanding"
-      curam_user.save!
-      subject.migrate
-      curam_user.reload
-    end
-    it "moves the person consumer_role to 'fully verified'" do
-      expect(curam_user.consumer_role.aasm_state).to eq "fully_verified"
+    context "people with SSN" do
+      let(:curam_user) { FactoryGirl.create(:person, :with_consumer_role)}
+      before :each do
+        curam_user.consumer_role.lawful_presence_determination.vlp_authority = "curam"
+        curam_user.consumer_role.lawful_presence_determination.aasm_state = "verification_pending"
+        curam_user.consumer_role.aasm_state = "verification_outstanding"
+        curam_user.save!
+        subject.migrate
+        curam_user.reload
+      end
+      it "moves the person consumer_role to 'fully verified'" do
+        expect(curam_user.consumer_role.aasm_state).to eq "fully_verified"
+      end
+
+      it "moves SSN to 'valid' state" do
+        expect(curam_user.consumer_role.ssn_validation).to eq 'valid'
+      end
+
+      it "saves ssn_update_reason as 'user in curam'" do
+        expect(curam_user.consumer_role.ssn_update_reason).to eq 'user in curam'
+      end
+
+      it "keeps vlp authority as curam" do
+        expect(curam_user.consumer_role.lawful_presence_determination.vlp_authority).to eq "curam"
+      end
+
+      it "moves lpd state to 'verification_successful'" do
+        expect(curam_user.consumer_role.lawful_presence_determination.aasm_state).to eq "verification_successful"
+      end
+
+      it "saves 'lawful_presence_update_reason' as Hash with update_reason and update_comment" do
+        expect(curam_user.consumer_role.lawful_presence_update_reason[:update_reason]).to eq "user in curam"
+        expect(curam_user.consumer_role.lawful_presence_update_reason[:update_comment]).to eq "fix data migration"
+      end
     end
 
-    it "moves SSN to 'valid' state" do
-      expect(curam_user.consumer_role.ssn_validation).to eq 'valid'
-    end
+    context "people with NO SSN" do
+      let(:curam_user) {
+        person = FactoryGirl.create(:person, :with_consumer_role)
+        person.ssn = nil
+        person.unset(:encrypted_ssn)
+        person.save!
+        person
+      }
 
-    it "saves ssn_update_reason as 'user in curam'" do
-      expect(curam_user.consumer_role.ssn_update_reason).to eq 'user in curam'
-    end
+      before :each do
+        curam_user.consumer_role.lawful_presence_determination.vlp_authority = "curam"
+        curam_user.consumer_role.lawful_presence_determination.aasm_state = "verification_pending"
+        curam_user.consumer_role.aasm_state = "verification_outstanding"
+        curam_user.save!
+        subject.migrate
+        curam_user.reload
+      end
 
-    it "keeps vlp authority as curam" do
-      expect(curam_user.consumer_role.lawful_presence_determination.vlp_authority).to eq "curam"
-    end
+      it "moves SSN to 'na' state" do
+        expect(curam_user.consumer_role.ssn_validation).to eq 'na'
+      end
 
-    it "moves lpd state to 'verification_successful'" do
-      expect(curam_user.consumer_role.lawful_presence_determination.aasm_state).to eq "verification_successful"
-    end
+      it "does not save ssn_update_reason" do
+        expect(curam_user.consumer_role.ssn_update_reason).to eq nil
+      end
 
-    it "saves 'lawful_presence_update_reason' as Hash with update_reason and update_comment" do
-      expect(curam_user.consumer_role.lawful_presence_update_reason[:update_reason]).to eq "user in curam"
-      expect(curam_user.consumer_role.lawful_presence_update_reason[:update_comment]).to eq "fix data migration"
+      it "keeps vlp authority as curam" do
+        expect(curam_user.consumer_role.lawful_presence_determination.vlp_authority).to eq "curam"
+      end
+
+      it "moves lpd state to 'verification_successful'" do
+        expect(curam_user.consumer_role.lawful_presence_determination.aasm_state).to eq "verification_successful"
+      end
+
+      it "saves 'lawful_presence_update_reason' with update_reason and update_comment" do
+        expect(curam_user.consumer_role.lawful_presence_update_reason[:update_reason]).to eq "user in curam"
+        expect(curam_user.consumer_role.lawful_presence_update_reason[:update_comment]).to eq "fix data migration"
+      end
     end
   end
+
 
   describe "given a person who has a vlp authority of 'ssa', in the 'pending' status" do
     subject { CorrectCuramVlpStatus.new("fix me task", double(:current_scope => nil)) }
