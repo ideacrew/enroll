bc74f58cac1813f60d918c445911fa73db238294
Ken Lu <ken@seeice.com>
2016-07-09 10:47:45 -0400

    Refs #7487 - add terminate date and remove the link of change plan in terminate selection page

--- spec/views/insured/group_selection/_enrollment.html.erb_spec.rb
+++ spec/views/insured/group_selection/_enrollment.html.erb_spec.rb
@@ -5,12 +5,15 @@ RSpec.describe "insured/group_selection/_enrollment.html.erb"  do
   let(:person) { FactoryGirl.build(:person) }
   let(:plan) { FactoryGirl.build(:plan) }
   let(:hbx_enrollment) { HbxEnrollment.new(plan: plan) }
+  let(:family) { Family.new }
   before :each do
     allow(hbx_enrollment).to receive(:can_complete_shopping?).and_return false
     allow(hbx_enrollment).to receive(:may_terminate_coverage?).and_return true
+    allow(hbx_enrollment).to receive(:is_shop?).and_return true
     assign :change_plan, 'change_plan'
     assign :employee_role, employee_role
     assign :person, person
+    assign :family, family
     render "insured/group_selection/enrollment", hbx_enrollment: hbx_enrollment
   end
 
@@ -20,5 +23,13 @@ RSpec.describe "insured/group_selection/_enrollment.html.erb"  do
 
   it "should have the link of terminate" do
     expect(rendered).to have_selector('a', text: 'Terminate Plan')
+  end
+
+  it "should have terminate date" do
+    expect(rendered).to have_selector('label', text: 'Termination date: ')
+  end
+
+  it "should not have button of change plan" do
+    expect(rendered).not_to have_selector('a', text: 'Change Plan')
   end
 end
