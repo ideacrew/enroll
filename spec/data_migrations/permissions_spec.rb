efece741f6aeac90f65c3f64697839151f13d87c
jfinucane <jimway@gmail.com>
2016-07-12 17:42:05 -0400

    Refs #7919 refactor rake spec

--- spec/data_migrations/permissions_spec.rb
+++ spec/data_migrations/permissions_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 require File.join(Rails.root, "app", "data_migrations", "define_permissions")
 
 describe DefinePermissions do
+  subject { DefinePermissions.new(given_task_name, double(:current_scope => nil))}
+  let(:roles) {%w{hbx_staff hbx_read_only hbx_csr_supervisor hbx_csr_tier2 hbx_csr_tier1} }
   describe 'create permissions' do
     let(:given_task_name) {':initial_hbx'}
-    let(:roles) {%w{hbx_staff hbx_read_only hbx_csr_supervisor hbx_csr_tier2 hbx_csr_tier1} }
-    subject { DefinePermissions.new(given_task_name, double(:current_scope => nil)).initial_hbx}	
     before do
       Person.all.delete
-    	person= FactoryGirl.create(:person)
-    	role = FactoryGirl.create(:hbx_staff_role, person: person)
-    	subject
+      person= FactoryGirl.create(:person)
+      role = FactoryGirl.create(:hbx_staff_role, person: person)
+      subject.initial_hbx
     end
     it "creates permissions" do
     	expect(Permission.count).to eq(5)
@@ -18,10 +18,9 @@ describe DefinePermissions do
       expect(Permission.all.map(&:name)).to match_array roles
     end
   end
+
   describe 'build test roles' do
     let(:given_task_name) {':build_test_roles'}
-    let(:roles) {%w{hbx_staff hbx_read_only hbx_csr_supervisor hbx_csr_tier2 hbx_csr_tier1} }
-    subject { DefinePermissions.new(given_task_name, double(:current_scope => nil)).build_test_roles}  
     before do
       User.all.delete
       Person.all.delete
@@ -30,7 +29,7 @@ describe DefinePermissions do
       allow(Permission).to receive_message_chain('hbx_csr_supervisor.id'){FactoryGirl.create(:permission, :hbx_csr_supervisor).id}
       allow(Permission).to receive_message_chain('hbx_csr_tier2.id'){FactoryGirl.create(:permission,  :hbx_csr_tier2).id}
       allow(Permission).to receive_message_chain('hbx_csr_tier1.id'){FactoryGirl.create(:permission,  :hbx_csr_tier1).id}
-      subject
+      subject.build_test_roles
     end
     it "creates permissions" do
       expect(User.all.count).to eq(5)
