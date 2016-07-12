2f7fd8112b86c38233a1312596c3266ee427c77a
jfinucane <jimway@gmail.com>
2016-07-12 16:16:35 -0400

    Refs #7919 spec coverage for the rake initial_hbx and build_test_roles

--- app/data_migrations/define_permissions.rb
+++ app/data_migrations/define_permissions.rb
@@ -1,38 +1,38 @@
+require File.join(Rails.root, "lib/migration_task")
+
+class DefinePermissions < MigrationTask 
 #All hbx_roles can view families, employers, broker_agencies, brokers and general agencies
 #The convention for a privilege group 'x' is  'modify_x', or view 'view_x'
 
-namespace :permissions do
-  desc "initial build hbx permissions table"
-  task initial_hbx: :environment do
+  def initial_hbx
     Permission.where(name: /^hbx/).delete_all   
-  	p_staff=Permission.create(name: 'hbx_staff', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
+  	Permission.create(name: 'hbx_staff', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
   	  send_broker_agency_message: true, approve_broker: true, approve_ga: true,
   	  modify_admin_tabs: true, view_admin_tabs: true)
-  	p_read_only=Permission.create(name: 'hbx_read_only', modify_family: false, modify_employer: false, revert_application: false, list_enrollments: true,
+  	Permission.create(name: 'hbx_read_only', modify_family: false, modify_employer: false, revert_application: false, list_enrollments: true,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: true)  
-  	p_supervisor = Permission.create(name: 'hbx_csr_supervisor', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: false,
+  	Permission.create(name: 'hbx_csr_supervisor', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: false)
-  	p_tier2 = Permission.create(name: 'hbx_csr_tier2', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: false,
+  	Permission.create(name: 'hbx_csr_tier2', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: false,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: false)  
-  	p_tier1 = Permission.create(name: 'hbx_csr_tier1', modify_family: true, modify_employer: false, revert_application: true, list_enrollments: false,
+    Permission.create(name: 'hbx_csr_tier1', modify_family: true, modify_employer: false, revert_application: true, list_enrollments: false,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: false)
-  end
-  task migrate_hbx: :environment do
   	permission = Permission.hbx_staff
     Person.where(hbx_staff_role: {:$exists => true}).all.each{|p|p.hbx_staff_role.update_attributes(permission_id: permission.id, subrole:'hbx_staff') unless p.hbx_staff_role.subrole.present?}
   end
-  task build_test_hbx: :environment do
+  def build_test_roles
     User.where(email: /themanda.*dc.gov/).delete_all
     Person.where(last_name: /^amanda\d+$/).delete_all
-    u1 = FactoryGirl.create(:user, email: 'themanda.staff@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)}")
-    u2 = FactoryGirl.create(:user, email: 'themanda.readonly@dc.gov', password: 'P@55word', password_confirmation: 'P@55word',  oim_id: "ex#{rand(5999999)}")
-    u3 = FactoryGirl.create(:user, email: 'themanda.csr_supervisor@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)}")
-    u4 = FactoryGirl.create(:user, email: 'themanda.csr_tier1@dc.gov', password: 'P@55word', password_confirmation: 'P@55word',  oim_id: "ex#{rand(5999999)}")
-    u5 = FactoryGirl.create(:user, email: 'themanda.csr_tier2@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)}")
+    a=10000000
+    u1 = FactoryGirl.create(:user, email: 'themanda.staff@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u2 = FactoryGirl.create(:user, email: 'themanda.readonly@dc.gov', password: 'P@55word', password_confirmation: 'P@55word',  oim_id: "ex#{rand(5999999)+a}")
+    u3 = FactoryGirl.create(:user, email: 'themanda.csr_supervisor@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u4 = FactoryGirl.create(:user, email: 'themanda.csr_tier1@dc.gov', password: 'P@55word', password_confirmation: 'P@55word',  oim_id: "ex#{rand(5999999)+a}")
+    u5 = FactoryGirl.create(:user, email: 'themanda.csr_tier2@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
   
   	p1 = FactoryGirl.create(:person, first_name: 'staff', last_name: "amanda#{rand(1000000)}", user: u1)
   	p2 = FactoryGirl.create(:person, first_name: 'read_only', last_name: "amanda#{rand(1000000)}", user: u2)
