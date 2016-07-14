8ae2ef91c8b98b3c7909131c96da79debc44c866
Raghuram <raghuramg83@gmail.com>
2016-07-14 16:16:09 -0400

    Updates for importer scripts

--- app/models/importers/conversion_employee_set.rb
+++ app/models/importers/conversion_employee_set.rb
@@ -328,12 +328,12 @@ module Importers
     def create_model(record_attrs)
       the_action = record_attrs[:action].blank? ? "add" : record_attrs[:action].to_s.strip.downcase
       case the_action
-      when "update"
-        ::Importers::ConversionEmployeeAction.new(record_attrs.merge({:default_hire_date => @default_hire_date}))
+      # when "update"
+      #   ::Importers::ConversionEmployeeAction.new(record_attrs.merge({:default_hire_date => @default_hire_date}))
       when "delete"
         ::Importers::ConversionEmployeeDelete.new(record_attrs.merge({:default_hire_date => @default_hire_date}))
       else
-        ::Importers::ConversionEmployeeCreate.new(record_attrs.merge({:default_hire_date => @default_hire_date}))
+        ::Importers::ConversionEmployeeAction.new(record_attrs.merge({:default_hire_date => @default_hire_date}))
       end
     end
   end
