2ef6b239e716350307228d2fcebeb4be03c1ef4c
lisyk <sergey.liskovoy@gmail.com>
2016-07-21 14:06:18 -0400

    Refs #8248. update the queries for datafix

--- app/data_migrations/correct_citizen_with_ssn.rb
+++ app/data_migrations/correct_citizen_with_ssn.rb
@@ -2,7 +2,10 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class CorrectCitizenStatus < MongoidMigrationTask
   def get_people
-    Person.where("consumer_role.lawful_presence_determination.ssa_responses.received_at" => {"$gte" => Time.mktime(2016,7,5,8,0,0)},
+    Person.where("consumer_role.lawful_presence_determination.ssa_responses" =>
+                     { "$elemMatch" =>
+                           {"received_at" =>
+                                {"$gte" => Time.mktime(2016,7,5,8,0,0), "$lte" => Time.mktime(2016,7,7,0,0,0) }}},
                  "consumer_role.lawful_presence_determination.vlp_authority" => {"$ne" => "curam"},
                  "consumer_role.lawful_presence_determination.citizen_status" => "us_citizen",
                  "encrypted_ssn" => {"$ne" => nil})
--- app/data_migrations/correct_non_citizen_with_ssn.rb
+++ app/data_migrations/correct_non_citizen_with_ssn.rb
@@ -3,7 +3,10 @@ require File.join(Rails.root, "app/data_migrations/correct_citizen_with_ssn")
 
 class CorrectNonCitizenStatus < CorrectCitizenStatus
   def get_people
-    Person.where("consumer_role.lawful_presence_determination.ssa_responses.received_at" => {"$gte" => Time.mktime(2016,7,5,8,0,0)},
+    Person.where("consumer_role.lawful_presence_determination.ssa_responses" =>
+                     { "$elemMatch" => {
+                         "received_at" => {"$gte" => Time.mktime(2016,7,5,8,0,0), "$lte" => Time.mktime(2016,7,7,0,0,0) }
+                     }},
                  "consumer_role.lawful_presence_determination.vlp_authority" => {"$ne" => "curam"},
                  "consumer_role.lawful_presence_determination.citizen_status" => {"$ne" => "us_citizen"},
                  "encrypted_ssn" => {"$ne" => nil})
