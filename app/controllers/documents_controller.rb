e818871aa4ac41492f8d2280b979eaf0ed902e61
jfinucane <jimway@gmail.com>
2016-07-23 08:37:57 -0400

    from review with Sunandra on Friday

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,5 @@
 class DocumentsController < ApplicationController
-  before_action :updateable?, except: [:show_docs]
+  before_action :updateable?, except: [:show_docs, :download]
   before_action :set_document, only: [:destroy, :update]
   before_action :set_person, only: [:enrollment_docs_state, :fed_hub_request, :enrollment_verification, :update_verification_type]
   respond_to :html, :js
