b25d5a4014f821861aa47643f69b41b80fb97e52
PRANAB <pranab.khanal@gmail.com>
2016-07-27 13:55:31 -0400

    Refs #8304 Renamed context / it blocks to something meaningful

--- spec/data_migrations/fix_plan_years_to_obey_minimum_open_enrollment_period_spec.rb
+++ spec/data_migrations/fix_plan_years_to_obey_minimum_open_enrollment_period_spec.rb
@@ -13,9 +13,9 @@ describe FixPlanYearsToObeyMinimumOpenEnrollmentPeriod do
     end
   end
 
-  describe "changing new hire rule" do
+  describe "fix bad plan year" do
 
-    context " changing effective on kind" do                                
+    context "change open_enrollment_start_on date" do
       let(:organization)      { FactoryGirl.create(:organization, fein: "123456789")}
       let(:plan_year)         { FactoryGirl.build(:plan_year, open_enrollment_start_on: Date.new(2016, 2, 8), open_enrollment_end_on: Date.new(2016, 2, 10), start_on: Date.new(2016, 3, 1)) }
       let(:employer_profile)  { FactoryGirl.build(:employer_profile, organization: organization, plan_years: [plan_year]) }
@@ -25,7 +25,7 @@ describe FixPlanYearsToObeyMinimumOpenEnrollmentPeriod do
         allow(ENV).to receive(:[]).with("fein").and_return("123456789")
       end
 
-      it "will change the effective on kind for the benefit group from date_of_hire to first_of_month" do
+      it "will set the open_enrollment_start_on date of the plan_year so that it follows the minimum 5 days rule" do
         subject.migrate
         plan_year.reload
         expect(plan_year.open_enrollment_end_on.mjd - plan_year.open_enrollment_start_on.mjd).to eq 4
