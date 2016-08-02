1fcaccb0c0e46ecfe02f23137afcd43b181bd197
Raghuram <raghuramg83@gmail.com>
2016-08-02 10:43:50 -0400

    Refs #5565 Added code for conversion employer renewal notice

--- app/notices/envelope.rb
+++ app/notices/envelope.rb
@@ -1,7 +1,8 @@
 class Envelope < PdfReport
 
-  def initialize
-    template = Rails.root.join('lib/pdf_templates', 'ivl_envelope.pdf')
+  def initialize(template_name = nil)
+    template_name ||= 'ivl_envelope.pdf'
+    template = Rails.root.join('lib/pdf_templates', template_name)
 
     super({:template => template, :margin => [30, 55]})
     font_size 11
@@ -11,8 +12,12 @@ class Envelope < PdfReport
 
   def fill_envelope(notice, mpi_indicator = nil)
     x_pos = mm2pt(21.83) - @margin[0]
-    y_pos = 790.86 - mm2pt(57.15) - 65
 
+    if notice.employer_name.present?
+      y_pos = 790.86 - mm2pt(57.15) - 45
+    else
+      y_pos = 790.86 - mm2pt(57.15) - 65
+    end
     bounding_box([x_pos, y_pos], :width => 300) do
       fill_recipient_contact(notice)
     end
@@ -29,6 +34,7 @@ class Envelope < PdfReport
 
   def fill_recipient_contact(notice)
     text notice.primary_fullname
+    text notice.employer_name if notice.employer_name.present?
     text notice.primary_address.street_1
     text notice.primary_address.street_2 unless notice.primary_address.street_2.blank?
     text "#{notice.primary_address.city}, #{notice.primary_address.state} #{notice.primary_address.zip}"      
