8363ecd49e25741c2cb98d6717b7bc8c493b2f34
Raghuram <raghuramg83@gmail.com>
2016-08-05 12:12:22 -0400

    updated notice envelope

--- app/notices/envelope.rb
+++ app/notices/envelope.rb
@@ -13,11 +13,12 @@ class Envelope < PdfReport
   def fill_envelope(notice, mpi_indicator = nil)
     x_pos = mm2pt(21.83) - @margin[0]
 
-    if notice.respond_to?(:employer_name) && notice.employer_name.present?
-      y_pos = 790.86 - mm2pt(57.15) - 55
-    else
+    # Enable this for Conversion renewal notice
+    # if notice.respond_to?(:employer_name) && notice.employer_name.present?
+    #   y_pos = 790.86 - mm2pt(57.15) - 55
+    # else
       y_pos = 790.86 - mm2pt(57.15) - 65
-    end
+    # end
 
     bounding_box([x_pos, y_pos], :width => 300) do
       fill_recipient_contact(notice)
@@ -36,15 +37,16 @@ class Envelope < PdfReport
   def fill_recipient_contact(notice)
     text notice.primary_fullname
 
-    if notice.respond_to?(:employer_name) && notice.employer_name.present?
-      text notice.employer_name
-      address = notice.primary_address.street_1.strip
-      address += ", " + notice.primary_address.street_2.strip unless notice.primary_address.street_2.blank?
-      text address
-    else
+    # Enable this for Conversion renewal notice
+    # if notice.respond_to?(:employer_name) && notice.employer_name.present?
+    #   text notice.employer_name
+    #   address = notice.primary_address.street_1.strip
+    #   address += ", " + notice.primary_address.street_2.strip unless notice.primary_address.street_2.blank?
+    #   text address
+    # else
       text notice.primary_address.street_1
       text notice.primary_address.street_2 unless notice.primary_address.street_2.blank?
-    end
+    # end
 
     text "#{notice.primary_address.city}, #{notice.primary_address.state} #{notice.primary_address.zip}"      
   end
