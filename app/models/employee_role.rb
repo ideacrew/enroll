47051ef85e0580c3076940d7fc662c612997b622
Raghuram <raghuramg83@gmail.com>
2016-08-08 09:28:33 -0400

    Refs #8586 updated method

--- app/models/employee_role.rb
+++ app/models/employee_role.rb
@@ -114,9 +114,9 @@ class EmployeeRole
   def is_dental_offered?
     plan_year = employer_profile.find_plan_year_by_effective_date(coverage_effective_on)
 
-    benefit_packages = [census_employee.renewal_benefit_group_assignment, census_employee.active_benefit_group_assignment].compact
-    benefit_package  = benefit_packages.detect{|bpkg| bpkg.plan_year == plan_year}
-    benefit_package.present? && benefit_package.benefit_group.is_offering_dental? ? true : false
+    benefit_group_assignments = [census_employee.renewal_benefit_group_assignment, census_employee.active_benefit_group_assignment].compact
+    benefit_group_assignment  = benefit_group_assignments.detect{|bpkg| bpkg.plan_year == plan_year}
+    benefit_group_assignment.present? && benefit_group_assignment.benefit_group.is_offering_dental? ? true : false
   end
 
   class << self
