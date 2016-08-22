005cd6a271d3598149300b4b904676bc1650ef14
jfinucane <jimway@gmail.com>
2016-08-22 15:08:55 -0400

    Refs #9871 developer permission is read only

--- app/data_migrations/define_permissions.rb
+++ app/data_migrations/define_permissions.rb
@@ -9,7 +9,7 @@ class DefinePermissions < MigrationTask
   	Permission.create(name: 'hbx_staff', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
   	  send_broker_agency_message: true, approve_broker: true, approve_ga: true,
   	  modify_admin_tabs: true, view_admin_tabs: true)
-  	Permission.create(name: 'hbx_read_only', modify_family: false, modify_employer: false, revert_application: false, list_enrollments: true,
+    Permission.create(name: 'hbx_read_only', modify_family: true, modify_employer: false, revert_application: false, list_enrollments: true,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: true)  
   	Permission.create(name: 'hbx_csr_supervisor', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
@@ -21,6 +21,9 @@ class DefinePermissions < MigrationTask
     Permission.create(name: 'hbx_csr_tier1', modify_family: true, modify_employer: false, revert_application: false, list_enrollments: false,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: false)
+    Permission.create(name: 'developer', modify_family: false, modify_employer: false, revert_application: false, list_enrollments: true,
+      send_broker_agency_message: false, approve_broker: false, approve_ga: false,
+      modify_admin_tabs: false, view_admin_tabs: true)
   	permission = Permission.hbx_staff
     Person.where(hbx_staff_role: {:$exists => true}).all.each{|p|p.hbx_staff_role.update_attributes(permission_id: permission.id, subrole:'hbx_staff')}
   end
