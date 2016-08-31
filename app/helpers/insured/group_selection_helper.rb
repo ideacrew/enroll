83a62984cf1edbdb4c35d15b24df986825eedb52
saimekala07 <saimekala07@gmail.com>
2016-08-31 15:54:11 -0400

    refs # 9872 refactor census employee health relationship

--- app/helpers/insured/group_selection_helper.rb
+++ app/helpers/insured/group_selection_helper.rb
@@ -13,10 +13,9 @@ module Insured
     end
 
     def health_relationship_benefits(employee_role)
-      if employee_role.census_employee.renewal_published_benefit_group.present?
-        employee_role.census_employee.renewal_published_benefit_group.relationship_benefits.select(&:offered).map(&:relationship)
-      elsif employee_role.census_employee.active_benefit_group.present?
-        employee_role.census_employee.active_benefit_group.relationship_benefits.select(&:offered).map(&:relationship)
+      benefit_group = employee_role.census_employee.renewal_published_benefit_group || employee_role.census_employee.active_benefit_group
+      if benefit_group.present?
+        benefit_group.relationship_benefits.select(&:offered).map(&:relationship)
       end
     end
 
