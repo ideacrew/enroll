8ec08d134bbfee36e005a7adfbd98806c98ededb
Battula <vrbattula@outlook.com>
2016-09-08 10:03:13 -0400

    refs #6724 Fixed failing spec

--- spec/views/broker_agencies/profiles/edit.html.erb_spec.rb
+++ spec/views/broker_agencies/profiles/edit.html.erb_spec.rb
@@ -19,7 +19,10 @@ RSpec.describe "broker_agencies/profiles/edit.html.erb" do
   it "should block the market kind dropdown refs #9818" do
     expect(rendered).to have_selector('.broker-agency-info.read_only_dropdown')
   end
-  it "should have four read only fields refs #9818"  do
-    expect(rendered).to have_selector("[readonly='readonly']", count: 4)
+  it "should have three read only fields refs #9818"  do
+    expect(rendered).to have_selector("[readonly='readonly']", count: 3)
+  end
+  it "should have a disabled field" do
+    expect(rendered).to have_selector("input[disabled='disabled']", count: 1)
   end
 end
