2d167074cf03d21877b025790c09844af0b8999d
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-09-22 17:27:29 -0400

    Refs #8033 grouping multiple census_employees under matching employer + spec

--- spec/views/events/census_employee/employer_response.xml.haml_spec.rb
+++ spec/views/events/census_employee/employer_response.xml.haml_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "events/census_employee/employer_response.xml.haml.erb" do
                              ce = FactoryGirl.create(:census_employee, employer_profile_id: ep.id);
                             [ce] }
 
-    describe "employer with no census employees" do
+    describe "employer with one census employees" do
 
       before(:each) do
         render :template => "events/census_employee/employer_response", :locals => {:census_employees => census_employees}
@@ -23,4 +23,33 @@ RSpec.describe "events/census_employee/employer_response.xml.haml.erb" do
       end
     end
   end
+
+  context "multiple census employees under one employer" do
+    let(:employer_profile) { FactoryGirl.create(:employer_profile) }
+
+    let(:census_employees) { [FactoryGirl.create(:census_employee, employer_profile_id: employer_profile.id),
+                              FactoryGirl.create(:census_employee, employer_profile_id: employer_profile.id)]}
+
+    describe "employer with two census employees" do
+
+      before(:each) do
+        render :template => "events/census_employee/employer_response", :locals => {:census_employees => census_employees}
+        @doc = Nokogiri::XML(rendered)
+        @doc.remove_namespaces!
+      end
+
+      it "generates valid xml" do
+        expect(validate_with_schema(Nokogiri::XML(rendered))).to eq []
+      end
+
+      it "has one employer" do
+        expect(@doc.xpath("//employers/employer").count).to eq 1
+      end
+
+      it "has two census_employers" do
+        expect(@doc.xpath("//employers/employer/employer_census_families/employer_census_family").count).to eq 2
+      end
+    end
+  end
+
 end
