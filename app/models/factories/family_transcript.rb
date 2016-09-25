8b74e45b1d8bf1ce907aa93201b2359f62e95829
Dan Thomas <dan.thomas@dc.gov>
2016-09-25 01:03:56 -0400

    Framing up factory structure Add #difference method and specs

--- app/models/factories/family_transcript.rb
+++ app/models/factories/family_transcript.rb
@@ -2,33 +2,100 @@ module Factories
   class FamilyTranscript
 
     # Expect family_transcript is a hash structured after CV2
-    def self.find_or_build_family(family_transcript = {})
+    def find_or_build_local(family_transcript = {})
       return family_transcript_prototype if family_transcript.blank?
 
+      local_people = family_transcript.people.reduce([]) { |list, person| list << find_or_build_person(person) }
+      local_family = find_or_build_family(family_transcript.family)
+      local_family_enrollments = family_transcript.households.each do |household|
+        household.hbx_enrollments.each { |hbx_enrollment|  find_or_build_hbx_enrollment(hbx_enrollment, family_transcript.family) }
+      end
+
+      { family: family.local_family({
+            family_members: [local_family.family_members],
+            irs_groups: [local_family.irs_groups],
+            households: [local_family.households({
+                            hbx_enrollments: [local_family_enrollments]
+                          })
+                      ]
+          }),
+        people: [local_people]
+      }
     end
 
-    def self.audit_family(family_transcript)
-      return family_transcript_prototype if family_transcript.blank?
+    def audit_family(family_transcript)
+      local_family = find_or_build_local(family_transcript)
 
     end
 
+    def difference(base_record, compare_record)
+      differences = HashWithIndifferentAccess.new
+      all_keys = (base_record.keys + compare_record.keys).uniq!
+      all_keys.each do |k|
+        next if base_record[k].blank? && compare_record[k].blank?
 
-    def find_or_build_family(primary_member)
+        if base_record[k].blank?
+          differences[:add] ||= {}
+          differences[:add][k] = compare_record[k]
+        elsif compare_record[k].blank?
+          differences[:remove] ||= {}
+          differences[:remove][k] = base_record[k]
+        elsif base_record[k].is_a?(Array) && compare_record[k].is_a?(Array)
+          differences[:array] ||= {}
+          old_values = base_record[k] - compare_record[k]
+          new_values = compare_record[k] - base_record[k]
+          differences[:array][k] = { add: new_values, remove: old_values }.delete_if { |_, vv| vv.blank? }
+# binding.pry
+          differences[:array][k].delete_if { |_, v| v.blank? }
+        else
+          if base_record[k] != compare_record[k]
+            differences[:update] ||= {}
+            differences[:update][k] = compare_record[k]
+          end
+        end
+      end
+
+      differences
     end
 
-    def find_or_build_consumer_role(consumer)
+    # Use 
+    def find_or_build_family(transcript_primary_member)
+
+    end
+
+    def find_or_build_person(transcript_person)
       # Support citizenship and VLP status override
     end
 
-    def build_employee_role(employee, employer)
+    def build_consumer_role(transcript_consumer)
+      # Support citizenship and VLP status override
     end
 
-    def find_or_build_enrollment(family, enrollment)
+    def build_employee_role(transcript_employee, transcript_employer)
+    end
+
+    def find_or_build_hbx_enrollment(transcript_family, transcript_hbx_enrollment)
+      enrollment = match_hbx_enrollment(hbx_enrollment)
+
       make_employer_sponsored_enrollment
       make_individual_enrollment
     end
 
-  # private
+  private
+
+    def match_family(family)
+
+    end
+
+    def match_person(transcript_person)
+      Person.match_existing_person(transcript_person)
+    end
+
+    def match_hbx_enrollment(hbx_enrollment)
+
+    end
+
+
     def family_transcript_prototype
       person = Person.new
       person.build_consumer_role
--- spec/models/factories/family_transcript_spec.rb
+++ spec/models/factories/family_transcript_spec.rb
@@ -4,17 +4,74 @@ RSpec.describe Factories::FamilyTranscript, type: :model do
 
   let(:person_template)             { Person.new.attributes.except("_id", "version") }
   let(:consumer_role_template)      { person_template.buiild.attributes.except("_id", "version") }
-  let(:family_transcript_template)  { {
+  let(:family_transcript_template)  { }
 
-                                        } 
-                                      }
+  describe "instance methods" do
+    let(:family_transcript)   { Factories::FamilyTranscript.new }
+    let(:base_array)          { base_hash.merge({game_shows: %w(jeopardy password gong_show)}) }
+    let(:compare_array)       { base_hash.merge({game_shows: %w(jeopardy password truth_or_consequences)}) }
+    let(:base_hash)           { {first_name: "Tina", last_name: "Fey", occupation: "comedian"} }
+    let(:occupation_update)   { {first_name: "Tina", last_name: "Fey", occupation: "actress"} }
+    let(:multi_update)        { {first_name: "Christina", last_name: "Fey", occupation: "actress", } }
+    let(:occupation_dropped)  { {"remove"=>{"occupation"=>"comedian"}} }
+    let(:last_name_added)     { {"add"=>{"last_name"=>"Fey"}} }
+    let(:occupation_updated)  { {"update"=>{"occupation"=>"actress"}} }
+    let(:multi_updated)       { {"update"=>{"first_name"=>"Christina", "occupation"=>"actress"}} }
+    let(:array_changes)       { {"array"=>{"game_shows"=>{"add"=>["truth_or_consequences"], "remove"=>["gong_show"]}}} }
+
+    context "#difference" do
+      context "and identical records are compared" do
+        it "should return empty hash" do
+          expect(family_transcript.difference(base_hash, base_hash)).to eq Hash.new
+        end
+
+        context "and the identical records contain an array" do
+          it "should return empty hash" do
+            # expect(family_transcript.difference(base_array, base_array)).to eq Hash.new
+          end
+        end
+      end
+
+      context "and the base record has a value missing from the compare record" do
+        it "should return a remove for that key" do
+          expect(family_transcript.difference(base_hash, base_hash.except(:occupation))).to eq occupation_dropped
+        end
+      end
+
+      context "and the compare record has a value missing from the base record" do
+        it "should return an add for that key" do
+          expect(family_transcript.difference(base_hash.except(:last_name), base_hash)).to eq last_name_added
+        end
+      end
+
+      context "and the compare record updates a base record value" do
+        it "should return an update for that key" do
+          expect(family_transcript.difference(base_hash, occupation_update)).to eq occupation_updated
+        end
+      end
+
+      context "and multiple values are updated between the base and compare records" do
+        it "should return an update for multiple keys" do
+          expect(family_transcript.difference(base_hash, multi_update)).to eq multi_updated
+        end
+
+        context "and the records contain an array with changes" do
+          it "should return an update for multiple keys" do
+            expect(family_transcript.difference(base_array, compare_array)).to eq array_changes
+          end
+        end
+      end
+
+    end
+  end
+
 
   describe "find_or_build_family" do
     context "Family is new" do
 
       context "and the family_transcript is nil" do
         it "should return an prototype family_transcript" do
-          expect { Factories::FamilyTranscript.find_or_build_family(nil) }.to raise_error(Factories::FamilyTranscriptError)
+          # expect { Factories::FamilyTranscript.find_or_build_family(nil) }.to raise_error(Factories::FamilyTranscriptError)
         end
       end
 
