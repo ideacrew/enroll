10e950117bb44528ddd8efb62b27954becdc55f7
Battula <vrbattula@outlook.com>
2016-09-27 11:52:51 -0400

    refs #9751 Populate ic_ref on the notice

--- script/ivlr2_notice.rb
+++ script/ivlr2_notice.rb
@@ -24,17 +24,18 @@
 
     event_kind = ApplicationEventKind.where(:event_name => 'ivl_renewal_notice_2').first
     notice_trigger = event_kind.notice_triggers.first
-    @data_hash.each do |ic_ref , members|
+    @data_hash.each do |ic_ref, members|
       primary_member = members[0]
       family = Family.where(:e_case_id => Regexp.new(primary_member["ic_ref"],true)).first
       consumer_role =family.primary_family_member.person.consumer_role
-      if  consumer_role.present?
+      if consumer_role.present?
         begin
           builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
                 template: notice_trigger.notice_template,
                 subject: event_kind.title,
                 mpi_indicator: notice_trigger.mpi_indicator,
-                data: members
+                data: members,
+                primary_identifier: ic_ref
                 }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)
                 )
           builder.deliver
