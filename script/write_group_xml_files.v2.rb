54f1b4e571de94d4ac93aee97992fa10552ea185
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-09-27 10:33:39 -0400

    Revert "Refs #8237 removed conditions for initial employers"
    This reverts commit 82e29fc11ecca8c918c991a8ff4d2af82fb3dce9.

--- script/write_group_xml_files.v2.rb
+++ script/write_group_xml_files.v2.rb
@@ -81,7 +81,9 @@ def remove_other_carrier_nodes(xml, trading_partner, employer_profile, pys, opti
     end
   else
     if has_this_year
-      event = "urn:openhbx:events:v1:employer#benefit_coverage_initial_application_eligible"
+      if employer_profile.eligible? || employer_profile.enrolled? || employer_profile.binder_paid? || employer_profile.registered?
+        event = "urn:openhbx:events:v1:employer#benefit_coverage_initial_application_eligible"
+      end
     end
   end
 
