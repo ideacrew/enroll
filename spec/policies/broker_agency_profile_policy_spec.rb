ec8823b95115dd3ccf66e42af69f4f3de9fb019b
jfinucane <jimway@gmail.com>
2016-09-27 17:17:02 -0400

    Refs #10836 improve ruby per Ram

--- spec/policies/broker_agency_profile_policy_spec.rb
+++ spec/policies/broker_agency_profile_policy_spec.rb
@@ -32,7 +32,7 @@ describe BrokerAgencyProfilePolicy do
 
     it 'broker_agency_staff_roles can see broker agency profile' do
       FactoryGirl.create(:broker_agency_staff_role, person: person, broker_agency_profile: broker_agency_profile, aasm_state: 'active')
-      expect(policy.access_to_broker_agency_profile?).to be person.broker_agency_staff_roles[0]
+      expect(policy.access_to_broker_agency_profile?).to be true
     end
 
     it 'broker_agency_staff_roles do not match broker agency profile' do
@@ -43,13 +43,13 @@ describe BrokerAgencyProfilePolicy do
     it 'broker_agency_staff_roles can find the  valid broker agency staff role' do
       FactoryGirl.create(:broker_agency_staff_role, person: person, broker_agency_profile: broker_agency_profile, aasm_state: 'active')
       FactoryGirl.create(:broker_agency_staff_role, person: person, broker_agency_profile: FactoryGirl.create(:broker_agency_profile), aasm_state: 'active')
-      expect(policy.access_to_broker_agency_profile?).to be person.broker_agency_staff_roles[0]
+      expect(policy.access_to_broker_agency_profile?).to be true
     end
 
     it 'broker_agency_staff_roles invalid broker agency staff role due to aasm_state' do
       FactoryGirl.create(:broker_agency_staff_role, person: person, broker_agency_profile: broker_agency_profile, aasm_state: 'is_applicant')
       FactoryGirl.create(:broker_agency_staff_role, person: person, broker_agency_profile: FactoryGirl.create(:broker_agency_profile), aasm_state: 'active')
-      expect(policy.access_to_broker_agency_profile?).to be nil
+      expect(policy.access_to_broker_agency_profile?).to be false
     end
 
   end
