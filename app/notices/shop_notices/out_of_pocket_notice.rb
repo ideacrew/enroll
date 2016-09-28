5bb5b2fa8bd6f73ab6c27f997d043049080cdb49
sarathpyerra <sarathpyerra@gmail.com>
2016-09-28 13:46:52 -0400

    Refs #9698 #10941 #10943 out of pocket calculator notices to employees

--- app/notices/shop_notices/out_of_pocket_notice.rb
+++ app/notices/shop_notices/out_of_pocket_notice.rb
@@ -1,25 +1,32 @@
 class ShopNotices::OutOfPocketNotice < ShopNotice
 
-  def initialize(employer_profile, args = {})
+  def initialize(census_employee, args = {})
+    args[:template] =  "notices/shop_notices/out_of_pocket_notice.html.erb"
+    args[:market_kind] = 'shop'
+    args[:notice] = PdfTemplates::EmployerNotice.new
+    args[:recipient] = census_employee
+    args[:recipient_document_store] = "Not needed"
+    self.header = "notices/shared/header_with_page_numbers.html.erb"
     super(args)
-    @employer_profile = employer_profile
-    @template = args[:template] || "notices/shop_notices/out_of_pocket_notice.html.erb"
-    @delivery_method = args[:delivery_method].split(',')
-    @recipient = @employer_profile.employee_roles.first.person
-    @secure_message_recipient = @recipient.primary_family
-    @family = @recipient.primary_family
-    @market_kind = 'shop'
-
-    @notice = PdfTemplates::EmployerNotice.new
+    # self.data = args[:data]
+    # @to= @recipient.email.address
   end
 
-
+  def deliver
+    build
+    generate_pdf_notice
+    send_email_notice
+  end
 
   def build
-    # @notice.primary_fullname = @recipient.full_name.titleize
-    # @notice.primary_identifier = @employer_profile.hbx_id
-    # append_primary_address(@recipient.mailing_address || @employer_profile.organization.office_locations.first.address)
-    # append_hbe
-    # append_broker(@employer_profile.broker_agency_profile)
+    @notice.primary_fullname = @recipient.full_name.titleize
+    ## Build all the data needed here 
+
+  end
+
+  # @param recipient is a Person object
+  def send_email_notice
+    attachments={"#{subject}": notice_path}
+    UserMailer.generic_notice_alert(@notice.primary_fullname,subject,to,attachments).deliver_now
   end
 end 
