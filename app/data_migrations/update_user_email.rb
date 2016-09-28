1f06e53954fd8e7f57e70edaa8f8dae8dae4c8f5
saimekala07 <saimekala07@gmail.com>
2016-09-28 14:09:48 -0400

    refs #10830 migrating email in user record based on oim_id

---
+++ app/data_migrations/update_user_email.rb
@@ -0,0 +1,25 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class UpdateUserEmail < MongoidMigrationTask
+
+  def migrate
+    file_path = File.join(Rails.root, 'db', 'seedfiles', "update_user_email2.xlsx") # path of excel file with email and oim_id data
+    if file_path.present?
+      result = Roo::Spreadsheet.open(file_path)
+      2.upto(result.last_row) do |row_number|
+        row_info = result.row(row_number)
+        begin
+          user = User.where(oim_id: /^#{row_info[0]}/i).first
+          if user.present?
+            user.email = row_info[1]
+            user.save!
+          else
+            puts "Unable to find oim_id:#{row_info[0]}" unless Rails.env == 'test'
+          end
+        rescue => e
+          puts "ERROR: #{row_info} Unable to update email" + e.message unless Rails.env == 'test'
+        end
+      end
+    end
+  end
+end
