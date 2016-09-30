e5757c04d26b8cab7d55604374c05bae3a8a0fa9
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-09-29 20:43:04 -0400

    Refs #8905 making changes to use state changes

--- script/terminate_incorrect_enrollments.rb
+++ script/terminate_incorrect_enrollments.rb
@@ -4,11 +4,11 @@ filename = "imported_terminated_policies-8905.csv"
 CSV.foreach(filename, headers: true) do |csv_row|
 	hbx_enrollment = HbxEnrollment.by_hbx_id(csv_row["Enrollment Group ID"]).first
 	end_date = Date.strptime(csv_row["Benefit End Date"],'%m/%d/%Y')
-	hbx_enrollment.terminated_on = end_date
 	if hbx_enrollment.benefit_group.plan_year.end_on != end_date
-		hbx_enrollment.aasm_state = "coverage_terminated"
+		hbx_enrollment.update_attribute(:terminated_on, end_date)
+		hbx_enrollment.terminate_coverage!
 	elsif hbx_enrollment.benefit_group.plan_year.end_on == end_date
-		hbx_enrollment.aasm_state = "coverage_expired"
+		hbx_enrollment.expire_coverage!
 	end
 	hbx_enrollment.save!
 end
