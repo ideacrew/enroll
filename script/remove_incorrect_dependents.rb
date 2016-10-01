32df60db42bb4f6853094a5779106e6c89bda82a
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-10-01 19:46:49 -0400

    Refs #8905 Added code to remove incorrect dependents

---
+++ script/remove_incorrect_dependents.rb
@@ -0,0 +1,92 @@
+def valid_dependent?(dependent,csv_data)
+	csv_hbx_id = csv_data.hbx_id
+	csv_ssn = csv_data.ssn
+	csv_dob = csv_data.dob
+	csv_first_name = csv_data.first_name
+	csv_last_name = csv_data.last_name
+	system_hbx_id = dependent.hbx_id
+	system_dob = dependent.dob
+	system_ssn = dependent.ssn
+	system_first_name = dependent.first_name
+	system_last_name = dependent.last_name
+
+	hbx_id_result = compare_on_hbx_id(system_hbx_id,csv_hbx_id)
+	ssn_result = compare_on_ssn(system_ssn,csv_ssn)
+	name_dob_result = compare_on_name_and_dob(csv_first_name,csv_last_name,csv_dob,system_first_name,system_last_name,system_dob)
+	results_array = [hbx_id_result,ssn_result,name_dob_result]
+	if results_array.any?{|result| result == true}
+		return true
+	else 
+		return false
+	end
+end
+
+def compare_on_hbx_id(system_hbx_id,csv_hbx_id)
+	if system_hbx_id == csv_hbx_id
+		return true
+	else
+		return false
+	end
+end
+
+def compare_on_ssn(system_ssn,csv_ssn)
+	if system_ssn == csv_ssn
+		return true
+	else
+		return false
+	end
+end
+
+def compare_on_name_and_dob(csv_first_name,csv_last_name,csv_dob,system_first_name,system_last_name,system_dob)
+	csv_full_name = "#{csv_first_name.downcase.strip} #{csv_last_name.downcase.strip}"
+	system_full_name = "#{system_first_name.downcase.strip} #{system_last_name.downcase.strip}"
+	if csv_full_name == system_full_name && csv_dob == system_dob
+		return true
+	else
+		return false
+	end
+end
+
+def format_date(date)
+	date = Date.strptime(date,'%m/%d/%Y')
+end
+
+def construct_csv_dependent(hbx_id,ssn,dob,first_name,last_name)
+	csv_dependent = OpenStruct.new
+	csv_dependent.hbx_id = hbx_id
+	csv_dependent.ssn = ssn
+	csv_dependent.dob = dob
+	csv_dependent.first_name = first_name
+	csv_dependent.last_name = last_name
+	return csv_dependent
+end
+
+filename = '8905_wrong_dependents_imported.csv'
+
+CSV.foreach(filename,headers: true) do |csv_row|
+	dependents_to_keep = []
+	hbx_enrollment = HbxEnrollment.by_hbx_id(csv_row["Enrollment Group ID"]).first
+	hbx_enrollment.hbx_enrollment_members.each do |hbx_em|
+		results = []
+		6.times do |i|
+			if csv_row["HBX ID (Dep #{i+1})"] != nil
+				csv_data = construct_csv_dependent(csv_row["HBX ID (Dep #{i+1})"],
+					                               csv_row["SSN (Dep #{i+1})"].to_s.gsub("-",""),
+					                               format_date(csv_row["DOB (Dep #{i+1})"]),
+					                               csv_row["First Name (Dep #{i+1})"],
+					                               csv_row["Last Name (Dep #{i+1})"])
+				results.push(valid_dependent?(hbx_em.person,csv_data))
+			else
+
+			end
+		end
+		if results.any?{|result| result = true}
+			dependents_to_keep.push(hbx_em)
+		end
+	end
+	hbx_enrollment.hbx_enrollment_members.each do |hbx_em|
+		unless dependents_to_keep.include?(hbx_em)
+			hbx_em.destroy!
+		end
+	end
+end
