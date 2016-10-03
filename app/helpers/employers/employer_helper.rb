2cf1b8fcd8ec06b64398c47238a51dc8c349e4f8
202chaz <chaz.haskins@gmail.com>
2016-10-03 12:03:10 -0400

    Refs 7892 Updated Renewal Enrollment Status

--- app/helpers/employers/employer_helper.rb
+++ app/helpers/employers/employer_helper.rb
@@ -20,7 +20,7 @@ module Employers::EmployerHelper
   end
 
   def renewal_enrollment_state(census_employee=nil)
-    humanize_enrollment_states(census_employee.renewal_benefit_group_assignment).gsub("Coverage Renewing", "Auto-Renewing").gsub("Coverage Selected", "Enrolled").gsub("Coverage Waived", "Waived").gsub("Coverage Terminated", "Terminated")
+    humanize_enrollment_states(census_employee.renewal_benefit_group_assignment).gsub("Coverage Renewing", "Auto-Renewing").gsub("Coverage Selected", "Enrolling").gsub("Coverage Waived", "Waiving").gsub("Coverage Terminated", "Terminating")
   end
 
   def humanize_enrollment_states(benefit_group_assignment)
