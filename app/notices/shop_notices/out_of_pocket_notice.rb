539b58a1d96c370967d88d516abaafa510d3ef0c
sarathpyerra <sarathpyerra@gmail.com>
2016-10-03 14:28:02 -0400

    Refs #9698 #10941 #10943 Adding checkbook service to support url  and out-of-pocket-calulator notice  template

--- app/notices/shop_notices/out_of_pocket_notice.rb
+++ app/notices/shop_notices/out_of_pocket_notice.rb
@@ -1,4 +1,5 @@
 class ShopNotices::OutOfPocketNotice < ShopNotice
+  attr_accessor :url
 
   def initialize(census_employee, args = {})
     args[:template] =  "notices/shop_notices/out_of_pocket_notice.html.erb"
@@ -7,6 +8,7 @@ class ShopNotices::OutOfPocketNotice < ShopNotice
     args[:recipient] = census_employee
     args[:recipient_document_store] = "Not needed"
     self.header = "notices/shared/header_with_page_numbers.html.erb"
+    self.url = args[:data]
     super(args)
     # self.data = args[:data]
     # @to= @recipient.email.address
@@ -20,6 +22,13 @@ class ShopNotices::OutOfPocketNotice < ShopNotice
 
   def build
     @notice.primary_fullname = @recipient.full_name.titleize
+    @notice.start_on= @recipient.employer_profile.plan_years.first.start_on
+    @notice.legal_name= @recipient.employer_profile.organization.legal_name
+    @notice.benefit_group_package_name= @recipient.employer_profile.plan_years.first.benefit_groups.first.title
+    @notice.family_contribution= @recipient.employer_profile.plan_years.first.benefit_groups.first.relationship_benefits
+    @notice.reference_plan =@recipient.active_benefit_group.reference_plan
+    @notice.data = {:url => url}
+    
     ## Build all the data needed here 
 
   end
