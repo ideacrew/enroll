a527ff14f50fb9eee2ef1df5b1d1d90e04d1ab6c
Kevin Wei <kevin.tw.wei@gmail.com>
2016-10-04 11:11:13 -0400

    rest of tabs to spaces

--- script/terminate_incorrect_enrollments.rb
+++ script/terminate_incorrect_enrollments.rb
@@ -1,16 +1,16 @@
-# Takes enrollments that are terminated in Glue and terminates them in Enroll. 
+# Takes enrollments that are terminated in Glue and terminates them in Enroll.
 filename = "8905_policies_to_terminate.csv"
 
 CSV.foreach(filename, headers: true) do |csv_row|
-	hbx_enrollment = HbxEnrollment.by_hbx_id(csv_row["Enrollment Group ID"]).first
-	end_date = Date.strptime(csv_row["Benefit End Date"],'%m/%d/%Y')
-	if hbx_enrollment.benefit_group.plan_year.end_on != end_date
-		hbx_enrollment.update_attribute(:terminated_on, end_date)
-		hbx_enrollment.terminate_coverage!
-	elsif hbx_enrollment.start_on == end_date
-		hbx_enrollment.update_attribute(:terminated_on, end_date)
-		hbx_enrollment.cancel_coverage!
-	elsif hbx_enrollment.benefit_group.plan_year.end_on == end_date
-		hbx_enrollment.expire_coverage!
-	end
+  hbx_enrollment = HbxEnrollment.by_hbx_id(csv_row["Enrollment Group ID"]).first
+  end_date = Date.strptime(csv_row["Benefit End Date"],'%m/%d/%Y')
+  if hbx_enrollment.benefit_group.plan_year.end_on != end_date
+    hbx_enrollment.update_attribute(:terminated_on, end_date)
+    hbx_enrollment.terminate_coverage!
+  elsif hbx_enrollment.start_on == end_date
+    hbx_enrollment.update_attribute(:terminated_on, end_date)
+    hbx_enrollment.cancel_coverage!
+  elsif hbx_enrollment.benefit_group.plan_year.end_on == end_date
+    hbx_enrollment.expire_coverage!
+  end
 end
