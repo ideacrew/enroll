f574318cc2772b1f28d688f0c8761dd56e82134b
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-10-04 11:58:02 -0400

    Refs #8905 adding line to skip if policy is already terminated

--- script/terminate_incorrect_enrollments.rb
+++ script/terminate_incorrect_enrollments.rb
@@ -6,11 +6,11 @@ CSV.foreach(filename, headers: true) do |csv_row|
   end_date = Date.strptime(csv_row["Benefit End Date"],'%m/%d/%Y')
   if hbx_enrollment.benefit_group.plan_year.end_on != end_date
     hbx_enrollment.update_attribute(:terminated_on, end_date)
-    hbx_enrollment.terminate_coverage!
+    hbx_enrollment.terminate_coverage! if hbx_enrollment.may_terminate_coverage?
   elsif hbx_enrollment.start_on == end_date
     hbx_enrollment.update_attribute(:terminated_on, end_date)
-    hbx_enrollment.cancel_coverage!
+    hbx_enrollment.cancel_coverage! if hbx_enrollment.may_terminate_coverage?
   elsif hbx_enrollment.benefit_group.plan_year.end_on == end_date
-    hbx_enrollment.expire_coverage!
+    hbx_enrollment.expire_coverage! if hbx_enrollment.may_terminate_coverage?
   end
 end
