e6802b8ce10a5c154176b7db17aa7589ec84a8e6
Kevin Wei <kevin.tw.wei@gmail.com>
2016-10-05 11:51:30 -0400

    Refs #6621 fix plan year in cucumber to be valid

--- features/step_definitions/conversion_employee_steps.rb
+++ features/step_definitions/conversion_employee_steps.rb
@@ -1,10 +1,10 @@
 Given(/^Multiple Conversion Employers for (.*) exist with active and renewing plan years$/) do |named_person|
   person = people[named_person]
-  secondary_organization = FactoryGirl.create :organization, legal_name: person[:mlegal_name], 
-                                                   dba: person[:mdba], 
+  secondary_organization = FactoryGirl.create :organization, legal_name: person[:mlegal_name],
+                                                   dba: person[:mdba],
                                                    fein: person[:mfein]
-  secondary_employer_profile = FactoryGirl.create :employer_profile, organization: secondary_organization, 
-                                                           profile_source:'conversion', 
+  secondary_employer_profile = FactoryGirl.create :employer_profile, organization: secondary_organization,
+                                                           profile_source:'conversion',
                                                            registered_on: TimeKeeper.date_of_record
   secondary_employee = FactoryGirl.create :census_employee, employer_profile: secondary_employer_profile,
                                                             first_name: person[:first_name],
@@ -14,33 +14,33 @@ Given(/^Multiple Conversion Employers for (.*) exist with active and renewing pl
   open_enrollment_start_on = (TimeKeeper.date_of_record-1.month).end_of_month + 1.day
   open_enrollment_end_on = open_enrollment_start_on.next_month + 12.days
   start_on = open_enrollment_start_on + 2.months
-  end_on = start_on + 1.year - 1.day                                                            
+  end_on = start_on + 1.year - 1.day
 
-  secondary_plan_year = FactoryGirl.create :plan_year, employer_profile: secondary_employer_profile, 
+  secondary_plan_year = FactoryGirl.create :plan_year, employer_profile: secondary_employer_profile,
                                                        start_on: start_on - 1.year - 3.months,
-                                                       end_on: end_on - 1.year - 3.months,
+                                                       end_on: (end_on - 1.year - 3.months).end_of_month,
                                                        open_enrollment_start_on: open_enrollment_start_on - 1.year - 3.months,
                                                        open_enrollment_end_on: open_enrollment_end_on - 1.year - 3.days - 3.months,
-                                                       fte_count: 2, 
+                                                       fte_count: 2,
                                                        aasm_state: :published
   secondary_benefit_group = FactoryGirl.create :benefit_group, plan_year: secondary_plan_year
   secondary_plan_year.expire!
 
   secondary_employee.add_benefit_group_assignment secondary_benefit_group, secondary_benefit_group.start_on
 
-  plan_year = FactoryGirl.create :plan_year, employer_profile: secondary_employer_profile, 
-                                             start_on: start_on, 
-                                             end_on: end_on, 
-                                             open_enrollment_start_on: open_enrollment_start_on, 
-                                             open_enrollment_end_on: open_enrollment_end_on, 
-                                             fte_count: 2, 
+  plan_year = FactoryGirl.create :plan_year, employer_profile: secondary_employer_profile,
+                                             start_on: start_on,
+                                             end_on: end_on,
+                                             open_enrollment_start_on: open_enrollment_start_on,
+                                             open_enrollment_end_on: open_enrollment_end_on,
+                                             fte_count: 2,
                                              aasm_state: :renewing_draft
-  benefit_group = FactoryGirl.create :benefit_group, plan_year: plan_year, 
+  benefit_group = FactoryGirl.create :benefit_group, plan_year: plan_year,
                                                      title: 'this is the BGGG'
   plan_year.publish!
   secondary_employee.add_renew_benefit_group_assignment benefit_group
 
-  FactoryGirl.create(:qualifying_life_event_kind, market_kind: "shop")                                                          
+  FactoryGirl.create(:qualifying_life_event_kind, market_kind: "shop")
 end
 
 Given(/(.*) Employer for (.*) exists with active and renewing plan year/) do |kind, named_person|
@@ -48,7 +48,7 @@ Given(/(.*) Employer for (.*) exists with active and renewing plan year/) do |ki
   organization = FactoryGirl.create :organization, legal_name: person[:legal_name], dba: person[:dba], fein: person[:fein]
   employer_profile = FactoryGirl.create :employer_profile, organization: organization, profile_source: (kind.downcase == 'conversion' ? kind.downcase : 'self_serve'), registered_on: TimeKeeper.date_of_record
   owner = FactoryGirl.create :census_employee, :owner, employer_profile: employer_profile
-  
+
   employee = FactoryGirl.create :census_employee, employer_profile: employer_profile,
     first_name: person[:first_name],
     last_name: person[:last_name],
@@ -83,19 +83,19 @@ end
 And(/(.*) already matched and logged into employee portal/) do |named_person|
   person = people[named_person]
   employer_profile = EmployerProfile.find_by_fein(person[:fein])
-  ce = employer_profile.census_employees.where(:first_name => /#{person[:first_name]}/i, 
+  ce = employer_profile.census_employees.where(:first_name => /#{person[:first_name]}/i,
                                                :last_name => /#{person[:last_name]}/i).first
-  person_record = FactoryGirl.create(:person_with_employee_role, first_name: person[:first_name], 
-                                                                 last_name: person[:last_name], 
-                                                                 ssn: person[:ssn], 
-                                                                 dob: person[:dob_date], 
-                                                                 census_employee_id: ce.id, 
-                                                                 employer_profile_id: employer_profile.id, 
+  person_record = FactoryGirl.create(:person_with_employee_role, first_name: person[:first_name],
+                                                                 last_name: person[:last_name],
+                                                                 ssn: person[:ssn],
+                                                                 dob: person[:dob_date],
+                                                                 census_employee_id: ce.id,
+                                                                 employer_profile_id: employer_profile.id,
                                                                  hired_on: ce.hired_on)
   FactoryGirl.create :family, :with_primary_family_member, person: person_record
-  user = FactoryGirl.create(:user, person: person_record, 
-                                   email: person[:email], 
-                                   password: person[:password], 
+  user = FactoryGirl.create(:user, person: person_record,
+                                   email: person[:email],
+                                   password: person[:password],
                                    password_confirmation: person[:password])
   login_as user
   visit "/families/home"
@@ -103,8 +103,8 @@ end
 
 And(/(.*) matches all employee roles to employers and is logged in/) do |named_person|
   person = people[named_person]
-  Person.all.select { |stored_person| stored_person["ssn"] == person.ssn && 
-                                      stored_person["dob"] == person.dob 
+  Person.all.select { |stored_person| stored_person["ssn"] == person.ssn &&
+                                      stored_person["dob"] == person.dob
                     }
   organizations = Organization.in(fein: [person[:fein], person[:mfein]])
   employer_profiles = organizations.map(&:employer_profile)
@@ -113,23 +113,23 @@ And(/(.*) matches all employee roles to employers and is logged in/) do |named_p
   user = nil
   employer_profiles.each do |employer_profile|
     if used_person.nil?
-      ce = employer_profile.census_employees.where(:first_name => /#{person[:first_name]}/i, 
+      ce = employer_profile.census_employees.where(:first_name => /#{person[:first_name]}/i,
                                                    :last_name => /#{person[:last_name]}/i).first
-      person_record = FactoryGirl.create(:person_with_employee_role, first_name: person[:first_name], 
-                                                                     last_name: person[:last_name], 
-                                                                     ssn: person[:ssn], 
-                                                                     dob: person[:dob_date], 
-                                                                     census_employee_id: ce.id, 
-                                                                     employer_profile_id: employer_profile.id, 
+      person_record = FactoryGirl.create(:person_with_employee_role, first_name: person[:first_name],
+                                                                     last_name: person[:last_name],
+                                                                     ssn: person[:ssn],
+                                                                     dob: person[:dob_date],
+                                                                     census_employee_id: ce.id,
+                                                                     employer_profile_id: employer_profile.id,
                                                                      hired_on: ce.hired_on)
       FactoryGirl.create :family, :with_primary_family_member, person: person_record
-      user = FactoryGirl.create(:user, person: person_record, 
-                                       email: person[:email], 
-                                       password: person[:password], 
+      user = FactoryGirl.create(:user, person: person_record,
+                                       email: person[:email],
+                                       password: person[:password],
                                        password_confirmation: person[:password])
       used_person = person_record
     else
-      ce = employer_profile.census_employees.where(:first_name => /#{person[:first_name]}/i, 
+      ce = employer_profile.census_employees.where(:first_name => /#{person[:first_name]}/i,
                                                    :last_name => /#{person[:last_name]}/i).first
       used_person.employee_roles.create!(employer_profile_id: employer_profile.id,
                                          ssn: ce.ssn,
