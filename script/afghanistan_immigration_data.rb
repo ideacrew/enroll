0d483b8ff22193005672ce495b9e1a5f20303887
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-10-10 15:38:27 -0400

    Refs #6134 updated script to return data in a CSV

--- script/afghanistan_immigration_data.rb
+++ script/afghanistan_immigration_data.rb
@@ -4,4 +4,17 @@ country_of_citizenship_people = Person.where("consumer_role.vlp_documents.countr
 
 issuing_country_people = Person.where("consumer_role.vlp_documents.issuing_country" => "Afghanistan")
 
-binding.pry
+all_afghanistan_people = (country_of_citizenship_people + issuing_country_people).uniq
+
+CSV.open("afghanistan_people.csv","w") do |csv|
+  csv << ["First Name","Last Name","DOB","Phone Number","Email Address"]
+  all_afghanistan_people.each do |person|
+    phone = person.phones.map(&:full_phone_number).uniq.join(",")
+    email = person.emails.map(&:address)
+    unless person.user.blank?
+      email = (email + [person.user.email]).uniq
+    end
+    email = email.join(",")
+    csv << [person.first_name, person.last_name, person.dob,phone,email]
+  end
+end
