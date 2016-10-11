565b147e802ee6f62335fdd93e0080e8948c9f27
Tony Schaffert <antonio.schaffert@gmail.com>
2016-10-11 08:35:22 -0400

    Column Filter

--- app/models/effective/datatables/array_backed.rb
+++ app/models/effective/datatables/array_backed.rb
@@ -6,14 +6,14 @@ module Effective
 
 
         bulk_actions_column do
-           bulk_action 'Generate Invoice', generate_invoice_exchanges_hbx_profiles_path, data: { method: :post, confirm: 'Generate Invoices?' }, :sortable => false
+           bulk_action 'Generate Invoice', generate_invoice_exchanges_hbx_profiles_path, data: { method: :post, confirm: 'Generate Invoices?' }
         end
 
-        table_column :legal_name, :width => '200px', :proc => Proc.new { |row| link_to row.legal_name.titleize, employers_employer_profile_path(row.employer_profile, :tab=>'home')}, :sortable => false
-        table_column :hbx_id, :width => '100px', :proc => Proc.new { |row| truncate(row.id.to_s, length: 8, omission: '' ) }, :sortable => false
+        table_column :legal_name, :proc => Proc.new { |row| link_to row.legal_name.titleize, employers_employer_profile_path(row.employer_profile, :tab=>'home')}, :sortable => false
+        table_column :hbx_id, :width => '100px', :proc => Proc.new { |row| truncate(row.id.to_s, length: 8, omission: '' ) }, :sortable => false, :filter => false
         table_column :fein, :width => '100px', :proc => Proc.new { |row| row.fein }, :sortable => false
-        table_column :plan_year_status, :proc => Proc.new { |row| row.employer_profile.renewing_plan_year.present? ? 'Renewing' : 'New'}, :sortable => false
-        table_column :conversion,:proc => Proc.new { |row| boolean_to_glyph(row.employer_profile.is_conversion?)}, :filter => {:as => :select, :collection => ['Yes', 'No']}, :sortable => false
+        table_column :plan_year_status, :width => '120px', :proc => Proc.new { |row| row.employer_profile.renewing_plan_year.present? ? 'Renewing' : 'New'}, :filter => false
+        table_column :conversion, :width => '120px', :proc => Proc.new { |row| boolean_to_glyph(row.employer_profile.is_conversion?)}, :filter => {include_blank: false, :as => :select, :collection => ['All','Yes', 'No'], :selected => 'All'}
         table_column :state,:proc => Proc.new { |row| row.primary_office_location.try(:address).try(:state)} , :filter => false
         table_column :plan_year_state,:proc => Proc.new { |row| row.employer_profile.try(:latest_plan_year).try(:aasm_state)}, :filter => false
         #table_column :update_at, :proc => Proc.new { |row| row[5].strftime('%m/%d/%Y')}
@@ -23,7 +23,6 @@ module Effective
 
 
       def collection
-        #binding.pry
         employers = Organization.all_employer_profiles
       end
 
@@ -32,10 +31,18 @@ module Effective
       end
 
       def search_column(collection, table_column, search_term, sql_column)
-        if table_column[:name] == 'fein'
+        if table_column[:name] == 'legal_name'
+          collection.datatable_search(search_term)
+        elsif table_column[:name] == 'fein'
           collection.datatable_search_fein(search_term)
         elsif table_column[:name] == 'conversion'
-          collection.where("employer_profile.profile_source" => (search_term == 'Yes' ? "conversion" : "self_serve"))
+          if search_term == "Yes"
+            collection.datatable_search_employer_profile_source("conversion")
+          elsif search_term == "No"
+            collection.datatable_search_employer_profile_source("self_serve")
+          else
+            super
+          end
         else
           super
         end
