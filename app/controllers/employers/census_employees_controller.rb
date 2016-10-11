17c41f399435deffc4f44d5e637f8d82013dca05
nisanth <nisanth.yaganti@gmail.com>
2016-10-11 15:24:35 -0400

    Refs #11123 Hiding Cancelled Enrollments from Enrollment History

--- app/controllers/employers/census_employees_controller.rb
+++ app/controllers/employers/census_employees_controller.rb
@@ -184,7 +184,7 @@ class Employers::CensusEmployeesController < ApplicationController
   end
 
   def show
-    past_enrollment_statuses = HbxEnrollment::TERMINATED_STATUSES + HbxEnrollment::CANCELED_STATUSES
+    past_enrollment_statuses = HbxEnrollment::TERMINATED_STATUSES
     @past_enrollments = @census_employee.employee_role.person.primary_family.all_enrollments.select { |hbx_enrollment| past_enrollment_statuses.include? hbx_enrollment.aasm_state } if @census_employee.employee_role.present?
     @status = params[:status] || ''
   end
