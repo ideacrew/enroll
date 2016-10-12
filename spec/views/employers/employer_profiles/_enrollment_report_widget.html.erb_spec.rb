fde272c10fa4c71bb26c550a1801d5c41f6c8768
202chaz <chaz.haskins@gmail.com>
2016-10-12 10:29:57 -0400

    Updated new widget spec

--- spec/views/employers/employer_profiles/_enrollment_report_widget.html.erb_spec.rb
+++ spec/views/employers/employer_profiles/_enrollment_report_widget.html.erb_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
-RSpec.describe "employers/employer_profiles/_enrollment_report_widget.html.erb" do
-  let(:plan_year) { FactoryGirl.build_stubbed(:plan_year) }
+RSpec.describe "_enrollment_report_widget.html.erb" do
+  let(:employer_profile){FactoryGirl.create(:employer_profile)}
+  let(:plan_year){FactoryGirl.create(:plan_year, employer_profile: employer_profile)}
 
   context "with active plan year billing date" do
-    let(:employer_profile) { FactoryGirl.build_stubbed(:employer_profile, plan_years: [plan_year]) }
     before :each do
       assign(:employer_profile, employer_profile)
-      render "employers/employer_profiles/enrollment_report_widget"
+      assign(:plan_year, plan_year)
+      plan_year.update_attributes(aasm_state:'published')
+      render 'employers/employer_profiles/enrollment_report_widget'
     end
-  end
-
+  
+      it "should display the following text" do
+       expect(rendered).to have_selector('td', text: /Total Premium:/i)
+       expect(rendered).to have_selector('td', text: /Employee Contributions:/i)
+       expect(rendered).to have_selector('td', text: /Employer Contributions:/i)
+      end
+    end
+    
   context "without active plan year" do
     let(:employer_profile) { FactoryGirl.build_stubbed(:employer_profile) }
     before :each do
       assign(:employer_profile, employer_profile)
       allow(employer_profile).to receive(:billing_plan_year).and_return([])
-      render "employers/employer_profiles/enrollment_report_widget"
+      render 'employers/employer_profiles/enrollment_report_widget'
     end
 
     it "should not display the widget" do
