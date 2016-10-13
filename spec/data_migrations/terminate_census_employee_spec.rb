03a6d5aac731723f943d4e95666d0476a97eac40
saimekala07 <saimekala07@gmail.com>
2016-10-13 16:29:06 -0400

    refs #11423 adding spec for census employee with past and future employment_terminated_on

--- spec/data_migrations/terminate_census_employee_spec.rb
+++ spec/data_migrations/terminate_census_employee_spec.rb
@@ -11,4 +11,54 @@ describe TerminateCensusEmployee do
       expect(subject.name).to eql given_task_name
     end
   end
+
+  describe "census employee's employment_terminated_on with past date" do
+    subject { TerminateCensusEmployee.new("termiante_census_employee", double(:current_scope => nil)) }
+
+    let(:employer_profile) { FactoryGirl.create(:employer_profile) }
+    let(:employer_profile_id) { employer_profile.id }
+    let(:census_employee) { FactoryGirl.create(:census_employee, employer_profile_id: employer_profile.id, employment_terminated_on: TimeKeeper::date_of_record - 5.days, hired_on: "2014-11-11") }
+    let(:census_employee_params) {
+      {"first_name" => "aqzz",
+       "middle_name" => "",
+       "last_name" => "White",
+       "gender" => "male",
+       "is_business_owner" => true,
+       "hired_on" => "05/02/2015",
+       "employer_profile_id" => employer_profile_id} }
+
+    before :each do
+      census_employee.aasm_state="employee_termination_pending"
+      census_employee.save!
+      subject.migrate
+      census_employee.reload
+    end
+    it "census employee should termianted" do
+      expect(census_employee.aasm_state).to eq "employment_terminated"
+    end
+  end
+
+  describe "census employee's employment_terminated_on with future date" do
+    subject { TerminateCensusEmployee.new("termiante_census_employee", double(:current_scope => nil)) }
+
+    let(:employer_profile) { FactoryGirl.create(:employer_profile) }
+    let(:employer_profile_id) { employer_profile.id }
+    let(:census_employee) { FactoryGirl.create(:census_employee, employer_profile_id: employer_profile.id, hired_on: "2014-11-11") }
+    let(:census_employee_params) {
+      {"first_name" => "aqzz",
+       "middle_name" => "",
+       "last_name" => "White",
+       "gender" => "male",
+       "is_business_owner" => true,
+       "hired_on" => "05/02/2015",
+       "employer_profile_id" => employer_profile_id} }
+    before :each do
+      census_employee.terminate_employment!(TimeKeeper.date_of_record + 5.days)
+      subject.migrate
+      census_employee.reload
+    end
+    it "census employee termination should be in pending state" do
+      expect(census_employee.aasm_state).to eq "employee_termination_pending"
+    end
+  end
 end
