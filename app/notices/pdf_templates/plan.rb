7a1c0b1ed1053e604c8d60c7adc2de60bd5df55d
Battula <vrbattula@outlook.com>
2016-10-14 10:34:34 -0400

    refs #10449 Use the same pdf template for the plan data

--- app/notices/pdf_templates/plan.rb
+++ app/notices/pdf_templates/plan.rb
@@ -6,6 +6,9 @@ module PdfTemplates
     attribute :open_enrollment_end_on, Date
     attribute :coverage_start_on, Date
     attribute :coverage_end_on, Date
-
+    attribute :metal_level, String
+    attribute :coverage_kind, String
+    attribute :plan_carrier, String
+    attribute :hsa_plan, Boolean
   end
 end
