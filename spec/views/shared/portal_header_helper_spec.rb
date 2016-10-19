94311e207f1bbec30f15b5786688b549847908c9
202chaz <chaz.haskins@gmail.com>
2016-10-19 13:16:21 -0400

    Corrected helper spec

--- spec/views/shared/portal_header_helper_spec.rb
+++ spec/views/shared/portal_header_helper_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "layouts/_portal_header_helper.erb" do
-  
-  let(:person_user){Person.new(first_name: 'fred', last_name: 'flintstone')}
-  let(:current_user){FactoryGirl.create(:user, :person=>person_user)}
-  let(:employer_profile){ FactoryGirl.build(:employer_profile) }
-  let(:employer_staff_role){ FactoryGirl.build(:employer_staff_role, :person=>person_user, :employer_profile_id=>employer_profile.id)}
-  let(:signed_in?){ true }
-  
-  before(:each) do
-  	sign_in current_user
+  RSpec.configure do |c|
+    c.include PortalHeaderHelper
   end
-  
-  it 'checks that a link to Im an Employer is present' do
-    allow(person_user).to receive(:employer_staff_roles).and_return(employer_staff_role)
-    current_user.roles=['employer_staff']
-    current_user.save
-    render :template => 'layouts/_header.html.erb'
-    expect(rendered).to match(/I'm an Employer/)
+
+  RSpec.describe "Builds user with correct role" do
+    let(:this_person) { FactoryGirl.build(:person) }
+    let(:current_user) { FactoryGirl.build(:user, :person=>this_person)}
+    let(:employer_profile){ FactoryGirl.build(:employer_profile) }
+    let(:employer_staff_role){ FactoryGirl.create(:employer_staff_role, :person=>this_person, :employer_profile_id=>employer_profile.id)}
+    let(:signed_in?){ true }
+    let(:emp_id) {employer_profile.id}
+    
+    before(:each) do
+    	sign_in current_user
+    end
+    
+    it "returns I'm an Employer is user has correct role" do
+      allow(this_person).to receive(:active_employer_staff_roles).and_return([employer_staff_role])
+      current_user.roles=['employer_staff']
+      current_user.save
+      expect(portal_display_name(controller)).to eq "<a class=\"portal\" href=\"/employers/employer_profiles/"+ emp_id.to_s + "\"><img src=\"/images/icons/icon-business-owner.png\" alt=\"Icon business owner\" /> &nbsp; I'm an Employer</a>"
+    end
+    
+    it "returns Welcome prompt if user doesnt have correct role" do
+      current_user.roles=['']
+      current_user.save
+      expect(portal_display_name(controller)).to eq "<a class='portal'>Welcome to the District's Health Insurance Marketplace</a>"
+    end
   end
-  
-end
