d5328a2384326e045b64294aaeb2e8149bd5c94f
nisanth <nisanth.yaganti@gmail.com>
2016-10-19 20:27:03 -0400

    Refs #10924 removed 7/1 ER from the group & included plan years with renewing publish pending status

--- app/data_migrations/group_conversion_employers_migration.rb
+++ app/data_migrations/group_conversion_employers_migration.rb
@@ -3,7 +3,7 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class GroupConversionEmployersMigration < MongoidMigrationTask
   def migrate
-    fein = ["520818450", "530160510", "262903254", "942233274", "264195952", "900874591", "311799296", "522351337", "731681983", "203142831", "272664900", "520907700", "274415158", "530068130", "521516688", "471925250"]
+    fein = ["520818450", "530160510", "262903254", "942233274", "264195952", "900874591", "311799296", "522351337", "731681983", "203142831", "272664900", "520907700", "530068130", "521516688", "471925250"]
     fein.each do |fein|
       organization = Organization.where(fein: fein)
       if organization.size != 1
@@ -15,7 +15,7 @@ class GroupConversionEmployersMigration < MongoidMigrationTask
           plan_year.migration_expire! if plan_year.may_migration_expire?
         end
       end
-      plan_years = organization.first.employer_profile.plan_years.published + organization.first.employer_profile.plan_years.renewing_published_state + organization.first.employer_profile.plan_years.where(aasm_state: "draft")
+      plan_years = organization.first.employer_profile.plan_years.published + organization.first.employer_profile.plan_years.renewing_published_state + organization.first.employer_profile.plan_years.where(aasm_state: "draft") + organization.first.employer_profile.plan_years.where(aasm_state: "renewing_publish_pending")
       plan_years.each do |plan_year|
         if plan_year.start_on.year == 2016
           plan_year.update_attribute(:aasm_state, "canceled")
