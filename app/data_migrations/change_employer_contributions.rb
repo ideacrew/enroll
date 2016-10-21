39ff8d8902ecdc958e3e66a7a8172929c485d096
nisanth <nisanth.yaganti@gmail.com>
2016-10-20 23:34:52 -0400

    Refs #11108 #11510 adding benefits check in rake task

--- app/data_migrations/change_employer_contributions.rb
+++ app/data_migrations/change_employer_contributions.rb
@@ -7,9 +7,11 @@ class ChangeEmployerContributions < MongoidMigrationTask
     state = ENV['aasm_state'].to_s
     relationship = ENV['relationship'].to_s
     premium = ENV['premium']
+    offered = ENV['offered']
     if organizations.size !=1
       raise 'Issues with fein'
     end
-    organizations.first.employer_profile.plan_years.where(aasm_state: state).first.benefit_groups.first.relationship_benefits.where(:relationship => relationship).first.update_attributes(:premium_pct => premium)
+    rb = organizations.first.employer_profile.plan_years.where(aasm_state: state).first.benefit_groups.first.relationship_benefits.where(:relationship => relationship).first
+    rb.update_attributes(:premium_pct => premium, offered: offered)
   end
 end
