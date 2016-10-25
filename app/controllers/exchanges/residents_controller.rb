8c5e1fd5175a6c5f8b7330568a10b01acf1bb8a1
Andrej Rasevic <andrej@rasevicengineering.com>
2016-10-24 22:30:47 -0400

    Refs #11076 fixing UI for consumer account view

--- app/controllers/exchanges/residents_controller.rb
+++ app/controllers/exchanges/residents_controller.rb
@@ -65,7 +65,7 @@ class Exchanges::ResidentsController < ApplicationController
     @resident_candidate = Forms::ResidentCandidate.new(@person_params)
     @person = @resident_candidate
     respond_to do |format|
-      if found_person = @residendt_candidate.match_person
+      if found_person = @resident_candidate.match_person
         session[:person_id] = found_person.id
         format.html { render 'match' }
       else
