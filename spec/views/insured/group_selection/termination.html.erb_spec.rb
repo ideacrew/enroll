7cb5a7162291d84e22408b3e17b458ab4a6be89f
Kevin Wei <kevin.tw.wei@gmail.com>
2016-10-28 14:06:29 -0400

    Refs #3998 #11476
    spec fix for stage

--- spec/views/insured/group_selection/termination.html.erb_spec.rb
+++ spec/views/insured/group_selection/termination.html.erb_spec.rb
@@ -5,11 +5,14 @@ RSpec.describe "insured/group_selection/terminate_confirm.html.erb"  do
   let(:person) { FactoryGirl.build(:person) }
   let(:plan) { FactoryGirl.build(:plan) }
   let(:hbx_enrollment) { HbxEnrollment.new(plan: plan) }
+  let(:employer_profile) { double("EmployerProfile", legal_name: "Merge Conflict")}
   let(:family) { Family.new }
   before :each do
     allow(hbx_enrollment).to receive(:can_complete_shopping?).and_return false
     allow(hbx_enrollment).to receive(:may_terminate_coverage?).and_return true
     allow(hbx_enrollment).to receive(:is_shop?).and_return true
+    allow(hbx_enrollment).to receive(:coverage_year).and_return(TimeKeeper.date_of_record.year)
+    allow(hbx_enrollment).to receive(:employer_profile).and_return(employer_profile)
     assign :change_plan, 'change_plan'
     assign :employee_role, employee_role
     assign :person, person
