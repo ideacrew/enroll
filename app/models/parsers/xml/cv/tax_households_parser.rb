fb3abd3e8ce37dc36e68b469184d4ba92b082d57
202chaz <chaz.haskins@gmail.com>
2016-10-31 12:31:38 -0400

    Refs Enhancement-11787

--- app/models/parsers/xml/cv/tax_households_parser.rb
+++ app/models/parsers/xml/cv/tax_households_parser.rb
@@ -11,6 +11,7 @@ module Parsers::Xml::Cv
     element :tax_household_size, String, tag: 'tax_household_size/ns0:total_count'
     has_many :eligibility_determinations, Parsers::Xml::Cv::EligibilityDeterminationsParser, tag: 'eligibility_determination', namespace: 'ns0'
     element :start_date, Date
+    element :end_date, Date
     element :submitted_at, DateTime
     element :is_active, Boolean
     element :created_at, DateTime
@@ -24,6 +25,7 @@ module Parsers::Xml::Cv
         tax_household_size: tax_household_size,
         eligibility_determinations: eligibility_determinations.map(&:to_hash),
         start_date: start_date,
+        end_date: end_date,
         submitted_at: submitted_at,
         is_active: is_active,
         created_at: created_at
