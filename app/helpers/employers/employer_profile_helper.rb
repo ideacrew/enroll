7e486c48ae2cb12463eec4fcbc9ff243e82ef735
sarathpyerra <sarathpyerra@gmail.com>
2016-11-02 19:55:19 -0400

    Refs #9698 adding plan year states to see generate oop link

--- app/helpers/employers/employer_profile_helper.rb
+++
@@ -1,5 +0,0 @@
-module Employers::EmployerProfileHelper
- def show_oop_pdf_link(aasm_state)
-  ["enrolling" ,"published", "enrolled"," active","renewing_published", "renewing_enrolling", "renewing_enrolled"].include?(aasm_state)
-  end
-end
