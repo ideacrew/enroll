655ae85781e3d32d91a349b3e178a9e189731b94
Tony Schaffert <antonio.schaffert@gmail.com>
2016-11-05 00:51:02 -0400

    Check bottleneck

--- app/models/effective/datatables/employer_datatable.rb
+++ app/models/effective/datatables/employer_datatable.rb
@@ -16,12 +16,12 @@ module Effective
         table_column :fein, :label => 'FEIN', :proc => Proc.new { |row| row.fein }, :sortable => false, :filter => false
         #table_column :plan_year_status, :width => '120px', :proc => Proc.new { |row| row.employer_profile.renewing_plan_year.present? ? 'Renewing' : 'New'}, :filter => false
         #table_column :eligibility, :proc => Proc.new { |row| eligibility_criteria(row.employer_profile) }, :filter => false
-        table_column :broker, :proc => Proc.new { |row|
-            row.employer_profile.broker_agency_profile.organization.legal_name.titleize if row.employer_profile.broker_agency_profile.present?
-          }, :filter => false
-        table_column :general_agency, :proc => Proc.new { |row|
-          row.employer_profile.active_general_agency_legal_name.titleize if row.employer_profile.active_general_agency_legal_name.present?
-        }, :filter => false
+        # table_column :broker, :proc => Proc.new { |row|
+        #     row.employer_profile.broker_agency_profile.organization.legal_name.titleize if row.employer_profile.broker_agency_profile.present?
+        #   }, :filter => false
+        # table_column :general_agency, :proc => Proc.new { |row|
+        #   row.employer_profile.active_general_agency_legal_name.titleize if row.employer_profile.active_general_agency_legal_name.present?
+        # }, :filter => false
         table_column :conversion, :proc => Proc.new { |row| boolean_to_glyph(row.employer_profile.is_conversion?)}, :filter => {include_blank: false, :as => :select, :collection => ['All','Yes', 'No'], :selected => 'All'}
 
         table_column :plan_year_state, :proc => Proc.new { |row|
@@ -32,7 +32,7 @@ module Effective
           @latest_plan_year.try(:start_on)
           }, :filter => false
         table_column :invoiced?, :proc => Proc.new { |row| boolean_to_glyph(row.current_month_invoice.present?)}, :filter => false
-        #table_column :participation, :proc => Proc.new { |row| row.employer_profile.try(:latest_plan_year).try(:employee_participation_percent)}, :filter => false
+        table_column :participation, :proc => Proc.new { |row| row.employer_profile.try(:latest_plan_year).try(:employee_participation_percent)}, :filter => false
         table_column :enrolled_waived, :label => 'Enrolled/Waived', :proc => Proc.new { |row|
           #plan_year = row.employer_profile.try(:latest_plan_year)
 
