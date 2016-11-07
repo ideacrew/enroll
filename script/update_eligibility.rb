4a90bde0df9a5177f593a7dc9915c1bb7ca7550f
Ken Lu <ken@seeice.com>
2016-11-06 23:54:32 -0500

    Refs #11879 #11895 - updated the script to support no family issue and rerun check it will not create the determination again

--- script/update_eligibility.rb
+++ script/update_eligibility.rb
@@ -1,16 +1,58 @@
 require 'csv'
-
 @pdc = 0
 @slcsp = HbxProfile.current_hbx.benefit_sponsorship.benefit_coverage_periods.last.slcsp_id
 @wrong_ssn_counter = 0
 
+def check
+  ran = []
+  not_run = []
+  CSV.foreach("spec/test_data/cne.csv") do |row|
+    person = Person.by_hbx_id(row[0]).first
+    deter = person.families.first.households.first.tax_households.where(effective_ending_on: nil).select{|x| x.effective_starting_on.year == 2017 }.first.try(:eligibility_determinations).try(:first)
+    if deter && deter.e_pdc_id =~ /MANUALLY_9_2_2016LOADING/
+      ran << row[0]
+    else
+      not_run << row[0]
+    end
+  end
+  [ran, not_run]
+end
+
+def check_and_run
+  ran = []
+  not_run = []
+  running = []
+
+  CSV.foreach("spec/test_data/cne2.csv") do |row_with_ssn|
+    person = Person.by_ssn(row_with_ssn.first).first
+    if person
+      deter = person.families.first.households.first.tax_households.where(effective_ending_on: nil).select{|x| x.effective_starting_on.year == 2017 }.first.try(:eligibility_determinations).try(:first)
+      if deter && deter.e_pdc_id =~ /MANUALLY_9_2_2016LOADING/
+        ran << row_with_ssn.first
+      else
+        hbx_id = person.hbx_id
+        row=[hbx_id, row_with_ssn[1], row_with_ssn[2]]
+        update_aptc(row)
+        running << row_with_ssn.first
+      end
+    else
+      not_run << row_with_ssn.first
+      puts "Person with ssn: #{row_with_ssn.first} can't be found."
+    end
+  end
+
+  [ran, not_run, running]
+end
+
+
 def update_aptc(row)
   person = Person.by_hbx_id(row[0]).first
+  return unless person.primary_family
   @household = person.primary_family.active_household
 
   if @household.tax_households.present?
     latest_tax_household = @household.tax_households.where(effective_ending_on: nil).last
-    latest_tax_household.update_attributes(effective_ending_on: Date.new(2016,12,31))
+    latest_tax_household.update_attributes(effective_ending_on: Date.new(2016,12,31)) if latest_tax_household
   end
 
   th = @household.tax_households.build(
@@ -51,20 +93,22 @@ def update_aptc(row)
   puts person.primary_family.active_household.tax_households.last.eligibility_determinations.inspect
 end
 
-CSV.foreach("spec/test_data/cne.csv") do |row|
-  update_aptc(row)
-end
-
-CSV.foreach("spec/test_data/cne2.csv") do |row_with_ssn|
-  person = Person.by_ssn(row_with_ssn.first).first
-  if person
-    hbx_id = person.hbx_id
-    row=[hbx_id, row_with_ssn[1], row_with_ssn[2]]
+def run
+  CSV.foreach("spec/test_data/cne.csv") do |row|
     update_aptc(row)
-  else
-    @wrong_ssn_counter += 1
-    puts "Person with ssn: #{row_with_ssn.first} can't be found."
   end
-end
 
-puts "#{@wrong_ssn_counter} people can't be found by SSN" if @wrong_ssn_counter > 0
+  CSV.foreach("spec/test_data/cne2.csv") do |row_with_ssn|
+    person = Person.by_ssn(row_with_ssn.first).first
+    if person
+      hbx_id = person.hbx_id
+      row=[hbx_id, row_with_ssn[1], row_with_ssn[2]]
+      update_aptc(row)
+    else
+      @wrong_ssn_counter += 1
+      puts "Person with ssn: #{row_with_ssn.first} can't be found."
+    end
+  end
+
+  puts "#{@wrong_ssn_counter} people can't be found by SSN" if @wrong_ssn_counter > 0
+end
