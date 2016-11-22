9e68e88d5d93a0b4fc4ca1e94ba6f82f3cc83b49
Trey Evans <lewis.r.evans@gmail.com>
2016-11-22 10:06:24 -0500

    Fixing data.

--- script/ivl_initial_enrollments_2017.rb
+++ script/ivl_initial_enrollments_2017.rb
@@ -17,12 +17,29 @@ active_selection_families = Family.where("households.hbx_enrollments.hbx_id" => 
 active_selection_new_enrollments = []
 active_selection_families.each do |fam|
   all_policies = fam.households.flat_map(&:hbx_enrollments)
-  policy_for_2017 = all_policies.detect { |pol| all_active_selections.include?(pol.hbx_id) }
-  found_a_2016 = all_policies.any? do |pol|
-    (pol.effective_on <=  Date.new(2016,12,31)) &&
-      (pol.effective_on >  Date.new(2015,12,31))
+  policies_for_2017 = all_policies.select { |pol| all_active_selections.include?(pol.hbx_id) }
+  policies_for_2017.each do |policy_for_2017|
+    found_a_2016 = all_policies.any? do |pol|
+      ((pol.effective_on <=  Date.new(2016,12,31)) &&
+       (pol.effective_on >  Date.new(2015,12,31))) &&
+      ((pol.terminated_on.blank?) || (!(pol.terminated_on < Date.new(2015,12,31)))) &&
+      ((!pol.plan_id.blank?) && (pol.coverage_kind == policy_for_2017.coverage_kind)) &&
+      (pol.is_shop? == policy_for_2017.is_shop?)
+    end
+    if !found_a_2016
+      if policy_for_2017.subscriber.present?
+      if policy_for_2017.subscriber.family_member == policy_for_2017.household.family.primary_applicant
+        active_selection_new_enrollments << policy_for_2017.hbx_id
+      end
+      end
+    end
   end
-  if !found_a_2016
-    puts policy_for_2017.hbx_id
-  end
+end
+
+glue_list = File.read("all_glue_policies.txt").split("\n").map(&:strip)
+
+missing = (active_selection_new_enrollments - glue_list)
+
+missing.each do |m|
+    puts m
 end
