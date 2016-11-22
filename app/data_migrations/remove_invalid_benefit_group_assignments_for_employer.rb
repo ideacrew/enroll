28bb6b6b2f9e1ce571de5fc4a5e42f282d058c03
saimekala07 <saimekala07@gmail.com>
2016-11-22 14:53:17 -0500

    changing as per comments

--- app/data_migrations/remove_invalid_benefit_group_assignments_for_employer.rb
+++ app/data_migrations/remove_invalid_benefit_group_assignments_for_employer.rb
@@ -6,15 +6,15 @@ class RemoveInvalidBenefitGroupAssignmentsForEmployer < MongoidMigrationTask
     feins=ENV['fein'].split(",")
     feins.each do |fein|
       organization = Organization.where(fein: fein)
-      if organization.size != 1 || !organization.first.employer_profile.present?
-         puts "*************Issues with the fein=#{fein}*******************" and return
-      else
+      if organization.size == 1 && organization.first.employer_profile.present?
         organization.first.employer_profile.census_employees.each do |ce|
           ce.benefit_group_assignments.select { |bga| bga.hbx_enrollments.blank? && bga.benefit_group.blank? }.each do |bga|
             bga.delete
             puts "Deleting invalid benefit group assignments for #{ce.first_name} #{ce.last_name} " unless Rails.env.test?
           end
         end
+      else
+        puts "*************Issues with the fein=#{fein}*******************" unless Rails.env.test?
       end
     end
   end
