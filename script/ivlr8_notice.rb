5099eef96985bb00011219b6021779b7e003bcc5
Battula <vrbattula@outlook.com>
2016-11-23 13:48:26 -0500

    refs #10449 Modify script

--- script/ivlr8_notice.rb
+++ script/ivlr8_notice.rb
@@ -29,29 +29,28 @@
     notice_trigger = event_kind.notice_triggers.first
 
     families.each do |fam|
-      begin
-        primary_member = fam.primary_applicant.person
-        consumer_role =primary_member.consumer_role
-        if consumer_role.present?
-            builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
-                  template: notice_trigger.notice_template,
-                  subject: event_kind.title,
-                  mpi_indicator: notice_trigger.mpi_indicator,
-                  enrollment_group_ids: enrollment_group_ids,
-                  plan_data_2016: plans_2016
-                  }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)
-                  )
-            builder.deliver
-          csv << [
-              primary_member.hbx_id,
-              primary_member.full_name
-            ]
-        else
-          puts "Unable to send notice to person : #{primary_member.hbx_id} #{e.backtrace}"
+      primary_member = fam.primary_applicant.person
+      consumer_role =primary_member.consumer_role
+      if consumer_role.present?
+        begin
+          builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
+                template: notice_trigger.notice_template,
+                subject: event_kind.title,
+                mpi_indicator: notice_trigger.mpi_indicator,
+                enrollment_group_ids: enrollment_group_ids,
+                plan_data_2016: plans_2016
+                }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)
+                )
+          builder.deliver
+        rescue Exception => e
+          puts "Unable to deliver to #{primary_member.hbx_id} for the following error #{e.backtrace}"
         end
-      rescue Exception => e
-        puts "Unable to deliver to #{primary_member.hbx_id} for the following error #{e.backtrace}"
-        next
+        csv << [
+            primary_member.hbx_id,
+            primary_member.full_name
+          ]
+      else
+        puts "Unable to send notice to person : #{primary_member.hbx_id} #{e.backtrace}"
       end
     end
   end
