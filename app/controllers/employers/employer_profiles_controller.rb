ce3efc63dcb2ca474aa3cc907a47ee1b841bbc15
saimekala07 <saimekala07@gmail.com>
2016-11-30 15:46:06 -0500

    refs #3443 census employee roaster search

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -324,7 +324,10 @@ class Employers::EmployerProfilesController < Employers::EmployersController
                        else
                          @employer_profile.census_employees.active.sorted
                        end
-    census_employees = census_employees.search_by(params.slice(:employee_name))
+    if params["employee_name"].present?
+      query_string = Person.search_hash(params["employee_name"])
+      census_employees = census_employees.any_of(query_string)
+    end
     @page_alphabets = page_alphabets(census_employees, "last_name")
 
     if params[:page].present?
