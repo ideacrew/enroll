62228dc1ed19feb3d8272739867195703ba02307
sarathpyerra <sarathpyerra@gmail.com>
2016-12-02 10:43:24 -0500

    Refs #9698 debugging debugging #testing purpose

--- app/models/subscribers/notification_subscriber.rb
+++ app/models/subscribers/notification_subscriber.rb
@@ -3,21 +3,26 @@ module Subscribers
     include Acapi::Notifiers
 
     def self.subscription_details
-      [/acapi\.info\.events\..*/]
+      ["acapi.info.events.employer.out_of_pocker_url_notifier"]
     end
 
     def call(event_name, e_start, e_end, msg_id, payload)
       application_event_kinds = ApplicationEventKind.application_events_for(event_name)
-      log("NOTICE EVENT: #{event_name} #{payload}", {:severity => 'info'})
+      Rails.logger.info("NOTICE EVENT: #{event_name} #{payload}")
       application_event_kinds.each do |aek|
         begin
-          log("strating to execute_notices")
+          Rails.logger.info("strating to execute_notices")
           aek.execute_notices(event_name, payload)
-          log("execute_notices finished")
+          Rails.logger.info("execute_notices finished")
         rescue => e
-          log("Failied to execute_notices #{e} #{e.backtrace}")
+          Rails.logger.info("Failed to execute_notices #{e} #{e.backtrace}")
         end
       end
+    end
+
+    def self.subscribe 
+      Rails.logger.info "notification_subscriber initialized"
+      super
     end
   end
 end
