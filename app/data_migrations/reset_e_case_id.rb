3b23a83c843bc397aeb7541cc0e4fb5f4836a502
Andrej Rasevic <andrej@rasevicengineering.com>
2016-12-03 12:29:10 -0500

    Refs #12305 added spec for rake task

--- app/data_migrations/reset_e_case_id.rb
+++ app/data_migrations/reset_e_case_id.rb
@@ -2,7 +2,8 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class ResetECaseId < MongoidMigrationTask
   def migrate
-    family = Person.where(hbx_id:  hbx_id).primary_family
+    #family = Person.where(hbx_id:  hbx_id).primary_family
+    family = Person.where(hbx_id: ENV['hbx_id'].to_s).first.primary_family
     if family.present?
       puts "Found family for person with hbx_id #{hbx_id}" unless Rails.env.test?
       family.unset(:e_case_id)
