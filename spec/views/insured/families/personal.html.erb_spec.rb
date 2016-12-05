36ee57323d7abd23031a156184adefb2e813cf57
Jayanth <jayanth.rordev@gmail.com>
2016-12-04 20:21:26 -0500

    refs#3764: Update Employee default communication preference

--- spec/views/insured/families/personal.html.erb_spec.rb
+++ spec/views/insured/families/personal.html.erb_spec.rb
@@ -50,7 +50,7 @@ RSpec.describe "insured/families/personal.html.erb" do
    it_should_behave_like "display_heading"
 
    it "should display contact method dropdown " do
-    expect(rendered).to have_select("person[consumer_role_attributes][contact_method]", :selected => "Only Electronic communications")
+    expect(rendered).to have_select("person[consumer_role_attributes][contact_method]", :selected => "Only Paper communication")
    end
 
    it "should display language preference dropdown " do
