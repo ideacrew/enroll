bb86e476a22f1d74ab6bb45571a2236fe4507f66
saikumar9 <saikumarkotagiri9@gmail.com>
2016-12-06 14:58:12 -0500

    Refs: #11041 #12348 Produce a CSV of all Primary Subscribers in Enroll with an associated intregrated case

--- app/reports/hbx_reports/primary_family_members_data_with_e_case_id.rb
+++ app/reports/hbx_reports/primary_family_members_data_with_e_case_id.rb
@@ -16,18 +16,11 @@ class PrimaryFamilyMembersDataWithECaseId < MongoidMigrationTask
          )
        count = 0
        file_name = "#{Rails.root}/public/primary_family_members_data_with_e_case_id.csv"
-       family_count =Family.where(:e_case_id.nin => ["", nil]).to_a.count
-       offset_count =0
-       limit_count = 500
+      CSV.open(file_name, "w", force_quotes: true) do |csv|
+         csv << field_names
+         families = Family.where(:e_case_id.nin => ["", nil])
 
-       CSV.open(file_name, "w", force_quotes: true) do |csv|
-        csv << field_names
-
-        while (offset_count < family_count) do
-          puts "offset_count: #{offset_count}"
-          families = Family.where(:e_case_id.nin => ["", nil]).limit(limit_count).offset(offset_count).to_a
-
-         families.each do |family|
+         families.all.each do |family|
           begin
             person = family.primary_family_member.person
           
@@ -63,9 +56,8 @@ class PrimaryFamilyMembersDataWithECaseId < MongoidMigrationTask
             puts "Bad Family record with id: #{family.id}" unless Rails.env.test?
           end
         end
-        offset_count += limit_count
       end
-    end
-    puts "Total number of families with e_case_id: #{count}" unless Rails.env.test?
+      puts "Total number of families with e_case_id: #{count}" unless Rails.env.test?
   end
+
 end
