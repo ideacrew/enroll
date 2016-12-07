5df1d8a2e3ec673c8477ac44149e50a21ed07a16
Kevin Wei <kevin.tw.wei@gmail.com>
2016-12-07 15:02:18 -0500

    Attempt to fix flickering parallel time helper spec

--- spec/helpers/time_helper_spec.rb
+++ spec/helpers/time_helper_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe TimeHelper, :type => :helper do
   end
 
   describe "time remaining in words" do
-    it "counts 90 days from user creating date" do
-      expect(helper.time_remaining_in_words(person.created_at)).to eq("95 days")
+    it "counts 95 days from the passed in date" do
+      expect(helper.time_remaining_in_words(TimeKeeper.date_of_record)).to eq("95 days")
     end
   end
 end
