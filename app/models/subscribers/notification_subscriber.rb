a2398037bf2b04f20ebe8db6d381ba28d81caf2d
sarathpyerra <sarathpyerra@gmail.com>
2016-12-08 10:27:23 -0500

    Refs Adding Subscriber for Employee-plan match out pf pocket calculator

--- app/models/subscribers/notification_subscriber.rb
+++ app/models/subscribers/notification_subscriber.rb
@@ -8,21 +8,15 @@ module Subscribers
 
     def call(event_name, e_start, e_end, msg_id, payload)
       application_event_kinds = ApplicationEventKind.application_events_for(event_name)
-      Rails.logger.info("NOTICE EVENT: #{event_name} #{payload}")
+      log("NOTICE EVENT: #{event_name} #{payload}", {:severity => 'info'})
+
       application_event_kinds.each do |aek|
         begin
-          Rails.logger.info("strating to execute_notices")
           aek.execute_notices(event_name, payload)
-          Rails.logger.info("execute_notices finished")
-        rescue => e
-          Rails.logger.info("Failed to execute_notices #{e} #{e.backtrace}")
+        rescue
+          # ADD LOGGING AND HANDLING
         end
       end
-    end
-
-    def self.subscribe 
-      Rails.logger.info "notification_subscriber initialized"
-      super
     end
   end
 end
