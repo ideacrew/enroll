d999396effaeee2b268f1a9003bc0a06263422a8
Battula <vrbattula@outlook.com>
2016-12-12 16:44:54 -0500

    refs #12490 Use DateTime instead of TimeKeeper

--- app/notices/ivl_notices/consumer_notice.rb
+++ app/notices/ivl_notices/consumer_notice.rb
@@ -63,7 +63,7 @@ class IvlNotices::ConsumerNotice < IvlNotice
       raise 'no family member found without uploaded documents'
     end
 
-    enrollments.each {|e| e.update_attributes(special_verification_period: TimeKeeper.date_of_record + 95.days)}
+    enrollments.each {|e| e.update_attributes(special_verification_period: Date.today + 95.days)}
 
     append_unverified_individuals(outstanding_people)
     notice.enrollments << (enrollments.detect{|e| e.enrolled_contingent?} || enrollments.first)
