c3cd5d8ee9d8ca21470de6618e1779882934ac00
saimekala07 <saimekala07@gmail.com>
2016-12-14 10:44:33 -0500

    refs# 11908 updating as per new requirment check is_child_age_satisfied on effective date

--- app/models/insured_eligible_for_benefit_rule.rb
+++ app/models/insured_eligible_for_benefit_rule.rb
@@ -15,7 +15,7 @@ class InsuredEligibleForBenefitRule
     @role = role
     @benefit_package = benefit_package
     @coverage_kind = options[:coverage_kind]
-    @enrollment_kind = options[:enrollment_kind]
+    @new_effective_on = options[:new_effective_on]
 
   end
 
@@ -84,16 +84,11 @@ class InsuredEligibleForBenefitRule
 
   def is_family_relationships_satisfied?
     return true unless relation_ship_with_primary_applicant == 'child'
-    is_child_age_satisfied? || is_child_26_with_sep_satisfied?
+    is_child_age_satisfied?
   end
 
   def is_child_age_satisfied?
-    age = (TimeKeeper.date_of_record.year - @role.dob.year)
-    relation_ship_with_primary_applicant == 'child' && age <= 25 ? true : false
-  end
-
-  def is_child_26_with_sep_satisfied?
-    (relation_ship_with_primary_applicant == 'child' && @role.dob.year+26 == TimeKeeper.date_of_record.year && @enrollment_kind =='sep' && @role.person.families.first.special_enrollment_periods.last.effective_on.year == TimeKeeper.date_of_record.year) ? true : false
+    relation_ship_with_primary_applicant == 'child' && @new_effective_on.kind_of?(Date) && @new_effective_on < @role.dob+26.years ? true : false
   end
 
   def is_benefit_categories_satisfied?
