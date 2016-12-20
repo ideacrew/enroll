84be7cccd6216be75429ff665135e977802d80e0
saikumar9 <saikumarkotagiri9@gmail.com>
2016-12-20 10:56:22 -0500

    Refs#11041 Enhancement

--- app/reports/hbx_reports/primary_family_members_data_with_e_case_id.rb
+++ app/reports/hbx_reports/primary_family_members_data_with_e_case_id.rb
@@ -13,17 +13,27 @@ class PrimaryFamilyMembersDataWithECaseId < MongoidMigrationTask
           Gender
           Primary_Person_Record_Create_Date
           Type
+          Active_Enrollment
+          Applied_aptc_amount
+          CSR_Enrollment
          )
        count = 0
        file_name = "#{Rails.root}/public/primary_family_members_data_with_e_case_id.csv"
-      CSV.open(file_name, "w", force_quotes: true) do |csv|
-         csv << field_names
-         families = Family.where(:e_case_id.nin => ["", nil])
+       CSV.open(file_name, "w", force_quotes: true) do |csv|
+        csv << field_names
+        families = Family.where(:e_case_id.nin => ["", nil])
 
-         families.all.each do |family|
+        families.all.each do |family|
           begin
             person = family.primary_family_member.person
-          
+            aptc = 0
+            csr = "No"
+            if family.has_aptc_hbx_enrollment?
+              enrollments = family.latest_household.hbx_enrollments.active
+              aptc = enrollments.each {|enrollment| enrollment.applied_aptc_amount > 0}.first.applied_aptc_amount.to_f
+              csr = "Yes" if family.active_household.hbx_enrollments.with_aptc.any? {|enrollment| enrollment.plan.is_csr? }
+            end
+
             csv << [
               family.e_case_id,
               person.first_name,
@@ -33,23 +43,28 @@ class PrimaryFamilyMembersDataWithECaseId < MongoidMigrationTask
               person.dob,
               person.gender,
               person.created_at.to_date,
-              "Primary"
+              "Primary",
+              Person.person_has_an_active_enrollment?(person) ? "Yes" : "No",
+              aptc,
+              csr
              ]
-             
+
              family.dependents.each do |dependent|
               dependent_person = dependent.person
               csv << [
-                  family.e_case_id,
-                  dependent_person.first_name,
-                  dependent_person.last_name,
-                  dependent_person.hbx_id,
-                  dependent_person.ssn,
-                  dependent_person.dob,
-                  dependent_person.gender,
-                  dependent_person.created_at.to_date,
-                  "Dependent"
+                family.e_case_id,
+                dependent_person.first_name,
+                dependent_person.last_name,
+                dependent_person.hbx_id,
+                dependent_person.ssn,
+                dependent_person.dob,
+                dependent_person.gender,
+                dependent_person.created_at.to_date,
+                "Dependent",
+                Person.person_has_an_active_enrollment?(dependent_person) ? "Yes" : "No",
+                aptc,
+                csr
               ]
-
              end
             count += 1
           rescue
@@ -59,5 +74,4 @@ class PrimaryFamilyMembersDataWithECaseId < MongoidMigrationTask
       end
       puts "Total number of families with e_case_id: #{count}" unless Rails.env.test?
   end
-
 end
