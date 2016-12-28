75c0a3fbc62b399ea93b3860e309bffe2fa80d7b
sarathpyerra <sarathpyerra@gmail.com>
2016-12-28 11:44:07 -0500

    Refs #11474 minor text changes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,9 @@
 class DashboardsController < ApplicationController
 
 def home
-  @fundtransfer=CioDashboard::FundTransfer.fundtransfer_dashboard_stats
-  @pipeline = CioDashboard::Pipeline.pipeline_dashboard_stats
-  @redmine=CioDashboard::Redmine.redmine_dashboard_stats
+  @fundtransfers=CioDashboard::FundTransfer.fundtransfer_dashboard_stats
+  @pipelines = CioDashboard::Pipeline.pipeline_dashboard_stats
+  @redmines=CioDashboard::Redmine.redmine_dashboard_stats
   @events=CioDashboard::UpcomingEvent.uc_events_dashboard_stats
 end
 
