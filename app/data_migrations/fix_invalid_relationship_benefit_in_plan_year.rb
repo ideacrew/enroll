203ddfd9c0c68e01c347f3e573bd58838e114a24
saimekala07 <saimekala07@gmail.com>
2017-01-13 15:14:30 -0500

    refs #13151 looping relationships

--- app/data_migrations/fix_invalid_relationship_benefit_in_plan_year.rb
+++ app/data_migrations/fix_invalid_relationship_benefit_in_plan_year.rb
@@ -5,10 +5,11 @@ class FixInvalidRelationshipBenefitInPlanYear < MongoidMigrationTask
     organizations = Organization.no_timeout.where("employer_profile" => {"$exists" => true})
     organizations.each do |org|
       org.employer_profile.plan_years.each do |plan_year|
-        child_over_26_relationship_benefit= plan_year.benefit_groups.map(&:relationship_benefits).flatten.select{|r| r.relationship == "child_26_and_over"}.first
-        if child_over_26_relationship_benefit.offered
-          child_over_26_relationship_benefit.update_attribute(:offered, false)
-          puts "#{org.employer_profile.legal_name} child_over_26_relationship_benefit updated" unless Rails.env.test?
+        plan_year.benefit_groups.map(&:relationship_benefits).flatten.select{|r| r.relationship == "child_26_and_over"}.each do |child_over_26_relationship|
+          if child_over_26_relationship.offered
+            child_over_26_relationship.update_attribute(:offered, false)
+            puts "#{org.employer_profile.legal_name} child_over_26_relationship_benefit updated" unless Rails.env.test?
+          end
         end
       end
     end
