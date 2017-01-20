1897f24c66a7c95e26c297e92d06f34f757be3ed
Tony Schaffert <antonio.schaffert@gmail.com>
2017-01-20 12:46:55 -0500

    Remove stuff

--- app/controllers/broker_agencies/profiles_controller.rb
+++ app/controllers/broker_agencies/profiles_controller.rb
@@ -136,22 +136,22 @@ class BrokerAgencies::ProfilesController < ApplicationController
       redirect_to new_broker_agencies_profile_path
       return
     end
-
-    total_families = @broker_agency_profile.families
-    @total = total_families.count
-    @page_alphabets = total_families.map{|f| f.primary_applicant.person.last_name[0]}.map(&:capitalize).uniq
-    if page.present?
-      @families = total_families.select{|v| v.primary_applicant.person.last_name =~ /^#{page}/i }
-    elsif @q.present?
-      query= Regexp.escape(@q)
-      query_args= query.split("\\ ")
-      reg_ex = query_args.join('(.*)?')
-      @families = total_families.select{|v| v.primary_applicant.person.full_name =~ /#{reg_ex}/i }
-    else
-      @families = total_families[0..20]
-    end
-
-    @family_count = @families.count
+    #
+    # total_families = @broker_agency_profile.families
+    # @total = total_families.count
+    # @page_alphabets = total_families.map{|f| f.primary_applicant.person.last_name[0]}.map(&:capitalize).uniq
+    # if page.present?
+    #   @families = total_families.select{|v| v.primary_applicant.person.last_name =~ /^#{page}/i }
+    # elsif @q.present?
+    #   query= Regexp.escape(@q)
+    #   query_args= query.split("\\ ")
+    #   reg_ex = query_args.join('(.*)?')
+    #   @families = total_families.select{|v| v.primary_applicant.person.full_name =~ /#{reg_ex}/i }
+    # else
+    #   @families = total_families[0..20]
+    # end
+    #
+    # @family_count = @families.count
     respond_to do |format|
       format.js {}
     end
