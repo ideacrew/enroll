16e6c4ffe7bf7dfd3292cb6a030087eb5369219e
krbhavith <krbhavith@gmail.com>
2017-01-26 16:28:01 -0500

    UI update of Enroll App's Messages to handle tax documents in the S3 cloud doc. store and allow retrieval

--- spec/views/insured/families/inbox.html.erb_spec.rb
+++ spec/views/insured/families/inbox.html.erb_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe "insured/families/inbox.html.erb" do
+describe "insured/families/inbox.html.erb", dbclean: :after_each do
   let(:user) { FactoryGirl.build_stubbed(:user, person: person) }
-  let(:person) { FactoryGirl.build_stubbed(:person) }
+  let(:person) { FactoryGirl.create(:person) }
 
   before :each do
     sign_in(user)
@@ -13,24 +13,52 @@ describe "insured/families/inbox.html.erb" do
   end
 
   context "as admin" do
-    before do
+    before :each do
       allow(view).to receive_message_chain("current_user.has_hbx_staff_role?").and_return(true)
-      render template: "insured/families/inbox.html.erb"
     end
 
     it "should display the upload notices button" do
+      render template: "insured/families/inbox.html.erb"
       expect(rendered).to match(/upload notices/i)
+    end
+    
+    it "should display the download tax documents button if consumer has SSN" do
+      allow(person).to receive(:ssn).and_return '123456789'
+      render template: "insured/families/inbox.html.erb"
+      expect(rendered).to match(/Download Tax Documents/i)
+    end
+
+    it "should not display the download tax documents button if consumer has no SSN" do
+      render template: "insured/families/inbox.html.erb"
+      expect(rendered).not_to match(/Download Tax Documents/i)
     end
   end
 
   context "as insured" do
+    let(:consumer_role) { double('consumer_role', :is_active? => true)}
     before do
       allow(view).to receive_message_chain("current_user.has_hbx_staff_role?").and_return(false)
-      render template: "insured/families/inbox.html.erb"
+      allow(person).to receive(:consumer_role).and_return consumer_role
+      stub_template "insured/families/_navigation.html.erb" => ""
     end
 
     it "should not display the upload notices button" do
+      render template: "insured/families/inbox.html.erb"
       expect(rendered).to_not match(/upload notices/i)
+    end
+
+    it "should display the download tax documents button if consumer has SSN" do
+      allow(person).to receive(:ssn).and_return '123456789'
+      render template: "insured/families/inbox.html.erb"
+      expect(rendered).to match(/Download Tax Documents/i)
+    end
+
+
+    it "should not display the download tax documents button if consumer has no SSN" do
+      render template: "insured/families/inbox.html.erb"
+      expect(rendered).to_not match(/Download Tax Documents/i)
     end
   end
 end
+
+
