4663b205b3b42cc5fc3e20aee17625b674cfc7ac
sarathpyerra <sarathpyerra@gmail.com>
2017-01-30 21:15:51 -0500

    Refs 11474 adding webactivity and call centers IV to CIO-Dashboards-Analytics

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,6 +5,8 @@ def home
   @pipelines = CioDashboard::Pipeline.pipeline_dashboard_stats
   @redmines=CioDashboard::Redmine.redmine_dashboard_stats
   @events=CioDashboard::UpcomingEvent.uc_events_dashboard_stats
+  @callcenters=CioDashboard::CallCenter.callcenter_dashboard_stats
+  @webactivitys=CioDashboard::WebActivity.webactivity_dashboard_stats
 end
 
 end
