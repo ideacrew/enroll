c19b194a94db007d9b89b96964622c4dfdaf411d
Battula <vrbattula@outlook.com>
2017-02-02 13:34:01 -0500

    refs #13795 Trigger employer renewal notice when the plan year enter renewing draft

--- app/models/plan_year.rb
+++ app/models/plan_year.rb
@@ -682,7 +682,7 @@ class PlanYear
 
     state :active         # Published plan year is in-force
 
-    state :renewing_draft
+    state :renewing_draft, :after_enter => :employer_renewal_notice # employer_renewal_notice - Sends a notice three months prior to plan year renewing
     state :renewing_published
     state :renewing_enrolling, :after_enter => [:trigger_passive_renewals, :send_employee_invites]
     state :renewing_enrolled
@@ -940,6 +940,11 @@ private
     elsif event_name == "force_publish"
       self.employer_profile.trigger_notices("planyear_renewal_3b")
     end
+  end
+
+  def employer_renewal_notice
+    return true if (benefit_groups.any?{|bg| bg.is_congress?} || self.employer_profile.is_conversion?)
+    self.employer_profile.trigger_notices("group_renewal_5")
   end
 
   def record_transition
