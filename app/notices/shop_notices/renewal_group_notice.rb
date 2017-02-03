f156f71e9e475163d52ab097f9cc6d9ab545dd64
Battula <vrbattula@outlook.com>
2017-02-03 16:06:14 -0500

    refs #13795 pick renewing_draft plan year

--- app/notices/shop_notices/renewal_group_notice.rb
+++ app/notices/shop_notices/renewal_group_notice.rb
@@ -7,7 +7,7 @@ class ShopNotices::RenewalGroupNotice < ShopNotices::EmployerRenewalNotice
   end
 
   def append_data
-    renewing_plan_year = employer_profile.plan_years.where(:aasm_state => "enrolling").first
+    renewing_plan_year = employer_profile.plan_years.where(:aasm_state => "renewing_draft").first
     notice.plan = PdfTemplates::Plan.new({
           :open_enrollment_end_on => renewing_plan_year.open_enrollment_end_on,
           :coverage_start_on => renewing_plan_year.start_on
