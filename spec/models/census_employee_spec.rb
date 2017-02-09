cc5f4bb30c7af81968409203ab2722e7d2224206
saimekala07 <saimekala07@gmail.com>
2017-02-09 10:03:46 -0500

    Refs #13429 updating specs

--- spec/models/census_employee_spec.rb
+++ spec/models/census_employee_spec.rb
@@ -1454,16 +1454,16 @@ RSpec.describe CensusEmployee, type: :model, dbclean: :after_each do
   describe "search_hash" do
     context 'census search query' do
 
-      it "should return query string for census employee name" do
+      it "query string for census employee firstname or last name" do
         employee_search = "test1"
-        expected_result = {"$or"=>[{"first_name"=>/test1/i}, {"last_name"=>/test1/i}, {"encrypted_ssn"=>"+MZq0qWj9VdyUd9MifJWpQ=="}]}
+        expected_result = {"$or" => [{"$or"=>[{"first_name"=>/test1/i}, {"last_name"=>/test1/i}]}, {"encrypted_ssn"=>"+MZq0qWj9VdyUd9MifJWpQ=="}]}
         result=CensusEmployee.search_hash(employee_search)
         expect(result).to eq expected_result
       end
 
       it "census employee query string for full name" do
         employee_search = "test1 test2"
-        expected_result = {"$or"=>[{"first_name"=>/test1\ test2/i}, {"last_name"=>/test1\ test2/i}, {"encrypted_ssn"=>"0m50gjJW7mR4HLnepJyFmg=="}, {:first_name=>/test1/i, :last_name=>/test2/i}]}
+        expected_result = {"$or"=>[{"$and"=>[{"first_name"=>/test1|test2/i}, {"last_name"=>/test1|test2/i}]}, {"encrypted_ssn"=>"0m50gjJW7mR4HLnepJyFmg=="}]}
         result=CensusEmployee.search_hash(employee_search)
         expect(result).to eq expected_result
       end
