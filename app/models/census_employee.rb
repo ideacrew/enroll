8b001486dc84c17979470ae7046bd79cd12123d6
Tony Schaffert <antonio.schaffert@gmail.com>
2017-02-10 14:04:36 -0500

    Fix another typo

--- app/models/census_employee.rb
+++ app/models/census_employee.rb
@@ -537,7 +537,6 @@ class CensusEmployee < CensusMember
   def benefit_group_assignments_for_cobra
     # 6 months buffer between create date and open enrollment start date indicates this is most likely a conversion and should not be picked up.
     benefit_group_assignments.select { |bga| (bga == renewal_benefit_group_assignment) || (bga.plan_year == employer_profile.published_plan_year && employer_profile.published_plan_year.created_at < (employer_profile.published_plan_year.open_enrollment_start_on + 6.months)) }
-    ce.benefit_group_assignments.select { |bga| (bga == ce.renewal_benefit_group_assignment) || (bga.plan_year == employer_profile.published_plan_year && employer_profile.published_plan_year.created_at < (employer_profile.published_plan_year.open_enrollment_start_on + 6.months)) }
   end
 
   def build_hbx_enrollment_for_cobra
