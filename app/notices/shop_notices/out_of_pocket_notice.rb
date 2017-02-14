582e1fe5787be2c25ef75f4e2990f2b6f600f971
sarathpyerra <sarathpyerra@gmail.com>
2017-02-14 11:50:42 -0500

    Refs #9698 commenting unused code

--- app/notices/shop_notices/out_of_pocket_notice.rb
+++ app/notices/shop_notices/out_of_pocket_notice.rb
@@ -30,7 +30,7 @@ class ShopNotices::OutOfPocketNotice < ShopNotice
       # @notice.metal_leval= @recipient.plan_years.order_by(:end_on => "desc").first.try(:benefit_groups).try(:first).try(:reference_plan).try(:metal_level)
       # @notice.benefit_group_package_name= @recipient.active_and_published_plan_year.benefit_groups.first.title
       # @notice.benefit_group_package_name = @notice.active_and_published_plan_year.benefit_groups.map(&:title).select {|r| puts r}
-      @notice.plan_year = @recipient.try(:active_and_published_plan_year).try(:benefit_groups).first.try(:reference_plan).name
+      # @notice.plan_year = @recipient.try(:active_and_published_plan_year).try(:benefit_groups).first.try(:reference_plan).name
       # @notice.family_contribution= @recipient.plan_years.first.benefit_groups.first.relationship_benefits.select{|r| r.relationship != "child_26_and_over" }
       @notice.family_contribution= @recipient.active_and_published_plan_year.benefit_groups.first.relationship_benefits.select{|r| r.relationship != "child_26_and_over" }
       @notice.carrier = @recipient.active_and_published_plan_year.benefit_groups.first.reference_plan.carrier_profile.legal_name
