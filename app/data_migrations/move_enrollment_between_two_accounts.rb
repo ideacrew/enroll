c79cbc8a3849fe9aaf23516ed7f7b7d0f0d971e3
Ji Wang <traceyw@vt.edu>
2017-02-15 16:00:45 -0500

    Ref#13363 Modified Rake task

--- app/data_migrations/move_enrollment_between_two_accounts.rb
+++ app/data_migrations/move_enrollment_between_two_accounts.rb
@@ -2,39 +2,24 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
   def migrate
-    #find the old and new accounts
     gp = Person.where(hbx_id:ENV['new_account_hbx_id']).first
     bp = Person.where(hbx_id:ENV['old_account_hbx_id']).first
-    #find the hbx enrollment to move
-    index=-1
-    bp.primary_family.active_household.hbx_enrollments.each do |enrollment|
-      index = index+1
-      if enrollment.hbx_id==ENV['enrollment_hbx_id']
-         break
-      end
-    end
-    if index == -1
-      raise "No enrollment found"
+    enroll_hbx_id=ENV['enrollment_hbx_id']
+    if  bp.primary_family.active_household.hbx_enrollments.where(hbx_id:enroll_hbx_id).first
+      hbx1 = bp.primary_family.active_household.hbx_enrollments.where(hbx_id:enroll_hbx_id).first
     else
-      hbx1 = bp.primary_family.active_household.hbx_enrollments[index]
+      raise "No enrollment found"
     end
-    #decide whether the hbx_enrollment can be moved
     move_index=moveable(gp.primary_family,hbx1)
-    # add the enrollment to the good person
     unless move_index
-      #update family member
       gp.primary_family.latest_household.update_attributes!(:hbx_enrollments => gp.primary_family.latest_household.hbx_enrollments.append(hbx1))
       fm= gp.primary_family.latest_household.hbx_enrollments[0].hbx_enrollment_members.first.family_member
       hbx_total=gp.primary_family.latest_household.hbx_enrollments.size
-      gp.primary_family.latest_household.hbx_enrollments[hbx_total-1].hbx_enrollment_members.first.update_attributes!(:family_member => fm)
-
-      #update the consumer role id of the family number
+      gp.primary_family.latest_household.hbx_enrollments[hbx_total-1].hbx_enrollment_members.each do |m|
+        m.update_attributes!(:family_member => fm)
+      end
       gp.primary_family.latest_household.hbx_enrollments[hbx_total-1].update_attributes!(:consumer_role_id => gp.consumer_role.id)
-
-      #delete the hbx_enrollment from the old account
       bp.primary_family.latest_household.hbx_enrollments.delete_at(index)
-
-      #update the old account's hbx_enrollments
       hbx = bp.primary_family.latest_household.hbx_enrollments
       hh = bp.primary_family.latest_household
       hh.hbx_enrollments = hbx
