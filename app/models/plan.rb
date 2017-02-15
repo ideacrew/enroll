a5e4e7985610e48ee8d1ad2dbac8e5f5aefef9da
Raghuram <raghuramg83@gmail.com>
2017-02-15 16:45:12 -0500

    Refs #13310 refactored methods

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -358,16 +358,15 @@ class Plan
   end
 
   def dental?
-    self.coverage_kind.downcase == "dental"
+    coverage_kind && coverage_kind.downcase == "dental"
   end
 
   def health?
-    self.coverage_kind.downcase == "health"
+    coverage_kind && coverage_kind.downcase == "health"
   end
 
   def is_dental_only?
-    return false if self.coverage_kind.blank?
-    self.coverage_kind.downcase == "dental"
+    dental?
   end
 
   def can_use_aptc?
