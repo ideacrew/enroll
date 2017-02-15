65f1c03e0f8ad23fe46dd171d96408a1eff6b78b
Battula <vrbattula@outlook.com>
2017-02-15 15:33:18 -0500

    refs #13923 populate py start_on & carrier name

--- app/notices/shop_notices/renewal_group_notice.rb
+++ app/notices/shop_notices/renewal_group_notice.rb
@@ -10,7 +10,8 @@ class ShopNotices::RenewalGroupNotice < ShopNotices::EmployerRenewalNotice
     renewing_plan_year = employer_profile.plan_years.where(:aasm_state => "renewing_draft").first
     notice.plan = PdfTemplates::Plan.new({
           :open_enrollment_end_on => renewing_plan_year.open_enrollment_end_on,
-          :coverage_start_on => renewing_plan_year.start_on
+          :coverage_start_on => renewing_plan_year.start_on,
+          :plan_carrier => renewing_plan_year.benefit_groups.first.reference_plan.carrier_profile.legal_name
         })
   end
 
