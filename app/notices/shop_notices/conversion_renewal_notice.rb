13473a2145bcc308dd126c9b1bf25bd1bd5a4b23
Battula <vrbattula@outlook.com>
2017-02-21 16:08:00 -0500

    refs #13923 #13795 refactor shop notices code

--- app/notices/shop_notices/conversion_renewal_notice.rb
+++ app/notices/shop_notices/conversion_renewal_notice.rb
@@ -9,6 +9,14 @@ class ShopNotices::ConversionRenewalNotice < ShopNotice
     send_generic_notice_alert
   end
 
+  def append_data
+    renewing_plan_year = employer_profile.plan_years.where(:aasm_state => "renewing_draft").first
+    notice.plan_year = PdfTemplates::PlanYear.new({
+          :start_on => renewing_plan_year.start_on,
+          :carrier_name => renewing_plan_year.benefit_groups.first.reference_plan.carrier_profile.legal_name
+        })
+  end
+
   def conversion_attachment
     join_pdfs [notice_path, Rails.root.join('lib/pdf_templates', 'conversion_employer_attachment.pdf')]
   end
--- app/notices/shop_notices/renewal_group_notice.rb
+++ app/notices/shop_notices/renewal_group_notice.rb
@@ -1,24 +1,19 @@
 class ShopNotices::RenewalGroupNotice < ShopNotice
 
   def deliver
+    build
+    generate_pdf_notice
     append_data
-    super
+    upload_and_send_secure_message
+    send_generic_notice_alert
   end
 
   def append_data
     renewing_plan_year = employer_profile.plan_years.where(:aasm_state => "renewing_draft").first
-    notice.plan = PdfTemplates::Plan.new({
+    notice.plan_year = PdfTemplates::PlanYear.new({
           :open_enrollment_end_on => renewing_plan_year.open_enrollment_end_on,
-          :coverage_start_on => renewing_plan_year.start_on,
-          :plan_carrier => renewing_plan_year.benefit_groups.first.reference_plan.carrier_profile.legal_name
+          :start_on => renewing_plan_year.start_on,
         })
   end
 
-  def create_secure_inbox_message(notice)
-    body = "<br>You can download the notice by clicking this link " +
-            "<a href=" + "#{Rails.application.routes.url_helpers.authorized_document_download_path(recipient.class.to_s, 
-              recipient.id, 'documents', notice.id )}?content_type=#{notice.format}&filename=#{notice.title.gsub(/[^0-9a-z]/i,'')}.pdf&disposition=inline" + " target='_blank'>" + notice.title + "</a>"
-    message = recipient.inbox.messages.build({ subject: subject, body: body, from: 'DC Health Link' })
-    message.save!
-  end
 end
