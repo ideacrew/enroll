3b775ef5685d4cb4b9da3fac152591e368fcaa99
Raghuram <raghuramg83@gmail.com>
2017-02-28 10:13:39 -0500

    Refs #12534 updated migration to cancel enrollments

--- app/data_migrations/expire_conversion_employers.rb
+++ app/data_migrations/expire_conversion_employers.rb
@@ -2,15 +2,10 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class ExpireConversionEmployers < MongoidMigrationTask
 
-  def prepend_zeros(number, n)
-    (n - number.to_s.size).times { number.prepend('0') }
-    number
-  end
-
   def migrate
     CSV.open("#{Rails.root}/ConversionERExpiration_Results.csv", "w", force_quotes: true) do |csv|
 
-      csv << ['FEIN', 'Legal Name', 'Plan Year Status(Before)', 'Plan Year Status(After)']
+      csv << ['FEIN', 'Legal Name', 'Conversion Employer', 'Plan Year Status(Before)', 'Plan Year Status(After)', "External Plan Year Status(Before)", "External Plan Year Status(After)"]
 
       CSV.foreach("#{Rails.root.to_s}/ConversionERExpiration.csv") do |row|
 
@@ -25,10 +20,15 @@ class ExpireConversionEmployers < MongoidMigrationTask
           puts "Processing (#{fein}) - #{employer.legal_name}"
         end
 
-        data = [employer.fein, employer.legal_name]
+        data = [employer.fein, employer.legal_name, employer.is_conversion?]
 
         if plan_year = employer.plan_years.where(:start_on => start_on).first
           data += [plan_year.aasm_state.camelcase]
+
+          plan_year_enrollments(plan_year).each do |enrollment|
+            enrollment.cancel_coverage! if enrollment.may_cancel_coverage?
+          end
+
           plan_year.cancel_renewal! if plan_year.may_cancel_renewal?
           plan_year.cancel! if plan_year.may_cancel?
           data += [plan_year.aasm_state.camelcase]
@@ -44,6 +44,20 @@ class ExpireConversionEmployers < MongoidMigrationTask
 
         csv << data 
       end
+    end
+  end
+
+  def prepend_zeros(number, n)
+    (n - number.to_s.size).times { number.prepend('0') }
+    number
+  end
+
+  def plan_year_enrollments(plan_year)
+    id_list = plan_year.benefit_groups.collect(&:_id).uniq
+
+    families = Family.where(:"households.hbx_enrollments.benefit_group_id".in => id_list)
+    families.inject([]) do |enrollments, family|
+      enrollments += family.active_household.hbx_enrollments.where(:benefit_group_id.in => id_list).enrolled_and_renewal.to_a
     end
   end
 end
