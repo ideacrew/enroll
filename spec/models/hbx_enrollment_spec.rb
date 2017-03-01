7006313b82191bd4c18d490f035f9be591e580b6
PRANAB <pranab.khanal@gmail.com>
2017-03-01 15:24:11 -0500

    Refs#13827 IVL/Shop Cancel instead of termination if effective date is same

--- spec/models/hbx_enrollment_spec.rb
+++ spec/models/hbx_enrollment_spec.rb
@@ -2236,3 +2236,81 @@ describe HbxEnrollment, 'state machine' do
     it_behaves_like "state machine transitions", :coverage_expired, :coverage_expired, :move_to_pending!
   end
 end
+
+describe HbxEnrollment, dbclean: :after_all do
+  include_context "BradyWorkAfterAll"
+
+  before :all do
+    create_brady_census_families
+  end
+
+  context "Cancel / Terminate Previous Enrollments for Shop" do
+    let(:household) {mikes_family.households.first}
+    let(:coverage_household) {household.coverage_households.first}
+    let(:hbx) {HbxEnrollment.new(consumer_role_id: consumer_role.id)}
+    let(:family) {FactoryGirl.build(:family)}
+
+    before :each do
+      allow(coverage_household).to receive(:household).and_return household
+      allow(household).to receive(:family).and_return family
+      @enrollment1 = household.create_hbx_enrollment_from(
+        employee_role: mikes_employee_role,
+        coverage_household: coverage_household,
+        benefit_group: mikes_benefit_group,
+        benefit_group_assignment: @mikes_benefit_group_assignments
+      )
+      @enrollment1.aasm_state = "coverage_selected"
+      @enrollment1.save
+      @enrollment2 = household.create_hbx_enrollment_from(
+        employee_role: mikes_employee_role,
+        coverage_household: coverage_household,
+        benefit_group: mikes_benefit_group,
+        benefit_group_assignment: @mikes_benefit_group_assignments
+      )
+      @enrollment2.save
+    end
+
+    it "should cancel the previous enrollment if the effective_on date of the previous and the current are the same." do
+      @enrollment2.update_existing_shop_coverage
+      expect(@enrollment1.aasm_state).to eq "coverage_canceled"
+    end
+  end
+
+  context "Cancel / Terminate Previous Enrollments for IVL" do
+    attr_reader :enrollment, :household, :coverage_household
+    before :all do
+      @household = mikes_family.households.first
+      @coverage_household = household.coverage_households.first
+      @enrollment = household.create_hbx_enrollment_from(
+        employee_role: mikes_employee_role,
+        coverage_household: coverage_household,
+        benefit_group: mikes_benefit_group,
+        benefit_group_assignment: @mikes_benefit_group_assignments
+      )
+    end
+    let(:consumer_role) {FactoryGirl.create(:consumer_role)}
+    let(:hbx_profile) { FactoryGirl.create(:hbx_profile) }
+    let(:benefit_package) { hbx_profile.benefit_sponsorship.benefit_coverage_periods.first.benefit_packages.first }
+    let(:benefit_coverage_period) { hbx_profile.benefit_sponsorship.benefit_coverage_periods.first }
+    let(:hbx) {HbxEnrollment.new(consumer_role_id: consumer_role.id)}
+    let(:family) {FactoryGirl.build(:family)}
+    before :each do
+      allow(benefit_coverage_period).to receive(:earliest_effective_date).and_return TimeKeeper.date_of_record
+      allow(coverage_household).to receive(:household).and_return household
+      allow(household).to receive(:family).and_return family
+      allow(family).to receive(:is_under_ivl_open_enrollment?).and_return true
+      @enrollment1 = household.create_hbx_enrollment_from(consumer_role: consumer_role, coverage_household: coverage_household, benefit_package: benefit_package)
+      @enrollment1.update_current( aasm_state: "coverage_selected",
+                                  enrollment_signature: "somerandomthing!",
+                                  effective_on: TimeKeeper.date_of_record.beginning_of_month
+                                )
+      @enrollment2 = household.create_hbx_enrollment_from(consumer_role: consumer_role, coverage_household: coverage_household, benefit_package: benefit_package)
+      @enrollment2.update_current(enrollment_signature: "somerandomthing!", effective_on: TimeKeeper.date_of_record.beginning_of_month)
+    end
+
+    it "should cancel the previous enrollment if the effective_on date of the previous and the current are the same." do
+      @enrollment2.cancel_previous(TimeKeeper.date_of_record.year)
+      expect(@enrollment1.aasm_state).to eq "coverage_canceled"
+    end
+  end
+end
