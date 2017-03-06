212809c628f626fe8c67426ec1ad7b842d711e7c
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-03-06 11:53:49 -0500

    Refs #14305 add 500 response to individual update controllers

--- app/controllers/events/individuals_controller.rb
+++ app/controllers/events/individuals_controller.rb
@@ -7,33 +7,45 @@ module Events
       headers = properties.headers || {}
       individual_id = headers.stringify_keys["individual_id"]
       individual = Person.by_hbx_id(individual_id).first
-      if !individual.nil?
-        response_payload = render_to_string "created", :formats => ["xml"], :locals => { :individual => individual }
-        with_response_exchange(connection) do |ex|
-          ex.publish(
-            response_payload,
-            {
-              :routing_key => reply_to,
-              :headers => {
-                :return_status => "200",
-                :individual_id => individual_id
+      begin
+        if !individual.nil?
+          response_payload = render_to_string "created", :formats => ["xml"], :locals => { :individual => individual }
+          with_response_exchange(connection) do |ex|
+            ex.publish(
+              response_payload,
+              {
+                :routing_key => reply_to,
+                :headers => {
+                  :return_status => "200",
+                  :individual_id => individual_id
+                }
               }
-            }
-          )
-        end
-      else
-        with_response_exchange(connection) do |ex|
-          ex.publish(
-            "",
-            {
-              :routing_key => reply_to,
-              :headers => {
-                :return_status => "404",
-                :individual_id => individual_id
+            )
+          end
+        else
+          with_response_exchange(connection) do |ex|
+            ex.publish(
+              "",
+              {
+                :routing_key => reply_to,
+                :headers => {
+                  :return_status => "404",
+                  :individual_id => individual_id
+                }
               }
-            }
-          )
+            )
+          end
         end
+      rescue Exception=>e
+        ex.publish(
+          "#{individual_id} update failed - #{e.inspect}",
+          {
+            :routing_key => reply_to,
+            :headers => {
+              :return_status => "500"
+              :individual_id => individual_id
+            }
+            })
       end
     end
   end
