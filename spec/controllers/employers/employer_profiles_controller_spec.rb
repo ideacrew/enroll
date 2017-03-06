8a2f5832e5b5f62084872610d611ac6f89e37576
saimekala07 <saimekala07@gmail.com>
2017-03-06 16:33:47 -0500

    refs #12714 updating method name and specs.

--- spec/controllers/employers/employer_profiles_controller_spec.rb
+++ spec/controllers/employers/employer_profiles_controller_spec.rb
@@ -597,7 +597,7 @@ RSpec.describe Employers::EmployerProfilesController do
       allow(person).to receive(:employer_staff_roles).and_return([EmployerStaffRole.new])
       allow(organization).to receive(:errors).and_return nil
       allow(organization).to receive(:notify_address_change).and_return(true)
-      allow(organization).to receive(:notify_legal_or_fein_change).and_return(true)
+      allow(organization).to receive(:notify_legal_name_or_fein_change).and_return(true)
       sign_in(user)
       expect(Organization).to receive(:find)
 
@@ -622,7 +622,7 @@ RSpec.describe Employers::EmployerProfilesController do
       it "should render edit template" do
         allow(user).to receive(:save).and_return(false)
         allow(organization).to receive(:notify_address_change).and_return(true)
-        allow(organization).to receive(:notify_legal_or_fein_change).and_return(true)
+        allow(organization).to receive(:notify_legal_name_or_fein_change).and_return(true)
         sign_in(user)
         put :update, id: organization.id
         expect(response).to be_redirect
@@ -633,7 +633,7 @@ RSpec.describe Employers::EmployerProfilesController do
       it "should render edit template" do
         allow(user).to receive(:save).and_return(true)
         allow(organization).to receive(:notify_address_change).and_return(true)
-        allow(organization).to receive(:notify_legal_or_fein_change).and_return(true)
+        allow(organization).to receive(:notify_legal_name_or_fein_change).and_return(true)
         sign_in(user)
         put :update, id: organization.id
         expect(response).to be_redirect
