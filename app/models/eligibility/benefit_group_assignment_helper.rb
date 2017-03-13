fbab0cfaed46c8b2955564a97feef814f8a1da91
Raghuram <raghuramg83@gmail.com>
2017-03-12 21:00:21 -0400

    Refs #7282 Refactored eligiblity rules and benefit group assignments

---
+++ app/models/eligibility/benefit_group_assignment_helper.rb
@@ -0,0 +1,99 @@
+module Eligibility
+  module BenefitGroupAssignmentHelper
+
+    def assign_default_benefit_package
+      py = employer_profile.plan_years.published.first || employer_profile.plan_years.where(aasm_state: 'draft').first
+      if py.present?
+        if active_benefit_group_assignment.blank? || active_benefit_group_assignment.benefit_group.plan_year != py
+          find_or_create_benefit_group_assignment(py.benefit_groups.first)
+        end
+      end
+
+      if py = employer_profile.plan_years.renewing.first
+        if benefit_group_assignments.where(:benefit_group_id.in => py.benefit_groups.map(&:id)).blank?
+          add_renew_benefit_group_assignment(py.benefit_groups.first)
+        end
+      end
+    end
+
+    def find_or_create_benefit_group_assignment(benefit_group)
+      bg_assignments = benefit_group_assignments.where(:benefit_group_id => benefit_group.id).order_by(:'created_at'.desc)
+
+      valid_bg_assignment = bg_assignments.detect{|bg_assign| bg_assign.aasm_state != 'initialized'}
+      valid_bg_assignment = bg_assignments.first if valid_bg_assignment.blank?
+
+      if valid_bg_assignment.present?
+        valid_bg_assignment.make_active
+      else
+        add_benefit_group_assignment(benefit_group, benefit_group.plan_year.start_on)
+      end
+    end
+
+    def add_default_benefit_group_assignment
+      if plan_year = (self.employer_profile.plan_years.published_plan_years_by_date(hired_on).first || self.employer_profile.published_plan_year)
+        add_benefit_group_assignment(plan_year.benefit_groups.first)
+        if self.employer_profile.renewing_plan_year.present?
+          add_renew_benefit_group_assignment(self.employer_profile.renewing_plan_year.benefit_groups.first)
+        end
+      end
+    end
+
+    def add_renew_benefit_group_assignment(new_benefit_group)
+      raise ArgumentError, "expected BenefitGroup" unless new_benefit_group.is_a?(BenefitGroup)
+
+      benefit_group_assignments.renewing.each do |benefit_group_assignment|
+        benefit_group_assignment.destroy
+      end
+
+      bga = BenefitGroupAssignment.new(benefit_group: new_benefit_group, start_on: new_benefit_group.start_on, is_active: false)
+      bga.renew_coverage
+      benefit_group_assignments << bga
+    end
+
+    def add_benefit_group_assignment(new_benefit_group, start_on = TimeKeeper.date_of_record)
+      raise ArgumentError, "expected BenefitGroup" unless new_benefit_group.is_a?(BenefitGroup)
+      reset_active_benefit_group_assignments(new_benefit_group)
+      benefit_group_assignments << BenefitGroupAssignment.new(benefit_group: new_benefit_group, start_on: start_on)
+    end
+
+    def active_benefit_group_assignment
+      benefit_group_assignments.detect { |assignment| assignment.is_active? }
+    end
+
+    def renewal_benefit_group_assignment
+      benefit_group_assignments.order_by(:'updated_at'.desc).detect{ |assignment| assignment.plan_year && assignment.plan_year.is_renewing? }
+    end
+
+    def published_benefit_group_assignment
+      benefit_group_assignments.detect do |benefit_group_assignment|
+        benefit_group_assignment.benefit_group.plan_year.employees_are_matchable?
+      end
+    end
+
+    def active_benefit_group
+      active_benefit_group_assignment.benefit_group if active_benefit_group_assignment.present?
+    end
+
+    def published_benefit_group
+      published_benefit_group_assignment.benefit_group if published_benefit_group_assignment
+    end
+
+    def renewal_published_benefit_group
+      if renewal_benefit_group_assignment && renewal_benefit_group_assignment.benefit_group.plan_year.employees_are_matchable?
+        renewal_benefit_group_assignment.benefit_group
+      end
+    end
+
+    def reset_active_benefit_group_assignments(new_benefit_group)
+      benefit_group_assignments.select { |assignment| assignment.is_active? }.each do |benefit_group_assignment|
+        benefit_group_assignment.end_on = [new_benefit_group.start_on - 1.day, benefit_group_assignment.start_on].max
+        benefit_group_assignment.update_attributes(is_active: false)
+      end
+    end
+
+    def has_benefit_group_assignment?
+      (active_benefit_group_assignment.present? && (PlanYear::PUBLISHED).include?(active_benefit_group_assignment.benefit_group.plan_year.aasm_state)) ||
+      (renewal_benefit_group_assignment.present? && (PlanYear::RENEWING_PUBLISHED_STATE).include?(renewal_benefit_group_assignment.benefit_group.plan_year.aasm_state))
+    end
+  end
+end
