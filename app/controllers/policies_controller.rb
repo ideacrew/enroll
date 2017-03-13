d1d05b53d87f3b586c97184f1b2188bff5195267
sarathpyerra <sarathpyerra@gmail.com>
2017-03-13 16:58:12 -0400

    Refs #14443 adding ivl-policies view/controller

---
+++ app/controllers/policies_controller.rb
@@ -0,0 +1,20 @@
+class PoliciesController < ApplicationController
+
+  def home
+    @policy_enrollments=IvlPolicie::PoliciesEnrollment.all
+    @policy_seps=IvlPolicie::PoliciesSeps.all
+    @policy_covered_lives=IvlPolicie::PoliciesDiscontinuedReinstatedCoveredLives.all
+    @policy_totalaccounts=IvlPolicie::PolicyTotalAccounts.all
+    @policy_annual_enrollments=IvlPolicie::PolicyAnnualEnrollmentType.all
+    @policy_annual_carriers=IvlPolicie::PolicyAnnualCarrierType.all
+    @metal_types=IvlPolicie::PolicyTotalMetalTypes.all
+    @policy_annual_aptc=IvlPolicie::PolicyAnnualAptc.all
+    @policy_family=IvlPolicie::PolicyFamily.all
+    @policy_csr=IvlPolicie::PolicyCsr.all
+
+
+    @policy_overall=IvlPolicie::PolicyOverall.all
+
+  end
+
+end
