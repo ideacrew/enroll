b5f750ae7b3801556160ddeb0acd971cfd617a86
sarathpyerra <sarathpyerra@gmail.com>
2017-03-14 10:47:55 -0400

    Refs #14335 bug fixes on ivl covered/policies -Analytics

--- app/models/ivl_policie/policy_annual_carrier_type.rb
+++ app/models/ivl_policie/policy_annual_carrier_type.rb
@@ -8,7 +8,7 @@ module IvlPolicie
     field :carrier_first_count, type: Integer
     field :carrier_first_share, type: String
     field :carrier_first_yoy, type: String
-    field :carrier_secondName, type: String
+    field :carrier_second_name, type: String
     field :carrier_second_count, type: Integer
     field :carrier_second_share, type: String
     field :carrier_second_yoy, type: String
