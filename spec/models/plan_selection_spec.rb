c64d974c767c44fbc32c27972e1137a1b82b69c8
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-03-17 12:12:59 -0400

    Refs #6715 spec for setting eligibility dates

--- spec/models/plan_selection_spec.rb
+++ spec/models/plan_selection_spec.rb
@@ -90,4 +90,20 @@ describe PlanSelection do
       end
     end
   end
+
+  describe '.set_enrollment_member_eligibility_dates' do 
+    context 'when a new enrollment has a previous enrollment' do 
+      def hash_key_creator(hbx_enrollment_member)
+        hbx_enrollment_member.person.hbx_id
+      end
+      it 'should set eligibility dates to that of the previous enrollment' do
+        subject.set_enrollment_member_eligibility_dates(hbx_enrollment)
+        previous_eligibility_dates = Hash[previous_coverage.hbx_enrollment_members.collect {|hbx_em| [hash_key_creator(hbx_em), hbx_em.eligibility_date]} ]
+        new_eligibility_dates = Hash[hbx_enrollment.hbx_enrollment_members.collect {|hbx_em| [hash_key_creator(hbx_em), hbx_em.eligibility_date]} ]
+        new_eligibility_dates.each do |hbx_id,date|
+          expect(previous_eligibility_dates[hbx_id]).to eq(date)
+        end
+      end
+    end
+  end
 end
