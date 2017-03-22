ffc25abcde0aa3218e37d26fb96567bfc363daa9
nisanth <nisanth.yaganti@gmail.com>
2017-03-22 17:16:15 -0400

    refs #14620 added specs

--- app/data_migrations/update_user_name_and_email_on_user.rb
+++ app/data_migrations/update_user_name_and_email_on_user.rb
@@ -8,22 +8,26 @@ class UpdateUserNameAndEmailOnUser < MongoidMigrationTask
       user_name = ENV['user_name'].to_s
       headless_user = ENV['headless_user'].to_s
       find_user_by = ENV['find_user_by']
-      record = find_user_by == /email/i ? find_user_from_email(email) : find_user_by_user_name(user_name)
-
+      record = (find_user_by.casecmp("email") == 0) ? find_user_from_email(email) : find_user_by_user_name(user_name) 
       if record.size != 1
-        puts "No user record (or) found more than 1 user record with e-mail/username you entered"
+        puts "No user record (or) found more than 1 user record with e-mail/username you entered" unless Rails.env.test?
         return
       end
 
-      if action == /update_username/i
+      if action.blank? && headless_user.casecmp("yes") != 0
+        puts "Please give input. Check your query" unless Rails.env.test?
+        return
+      end
+
+      if action.casecmp("update_username") == 0
         update_oim_id(record.first, user_name)
       end
 
-      if action == /update_email/i
+      if action.casecmp("update_email") == 0
         update_email(record.first, email)
       end
 
-      if headless_user == /yes/i
+      if headless_user.casecmp("yes") == 0
         handle_headless_user(record.first)
       end
 
@@ -42,20 +46,20 @@ class UpdateUserNameAndEmailOnUser < MongoidMigrationTask
 
   def update_oim_id(user, user_name)
     user.update_attributes!(oim_id: user_name)
-    puts "Succesfully updated username"
+    puts "Succesfully updated username" unless Rails.env.test?
   end
 
   def update_email(user, email)
     user.update_attributes!(email: email)
-    puts "Succesfully updated email"
+    puts "Succesfully updated email" unless Rails.env.test?
   end
 
   def handle_headless_user(user)
     if user.person.blank?
       user.destroy!
-      puts "Succesfully destroyed headless user record"
+      puts "Succesfully destroyed headless user record" unless Rails.env.test?
     else
-      puts "This is not a headless user"
+      puts "This is not a headless user" unless Rails.env.test?
     end
   end
 end
