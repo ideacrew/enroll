20dcf46b2f8b6c8acc34fdba0a382b5de79ec94e
sarathpyerra <sarathpyerra@gmail.com>
2017-03-29 12:15:01 -0400

    Refs #14443  refactoring ivl-policies and adding styles-Analytics

--- app/models/ivl_covered/annual_status_type.rb
+++ app/models/ivl_covered/annual_status_type.rb
@@ -5,12 +5,12 @@ module IvlCovered
 
     field :tile , type: String
     field :primary_select, type: Integer
-    field :primary_effectuate, type: String
-    field :primary_paying, type: String
+    field :primary_effectuate, type: Integer
+    field :primary_paying, type: Integer
     field :primary_pay_share, type: String
-    field :dependent_select, type: String
-    field :dependent_effectuate, type: String
-    field :dependent_paying, type: String
+    field :dependent_select, type: Integer
+    field :dependent_effectuate, type: Integer
+    field :dependent_paying, type: Integer
     field :dependent_pay_share, type: String
 
     default_scope ->{where(tile: "left_status" )}
