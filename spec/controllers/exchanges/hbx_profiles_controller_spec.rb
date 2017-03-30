156e4a86675e65c94306cb9ee7155298dc40d8c8
sagarlekar <sagar.arlekar@gmail.com>
2017-03-30 16:12:26 -0400

    Refs #13896 fixed specs

--- spec/controllers/exchanges/hbx_profiles_controller_spec.rb
+++ spec/controllers/exchanges/hbx_profiles_controller_spec.rb
@@ -524,26 +524,34 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :after_each do
     let(:user) { FactoryGirl.create(:user, roles: ["hbx_staff"]) }
     let(:family) { FactoryGirl.create(:family, :with_primary_family_member)}
     let(:hbx_enrollment) { FactoryGirl.build(:hbx_enrollment, :with_enrollment_members, { household: family.households.first })}
-    let(:employer_profile) { FactoryGirl.create(:employer_with_planyear) }
-    let(:plan_year) { FactoryGirl.create(:plan_year) }
+    let(:plan_year) { FactoryGirl.create(:plan_year, {aasm_state: 'enrolling'}) }
+    let(:employer_profile) { plan_year.employer_profile }
 
     before :each do
-      plan_year.update_attributes({aasm_state: 'enrolling'})
-      allow_any_instance_of(EmployerProfile).to receive(:active_plan_year).and_return(plan_year)
       allow(user).to receive(:has_hbx_staff_role?).and_return(true)
-      allow(employer_profile).to receive(:active_plan_year).and_return(plan_year)
-      allow(plan_year).to receive(:hbx_enrollments).and_return([hbx_enrollment])
+      allow(employer_profile).to receive(:published_plan_year).and_return(plan_year)
+      allow_any_instance_of(PlanYear).to receive(:hbx_enrollments).and_return([hbx_enrollment])
       sign_in user
     end
 
-    context "cancel case" do
+    context "success" do
       it "should returns http success" do
         allow_any_instance_of(EventsHelper).to receive(:is_initial_or_conversion_employer?).with(employer_profile).and_return(true)
         allow(controller).to receive(:process_cancel_initial_plan_year).with([hbx_enrollment], employer_profile)
+
         xhr :get, :cancel_initial_plan_year, format: :js, id:employer_profile.id
         expect(response).to have_http_status(200)
         expect(flash.notice).to match(/Initial plan year cancelled for employer/)
         expect(employer_profile.applicant?).to be_truthy
+      end
+    end
+
+    context "failure" do
+      it "should returns http status 500 with error message" do
+        allow_any_instance_of(EventsHelper).to receive(:is_initial_or_conversion_employer?).with(employer_profile).and_return(false)
+        xhr :get, :cancel_initial_plan_year, format: :js, id:employer_profile.id
+        expect(response).to have_http_status(500)
+        expect(flash["error"]).to match(/Could not cancel initial plan year for employer/)
       end
     end
   end
@@ -551,10 +559,12 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :after_each do
   describe "process_cancel_initial_plan_year" do
     let(:family) { FactoryGirl.create(:family, :with_primary_family_member)}
     let(:hbx_enrollments) { [FactoryGirl.build(:hbx_enrollment, :with_enrollment_members, { household: family.households.first })]}
-    let(:employer_profile) { FactoryGirl.create(:employer_with_planyear) }
+    let(:plan_year) { FactoryGirl.create(:plan_year, {aasm_state: 'enrolling'}) }
+    let(:employer_profile) { plan_year.employer_profile }
 
     before do
-      allow(employer_profile).to receive(:active_plan_year).and_return(employer_profile.plan_years.first)
+      allow(employer_profile).to receive(:published_plan_year).and_return(plan_year)
+      allow_any_instance_of(PlanYear).to receive(:hbx_enrollments).and_return(hbx_enrollments)
       controller.send(:process_cancel_initial_plan_year, hbx_enrollments, employer_profile)
     end
 
@@ -565,8 +575,7 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :after_each do
     end
 
     it "should cancel active plan year for employer" do
-      expect(employer_profile.active_plan_year.canceled?).to be_truthy
+      expect(employer_profile.published_plan_year.canceled?).to be_truthy
     end
   end
 end
-
