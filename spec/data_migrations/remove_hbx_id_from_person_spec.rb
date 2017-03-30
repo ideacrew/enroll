075fbc2a56ff50df73b6384e798374241a93f51a
nisanth <nisanth.yaganti@gmail.com>
2017-03-30 13:58:29 -0400

    refs #12908 spec update

--- spec/data_migrations/remove_hbx_id_from_person_spec.rb
+++ spec/data_migrations/remove_hbx_id_from_person_spec.rb
@@ -5,13 +5,18 @@ describe RemoveHbxIdFromPerson do
   let(:given_task_name) { "remove_hbx_id_from_person" }
   subject { RemoveHbxIdFromPerson.new(given_task_name, double(:current_scope => nil)) }
   
-  let(:person_1) { FactoryGirl.create(:person, hbx_id:"911911911") }
+  let(:person_1) { FactoryGirl.create(:person) }
   let(:person_2) { FactoryGirl.create(:person) }
+
+  before do
+    allow(ENV).to receive(:[]).with("p1_id").and_return person_1.hbx_id
+    allow(ENV).to receive(:[]).with("p2_id").and_return person_2.hbx_id
+    allow(ENV).to receive(:[]).with("hbx").and_return "911911911"
+  end
   
   it "changes person 2 hbx id to person 1 hbx id" do
-    person_1.update(hbx_id:"011011011")
-    person_2.update(hbx_id:"911911911")
-    #I know this feels dirty (Kevin)
-    expect(person_2.hbx_id).to eq "911911911"
+    subject.migrate
+    person_1.reload
+    expect(person_1.hbx_id).to eq "911911911"
   end
 end
