dd0a3442e697123fb0937cbc816370d652c06928
sarathpyerra <sarathpyerra@gmail.com>
2017-03-29 23:18:08 -0400

    Refs #14955 adding Non Congress SHOP Dashboards-Analytics

--- app/controllers/shop_widget_controller.rb
+++ app/controllers/shop_widget_controller.rb
@@ -1,16 +1,7 @@
 class ShopWidgetController < ApplicationController
 
   def home
-    @conversion_widgets=ShopWidget::ShopConversionWidget.all
-    @family_members=ShopWidget::ShopFamilyMembers.all
-    @census_members=ShopWidget::ShopCensusMembers.all
-    @employer_benefits=ShopWidget::ShopBenefits.all
-    @employer_contribution=ShopWidget::EmployerContributions.all
-    @plans=ShopWidget::Plans.all
-    @brokers=ShopWidget::Brokers.all
-    @total_policies=ShopWidget::ShopTotalPolicies.all
-    @policies_by_month=ShopWidget::ShopPoliciesMonth.all
-    @total_employers=ShopWidget::ShopOverallEmployers.all
+   
   end
 
 
