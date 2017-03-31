093cc1b2c8ea88fb55d3e3bd15a91f2faf2d7b78
Raghuram <raghuramg83@gmail.com>
2017-03-31 10:25:32 -0400

    Refs #14775 refactored queries

--- app/models/queries/named_policy_queries.rb
+++ app/models/queries/named_policy_queries.rb
@@ -12,38 +12,36 @@ module Queries
     end
 
     def self.shop_monthly_enrollments(feins, effective_on)
-      result = employer_shop_coverages(feins, effective_on)
-      result.select{|r| r['_id']["effective_on"] == effective_on}.collect{|r| r['enrollment_hbx_id']}
+      qs = ::Queries::ShopMonthlyEnrollments.new(feins, effective_on)
+      qs.query_families_with_active_enrollments
+        .unwind_enrollments
+        .query_active_enrollments
+        .sort_enrollments
+        .group_enrollments
+        .project_enrollment_ids
+
+      qs.evaluate.collect{|r| r['enrollment_hbx_id']}
     end
+
 
     def self.shop_monthly_terminations(feins, effective_on)
-      result = employer_shop_coverages(feins, effective_on)
-      result.group_by{|r| r["_id"]["subscriber"]}.inject([]) do |termed_enrollments, (subscriber, records)|
-        prev_enrollments = records.select{|r| r["_id"]["effective_on"] < effective_on}
-
-        termed_enrollments += prev_enrollments.select do |prev_e|
-          records.none? do |r|
-            prev_e["_id"]["employee_role_id"] == r["_id"]["employee_role_id"] && 
-            prev_e["_id"]["coverage_kind"] == r["_id"]["coverage_kind"] &&
-            r["_id"]["effective_on"] == effective_on 
-          end
-        end.collect{|record| record['enrollment_hbx_id']}
-      end
-    end
-
-    def self.employer_shop_coverages(feins, effective_on)
       qs = ::Queries::ShopMonthlyEnrollments.new(feins, effective_on)
       qs.query_families
         .unwind_enrollments
         .query_enrollments
-        .unwind_enrollment_members
-        .project_enrollments_with_subscriber
-        .filter_missing_subscribers
-        .sort("submitted_at")
+        .sort_enrollments
         .group_enrollments
         .project_enrollment_ids
 
-      qs.evaluate
+      qs.evaluate.group_by{|r| r["_id"]["employee_role_id"]}.inject([]) do |termed_enrollments, (subscriber, records)|
+        prev_enrollments = records.select{|r| r["_id"]["effective_on"] < effective_on}
+
+        termed_enrollments += prev_enrollments.select do |prev_e|
+          records.none? do |r|
+            prev_e["_id"]["coverage_kind"] == r["_id"]["coverage_kind"] && r["_id"]["effective_on"] == effective_on 
+          end
+        end.collect{|record| record['enrollment_hbx_id']}
+      end
     end
   end
 end
