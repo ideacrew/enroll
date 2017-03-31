9dbbfd08825cacf43f8ce1854cf9589fb1ef0e06
sagarlekar <sagar.arlekar@gmail.com>
2017-03-31 16:45:12 -0400

    Refs #13896 using published_plan_year

--- spec/views/exchanges/hbx_profiles/_cancel_plan_year.html.erb_spec.rb
+++ spec/views/exchanges/hbx_profiles/_cancel_plan_year.html.erb_spec.rb
@@ -4,12 +4,11 @@ describe "exchanges/hbx_profiles/_cancel_plan_year.html.erb" do
   let(:employer_profile) { FactoryGirl.create(:employer_with_planyear) }
 
   before :each do
-    allow(employer_profile).to receive(:active_plan_year).and_return(employer_profile.plan_years.first)
+    allow(employer_profile).to receive(:published_plan_year).and_return(employer_profile.plan_years.first)
     @employer_profile = employer_profile
   end
 
-  it "displays cancel fields" do
-    allow_any_instance_of(Exchanges::HbxProfilesHelper).to receive(:can_cancel_employer_plan_year?).with(employer_profile).and_return(true)
+  it "displays cancel form" do
     render template: 'exchanges/hbx_profiles/_cancel_plan_year'
     expect(rendered).to have_text(/Cancelling Plan Year for Employer/)
     expect(rendered).to have_button("Cancel Plan Year")
