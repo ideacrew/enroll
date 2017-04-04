0ead7ca0be02ebb99813555bbfab416428fa51eb
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-04-04 15:38:37 -0400

    Refs #14953 passing all cucumber tests

--- app/controllers/insured/consumer_roles_controller.rb
+++ app/controllers/insured/consumer_roles_controller.rb
@@ -84,11 +84,11 @@ class Insured::ConsumerRolesController < ApplicationController
               end
             end
           end
-          
+
           @resident_candidate = Forms::ResidentCandidate.new(@person_params)
           if @resident_candidate.valid?
             found_person = @resident_candidate.match_person
-            if found_person.resident_role.present?
+            if found_person.present? && found_person.resident_role.present?
               begin
                 @resident_role = Factories::EnrollmentFactory.construct_resident_role(params.permit!, actual_user)
                 if @resident_role.present?
