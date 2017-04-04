7b57ddef220fba3e72fc17ef7f3e0a89268a6ad8
202chaz <chaz.haskins@gmail.com>
2017-04-04 12:02:51 -0400

    Updated spec

--- spec/data_migrations/clean_up_enrollments_in_cancel_state_erroneously_spec.rb
+++ spec/data_migrations/clean_up_enrollments_in_cancel_state_erroneously_spec.rb
@@ -3,7 +3,17 @@ require File.join(Rails.root, "app", "data_migrations", "clean_up_enrollments_in
 
 describe CleanUpEnrollmentsInCancelStateErroneously do
   
+  let(:given_task_name) { "clean_up_enrollments_in_cancel_state_erroneously" }
+  subject { CleanUpEnrollmentsInCancelStateErroneously.new(given_task_name, double(:current_scope => nil)) }
+  
+  describe "given a task name" do
+      it "has the given task name" do
+        expect(subject.name).to eql given_task_name
+      end
+  end
+  
   let(:family) { FactoryGirl.create(:family, :with_primary_family_member) }
+  
   let(:e1) { FactoryGirl.create(:hbx_enrollment,
                        household: family.active_household,
                        coverage_kind: "health",
@@ -26,6 +36,9 @@ describe CleanUpEnrollmentsInCancelStateErroneously do
                        aasm_state: 'coverage_enrolled')
   }
   
+  let!(:hbx_enrollment_member_one) { FactoryGirl.create(:hbx_enrollment_member, applicant_id:family.primary_family_member.id, eligibility_date: "2017-01-01", hbx_enrollment: e1) }
+  let!(:hbx_enrollment_member_two) { FactoryGirl.create(:hbx_enrollment_member, applicant_id:family.primary_family_member.id, eligibility_date: "2017-01-01", hbx_enrollment: e2) }
+  
   it "should check enrollments that were placed into the Terminated state erroneously" do
     expect(e1.effective_on).to eq (e2.effective_on)
     expect(e1.effective_on).to be > (e2.submitted_at)
@@ -33,8 +46,9 @@ describe CleanUpEnrollmentsInCancelStateErroneously do
   end
   
   it "updates erroneous effective date of e1" do
-    e1.update(aasm_state:'coverage_canceled')
-    e1.update(terminated_on:e2.effective_on)
+    subject.migrate
+    e1.reload
+    e2.reload
     expect(e1.terminated_on).to eq (e2.effective_on)
     expect(e1.aasm_state).to eq "coverage_canceled"
   end
