0d16d15a7427d0d123f8c2f2e9f693321b9a4f31
saikumar9 <saikumarkotagiri9@gmail.com>
2017-04-11 13:54:07 -0400

    Refs#14221, 14222 Remove Relationship Types from the UI, Add Relationship Types

--- app/models/person_relationship.rb
+++ app/models/person_relationship.rb
@@ -16,6 +16,37 @@ class PersonRelationship
 
   Relationships = [
     "spouse",
+    "life_partner",
+    "child",
+    "adopted_child",
+    "annuitant", # no inverse
+    "aunt_or_uncle",
+    "brother_or_sister_in_law",
+    "collateral_dependent",
+    "court_appointed_guardian",
+    "daughter_or_son_in_law",
+    "dependent_of_a_minor_dependent",
+    "father_or_mother_in_law",
+    "foster_child",
+    "grandchild",
+    "grandparent",
+    "great_grandchild",
+    "great_grandparent",
+    "guardian",
+    "nephew_or_niece",
+    "other_relationship",
+    "parent",
+    "sibling",
+    "sponsored_dependent",
+    "stepchild",
+    "stepparent",
+    "trustee", # no inverse
+    "unrelated",
+    "ward"
+  ]
+
+  Relationships_UI = [
+    "spouse",
     "domestic_partner",
     "child",
     "parent",
@@ -32,15 +63,31 @@ class PersonRelationship
     "parent" => "child",
     "grandparent" => "grandchild",
     "grandchild" => "grandparent",
+    "great_grandparent" => "great_grandchild",
+    "great_grandchild" => "great_grandparent",
+    "stepparent" => "stepchild",
+    "stepchild" => "stepparent",
     "aunt_or_uncle" => "nephew_or_niece",
     "nephew_or_niece" => "aunt_or_uncle",
+    "father_or_mother_in_law" => "daughter_or_son_in_law",
+    "daughter_or_son_in_law" => "father_or_mother_in_law",
+    "guardian" => "ward",
+    "ward" => "guardian",
 
     # bi directional
     "self" => "self",
+    "brother_or_sister_in_law" => "brother_or_sister_in_law",
     "sibling" => "sibling",
-    "domestic_partner" => "domestic_partner",
+    "life_partner" => "life_partner",
     "spouse" => "spouse",
-    "unrelated" => "unrelated"
+    "other_relationship" => "other_relationship",
+    "cousin" => "cousin",
+    "unrelated" => "unrelated",
+
+    #one directional
+    "foster_child" => "guardian",
+    "court_appointed_guardian" => "ward",
+    "adopted_child" => "parent"
   }
 
   SymmetricalRelationships = %W[head\ of\ household spouse ex-spouse cousin ward trustee annuitant other\ relationship other\ relative self]
--- spec/models/person_relationship_spec.rb
+++ spec/models/person_relationship_spec.rb
@@ -22,20 +22,24 @@ describe PersonRelationship, dbclean: :after_each do
     end
 
     let(:consumer_relationship_kinds) { [
-      "self",
-      "spouse",
-      "domestic_partner",
-      "child",
-      "parent",
-      "sibling",
-      "unrelated",
-      "aunt_or_uncle",
-      "nephew_or_niece",
-      "grandchild",
-      "grandparent"
+        "self",
+        "spouse",
+        "domestic_partner",
+        "child",
+        "parent",
+        "sibling",
+        "ward",
+        "guardian",
+        "unrelated",
+        "other_tax_dependent",
+        "aunt_or_uncle",
+        "nephew_or_niece",
+        "grandchild",
+        "grandparent"
       ] }
 
-    let(:kinds) {  [
+    let(:relationships_UI)  { [
+      "self",
       "spouse",
       "domestic_partner",
       "child",
@@ -48,22 +52,51 @@ describe PersonRelationship, dbclean: :after_each do
       "grandparent"
     ] }
 
+    let(:kinds) {  [
+      "spouse",
+      "life_partner",
+      "child",
+      "adopted_child",
+      "annuitant",
+      "aunt_or_uncle",
+      "brother_or_sister_in_law",
+      "collateral_dependent",
+      "court_appointed_guardian",
+      "daughter_or_son_in_law",
+      "dependent_of_a_minor_dependent",
+      "father_or_mother_in_law",
+      "foster_child",
+      "grandchild",
+      "grandparent",
+      "great_grandchild",
+      "great_grandparent",
+      "guardian",
+      "nephew_or_niece",
+      "other_relationship",
+      "parent",
+      "sibling",
+      "sponsored_dependent",
+      "stepchild",
+      "stepparent",
+      "trustee",
+      "unrelated",
+      "ward"
+    ] }
+
     context "consumer relationship dropdown list(family member page)" do
       let(:params){ valid_params.deep_merge!({kind: "other_tax_dependent"}) }
-      let(:params2){ valid_params.deep_merge!({kind: "spouse"}) }
 
       it "consumer relationships should be matched" do
         expect(BenefitEligibilityElementGroup::INDIVIDUAL_MARKET_RELATIONSHIP_CATEGORY_KINDS).to eq consumer_relationship_kinds
       end
 
-      it "should not be valid if kind is not present in person_relationship" do
-        expect(PersonRelationship.new(**params).valid?).to be_falsey
+      it "consumer relationships displayed on UI should match" do
+        expect(BenefitEligibilityElementGroup::Relationships_UI).to eq relationships_UI
       end
 
-      it "should be valid if kind is present" do
-        expect(PersonRelationship.new(**params2).valid?).to be_truthy
+      it "should be valid if kind is present in person_relationship" do
+        expect(PersonRelationship.new(**params).valid?).to be_truthy
       end
-
     end
 
     it "relationships should be sorted" do
