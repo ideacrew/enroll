9c7122b6ea3ec859e20165717cbe048cbddd271d
Kevin Wei <kevin.tw.wei@gmail.com>
2017-04-12 16:09:11 -0400

    Refs #13653 mute output during specs

--- app/data_migrations/remove_dependent_from_ee_enrollment.rb
+++ app/data_migrations/remove_dependent_from_ee_enrollment.rb
@@ -6,14 +6,14 @@ class RemoveDependentFromEeEnrollment < MongoidMigrationTask
     enrollment_memeber_id = ENV["enrollment_member_id"]
     hbx_enrollment=HbxEnrollment.find(enrollment_id)
     if hbx_enrollment.nil?
-      puts "No hbx_enrollment found with the given id"
+      puts "No hbx_enrollment found with the given id" unless Rails.env.test?
     else
       member=hbx_enrollment.hbx_enrollment_members.where(id:enrollment_memeber_id).first
       if member.nil?
-        puts "No enrollment member found with the given id"
+        puts "No enrollment member found with the given id" unless Rails.env.test?
       else
         member.delete
-        puts "delete the member with id #{enrollment_memeber_id} from hbx_enrollment #{enrollment_id}"
+        puts "delete the member with id #{enrollment_memeber_id} from hbx_enrollment #{enrollment_id}" unless Rails.env.test?
       end
     end
   end
