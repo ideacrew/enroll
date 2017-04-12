ed2c0bf2bd61cb1d0b6896225c5875223ef26ba4
nisanth <nisanth.yaganti@gmail.com>
2017-04-12 11:55:37 -0400

    refs #13903 using regexp to determine invalid formats for SSN

--- app/assets/javascripts/invalid_ssn_formats.js
+++ app/assets/javascripts/invalid_ssn_formats.js
@@ -1,17 +1,14 @@
 $(document).on('ready page:load', function () {
   $('#person_ssn').blur(function(){
     var user_input = $(this).val();
-    end_val = 999;
-    invalid_array = [000, 666];
-    for(var start_val=900;start_val<=end_val;start_val++){
-      invalid_array.push(start_val)
-    }
-// Invalid array has all the invalid sequences
-    var is_invalid = $.inArray(parseInt(user_input.split('-')[0]), invalid_array) > -1 || $.inArray(parseInt(user_input.split('-')[1]), invalid_array) > -1 || $.inArray(parseInt(user_input.split('-')[2]), invalid_array) > -1
 
+    // RegEx below checks for the invalid formats.
+    var reg_ex = new RegExp("^(?!000|666|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$");
+    var is_valid = reg_ex.test(user_input);
     var array = user_input.split('').map(function(val){return parseInt(val);});
     user_input = array.filter(Boolean);
 
+    // checks if user entered sequential number as SSN
     var seq_size = 0
     var is_sequential = false;
     for(var i=0; i<9; i++) {
@@ -28,6 +25,7 @@ $(document).on('ready page:load', function () {
       })();
     }
 
+    // checks if user entered same digits in all positions in SSN
     var same_num_size = 0;
     var is_same_number = false;
     for(var i=0; i<9; i++) {
@@ -44,7 +42,7 @@ $(document).on('ready page:load', function () {
       })();
     }
 
-    if ( user_input.length != 0 && (is_invalid || is_sequential || is_same_number)) {
+    if ( user_input.length != 0 && (!is_valid || is_sequential || is_same_number)) {
       $( function() {
         $( "#dialog" ).dialog();
       });
