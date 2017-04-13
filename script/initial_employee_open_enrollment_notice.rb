d7fab054b7c75b3ae8f637b1a500f2f44febc4e0
Battula <vrbattula@outlook.com>
2017-04-13 15:01:23 -0400

    refs #14743 typo

--- script/initial_employee_open_enrollment_notice.rb
+++ script/initial_employee_open_enrollment_notice.rb
@@ -2,14 +2,14 @@ date = TimeKeeper.date_of_record.yesterday
 
 census_employees = CensusEmployee.where(:"created_at" => {"$gte" => date}).eligible
 
-begin
   census_employees.each do |ce|
-    if ce.hired_on <= date
-      Invitation.invite_initial_employee_for_open_enrollment!(ce)
-    elsif ce.hired_on > date
-      Invitation.invite_future_employee_for_open_enrollment!(ce)
+    begin
+      if ce.hired_on <= date
+        Invitation.invite_initial_employee_for_open_enrollment!(ce)
+      elsif ce.hired_on > date
+        Invitation.invite_future_employee_for_open_enrollment!(ce)
+      end
+    rescue Exception => e
+      puts "Unable to deliver to #{ce.full_name} due to --- #{e}" unless Rails.env.test?
     end
   end
-rescue Exception => e
-  puts "#{Unable to deliver to ce.full_name due to} --- #{e}" unless Rails.env.test?
-end
