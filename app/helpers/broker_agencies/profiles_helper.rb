905df2c8893b0bc47929bbccef6ca13152d38ae2
Brian Weiner <brian@320ny.com>
2017-04-13 11:43:25 -0400

    Update profiles_helper.rb

--- app/helpers/broker_agencies/profiles_helper.rb
+++ app/helpers/broker_agencies/profiles_helper.rb
@@ -3,12 +3,4 @@ module BrokerAgencies::ProfilesHelper
     (broker_agency_profile.organization.is_fake_fein? && !current_user.has_broker_agency_staff_role?)|| (broker_agency_profile.organization.is_fake_fein? && current_user.has_hbx_staff_role?) || !broker_agency_profile.organization.is_fake_fein?
   end
 
-  def selected_market_kind(object)
-	if individual_market_is_enabled?
-	object.try(:market_kind)
-	else
-	BrokerAgencyProfile::MARKET_KINDS_OPTIONS.values[0]
-	end
-  end
-
 end
