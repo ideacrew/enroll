8937f456ec47aa86ce944041d0e3f1b3c59b589e
Brian Weiner <brian.weiner@dc.gov>
2017-04-14 11:23:22 -0400

    simplified consumer roles controller

--- spec/controllers/insured/consumer_roles_controller_spec.rb
+++ spec/controllers/insured/consumer_roles_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
+RSpec.describe Insured::ConsumerRolesController, :type => :controller do
+  let(:user){ FactoryGirl.create(:user, :consumer) }
+
+  context "When individual market is disabled" do
+    before do
+      Settings.aca.market_kinds = %W[shop]
+      sign_in user
+      get :search
+    end
+
+    it "redirects to root" do
+      expect(response).to redirect_to(root_path)
+    end
+  end
+end
 
 RSpec.describe Insured::ConsumerRolesController, :type => :controller do
   let(:user){ FactoryGirl.create(:user, :consumer) }
@@ -8,31 +23,28 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
   let(:consumer_role){ FactoryGirl.build(:consumer_role) }
   let(:bookmark_url) {'localhost:3000'}
 
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:individual_market_is_enabled?).and_return(true)
+  end
+
   context "GET privacy" do
     before(:each) do
       sign_in user
       allow(user).to receive(:person).and_return(person)
     end
-
     it "should redirect" do
       allow(person).to receive(:consumer_role?).and_return(true)
       allow(person).to receive(:consumer_role).and_return(consumer_role)
       allow(consumer_role).to receive(:bookmark_url).and_return("test")
       get :privacy, {:aqhp => 'true'}
       expect(response).to have_http_status(:redirect)
-      if individual_market_is_enabled?
-        expect(response).to redirect_to(person.consumer_role.bookmark_url+"?aqhp=true")
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to redirect_to(person.consumer_role.bookmark_url+"?aqhp=true")
     end
     it "should render privacy" do
       allow(person).to receive(:consumer_role?).and_return(false)
       get :privacy
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:privacy)
-      end
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:privacy)
     end
   end
 
@@ -52,29 +64,22 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
 
     it "should render search template" do
       get :search
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:search)
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:search)
     end
 
     it "should set the session flag for aqhp the param exists" do
       get :search, aqhp: true
-      if individual_market_is_enabled?
-        expect(session[:individual_assistance_path]).to be_truthy
-      end
+      expect(session[:individual_assistance_path]).to be_truthy
     end
 
     it "should unset the session flag for aqhp if the param does not exist upon return" do
-      if individual_market_is_enabled?
-        get :search, aqhp: true
-        expect(session[:individual_assistance_path]).to be_truthy
-        get :search, uqhp: true
-        expect(session[:individual_assistance_path]).to be_falsey
-      end
+      get :search, aqhp: true
+      expect(session[:individual_assistance_path]).to be_truthy
+      get :search, uqhp: true
+      expect(session[:individual_assistance_path]).to be_falsey
     end
+
   end
 
   describe "POST match" do
@@ -103,13 +108,9 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
       it "renders the 'search' template" do
         allow(mock_consumer_candidate).to receive(:errors).and_return({})
         post :match, :person => person_parameters
-        if individual_market_is_enabled?
-          expect(response).to have_http_status(:success)
-          expect(response).to render_template("search")
-          expect(assigns[:consumer_candidate]).to eq mock_consumer_candidate
-        else
-          expect(response).to redirect_to(root_path)
-        end
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("search")
+        expect(assigns[:consumer_candidate]).to eq mock_consumer_candidate
       end
     end
 
@@ -121,18 +122,14 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
         let(:person){ double("Person") }
         let(:person_parameters){{"dob"=>"1985-10-01", "first_name"=>"martin","gender"=>"male","last_name"=>"york","middle_name"=>"","name_sfx"=>"","ssn"=>"000000111"}}
         before :each do
-          sign_in user
           post :match, :person => person_parameters
         end
 
         it "renders the 'no_match' template" do
-          if individual_market_is_enabled?
-            expect(response).to have_http_status(:success)
-            expect(response).to render_template("no_match")
-            expect(assigns[:consumer_candidate]).to eq mock_consumer_candidate
-          else
-            expect(response).to redirect_to(root_path)
-          end
+          post :match, :person => person_parameters
+          expect(response).to have_http_status(:success)
+          expect(response).to render_template("no_match")
+          expect(assigns[:consumer_candidate]).to eq mock_consumer_candidate
         end
 
         context "that find a matching employee" do
@@ -140,11 +137,9 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
 
           it "renders the 'match' template" do
             post :match, :person => person_parameters
-            if individual_market_is_enabled?
-              expect(response).to have_http_status(:success)
-              expect(response).to render_template("match")
-              expect(assigns[:consumer_candidate]).to eq mock_consumer_candidate
-            end
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template("match")
+            expect(assigns[:consumer_candidate]).to eq mock_consumer_candidate
           end
         end
       end
@@ -158,14 +153,11 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
           allow(Factories::EmploymentRelationshipFactory).to receive(:build).and_return(true)
           post :match, :person => person_parameters
         end
+
         it "render employee role match template" do
-          if individual_market_is_enabled?
-            expect(response).to have_http_status(:success)
-            expect(response).to render_template('insured/employee_roles/match')
-            expect(assigns[:employee_candidate]).to eq mock_employee_candidate
-          else
-            expect(response).to redirect_to(root_path)
-          end
+          expect(response).to have_http_status(:success)
+          expect(response).to render_template('insured/employee_roles/match')
+          expect(assigns[:employee_candidate]).to eq mock_employee_candidate
         end
       end
     end
@@ -176,14 +168,10 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
         allow(mock_consumer_candidate).to receive(:valid?).and_return(false)
         allow(mock_consumer_candidate).to receive(:errors).and_return({:ssn_taken => "test test test"})
       end
-      it "should navigate to another page which has information for user to signin/recover familount" do
+      it "should navigate to another page which has information for user to signin/recover account" do
         post :match, :person => person_parameters
-        if individual_market_is_enabled?
-          expect(response).to redirect_to(ssn_taken_insured_consumer_role_index_path)
-          expect(flash[:alert]).to eq "The SSN entered is associated with an existing user. Please <a href=\"https://iam_login_url\">Sign In</a> with your user name and password or <a href=\"https://account_recovery\">Click here</a> if you've forgotten your password."
-        else
-          expect(response).to redirect_to(root_path)
-        end
+        expect(response).to redirect_to(ssn_taken_insured_consumer_role_index_path)
+        expect(flash[:alert]).to eq "The SSN entered is associated with an existing user. Please <a href=\"https://iam_login_url\">Sign In</a> with your user name and password or <a href=\"https://account_recovery\">Click here</a> if you've forgotten your password."
       end
     end
   end
@@ -193,15 +181,14 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
     before(:each) do
       allow(Factories::EnrollmentFactory).to receive(:construct_employee_role).and_return(consumer_role)
       allow(consumer_role).to receive(:person).and_return(person)
+    end
+    it "should create new person/consumer role object" do
       sign_in user
+      post :create, person: person_params
+      expect(response).to have_http_status(:redirect)
     end
 
-    it "should create new person/consumer role object" do
-      post :create, person: person_params
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:redirect)
-      end
-    end
+
   end
 
 
@@ -209,16 +196,11 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
     let(:person_params){{"dob"=>"1985-10-01", "first_name"=>"martin","gender"=>"male","last_name"=>"york","middle_name"=>"","name_sfx"=>"","ssn"=>"000000111","user_id"=>"xyz"}}
     before(:each) do
       allow(Factories::EnrollmentFactory).to receive(:construct_consumer_role).and_return(nil)
-      sign_in user
     end
-
     it "should throw a 500 error" do
+      sign_in user
       post :create, person: person_params
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(500)
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(500)
     end
   end
 
@@ -231,17 +213,12 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
       allow(person).to receive(:consumer_role).and_return(consumer_role)
       allow(consumer_role).to receive(:save!).and_return(true)
       allow(consumer_role).to receive(:bookmark_url=).and_return(true)
-      sign_in user
     end
-
     it "should render new template" do
+      sign_in user
       get :edit, id: "test"
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:edit)
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
     end
   end
 
@@ -262,120 +239,87 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
       allow(consumer_role).to receive(:update_by_person).and_return(true)
       allow(controller).to receive(:update_vlp_documents).and_return(true)
       put :update, person: person_params, id: "test"
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(ridp_agreement_insured_consumer_role_index_path)
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(ridp_agreement_insured_consumer_role_index_path)
     end
 
     it "should not update the person" do
       allow(controller).to receive(:update_vlp_documents).and_return(false)
       allow(consumer_role).to receive(:update_by_person).and_return(true)
       put :update, person: person_params, id: "test"
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:edit)
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
     end
 
     it "should not update the person" do
       allow(controller).to receive(:update_vlp_documents).and_return(false)
       allow(consumer_role).to receive(:update_by_person).and_return(false)
       put :update, person: person_params, id: "test"
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:edit)
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
     end
 
     it "should raise error" do
       put :update, person: person_params, id: "test"
-      if individual_market_is_enabled?
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:edit)
-        expect(person.errors.full_messages).to include "Document type cannot be blank"
-      else
-        expect(response).to redirect_to(root_path)
-      end
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
+      expect(person.errors.full_messages).to include "Document type cannot be blank"
     end
 
     it "should call bubble_address_errors_by_person" do
       allow(controller).to receive(:update_vlp_documents).and_return(true)
       allow(consumer_role).to receive(:update_by_person).and_return(false)
-      if individual_market_is_enabled?
-        expect(controller).to receive(:bubble_address_errors_by_person)
-        put :update, person: person_params, id: "test"
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:edit)
-      else
-        put :update, person: person_params, id: "test"
-        expect(response).to redirect_to(root_path)
-      end
+      expect(controller).to receive(:bubble_address_errors_by_person)
+      put :update, person: person_params, id: "test"
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
     end
   end
 
   context "GET immigration_document_options" do
     let(:person) {FactoryGirl.create(:person, :with_consumer_role)}
+    let(:params) {{target_type: 'Person', target_id: "person_id", vlp_doc_target: "vlp doc", vlp_doc_subject: "I-327 (Reentry Permit)"}}
     let(:family_member) {FactoryGirl.create(:person, :with_consumer_role)}
     before :each do
       sign_in user
     end
-  end
 
-  context "target type is Person" do
-    before :each do
-      allow(Person).to receive(:find).and_return person
-      params = {target_type: 'Person', target_id: "person_id", vlp_doc_target: "vlp doc", vlp_doc_subject: "I-327 (Reentry Permit)"}
-      xhr :get, 'immigration_document_options', params, format: :js
-    end
-    it "should get person" do
-      if individual_market_is_enabled?
+    context "target type is Person" do
+      before :each do
+        allow(Person).to receive(:find).and_return person
+        xhr :get, 'immigration_document_options', params, format: :js
+      end
+      it "should get person" do
         expect(response).to have_http_status(:success)
         expect(assigns(:target)).to eq person
       end
-    end
 
-    it "assign vlp_doc_target from params" do
-      if individual_market_is_enabled?
+      it "assign vlp_doc_target from params" do
         expect(assigns(:vlp_doc_target)).to eq "vlp doc"
       end
-   end
 
-    it "assign country of citizenship based on vlp document" do
-      if individual_market_is_enabled?
+      it "assign country of citizenship based on vlp document" do
         expect(assigns(:country)).to eq "Ukraine"
-     end
+      end
     end
-  end
 
     context "target type is family member" do
       xit "should get FamilyMember" do
-        if individual_market_is_enabled?
-          allow(Forms::FamilyMember).to receive(:find).and_return family_member
-          xhr :get, 'immigration_document_options', {target_type: 'Forms::FamilyMember', target_id: "id", vlp_doc_target: "vlp doc", format: :js}
-          expect(response).to have_http_status(:success)
-          expect(assigns(:target)).to eq family_member
-          expect(assigns(:vlp_doc_target)).to eq "vlp doc"
-        else
-          expect(assigns(:country)).to eq "Ukraine"
-        end
+        allow(Forms::FamilyMember).to receive(:find).and_return family_member
+        xhr :get, 'immigration_document_options', {target_type: 'Forms::FamilyMember', target_id: "id", vlp_doc_target: "vlp doc", format: :js}
+        expect(response).to have_http_status(:success)
+        expect(assigns(:target)).to eq family_member
+        expect(assigns(:vlp_doc_target)).to eq "vlp doc"
       end
     end
 
     it "render javascript template" do
-      if individual_market_is_enabled?
-        allow(Person).to receive(:find).and_return person
-        xhr :get, 'immigration_document_options', params, format: :js
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:immigration_document_options)
-      end
+      allow(Person).to receive(:find).and_return person
+      xhr :get, 'immigration_document_options', params, format: :js
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:immigration_document_options)
     end
+  end
 
   context "GET ridp_agreement" do
 
@@ -383,7 +327,7 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
       before :each do
         sign_in user
       end
-    end
+
       before :each do
         allow(user).to receive(:person).and_return(person)
         allow(person).to receive(:consumer_role?).and_return(true)
@@ -393,8 +337,9 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
       end
 
       it "should redirect" do
-          expect(response).to be_redirect
+        expect(response).to be_redirect
       end
+    end
 
     context "with a user who has not passed RIDP" do
       before :each do
@@ -408,11 +353,7 @@ RSpec.describe Insured::ConsumerRolesController, :type => :controller do
       end
 
       it "should render the agreement page" do
-        if individual_market_is_enabled?
-          expect(response).to render_template("ridp_agreement")
-        else
-          expect(response).to redirect_to(root_path)
-        end
+        expect(response).to render_template("ridp_agreement")
       end
     end
   end
