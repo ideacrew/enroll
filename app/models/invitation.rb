5b751905f02956c89f051d517e2a17bf308810d1
Battula <vrbattula@outlook.com>
2017-04-14 13:12:31 -0400

    refs #14745 Invite new hire with past doh for open enrollment

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -185,6 +185,10 @@ class Invitation
     UserMailer.invitation_email(invitation_email, invitee_name, self).deliver_now
   end
 
+  def send_initial_employee_invitation_for_open_enrollment!(census_employee)
+    UserMailer.send_initial_employee_open_enrollment_email(invitation_email, census_employee, self).deliver_now
+  end
+
   def send_renewal_invitation!(census_employee)
     UserMailer.renewal_invitation_email(invitation_email, census_employee, self).deliver_now
   end
@@ -210,6 +214,19 @@ class Invitation
         :invitation_email => census_employee.email_address
       )
       invitation.send_invitation!(census_employee.full_name)
+      invitation
+    end
+  end
+
+  def self.invite_initial_employee_for_open_enrollment!(census_employee)
+    if !census_employee.email_address.blank?
+      invitation = self.create(
+        :role => "employee_role",
+        :source_kind => "census_employee",
+        :source_id => census_employee.id,
+        :invitation_email => census_employee.email_address
+      )
+      invitation.send_initial_employee_invitation_for_open_enrollment!(census_employee)
       invitation
     end
   end
