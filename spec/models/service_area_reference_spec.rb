aa15c3a375ac4320c63f5a9c091c3873090cf688
Brian Weiner <brian.weiner@dc.gov>
2017-04-18 15:08:54 -0400

    test use different name also

--- spec/models/service_area_reference_spec.rb
+++ spec/models/service_area_reference_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe ServiceAreaReference, type: :model do
 
   it { is_expected.to validate_presence_of :service_area_id }
   it { is_expected.to validate_presence_of :service_area_name }
-  it { is_expected.to validate_presence_of :covers_entire_state }
+  it { is_expected.to validate_presence_of :serves_entire_state }
 
-  context "covers_entire_state is false" do
-    subject { build(:service_area_reference, covers_entire_state: false) }
+  context "serves_entire_state is false" do
+    subject { build(:service_area_reference, serves_entire_state: false) }
 
     it { is_expected.to validate_presence_of :county_name }
     it { is_expected.to validate_presence_of :serves_partial_county }
 
     context "serves_partial_county is true" do
-      subject { build(:service_area_reference, covers_entire_state: false, serves_partial_county: true) }
+      subject { build(:service_area_reference, serves_entire_state: false, serves_partial_county: true) }
 
       it { is_expected.to validate_presence_of :partial_county_justification }
       it { is_expected.to validate_presence_of :service_area_zipcode }
