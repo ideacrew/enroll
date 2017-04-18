3bb994ebf9a1588eeb0d4b8615a3d773a10ca1ed
Brian Weiner <brian.weiner@dc.gov>
2017-04-18 10:50:24 -0400

    slight refactor

--- spec/lib/migrations/load_rate_reference_spec.rb
+++ spec/lib/migrations/load_rate_reference_spec.rb
@@ -3,8 +3,11 @@ require 'rake'
 require 'roo'
 
 RSpec.describe 'Load Rating Regions Task', :type => :task do
-  
+
   context "rate_reference:update_rating_regions" do
+    let(:file_path) { File.join(Rails.root,'lib', 'xls_templates', "SHOP_ZipCode_CY2017_FINAL.xlsx") }
+    let(:subject) { Roo::Spreadsheet.open(file_path) }
+
     before :all do
       Rake.application.rake_require "tasks/migrations/load_rate_reference"
       Rake::Task.define_task(:environment)
@@ -12,44 +15,38 @@ RSpec.describe 'Load Rating Regions Task', :type => :task do
     it 'expect not raise errors' do
       expect { invoke_task }.not_to raise_error
     end
-    
+
     it 'expect to read xlsx file' do
-      file_path = File.join(Rails.root,'lib', 'xls_templates', "SHOP_ZipCode_CY2017_FINAL.xlsx")
-      xlsx = Roo::Spreadsheet.open(file_path)
-      expect(xlsx.sheet(0).row(2)).to match_array(["01001", "Hampden", "No", "Rating Area 1"])
-      expect(xlsx.sheet(0).row(3)).to match_array(["01002", "Hampshire", "Yes", "Rating Area 1"])
-      expect(xlsx.sheet(0).row(4)).to match_array(["01002", "Franklin", "Yes", "Rating Area 1"])
-      expect(xlsx.sheet(0).row(699)).to match_array(["02791", "Bristol", "No", "Rating Area 6"])
+      expect(subject.sheet(0).row(2)).to match_array(["01001", "Hampden", "No", "Rating Area 1"])
+      expect(subject.sheet(0).row(3)).to match_array(["01002", "Hampshire", "Yes", "Rating Area 1"])
+      expect(subject.sheet(0).row(4)).to match_array(["01002", "Franklin", "Yes", "Rating Area 1"])
+      expect(subject.sheet(0).row(699)).to match_array(["02791", "Bristol", "No", "Rating Area 6"])
     end
-    
+
     it "should have the right data" do
-      file_path = File.join(Rails.root,'lib', 'xls_templates', "SHOP_ZipCode_CY2017_FINAL.xlsx")
-      xlsx = Roo::Spreadsheet.open(file_path)
-      expect(xlsx.sheet(0).row(2).first.to_i).to eq RateReference.first.zip_code
-      expect(xlsx.sheet(0).row(2)[1]).to eq RateReference.first.county
-      expect(xlsx.sheet(0).row(2).last).to eq RateReference.first.rating_region
+      expect(subject.sheet(0).row(2).first.to_i).to eq RateReference.first.zip_code
+      expect(subject.sheet(0).row(2)[1]).to eq RateReference.first.county
+      expect(subject.sheet(0).row(2).last).to eq RateReference.first.rating_region
     end
 
     it "should not have duplicate data" do
-      file_path = File.join(Rails.root,'lib', 'xls_templates', "SHOP_ZipCode_CY2017_FINAL.xlsx")
-      xlsx = Roo::Spreadsheet.open(file_path)
-      sheet = xlsx.sheet(0)
+      sheet = subject.sheet(0)
       rate_references = RateReference.where(zip_code: sheet.cell(2,1), county: sheet.cell(2,2), multiple_counties: to_boolean(sheet.cell(2,3)),
         rating_region: sheet.cell(2,4))
       expect(rate_references.size).to eq 1
     end
-    
+
     private
-    
+
     def invoke_task
       Rake::Task["load_rate_reference:update_rating_regions"].invoke
     end
-    
+
     def to_boolean(value)
       return true   if value == true   || value =~ (/(true|t|yes|y|1)$/i)
       return false  if value == false  || value =~ (/(false|f|no|n|0)$/i)
       raise ArgumentError.new("invalid value for Boolean: \"#{value}\"")
     end
   end
-  
+
 end
