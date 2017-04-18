1730f2eb5ae3f0149b6e343a1c3e3a016df1c1e7
Brian Weiner <brian.weiner@dc.gov>
2017-04-18 15:01:23 -0400

    this is a clearer name and it matches the partial county syntax

--- app/models/service_area_reference.rb
+++ app/models/service_area_reference.rb
@@ -3,15 +3,15 @@ class ServiceAreaReference
 
   field :service_area_id, type: String
   field :service_area_name, type: String
-  field :covers_entire_state, type: Boolean, default: false
+  field :serves_entire_state, type: Boolean, default: false
   field :county_name, type: String
   field :serves_partial_county, type: Boolean, default: false
   field :service_area_zipcode, type: String
   field :partial_county_justification, type: String
-  validates_presence_of :service_area_id, :service_area_name, :covers_entire_state
+  validates_presence_of :service_area_id, :service_area_name, :serves_entire_state
 
   validates :county_name, :serves_partial_county, presence: true,
-   unless: Proc.new { |a| a.covers_entire_state? }
+   unless: Proc.new { |a| a.serves_entire_state? }
   validates :partial_county_justification, :service_area_zipcode, presence: true,
    if: Proc.new { |a| a.serves_partial_county? }
 end
