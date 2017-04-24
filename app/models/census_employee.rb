3974d0754b98d5c243ad287347cca434fe9e83b7
Battula <vrbattula@outlook.com>
2017-04-24 11:21:32 -0400

    refs #14743 use Timekeeper advance day to send initial EE OE notices

--- app/models/census_employee.rb
+++ app/models/census_employee.rb
@@ -454,6 +454,21 @@ class CensusEmployee < CensusMember
       CensusEmployee.terminate_scheduled_census_employees
       CensusEmployee.rebase_newly_designated_employees
       CensusEmployee.terminate_future_scheduled_census_employees(new_date)
+      CensusEmployee.initial_employee_open_enrollment_notice(new_date)
+    end
+
+    def initial_employee_open_enrollment_notice(date)
+      census_employees = CensusEmployee.where(:"hired_on" => date)
+      census_employees.each do |ce|
+        begin
+          if ce.has_benefit_group_assignment?
+            plan_year = ce.active_benefit_group_assignment.benefit_group.plan_year
+            Invitation.invite_initial_employee_for_open_enrollment!(ce) if (ce.hired_on == date && plan_year.employees_are_matchable?)
+          end
+        rescue Exception => e
+          puts "Unable to deliver to #{ce.full_name} due to --- #{e}" unless Rails.env.test?
+        end
+      end
     end
 
     def terminate_scheduled_census_employees(as_of_date = TimeKeeper.date_of_record)
