b2f9baeae7061fdb92f7197c330438233776c21c
Battula <vrbattula@outlook.com>
2017-04-25 13:55:53 -0400

    refs #15201 Added notice builder and trigger

--- db/seedfiles/notice_triggers_seed.rb
+++ db/seedfiles/notice_triggers_seed.rb
@@ -254,6 +254,27 @@ shop_notice_triggers = [
       }
     ]
   },
+  {
+    hbx_id: 'SHOP15',
+    title: 'Notice of Low Enrollment - Action Needed',
+    description: 'Notifies all the employers who doesnt meet minimum participation requirement',
+    resource_name: 'employer',
+    event_name: 'low_enrollment_notice_for_employer',
+    notice_triggers: [
+      {
+        name: 'Low Enrollment Notice',
+        notice_template: 'notices/shop_employee_notices/low_enrollment_notice_for_employer',
+        notice_builder: 'ShopEmployeeNotices::LowEnrollmentNotice',
+        mpi_indicator: 'MPI_SHOP15',
+        notice_trigger_element_group: {
+          market_places: ['shop'],
+          primary_recipients: ["employer"],
+          primary_recipient_delivery_method: ["secure_message"],
+          secondary_recipients: []
+        }
+      }
+    ]
+  },
 ]
 
 
