3655db661c8140e1a7918395d681b86e227ce116
shaibazalisyed <shaibazali34@gmail.com>
2017-04-25 11:25:54 -0400

    made required changes

--- app/helpers/insured/shop_plans_helper.rb
+++ app/helpers/insured/shop_plans_helper.rb
@@ -1,6 +1,6 @@
 module Insured::ShopPlansHelper
   def redirect_to_plans
-    (@person.active_employee_roles.blank? && (@person.consumer_role.present? || @person.active_employee_roles.any?) && !is_under_open_enrollment? && !@employee_role.try(:is_under_open_enrollment?)) || !@person.active_employee_roles.blank?
+    (@person.active_employee_roles.blank? && @person.consumer_role.present? && !is_under_open_enrollment? && !@employee_role.try(:is_under_open_enrollment?)) || !@person.active_employee_roles.blank?
   end
 
   def shop_plans_action
@@ -8,7 +8,6 @@ module Insured::ShopPlansHelper
     if @person.active_employee_roles.blank?
       action_path = find_sep_insured_families_path
     else
-      action_path = find_sep_insured_families_path
       if is_under_open_enrollment? || @employee_role.try(:is_eligible_to_enroll_without_qle?) || @person.consumer_role.present?
         action_path = new_insured_group_selection_path
       end
