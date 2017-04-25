12f90920340164c3b0cae543de89ae6a986b4e4b
Kevin Wei <kevin.tw.wei@gmail.com>
2017-04-25 16:02:30 -0400

    Refs #15265 suppress output during tests

--- app/data_migrations/remove_member_from_hbx_enrollment.rb
+++ app/data_migrations/remove_member_from_hbx_enrollment.rb
@@ -6,14 +6,14 @@ class RemoveMemberFromHbxEnrollment < MongoidMigrationTask
     hbx_enrollment_member_id = ENV["hbx_enrollment_member_id"]
     hbx_enrollment=HbxEnrollment.by_hbx_id(enrollment_hbx_id)
     if hbx_enrollment.blank?
-      puts "No hbx_enrollment was found with the given hbx_id"
+      puts "No hbx_enrollment was found with the given hbx_id" unless Rails.env.test?
     else
       enrollment_member=hbx_enrollment[0].hbx_enrollment_members.where(id:hbx_enrollment_member_id).first
       if enrollment_member.nil?
-        puts "No enrollment member was found with the given applicant id"
+        puts "No enrollment member was found with the given applicant id" unless Rails.env.test?
       else
         enrollment_member.destroy
-        puts "The enrollment member with id #{hbx_enrollment_member_id} was removed from enrollment #{enrollment_hbx_id}"
+        puts "The enrollment member with id #{hbx_enrollment_member_id} was removed from enrollment #{enrollment_hbx_id}" unless Rails.env.test?
       end
     end
   end
