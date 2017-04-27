c946793cbc4ed369dfe8d65ff558f0dda6dd222c
Brian Weiner <brian.weiner@dc.gov>
2017-04-27 12:18:14 -0400

    added tests for rate reference, added a uniqueness constraint for zip county pairs, some finder methods

--- app/models/rate_reference.rb
+++ app/models/rate_reference.rb
@@ -2,16 +2,23 @@ class RateReference
   include Mongoid::Document
 
   field :zip_code, type: String
-  field :county, type: String
+  field :county_name, type: String
   field :rating_region, type: String
-  field :multiple_counties, type: Boolean, default: false
+  field :zip_code_in_multiple_counties, type: Boolean, default: false
 
-  validates_presence_of :zip_code, :county, :rating_region, :multiple_counties
+  validates_presence_of :zip_code, :county_name, :rating_region, :zip_code_in_multiple_counties
+  validates_uniqueness_of :zip_code, :scope => [:county_name]
 
   class << self
-  def find_rating_region(zip, county)
-    record = self.where(zip_code: zip, county: county).first
-   record.present? ? record : nil
-  end
+    def find_rating_region(zip_code:, county_name:)
+      region = self.where(zip_code: zip_code, county_name: county_name)
+      raise "Multiple Regions Returned" if region.size > 1
+      return nil if region.empty?
+      region
+    end
+
+    def find_zip_codes_for_county(county_name:)
+      self.where(county_name: county_name).pluck(:zip_code)
+    end
   end
 end
