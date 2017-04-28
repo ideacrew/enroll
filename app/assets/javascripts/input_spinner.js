d8306cf20b7d6af3e63e21f3050b6ba0b120b978
sarathpyerra <sarathpyerra@gmail.com>
2017-04-28 11:47:44 -0400

    Refs #7501 #15379 adjustments to spinner js

--- app/assets/javascripts/input_spinner.js
+++ app/assets/javascripts/input_spinner.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-  $('.input-spinner .btn:first-of-type').on('click', function() {
+$(document).on('click', '.input-spinner .btn:first-of-type' , function() {
     input_value= parseInt($('.input-spinner input').val(), 10);
     if(input_value > 0 && input_value < 36){
       $('.input-spinner input').val( input_value + 1);
     }
-  });
-  $('.input-spinner .btn:last-of-type').on('click', function() {
+});
+
+$(document).on('click', '.input-spinner .btn:last-of-type' , function() {
     input_value= parseInt($('.input-spinner input').val(), 10);
     if(input_value > 1 && input_value <= 36){
       $('.input-spinner input').val(input_value- 1);
     }
-  });
 });
