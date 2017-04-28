273d65445bbc22061a8872c1534c34334f02693f
vinner <vineelsonnathi@users.noreply.github.com>
2017-04-28 00:35:33 -0400

    Refs #15353 : Update rake task as per kevins suggestion

--- app/data_migrations/change_fein.rb
+++ app/data_migrations/change_fein.rb
@@ -1,12 +1,16 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
+
 class ChangeFein< MongoidMigrationTask
   def migrate
     trigger_single_table_inheritance_auto_load_of_child = VlpDocument
     organization = Organization.where(fein:ENV['old_fein']).first
+
     new_fein = ENV['new_fein']
+
     if organization.nil?
-      puts "No organization was found by the given fein"
+      puts "No organization was found by the given fein" unless Rails.env.test?
     else
+      organization.unset(:fein)
       organization.update_attributes(fein:new_fein)
       puts "Changed fein to #{new_fein}" unless Rails.env.test?
     end
