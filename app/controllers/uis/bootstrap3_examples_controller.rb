714bf815bdbc0179eda42e1c21d9ec8f328977cf
Dan Thomas <dan.thomas@dc.gov>
2017-05-01 10:25:57 -0400

    Initial commit
     Conflicts:
    	config/routes.rb conflicts on line 3-25, kept both changes

---
+++ app/controllers/uis/bootstrap3_examples_controller.rb
@@ -0,0 +1,15 @@
+class Uis::Bootstrap3ExamplesController < ApplicationController
+
+  def index
+  end
+
+  def getting_started
+  end
+
+  def components
+  end
+
+  def show
+  end
+
+end
---
+++ spec/controllers/uis/bootstrap3_examples_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Uis::Bootstrap3ExamplesController, type: :controller do
+
+end
