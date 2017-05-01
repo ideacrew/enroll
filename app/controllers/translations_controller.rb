75089482c111c9cecf2b7761997afcaa7eb7de81
Clinton De Young <clinton@axisavant.com>
2017-05-01 13:21:48 -0600

    Update the translation controller to use MongoI1tn.store.

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,10 +1,11 @@
 class TranslationsController < ApplicationController
   def index
-    @translations = Translation.all
+    @translations = MongoI18n.store
   end
 
   def create
-    I18n.backend.store_translations(params[:locale], {params[:key] => params[:value]}, :escape => false)
+    I18n.backend.store_translations(params[:locale], {params[:key] => params[:value]}, escape: false)
     redirect_to translations_url, :notice => "Added translations"
   end
 end
+
