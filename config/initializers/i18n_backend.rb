e5558f4b61518fc69e5d79e9c52a8a81f3cdd23e
Clinton De Young <clinton@axisavant.com>
2017-05-02 10:08:41 -0600

    (I18N) - Use the browser's settings to determine which language is loaded

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,4 +1,4 @@
 require 'mongo_i18n'
-I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::KeyValue.new(MongoI18n.store))
+I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(MongoI18n.store), I18n.backend)
 I18n::Backend::KeyValue.send(:include, I18n::Backend::Cache)
 I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
