72c30bbb22e24701cdd197aaf726fe60e9d81526
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-05-03 16:45:20 -0400

    Refs #15356 update error messaging

--- app/data_migrations/change_person_dob.rb
+++ app/data_migrations/change_person_dob.rb
@@ -5,7 +5,7 @@ class ChangePersonDob< MongoidMigrationTask
     person=Person.where(hbx_id:ENV['hbx_id']).first
     new_dob = Date.strptime(ENV['new_dob'],'%m/%d/%Y')
     if person.nil?
-      puts "No person was found by the given fein"
+      puts "No person was found by the given hbx_id" unless Rails.env.test?
     else
       person.update_attributes(dob:new_dob)
       puts "Changed date of birth to #{new_dob}" unless Rails.env.test?
