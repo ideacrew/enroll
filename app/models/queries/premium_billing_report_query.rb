a3dce9e7486060778fa93683343037079d582448
krbhavith <krbhavith@gmail.com>
2017-05-04 15:34:14 -0400

    Refs#15468 to display each enrollment in each row

--- app/models/queries/premium_billing_report_query.rb
+++ app/models/queries/premium_billing_report_query.rb
@@ -8,8 +8,8 @@ module Queries
       self
     end
 
-    def initialize(enrollment_id)
-      @hbx_enrollment_ids = enrollment_id
+    def initialize(enroll_ids)
+      @hbx_enrollment_ids = enroll_ids
       
     end
 
@@ -24,7 +24,6 @@ module Queries
     def klass
       Person
       Family
-      HbxEnrollment
     end
 
     def size
@@ -37,20 +36,20 @@ module Queries
     end
 
     def build_scope
-       hbx_enrollment_ids = {"households.hbx_enrollments._id" => {"$in" => @hbx_enrollment_ids}}
+      enrollment_ids = {"households.hbx_enrollments._id" => {"$in" => @hbx_enrollment_ids}}
       if @search_string
         employee_roles = Person.any_of({ :first_name => /.*#{@search_string}.*/ }, { :last_name => /.*#{@search_string}.*/ }).collect{|p| p.employee_roles.pluck(:_id)}.flatten
-        hbx_enrollment_ids = {
+        enrollment_ids = {
           "$and" => [
             { 'households.hbx_enrollments.employee_role_id' => {"$in" => employee_roles} },
-            hbx_enrollment_ids
+            enrollment_ids 
           ]
         }
       end
       # if @order_by
       #   return Family.unscoped.where(criteria).order_by(@order_by)
       # end
-      Family.unscoped.where(hbx_enrollment_ids).order_by(:'households.hbx_enrollments._id'.asc)
+      Family.unscoped.where(enrollment_ids).order_by(:'households.hbx_enrollments._id'.asc)
     end
 
   end
