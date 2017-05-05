79fd63d8ed2216fb2c6edbef97591826ab6fe198
Trey Evans <lewis.r.evans@gmail.com>
2017-05-05 10:46:45 -0400

    Refs #15696.
    Added policy and initial checks.
    Right now only handles user, brokers, and GAs.
    Need to add code to allow admins and CSRs access.

--- app/policies/family_policy.rb
+++ app/policies/family_policy.rb
@@ -1,4 +1,38 @@
 class FamilyPolicy < ApplicationPolicy
+  def show?
+    user_person = @user.person
+    if user_person
+      primary_applicant = @record.primary_applicant
+      return true if (@record.primary_applicant.person_id == user_person.id)
+      broker_role = user_person.broker_role
+      employee_roles = primary_applicant.person.active_employee_roles
+      if broker_role
+        ivl_broker_account = @record.active_broker_agency_account
+        if ivl_broker_account
+          return true if (ivl_broker_account.broker_agency_profile_id == broker_role.broker_agency_profile_id)
+        end
+        if employee_roles.any?
+          broker_agency_profile_account_ids = employee_roles.map do |er|
+            er.employer_profile.active_broker_agency_account
+          end.compact.map(&:broker_agency_profile_id)
+          return true if broker_agency_profile_account_ids.include?(broker_role.broker_agency_profile_id)
+        end
+      end
+      ga_roles = user_person.active_general_agency_staff_roles
+      if ga_roles.any?
+        if employee_roles.any?
+          general_agency_profile_account_ids = employee_roles.map do |er|
+            er.employer_profile.active_general_agency_account
+          end.compact.map(&:general_agency_profile_id)
+          ga_roles.each do |ga_role|
+            return true if general_agency_profile_account_ids.include?(ga_role.general_agency_profile_id)
+          end
+        end
+      end
+    end
+    false
+  end
+
   def updateable?
     return true unless role = user.person && user.person.hbx_staff_role
     role.permission.modify_family
---
+++ spec/policies/family_policy_spec.rb
@@ -0,0 +1,124 @@
+require "rails_helper"
+
+describe FamilyPolicy, "given a user who has no properties" do
+  let(:primary_person_id) { double }
+  let(:family) { instance_double(Family) }
+  let(:user) { instance_double(User, :person => nil) }
+
+  subject { FamilyPolicy.new(user, family) }
+
+  it "can't show" do
+    expect(subject.show?).to be_falsey
+  end
+end
+
+describe FamilyPolicy, "given a user who is the primary member" do
+  let(:primary_person_id) { double }
+  let(:family) { instance_double(Family, :primary_applicant => primary_member) }
+  let(:person) { instance_double(Person, :id => primary_person_id) }
+  let(:user) { instance_double(User, :person => person) }
+  let(:primary_member) { instance_double(FamilyMember, :person_id => primary_person_id) } 
+
+  subject { FamilyPolicy.new(user, family) }
+
+  it "can show" do
+    expect(subject.show?).to be_truthy
+  end
+end
+
+describe FamilyPolicy, "given a family with an active broker agency account" do
+  let(:primary_person_id) { double }
+  let(:broker_person_id) { double }
+  let(:broker_agency_profile_id) { double }
+  let(:family) { instance_double(Family, :primary_applicant => primary_member, :active_broker_agency_account => broker_agency_account) }
+  let(:person) { instance_double(Person, :id => primary_person_id, :active_employee_roles => []) }
+  let(:user) { instance_double(User, :person => broker_person) }
+  let(:broker_role) { instance_double(BrokerRole, :broker_agency_profile_id => broker_agency_profile_id) }
+  let(:broker_person) { instance_double(Person, :id => broker_person_id, :broker_role => broker_role, :active_general_agency_staff_roles => []) }
+  let(:broker_agency_account) { instance_double(BrokerAgencyAccount, :broker_agency_profile_id => broker_agency_profile_id_account) }
+  let(:primary_member) { instance_double(FamilyMember, :person_id => primary_person_id, :person => person) }
+
+  subject { FamilyPolicy.new(user, family) }
+
+  describe "when the user is an active member of the same broker agency as the account" do
+    let(:broker_agency_profile_id_account) { broker_agency_profile_id }
+
+    it "can show" do
+      expect(subject.show?).to be_truthy
+    end
+  end
+
+  describe "when the user is an active member of a different broker agency from the account" do
+    let(:broker_agency_profile_id_account) { double }
+
+    it "can't show" do
+      expect(subject.show?).to be_falsey
+    end
+  end
+end
+
+describe FamilyPolicy, "given a family where the primary has an active employer broker account" do
+  let(:primary_person_id) { double }
+  let(:broker_person_id) { double }
+  let(:broker_agency_profile_id) { double }
+  let(:family) { instance_double(Family, :primary_applicant => primary_member, :active_broker_agency_account => nil) }
+  let(:employer_profile) { instance_double(EmployerProfile, :active_broker_agency_account => broker_agency_account) }
+  let(:employee_role) { instance_double(EmployeeRole, :employer_profile => employer_profile) }
+  let(:person) { instance_double(Person, :id => primary_person_id, :active_employee_roles => [employee_role]) }
+  let(:user) { instance_double(User, :person => broker_person) }
+  let(:broker_role) { instance_double(BrokerRole, :broker_agency_profile_id => broker_agency_profile_id) }
+  let(:broker_person) { instance_double(Person, :id => broker_person_id, :broker_role => broker_role, :active_general_agency_staff_roles => []) }
+  let(:broker_agency_account) { instance_double(BrokerAgencyAccount, :broker_agency_profile_id => broker_agency_profile_id_account) }
+  let(:primary_member) { instance_double(FamilyMember, :person_id => primary_person_id, :person => person) }
+
+  subject { FamilyPolicy.new(user, family) }
+
+  describe "when the user is an active member of the same broker agency as the account" do
+    let(:broker_agency_profile_id_account) { broker_agency_profile_id }
+
+    it "can show" do
+      expect(subject.show?).to be_truthy
+    end
+  end
+
+  describe "when the user is an active member of a different broker agency from the account" do
+    let(:broker_agency_profile_id_account) { double }
+
+    it "can't show" do
+      expect(subject.show?).to be_falsey
+    end
+  end
+end
+
+describe FamilyPolicy, "given a family where the primary has an active employer general agency account account" do
+  let(:primary_person_id) { double }
+  let(:ga_person_id) { double }
+  let(:general_agency_profile_id) { double }
+  let(:family) { instance_double(Family, :primary_applicant => primary_member, :active_broker_agency_account => nil) }
+  let(:employer_profile) { instance_double(EmployerProfile, :active_broker_agency_account => nil, :active_general_agency_account => general_agency_account) }
+  let(:employee_role) { instance_double(EmployeeRole, :employer_profile => employer_profile) }
+  let(:person) { instance_double(Person, :id => primary_person_id, :active_employee_roles => [employee_role]) }
+  let(:user) { instance_double(User, :person => ga_person) }
+  let(:ga_person) { instance_double(Person, :id => ga_person_id, :broker_role => nil, :active_general_agency_staff_roles => [general_agency_staff_role]) }
+  let(:general_agency_staff_role) { instance_double(GeneralAgencyStaffRole, :general_agency_profile_id => general_agency_profile_id) }
+  let(:general_agency_account) { instance_double(GeneralAgencyAccount, :general_agency_profile_id => general_agency_account_profile_id ) }
+  let(:primary_member) { instance_double(FamilyMember, :person_id => primary_person_id, :person => person) }
+
+  subject { FamilyPolicy.new(user, family) }
+
+  describe "when the user is an active member of the same general agency as the account" do
+    let(:general_agency_account_profile_id) { general_agency_profile_id }
+
+    it "can show" do
+      expect(subject.show?).to be_truthy
+    end
+  end
+
+  describe "when the user is an active member of a different general agency from the account" do
+    let(:general_agency_account_profile_id) { double }
+
+    it "can't show" do
+      expect(subject.show?).to be_falsey
+    end
+  end
+end
