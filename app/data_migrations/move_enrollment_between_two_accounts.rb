66201aa637eadfb176d2ca68b52b649660708e48
Ji Wang <traceyw@vt.edu>
2017-05-05 16:12:03 -0400

    Ref update the rake task to move enrollment between two accounts

--- app/data_migrations/move_enrollment_between_two_accounts.rb
+++ app/data_migrations/move_enrollment_between_two_accounts.rb
@@ -3,137 +3,79 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
   def migrate
     #find two person accounts and all the hbx_enrollments
-    gp = Person.where(hbx_id:ENV['new_account_hbx_id']).first
-    bp = Person.where(hbx_id:ENV['old_account_hbx_id']).first
-    hbx_enrollments= bp.primary_family.active_household.hbx_enrollments
-    #rebuild the family
-    #add the family members of the old enrollments to the new enrollment
-    family=gp.primary_family
-    hbx_enrollments.each do |hbx_enrollment|
-      unless hbx_enrollment.hbx_enrollment_members
-        hbx_enrollment.hbx_enrollment_members.each do |hbx_enrollment_member|
-          family.add_family_member(hbx_enrollment_member.person)
-        end
-      end
-    end
-    #move the hbx_enrollments
-
-    hbx_enrollments.each do |hbx_enrollment|
-      #check whether the enrollment member of the hbx_enrollment match with each other
-      enrollment_person=hbx_enrollment_members.map{|a|a.person}
-
-      if hbx_enrollment.is_shop?
-        unless hbx_enrollment.census_employee.nil?
-           if hbx_enrollment.census_employee.id == gp.employee_roles.first.census_employee.id
-
-
-           #   enrollment = HbxEnrollment.new
-           #   enrollment.household = coverage_household.household
-           #
-           #   enrollment.submitted_at = submitted_at
-           #   case
-           #     when employee_role.present?
-           #       if benefit_group.blank? || benefit_group_assignment.blank?
-           #         benefit_group, benefit_group_assignment = employee_current_benefit_group(employee_role, enrollment, qle)
-           #       end
-           #
-           #       enrollment.kind = "employer_sponsored"
-           #       enrollment.employee_role = employee_role
-           #
-           #       if qle && enrollment.family.is_under_special_enrollment_period?
-           #         enrollment.effective_on = [enrollment.family.current_sep.effective_on, benefit_group.start_on].max
-           #         enrollment.enrollment_kind = "special_enrollment"
-           #       else
-           #         if external_enrollment && coverage_start.present?
-           #           enrollment.effective_on = coverage_start
-           #         else
-           #           enrollment.effective_on = calculate_start_date_from(employee_role, coverage_household, benefit_group)
-           #         end
-           #         enrollment.enrollment_kind = "open_enrollment"
-           #       end
-           #
-           #       enrollment.benefit_group_id = benefit_group.id
-           #       enrollment.benefit_group_assignment_id = benefit_group_assignment.id
-           #     when consumer_role.present?
-           #       enrollment.consumer_role = consumer_role
-           #       enrollment.kind = "individual"
-           #       enrollment.benefit_package_id = benefit_package.try(:id)
-           #       benefit_sponsorship = HbxProfile.current_hbx.benefit_sponsorship
-           #
-           #       if qle && enrollment.family.is_under_special_enrollment_period?
-           #         enrollment.effective_on = enrollment.family.current_sep.effective_on
-           #         enrollment.enrollment_kind = "special_enrollment"
-           #       elsif enrollment.family.is_under_ivl_open_enrollment?
-           #         enrollment.effective_on = benefit_sponsorship.current_benefit_period.earliest_effective_date
-           #         enrollment.enrollment_kind = "open_enrollment"
-           #       else
-           #         raise "You may not enroll until you're eligible under an enrollment period"
-           #       end
-           #     when resident_role.present?
-           #       enrollment.kind = "coverall"
-           #       enrollment.resident_role = resident_role
-           #       enrollment.benefit_package_id = benefit_package.try(:id)
-           #       benefit_sponsorship = HbxProfile.current_hbx.benefit_sponsorship
-           #
-           #       if qle && enrollment.family.is_under_special_enrollment_period?
-           #         enrollment.effective_on = enrollment.family.current_sep.effective_on
-           #         enrollment.enrollment_kind = "special_enrollment"
-           #       elsif enrollment.family.is_under_ivl_open_enrollment?
-           #         enrollment.effective_on = benefit_sponsorship.current_benefit_period.earliest_effective_date
-           #         enrollment.enrollment_kind = "open_enrollment"
-           #       else
-           #         raise "You may not enroll until you're eligible under an enrollment period"
-           #       end
-           #     else
-           #       raise "either employee_role or consumer_role is required" unless resident_role.present?
-           #   end
-           #   coverage_household.coverage_household_members.each do |coverage_member|
-           #     enrollment_member = HbxEnrollmentMember.new_from(coverage_household_member: coverage_member)
-           #     enrollment_member.eligibility_date = enrollment.effective_on
-           #     enrollment_member.coverage_start_on = enrollment.effective_on
-           #     enrollment.hbx_enrollment_members << enrollment_member
-           #   end
-           #   enrollment
-           # end
-           #
-           #
-
-
-
-
-
-
-
-
-
-             gp.primary_family.active_household.create_hbx_enrollment_from(
-                 employee_role: hbx_enrollment.employee_role,
-                 consumer_role: hbx_enrollment.consumer_role,
-                 coverage_household:gp.primary_family.active_household.immediate_family_coverage_household,
-                 benefit_group: hbx_enrollment.benefit_group,
-                 benefit_group_assignment:hbx_enrollment.benefit_group_assignment
-             )
-             bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment.id)
-           else
-             puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to census employee role mismatch"
-           end
-        else
-          puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to no census employee role"
-        end
-      elsif hbx_enrollment.kind == "individual"
-        gp.primary_family.active_household.create_hbx_enrollment_from(
-
-            consumer_role: hbx_enrollment.consumer_role,
-            employee_role: hbx_enrollment.employee_role,
-            coverage_household:gp.primary_family.active_household.immediate_family_coverage_household
-        )
-        bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment.id)
+    new_account_hbx_id = ENV['new_account_hbx_id']
+    old_account_hbx_id = ENV['old_account_hbx_id']
+    gp = Person.where(hbx_id:new_account_hbx_id).first
+    bp = Person.where(hbx_id:old_account_hbx_id).first
+    if gp.nil?
+      puts "No person record was found with given hbx_id #{new_account_hbx_id}" unless Rails.env.test?
+    elsif bp.nil?
+      puts "No person record was found with given hbx_id #{old_account_hbx_id}" unless Rails.env.test?
+    else
+      enrollment_hbx_id = HbxEnrollment.by_hbx_id(ENV['enrollment_hbx_id'])
+      hbx_enrollment= bp.primary_family.active_household.hbx_enrollments.where(hbx_id:enrollment_hbx_id).first
+      if hbx_enrollment.nil?
+        puts "No hbx_enrollment was found with given hbx_id #{enrollment_hbx_id}" unless Rails.env.test?
+      elsif !check_family_member_match(bp.primary_family,gp.primary_family)
+        puts "The enrollment with hbx_id #{enrollment_hbx_id} can not be moved due to a mismatch of family members" unless Rails.env.test?
       else
-        puts "Enrollment #{hbx_enrollment.id} can not be moved due to it is neither ivl or shop"
+        if hbx_enrollment.is_shop?
+          #1 check whether moveable
+          #2create the new hbx_enrollment
+          #3delete the old hbx_enrollment
+          if gp.employee_roles.empty? ||  bp.employee_roles.empty?
+            puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to employee.role " unless Rails.env.test?
+          else
+            good_employer=gp.employee_roles.first.census_employee.employer_profile.first
+            bad_employer=bp.employee_roles.first.census_employee.employer_profile.first
+            if good_employer || bad_employer || good_employer.id != bad_employer.id
+              puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to the same employer" unless Rails.env.test?
+            elsif good_employer.active_plan_year.benefit_groups.map{|a| a.id}.include? hbx_enrollment.benefit_group.id
+              #create the new hbx_enrollment
+              gp.primary_family.active_household.create_hbx_enrollment_from(
+                  employee_role: hbx_enrollment.employee_role,
+                  consumer_role: hbx_enrollment.consumer_role,
+                  coverage_household:gp.primary_family.active_household.immediate_family_coverage_household,
+                  benefit_group: hbx_enrollment.benefit_group,
+                  benefit_group_assignment:hbx_enrollment.benefit_group_assignment
+              )
+              bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment.id)
+            else
+              puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to a mismatch of benegit groups" unless Rails.env.test?
+            end
+
+          end
+       elsif hbx_enrollment.kind == "individual"
+          #1 check whether moveable
+          if gp.consumer_role.present? && bp.consumer_role.present?
+            #2create the new hbx_enrollment
+            #? how to add enrollment member
+            #3delete the old hbx_enrollment
+            gp.primary_family.active_household.create_hbx_enrollment_from(
+                consumer_role: hbx_enrollment.consumer_role,
+                employee_role: hbx_enrollment.employee_role,
+                coverage_household:gp.primary_family.active_household.immediate_family_coverage_household
+            )
+            bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment.id)
+
+          else
+            puts "the enrollment can not be moved due to at least one of the person has no consumer role" unless Rails.env.test?
+          end
+        else
+          puts "Enrollment #{hbx_enrollment.id} can not be moved due to it is neither ivl or shop" unless Rails.env.test?
+        end
       end
     end
+end
+  def check_family_member_match(family_from,family_to)
+  #if family_to's family members include all family_from's family members, return true
+    return true if family_from.id == family_to.id
+    family_from_members = family_from.family_members.to_a
+    family_to_members = family_to.family_members.to_a
+    return true if family_from_members == family_to_members
+    return true if (family_to_members-family_from_members).empty? == false
+    return false
   end
 end
-
 
 
--- lib/tasks/migrations/move_enrollment_between_two_accounts.rake
+++ lib/tasks/migrations/move_enrollment_between_two_accounts.rake
@@ -1,6 +1,6 @@
 require File.join(Rails.root, "app", "data_migrations", "move_enrollment_between_two_accounts")
-# This rake task is to move all hbx_enrollments between two accounts with consumer role
-# RAILS_ENV=production bundle exec rake migrations:move_enrollment_between_two_accounts old_account_hbx_id=19778757  new_account_hbx_id=19778757
+# This rake task is to move hbx_enrollment between two accounts
+# RAILS_ENV=production bundle exec rake migrations:move_enrollment_between_two_accounts old_account_hbx_id=19778757  new_account_hbx_id=19778757 enrollment_hbx_id=123123123
 
 namespace :migrations do
   desc "move enrollment between two accounts"
