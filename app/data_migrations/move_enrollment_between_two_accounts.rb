44ee5bb31a61a71efc2cb8d5c82875027de19821
Ji Wang <traceyw@vt.edu>
2017-05-10 17:18:47 -0400

    Ref#13363 need to fix rspec

--- app/data_migrations/move_enrollment_between_two_accounts.rb
+++ app/data_migrations/move_enrollment_between_two_accounts.rb
@@ -1,10 +1,10 @@
-require File.join(Rails.root, "lib/mongoid_migration_task")
-
+require File.join(Rails.root,"lib/mongoid_migration_task")
 class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
   def migrate
     #find two person accounts and all the hbx_enrollments
     new_account_hbx_id = ENV['new_account_hbx_id']
     old_account_hbx_id = ENV['old_account_hbx_id']
+    hbx_enrollment_hbx_id = ENV['enrollment_hbx_id']
     gp = Person.where(hbx_id:new_account_hbx_id).first
     bp = Person.where(hbx_id:old_account_hbx_id).first
     if gp.nil?
@@ -12,8 +12,8 @@ class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
     elsif bp.nil?
       puts "No person record was found with given hbx_id #{old_account_hbx_id}" unless Rails.env.test?
     else
-      enrollment_hbx_id = HbxEnrollment.by_hbx_id(ENV['enrollment_hbx_id'])
-      hbx_enrollment= bp.primary_family.active_household.hbx_enrollments.where(hbx_id:enrollment_hbx_id).first
+      #enrollment_hbx_id = HbxEnrollment.by_hbx_id(hbx_enrollment_hbx_id)
+      hbx_enrollment= bp.primary_family.active_household.hbx_enrollments.where(hbx_id:hbx_enrollment_hbx_id).first
       if hbx_enrollment.nil?
         puts "No hbx_enrollment was found with given hbx_id #{enrollment_hbx_id}" unless Rails.env.test?
       elsif !check_family_member_match(bp.primary_family,gp.primary_family)
@@ -23,15 +23,20 @@ class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
           #1 check whether moveable
           #2create the new hbx_enrollment
           #3delete the old hbx_enrollment
+
           if gp.employee_roles.empty? ||  bp.employee_roles.empty?
-            puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to employee.role " unless Rails.env.test?
+            puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to employee.role" unless Rails.env.test?
           else
-            good_employer=gp.employee_roles.first.census_employee.employer_profile.first
-            bad_employer=bp.employee_roles.first.census_employee.employer_profile.first
-            if good_employer || bad_employer || good_employer.id != bad_employer.id
+            good_employer=gp.employee_roles.first.census_employee.employer_profile
+            bad_employer=bp.employee_roles.first.census_employee.employer_profile
+            puts good_employer.active_plan_year
+            puts good_employer.active_plan_year.benefit_groups
+            puts hbx_enrollment.benefit_group_id
+            if good_employer.nil? || bad_employer.nil? || good_employer.id != bad_employer.id
               puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to the same employer" unless Rails.env.test?
-            elsif good_employer.active_plan_year.benefit_groups.map{|a| a.id}.include? hbx_enrollment.benefit_group.id
+            elsif good_employer.active_plan_year.benefit_groups.map{|a| a.id}.include? hbx_enrollment.benefit_group_id
               #create the new hbx_enrollment
+              puts "hihihihihi"
               gp.primary_family.active_household.create_hbx_enrollment_from(
                   employee_role: hbx_enrollment.employee_role,
                   consumer_role: hbx_enrollment.consumer_role,
@@ -43,32 +48,30 @@ class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
             else
               puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to a mismatch of benegit groups" unless Rails.env.test?
             end
-
           end
-       elsif hbx_enrollment.kind == "individual"
-          #1 check whether moveable
-          if gp.consumer_role.present? && bp.consumer_role.present?
-            #2create the new hbx_enrollment
-            #? how to add enrollment member
-            #3delete the old hbx_enrollment
-            gp.primary_family.active_household.create_hbx_enrollment_from(
-                consumer_role: hbx_enrollment.consumer_role,
-                employee_role: hbx_enrollment.employee_role,
-                coverage_household:gp.primary_family.active_household.immediate_family_coverage_household
-            )
-            bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment.id)
-
-          else
-            puts "the enrollment can not be moved due to at least one of the person has no consumer role" unless Rails.env.test?
-          end
+        elsif hbx_enrollment.kind == "individual"
+            #1 check whether moveable
+            if gp.consumer_role.present? && bp.consumer_role.present?
+              #2create the new hbx_enrollment
+              #? how to add enrollment member
+              #3delete the old hbx_enrollment
+              gp.primary_family.active_household.create_hbx_enrollment_from(
+                  consumer_role: hbx_enrollment.consumer_role,
+                  employee_role: hbx_enrollment.employee_role,
+                  coverage_household:gp.primary_family.active_household.immediate_family_coverage_household
+              )
+              bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment_hbx_id)
+            else
+              puts "the enrollment can not be moved due to at least one of the person has no consumer role" unless Rails.env.test?
+            end
         else
           puts "Enrollment #{hbx_enrollment.id} can not be moved due to it is neither ivl or shop" unless Rails.env.test?
         end
       end
     end
-end
+  end
   def check_family_member_match(family_from,family_to)
-  #if family_to's family members include all family_from's family members, return true
+    #if family_to’s family members include all family_from’s family members, return true
     return true if family_from.id == family_to.id
     family_from_members = family_from.family_members.to_a
     family_to_members = family_to.family_members.to_a
@@ -77,5 +80,3 @@ end
     return false
   end
 end
-
-
--- spec/data_migrations/move_enrollment_between_two_accounts_spec.rb
+++ spec/data_migrations/move_enrollment_between_two_accounts_spec.rb
@@ -24,7 +24,8 @@ describe MoveEnrollmentBetweenTwoAccount do
                            consumer_role: consumer_role,
                            aasm_state: 'shopping')
                         }
-    let(:consumer_role) {FactoryGirl.create(:consumer_role,person: family1.family_members[0].person)}
+    let!(:consumer_role) {FactoryGirl.create(:consumer_role,person: family1.family_members[0].person)}
+    let!(:consumer_role1) {FactoryGirl.create(:consumer_role,person: family2.family_members[0].person)}
     let!(:hbx_profile) {FactoryGirl.create(:hbx_profile,:open_enrollment_coverage_period)}
     before do
       coverage_household_member = family1.households.first.coverage_households.first.coverage_household_members.first
@@ -36,18 +37,21 @@ describe MoveEnrollmentBetweenTwoAccount do
 
       allow(ENV).to receive(:[]).with('old_account_hbx_id').and_return family1.family_members[0].person.hbx_id
       allow(ENV).to receive(:[]).with('new_account_hbx_id').and_return family2.family_members[0].person.hbx_id
+      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return hbx_enrollment.hbx_id
     end
     it "should move an ivl enrollment" do
       expect(family1.active_household.hbx_enrollments).to include(hbx_enrollment)
       expect(family2.active_household.hbx_enrollments).not_to include(hbx_enrollment)
-      @size=family2.active_household.hbx_enrollments.size
+      @size1=family1.active_household.hbx_enrollments.size
+      @size2=family2.active_household.hbx_enrollments.size
       subject.migrate
       family1.reload
       family2.reload
       expect(family1.active_household.hbx_enrollments).not_to include(hbx_enrollment)
-      expect(family2.active_household.hbx_enrollments.last.kind).to eq hbx_enrollment.kind
+      expect(family2.active_household.hbx_enrollments.last.kind).to eq "individual"
       expect(family2.active_household.hbx_enrollments.last.consumer_role).to eq hbx_enrollment.consumer_role
-      expect(family2.active_household.hbx_enrollments.size).to eq @size+1
+      expect(family1.active_household.hbx_enrollments.size).to eq @size1-1
+      expect(family2.active_household.hbx_enrollments.size).to eq @size2+1
     end
   end
   describe "it should move a shop enrollment" do
@@ -61,7 +65,8 @@ describe MoveEnrollmentBetweenTwoAccount do
                          submitted_at: TimeKeeper.date_of_record,
                          employee_role: employee_role,
                          aasm_state: 'shopping',
-                         benefit_group_assignment: benefit_group_assignment)
+                         benefit_group_assignment: benefit_group_assignment),
+                         benefit_group_id: benefit_group_assignment.benefit_group.id
     }
     let(:employee_role) {FactoryGirl.create(:employee_role,person: family1.family_members[0].person, census_employee:census_employee, employer_profile: benefit_group_assignment.benefit_group.plan_year.employer_profile)}
     let!(:employee_role2) {FactoryGirl.create(:employee_role,person: family2.family_members[0].person, census_employee:census_employee, employer_profile: benefit_group_assignment.benefit_group.plan_year.employer_profile)}
@@ -69,6 +74,7 @@ describe MoveEnrollmentBetweenTwoAccount do
     let(:benefit_group_assignment) {FactoryGirl.create(:benefit_group_assignment,census_employee:census_employee)}
     before do
       coverage_household_member = family1.households.first.coverage_households.first.coverage_household_members.first
+      census_employee.update_attributes(employer_profile_id: benefit_group_assignment.plan_year.employer_profile.id)
       h = HbxEnrollmentMember.new_from(coverage_household_member: coverage_household_member)
       h.eligibility_date = TimeKeeper.date_of_record
       h.coverage_start_on = TimeKeeper.date_of_record
@@ -78,18 +84,21 @@ describe MoveEnrollmentBetweenTwoAccount do
 
       allow(ENV).to receive(:[]).with('old_account_hbx_id').and_return family1.family_members[0].person.hbx_id
       allow(ENV).to receive(:[]).with('new_account_hbx_id').and_return family2.family_members[0].person.hbx_id
+      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return hbx_enrollment.hbx_id
     end
     it "should move an ivl enrollment" do
       expect(family1.active_household.hbx_enrollments).to include(hbx_enrollment)
       expect(family2.active_household.hbx_enrollments).not_to include(hbx_enrollment)
-      @size=family2.active_household.hbx_enrollments.size
+      @size1=family1.active_household.hbx_enrollments.size
+      @size2=family2.active_household.hbx_enrollments.size
       subject.migrate
       family1.reload
       family2.reload
       expect(family1.active_household.hbx_enrollments).not_to include(hbx_enrollment)
-      expect(family2.active_household.hbx_enrollments.last.kind).to eq hbx_enrollment.kind
-      expect(family2.active_household.hbx_enrollments.last.employee_role).to eq hbx_enrollment.employee_role
-      expect(family2.active_household.hbx_enrollments.size).to eq @size+1
+      expect(family2.active_household.hbx_enrollments.last.kind).to eq "individual"
+      expect(family2.active_household.hbx_enrollments.last.consumer_role).to eq hbx_enrollment.consumer_role
+      expect(family1.active_household.hbx_enrollments.size).to eq @size1-1
+      expect(family2.active_household.hbx_enrollments.size).to eq @size2+1
     end
   end
  end
