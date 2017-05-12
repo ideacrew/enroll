6fbe2d7e3f8ad06812a45d80bc0bebb204a2cc06
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-05-12 14:20:13 -0400

    Refs #15674 completed specs for pundit authorizartion for add_sep functionality

--- app/data_migrations/define_permissions.rb
+++ app/data_migrations/define_permissions.rb
@@ -8,7 +8,7 @@ class DefinePermissions < MigrationTask
     Permission.where(name: /^hbx/).delete_all
   	Permission.create(name: 'hbx_staff', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
   	  send_broker_agency_message: true, approve_broker: true, approve_ga: true,
-  	  modify_admin_tabs: true, view_admin_tabs: true, can_add_sep: true)
+  	  modify_admin_tabs: true, view_admin_tabs: true, can_complete_resident_application: true, can_add_sep: true)
     Permission.create(name: 'hbx_read_only', modify_family: true, modify_employer: false, revert_application: false, list_enrollments: true,
   	  send_broker_agency_message: false, approve_broker: false, approve_ga: false,
   	  modify_admin_tabs: false, view_admin_tabs: true, can_add_sep: true)
