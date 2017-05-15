2984f3f2b07264642f95f806cfd140ee5203ba1f
sarathpyerra <sarathpyerra@gmail.com>
2017-05-15 14:06:18 -0400

    Refs #14956 adding specs

--- app/data_migrations/delink_broker.rb
+++ app/data_migrations/delink_broker.rb
@@ -1,15 +1,17 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 class DelinkBroker < MongoidMigrationTask
   def migrate
-    person=Person.where(first_name:"Carolyn",last_name:"Robbins").first
+    first_name = ENV['first_name']
+    last_name = ENV['last_name']
+    legal_name = ENV['legal_name']
+    person=Person.where(first_name: first_name,last_name: last_name).first
     if person.broker_role
-      organization = FactoryGirl.create(:broker_agency_profile)
-      bpa_id = organization.id
-      person.broker_role.update_attributes(broker_agency_profile_id: bpa_id)
+      broker_agency = FactoryGirl.create(:broker_agency_profile,:organization => {:legal_name => legal_name, :fein => "99999322"})
+      #update fields
+      person.broker_role.update_attributes(broker_agency_profile_id: broker_agency.id)
       person.broker_role.save
     else
       puts "Error for Person id: #{person.first_name}"
-
     end
   end
 end
--- lib/tasks/migrations/delink_broker.rake
+++ lib/tasks/migrations/delink_broker.rake
@@ -1,7 +1,9 @@
 require File.join(Rails.root, "app", "data_migrations", "delink_broker")
-# This rake task is to add employee role
-# RAILS_ENV=production bundle exec rake migrations:adding_employee_role census_employee_id=5835bff6082e7645b70000de person_id=53e69677eb899ad9ca02cbfd
+# This rake task is to delink broker
+# RAILS_ENV=production bundle exec rake migrations:delink_broker first_name=Carolyn last_name=Robbins legal_name=Insurance Associates
 namespace :migrations do
   desc "Delinking broker"
-  DelinkBroker.define_task :delink_broker_agency_profile_id => :environment
+  task :delink_broker => :environment do 
+    DelinkBroker.migrate()
+  end
 end 
