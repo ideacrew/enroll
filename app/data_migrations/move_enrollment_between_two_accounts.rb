7341dede6fc30424d544c2b61ca496b3453cc954
nisanth <nisanth.yaganti@gmail.com>
2017-05-15 16:52:36 -0400

    refs #13363 moving enrollment

--- app/data_migrations/move_enrollment_between_two_accounts.rb
+++ app/data_migrations/move_enrollment_between_two_accounts.rb
@@ -1,77 +1,77 @@
 require File.join(Rails.root,"lib/mongoid_migration_task")
 class MoveEnrollmentBetweenTwoAccount < MongoidMigrationTask
   def migrate
-    #find two person accounts and all the hbx_enrollments
     new_account_hbx_id = ENV['new_account_hbx_id']
     old_account_hbx_id = ENV['old_account_hbx_id']
-    hbx_enrollment_hbx_id = ENV['enrollment_hbx_id']
+    enr_id = ENV['enrollment_hbx_id'].to_s
     gp = Person.where(hbx_id:new_account_hbx_id).first
     bp = Person.where(hbx_id:old_account_hbx_id).first
-    if gp.nil?
-      puts "No person record was found with given hbx_id #{new_account_hbx_id}" unless Rails.env.test?
-    elsif bp.nil?
-      puts "No person record was found with given hbx_id #{old_account_hbx_id}" unless Rails.env.test?
-    else
-      #enrollment_hbx_id = HbxEnrollment.by_hbx_id(hbx_enrollment_hbx_id)
-      hbx_enrollment= bp.primary_family.active_household.hbx_enrollments.where(hbx_id:hbx_enrollment_hbx_id).first
-      if hbx_enrollment.nil?
-        puts "No hbx_enrollment was found with given hbx_id #{enrollment_hbx_id}" unless Rails.env.test?
-      elsif !check_family_member_match(bp.primary_family,gp.primary_family)
-        puts "The enrollment with hbx_id #{enrollment_hbx_id} can not be moved due to a mismatch of family members" unless Rails.env.test?
-      else
-        if hbx_enrollment.is_shop?
-          #1 check whether moveable
-          #2create the new hbx_enrollment
-          #3delete the old hbx_enrollment
-          if gp.employee_roles.empty? ||  bp.employee_roles.empty?
-            puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to employee.role" unless Rails.env.test?
-          else
-            good_employer=gp.employee_roles.first.census_employee.employer_profile
-            bad_employer=bp.employee_roles.first.census_employee.employer_profile
-            if good_employer.nil? || bad_employer.nil? || good_employer.id != bad_employer.id
-              puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to two people dont linked to the same employer" unless Rails.env.test?
-            elsif good_employer.active_plan_year.benefit_groups.map{|a| a.id}.include? hbx_enrollment.benefit_group_id
-              #create the new hbx_enrollment
-              gp.primary_family.active_household.create_hbx_enrollment_from(
-                  employee_role: hbx_enrollment.employee_role,
-                  consumer_role: hbx_enrollment.consumer_role,
-                  coverage_household:gp.primary_family.active_household.immediate_family_coverage_household,
-                  benefit_group: hbx_enrollment.benefit_group,
-                  benefit_group_assignment:hbx_enrollment.benefit_group_assignment
-              )
-              bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment.id)
-            else
-              puts "The shop hbx_enrollment #{hbx_enrollment.id} can not moved due to a mismatch of benegit groups" unless Rails.env.test?
-            end
-          end
-        elsif hbx_enrollment.kind == "individual"
-            #1 check whether moveable
-            if gp.consumer_role.present? && bp.consumer_role.present?
-              #2create the new hbx_enrollment
-              #? how to add enrollment member
-              #3delete the old hbx_enrollment
-              gp.primary_family.active_household.create_hbx_enrollment_from(
-                  consumer_role: hbx_enrollment.consumer_role,
-                  employee_role: hbx_enrollment.employee_role,
-                  coverage_household:gp.primary_family.active_household.immediate_family_coverage_household
-              )
-              bp.primary_family.active_household.delete_hbx_enrollment(hbx_enrollment_hbx_id)
-            else
-              puts "the enrollment can not be moved due to at least one of the person has no consumer role" unless Rails.env.test?
-            end
-        else
-          puts "Enrollment #{hbx_enrollment.id} can not be moved due to it is neither ivl or shop" unless Rails.env.test?
-        end
-      end
+    hbx_enrollment = HbxEnrollment.by_hbx_id(enr_id).first
+
+    if gp.nil? || bp.nil?
+      puts "Check Input. Found No person records" unless Rails.env.test?
+      return
+    elsif hbx_enrollment.nil?
+      puts "No Enrollment found with given hbx Id" unless Rails.env.test?
+      return
     end
-  end
-  def check_family_member_match(family_from,family_to)
-    #if family_to’s family members include all family_from’s family members, return true
-    return true if family_from.id == family_to.id
-    family_from_members = family_from.family_members.to_a
-    family_to_members = family_to.family_members.to_a
-    return true if family_from_members == family_to_members
-    return true if (family_to_members-family_from_members).empty? == false
-    return false
+
+    if hbx_enrollment.is_shop?
+      employee_role = gp.employee_roles.detect { |er| er.employer_profile_id == hbx_enrollment.employee_role.employer_profile.id}
+      if employee_role.blank?
+        puts "New person don't have ER sponsored benefits related to Enrollment" unless Rails.env.test?
+        return
+      end
+
+      benefit_group_assignment = employee_role.census_employee.benefit_group_assignments.detect { |bga| bga.benefit_group_id == hbx_enrollment.benefit_group_id}
+      if benefit_group_assignment.blank?
+        puts "No Benefit Group Assignment Found" unless Rails.env.test?
+        return
+      end
+
+      employee_role_id = employee_role.id
+      benefit_group_assignment_id = benefit_group_assignment.id
+      consumer_role_id = nil
+    else
+      consumer_role_id = gp.consumer_role.id
+      employee_role_id = nil
+      consumer_role_id = nil
+    end
+
+
+    enrollment = HbxEnrollment.new( kind: hbx_enrollment.kind, enrollment_kind: hbx_enrollment.enrollment_kind, 
+                                    employee_role_id: employee_role_id, benefit_group_id: hbx_enrollment.benefit_group_id, 
+                                    benefit_group_assignment_id: benefit_group_assignment_id, effective_on: hbx_enrollment.effective_on,
+                                    plan_id: hbx_enrollment.plan_id, aasm_state: hbx_enrollment.aasm_state, hbx_id: hbx_enrollment.hbx_id,
+                                    coverage_kind: hbx_enrollment.coverage_kind, elected_amount: hbx_enrollment.elected_amount, 
+                                    elected_premium_credit: hbx_enrollment.elected_premium_credit, applied_premium_credit: hbx_enrollment.applied_premium_credit,
+                                    elected_aptc_pct: hbx_enrollment.elected_aptc_pct, applied_aptc_amount: hbx_enrollment.applied_aptc_amount,
+                                    changing: hbx_enrollment.changing, terminated_on: hbx_enrollment.terminated_on,
+                                    terminate_reason: hbx_enrollment.terminate_reason, carrier_profile_id: hbx_enrollment.carrier_profile_id,
+                                    special_enrollment_period_id: hbx_enrollment.special_enrollment_period_id, enrollment_signature: hbx_enrollment.enrollment_signature,
+                                    aasm_state_date: hbx_enrollment.aasm_state_date, submitted_at: hbx_enrollment.submitted_at,
+                                    original_application_type: hbx_enrollment.original_application_type, consumer_role_id: consumer_role_id,
+                                    benefit_package_id: hbx_enrollment.benefit_package_id, 
+                                    benefit_coverage_period_id: hbx_enrollment.benefit_coverage_period_id, updated_by: hbx_enrollment.updated_by,
+                                    is_active: hbx_enrollment.is_active, waiver_reason: hbx_enrollment.waiver_reason,
+                                    published_to_bus_at: hbx_enrollment.published_to_bus_at, review_status: hbx_enrollment.review_status,
+                                    special_verification_period: hbx_enrollment.special_verification_period, termination_submitted_on: hbx_enrollment.termination_submitted_on,
+                                    external_enrollment: hbx_enrollment.external_enrollment, is_tranding_partner_transmittable: hbx_enrollment.is_tranding_partner_transmittable
+                                  )
+
+    family_members = gp.primary_family.active_family_members.select { |fm| Family::ImmediateFamily.include? fm.primary_relationship }
+    family_members.each do |fm|
+      hem = HbxEnrollmentMember.new(applicant_id: fm.id, is_subscriber: fm.is_primary_applicant, 
+                                    eligibility_date: hbx_enrollment.effective_on, coverage_start_on: hbx_enrollment.effective_on
+                                   )
+      enrollment.hbx_enrollment_members << hem
+    end
+
+
+    hbx_enrollment.destroy!
+    gp.primary_family.active_household.hbx_enrollments << enrollment
+    gp.primary_family.active_household.save!
+    puts "Hbx Enrollment succesfully moved!!"
+    puts "Confirm the people under covered section on the enrollment!!"
   end
 end
