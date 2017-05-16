d46eb86782ff2b7a2ab6dc250f914f893f5aef24
harsha chalamala <chvreddy1015@gmail.com>
2017-05-16 11:55:08 -0400

    specs and fixes

--- app/models/scheduled_event.rb
+++ app/models/scheduled_event.rb
@@ -16,6 +16,7 @@ class ScheduledEvent
   validates_presence_of :type, :event_name, :one_time, :start_time, :offset_rule
 >>>>>>> minor issues
 
+<<<<<<< HEAD
   validates_presence_of :type, :event_name, :one_time, :start_time, :message => "fields type, event_name can't be empty"
 
   EVENT_TYPES = %W(system holiday)
@@ -24,6 +25,11 @@ class ScheduledEvent
   SYSTEM_EVENTS = %W(binder_payment_due_date publish_due_date_of_month ivl_monthly_open_enrollment_due_on shop_initial_application_publish_due_day_of_month shop_renewal_application_monthly_open_enrollment_end_on
                        shop_renewal_application_publish_due_day_of_month shop_renewal_application_force_publish_day_of_month shop_open_enrollment_monthly_end_on shop_group_file_new_enrollment_transmit_on
                        shop_group_file_update_transmit_day_of_week)
+=======
+  EVENT_TYPES = %W(holiday system_event)
+  HOLIDAYS = %W(New_Year MartinLuthor_bday washingtons_day memorial_day independence_day Labour_day columbus_day veterans_day Christmas Thanksgiving_day)
+  SYSTEMS_EVENTS = %W(Binder_Payment_due_Date Publish_Due_Date_Of_Month )
+>>>>>>> specs and fixes
 
   def recurring_rules=(value)
     if RecurringSelect.is_valid_rule?(value)
--- spec/factories/scheduled_event.rb
+++ spec/factories/scheduled_event.rb
@@ -2,7 +2,11 @@ FactoryGirl.define do
   factory :scheduled_event do
   	type 'holiday'
   	event_name 'Christmas'
+<<<<<<< HEAD
   	start_time { Date.new(2017, 1, 1) }
+=======
+  	start_time {Date.today}
+>>>>>>> specs and fixes
   	one_time true
   	recurring_rules nil
   	offset_rule '3'
