1e5c4c949612715a08c606225cc856b975913b21
harsha chalamala <chvreddy1015@gmail.com>
2017-05-16 17:37:29 -0400

    model specs

--- spec/factories/scheduled_event.rb
+++ spec/factories/scheduled_event.rb
@@ -10,8 +10,5 @@ FactoryGirl.define do
   	one_time true
   	recurring_rules nil
   	offset_rule '3'
-  	trait :recurring_rule do
-      recurring_rules {"validations"=>{"day_of_month"=>[27]}, "rule_type"=>"IceCube::MonthlyRule", "interval"=>1}
-    end
   end
 end
