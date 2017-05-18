417df2cba7c2a54d40a8003cf5e9875944780b9a
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-05-18 14:41:51 -0400

    Refs #13138 dont print output in tests

--- app/data_migrations/update_ce_coverage_terminated_on.rb
+++ app/data_migrations/update_ce_coverage_terminated_on.rb
@@ -7,11 +7,11 @@ class UpdateCeCoverageTerminatedOn < MongoidMigrationTask
       ce = CensusEmployee.where(id: ENV['ce_id'].to_s).first
       new_terminated_on = Date.strptime(ENV['new_coverage_termination_date'].to_s, "%m/%d/%Y")
       if ce.nil?
-        puts "No census employee was found the given id" #unless Rails.env.test?
+        puts "No census employee was found the given id" unless Rails.env.test?
         return
       end
       ce.update_attributes(coverage_terminated_on:new_terminated_on)
-      puts "Changed employment terminated date to #{new_terminated_on}" #unless Rails.env.test?
+      puts "Changed employment terminated date to #{new_terminated_on}" unless Rails.env.test?
     rescue => e
       puts "Error for Census Employee with id: #{ce}. Error: #{e.message}"
     end
--- app/data_migrations/update_ce_employment_terminated_on.rb
+++ app/data_migrations/update_ce_employment_terminated_on.rb
@@ -7,13 +7,13 @@ class UpdateCeEmploymentTerminatedOn < MongoidMigrationTask
       ce = CensusEmployee.where(id: ENV['ce_id'].to_s).first
       new_terminated_on = Date.strptime(ENV['new_employment_termination_date'].to_s, "%m/%d/%Y")
       if ce.nil?
-        puts "No census employee was found the given id" #unless Rails.env.test?
+        puts "No census employee was found the given id" unless Rails.env.test?
         return
       end
       ce.update_attributes(aasm_state:"employment_terminated")
       ce.update_attributes(employment_terminated_on:new_terminated_on)
       ce.errors
-      puts "Changed employment terminated date to #{new_terminated_on}" #unless Rails.env.test?
+      puts "Changed employment terminated date to #{new_terminated_on}" unless Rails.env.test?
     rescue => e
       puts "Error for Census Employee with id: #{ce}. Error: #{e.message}"
     end
