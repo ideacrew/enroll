ffc02bb5b1458ecc2bda60bfb21bba36790d3ea5
Mahesh Kurma <maheshruby405@gmail.com>
2017-05-19 15:03:51 -0400

    refs #15092

--- app/data_migrations/general_agency_assignment.rb
+++ app/data_migrations/general_agency_assignment.rb
@@ -8,12 +8,12 @@ class GeneralAgencyAssignment < MongoidMigrationTask
       broker_agency_profile = BrokerAgencyProfile.find(ENV['broker_agency_id'])
       open_enrollment_end_on = Date.strptime(ENV['open_enrollment_end_on'].to_s, "%m/%d/%Y")
 
-      employer_profile.plan_years.first.update_attributes({open_enrollment_end_on: open_enrollment_end_on})
+      employer_profile.plan_years.first.update_attributes!({open_enrollment_end_on: open_enrollment_end_on})
 
       broker_agency_profile_id = broker_agency_profile.primary_broker_role_id
       employer_profile.general_agency_accounts.build(general_agency_profile: general_agency_profile, start_on: TimeKeeper.datetime_of_record, broker_role_id: broker_agency_profile_id)
 
-      employer_profile.save
+      employer_profile.save!
 
       puts "GeneralAgencyProfile with id: #{ENV['general_agency_id']} is assigned to the given EmployerProfile with id: #{ENV['employer_profile_id']}" unless Rails.env.test?
     rescue Exception => e
