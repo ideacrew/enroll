92c65426b2dc7cca1eafc82d4d53a7bd0d2a5730
sarathpyerra <sarathpyerra@gmail.com>
2017-05-23 15:11:59 -0400

    Refs #15809 passing hbx_id

--- app/data_migrations/change_suffix_person.rb
+++ app/data_migrations/change_suffix_person.rb
@@ -4,25 +4,14 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class ChangeSuffixPerson < MongoidMigrationTask
   def migrate
     puts "updating person suffix" unless Rails.env.test?
-    hbx_id = [
-      "19760877", "19776645", "19747772", "19758664", "19776417",
-       "18941825", "19766429", "19761376", "19753221", "19762647",
-        "19775694", "19757825", "19749172", "19772583", "19771579",
-         "19745475", "19744827", "19761611", "19763400", "19773230",
-          "19743457", "2085463", "19753992", "2166772", "19771972",
-           "19756452", "19771773", "19759229", "19753432", "19760652",
-            "18942772", "19759405", "19771826", "19743273"
-          ]
-    hbx_id.each do |hbx|
-      person = Person.where(hbx_id: hbx).first
+    hbx_ids=ENV['hbx_ids'].split(' ').uniq
+    hbx_ids.each do |hbx_id|
+      person = Person.where(hbx_id: hbx_id).first
       if person.present?
         person.update_attributes(name_sfx: "")
         person.save
       else
-        puts "Person not found for hbx_id #{hbx}"
-      end
-      unless Rails.env.test?
-        puts "Updated person Suffix"
+        puts "Person not found for hbx_id #{hbx_ids}"
       end
     end
   end
--- spec/data_migrations/change_suffix_person_spec.rb
+++ spec/data_migrations/change_suffix_person_spec.rb
@@ -11,8 +11,9 @@ describe ChangeSuffixPerson do
   end
   describe "changing person's name suffix " do
     let(:person) { FactoryGirl.create(:person, name_sfx: nil)}
+    let(:hbx_id) { person.hbx_id}
     before(:each) do
-      allow(ENV).to receive(:[]).with("name_sfx").and_return(nil)
+      allow(ENV).to receive(:[]).with("hbx_ids").and_return(hbx_id)
     end
 
     it "should change person suffix name" do
@@ -20,7 +21,7 @@ describe ChangeSuffixPerson do
       expect(person.name_sfx).to eq name_sfx
       subject.migrate
       person.reload
-      expect(person.name_sfx).to eq nil
+      expect(person.name_sfx).to eq name_sfx
     end
   end
 end
