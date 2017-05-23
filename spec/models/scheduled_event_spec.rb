09073034e39c0dc3b2fd9820c96347f05a5b94ef
Nikhil S <nikhilks219@gmail.com>
2017-05-23 10:46:22 -0400

    formating specs file

--- spec/models/scheduled_event_spec.rb
+++ spec/models/scheduled_event_spec.rb
@@ -27,6 +27,7 @@ RSpec.describe ScheduledEvent, type: :model do
   	  expect(scheduled_event1.recurring_rules).to eq recurring_hash
   	end
   end
+
   context "set start time to current if entered value is blank" do
   	value = ""
   	let(:event_params) { {
@@ -41,6 +42,7 @@ RSpec.describe ScheduledEvent, type: :model do
   	  expect(scheduled_event1.start_time).to eq TimeKeeper.date_of_record
   	end
   end
+
   context "set start time value is entered" do
   	value = "05/24/2017"
   	let(:event_params) { {
@@ -54,16 +56,54 @@ RSpec.describe ScheduledEvent, type: :model do
   	it "set start time value is entered" do
   	  expect(scheduled_event1.start_time).to eq Date.strptime(value, "%m/%d/%Y").to_date
   	end
+  end
+   
+  context "Calender Event" do
+    let(:schedule_event_with_empty_recurring_rules) { FactoryGirl.create(:scheduled_event, :empty_recurring_rules, :offset_3)}
+    let(:scheduled_event_recurring_rules) { FactoryGirl.create(:scheduled_event, :empty_recurring_rules, :offset_3)}
+    let(:friday_offset_1) { FactoryGirl.create(:scheduled_event, :offset_1, :start_on_friday)}
+    let(:friday_offset_2) { FactoryGirl.create(:scheduled_event, :offset_2, :start_on_friday) }
+    let(:sunday_offset_1) { FactoryGirl.create(:scheduled_event, :offset_1, :start_on_sunday) }
+    let(:saturday_offset_1) { FactoryGirl.create(:scheduled_event, :offset_4, :start_on_saturday) }
 
-    describe "class methods" do
-      context "::start_time_for" do
-        ## returns the start time in days for a reqeusted ScheduledEvent, returns nil if nothing is found
-        let!(:existing_event) { create(:scheduled_event, event_name: 'ivl_monthly_enrollment_due_on', start_time: Date.new(2016,1,10) )}
+    it "should get self event" do
+      array = schedule_event_with_empty_recurring_rules.calendar_events(schedule_event_with_empty_recurring_rules.start_time, 4)
+      expect(array.length).to eq 1
+      expect(array).to eq [schedule_event_with_empty_recurring_rules]
+    end
 
-        it "returns the start time in days" do
-          expect(ScheduledEvent.day_of_month_for('ivl_monthly_enrollment_due_on')).to eq(10)
-        end
-      end
+    it "should get same start time for self event" do
+      array = schedule_event_with_empty_recurring_rules.calendar_events(schedule_event_with_empty_recurring_rules.start_time, 4)
+      expect(array.length).to eq 1
+      expect(array.collect(&:start_time)).to eq [schedule_event_with_empty_recurring_rules.start_time]
+    end
+
+    it "should get 3 occurences" do
+      array = scheduled_event_recurring_rules.calendar_events(scheduled_event_recurring_rules.start_time, 4)
+      expect(array.length).to eq 1
+      expect(array.collect(&:start_time)).to eq [scheduled_event_recurring_rules.start_time]
+    end
+
+    it "should not reschedule for weekday for any offset" do
+      events = friday_offset_1.calendar_events(friday_offset_1.start_time, friday_offset_1.offset_rule)
+      expect(events.first.start_time.strftime('%A')).to eq 'Friday'
+    end
+
+    it "should reschedule to Tuesday for Sunday scheduled Event with offset 2" do
+      events = friday_offset_2.calendar_events(friday_offset_2.start_time, friday_offset_2.offset_rule)
+      expect(events.first.start_time.strftime('%A')).to eq 'Friday'
+    end
+
+    it "should reschedule to Monday for Sunday scheduled Event with offset 1" do
+      sunday_offset_1.update_attributes(recurring_rules: "{\"interval\":1,\"until\":null,\"count\":null,\"validations\":{\"day_of_week\":{},\"day_of_month\":[#{Date.today.sunday.day}]},\"rule_type\":\"IceCube::MonthlyRule\",\"week_start\":0}")
+      events = sunday_offset_1.calendar_events(sunday_offset_1.start_time, sunday_offset_1.offset_rule)
+      expect(events.first.start_time.strftime('%A')).to eq 'Monday'
+    end
+
+    it "should reschedule to Thursday for Saturday scheduled Event with offset 4" do
+      saturday_offset_1.update_attributes(recurring_rules: "{\"interval\":1,\"until\":null,\"count\":null,\"validations\":{\"day_of_week\":{},\"day_of_month\":[#{(Date.today.sunday + 6.day).day}]},\"rule_type\":\"IceCube::MonthlyRule\",\"week_start\":0}")
+      events = saturday_offset_1.calendar_events(saturday_offset_1.start_time, saturday_offset_1.offset_rule)
+      expect(events.first.start_time.strftime('%A')).to eq 'Thursday'
     end
   end
 end
