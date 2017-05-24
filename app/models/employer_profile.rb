2c2e9a074bdfc4250ff5f95eda25c56111445677
Battula <vrbattula@outlook.com>
2017-05-24 16:26:04 -0400

    refs #15201 typo

--- app/models/employer_profile.rb
+++ app/models/employer_profile.rb
@@ -490,8 +490,8 @@ class EmployerProfile
     def organizations_for_low_enrollment_notice(new_date)
       Organization.where(:"employer_profile.plan_years" =>
         { :$elemMatch => {
-          :aasm_state.in => ["enrolling", "renewing_enrolling"],
-          :open_enrollment_end_on => new_date+2.days
+          :"aasm_state".in => ["enrolling", "renewing_enrolling"],
+          :"open_enrollment_end_on" => new_date+2.days
           }
       })
 
@@ -606,7 +606,7 @@ class EmployerProfile
           #exclude congressional employees
           next if ((plan_year.benefit_groups.any?{|bg| bg.is_congress?}) || (plan_year.effective_date.yday == 1))
           if plan_year.enrollment_ratio < Settings.aca.shop_market.employee_participation_ratio_minimum
-            trigger_notices("low_enrollment_notice_for_employer")
+            organization.employer_profile.trigger_notices("low_enrollment_notice_for_employer")
           end
         rescue Exception => e
           puts "Unable to deliver Low Enrollment Notice to #{organization.legal_name} due to #{e}"
