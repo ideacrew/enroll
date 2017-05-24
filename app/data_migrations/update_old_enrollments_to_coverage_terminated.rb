dcbc62b4f6391b5de17a8019b2138a755bbc85e7
202chaz <chaz.haskins@gmail.com>
2017-05-24 07:30:34 -0400

    Updated rake task to fix plan id

--- app/data_migrations/update_old_enrollments_to_coverage_terminated.rb
+++ app/data_migrations/update_old_enrollments_to_coverage_terminated.rb
@@ -6,7 +6,7 @@ class UpdateOldEnrollmentsToCoverageTerminated < MongoidMigrationTask
     enrollment2 = HbxEnrollment.by_hbx_id(ENV['hbx_id_2'].to_s)
     bad_enrollment = HbxEnrollment.by_hbx_id(ENV['hbx_id_3'].to_s)
     bad_enrollment.first.update(hbx_id:enrollment2.first.hbx_id)
-    fix_plan_id = enrollment.first.update(plan_id:enrollment2.first.plan_id)
+    enrollment.first.update(plan_id:enrollment2.first.plan_id)
     enrollment.first.terminate_coverage!
     enrollment2.first.cancel_coverage!
     puts "Changed Enrollment states" unless Rails.env.test?
