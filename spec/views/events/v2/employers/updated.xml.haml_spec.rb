6816301f7e9e506897b138e25ba339a8baff0466
saimekala07 <saimekala07@gmail.com>
2017-05-25 16:09:24 -0400

    refs #16060 updating spec plan year to eligible to export state

--- spec/views/events/v2/employers/updated.xml.haml_spec.rb
+++ spec/views/events/v2/employers/updated.xml.haml_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe "events/v2/employer/updated.haml.erb" do
     let(:benefit_group)     { FactoryGirl.build(:benefit_group)}
     let(:benefit_group2)     { FactoryGirl.build(:benefit_group)}
     let(:plan_year)         { FactoryGirl.build(:plan_year, start_on:TimeKeeper.date_of_record.beginning_of_month+ 2.month - 1.year, end_on:TimeKeeper.date_of_record.end_of_month + 1.months, benefit_groups: [benefit_group], aasm_state:'active',created_at:TimeKeeper.date_of_record)}
-    let(:future_plan_year)         { FactoryGirl.build(:plan_year, start_on:TimeKeeper.date_of_record.beginning_of_month + 2.months, end_on:TimeKeeper.date_of_record.end_of_month + 1.month, benefit_groups: [benefit_group2], aasm_state:'renewing_enrolling')}
+    let(:future_plan_year)         { FactoryGirl.build(:plan_year, start_on:TimeKeeper.date_of_record.beginning_of_month + 2.months, end_on:TimeKeeper.date_of_record.end_of_month + 1.month, benefit_groups: [benefit_group2], aasm_state:'renewing_enrolled')}
     let!(:employer)  { EmployerProfile.new(**valid_params, plan_years: [plan_year,future_plan_year]) }
 
     let(:staff) { FactoryGirl.create(:person, :with_work_email, :with_work_phone)}
