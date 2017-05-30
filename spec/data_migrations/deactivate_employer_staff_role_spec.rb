f3eeb46963f6aedeeab0130abf727c39c589eb45
saimekala07 <saimekala07@gmail.com>
2017-05-30 13:49:09 -0400

    refs #16113 Adding spec for migrating employer staff role active to is_closed.

--- spec/data_migrations/deactivate_employer_staff_role_spec.rb
+++ spec/data_migrations/deactivate_employer_staff_role_spec.rb
@@ -11,6 +11,25 @@ describe DeactivateEmployerStaffRole do
     end
   end
 
- 
+  describe "deactivate employer staff role" do
+    let(:employer_profile) { FactoryGirl.build(:employer_profile)}
+
+    let(:employer_staff_role) {FactoryGirl.build(:employer_staff_role, aasm_state:'is_active', employer_profile_id: employer_profile.id)}
+    let(:person) { FactoryGirl.create(:person,employer_staff_roles:[employer_staff_role])}
+    let(:organization) { FactoryGirl.create(:organization, employer_profile:employer_profile)}
+    let(:staff_role) { Person.by_hbx_id(person.hbx_id).first.employer_staff_roles.detect{|role| role.employer_profile_id.to_s == employer_profile.id.to_s} }
+
+    before(:each) do
+      allow(ENV).to receive(:[]).with("fein").and_return(organization.fein)
+      allow(ENV).to receive(:[]).with("hbx_id").and_return(person.hbx_id)
+    end
+
+    it "should update aasm state of staff role" do
+      expect(staff_role.aasm_state).to eq "is_active"
+      subject.migrate
+      staff_role.reload
+      expect(staff_role.aasm_state).to eq "is_closed"
+    end
+  end
 
 end
