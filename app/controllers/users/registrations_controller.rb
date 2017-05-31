cc08aa9cdaaa2e1ac8a5dc44da49f8e1162403a6
ymhari <hariym471@gmail.com>
2017-05-31 13:25:02 -0400

    Implementing Captcha for Signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+  prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
+  before_filter :configure_sign_up_params, only: [:create]
+  # before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -16,7 +17,7 @@ before_filter :configure_sign_up_params, only: [:create]
       render :new and return
     end
 
-    if resource.email.strip.present? && CuramUser.match_unique_login(resource.email.strip).first.present? 
+    if resource.email.strip.present? && CuramUser.match_unique_login(resource.email.strip).first.present?
       flash[:alert] = "An account with this email ( #{params[:user][:email]} ) already exists. #{view_context.link_to('Click here', SamlInformation.account_recovery_url)} if you've forgotten your password."
       render :new and return
     end
@@ -26,7 +27,7 @@ before_filter :configure_sign_up_params, only: [:create]
     if headless.present? && !headless.person.present?
       headless.destroy
     end
-    
+
     resource.email = resource.oim_id if resource.email.blank? && resource.oim_id =~ Devise.email_regexp
 
     headless = User.where(oim_id: /^#{Regexp.quote(resource.oim_id)}$/i).first
@@ -109,4 +110,13 @@ before_filter :configure_sign_up_params, only: [:create]
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+  def check_captcha
+    unless verify_recaptcha
+      self.resource = resource_class.new sign_up_params
+      respond_with_navigational(resource) { render :new }
+    end
+  end
+
 end
