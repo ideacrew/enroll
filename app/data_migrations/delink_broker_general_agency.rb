d03799854d6b7902941cd05c4aad1d38b36da51f
Kevin Wei <kevin.tw.wei@gmail.com>
2017-05-31 16:40:34 -0400

    Refs #16128 mute output during specs

--- app/data_migrations/delink_broker_general_agency.rb
+++ app/data_migrations/delink_broker_general_agency.rb
@@ -1,7 +1,7 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 class DelinkBrokerGeneralAgency < MongoidMigrationTask
   def migrate
-    
+
     ga_org = Organization.where(fein: "521698168").first
     ga_org.update_attributes!(:legal_name => "Insurance Marketing Center", :dba => "Insurance Marketing Center")
     # br_id = ga_org.broker_agency_profile.id
@@ -19,7 +19,7 @@ class DelinkBrokerGeneralAgency < MongoidMigrationTask
     organization = Organization.new(:legal_name => "Alvin Frager", :fein => "521376450", :dba => "Alvin Frager Insurance", office_locations: [office_location])
 
     broker_agency_profile = BrokerAgencyProfile.new(:id => BSON::ObjectId('57961bf1f1244e3ece000064'), :organization => organization,entity_kind: "s_corporation", market_kind: "both", corporate_npn: nil, primary_broker_role_id: BSON::ObjectId('57961bf1f1244e3ece000062'), default_general_agency_profile_id: BSON::ObjectId('57589d8bf1244e54170000ab'), languages_spoken: ["", "en"], working_hours: true, accept_new_clients: true, aasm_state: "is_approved")
-    puts "created new broker agency profile and delinked GA & BA."
+    puts "created new broker agency profile and delinked GA & BA." unless Rails.env.test?
     organization.save!
   end
 end
