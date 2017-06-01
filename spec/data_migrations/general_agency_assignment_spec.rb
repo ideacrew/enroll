26d5178428aded464f63a167d6c466012f4aac8a
Kevin Wei <kevin.tw.wei@gmail.com>
2017-06-01 11:30:03 -0400

    Refs #15092 fix spec remove hard coded date.

--- spec/data_migrations/general_agency_assignment_spec.rb
+++ spec/data_migrations/general_agency_assignment_spec.rb
@@ -19,12 +19,13 @@ describe GeneralAgencyAssignment do
     let(:general_agency_account) { FactoryGirl.create(:general_agency_account) }
     let(:broker_agency_account) { BrokerAgencyAccount.new(broker_agency_profile_id: broker_agency_profile.id, start_on: TimeKeeper.date_of_record, is_active: true)}
     let(:employer_profile){ FactoryGirl.create(:employer_profile, plan_years: [new_plan_year], broker_agency_accounts: [broker_agency_account]) }
+    let(:end_on) { TimeKeeper.date_of_record.last_month + 7.days }
 
     before(:each) do
       allow(ENV).to receive(:[]).with("general_agency_id").and_return(general_agency_account.general_agency_profile_id.to_s)
       allow(ENV).to receive(:[]).with("employer_profile_id").and_return(employer_profile.id.to_s)
       allow(ENV).to receive(:[]).with("broker_agency_id").and_return(broker_agency_profile.id.to_s)
-      allow(ENV).to receive(:[]).with("open_enrollment_end_on").and_return("04/08/2017")
+      allow(ENV).to receive(:[]).with("open_enrollment_end_on").and_return(end_on.to_s)
       allow(ENV).to receive(:[]).with("aasm_state").and_return("active")
     end
 
