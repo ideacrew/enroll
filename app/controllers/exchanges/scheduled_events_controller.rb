be3c775651493c492a99a633bbfb17a731ebdaa5
Brian Weiner <brian.weiner@dc.gov>
2017-06-02 12:31:33 -0400

    use setters and private helpers

--- app/controllers/exchanges/scheduled_events_controller.rb
+++ app/controllers/exchanges/scheduled_events_controller.rb
@@ -1,8 +1,5 @@
 class Exchanges::ScheduledEventsController < ApplicationController
-layout 'single_column'
-before_action :set_event, only: [:show, :edit, :update, :destroy]
-before_action :get_events, only: [:index, :list]
- 
+
   def new
   	@scheduled_event = ScheduledEvent.new
   end
@@ -12,12 +9,13 @@ before_action :get_events, only: [:index, :list]
 
   def create
     params.permit!
-    @scheduled_event = ScheduledEvent.new(scheduled_event_params)
-    if @scheduled_event.save
-      @scheduled_event.update_attributes!(one_time: false) if @scheduled_event.recurring_rules.present?
+    scheduled_event = ScheduledEvent.new(scheduled_event_params)
+    if scheduled_event.save
+      scheduled_event.update_attributes!(one_time: false) if scheduled_event.recurring_rules.present?
       redirect_to exchanges_scheduled_events_path
     else
-      flash[:error] = @scheduled_event.errors.values.flatten.to_sentence
+      flash[:error] = scheduled_event.errors.values.flatten.to_sentence
+      @scheduled_event = scheduled_event
       render action: :new
     end
   end
@@ -35,19 +33,19 @@ before_action :get_events, only: [:index, :list]
 
   def update
     params.permit!
-    if @scheduled_event.update_attributes!(scheduled_event_params)
-      @scheduled_event.event_exceptions.delete_all
-      if @scheduled_event.recurring_rules.present?
-        @scheduled_event.update_attributes!(one_time: false)
+    if scheduled_event.update_attributes!(scheduled_event_params)
+      scheduled_event.event_exceptions.delete_all
+      if scheduled_event.recurring_rules.present?
+        scheduled_event.update_attributes!(one_time: false)
       else
-        @scheduled_event.update_attributes!(one_time: true)
+        scheduled_event.update_attributes!(one_time: true)
       end
       redirect_to exchanges_scheduled_events_path
     end
   end
 
   def index
-    @calendar_events = @scheduled_events.flat_map do |e|
+    @calendar_events = scheduled_events.flat_map do |e|
       if params.key?("start_date")
         e.calendar_events(Date.strptime(params.fetch(:start_date, TimeKeeper.date_of_record ), "%m/%d/%Y").to_date)
       else
@@ -57,7 +55,7 @@ before_action :get_events, only: [:index, :list]
   end
 
   def destroy
-    @scheduled_event.destroy
+    scheduled_event.destroy
     respond_to do |format|
       format.html { redirect_to exchanges_scheduled_events_path, notice: 'Event was successfully destroyed.' }
     end
@@ -68,12 +66,11 @@ before_action :get_events, only: [:index, :list]
       @events = ScheduledEvent::SYSTEM_EVENTS
     end
     render partial: 'exchanges/scheduled_events/get_events_field', locals: { event: params[:event] }
-    
+
   end
 
   def delete_current_event
-    @scheduled_event = ScheduledEvent.find(params[:id])
-    if @exception = @scheduled_event.event_exceptions.create(time: params[:time])
+    if @exception = scheduled_event.event_exceptions.create(time: params[:time])
       redirect_to exchanges_scheduled_events_path, notice: 'Current Event was successfully destroyed.'
     else
       flash.alert = "Unable to add exception"
@@ -83,15 +80,17 @@ before_action :get_events, only: [:index, :list]
 
   private
 
+    helper_method :scheduled_event, :scheduled_events
+
     def scheduled_event_params
       params.require(:scheduled_event).permit(:type, :event_name, :start_time, :recurring_rules, :one_time, :offset_rule)
     end
-  
-    def set_event
-      @scheduled_event = ScheduledEvent.find(params[:id])
+
+    def scheduled_event
+      @scheduled_event ||= ScheduledEvent.find(params[:id])
     end
 
-    def get_events
-      @scheduled_events = ScheduledEvent.all
+    def scheduled_events
+      @scheduled_events ||= ScheduledEvent.all
     end
 end
