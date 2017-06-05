64bbc9703a162365defaa95129980e11472caadd
vinner <vineelsonnathi@users.noreply.github.com>
2017-06-04 21:32:56 -0400

    Refs #16264 : Only map shop dental plans

--- app/data_migrations/map_dental_plans.rb
+++ app/data_migrations/map_dental_plans.rb
@@ -6,7 +6,7 @@ class MapDentalPlans < MongoidMigrationTask
     previous_year = ENV["previous_year"]
     current_year = ENV["current_year"]
 
-    previous_year_plans = Plan.dental_coverage.where(active_year: previous_year)
+    previous_year_plans = Plan.dental_coverage.shop_market.where(active_year: previous_year)
 
     previous_year_plans.each do |old_plan|
       new_plan = Plan.dental_coverage.where(active_year: current_year, hios_id: old_plan.hios_id).first
