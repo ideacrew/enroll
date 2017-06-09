826514fb05d83b5b91672d8287b2b956c4523b5a
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-06-09 12:31:02 -0400

    Refs #13824 fix output

--- app/data_migrations/cancel_enrollment.rb
+++ app/data_migrations/cancel_enrollment.rb
@@ -5,6 +5,6 @@ class CancelEnrollment < MongoidMigrationTask
   def migrate
     enrollment = HbxEnrollment.by_hbx_id(ENV['hbx_id'].to_s)
     enrollment.first.update_attribute(:aasm_state, "coverage_canceled")
-    puts "Changed Enrollment with hbx_id #{hbx_id} to  coverage_canceled state" unless Rails.env.test?
+    puts "Changed Enrollment with hbx_id #{enrollment.first.hbx_id} to  coverage_canceled state" unless Rails.env.test?
   end
 end
