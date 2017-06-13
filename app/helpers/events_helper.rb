4527f3420b63912f25259ac05c17cd9509cf954f
Kevin Wei <kevin.tw.wei@gmail.com>
2017-06-13 15:25:54 -0400

    Revert "Merge branch 'hotfix-15849' of github.com:dchbx/enroll into stage"
    This reverts commit bcac419dced641dfc42af81cf8b58f854b4be68f, reversing
    changes made to bd03721ad9954f6aa82c449ec238638febc662df.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -69,7 +69,11 @@ module EventsHelper
   end
 
   def employer_plan_years(employer)
-    employer.plan_years.select(&:eligible_for_export?)
+    if (is_renewal_or_conversion_employer?(employer) && TimeKeeper.date_of_record >= ((employer.renewing_published_plan_year.start_on - 1.month)+15.days))  || (is_initial_or_conversion_employer?(employer) && TimeKeeper.date_of_record >= ((employer.published_plan_year.start_on - 1.month)+15.days))
+      employer.plan_years.select(&:eligible_for_export?)
+    elsif is_renewal_employer?(employer) || is_renewing_conversion_employer?(employer)
+      employer.active_plan_year.to_a
+    end
   end
 
   def is_initial_or_conversion_employer?(employer)
