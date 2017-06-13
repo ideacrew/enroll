26cff3efcf26458a68b3075d36d3f6a6d52b73ce
krbhavith <krbhavith@gmail.com>
2017-06-13 16:42:17 -0400

    refs #16177 Updates to Employee Term/Waiver Flows and UI

--- spec/models/hbx_enrollment_spec.rb
+++ spec/models/hbx_enrollment_spec.rb
@@ -672,7 +672,7 @@ describe HbxEnrollment, dbclean: :after_all do
 
     it "should cancel the shop enrollment" do
       enrollment_two.propogate_waiver
-      expect(enrollment_two.aasm_state).to eq "coverage_canceled"
+      expect(enrollment_two.aasm_state).to eq "coverage_termination_pending"
     end
   end
 end
