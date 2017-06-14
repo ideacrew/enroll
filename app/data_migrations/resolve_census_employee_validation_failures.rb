0b3a7d6b9e62016e4135c1cf9f129bb33e6d00bd
Raghuram <raghuramg83@gmail.com>
2017-06-14 15:18:48 -0400

    Refs #16141 Updated logic to resolve benefit group assignment errors

--- app/data_migrations/resolve_census_employee_validation_failures.rb
+++ app/data_migrations/resolve_census_employee_validation_failures.rb
@@ -2,40 +2,9 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class ResolveCensusEmployeeValidationFailures < MongoidMigrationTask
 
-  def create_new_benefit_group_assignment(employee, benefit_group)
-    assignment = employee.benefit_group_assignments.build(benefit_group: benefit_group, start_on: benefit_group.start_on)
-    assignment.save
-
-    if (PlanYear::PUBLISHED + PlanYear::RENEWING_PUBLISHED_STATE).include?(benefit_group.plan_year.aasm_state)
-      assignment.make_active
-    end
-
-    assignment
-  end
-
-  def fix_enrollment_benefit_group_assignments(enrollment)
-    benefit_group = enrollment.benefit_group
-    enrollments = enrollment.family.active_household.hbx_enrollments.shop_market.where(:aasm_state.nin => ['shopping', 'coverage_canceld'])
-    
-    # Fix enrollment benefit group assignment
-    enrollments.each do |enrollment|
-      if enrollment.benefit_group_id != enrollment.benefit_group_assignment.benefit_group_id
-        employee = enrollment.benefit_group_assignment.census_employee
-        new_assignment = employee.benefit_group_assignments.where(:benefit_group_id => enrollment.benefit_group_id).first
-        new_assignment = create_new_benefit_group_assignment(employee, benefit_group) if new_assignment.blank?
-        enrollment.update(benefit_group_assignment_id: new_assignment.id)
-        puts "Updated benefit group assignment for HbxId: #{enrollment.hbx_id}" unless Rails.env.test?
-      end
-    end
-  end
-
   def migrate
     Organization.exists(:employer_profile => true).each do |org|
-
       employer_profile = org.employer_profile
-      active_plan_year = employer_profile.active_plan_year
-      next if active_plan_year.blank?
-
       puts "Processing #{org.legal_name}" unless Rails.env.test?
 
       count = 0
@@ -52,8 +21,14 @@ class ResolveCensusEmployeeValidationFailures < MongoidMigrationTask
 
           if assignment.errors.messages[:hbx_enrollment].present?
             if assignment.errors.messages[:hbx_enrollment].include?("hbx_enrollment required")
-              assignment.delink_coverage! if assignment.may_delink_coverage?
-              assignment.update(hbx_enrollment_id: nil)
+              assignment.hbx_enrollment_id = nil
+              assignment.save(:validate => false)
+
+              if assignment.hbx_enrollment.present?
+                assignment.update(hbx_enrollment_id: assignment.hbx_enrollment.id)
+              else
+                assignment.delink_coverage! if assignment.may_delink_coverage?
+              end
             else
               enrollment = assignment.hbx_enrollment
               fix_enrollment_benefit_group_assignments(enrollment)
@@ -70,6 +45,7 @@ class ResolveCensusEmployeeValidationFailures < MongoidMigrationTask
           end
         end
 
+        active_plan_year = employer_profile.active_plan_year
         if active_plan_year.present?
           bg_ids = active_plan_year.benefit_groups.pluck(:_id)
 
@@ -84,5 +60,38 @@ class ResolveCensusEmployeeValidationFailures < MongoidMigrationTask
         end
       end
     end
+  end
+
+  def fix_enrollment_benefit_group_assignments(enrollment)
+    benefit_group = enrollment.benefit_group
+    family = enrollment.family
+    
+    # Fix enrollments with mismatching benefit group, benefit group assignment
+    family.active_household.hbx_enrollments.shop_market.each do |enrollment|
+      if enrollment.benefit_group_id != enrollment.benefit_group_assignment.benefit_group_id
+        benefit_group = enrollment.benefit_group_assignment.benefit_group
+        if (benefit_group.start_on..benefit_group.end_on).cover?(enrollment.effective_on)
+          enrollment.update(benefit_group_id: benefit_group.id)
+        elsif (enrollment.benefit_group.start_on..enrollment.benefit_group.end_on).cover?(enrollment.effective_on)
+          employee = enrollment.benefit_group_assignment.census_employee
+          new_assignment = employee.benefit_group_assignments.where(:benefit_group_id => enrollment.benefit_group_id).first
+          new_assignment = create_new_benefit_group_assignment(employee, benefit_group) if new_assignment.blank?
+          enrollment.update(benefit_group_assignment_id: new_assignment.id)
+        end
+
+        puts "Updated benefit group assignment for HbxId: #{enrollment.hbx_id}" unless Rails.env.test?
+      end
+    end
+  end
+
+  def create_new_benefit_group_assignment(employee, benefit_group)
+    assignment = employee.benefit_group_assignments.build(benefit_group: benefit_group, start_on: benefit_group.start_on)
+    assignment.save
+
+    if (PlanYear::PUBLISHED + PlanYear::RENEWING_PUBLISHED_STATE).include?(benefit_group.plan_year.aasm_state)
+      assignment.make_active
+    end
+
+    assignment
   end
 end
