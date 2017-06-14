7a62f87c7e69251b0dcc280599296ad1dc1c0aa8
nisanth <nisanth.yaganti@gmail.com>
2017-06-14 17:02:44 -0400

    refs #16410 fixed existing rake task & added script to fix data for bga errors

---
+++ script/fix_benefit_group_assignments_for_employees.rb
@@ -0,0 +1,51 @@
+
+
+# This ER had a terminated plan year with a custom end_on date i.e., plan year duration is less than 12 months
+
+# 16410
+
+organization = Organization.where(legal_name: /Preventive Measures Of Washing/i).first
+census_employees = organization.employer_profile.census_employees
+census_employees.each do |ce|
+  ce.save
+  ce.benefit_group_assignments.each do |bga|
+    unless bga.valid?
+      bga.update_attributes!(end_on: bga.benefit_group.plan_year.end_on) if bga.end_on.present? && bga.end_on > bga.benefit_group.plan_year.end_on
+      puts "update benefit_group_assignment end_on date for #{ce.full_name}"
+    end
+  end
+end
+
+
+# 16505
+
+# Benefit Group Mismatch Issue. BG & BGA must point to same plan year, here BG pointing to 2015, whereas BGA is pointing to 2016 plan year which is incorrect
+census_employee = census_employees.where(first_name: 'Jessica', last_name: 'Jones').first
+# This is 2015 enrollment
+enrollment = HbxEnrollment.by_hbx_id('213881').first
+incorrect_bga = enrollment.benefit_group_assignment
+enrollment.unset(:benefit_group_assignment_id)
+incorrect_bga.unset(:hbx_enrollment_id)
+enrollment.save!
+incorrect_bga.save!
+bga = census_employee.benefit_group_assignments.where(benefit_group_id: enrollment.benefit_group.id).first
+if enrollment.update_attributes!(benefit_group_assignment_id: bga.id) && bga.update_attributes!(hbx_enrollment_id: enrollment.id)
+  puts "Fixed benefit_group mismatch issue"
+end
+
+
+# 16512
+
+census_employee = CensusEmployee.where(first_name: "Michael", middle_name: nil, last_name: "Mercier").first
+enrollment = HbxEnrollment.by_hbx_id('773640').first
+incorrect_bga = enrollment.benefit_group_assignment
+enrollment.unset(:benefit_group_assignment_id)
+incorrect_bga.unset(:hbx_enrollment_id)
+enrollment.save!
+incorrect_bga.save!
+bga = census_employee.benefit_group_assignments.where(benefit_group_id: enrollment.benefit_group.id).first
+if enrollment.update_attributes!(benefit_group_assignment_id: bga.id)
+  puts "Fixed benefit_group mismatch issue"
+end
+
+
