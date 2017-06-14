edbc70be9dcf280ccef75faa34604933c1badff5
saikumar9 <saikumarkotagiri9@gmail.com>
2017-06-14 11:32:50 -0400

    Refs #14597 Updated the query to look for families which have members more than 1

--- app/data_migrations/relationships_update.rb
+++ app/data_migrations/relationships_update.rb
@@ -4,14 +4,16 @@ class RelationshipsUpdate < MongoidMigrationTask
   def migrate
     Family.all.each do |family|
       begin
-        primary = family.primary_applicant
-        primary_person = primary.person
+        if family.family_members.count > 1
+          primary = family.primary_applicant
+          primary_person = primary.person
 
-        primary_person.person_relationships.each do |relation|
-          dependent_person = Person.find(relation.relative_id)
-          dependent_person.add_relationship(primary_person, relation.kind, family.id)
-          relation.update_attributes(kind: PersonRelationship::InverseMap[relation.kind],successor_id: dependent_person.id, predecessor_id: primary_person.id,family_id: family.id)
-          puts "Updated the relationships for all the members of family with family_id: #{family.id}" unless Rails.env.test?
+          primary_person.person_relationships.each do |relation|
+            dependent_person = Person.find(relation.relative_id)
+            dependent_person.add_relationship(primary_person, relation.kind, family.id)
+            relation.update_attributes(kind: PersonRelationship::InverseMap[relation.kind],successor_id: dependent_person.id, predecessor_id: primary_person.id,family_id: family.id)
+            puts "Updated the relationships for all the members of family with family_id: #{family.id}" unless Rails.env.test?
+          end
         end
       rescue
         puts "Bad Family Record: #{family.id}" unless Rails.env.test?
