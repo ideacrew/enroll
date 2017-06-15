87143d939d0d9ebf9ef9f09cfcaf7bc1a6e9d14f
nisanth <nisanth.yaganti@gmail.com>
2017-06-15 15:02:53 -0400

    refs #16410 removing fix for 16512

--- script/fix_benefit_group_assignments_for_employees.rb
+++ script/fix_benefit_group_assignments_for_employees.rb
@@ -21,31 +21,13 @@ end
 
 # Benefit Group Mismatch Issue. BG & BGA must point to same plan year, here BG pointing to 2015, whereas BGA is pointing to 2016 plan year which is incorrect
 census_employee = census_employees.where(first_name: 'Jessica', last_name: 'Jones').first
+# Only one benefit group assignment has this issue.
+census_employee.benefit_group_assignments.detect {|bga| !bga.valid? }.unset(:hbx_enrollment_id)
+census_employee.save!
 # This is 2015 enrollment
 enrollment = HbxEnrollment.by_hbx_id('213881').first
-incorrect_bga = enrollment.benefit_group_assignment
-enrollment.unset(:benefit_group_assignment_id)
-incorrect_bga.unset(:hbx_enrollment_id)
-enrollment.save!
-incorrect_bga.save!
 bga = census_employee.benefit_group_assignments.where(benefit_group_id: enrollment.benefit_group.id).first
 if enrollment.update_attributes!(benefit_group_assignment_id: bga.id) && bga.update_attributes!(hbx_enrollment_id: enrollment.id)
   puts "Fixed benefit_group mismatch issue"
 end
-
-
-# 16512
-
-census_employee = CensusEmployee.where(first_name: "Michael", middle_name: nil, last_name: "Mercier").first
-enrollment = HbxEnrollment.by_hbx_id('773640').first
-incorrect_bga = enrollment.benefit_group_assignment
-enrollment.unset(:benefit_group_assignment_id)
-incorrect_bga.unset(:hbx_enrollment_id)
-enrollment.save!
-incorrect_bga.save!
-bga = census_employee.benefit_group_assignments.where(benefit_group_id: enrollment.benefit_group.id).first
-if enrollment.update_attributes!(benefit_group_assignment_id: bga.id)
-  puts "Fixed benefit_group mismatch issue"
-end
-
 
