d12b64260bda9071701ff814544abbdd53fec18c
shaibazalisyed <shaibazali34@gmail.com>
2017-06-19 09:00:47 -0400

    allowing user to go to previous links on left nav

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,12 +1,12 @@
 module NavigationHelper
   TABS_WITH_TITLE = {"accountRegistration" => { "title" => "Account Registration", "id" => "accountRegistration"}, "moreAboutYou" => { "title" => "Tell us About Yourself", "id" => "moreAboutYou" }, "householdinfo" => { "title" => "Household Info", "id" => "householdInfo" } }
-  YML_TABS_WITH_TITLE = {"personalInfo" => {"title" => "Personal Info", "id" => "personalInfo"}, "incomeAndCoverageInfo" => {"title" => "Income and Coverage Info", "id" => "incomeAndCoverageInfo"}, "taxInfo" => {"title" => "Tax Info", "id" => "taxInfo"}, "jobIncome" => {"title" => "Job Income", "id" => "jobIncome"}, "otherIncome" => {"title" => "Other income", "id" => "otherIncome"}, "incomeAdjustments" => {"title" => "Income Adjustments", "id" => "incomeAdjustments"}, "healthCoverage" => {"title" => "Health Coverage", "id" => "healthCoverage"}, "otherQuestions" => {"title" => "Other Questions", "id" => "otherQuestions"}}
+  YML_TABS_WITH_TITLE = {"personalInfo" => {"title" => "Personal Info", "id" => "personalInfo"}, "incomeAndCoverageInfo" => {"title" => "Income and Coverage Info", "id" => "incomeAndCoverageInfo"}, "taxInfo" => {"title" => "Tax Info", "id" => "taxInfo", "link" => "/step/1"}, "jobIncome" => {"title" => "Job Income", "id" => "jobIncome", "link" => "/incomes/new"}, "otherIncome" => {"title" => "Other income", "id" => "otherIncome"}, "incomeAdjustments" => {"title" => "Income Adjustments", "id" => "incomeAdjustments", "link"=>"/deductions/new"}, "healthCoverage" => {"title" => "Health Coverage", "id" => "healthCoverage", "link" => "/benefits/new"}, "otherQuestions" => {"title" => "Other Questions", "id" => "otherQuestions"}}
 
   def self.getAllTabs
     TABS_WITH_TITLE.map {|tab, tabValue| {"title" => tabValue["title"], "id"=> tabValue["id"]}}
   end
 
   def self.getAllYmlTabs
-    YML_TABS_WITH_TITLE.map {|tab, tabValue| {"title" => tabValue["title"], "id"=> tabValue["id"]}}
+    YML_TABS_WITH_TITLE.map {|tab, tabValue| {"title" => tabValue["title"], "id"=> tabValue["id"], "link"=>tabValue["link"]}}
   end
 end
