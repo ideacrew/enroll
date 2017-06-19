168ba55815799e9f4619d17f9ffa33c57ed2f99b
Pranab <pranab.khanal@gmail.com>
2017-06-19 11:57:15 -0400

    Fixed Income Validations and saves

--- app/controllers/financial_assistance/incomes_controller.rb
+++ app/controllers/financial_assistance/incomes_controller.rb
@@ -14,11 +14,8 @@
   def step
     model_name = @model.class.to_s.split('::').last.downcase
     model_params = params[model_name]
-
     format_date_params model_params
 
-    # TODO: Fix the issue of incrementing current_step even when there is only one step. 
-    # We cant check params.key?(model_name) and expect to got to 2nd step.
     if params.key?(model_name)
       @model.workflow = { current_step: @current_step.to_i + 1 }
       @current_step = @current_step.next_step if @current_step.next_step.present?
@@ -26,13 +23,11 @@
       @model.workflow = { current_step: @current_step.to_i }
     end
 
-    #@model.save!
+    @model.assign_attributes(permit_params(model_params)) if model_params.present?
+    update_employer_contact(@model, params) if @model.income_type == job_income_type
 
     begin
-      @model.update_attributes!(permit_params(model_params)) if model_params.present?
-
-      update_employer_contact(@model, params) if @model.income_type == job_income_type
-
+      @model.save!
       if params[:commit] == "Finish"
         flash[:notice] = 'Income Info Added.'
         redirect_to edit_financial_assistance_application_applicant_path(@application, @applicant)
@@ -63,19 +58,19 @@
     # all_errors = all_errors + model.employer_address.errors.full_messages.join(', ') if model.employer_address.present? && model.employer_address.errors.messages.present?
     # all_errors = all_errors + model.employer_phone.errors.full_messages.join(', ') if model.employer_phone.present? && model.employer_phone.errors.messages.present?
     # return all_errors
-    model.valid? ? nil : model.errors.messages
+    model.valid? ? nil : model.errors.messages.first.flatten.flatten.join(',').gsub(",", " ").titleize
   end
 
   def update_employer_contact model, params
     if params[:employer_phone].present?
       @model.build_employer_phone
       params[:employer_phone].merge!(kind: "work") # hack to get pass phone validations
-      @model.employer_phone.update_attributes!(permit_params(params[:employer_phone]))
+      @model.employer_phone.assign_attributes(permit_params(params[:employer_phone]))
     end
     if params[:employer_address].present?
       @model.build_employer_address
       params[:employer_address].merge!(kind: "work") # hack to get pass phone validations
-      @model.employer_address.update_attributes!(permit_params(params[:employer_address]))
+      @model.employer_address.assign_attributes(permit_params(params[:employer_address]))
     end
   end
 
