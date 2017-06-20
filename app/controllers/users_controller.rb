1b990e659847762d95ca749609a2db2e36a9bd59
ymhari <hariym471@gmail.com>
2017-06-19 23:12:44 -0400

    Flash message for lock/unlock function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
 
   def lockable
     user = User.find(params[:id])
-    authorize user
+    authorize User, :lockable?
     user.update_lockable
     redirect_to user_account_index_exchanges_hbx_profiles_url, notice: "User #{user.person.full_name} is successfully #{user.lockable_notice}."
   rescue Exception => e
-    redirect_to user_account_index_exchanges_hbx_profiles_url, notice: "You are not authorized for this action."
+    redirect_to user_account_index_exchanges_hbx_profiles_url, alert: "You are not authorized for this action."
   end
 end
