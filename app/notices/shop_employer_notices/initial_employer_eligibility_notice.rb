c3680a5a3f0960b1bc36b241ac891bf7c5f0584f
Battula <vrbattula@outlook.com>
2017-06-20 20:55:29 -0400

    refs #16270 Added shop employer appeals and non-discrimination attachment and updated the template.

--- app/notices/shop_employer_notices/initial_employer_eligibility_notice.rb
+++ app/notices/shop_employer_notices/initial_employer_eligibility_notice.rb
@@ -4,6 +4,8 @@ class ShopEmployerNotices::InitialEmployerEligibilityNotice < ShopEmployerNotice
     build
     append_data
     generate_pdf_notice
+    shop_dchl_rights_attachment
+    non_discrimination_attachment
     attach_envelope
     upload_and_send_secure_message
     send_generic_notice_alert
