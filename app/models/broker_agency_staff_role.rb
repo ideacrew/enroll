4d9a8d868e0d9da3f842c74c67d07a6c6f73fb2f
lisyk <sergey.liskovoy@gmail.com>
2017-06-22 15:00:44 -0400

    Refs #16029. log state machine events

--- app/models/broker_agency_staff_role.rb
+++ app/models/broker_agency_staff_role.rb
@@ -86,7 +86,8 @@ class BrokerAgencyStaffRole
   def record_transition
     self.workflow_state_transitions << WorkflowStateTransition.new(
       from_state: aasm.from_state,
-      to_state: aasm.to_state
+      to_state: aasm.to_state,
+      event: aasm.current_event
     )
   end
 end
--- app/models/census_employee.rb
+++ app/models/census_employee.rb
@@ -746,7 +746,8 @@ class CensusEmployee < CensusMember
   def record_transition
     self.workflow_state_transitions << WorkflowStateTransition.new(
       from_state: aasm.from_state,
-      to_state: aasm.to_state
+      to_state: aasm.to_state,
+      event: aasm.current_event
     )
   end
 
