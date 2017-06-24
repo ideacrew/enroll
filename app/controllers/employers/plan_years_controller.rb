79fdee63c1ef0ce7a27d776a740934c357570c77
Brian Weiner <brian.weiner@dc.gov>
2017-06-24 13:00:45 -0400

    on creation make sure we have contribution tiers and we assign estimates

--- app/controllers/employers/plan_years_controller.rb
+++ app/controllers/employers/plan_years_controller.rb
@@ -124,7 +124,12 @@ class Employers::PlanYearsController < ApplicationController
     @plan_year = ::Forms::PlanYearForm.build(@employer_profile, plan_year_params)
 
     @plan_year.benefit_groups.each_with_index do |benefit_group, i|
-      benefit_group.assign_estimated_premiums
+      if benefit_group.sole_source?
+        if benefit_group.composite_tier_contributions.empty?
+          benefit_group.build_composite_tier_contributions
+        end
+        benefit_group.estimate_composite_rates
+      end
       benefit_group.elected_plans = benefit_group.elected_plans_by_option_kind
       benefit_group.elected_dental_plans = if benefit_group.dental_plan_option_kind == "single_plan"
         if i == 0
