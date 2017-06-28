80386778dea2f460f1213415a4a20158f9515871
Nikhil S <nikhilks219@gmail.com>
2017-06-28 15:46:08 -0400

    fixed spec employer_profiles_my_accoun_census_employees

--- spec/views/employers/employer_profiles/my_account/_census_employees.html.erb_spec.rb
+++ spec/views/employers/employer_profiles/my_account/_census_employees.html.erb_spec.rb
@@ -24,11 +24,10 @@ RSpec.describe "employers/employer_profiles/my_account/_census_employees.html.er
   end
 
   it "should have the link of add employee" do
-    expect(rendered).to have_selector("a", text: 'Add New Employee')
+    expect(rendered).to have_selector("a", text: 'Add Employee')
   end
 
   it "should have tab of cobra" do
-    expect(rendered).to match(/COBRA Continuation/)
-    expect(rendered).to have_selector('input#cobra')
+    expect(rendered).to match(/COBRA only/)
   end
 end
