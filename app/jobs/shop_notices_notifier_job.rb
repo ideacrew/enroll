13046e9149ce253b9f1eeaed5810d040765c52be
saip7795 <saip7795@gmail.com>
2017-06-29 11:21:12 -0400

    16773:Initial Draft for Employer Approval Notice

--- app/jobs/shop_notices_notifier_job.rb
+++ app/jobs/shop_notices_notifier_job.rb
@@ -3,14 +3,15 @@ class ShopNoticesNotifierJob < ActiveJob::Base
 
   def perform(id, event)
     Resque.logger.level = Logger::DEBUG
+    id = "5951291db79d5014c200000a"
     profile = EmployerProfile.find(id) || CensusEmployee.where(id: id).first
-    event_kind = ApplicationEventKind.where(:event_name => event).first
+    event_kind = ApplicationEventKind.where(:event_name => "initial_employer_approval").first
     notice_trigger = event_kind.notice_triggers.first
     builder = notice_trigger.notice_builder.camelize.constantize.new(profile, {
               template: notice_trigger.notice_template,
               subject: event_kind.title,
               mpi_indicator: notice_trigger.mpi_indicator,
-              }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)).deliver
-
+              event_name: "initial_employer_approval",
+              }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)).deliver 
   end
 end
