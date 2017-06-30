c07ae45ae29125f5c3c0f17760fa0dc4ac2c869a
Brian Weiner <brian.weiner@dc.gov>
2017-06-30 14:23:25 -0400

    create a carrier service area in factory

--- spec/factories/carrier_profiles.rb
+++ spec/factories/carrier_profiles.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
     abbrev        "UHIC"
     offers_sole_source false
     issuer_hios_ids ['11111']
+
+    after(:create) do |carrier_profile, evaluator|
+      carrier_profile.issuer_hios_ids.each do |hios_id|
+        create(:carrier_service_area, issuer_hios_id: hios_id)
+      end
+    end
   end
 end
