469e22022095ac2c2258f80509f0bcecad9189e4
harsha chalamala <chvreddy1015@gmail.com>
2017-06-30 11:41:22 -0400

    minor changes

--- app/models/effective/datatables/user_account_datatable.rb
+++ app/models/effective/datatables/user_account_datatable.rb
@@ -7,7 +7,7 @@ module Effective
           bulk_action 'action 1', nil, data: { confirm: 'Generate Invoices?', no_turbolink: true }
           bulk_action 'action 2', nil, data: {  confirm: 'Mark Binder Paid?', no_turbolink: true }
         end
-        table_column :name, :label => 'USERNAME', :proc => Proc.new { |row| row.oim_id }, :filter => false, :sortable => false
+        table_column :name, :label => 'USERNAME', :proc => Proc.new { |row| row.oim_id }, :filter => false, :sortable => true
         table_column :ssn, :label => 'SSN', :proc => Proc.new { |row| truncate(number_to_obscured_ssn(row.person.ssn)) if row.person.present? }, :filter => false, :sortable => false
         table_column :dob, :label => 'DOB', :proc => Proc.new { |row| format_date(row.person.dob) if row.person.present?}, :filter => false, :sortable => false
         table_column :hbx_id, :label => 'HBX ID', :proc => Proc.new { |row| row.person.hbx_id if row.person.present?}, :filter => false, :sortable => false
