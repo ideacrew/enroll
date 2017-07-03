a59feeb981d7c9742d9a0070398f1b126ec92944
Antonio Schaffert <antonio.schaffert@gmail.com>
2017-07-03 18:34:27 -0400

    A better library for NFP

--- lib/nfp_integration/soap_services/nfp.rb
+++ lib/nfp_integration/soap_services/nfp.rb
@@ -1,11 +1,12 @@
 require 'net/http'
 require 'uri'
-require 'pry'
 require 'nokogiri'
 
 module NfpIntegration
   module SoapServices
     class Nfp
+
+      include NfpIntegration::SoapServices::Base
 
       # # Change below to Pre Prod 10.0.3.51
       # NFP_URL = "http://localhost:9000/cpbservices/PremiumBillingIntegrationServices.svc"
@@ -178,62 +179,49 @@ XMLCODE
       end
 
       def display_token
-        @token
+        @token.present? ? @token : nil
       end
 
-      def parse_statement_summary(response)
-        past_due = get_element_text(response.xpath("//PastDue"))
-        previous_balance = get_element_text(response.xpath("//PreviousBalance"))
-        new_charges = get_element_text(response.xpath("//NewCharges"))
-        adjustments = get_element_text(response.xpath("//Adjustments"))
-        payments = get_element_text(response.xpath("//Payments"))
-        total_due = get_element_text(response.xpath("//TotalDue"))
-        return past_due, previous_balance, new_charges, adjustments, payments, total_due
-      end
+      # Gets token info from NFP Server
+      def get_token_info(token)
 
-      def get_most_recent_payment_date(response)
-        # assumes payload response lists elements in order of most recent to latest
-        # Should we write a helper to extract the latest payment date by searching through all of them??
+        uri, request = build_request(NfpGetTokenInfo.new, {:token => token})
 
-        date = get_element_text(response.xpath("//DateReceived"))
-        unless (date.blank?)
-          formatted_date = DateTime.parse(date).to_date.to_s
+        req_options = {
+          use_ssl: uri.scheme == "https",
+        }
+
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+          http.request(request)
         end
+
+        puts response.code
+        puts response.body
+
       end
 
       private
 
-        def get_element_text(value)
-          text = value.try(:first).try(:text)
-          text.nil? ? "" : text.strip
+        def build_request(soap_object, parms = {})
+
+          uri = URI.parse(NFP_URL)
+          request = Net::HTTP::Post.new(uri)
+          request.content_type = "text/xml;charset=UTF-8"
+          request["Soapaction"] = soap_object.soap_action
+          request.body = soap_object.body % parms
+
+
+          return uri, request
+
         end
 
         def token
 
           return @token if defined? @token
 
-          uri = URI.parse(NFP_URL)
-          request = Net::HTTP::Post.new(uri)
-          request.content_type = "text/xml;charset=UTF-8"
-          request["Soapaction"] = "http://www.nfp.com/schemas/hbcore/IPremiumBillingIntegrationServices/AuthenticateUser"
-          request.body = ""
-          request.body = <<-XMLCODE
-<?xml version="1.0" encoding="utf-8"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-      <SOAP-ENV:Body>
-              <m:AuthenticationReq xmlns:m="http://www.nfp.com/schemas/hbcore">
-                      <m:UserName>#{NFP_USER_ID}</m:UserName>
-                      <m:Password>#{NFP_PASS}</m:Password>
-                      <m:SubscriptionId>NFP will allocate to DC</m:SubscriptionId>
-                      <m:CertThumbprint>For future scope</m:CertThumbprint>
-                      <m:ExchangeId>NFP will allocate to DC</m:ExchangeId>
-              </m:AuthenticationReq>
-      </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-XMLCODE
+          return nil if NFP_PASS == nil || NFP_USER_ID == nil
 
-  puts request.body
-
+          uri, request = build_request(NfpAuthenticateUser.new, {:user => NFP_USER_ID, :password => NFP_PASS})
 
           req_options = {
             use_ssl: uri.scheme == "https",
@@ -249,8 +237,9 @@ XMLCODE
           doc = Nokogiri::XML(response.body)
           doc.remove_namespaces!
 
-          puts doc.xpath("//AuthToken").text
-          @token = doc.xpath("//AuthToken").text
+          puts get_element_text(doc.xpath("//AuthToken"))
+          @token_status = get_element_text(doc.xpath("//Success")) == "true" ? true : false
+          @token = get_element_text(doc.xpath("//AuthToken"))
 
         end
     end
