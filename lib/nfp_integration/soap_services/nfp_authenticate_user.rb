523b78bc57495fde583a9235ba0cdef6fb46f10c
Antonio Schaffert <antonio.schaffert@gmail.com>
2017-07-03 21:29:40 -0400

    More refactoring

--- lib/nfp_integration/soap_services/nfp_authenticate_user.rb
+++ lib/nfp_integration/soap_services/nfp_authenticate_user.rb
@@ -5,18 +5,18 @@ module NfpIntegration
       SOAP_ACTION = "http://www.nfp.com/schemas/hbcore/IPremiumBillingIntegrationServices/AuthenticateUser"
 
       SOAP_BODY = <<-XMLCODE
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:hbc="http://www.nfp.com/schemas/hbcore">
-   <soapenv:Header/>
-   <soapenv:Body>
-      <hbc:AuthenticationReq>
-         <!--Optional:-->
-         <hbc:UserName>%{user}</hbc:UserName>
-         <!--Optional:-->
-         <hbc:Password>%{password}</hbc:Password>
-      </hbc:AuthenticationReq>
-   </soapenv:Body>
-</soapenv:Envelope>
-XMLCODE
+      <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:hbc="http://www.nfp.com/schemas/hbcore">
+         <soapenv:Header/>
+         <soapenv:Body>
+            <hbc:AuthenticationReq>
+               <!--Optional:-->
+               <hbc:UserName>%{user}</hbc:UserName>
+               <!--Optional:-->
+               <hbc:Password>%{password}</hbc:Password>
+            </hbc:AuthenticationReq>
+         </soapenv:Body>
+      </soapenv:Envelope>
+      XMLCODE
 
       def initialize
         SOAP_BODY.freeze
