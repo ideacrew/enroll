d6eb7eb15eedb9376cdd79a33f8d9b5f0c0aa0e2
sagarlekar <sagar.arlekar@gmail.com>
2017-07-05 12:21:31 -0400

    Refs #13908 1) fixed typo in can_termiante_application?() 2) changed can_cancel_application? logic

--- app/helpers/exchanges/employer_applications_helper.rb
+++ app/helpers/exchanges/employer_applications_helper.rb
@@ -1,14 +1,16 @@
 module Exchanges
   module EmployerApplicationsHelper
 
-    def can_termiante_application?(plan_year)
+    def can_terminate_application?(plan_year)
       (plan_year.active? || plan_year.expired? || plan_year.suspended?) && plan_year.may_terminate?
     end
 
     def can_cancel_application?(plan_year)
-      return false if plan_year.active? || plan_year.expired? || TimeKeeper.date_of_record >= plan_year.start_on
-      return false if plan_year.enrolled? || plan_year.renewing_enrolled? || plan_year.application_ineligible? || plan_year.renewing_application_ineligible?
-      true
+      if plan_year.enrolling? && TimeKeeper.date_of_record < plan_year.start_on
+        true
+      else
+        false
+      end
     end
   end
 end
