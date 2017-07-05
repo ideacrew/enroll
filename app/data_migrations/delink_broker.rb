8475f7c5d3a786b1bf2530700280802585b03c3f
sarathpyerra <sarathpyerra@gmail.com>
2017-07-05 13:48:31 -0400

    Refs #14956 adding address to broker agency

--- app/data_migrations/delink_broker.rb
+++ app/data_migrations/delink_broker.rb
@@ -1,6 +1,7 @@
 require File.join Rails.root, "lib/mongoid_migration_task"
 
 class DelinkBroker < MongoidMigrationTask
+
   def migrate
     hbx_id = ENV['person_hbx_id']
     legal_name = ENV['legal_name']
@@ -11,7 +12,7 @@ class DelinkBroker < MongoidMigrationTask
       # broker_agency = FactoryGirl.create(:broker_agency_profile,:organization => {:legal_name => legal_name, :fein => fein})
       hbx_office = OfficeLocation.new(
       is_primary: true, 
-      address: {kind: "work", address_1: "address_placeholder", address_2: "609 H St, Room 415", city: "Washington", state: "DC", zip: "20002" }, 
+      address: {kind: "work", address_1: "address_placeholder", address_2: "21 Church St, Suite 100", city: "Rockville", state: "MD", zip: "20850" },
       phone: {kind: "main", area_code: "202", number: "555-1212"}
       )
 
@@ -24,15 +25,15 @@ class DelinkBroker < MongoidMigrationTask
       person.broker_role.save
       #move organizations under to newly created borker agency profile 
       if organization_ids_to_move.present?
-      organization_ids_to_move.each do |organization_id|
-      org = Organization.find(organization_id)
-      old_broker_agency = org.employer_profile.broker_agency_accounts.where(:is_active => true).first
-      unless old_broker_agency.nil?
-        old_broker_agency.update_attributes!(:is_active => false)
-        broker_agency_account = BrokerAgencyAccount.new(broker_agency_profile_id: org.broker_agency_profile.id, writing_agent_id: person.broker_role.id, start_on: TimeKeeper.datetime_of_record, is_active: true)
-        org.employer_profile.broker_agency_accounts.push(broker_agency_account)
-    end
-  end
+        organization_ids_to_move.each do |organization_id|
+          org = Organization.find(organization_id)
+          old_broker_agency = org.employer_profile.broker_agency_accounts.where(:is_active => true).first
+          old_broker_agency.update_attributes!(:is_active => false) unless old_broker_agency.nil?
+          broker_agency_account = BrokerAgencyAccount.new(broker_agency_profile_id: org.broker_agency_profile.id, writing_agent_id: person.broker_role.id, start_on: TimeKeeper.datetime_of_record, is_active: true)
+          org.employer_profile.broker_agency_accounts.push(broker_agency_account)
+          org.save
+        end
+      end
     else
       puts "Error for Person: #{person.first_name}" unless Rails.env.test?
     end
