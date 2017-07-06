2c9ae67ab0ba389e681b763263e5cbe53d17c875
shaibazalisyed <shaibazali34@gmail.com>
2017-07-06 15:00:10 -0400

    cleaning up code and adding styling to in-progress icon

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,14 +1,9 @@
 module NavigationHelper
   TABS_WITH_TITLE = {"accountRegistration" => { "title" => "Account Registration", "id" => "accountRegistration", active: true}, "moreAboutYou" => { "title" => "Tell us About Yourself", "id" => "moreAboutYou", active: true }, "householdinfo" => { "title" => "Household Info", "id" => "householdInfo" } }
-  YML_TABS_WITH_TITLE = {"personalInfo" => {"title" => "Personal Info", "id" => "personalInfo"}, "incomeAndCoverageInfo" => {"title" => "Income and Coverage Info", "id" => "incomeAndCoverageInfo"}, "taxInfo" => {"title" => "Tax Info", "id" => "taxInfo", "link" => "/step/1"}, "jobIncome" => {"title" => "Job Income", "id" => "jobIncome", "link" => "/incomes/new"}, "otherIncome" => {"title" => "Other income", "id" => "otherIncome"}, "incomeAdjustments" => {"title" => "Income Adjustments", "id" => "incomeAdjustments", "link"=>"/deductions/new"}, "healthCoverage" => {"title" => "Health Coverage", "id" => "healthCoverage", "link" => "/benefits/new"}, "otherQuestions" => {"title" => "Other Questions", "id" => "otherQuestions"}}
 
   def self.getAllTabs
     TABS_WITH_TITLE.map {|tab, tabValue| {"title" => tabValue["title"], "id"=> tabValue["id"], active: tabValue[:active]}}
   end
-
-  # def self.getAllYmlTabs
-  #   YML_TABS_WITH_TITLE.map {|tab, tabValue| {"title" => tabValue["title"], "id"=> tabValue["id"], "link"=>tabValue["link"]}}
-  # end
 
   def nav_for_applicants
     @application.applicants.inject(raw('')) do |list, applicant|
