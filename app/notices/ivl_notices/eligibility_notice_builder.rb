c0e6d4f10ce5fa0ff79df677de6893dcd1504bc0
Battula <vrbattula@outlook.com>
2017-07-11 14:03:43 -0400

    refs #16543 Added Eligibility Notice builder.

--- app/notices/ivl_notices/eligibility_notice_builder.rb
+++ app/notices/ivl_notices/eligibility_notice_builder.rb
@@ -1,26 +1,77 @@
-class IvlNotices::EligibilityNoticeBuilder < IvlNotices::NoticeBuilder
+class IvlNotices::EligibilityNoticeBuilder < IvlNotice
 
-  def initialize(person_id, template_name=nil)
-    template_obj = {}
-    template_obj[:template] = template_name || "notices/ivl/9c_eligibility_confirmation_notification.html.erb"
-    super(PdfTemplates::EligibilityNotice,template_obj)
-    @person_id = person_id
+  def initialize(consumer_role, args = {})
+    args[:recipient] = consumer_role.person.families.first.primary_applicant.person
+    args[:notice] = PdfTemplates::ConditionalEligibilityNotice.new
+    args[:market_kind] = 'individual'
+    args[:recipient_document_store]= consumer_role.person.families.first.primary_applicant.person
+    args[:to] = consumer_role.person.families.first.primary_applicant.person.work_email_or_best
+    self.header = "notices/shared/header_ivl.html.erb"
+    super(args)
+  end
+
+  def deliver
     build
+    generate_pdf_notice
+    attach_blank_page
+    # attach_voter_application
+    # prepend_envelope
+    upload_and_send_secure_message
+
+    if recipient.consumer_role.can_receive_electronic_communication?
+      send_generic_notice_alert
+    end
+
+    if recipient.consumer_role.can_receive_paper_communication?
+      store_paper_notice
+    end
+  end
+
+  def attach_voter_application
+    join_pdfs [notice_path, Rails.root.join('lib/pdf_templates', 'voter_application.pdf')]
   end
 
   def build
-    # @hbx_enrollment = HbxEnrollment.find(@hbx_enrollment_id)
-    @consumer = Person.find(@person_id)
-    super
-    hbx_enrollments = @consumer.primary_family.active_household.hbx_enrollments.with_in(24.hours.ago)
-
-    health = hbx_enrollments.select{|e| e.plan.market == 'individual' && e.plan.coverage_kind == 'health'}
-    dental = hbx_enrollments.select{|e| e.plan.market == 'individual' && e.plan.coverage_kind == 'dental'}
-
-    # @notice.enrollments << build_enrollment(health.first) if health.first
-    # @notice.enrollments << build_enrollment(dental.first) if dental.first
-    hbx_enrollments = [health.first, dental.first]
-
-    append_enrollments(hbx_enrollments)
+    family = recipient.primary_family
+    append_data
+    notice.primary_fullname = recipient.full_name.titleize || ""
+    if recipient.mailing_address
+      append_address(recipient.mailing_address)
+    else
+      # @notice.primary_address = nil
+      raise 'mailing address not present'
+    end
   end
+
+  def append_data
+    recipient.primary_family.family_members.map(&:person).uniq.each do |person|
+      notice.individuals << append_family_members(person)
+    end
+  end
+
+  def append_family_members(person)
+    params = {full_name: person.full_name}
+    if consumer_role = person.consumer_role
+      params.merge!({
+        :age => person.age_on(TimeKeeper.date_of_record)
+        })
+    end
+  end
+
+  def append_address(primary_address)
+    notice.primary_address = PdfTemplates::NoticeAddress.new({
+      street_1: capitalize_quadrant(primary_address.address_1.to_s.titleize),
+      street_2: capitalize_quadrant(primary_address.address_2.to_s.titleize),
+      city: primary_address.city.titleize,
+      state: primary_address.state,
+      zip: primary_address.zip
+      })
+  end
+
+  def capitalize_quadrant(address_line)
+    address_line.split(/\s/).map do |x|
+      x.strip.match(/^NW$|^NE$|^SE$|^SW$/i).present? ? x.strip.upcase : x.strip
+    end.join(' ')
+  end
+
 end
