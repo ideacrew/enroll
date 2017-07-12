259f03470411ddd3550085a539063bf319caaefe
Antonio Schaffert <antonio.schaffert@gmail.com>
2017-07-12 13:33:26 -0400

    New NFP URL

--- config/initializers/nfp_integration.rb
+++ config/initializers/nfp_integration.rb
@@ -2,4 +2,4 @@
 
 NFP_USER_ID = "testuser" #TEST ONLY
 NFP_PASS = "M0rph!us007" #TEST ONLY
-NFP_URL = Rails.env.production? ? "http://10.0.3.51/cpbservices/PremiumBillingIntegrationServices.svc" : "http://localhost:9000/cpbservices/PremiumBillingIntegrationServices.svc"
+NFP_URL = Rails.env.production? ? "http://nfp-wsdl.priv.dchbx.org/cpbservices/PremiumBillingIntegrationServices.svc" : "http://nfp-wsdl.priv.dchbx.org/cpbservices/PremiumBillingIntegrationServices.svc"
