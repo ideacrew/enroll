a9d7435f9cfb7b51dc330486277d7f9059a17026
Mahesh Kurma <maheshruby405@gmail.com>
2017-07-13 14:17:51 -0400

    Refs#13067 Listing qualified applicants on sub-sections of the Eligibility determination page.

--- app/helpers/financial_assistance_helper.rb
+++ app/helpers/financial_assistance_helper.rb
@@ -2,4 +2,18 @@ module FinancialAssistanceHelper
   def to_est datetime
     datetime.in_time_zone("Eastern Time (US & Canada)") if datetime.present?
   end
+
+  def total_aptc_sum application_id
+    application = FinancialAssistance::Application.find(application_id)
+    sum = 0.0
+    application.tax_households.each do |thh|
+      sum = sum + thh.preferred_eligibility_determination.max_aptc
+    end
+    return sum
+  end
+
+  def eligible_applicants application_id, eligibility_flag
+    application = FinancialAssistance::Application.find(application_id)
+    application.applicants.where(eligibility_flag => true).map(&:person).map(&:full_name).map(&:titleize)
+  end
 end
