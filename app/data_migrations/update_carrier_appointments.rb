b99eb942d3ab1b52e6ac2eeb1e982e6143aed8c4
harsha chalamala <chvreddy1015@gmail.com>
2017-07-14 10:30:56 -0400

    REF #17014 changed to MA carriers

--- app/data_migrations/update_carrier_appointments.rb
+++ app/data_migrations/update_carrier_appointments.rb
@@ -4,15 +4,19 @@ class UpdateCarrierAppointments < MongoidMigrationTask
   def migrate
 
     mappings = {
-            "aetna_health_inc" => "Aetna Health Inc" ,
-            "aetna_life_insurance_company" => "Aetna Life Insurance Company", 
-            "carefirst_bluechoice_inc" => "Carefirst Bluechoice Inc",
-            "group_hospitalization_and_medical_services_inc" => "Group Hospitalization and Medical Services Inc",
-            "kaiser_foundation" => "Kaiser Foundation",
-            "optimum_choice" => "Optimum Choice",
-            "united_health_care_insurance" => "United Health Care Insurance", 
-            "united_health_care_mid_atlantic" => "United Health Care Mid Atlantic"
-            }
+                "Altus Dental" => "Altus Dental",
+                "Blue Cross Blue Shield of MA" => "Blue Cross Blue Shield of MA",
+                "Boston Medical Center Health Planc" => "Boston Medical Center Health Planc",
+                "Delta Dental" => "Delta Dental",
+                "Kaiser Foundation" => "Kaiser Foundation",
+                "Guardian" => "Guardian",
+                "Health New England" => "Health New England",
+                "Harvard Pilgrim Health Car" => "Harvard Pilgrim Health Car",
+                "Minuteman Health" => "Minuteman Health",
+                "Neighborhood Health Plan" => "Neighborhood Health Plan",
+                "Tufts Health Plan Direct" => "Tufts Health Plan Direct",
+                "Tufts Health Plan Premier" => "Tufts Health Plan Premier"
+               }
     BrokerRole.all.each do |br|
       if br.carrier_appointments.all?{|key, value| key.include?('_') }
           br.carrier_appointments = br.carrier_appointments.map {|k, v| [mappings[k], v] }.to_h
