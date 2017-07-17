cd3c7878cd42369e5a36fcf09f8d4125a3d45fc9
saimekala07 <saimekala07@gmail.com>
2017-07-17 17:09:08 -0400

    refs #17168 Typo

--- app/data_migrations/create_broker_agency_account_for_employer.rb
+++ app/data_migrations/create_broker_agency_account_for_employer.rb
@@ -7,12 +7,9 @@ class CreateBrokerAgencyAccountForEmployer < MongoidMigrationTask
     start_on = Date.strptime(ENV['br_start_on'].to_s, "%m/%d/%Y")
 
     if emp_org.present? && br_agency_profile_org.present?
-      binding.pry
-
       employer_profile=emp_org.employer_profile
       broker_agency_profile = br_agency_profile_org.broker_agency_profile
       broker = br_agency_profile_org.broker_agency_profile.brokers.select{|broker| broker.npn == ENV['br_npn']}.first
-
       if broker_agency_profile.present? && broker.present?
         employer_profile.broker_agency_accounts.build(broker_agency_profile_id:broker_agency_profile.id, start_on:start_on, writing_agent_id:broker.id)
         employer_profile.save!
