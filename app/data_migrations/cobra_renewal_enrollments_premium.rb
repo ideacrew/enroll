b5398e11ca1743e8a733ee6015329f8ec93e6f92
sarathpyerra <sarathpyerra@gmail.com>
2017-07-20 11:50:13 -0400

    Refs #16990 adding changes to rake

--- app/data_migrations/cobra_renewal_enrollments_premium.rb
+++ app/data_migrations/cobra_renewal_enrollments_premium.rb
@@ -4,14 +4,13 @@ class CobraRenewalEnrollmentsPremium< MongoidMigrationTask
 
   def migrate
     begin
-      hbx_id = ENV['hbx_id']
-      hbx_id1 = ENV['hbx_id1']
-      p = Person.where(hbx_id: hbx_id).first
+      person_hbx_id = ENV['person_hbx_id']
+      enrollment_hbx_id = ENV['enrollment_hbx_id']
+      p = Person.where(hbx_id: person_hbx_id).first
       if p.present?
-      hbx= p.primary_family.active_household.hbx_enrollments.where(hbx_id: hbx_id1).first
+      hbx= p.primary_family.active_household.hbx_enrollments.where(hbx_id: enrollment_hbx_id).first
       mem= HbxEnrollmentMember.new({ applicant_id: p.primary_family.family_members.first.id, eligibility_date: hbx.effective_on, coverage_start_on: hbx.effective_on, is_subscriber: true})
       hbx.hbx_enrollment_members << mem
-      puts hbx.hbx_enrollment_members.count
       cost=PlanCostDecorator.new(hbx.plan, hbx , hbx.benefit_group, hbx.benefit_group.reference_plan)
       puts "cobra renewal enrollments premium is changed" unless Rails.env.test?
     else
--- spec/data_migrations/cobra_renewal_enrollments_premeium_spec.rb
+++ spec/data_migrations/cobra_renewal_enrollments_premeium_spec.rb
@@ -17,17 +17,17 @@ describe "UpdateDentalRelationshipBenefits", dbclean: :after_each do
     let!(:family) { FactoryGirl.create(:family, :with_family_members, person: person, people: [person]) }
     let(:person) { FactoryGirl.create(:person, :with_employee_role)}
     let!(:benefit_group) { FactoryGirl.create(:benefit_group)}
-     let(:hbx_enrollment) { FactoryGirl.create(:hbx_enrollment, household: family.active_household)}
+    let(:hbx_enrollment) { FactoryGirl.create(:hbx_enrollment, household: family.active_household)}
     before(:each) do
-      allow(ENV).to receive(:[]).with('hbx_id').and_return person.hbx_id
-       allow(ENV).to receive(:[]).with('hbx_id1').and_return hbx_enrollment.hbx_id
+      allow(ENV).to receive(:[]).with('person_hbx_id').and_return person.hbx_id
+      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return hbx_enrollment.hbx_id
     end
 
     it "should change person relationships kind" do
       expect(hbx_enrollment.hbx_enrollment_members.count).to eq 0
       subject.migrate
       hbx_enrollment.reload
-     expect(hbx_enrollment.hbx_enrollment_members.count).to eq 1
+      expect(hbx_enrollment.hbx_enrollment_members.count).to eq 1
     end
   end
 end
