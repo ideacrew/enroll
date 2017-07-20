976f50b9799228da61c850b9d60a50a9ee75f4a4
Brian Weiner <brian.weiner@dc.gov>
2017-07-20 11:43:08 -0400

    ref#16011 admin can reset user passwords

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,17 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_filter :confirm_identity, only: [:create]
   def create
-    super
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+    if successfully_sent?(resource)
+      respond_to do |format|
+       format.html { respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name)) }
+       format.js
+      end
+
+    else
+      respond_with(resource)
+    end
   end
 
   private
