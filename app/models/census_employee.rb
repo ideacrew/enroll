dc2de4c522a04dbe5d647d094cadd8a56331c726
krbhavith <krbhavith@gmail.com>
2017-07-20 14:42:56 -0400

    refs #15971 code refactor

--- app/models/census_employee.rb
+++ app/models/census_employee.rb
@@ -452,21 +452,22 @@ class CensusEmployee < CensusMember
       CensusEmployee.employee_dependent_age_off_termination
     end
 
-
     def employee_dependent_age_off_termination
       return unless TimeKeeper.date_of_record.mday == 1
       CensusEmployee.all.each do |census_employee|
         if census_employee.employee_role.present?
           plan_year = census_employee.employee_role.employer_profile.plan_years.where(:aasm_state.nin => PlanYear::INELIGIBLE_FOR_EXPORT_STATES).first
-          next if plan_year.benefit_groups.any?{|bg| bg.is_congress?}
-          census_employee.active_benefit_group_assignment.hbx_enrollment.hbx_enrollment_members.reject(&:is_subscriber).each do |dependent|
-            if PlanCostDecorator.benefit_relationship(dependent.primary_relationship).include? "child_under_26"
-              dep = dependent.person
-              now = TimeKeeper.date_of_record
-              age = now.year - dep.dob.year - ((now.month > dep.dob.month || (now.month == dep.dob.month && now.day >= dep.dob.day)) ? 0 : 1)
-              if age >= 25
-                if (now.month == 12 && now.day == 1) || (now.month == dep.dob.month && now.day == 1)
-                  ShopNoticesNotifierJob.perform_later(self.census_employee.id.to_s, "employee_dependent_age_off_termination")
+          next if (plan_year.nil? || plan_year.benefit_groups.any?{|bg| bg.is_congress?})
+          if census_employee.active_benefit_group_assignment.present? && census_employee.active_benefit_group_assignment.hbx_enrollment.present? && census_employee.active_benefit_group_assignment.hbx_enrollment.hbx_enrollment_members.count >= 1
+            census_employee.active_benefit_group_assignment.hbx_enrollment.hbx_enrollment_members.reject(&:is_subscriber).each do |dependent|
+              if PlanCostDecorator.benefit_relationship(dependent.primary_relationship).include? "child_under_26"
+                dep = dependent.person
+                now = TimeKeeper.date_of_record
+                age = now.year - dep.dob.year - ((now.month > dep.dob.month || (now.month == dep.dob.month && now.day >= dep.dob.day)) ? 0 : 1)
+                if age >= 25
+                  if (now.month == 12 && now.day == 1) || (now.month == dep.dob.month && now.day == 1)
+                    ShopNoticesNotifierJob.perform_later(census_employee.id.to_s, "employee_dependent_age_off_termination")
+                  end
                 end
               end
             end
--- app/notices/shop_employee_notices/employee_dependent_age_off_termination.rb
+++ app/notices/shop_employee_notices/employee_dependent_age_off_termination.rb
@@ -15,25 +15,29 @@ class ShopEmployeeNotices::EmployeeDependentAgeOffTermination < ShopEmployeeNoti
   def append_data
     now = TimeKeeper.date_of_record
     hbx = HbxProfile.current_hbx
-    bc_period = hbx.benefit_sponsorship.benefit_coverage_periods.detect { |bcp| bcp if (bcp.start_on..bcp.end_on).cover?(TimeKeeper.date_of_record.next_year) }
+    bc_period = hbx.benefit_sponsorship.benefit_coverage_periods.detect { |bcp| bcp if (bcp.start_on..bcp.end_on).cover?(now) }
     names = []
-    census_employee.active_benefit_group_assignment.hbx_enrollment.hbx_enrollment_members.reject(&:is_subscriber).each do |dependent|
-      if PlanCostDecorator.benefit_relationship(dependent.primary_relationship).include? "child_under_26"
-        dep = dependent.person
-        age = now.year - dep.dob.year - ((now.month > dep.dob.month || (now.month == dep.dob.month && now.day >= dep.dob.day)) ? 0 : 1)
-        if age >= 25
-          if (now.month == 12 && now.day == 1) || (now.month == dep.dob.month && now.day == 1)
-            names << dep.full_name
+    if bc_period.present?
+      if census_employee.active_benefit_group_assignment.present? && census_employee.active_benefit_group_assignment.hbx_enrollment.present? && census_employee.active_benefit_group_assignment.hbx_enrollment.hbx_enrollment_members.count >= 1
+        census_employee.active_benefit_group_assignment.hbx_enrollment.hbx_enrollment_members.reject(&:is_subscriber).each do |dependent|
+          if PlanCostDecorator.benefit_relationship(dependent.primary_relationship).include? "child_under_26"
+            dep = dependent.person
+            age = now.year - dep.dob.year - ((now.month > dep.dob.month || (now.month == dep.dob.month && now.day >= dep.dob.day)) ? 0 : 1)
+            if age >= 25
+              if (now.month == 12 && now.day == 1) || (now.month == dep.dob.month && now.day == 1)
+                names << dep.full_name
+                notice.enrollment = PdfTemplates::Enrollment.new({
+                  :dependents => names,
+                  :plan_year => bc_period.start_on.year+1,
+                  :effective_on => bc_period.start_on+1.year,
+                  :ivl_open_enrollment_start_on => bc_period.open_enrollment_start_on+1.year,
+                  :ivl_open_enrollment_end_on => bc_period.open_enrollment_end_on+1.year
+                  })
+              end
+            end
           end
         end
       end
     end
-    notice.enrollment = PdfTemplates::Enrollment.new({
-              :dependents => names,
-              :plan_year => bc_period.start_on.year,
-              :effective_on => bc_period.start_on,
-              :ivl_open_enrollment_start_on => bc_period.open_enrollment_start_on,
-              :ivl_open_enrollment_end_on => bc_period.open_enrollment_end_on
-              })
   end
 end
