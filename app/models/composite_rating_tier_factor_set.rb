93d3a0ad31723d3a6f8634694371be3a8dba7e30
Brian Weiner <brian.weiner@dc.gov>
2017-07-24 15:26:12 -0400

    added default of 1.0 for lookup failure

--- app/models/composite_rating_tier_factor_set.rb
+++ app/models/composite_rating_tier_factor_set.rb
@@ -3,7 +3,7 @@ class CompositeRatingTierFactorSet < RatingFactorSet
 
   def self.value_for(carrier_profile_id, year, val)
     record = self.where(carrier_profile_id: carrier_profile_id, active_year: year).first
-    record.lookup(val)
+    record.try(:lookup, val) || 1.0
   end
 
   def only_valid_tier_names
