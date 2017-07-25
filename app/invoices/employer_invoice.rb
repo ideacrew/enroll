02ae595869e9093198ebcb9db9c92756052b76fa
venkatbhattiprolu <vbhat847@gmail.com>
2017-07-25 11:16:40 -0400

    mails shows Health Connector now

--- app/invoices/employer_invoice.rb
+++ app/invoices/employer_invoice.rb
@@ -59,7 +59,7 @@ class EmployerInvoice
     message_params = {
       sender_id: "admins",
       parent_message_id: @organization.employer_profile.id,
-      from: "DC Health Link",
+      from: Settings.site.short_name,
       to: "Employer Mailbox",
       subject: subject,
       body: body
