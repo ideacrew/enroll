7eb96fc7a34bf9263909a98f1b0769bbc0585a76
Kevin Wei <kevin.tw.wei@gmail.com>
2017-07-26 16:52:10 -0400

    Revert "Merge branch 'hotfix-14606' of github.com:dchbx/enroll into stage"
    This reverts commit 02036745c8c100beab5bfa1ced89d6241a9d88cc, reversing
    changes made to 184239489aabaae2bf68ceb6abb8e088565bb4d5.

--- app/data_migrations/person_add_user.rb
+++
@@ -1,9 +0,0 @@
-require File.join(Rails.root, "lib/mongoid_migration_task")
-
-class PersonAddUser < MongoidMigrationTask
-  def migrate
-    user = User.where(email:ENV['email']).first
-    person = Person.where(hbx_id:ENV['hbx_id']).first
-    person.update(user_id:user.id)
-  end
-end
