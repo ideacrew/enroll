8b1c9594a7a98635bb98beba4f4e6b92a0c19ab8
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-07-26 11:29:05 -0400

    Refs #15172 adding new logging statements for testing purposes

--- app/models/subscribers/nfp_statement_history.rb
+++ app/models/subscribers/nfp_statement_history.rb
@@ -15,11 +15,16 @@ module Subscribers
       begin
         stringed_key_payload = payload.stringify_keys
         xml = stringed_key_payload['body']
+        header = stringed_key_payload['header']
 
         Rails.logger.info "==================="
         Rails.logger.info "Enroll received nfp_statement_summary_success"
         Rails.logger.info xml.to_s
+        Rails.logger.info header.to_s
         Rails.logger.info "==================="
+
+        # ep = Organization.where("hbx_id" = > header[:customer_id]).first.employer_profile
+        # ep.employer_profile_account.next_premium_amount = 9999
 
         #TODO change response handler
         if "503" == return_status.to_s
