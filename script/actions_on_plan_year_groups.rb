553a9c3ea3baa6f22892395380598df60c33a393
nisanth <nisanth.yaganti@gmail.com>
2017-07-26 15:26:14 -0400

    refs #12834 keeping script inline with changes in data

--- script/actions_on_plan_year_groups.rb
+++ script/actions_on_plan_year_groups.rb
@@ -14,25 +14,34 @@ FEINS.each do |fein|
       Family.where(:"households.hbx_enrollments.benefit_group_id".in => bg_list).each do |family|
         enrollments = family.active_household.hbx_enrollments.where(:benefit_group_id.in => bg_list).to_a
         if enrollments.present?
-          puts "caneled 2017 enrollments for #{organization.legal_name}"
+          puts "caneled 2017 enrollments for family of #{family.primary_applicant.person.full_name} with ER: #{organization.legal_name}"
         end
 
         enrollments.each do |enrollment|
           enrollment.cancel_coverage! if enrollment.may_cancel_coverage?
         end
       end
-
+      # cannot use state machine events, b'coz plan year is in active status
       plan_year.update_attributes(aasm_state: 'renewing_canceled')
       puts "moved 2017 plan year state to renewing canceled"
     end
 
-    if organization.employer_profile.is_coversion_employer?
-      plan_year = organization.employer_profile.plan_years.detect { |py| py.start_on.year == 2016}
-      if plan_year.present? && plan_year.may_conversion_expire?
+    plan_year = organization.employer_profile.plan_years.detect { |py| py.start_on.year == 2016 && py.is_conversion }
+
+    if plan_year.present?
+      if plan_year.may_conversion_expire?
         plan_year.conversion_expire!
-        puts 'moved 2016 plan year state to migration expired'
+        puts 'moved 2016 plan year state to conversion expired'
       end
     end
     organization.employer_profile.revert_application! if organization.employer_profile.may_revert_application?
   end
 end
+
+HbxEnrollment.by_hbx_id('412161').first.update_attributes(terminated_on: Date.new(2016,12,31))
+
+Organization.where(legal_name: /Early Autism Solutions/i).first.employer_profile.plan_years.where(start_on: Date.new(2016,1,1)).first.update_attributes(:terminated_on => Date.new(2016,12,31), aasm_state: 'terminated')
+
+CensusEmployee.where(first_name: "Sofia", middle_name: "", last_name: "Caldwell").first.update_attributes(employment_terminated_on: Date.new(2016,12,31), coverage_terminated_on: Date.new(2016,12,31))
+
+CensusEmployee.where(first_name: "Sofia", middle_name: "", last_name: "Caldwell").first.terminate_employee_role!
