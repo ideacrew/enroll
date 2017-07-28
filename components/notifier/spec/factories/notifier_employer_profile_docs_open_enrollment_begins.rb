f75c6480a90ea0957d21ceaf2622aca299440290
Dan Thomas <dan.thomas@dc.gov>
2017-07-28 15:08:47 -0400

    add initial document types

---
+++ components/notifier/spec/factories/notifier_employer_profile_docs_open_enrollment_begins.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :notifier_employer_profile_docs_open_enrollment_begin, class: 'Notifier::EmployerProfileDocs::OpenEnrollmentBegin' do
+    
+  end
+end
---
+++ components/notifier/spec/models/notifier/employee_role_docs/open_enrollment_begin_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Notifier
+  RSpec.describe EmployeeRoleDocs::OpenEnrollmentBegin, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
