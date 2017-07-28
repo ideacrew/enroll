b29f660f31795ebd0b68eb69ca79e3d8ea4693c7
Dan Thomas <dan.thomas@dc.gov>
2017-07-28 15:07:36 -0400

    Add initial merge_data_models

---
+++ components/notifier/app/models/notifier/merge_data_models/plan.rb
@@ -0,0 +1,16 @@
+module Notifier
+  class MergeDataModels::Plan
+    include Virtus.model
+
+    attribute :open_enrollment_start_on, Date
+    attribute :open_enrollment_end_on, Date
+    attribute :coverage_start_on, Date
+    attribute :coverage_end_on, Date
+    attribute :plan_name, String
+    attribute :metal_level, String
+    attribute :coverage_kind, String
+    attribute :plan_carrier, String
+    attribute :hsa_plan, Boolean
+    attribute :renewal_plan_type, String
+  end
+end
