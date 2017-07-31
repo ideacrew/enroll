66a8a93a0607eec96cb7f9be288e1a93f61e4097
Brian Weiner <brian.weiner@dc.gov>
2017-07-31 16:12:59 -0400

    ref#17409 - added division calculation for composite rated plan years

--- app/models/composite_rated_plan_cost_decorator.rb
+++ app/models/composite_rated_plan_cost_decorator.rb
@@ -7,15 +7,45 @@ class CompositeRatedPlanCostDecorator < SimpleDelegator
   end
 
   def employer_contribution_for(member)
-    0.00
+    member_family_size = member.family.family_members.count
+    return contribution_for_subscriber(member_family_size) if member.is_subscriber?
+
+    (0...(member_family_size-1)).inject(total_employer_contribution) do |contribution, n|
+      contribution - contribution_for_subscriber(member_family_size)
+    end
   end
 
   def employee_cost_for(member)
-    0.00
+    member_family_size = member.family.family_members.count
+    return cost_for_subscriber(member_family_size) if member.is_subscriber?
+
+    (0...(member_family_size-1)).inject(total_employee_cost) do |dependent_cost, n|
+      dependent_cost - cost_for_subscriber(member_family_size)
+    end
+  end
+
+  def premium_for(member)
+    member_family_size = member.family.family_members.count
+    return premium_for_subscriber(member_family_size) if member.is_subscriber?
+    (0...(member_family_size-1)).inject(total_premium) do |dependent_share, n|
+      dependent_share - premium_for_subscriber(member_family_size)
+    end
+  end
+
+  def premium_for_subscriber(member_family_size)
+    (total_premium / member_family_size).round(2)
+  end
+
+  def contribution_for_subscriber(member_family_size)
+    (total_employer_contribution / member_family_size).round(2)
+  end
+
+  def cost_for_subscriber(member_family_size)
+    (total_employee_cost / member_family_size).round(2)
   end
 
   def total_premium
-    @benefit_group.composite_rating_tier_premium_for(@composite_rating_tier)
+    @total_premium ||= @benefit_group.composite_rating_tier_premium_for(@composite_rating_tier)
   end
 
   def total_employer_contribution
@@ -24,10 +54,6 @@ class CompositeRatedPlanCostDecorator < SimpleDelegator
 
   def total_employee_cost
     (total_premium - total_employer_contribution).round(2)
-  end
-
-  def premium_for(_member)
-    0.00
   end
 
   def employer_contribution_factor
