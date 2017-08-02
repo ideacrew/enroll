f4bcc84f58c31f3e7f78e4b70b989ba48e3b29b8
Pranab <pranab.khanal@gmail.com>
2017-08-02 10:05:04 -0400

    hide edit form on cancel button when editing Primary Member

--- app/assets/javascripts/financial_assistance/fa_integration.js
+++ app/assets/javascripts/financial_assistance/fa_integration.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   if ($("#origin_source").val() == "fa_application_edit_add_member")  {
     $('#household_info_add_member').click();
   }
+
+  $("body").on("click", ".close_member_form", function () {
+    $(".append_consumer_info").html("");
+  });
 });
