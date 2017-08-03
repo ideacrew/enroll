344537d16f3509c9c2d4fae9583424dad0890799
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-08-03 12:10:16 -0400

    refs #15172 final tweaks hopefully

--- app/models/employer_profile_account.rb
+++ app/models/employer_profile_account.rb
@@ -8,6 +8,15 @@ class EmployerProfileAccount
 
   field :next_premium_due_on, type: Date
   field :next_premium_amount, type: Money
+  # temp fields
+  field :message, type: String
+  field :past_due, type: String
+  field :previous_balance, type: String
+  field :new_charges, type: String
+  field :adjustments, type: String
+  field :payments, type: String
+  field :total_due, type: String
+
   field :aasm_state, type: String, default: "binder_pending"
 
   embeds_many :premium_payments
@@ -19,6 +28,11 @@ class EmployerProfileAccount
 
   scope :active,      ->{ not_in(aasm_state: %w(canceled terminated)) }
 
+  def nfp_process
+    # if past_due > 0
+    #   #trans to past_due
+    #   self.
+  end
 
   def last_premium_payment
     return premium_payments.first if premium_payments.size == 1
@@ -60,6 +74,12 @@ class EmployerProfileAccount
     event :allocate_binder_payment, :after => :record_transition do
       transitions from: :binder_pending, to: :binder_paid
     end
+
+    event :invoice  do
+      transitions from: :binder_pending, to: :invoiced
+    end
+
+
 
     # A new billing period begins the first day of each month
     event :advance_billing_period, :guard => :first_day_of_month?, :after => :record_transition do
