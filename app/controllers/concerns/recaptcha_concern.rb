dea8f7d8ad9cff076868f098800ecb2b0849ed0a
Brian Weiner <brian.weiner@dc.gov>
2017-08-04 12:32:17 -0400

    ref#17597 recaptcha shouldnt run if it isnt needed

--- app/controllers/concerns/recaptcha_concern.rb
+++ app/controllers/concerns/recaptcha_concern.rb
@@ -7,14 +7,16 @@ module RecaptchaConcern
     private
 
     def check_captcha
-      unless verify_recaptcha
-        case self.class.to_s
-        when 'Users::RegistrationsController'
+      case self.class.to_s
+      when 'Users::RegistrationsController'
+        unless verify_recaptcha
           self.resource = resource_class.new sign_up_params
           resource.oim_id = params[:user][:oim_id]
           respond_with_navigational(resource) { render :new }
-        when 'Users::SessionsController'
-          if User.login_captcha_required?(params[:user][:login])
+        end
+      when 'Users::SessionsController'
+        if User.login_captcha_required?(params[:user][:login])
+          unless verify_recaptcha
             self.resource = resource_class.new
             resource.login = params[:user][:login]
             respond_with_navigational(resource) { render :new }
