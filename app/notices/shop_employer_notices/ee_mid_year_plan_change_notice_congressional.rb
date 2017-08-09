89122c70109eb4958e8be522c3a794c198ccddf5
harshaellanki <h_vardhan@yahoo.com>
2017-08-09 14:23:13 -0400

    refs #15794 fixed specs and refactored code

--- app/notices/shop_employer_notices/ee_mid_year_plan_change_notice_congressional.rb
+++ app/notices/shop_employer_notices/ee_mid_year_plan_change_notice_congressional.rb
@@ -40,6 +40,7 @@ class ShopEmployerNotices::EeMidYearPlanChangeNoticeCongressional < Notice
     append_address(census_employee.employer_profile.organization.primary_office_location.address)
     append_hbe
     append_broker(census_employee.employer_profile.broker_agency_profile)
+    notice.mpi_indicator = self.mpi_indicator
   end
 
   def append_hbe
