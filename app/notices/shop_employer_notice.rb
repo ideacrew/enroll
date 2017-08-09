8a348618bd314eb0b43132698a861fc44b7c55a3
Battula <vrbattula@outlook.com>
2017-08-09 16:34:18 -0400

    refs #17068.

--- app/notices/shop_employer_notice.rb
+++ app/notices/shop_employer_notice.rb
@@ -13,7 +13,7 @@ class ShopEmployerNotice < Notice
     args[:name] = employer_profile.staff_roles.first.full_name.titleize
     args[:recipient_document_store]= employer_profile
     self.key = args[:key]
-    self.header = "notices/shared/header_with_page_numbers.html.erb"
+    self.header = "notices/shared/shop_header.html.erb"
     super(args)
   end
 
