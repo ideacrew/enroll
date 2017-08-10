ccec069f6949adec3874718bcad68a4980bc82a2
sudhirreddysama <sudhirsama586@gmail.com>
2017-08-10 10:43:33 -0400

    refs #15793 rendered cover and header to template

--- app/notices/pdf_templates/employer_notice.rb
+++ app/notices/pdf_templates/employer_notice.rb
@@ -31,7 +31,6 @@ module PdfTemplates
     attribute :benefit_group_assignments, Hash
     attribute :plan_year, PdfTemplates::PlanYear
     attribute :sep, PdfTemplates::SpecialEnrollmentPeriod
-    attribute :mpi_indicator, String
 
     def shop?
       return true
