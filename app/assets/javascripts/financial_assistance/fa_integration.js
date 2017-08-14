ac51c5655ace73fd4d36116cc40d6dbb9720324d
saikumar9 <saikumarkotagiri9@gmail.com>
2017-08-13 23:03:27 -0400

    Ajax request to read the http code sent by Haven

--- app/assets/javascripts/financial_assistance/fa_integration.js
+++ app/assets/javascripts/financial_assistance/fa_integration.js
@@ -10,26 +10,29 @@ $(document).ready(function() {
 
   // Check for Eligibility Result response every "interval_time" seconds a total of "number_of_times" times.
   if (/wait_for_eligibility_response/.test(window.location.href)) {
+    var i = 1;
     var number_of_times = 10;
     var interval_time = 2000;
-
-    for (var i = 0; i < number_of_times; i++) {
-      setTimeout(function () {
+    var repeater = setInterval(function () {
+      if ( i < number_of_times) {
         $.ajax({
           type: "GET",
           data:{},
           url: window.location.href.replace(/wait_for_eligibility_response/, "check_eligibility_results_received"),
           success: function (response_received_flag) {
-            if (response_received_flag == "true")
+            if (response_received_flag == "true"){
               // redirect to the existing eligibility_results page
               window.location = window.location.href.replace(/wait_for_eligibility_response/, "eligibility_results")
-            else
-              // redirect to error page
-              window.location = window.location.href.replace(/wait_for_eligibility_response/, "eligibility_response_error")
+            }
           }
         });
-      }, i * interval_time)
-    }
+        i += 1;
+      } else {
+        clearInterval(repeater);
+        if ( i > 9 ){
+          window.location = window.location.href.replace(/wait_for_eligibility_response/, "eligibility_response_error")
+        }
+      }
+    }, interval_time);
   }
-
 });
