a16b0759b3940ef6e2b67c83d7bb390d233bc987
harsha chalamala <chvreddy1015@gmail.com>
2017-08-16 12:23:13 -0400

    refs 17506 missing end added

--- app/helpers/config/site_helper.rb
+++ app/helpers/config/site_helper.rb
@@ -45,6 +45,7 @@ module Config::SiteHelper
 
   def site_registration_path(resource_name, params)
     Settings.site.registration_path.present? ? Settings.site.registration_path : new_registration_path(resource_name, :invitation_id => params[:invitation_id])
+  end
 
   def site_long_name
     Settings.site.long_name
