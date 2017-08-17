7813d4c786bdd6f7e64b3bd58d5e6263b0fdf2aa
202chaz <chaz.haskins@gmail.com>
2017-08-17 09:20:07 -0400

    Refs Employee receiving 500 error when attempting to match User to Person

--- app/data_migrations/remove_congress_census_employee.rb
+++ app/data_migrations/remove_congress_census_employee.rb
@@ -2,11 +2,14 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class RemoveCongressCensusEmployee < MongoidMigrationTask
   def migrate
     begin
-      ce = CensusEmployee.find(ENV['census_employee_id'])
-      if ce.present?
+      ce = CensusEmployee.where(id:ENV['census_employee_id']).first
+      if ce.present? && ce.employer_profile.active_plan_year.benefit_groups.any?{|bg| bg.is_congress}
         ce.delete
-      else
+        puts "Census Employee was successfully removed."
+      elsif ce.blank?
         puts "Unable to find a Census Employee with ID provided."
+      elsif ce.present? && !ce.employer_profile.active_plan_year.benefit_groups.any?{|bg| bg.is_congress}
+        puts "Census Employee is not a congress employee."
       end
     end
   end
--- spec/data_migrations/remove_congress_census_employee_spec.rb
+++ spec/data_migrations/remove_congress_census_employee_spec.rb
@@ -16,19 +16,25 @@ describe RemoveCongressCensusEmployee, dbclean: :after_each do
     let(:employee_role) {FactoryGirl.create(:employee_role, person: person, employer_profile: employer_profile)}
     let(:census_employee) { FactoryGirl.create(:census_employee, employee_role_id: employee_role.id, employer_profile_id: employer_profile.id) }
     let!(:plan_year) { FactoryGirl.create(:plan_year, employer_profile: employer_profile, start_on: TimeKeeper.date_of_record.beginning_of_year, :aasm_state => 'published' ) }
-    let!(:active_benefit_group) { FactoryGirl.create(:benefit_group, plan_year: plan_year, title: "Benefits #{plan_year.start_on.year}") }
+    let!(:active_benefit_group) { FactoryGirl.create(:benefit_group, is_congress: false, plan_year: plan_year, title: "Benefits #{plan_year.start_on.year}") }
     let(:benefit_group_assignment)  { FactoryGirl.create(:benefit_group_assignment, benefit_group: active_benefit_group, census_employee: census_employee) }
     
       before :each do
         allow(ENV).to receive(:[]).with('census_employee_id').and_return census_employee.id
       end
       
-      it "should change dot of ce not in employment termination state" do
-        expect(census_employee.employment_terminated_on).to eq Date.new(2016,01,01)
-        expect(census_employee.aasm_state).to eq "employment_terminated"
-
+      it "should not remove census employee if they are NOT is_congress" do
+        expect(employer_profile.census_employees.count).to eq 1
+        subject.migrate
+        expect(employer_profile.census_employees.count).to eq 1
+      end
+      
+      it "should do remove census employee if benefit_group is_congress is set to true" do
+        active_benefit_group.update(is_congress:true)
+        expect(employer_profile.census_employees.count).to eq 1
+        subject.migrate
+        expect(employer_profile.census_employees.count).to eq 0
       end
     end
-
    
 end
