b41cc49b54b9d5a1d94f0ab953a899b86d88c2b6
Trey Evans <lewis.r.evans@gmail.com>
2017-08-17 15:56:22 -0400

    Refs #17878.  Fixes typo.

--- components/transport_profiles/app/models/transport_profiles/processes/legacy/push_employee_terminations_report.rb
+++ components/transport_profiles/app/models/transport_profiles/processes/legacy/push_employee_terminations_report.rb
@@ -1,5 +1,5 @@
 module TransportProfiles
-  class Processes::PushEmployeeTerminationsReport < Processes::Process
+  class Processes::Legacy::PushEmployeeTerminationsReport < Processes::Process
     def initialize(report_file_name, gateway)
       super("Distribute system generated legacy employee terminations report", gateway)
       @report_file_name = report_file_name
