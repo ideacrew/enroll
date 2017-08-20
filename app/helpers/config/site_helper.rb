931e02045cd321dc41115dd4ff06a1246efd82a7
Prem Shaganti <prem.shaganti@dc.gov>
2017-08-20 01:25:05 -0400

    ref:17088 fix broken links using helpers
     Conflicts:
    	app/helpers/config/site_helper.rb conflicts on line 142-164, removed both the changes
    	app/views/user_mailer/invite_future_employee_for_open_enrollment.html.erb conflicts on line 29-33 and 60-64, kept the branch changes

--- app/helpers/config/site_helper.rb
+++ app/helpers/config/site_helper.rb
@@ -138,9 +138,4 @@ module Config::SiteHelper
   def site_document_verification_checklist_url
     Settings.site.document_verification_checklist_url
   end
-
-  def site_main_web_address_text
-   Settings.site.main_web_address_text
-  end
-
 end
