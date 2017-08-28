1bb7c6bfbe69a4f6d1e8bcedfcb74006f38755a4
saikumar9 <saikumarkotagiri9@gmail.com>
2017-08-28 16:03:19 -0400

    Removed Unused Code

--- app/models/subscribers/financial_assistance_application_eligibility_response.rb
+++ app/models/subscribers/financial_assistance_application_eligibility_response.rb
@@ -33,10 +33,6 @@ module Subscribers
       sc.call(xml)
     end
 
-    def ecase_id_valid?(family, verified_family)
-      !family.e_case_id.present? || (family.e_case_id.include? "curam_landing") || family.e_case_id == verified_family.integrated_case_id
-    end
-
     def haven_import_from_xml(xml)
       verified_family = Parsers::Xml::Cv::HavenVerifiedFamilyParser.new
       verified_family.parse(xml)
@@ -81,84 +77,13 @@ module Subscribers
       application_in_context.determine!
     end
 
-    def update_vlp_for_consumer_role(consumer_role, verified_primary_family_member )
-      begin
-        verified_verifications = verified_primary_family_member.verifications
-        consumer_role.import
-        consumer_role.pass_ssn
-        consumer_role.vlp_authority = "curam"
-        consumer_role.residency_determined_at = verified_primary_family_member.created_at
-        consumer_role.citizen_status = verified_verifications.citizen_status.split('#').last
-        consumer_role.is_state_resident = verified_verifications.is_lawfully_present
-        consumer_role.is_incarcerated = verified_primary_family_member.person_demographics.is_incarcerated
-        consumer_role.save!
-        if consumer_role.person.user.present?
-          consumer_role.person.user.ridp_by_payload!
-        end
-      rescue => e
-        errors_list = consumer_role.errors.full_messages + [e.message] + e.backtrace
-        throw(:processing_issue, "Unable to update consumer vlp: #{errors_list.join("\n")}")
-      end
-    end
-
-    def import_home_address(person, verified_new_address)
-      verified_address_hash = verified_new_address.to_hash
-      verified_address_hash.delete(:country)
-      new_address = Address.new(
-        verified_address_hash
-      )
-      throw(:processing_issue, "ERROR: Failed to load home address from xml") unless new_address.valid?
-      if person.home_address.present?
-        person.home_address.delete
-      end
-      person.addresses << new_address
-      person.save!
-    end
-
-    def find_or_create_new_members(verified_dependents, verified_primary_family_member)
-      new_people = []
-      if verified_dependents.present?
-        verified_dependents.each do |verified_family_member|
-          existing_person = search_person(verified_family_member)
-          successor_id = Person.where(hbx_id: verified_family_member.person.hbx_id).first.id
-          primary_person = Person.where(hbx_id: verified_primary_family_member.person.hbx_id).first
-          relationship = primary_person.person_relationships.where(successor_id: successor_id).first.kind
-          if existing_person.present?
-            find_or_build_consumer_role(existing_person)
-            new_people << [existing_person, relationship, [verified_family_member.id]]
-          end
-        end
-      end
-      new_people
-    end
-
-    def update_broker_for_family(family, npn)
-      broker = BrokerRole.find_by_npn(npn)
-      if broker.present?
-        if broker.broker_agency_profile_id.present?
-          family.hire_broker_agency(broker.id)
-        else
-          throw(:processing_issue, "ERROR: Broker with npn: #{npn} has no broker agency profile id")
-        end
-      else
-        throw(:processing_issue, "ERROR: Failed to match broker with npn: #{npn}")
-      end
-    end
-
-    def find_or_build_consumer_role(person)
-      return person.consumer_role if person.consumer_role.present?
-      person.build_consumer_role(is_applicant: true)
-    end
-
     def search_person(verified_family_member)
       ssn = verified_family_member.person_demographics.ssn
       ssn = '' if ssn == "999999999"
       dob = verified_family_member.person_demographics.birth_date
-      dob = Date.strptime( dob , '%Y%m%d')
       last_name_regex = /^#{verified_family_member.person.name_last}$/i
       first_name_regex = /^#{verified_family_member.person.name_first}$/i
 
-      #Verify how it is comparing the dob string to a date format.
       if !ssn.blank?
         Person.where({
           :encrypted_ssn => Person.encrypt_ssn(ssn),
