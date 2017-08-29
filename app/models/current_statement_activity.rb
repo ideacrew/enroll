36c8a8c3e863132a172bc3dc9c2f6898db8de6db
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-08-29 16:07:48 -0400

    Refs #15172 refactor of current payments

--- app/models/current_statement_activity.rb
+++ app/models/current_statement_activity.rb
@@ -4,13 +4,12 @@ class CurrentStatementActivity
 
   embedded_in :employer_profile_account
 
-  MethodKinds = %w(ach credit_card check)
-
   field :description, type: String
   field :name, type: String
   field :type, type: String
   field :posting_date, type: Date
   field :amount, type: Money
   field :coverage_month, type: Date
+  field :payment_method, type: String
 
 end
