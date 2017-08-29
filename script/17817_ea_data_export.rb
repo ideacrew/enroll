e189b12214cfe4a5602d3fe12bd256cbaaa5198a
Battula <vrbattula@outlook.com>
2017-08-29 11:58:01 -0400

    refs #17817 Include catastrophic plans, add plan name to the report.

--- script/17817_ea_data_export.rb
+++ script/17817_ea_data_export.rb
@@ -3,14 +3,13 @@ offset = 0
 family_count = Family.count
 
 csv = CSV.open("17817_export_ea_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv", "w")
-csv << %w(policy.id policy.subscriber.coverage_start_on policy.aasm_state policy.plan.coverage_kind policy.plan.metal_level
-        policy.subscriber.person.hbx_id policy.subscriber.person.is_incarcerated  policy.subscriber.person.citizen_status
-        policy.subscriber.person.is_dc_resident? dependent.hbx_id person.is_incarcerated
-        dependent.citizen_status dependent.is_dc_resident?)
+csv << %w(family.id policy.id policy.subscriber.coverage_start_on policy.aasm_state policy.plan.coverage_kind policy.plan.metal_level policy.plan.plan_name policy.subscriber.person.hbx_id
+        policy.subscriber.person.is_incarcerated  policy.subscriber.person.citizen_status
+        policy.subscriber.person.is_dc_resident? is_dependent)
 
 
 def add_to_csv(csv, policy, person, is_dependent)
-  csv << [policy.hbx_id, policy.effective_on, policy.aasm_state, policy.plan.coverage_kind, policy.plan.metal_level, person.hbx_id,
+  csv << [policy.family.id, policy.hbx_id, policy.effective_on, policy.aasm_state, policy.plan.coverage_kind, policy.plan.metal_level, policy.plan.name, person.hbx_id,
           person.is_incarcerated, person.citizen_status,
           person.is_dc_resident?] + [is_dependent]
 end
@@ -21,24 +20,18 @@ while offset < family_count
       next if policy.plan.nil?
       next if policy.effective_on < Date.new(2017, 01, 01)
       next if !policy.is_active?
-      next if policy.plan.csr_variant_id != '01'
-      next if (policy.plan.coverage_kind != 'health') || (policy.plan.metal_level == "catastrophic") ||
-          (!(['unassisted_qhp', 'individual'].include? policy.kind)) || policy.family.has_aptc_hbx_enrollment?
+      next if !(['01', '03'].include?(policy.plan.csr_variant_id))
+      next if policy.plan.market != 'individual'
+      next if (!(['unassisted_qhp', 'individual'].include? policy.kind)) || policy.family.has_aptc_hbx_enrollment?
 
       person = policy.subscriber.person
 
-      #add_to_csv(csv, policy, person, false)
-      row = [policy.family.id, policy.hbx_id, policy.effective_on, policy.aasm_state, policy.plan.coverage_kind, policy.plan.metal_level, person.hbx_id,
-             person.is_incarcerated, person.citizen_status,
-             person.is_dc_resident?]
+      add_to_csv(csv, policy, person, false)
 
-      # policy.hbx_enrollment_members.each do |hbx_enrollment_member|
-      #   next if hbx_enrollment_member.person == person
-      #   row = row + [hbx_enrollment_member.person.hbx_id,
-      #                hbx_enrollment_member.person.is_incarcerated, hbx_enrollment_member.person.citizen_status,
-      #                hbx_enrollment_member.person.is_dc_resident?]
-      # end
-      csv << row
+      policy.hbx_enrollment_members.each do |hbx_enrollment_member|
+        add_to_csv(csv, policy, hbx_enrollment_member.person, true) if hbx_enrollment_member.person != person
+      end
+
     rescue => e
       puts "Error policy id #{policy.id} family id #{policy.family.id}" + e.message + "   " + e.backtrace.first
     end
