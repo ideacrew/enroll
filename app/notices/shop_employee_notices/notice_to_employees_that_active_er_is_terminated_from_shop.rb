c020ac53ad8c617194bcd8f907fd4513e6d24b4e
harsha280 <harsha.vskr@gmail.com>
2017-08-31 10:51:33 -0400

    Refs 17651 Notice to EEs that active ER is terminated from SHOP

---
+++ app/notices/shop_employee_notices/notice_to_employees_that_active_er_is_terminated_from_shop.rb
@@ -0,0 +1,19 @@
+class ShopEmployeeNotices::NoticeToEmployeesThatActiveErIsTerminatedFromShop < ShopEmployeeNotice
+
+  def deliver
+    build
+    append_data
+    generate_pdf_notice
+    attach_envelope
+    non_discrimination_attachment
+    upload_and_send_secure_message
+    send_generic_notice_alert
+  end
+
+  def append_data
+    active_plan_year = census_employee.employer_profile.plan_years.first
+    notice.plan_year = PdfTemplates::PlanYear.new({
+                      :start_on => active_plan_year.start_on
+      })
+  end
+end
