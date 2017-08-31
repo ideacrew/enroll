7d5ca7e5cb0c6f88c205fa888de0c09196f14b3e
Trey Evans <lewis.r.evans@gmail.com>
2017-08-31 19:04:04 -0400

    Adding tracing for async.

--- script/remote_event_listener.rb
+++ script/remote_event_listener.rb
@@ -1,3 +1,11 @@
+if defined?(NewRelic)
+  require 'new_relic/agent/method_tracer'
+  Acapi::Publishers::UpstreamEventPublisher.class_eval do
+    include ::NewRelic::Agent::MethodTracer
+    add_method_tracer :handle_message
+  end
+end
+
 listener = Acapi::Publishers::UpstreamEventPublisher.new
 listener.register_subscribers!
 listener.run
