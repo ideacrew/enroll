83e13b1bd9cab5a46dc2ada68708e8bd0d421c3c
Vishal Kalletla <vishuk1201@gmail.com>
2017-09-05 15:15:17 -0400

    code refactor for aptc_spec & updated specs for plan_filters

--- spec/controllers/concerns/aptc_spec.rb
+++ spec/controllers/concerns/aptc_spec.rb
@@ -6,37 +6,16 @@ class FakesController < ApplicationController
 end
 
 RSpec.describe FakesController, :type => :controller do
-
-  let(:household) { FactoryGirl.build_stubbed(:household, family: family) }
-  let(:family) { FactoryGirl.build_stubbed(:family, :with_primary_family_member, person: person )}
-  let(:application) { FactoryGirl.create(:application, family: family) }
-  let(:hbx_enrollment_one) { FactoryGirl.build_stubbed(:hbx_enrollment, household: household) }
-
   before :each do
     allow_any_instance_of(FinancialAssistance::Application).to receive(:set_benchmark_plan_id)
     allow(person).to receive_message_chain("primary_family.enrolled_hbx_enrollments").and_return([hbx_enrollment_one])
     allow(person.primary_family).to receive(:active_approved_application).and_return(application)
   end
 
-  describe FakesController do
-
-    let(:person) {FactoryGirl.build(:person)}
-
-    context "#get_shopping_tax_household_from_person" do
-      it "should get nil without person" do
-        expect(subject.get_shopping_tax_households_from_person(nil, 2017)).to eq nil
-      end
-
-      it "should get nil when person without consumer_role" do
-        allow(person).to receive(:has_active_consumer_role?).and_return true
-        expect(subject.get_shopping_tax_households_from_person(person, 2017)).to eq nil
-      end
-    end
-
-    describe "get_tax_household_from_family_members" do
-
-      let!(:person1) {FactoryGirl.create(:person, :with_family, :with_consumer_role)}
-      let!(:family)  { person1.primary_family}
+      let!(:person) {FactoryGirl.create(:person, :with_family, :with_consumer_role)}
+      let!(:family)  { person.primary_family}
+      let(:application) { FactoryGirl.create(:application, family: family) }
+      let(:hbx_enrollment_one) { FactoryGirl.build_stubbed(:hbx_enrollment, household: household) }
       let!(:primary_member) { family.primary_applicant}
       let!(:family_member1) { FactoryGirl.create(:family_member, family: family) }
       let!(:family_member2) { FactoryGirl.create(:family_member, family: family) }
@@ -48,15 +27,32 @@ RSpec.describe FakesController, :type => :controller do
       let!(:applicant2) { FactoryGirl.create(:applicant, tax_household_id: tax_household2.id, application: application, family_member_id: family_member1.id) }
       let!(:applicant3) { FactoryGirl.create(:applicant, tax_household_id: tax_household3.id, application: application, family_member_id: family_member2.id) }
 
+  describe FakesController do
+    context "#get_shopping_tax_household_from_person" do
+      it "should get nil without person" do
+        expect(subject.get_shopping_tax_households_from_person(nil, 2017)).to eq nil
+      end
+
+      it "should get taxhousehold with person" do
+        expect(subject.get_shopping_tax_households_from_person(person, 2017)).to eq tax_household1
+      end
+
+      it "should get nil when person without consumer_role" do
+        allow(person).to receive(:has_active_consumer_role?).and_return false
+        expect(subject.get_shopping_tax_households_from_person(person, 2017)).to eq nil
+      end
+    end
+
+    describe "get_tax_household_from_family_members" do
       context "get_tax_household_from_family_members" do
         it "should return all the tax housholds of the given family members" do
-          allow(person1).to receive(:has_active_consumer_role?).and_return true
+          allow(person).to receive(:has_active_consumer_role?).and_return true
           family_member_ids = {"0" => primary_member.id.to_s, "1" => family_member1.id.to_s, "2" => family_member2.id.to_s}
-          expect(subject.get_tax_households_from_family_members(person1, family_member_ids)).to eq [tax_household1, tax_household2, tax_household3]
+          expect(subject.get_tax_households_from_family_members(person, family_member_ids)).to eq [tax_household1, tax_household2, tax_household3]
         end
 
         it "should return no tax housholds for no family members" do
-          expect(subject.get_tax_households_from_family_members(person1, [])).to eq []
+          expect(subject.get_tax_households_from_family_members(person, [])).to eq []
         end
       end
 
