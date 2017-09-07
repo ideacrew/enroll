e20511003f6c2d2485712e3e4a74fdd6b4ec4c65
saip7795 <saip7795@gmail.com>
2017-09-07 09:29:55 -0400

    Ref17810 : Minor Change in the condition for trigger

--- app/controllers/insured/plan_shoppings_controller.rb
+++ app/controllers/insured/plan_shoppings_controller.rb
@@ -57,8 +57,7 @@ class Insured::PlanShoppingsController < ApplicationController
 
     @change_plan = params[:change_plan].present? ? params[:change_plan] : ''
     @enrollment_kind = params[:enrollment_kind].present? ? params[:enrollment_kind] : ''
-    employee_mid_year_plan_change(@person) if @change_plan.present? or @enrollment_kind.present?
-
+    employee_mid_year_plan_change(@person)
     send_receipt_emails if @person.emails.first
   end
 
@@ -137,10 +136,12 @@ class Insured::PlanShoppingsController < ApplicationController
 
     def employee_mid_year_plan_change(person)
      begin
-          employee_role_id = person.active_employee_roles.first.census_employee.id
-          if employee_role_id.present?
-         ShopNoticesNotifierJob.perform_later(employee_role_id.to_s, "employee_mid_year_plan_change")
-       end
+      employee_role_id = person.active_employee_roles.first.census_employee.id
+      if employee_role_id.present?
+        if @change_plan.present? or person.active_employee_roles.first.census_employee.new_hire_enrollment_period.present?
+          ShopNoticesNotifierJob.perform_later(employee_role_id.to_s, "employee_mid_year_plan_change")
+        end
+      end
      rescue Exception => e
        log("#{e.message}; person_id: #{person.id}")
      end
