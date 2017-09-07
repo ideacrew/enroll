dc672c40642d05ebfeed219d70c7c0afdd752e10
Mahesh Kurma <maheshruby405@gmail.com>
2017-09-07 17:34:02 -0400

    refs #18120 Specs for benefits,deductions,incomes controllers

--- spec/controllers/financial_asistance/benefits_controller_spec.rb
+++ spec/controllers/financial_asistance/benefits_controller_spec.rb
@@ -1,20 +1,113 @@
 require 'rails_helper'
 
 RSpec.describe FinancialAssistance::BenefitsController, type: :controller do
-	let(:person) { FactoryGirl.create(:person)}
-  let(:family) { FactoryGirl.create(:family, :with_primary_family_member,person: person) }
+  let(:person) { FactoryGirl.create(:person, :with_consumer_role)}
+  let(:user) { double("user", :has_hbx_staff_role? => true, :has_employer_staff_role? => false, :person => person, oim_id: "mahesh.")}
+  let!(:family) { FactoryGirl.create(:family, :with_primary_family_member,person: person) }
+  let!(:plan) { FactoryGirl.create(:plan, active_year: 2017, hios_id: "86052DC0400001-01") }
+  let!(:application) { FactoryGirl.create(:application,family: family, aasm_state: "draft",effective_date:TimeKeeper.date_of_record) }
+  let!(:applicant) { FactoryGirl.create(:applicant, application: application,family_member_id: family.primary_applicant.id) }
+  let!(:benefit) {FactoryGirl.create(:financial_assistance_benefit, applicant: applicant)}
+  let!(:valid_params1) {
+    {
+      "kind"=>"is_eligible",
+      "start_on"=>"09/04/2017",
+      "end_on"=>"09/20/2017",
+      "insurance_kind"=>"child_health_insurance_plan",
+      "esi_covered"=>"self",
+      "employer_name"=>"",
+      "employer_id"=>"",
+      "employee_cost"=>"",
+      "employee_cost_frequency"=>""
+    }
+  }
+  let!(:invalid_params) {
+    {
+      "kind"=>"pp",
+      "start_on"=>"09/04/2017",
+      "end_on"=>"09/20/2017",
+      "insurance_kind"=>"child_health_insurance_plan",
+      "esi_covered"=>"self",
+      "employer_name"=>"",
+      "employer_id"=>"",
+      "employee_cost"=>"",
+      "employee_cost_frequency"=>""
+    }
+  }
+  let(:employer_address){ {"address_1"=>"", "address_2"=>"", "city"=>"", "state"=>"", "zip"=>""}}
+  let(:employer_phone) {{"full_phone_number"=>""}}
 
-  context "deleting a benefit" do
-  	before(:each) do
-  		family.applications.create!
-  	end
+  before do
+    sign_in(user)
+  end
 
-	  it "should remove the benefit instance and decrease count by 1" do
-	  	applicant = family.application_in_progress.applicants.create!({family_member_id: family.primary_applicant.id})
-	    income = applicant.benefits.create!({kind: "employer_sponsored_insurance", employee_cost: 1000, start_on: TimeKeeper.date_of_record, end_on: TimeKeeper.date_of_record + 7.days})
-	    expect(applicant.benefits.size).to eq 1
-	    income.destroy
-	    expect(applicant.benefits.size).to eq 0
-	  end
- 	end
+  context "GET index" do
+    it "should render template financial assistance" do
+      get :index, application_id: application.id , applicant_id: applicant.id
+      expect(response).to render_template(:financial_assistance)
+    end
+  end
+
+  context "POST new" do
+    it "should load template work flow steps" do
+      post :new, application_id: application.id , applicant_id: applicant.id
+      expect(response).to render_template(:financial_assistance)
+      expect(response).to render_template 'workflow/step'
+    end
+  end
+
+  context "POST step" do
+    before do
+      controller.instance_variable_set(:@modal, application)
+    end
+
+    it "should show flash error message nil" do
+      expect(flash[:error]).to match(nil)
+    end
+
+    it "should render step if no key present in params with modal_name" do
+      post :step, application_id: application.id , applicant_id: applicant.id
+      expect(response).to render_template 'workflow/step'
+    end
+
+    context "when params has application key" do
+      it "When model is saved" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: benefit.id, employer_address: employer_address, employer_phone: employer_phone
+        expect(applicant.save).to eq true
+      end
+      it "should redirect to find_applicant_path when passing params last step" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: benefit.id,benefit: valid_params1, employer_address: employer_address, employer_phone: employer_phone, commit: "CONTINUE", last_step: true
+        expect(response.headers['Location']).to have_content 'benefits'
+        expect(response.status).to eq 302
+        expect(flash[:notice]).to match('Benefit Info Added.')
+        expect(response).to redirect_to(financial_assistance_application_applicant_benefits_path(application, applicant))
+      end
+      it "should not redirect to find_applicant_path when not passing params last step" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: benefit.id,benefit: valid_params1, employer_address: employer_address, employer_phone: employer_phone, commit: "CONTINUE"
+        expect(response.status).to eq 200
+        expect(response).to render_template 'workflow/step'
+      end
+      it "should render workflow/step when we are not params last step" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: benefit.id,benefit: valid_params1, employer_address: employer_address, employer_phone: employer_phone, commit: "CONTINUE"
+        expect(response).to render_template 'workflow/step'
+      end
+    end
+
+    it "should render step if model is not saved" do
+      post :step, application_id: application.id , applicant_id: applicant.id, id: benefit.id, benefit: invalid_params, employer_address: employer_address, employer_phone: employer_phone
+      expect(flash[:error]).to match("Pp Is Not A Valid Benefit Kind Type")
+      expect(response).to render_template 'workflow/step'
+    end
+  end
+
+  context "destroy" do
+    it "should create new benefit" do
+      expect(applicant.benefits.count).to eq 1
+      delete :destroy, application_id: application.id , applicant_id: applicant.id, id: benefit.id
+      applicant.reload
+      expect(applicant.benefits.count).to eq 0
+      expect(flash[:success]).to eq "Benefit deleted - (#{benefit.kind}, #{benefit.insurance_kind})"
+      expect(response).to redirect_to(financial_assistance_application_applicant_benefits_path(application, applicant))
+    end
+  end
 end
--- spec/controllers/financial_asistance/incomes_controller_spec.rb
+++ spec/controllers/financial_asistance/incomes_controller_spec.rb
@@ -1,21 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe FinancialAssistance::IncomesController, type: :controller do
-  let(:person) { FactoryGirl.create(:person)}
-  let(:family) { FactoryGirl.create(:family, :with_primary_family_member,person: person) }
+  let(:person) { FactoryGirl.create(:person, :with_consumer_role)}
+  let(:user) { double("user", :has_hbx_staff_role? => true, :has_employer_staff_role? => false, :person => person, oim_id: "mahesh.")}
+  let!(:family) { FactoryGirl.create(:family, :with_primary_family_member,person: person) }
+  let!(:plan) { FactoryGirl.create(:plan, active_year: 2017, hios_id: "86052DC0400001-01") }
+  let!(:application) { FactoryGirl.create(:application,family: family, aasm_state: "draft",effective_date:TimeKeeper.date_of_record) }
+  let!(:applicant) { FactoryGirl.create(:applicant, application: application,family_member_id: family.primary_applicant.id) }
+  let!(:income) {FactoryGirl.create(:financial_assistance_income, applicant: applicant)}
+  let!(:valid_income_params){ 
+    {"kind"=>"capital_gains", "amount"=>"34.8", "frequency_kind"=>"monthly", "start_on"=>"09/04/2017", "end_on"=>"09/24/2017", "employer_name"=>""}
+  }
+  let!(:invalid_income_params){ 
+    {"kind"=>"ppp", "amount"=>"45.3", "frequency_kind"=>"monthly", "start_on"=>"09/04/2017", "end_on"=>"09/24/2017", "employer_name"=>""}
+  }
+  let(:income_employer_address_params){ {"address_1"=>"23 main st", "address_2"=>"", "city"=>"washington", "state"=>"dc", "zip"=>"12343"}}
+  let(:income_employer_phone_params) {{"full_phone_number"=>""}}
 
-  context "deleting an income" do
-  	before(:each) do
-  		family.applications.create!
-  	end
 
-    it "should remove the income instance and decrease count by 1" do
-    	applicant = family.application_in_progress.applicants.create!({family_member_id: family.primary_applicant.id})
-      income = applicant.incomes.create!({kind: "wages_and_salaries", frequency_kind: "weekly", amount: 1000, start_on: TimeKeeper.date_of_record, end_on: TimeKeeper.date_of_record + 7.days})
-      expect(applicant.incomes.size).to eq 1
-      income.destroy
-      expect(applicant.incomes.size).to eq 0
+  before do
+    sign_in(user)
+  end
+
+  context "GET index" do
+    it "should render template financial assistance" do
+      get :index, application_id: application.id , applicant_id: applicant.id
+      expect(response).to render_template(:financial_assistance)
+    end
+  end
+
+  context "POST new" do
+    it "should load template work flow steps" do
+      post :new, application_id: application.id , applicant_id: applicant.id
+      expect(response).to render_template(:financial_assistance)
+      expect(response).to render_template 'workflow/step'
+    end
+  end
+
+  context "POST step" do
+    before do
+      controller.instance_variable_set(:@modal, application)
+    end
+
+    it "should show flash error message nil" do
+      expect(flash[:error]).to match(nil)
+    end
+
+    it "should render step if no key present in params with modal_name" do
+      post :step, application_id: application.id , applicant_id: applicant.id
+      expect(response).to render_template 'workflow/step'
+    end
+
+    context "when params has application key" do
+      it "When model is saved" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: income.id, income: valid_income_params, employer_phone: income_employer_phone_params
+        expect(applicant.save).to eq true
+      end
+      it "should redirect to find_applicant_path when passing params last step" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: income.id,income: valid_income_params, employer_address: income_employer_address_params, employer_phone: income_employer_phone_params, commit: "CONTINUE", last_step: true
+        expect(response.headers['Location']).to have_content 'incomes'
+        expect(response.status).to eq 302
+        expect(flash[:notice]).to match('Income Added')
+        expect(response).to redirect_to(financial_assistance_application_applicant_incomes_path(application, applicant))
+      end
+      it "should not redirect to find_applicant_path when not passing params last step" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: income.id,income: valid_income_params, employer_address: income_employer_address_params, employer_phone: income_employer_phone_params, commit: "CONTINUE"
+        expect(response.status).to eq 200
+        expect(response).to render_template 'workflow/step'
+      end
+      it "should render workflow/step when we are not params last step" do
+        post :step, application_id: application.id , applicant_id: applicant.id, id: income.id,income: valid_income_params, employer_address: income_employer_address_params, employer_phone: income_employer_phone_params, commit: "CONTINUE"
+        expect(response).to render_template 'workflow/step'
+      end
+    end
+
+    it "should render step if model is not saved" do
+      post :step, application_id: application.id , applicant_id: applicant.id, id: income.id, income: invalid_income_params, employer_address: income_employer_address_params, employer_phone: income_employer_phone_params
+      expect(flash[:error]).to match("Ppp Is Not A Valid Income Type")
+      expect(response).to render_template 'workflow/step'
+    end
+  end
+
+  context "destroy" do
+    it "should create new income" do
+      expect(applicant.incomes.count).to eq 1
+      delete :destroy, application_id: application.id , applicant_id: applicant.id, id: income.id
+      applicant.reload
+      expect(applicant.incomes.count).to eq 0
+      expect(flash[:success]).to eq "Income deleted - (#{income.kind})"
+      expect(response).to redirect_to(financial_assistance_application_applicant_incomes_path(application, applicant))
     end
   end
 end
-
