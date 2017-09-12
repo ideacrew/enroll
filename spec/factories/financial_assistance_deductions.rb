4020168dd3a385d4ebeed0300ac04ded85b09d39
sudhirreddysama <sudhirsama586@gmail.com>
2017-09-11 22:56:12 -0400

    Additional model spec for curam importer

--- spec/factories/financial_assistance_deductions.rb
+++ spec/factories/financial_assistance_deductions.rb
@@ -1,5 +1,13 @@
-FactoryGirl.define do
-  factory :financial_assistance_deduction, class: 'FinancialAssistance::Deduction' do
-    
-  end
-end
+	FactoryGirl.define do
+	  factory :financial_assistance_deduction, class: 'FinancialAssistance::Deduction' do
+
+	  	association :applicant
+
+	   title ‘Test’
+	    amount 10
+	    frequency_kind ‘monthly’
+	    start_on Date.today.beginning_of_month
+	    end_on Date.today.end_of_month
+	    
+	  end
+	end
