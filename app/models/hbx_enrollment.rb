621d78fd90b3bd12630695ef9191998afcaa6eab
Brian Weiner <brian.weiner@dc.gov>
2017-09-12 19:50:28 -0400

    ref#17452 use qhp_plan instead of self.plan

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -808,7 +808,7 @@ class HbxEnrollment
     if self.is_shop?
       if benefit_group.is_congress
         PlanCostDecoratorCongress.new(qhp_plan, self, benefit_group)
-      elsif self.composite_rated? && (!plan.dental?)
+      elsif self.composite_rated? && (!qhp_plan.dental?)
         CompositeRatedPlanCostDecorator.new(qhp_plan, benefit_group, self.composite_rating_tier, is_cobra_status?)
       else
         reference_plan = (coverage_kind == "health") ? benefit_group.reference_plan : benefit_group.dental_reference_plan
@@ -1448,7 +1448,7 @@ class HbxEnrollment
   end
 
   def ee_select_plan_during_oe
-    if is_shop? && self.census_employee.present? 
+    if is_shop? && self.census_employee.present?
       ShopNoticesNotifierJob.perform_later(self.census_employee.id.to_s, "select_plan_year_during_oe")
     end
   end
