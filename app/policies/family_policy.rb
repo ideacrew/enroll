4d1fe4a04cd9aa7a48e704f0b7a3e1058e28b516
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-09-17 22:07:34 -0400

    Refs #18395 removed private key

--- app/policies/family_policy.rb
+++ app/policies/family_policy.rb
@@ -52,6 +52,11 @@ class FamilyPolicy < ApplicationPolicy
     role.permission.can_update_ssn
   end
 
+  def can_view_username_and_email?
+    return false unless role = user.person && user.person.hbx_staff_role
+    role.permission.can_view_username_and_email
+  end
+
   def hbx_super_admin_visible?
     return false unless role = user.person && user.person.hbx_staff_role
     role.permission.can_update_ssn
