5732bc610a2d0bd70ac7e74ddd78156e07da4227
saikumar9 <saikumarkotagiri9@gmail.com>
2017-09-20 12:16:35 -0400

    Benefit Refactor

--- app/assets/javascripts/financial_assistance/benefit.js
+++ app/assets/javascripts/financial_assistance/benefit.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
       $(newBenefitFormEl).find("#financial_assistance_benefit_start_on").datepicker();
       $(newBenefitFormEl).find("#financial_assistance_benefit_end_on").datepicker();
     } else {
+      alert("here");
       // prompt to delete all these benefits
     }
   });
@@ -39,6 +40,48 @@ $(document).ready(function() {
       $(this).parents('.benefit-kind').find('input[type="checkbox"]').prop('checked', false);
     }
     $(this).parents('.new-benefit-form').remove();
+  });
+
+/* Condtional Display Enrolled Benefit Questions */
+  $("#enrolled-benefit-kinds").addClass('hide');
+  $("#enrolled_in_health_coverage_yes").prop('checked', false)
+  $("#enrolled_in_health_coverage_no").prop('checked', false)
+
+  $("body").on("change", "#enrolled_in_health_coverage_yes", function(){
+    if ($('#enrolled_in_health_coverage_yes').is(':checked')) {
+      $("#enrolled-benefit-kinds").removeClass('hide');
+    } else{
+      $("#enrolled-benefit-kinds").addClass('hide');
+    }
+  });
+
+  $("body").on("change", "#enrolled_in_health_coverage_no", function(){
+    if ($('#enrolled_in_health_coverage_no').is(':checked')) {
+      $("#enrolled-benefit-kinds").addClass('hide');
+    } else{
+      $("#enrolled-benefit-kinds").removeClass('hide');
+    }
+  });
+
+/* Condtional Display Eligible Benefit Questions */
+  $("#eligible-benefit-kinds").addClass('hide');
+  $("#eligible_for_health_coverage_yes").prop('checked', false)
+  $("#eligible_for_health_coverage_no").prop('checked', false)
+
+  $("body").on("change", "#eligible_for_health_coverage_yes", function(){
+    if ($('#eligible_for_health_coverage_yes').is(':checked')) {
+      $("#eligible-benefit-kinds").removeClass('hide');
+    } else{
+      $("#eligible-benefit-kinds").addClass('hide');
+    }
+  });
+
+  $("body").on("change", "#eligible_for_health_coverage_no", function(){
+    if ($('#eligible_for_health_coverage_no').is(':checked')) {
+      $("#eligible-benefit-kinds").addClass('hide');
+    } else{
+      $("#eligible-benefit-kinds").removeClass('hide');
+    }
   });
 
 });
