74622260de617fc1a50a91378a55d5c8e1ce3505
saikumar9 <saikumarkotagiri9@gmail.com>
2017-09-21 11:40:22 -0400

    Benefit Refactor, CRUD

--- app/assets/javascripts/financial_assistance/benefit.js
+++ app/assets/javascripts/financial_assistance/benefit.js
@@ -1,10 +1,14 @@
 function stopEditingBenefit() {
   $('input.benefit-checkbox').prop('disabled', false);
+  $('a.benefit-edit').removeClass('disabled');
+  // $('a.benefit-delete').removeClass('disabled');
   $('.col-md-2 > .interaction-click-control-continue').removeClass('disabled');
 };
 
 function startEditingBenefit() {
   $('input.benefit-checkbox').prop('disabled', true);
+  $('a.benefit-edit').addClass('disabled');
+  // $('a.benefit-delete').removeClass('disabled');
   $('.col-md-2 > .interaction-click-control-continue').addClass('disabled');
 };
 
@@ -25,27 +29,63 @@ $(document).ready(function() {
       $(newBenefitFormEl).find("#financial_assistance_benefit_start_on").datepicker();
       $(newBenefitFormEl).find("#financial_assistance_benefit_end_on").datepicker();
     } else {
-      alert("here");
       // prompt to delete all these benefits
     }
   });
 
+  /* edit existing benefits */
+  $('.benefits-list').on('click', 'a.benefit-edit:not(.disabled)', function(e) {
+    e.preventDefault();
+    var benefitEl = $(this).parents('.benefit');
+    benefitEl.find('.benefit-show').addClass('hidden');
+    benefitEl.find('.edit-benefit-form').removeClass('hidden');
+    startEditingBenefit();
+  });
+
+
+  /* destroy existing benefits */
+  $('.benefits-list').on('click', 'a.benefit-delete:not(.disabled)', function(e) {
+    var self = this;
+    e.preventDefault();
+    $("#destroyBenefit").modal();
+
+    $("#destroyBenefit .modal-cancel-button").click(function(e) {
+      $("#destroyBenefit").modal('hide');
+    });
+
+    $("#destroyBenefit .modal-continue-button").click(function(e) {
+      $("#destroyBenefit").modal('hide');
+      $(self).parents('.benefit').remove();
+
+      var url = $(self).parents('.benefit').attr('id').replace('financial_assistance_benefit_', 'benefits/')
+      $.ajax({
+        type: 'delete',
+        url: url
+      })
+    });
+  });
+
   /* cancel benefit edits */
-  $('a.benefit-cancel').click(function(e) {
+  $('.benefits-list').on('click', 'a.benefit-cancel', function(e) {
     e.preventDefault();
     stopEditingBenefit();
 
-    //debugger
-    if (!$(this).parents('.benefits-list > div.benefit').length) {
-      $(this).parents('.benefit-kind').find('input[type="checkbox"]').prop('checked', false);
+    var benefitEl = $(this).parents('.benefit');
+    if (benefitEl.length) {
+      benefitEl.find('.benefit-show').removeClass('hidden');
+      benefitEl.find('.edit-benefit-form').addClass('hidden');
+    } else {
+      if (!$(this).parents('.benefits-list > div.benefit').length) {
+        $(this).parents('.benefit-kind').find('input[type="checkbox"]').prop('checked', false);
+      }
+      $(this).parents('.new-benefit-form').remove();
+      $(this).parents('.edit-benefit-form').remove();
     }
-    $(this).parents('.new-benefit-form').remove();
   });
 
 /* Condtional Display Enrolled Benefit Questions */
-  $("#enrolled-benefit-kinds").addClass('hide');
-  $("#has_enrolled_health_coverage_true").prop('checked', false)
-  $("#has_enrolled_health_coverage_false").prop('checked', false)
+
+  if (!$("#has_enrolled_health_coverage_true").is(':checked')) $("#enrolled-benefit-kinds").addClass('hide');
 
   $("body").on("change", "#has_enrolled_health_coverage_true", function(){
     if ($('#has_enrolled_health_coverage_true').is(':checked')) {
@@ -64,9 +104,7 @@ $(document).ready(function() {
   });
 
 /* Condtional Display Eligible Benefit Questions */
-  $("#eligible-benefit-kinds").addClass('hide');
-  $("#has_eligible_health_coverage_true").prop('checked', false)
-  $("#has_eligible_health_coverage_false").prop('checked', false)
+  if (!$("#has_eligible_health_coverage_true").is(':checked')) $("#eligible-benefit-kinds").addClass('hide');
 
   $("body").on("change", "#has_eligible_health_coverage_true", function(){
     if ($('#has_eligible_health_coverage_true').is(':checked')) {
