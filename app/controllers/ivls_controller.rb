a176483240158c4fd19adcb7ec95ed7993eeabb8
sarathpyerra <sarathpyerra@gmail.com>
2017-09-21 16:34:17 -0400

    Refs #18340 final UI for Covered Lives -Analytics

--- app/controllers/ivls_controller.rb
+++ app/controllers/ivls_controller.rb
@@ -1,21 +1,20 @@
 class IvlsController < ApplicationController
 
   def home
-    # @enrollments=IvlCovered::CoveredLivesMonth.all
-    # @seps=IvlCovered::SepType.all
-    # @discontinuedreinstatedlives=IvlCovered::SepType.all
-    # @totalaccounts=IvlCovered::TotalAccounts.all
-    # @annual_enrollments=IvlCovered::AnnualEnrollment.all
-    # @annual_carriers=IvlCovered::AnnualCarrierType.all
-    # @covered_lives=IvlCovered::DiscontinuedReinstatedCoveredLives.all
-    # @metal_types=IvlCovered::TotalMetalTypes.all
-    # @annual_status=IvlCovered::AnnualStatusType.all
-    # @age_groups=IvlCovered::OverallAgeGroups.all
-    # @over_all_aptc=IvlCovered::OverallAptc.all
-    # @overall_genders=IvlCovered::OverallGenderTypes.all
+    @enrollments=IvlCovered::CoveredLivesMonth.all
+    @seps=IvlCovered::SepType.all
+    @discontinuedreinstatedlives=IvlCovered::SepType.all
+    @annual_enrollments=IvlCovered::AnnualEnrollment.all
+    @annual_carriers=IvlCovered::AnnualCarrierType.all
+    @covered_lives=IvlCovered::DiscontinuedReinstatedCoveredLives.all
+    @metal_types=IvlCovered::TotalMetalTypes.all
+    @annual_status=IvlCovered::AnnualStatusType.all
+    @age_groups=IvlCovered::OverallAgeGroups.all
+    @overall_genders=IvlCovered::OverallGenderTypes.all
     @annual_covered_lives=IvlCovered::AnnualCoveredLives.all
-
     @annual_covered=IvlCovered::ActiveCoveredLives.all
+    # @over_all_aptc=IvlCovered::OverallAptc.all
+    # @totalaccounts=IvlCovered::TotalAccounts.all
   end
 
   def enrollment
