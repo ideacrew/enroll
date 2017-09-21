add4b28215bef4c388a4821cca5312901049e513
Pranab <pranab.khanal@gmail.com>
2017-09-21 14:01:33 -0400

    deductions CRUD functionalities

--- app/assets/javascripts/financial_assistance/deduction.js
+++ app/assets/javascripts/financial_assistance/deduction.js
@@ -1,10 +1,12 @@
 function stopEditingDeduction() {
   $('input.deduction-checkbox').prop('disabled', false);
+  $('a.deduciton-edit').removeClass('disabled');
   $('.col-md-2 > .interaction-click-control-continue').removeClass('disabled');
 };
 
 function startEditingDeduction() {
   $('input.deduction-checkbox').prop('disabled', true);
+  $('a.deduction-edit').addClass('disabled');
   $('.col-md-2 > .interaction-click-control-continue').addClass('disabled');
 };
 
@@ -29,16 +31,55 @@ $(document).ready(function() {
     }
   });
 
-  /* cancel deduction edits */
-  $('a.deduction-cancel').click(function(e) {
+  /* edit existing deductions */
+  $('.deductions-list').on('click', 'a.deduction-edit:not(.disabled)', function(e) {
+    e.preventDefault();
+    var deductionEl = $(this).parents('.deduction');
+    deductionEl.find('.deduction-show').addClass('hidden');
+    deductionEl.find('.edit-deduction-form').removeClass('hidden');
+    startEditingDeduction();
+  });
+
+    /* destroy existing deducitons */
+  $('.deductions-list').on('click', 'a.deduction-delete:not(.disabled)', function(e) {
+    var self = this;
+    e.preventDefault();
+    $("#destroyDeduction").modal();
+
+    $("#destroyDeduction .modal-cancel-button").click(function(e) {
+      $("#destroyDeduction").modal('hide');
+    });
+
+    $("#destroyDeduction .modal-continue-button").click(function(e) {
+      $("#destroyDeduction").modal('hide');
+      $(self).parents('.deduction').remove();
+
+      var url = $(self).parents('.deduction').attr('id').replace('financial_assistance_deduction_', 'deductions/');
+      $.ajax({
+        type: 'DELETE',
+        url: url
+      })
+    });
+  });
+
+
+
+  /* cancel benefit edits */
+  $('.deductions-list').on('click', 'a.deduction-cancel', function(e) {
     e.preventDefault();
     stopEditingDeduction();
 
-    //debugger
-    if (!$(this).parents('.deductions-list > div.deduction').length) {
-      $(this).parents('.deduction-kind').find('input[type="checkbox"]').prop('checked', false);
+    var benefitEl = $(this).parents('.deduction');
+    if (benefitEl.length) {
+      benefitEl.find('.deduction-show').removeClass('hidden');
+      benefitEl.find('.edit-deduction-form').addClass('hidden');
+    } else {
+      if (!$(this).parents('.deductions-list > div.deduction').length) {
+        $(this).parents('.deduction-kind').find('input[type="checkbox"]').prop('checked', false);
+      }
+      $(this).parents('.new-deduction-form').remove();
+      $(this).parents('.edit-deduction-form').remove();
     }
-    $(this).parents('.new-deduction-form').remove();
   });
 
 });
