3585f7eee91c5306b6af32036d84649c2222bfdd
Vishal Kalletla <vishuk1201@gmail.com>
2017-09-22 15:54:30 -0400

    Refs#17318 Listener: Verification of Income & MEC

---
+++ app/models/parsers/xml/cv/haven_individual_person_parser.rb
@@ -0,0 +1,10 @@
+module Parsers::Xml::Cv
+  class HavenIndividualPersonParser
+    register_namespace 'n1', 'http://openhbx.org/api/terms/1.0'
+    namespace 'n1'
+
+    tag 'person'
+    element :id, String, tag: 'id/n1:id'
+
+  end
+end
