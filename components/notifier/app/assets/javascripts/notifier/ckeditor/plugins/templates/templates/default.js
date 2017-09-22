2c7c0298209bbc0c0bd5c9e1f82c7cfca2f59f8f
raghuram <raghuramg83@gmail.com>
2017-09-22 14:50:41 -0400

    Refs #17404 Added templates for notifier

--- components/notifier/app/assets/javascripts/notifier/ckeditor/plugins/templates/templates/default.js
+++ components/notifier/app/assets/javascripts/notifier/ckeditor/plugins/templates/templates/default.js
@@ -13,7 +13,7 @@ CKEDITOR.addTemplates( 'default', {
 	templates: [ {
 		title: 'Employer Template',
 		image: 'template1.gif',
-		description: 'One main image with a title and text that surround the image.',
+		description: 'Standard template for the notices received by Employers.',
 		html: "<p>&nbsp;</p>" +
 "<p>#{employer_profile.notice_date}</p>" +
 "<p><strong>SUBJECT: &lt;Change subject&gt;</strong></p>" +
@@ -27,10 +27,10 @@ CKEDITOR.addTemplates( 'default', {
 "</ul>" +
 "<p>You can also find more information on our website at <a href='http://​#{Settings.site.main_web_address}'>#{Settings.site.main_web_address}</a></p>" +
 "<p>[[ if employer_profile.broker_present? ]]</p>" +
-"<table border='0'cellpadding='0' cellspacing='0' style='height:81px; width:315px'>" +
+"<table border='0'cellpadding='0' cellspacing='0' style='height:auto; width:auto'>" +
 	"<tbody>" +
 		"<tr>" +
-			"<td><strong>Broker:</strong></td>" +
+			"<td><strong>Broker: &nbsp;&nbsp;</strong></td>" +
 			"<td>#{employer_profile.broker.primary_fullname}</td>" +
 		"</tr>" +
 		"<tr>" +
@@ -48,70 +48,55 @@ CKEDITOR.addTemplates( 'default', {
 	"</tbody>" +
 "</table>" +
 "<p>[[ else ]]</p>" +
-"<p>If you do not currently have a broker, you can reach out to one of our many trained experts by clicking on the &ldquo;Find a Broker&rdquo; link in your employer account or calling 1-888-813-922<br />" +
+"<p>If you do not currently have a broker, you can reach out to one of our many trained experts by clicking on the &ldquo;Find a Broker&rdquo; link in your employer account or calling #{Settings.contact_center.phone_number}<br />" +
 "[[ end ]]</p>" +
 "<p>___________________________________________________________________________________________________________________________________________________</p>" +
 "<p><small>This notice is being provided in accordance with 45 C.F.R. 155.720.</small></p>"
 	},
 	{
 		title: 'Employee Template',
-		image: 'template2.gif',
-		description: 'A template that defines two columns, each one with a title, and some text.',
-		html: '<table cellspacing="0" cellpadding="0" style="width:100%" border="0">' +
-			'<tr>' +
-				'<td style="width:50%">' +
-					'<h3>Title 1</h3>' +
-				'</td>' +
-				'<td></td>' +
-				'<td style="width:50%">' +
-					'<h3>Title 2</h3>' +
-				'</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td>' +
-					'Text 1' +
-				'</td>' +
-				'<td></td>' +
-				'<td>' +
-					'Text 2' +
-				'</td>' +
-			'</tr>' +
-			'</table>' +
-			'<p>' +
-			'More text goes here.' +
-			'</p>'
-	},
-	{
-		title: 'Broker Template',
-		image: 'template3.gif',
-		description: 'A title with some text and a table.',
-		html: '<div style="width: 80%">' +
-			'<h3>' +
-				'Title goes here' +
-			'</h3>' +
-			'<table style="width:150px;float: right" cellspacing="0" cellpadding="0" border="1">' +
-				'<caption style="border:solid 1px black">' +
-					'<strong>Table title</strong>' +
-				'</caption>' +
-				'<tr>' +
-					'<td>&nbsp;</td>' +
-					'<td>&nbsp;</td>' +
-					'<td>&nbsp;</td>' +
-				'</tr>' +
-				'<tr>' +
-					'<td>&nbsp;</td>' +
-					'<td>&nbsp;</td>' +
-					'<td>&nbsp;</td>' +
-				'</tr>' +
-				'<tr>' +
-					'<td>&nbsp;</td>' +
-					'<td>&nbsp;</td>' +
-					'<td>&nbsp;</td>' +
-				'</tr>' +
-			'</table>' +
-			'<p>' +
-				'Type the text here' +
-			'</p>' +
-			'</div>'
-	} ]
+		image: 'template1.gif',
+		description: 'Standard template for the notices received by Employees.',
+		html: "<p>&nbsp;</p>" +
+"<p>​#{employee_profile.notice_date}</p>" +
+"<p><strong>SUBJECT: &lt;Change subject&gt;</strong></p>" +
+"<p>Dear ​#{employee_profile.first_name} ​#{employee_profile.last_name}:</p>" +
+"<p>&lt;Paste Your Notice Body Here&gt;</p>" +
+"<h3>For Questions or Assistance:</h3>"+
+"<p>[[ if employer_profile.broker_present? ]]</p>" +
+"<p>Contact your employer or your broker for further assistance.</p>" +
+"<p>[[ else ]]</p>" +
+"<p>Contact your employer for further assistance.<br />" +
+"[[ end ]]</p>" +
+"<p>You can also contact the Health Connector with any questions:</p>" +
+"<ul>" +
+	"<li>By calling #{Settings.contact_center.phone_number}. TTY: #{Settings.contact_center.tty_number}</li>" +
+	"<li>By email: <a href='mailto:​#{Settings.contact_center.small_business_email}'>#{Settings.contact_center.small_business_email}</a></li>" +
+"</ul>" +
+"<p>You can also find more information on our website at <a href='http://​#{Settings.site.main_web_address}'>#{Settings.site.main_web_address}</a></p>" +
+"[[ if employer_profile.broker_present? ]]" +
+"<table border='0'cellpadding='0' cellspacing='0' style='height:auto; width:auto'>" +
+	"<tbody>" +
+		"<tr>" +
+			"<td><strong>Broker: &nbsp;&nbsp;</strong></td>" +
+			"<td>#{employer_profile.broker.primary_fullname}</td>" +
+		"</tr>" +
+		"<tr>" +
+			"<td>&nbsp;</td>" +
+			"<td>#{employer_profile.broker.organization}</td>" +
+		"</tr>" +
+		"<tr>" +
+			"<td>&nbsp;</td>" +
+			"<td>#{employer_profile.broker.phone}</td>" +
+		"</tr>" +
+		"<tr>" +
+			"<td>&nbsp;</td>" +
+			"<td>#{employer_profile.broker.email}</td>" +
+		"</tr>" +
+	"</tbody>" +
+"</table>" +
+"[[ end ]]" +
+"<p>___________________________________________________________________________________________________________________________________________________</p>" +
+"<p><small>This notice is being provided in accordance with 45 C.F.R. 155.735(g).</small></p>"
+	}]
 } );
