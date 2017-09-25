e615b8c8c19513075656e0921295afca76bae2fc
saikumar9 <saikumarkotagiri9@gmail.com>
2017-09-25 11:09:54 -0400

    Refs#18391 EA HAVEN Eligibility Importer Submits Eligibility Determinaton Rejection Message to HAVEN

--- app/models/financial_assistance/application.rb
+++ app/models/financial_assistance/application.rb
@@ -73,6 +73,7 @@ class FinancialAssistance::Application
   field :is_ridp_verified, type: Boolean
   field :determination_http_status_code, type: Integer
   field :determination_error_message, type: String
+  field :has_eligibility_response, type: Boolean, default: false
 
   field :workflow, type: Hash, default: { }
 
@@ -394,6 +395,34 @@ class FinancialAssistance::Application
       false
     end
     validity
+  end
+
+  def send_failed_response
+    if !has_eligibility_response
+      message = "Timed-out waiting for eligibility determination response"
+      return_status = 504
+      notify("acapi.info.events.eligibility_determination.rejected",
+          {:correlation_id => SecureRandom.uuid.gsub("-",""),
+            :body => { error_message: message },
+            :family_id => family_id.to_s,
+            :assistance_application_id => _id.to_s,
+            :return_status => return_status.to_s,
+            :submitted_timestamp => TimeKeeper.date_of_record.strftime('%Y-%m-%dT%H:%M:%S')})
+    end
+
+    if has_eligibility_response && determination_http_status_code == 422
+      message = "Invalid schema eligibility determination response provided"
+      notify("acapi.info.events.eligibility_determination.rejected",
+          {:correlation_id => SecureRandom.uuid.gsub("-",""),
+            :body => { error_message: message },
+            :family_id => family_id.to_s,
+            :assistance_application_id => _id.to_s,
+            :return_status => determination_http_status_code.to_s,
+            :submitted_timestamp => TimeKeeper.date_of_record.strftime('%Y-%m-%dT%H:%M:%S'),
+            :haven_application_id => haven_app_id,
+            :haven_ic_id => haven_ic_id,
+            :primary_applicant_id => family.primary_applicant.person.hbx_id.to_s })
+    end
   end
 
   def ready_for_attestation?
--- app/models/subscribers/financial_assistance_application_eligibility_response.rb
+++ app/models/subscribers/financial_assistance_application_eligibility_response.rb
@@ -15,7 +15,7 @@ module Subscribers
       application = FinancialAssistance::Application.find(stringed_key_payload["assistance_application_id"]) if stringed_key_payload["assistance_application_id"].present?
       if application.present?
         payload_http_status_code = stringed_key_payload["return_status"]
-        application.update_attributes(determination_http_status_code: payload_http_status_code)
+        application.update_attributes(determination_http_status_code: payload_http_status_code, has_eligibility_response: true, haven_app_id: stringed_key_payload["haven_application_id"], haven_ic_id: stringed_key_payload["haven_ic_id"])
         if application.success_status_codes?(payload_http_status_code.to_i)
           if eligibility_payload_schema_valid?(xml)
             sc = ShortCircuit.on(:processing_issue) do |err|
