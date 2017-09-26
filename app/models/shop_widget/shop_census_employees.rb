c344222726a91d1fd23e9c8809424d61ece6e0cc
sarathpyerra <sarathpyerra@gmail.com>
2017-09-26 12:03:41 -0400

    Refs #18584 add missing file

---
+++ app/models/shop_widget/shop_census_employees.rb
@@ -0,0 +1,16 @@
+module ShopWidget
+  class ShopCensusEmployees
+    include Mongoid::Document
+    store_in collection: "shp"
+
+    field :tile , type: String
+    field :num_100, type: Integer
+    field :pct_100, type: String
+    field :carrier1_num, type: String
+    field :carrier1_pct, type: Integer
+    field :metal1_num, type: String
+    field :metal1_pct, type: String
+
+    default_scope ->{where(tile: "left_census" )}
+  end
+end
