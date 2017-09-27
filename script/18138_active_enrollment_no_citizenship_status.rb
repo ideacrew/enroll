c3b601fe82da5c5d84eabf09d04d9259f307c229
hitheshkolla <hitheshk07@gmail.com>
2017-09-27 16:00:48 -0400

    refs #18138 csv file path updated

--- script/18138_active_enrollment_no_citizenship_status.rb
+++ script/18138_active_enrollment_no_citizenship_status.rb
@@ -1,7 +1,7 @@
 require "csv"
-CSV.open("/Users/hithu/Downloads/new_data.csv", "wb") do |csv_out|
+CSV.open("new_data.csv", "wb") do |csv_out|
   csv_out << ["family.id", "policy.id", "policy.subscriber.coverage_start_on", "policy.aasm_state", "policy.plan.coverage_kind", "policy.plan.metal_level", "policy.plan.name", "policy.subscriber.person.hbx_id", "policy.subscriber.person.is_incarcerated", "policy.subscriber.person.citizen_status", "policy.subscriber.person.is_dc_resident?", "is_dependent", "has_active_employee_role?", "has_active_consumer_role?"]
-  CSV.foreach("/Users/hithu/Downloads/no_citizen_status_enrolled.csv",{:headers=>:first_row}) do |row, i|
+  CSV.foreach("no_citizen_status_enrolled.csv",{:headers=>:first_row}) do |row, i|
     next if i == 0
     hbx_id = row["policy.subscriber.person.hbx_id"]
     person = Person.where(:hbx_id => hbx_id).first
