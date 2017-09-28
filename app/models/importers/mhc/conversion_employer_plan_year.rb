d93ed9c99a5081a9051cf4659ce4b99583762d63
raghuram <raghuramg83@gmail.com>
2017-09-28 15:08:08 -0400

    Refs #18643 Added service areas validation for employer reference plan

--- app/models/importers/mhc/conversion_employer_plan_year.rb
+++ app/models/importers/mhc/conversion_employer_plan_year.rb
@@ -31,13 +31,24 @@ module Importers::Mhc
       end
     end
 
+    def service_area_plan_hios_ids
+      employer_profile = find_employer
+      profile_and_service_area_pairs = CarrierProfile.carrier_profile_service_area_pairs_for(employer_profile)
+      Plan.valid_shop_health_plans_for_service_area("carrier", carrier_for_elected_plan, start_on.year, profile_and_service_area_pairs.select { |pair| pair.first == carrier_profile_id }).pluck(:hios_id)
+    end
+
     def select_reference_plan(available_plans)
+      employer = find_employer
       if plan_selection == 'sole_source'
         if !single_plan_hios_id.blank?
           sp_hios = single_plan_hios_id.strip
-          found_sole_source_plan = available_plans.detect { |pl| (pl.hios_id == sp_hios) || (pl.hios_id == "#{sp_hios}-01") }
-          return found_sole_source_plan if found_sole_source_plan
-          errors.add(:single_plan_hios_id, "hios id #{single_plan_hios_id.strip} not found for single plan benefit group")
+          if service_area_plan_hios_ids.include?(sp_hios) || service_area_plan_hios_ids.include?("#{sp_hios}-01")
+            found_sole_source_plan = available_plans.detect { |pl| (pl.hios_id == sp_hios) || (pl.hios_id == "#{sp_hios}-01") }
+            return found_sole_source_plan if found_sole_source_plan
+            errors.add(:single_plan_hios_id, "hios id #{single_plan_hios_id.strip} not found for single plan benefit group")
+          else
+            errors.add(:single_plan_hios_id, "hios id #{sp_hios} not offered in employer service areas")
+          end
         else
           errors.add(:single_plan_hios_id, "no hios id specified for single plan benefit group")
         end
