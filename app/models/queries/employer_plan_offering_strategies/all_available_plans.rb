e960024d72d645cb1db173015a636c642c441e0a
vsonnathi <sonnathivinil@gmail.com>
2017-10-01 03:35:49 -0400

    Refs #18678

--- app/models/queries/employer_plan_offering_strategies/all_available_plans.rb
+++ app/models/queries/employer_plan_offering_strategies/all_available_plans.rb
@@ -13,7 +13,11 @@ module Queries
       end
 
       def metal_level_offered_health_plans(metal_level, start_on)
-        Plan.by_active_year(start_on).shop_market.health_coverage.by_metal_level(metal_level).and(hios_id: /-01/)
+        plans = if metal_level == "bronze"
+          Plan.by_active_year(start_on).shop_market.health_coverage.by_bronze_metal_level.and(hios_id: /-01/)
+        else
+          Plan.by_active_year(start_on).shop_market.health_coverage.by_metal_level(metal_level).and(hios_id: /-01/)
+        end
       end
 
       def single_option_offered_health_plans(carrier_id, start_on)
--- app/models/queries/employer_plan_offering_strategies/for_service_area.rb
+++ app/models/queries/employer_plan_offering_strategies/for_service_area.rb
@@ -16,7 +16,11 @@ module Queries
 
       def metal_level_offered_health_plans(metal_level, start_on)
         profile_and_service_area_pairs = CarrierProfile.carrier_profile_service_area_pairs_for(employer_profile)
-        Plan.for_service_areas_and_carriers(profile_and_service_area_pairs, start_on).shop_market.health_coverage.by_metal_level(metal_level).and(hios_id: /-01/)
+        plans = if metal_level == "bronze"
+          Plan.for_service_areas_and_carriers(profile_and_service_area_pairs, start_on).shop_market.health_coverage.by_bronze_metal_level.and(hios_id: /-01/)
+        else
+          Plan.for_service_areas_and_carriers(profile_and_service_area_pairs, start_on).shop_market.health_coverage.by_metal_level(metal_level).and(hios_id: /-01/)
+        end
       end
 
       def single_option_offered_health_plans(carrier_id, start_on)
