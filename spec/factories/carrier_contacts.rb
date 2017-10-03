ed536ccd65e5533dcc12f955c7539dac7aba3f9f
202chaz <chaz.haskins@gmail.com>
2017-10-03 06:44:39 -0400

    Spec updates

---
+++ spec/factories/carrier_contacts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :carrier_contact do
+    carrier_profile
+    kind "main"
+    country_code "1"
+    area_code "877"
+    number "8562430"
+  end
+end
--- spec/views/shared/_carrier_contact_information.html.erb_spec.rb
+++ spec/views/shared/_carrier_contact_information.html.erb_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 describe "shared/_carrier_contact_information.html.erb" do
   let(:plan) { FactoryGirl.build_stubbed(:plan) }
+  let (:carrier_contacts) { FactoryGirl.create(:carrier_contact) }
   before :each do
+    allow(plan.carrier_profile).to receive(:carrier_contacts).and_return([carrier_contacts])
     render partial: "shared/carrier_contact_information", locals: { plan: plan }
   end
   it "should display the carrier name and number" do
     expect(rendered).to match plan.carrier_profile.legal_name
-    expect(rendered).to match("1-877-856-2430")
+    expect(rendered).to include("+1-877-856-2430")
   end
 end
