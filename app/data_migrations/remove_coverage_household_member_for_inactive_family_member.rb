6dcfb62130b2ca0c49bab4c482f7e5a00dd739fd
hitheshkolla <hitheshk07@gmail.com>
2017-10-05 10:37:47 -0400

    refs #18691 updated test env

--- app/data_migrations/remove_coverage_household_member_for_inactive_family_member.rb
+++ app/data_migrations/remove_coverage_household_member_for_inactive_family_member.rb
@@ -14,9 +14,9 @@ class RemoveCoverageHouseHoldMemberForInactiveFamilyMember < MongoidMigrationTas
       chm_to_be_removed = family_member_ids - valid_family_members
       #chm_to_be_added = valid_family_members - family_member_ids
       if chm_to_be_removed.empty?
-        puts "No Coverage Household to remove"
+        puts "No Coverage Household to remove" unless Rails.env.test?
       else
-        puts "Removing the following CHM with Family member id #{chm_to_be_removed.join(", ")}"
+        puts "Removing the following CHM with Family member id #{chm_to_be_removed.join(", ")}" unless Rails.env.test?
         coverage_house_hold_members.where(:family_member_id.in => chm_to_be_removed.map{ |e| e.empty? ? nil : e }).map(&:delete)
         coverage_household.save
       end
