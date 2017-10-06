74feba58a0613ea2836c7d8189e43e681eeb0d03
Raghuram <raghuramg83@gmail.com>
2017-10-06 17:14:46 -0400

    Refs #18721 Added missing line of code

--- app/models/benefit_sponsorship.rb
+++ app/models/benefit_sponsorship.rb
@@ -95,6 +95,7 @@ class BenefitSponsorship
         hbx_sponsor.advance_year    if new_date.day == 1 && new_date.month == 1
       end
 
+      renewal_benefit_coverage_period = HbxProfile.current_hbx.benefit_sponsorship.renewal_benefit_coverage_period
       if renewal_benefit_coverage_period.open_enrollment_start_on == new_date
         oe_begin = Enrollments::IndividualMarket::OpenEnrollmentBegin.new
         oe_begin.process_renewals
