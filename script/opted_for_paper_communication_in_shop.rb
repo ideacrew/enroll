4d0a6fa9954a56b28ed9065805fd7b82b9fee17e
Battula <vrbattula@outlook.com>
2017-10-11 11:29:13 -0400

    refs #18872 Added extra fields to the report.

--- script/opted_for_paper_communication_in_shop.rb
+++ script/opted_for_paper_communication_in_shop.rb
@@ -8,14 +8,14 @@ employer_csv = CSV.open("employers_opted_for_paper_communication_#{TimeKeeper.da
 employer_csv << %w(legal_name fein communication_preference)
 
 employee_csv = CSV.open("employees_opted_for_paper_communication_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv", "w")
-employee_csv << %w(full_name group_name communication_preference)
+employee_csv << %w(hbx_id consumer first_name last_name home_email work_email group_name fein communication_preference)
 
 def add_to_employer_csv(employer_csv, employer)
   employer_csv << [employer.legal_name, employer.fein, employer.contact_method]
 end
 
-def add_to_employee_csv(employee_csv, census_employee, employer)
-  employee_csv << [census_employee.full_name, employer.legal_name, census_employee.employee_role.contact_method]
+def add_to_employee_csv(employee_csv, census_employee, employer, person)
+  employee_csv << [person.hbx_id, person.has_active_consumer_role?, person.first_name, person.last_name, person.home_email.try(:address), person.work_email.try(:address), employer.legal_name, employer.fein, census_employee.employee_role.contact_method]
 end
 
 while offset < org_count
@@ -24,8 +24,9 @@ while offset < org_count
       employer = org.employer_profile
       add_to_employer_csv(employer_csv, employer)
       employer.census_employees.active.each do |ce|
-        if ce.employee_role.present?
-          add_to_employee_csv(employee_csv, ce, employer)
+        if ce.employee_role.present? && ce.employee_role.person
+          person = ce.employee_role.person
+          add_to_employee_csv(employee_csv, ce, employer, person)
         end
       end
     rescue => e
