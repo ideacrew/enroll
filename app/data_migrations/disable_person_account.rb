0ff93bfbc70db1ea8007c25bc817a2b8d98aa920
Ji Wang <traceyw@vt.edu>
2017-10-11 15:05:41 -0400

    Ref#13968 modified rake task to be coherent with master branch

--- app/data_migrations/disable_person_account.rb
+++ app/data_migrations/disable_person_account.rb
@@ -4,10 +4,10 @@ class DisablePersonAccount< MongoidMigrationTask
     hbx_id = ENV['hbx_id']
     person = Person.where(hbx_id:hbx_id).first
     if person.nil?
-      puts "No person was found by the given hbx_id" unless Rails.env.test?
-      return
+      puts "No person was found by the given fein"
+    else
+      person.update_attributes!(is_active: false, is_disabled: true)
+      puts "Disable person with hbx_id #{hbx_id}" unless Rails.env.test?
     end
-    person.update_attributes!(is_active: false)
-    puts "Disable person with hbx_id #{hbx_id}" unless Rails.env.test?
   end
 end
