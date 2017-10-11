5a6ba5cc7ba537a6a560c829c81426c90bd076aa
saip7795 <saip7795@gmail.com>
2017-10-11 11:00:03 -0400

    Ref18176 : Added Spec file and slight changes in the rake task

--- app/data_migrations/change_general_agency_staff_role.rb
+++ app/data_migrations/change_general_agency_staff_role.rb
@@ -4,21 +4,25 @@ class ChangeGeneralAgencyStaffRole < MongoidMigrationTask
   def migrate
     incorrect_person_hbx_id = ENV['incorrect_person_hbx_id']
     correct_person_hbx_id = ENV['correct_person_hbx_id']
-    incorrect_person = Person.where(hbx_id:incorrect_person_hbx_id).general_agency_staff_roles.first
-    if incorrect_person.nil?
-      puts "No person was found with given hbx_id" unless Rails.env.test?
-    else
-      correct_person = Person.where(hbx_id:correct_person_hbx_id).first
-      correct_person.general_agency_staff_roles << GeneralAgencyStaffRole.new(
-                        npn: ga.npn,
-                        general_agency_profile_id: ga.general_agency_profile_id,
-                        aasm_state: ga.aasm_state )
-      correct_person.save!
-      puts "Correct Person is Assigned the GA Staff Role" unless Rails.env.test?
 
-      incorrect_person.general_agency_staff_roles[0].destroy!
-      incorrect_person.save!
-      puts "Incorrect Person is Removed the GA Staff Role" unless Rails.env.test?
+    if incorrect_person_hbx_id && correct_person_hbx_id
+      incorrect_person = Person.where(hbx_id:incorrect_person_hbx_id).first
+        if incorrect_person.nil?
+        puts "No person was found with given hbx_id" unless Rails.env.test?
+        else
+          gen_staff_role = incorrect_person.general_agency_staff_roles.first
+          correct_person = Person.where(hbx_id:correct_person_hbx_id).first
+          correct_person.general_agency_staff_roles << GeneralAgencyStaffRole.new(
+            npn: gen_staff_role.npn,
+            general_agency_profile_id: gen_staff_role.general_agency_profile_id,
+            aasm_state: gen_staff_role.aasm_state)
+          correct_person.save!
+          puts "Correct Person is Assigned the GA Staff Role" unless Rails.env.test?
+          incorrect_person.general_agency_staff_roles.first.destroy!
+          incorrect_person.save!
+        end
+    else
+      puts "Please pass incorrect and correct hbx_ids as respective arguments" unless Rails.env.test?
     end
   end
 end
