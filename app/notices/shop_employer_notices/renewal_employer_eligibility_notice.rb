1f532fb714c19ae3cc7ccbde689609037d2ad8ec
Battula <vrbattula@outlook.com>
2017-10-13 20:57:58 -0400

    refs #19049 Modified template and added attachments.

--- app/notices/shop_employer_notices/renewal_employer_eligibility_notice.rb
+++ app/notices/shop_employer_notices/renewal_employer_eligibility_notice.rb
@@ -4,7 +4,13 @@ class ShopEmployerNotices::RenewalEmployerEligibilityNotice < ShopEmployerNotice
 
   def deliver
     build
-    super
+    generate_pdf_notice
+    shop_dchl_rights_attachment
+    non_discrimination_attachment
+    attach_envelope
+    upload_and_send_secure_message
+    send_generic_notice_alert
+    send_generic_notice_alert_to_broker_and_ga
   end
 
 end 
