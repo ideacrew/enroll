3936a3180b9e66d69e7d89a2d4842878ee49b087
raghuram <raghuramg83@gmail.com>
2017-10-16 16:53:53 -0400

    Refs #17404 pushed updates for observable module

--- app/models/application_event_mapper.rb
+++ app/models/application_event_mapper.rb
@@ -12,7 +12,8 @@ class ApplicationEventMapper
 
   RESOURCE_MAP = {
     "EmployerProfile" => Resource.new(:employer, :hbx_id, :employer_id, :by_hbx_id),
-    "ConsumerRole" => Resource.new(:consumer_role, :id, :consumer_role_id, :find)
+    "ConsumerRole" => Resource.new(:consumer_role, :id, :consumer_role_id, :find),
+    "CensusEmployee" => Resource.new(:census_employee, :id, :census_employee_id, :find)
   }
 
   REVERSE_LOOKUP_MAP = RESOURCE_MAP.inject({}) do |acc, vals|
--- app/models/observers/observer.rb
+++ app/models/observers/observer.rb
@@ -2,12 +2,12 @@ module Observers
   class Observer
     include Acapi::Notifiers
 
-    EVENT_PREFIX = "acapi.info.events."
-
     def trigger_notice(receipient, notice_event)
       resource_mapping = ApplicationEventMapper.map_resource(receipient.class)
       event_name = ApplicationEventMapper.map_event_name(resource_mapping, notice_event)
-      notify(event_name, {resource_mapping.identifier_key => receipient.send(resource_mapping.identifier_method).to_s})
+      notify(event_name, {
+        resource_mapping.identifier_key => receipient.send(resource_mapping.identifier_method).to_s
+      })
     end
   end
 end
