9e14399ed5d768be07f3c8a5c1b2bea0f5db7b40
raghuram <raghuramg83@gmail.com>
2017-10-18 18:23:08 -0400

    Refs #17404 removed IVL dates

--- components/notifier/app/models/notifier/merge_data_models/employee_profile.rb
+++ components/notifier/app/models/notifier/merge_data_models/employee_profile.rb
@@ -8,12 +8,9 @@ module Notifier
     attribute :first_name, String
     attribute :last_name, String
     attribute :mailing_address, MergeDataModels::Address
-    attribute :ivl_sep_begin_date, Date
-    attribute :ivl_sep_end_date, Date
     attribute :employer_name, String
     attribute :coverage_begin_date, Date
     attribute :broker, MergeDataModels::Broker
-
     attribute :date_of_hire, Date
     attribute :earliest_coverage_begin_date, Date
     attribute :new_hire_oe_end_date, Date
@@ -25,8 +22,6 @@ module Notifier
         notice_date: TimeKeeper.date_of_record.strftime('%m/%d/%Y'),
         first_name: 'John',
         last_name: 'Whitmore',
-        ivl_sep_begin_date: TimeKeeper.date_of_record.strftime('%m/%d/%Y'),
-        ivl_sep_end_date: (TimeKeeper.date_of_record + 60.days).strftime('%m/%d/%Y'),
         employer_name: 'MA Health Connector',
         coverage_begin_date: TimeKeeper.date_of_record.strftime('%m/%d/%Y'),
         date_of_hire: TimeKeeper.date_of_record.strftime('%m/%d/%Y') ,
