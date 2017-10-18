78df1895c8bff90ea7e4436dc65aed834b86bdc1
raghuram <raghuramg83@gmail.com>
2017-10-18 17:40:14 -0400

    Refs #17404 added new tokens

--- components/notifier/app/models/notifier/merge_data_models/plan_year.rb
+++ components/notifier/app/models/notifier/merge_data_models/plan_year.rb
@@ -2,15 +2,25 @@ module Notifier
   class MergeDataModels::PlanYear
     include Virtus.model
 
-    attribute :open_enrollment_start_on, Date
-    attribute :open_enrollment_end_on, Date
-    attribute :start_on, Date
-    attribute :end_on, Date
+    attribute :current_py_oe_start_date, String
+    attribute :current_py_oe_end_date, String
+    attribute :current_py_start_date, String
+    attribute :current_py_end_date, String
 
-    attribute :renewal_open_enrollment_start_on, Date
-    attribute :renewal_open_enrollment_end_on, Date
-    attribute :renewal_start_on, Date
-    attribute :renewal_end_on, Date
+    attribute :renewal_py_oe_start_date, String
+    attribute :renewal_py_oe_end_date, String
+    attribute :renewal_py_start_date, String
+    attribute :renewal_py_end_date, String
+
+    attribute :renewal_py_submit_soft_due_date, String
+    attribute :renewal_py_submit_due_date, String
+    attribute :binder_payment_due_date, String
+
+    # Following date fileds are defined to allow business enter tokens like <Current Plan Year END On Date, MM/DD/YYYY, + 60 Days>
+    attribute :current_py_start_on, Date
+    attribute :current_py_end_on, Date
+    attribute :renewal_py_start_on, Date
+    attribute :renewal_py_end_on, Date
 
     attribute :carrier_name, String
     attribute :renewal_carrier_name, String
@@ -18,24 +28,29 @@ module Notifier
     attribute :warnings, Array[String]
     attribute :errors, Array[String]
 
-    attribute :binder_due_date, Date
-    attribute :renewal_binder_due_data, Date
-
-
     def self.stubbed_object
+      reference_date = TimeKeeper.date_of_record.next_month.beginning_of_month
+      current_py_start = reference_date.prev_year
+      renewal_py_start = reference_date
+
       Notifier::MergeDataModels::PlanYear.new({
-        open_enrollment_start_on: '09/1/2017',
-        open_enrollment_end_on: '09/10/2017',
-        start_on: '10/01/2017',
-        end_on: '09/30/2018',
-        renewal_open_enrollment_start_on: '09/1/2017',
-        renewal_open_enrollment_end_on: '09/10/2017',
-        renewal_start_on: '10/01/2017',
-        renewal_end_on: '09/30/2018',
+        current_py_oe_start_date: (current_py_start.prev_month).strftime('%m/%d/%Y'),
+        current_py_oe_end_date: (current_py_start.prev_month + 19.days).strftime('%m/%d/%Y'),
+        current_py_start_date: current_py_start.strftime('%m/%d/%Y'),
+        current_py_end_date: renewal_py_start.prev_day.strftime('%m/%d/%Y'),
+        renewal_py_oe_start_date: renewal_py_start.prev_month.strftime('%m/%d/%Y'),
+        renewal_py_oe_end_date: (renewal_py_start.prev_month + 19.days).strftime('%m/%d/%Y'),
+        renewal_py_start_date: renewal_py_start.strftime('%m/%d/%Y'),
+        renewal_py_end_date: renewal_py_start.next_year.prev_day.strftime('%m/%d/%Y'),
+        renewal_py_submit_soft_due_date: (renewal_py_start.prev_month + 9.days).strftime('%m/%d/%Y'),
+        renewal_py_submit_due_date: (renewal_py_start.prev_month + 14.days).strftime('%m/%d/%Y'),
+        binder_payment_due_date: (current_py_start.prev_month + 22.days).strftime('%m/%d/%Y'),
+        current_py_start_on: current_py_start,
+        current_py_end_on: renewal_py_start.prev_day,
+        renewal_py_start_on: renewal_py_start,
+        renewal_py_end_on: renewal_py_start.next_year.prev_day,
         carrier_name: 'Kaiser',
-        renewal_carrier_name: 'Kaiser',
-        binder_due_date: '09/25/2017',
-        renewal_binder_due_data: '09/25/2017'
+        renewal_carrier_name: 'Kaiser'
       })
     end
   end
