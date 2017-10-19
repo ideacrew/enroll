a20692c6cd4f427d622d51f8e6ec1fc42915289d
Mahesh Kurma <maheshruby405@gmail.com>
2017-10-19 10:17:53 -0400

    spec fix - models/consumer_role_spec

--- spec/models/consumer_role_spec.rb
+++ spec/models/consumer_role_spec.rb
@@ -184,20 +184,20 @@ describe "#build_nested_models_for_person" do
   end
 end
 
-describe "#latest_active_tax_household_with_year" do
+describe "#latest_active_tax_households_with_year" do
   include_context "BradyBunchAfterAll"
   before :all do
     create_tax_household_for_mikes_family
     @consumer_role = mike.consumer_role
-    @taxhouhold = mikes_family.latest_household.tax_households.last
+    @taxhouhold = mikes_family.latest_household.tax_households
   end
 
   it "should rerturn active taxhousehold of this year" do
-    expect(@consumer_role.latest_active_tax_household_with_year(TimeKeeper.date_of_record.year)).to eq @taxhouhold
+    expect(@consumer_role.latest_active_tax_households_with_year(TimeKeeper.date_of_record.year)).to eq @taxhouhold
   end
 
   it "should rerturn nil when can not found taxhousehold" do
-    expect(ConsumerRole.new.latest_active_tax_household_with_year(TimeKeeper.date_of_record.year)).to eq nil
+    expect(ConsumerRole.new.latest_active_tax_households_with_year(TimeKeeper.date_of_record.year)).to eq nil
   end
 end
 
