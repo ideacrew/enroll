b1b81d1306ac4a4734102cf506e85356b889ee7c
Prem Shaganti <prem.shaganti@dc.gov>
2017-10-21 20:36:50 -0400

    ref: address reference plans carrier loading    1) aca_helper.rb line:63-82 took 18751 and 472d06e029 changes in.    2) settings.yml line:94 took 472d06e02 and 472d06e029 changes in.

--- app/helpers/config/aca_helper.rb
+++ app/helpers/config/aca_helper.rb
@@ -60,6 +60,26 @@ module Config::AcaHelper
     @offers_nationwide_plans ||= Settings.aca.nationwide_markets
   end
 
+  def check_plan_options_title
+    Settings.site.plan_options_title_for_ma
+  end
+
+  def reference_plans_for_metal_level
+    Settings.aca.reference_carriers_for_metal_level
+  end
+
+  def fetch_plan_title_for_sole_source
+    Settings.plan_option_titles.sole_source
+  end
+
+  def fetch_plan_title_for_metal_level
+    Settings.plan_option_titles.metal_level
+  end
+
+  def fetch_plan_title_for_single_carrier
+    Settings.plan_option_titles.single_carrier
+  end
+
   def carrier_special_plan_identifier_namespace
     @carrier_special_plan_identifier_namespace ||= Settings.aca.carrier_special_plan_identifier_namespace
   end
