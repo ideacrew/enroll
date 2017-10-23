74890016d6c7fea58b3c0691d97d131aa233a907
Kevin Wei <kevin.tw.wei@gmail.com>
2017-10-23 13:31:22 -0400

    Revert "Merge remote-tracking branch 'origin/feature#18337'"
    This reverts commit c914f83615aa16865c3727c716d49da63067ce9c, reversing
    changes made to d3baf8dd749d2d899bf6b812dc8a9ae112334d30.

--- app/models/person.rb
+++ app/models/person.rb
@@ -37,7 +37,6 @@ class Person
   field :dob_check, type: Boolean
 
   field :is_incarcerated, type: Boolean
-  field :is_physically_disabled, type: Boolean
 
   field :is_disabled, type: Boolean
   field :ethnicity, type: Array
@@ -969,9 +968,5 @@ class Person
 
   def incarceration_validation
     self.errors.add(:base, "Incarceration status is required.") if is_incarcerated.to_s.blank?
-  end
-
-  def family_member
-    primary_family.family_members.find_by(person_id: id)
   end
 end
