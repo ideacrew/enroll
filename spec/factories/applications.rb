1ea5cecdd3fa6ce7aa3c566fed2e900881ea4b3b
saikumar9 <saikumarkotagiri9@gmail.com>
2017-10-25 14:29:06 -0400

    Pick the Application applicable to that year

--- spec/factories/applications.rb
+++ spec/factories/applications.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory(:application, :class => FinancialAssistance::Application) do
     family
-    assistance_year 2017
+    assistance_year TimeKeeper.date_of_record.year
     submitted_at {2.months.ago}
     aasm_state "determined"
   end
