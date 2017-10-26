ff7c96d79392129e4a6b8bf9eda8076df5d5afeb
Brian Weiner <brian.weiner@dc.gov>
2017-10-26 17:11:53 -0400

    restrict feins in 2017 to certain carriers
     Conflicts:
    	app/models/organization.rb conflicts on line 251-260, kept the branch changes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -248,6 +248,13 @@ class Organization
       Organization.exists(carrier_profile: true).inject({}) do |carrier_names, org|
         unless (filters[:primary_office_location].nil?)
           next carrier_names unless CarrierServiceArea.valid_for?(office_location: office_location, carrier_profile: org.carrier_profile)
+          if filters[:active_year]
+            if filters[:active_year] == '2017'
+              # only include HNE, BMCHP, Fallon
+              next unless ['043373331','237442369','042864973'].include? org.fein
+            end
+            next carrier_names if CarrierServiceArea.valid_for_carrier_on(address: office_location.address, carrier_profile: org.carrier_profile, year: filters[:active_year]).empty?
+          end
         end
         if (filters[:sole_source_only]) ## Only sole source carriers requested
           next carrier_names unless org.carrier_profile.offers_sole_source?  # skip carrier unless it is a sole source provider
