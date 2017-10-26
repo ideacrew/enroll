1cefb0f3afc4406a0d47f3039f0319a7ae9db577
Sai praveen Gudimetla <saipraveengudimetla@Sais-MacBook-Pro-2.local>
2017-10-26 16:57:18 -0400

    Refs #18843 Code clean up and adding specs

--- spec/controllers/ridp_documents_controller_spec.rb
+++ spec/controllers/ridp_documents_controller_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe Insured::RidpDocumentsController, :type => :controller do
   let(:consumer_wrapper) { double }
 
   context "Failed Upload" do
-    xit "redirects" do
+    it "redirects" do
       request.env["HTTP_REFERER"] = "/home"
       allow_any_instance_of(Insured::RidpDocumentsController).to receive(:get_person)
       allow_any_instance_of(Insured::RidpDocumentsController).to receive(:person_consumer_role)
-      allow_any_instance_of(Insured::RidpDocumentsController).to receive(:params_clean_ridp_documents).and_return({sample: 'sample'})
 
       sign_in user
       post :upload, consumer_role: consumer_role
@@ -41,7 +40,7 @@ RSpec.describe Insured::RidpDocumentsController, :type => :controller do
       let(:file_path) { File.dirname(__FILE__) } # a sample file path
       let(:cleaned_params) { {"0" => {"subject" => "I-327 (Reentry Permit)", "id" => "55e7fef5536167bb822e0000", "alien_number" => "999999999"}} }
 
-      xit "redirects" do
+      it "redirects" do
         request.env["HTTP_REFERER"] = "/home"
         allow(file).to receive(:original_filename).and_return("some-filename")
         allow(file).to receive(:tempfile).and_return(temp_file)
@@ -50,8 +49,7 @@ RSpec.describe Insured::RidpDocumentsController, :type => :controller do
         allow_any_instance_of(Insured::RidpDocumentsController).to receive(:person_consumer_role)
         allow_any_instance_of(Insured::RidpDocumentsController).to receive(:file_path).and_return(file_path)
         allow_any_instance_of(Insured::RidpDocumentsController).to receive(:file_name).and_return("sample-filename")
-        allow_any_instance_of(Insured::RidpDocumentsController).to receive(:params_clean_ridp_documents).and_return(cleaned_params)
-        allow_any_instance_of(Insured::RidpDocumentsController).to receive(:update_ridp_documents).with(cleaned_params, "sample-filename", doc_id).and_return(true)
+        allow_any_instance_of(Insured::RidpDocumentsController).to receive(:update_ridp_documents).with("sample-filename", doc_id).and_return(true)
         allow(Aws::S3Storage).to receive(:save).with(file_path, bucket_name).and_return(doc_id)
         sign_in user
         post :upload, params
