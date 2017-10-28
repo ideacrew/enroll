8672ea251c6c9ed01d4f3d76d81ee0a3d5edde64
raghuram <raghuramg83@gmail.com>
2017-10-28 14:01:02 -0400

    Refs #17404 fixed indentation

--- app/models/observers/notice_observer.rb
+++ app/models/observers/notice_observer.rb
@@ -31,14 +31,12 @@ module Observers
             trigger_notice(recipient: plan_year.employer_profile, event_object: plan_year, notice_event: "renewal_employer_ineligibility_notice")
 
             plan_year.employer_profile.census_employees.non_terminated.each do |ce|
-                if ce.employee_role.present?
-                  trigger_notice(recipient: ce.employee_role, event_object: plan_year, notice_event: "employee_renewal_employer_ineligibility_notice")
-                end
+              if ce.employee_role.present?
+                trigger_notice(recipient: ce.employee_role, event_object: plan_year, notice_event: "employee_renewal_employer_ineligibility_notice")
               end
-
+            end
           end
         end
-
         
         if new_model_event.event_key == :renewal_application_submitted
           trigger_notice(recipient: plan_year.employer_profile, event_object: plan_year, notice_event: "renewal_application_published")
@@ -49,6 +47,7 @@ module Observers
         end
 
         if new_model_event.event_key == :ineligible_renewal_application_submitted
+          
           if plan_year.application_eligibility_warnings.include?(:primary_office_location)
             trigger_notice(recipient: plan_year.employer_profile, event_object: plan_year, notice_event: "employer_renewal_eligibility_denial_notice")
 
