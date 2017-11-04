655691d27d097cc89e1f78424ade7e193777450c
vsonnathi <sonnathivinil@gmail.com>
2017-11-03 21:09:48 -0400

    Fix spec.

--- spec/models/aws/s3_storage_spec.rb
+++ spec/models/aws/s3_storage_spec.rb
@@ -51,7 +51,7 @@ describe Aws::S3Storage do
 
     context "failure (invalid uri)" do
       it "returns nil" do
-        allow_any_instance_of(Aws::S3Storage).to receive(:get_object).with('local', nil).and_raise(StandardError)
+        allow_any_instance_of(Aws::S3Storage).to receive(:get_object).with('qa', nil).and_raise(StandardError)
         expect do
           subject.find(invalid_url)
         end.to raise_error(StandardError)
