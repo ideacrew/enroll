29f2347822241923baa84c895b5f57f9626c0326
Sai praveen Gudimetla <saipraveengudimetla@Sais-MacBook-Pro-2.local>
2017-11-06 11:40:47 -0500

    Refs #19536 Moved New DC Resident Application into the Families dropdown

--- features/step_definitions/edit_aptc_csr_steps.rb
+++ features/step_definitions/edit_aptc_csr_steps.rb
@@ -13,7 +13,3 @@ end
 Then(/^Hbx Admin should see an Edit APTC \/ CSR link$/) do
   find_link('Edit APTC / CSR').visible?
 end
-
-Then(/^Hbx Admin should not see an Edit APTC \/ CSR link$/) do
-  expect(page).to_not have_text("Edit APTC / CSR")
-end
