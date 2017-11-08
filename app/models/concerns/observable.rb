ed1806c016a9da52cccb229b28bb746630d11e31
raghuram <raghuramg83@gmail.com>
2017-11-08 13:59:08 -0500

    Refs #18950 Observable: Added observers on both instance as well as class
    	app/models/observers/notice_observer.rb took from commit but commented those as in main MA master branch

--- app/models/concerns/observable.rb
+++ app/models/concerns/observable.rb
@@ -2,37 +2,54 @@ module Concerns::Observable
   extend ActiveSupport::Concern
 
   included do
-    attr_reader :observer_peers
+    extend ObservableMethods
+    include ObservableMethods
 
     after_initialize do |instance|
       register_observers
     end
   end
 
-  def register_observers
-    add_observer(Observers::NoticeObserver.new, (self.class.model_name.param_key + '_update').to_sym)
-  end
+  module ObservableMethods
+    attr_reader :observer_peers
 
-  def add_observer(observer, func=:update)
-    @observer_peers = {} unless defined? @observer_peers
-
-    unless observer.respond_to? func
-      raise NoMethodError, "observer does not respond to '#{func.to_s}'"
+    def self.extended(base)
+      base.register_observers
     end
 
-    if @observer_peers.none?{|k, v| k.is_a?(observer.class)}
-      @observer_peers[observer] = func
+    def update_method_name
+      if self.is_a?(Class)
+        self.model_name.param_key + '_date_change'
+      else
+        self.class.model_name.param_key + '_update'
+      end
     end
-  end
 
-  def delete_observers
-    @observer_peers.clear if defined? @observer_peers
-  end
+    def register_observers
+      add_observer(Observers::NoticeObserver.new, update_method_name.to_sym)
+    end
 
-  def notify_observers(*arg)
-    if defined? @observer_peers
-      @observer_peers.each do |k, v|
-        k.send v, *arg
+    def add_observer(observer, func=:update)
+      @observer_peers = {} unless defined? @observer_peers
+
+      unless observer.respond_to? func
+        raise NoMethodError, "observer does not respond to '#{func.to_s}'"
+      end
+
+      if @observer_peers.none?{|k, v| k.is_a?(observer.class)}
+        @observer_peers[observer] = func
+      end
+    end
+
+    def delete_observers
+      @observer_peers.clear if defined? @observer_peers
+    end
+
+    def notify_observers(*arg)
+      if defined? @observer_peers
+        @observer_peers.each do |k, v|
+          k.send v, *arg
+        end
       end
     end
   end
