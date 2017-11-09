b62b9e0a2a40c1a68d89f14e171c85e64d559f76
harsha280 <harsha.vskr@gmail.com>
2017-11-09 14:14:03 -0500

    Refs#19396 Include cobra event date in enrollment event xmls

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -34,6 +34,8 @@ module EventsHelper
                   "drop_family_member_due_to_new_eligibility"
                 when "employer_sponsored_coverage_termination"
                   "eligibility_change_employer_ineligible"
+                when "employer_sponsored_cobra"
+                  "employer_cobra_non_payment"
                 else
                   eligibility_kind
                 end
