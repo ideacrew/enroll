2065c6766a75921e3a88c0fd3f40e0b6a1dd3b5e
202chaz <chaz.haskins@gmail.com>
2017-11-09 11:02:32 -0500

    refs 19085 Admin Action: Paper Application

--- app/controllers/insured/consumer_roles_controller.rb
+++ app/controllers/insured/consumer_roles_controller.rb
@@ -210,7 +210,9 @@ class Insured::ConsumerRolesController < ApplicationController
           format.html {redirect_to destroy_user_session_path}
         end
       else
-        if is_new_paper_application?(current_user, session[:original_application_type]) || @person.primary_family.has_curam_or_mobile_application_type?
+        if current_user.has_hbx_staff_role? && params["person"]["family"]["application_type"] == "Paper"
+          redirect_to new_insured_interactive_identity_verification_path
+        elsif is_new_paper_application?(current_user, session[:original_application_type]) || @person.primary_family.has_curam_or_mobile_application_type?
           redirect_to insured_family_members_path(consumer_role_id: @consumer_role.id)
         else
           redirect_to ridp_agreement_insured_consumer_role_index_path
