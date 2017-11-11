9e956c4d968c147ff8676f656f197b78ae95bba6
Raghuram <raghuramg83@gmail.com>
2017-11-11 14:11:25 -0500

    Refs #19158 update renewal scripts to make sure we skip terminated/term pending employees

--- script/process_families_missing_passives.rb
+++ script/process_families_missing_passives.rb
@@ -8,16 +8,19 @@ Organization.where(:fein.in => feins).each do |organization|
   employer = organization.employer_profile
   bg_ids = employer.active_plan_year.benefit_groups.pluck(:id)
 
-  Family.where(:"households.hbx_enrollments" => {:$elemMatch => {:benefit_group_id.in => bg_ids, :aasm_state.in => (HbxEnrollment::ENROLLED_STATUSES + HbxEnrollment::WAIVED_STATUSES)}}).each do |family|
+  Family.where(:"households.hbx_enrollments" => {:$elemMatch => {:benefit_group_id.in => bg_ids, :aasm_state.in => (HbxEnrollment::ENROLLED_STATUSES + HbxEnrollment::WAIVED_STATUSES - ["coverage_termination_pending"])}}).each do |family|
 
     enrollment = family.active_household.hbx_enrollments.where({
       :benefit_group_id.in => bg_ids, 
-      :aasm_state.in => (HbxEnrollment::ENROLLED_STATUSES + HbxEnrollment::WAIVED_STATUSES)
+      :aasm_state.in => (HbxEnrollment::ENROLLED_STATUSES + HbxEnrollment::WAIVED_STATUSES - ["coverage_termination_pending"])
       }).sort_by(&:submitted_at).last
 
     ce = enrollment.benefit_group_assignment.try(:census_employee)
     next if ce.blank?
     next unless CensusEmployee::EMPLOYMENT_ACTIVE_STATES.include?(ce.aasm_state)
+    if CensusEmployee::PENDING_STATES.include?(ce.aasm_state)
+      next if ce.coverage_terminated_on.blank? || ce.coverage_terminated_on < employer.renewing_plan_year.start_on
+    end
 
     if enrollment.present?
       renewal_bg_ids = employer.renewing_plan_year.benefit_groups.pluck(:id)
