9e2759e3d2b33349828ee2a425a78f08b5e489f8
Brian Weiner <brian.weiner@dc.gov>
2017-11-14 13:36:16 -0500

    ref#18950 separate class and instance methods within concern

--- app/models/concerns/observable.rb
+++ app/models/concerns/observable.rb
@@ -2,31 +2,18 @@ module Concerns::Observable
   extend ActiveSupport::Concern
 
   included do
-    extend ObservableMethods
-    include ObservableMethods
+    attr_reader :observer_peers
 
     after_initialize do |instance|
       register_observers
     end
-  end
-
-  module ObservableMethods
-    attr_reader :observer_peers
-
-    def self.extended(base)
-      base.register_observers
-    end
-
-    def update_method_name
-      if self.is_a?(Class)
-        self.model_name.param_key + '_date_change'
-      else
-        self.class.model_name.param_key + '_update'
-      end
-    end
 
     def register_observers
       add_observer(Observers::NoticeObserver.new, update_method_name.to_sym)
+    end
+
+    def update_method_name
+      self.class.model_name.param_key + '_update'
     end
 
     def add_observer(observer, func=:update)
@@ -45,6 +32,20 @@ module Concerns::Observable
       @observer_peers.clear if defined? @observer_peers
     end
 
+    def notify_observers(*arg)
+      if defined? @observer_peers
+        @observer_peers.each do |k, v|
+          k.send v, *arg
+        end
+      end
+    end
+  end
+
+  class_methods do
+    def update_method_name
+      self.model_name.param_key + '_date_change'
+    end
+    
     def notify_observers(*arg)
       if defined? @observer_peers
         @observer_peers.each do |k, v|
