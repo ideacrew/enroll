621624163a9a01cce72e62a71c4fc4cac14e38b8
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-11-14 13:38:24 -0500

    added missing route

--- config/routes.rb
+++ config/routes.rb
@@ -286,6 +286,7 @@ Rails.application.routes.draw do
       post 'bulk_employee_upload'
       member do
         get "download_invoice"
+        get 'show_invoice'
         post 'generate_checkbook_urls'
         get "show_invoice"
       end
