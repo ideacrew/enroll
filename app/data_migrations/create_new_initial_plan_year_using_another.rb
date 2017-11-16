cdd407b20175e37ca23ee8731bf309710a3c74fa
saimekala07 <saimekala07@gmail.com>
2017-11-16 16:49:55 -0500

    refs #19791 updating migration to handle other cases of new plan year creation

--- app/data_migrations/create_new_initial_plan_year_using_another.rb
+++ app/data_migrations/create_new_initial_plan_year_using_another.rb
@@ -3,12 +3,16 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class CreateNewInitialPlanYearUsingAnother < MongoidMigrationTask
 
   def create_initial_plan_year(organization, old_plan_year, start_on)
+
     start_date = Date.strptime(start_on, "%m%d%Y")
+    open_enrollment_start_on = ENV['open_enrollment_start_on'].present? ? DateTime.strptime(ENV['open_enrollment_start_on'], "%m%d%Y") : start_date - 1.month
+    open_enrollment_end_on = ENV['open_enrollment_end_on'].present? ? DateTime.strptime(ENV['open_enrollment_end_on'], "%m%d%Y") : (start_date - 1.month).beginning_of_month + 13.days
+
     new_plan_year = organization.employer_profile.plan_years.build({
                                                                        start_on: start_date,
                                                                        end_on: start_date + 1.year - 1.day,
-                                                                       open_enrollment_start_on: start_date - 1.month,
-                                                                       open_enrollment_end_on: (start_date - 1.month).beginning_of_month + 9.days,
+                                                                       open_enrollment_start_on: open_enrollment_start_on,
+                                                                       open_enrollment_end_on: open_enrollment_end_on,
                                                                        fte_count: old_plan_year.fte_count,
                                                                        pte_count: old_plan_year.pte_count,
                                                                        msp_count: old_plan_year.msp_count,
@@ -31,24 +35,85 @@ class CreateNewInitialPlanYearUsingAnother < MongoidMigrationTask
   end
 
   def clone_benefit_group(active_group, new_plan_year)
-    new_plan_year.benefit_groups.build({
+    effective_on_offset = ENV['effective_on_offset'].present? ? ENV['effective_on_offset'] : active_group.effective_on_offset
+    health_attributes = get_health_attributes(active_group,new_plan_year)
+
+    new_benefit_group = new_plan_year.benefit_groups.build({
                                            title: "#{active_group.title.titleize} New",
                                            effective_on_kind: active_group.effective_on_kind,
                                            terminate_on_kind: active_group.terminate_on_kind,
-                                           plan_option_kind: active_group.plan_option_kind,
+                                           plan_option_kind: health_attributes[0],
                                            default: active_group.default,
-                                           effective_on_offset: active_group.effective_on_offset,
+                                           effective_on_offset: effective_on_offset,
                                            employer_max_amt_in_cents: active_group.employer_max_amt_in_cents,
                                            relationship_benefits: active_group.relationship_benefits,
-                                           reference_plan_id: active_group.reference_plan_id,
-                                           elected_plan_ids: active_group.elected_plan_ids,
+                                           reference_plan_id: health_attributes[1],
+                                           elected_plan_ids: health_attributes[2],
                                            is_congress: false
                                        })
+
+    return new_benefit_group unless active_group.is_offering_dental?
+
+    dental_attributes = get_dental_attributes(active_group,new_plan_year)
+
+    new_benefit_group.dental_relationship_benefits = active_group.dental_relationship_benefits
+    new_benefit_group.dental_plan_option_kind = dental_attributes[0]
+    new_benefit_group.dental_reference_plan_id = dental_attributes[1]
+    new_benefit_group.elected_dental_plan_ids = dental_attributes[2]
+
+    new_benefit_group
+  end
+
+  def get_health_attributes(active_group,new_plan_year)
+
+    health_plan_option_kind = ENV['plan_option_kind'].present? ? ENV['plan_option_kind'] : active_group.plan_option_kind
+    reference_plan = active_group.reference_plan_id
+    elected_plans = active_group.elected_plan_ids
+
+    if health_plan_option_kind != active_group.plan_option_kind || active_group.reference_plan.active_year != new_plan_year.start_on.year
+      if health_plan_option_kind == "single_plan"
+        reference_plan = Plan.by_active_year(new_plan_year.start_on.year).shop_market.health_coverage.where(hios_id: active_group.reference_plan.hios_id).last.id
+        elected_plans = Plan.by_active_year(new_plan_year.start_on.year).shop_market.health_coverage.where(hios_id: active_group.reference_plan.hios_id).map(&:id)
+      elsif health_plan_option_kind == "single_carrier"
+        reference_plan =  Plan.by_active_year(new_plan_year.start_on.year).shop_market.health_coverage.where(hios_id: active_group.reference_plan.hios_id).last.id
+        elected_plans = Plan.health_coverage.valid_shop_by_carrier_and_year(active_group.reference_plan.carrier_profile.id,new_plan_year.start_on.year).map(&:id)
+      else
+        reference_plan =  Plan.by_active_year(new_plan_year.start_on.year).shop_market.health_coverage.where(hios_id: active_group.reference_plan.hios_id).last.id
+        elected_plans = Plan.health_coverage.valid_shop_by_metal_level_and_year(active_group.reference_plan.metal_level,new_plan_year.start_on.year).map(&:id)
+      end
+    end
+
+    return health_plan_option_kind, reference_plan, elected_plans
+
+  end
+
+
+  def get_dental_attributes(active_group,new_plan_year)
+
+    return '', '', '' unless active_group.is_offering_dental?
+
+    dental_plan_option_kind = ENV['dental_plan_option_kind'].present? ? ENV['dental_plan_option_kind'] : active_group.dental_plan_option_kind
+    reference_plan = active_group.dental_reference_plan_id
+    elected_plans = active_group.elected_dental_plan_ids
+
+    if dental_plan_option_kind != active_group.dental_plan_option_kind || active_group.dental_reference_plan.active_year != new_plan_year.start_on.year
+      if dental_plan_option_kind == "single_plan"
+        reference_plan = Plan.by_active_year(new_plan_year.start_on.year).shop_market.dental_coverage.where(hios_id: active_group.dental_reference_plan.hios_id).last.id
+        elected_plans = Plan.by_active_year(new_plan_year.start_on.year).shop_market.dental_coverage.where(hios_id: active_group.dental_reference_plan.hios_id).map(&:id)
+      elsif dental_plan_option_kind == "single_carrier"
+        reference_plan =  Plan.by_active_year(new_plan_year.start_on.year).shop_market.dental_coverage.where(hios_id: active_group.dental_reference_plan.hios_id).last.id
+        elected_plans = Plan.dental_coverage.valid_shop_by_carrier_and_year(active_group.reference_plan.carrier_profile.id,new_plan_year.start_on.year).map(&:id)
+      end
+    end
+
+    return dental_plan_option_kind, reference_plan, elected_plans
+
   end
 
   def force_publish!(new_plan_year)
     if new_plan_year.may_force_publish? && new_plan_year.application_errors.empty?
       new_plan_year.force_publish!
+      new_plan_year.activate! if new_plan_year.may_activate?
     else
       new_plan_year.workflow_state_transitions << WorkflowStateTransition.new(
           from_state: new_plan_year.aasm_state,
