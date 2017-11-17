cf8d6169355f8636686bf835fb342ca54c2aff6a
Prem Shaganti <prem.shaganti@dc.gov>
2017-11-17 16:02:56 -0500

    ref:18955 little tweak on method invoking

--- app/models/observers/notice_observer.rb
+++ app/models/observers/notice_observer.rb
@@ -81,28 +81,27 @@ module Observers
     def census_employee_update; end
 
     def plan_year_date_change(model_event)
-      current_date = TimeKeeper.date_of_record
       if PlanYear::DATA_CHANGE_EVENTS.include?(model_event.event_key)
         if model_event.event_key == :renewal_employer_publish_plan_year_reminder_after_soft_dead_line
-          organizations_for_force_publish(current_date).each do |organization|
-            plan_year = organization.employer_profile.plan_years.where(:aasm_state => 'renewing_draft').first
-            trigger_notice(recipient: organization.employer_profile, event_object: plan_year, notice_event: "renewal_employer_publish_plan_year_reminder_after_soft_dead_line")
-          end
+          trigger_on_queried_records("renewal_employer_publish_plan_year_reminder_after_soft_dead_line")
         end
 
         if model_event.event_key == :renewal_plan_year_first_reminder_before_soft_dead_line
-          organizations_for_force_publish(current_date).each do |organization|
-            plan_year = organization.employer_profile.plan_years.where(:aasm_state => 'renewing_draft').first
-            trigger_notice(recipient: organization.employer_profile, event_object: plan_year, notice_event: "renewal_plan_year_first_reminder_before_soft_dead_line")
-          end
+          trigger_on_queried_records("renewal_plan_year_first_reminder_before_soft_dead_line")
         end
 
         if model_event.event_key == :renewal_plan_year_publish_dead_line
-          organizations_for_force_publish(TimeKeeper.date_of_record).each do |organization|
-              plan_year = organization.employer_profile.plan_years.where(:aasm_state => 'renewing_draft').first
-              trigger_notice(recipient: organization.employer_profile, event_object: plan_year, notice_event:"renewal_plan_year_publish_dead_line" )
-          end
+          trigger_on_queried_records("renewal_plan_year_publish_dead_line")
         end
+      end
+    end
+
+
+    def trigger_on_queried_records(event_name)
+      current_date = TimeKeeper.date_of_record
+      organizations_for_force_publish(current_date).each do |organization|
+        plan_year = organization.employer_profile.plan_years.where(:aasm_state => 'renewing_draft').first
+        trigger_notice(recipient: organization.employer_profile, event_object: plan_year, notice_event:event_name)
       end
     end
 
