338b0413a893f75cc7ba1eb0abf19e80bb9f377d
Prem Shaganti <prem.shaganti@dc.gov>
2017-12-06 15:56:02 -0500

    ref:18954 address comments to update the rake to pass arguments

--- lib/tasks/nfp.rake
+++ lib/tasks/nfp.rake
@@ -1,13 +1,10 @@
+# This will upload the invoice to S3 and trigger notice
+# To Run this Rake we have to pass invoice path as argument
+# For Ex: RAILS_ENV=production bundle exec rake nfp:invoice_upload['/home/premshaganti/Downloads/invoices/']
 namespace :nfp do
   desc "Upload invoice to and associate with employer"
-  task :invoice_upload => :environment do |task, args|
-    invoice_folder = "invoices"
-    current_month_folder = TimeKeeper.date_of_record.strftime("%b-%Y")
-    if Rails.env.test?
-      absolute_folder_path = File.expand_path("spec/test_data/invoices/#{current_month_folder}")
-    else
-      absolute_folder_path = File.expand_path("#{invoice_folder}/#{current_month_folder}")
-    end
+  task :invoice_upload, [:invoice_path] => :environment do |task, args|
+    absolute_folder_path = args.invoice_path
     if Dir.exists?(absolute_folder_path)
       Dir.entries(absolute_folder_path).each do |file|
         next if File.directory?(file) #skipping directories
--- spec/lib/tasks/nfp_spec.rb
+++ spec/lib/tasks/nfp_spec.rb
@@ -6,17 +6,22 @@ Rake::Task.define_task(:environment)
 RSpec.describe 'upload the invoice to s3 and trigger the notice', :type => :task do
 
   let!(:employer_profile) { FactoryGirl.create(:employer_with_renewing_planyear, start_on: TimeKeeper.date_of_record.next_month.beginning_of_month, renewal_plan_year_state: 'renewing_enrolling') }
-  # let!(:employer_profile) { FactoryGirl.create(:employer_with_planyear) }
-  let!(:organization) { FactoryGirl.create(:organization, employer_profile: employer_profile, hbx_id: "210504") }
-  let!(:file_path) { "#{Rails.root}/spec/test_data/invoices/Dec-2017/210504_12212017_INVOICE_R.pdf"}
-  let!(:params) { { recipient: employer_profile, event_object: employer_profile.active_plan_year, notice_event: "employer_invoice_available"} }
+  let!(:organization) { FactoryGirl.create(:organization, employer_profile: employer_profile, hbx_id: "211045") }
+  let!(:file_path) { "spec/test_data/invoices/211045_12062017_INVOICE_R.pdf" }
+  let!(:params) { {recipient: employer_profile, event_object: employer_profile.active_plan_year, notice_event: "employer_invoice_available"} }
 
   context "Upload the notice to s3 and trigger the notice " do
     it "file invoice file exists it should upload and trigger notice" do
-      allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,12,23))
-      allow(Organization).to receive(:upload_invoice).with(file_path, "210504_12212017_INVOICE_R.pdf").and_return nil
+      allow(Organization).to receive(:upload_invoice).with(file_path, "211045_12062017_INVOICE_R.pdf").and_return nil
       expect_any_instance_of(Observers::Observer).to receive(:trigger_notice).with(params).and_return(true)
-      Rake::Task['nfp:invoice_upload'].invoke
+      Rake::Task['nfp:invoice_upload'].invoke('spec/test_data/invoices/')
     end
+
+    it "should not upload or trigger when no file is exists in direcotry" do
+      expect_any_instance_of(Observers::Observer).to_not receive(:trigger_notice).with(params).and_return(true)
+      Rake::Task['nfp:invoice_upload'].invoke('spec/test_data/user_name/')
+    end
+
   end
 end
+
