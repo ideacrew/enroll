ac2df60744cdd68fa879e6e80e6d05e38a2173b1
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2017-12-06 16:52:47 -0500

    Refs #20031. store dhs hub request raw payload.

--- spec/controllers/events/vlp_verification_requests_controller_spec.rb
+++ spec/controllers/events/vlp_verification_requests_controller_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 describe Events::VlpVerificationRequestsController do
   describe "#call with a person" do
-    let(:person) { double(hbx_id: "123") }
+    let(:person) { FactoryGirl.create(:person, :with_consumer_role) }
     let(:outbound_event_name) { "acapi.info.events.lawful_presence.vlp_verification_request" }
     let(:rendered_template) { double }
     let(:coverage_start_date) { double }
     let(:mock_end_time) { (mock_now + 24.hours).to_i }
     let(:mock_now) { Time.mktime(2015,5,21,12,29,39) }
 
-    it "should send out a message to the bus with the request to validate ssa" do
+    before do
       @event_name = ""
       @body = nil
       event_subscriber = ActiveSupport::Notifications.subscribe(outbound_event_name) do |e_name, s_at, e_at, m_id, payload|
@@ -18,14 +18,30 @@ describe Events::VlpVerificationRequestsController do
       end
       allow(Time).to receive(:now).and_return(mock_now)
       expect(controller).to receive(:render_to_string).with(
-        "events/lawful_presence/vlp_verification_request", {:formats => ["xml"], :locals => {
-         :individual => person,
-         :coverage_start_date => coverage_start_date
-        }}).and_return(rendered_template)
+          "events/lawful_presence/vlp_verification_request", {:formats => ["xml"], :locals => {
+          :individual => person,
+          :coverage_start_date => coverage_start_date
+      }}).and_return(rendered_template)
       controller.call(LawfulPresenceDetermination::VLP_VERIFICATION_REQUEST_EVENT_NAME, nil, nil, nil, {:person => person, :coverage_start_date => coverage_start_date } )
       ActiveSupport::Notifications.unsubscribe(event_subscriber)
+    end
+
+    it "should send out a message to the bus with the request to validate ssa" do
       expect(@event_name).to eq outbound_event_name
       expect(@body).to eq ({:body => rendered_template, :individual_id => person.hbx_id, :retry_deadline => mock_end_time})
+    end
+
+    it "stores verification history element" do
+      expect(person.consumer_role.lawful_presence_determination.vlp_requests.count).to be > 0
+    end
+
+    it "stores verification history element with proper verification type" do
+      expect(person.consumer_role.verification_type_history_elements.first.verification_type).to eq "Immigration status"
+    end
+
+    it "stores reference to event_request document" do
+      expect(person.consumer_role.lawful_presence_determination.vlp_requests.first.id).to eq BSON::ObjectId.from_string(
+          person.consumer_role.verification_type_history_elements.first.event_request_record_id)
     end
   end
 end
