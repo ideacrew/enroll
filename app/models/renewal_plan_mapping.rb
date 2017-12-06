810f34b7b47b4ec0c3cd8eed2bda6797f475e27d
vsonnathi <sonnathivinil@gmail.com>
2017-12-06 00:15:50 -0500

    Refs #20252: Plan mappings for tufts premier

--- app/models/renewal_plan_mapping.rb
+++ app/models/renewal_plan_mapping.rb
@@ -7,6 +7,8 @@ class RenewalPlanMapping
   field :renewal_plan_id, type: BSON::ObjectId
   field :is_active, type: Boolean, default: true
 
+  embedded_in :plan
+
   validates_presence_of :start_on, :end_on, :renewal_plan_id
 
   scope :by_date, ->(effective_date) { where({:start_on.lte => effective_date, :end_on.gte => effective_date, :is_active => true}) }
