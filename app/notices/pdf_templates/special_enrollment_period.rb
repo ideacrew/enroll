a99150d9f7588ae8fa7fab235c56f99c4e3850c5
krbhavith <krbhavith@gmail.com>
2017-12-10 16:10:29 -0500

    #15793_15794 rebase edits

--- app/notices/pdf_templates/special_enrollment_period.rb
+++ app/notices/pdf_templates/special_enrollment_period.rb
@@ -8,9 +8,5 @@ module PdfTemplates
     attribute :start_on, Date
     attribute :end_on, Date
     attribute :effective_on, Date
-<<<<<<< HEAD
-=======
-    
->>>>>>> # This is a combination of 10 commits.
   end
 end
