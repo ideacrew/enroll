1ae3796193151d68e6a2333bd92a01ae0c5aac4d
saip7795 <saip7795@gmail.com>
2017-12-10 16:32:51 -0500

    Ref18244: Changes in spec added positive and negative scenarios

--- app/data_migrations/reactivate_tax_household_with_assistance_year.rb
+++ app/data_migrations/reactivate_tax_household_with_assistance_year.rb
@@ -3,17 +3,17 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class ReactivateTaxHouseholdWithAssistanceYear < MongoidMigrationTask
   def migrate
     primary_person_hbx_id = ENV['primary_person_hbx_id']
-    applicable_year = ENV['applicable_year']
-    max_aptc = ENV['max_aptc']
-    csr_percent = ENV['csr_percent']
+    applicable_year = ENV['applicable_year'].to_i
+    max_aptc = ENV['max_aptc'].to_i
+    csr_percent = ENV['csr_percent'].to_i
     primary_person = Person.where(hbx_id: primary_person_hbx_id, is_active: true).first
     if primary_person
       begin
-        tax_household = primary_person.primary_family.active_household.tax_households.tax_household_with_year(applicable_year).each do |thh|
+        tax_households = primary_person.primary_family.active_household.tax_households.tax_household_with_year(applicable_year).select do |thh|
           ed = thh.latest_eligibility_determination
-          thh if ed.max_aptc.to_f == max_aptc.to_f && ed.csr_percent_as_integer == csr_percent
+          ed.max_aptc.to_f == max_aptc.to_f && ed.csr_percent_as_integer == csr_percent
         end
-        tax_household.update_attributes!(effective_ending_on: nil) if tax_household.present?
+        tax_households.each {|tax_household| tax_household.update_attributes(effective_ending_on: nil)} if tax_households.present?
       rescue => e
         puts "Could not update tax_household for this reason: #{e}"
       end
