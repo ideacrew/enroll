cc2a9fee5f78b1630d742b76c2bdd6fdcc144912
Dan Thomas <dan.thomas@dc.gov>
2017-12-13 12:29:46 -0500

    benefit applications initial commit

---
+++ components/sponsored_applications/spec/factories/sponsored_applications_benefit_applications_aca_shop_congress_benefit_applications.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :sponsored_applications_benefit_applications_aca_shop_congress_benefit_application, class: 'SponsoredApplications::BenefitApplications::AcaShopCongressBenefitApplication' do
+    
+  end
+end
