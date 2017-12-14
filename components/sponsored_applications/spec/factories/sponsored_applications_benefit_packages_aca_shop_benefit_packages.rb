8583dd29a2ccd66ba7255e57f4e8fd6363b4a2d9
Dan Thomas <dan.thomas@dc.gov>
2017-12-13 19:37:25 -0500

    add benefit package models

---
+++ components/sponsored_applications/spec/factories/sponsored_applications_benefit_packages_aca_shop_benefit_packages.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :sponsored_applications_benefit_packages_aca_shop_benefit_package, class: 'SponsoredApplications::BenefitPackages::AcaShopBenefitPackage' do
+    
+  end
+end
