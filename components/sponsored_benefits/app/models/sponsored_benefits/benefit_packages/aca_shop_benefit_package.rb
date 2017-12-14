ab344af815743fe9fbb84a9e00fb973a9df7620d
Dan Thomas <dan.thomas@dc.gov>
2017-12-14 14:48:01 -0500

    initial commit

---
+++ components/sponsored_benefits/app/models/sponsored_benefits/benefit_packages/aca_shop_benefit_package.rb
@@ -0,0 +1,5 @@
+module SponsoredBenefits
+  class BenefitPackages::AcaShopBenefitPackage
+    include Mongoid::Document
+  end
+end
---
+++ components/sponsored_benefits/app/models/sponsored_benefits/census_members/plan_design_census_member.rb
@@ -0,0 +1,5 @@
+module SponsoredBenefits
+  class CensusMembers::PlanDesignCensusMember
+    include Mongoid::Document
+  end
+end
