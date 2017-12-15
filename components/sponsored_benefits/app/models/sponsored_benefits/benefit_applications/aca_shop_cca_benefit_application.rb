b0fe94793f6c250114255c00bf2477d2ad5035fe
Dan Thomas <dan.thomas@dc.gov>
2017-12-15 11:17:22 -0500

    Add benefit_application and benefit_application_builder templates

---
+++ components/sponsored_benefits/app/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application.rb
@@ -0,0 +1,9 @@
+module SponsoredBenefits
+  class BenefitApplications::AcaShopCcaBenefitApplication < AcaShopBenefitApplication
+
+    # Move CCA-specific PlanYear code here. e.g. Rating areas, Employer Attestation, Geographic Rating Areas,
+    # SIC codes, etc
+
+
+  end
+end
