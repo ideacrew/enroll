f19c0c5ea228d0f4f9e1df86bb70caf0b9440753
Dan Thomas <dan.thomas@dc.gov>
2017-12-15 11:37:32 -0500

    initial commit: benefit_package_assignment (EA benefit_group_assignment)

---
+++ app/models/sponsored_benefits/benefit_package_assignment.rb
@@ -0,0 +1,8 @@
+module SponsoredBenefits
+  class BenefitPackageAssignment
+    include Mongoid::Document
+
+    # Copy EA BenefitGroupAssignment code here
+
+  end
+end
---
+++ spec/factories/sponsored_benefits_benefit_package_assignments.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :sponsored_benefits_benefit_package_assignment, class: 'SponsoredBenefits::BenefitPackageAssignment' do
+    
+  end
+end
