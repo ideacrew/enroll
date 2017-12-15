1d6a2f466d7cab15fa3d1abb9d77513880f229b2
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-12-15 13:22:33 -0500

    Refs #17516 fix specs to include hbx enrollment members for generation

--- spec/views/events/enrollment_event.xml.haml_spec.rb
+++ spec/views/events/enrollment_event.xml.haml_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
-Rspec.describe "events/enrollment_event.haml.erb" do 
+RSpec.describe "events/enrollment_event.haml.erb" do
+  let(:person) { FactoryGirl.create(:person)}
+  let(:family) { FactoryGirl.create(:family, :with_primary_family_member,person: person) }
+  let(:primary_applicant) { family.primary_applicant }
   let(:plan) { FactoryGirl.build(:plan) }
   let(:census_employee) { FactoryGirl.build(:census_employee, :benefit_group_assignments => [FactoryGirl.build(:benefit_group_assignment)]) }
   let(:employee_role) { FactoryGirl.build(:employee_role, census_employee: census_employee) }
   let(:benefit_group_assignment) { employee_role.census_employee.benefit_group_assignments.first }
   let(:benefit_group) { benefit_group_assignment.benefit_group }
-  let(:hbx_enrollment) {  HbxEnrollment.new(plan:plan, employee_role: employee_role, created_at: Time.now) }
+  let(:hbx_enrollment) {  FactoryGirl.create(:hbx_enrollment, :with_enrollment_members, household: family.active_household) }
+  let(:hbx_enrollment_member) { HbxEnrollmentMember.create(applicant_id: primary_applicant.id, hbx_enrollment: hbx_enrollment, is_subscriber: primary_applicant.is_primary_applicant, coverage_start_on: hbx_enrollment.effective_on, eligibility_date: hbx_enrollment.effective_on) }
 
   before :each do
     allow(hbx_enrollment).to receive(:broker_agency_account).and_return(nil)
     allow(hbx_enrollment).to receive(:benefit_group_assignment).and_return(benefit_group_assignment)
     allow(hbx_enrollment).to receive(:benefit_group).and_return(benefit_group)
+    hbx_enrollment.hbx_enrollment_members << hbx_enrollment_member
   end
 
   it "generates an enrollment event CV" do 
