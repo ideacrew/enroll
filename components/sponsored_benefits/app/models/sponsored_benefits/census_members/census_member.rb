19b106eeb72e63e89804a15fb8e70dc446398f37
raghuram <raghuramg83@gmail.com>
2017-12-16 11:19:59 -0500

    Refs #19401 Added ssn, dob, gender concerns and updated census members

--- components/sponsored_benefits/app/models/sponsored_benefits/census_members/census_member.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/census_members/census_member.rb
@@ -3,9 +3,37 @@ module SponsoredBenefits
 
     include Mongoid::Document
     include Mongoid::Timestamps
-    
+    include UnsetableSparseFields
+    include SponsoredBenefits::Concerns::Ssn
+    include SponsoredBenefits::Concerns::Dob
+    include SponsoredBenefits::Concerns::Gender
+
     store_in collection: 'census_members'
 
+    validates_with Validations::DateRangeValidator
+
+
+    field :first_name, type: String
+    field :middle_name, type: String
+    field :last_name, type: String
+    field :name_sfx, type: String
+
+    include StrippedNames
+
+    field :employee_relationship, type: String
+    field :employer_assigned_family_id, type: String
+
+    embeds_one :address
+    accepts_nested_attributes_for :address, reject_if: :all_blank, allow_destroy: true
+
+    embeds_one :email
+    accepts_nested_attributes_for :email, allow_destroy: true
+
+    validates_presence_of :first_name, :last_name, :employee_relationship
+
+    def full_name
+      [first_name, middle_name, last_name, name_sfx].compact.join(" ")
+    end
 
   end
 end
