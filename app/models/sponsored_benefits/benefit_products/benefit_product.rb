38b3ea73d7efa8e60a196a1ecd5357a0c9715217
Dan Thomas <dan.thomas@dc.gov>
2017-12-16 18:41:30 -0500

    add associations

--- app/models/sponsored_benefits/benefit_products/benefit_product.rb
+++ app/models/sponsored_benefits/benefit_products/benefit_product.rb
@@ -1,5 +1,11 @@
 module SponsoredBenefits
   class BenefitProducts::BenefitProduct
     include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :benefit_coverage_period, type: Range
+
+    belongs_to :issuer_profile
+
   end
 end
--- app/models/sponsored_benefits/benefit_sponsorship.rb
+++ app/models/sponsored_benefits/benefit_sponsorship.rb
@@ -1,12 +1,18 @@
 module SponsoredBenefits
   class BenefitSponsorship
     include Mongoid::Document
+    include Mongoid::Timestamps
+
+    embedded_in :sponsorable, polymorphic: true
+
+    embeds_one  :geographic_rating_area, class_name: "SponsoredBenefits::Locations::GeographicRatingArea"
+
+    has_many    :benefit_markets, class_name: "SponsoredBenefits::BenefitMarket" # (temporal: past, current and renewing)
+    has_many    :offered_benefit_products, class_name: "SponsoredBenefits::BenefitProducts::BenefitProduct"
+    has_many    :benefit_applications, cascade_callbacks: true, validate: true
+    embeds_many :broker_agency_accounts, cascade_callbacks: true, validate: true
 
 
-    field :benefit_market, type: Symbol
-    
-
-    has_many :benefit_applications, class_name: "SponsoredBenefits::BenefitSponsorship::BenefitApplications::BenefitApplication"
 
 
   end
