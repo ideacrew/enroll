55bc597870d26dc29d1ed753d93cd0af02330d2d
Brian Weiner <brian.weiner@dc.gov>
2017-12-16 11:38:33 -0500

    wip- benefit application builder spec

--- app/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application.rb
+++ app/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application.rb
@@ -1,9 +1,13 @@
 module SponsoredBenefits
-  class BenefitApplications::AcaShopCcaBenefitApplication < AcaShopBenefitApplication
+  module BenefitApplications
+    class AcaShopCcaBenefitApplication < AcaShopBenefitApplication
+      # Move CCA-specific PlanYear code here. e.g. Rating areas, Employer Attestation, Geographic Rating Areas,
+      # SIC codes, etc
+      field :recorded_sic_code, type: String
 
-    # Move CCA-specific PlanYear code here. e.g. Rating areas, Employer Attestation, Geographic Rating Areas,
-    # SIC codes, etc
+      field :record_rating_area, type: String
 
-
+    #  validates_inclusion_of :recorded_rating_area, :in => market_rating_areas, :allow_nil => true
+    end
   end
 end
--- app/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application_builder.rb
+++ app/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application_builder.rb
@@ -1,22 +1,18 @@
 module SponsoredBenefits
-  class BenefitApplications::AcaShopCcaBenefitApplicationBuilder < BenefitApplicationBuilder
+  module BenefitApplications
+    class AcaShopCcaBenefitApplicationBuilder < BenefitApplicationBuilder
 
-    def initialize(options={})
-      super(options)
+      def initialize(options={})
+        @application_class = AcaShopCcaBenefitApplication
+        super(options)
+      end
+
+      def add_employer_attestation(new_employer_attestation)
+      end
+
+      def add_marketplace_kind(marketplace_kind)
+        # raise "marketplace must be aca_shop" unless marketplace_kind == :aca_shop
+      end
     end
-
-    def add_employer_attestation(new_employer_attestation)
-    end
-
-
-    def add_marketplace_kind(marketplace_kind)
-      # raise "marketplace must be aca_shop" unless marketplace_kind == :aca_shop
-    end
-
-    def benefit_application
-      # raise "" if open_enrollment_term.blank?
-    end
-
-
   end
 end
