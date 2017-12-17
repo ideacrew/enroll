e448e7e29d8c83c121e865724486da77c287b6a5
Brian Weiner <brian.weiner@dc.gov>
2017-12-17 13:26:04 -0500

    basic route and controller started

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ SponsoredBenefits::Engine.routes.draw do
   namespace :benefit_sponsorships do
     resources :plan_design_employer_profiles
   end
+
+  resources :benefit_sponsorships, only: [] do
+    resources :benefit_applications
+  end
 end
