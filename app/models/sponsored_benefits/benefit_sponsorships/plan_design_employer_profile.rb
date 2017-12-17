c57190944031219145cdd1b9f67993bf70887806
Pranab <pranab.khanal@gmail.com>
2017-12-17 09:05:58 -0500

    Refs#20477 Added references between BrokerAgencyProfile and PlanDesignEmployerProfile models

--- app/models/sponsored_benefits/benefit_sponsorships/plan_design_employer_profile.rb
+++ app/models/sponsored_benefits/benefit_sponsorships/plan_design_employer_profile.rb
@@ -2,11 +2,12 @@ module SponsoredBenefits
   class BenefitSponsorships::PlanDesignEmployerProfile
     include Mongoid::Document
 
+    embedded_in :broker_agency_profile, class_name: "::BrokerAgencyProfile"
+
     field :entity_kind, type: String
     field :sic_code, type: String
     field :legal_name, type: String
     field :dba, type: String
-    field :entity_kind, type: String
 
     embeds_many :office_locations, class_name:"SponsoredBenefits::OfficeLocation"
   end
