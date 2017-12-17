37258b4e7b25743aeba2b9a6fee2fb64c304f049
raghuram <raghuramg83@gmail.com>
2017-12-17 16:02:50 -0500

    refs #19401 added plan design census member controller and  builders

--- components/sponsored_benefits/config/routes.rb
+++ components/sponsored_benefits/config/routes.rb
@@ -1,4 +1,11 @@
 SponsoredBenefits::Engine.routes.draw do
+
+  namespace :sponsored_benefits do
+    namespace :census_members do
+      resources :plan_design_census_employees
+    end
+  end
+
   namespace :benefit_sponsorships do
     resources :plan_design_employer_profiles
   end
