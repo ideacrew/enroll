ec98687ced42c8a2788dd74374ae4d6202b7a942
raghuram <raghuramg83@gmail.com>
2017-12-16 19:49:28 -0500

    Refs #19401 updated concerns

--- app/models/census_member.rb
+++ app/models/census_member.rb
@@ -4,15 +4,19 @@ class CensusMember
   include UnsetableSparseFields
   include SponsoredBenefits::Concerns::Ssn
   include SponsoredBenefits::Concerns::Dob
-  include SponsoredBenefits::Concerns::Gender
 
   validates_with Validations::DateRangeValidator
 
+  GENDER_KINDS = %W(male female)
 
   field :first_name, type: String
   field :middle_name, type: String
   field :last_name, type: String
   field :name_sfx, type: String
+
+  field :encrypted_ssn, type: String
+  field :gender, type: String
+  field :dob, type: Date
 
   include StrippedNames
 
@@ -25,9 +29,26 @@ class CensusMember
   embeds_one :email
   accepts_nested_attributes_for :email, allow_destroy: true
 
+  validates :gender,
+  allow_blank: false,
+  inclusion: { in: GENDER_KINDS, message: "must be selected" }
+
+
   validates_presence_of :first_name, :last_name, :employee_relationship
 
   def full_name
     [first_name, middle_name, last_name, name_sfx].compact.join(" ")
+  end
+
+  def date_of_birth=(val)
+    self.dob = Date.strptime(val, "%Y-%m-%d").to_date rescue nil
+  end
+
+  def gender=(val)
+    if val.blank?
+      write_attribute(:gender, nil)
+      return
+    end
+    write_attribute(:gender, val.downcase)
   end
 end
