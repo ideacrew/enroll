b59340a9f251b4b984724439541a017558e570f6
krbhavith <krbhavith@gmail.com>
2017-12-17 20:40:46 -0500

    refs #spec fix

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -1424,7 +1424,7 @@ class HbxEnrollment
   def ee_select_plan_during_oe
     begin
       if self.census_employee.present? && self.employee_role.is_under_open_enrollment? && self.enrollment_kind == "open_enrollment"
-        self.census_employee.update_attributes!(employee_role_id: hbx_enrollment.employee_role.id.to_s ) if !census_employee.employee_role.present?
+        self.census_employee.update_attributes!(employee_role_id: self.employee_role.id.to_s ) if !census_employee.employee_role.present?
         ShopNoticesNotifierJob.perform_later(self.census_employee.id.to_s, "ee_select_plan_during_oe", hbx_enrollment: self.hbx_id.to_s)
       end
     rescue Exception => e
