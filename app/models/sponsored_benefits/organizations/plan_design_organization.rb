e307238334576aff7070f52442138cd99e9e71d7
Dan Thomas <dan.thomas@dc.gov>
2017-12-18 07:08:41 -0500

    Subclass from Organization and remove redundant includes and field definitions

--- app/models/sponsored_benefits/organizations/plan_design_organization.rb
+++ app/models/sponsored_benefits/organizations/plan_design_organization.rb
@@ -1,15 +1,13 @@
 # Broker-owned model to manage attributes of the prospective of existing employer
 module SponsoredBenefits
   module Organizations
-    class PlanDesignOrganization
-      include Mongoid::Document
-      include Mongoid::Timestamps
+    class PlanDesignOrganization < Organization
 
       belongs_to :broker_agency_profile, class_name: "SponsoredBenefits::Organizations::BrokerAgencyProfile"
 
       # Plan design owner profile type & ID
-      # field :owner_profile_id,    type: BSON::ObjectId
-      # field :owner_profile_kind,  type: String
+      field :owner_profile_id,    type: BSON::ObjectId
+      field :owner_profile_kind,  type: String, default: "::BrokerAgency_profile"
 
       # Plan design owner role type & ID
       # field :owner_role_id, type: BSON::ObjectId
@@ -20,16 +18,7 @@ module SponsoredBenefits
       field :customer_profile_class_name, type: String, default: "::EmployerProfile"
       field :entity_kind, type: String
 
-      # Registered legal name
-      field :legal_name, type: String
 
-      # Doing Business As (alternate name)
-      field :dba, type: String
-
-      # Federal Employer ID Number
-      field :fein, type: String
-
-      belongs_to :broker_agency_profile, class_name: "SponsoredBenefits::Organizations::BrokerAgencyProfile"
       embeds_one :plan_design_profile, class_name: "SponsoredBenefits::Organizations::AcaShopCcaEmployerProfile"
 
       embeds_one :plan_design_employer_profile, class_name: "SponsoredBenefits::BenefitSponsorships::PlanDesignEmployerProfile"
