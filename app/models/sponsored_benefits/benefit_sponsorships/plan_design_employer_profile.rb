b3950c3d0dfc88bc809d97ffb5a76f4728632d48
Pranab <pranab.khanal@gmail.com>
2017-12-20 07:37:33 -0500

    Refs#20477 Datatables and Intake form for creating prospect employer

---
+++ app/models/sponsored_benefits/benefit_sponsorships/plan_design_employer_profile.rb
@@ -0,0 +1,24 @@
+module SponsoredBenefits
+  class BenefitSponsorships::PlanDesignEmployerProfile
+    include Mongoid::Document
+
+    #embedded_in :plan_design_organization, class_name: "SponsoredBenefits::Organizations::PlanDesignOrganization"
+
+    field :entity_kind, type: String
+    field :sic_code, type: String
+    field :legal_name, type: String
+    field :dba, type: String
+
+    embeds_many :office_locations, class_name:"SponsoredBenefits::Organizations::OfficeLocation"
+
+
+
+    def self.find(id)
+      organizations = PlanDesignOrganization.where("employer_profile._id" => BSON::ObjectId.from_string(id))
+      organizations.size > 0 ? organizations.first.employer_profile : nil
+    rescue
+      log("Can not find employer_profile with id #{id}", {:severity => "error"})
+      nil
+    end
+  end
+end
