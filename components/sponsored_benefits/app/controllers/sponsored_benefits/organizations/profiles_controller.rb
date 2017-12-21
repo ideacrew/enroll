3ebfe03a657137867218f9c27568460d96fd616a
Brian Weiner <brian.weiner@dc.gov>
2017-12-21 16:45:38 -0500

    basic menu displayable

--- components/sponsored_benefits/app/controllers/sponsored_benefits/organizations/profiles_controller.rb
+++ components/sponsored_benefits/app/controllers/sponsored_benefits/organizations/profiles_controller.rb
@@ -32,7 +32,13 @@ module SponsoredBenefits
     def update
     end
 
-  private  
+  private
+    helper_method :active_tab
+
+    def active_tab
+      "employers-tab"
+    end
+    
     def find_broker_agency_profile
       @broker_agency_profile = ::BrokerAgencyProfile.find(params[:profile_id])
       #authorize @broker_agency_profile, :access_to_broker_agency_profile?
