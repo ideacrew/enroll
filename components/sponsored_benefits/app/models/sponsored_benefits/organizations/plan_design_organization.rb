a47f3e97208ce8e7f8ebb15b0d30d358ebda5daf
raghuram <raghuramg83@gmail.com>
2017-12-21 15:11:56 -0500

    Refs #19401 Added Plan Design proposal model

--- components/sponsored_benefits/app/models/sponsored_benefits/organizations/plan_design_organization.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/organizations/plan_design_organization.rb
@@ -1,10 +1,7 @@
 # Broker-owned model to manage attributes of the prospective of existing employer
 module SponsoredBenefits
   module Organizations
-    class PlanDesignOrganization
-      include Concerns::OrganizationConcern
-
-      belongs_to :broker_agency_profile, class_name: "SponsoredBenefits::Organizations::BrokerAgencyProfile", inverse_of: :plan_design_organization
+    class PlanDesignOrganization < Organization
 
       # Plan design owner profile type & ID
       field :owner_profile_id,    type: BSON::ObjectId
@@ -19,16 +16,16 @@ module SponsoredBenefits
       field :customer_profile_class_name, type: String, default: "::EmployerProfile"
       field :entity_kind, type: String
 
-      embeds_one :profile
+      belongs_to :broker_agency_profile, class_name: "SponsoredBenefits::Organizations::BrokerAgencyProfile", foreign_key: 'customer_profile_id'
+      embeds_one :plan_design_profile, class_name: "SponsoredBenefits::Organizations::PlanDesignProfile"
 
-      scope :find_by_profile,  -> (profile) { where(:"profile._id" => BSON::ObjectId.from_string(profile)) }
+      scope :find_by_profile,  -> (profile) { where(:"plan_design_profile._id" => BSON::ObjectId.from_string(profile)) }
       scope :find_by_customer, -> (customer_id) { where(:"customer_profile_id" => BSON::ObjectId.from_string(customer_id)) }
       scope :find_by_owner, -> (owner_id) { where(:"owner_profile_id" => BSON::ObjectId.from_string(owner_id)) }
 
       def employer_profile
         ::EmployerProfile.find(customer_profile_id)
       end
-
     end
   end
 end
