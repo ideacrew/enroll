c4066a52230bf1b3142a9ebca0971f33021b93f9
Pranab <pranab.khanal@gmail.com>
2017-12-21 17:33:02 -0500

    datatable and prospect employer intake complete

--- app/models/effective/datatables/broker_agency_employer_datatable.rb
+++ app/models/effective/datatables/broker_agency_employer_datatable.rb
@@ -6,16 +6,16 @@
 
           table_column :legal_name, :label => 'Legal Name', :proc => Proc.new { |row| row.legal_name }, :sortable => false, :filter => false
           table_column :fein, :label => 'FEIN', :proc => Proc.new { |row| row.fein }, :sortable => false, :filter => false
-          table_column :ee_count, :label => 'EE Count', :proc => Proc.new { |row| row.employer_profile.roster_size }, :sortable => false, :filter => false
-          table_column :er_state, :label => 'ER State', :proc => Proc.new { |row| row.employer_profile.aasm_state }, :sortable => false, :filter => false
-          table_column :effective_date, :label => 'Effective Date', :proc => Proc.new { |row| row.employer_profile.registered_on }, :sortable => false, :filter => false
+          table_column :ee_count, :label => 'EE Count', :proc => Proc.new { |row| "N/A" }, :sortable => false, :filter => false
+          table_column :er_state, :label => 'ER State', :proc => Proc.new { |row| "N/A" }, :sortable => false, :filter => false
+          table_column :effective_date, :label => 'Effective Date', :proc => Proc.new { |row| row.try(:employer_profile).try(:registered_on) }, :sortable => false, :filter => false
           #table_column :broker, :label => 'Broker', :proc => Proc.new { |row| row.employer_profile.active_broker.full_name }, :sortable => false, :filter => false
 
           table_column :actions, :width => '50px', :proc => Proc.new { |row|
             dropdown = [
              # Link Structure: ['Link Name', link_path(:params), 'link_type'], link_type can be 'ajax', 'static', or 'disabled'
              #['Create Quote', new_broker_agencies_broker_role_quote_path(broker_role_id: BrokerAgencyEmployerDatatable.profile_id, id: row.id), 'static'],
-             ['Create Quote', sponsored_benefits.broker_client_benefit_applications_path(broker_id: row.owner_profile_id, client_id: row.customer_profile_id), 'static'],
+             ['Create Quote', "sponsored_benefits.broker_client_benefit_applications_path(broker_id: row.owner_profile_id, client_id: row.customer_profile_id)", 'static'],
              ['Remove Quote', 'some-quote-remove-path', 'static']
 
             ]
@@ -33,14 +33,8 @@
 
         def collection
           profile_id = attributes["collection_scope"] || BrokerAgencyEmployerDatatable.profile_id
-
-          # Existing
-          existing_employers = Organization.by_broker_agency_profile(profile_id)
-
-          # Prospective
-          prospective_employers = SponsoredBenefits::Organizations::PlanDesignOrganization.find_by_owner(profile_id)
-
-          return prospective_employers #for now
+          employers = SponsoredBenefits::Organizations::Organization.where(broker_agency_profile_id: profile_id)
+          return employers
         end
 
         def global_search?
