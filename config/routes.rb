58bd290ec17e88ab8bce3acddc76c8f01e96dad9
Brian Weiner <brian.weiner@dc.gov>
2017-12-21 11:27:59 -0500

    create a plan design org on broker assignment, basic link to create quote

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,15 @@ SponsoredBenefits::Engine.routes.draw do
   #   resources :plan_design_employer_profiles
   # end
   #
-  resources :benefit_sponsorships, only: [] do
-    resources :benefit_applications, controller: 'benefit_applications/benefit_applications', only: [:index, :new, :create]
-  end
+  # resources :benefit_sponsorships, only: [] do
+  #   resources :benefit_applications, controller: 'benefit_applications/benefit_applications', only: [:index, :new, :create]
+  # end
 
   resources :benefit_applications, controller: 'benefit_applications/benefit_applications', only: [:show, :edit, :update, :destroy]
 
   resources :broker_agency_profile, path: 'broker', as: 'broker', only: [] do
-    resources :plan_design_organization, path: 'client', as: 'client'
+    resources :plan_design_organization, path: 'client', as: 'client', only: [] do
+      resources :benefit_applications, controller: 'benefit_applications/benefit_applications', only: [:index, :new, :create]
+    end
   end
 end
