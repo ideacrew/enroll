6282724b02c25c1a788c5fe3a291b7db473689a7
Pranab <pranab.khanal@gmail.com>
2017-12-21 17:33:02 -0500

    datatable and prospect employer intake complete

--- app/models/sponsored_benefits/organizations/plan_design_organization.rb
+++ app/models/sponsored_benefits/organizations/plan_design_organization.rb
@@ -1,7 +1,11 @@
 # Broker-owned model to manage attributes of the prospective of existing employer
 module SponsoredBenefits
-  module Organizations
-    class PlanDesignOrganization < Organization
+  module  Organizations
+    class PlanDesignOrganization < SponsoredBenefits::Organizations::Organization
+
+      belongs_to :broker_agency_profile, class_name: "::BrokerAgencyProfile", foreign_key: 'broker_agency_profile_id'
+
+      field :profile_kind, type: String, default: ":plan_design_profile"
 
       # Plan design owner profile type & ID
       field :owner_profile_id,    type: BSON::ObjectId
@@ -14,10 +18,13 @@ module SponsoredBenefits
       # Plan design customer profile type & ID
       field :customer_profile_id,         type: BSON::ObjectId
       field :customer_profile_class_name, type: String, default: "::EmployerProfile"
-      field :entity_kind, type: String
 
       belongs_to :broker_agency_profile, class_name: "SponsoredBenefits::Organizations::BrokerAgencyProfile", foreign_key: 'customer_profile_id'
-      embeds_one :plan_design_profile, class_name: "SponsoredBenefits::Organizations::PlanDesignProfile"
+      
+      #embeds_one :plan_design_profile, class_name: "SponsoredBenefits::Organizations::PlanDesignProfile"
+
+      embeds_one :profile
+      accepts_nested_attributes_for :profile
 
       scope :find_by_profile,  -> (profile) { where(:"plan_design_profile._id" => BSON::ObjectId.from_string(profile)) }
       scope :find_by_customer, -> (customer_id) { where(:"customer_profile_id" => BSON::ObjectId.from_string(customer_id)) }
@@ -26,6 +33,12 @@ module SponsoredBenefits
       def employer_profile
         ::EmployerProfile.find(customer_profile_id)
       end
+
+
+      def is_prospect?
+        customer_profile_id.nil? 
+      end
+
     end
   end
 end
