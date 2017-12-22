e2268a75bbba7a0471f2245381d4dfe52f264893
Dan Thomas <dan.thomas@dc.gov>
2017-12-21 20:27:51 -0500

    Initial commit

--- spec/factories/sponsored_benefits_organizations_aca_shop_cca_employer_profiles.rb
+++ spec/factories/sponsored_benefits_organizations_aca_shop_cca_employer_profiles.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :shop_cca_employer_profile, class: 'SponsoredBenefits::Organizations::AcaShopCcaEmployerProfile' do
 
+    sic_code '0111'
+    profile_source 'broker_quote'
+    contact_method 'Only Electronic communications'
+
   end
 end
