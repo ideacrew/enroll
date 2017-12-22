74d489eb43554de5c49e441de73765adc0474732
Brian Weiner <brian.weiner@dc.gov>
2017-12-22 09:46:25 -0500

    basic employer quotes index datatable started, removed prospect employers and used employers tab intsead

---
+++ components/sponsored_benefits/app/models/effective/datatables/broker_employer_quotes_datatable.rb
@@ -0,0 +1,68 @@
+# module SponsoredBenefits
+  module Effective
+    module Datatables
+      class BrokerEmployerQuotesDatatable < ::Effective::MongoidDatatable
+        datatable do
+
+          table_column :title, :label => 'Legal Name', :proc => Proc.new { |row| row.title }, :sortable => false, :filter => false
+          table_column :claim_date, :label => 'Claim Date', :proc => Proc.new { |row| row.claim_date }, :sortable => false, :filter => false
+          #table_column :broker, :label => 'Broker', :proc => Proc.new { |row| row.employer_profile.active_broker.full_name }, :sortable => false, :filter => false
+
+          table_column :actions, :width => '50px', :proc => Proc.new { |row|
+            dropdown = [
+             # Link Structure: ['Link Name', link_path(:params), 'link_type'], link_type can be 'ajax', 'static', or 'disabled'
+             # ['View Quotes', sponsored_benefits.plan_design_organization_plan_design_proposals_path(plan_design_organization_id: row._id), 'static'],
+             # ['Create Quote', sponsored_benefits.new_plan_design_organization_plan_design_proposal_path(plan_design_organization_id: row._id), 'static'],
+             # ['Remove Quote', 'some-quote-remove-path', 'static'],
+             # ['Edit Employer', sponsored_benefits.edit_organizations_profile_path(row), 'static']
+            ]
+            render partial: 'datatables/shared/dropdown', locals: {dropdowns: dropdown, row_actions_id: "quotes_actions_#{row.id.to_s}"}, formats: :html
+          }, :filter => false, :sortable => false
+        end
+
+        class << self
+        	attr_accessor :organization_id
+        end
+
+        def collection
+          unless (defined? @quotes) && @quotes.present?
+            @quotes = Queries::PlanDesignOrganizationQuotesQuery.new(attributes)
+          end
+          @quotes
+        end
+
+        def global_search?
+          true
+        end
+
+        def global_search_method
+          :datatable_search
+        end
+
+        def search_column(collection, table_column, search_term, sql_column)
+          # if table_column[:name] == 'legal_name'
+          #   collection.datatable_search(search_term)
+          # elsif table_column[:name] == 'fein'
+          #   collection.datatable_search_fein(search_term)
+          # else
+          #   super
+          # end
+        end
+
+        def nested_filter_definition
+          # {
+          #   clients:[
+          #         { scope: 'all', label: 'All'},
+          #         { scope: 'active_clients', label: 'Active'},
+          #         { scope: 'inactive_clients', label: 'Inactive'},
+          #         { scope: 'prospect_employers', label: "Prospects" }
+          #       ],
+          #   top_scope: :clients
+          # }
+        end
+
+
+      end
+    end
+  end
+# end
