0e0d2a6cdaff456f871d28a5d7c66ee8915719a6
Battula <vrbattula@outlook.com>
2017-12-23 14:17:48 -0500

    refs #17703 Template updates.

--- db/seedfiles/notice_triggers_seed.rb
+++ db/seedfiles/notice_triggers_seed.rb
@@ -446,7 +446,7 @@ shop_notice_triggers = [
   },
   {
     hbx_id: 'SHOP21',
-    title: 'Monthly Invoice Available',
+    title: 'Monthly Invoice Available Notice',
     description: 'When groups invoice is available in their account, this notice is sent to them.',
     resource_name: 'employer',
     event_name: 'employer_invoice_available',
