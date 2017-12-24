abd0e1cffa47eefb6536ed85e12541326a87602a
raghuram <raghuramg83@gmail.com>
2017-12-23 22:19:24 -0500

    Refs #19401 updated plan design forms

--- app/controllers/sponsored_benefits/organizations/plan_design_proposals_controller.rb
+++ app/controllers/sponsored_benefits/organizations/plan_design_proposals_controller.rb
@@ -8,7 +8,6 @@ module SponsoredBenefits
     before_action :find_broker_agency_profile, only: [:employers]
     before_action :init_datatable, only: [:employers]
 
-
     def index
       @datatable = effective_datatable
     end
@@ -18,7 +17,6 @@ module SponsoredBenefits
       init_employee_datatable
     end
 
-  
     def show
       # load relevant quote (not nested)
       # plan_design_proposal
@@ -30,32 +28,31 @@ module SponsoredBenefits
     end
 
     def create
-      # create quote for sponsorship
       proposal_form = SponsoredBenefits::Forms::PlanDesignProposal.new({
         organization: plan_design_organization
-        }.merge(params.require(:forms_plan_design_proposal)))
+        }.merge(plan_design_proposal_params))
 
       respond_to do |format|
         if proposal_form.save
           @plan_design_proposal = proposal_form
           format.html { redirect_to new_plan_design_organization_plan_design_proposal_path(proposal_id: proposal_form.proposal.id), :flash => { :success => "Quote information save successfully."} } 
         else
-          format.html { redirect_to :back, :flash => { :success => "Failed."} }
+          format.html { redirect_to :back, :flash => { :error => "Failed! unable to save quote information."} }
         end
       end
     end
 
     def update
       proposal_form = SponsoredBenefits::Forms::PlanDesignProposal.new({
-        organization: plan_design_proposal.plan_design_organization, 
-        proposal_id: params[:id]}.merge(params.require(:forms_plan_design_proposal)))
+        organization: plan_design_proposal.plan_design_organization, proposal_id: params[:id]
+        }.merge(plan_design_proposal_params))
 
       respond_to do |format|
         if proposal_form.save
           @plan_design_proposal = proposal_form
           format.html { redirect_to edit_plan_design_proposal_path(proposal_form.proposal), :flash => { :success => "Quote information updated successfully."} } 
         else
-          format.html { redirect_to :back, :flash => { :success => "Update failed."} }
+          format.html { redirect_to :back, :flash => { :error => "Failed! unable to update quote information."} }
         end
       end
     end
@@ -66,58 +63,58 @@ module SponsoredBenefits
     end
 
     private
-      helper_method :customer, :broker, :plan_design_organization
 
-      def broker
-        @broker ||= SponsoredBenefits::Organizations::BrokerAgencyProfile.find(plan_design_organization.owner_profile_id)
-      end
+    helper_method :customer, :broker, :plan_design_organization
 
-      def customer
-        @customer ||= ::EmployerProfile.find(plan_design_organization.customer_profile_id)
-      end
+    def broker
+      @broker ||= SponsoredBenefits::Organizations::BrokerAgencyProfile.find(plan_design_organization.owner_profile_id)
+    end
 
-      def effective_datatable
-        ::Effective::Datatables::BrokerEmployerQuotesDatatable.new(organization_id: plan_design_organization._id)
-      end
+    def customer
+      @customer ||= ::EmployerProfile.find(plan_design_organization.customer_profile_id)
+    end
 
-      def plan_design_organization
-        @plan_design_organization ||= SponsoredBenefits::Organizations::PlanDesignOrganization.find(params[:plan_design_organization_id])
-      end
+    def effective_datatable
+      ::Effective::Datatables::BrokerEmployerQuotesDatatable.new(organization_id: plan_design_organization._id)
+    end
 
-      # def benefit_sponsorship
-      #   broker.benefit_sponsorships.first || broker.benefit_sponsorships.new
-      # end
+    def plan_design_organization
+      @plan_design_organization ||= SponsoredBenefits::Organizations::PlanDesignOrganization.find(params[:plan_design_organization_id])
+    end
 
-      # def benefit_sponsorship_applications
-      #   @benefit_sponsorship_applicatios ||= benefit_sponsorship.plan_design_proposals
-      # end
+    # def benefit_sponsorship
+    #   broker.benefit_sponsorships.first || broker.benefit_sponsorships.new
+    # end
 
-      def plan_design_proposal
-        @plan_design_proposal ||= Organizations::PlanDesignProposal.find(params[:id])
-      end
+    # def benefit_sponsorship_applications
+    #   @benefit_sponsorship_applicatios ||= benefit_sponsorship.plan_design_proposals
+    # end
 
-      def plan_design_proposal_params
-        params.require(:plan_design_proposal).permit(
-          :title,
-          profile: [
-            benefit_sponsorship: [
-              :initial_enrollment_period,
-              :annual_enrollment_period_begin_month_of_year,
-              benefit_application: [
-                :effective_period,
-                :open_enrollment_period
-              ]
+    def plan_design_proposal
+      @plan_design_proposal ||= Organizations::PlanDesignProposal.find(params[:id])
+    end
+
+    def plan_design_proposal_params
+      params.require(:forms_plan_design_proposal).permit(
+        :title,
+        :effective_date,
+        profile: [
+          benefit_sponsorship: [
+            :initial_enrollment_period,
+            :annual_enrollment_period_begin_month_of_year,
+            benefit_application: [
+              :effective_period,
+              :open_enrollment_period
             ]
           ]
+        ]
         )
-      end
+    end
 
     def init_employee_datatable
       sponsorship = @plan_design_proposal.profile.benefit_sponsorships.first
-      data_table_params = {id: sponsorship.id, scopes: params[:scopes]}
       @census_employees = sponsorship.census_employees
-      @datatable = Effective::Datatables::PlanDesignEmployeeDatatable.new(data_table_params)
+      @datatable = Effective::Datatables::PlanDesignEmployeeDatatable.new({id: sponsorship.id, scopes: params[:scopes]})
     end
-
   end
 end
--- app/models/sponsored_benefits/forms/plan_design_proposal.rb
+++ app/models/sponsored_benefits/forms/plan_design_proposal.rb
@@ -9,7 +9,6 @@ module SponsoredBenefits
       attr_reader :profile
       attr_reader :plan_design_organization
       attr_reader :proposal
-      attr_reader :file
 
       validates_presence_of :title, :effective_date, :sic_code, :county, :zip_code
 
@@ -62,9 +61,10 @@ module SponsoredBenefits
 
       def ensure_sic_zip_county
         @sic_code = @plan_design_organization.sic_code
-        location = @plan_design_organization.office_locations.first
-        @zip_code = location.address.zip
-        @county = location.address.county
+        if location = @plan_design_organization.office_locations.first
+          @zip_code = location.address.zip
+          @county = location.address.county
+        end
       end
 
       def ensure_profile
@@ -84,26 +84,11 @@ module SponsoredBenefits
         else
           profile = SponsoredBenefits::Organizations::AcaShopCcaEmployerProfile.new({sic_code: @sic_code})
           @proposal = @plan_design_organization.plan_design_proposals.build({title: @title, profile: profile})
-          sponsorship = @proposal.profile.benefit_sponsorships.build({benefit_market: :aca_shop_cca, initial_enrollment_period: initial_enrollment_period, annual_enrollment_period_begin_month: @effective_date.month})
+          @proposal.profile.benefit_sponsorships.build({benefit_market: :aca_shop_cca, initial_enrollment_period: initial_enrollment_period, annual_enrollment_period_begin_month: @effective_date.month})
         end
 
         @plan_design_organization.save
       end
-
-      # def census_dependents=(attrs)
-      # end
-
-      # def save
-      #   census_employee = SponsoredBenefits::CensusMembers::PlanDesignCensusEmployeeBuilder.build do |builder|
-      #     builder.add_first_name(first_name)
-      #     builder.add_last_name(last_name)
-      #     builder.add_ssn(ssn)
-      #     builder.add_dob(dob)
-      #     builder.add_dependent(dependent)
-      #   end
-
-      #   census_employee.save
-      # end
     end
   end
 end
