bb529396dbfa5d0cdd4537acae88d5659b0aac4f
raghuram <raghuramg83@gmail.com>
2017-12-25 10:04:55 -0500

    Refs #19401 Added fix to load broker agency profile for hbx staff

--- app/controllers/sponsored_benefits/application_controller.rb
+++ app/controllers/sponsored_benefits/application_controller.rb
@@ -10,7 +10,13 @@ module SponsoredBenefits
       end
 
       def set_broker_agency_profile_from_user
-        @broker_agency_profile = ::BrokerAgencyProfile.find(current_person.broker_role.broker_agency_profile_id)
+        if current_person.broker_role.present?
+          @broker_agency_profile = ::BrokerAgencyProfile.find(current_person.broker_role.broker_agency_profile_id)
+        end
+
+        if current_user.has_hbx_staff_role? && params[:plan_design_organization_id].present?
+          @broker_agency_profile = ::BrokerAgencyProfile.find(params[:plan_design_organization_id])
+        end
       end
 
       def current_person
