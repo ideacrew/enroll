f33dfea2bd6a90079931c46fbd6bbe2e6fb1077e
Dan Thomas <dan.thomas@dc.gov>
2017-12-26 18:04:56 -0500

    WIP refactoring benefit_application enrollment period and open enrollment date calculations

--- spec/models/sponsored_benefits/benefit_applications/benefit_application_spec.rb
+++ spec/models/sponsored_benefits/benefit_applications/benefit_application_spec.rb
@@ -62,7 +62,76 @@ module SponsoredBenefits
       end
     end
 
+    context "a BenefitApplication class" do
+      let(:subject)             { BenefitApplications::BenefitApplication }
+      let(:standard_begin_day)  { 10 }
+      let(:grace_begin_day)     { 15 }
 
+      it "should return the day of month deadline for an open enrollment standard period to begin" do
+        expect(subject.open_enrollment_begin_deadline_day_of_month).to eq standard_begin_day
+      end
+
+      it "should return the day of month deadline for an open enrollment grace period to begin" do
+        expect(subject.open_enrollment_begin_deadline_day_of_month(true)).to eq grace_begin_day
+      end
+
+      context "and passed a calendar date" do
+        let(:seed_date)                         { Date.new(2018,4,4) }
+        let(:next_month)                        { seed_date.beginning_of_month + 1.month }
+        let(:following_month)                   { seed_date.beginning_of_month + 2.months }
+
+        let(:standard_period_last_day)          { subject.open_enrollment_begin_deadline_day_of_month }
+        let(:standard_period_deadline_date)     { Date.new(seed_date.year, seed_date.month, standard_period_last_day) }
+        let(:standard_period_pre_deadline_date) { standard_period_deadline_date - 1.day }
+
+        let(:grace_period_last_day)             { subject.open_enrollment_begin_deadline_day_of_month(true) }
+        let(:grace_period_deadline_date)        { Date.new(seed_date.year, seed_date.month, grace_period_last_day) }
+        let(:grace_period_post_deadline_date)   { grace_period_deadline_date + 1.day }
+
+
+        context "that is before standard period deadline" do
+          it "should provide an effective (standard) period begin date for the first of next month" do
+            expect(subject.effective_period_begin_on_by_date(standard_period_pre_deadline_date)).to eq next_month
+          end
+
+          it "should provide an effective (grace) period begin date for the first of next month" do
+            expect(subject.effective_period_begin_on_by_date(standard_period_pre_deadline_date, true)).to eq next_month
+          end
+        end
+
+        context "that is the same day as the standard period deadline" do
+          it "should provide an effective (standard) period begin date for the first of next month" do
+            expect(subject.effective_period_begin_on_by_date(standard_period_deadline_date)).to eq next_month
+          end
+
+          it "should provide an effective (grace) period begin date for the first of next month" do
+            expect(subject.effective_period_begin_on_by_date(standard_period_deadline_date, true)).to eq next_month
+          end
+        end
+
+        context "that is after the standard period, but before the grace period deadline" do
+          it "should provide an effective (standard) period begin date for the first of month following next month" do
+            expect(subject.effective_period_begin_on_by_date(grace_period_deadline_date)).to eq following_month
+          end
+
+          it "should provide an effective (grace) period begin date for the of first next month" do
+            expect(subject.effective_period_begin_on_by_date(grace_period_deadline_date, true)).to eq next_month
+          end
+        end
+
+        context "that is after both the standard and grace period deadlines" do
+          it "should provide an effective (standard) period begin date for the first of month following next month" do
+            expect(subject.effective_period_begin_on_by_date(grace_period_post_deadline_date)).to eq following_month
+          end
+
+          it "should provide an effective (grace) period begin date for the first of month following next month" do
+            expect(subject.effective_period_begin_on_by_date(grace_period_post_deadline_date, true)).to eq following_month
+          end
+
+        end
+
+      end
+    end
 
 
     # it "assigns date ranges correctly" do
