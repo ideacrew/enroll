9a91117cc843e3ac505eb0e09758924fd330497b
nisanth <nisanth.yaganti@gmail.com>
2017-12-28 15:42:53 -0500

    refs #15206 trigger sep accepted notice after providing qle date

--- app/controllers/insured/family_members_controller.rb
+++ app/controllers/insured/family_members_controller.rb
@@ -43,9 +43,6 @@ class Insured::FamilyMembersController < ApplicationController
       special_enrollment_period.qle_answer = params[:qle_reason_choice] if params[:qle_reason_choice].present?
       special_enrollment_period.save
       @market_kind = qle.market_kind
-      if !@person.has_multiple_active_employers? && qle.present? && qle.shop? && @employee_role.present? && @employee_role.census_employee.present?
-        @employee_role.census_employee.trigger_notice("ee_sep_request_accepted_notice")
-      end
     end
 
     if request.referer.present?
