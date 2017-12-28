9f982d6acca656272da9bcee8ed4959038bd3912
saikumar9 <saikumarkotagiri9@gmail.com>
2017-12-28 11:11:44 -0500

    Refs 20514 pick enrolled enrollments

--- script/enrollment_notice_generator_with_date_range.rb
+++ script/enrollment_notice_generator_with_date_range.rb
@@ -21,7 +21,7 @@ def valid_enrollment_hbx_ids(family)
   auto_renewing_enrollments = enrollments.where(kind: "individual").renewing.by_submitted_datetime_range(@start_date, @end_date)
   bad_enrollments_fre = enrollments.where(kind: "individual").enrolled.by_submitted_datetime_range(Date.new(2017, 1, 1), @start_date - 1.days)
   has_renewals = good_enrollments.any?{ |hbx_enr| hbx_enr.was_in_renewal_status? } ? true : false
-  future_enrollments = enrollments.where(kind: "individual").any? { |enr| enr.submitted_at.present? && (enr.submitted_at.to_date > @end_date) }
+  future_enrollments = enrollments.enrolled.where(kind: "individual").any? { |enr| enr.submitted_at.present? && (enr.submitted_at.to_date > @end_date) }
 
   good_enrollments.uniq!
 
