8a05c4348aa19b209ef5f35a7f8de279d7a58761
Prem Shaganti <prem.shaganti@dc.gov>
2017-12-29 15:50:03 -0500

    ref:18069 address notice global change

--- app/notices/general_agency_notice.rb
+++ app/notices/general_agency_notice.rb
@@ -38,7 +38,7 @@ class GeneralAgencyNotice < Notice
   end
 
   def attach_envelope
-    join_pdfs [notice_path, Rails.root.join('lib/pdf_templates', 'envelope_without_address.pdf')]
+    join_pdfs [notice_path, Rails.root.join('lib/pdf_templates', 'taglines.pdf')]
   end
 
   def non_discrimination_attachment
