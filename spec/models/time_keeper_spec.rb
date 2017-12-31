34481668b9ea78c00398b42da426ebc3355f5a62
harsha chalamala <chvreddy1015@gmail.com>
2017-12-31 04:25:28 -0500

    spec fix creating hbx profile benefit sponsorship for current and next year

--- spec/models/time_keeper_spec.rb
+++ spec/models/time_keeper_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe TimeKeeper, type: :model do
     end
 
     context "and new date is one day later than current date_of_record" do
-      let!(:hbx_profile) { FactoryGirl.create(:hbx_profile) }
+      let!(:hbx_profile) { FactoryGirl.create(:hbx_profile, :open_enrollment_coverage_period) }
       it "should advance the date" do
         expect(TimeKeeper.set_date_of_record(next_day)).to eq next_day
       end
