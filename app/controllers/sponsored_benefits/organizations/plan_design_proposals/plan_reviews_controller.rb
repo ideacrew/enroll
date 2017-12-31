0a43cce5637104daa663da1fc6d6f042b3ce66b3
Brian Weiner <brian.weiner@dc.gov>
2017-12-31 12:13:58 -0500

    wip - saving and basic review quote framework

--- app/controllers/sponsored_benefits/organizations/plan_design_proposals/plan_reviews_controller.rb
+++ app/controllers/sponsored_benefits/organizations/plan_design_proposals/plan_reviews_controller.rb
@@ -1,24 +1,8 @@
 module SponsoredBenefits
   module Organizations
-    class PlanDesignProposals::BenefitGroupsController < ApplicationController
+    class PlanDesignProposals::PlanReviewsController < ApplicationController
 
-      def create
-        update_benefit_group_attributes if benefit_group.persisted?
-
-        benefit_group.title = "Benefit Group Created for: #{plan_design_organization.legal_name} by #{plan_design_organization.broker_agency_profile.legal_name}"
-        benefit_group.elected_plans = benefit_group.elected_plans_by_option_kind
-        if benefit_group.sole_source?
-          benefit_group.build_relationship_benefits
-        else
-          benefit_group.build_composite_tier_contributions
-        end
-
-        if plan_design_proposal.save
-          render json: { url: new_organizations_plan_design_proposal_plan_review_path(plan_design_proposal) }
-        else
-          puts 'did not save'
-          flash[:error] = "Something went wrong"
-        end
+      def new
       end
 
       private
@@ -42,12 +26,6 @@ module SponsoredBenefits
 
         def plan_design_form
           SponsoredBenefits::Forms::PlanDesignProposal.new(organization: plan_design_organization, proposal_id: params[:plan_design_proposal_id])
-        end
-
-        def update_benefit_group_attributes
-          benefit_group.composite_tier_contributions.destroy_all
-          benefit_group.relationship_benefits.destroy_all
-          benefit_group.update_attributes(benefit_group_params)
         end
 
         def benefit_group_params
