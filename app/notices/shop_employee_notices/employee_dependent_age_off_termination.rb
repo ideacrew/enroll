14e2b17d2b8f5b55cf2a1144d47b7799b8de6aeb
Battula <vrbattula@outlook.com>
2018-01-01 13:06:47 -0500

    refs #15971 Remove deprecated code.

--- app/notices/shop_employee_notices/employee_dependent_age_off_termination.rb
+++ app/notices/shop_employee_notices/employee_dependent_age_off_termination.rb
@@ -19,24 +19,15 @@ class ShopEmployeeNotices::EmployeeDependentAgeOffTermination < ShopEmployeeNoti
 
   def append_data
     current_date = TimeKeeper.date_of_record
-    ben_sponsorship = HbxProfile.current_hbx.benefit_sponsorship
-    current_open_enrollment_coverage = ben_sponsorship.benefit_coverage_periods.where(open_enrollment_end_on: Settings.aca.individual_market.open_enrollment.end_on ).first
-    bc_period = current_open_enrollment_coverage.open_enrollment_end_on >= current_date ? current_open_enrollment_coverage : ben_sponsorship.renewal_benefit_coverage_period
-    is_congress = census_employee.employee_role.benefit_group.is_congress
+    benefit_group = census_employee.employee_role.benefit_group
     names = []
-    if bc_period.present?
-      self.dep_hbx_ids.each do |dep_id|
-        names << Person.where(hbx_id: dep_id).first.full_name
-        notice.enrollment = PdfTemplates::Enrollment.new({
-          :dependents => names,
-          :terminated_on => current_date.end_of_month,
-          :is_congress => is_congress,
-          :plan_year => bc_period.start_on.year,
-          :effective_on => bc_period.start_on,
-          :ivl_open_enrollment_start_on => bc_period.open_enrollment_start_on,
-          :ivl_open_enrollment_end_on => bc_period.open_enrollment_end_on
-          })
-      end
+    self.dep_hbx_ids.each do |dep_id|
+      names << Person.where(hbx_id: dep_id).first.full_name
     end
+    notice.enrollment = PdfTemplates::Enrollment.new({
+      :dependents => names,
+      :terminated_on => current_date.end_of_month,
+      :is_congress => benefit_group.is_congress
+      })
   end
 end
