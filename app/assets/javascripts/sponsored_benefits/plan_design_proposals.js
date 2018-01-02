6690355aec453f66f8f2ba7e327d01615d6a3cce
202chaz <chaz.haskins@gmail.com>
2018-01-02 13:32:49 -0500

    Comparison disables on Carrier and Plan changes

--- app/assets/javascripts/sponsored_benefits/plan_design_proposals.js
+++ app/assets/javascripts/sponsored_benefits/plan_design_proposals.js
@@ -69,6 +69,7 @@ function fetchCarriers() {
   displayActiveCarriers();
   hidePlanContainer();
   toggleSliders(selected_carrier_level);
+  clearComparisons();
 }
 
 function displayActiveCarriers() {
