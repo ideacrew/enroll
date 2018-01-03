6562164cde16d60d8da0af73a994bb707d89a03f
Brian Weiner <brian.weiner@dc.gov>
2018-01-02 21:25:36 -0500

    plan details export working with basic styles, application controller fix hopefully

--- components/sponsored_benefits/app/models/sponsored_benefits/benefit_sponsorships/benefit_sponsorship.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/benefit_sponsorships/benefit_sponsorship.rb
@@ -69,6 +69,12 @@ module SponsoredBenefits
         write_attribute(:initial_enrollment_period, initial_enrollment_range) if initial_enrollment_range.present?
       end
 
+      def initial_enrollment_period_to_s
+        date_start = initial_enrollment_period.begin
+        date_end = initial_enrollment_period.end
+        "#{date_start.day} #{date_start.strftime('%B %Y')} - #{date_end.day} #{date_end.strftime('%B %Y')}"
+      end
+
       def census_employees
         SponsoredBenefits::CensusMembers::PlanDesignCensusEmployee.find_by_benefit_sponsor(self)
       end
