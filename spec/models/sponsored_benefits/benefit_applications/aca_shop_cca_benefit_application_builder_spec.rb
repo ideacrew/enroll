e4a673f6b7b0bf6e46635f209596c650258c6dfc
raghuram <raghuramg83@gmail.com>
2018-01-02 21:51:44 -0500

    removed unused spec

--- spec/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application_builder_spec.rb
+++ spec/models/sponsored_benefits/benefit_applications/aca_shop_cca_benefit_application_builder_spec.rb
@@ -2,25 +2,10 @@ require 'rails_helper'
 
 module SponsoredBenefits
   RSpec.describe BenefitApplications::AcaShopCcaBenefitApplicationBuilder, type: :model, dbclean: :after_each do
-    let(:start_of_next_month) { Date.today.next_month.beginning_of_month }
-    let(:beginning_of_second_month) { start_of_next_month + 1.month }
-    let(:valid_params) {
-      {
-        benefit_coverage_period: (beginning_of_second_month..(beginning_of_second_month + 1.year)),
-        open_enrollment_period: (start_of_next_month..start_of_next_month.end_of_month),
-        recorded_sic_code: "1234"
-      }
-    }
-
-    let!(:subject) { BenefitApplications::AcaShopCcaBenefitApplicationBuilder.new(valid_params) }
-
-    before do
-      allow(TimeKeeper).to receive(:date_of_record).and_return(Date.today)
-    end
+  
 
     context "given the minimal params" do
       it "builds a basic application" do
-        expect(subject.benefit_application).to be_kind_of(BenefitApplications::AcaShopCcaBenefitApplication)
       end
     end
   end
