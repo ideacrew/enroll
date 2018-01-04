dfa8f22cd08e5e903c50d28c7c3a729be771bbd6
saipraveen18 <saipraveen.gudimetla@gmail.com>
2018-01-04 18:51:31 -0500

    refs 20897 changed effective on date included seps

--- script/enrollment_affected_people.rb
+++ script/enrollment_affected_people.rb
@@ -1,9 +1,6 @@
 require 'csv'
 include VerificationHelper
 
-@logger = Logger.new("#{Rails.root}/log/enrollment_notice_data_set.log")
-@logger.info "Script Start #{TimeKeeper.datetime_of_record}"
-
 @end_date = Date.new(2017, 12, 29)
 @start_date = Date.new(2017, 12, 20)
 
@@ -18,7 +15,7 @@ families = Family.where({
                                     "kind" => "individual",
                                     "submitted_at" => {:"$gte" => @start_date, :"$lte" => @end_date},
                                     "aasm_state" => { "$in" =>  HbxEnrollment::ENROLLED_STATUSES },
-                                    "effective_on" => Date.new(2018, 1, 1)
+                                    "effective_on" => { :"$gte" => Date.new(2017,12,20)}
                                 }
                             }
                         })
@@ -31,10 +28,7 @@ limit_count = 500
 processed_count = 0
 
 def valid_person?(person)
-  verification_created_at = DateTime.new(2017,12,20)
-  verification_ended_at = DateTime.new(2017,12,30)
-  person.consumer_role.special_verifications.where(verification_type: 'Citizenship', :created_at => {:"$gte" => verification_created_at, :"$lte" => verification_ended_at}).present?
-
+  person.consumer_role.special_verifications.where(verification_type: 'Citizenship').present?
 end
 
 CSV.open(report_name, "w", force_quotes: true) do |csv|
@@ -61,11 +55,9 @@ CSV.open(report_name, "w", force_quotes: true) do |csv|
           end
         end
       rescue Exception => e
-        @logger.error "Unable to generate report #{e.backtrace}" unless Rails.env.test?
+        puts "Exception #{e.backtrace}" unless Rails.env.test?
       end
     end
-    @logger.info " #{offset_count} number of families processed at this point." unless Rails.env.test?
     offset_count = offset_count + limit_count
   end
-  @logger.info "End of the script. #{processed_count} number of families processed." unless Rails.env.test?
 end
