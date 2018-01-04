e7c53bab7d3254ad7e5a911b19457c66827a4e7f
saikumar9 <saikumarkotagiri9@gmail.com>
2018-01-04 18:00:26 -0500

    Resf #20015 refeactor to send notices one per Household

--- script/final_catastrophic_notice.rb
+++ script/final_catastrophic_notice.rb
@@ -12,17 +12,19 @@ rescue Exception => e
   puts "Unable to open file #{e}"
 end
 
+@data_hash = {}
+CSV.foreach(file_1,:headers =>true).each do |d|
+  if @data_hash[d["Subscriber_HBX_ID"]].present?
+    @data_hash[d["Subscriber_HBX_ID"]] << d
+  else
+    @data_hash[d["Subscriber_HBX_ID"]] = [d]
+  end
+end
+
 field_names = %w(
         primary_hbx_id
         primary_first_name
         primary_last_name
-        consumer
-        employee
-        enrollment_kind
-        ea_enrollment_metal_level
-        ea_enrollment_market_kind
-        ea_enrollment_state
-        ea_enrollment_effective_on
       )
 
 file_2 = "#{Rails.root}/public/ivl_catastrophic_notice_report.csv"
@@ -32,39 +34,48 @@ notice_trigger = event_kind.notice_triggers.first
 
 CSV.open(file_2, "w", force_quotes: true) do |csv|
   csv << field_names
-  csv_ea.each do |row|
-    next if row["State"] == "canceled"
-    hbx_enrollment = HbxEnrollment.by_hbx_id(row["Enrollment_Group_ID"]).first
-    person = Person.where(:hbx_id => row["Subscriber_HBX_ID"]).first
-    consumer_role = person.consumer_role if person.present?
-    if person.present? && consumer_role.present? && hbx_enrollment.present? && hbx_enrollment.plan_id.present?
-      begin
-        builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
-                      template: notice_trigger.notice_template,
-                      subject: event_kind.title,
-                      event_name: 'final_catastrophic_plan',
-                      mpi_indicator: notice_trigger.mpi_indicator
-                      }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)
-                      )
-        builder.deliver
+  @data_hash.each do |person_hbx_id , enrollments|
+    #To verify the aasm state
+    enrollment_aasm_states = []
+    enrollments.each do |enrollment|
+      enrollment_aasm_states << enrollment["State"]
+    end
+    enrollment_aasm_states.uniq!
 
-        csv << [
-          person.hbx_id,
-          person.first_name,
-          person.last_name,
-          person.has_active_consumer_role?,
-          person.has_active_employee_role?,
-          hbx_enrollment.kind,
-          hbx_enrollment.plan.metal_level,
-          hbx_enrollment.aasm_state,
-          hbx_enrollment.effective_on.to_s
-        ]
-        puts "CAT Notice is sent to primary person with hbx_id: #{person.hbx_id} and enrollment_hbx_id: #{hbx_enrollment.hbx_id}"
-      rescue Exception => e
-        puts "Unable to deliver to #{person.hbx_id} for the following error #{e.backtrace}"
+    #To verify the market kind
+    enrollment_market_kinds = []
+    enrollments.each do |enrollment|
+      ea_enrollment = HbxEnrollment.by_hbx_id(enrollment["Enrollment_Group_ID"]).first
+      enrollment_market_kinds << !ea_enrollment.is_shop? if ea_enrollment.present?
+    end
+
+    #Reject if aasm_state is CANCELED and also reject if market kind is SHOP
+    if (enrollment_aasm_states.count > 1 || ( enrollment_aasm_states.count == 1 && enrollment_aasm_states.first != "canceled")) && !(enrollment_market_kinds.include? false)
+      person = Person.where(:hbx_id => person_hbx_id).first
+      consumer_role = person.consumer_role if person.present?
+      if person.present? && consumer_role.present?
+        begin
+          builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
+                        template: notice_trigger.notice_template,
+                        subject: event_kind.title,
+                        event_name: 'final_catastrophic_plan',
+                        mpi_indicator: notice_trigger.mpi_indicator
+                        }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)
+                        )
+          builder.deliver
+
+          csv << [
+            person.hbx_id,
+            person.first_name,
+            person.last_name
+          ]
+          puts "CAT Notice is sent to primary person with hbx_id: #{person.hbx_id}" unless Rails.env.test?
+        rescue Exception => e
+          puts "Unable to deliver to #{person.hbx_id} for the following error #{e.backtrace}" unless Rails.env.test?
+        end
+      else
+        puts "No Person or No consumer role exists for hbx_id: #{person_hbx_id}" unless Rails.env.test?
       end
-    else
-      puts "No Person or No consumer role or No enrollment exists for hbx_id: #{row["Subscriber_HBX_ID"]} and enrollment_hbx_id: #{row["Enrollment_Group_ID"]}."
     end
   end
 end
