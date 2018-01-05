2640f1e3ff2e955afb20edb7c9f397c153d624c6
saikumar9 <saikumarkotagiri9@gmail.com>
2018-01-05 10:36:06 -0500

    Refs #20015 Change in font size, font style an ddisplay of HBX ID

--- app/notices/ivl_notices/final_catastrophic_plan_notice.rb
+++ app/notices/ivl_notices/final_catastrophic_plan_notice.rb
@@ -7,7 +7,7 @@ class IvlNotices::FinalCatastrophicPlanNotice < IvlNotice
     args[:market_kind] = 'individual'
     args[:recipient_document_store]= consumer_role.person
     args[:to] = consumer_role.person.work_email_or_best
-    self.header = "notices/shared/header_with_page_numbers.html.erb"
+    self.header = "notices/shared/header_ivl.html.erb"
     super(args)
   end
 
