9321b7c52faec12304b1df32fefa55ac6fafae72
saip7795 <saip7795@gmail.com>
2018-01-08 11:22:13 -0500

    Ref20218: Minor Change

--- app/data_migrations/add_benefit_group_id.rb
+++ app/data_migrations/add_benefit_group_id.rb
@@ -7,7 +7,7 @@ class AddBenefitGroupId < MongoidMigrationTask
       if hbx_enrollments.present?
         employer_sponsored_hbx_enrollment = hbx_enrollments.where(kind:'employer_sponsored').first
         hbx_enroll_first = hbx_enrollments.first
-        if hbx_enroll_first.benefit_group_id.nil? and employer_sponsored_hbx_enrollment.present? and employer_sponsored_hbx_enrollment.benefit_group_id.present?
+        if hbx_enroll_first.benefit_group.nil? and employer_sponsored_hbx_enrollment.present? and employer_sponsored_hbx_enrollment.benefit_group_id.present?
           hbx_enroll_first.benefit_group_id = employer_sponsored_hbx_enrollment.benefit_group_id
           if hbx_enroll_first.valid?
             hbx_enroll_first.save!
