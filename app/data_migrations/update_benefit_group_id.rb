6d7d4c179a5be29a69ddaab0d32abae17c851c9a
saip7795 <saip7795@gmail.com>
2018-01-09 17:23:34 -0500

    Ref20218 : Updating the rake task to take the Benefit Group Id as a parameter

--- app/data_migrations/update_benefit_group_id.rb
+++ app/data_migrations/update_benefit_group_id.rb
@@ -1,14 +1,14 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
-class AddBenefitGroupId < MongoidMigrationTask
+class UpdateBenefitGroupId < MongoidMigrationTask
   def migrate
     begin
       hbx_id = ENV['hbx_id']
+      benefit_group_id = ENV['benefit_group_id'].to_s
       hbx_enrollments = Person.where(:hbx_id => hbx_id).first.try(:primary_family).try(:active_household).try(:hbx_enrollments)
       if hbx_enrollments.present?
-        employer_sponsored_hbx_enrollment = hbx_enrollments.where(kind:'employer_sponsored').last
-        hbx_enroll_first = hbx_enrollments.first
-        if hbx_enroll_first.benefit_group.nil? and employer_sponsored_hbx_enrollment.present? and employer_sponsored_hbx_enrollment.benefit_group_id.present?
-          hbx_enroll_first.benefit_group_id = employer_sponsored_hbx_enrollment.benefit_group_id
+         hbx_enroll_first = hbx_enrollments.first
+        if hbx_enroll_first.benefit_group.nil?
+          hbx_enroll_first.benefit_group_id = benefit_group_id
           if hbx_enroll_first.valid?
             hbx_enroll_first.save!
             hbx_enroll_first.hbx_id
--- lib/tasks/migrations/add_benefit_group_id.rake
+++
@@ -1,8 +0,0 @@
-# Rake task to interchange
-# To run rake task: RAILS_ENV=production bundle exec rake migrations:add_benefit_group_id hbx_id="123456789"
-
-require File.join(Rails.root, "app", "data_migrations", "add_benefit_group_id")
-namespace :migrations do
-  desc "add_benefit_group_id"
-  AddBenefitGroupId.define_task :add_benefit_group_id => :environment
-end
