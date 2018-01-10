e172c11f7c703f41afd54c14f6799e7e17287d7d
Kevin Wei <kevin.tw.wei@gmail.com>
2018-01-10 18:24:03 -0500

    fix cucumbers

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -293,11 +293,11 @@ Given(/^(.*)Employer for (.*) exists with a published health plan year$/) do |ki
   plan_year = FactoryGirl.create :plan_year, employer_profile: employer_profile, fte_count: 2, aasm_state: :published
   benefit_group = FactoryGirl.create :benefit_group, plan_year: plan_year
   carrier_profile = benefit_group.reference_plan.carrier_profile
-  sic_factors = SicCodeRatingFactorSet.new(active_year: 2018, default_factor_value: 1.0, carrier_profile: carrier_profile).tap do |factor_set|
+  sic_factors = SicCodeRatingFactorSet.new(active_year: TimeKeeper.date_of_record.year, default_factor_value: 1.0, carrier_profile: carrier_profile).tap do |factor_set|
     factor_set.rating_factor_entries.new(factor_key: employer_profile.sic_code, factor_value: 1.0)
   end
   sic_factors.save!
-  group_size_factors = EmployerGroupSizeRatingFactorSet.new(active_year: 2018, default_factor_value: 1.0, max_integer_factor_key: 5, carrier_profile: carrier_profile).tap do |factor_set|
+  group_size_factors = EmployerGroupSizeRatingFactorSet.new(active_year: TimeKeeper.date_of_record.year, default_factor_value: 1.0, max_integer_factor_key: 5, carrier_profile: carrier_profile).tap do |factor_set|
     [0..5].each do |size|
       factor_set.rating_factor_entries.new(factor_key: size, factor_value: 1.0)
     end
@@ -323,11 +323,11 @@ Given(/^Employer for (.*) exists with a published plan year offering health and 
   plan_year = FactoryGirl.create :plan_year, employer_profile: employer_profile, fte_count: 2, aasm_state: :published
   benefit_group = FactoryGirl.create :benefit_group, :with_valid_dental, plan_year: plan_year
   carrier_profile = benefit_group.dental_reference_plan.carrier_profile #THIS FIXES empty SicCodeRatingFactorSet from plan shopping show controller but... WHY???
-  sic_factors = SicCodeRatingFactorSet.new(active_year: 2017, default_factor_value: 1.0, carrier_profile: carrier_profile).tap do |factor_set|
+  sic_factors = SicCodeRatingFactorSet.new(active_year: TimeKeeper.date_of_record.year, default_factor_value: 1.0, carrier_profile: carrier_profile).tap do |factor_set|
     factor_set.rating_factor_entries.new(factor_key: employer_profile.sic_code, factor_value: 1.0)
   end
   sic_factors.save!
-  group_size_factors = EmployerGroupSizeRatingFactorSet.new(active_year: 2017, default_factor_value: 1.0, max_integer_factor_key: 5, carrier_profile: carrier_profile).tap do |factor_set|
+  group_size_factors = EmployerGroupSizeRatingFactorSet.new(active_year: TimeKeeper.date_of_record.year, default_factor_value: 1.0, max_integer_factor_key: 5, carrier_profile: carrier_profile).tap do |factor_set|
     [0..5].each do |size|
       factor_set.rating_factor_entries.new(factor_key: size, factor_value: 1.0)
     end
