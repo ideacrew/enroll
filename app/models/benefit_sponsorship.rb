f9da672c5e50b0af968dcd5cd545dc660a3467ed
Brian Weiner <brian.weiner@dc.gov>
2018-01-10 11:42:17 -0500

    validate presence of service area

--- app/models/benefit_sponsorship.rb
+++ app/models/benefit_sponsorship.rb
@@ -11,13 +11,13 @@ class BenefitSponsorship
   SERVICE_MARKET_KINDS = %w(shop individual coverall)
 
   field :service_markets, type: Array, default: []
+  validates_presence_of :service_markets
 
   # 2015, 2016, etc. (aka plan_year)
   embeds_many :benefit_coverage_periods
   embeds_many :geographic_rating_areas
 
   accepts_nested_attributes_for :benefit_coverage_periods, :geographic_rating_areas
-
   # Query Census member collection
   def census_employees
     @census_employees = CensusMembers::PlanDesignCensusEmployee.by_benefit_sponsorship(self)
