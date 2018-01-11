e39021375963d1db38140b98207cd5bb88481ecf
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-01-10 23:17:55 -0500

    Refs #21038 completed rake task and associated specs

--- app/data_migrations/remove_resident_role.rb
+++ app/data_migrations/remove_resident_role.rb
@@ -2,24 +2,48 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class RemoveResidentRole < MongoidMigrationTask
 
-  # this script will remove the resident role from any person who does not have
-  # any enrollments designated as coverall but has an erroneous resident role
+  # this script will remove the resident role from any person who is not in the
+  # explicit list of people who should have received the resident role and have a
+  # corresponding coverall enrollment. For people not in this group this rake task
+  # will remove the resident role and fix any enrollments associated with that person
+  # designated as coverall.
+
   def migrate
+    correct_assignments = ['58b71497f1244e4a42000095', '572a6491f1244e025a00007f',
+      '58b9d9da082e7653ea000106', '58e3dc7d50526c33c5000187']
     people = Person.where("resident_role" => {"$exists" => true, "$ne" => nil})
     people.each do |person|
-      # initialize flag to be true at the beginning of each iteration
-      remove_resident_role = true
-      person.primary_family && person.primary_family.active_household.hbx_enrollments.each do |enrollment|
-        # set flag to false
-        if enrollment.kind == "coverall"
-          remove_resident_role = false
+      # exclude the valid coverall enrollments
+      unless correct_assignments.include?(person.id.to_s)
+        person.primary_family && person.primary_family.active_household.hbx_enrollments.each do |enrollment|
+          # indicates enrollment needs to be fixed as well
+          if enrollment.kind == "coverall"
+            enrollment.kind = "individual"
+            enrollment.save
+            if person.consumer_role.nil?
+              clone_resident_role(person.resident_role)
+            end
+          end
+          person.resident_role.destroy
+          puts "removed resident role for Person: #{person.hbx_id}" unless Rails.env.test?
         end
       end
-      # remove resident role if none of the enrollments are designated as coverall
-      if remove_resident_role == true
-        person.resident_role.destroy
-        puts "removed resident role for Person: #{person.hbx_id} unless Rails.env.test?"
-      end
     end
+  end
+
+  def clone_resident_role(r_role)
+    c_role = ConsumerRole.new
+    c_role.is_applicant = r_role.is_applicant
+    c_role.is_active = r_role.is_active
+    c_role.bookmark_url = r_role.bookmark_url
+    c_role.is_state_resident = r_role.is_state_resident
+    c_role.residency_determined_at = r_role.residency_determined_at
+    c_role.contact_method = r_role.contact_method
+    c_role.language_preference = r_role.language_preference
+    c_role.lawful_presence_determination = r_role.lawful_presence_determination
+    c_role.documents = r_role.paper_applications
+    c_role.person = r_role.person
+    c_role.save!
+
   end
 end
--- spec/data_migrations/remove_resident_role_spec.rb
+++ spec/data_migrations/remove_resident_role_spec.rb
@@ -13,15 +13,22 @@ describe RemoveResidentRole do
   end
 
   describe "remove resident role for person with no enrollments" do
-    let!(:person1) { FactoryGirl.create(:person, :with_resident_role)}
-    let!(:person2) { FactoryGirl.create(:person, :with_resident_role, id:'58e3dc7d50526c33c5000187')}
+    let!(:person1) { FactoryGirl.create(:person, :with_resident_role, id:'58e3dc7d50526c33c5000187')}
+    let!(:person2) { FactoryGirl.create(:person, :with_consumer_role)}
 
-    it "should delete the resident role for person1 and not for person2" do
+    it "should delete the resident role for person2 and not for person1" do
+      primary_family = FactoryGirl.create(:family, :with_primary_family_member, person: person2)
+      ivl_enrollment =  FactoryGirl.create(:hbx_enrollment, :individual_unassisted, household: primary_family.active_household, kind: 'coverall')
+      r_role = FactoryGirl.build(:resident_role)
+      r_role.person = person2
+      r_role.save!
       subject.migrate
       person1.reload
       person2.reload
-      expect(person1.resident_role).to be(nil)
+      expect(person1.resident_role).not_to be_nil
       expect(person2.resident_role).to be(nil)
+      expect(person2.primary_family.active_household.hbx_enrollments.first.kind).to eq("individual")
+      expect(person2.consumer_role).not_to be_nil
     end
   end
 end
