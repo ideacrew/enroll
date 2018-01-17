a46cd8c0a3fab2b55b5a5adfe001f49e9ac91d70
saimekala07 <saimekala07@gmail.com>
2018-01-17 15:44:10 -0500

    refs# 20657 updating rake to approve employer attestation for conversion employer

--- app/data_migrations/update_conversion_flag.rb
+++ app/data_migrations/update_conversion_flag.rb
@@ -10,8 +10,16 @@ class UpdateConversionFlag < MongoidMigrationTask
         puts "Issues with organization of fein #{fein}" unless Rails.env.test?
         next
       end
-        organization.first.employer_profile.update_attributes!(profile_source: ENV['profile_source'])
-        puts "Conversion flag updated #{ENV['profile_source']} for #{fein}" unless Rails.env.test?
+      organization.first.employer_profile.update_attributes!(profile_source: ENV['profile_source'])
+      puts "Conversion flag updated #{ENV['profile_source']} for #{fein}" unless Rails.env.test?
+      return unless ENV['profile_source'] == "conversion"
+      emp = organization.first.employer_profile
+      attestation = emp.employer_attestation.blank?  ? emp.build_employer_attestation : emp.employer_attestation
+      attestation.submit! if attestation.may_submit?
+      attestation.approve! if attestation.may_approve?
+      attestation.updated_by_id = "test"
+      attestation.save
+      puts "Employer Attestation approved for #{fein}" unless Rails.env.test?
       end
     rescue
       puts "Bad Employer Record" unless Rails.env.test?
