e6f89078d2622d397dd23471183cce38af7ec16f
saikumar9 <saikumarkotagiri9@gmail.com>
2018-01-18 17:49:19 -0500

    refs #12504 typo

--- app/reports/hbx_reports/update_aptc_amount.rb
+++ app/reports/hbx_reports/update_aptc_amount.rb
@@ -14,7 +14,7 @@ class UpdateAptcAmount < MongoidMigrationTask
           ehb_premium = (en.plan.present? && en.plan.ehb > 0) ? (en.total_premium * en.plan.ehb).round(2) : en.total_premium
 
           if ehb_premium < en.applied_aptc_amount.to_f
-            en.update_attributes!(:applied_aptc_amount, ehb_premium)
+            en.update_attributes!(applied_aptc_amount: ehb_premium)
             puts "Primary Person's hbx_id: #{person.hbx_id}.The applied_aptc_amount: #{en.applied_aptc_amount}, ehb_premium: #{ehb_premium} for enr_hbx_id: #{en.hbx_id}" unless Rails.env.test?
             count += 1
           end
