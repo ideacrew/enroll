0e4788db42ebd38b30170f38004fa9edd7cff672
Raghuram <raghuramg83@gmail.com>
2018-02-05 14:18:47 -0500

    Refs #21713 Added initialization of DC employer profile and fixed datatable error

--- app/models/sponsored_benefits/organizations/aca_shop_dc_employer_profile.rb
+++ app/models/sponsored_benefits/organizations/aca_shop_dc_employer_profile.rb
@@ -6,7 +6,15 @@ module SponsoredBenefits
       field :contact_method, type: String, default: "Only Electronic communications"
 
       embeds_one :general_agency_profile, cascade_callbacks: true, validate: true
+      embedded_in :plan_design_proposal, class_name: "SponsoredBenefits::Organizations::PlanDesignProposal"
 
+      after_initialize :initialize_benefit_sponsorship
+
+      private
+
+      def initialize_benefit_sponsorship
+        benefit_sponsorships.build(benefit_market: :aca_shop_dc, enrollment_frequency: :rolling_month) if benefit_sponsorships.blank?
+      end
     end
   end
 end
