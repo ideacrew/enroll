d3b940df5714f9441ee0b9acd5b71eda5723aef7
Dan Thomas <dan.thomas@dc.gov>
2018-02-05 17:07:11 -0500

    WIP on model redesign

---
+++ app/models/sponsored_benefits/benefit_markets/effective_date.rb
@@ -0,0 +1,20 @@
+module SponsoredBenefits
+  module BenefitMarkets
+    class EffectiveDate
+
+      def initialize(date, benefit_market)
+      end
+
+      def benefit_service_periods
+      end
+
+      def issuer_profiles
+      end
+
+      def benefit_products
+      end
+
+
+    end
+  end
+end
---
+++ spec/factories/sponsored_benefits_benefit_markets_effective_dates.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :sponsored_benefits_benefit_markets_effective_date, class: 'SponsoredBenefits::BenefitMarkets::EffectiveDate' do
+    
+  end
+end
