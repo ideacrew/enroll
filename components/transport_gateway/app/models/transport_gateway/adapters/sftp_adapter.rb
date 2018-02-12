816ab1e4cba0f14a0e1295d86f9d2a2c42b8dab8
Trey Evans <lewis.r.evans@gmail.com>
2018-02-12 09:54:19 -0500

    Refs #18811.  Correct URI encoding.

--- components/transport_gateway/app/models/transport_gateway/adapters/sftp_adapter.rb
+++ components/transport_gateway/app/models/transport_gateway/adapters/sftp_adapter.rb
@@ -28,7 +28,7 @@ module TransportGateway
         Net::SFTP.start(target_uri.host, @user, default_options.merge(credential_options)) do |sftp|
           sftp.dir.foreach(target_uri.path) do |entry|
             if entry.file?
-              full_uri = URI.join(target_uri, URI::Escape.encode(entry.name))
+              full_uri = URI.join(target_uri, URI.encode(entry.name))
               result_list << TransportGateway::ResourceEntry.new(entry.name, full_uri, entry.attributes.size, entry.attributes.mtime)
             end
           end
@@ -59,7 +59,7 @@ module TransportGateway
 
       begin
         Net::SFTP.start(target_uri.host, @user, default_options.merge(credential_options)) do |sftp|
-          sftp.download!(target_uri.path, source_stream)
+          sftp.download!(URI.decode(target_uri.path), source_stream)
         end
         Sources::TempfileSource.new(source_stream)
       rescue Exception => e
@@ -92,9 +92,9 @@ module TransportGateway
 
       begin
         Net::SFTP.start(target_uri.host, @user, default_options.merge(@credential_options)) do |sftp|
-          find_or_create_target_folder_for(sftp, target_uri.path)
+          find_or_create_target_folder_for(sftp, URI.decode(target_uri.path))
 
-          sftp.upload!(source.stream, target_uri.path)
+          sftp.upload!(source.stream, URI.decode(target_uri.path))
         end
       rescue Exception => e
         log(:error, "transport_gateway.sftp_adapter") { e }
