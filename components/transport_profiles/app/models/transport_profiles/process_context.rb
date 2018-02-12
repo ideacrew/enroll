fb355a25037e2a599134c6538d67971d450f28b5
Trey Evans <lewis.r.evans@gmail.com>
2018-02-12 15:10:23 -0500

    Refs #22221.  Add transfer callback.

--- components/transport_profiles/app/models/transport_profiles/process_context.rb
+++ components/transport_profiles/app/models/transport_profiles/process_context.rb
@@ -20,5 +20,18 @@ module TransportProfiles
     def get(key)
       @context_values.fetch(key.to_sym)
     end
+
+    # Transform a given value in the process context.
+    # @param key [String, Symbol] the key of the managed value
+    # @param initial_value [Object] the value to use if the key has not yet been set
+    # @yieldparam context_value [Object] the current key value, or initial_value if unset
+    # @yieldreturn [Object] the new value to place in the context
+    def update(key, initial_value = nil)
+      if @context_values.has_key?(key.to_sym)
+        @context_values[key.to_sym] = yield @context_values[key.to_sym]
+      else
+        @context_values[key.to_sym] = yield initial_value
+      end
+    end
   end
 end
