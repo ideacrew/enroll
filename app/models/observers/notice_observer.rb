7bc2dd8b1f6988e5b66dab699f2b9740b8d363c7
harshaellanki <h_vardhan@yahoo.com>
2018-02-16 13:18:00 -0500

    ref:21896 changed a conditional check

--- app/models/observers/notice_observer.rb
+++ app/models/observers/notice_observer.rb
@@ -62,7 +62,7 @@ module Observers
 
         if new_model_event.event_key == :notify_employee_of_initial_employer_ineligibility
           errors = plan_year.enrollment_errors
-          if(errors.include?(:eligible_to_enroll_count) || errors.include?(:non_business_owner_enrollment_count))
+          if(errors.include?(:enrollment_ratio) || errors.include?(:non_business_owner_enrollment_count))
             plan_year.employer_profile.census_employees.non_terminated.each do |ce|
               if ce.employee_role.present?
                 trigger_notice(recipient: ce.employee_role, event_object: plan_year, notice_event: "notify_employee_of_initial_employer_ineligibility")
