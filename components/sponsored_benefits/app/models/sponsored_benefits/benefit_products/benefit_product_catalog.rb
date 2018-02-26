af84081c0a3851baa5993d6128b980daba0f0234
Dan Thomas <dan.thomas@dc.gov>
2018-02-26 09:07:17 -0500

    Merge BenefitMarketServicePeriod with BenefitMarketCatalog

--- components/sponsored_benefits/app/models/sponsored_benefits/benefit_products/benefit_product_catalog.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/benefit_products/benefit_product_catalog.rb
@@ -1,8 +1,19 @@
 module SponsoredBenefits
   module BenefitProducts
     class BenefitProductCatalog
+      include Mongoid::Document
+      include Mongoid::Timestamps
 
-      attr_reader :effective_period, :benefit_market, 
+      # Time periods when sponsors may initially offer, and subsequently renew, benefits
+      #   :monthly - may start first of any month of the year and renews each year in same month
+      #   :annual  - may start only on benefit market's annual effective date month and renews each year in same month
+      #   :annual_with_midyear_initial - may start mid-year and renew at subsequent annual effective date month
+      APPLICATION_INTERVAL_KINDS = [:monthly, :annual, :annual_with_midyear_initial]
+
+      belongs_to  :benefit_market, class_name: "SponsoredBenefits::BenefitMarkets::BenefitMarket"
+
+      attr_reader :service_area, :benefit_products,
+                  :effective_period, :benefit_market, 
                   :open_enrollment_period, :open_enrollment_period_minimum, 
                   :binder_payment_due_on,
                   :probation_period_kinds,
@@ -10,16 +21,48 @@ module SponsoredBenefits
                   :sponsor_eligibility_policy
 
 
-      def initialize(effective_date, benefit_market)
-        @effective_date         = effective_date.to_date.beginning_of_month
-        @open_enrollment_period = open_enrollment_period_by_effective_date(effective_date)
+      # Frequency at which sponsors may submit an initial or renewal application
+      # Example application interval kinds:
+      #   DC Individual Market, Congress:
+      #     :application_interval_kind => :annual
+      #   MA GIC
+      #     :application_interval_kind => :annual_with_midyear_initial
+      #   DC/MA SHOP Market:
+      #     :application_interval_kind => :monthly
+      field :application_interval_kind,  type: Symbol
 
-        open_enrollment_period
+      # Effective date range during which associated benefits may be offered by sponsors
+      # Example application periods:
+      #   DC Individual Market Initial & Renewal, Congress:
+      #     :application_period => Date.new(2018,1,1)..Date.new(2018,12,31)
+      #   MA GIC
+      #     :application_period => Date.new(2018,7,1)..Date.new(2018,6,30)
+      #   DC/MA SHOP Market:
+      #     :application_period => Date.new(2018,1,1)..Date.new(2018,12,31)
+      field :application_period,          type: Range
 
+      # Length of time new members must wait before they're eligible to enroll
+      field :probation_period_kinds,      type: Array,  default: SponsoredBenefits::PROBATION_PERIOD_KINDS
 
-        prior_month           = effective_date - 1.month
-        binder_payment_due_on = Date.new(prior_month.year, prior_month.month, Settings.aca.shop_market.binder_payment_due_on)
-      end
+      field :title,                       type: String, default: ""
+      field :description,                 type: String, default: ""
+
+      embeds_one  :sponsor_eligibility_policy,          class_name: "SponsoredBenefits::BenefitMarkets::SponsorEligibilityPolicy"
+      embeds_one  :benefit_product_eligibility_policy,  class_name: "SponsoredBenefits::BenefitMarkets::BenefitProductEligibilityPolicy"
+      embeds_many :benefit_products,                    class_name: "SponsoredBenefits::BenefitProducts::BenefitProduct"
+
+      belongs_to :service_area,                         class_name: "SponsoredBenefits::Locations::ServiceArea"
+
+      validates_presence_of :application_interval_kind, :application_period, :probation_period_kinds
+
+      validates :application_interval_kind,
+        inclusion:    { in: APPLICATION_INTERVAL_KINDS, message: "%{value} is not a valid application interval kind" },
+        allow_nil:    false
+
+      # validates :probation_period_kinds,
+      #   inclusion:    { in: SponsoredBenefits::PROBATION_PERIOD_KINDS, message: "%{value} is not a valid probation period kind" },
+      #   allow_nil:    false
+
 
       def effective_period
         @effective_period  = effective_date..(effective_date + 1.year - 1.day)
--- components/sponsored_benefits/spec/factories/sponsored_benefits_benefit_markets_benefit_market_service_periods.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :sponsored_benefits_benefit_markets_benefit_market_service_period, class: 'SponsoredBenefits::BenefitMarkets::BenefitMarketServicePeriod' do
-    
-  end
-end
