64aa1d2957386e606572961cec1304372d4b96f7
Dan Thomas <dan.thomas@dc.gov>
2018-02-26 14:28:19 -0500

    initial commit

---
+++ app/models/sponsored_benefits/organizations/general_organization.rb
@@ -0,0 +1,8 @@
+# Organization type with strict data entry/validation policies used for S-Corp, C-Corp, LLC and similar where FEIN is assigned
+module SponsoredBenefits
+  module Organizations
+    class GeneralOrganization < Organization
+
+    end
+  end
+end
---
+++ spec/factories/sponsored_benefits_organizations_exempt_organizations.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :sponsored_benefits_organizations_exempt_organization, class: 'SponsoredBenefits::Organizations::ExemptOrganization' do
+    
+  end
+end
