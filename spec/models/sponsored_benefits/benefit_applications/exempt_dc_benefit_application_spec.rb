0b16471018272e83da6d94e4266227c71e5b55d2
Dan Thomas <dan.thomas@dc.gov>
2018-03-01 18:16:50 -0500

    initial commit

---
+++ spec/models/sponsored_benefits/benefit_applications/exempt_dc_benefit_application_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module SponsoredBenefits
+  RSpec.describe BenefitApplications::ExemptDcBenefitApplication, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
