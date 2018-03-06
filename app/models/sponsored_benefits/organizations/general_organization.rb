17d7b78f8e6dd54ce4a4b90356aac156626cf141
Dan Thomas <dan.thomas@dc.gov>
2018-03-06 07:44:53 -0500

    Site model passing basic params spec

--- app/models/sponsored_benefits/organizations/general_organization.rb
+++ app/models/sponsored_benefits/organizations/general_organization.rb
@@ -3,6 +3,18 @@ module SponsoredBenefits
   module Organizations
     class GeneralOrganization < Organization
 
+      # Federal Employer ID Number
+      field :fein, type: String
+
+      validates_presence_of :entity_kind, :legal_name
+
+      validates :fein,
+        presence: true,
+        length: { is: 9, message: "%{value} is not a valid FEIN" },
+        numericality: true,
+        uniqueness: true
+
+
     end
   end
 end
