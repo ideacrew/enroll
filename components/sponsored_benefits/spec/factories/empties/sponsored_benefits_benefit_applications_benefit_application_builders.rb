cb151c7b908d3d1ff310b2d1b41f7a50ffa63002
Raghuram <raghuramg83@gmail.com>
2018-03-14 18:59:18 -0400

    Updated factories: site, general organization and sponsorships

--- components/sponsored_benefits/spec/factories/empties/sponsored_benefits_benefit_applications_benefit_application_builders.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :sponsored_benefits_benefit_applications_benefit_application_builder, class: 'SponsoredBenefits::BenefitApplications::BenefitApplicationBuilder' do
-    
-  end
-end
