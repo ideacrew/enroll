7b188628d9142415b79a9d2b2af97665f5cf0701
Dan Thomas <dan.thomas@dc.gov>
2018-03-14 12:00:22 -0400

    clear unused models

--- components/sponsored_benefits/spec/factories/sponsored_benefits_benefit_sponsorships_benefit_package_assignments.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :sponsored_benefits_benefit_sponsorships_benefit_package_assignment, class: 'SponsoredBenefits::BenefitSponsorships::BenefitPackageAssignment' do
-    
-  end
-end
