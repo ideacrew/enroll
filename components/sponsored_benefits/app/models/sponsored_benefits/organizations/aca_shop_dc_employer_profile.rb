1547ff91a60a83055258e0d7054cce83785ba35c
Dan Thomas <dan.thomas@dc.gov>
2018-03-14 11:59:05 -0400

    Add initialize_profile method.  clear irrelevant content as result of BQT 1 refactor

--- components/sponsored_benefits/app/models/sponsored_benefits/organizations/aca_shop_dc_employer_profile.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/organizations/aca_shop_dc_employer_profile.rb
@@ -2,19 +2,15 @@ module SponsoredBenefits
   module Organizations
     class AcaShopDcEmployerProfile < Profile
 
-      embeds_one  :general_agency_profile, cascade_callbacks: true, validate: true
-
-      # after_initialize :initialize_benefit_sponsorship
 
       private
 
-      def initialize_benefit_sponsorship
-        return unless benefit_sponsorships.blank?
-        benefit_sponsorship = benefit_sponsorships.build(site_id: :dc, benefit_market: :aca_shop)
-        
-        ## TEMP figure out what we actually want here
-        # benefit_market = SponsoredBenefits::BenefitMarkets::BenefitMarket.find_by_benefit_sponsorship(:dc, :aca_shop)
-        # benefit_sponsorship.benefit_market = benefit_market.first
+      def initialize_profile
+        return unless benefit_sponsorship_eligible.blank?
+
+        write_attribute(:benefit_sponsorship_eligible, true)
+        @benefit_sponsorship_eligible = true
+        self
       end
     end
   end
