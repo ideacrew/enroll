1fdce569c6a7a0a183ba27c645bafc9410298c94
Trey Evans <lewis.r.evans@gmail.com>
2018-03-15 13:42:14 -0400

    Clean factory names.

--- spec/components/sponsored_benefits_spec.rb
+++ spec/components/sponsored_benefits_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 if ("SponsoredBenefits::Engine".constantize rescue nil)
+  Dir[Rails.root.join("components/sponsored_benefits/spec/factories/*.rb")].each do |f|
+    require f
+  end
   Dir[Rails.root.join("components/sponsored_benefits/spec/**/*_spec.rb")].each do |f|
     require f
   end
