702d7663c5ed206cfd7ea2b36a039074e28c3887
Pranab <pranab.khanal@gmail.com>
2018-03-15 06:59:47 -0400

    Refs#22722 Admin Interface to transition Family Members between Coverall  / Resident markets. WIP

--- app/models/effective/datatables/family_data_table.rb
+++ app/models/effective/datatables/family_data_table.rb
@@ -27,7 +27,9 @@ module Effective
             aptc_csr_link_type(row, pundit_allow(Family, :can_update_ssn?))],
            ['Collapse Form', hide_form_exchanges_hbx_profiles_path(family_id: row.id, person_id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"),'ajax'],
            ['Paper', resume_enrollment_exchanges_agents_path(person_id: row.primary_applicant.person.id, original_application_type: 'paper'), 'static'],
-           ['View Username and Email', get_user_info_exchanges_hbx_profiles_path(person_id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"), pundit_allow(Family, :can_view_username_and_email?) ? 'ajax' : 'disabled']
+           ['View Username and Email', get_user_info_exchanges_hbx_profiles_path(person_id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"), pundit_allow(Family, :can_view_username_and_email?) ? 'ajax' : 'disabled'],
+           ['Transition Family Members', transition_family_members_insured_families_path(family: row.id, family_actions_id: "family_actions_#{row.id.to_s}"), transition_family_members_link_type(row, pundit_allow(Family, :can_update_ssn?)) ]
+
           ]
           render partial: 'datatables/shared/dropdown', locals: {dropdowns: dropdown, row_actions_id: "family_actions_#{row.id.to_s}"}, formats: :html
         }, :filter => false, :sortable => false
@@ -68,6 +70,10 @@ module Effective
 
       def terminate_enrollment_type(family, allow)
         (family.all_enrollments.can_terminate.present? && allow) ? 'ajax' : 'disabled'
+      end
+
+      def transition_family_members_link_type(row, allow)
+        'ajax'
       end
 
       def nested_filter_definition
--- config/routes.rb
+++ config/routes.rb
@@ -169,6 +169,8 @@ Rails.application.routes.draw do
         get 'family'
         get 'upload_notice_form'
         post 'upload_notice'
+        get 'transition_family_members'
+        post 'transition_family_members_update'
       end
 
       resources :people do
