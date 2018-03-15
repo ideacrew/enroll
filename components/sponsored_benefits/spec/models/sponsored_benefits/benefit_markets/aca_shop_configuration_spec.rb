89b9733b6743080e3ce6f9e3e61d3713bc550839
Nikhil S <nikhilks219@gmail.com>
2018-03-15 10:09:53 -0400

    22980: added specs

--- components/sponsored_benefits/spec/models/sponsored_benefits/benefit_markets/aca_shop_configuration_spec.rb
+++ components/sponsored_benefits/spec/models/sponsored_benefits/benefit_markets/aca_shop_configuration_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
+# AcaShopApplicationConfiguration test cases
 module SponsoredBenefits
   RSpec.describe BenefitMarkets::AcaShopConfiguration, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:ee_ct_max) { 50 }
+    let(:binder_due_dom) { 23 }
+
+    context "ability to create, validate and persist instances of this class" do
+      context "with no arguments" do
+        subject { described_class.new }
+
+        it "should be valid" do
+          subject.validate
+          expect(subject).to be_valid
+        end
+      end
+    end
   end
 end
