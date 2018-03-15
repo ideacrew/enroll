0ed345133c51c65b4fd398deb9d4141eba414d6b
Nikhil S <nikhilks219@gmail.com>
2018-03-15 10:09:53 -0400

    22980: added specs

---
+++ spec/factories/sponsored_benefits_benefit_markets_aca_shop_initial_application_configurations.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :sponsored_benefits_benefit_markets_aca_shop_initial_application_configuration, class: 'SponsoredBenefits::BenefitMarkets::AcaShopInitialApplicationConfiguration' do
+  end
+end
