b93baf902c09e29bcc54d0fd2d7658f1d619e7f6
venkatbhattiprolu <vbhat847@gmail.com>
2018-03-16 12:13:54 -0400

    refs#23013 refactored existing rake to support non conversion er's

--- spec/data_migrations/update_conversion_flag_spec.rb
+++ spec/data_migrations/update_conversion_flag_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 require File.join(Rails.root, "app", "data_migrations", "update_conversion_flag")
 
-describe UpdateConversionFlag, dbclean: :after_each do
+describe UpdateConversionFlag, :dbclean => :after_each do
 
   let(:given_task_name) { "update_conversion_flag" }
   subject { UpdateConversionFlag.new(given_task_name, double(:current_scope => nil)) }
-
   describe "given a task name" do
     it "has the given task name" do
       expect(subject.name).to eql given_task_name
@@ -21,7 +20,7 @@ describe UpdateConversionFlag, dbclean: :after_each do
       subject.migrate
       employer_profile.reload
       expect(employer_profile.profile_source).to eq "self_serve"
-      expect(employer_profile.employer_attestation.aasm_state).to eq "submitted"
+      expect(employer_profile.employer_attestation.aasm_state).to eq "approved"
     end
 
     it "should update profile source to conversion and approve employer attestation" do
