a58d5f0157be2866236059c136eebd0f9722c78f
saimekala07 <saimekala07@gmail.com>
2018-03-19 15:25:22 -0400

    refs #22891 refactor

--- app/data_migrations/update_ee_dot.rb
+++ app/data_migrations/update_ee_dot.rb
@@ -4,11 +4,17 @@ class UpdateEeDot < MongoidMigrationTask
   def migrate
     begin
       id = (ENV['id']).to_s
-      employment_terminated_on = Date.strptime(ENV['employment_terminated_on'].to_s, "%m/%d/%Y") if ENV['employment_terminated_on'].present?
-      coverage_terminated_on = Date.strptime(ENV['coverage_terminated_on'].to_s, "%m/%d/%Y") if ENV['coverage_terminated_on'].present?
       ce = CensusEmployee.where(id: id).first
-      ce.employment_terminated_on = employment_terminated_on
-      ce.coverage_terminated_on = coverage_terminated_on
+
+      if ENV['employment_terminated_on'].present?
+        employment_terminated_on = Date.strptime(ENV['employment_terminated_on'].to_s, "%m/%d/%Y")
+        ce.employment_terminated_on = employment_terminated_on
+      end
+
+      if ENV['coverage_terminated_on'].present?
+        coverage_terminated_on = Date.strptime(ENV['coverage_terminated_on'].to_s, "%m/%d/%Y")
+        ce.coverage_terminated_on = coverage_terminated_on
+      end
       ce.save!
       puts "Changed Date of Termination to #{employment_terminated_on} and Coverage Termination to #{coverage_terminated_on}" unless Rails.env.test?
     rescue
