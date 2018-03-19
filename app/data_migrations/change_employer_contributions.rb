9a731ad38e44bb655451f20ca1135df29b7f1441
ymhari <hariym471@gmail.com>
2018-03-19 10:30:34 -0400

    ref: 22458 updates the employer contribution rake, applies changes for both dental and health benefits

--- app/data_migrations/change_employer_contributions.rb
+++ app/data_migrations/change_employer_contributions.rb
@@ -5,13 +5,21 @@ class ChangeEmployerContributions < MongoidMigrationTask
   def migrate
     organizations = Organization.where(fein: ENV['fein'])
     state = ENV['aasm_state'].to_s
+    kind = ENV['coverage_kind'].to_s
     relationship = ENV['relationship'].to_s
     premium = ENV['premium']
     offered = ENV['offered']
     if organizations.size !=1
       raise 'Issues with fein'
     end
-    rb = organizations.first.employer_profile.plan_years.where(aasm_state: state).first.benefit_groups.first.relationship_benefits.where(:relationship => relationship).first
-    rb.update_attributes(:premium_pct => premium, offered: offered)
+    if kind == "health"
+      rb = organizations.first.employer_profile.plan_years.where(aasm_state: state).first.benefit_groups.first.relationship_benefits.where(:relationship => relationship).first
+      rb.update_attributes(:premium_pct => premium, offered: offered)
+    elsif kind == "dental"
+      rb = organizations.first.employer_profile.plan_years.where(aasm_state: state).first.benefit_groups.first.dental_relationship_benefits.where(:relationship => relationship).first
+      rb.update_attributes(:premium_pct => premium, offered: offered)
+    else
+      raise "Please provide accurate coverage kind"
+    end
   end
 end
