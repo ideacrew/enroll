5d73d97f780412fda0412b6aadeb7d51af21f8fc
Bill Transue <transue@gmail.com>
2018-03-19 15:50:38 -0400

    Move BenefitMarkets to its own engine

--- spec/factories/sponsored_benefits_benefit_markets_aca_shop_initial_application_configurations.rb
+++
@@ -1,4 +0,0 @@
-FactoryGirl.define do
-  factory :sponsored_benefits_benefit_markets_aca_shop_initial_application_configuration, class: 'SponsoredBenefits::BenefitMarkets::AcaShopInitialApplicationConfiguration' do
-  end
-end
