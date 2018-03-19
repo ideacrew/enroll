e01c5904eaaed04a620d5112f6d38d670c173379
Dan Thomas <dan.thomas@dc.gov>
2018-03-19 09:10:25 -0400

    Initialize RatingFactor class/sublasses

---
+++ app/models/sponsored_benefits/rating_models/sic_rating_factor.rb
@@ -0,0 +1,14 @@
+module SponsoredBenefits
+  module RatingModels
+    class SicRatingFactor < RatingFactor
+
+      field :sic_factor_table, type: Hash
+
+
+      def result
+      end
+
+
+    end
+  end
+end
