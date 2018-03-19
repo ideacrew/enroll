9756908741744f57a49132ca6e46661463279a9e
Dan Thomas <dan.thomas@dc.gov>
2018-03-19 11:52:06 -0400

    Fix namespace

--- app/models/sponsored_benefits/benefit_applications/congress_benefit_application.rb
+++ app/models/sponsored_benefits/benefit_applications/congress_benefit_application.rb
@@ -1,5 +1,12 @@
 module SponsoredBenefits
-  class BenefitApplications::CongressBenefitApplication
+  module BenefitApplications
+    class CongressBenefitApplication < BenefitApplication
     include Mongoid::Document
+
+      ## Congress member level
+      # 'newly designated', special rules (bypass probation period)
+
+
+    end
   end
 end
