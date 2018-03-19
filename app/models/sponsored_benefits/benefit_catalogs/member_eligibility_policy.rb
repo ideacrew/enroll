a2374813ef294114d997fe5e2ac378e1b067d210
Dan Thomas <dan.thomas@dc.gov>
2018-03-19 09:08:57 -0400

    Add some basic policy kinds

--- app/models/sponsored_benefits/benefit_catalogs/member_eligibility_policy.rb
+++ app/models/sponsored_benefits/benefit_catalogs/member_eligibility_policy.rb
@@ -2,24 +2,31 @@
 module SponsoredBenefits
   module BenefitCatalogs
     class MemberEligibilityPolicy
+      include Mongoid::Document
+      include Mongoid::Timestamps
+
+      field :age_range,               type: Range,      default: 0..0
+      field :child_age_off,           type: Integer,    default: 26
+      field :incarceration_status,    type: Array,      default: [ :any ]   # => [:any, :unincarcerated],
+      field :citizenship_status,      type: Array,      default: [ :any ]   # => [:any, :us_citizen :naturalized_citizen :alien_lawfully_present :lawful_permanent_resident]
+      field :residency_status,        type: Array,      default: [ :any ]   # => [:any, :state_resident],
+      field :ethnicity,               type: Array,      default: [ :any ]   # => [:any, :indian_tribe_member]
+
+      field :product_dependencies,    type: Array,      default: [ :any ]   # => DC SHOP: must purchase health to purchase dental { dental: [ :health ] }
+
+      field :cost_sharing,            type: String,     default: ""
+      field :lawful_presence_status,  type: String,     default: ""
 
       # DC Individual
       # age-off 26, 65
-      # catestrophic plans - enrollment group must be < 30
+      # catastrophic plans - enrollment group must be < 30
       ## See existing rules
-
-      # DC SHOP 
-      # must purchase health to purchase dental
-
-      ## Congress member level
-      # 'newly designated' special rules (bypass probation period)
 
       # CCA SHOP
       # access frozen plans if member enrolled in last year's mapped plan == true
 
       # GIC 
       # must purchase life to purchase health
-
 
     end
   end
