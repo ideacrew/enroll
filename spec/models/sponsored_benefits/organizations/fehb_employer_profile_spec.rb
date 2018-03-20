583750cb402955cc16c5e70f580910b3b915ebe7
Dan Thomas <dan.thomas@dc.gov>
2018-03-20 09:55:13 -0400

    Replace congress_profile with fehb_profile, change attribute to is_benefit_sponsorship_eligible

--- spec/models/sponsored_benefits/organizations/fehb_employer_profile_spec.rb
+++ spec/models/sponsored_benefits/organizations/fehb_employer_profile_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 module SponsoredBenefits
-  RSpec.describe Organizations::CongressEmployerProfile, type: :model do
+  RSpec.describe Organizations::FehbEmployerProfile, type: :model do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
