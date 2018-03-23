f281cde66c702212d7605ce65e8321a845c3cfbc
Pranab <pranab.khanal@gmail.com>
2018-03-23 11:08:31 -0400

    Refs#23225 moving broker roles related code.

---
+++ components/benefit_sponsors/app/models/benefit_sponsors/forms/npn_field.rb
@@ -0,0 +1,19 @@
+module BenefitSponsors
+  module Forms
+    module NpnField
+
+      def self.included(base)
+        base.class_eval do
+          attr_reader :npn
+
+          def npn=(new_npn)
+            if !new_npn.blank?
+              @npn = new_npn.to_s.gsub(/\D/, '')
+            end
+          end
+        end
+      end
+
+    end
+  end
+end
--- components/benefit_sponsors/config/routes.rb
+++ components/benefit_sponsors/config/routes.rb
@@ -1,2 +1,13 @@
 BenefitSponsors::Engine.routes.draw do
+  namespace :broker_agencies do
+    resources :broker_roles, only: [:create] do
+      collection do
+        get :new_broker
+        get :new_staff_member
+        get :new_broker_agency, as: 'broker_registration'
+        get :search_broker_agency
+      end
+    end
+  end
+
 end
