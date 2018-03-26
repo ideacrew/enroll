e4387583b8e87a69b3b8c74cf2ce4bdd3d724ea2
Raghuram <raghuramg83@gmail.com>
2018-03-26 17:07:12 -0400

    Benefit application spec fixes

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_to_plan_year_converter.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_to_plan_year_converter.rb
@@ -15,24 +15,24 @@ module BenefitSponsors
           recorded_rating_area            = ""
 
           copied_benefit_groups = []
-          benefit_groups.each do |benefit_group|
-            benefit_group.attributes.delete("_type")
-            copied_benefit_groups << ::BenefitGroup.new(benefit_group.attributes)
+          benefit_application.benefit_packages.each do |benefit_package|
+            benefit_package.attributes.delete("_type")
+            copied_benefit_groups << ::BenefitGroup.new(benefit_package.attributes)
           end
 
           ::PlanYear.new(
-            start_on: effective_period.begin,
-            end_on: effective_period.end,
-            open_enrollment_start_on: open_enrollment_period.begin,
-            open_enrollment_end_on: open_enrollment_period.end,
+            start_on: benefit_application.effective_period.begin,
+            end_on: benefit_application.effective_period.end,
+            open_enrollment_start_on: benefit_application.open_enrollment_period.begin,
+            open_enrollment_end_on: benefit_application.open_enrollment_period.end,
             benefit_groups: copied_benefit_groups
             )
         end
       end
 
       def application_valid?
-       return false unless benefit_application.benefit_sponsorship.present? && benefit_application.effective_period.present? && benefit_application.open_enrollment_period.present?
-       raise "Invalid number of benefit_groups: #{benefit_groups.size}" if benefit_groups.size != 1
+        raise "Invalid number of benefit_packages: #{benefit_application.benefit_packages.size}" if benefit_application.benefit_packages.size != 1
+        benefit_application.benefit_sponsorship.present? && benefit_application.effective_period.present? && benefit_application.open_enrollment_period.present?
       end
     end
   end
