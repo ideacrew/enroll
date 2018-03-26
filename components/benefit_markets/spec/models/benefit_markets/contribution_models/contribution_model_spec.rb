c58204ad7be65514b21cae09817faf625dfc1eb9
Trey Evans <lewis.r.evans@gmail.com>
2018-03-26 15:37:18 -0400

    Specs for valid contribution units.

--- components/benefit_markets/spec/models/benefit_markets/contribution_models/contribution_model_spec.rb
+++ components/benefit_markets/spec/models/benefit_markets/contribution_models/contribution_model_spec.rb
@@ -16,6 +16,11 @@ module BenefitMarkets
         subject.valid?
         expect(subject.errors.has_key?(:contribution_units)).to be_truthy
       end
+
+      it "is missing member relationships" do
+        subject.valid?
+        expect(subject.errors.has_key?(:member_relationships)).to be_truthy
+      end
     end
   end
 end
