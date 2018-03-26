5f3fb0f05f3405612a703e3c82634bc787936465
Battula <vrbattula@outlook.com>
2018-03-26 13:54:20 -0400

    refs #23193 Moving views related to Benefit Application to engine.

--- components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
@@ -2,6 +2,18 @@ module BenefitSponsors
   module BenefitApplications
     class BenefitApplicationsController < ApplicationController
 
+      def new
+        @benefit_application = build_benefit_application
+      end
+
+      def build_benefit_application
+        benefit_application = BenefitSponsors::BenefitApplications::BenefitApplication.new
+        benefit_application.benefit_packages.build
+        benefit_application.benefit_packages.first.build_relationship_benefits
+        benefit_application.benefit_packages.first.build_dental_relationship_benefits
+        BenefitSponsors::Forms::BenefitApplicationForm.new(benefit_application)
+      end
+
     end
   end
 end
