193c25b57ac9a9d76b27913fa949315c1517bfa9
Bill Transue <transue@gmail.com>
2018-03-27 10:05:03 -0400

    Start marking Site and BenefitMarket controllers

--- components/benefit_markets/config/routes.rb
+++ components/benefit_markets/config/routes.rb
@@ -1,4 +1,12 @@
 BenefitMarkets::Engine.routes.draw do
+  resources :sites, only: [] do
+    resources :benefit_markets, shallow_nested: true
+  end
+
+  resources :benefit_markets, only: [] do
+    resource :configuration
+  end
+
   namespace :products do
     resources :product_packages, :only => [:new, :create, :show, :edit, :update]
   end
