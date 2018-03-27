8c4668f6a06e118f9b791371f2ffaecb8352232c
Dan Thomas <dan.thomas@dc.gov>
2018-03-26 22:06:10 -0400

    Add new builder pattern

--- components/benefit_markets/app/models/benefit_markets/sponsored_benefits/sponsored_benefit.rb
+++ components/benefit_markets/app/models/benefit_markets/sponsored_benefits/sponsored_benefit.rb
@@ -7,6 +7,7 @@ module BenefitMarkets
       field :hbx_id,      type: String
       field :title,       type: String
       field :description, type: String
+      field :kind,        type: Symbol
 
       embeds_one :product_package,    class_name: "BenefitMarkets::ProductPackages::ProductPackage"
       embeds_one :contribution_model, class_name: "BenefitMarkets::ContributionModels::ContributionModel"
