82ce160b5fe2bea8c206152ea3368933134a78a7
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-03-28 13:33:38 -0400

    Refs #22470 using new transition model to verify active role

--- app/models/individual_market_transition.rb
+++ app/models/individual_market_transition.rb
@@ -5,12 +5,14 @@ class IndividualMarketTransition
 
   embedded_in :person
 
-  ROLE_TYPES   = %W(consumer coverall)
+  ROLE_TYPES   = %W(consumer resident)
   REASON_CODES = %W(reason1 reason2)
 
   field :role_type, type: String
   field :effective_starting_on, type: Date
-  field :effective_ending_on, type: Date
+  # make ending on default to nil to help enforce logic that it needs to be set upon a market transtion so there is always
+  # a single individual_market_transition on a person that is not nil, e.g. the active role
+  field :effective_ending_on, type: Date, default: nil
   field :reason_code, type: String
   field :submitted_at, type: DateTime
   field :user_id, type: BSON::ObjectId
@@ -29,9 +31,18 @@ class IndividualMarketTransition
           allow_nil:   false,
           inclusion: {in: REASON_CODES, message: "%{value} is not a valid transistion reason code"}
 
-  before_save :set_submitted_at
+  before_save :set_submitted_at, :set_submitted_by
 
   def set_submitted_at
     self.submitted_at ||= TimeKeeper.datetime_of_record
+  end
+
+  #TODO set the id of the admin who is making the transition before saving
+  def set_submitted_by
+
+  end
+
+  def self.all
+    Person.all_individual_market_transitions
   end
 end
