5c6f7a66e3dff5310b5c84eeee646b2488c7370e
Battula <vrbattula@outlook.com>
2018-03-29 14:12:01 -0400

    Added BenefitApplicationsController#edit.

--- components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
@@ -2,6 +2,8 @@ module BenefitSponsors
   module BenefitApplications
     class BenefitApplicationsController < ApplicationController
 
+      before_action :find_benefit_sponsorship
+
       def new
         @benefit_application = build_benefit_application
       end
@@ -12,6 +14,30 @@ module BenefitSponsors
         benefit_application.benefit_packages.first.build_relationship_benefits
         benefit_application.benefit_packages.first.build_dental_relationship_benefits
         BenefitSponsors::Forms::BenefitApplicationForm.new(benefit_application)
+      end
+
+      def edit
+        benefit_application = @benefit_sponsorship.find_benefit_application(params[:id])
+        if params[:publish]
+          @just_a_warning = !benefit_application.is_application_eligible? ? true : false
+          benefit_application.application_warnings
+        end
+        @benefit_application = BenefitSponsors::Forms::BenefitApplicationForm.new(benefit_application)
+        @benefit_application.benefit_packages.each do |benefit_package|
+          benefit_package.build_relationship_benefits if benefit_package.relationship_benefits.empty?
+          benefit_package.build_dental_relationship_benefits if benefit_package.dental_relationship_benefits.empty?
+        end
+
+        respond_to do |format|
+          format.js { render 'edit' }
+          format.html { render 'edit' }
+        end
+      end
+
+      private
+
+      def find_benefit_sponsorship
+        @benefit_sponsorship = BenefitSponsors::BenefitSponsorships::BenefitSponsorship.find(params[:benefit_sponsorship_id])
       end
 
     end
