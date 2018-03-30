ff334de5f8ce623f710347a042fa14b604eea3db
Dan Thomas <dan.thomas@dc.gov>
2018-03-30 10:01:18 -0400

    WIP on PlanDesigns composite pattern

--- components/benefit_sponsors/spec/models/benefit_sponsors/plan_designs/build_health_sponsored_benefit_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/plan_designs/build_health_sponsored_benefit_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 module BenefitSponsors
-  RSpec.describe PlanDesigns::TransformSelectedProductToBenefit, type: :model do
+  RSpec.describe PlanDesigns::BuildHealthSponsoredBenefit, type: :model do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
