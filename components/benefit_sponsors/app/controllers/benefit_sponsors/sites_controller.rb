ddc750a2b10d5abcba160edf1416b42bf0f1fbed
Bill Transue <transue@gmail.com>
2018-04-04 09:55:35 -0400

    Flesh out Sites actions

--- components/benefit_sponsors/app/controllers/benefit_sponsors/sites_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/sites_controller.rb
@@ -1,15 +1,45 @@
-module BenefitSponsers
+module BenefitSponsors
   # Sites Controller
   class SitesController < ApplicationController
     before_action :find_hbx_admin_user
 
     def index
+      @sites = BenefitSponsors::Site.all
+    end
+
+    def new
+      @site = BenefitSponsors::Site.new
+    end
+
+    def create
+      @site = BenefitSponsors::Site.new params[:site]
+
+      if @site.save
+        redirect_to :index
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @site = BenefitSponsors::Site.find params[:id]
     end
 
     def update
+      @site = BenefitSponsors::Site.find params[:id]
+
+      if @site.update_attributes params[:site]
+        redirect_to :index
+      else
+        render 'edit'
+      end
     end
 
     def destroy
+      @site = BenefitSponsors::Site.find params[:id]
+      @site.destroy
+
+      redirect_to 'index'
     end
 
     private
