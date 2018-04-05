daf6a734fc09445023bb5e74ef146050a9cd85aa
Battula <vrbattula@outlook.com>
2018-04-05 15:17:48 -0400

    refs #21938 Updated stubbed data.

--- components/notifier/app/models/notifier/merge_data_models/qualifying_life_event_kind.rb
+++ components/notifier/app/models/notifier/merge_data_models/qualifying_life_event_kind.rb
@@ -11,11 +11,11 @@ module Notifier
 
     def self.stubbed_object
       Notifier::MergeDataModels::QualifyingLifeEventKind.new({
-        title: Settings.aca.qle.title,
-        event_on: TimeKeeper.date_of_record - Settings.aca.qle.eligible_event_on.days,
-        start_on: TimeKeeper.date_of_record - Settings.aca.qle.start_on.days,
-        end_on: TimeKeeper.date_of_record - Settings.aca.qle.end_on.days,
-        reported_on: TimeKeeper.date_of_record - Settings.aca.qle.reported_on.days
+        title: 'Married',
+        event_on: TimeKeeper.date_of_record,
+        start_on: TimeKeeper.date_of_record,
+        end_on: TimeKeeper.date_of_record.next_month.prev_day,
+        reported_on: TimeKeeper.date_of_record
         })
     end
   end
