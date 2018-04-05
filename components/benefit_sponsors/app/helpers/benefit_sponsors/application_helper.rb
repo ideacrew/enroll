3b6275c64635d4d5b514d977c23d3bec060511cf
202chaz <chaz.haskins@gmail.com>
2018-04-05 06:50:56 -0400

    flash messages updates

--- components/benefit_sponsors/app/helpers/benefit_sponsors/application_helper.rb
+++ components/benefit_sponsors/app/helpers/benefit_sponsors/application_helper.rb
@@ -1,4 +1,30 @@
 module BenefitSponsors
   module ApplicationHelper
+    
+    def bootstrap_class_for flash_type
+      #{ success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+      case flash_type
+        when "notice"
+          "alert-info"
+        when "success"
+          "alert-success"
+        when "error"
+          "alert-danger"
+        when "alert"
+          "alert-warning"
+      end
+    end
+
+    def flash_messages(opts = {})
+      flash.each do |msg_type, message|
+        binding.pry
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show") do 
+          concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+          concat message 
+        end)
+      end
+      nil
+    end
+      
   end
 end
