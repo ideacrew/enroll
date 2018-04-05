1bc272a129bab3576b0a129ff54231e60c66198e
202chaz <chaz.haskins@gmail.com>
2018-04-05 10:02:16 -0400

    dynamic fields update

--- components/benefit_sponsors/app/helpers/benefit_sponsors/application_helper.rb
+++ components/benefit_sponsors/app/helpers/benefit_sponsors/application_helper.rb
@@ -2,7 +2,6 @@ module BenefitSponsors
   module ApplicationHelper
     
     def bootstrap_class_for flash_type
-      #{ success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
       case flash_type
         when "notice"
           "alert-info"
@@ -17,7 +16,6 @@ module BenefitSponsors
 
     def flash_messages(opts = {})
       flash.each do |msg_type, message|
-        binding.pry
         concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show") do 
           concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
           concat message 
