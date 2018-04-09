23a113ac65da26d8f5de11fff1f45791de2f367d
Trey Evans <lewis.r.evans@gmail.com>
2018-04-09 12:45:10 -0400

    Adding benefit market and catalog for DC.

--- db/seedfiles/dc_shop_benefit_market_seed.rb
+++ db/seedfiles/dc_shop_benefit_market_seed.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "dc_pricing_and_contribution_models_seed.rb"))
+
 benefit_market = ::BenefitMarkets::BenefitMarket.create!({
   kind: :aca_shop,
   title: "DC Health Link SHOP Market"
@@ -8,4 +10,45 @@ benefit_market_catalog = ::BenefitMarkets::BenefitMarketCatalog.create!({
   application_interval_kind: :monthly,
   benefit_market: benefit_market,
   application_period: Date.new(2018,1,1)..Date.new(2018,12,31)
+})
+
+dc_contribution_model = ::BenefitMarkets::ContributionModels::ContributionModel.where(name: "DC Shop Contribution Model").first
+dc_pricing_model = ::BenefitMarkets::PricingModels::PricingModel.where(name: "DC Shop Pricing Model").first
+
+["bronze", "silver", "gold", "platinum"].each do |ml|
+  ::BenefitMarkets::Products::HealthProducts::MetalLevelHealthProductPackage.create!({
+    :metal_level => ml,
+    :title => "DC Health #{ml.capitalize} Metal Level",
+    :contribution_model_id => dc_contribution_model.id,
+    :pricing_model_id => dc_pricing_model.id,
+    :benefit_catalog => benefit_market_catalog
+  })
+end
+
+# Clean this up once we have migrated Plans => Products
+Organization.where(carrier_profile: {"$ne" => nil}).each do |org|
+  carrier_profile = org.carrier_profile
+  if Plan.where(:carrier_profile_id => carrier_profile.id, :market => "shop", :coverage_kind => "health", :active_year => benefit_market_catalog.application_period.first.year).any?
+    ::BenefitMarkets::Products::HealthProducts::IssuerHealthProductPackage.create!({
+      :issuer_id => carrier_profile.id,
+      :title => "DC Health #{carrier_profile.legal_name} Issuer",
+      :contribution_model_id => dc_contribution_model.id,
+      :pricing_model_id => dc_pricing_model.id,
+      :benefit_catalog => benefit_market_catalog
+    })
+  end
+end
+
+::BenefitMarkets::Products::HealthProducts::SingleProductHealthProductPackage.create!({
+  :title => "DC Health Single Product",
+  :contribution_model_id => dc_contribution_model.id,
+  :pricing_model_id => dc_pricing_model.id,
+  :benefit_catalog => benefit_market_catalog
+})
+
+::BenefitMarkets::Products::DentalProducts::AnyDentalProductPackage.create!({
+  :title => "DC Dental Any Product",
+  :contribution_model_id => dc_contribution_model.id,
+  :pricing_model_id => dc_pricing_model.id,
+  :benefit_catalog => benefit_market_catalog
 })
