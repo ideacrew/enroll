5ea28fc69a0e236cd5ace510dc8e947e49392979
Nikhil S <nikhilks219@gmail.com>
2018-04-09 10:54:18 -0400

    Ref#23083: added site actions and views

--- components/benefit_sponsors/app/controllers/benefit_sponsors/sites_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/sites_controller.rb
@@ -1,6 +1,22 @@
 module BenefitSponsers
+  # Sites Controller
   class SitesController < ApplicationController
+    before_action :find_hbx_admin_user
+
     def index
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def find_hbx_admin_user
+      fail NotAuthorizedError unless current_user.has_hbx_staff_role?
+      # redirect_to root_url
     end
   end
 end
