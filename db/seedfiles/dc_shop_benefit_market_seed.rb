a5d73e4a4499c40ef61997dda6e36588691eb888
Trey Evans <lewis.r.evans@gmail.com>
2018-04-09 12:51:32 -0400

    Fix

--- db/seedfiles/dc_shop_benefit_market_seed.rb
+++ db/seedfiles/dc_shop_benefit_market_seed.rb
@@ -15,20 +15,24 @@ benefit_market_catalog = ::BenefitMarkets::BenefitMarketCatalog.create!({
 dc_contribution_model = ::BenefitMarkets::ContributionModels::ContributionModel.where(name: "DC Shop Contribution Model").first
 dc_pricing_model = ::BenefitMarkets::PricingModels::PricingModel.where(name: "DC Shop Pricing Model").first
 
+plan_active_year = benefit_market_catalog.application_period.first.year
+
 ["bronze", "silver", "gold", "platinum"].each do |ml|
-  ::BenefitMarkets::Products::HealthProducts::MetalLevelHealthProductPackage.create!({
-    :metal_level => ml,
-    :title => "DC Health #{ml.capitalize} Metal Level",
-    :contribution_model_id => dc_contribution_model.id,
-    :pricing_model_id => dc_pricing_model.id,
-    :benefit_catalog => benefit_market_catalog
-  })
+  if Plan.where(:metal_level => ml, :market => "shop", :coverage_kind => "health", :active_year => plan_active_year).any?
+    ::BenefitMarkets::Products::HealthProducts::MetalLevelHealthProductPackage.create!({
+      :metal_level => ml,
+      :title => "DC Health #{ml.capitalize} Metal Level",
+      :contribution_model_id => dc_contribution_model.id,
+      :pricing_model_id => dc_pricing_model.id,
+      :benefit_catalog => benefit_market_catalog
+    })
+  end
 end
 
 # Clean this up once we have migrated Plans => Products
 Organization.where(carrier_profile: {"$ne" => nil}).each do |org|
   carrier_profile = org.carrier_profile
-  if Plan.where(:carrier_profile_id => carrier_profile.id, :market => "shop", :coverage_kind => "health", :active_year => benefit_market_catalog.application_period.first.year).any?
+  if Plan.where(:carrier_profile_id => carrier_profile.id, :market => "shop", :coverage_kind => "health", :active_year => plan_active_year).any?
     ::BenefitMarkets::Products::HealthProducts::IssuerHealthProductPackage.create!({
       :issuer_id => carrier_profile.id,
       :title => "DC Health #{carrier_profile.legal_name} Issuer",
@@ -46,9 +50,11 @@ end
   :benefit_catalog => benefit_market_catalog
 })
 
-::BenefitMarkets::Products::DentalProducts::AnyDentalProductPackage.create!({
-  :title => "DC Dental Any Product",
-  :contribution_model_id => dc_contribution_model.id,
-  :pricing_model_id => dc_pricing_model.id,
-  :benefit_catalog => benefit_market_catalog
-})
+if Plan.where(:market => "shop", :coverage_kind => "dental", :active_year => plan_active_year).any?
+  ::BenefitMarkets::Products::DentalProducts::AnyDentalProductPackage.create!({
+    :title => "DC Dental Any Product",
+    :contribution_model_id => dc_contribution_model.id,
+    :pricing_model_id => dc_pricing_model.id,
+    :benefit_catalog => benefit_market_catalog
+  })
+end
