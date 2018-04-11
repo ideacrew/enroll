47f7ab0d19c07502d8907cf747db428813647e46
Trey Evans <lewis.r.evans@gmail.com>
2018-04-11 16:49:21 -0400

    Fix spec I broke and update seed.

--- db/seedfiles/dc_shop_benefit_market_seed.rb
+++ db/seedfiles/dc_shop_benefit_market_seed.rb
@@ -9,7 +9,8 @@ benefit_market_catalog = ::BenefitMarkets::BenefitMarketCatalog.create!({
   title: "DC Health Link SHOP Benefit Catalog",
   application_interval_kind: :monthly,
   benefit_market: benefit_market,
-  application_period: Date.new(2018,1,1)..Date.new(2018,12,31)
+  application_period: Date.new(2018,1,1)..Date.new(2018,12,31),
+  probation_period_kinds: ::BenefitMarkets::PROBATION_PERIOD_KINDS
 })
 
 dc_contribution_model = ::BenefitMarkets::ContributionModels::ContributionModel.where(name: "DC Shop Contribution Model").first
