f178cbeea4b5a0419e61a899f46fada7b72df1c0
Nikhil S <nikhilks219@gmail.com>
2018-04-11 15:19:28 -0400

    bqt_engine_dc_sites_form_and_factor

--- components/benefit_sponsors/app/models/benefit_sponsors/forms/site.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/forms/site.rb
@@ -4,29 +4,30 @@ module BenefitSponsors
       include ActiveModel::Model
       include ActiveModel::Validations
 
-      attr_accessor :id, :site_key, :long_name, :short_name, :byline
+      attr_accessor :id, :site_key, :long_name, :short_name, :byline, :copyright_period_start, :site_organizations_count, :updated_at, :created_at
 
       validates :short_name, presence: true, :allow_blank => false
       validates :long_name, presence: true, :allow_blank => false
 
       def initialize(user, attributes={})
-        assign_attributes attributes
+        @user = user
+        assign_wrapper_attributes attributes
       end
 
-      def assign_attributes(atts = {})
-        atts.each_pair do |k, v|
-          if k.to_sym == :id
-            #find model with factory, set attributes with model.attributes
-            assign_attributes BenefitSponsors::SiteFactory.find(v).attributes.except(:id)
-          else
-            self.send("#{k}||=".to_sym, v)
-          end
+      def assign_wrapper_attributes(attributes = {}, options={})
+        if self.id = attributes.delete(:id)
+          #find model with factory, set attributes with model.attributes
+          assign_wrapper_attributes BenefitSponsors::SiteFactory.find(@user, id).attributes.except("_id"), protect: true
+        end
+
+         attributes.each_pair do |k, v|
+          self.send("#{k}=", v) unless self.send(k) && options[:protect]
         end
       end
 
       def save
         return false unless self.valid?
-        factory_object = BenefitSponsors::SiteFactory.build attributes
+        BenefitSponsors::SiteFactory.call(self).persist
       end
 
       def attributes
