3ccd5beffe7fa38d7fcad69bf8424481ed335472
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-04-12 14:03:46 -0400

    Refs #22470 group seclection new view specs passing

--- spec/factories/individual_market_transition.rb
+++ spec/factories/individual_market_transition.rb
@@ -3,12 +3,12 @@ FactoryGirl.define do
     association :person
 
     role_type 'consumer'
-    reason_code 'reason1'
+    reason_code 'generating_consumer_role'
 
 
     trait :resident do
       role_type 'resident'
-      reason_code 'reason2'
+      reason_code 'generating_resident_role'
     end
   end
 end
