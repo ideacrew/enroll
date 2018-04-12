e06e144f521ddfad5e08efddef54fb5b6540d77d
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-04-12 14:35:38 -0400

    Refs #22470 fixed group selection helper spec

--- spec/helpers/insured/group_selection_helper_spec.rb
+++ spec/helpers/insured/group_selection_helper_spec.rb
@@ -6,12 +6,21 @@ RSpec.describe Insured::GroupSelectionHelper, :type => :helper do
   describe "#can shop individual" do
     let(:person) { FactoryGirl.create(:person) }
 
+    before(:each) do
+      allow(person).to receive(:is_consumer_role_active?).and_return(false)
+    end
+
+
     it "should not have an active consumer role" do
       expect(subject.can_shop_individual?(person)).not_to be_truthy
     end
 
     context "with active consumer role" do
       let(:person) { FactoryGirl.create(:person, :with_consumer_role) }
+
+      before(:each) do
+        allow(person).to receive(:is_consumer_role_active?).and_return(true)
+      end
       it "should have active consumer role" do
         expect(subject.can_shop_individual?(person)).to be_truthy
       end
@@ -57,6 +66,7 @@ RSpec.describe Insured::GroupSelectionHelper, :type => :helper do
       let(:person) { FactoryGirl.create(:person, :with_consumer_role, :with_employee_role) }
       before do
         allow(person).to receive(:has_active_employee_role?).and_return(true)
+        allow(person).to receive(:is_consumer_role_active?).and_return(true)
       end
       it "should have both active consumer and employee role" do
         expect(subject.can_shop_both_markets?(person)).not_to be_truthy
@@ -68,6 +78,8 @@ RSpec.describe Insured::GroupSelectionHelper, :type => :helper do
       before do
         allow(person).to receive(:has_active_employee_role?).and_return(true)
         allow(person).to receive(:has_employer_benefits?).and_return(true)
+        allow(person).to receive(:is_consumer_role_active?).and_return(true)
+
       end
       it "should have both active consumer and employee role" do
         expect(subject.can_shop_both_markets?(person)).to be_truthy
