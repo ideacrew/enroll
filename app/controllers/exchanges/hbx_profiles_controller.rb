f5d09573d9f83cc9db1f8d7a74740480793ed78e
sudhirreddysama <sudhirsama586@gmail.com>
2018-04-13 14:13:06 -0400

    Specs and UI changes
    cherry pick for new model

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -565,7 +565,7 @@ def employer_poc
     effective_date = params[:person][:effective_date].to_date
     slcsp = HbxProfile.current_hbx.benefit_sponsorship.current_benefit_coverage_period.slcsp_id
     @active_household = @primary_family.active_household
-    @active_household.build_thh_and_eligibility(params[:person][:max_aptc], params[:person][:csr], effective_date, slcsp, params[:person][:reason])
+    @active_household.build_thh_and_eligibility(params[:person][:max_aptc], params[:person][:csr], effective_date, slcsp)
     update_eligibility_kinds
   end
 
@@ -581,6 +581,7 @@ def employer_poc
       person = Person.by_hbx_id(person_hbx_id).first
       family_member = family_members.where(person_id: person.id).first
       tax_household_member = tax_household_members.where(applicant_id: family_member.id).first
+      tax_household_member.update_attribute(:reason, value['reason'])
       tax_household_member.update_eligibility_kinds(eligibility_kinds_hash(value))
     end
   end
