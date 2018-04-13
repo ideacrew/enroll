3ef6a2c32b4723d2447a9191c9a23a484b1eca4b
Raghuram <raghuramg83@gmail.com>
2018-04-13 14:11:29 -0400

    Updated benefit applications controller/form/factories

--- components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
@@ -2,55 +2,43 @@ module BenefitSponsors
   module BenefitApplications
     class BenefitApplicationsController < ApplicationController
 
-      before_action :load_benefit_sponsorship
-      before_action :load_benefit_application, only: [:edit, :update, :delete]
-
       def new
-        @benefit_application_form = BenefitSponsors::Forms::BenefitApplication.new
+        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(params)
       end
 
       def create
-        @benefit_application_form = BenefitSponsors::Forms::BenefitApplication.new(@benefit_sponsorship, benefit_application_params)
+        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(params)
 
-        if @benefit_application_form.save
-          redirect_to new_benefit_sponsorship_benefit_application_benefit_package_path(@benefit_sponsorship, @benefit_application_form.benefit_application)
+        if @benefit_application.save
+          redirect_to new_benefit_sponsorship_benefit_application_benefit_package_path(@benefit_application.benefit_sponsorship, @benefit_application.resource)
         else
-          flash[:error] = error_messages(@benefit_application_form)
+          flash[:error] = error_messages(@benefit_application)
           render :new
         end
       end
 
       def edit
-        @benefit_application_form = BenefitSponsors::Forms::BenefitApplication.load_from_object(@benefit_application)
+        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(params)
       end
 
       def update
-        @benefit_application_form = BenefitSponsors::Forms::BenefitApplication.new(@benefit_spnsorship, benefit_application_params)
-        @benefit_application_form.reference_benefit_application = @benefit_application
+        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(params)
         
-        if @benefit_application_form.save
-          redirect_to benefit_sponsorship_benefit_application_benefit_packages_path(@benefit_sponsorship, @benefit_application_form.benefit_application)
+        if @benefit_application.save
+          redirect_to benefit_sponsorship_benefit_application_benefit_packages_path(@benefit_application.benefit_sponsorship, @benefit_application.resource)
         else
-          flash[:error] = error_messages(@benefit_application_form)
+          flash[:error] = error_messages(@benefit_application)
           render :edit
         end
       end
 
       private
 
-      def load_benefit_sponsorship
-        @benefit_sponsorship = BenefitSponsors::BenefitSponsorships::BenefitSponsorship.find(params[:benefit_sponsorship_id])
-      end
-
-      def load_benefit_sponsorship
-        @benefit_application = @benefit_sponsorship.benefit_applications.find(params[:benefit_application_id])
-      end
-
       def error_messages(instance)
         instance.errors.full_messages.inject(""){|memo, error| "#{memo}<li>#{error}</li>"}.html_safe
       end
 
-      def benefit_application_params
+      def application_params
         params.require(:benefit_application).permit(
           :start_on, :end_on, :fte_count, :pte_count, :msp_count,
           :open_enrollment_start_on, :open_enrollment_end_on 
