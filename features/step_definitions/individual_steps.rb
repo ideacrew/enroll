9ccfc574b96933bd1c123553f596f142098b8049
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-04-17 15:14:14 -0400

    Refs #22470 1 to go

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -75,6 +75,7 @@ When(/^\w+ clicks? on continue button$/) do
 end
 
 Then(/^.+ should see heading labeled personal information/) do
+  #binding.pry
   expect(page).to have_content("Personal Information")
 end
 
@@ -270,6 +271,7 @@ end
 
 
 When(/^I click on continue button on group selection page during a sep$/) do
+  #binding.pry
   expect(page).to have_content "Choose Coverage for your Household"
   click_button "CONTINUE"
 end
