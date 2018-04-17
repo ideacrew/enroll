92b5a22044ad620760baae49949958154205efa1
Mahesh Kurma <maheshruby405@gmail.com>
2018-04-17 13:50:20 -0400

    refs #22885 add check for is_resident_role_active

--- app/controllers/insured/group_selection_controller.rb
+++ app/controllers/insured/group_selection_controller.rb
@@ -12,7 +12,7 @@ class Insured::GroupSelectionController < ApplicationController
     @employee_role = @person.active_employee_roles.first if @employee_role.blank? && @person.has_active_employee_role?
     @market_kind = select_market(@person, params)
     @resident = Person.find(params[:person_id]) if Person.find(params[:person_id]).resident_role?
-    if @market_kind == 'individual' || (@person.try(:has_active_employee_role?) && @person.try(:is_consumer_role_active?)) || @resident
+    if @market_kind == 'individual' || (@person.try(:has_active_employee_role?) && @person.try(:is_consumer_role_active?)) || @person.try(:is_resident_role_active?) || @resident
       if params[:hbx_enrollment_id].present?
         session[:pre_hbx_enrollment_id] = params[:hbx_enrollment_id]
         pre_hbx = HbxEnrollment.find(params[:hbx_enrollment_id])
