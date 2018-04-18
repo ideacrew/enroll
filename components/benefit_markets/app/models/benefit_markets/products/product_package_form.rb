920b7dfa21e216cf5a28cb557e3cedbf21f37246
Trey Evans <lewis.r.evans@gmail.com>
2018-04-18 11:36:55 -0400

    Moving to service.

--- components/benefit_markets/app/models/benefit_markets/products/product_package_form.rb
+++ components/benefit_markets/app/models/benefit_markets/products/product_package_form.rb
@@ -1,16 +1,19 @@
 module BenefitMarkets
   module Products
     class ProductPackageForm
+      include Virtus.model
       include ActiveModel::Model
       include ActiveModel::Validations
 
-      attr_accessor :title
-      attr_accessor :contribution_model_id
-      attr_accessor :pricing_model_id
-      attr_accessor :benefit_catalog_id
-      attr_accessor :benefit_option_kind
-      
-      attr_accessor :form_mapping
+      attribute :title, String
+      attribute :contribution_model_id, String
+      attribute :pricing_model_id, String
+      attribute :benefit_catalog_id, String
+      attribute :benefit_option_kind, String
+      attribute :allowed_benefit_option_kinds, Array
+      attribute :available_pricing_models, Array
+      attribute :available_contribution_models, Array
+      attribute :available_benefit_catalogs, Array
 
       attr_reader :show_page_model
 
@@ -20,24 +23,21 @@ module BenefitMarkets
       validates_presence_of :benefit_catalog_id, :allow_blank => false
       validates_inclusion_of :benefit_option_kind, :in => :allowed_benefit_option_kinds, :allow_blank => false
 
-      def allowed_benefit_option_kinds
-        form_mapping.benefit_option_kinds
-      end
-
       def self.for_new(current_user, benefit_option_kind)
-        resolve_form_subclass(benefit_option_kind).new(
-          :benefit_option_kind => benefit_option_kind,
-          :form_mapping => ::BenefitMarkets::Products::ProductPackageFormMapping.new
+        service = NewProductPackageService.new
+        form = resolve_form_subclass(benefit_option_kind).new(
+          :benefit_option_kind => benefit_option_kind
         )
+        service.populate_options(form)
+        form
       end
 
       def self.for_create(current_user, opts)
+        service = NewProductPackageService.new
         benefit_option_kind = opts.require(:benefit_option_kind)
-        resolve_form_subclass(benefit_option_kind).new(
-          opts.merge({
-            :form_mapping => ::BenefitMarkets::Products::ProductPackageFormMapping.new
-          })
-        )
+        form = resolve_form_subclass(benefit_option_kind).new(opts)
+        service.populate_options(form)
+        form
       end
 
       def self.resolve_form_subclass(benefit_option_kind)
@@ -51,25 +51,14 @@ module BenefitMarkets
         end
       end
 
-      def available_pricing_models
-        form_mapping.options_for_pricing_model_id(self)
-      end
-
-      def available_benefit_catalogs
-        form_mapping.options_for_benefit_catalog_id
-      end
-
-      def available_contribution_models
-        form_mapping.options_for_contribution_model_id
-      end
-
       def has_additional_attributes?
         false
       end
 
       def save
+        service = NewProductPackageService.new
         return false unless self.valid?
-        save_result, persisted_object = form_mapping.save(self)
+        save_result, persisted_object = service.save(self)
         return false unless save_result
         @show_page_model = persisted_object
         true
