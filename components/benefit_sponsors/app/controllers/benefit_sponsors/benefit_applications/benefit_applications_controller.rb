59d6d7a900911b64b98ef693a30789c59894cc4e
Raghuram <raghuramg83@gmail.com>
2018-04-18 12:58:29 -0400

    Added virtus for benefit application form, benefit application service and refactored controller

--- components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller.rb
@@ -1,52 +1,40 @@
 module BenefitSponsors
   module BenefitApplications
     class BenefitApplicationsController < ApplicationController
+      before_action :initalize_form
 
       def new
-        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(application_params)
       end
 
       def create
-        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(application_params)
-
-        if @benefit_application.save
-          redirect_to new_benefit_sponsorship_benefit_application_benefit_package_path(@benefit_application.benefit_sponsorship, @benefit_application.resource)
+        if @benefit_application_form.save
+          redirect_to new_benefit_sponsorship_benefit_application_benefit_package_path(@benefit_application_form.benefit_sponsorship, @benefit_application_form.benefit_application)
         else
-          flash[:error] = error_messages(@benefit_application)
+          flash[:error] = error_messages(@benefit_application_form)
           render :new
         end
       end
 
       def edit
-        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(application_params)
       end
 
       def update
-        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(application_params)
-        
-        if @benefit_application.save
-          redirect_to benefit_sponsorship_benefit_application_benefit_packages_path(@benefit_application.benefit_sponsorship, @benefit_application.resource)
+        if @benefit_application_form.save
+          redirect_to benefit_sponsorship_benefit_application_benefit_packages_path(@benefit_application_form.benefit_sponsorship, @benefit_application_form.benefit_application)
         else
-          flash[:error] = error_messages(@benefit_application)
+          flash[:error] = error_messages(@benefit_application_form)
           render :edit
         end
       end
 
       def recommend_dates
-        @benefit_application = BenefitSponsors::Forms::BenefitApplication.new(application_params)
-
-        if params[:start_on].present?
-          @start_on = params[:start_on].to_date
-          @result = @benefit_application.check_start_on(@start_on)
-
-          if @result[:result] == "ok"
-            @open_enrollment_dates = @benefit_application.calculate_open_enrollment_date(@start_on)
-            @schedule = @benefit_application.shop_enrollment_timetable(@start_on)
-          end
-        end
       end
 
       private
+
+      def initalize_form
+        @benefit_application_form = BenefitSponsors::Forms::BenefitApplicationForm.new(application_params)
+      end
 
       def error_messages(instance)
         instance.errors.full_messages.inject(""){|memo, error| "#{memo}<li>#{error}</li>"}.html_safe
---
+++ components/benefit_sponsors/app/models/benefit_sponsors/forms/benefit_application_form.rb
@@ -0,0 +1,75 @@
+module BenefitSponsors
+  module Forms
+    class BenefitApplicationForm
+
+      extend ActiveModel::Naming
+      include ActiveModel::Conversion
+      include ActiveModel::Validations
+      include Virtus
+
+      attribute :start_on, Date
+      attribute :end_on, Date
+      attribute :open_enrollment_start_on, Date
+      attribute :open_enrollment_end_on, Date
+      attribute :fte_count, Integer
+      attribute :pte_count, Integer
+      attribute :msp_count, Integer
+
+      validates :start_on, presence: true
+      validates :end_on, presence: true
+      validates :open_enrollment_start_on, presence: true
+      validates :open_enrollment_end_on, presence: true
+
+      # validates :validate_application_dates
+
+      delegate :benefit_sponsorship, :benefit_application, to: :benefit_application_service
+
+      def initialize(params)
+        @scheduler = ::BenefitSponsors::BenefitApplications::BenefitApplicationSchedular.new
+        @benefit_application_service = BenefitSponsors::BenefitApplications::BenefitApplicationFormMapping.new(params)
+      end
+
+      def save
+        return false unless self.valid?
+        save_result, persisted_object = @form_mapping.save(self)
+        return false unless save_result
+        @resource = persisted_object
+        true
+      end
+    
+      def validate_application_dates
+        if start_on <= end_on
+          errors.add(:start_on, "can't be later than end on date")
+        end
+      end
+
+      def calculate_start_on_options
+        scheduler.calculate_start_on_dates.map {|date| [date.strftime("%B %Y"), date.to_s(:db) ]}
+      end
+    
+      def start_on=(date)
+        super(date.to_date)
+      end
+
+      def is_start_on_valid?
+        start_on_result[:result] == "ok"
+      end
+
+      def start_on_result
+        scheduler.check_start_on(start_on)
+      end
+
+      def open_enrollment_dates
+        scheduler.calculate_open_enrollment_date(start_on) if is_start_on_valid?
+      end
+
+      def enrollment_schedule
+        scheduler.shop_enrollment_timetable(start_on) if is_start_on_valid?
+      end
+
+      private 
+
+      attr_reader :scheduler, :benefit_application_service
+    end
+  end
+end
