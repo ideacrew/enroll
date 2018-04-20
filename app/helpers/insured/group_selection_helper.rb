926a83182eba028bbed4e83aae9085a5391b3b7c
Mahesh Kurma <maheshruby405@gmail.com>
2018-04-20 16:39:44 -0400

    refs #22887 typo

--- app/helpers/insured/group_selection_helper.rb
+++ app/helpers/insured/group_selection_helper.rb
@@ -39,7 +39,7 @@ module Insured
         benefit_sponsorship: HbxProfile.current_hbx.try(:benefit_sponsorship))
     end
 
-    def  view_market_palces(person)
+    def  view_market_places(person)
       if can_shop_both_markets?(person)
         Plan::MARKET_KINDS
       elsif can_shop_individual?(person)
