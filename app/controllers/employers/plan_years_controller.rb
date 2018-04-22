080e3282beee06d88fe2aaa53f48db8645137562
vsonnathi <sonnathivinil@gmail.com>
2018-04-22 19:53:02 -0400

    Refs #22966

--- app/controllers/employers/plan_years_controller.rb
+++ app/controllers/employers/plan_years_controller.rb
@@ -15,7 +15,7 @@ class Employers::PlanYearsController < ApplicationController
   end
 
   def late_rates_check
-    date = params[:start_on_date].split('/')
+    date = params[:start_on_date].present? ? params[:start_on_date].split("/") : nil
     formatted_date = date.present? ? (Date.new(date[2].to_i,date[0].to_i,date[1].to_i) + 1.month).beginning_of_month : nil
     render json: Plan.has_rates_for_all_carriers?(formatted_date)
   end
