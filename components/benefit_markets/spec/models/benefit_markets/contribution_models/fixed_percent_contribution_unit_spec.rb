0e7f52166e871c6531a5496fd2005f1c0a1d875b
Trey Evans <lewis.r.evans@gmail.com>
2018-04-23 16:15:48 -0400

    Reduce pending specs where needed.

--- components/benefit_markets/spec/models/benefit_markets/contribution_models/fixed_percent_contribution_unit_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module BenefitMarkets
-  RSpec.describe ContributionModels::FixedPercentContributionUnit, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
