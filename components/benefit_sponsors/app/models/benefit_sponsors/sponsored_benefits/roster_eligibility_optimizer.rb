f72376ada77ab65e161414d80cbd9ea3b8ab8be4
Trey Evans <lewis.r.evans@gmail.com>
2018-04-23 14:34:07 -0400

    Extract calculator.
    Still needs to be used by pricing and contribution calculators.

--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/roster_eligibility_optimizer.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/roster_eligibility_optimizer.rb
@@ -52,8 +52,8 @@ module BenefitSponsors
 
         attr_reader :excluded_dependent_ids
 
-        def initialize(o_calc, c_model, level_map, elig_dates, c_start, r_coverage, primary_id)
-          @offered_calculator = o_calc
+        def initialize(c_model, level_map, elig_dates, c_start, r_coverage, primary_id)
+          @offered_calculator = ::BenefitSponsors::CoverageAgeCalculator.new
           @eligibility_dates = elig_dates
           @coverage_start = c_start
           @level_map = level_map
@@ -69,7 +69,7 @@ module BenefitSponsors
         end
 
         def add(member)
-          coverage_age = @offered_calculator.calc_coverage_age_for(@eligibility_dates, @coverage_start, member, @product, @previous_product)
+          coverage_age = @offered_calculator.calc_coverage_age_for(member, @product, @coverage_start, @eligibility_dates, @previous_product)
           rel_name = @contribution_model.map_relationship_for(member.relationship, coverage_age, member.is_disabled?)
           if rel_name
             @relationship_totals[rel_name.to_s] = @relationship_totals[rel_name] + 1
@@ -124,7 +124,7 @@ module BenefitSponsors
       def calculate_optimal_group_for(contribution_model, covered_roster_entry, sponsor_contribution)
         level_map = level_map_for(sponsor_contribution)
         roster_coverage = covered_roster_entry.roster_coverage
-        state = OptimizerState.new(self, contribution_model, level_map, roster_coverage.coverage_eligibility_dates, roster_coverage.coverage_start_date, roster_coverage, covered_roster_entry.member_id)
+        state = OptimizerState.new(contribution_model, level_map, roster_coverage.coverage_eligibility_dates, roster_coverage.coverage_start_date, roster_coverage, covered_roster_entry.member_id)
         member_list = [covered_roster_entry] + covered_roster_entry.dependents
         member_list.each do |member|
           state.add(member)
@@ -138,23 +138,6 @@ module BenefitSponsors
           covered_roster_entry.dependents.reject { |dep| state.excluded_dependent_ids.include?(dep.member_id) } ,
           covered_roster_entry.is_disabled?
         )
-      end
-
-      def calc_coverage_age_for(eligibility_dates, coverage_start_date, member, product, previous_product)
-        coverage_elig_date = eligibility_dates[member.member_id]
-        coverage_as_of_date = if (!previous_product.blank?) && (product.id == previous_product.id) && (!coverage_elig_date.blank?)
-                                coverage_elig_date
-                              else
-                                coverage_start_date
-                              end
-        before_factor = if (coverage_as_of_date.month < member.dob.month)
-                          -1
-                        elsif ((coverage_as_of_date.month == member.dob.month) && (coverage_as_of_date.day < member.dob.day))
-                          -1
-                        else
-                          0
-                        end
-        coverage_as_of_date.year - member.dob.year + (before_factor)
       end
 
       protected
