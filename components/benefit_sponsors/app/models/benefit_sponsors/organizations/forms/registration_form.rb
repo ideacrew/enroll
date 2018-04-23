8c400d76dbec86d652f675f5918f4af18641e297
Pranab <pranab.khanal@gmail.com>
2018-04-23 13:26:25 -0400

    typo

--- components/benefit_sponsors/app/models/benefit_sponsors/organizations/forms/registration_form.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/organizations/forms/registration_form.rb
@@ -81,7 +81,7 @@ module BenefitSponsors
       # TODO : Refactor validating sub-documents.
       def registration_form
         self.staff_roles.each do |staff_role|
-          validate_form(self.staff_role)
+          validate_form(staff_role)
         end
         validate_form(self.organization)
         self.organization.profiles.each do |profile_form|
