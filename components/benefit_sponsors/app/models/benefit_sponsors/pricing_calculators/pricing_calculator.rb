4218e5373891bada7a18acc34593d7db1ad996f0
Trey Evans <lewis.r.evans@gmail.com>
2018-04-23 15:34:52 -0400

    Factor out age calculation for pricing.

--- components/benefit_sponsors/app/models/benefit_sponsors/pricing_calculators/pricing_calculator.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/pricing_calculators/pricing_calculator.rb
@@ -1,22 +1,6 @@
 module BenefitSponsors
   module PricingCalculators
     class PricingCalculator
-        def calc_coverage_age_for(member, eligibility_dates, coverage_start_date, product, previous_product)
-          coverage_elig_date = eligibility_dates[member.member_id]
-          coverage_as_of_date = if (!previous_product.blank?) && (product.id == previous_product.id) && (!coverage_elig_date.blank?)
-                                 coverage_elig_date
-                                else
-                                  coverage_start_date
-                                end
-          before_factor = if (coverage_as_of_date.month < member.dob.month)
-            -1
-          elsif ((coverage_as_of_date.month == member.dob.month) && (coverage_as_of_date.day < member.dob.day))
-            -1
-          else
-            0
-          end
-          coverage_as_of_date.year - member.dob.year + (before_factor)
-        end
     end
   end
 end
