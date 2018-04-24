50fbc88e888f44151fe9ae295f8041fdc03ab410
harsha280 <harsha.vskr@gmail.com>
2018-04-24 10:08:12 -0400

    Refs 23321 Index Action/View for benefits market controller

--- components/benefit_markets/app/controllers/benefit_markets/benefit_markets_controller.rb
+++ components/benefit_markets/app/controllers/benefit_markets/benefit_markets_controller.rb
@@ -1,4 +1,16 @@
 module BenefitMarkets
   class BenefitMarketsController < ApplicationController
+
+  	before_filter :set_site_id, only: [:index]
+
+  	def index
+  		@benifit_markets = BenefitMarkets::BenefitMarket.where(site_id: @site_id)
+  	end
+
+  	private 
+
+  	def set_site_id
+        @site_id = params[:site_id]
+  	end
   end
 end
