322b577076bf281878c0d996ccfd81900e4173bd
Trey Evans <lewis.r.evans@gmail.com>
2018-04-30 14:52:38 -0400

    Extract MA rating areas.

--- components/benefit_markets/app/models/benefit_markets/locations/county_zip.rb
+++ components/benefit_markets/app/models/benefit_markets/locations/county_zip.rb
@@ -1,4 +1,4 @@
-module BenefitMarket
+module BenefitMarkets
   module Locations
     # A County-Zip Code pair.  Used as the building block of the geographic
     # locations covered by both service areas and rating areas.
@@ -7,10 +7,10 @@ module BenefitMarket
       include Mongoid::Timestamps
 
       field :county_name, type: String
-      field :county_code, type: String
       field :zip, type: String
       field :state, type: String
-      field :state_code, type: String
+
+      index({state: 1, county_name: 1, zip: 1})
     end
   end
 end
--- db/migrate/20180430172054_extract_county_zips_for_ma.rb
+++ db/migrate/20180430172054_extract_county_zips_for_ma.rb
@@ -1,14 +1,35 @@
 class ExtractCountyZipsForMa < Mongoid::Migration
   def self.up
     carrier_service_areas = connection["carrier_service_areas"]
+    rating_areas = connection["rating_areas"]
     if carrier_service_areas
       say_with_time("Extract counties and zips using service areas as our source") do
         carrier_service_areas.find({}).aggregate([
           {"$match" => {serves_entire_state: false}},
-          {"$group" => {_id: {zip_code: "$service_area_zipcode", county_name: "$county_name", county_code: "$county_code", active_year: "$active_year", state_code: "$state_code" }}},
-          {"$project" => {zip_code: "$_id.zip_code", county_name: "$_id.county_name", state: "MA", county_code: "$_id.county_code", active_year: 2018, state_code: "$_id.state_code", _id: 0}},
+          {"$group" => {_id: {zip_code: "$service_area_zipcode", county_name: "$county_name", active_year: "$active_year"}}},
+          {"$project" => {zip: "$_id.zip_code", county_name: "$_id.county_name", state: "MA", active_year: 2018, _id: 0}},
           {"$out" => "benefit_markets_locations_county_zips"}
         ]).each 
+      end
+    end
+    if rating_areas
+      say_with_time("Extract additional counties and zips using rating areas as our source") do
+        rating_areas.find({}).aggregate([
+          {"$group" => {_id: {zip_code: "$zip_code", county_name: "$county_name"}}},
+          {"$project" => {zip: "$_id.zip_code", county_name: "$_id.county_name", state: "MA", active_year: 2018, _id: 0}},
+        ]).each do |ra_record|
+          found_existing_record = ::BenefitMarkets::Locations::CountyZip.where({
+              county_name: ra_record['county_name'],
+              zip: ra_record['zip']
+          }).any?
+          unless found_existing_record
+            ::BenefitMarkets::Locations::CountyZip.create!({
+              county_name: ra_record['county_name'],
+              zip: ra_record['zip'],
+              state: "MA"
+            }) 
+          end
+        end
       end
     end
   end
