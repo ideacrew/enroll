a869934170ad19d6c1cb243e3bc63e67c92b9c19
Raghuram <raghuramg83@gmail.com>
2018-05-01 09:59:05 -0400

    Fixed benefit application controller specs

--- components/benefit_sponsors/spec/factories/benefit_sponsors_benefit_sponsorships_benefit_sponsorships.rb
+++ components/benefit_sponsors/spec/factories/benefit_sponsors_benefit_sponsorships_benefit_sponsorships.rb
@@ -9,7 +9,8 @@ FactoryGirl.define do
     end
 
     trait :with_organization_dc_profile do
-      # build organizationa and employer profile
+      organization { FactoryGirl.build(:benefit_sponsors_organizations_general_organization) }
+      profile { FactoryGirl.build(:benefit_sponsors_organizations_aca_shop_dc_employer_profile) }
     end
 
     trait :with_full_package do
@@ -19,18 +20,13 @@ FactoryGirl.define do
       profile { FactoryGirl.build(:benefit_sponsors_organizations_aca_shop_dc_employer_profile) }
     end
 
-   trait :with_market_profile do
-     # we have to update the factory create instead of build
-    before(:create) do |sponser_ship, evaluator|
-      sponser_ship.organization = FactoryGirl.build(:benefit_sponsors_organizations_general_organization)
-      sponser_ship.benefit_market = ::BenefitMarkets::BenefitMarket.new(kind: :aca_shop, title: "DC Health SHOP", site_urn: :dc)
-      sponser_ship.profile = FactoryGirl.build(:benefit_sponsors_organizations_aca_shop_dc_employer_profile)
+    trait :with_market_profile do
+      # we have to update the factory create instead of build
+      before(:create) do |sponsorship, evaluator|
+        sponsorship.organization = FactoryGirl.build(:benefit_sponsors_organizations_general_organization)
+        sponsorship.benefit_market = ::BenefitMarkets::BenefitMarket.new(kind: :aca_shop, title: "DC Health SHOP", site_urn: :dc)
+        sponsorship.profile = FactoryGirl.build(:benefit_sponsors_organizations_aca_shop_dc_employer_profile)
+      end
     end
-  end
-
-
-
-
-
   end
 end
