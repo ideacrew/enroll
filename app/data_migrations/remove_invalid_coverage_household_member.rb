e5527163271ab7bcd933b37483c8058cb9594d09
venkatbhattiprolu <vbhat847@gmail.com>
2018-05-02 12:04:43 -0400

    refs#23907 adding missing param

--- app/data_migrations/remove_invalid_coverage_household_member.rb
+++ app/data_migrations/remove_invalid_coverage_household_member.rb
@@ -5,11 +5,12 @@ class RemoveInvalidCoverageHouseholdMember < MongoidMigrationTask
     begin
       person = Person.where(hbx_id: ENV['person_hbx_id'])
       family_member_id = ENV['family_member_id'].to_s
+      coverage_household_member_id = ENV['coverage_household_member_id'].to_s
       ch = person.first.primary_family.active_household.coverage_households.where(:is_immediate_family => true).first
       coverage_household_member = ch.coverage_household_members.where(family_member_id: family_member_id).first
       if !coverage_household_member_id.nil? && coverage_household_member.present?
         chm = ch.remove_coverage_household_member(coverage_household_member.id, family_member_id)
-        puts "removed invalid coverage household member: #{coverage_household_member.id}" unless Rails.env.test?
+        puts "removed invalid coverage household member: #{coverage_household_member_id}" unless Rails.env.test?
       end
     rescue Exception => e
       puts e.message
