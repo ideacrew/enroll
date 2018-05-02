90c67826637cb692d31e70e9c8e362a64bcba9b2
venkatbhattiprolu <vbhat847@gmail.com>
2018-05-02 10:00:51 -0400

    refs#23907 mod to rake

--- app/data_migrations/remove_invalid_coverage_household_member.rb
+++ app/data_migrations/remove_invalid_coverage_household_member.rb
@@ -5,15 +5,11 @@ class RemoveInvalidCoverageHouseholdMember < MongoidMigrationTask
     begin
       person = Person.where(hbx_id: ENV['person_hbx_id'])
       family_member_id = ENV['family_member_id'].to_s
-      coverage_household_member_id = ENV['coverage_household_member_id'].to_s
-      action = ENV['action'].to_s
       ch = person.first.primary_family.active_household.coverage_households.where(:is_immediate_family => true).first
-      coverage_household_members = ch.coverage_household_members.where(family_member_id: family_member.id).first
-      if !coverage_household_member_id.nil? && coverage_household_members.count > 1 
-        chm = ch.remove_coverage_household_member(coverage_household_member_id, family_member.id)#needed
-        coverage_household_members = ch.coverage_household_members.where(family_member_id: family_member.id)
-        puts "removed duplicate coverage household member: #{coverage_household_member_id}" unless Rails.env.test?
-        puts "total coverage household members for the family member(#{family_member.id}) after delete: #{coverage_household_members.count}" unless Rails.env.test?
+      coverage_household_member = ch.coverage_household_members.where(family_member_id: family_member_id).first
+      if !coverage_household_member_id.nil? && coverage_household_member.present?
+        chm = ch.remove_coverage_household_member(coverage_household_member.id, family_member_id)
+        puts "removed invalid coverage household member: #{coverage_household_member.id}" unless Rails.env.test?
       end
     rescue Exception => e
       puts e.message
--- lib/tasks/migrations/remove_invalid_coverage_household_member.rake
+++ lib/tasks/migrations/remove_invalid_coverage_household_member.rake
@@ -1,5 +1,5 @@
 # Rake to remove invalid coverage household member
-# RAILS_ENV=production bundle exec rake migrations:remove_coverage_household_member person_hbx_id=19810927 family_member_id=123123123
+# RAILS_ENV=production bundle exec rake migrations:remove_coverage_household_member person_hbx_id=19892737 family_member_id="5acb6fdd082e7617bc000192"
 require File.join(Rails.root, "app", "data_migrations", "remove_invalid_coverage_household_member")
 namespace :migrations do
   desc "removefamily member from coverage household"
