68b61865ae29974c5950513ef549971573127975
Dan Thomas <dan.thomas@dc.gov>
2018-05-04 16:05:55 -0400

    WIP on products packages

--- components/benefit_markets/app/models/benefit_markets/products/product_package.rb
+++ components/benefit_markets/app/models/benefit_markets/products/product_package.rb
@@ -11,8 +11,10 @@ module BenefitMarkets
 
     embedded_in :packagable, polymorphic: true
 
-    field :key,                     type: Symbol
+    field :application_period,      type: Range
     field :hbx_id,                  type: String
+
+    field :key,                     type: Symbol
     field :title,                   type: String
     field :description,             type: String
 
@@ -20,32 +22,44 @@ module BenefitMarkets
                 class_name: "BenefitMarkets::Products::Product"
 
     embeds_one  :contribution_model, 
-                class_name: "::BenefitMarkets::ContributionModels::ContributionModel"
+                class_name: "BenefitMarkets::ContributionModels::ContributionModel"
 
     embeds_one  :pricing_model, 
-                class_name: "::BenefitMarkets::PricingModels::PricingModel"
+                class_name: "BenefitMarkets::PricingModels::PricingModel"
 
 
     validates_presence_of :title, :allow_blank => false
     validate :has_products
 
-    # TODO: Override this once the actual product implementation is available
+
+    # Implement in subclass
+    def to_sponsored_benefit
+    end
+
     # Implement in subclass
     def all_products
       Plan.where(:active_year => packagable.product_active_year, :market => packagable.product_market_kind)
     end
 
     # Implement in subclass
-    def benefit_package_kind
+    def add_product(new_product)
     end
 
-# {
-#     benefit_kind: :health, # => :health, :dental
-#     by_issuer: "urn", #|| by_metal_level: :gold,
-# }
+    def drop_product(new_product)
+      products.delete(new_product) { "not found" }
+    end
+
+    def issuer_profiles
+      return @issuer_profiles if is_defined?(@issuer_profiles)
+      @issuer_profiles = products.select { |product| product.issuer_profile }.uniq!
+    end
+
+    def all_products_for(issuer_profile)
+      products.collect { |issuer_profile| product.issuer_profile == issuer_profile }
+    end
 
     # Intersection of product that match service area and effective dates
-    def products_available_for?(service_area, effective_date)
+    def products_available_for(service_area, effective_date)
       products_available_on(effective_date) & products_available_where(service_area)
     end
 
@@ -56,6 +70,7 @@ module BenefitMarkets
     def products_available_where(service_area)
       products.collect { |product| product.service_area == service_area }
     end
+
 
 
     def self.subclass_for(benefit_option_kind)
---
+++ components/benefit_markets/spec/factories/benefit_markets_products_health_products_issuer_health_product_packages.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :benefit_markets_products_health_products_issuer_health_product_package, class: 'BenefitMarkets::Products::HealthProducts::IssuerHealthProductPackage' do
+    
+  end
+end
