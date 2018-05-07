3dfbb3eea82a519d5abf2edd8ad2fe508eb1141f
Trey Evans <lewis.r.evans@gmail.com>
2018-05-07 17:20:59 -0400

    Sample, simple example.

--- components/benefit_markets/app/models/benefit_markets/rules_engine/policy_rule.rb
+++ components/benefit_markets/app/models/benefit_markets/rules_engine/policy_rule.rb
@@ -10,10 +10,15 @@ module BenefitMarkets
       end
 
       attr_reader :requires
-      attr_reader :test
+      attr_reader :validate
       attr_reader :success
       attr_reader :failure
       attr_reader :name
+      attr_reader :any_of
+      attr_reader :all_of
+      attr_reader :is_applicable
+      attr_reader :is_parent_rule
+      attr_reader :child_rules
 
       NO_OP = lambda {|o| true }
 
@@ -21,25 +26,72 @@ module BenefitMarkets
                       requires: [],
                       validate: NO_OP,
                       failure: NO_OP,
-                      success: NO_OP
+                      success: NO_OP,
+                      any_of: nil,
+                      all_of: nil,
+                      applicable_if: NO_OP 
                     )
 
         @name = name
         @failure = failure
         @success = success
-        @validate = validate
+        @is_parent_rule = false
+        @is_applicable = applicable_if
         @requires = requires
+        if !all_of.blank?
+          @all_of = all_of
+          @child_rules = all_of
+          @is_parent_rule = true
+        elsif !any_of.blank?
+          @any_of = any_of
+          @child_rules = any_of
+          @is_parent_rule = true
+        else
+          @validate = validate
+        end
       end
 
-      def execute(context)
+      def evaluate(context)
         missing_keys = @requires - context.provided_values
         if missing_keys.any?
           raise RequiredInformationMissingError.new("required keys missing", missing_keys)
         end
-        if validate.call(context)
-          success.call(context)
-        else
-          failure.call(context)
+        if !is_applicable.call(context)
+          context.set_rule_result(name, :not_applicable)
+          return
+        end
+        if @is_parent_rule
+          if !@any_of.blank?
+            results = @any_of.map do |r_name|
+              context.rule_result(r_name)
+            end
+            end_result = results.any? { |r| r == true }
+            if end_result
+              success.call(context)
+            else
+              failure.call(context)
+            end
+            context.set_rule_result(self, end_result)
+          elsif !@all_of.blank?
+            results = @all_of.map do |r_name|
+              context.rule_result(r_name)
+            end
+            end_result = results.all? { |r| r == true }
+            if end_result
+              success.call(context)
+            else
+              failure.call(context)
+            end
+            context.set_rule_result(self, end_result)
+          end
+        else 
+          if validate.call(context)
+            success.call(context)
+            context.set_rule_result(name, true)
+          else
+            failure.call(context)
+            context.set_rule_result(name, false)
+          end
         end
       end
     end
