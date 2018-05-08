e89fa709d213960f6732116f08cbbd4cd363f6f1
Jayanth <jayanth.rordev@gmail.com>
2018-05-08 13:35:40 -0400

    refs#21929 refactor code

--- app/models/model_events/census_employee.rb
+++ app/models/model_events/census_employee.rb
@@ -2,18 +2,26 @@ module ModelEvents
   module CensusEmployee
 
     REGISTERED_EVENTS = [
-      :employee_coverage_passively_waived,
-      :employee_coverage_passively_renewed,
-      :employee_coverage_passive_renewal_failed
+      :employee_notice_for_employee_terminated_from_roster
     ]
 
     OTHER_EVENTS = [
-      :employee_notice_for_employee_terminated_from_roster
+      :employee_coverage_passively_waived,
+      :employee_coverage_passively_renewed,
+      :employee_coverage_passive_renewal_failed,
     ]
 
     def notify_on_save
       if is_transition_matching?(to: [:employment_terminated, :employee_termination_pending], from: [:eligible, :employee_role_linked, :newly_designated_eligible, :newly_designated_linked], event: [:terminate_employee_role, :schedule_employee_termination])
         is_employee_notice_for_employee_terminated_from_roster = true
+      end
+
+      REGISTERED_EVENTS.each do |event|
+        if event_fired = instance_eval("is_" + event.to_s)
+          # event_name = ("on_" + event.to_s).to_sym
+          event_options = {} # instance_eval(event.to_s + "_options") || {}
+          notify_observers(ModelEvent.new(event, self, event_options))
+        end
       end
     end
 
@@ -27,7 +35,7 @@ module ModelEvents
     end
 
     def trigger_model_event(event_name, event_options = {})
-      if REGISTERED_EVENTS.include?(event_name)
+      if OTHER_EVENTS.include?(event_name)
         notify_observers(ModelEvent.new(event_name, self, event_options))
       end
     end
--- app/models/observers/notice_observer.rb
+++ app/models/observers/notice_observer.rb
@@ -107,11 +107,11 @@ module Observers
     def census_employee_update(new_model_event)
       raise ArgumentError.new("expected ModelEvents::ModelEvent") unless new_model_event.is_a?(ModelEvents::ModelEvent)
       census_employee = new_model_event.klass_instance
-      if CensusEmployee::REGISTERED_EVENTS.include?(new_model_event.event_key)
+      if CensusEmployee::OTHER_EVENTS.include?(new_model_event.event_key)
         trigger_notice(recipient: census_employee.employee_role, event_object: new_model_event.options[:event_object], notice_event: new_model_event.event_key.to_s)
       end
 
-      if  CensusEmployee::OTHER_EVENTS.include?(new_model_event.event_key)
+      if  CensusEmployee::REGISTERED_EVENTS.include?(new_model_event.event_key)
         if new_model_event.event_key == :employee_notice_for_employee_terminated_from_roster
           trigger_notice(recipient: census_employee.employee_role, event_object: census_employee, notice_event: "employee_notice_for_employee_terminated_from_roster")
         end
