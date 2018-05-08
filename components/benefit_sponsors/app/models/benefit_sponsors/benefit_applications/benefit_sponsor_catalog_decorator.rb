38fce75c52c3a8d77401110b1275907c1b1c273d
Raghuram <raghuramg83@gmail.com>
2018-05-08 09:56:15 -0400

    Added needed filters and plan options to benefit sponsor catalog decorator

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_sponsor_catalog_decorator.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_sponsor_catalog_decorator.rb
@@ -4,7 +4,19 @@ module BenefitSponsors
 
       Product = Struct.new(:id, :title, :metal_level_kind, :carrier_name, :sole_source, :coverage_kind)
 
+      def plan_option_kinds
+        plan_options.keys
+      end
+
+      def plan_option_kind_filters
+        plan_option_kinds.inject({}) do |filters, option_kind|
+          filters[option_kind] = plan_options[option_kind].keys
+          filters
+        end
+      end
+
       def carrier_names
+        catalog.plan_options[:single_issuer].keys
         plan_options.map(&:carrier_name).uniq
       end
 
@@ -14,23 +26,33 @@ module BenefitSponsors
 
       def plan_options
         return @products if defined? @products
-        @products = []
+        @products = {}
+
         product_packages.each do |product_package|
-          @products += product_package.products.collect do |product|
+          package_products = product_package.products.collect do |product|
             Product.new(product.id, product.title, product.metal_level_kind, carriers[product.issuer_profile_id.to_s], false, product.is_a?(BenefitMarkets::Products::HealthProducts::HealthProduct) ? "health" : "dental")
           end
+          @products[product_package.kind] = case product_package.kind
+            when :single_issuer
+              package_products.group_by(&:carrier_name)
+            when :metal_level
+              package_products.group_by(&:metal_level_kind)
+            else
+              {:single_product => package_products}
+            end
         end
+
         @products
       end
 
       def carriers
         return @carriers if defined? @carriers
-        CarrierProfile.all.inject({}) {|carriers, carrier| carriers[carrier.id.to_s] = carrier.legal_name; carriers}
-      end
-
-      # TODO: calculate option kinds dynamically from products
-      def plan_option_kinds
-        ['Single Carrier', 'Metal Level', 'Single Plan']
+        issuer_orgs = BenefitSponsors::Organizations::Organization.where(:"profiles._type" => "BenefitSponsors::Organizations::IssuerProfile")
+        @carriers = issuer_orgs.inject({}) do |issuer_hash, issuer_org|
+          issuer_profile  = issuer_org.profiles.where(:"_type" => "BenefitSponsors::Organizations::IssuerProfile").first
+          issuer_hash[issuer_profile.id.to_s] = issuer_org.legal_name
+          issuer_hash
+        end
       end
 
       def carrier_level_options
