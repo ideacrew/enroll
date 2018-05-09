f37dc793ce45e94b3519453f50d9c335222939a0
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-05-09 13:02:02 -0400

    removed unused import statements

--- spec/javascript/benefit_sponsors/controllers/site_controller.spec.js
+++ spec/javascript/benefit_sponsors/controllers/site_controller.spec.js
@@ -1,24 +1,11 @@
-import {Application as StimulusApp} from 'stimulus';
-import SiteController from '../../../../app/javascript/benefit_sponsors/controllers/site_controller';
-//import puppeteer from 'puppeteer';
-import {
-  getByLabelText,
-  getByText,
-  getByTestId,
-  queryByTestId,
-  // Tip: all queries are also exposed on an object
-  // called "queries" which you could import here as well
-  wait,
-} from 'dom-testing-library';
-// adds special assertions like toHaveTextContent and toBeInTheDOM
-import 'dom-testing-library/extend-expect';
-
+import SiteController from '../../../../app/javascript/benefit_sponsors/controllers/site_controller'
 
 describe('BenefitSponsorsSiteController', () => {
   let controller;
-  
+
   beforeEach( () => {
     // This uses jsdom as it is built-in as part of Jest
+    // sets up mock html for testing purposes
     controller = new SiteController();
     let node = document.createElement('div');
     node.innerHTML = `
@@ -37,7 +24,7 @@ describe('BenefitSponsorsSiteController', () => {
 
   describe('#addLocation', () => {
     it('unhides the remove button', () => {
-      
+
       // replaces acually clicking he buon in he UI
       const testDiv = document.getElementById("test");
       expect(testDiv.getAttribute("class")).toEqual('row d-none js-non-primary');
