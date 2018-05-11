4947469490958e4426f62cca51e6c0f8c2e3729d
Dan Thomas <dan.thomas@dc.gov>
2018-05-11 08:20:26 -0400

    Update comments

--- components/benefit_markets/app/models/benefit_markets/market_policies/member_market_policy.rb
+++ components/benefit_markets/app/models/benefit_markets/market_policies/member_market_policy.rb
@@ -1,16 +1,20 @@
 module BenefitMarkets
   class MarketPolicies::MemberMarketPolicy < MarketPolicies::MarketPolicy
-    
+
+    # DC SHOP
+      # Congress members
+      # 'newly designated' bypass probation period
+
     # DC Individual
-    # age-off 26, 65
-    # catastrophic plans - enrollment group must be < 30
-    ## See existing rules
+      # age-off 26, 65
+      # catastrophic plans - enrollment group must be < 30
+      ## See existing rules
 
     # CCA SHOP
-    # access frozen plans if member enrolled in last year's mapped plan == true
+      # access frozen plans if member enrolled in last year's mapped plan == true
 
-    # GIC 
-    # must purchase life to purchase health
+    # GIC
+      # must purchase life to purchase health
 
     field :age_range_policy,              type: Range,   default: 0..0
     field :child_age_off_policy,          type: Integer, default: 26
