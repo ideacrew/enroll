2fb3f1cb0e6116fd7307d3eeaac0112f30cddc17
Dan Thomas <dan.thomas@dc.gov>
2018-05-11 08:19:30 -0400

    Remove BenefitApplication subclasses

--- components/benefit_sponsors/spec/factories/benefit_sponsors_benefit_applications_exempt_dc_benefit_applications.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :benefit_sponsors_benefit_applications_exempt_dc_benefit_application, class: 'BenefitSponsors::BenefitApplications::ExemptDcBenefitApplication' do
-    
-  end
-end
