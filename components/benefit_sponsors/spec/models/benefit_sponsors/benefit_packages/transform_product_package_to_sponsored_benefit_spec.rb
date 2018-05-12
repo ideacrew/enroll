d7c1b910d415f3d1ffae50e5f8cddffe14e1d48a
Dan Thomas <dan.thomas@dc.gov>
2018-05-12 18:29:21 -0400

    Clear cruft

--- components/benefit_sponsors/spec/models/benefit_sponsors/benefit_packages/transform_product_package_to_sponsored_benefit_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module BenefitSponsors
-  RSpec.describe BenefitPackages::TransformProductPackageToSponsoredBenefit, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
