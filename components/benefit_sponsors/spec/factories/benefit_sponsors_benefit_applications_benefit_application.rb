7bda84251bba96ab63fec90ae978bac1685f7c78
Raghuram <raghuramg83@gmail.com>
2018-05-13 01:52:49 -0400

    Added factories to instantiate benefit packages with sponsored benefits

--- components/benefit_sponsors/spec/factories/benefit_sponsors_benefit_applications_benefit_application.rb
+++ components/benefit_sponsors/spec/factories/benefit_sponsors_benefit_applications_benefit_application.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :benefit_sponsors_benefit_applications, class: 'BenefitSponsors::BenefitApplications::BenefitApplication' do
-    benefit_sponsorship { FactoryGirl.create(:benefit_sponsors_benefit_sponsorship, :with_full_package)}
+    benefit_sponsorship { create(:benefit_sponsors_benefit_sponsorship, :with_full_package)}
 
     # design using defining module spec helpers
     effective_period do
@@ -8,6 +8,7 @@ FactoryGirl.define do
       effective_period_end_on = effective_period_start_on + 1.year - 1.day
       effective_period_start_on..effective_period_end_on
     end
+
     open_enrollment_period do
       effective_period_start_on = TimeKeeper.date_of_record.end_of_month + 1.day + 1.month
       open_enrollment_period_start_on = effective_period_start_on.prev_month
