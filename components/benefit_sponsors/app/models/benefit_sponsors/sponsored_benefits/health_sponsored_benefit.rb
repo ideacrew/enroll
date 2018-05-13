d56da4d71cde0d7f7bf4e4579b1f5a54f4ec7318
Raghuram <raghuramg83@gmail.com>
2018-05-13 01:49:38 -0400

    Added changes for benefit packages and sponsored benefits

--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/health_sponsored_benefit.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/health_sponsored_benefit.rb
@@ -2,6 +2,17 @@ module BenefitSponsors
   module SponsoredBenefits
     class HealthSponsoredBenefit < SponsoredBenefit
 
+
+      FILTER_MAP =  { 
+                      issuer_profiles: -> { products.issuer_profiles },
+                      # metal_level_kinds: BenefitMarkets::Products::HealthProducts::HealthProduct::METAL_LEVEL_KINDS,
+                      health_plan_kinds: BenefitMarkets::Products::HealthProducts::HealthProduct::HEALTH_PLAN_MAP.keys,
+                    }
+
+      embeds_one  :reference_product, 
+                  class_name: "::BenefitMarkets::Products::HealthProducts::HealthProduct"
+      embeds_many :products, 
+                  class_name: "::BenefitMarkets::Products::HealthProducts::HealthProduct"
     end
   end
 end
--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/sponsored_benefit.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/sponsored_benefit.rb
@@ -4,25 +4,17 @@ module BenefitSponsors
       include Mongoid::Document
       include Mongoid::Timestamps
 
-      FILTER_MAP =  { 
-                      issuer_profiles: -> { products.issuer_profiles },
-                      # metal_level_kinds: BenefitMarkets::Products::HealthProducts::HealthProduct::METAL_LEVEL_KINDS,
-                      health_plan_kinds: BenefitMarkets::Products::HealthProducts::HealthProduct::HEALTH_PLAN_MAP.keys,
-                    }
+      embedded_in :benefit_package, 
+                  class_name: "BenefitSponsors::BenefitPackages::BenefitPackage"
 
-      embedded_in :benefit_package, class_name: "BenefitSponsors::BenefitPackages::BenefitPackage"
+      field :product_package_kind, type: Symbol
 
-      field :hbx_id,      type: String
-      field :kind,        type: Symbol
+      # field :plan_option_choice , type: String # carrier id / metal level
 
-      field :plan_option_kind, type: String
-      field :plan_option_choice , type: String
-
-      embeds_many :products, class_name: "::BenefitMarkets::Products::Product"
-      embeds_one  :reference_product, class_name: "::BenefitMarkets::Products::Product"
-
-      embeds_one  :sponsor_contribution, class_name: "::BenefitSponsors::SponsoredBenefits::SponsorContribution"
-      embeds_many :pricing_determinations, class_name: "::BenefitSponsors::SponsoredBenefits::PricingDetermination"
+      embeds_one  :sponsor_contribution, 
+                  class_name: "::BenefitSponsors::SponsoredBenefits::SponsorContribution"
+      embeds_many :pricing_determinations, 
+                  class_name: "::BenefitSponsors::SponsoredBenefits::PricingDetermination"
 
       delegate :benefit_sponsor_catalog, to: :benefit_package
 
@@ -30,6 +22,15 @@ module BenefitSponsors
         pricing_determinations.sort_by(&:created_at).last
       end
 
+      def renew(new_product_package)
+        #   map products
+        #   map reference product
+        #   build sponsor contribution model
+        #   DO NOT COPY pricing determinations        
+      end
+
+      # plan_option_kind & plan_option_choice
+      # Gold/silver/Carefirst/Kaiser
       def plan_option_choice=(choice)
         return if choice.blank?
         self.products = benefit_sponsor_catalog.products_for(plan_option_kind, choice)
