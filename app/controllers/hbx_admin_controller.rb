9567e25682ce19434553db8d80ddfc5ffa612a7b
sudhirreddysama <sudhirsama586@gmail.com>
2018-05-14 09:42:06 -0400

    Revert "Updating available APTC value"
    This reverts commit 6cc690b827e307bce542387f0054350e9380af95.

--- app/controllers/hbx_admin_controller.rb
+++ app/controllers/hbx_admin_controller.rb
@@ -9,12 +9,12 @@ class HbxAdminController < ApplicationController
     @hbxs = @family.active_household.hbx_enrollments.enrolled_and_renewing.by_year(@current_year).by_coverage_kind("health")
     @slcsp_value = Admin::Aptc.calculate_slcsp_value(@current_year, @family)
     @household_members = Admin::Aptc.build_household_members(@current_year, @family)
-    @active_tax_household_for_current_year = @family.active_household.latest_active_tax_household_with_year(@current_year)
-    @max_aptc = @active_tax_household_for_current_year.try(:latest_eligibility_determination).try(:max_aptc) || 0
-    @household_info = Admin::Aptc.build_household_level_aptc_csr_data(@current_year, @family, @hbxs, @max_aptc.to_f)
+    @household_info = Admin::Aptc.build_household_level_aptc_csr_data(@current_year, @family, @hbxs)
     @enrollments_info = Admin::Aptc.build_enrollments_data(@current_year, @family, @hbxs) if @hbxs.present?
     @current_aptc_applied_hash =  Admin::Aptc.build_current_aptc_applied_hash(@hbxs)
     @plan_premium_for_enrollments = Admin::Aptc.build_plan_premium_hash_for_enrollments(@hbxs)
+    @active_tax_household_for_current_year = @family.active_household.latest_active_tax_household_with_year(@current_year)
+    @max_aptc = @active_tax_household_for_current_year.try(:latest_eligibility_determination).try(:max_aptc) || 0
     @csr_percent_as_integer = @active_tax_household_for_current_year.try(:latest_eligibility_determination).try(:csr_percent_as_integer) || 0
     @year_options = Admin::Aptc::years_with_tax_household(@family)
     respond_to do |format|
