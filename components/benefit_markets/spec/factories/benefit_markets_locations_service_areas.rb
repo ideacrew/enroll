7b53f05ef1120a38e84229e825263a2af5e9e4a7
Trey Evans <lewis.r.evans@gmail.com>
2018-05-15 15:11:53 -0400

    Fixing factories to be valid.

--- components/benefit_markets/spec/factories/benefit_markets_locations_service_areas.rb
+++ components/benefit_markets/spec/factories/benefit_markets_locations_service_areas.rb
@@ -4,8 +4,9 @@ FactoryGirl.define do
     active_year TimeKeeper.date_of_record.year
     issuer_provided_code "issuer name"
     issuer_id BenefitSponsors::Organizations::IssuerProfile.new.id
+    # Both of these would never happen at the same time
     county_zip_ids [FactoryGirl.create(:benefit_markets_locations_county_zip).id.to_s]
-    covered_states ["MA"]
+    # covered_states ["MA"]
 
   end
 end
