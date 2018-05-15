849d3763a7b0c1c17e99b15425e60598454ca250
Dan Thomas <dan.thomas@dc.gov>
2018-05-15 08:00:22 -0400

    initial commit

---
+++ components/benefit_sponsors/spec/models/benefit_sponsors/benefit_applications/benefit_application_service_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module BenefitSponsors
+  RSpec.describe BenefitApplications::BenefitApplicationService, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
