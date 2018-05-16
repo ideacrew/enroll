70e2d96729873dd4eaeababbac6e5e1c6f5a1946
Pranab <pranab.khanal@gmail.com>
2018-05-16 16:42:09 -0400

    re-comment this; causing other main app spec to fail

--- components/benefit_sponsors/app/models/benefit_sponsors/organizations/profile.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/organizations/profile.rb
@@ -55,10 +55,9 @@ module BenefitSponsors
       #   @benefit_sponsorship = benefit_sponsorship
       # end
 
-      # spec looks for validation. uncommenting the validation below...
-      validates :contact_method,
-        inclusion: { in: ::BenefitMarkets::CONTACT_METHOD_KINDS, message: "%{value} is not a valid contact method" },
-        allow_blank: false
+      # validates :contact_method,
+      #   inclusion: { in: ::BenefitMarkets::CONTACT_METHOD_KINDS, message: "%{value} is not a valid contact method" },
+      #   allow_blank: false
 
       def primary_office_location
         office_locations.detect(&:is_primary?)
