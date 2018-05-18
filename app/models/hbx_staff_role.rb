208c5c5d2249fc6a5b5813fa5f489401273b14ea
saimekala07 <saimekala07@gmail.com>
2018-05-18 15:17:41 -0400

    Fixing hbx_staff_role model

--- app/models/hbx_staff_role.rb
+++ app/models/hbx_staff_role.rb
@@ -6,6 +6,7 @@ class HbxStaffRole
   embedded_in :person
 
   field :hbx_profile_id, type: BSON::ObjectId
+  field :benefit_sponsor_hbx_profile_id, type: BSON::ObjectId
   field :job_title, type: String, default: ""
   field :department, type: String, default: ""
   field :is_active, type: Boolean, default: true
@@ -15,7 +16,8 @@ class HbxStaffRole
 
   accepts_nested_attributes_for :person
 
-  validates_presence_of :hbx_profile_id
+  validates_presence_of :hbx_profile_id, :if => Proc.new { |m| m.benefit_sponsor_hbx_profile_id.blank? }
+  validates_presence_of :benefit_sponsor_hbx_profile_id , :if => Proc.new { |m| m.hbx_profile_id.blank? }
 
   alias_method :is_active?, :is_active
   #subrole is for documentation. should be redundant with permission_id
@@ -33,14 +35,22 @@ class HbxStaffRole
 
   # belongs_to Hbx
   def hbx_profile=(new_hbx_profile)
-    raise ArgumentError.new("expected HbxProfile") unless new_hbx_profile.is_a? HbxProfile
-    self.hbx_profile_id = new_hbx_profile._id
+    raise ArgumentError.new("expected HbxProfile") unless (new_hbx_profile.is_a? BenefitSponsors::Organizations::HbxProfile) || (new_hbx_profile.is_a? HbxProfile)
+    if new_hbx_profile.is_a? BenefitSponsors::Organizations::HbxProfile
+      self.benefit_sponsor_hbx_profile_id = new_hbx_profile._id
+    else
+      self.hbx_profile_id = new_hbx_profile._id
+    end
     @hbx_profile = new_hbx_profile
   end
 
   def hbx_profile
     return @hbx_profile if defined? @hbx_profile
-    @hbx_profile = HbxProfile.find(self.hbx_profile_id)
+    if self.benefit_sponsor_hbx_profile_id.present?
+      @hbx_profile = BenefitSponsors::Organizations::HbxProfile.find(self.benefit_sponsor_hbx_profile_id)
+    else
+      @hbx_profile = HbxProfile.find(self.hbx_profile_id)
+    end
   end
 
   def parent
