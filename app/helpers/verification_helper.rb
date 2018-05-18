252e2b810c69df88b116601536e8b4b0e754181f
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2018-05-18 15:24:28 -0400

    Refs #23025 admin actions update to fix ridp.

--- app/helpers/verification_helper.rb
+++ app/helpers/verification_helper.rb
@@ -167,7 +167,7 @@ module VerificationHelper
   def build_admin_actions_list(v_type, f_member)
     if f_member.consumer_role.aasm_state == 'unverified'
       ::VlpDocument::ADMIN_VERIFICATION_ACTIONS.reject{ |el| el == 'Call HUB' }
-    elsif v_type.validation_status == 'outstanding'
+    elsif verification_type_status(v_type, f_member) == 'outstanding'
       ::VlpDocument::ADMIN_VERIFICATION_ACTIONS.reject{|el| el == "Reject" }
     else
       ::VlpDocument::ADMIN_VERIFICATION_ACTIONS
