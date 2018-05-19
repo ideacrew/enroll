fdd4bb486b1318199f092cec770931ff24cb1a75
Dan Thomas <dan.thomas@dc.gov>
2018-05-19 14:00:35 -0400

    Add Compare Products, Compare Premium Tables, Compare PremiumTuples

--- components/benefit_markets/app/models/benefit_markets/products/product.rb
+++ components/benefit_markets/app/models/benefit_markets/products/product.rb
@@ -10,6 +10,8 @@ module BenefitMarkets
   class Products::Product
     include Mongoid::Document
     include Mongoid::Timestamps
+    include Comparable
+
 
     field :benefit_market_kind,   type: Symbol
 
@@ -43,15 +45,15 @@ module BenefitMarkets
 
 
     index({ hbx_id: 1 })
-    index({ "benefit_market_kind" => 1, 
-            "application_period.min" => 1, 
-            "application_period.max" => 1, 
-            "product_package_kinds" => 1, 
+    index({ "benefit_market_kind" => 1,
+            "application_period.min" => 1,
+            "application_period.max" => 1,
+            "product_package_kinds" => 1,
             "_type" => 1 },
             {name: "product_package"})
 
-    index({ "premium_tables.rating_area" => 1, 
-            "premium_tables.effective_period.min" => 1, 
+    index({ "premium_tables.rating_area" => 1,
+            "premium_tables.effective_period.min" => 1,
             "premium_tables.effective_period.max" => 1 },
             {name: "premium_tables"})
 
@@ -71,9 +73,31 @@ module BenefitMarkets
 
     scope :by_metal_level_kind,   ->(metal_level){ where(metal_level_kind: /#{metal_level}/i) }
     scope :by_issuer_profile,     ->(issuer_profile){ where(issuer_profile_id: issuer_profile.id) }
-    
+
     scope :by_application_period, ->(application_period){ where(:application_period => application_period) }
     scope :effective_with_premiums_on,  ->(effective_date){ where(:"premium_tables.effective_period.min".lte => effective_date, :"premium_tables.effective_period.max".gte => effective_date) }
+
+
+    def comparable_attrs
+      [
+        :hbx_id, :benefit_market_kind, :application_period, :title, :description,
+        :issuer_profile_id, :service_area,
+        ]
+    end
+
+    # Define Comparable operator
+    # If instance attributes are the same, compare PremiumTables
+    def <=>(other)
+      if comparable_attrs.all? { |attr| eval(attr.to_s) == eval("other.#{attr.to_s}") }
+        if premium_tables == other.premium_tables
+          0
+        else
+          premium_tables <=> other.premium_tables
+        end
+      else
+        other.updated_at.blank? || (updated_at < other.updated_at) ? -1 : 1
+      end
+    end
 
     def issuer_profile
       return @issuer_profile if defined?(@issuer_profile)
@@ -90,11 +114,11 @@ module BenefitMarkets
     end
 
     # Add premium table, covering extended time period, to existing product.  Used for products that
-    # have periodic rate changes, such as ACA SHOP products that are updated quarterly.  
+    # have periodic rate changes, such as ACA SHOP products that are updated quarterly.
     def add_premium_table(new_premium_table)
       raise InvalidEffectivePeriodError unless is_valid_premium_table_effective_period?(new_premium_table)
 
-      if premium_table_effective_on(new_premium_table.effective_period.min).present? || 
+      if premium_table_effective_on(new_premium_table.effective_period.min).present? ||
           premium_table_effective_on(new_premium_table.effective_period.max).present?
         raise DuplicatePremiumTableError, "effective_period may not overlap existing premium_table"
       else
@@ -117,7 +141,7 @@ module BenefitMarkets
     def is_valid_premium_table_effective_period?(compare_premium_table)
       return false unless application_period.present? && compare_premium_table.effective_period.present?
 
-      if application_period.cover?(compare_premium_table.effective_period.min) && 
+      if application_period.cover?(compare_premium_table.effective_period.min) &&
           application_period.cover?(compare_premium_table.effective_period.max)
         true
       else
