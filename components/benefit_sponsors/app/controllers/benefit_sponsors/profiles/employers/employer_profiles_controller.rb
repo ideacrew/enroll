a2e7a2e7d71689e02a47f7aaf4dd24dc5808d3db
Trey Evans <lewis.r.evans@gmail.com>
2018-05-21 10:50:17 -0400

    Attempting to fix constant loading.

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller.rb
@@ -1,25 +1,26 @@
 module BenefitSponsors
   module Profiles
-    class Employers::EmployerProfilesController < ApplicationController
+    module Employers
+      class EmployerProfilesController < ::BenefitSponsors::ApplicationController
 
-      before_action :find_employer, only: [:show, :bulk_employee_upload]
+        before_action :find_employer, only: [:show, :bulk_employee_upload]
 
-      #New person registered with existing organization and approval request submitted to employer
-      def show_pending
-        authorize [:benefit_sponsors, :employer_profile]
-        respond_to do |format|
-          format.html
-          format.js
+        #New person registered with existing organization and approval request submitted to employer
+        def show_pending
+          authorize [:benefit_sponsors, :employer_profile]
+          respond_to do |format|
+            format.html
+            format.js
+          end
         end
-      end
 
-      def show
-        authorize @employer_profile
-        @tab = params['tab']
-        if params[:q] || params[:page] || params[:commit] || params[:status]
-          # paginate_employees
-        else
-          case @tab
+        def show
+          authorize @employer_profile
+          @tab = params['tab']
+          if params[:q] || params[:page] || params[:commit] || params[:status]
+            # paginate_employees
+          else
+            case @tab
             when 'benefits'
               @benefit_applications = @employer_profile.benefit_applications
             when 'documents'
@@ -42,49 +43,50 @@ module BenefitSponsors
                 format.html
                 format.js
               end
+            end
           end
         end
-      end
 
-      def export_census_employees
-        respond_to do |format|
-          format.csv { send_data @employer_profile.census_employees.sorted.to_csv, filename: "#{@employer_profile.legal_name.parameterize.underscore}_census_employees_#{TimeKeeper.date_of_record}.csv" }
-        end
-      end
-
-      def bulk_employee_upload
-        authorize @employer_profile, :show?
-        file = params.require(:file)
-        @roster_upload_form = BenefitSponsors::Forms::RosterUploadForm.call(file, @employer_profile)
-        begin
-          if @roster_upload_form.save
-            redirect_to @roster_upload_form.redirection_url
-          else
-            render @roster_upload_form.redirection_url
+        def export_census_employees
+          respond_to do |format|
+            format.csv { send_data @employer_profile.census_employees.sorted.to_csv, filename: "#{@employer_profile.legal_name.parameterize.underscore}_census_employees_#{TimeKeeper.date_of_record}.csv" }
           end
-        rescue Exception => e
-          @roster_upload_form.errors.add(:base, e.message)
-          render (@roster_upload_form.redirection_url || default_url)
         end
-      end
 
-      def inbox
-        @folder = params[:folder] || 'Inbox'
-        @sent_box = false
-      end
+        def bulk_employee_upload
+          authorize @employer_profile, :show?
+          file = params.require(:file)
+          @roster_upload_form = BenefitSponsors::Forms::RosterUploadForm.call(file, @employer_profile)
+          begin
+            if @roster_upload_form.save
+              redirect_to @roster_upload_form.redirection_url
+            else
+              render @roster_upload_form.redirection_url
+            end
+          rescue Exception => e
+            @roster_upload_form.errors.add(:base, e.message)
+            render (@roster_upload_form.redirection_url || default_url)
+          end
+        end
 
-      private
+        def inbox
+          @folder = params[:folder] || 'Inbox'
+          @sent_box = false
+        end
 
-      def find_employer
-        id_params = params.permit(:id, :employer_profile_id)
-        id = id_params[:id] || id_params[:employer_profile_id]
-        @organization = BenefitSponsors::Organizations::Organization.employer_profiles.where(:"profiles._id" => BSON::ObjectId.from_string(id)).first
-        @employer_profile = @organization.employer_profile
-        render file: 'public/404.html', status: 404 if @employer_profile.blank?
-      end
+        private
 
-      def default_url
-        "employers/employer_profiles/employee_csv_upload_errors"
+        def find_employer
+          id_params = params.permit(:id, :employer_profile_id)
+          id = id_params[:id] || id_params[:employer_profile_id]
+          @organization = BenefitSponsors::Organizations::Organization.employer_profiles.where(:"profiles._id" => BSON::ObjectId.from_string(id)).first
+          @employer_profile = @organization.employer_profile
+          render file: 'public/404.html', status: 404 if @employer_profile.blank?
+        end
+
+        def default_url
+          "employers/employer_profiles/employee_csv_upload_errors"
+        end
       end
     end
   end
