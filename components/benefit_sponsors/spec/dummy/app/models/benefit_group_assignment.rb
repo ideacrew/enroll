187c87e2de509e0e622edb96c40b0e5233ee8f7f
Raghuram <raghuramg83@gmail.com>
2018-05-22 16:27:17 -0400

    Adde census employees and related models, factories to benefit_sponsors/dummy

---
+++ components/benefit_sponsors/spec/dummy/app/models/benefit_group_assignment.rb
@@ -0,0 +1,31 @@
+class BenefitGroupAssignment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include AASM
+
+  embedded_in :census_employee
+
+  field :benefit_group_id, type: BSON::ObjectId
+  field :benefit_package_id, type: BSON::ObjectId # Engine Benefit Package
+
+  # Represents the most recent completed enrollment
+  field :hbx_enrollment_id, type: BSON::ObjectId
+
+  field :start_on, type: Date
+  field :end_on, type: Date
+  field :coverage_end_on, type: Date
+  field :aasm_state, type: String, default: "initialized"
+  field :is_active, type: Boolean, default: true
+  field :activated_at, type: DateTime
+
+  aasm do
+    state :initialized, initial: true
+    state :coverage_selected
+    state :coverage_waived
+    state :coverage_terminated
+    state :coverage_void
+    state :coverage_renewing
+    state :coverage_expired
+
+  end
+end
---
+++ components/benefit_sponsors/spec/dummy/spec/factories/benefit_group_assignments.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :benefit_group_assignment, class: 'BenefitGroupAssignment' do
+    benefit_group
+    start_on { benefit_group.plan_year.start_on }
+    is_active true
+
+    trait :coverage_selected do
+      hbx_enrollment
+      aasm_state "coverage_selected"
+    end
+  end
+end
