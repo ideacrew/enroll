dbf226ce3e2fdc30e13ff213f6da0cf75cd32a1b
Trey Evans <lewis.r.evans@gmail.com>
2018-05-22 15:57:36 -0400

    Adding seeds for locations.

--- db/seedfiles/cca/cca_seed.rb
+++ db/seedfiles/cca/cca_seed.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "site_seed"))
 require File.expand_path(File.join(File.dirname(__FILE__), "issuer_profiles_seed"))
 require File.expand_path(File.join(File.dirname(__FILE__), "benefit_markets_seed"))
+require File.expand_path(File.join(File.dirname(__FILE__), "locations_seed"))
 Mongoid::Migration.say_with_time("Loading MA Contribution and Pricing Models") do
   require File.expand_path(File.join(File.dirname(__FILE__),'..', 'ma_employer_pricing_and_contribution_models'))
 end
