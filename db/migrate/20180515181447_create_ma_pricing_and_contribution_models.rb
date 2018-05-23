3a5d781e623ae4c221c397869884dc1577fcc18c
Trey Evans <lewis.r.evans@gmail.com>
2018-05-23 12:34:05 -0400

    No extra announcments.

--- db/migrate/20180515181447_create_ma_pricing_and_contribution_models.rb
+++ db/migrate/20180515181447_create_ma_pricing_and_contribution_models.rb
@@ -1,9 +1,7 @@
 class CreateMaPricingAndContributionModels < Mongoid::Migration
   def self.up
     if Settings.site.key.to_s == "cca"
-      say_with_time("Loading MA pricing and contribution models") do
-        require File.expand_path(File.join(Rails.root, "db/seedfiles/cca/pricing_and_contribution_models_seed"))
-      end
+      require File.expand_path(File.join(Rails.root, "db/seedfiles/cca/pricing_and_contribution_models_seed"))
     else
       say("Skipping migration for non-MHC site")
     end
