d6e7ad709249c9fbb1b870454b1a79f635cac594
Dan Thomas <dan.thomas@dc.gov>
2018-05-28 10:39:16 -0400

    Add stub for BenefitMarkets engine seed file

---
+++ components/benefit_markets/db/seeds.rb
@@ -0,0 +1,10 @@
+## Populate namespaced seed content in this file
+## To run:
+##  1) In spec/dummy/db folder, create seed.rb file with following: BenefitMarkets::Engine.load_seed
+##  2) In terminal, chdir to spec/dummy, run rake db:seed
+
+puts "*"*80
+puts "Start of Engine BenefitMarkets seed"
+puts "..."
+puts "End of Engine BenefitMarkets seed"
+puts "*"*80
