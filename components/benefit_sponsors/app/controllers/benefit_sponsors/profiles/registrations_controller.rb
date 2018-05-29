82d959b949527cdb844359b5a174876b7f03b47e
202chaz <chaz.haskins@gmail.com>
2018-05-29 10:21:13 -0400

    Business Info page updates

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/registrations_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/registrations_controller.rb
@@ -8,7 +8,7 @@ module BenefitSponsors
 
       rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-      layout 'single_column', :only => :edit
+      layout 'two_column', :only => :edit
 
       def new
         @agency = BenefitSponsors::Organizations::OrganizationForms::RegistrationForm.for_new(profile_type: profile_type)
