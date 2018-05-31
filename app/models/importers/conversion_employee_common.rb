b82f37205f13b9bd6703fb5dfb13f2e83426e08d
Prem Shaganti <prem.shaganti@dc.gov>
2018-05-31 14:46:39 -0400

    ref: add census employee

--- app/models/importers/conversion_employee_common.rb
+++ app/models/importers/conversion_employee_common.rb
@@ -111,22 +111,22 @@ module Importers
       RUBYCODE
     end
 
-    def find_employee
-      return @found_employee unless @found_employee.nil?
-      return nil if subscriber_ssn.blank?
-      found_employer = find_employer
-      return nil if found_employer.nil?
-      candidate_employees = CensusEmployee.where({
-                                                     employer_profile_id: found_employer.id,
-                                                     # hired_on: {"$lte" => start_date},
-                                                     encrypted_ssn: CensusMember.encrypt_ssn(subscriber_ssn)
-                                                 })
-      non_terminated_employees = candidate_employees.reject do |ce|
-        (!ce.employment_terminated_on.blank?) && ce.employment_terminated_on <= Date.today
-      end
-
-      @found_employee = non_terminated_employees.sort_by(&:hired_on).last
-    end
+    # def find_employee
+    #   return @found_employee unless @found_employee.nil?
+    #   return nil if subscriber_ssn.blank?
+    #   found_employer = find_employer
+    #   return nil if found_employer.nil?
+    #   candidate_employees = CensusEmployee.where({
+    #                                                  employer_profile_id: found_employer.id,
+    #                                                  # hired_on: {"$lte" => start_date},
+    #                                                  encrypted_ssn: CensusMember.encrypt_ssn(subscriber_ssn)
+    #                                              })
+    #   non_terminated_employees = candidate_employees.reject do |ce|
+    #     (!ce.employment_terminated_on.blank?) && ce.employment_terminated_on <= Date.today
+    #   end
+    #
+    #   @found_employee = non_terminated_employees.sort_by(&:hired_on).last
+    # end
 
   end
 end
