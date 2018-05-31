6845ef339757d0106ed7b8354289972e62e70461
venkatbhattiprolu <vbhat847@gmail.com>
2018-05-31 14:49:46 -0400

    refs#24103 removing specific plans from er plan offerings based on caiier profile id

---
+++ app/data_migrations/remove_plan_offerings_for_er.rb
@@ -0,0 +1,17 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class RemovePlanOfferings< MongoidMigrationTask
+  def migrate
+    organizations = Organization.where(fein: ENV['fein'])
+    aasm_state = ENV['aasm_state']
+    given_carrier_profile_id = ENV['carrier_profile_id'].to_s
+    if organizations.size!= 1
+      raise "issues with given fein"
+    end
+    carrier_profile_ids = organizations.first.employer_profile.plan_years.where(aasm_state: aasm_state).first.benefit_groups.first.elected_plans.select{|p| p.carrier_profile_id == given_carrier_profile_id}.each(&:destroy)
+    puts "removed the carrier from employer level offerings" unless Rails.env.test?
+  end
+end
+
+
+
