b1e554c1cb994c87a86296877047009759eaa8f6
Raghuram <raghuramg83@gmail.com>
2018-06-01 12:51:54 -0400

    removed duplicate methods from conversion policy script and added new setting for original begin date of mid year conversion application

--- components/benefit_sponsors/app/models/benefit_sponsors/importers/conversion_employee_policy_action.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/importers/conversion_employee_policy_action.rb
@@ -1,45 +1,6 @@
 module BenefitSponsors
   module Importers
-    class ConversionEmployeePolicyAction < ::Importers::ConversionEmployeePolicyCommon
-
-      # validate :validate_benefit_group_assignment
-      validate :validate_census_employee
-      validate :validate_fein
-      validate :validate_plan
-      validates_length_of :fein, is: 9
-      validates_length_of :subscriber_ssn, is: 9
-      validates_presence_of :hios_id
-
-      def initialize(opts = {})
-        super(opts)
-        @original_attributes = opts
-      end
-
-      def validate_fein
-        return true if fein.blank?
-        found_employer = find_employer
-        if found_employer.nil?
-          errors.add(:fein, "does not exist")
-        end
-      end
-
-      def validate_census_employee
-        return true if subscriber_ssn.blank?
-        found_employee = find_employee
-        if found_employee.nil?
-          errors.add(:subscriber_ssn, "no census employee found")
-        end
-      end
-
-      def validate_benefit_group_assignment
-        return true if subscriber_ssn.blank?
-        found_employee = find_employee
-        return true unless find_employee
-        found_bga = find_benefit_group_assignment
-        if found_bga.nil?
-          errors.add(:subscriber_ssn, "no benefit group assignment found")
-        end
-      end
+    class ConversionEmployeePolicyAction < ::Importers::ConversionEmployeePolicyAction
 
       def find_benefit_group_assignment
         return @found_benefit_group_assignment unless @found_benefit_group_assignment.nil?
@@ -51,16 +12,8 @@ module BenefitSponsors
         # try to fetch benefit_applciation which were active
 
         if benefit_application
-          candidate_bgas = census_employee.benefit_group_assignments.where(:"benefit_group_id".in  => benefit_application.benefit_packages.map(&:id))
+          candidate_bgas = census_employee.benefit_group_assignments.where(:"benefit_package_id".in  => benefit_application.benefit_packages.map(&:id))
           @found_benefit_group_assignment = candidate_bgas.sort_by(&:start_on).last
-        end
-      end
-
-      def validate_plan
-        return true if hios_id.blank?
-        found_plan = find_plan
-        if found_plan.nil?
-          errors.add(:hios_id, "no plan found with hios_id #{hios_id} and active year #{plan_year}")
         end
       end
 
@@ -68,14 +21,13 @@ module BenefitSponsors
         return @found_employee unless @found_employee.nil?
         return nil if subscriber_ssn.blank?
         found_employer = find_employer
-        benefit_sponsorship = find_sponsorship
         return nil if found_employer.nil?
+        benefit_sponsorship = found_employer.active_benefit_sponsorship
         candidate_employees = CensusEmployee.where({
-                                                       employer_profile_id: found_employer.id,
+                                                       benefit_sponsors_employer_profile_id: found_employer.id,
                                                        benefit_sponsorship_id: benefit_sponsorship.id,
-
                                                        # hired_on: {"$lte" => start_date},
-                                                       # encrypted_ssn: CensusMember.encrypt_ssn(subscriber_ssn)
+                                                       encrypted_ssn: CensusMember.encrypt_ssn(subscriber_ssn)
                                                    })
         non_terminated_employees = candidate_employees.reject do |ce|
           (!ce.employment_terminated_on.blank?) && ce.employment_terminated_on <= Date.today
@@ -84,19 +36,13 @@ module BenefitSponsors
         @found_employee = non_terminated_employees.sort_by(&:hired_on).last
       end
 
-      def find_sponsorship
-        return @sponsorship unless @sponsorship.nil?
-        org = BenefitSponsors::Organizations::Organization.where(:fein => fein).first
-        return nil if org.nil?
-        org.active_benefit_sponsorship
-      end
-
+      # TODO: Fix below method to fetch product from sponsored_benefit#product_package#products
       def find_plan
         return @plan unless @plan.nil?
         return nil if hios_id.blank?
         clean_hios = hios_id.strip
         corrected_hios_id = (clean_hios.end_with?("-01") ? clean_hios : clean_hios + "-01")
-        @plan = BenefitMarkets::Products::Product.where(hios_id: single_plan_hios_id).first
+        @plan = BenefitMarkets::Products::Product.where(hios_id: corrected_hios_id).first
       end
 
       def find_employer
@@ -106,6 +52,7 @@ module BenefitSponsors
         @found_employer = org.employer_profile
       end
 
+      # TODO: update references for plan years with benefit applications
       def save
         return false unless valid?
         employer = find_employer
@@ -134,18 +81,7 @@ module BenefitSponsors
         result
       end
 
-      def propagate_warnings(proxy)
-        proxy.warnings.each do |attr, err|
-          warnings.add(attr, err)
-        end
-      end
-
-      def propagate_errors(proxy)
-        proxy.errors.each do |attr, err|
-          errors.add(attr, err)
-        end
-      end
-
+      # TODO: update references for plan years with benefit applications
       def find_enrollments(employee_role)
         person = employee_role.person
 
