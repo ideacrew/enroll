2bb631445a523e79be7caa2870a25025c1d22048
venkatbhattiprolu <vbhat847@gmail.com>
2018-06-01 11:56:38 -0400

    refs#24103 handling exception

--- app/data_migrations/remove_plan_offerings_for_er.rb
+++ app/data_migrations/remove_plan_offerings_for_er.rb
@@ -2,14 +2,19 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class RemovePlanOfferings< MongoidMigrationTask
   def migrate
-    organizations = Organization.where(fein: ENV['fein'])
-    aasm_state = ENV['aasm_state']
-    given_carrier_profile_id = ENV['carrier_profile_id'].to_s
-    if organizations.size!= 1
-      raise "issues with given fein"
+    begin
+      organizations = Organization.where(fein: ENV['fein'])
+      aasm_state = ENV['aasm_state']
+      given_carrier_profile_id = ENV['carrier_profile_id'].to_s
+      if organizations.size!= 1
+        raise "issues with given fein"
+      else
+        organizations.first.employer_profile.plan_years.where(aasm_state: aasm_state).first.benefit_groups.first.elected_plans.select{|p| p.carrier_profile_id.to_s == given_carrier_profile_id}.each(&:destroy)
+        puts "removed the carrier from employer level offerings" unless Rails.env.test?
+      end
+    rescue => e
+      e.message
     end
-    organizations.first.employer_profile.plan_years.where(aasm_state: aasm_state).first.benefit_groups.first.elected_plans.select{|p| p.carrier_profile_id.to_s == given_carrier_profile_id}.each(&:destroy)
-    puts "removed the carrier from employer level offerings" unless Rails.env.test?
   end
 end
 
