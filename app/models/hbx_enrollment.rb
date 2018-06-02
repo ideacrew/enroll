ef89b6fbc053ba21888908464e9cedd6ab20bc98
Raghuram <raghuramg83@gmail.com>
2018-06-02 10:59:32 -0400

    Added product_id, issuer_profile_id to hbx_enrollment

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -103,6 +103,9 @@ class HbxEnrollment
   field :sponsored_benefit_id, type: BSON::ObjectId
   field :rating_area_id, type: BSON::ObjectId
 
+  field :product_id, type: BSON::ObjectId
+  field :issuer_profile_id, type: BSON::ObjectId
+
   field :original_application_type, type: String
 
   field :submitted_at, type: DateTime
@@ -725,6 +728,18 @@ class HbxEnrollment
   def plan
     return @plan if defined? @plan
     @plan = Plan.find(self.plan_id) unless plan_id.blank?
+  end
+
+  def product=(new_product)
+    raise ArgumentError.new("expected product") unless new_product.is_a?(BenefitMarkets::Products::Product)
+    self.product_id = new_product._id
+    self.issuer_profile_id = new_product.issuer_profile_id
+    @product = new_product
+  end
+
+  def product
+    return @product if defined? @product
+    @product = BenefitMarkets::Products::Product.find(self.product_id) unless product_id.blank?
   end
 
   def set_coverage_termination_date(coverage_terminated_on=TimeKeeper.date_of_record)
