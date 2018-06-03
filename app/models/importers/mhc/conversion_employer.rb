b39d0a19e691eb9eee376cf0aa5404c56950fa5c
Raghuram <raghuramg83@gmail.com>
2018-06-03 19:35:46 -0400

    make office address by default primary

--- app/models/importers/mhc/conversion_employer.rb
+++ app/models/importers/mhc/conversion_employer.rb
@@ -32,7 +32,7 @@ module Importers::Mhc
 
     def build_primary_address
       Address.new(
-          :kind => "work",
+          :kind => "primary",
           :address_1 => primary_location_address_1,
           :address_2 => primary_location_address_2,
           :city =>  primary_location_city,
