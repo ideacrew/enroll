105b4d9909d8b55b585e2b7c3911b535787370c3
krbhavith <krbhavith@gmail.com>
2018-06-04 16:55:11 -0400

    refs #21883 rspec for invoices migration

--- app/data_migrations/migrate_invoices_to_employer_profile.rb
+++ app/data_migrations/migrate_invoices_to_employer_profile.rb
@@ -4,16 +4,19 @@ class MigrateInvoicesToEmployerProfile < MongoidMigrationTask
   def migrate
     
     Organization.all_employer_profiles.each do |org|
-    	begin
-    		invoices = []
-    		invoices = org.documents.select{ |invoice| invoice.subject == "invoice" }
-    		next if invoices.empty?
-      	puts "Found #{invoices.count} invoices for employer #{org.fein}"
-      	org.employer_profile.documents.push(invoices)
-      	org.documents.destroy_all{ |invoice| (invoice.subject == "invoice") && (org.employer_profile.documents.include?(invoice)) }
-      	puts "migrated invoices from Organization to employer_profile for FEIN: #{org.fein}"
+      begin
+        invoices = org.documents.select{ |invoice| invoice.subject == "invoice" }
+        next if invoices.empty?
+        puts "Found #{invoices.count} invoices for employer #{org.fein}"
+        invoices.each do |invoice|
+          org.employer_profile.documents << invoice
+        end
+        org.save!
+        org.reload
+        org.documents.select{ |invoice| (invoice.subject == "invoice") && (org.employer_profile.documents.include?(invoice)) }.each{|doc| doc.delete}
+        puts "Successfully migrated invoices from Organization to employer_profile for FEIN: #{org.fein}"
       rescue Exception => e
-      	log("#{e.message}; for employer with FEIN: #{org.fein}")
+        log("#{e.message}; for employer with FEIN: #{org.fein}")
       end
     end
   end
