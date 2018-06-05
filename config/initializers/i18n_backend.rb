5ee221ced9830b5e451b3af3b4861af521b6bdf2
Trey Evans <tevans@MacBook-Pro-3.local>
2018-06-05 11:47:38 -0400

    Translation cache fix.

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,6 +1,6 @@
 require 'mongo_i18n'
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(MongoI18n.store), I18n.backend)
-if Rails.env.production?
+if ENV["NO_CACHE_TRANSLATIONS"] == "true"
   I18n::Backend::KeyValue.send(:include, I18n::Backend::Cache)
   I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 end
