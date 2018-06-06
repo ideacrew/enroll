f93d526914988522c2189ab01a5a5a149f95988c
Trey Evans <tevans@MacBook-Pro-3.local>
2018-06-06 13:26:36 -0400

    FIxing shopping experience.

--- app/models/enrollment_shopping/affected_enrollment_finder.rb
+++ app/models/enrollment_shopping/affected_enrollment_finder.rb
@@ -3,7 +3,7 @@ module EnrollmentShopping
   # purchase.
   class AffectedEnrollmentFinder
     def for_sponsored_benefit_and_date(family, sponsored_benefit, coverage_date)
-      family.map(&:households).flat_map(:hbx_enrollments).select do |hbx_enrollment|
+      family.households.flat_map(&:hbx_enrollments).select do |hbx_enrollment|
         (hbx_enrollment.sponsored_benefit_id == sponsored_benefit.id) && hbx_enrollment.active_during?(coverage_date)
       end
     end
