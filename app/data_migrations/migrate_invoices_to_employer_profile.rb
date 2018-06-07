0fe2ec4235740765e525ebb94bef910d3664a805
Battula <vrbattula@outlook.com>
2018-06-06 22:41:22 -0400

    Refs #21883 Run in batches.

--- app/data_migrations/migrate_invoices_to_employer_profile.rb
+++ app/data_migrations/migrate_invoices_to_employer_profile.rb
@@ -2,22 +2,29 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class MigrateInvoicesToEmployerProfile < MongoidMigrationTask
   def migrate
-    
-    Organization.all_employer_profiles.each do |org|
-      begin
-        invoices = org.documents.select{ |invoice| ["invoice", "initial_invoice"].include? invoice.subject }
-        next if invoices.empty?
-        puts "Found #{invoices.count} invoices for employer #{org.fein}"
-        invoices.each do |invoice|
-          org.employer_profile.documents << invoice
+    batch_size = 500
+    offset = 0
+
+    orgs = Organization.all_employer_profiles
+
+    while (offset <= orgs.count)
+      orgs.each do |org|
+        begin
+          invoices = org.documents.select{ |invoice| ["invoice", "initial_invoice"].include? invoice.subject }
+          next if invoices.empty?
+          puts "Found #{invoices.count} invoices for employer #{org.fein}"
+          invoices.each do |invoice|
+            org.employer_profile.documents << invoice
+          end
+          org.save!
+          org.reload
+          org.documents.select{ |invoice| (["invoice", "initial_invoice"].include? invoice.subject) && (org.employer_profile.documents.include?(invoice)) }.each{|doc| doc.delete}
+          puts "Successfully migrated invoices from Organization to employer_profile for FEIN: #{org.fein}"
+        rescue Exception => e
+          log("#{e.message}; for employer with FEIN: #{org.fein}")
         end
-        org.save!
-        org.reload
-        org.documents.select{ |invoice| (["invoice", "initial_invoice"].include? invoice.subject) && (org.employer_profile.documents.include?(invoice)) }.each{|doc| doc.delete}
-        puts "Successfully migrated invoices from Organization to employer_profile for FEIN: #{org.fein}"
-      rescue Exception => e
-        log("#{e.message}; for employer with FEIN: #{org.fein}")
       end
+      offset = offset + batch_size
     end
   end
 end
