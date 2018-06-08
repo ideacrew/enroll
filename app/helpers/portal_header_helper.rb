e3b980d54eccf1071e7906d67637aa2976e8ffc9
Bill Transue <transue@gmail.com>
2018-06-08 15:43:03 -0400

    Fix Waive Coverage

--- app/helpers/portal_header_helper.rb
+++ app/helpers/portal_header_helper.rb
@@ -16,7 +16,7 @@ module PortalHeaderHelper
     elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
       link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Trained Expert".html_safe, home_exchanges_agents_path, class: "portal"
     elsif current_user.person && current_user.person.active_employee_roles.any?
-      link_to "#{image_tag exchange_icon_path('icon-individual.png')} &nbsp; I'm an #{controller=='employer_profiles'? 'Employer': 'Employee'}".html_safe, family_account_path, class: "portal"
+      link_to "#{image_tag exchange_icon_path('icon-individual.png')} &nbsp; I'm an #{controller=='employer_profiles'? 'Employer': 'Employee'}".html_safe, main_app.family_account_path, class: "portal"
     elsif (controller_path.include?("insured") && current_user.try(:has_consumer_role?))
       if current_user.identity_verified_date.present?
         link_to "#{image_tag 'icons/icon-family.png'} &nbsp; Individual and Family".html_safe, family_account_path, class: "portal"
