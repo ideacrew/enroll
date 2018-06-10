9452c8edcef919b18f9b24dbde12b3c695bf99c0
Trey Evans <tevans@macbook-pro-3.lan>
2018-06-10 15:40:49 -0400

    Found issue with spec combinations.

--- spec/views/broker_agencies/profiles/_assign_history.html.erb_spec.rb
+++ spec/views/broker_agencies/profiles/_assign_history.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "broker_agencies/profiles/_assign_history.html.erb" do
+RSpec.describe "broker_agencies/profiles/_assign_history.html.erb", dbclean: :after_each do
   let(:broker_agency_profile) { FactoryGirl.create(:broker_agency_profile) }
 
   before :each do
