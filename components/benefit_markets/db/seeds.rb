3554b947552bd7f4f2ed35576271530e876ebebf
Dan Thomas <dan.thomas@dc.gov>
2018-06-15 21:06:35 -0400

    Enable BenefitMarkets and BenefitSponsors local engine seed files

--- components/benefit_markets/db/seeds.rb
+++ components/benefit_markets/db/seeds.rb
@@ -3,8 +3,32 @@
 ##  1) In spec/dummy/db folder, create seed.rb file with following: BenefitMarkets::Engine.load_seed
 ##  2) In terminal, chdir to spec/dummy, run rake db:seed
 
-puts "*"*80
+puts "\n"*3
 puts "Start of Engine BenefitMarkets seed"
-puts "..."
+puts "*"*80
+
+collection_names = %w(
+    benefit_markets_benefit_markets
+    benefit_markets_pricing_models_pricing_models
+    benefit_markets_locations_service_areas
+    benefit_markets_locations_rating_areas
+    benefit_markets_contribution_models_contribution_models
+    benefit_markets_locations_county_zips
+    benefit_markets_products_products
+    benefit_markets_products_actuarial_factors_actuarial_factors
+  )
+
+puts "Dropping engine-specific collections"
+Mongoid.default_client.collections.each do |collection|
+  if collection_names.include?(collection.name)
+    puts "  dropping collection: #{collection.name}"
+    collection.drop
+  end
+end
+puts "*"*80
+puts "*"*80
+
+require File.join(File.dirname(__FILE__),'seedfiles', 'cca_seed')
+
 puts "End of Engine BenefitMarkets seed"
 puts "*"*80
--- components/benefit_sponsors/db/seeds.rb
+++ components/benefit_sponsors/db/seeds.rb
@@ -3,8 +3,27 @@
 ##  1) In spec/dummy/db folder, create seed.rb file with following: BenefitSponsors::Engine.load_seed
 ##  2) In terminal, chdir to spec/dummy, run rake db:seed
 
-puts "*"*80
+puts "\n"*3
 puts "Start of Engine BenefitSponsors seed"
-puts "..."
+puts "*"*80
+
+collection_names = %w(
+    benefit_sponsors_benefit_sponsorships_benefit_sponsorships
+    benefit_sponsors_organizations_organizations
+    benefit_sponsors_sites
+  )
+
+puts "Dropping engine-specific collections"
+Mongoid.default_client.collections.each do |collection|
+  if collection_names.include?(collection.name)
+    puts "  dropping collection: #{collection.name}"
+    collection.drop
+  end
+end
+puts "*"*80
+puts "*"*80
+
+require File.join(File.dirname(__FILE__),'seedfiles', 'cca_seed')
+
 puts "End of Engine BenefitSponsors seed"
 puts "*"*80
