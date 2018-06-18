5ce3189ecaed1e579068818b4dcd96bb88c2444e
Battula <vrbattula@outlook.com>
2018-06-18 17:46:43 -0400

    Added Notice Service, Notice Observer and Model Events for Notices.

---
+++ components/benefit_sponsors/app/models/benefit_sponsors/queries/notice_queries.rb
@@ -0,0 +1,47 @@
+module BenefitSponsors
+  module Queries
+    class NoticeQueries
+
+      def self.initial_employers_by_effective_on_and_state(start_on: TimeKeeper.date_of_record, aasm_state:)
+        BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
+          :'benefit_applications' => {
+            :$elemMatch => {
+              :effective_period.min => new_date.next_month.beginning_of_month,
+              :aasm_state => aasm_state
+          }
+      end
+
+      def self.organizations_for_force_publish(new_date)
+        BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
+          :'benefit_applications' =>
+          { :$elemMatch => {
+            :effective_period.min => new_date.next_month.beginning_of_month,
+            :aasm_state => :draft
+          }}
+        })
+      end
+
+      def self.organizations_for_low_enrollment_notice(current_date)
+        BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
+          :"benefit_applications" => { 
+            :$elemMatch => {
+              :"aasm_state" => :enrollment_open,
+              :"enrollment_period.min" => current_date + 2.days
+            }
+          }
+        })
+      end
+
+      def self.initial_employers_in_enrolled_state
+        BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
+          :"benefit_applications" => { 
+            :$elemMatch => {
+              :"aasm_state" => :enrollment_eligible,
+              
+            }
+          }
+        })
+      end
+    end
+  end
+end
