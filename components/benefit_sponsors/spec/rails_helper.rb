425240c029a01a88e66d39ce1cb3c070d193e52c
Trey Evans <tevans@MacBook-Pro-3.local>
2018-06-20 13:40:01 -0400

    Spec fixes, and consistent failures.

--- components/benefit_sponsors/spec/rails_helper.rb
+++ components/benefit_sponsors/spec/rails_helper.rb
@@ -56,7 +56,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -71,6 +71,7 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+  DatabaseCleaner.strategy = :truncation
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
