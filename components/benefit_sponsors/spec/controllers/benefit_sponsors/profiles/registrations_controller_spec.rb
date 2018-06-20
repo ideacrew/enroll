e39ac568271f06f955bc3bf3f46cd7724f300991
saimekala07 <saimekala07@gmail.com>
2018-06-20 15:13:34 -0400

    Fixing registartion controller specs

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/registrations_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/registrations_controller_spec.rb
@@ -5,7 +5,7 @@ module BenefitSponsors
 
     routes { BenefitSponsors::Engine.routes }
     let!(:security_question)  { FactoryGirl.create_default :security_question }
-
+    let!(:rating_area) { create_default(:benefit_markets_locations_rating_area) }
     let(:agency_class) { BenefitSponsors::Organizations::OrganizationForms::RegistrationForm }
     # let!(:site)  { FactoryGirl.create(:benefit_sponsors_site, :with_owner_exempt_organization, :dc, :with_benefit_market) }
     let!(:site) { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :dc) }
@@ -31,8 +31,9 @@ module BenefitSponsors
         :address_1 => "address 1",
         :address_2 => "address 2",
         :city => "city",
-        :state => "MD",
-        :zip => "22222"
+        :state => "MA",
+        :zip => "01026",
+        :county =>'Berkshire'
       }
     }
 
@@ -288,9 +289,15 @@ module BenefitSponsors
     end
 
     describe "GET edit", dbclean: :after_each do
-
-      let(:benefit_sponsor) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+      let!(:benefit_sponsor) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+      let!(:benefit_sponsorship) {
+        benefit_sponsorship = benefit_sponsor.profiles.first.add_benefit_sponsorship
+        benefit_sponsorship.save }
       let(:broker_agency)   { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site) }
+      let!(:staff_role) {FactoryGirl.build(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: benefit_sponsor.profiles.first.id)}
+      let!(:employer_staff_roles) { person.employer_staff_roles << staff_role }
+      let!(:broker_role) {FactoryGirl.build(:broker_role, benefit_sponsors_broker_agency_profile_id: broker_agency.profiles.first.id,person:person)}
+      let!(:update_profile) { broker_agency.profiles.first.update_attributes(primary_broker_role_id: broker_role.id)}
 
       shared_examples_for "initialize profile for edit" do |profile_type|
 
