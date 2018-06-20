c01de6865a833865139b2fd66e780cf19cacf7e2
Josh <karperj@umd.edu>
2018-06-20 16:02:40 -0400

    refs#21284 save button disabled by default on employer income form

--- app/assets/javascripts/financial_assistance/income.js
+++ app/assets/javascripts/financial_assistance/income.js
@@ -14,6 +14,7 @@ function startEditingIncome(income_kind) {
   $('.col-md-3 > .interaction-click-control-continue').addClass('disabled');
   $("a.interaction-click-control-add-more").addClass('hide');
   $('input.interaction-choice-control-value-other-income-kind').prop('disabled', true);
+  $('.interaction-click-control-save').addClass('disabled');
 };
 
 function currentlyEditing() {
