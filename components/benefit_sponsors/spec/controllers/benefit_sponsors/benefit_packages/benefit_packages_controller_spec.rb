fd51e8f8106602e8a283dd7bc28d076444078e20
Trey Evans <tevans@MacBook-Pro-3.local>
2018-06-21 12:16:15 -0400

    Fix more factory failures.

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/benefit_packages/benefit_packages_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/benefit_packages/benefit_packages_controller_spec.rb
@@ -43,7 +43,7 @@ module BenefitSponsors
       application
     }
     let!(:benefit_application_id) { benefit_application.id.to_s }
-    let!(:issuer_profile)  { FactoryGirl.create :benefit_sponsors_organizations_issuer_profile }
+    let!(:issuer_profile)  { FactoryGirl.create :benefit_sponsors_organizations_issuer_profile, assigned_site: site}
     let!(:product_package_kind) { :single_issuer }
     let!(:product_package) { benefit_market_catalog.product_packages.where(package_kind: product_package_kind).first }
     let!(:product) { product_package.products.first }
