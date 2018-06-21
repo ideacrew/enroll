4f265e8cf6d12326805d28884aa836b9a3508478
Josh <karperj@umd.edu>
2018-06-21 12:27:24 -0400

    fix create_employer, employee_passive_renewal_update features, and added internal use method

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -7,6 +7,12 @@ When(/I use unique values/) do
   @u = UniqueValueStash::UniqueValues.new unless defined?(@u)
 end
 
+#Shows page with relevant CSS and javascripts loaded
+def show_page
+  save_page Rails.root.join( 'public', 'capybara.html' )
+  %x(launchy http://localhost:3000/capybara.html)
+end
+
 def people
   return @a if defined?(@a)
   @a = {
