2d9924d7942ba8e6acb605eca7bf09c7cb08b191
nisanth <nisanth.yaganti@gmail.com>
2018-06-20 22:55:10 -0400

    fix pundit for show pending action & fixing employer profile controller specs

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller_spec.rb
@@ -12,11 +12,14 @@ module BenefitSponsors
     let!(:site)                  { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
     let!(:benefit_sponsor)       { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
     let(:employer_profile)      { benefit_sponsor.employer_profile }
+    let!(:rating_area)           { FactoryGirl.create_default :benefit_markets_locations_rating_area }
+    let!(:service_area)          { FactoryGirl.create_default :benefit_markets_locations_service_area }
+    let(:benefit_sponsorship)    { employer_profile.add_benefit_sponsorship }
 
     describe "GET show_pending" do
       before do
         sign_in user
-        get :show_pending, id: benefit_sponsor.profiles.first.id
+        get :show_pending
       end
 
       it "should render show template" do
@@ -29,15 +32,16 @@ module BenefitSponsors
     end
 
     describe "GET show" do
-      let(:benefit_sponsorship) { benefit_sponsor.profiles.first.parent.active_benefit_sponsorship }
       let!(:employees) {
         FactoryGirl.create_list(:census_employee, 2, employer_profile: employer_profile, benefit_sponsorship: benefit_sponsorship)
       }
       render_views
 
       before do
+        benefit_sponsorship.save!
         sign_in user
         get :show, id: benefit_sponsor.profiles.first.id, tab: 'employees'
+        allow(employer_profile).to receive(:active_benefit_sponsorship).and_return benefit_sponsorship
       end
 
       it "should render show template" do
