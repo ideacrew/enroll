7d1471e9714a1504e459863e33e12400e3da6ad4
saikumar9 <saikumarkotagiri9@gmail.com>
2018-06-23 00:25:50 -0400

    use benefit_sponsorship_id instead of organization_id for bulk actions

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -61,7 +61,8 @@ class Exchanges::HbxProfilesController < ApplicationController
   end
 
   def generate_invoice
-    @organizations = BenefitSponsors::Organizations::Organization.where(:"_id".in => params[:ids]).all
+    @benfit_sponsorships = ::BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(:"_id".in => params[:ids])
+    @organizations = @benfit_sponsorships.map(&:organization)
     @employer_profiles = @organizations.flat_map(&:employer_profile)
     @employer_profiles.each do |employer_profile|
       @employer_invoice = BenefitSponsors::EmployerInvoice.new(employer_profile.organization)
