0536e8524abb92e8e4b3044ff2123b2b0d5c560a
Trey Evans <tevans@macbook-pro-3.lan>
2018-06-24 10:42:33 -0400

    Fix query for initial enrollment policies.

--- app/assets/javascripts/group_selection.js
+++ app/assets/javascripts/group_selection.js
@@ -151,15 +151,6 @@ function errorsForChangeInCoverageKind(employer_id){
       disableIvlIneligible();
     }
   });
-
-  if ($("#waiver_reasons_selection_modal_form").length) {
-    $("#waiver_reasons_selection_modal_form").on( "submit", function( event ) {
-      event.preventDefault();
-      $("#is_waiving_hidden_value_field").val("true");
-      $("#waiver_reason_hidden_value_field").val($("#waiver_reason_selection_dropdown").val());
-      $("#group-selection-form").trigger("submit");
-    });
-  }
 }
 
 function disableIvlIneligible() {
