ec2b3fb4e977239b17caba607df201da7b2aa156
Vishal Kalletla <vishuk1201@gmail.com>
2018-06-26 17:27:10 -0400

    WIP more code - plan design organization

--- app/models/sponsored_benefits/organizations/plan_design_organization.rb
+++ app/models/sponsored_benefits/organizations/plan_design_organization.rb
@@ -29,6 +29,12 @@ module SponsoredBenefits
       field :sponsor_profile_id,         type: BSON::ObjectId
       field :sponsor_profile_class_name, type: String, default: "::BenefitSponsors::Organizations::Profile"
 
+      #temporary fields to store old model profile data as history while migrating
+      field :history_owner_profile_id,type: BSON::ObjectId
+      field :history_owner_profile_class_name,type: String
+      field :history_sponsor_profile_id,type: BSON::ObjectId
+      field :history_sponsor_profile_class_name,type: String
+
       field :has_active_broker_relationship, type: Boolean, default: false
 
       embeds_many :plan_design_proposals, class_name: "SponsoredBenefits::Organizations::PlanDesignProposal", cascade_callbacks: true
