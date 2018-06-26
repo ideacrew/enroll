16535f21f4408d1ea1770dfe98e38adcb498e8d6
Prem Shaganti <prem.shaganti@dc.gov>
2018-06-26 17:43:13 -0400

    ref: conversion plan year import update

--- app/models/importers/row_set.rb
+++ app/models/importers/row_set.rb
@@ -36,39 +36,51 @@ module Importers
 
       import_details = []
 
-      result = record.save
+      result = false
+
+      errors = String.new
+
       census_employee_info = Array.new
-      if result
-        if /conversion_employer_results/.match(@out_csv.path)
-          organization = find_organization(record.fein)
-        end
 
-        if /conversion_employee_policy_results/.match(@out_csv.path)
-          benefit_sponsor = find_organization(record.fein)
-          census_employee = find_census_employee(benefit_sponsor.employer_profile, benefit_sponsor.active_benefit_sponsorship, record.subscriber_ssn)
-          person = census_employee.first.employee_role.person
-          hbx_enroll_ment = find_hbx_enrollments(person)
-          census_employee_info << hbx_enroll_ment.hbx_id
-          census_employee_info << person.hbx_id
-          family_dependents = person.primary_family.family_members.find_all {|family_member| !family_member.is_primary_applicant?}
+      begin
+        result = record.save
 
-          family_dependents.each do |family_member|
-            census_employee_info.push family_member.person.hbx_id
+        if result
+          if /conversion_employer_results/.match(@out_csv.path)
+            organization = find_organization(record.fein)
           end
-        end
 
-        if record.warnings.any?
-          import_details = ["imported with warnings", JSON.dump(record.warnings.to_hash)]
+          if /conversion_employee_policy_results/.match(@out_csv.path)
+            benefit_sponsor = find_organization(record.fein)
+            census_employee = find_census_employee(benefit_sponsor.employer_profile, benefit_sponsor.active_benefit_sponsorship, record.subscriber_ssn)
+            person = census_employee.first.employee_role.person
+            hbx_enroll_ment = find_hbx_enrollments(person)
+            census_employee_info << hbx_enroll_ment.hbx_id
+            census_employee_info << person.hbx_id
+            family_dependents = person.primary_family.family_members.find_all {|family_member| !family_member.is_primary_applicant?}
+
+            family_dependents.each do |family_member|
+              census_employee_info.push family_member.person.hbx_id
+            end
+          end
+
+          if record.warnings.any?
+            import_details = ["imported with warnings", JSON.dump(record.warnings.to_hash)]
+          else
+            import_details = ["imported", ""]
+            import_details.push(organization.hbx_id) if organization
+          end
         else
-          import_details = ["imported", ""]
-          import_details.push(organization.hbx_id) if organization
+          import_details = []
+          import_details << ["import failed", JSON.dump(record.errors.to_hash)]
+          import_details << ["warnings", JSON.dump(record.warnings.to_hash)] if record.warnings.any?
         end
-      else
-        import_details = []
-        import_details << ["import failed", JSON.dump(record.errors.to_hash)]
-        import_details << ["warnings", JSON.dump(record.warnings.to_hash)] if record.warnings.any?
+        @out_csv << (row.map(&:to_s) + import_details + census_employee_info)
+      rescue Exception => e
+        result = false
+        import_details = ["import failed", JSON.dump(e)]
+        @out_csv << (row.map(&:to_s) + import_details + census_employee_info)
       end
-      @out_csv << (row.map(&:to_s) + import_details + census_employee_info)
     end
 
 
