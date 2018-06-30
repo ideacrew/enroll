35c488569ba2e0adaba1cbb4f12a2a3268798832
Battula <vrbattula@outlook.com>
2018-06-30 18:53:50 -0400

    Clean up SponsoredBenefits Engine specs.

--- spec/models/sponsored_benefits/benefit_applications/benefit_application_spec.rb
+++ spec/models/sponsored_benefits/benefit_applications/benefit_application_spec.rb
@@ -14,10 +14,6 @@ module SponsoredBenefits
     let(:open_enrollment_period_end_on)   { open_enrollment_period_start_on + 9.days }
     let(:open_enrollment_period)          { open_enrollment_period_start_on..open_enrollment_period_end_on }
 
-  # field :recorded_sic_code, type: String
-  # field :recorded_rating_area, type: String
-
-
     let(:params) do
       {
         effective_period: effective_period,
@@ -62,7 +58,7 @@ module SponsoredBenefits
       end
     end
 
-    context "#to_plan_year" do
+    context "#to_benefit_sponsors_benefit_application" do
       let(:benefit_application)       { SponsoredBenefits::BenefitApplications::BenefitApplication.new(params) }
       let(:benefit_sponsorship)       { SponsoredBenefits::BenefitSponsorships::BenefitSponsorship.new(
         benefit_market: "aca_shop_cca",
@@ -77,68 +73,45 @@ module SponsoredBenefits
           phone: phone
         )
       }
+      let(:benefit_group)             { FactoryGirl.create :benefit_group }
+
+      let(:benefit_market)      { site.benefit_markets.first }
+      let(:current_effective_date)  { TimeKeeper.date_of_record }
+      let!(:benefit_market_catalog) { create(:benefit_markets_benefit_market_catalog, :with_product_packages,
+                                              benefit_market: benefit_market,
+                                              title: "SHOP Benefits for #{current_effective_date.year}",
+                                              application_period: (current_effective_date.beginning_of_year..current_effective_date.end_of_year))
+                                      }
+      let!(:rating_area)   { FactoryGirl.create_default :benefit_markets_locations_rating_area }
+      let!(:service_area)  { FactoryGirl.create_default :benefit_markets_locations_service_area }
+      let(:site)                { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+      let(:benefit_sponsor_organization) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+      let(:sponsor_benefit_sponsorship) { benefit_sponsor_organization.employer_profile.add_benefit_sponsorship }
 
       let(:plan_design_organization)  { SponsoredBenefits::Organizations::PlanDesignOrganization.new(legal_name: "xyz llc", office_locations: [office_location]) }
       let(:plan_design_proposal)      { SponsoredBenefits::Organizations::PlanDesignProposal.new(title: "New Proposal") }
       let(:profile) {SponsoredBenefits::Organizations::AcaShopCcaEmployerProfile.new}
 
       before(:each) do
+        sponsor_benefit_sponsorship.rating_area = rating_area
+        sponsor_benefit_sponsorship.service_areas = [service_area]
+        sponsor_benefit_sponsorship.save
         plan_design_organization.plan_design_proposals << [plan_design_proposal]
         plan_design_proposal.profile = profile
         profile.benefit_sponsorships = [benefit_sponsorship]
         benefit_sponsorship.benefit_applications = [benefit_application]
-        benefit_application.benefit_groups.build
+        benefit_application.benefit_groups << benefit_group
         plan_design_organization.save
       end
 
       it "should instantiate a plan year object and must have correct values assigned" do
-        plan_year = benefit_application.to_plan_year
-        expect(plan_year.class).to eq PlanYear
-        expect(plan_year.benefit_groups.present?).to eq true
-        expect(plan_year.start_on).to eq benefit_application.effective_period.begin
-        expect(plan_year.end_on).to eq benefit_application.effective_period.end
-        expect(plan_year.open_enrollment_start_on).to eq benefit_application.open_enrollment_period.begin
-        expect(plan_year.open_enrollment_end_on).to eq benefit_application.open_enrollment_period.end
-      end
-    end
-
-    context "#to_plan_year" do
-      let(:benefit_application)       { SponsoredBenefits::BenefitApplications::BenefitApplication.new(params) }
-      let(:benefit_sponsorship)       { SponsoredBenefits::BenefitSponsorships::BenefitSponsorship.new(
-        benefit_market: "aca_shop_cca",
-        enrollment_frequency: "rolling_month"
-      )}
-
-      let(:address)  { Address.new(kind: "primary", address_1: "609 H St", city: "Washington", state: "DC", zip: "20002", county: "County") }
-      let(:phone  )  { Phone.new(kind: "main", area_code: "202", number: "555-9999") }
-      let(:office_location) { OfficeLocation.new(
-          is_primary: true,
-          address: address,
-          phone: phone
-        )
-      }
-
-      let(:plan_design_organization)  { SponsoredBenefits::Organizations::PlanDesignOrganization.new(legal_name: "xyz llc", office_locations: [office_location]) }
-      let(:plan_design_proposal)      { SponsoredBenefits::Organizations::PlanDesignProposal.new(title: "New Proposal") }
-      let(:profile) {SponsoredBenefits::Organizations::AcaShopCcaEmployerProfile.new}
-
-      before(:each) do
-        plan_design_organization.plan_design_proposals << [plan_design_proposal]
-        plan_design_proposal.profile = profile
-        profile.benefit_sponsorships = [benefit_sponsorship]
-        benefit_sponsorship.benefit_applications = [benefit_application]
-        benefit_application.benefit_groups.build
-        plan_design_organization.save
-      end
-
-      it "should instantiate a plan year object and must have correct values assigned" do
-        plan_year = benefit_application.to_plan_year
-        expect(plan_year.class).to eq PlanYear
-        expect(plan_year.benefit_groups.present?).to eq true
-        expect(plan_year.start_on).to eq benefit_application.effective_period.begin
-        expect(plan_year.end_on).to eq benefit_application.effective_period.end
-        expect(plan_year.open_enrollment_start_on).to eq benefit_application.open_enrollment_period.begin
-        expect(plan_year.open_enrollment_end_on).to eq benefit_application.open_enrollment_period.end
+        ben_app = benefit_application.to_benefit_sponsors_benefit_application(benefit_sponsor_organization)
+        expect(ben_app.class).to eq BenefitSponsors::BenefitApplications::BenefitApplication
+        expect(ben_app.benefit_packages.present?).to eq true
+        expect(ben_app.start_on).to eq benefit_application.effective_period.begin
+        expect(ben_app.end_on).to eq benefit_application.effective_period.end
+        expect(ben_app.open_enrollment_start_on).to eq benefit_application.open_enrollment_period.begin
+        expect(ben_app.open_enrollment_end_on).to eq benefit_application.open_enrollment_period.end
       end
     end
 
