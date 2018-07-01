55c13f97c176993e97785abdbfb919115fbe1e5f
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-07-01 18:11:31 -0400

    Refs #25579 fix start on date for force publish script

--- script/force_publish_benefit_applications.rb
+++ script/force_publish_benefit_applications.rb
@@ -1,6 +1,18 @@
 # Renew everybody who needs to be renewed
 
-start_on_date = Date.today.next_month.beginning_of_month
+date = Date.today
+
+if date.day > 15
+  window_start = Date.new(date.year,date.month,16)
+  window_end = Date.new(date.next_month.year,date.next_month.month,15)
+  window = (window_start..window_end)
+elsif date.day <= 15
+  window_start = Date.new((date - 1.month).year,(date - 1.month).month,16)
+  window_end = Date.new(date.year,date.month,15)
+  window = (window_start..window_end)
+end
+
+start_on_date = window.end.next_month.beginning_of_month.to_time.utc.beginning_of_day
 
 def find_renewed_sponsorships(start_date)
   BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
