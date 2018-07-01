bf8f13553db821c6cae81ae3c4c9493dbf5341be
Dan Thomas <dan.thomas@dc.gov>
2018-07-01 09:12:58 -0400

    Passing basic specs

--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/sponsored_benefit.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/sponsored_benefit.rb
@@ -1,106 +1,168 @@
 module BenefitSponsors
-  module SponsoredBenefits
-    class SponsoredBenefit
-      include Mongoid::Document
-      include Mongoid::Timestamps
+  class SponsoredBenefits::SponsoredBenefit
+    include Mongoid::Document
+    include Mongoid::Timestamps
 
-      embedded_in :benefit_package, 
-                  class_name: "::BenefitSponsors::BenefitPackages::BenefitPackage", inverse_of: :sponsored_benefits
+    embedded_in :benefit_package,
+                class_name: "::BenefitSponsors::BenefitPackages::BenefitPackage",
+                inverse_of: :sponsored_benefits
 
-      field :product_package_kind,  type: Symbol
-      field :product_option_choice, type: String # carrier id / metal level
+    field :product_package_id,    type: BSON::ObjectId
+    field :reference_product_id,  type: BSON::ObjectId
 
-      belongs_to :reference_product, class_name: "::BenefitMarkets::Products::Product", inverse_of: nil
+    # field :product_kind,          type: Symbol
+    # field :product_package_kind,  type: Symbol
+    # field :product_option_choice, type: String # carrier id / metal level
 
-      embeds_one  :sponsor_contribution, 
-                  class_name: "::BenefitSponsors::SponsoredBenefits::SponsorContribution"
+    # FIXME -- This must reference a Product available in the BenefitSponsorCatalog
+    # belongs_to :reference_product, class_name: "::BenefitMarkets::Products::Product", inverse_of: nil
 
-      embeds_many :pricing_determinations, 
-                  class_name: "::BenefitSponsors::SponsoredBenefits::PricingDetermination"
+    embeds_one  :sponsor_contribution,
+                class_name: "::BenefitSponsors::SponsoredBenefits::SponsorContribution"
 
-      delegate :contribution_model, to: :product_package, allow_nil: true
-      delegate :pricing_model, to: :product_package, allow_nil: true
-      delegate :pricing_calculator, to: :product_package, allow_nil: true
-      delegate :contribution_calculator, to: :product_package, allow_nil: true
-      delegate :recorded_rating_area, to: :benefit_package
-      delegate :recorded_service_area_ids, to: :benefit_package
-      delegate :rate_schedule_date, to: :benefit_package
-      delegate :benefit_sponsor_catalog, to: :benefit_package
-      delegate :benefit_sponsorship, to: :benefit_package
-      delegate :recorded_sic_code, to: :benefit_package
+    embeds_many :pricing_determinations,
+                class_name: "::BenefitSponsors::SponsoredBenefits::PricingDetermination"
 
-      validate :product_package_exists
-#      validates_presence_of :sponsor_contribution
+    delegate :contribution_model,         to: :product_package, allow_nil: true
+    delegate :pricing_model,              to: :product_package, allow_nil: true
+    delegate :pricing_calculator,         to: :product_package, allow_nil: true
+    delegate :contribution_calculator,    to: :product_package, allow_nil: true
+    delegate :recorded_rating_area,       to: :benefit_package
+    delegate :recorded_service_area_ids,  to: :benefit_package
+    delegate :recorded_sic_code,          to: :benefit_package
+    delegate :rate_schedule_date,         to: :benefit_package
+    delegate :benefit_sponsor_catalog,    to: :benefit_package
+    delegate :benefit_sponsorship,        to: :benefit_package
 
-      def product_package_exists
-        if product_package.blank?
-          self.errors.add(:base => "Unable to find mappable product package")
+    validates_presence_of :product_package, :sponsor_contribution, :pricing_determinations
+    # validate :product_package_exists
+    #      validates_presence_of :sponsor_contribution
+
+    # def product_package_exists
+    #   if product_package.blank?
+    #     self.errors.add(:base => "Unable to find mappable product package")
+    #   end
+    # end
+
+
+#### FIXME - these attributes should be set when BenefitSponsorCatalog product package is assigned
+    def product_package_present?
+      product_package_id.present?
+    end
+
+    def product_package_kind=(new_product_package_kind)
+      @product_package_kind = new_product_package_kind
+    end
+
+    def product_package_kind
+      @product_package_kind
+      # product_package.package_kind if product_package.present?
+    end
+
+    def product_kind
+      product_package.product_kind if product_package.present?
+    end
+
+#####
+
+    # Don't remove this. Added it to get around mass assignment
+    def kind=(kind)
+      # do nothing
+    end
+
+    def single_plan_type?
+      product_package_kind == :single_product
+    end
+
+
+    # FIXME Remove date argument
+    def products(coverage_date = nil)
+      return [reference_product] if product_package_kind == :single_product
+      product_package.products.present? ? product_package.products : []
+    end
+
+    # FIXME Deprecate
+    def lookup_package_products(coverage_date)
+      products
+
+      # return [reference_product] if product_package_kind == :single_product
+      # product_package.products
+      # BenefitMarkets::Products::Product.by_coverage_date(product_package.products_for_plan_option_choice(product_option_choice).by_service_areas(recorded_service_area_ids), coverage_date)
+    end
+
+    def product_package=(new_product_package)
+      if new_product_package.nil?
+        write_attribute(:product_package_id, nil)
+      else
+        raise ArgumentError.new("expected ProductPackage") unless new_product_package.is_a? ::BenefitMarkets::Products::ProductPackage
+        write_attribute(:product_package_id, new_product_package._id)
+      end
+      @product_package = new_product_package
+    end
+
+    def product_package
+      return nil if product_package_id.nil?
+      return @product_package if defined? @product_package
+      @product_package = benefit_sponsor_catalog.product_package_for(self)
+    end
+
+    def reference_product=(new_reference_product)
+      if new_reference_product.nil?
+        write_attribute(:reference_product_id, nil)
+      else
+        raise ArgumentError.new("expected ReferenceProduct") unless new_reference_product.is_a? ::BenefitMarkets::Products::ProductPackage
+        write_attribute(:reference_product_id, new_reference_product._id)
+      end
+      @reference_product = new_reference_product
+    end
+
+    def reference_product
+      return nil if reference_product_id.nil?
+      return @reference_product if defined? @reference_product
+      @reference_product = products.detect { |product| product._id == reference_product_id }
+    end
+
+    # FIX this stub
+    ## Deprecate for reference_product getter/setter above
+    def reference_product_id=(reference_product_id)
+      write_attribute(:reference_product_id, reference_product_id)
+    end
+
+    ## Deprecate for reference_product getter/setter above
+    def reference_plan_id=(product_id)
+      reference_product_id = product_id
+    end
+
+    def issuers_offered
+      product_package.present? ? product_package.products.pluck(:issuer_profile_id).uniq : []
+    end
+
+    def latest_pricing_determination
+      pricing_determinations.sort_by(&:created_at).last
+    end
+
+    def renew(new_benefit_package)
+      new_benefit_sponsor_catalog = new_benefit_package.benefit_sponsor_catalog
+      new_product_package = new_benefit_sponsor_catalog.product_package_for(self)
+
+      if new_product_package.present? && reference_product.present?
+        if reference_product.renewal_product.present? && new_product_package.active_products.include?(reference_product.renewal_product)
+          self.class.new(
+            product_package_kind: product_package_kind,
+            product_option_choice: product_option_choice,
+            reference_product: reference_product.renewal_product,
+            sponsor_contribution: sponsor_contribution.renew(new_product_package)
+            # pricing_determinations: renew_pricing_determinations(new_product_package)
+          )
         end
       end
+    end
 
-      def product_kind
-        self.class.name.demodulize.split('SponsoredBenefit')[0].downcase.to_sym
-      end
+    def renew_pricing_determinations(new_product_package)
+    end
 
-      # Don't remove this. Added it to get around mass assignment
-      def kind=(kind)
-        # do nothing
-      end
-
-      def single_plan_type?
-        product_package_kind == :single_product
-      end
-
-      def products(coverage_date)
-        lookup_package_products(coverage_date)
-      end
-
-      def lookup_package_products(coverage_date)
-        return [reference_product] if product_package_kind == :single_product
-        BenefitMarkets::Products::Product.by_coverage_date(product_package.products_for_plan_option_choice(product_option_choice).by_service_areas(recorded_service_area_ids), coverage_date)
-      end
-
-      def product_package
-        return @product_package if defined? @product_package
-        @product_package = benefit_sponsor_catalog.product_package_for(self)
-      end
-
-      def issuers_offered
-        return [] if product_package.blank?
-        product_package.products.pluck(:issuer_profile_id).uniq
-      end
-
-      def latest_pricing_determination
-        pricing_determinations.sort_by(&:created_at).last
-      end
-
-      def renew(new_benefit_package)
-        new_benefit_sponsor_catalog = new_benefit_package.benefit_sponsor_catalog
-        new_product_package = new_benefit_sponsor_catalog.product_package_for(self)
-
-        if new_product_package.present? && reference_product.present?
-          if reference_product.renewal_product.present? && new_product_package.active_products.include?(reference_product.renewal_product)
-            self.class.new(
-              product_package_kind: product_package_kind,
-              product_option_choice: product_option_choice,
-              reference_product: reference_product.renewal_product,
-              sponsor_contribution: sponsor_contribution.renew(new_product_package)
-              # pricing_determinations: renew_pricing_determinations(new_product_package)
-            )
-          end
-        end
-      end
-
-      def renew_pricing_determinations(new_product_package)
-      end
-
-      def reference_plan_id=(product_id)
-        self.reference_product_id = product_id
-      end
-
-      def sponsor_contribution_attributes=(sponsor_contribution_attrs)
-        # build_sponsor_contribution(sponsor_contribution_attrs)
-      end
+    def sponsor_contribution_attributes=(sponsor_contribution_attrs)
+      # build_sponsor_contribution(sponsor_contribution_attrs)
     end
   end
 end
--- components/benefit_sponsors/spec/models/benefit_sponsors/sponsored_benefits/sponsored_benefit_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/sponsored_benefits/sponsored_benefit_spec.rb
@@ -1,7 +1,98 @@
 require 'rails_helper'
 
-module BenefitSponsors
-  RSpec.describe SponsoredBenefits::SponsoredBenefit, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+# require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_market.rb"
+# require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_application.rb"
+
+RSpec.describe ::BenefitSponsors::SponsoredBenefits::SponsoredBenefit, type: :model, :dbclean => :after_each do
+  let(:site)                    { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+  let(:benefit_market)          { site.benefit_markets.first }
+  let(:employer_organization)   { create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+  let(:benefit_sponsorship)     { BenefitSponsors::BenefitSponsorships::BenefitSponsorship.new(profile: employer_organization.employer_profile) }
+  let(:effective_period)        { Date.today.end_of_month..Date.today.end_of_month.next_year.prev_day }
+  let(:open_enrollment_period)  { (effective_period.min - 1.month)..(effective_period.min - 1.month + 10.days) }
+  let(:benefit_application)     { BenefitSponsors::BenefitApplications::BenefitApplication.new(
+                                      benefit_sponsorship: benefit_sponsorship,
+                                      # benefit_sponsor_catalog: benefit_sponsor_catalog,
+                                      effective_period: effective_period,
+                                      open_enrollment_period: open_enrollment_period,
+                                      fte_count: 5,
+                                      pte_count: 0,
+                                      msp_count: 0
+                                  ) }
+
+  let(:benefit_package)         { build(:benefit_sponsors_benefit_packages_benefit_package, benefit_application: benefit_application) }
+  let(:product_package)         { build(:benefit_markets_products_product_package) }
+  let(:sponsor_contribution)    { build(:benefit_sponsors_sponsored_benefits_sponsor_contribution) }
+  let(:pricing_determinations)  { BenefitSponsors::SponsoredBenefits::PricingDetermination.new.to_a }
+
+  let(:params) do
+    {
+      benefit_package: benefit_package,
+      product_package: product_package,
+      sponsor_contribution: sponsor_contribution,
+      pricing_determinations: pricing_determinations,
+    }
   end
+
+  describe "A new SponsoredBenefit instance" do
+    it { is_expected.to be_mongoid_document }
+    it { is_expected.to have_fields(:product_package_id)}
+    it { is_expected.to embed_one(:sponsor_contribution)}
+    it { is_expected.to embed_many(:pricing_determinations)}
+
+    context "with no arguments" do
+      subject { described_class.new }
+
+      it "should not be valid" do
+        subject.validate
+        expect(subject).to_not be_valid
+      end
+    end
+
+    context "with no product_package" do
+      subject { described_class.new(params.except(:product_package)) }
+
+      it "should not be valid" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors[:product_package].first).to match(/can't be blank/)
+      end
+    end
+
+    context "with no sponsor_contribution" do
+      subject { described_class.new(params.except(:sponsor_contribution)) }
+
+      it "should not be valid" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors[:sponsor_contribution].first).to match(/can't be blank/)
+      end
+    end
+
+    context "with no pricing_determinations" do
+      subject { described_class.new(params.except(:pricing_determinations)) }
+
+      it "should not be valid" do
+        subject.validate
+        expect(subject).to_not be_valid
+        expect(subject.errors[:pricing_determinations].first).to match(/can't be blank/)
+      end
+    end
+
+    context "with all required arguments" do
+      subject {described_class.new(params) }
+
+      it "should be valid" do
+        subject.validate
+        expect(subject).to be_valid
+      end
+
+      context "and it is saved" do
+        it "should persist" do
+          expect(subject.save).to eq true
+        end
+      end
+    end
+  end
+
 end
