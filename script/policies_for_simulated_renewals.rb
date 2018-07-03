28d84daf76cdfef5d7b5f1d50b7b2b47452a8077
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-07-03 11:23:24 -0400

    Refs #25579 filter initial vs renewal enrollments

--- script/policies_for_simulated_renewals.rb
+++ script/policies_for_simulated_renewals.rb
@@ -12,6 +12,12 @@ end
 
 start_on_date = window.end.next_month.beginning_of_month.to_time.utc.beginning_of_day
 
+plan_cache = {}
+
+Plan.all.each do |plan|
+  plan_cache[plan.id] = plan
+end
+
 def find_renewed_sponsorships(start_date)
   BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where({
     "benefit_applications" => {
@@ -29,6 +35,29 @@ def find_renewed_sponsorships(start_date)
   })
 end
 
+def matching_plan_details(enrollment, other_hbx_enrollment, plan_cache)
+  return false if other_hbx_enrollment.plan_id.blank?
+  new_plan = plan_cache[enrollment.plan_id]
+  old_plan = plan_cache[other_hbx_enrollment.plan_id]
+  (old_plan.carrier_profile_id == new_plan.carrier_profile_id) &&
+    (old_plan.active_year == new_plan.active_year - 1)
+end
+
+def initial_or_renewal(enrollment,plan_cache)
+  renewal_enrollments = enrollment.family.households.flat_map(&:hbx_enrollments).select do |hbx_enrollment|
+    hbx_enrollment.is_shop? &&
+      (hbx_enrollment.employee_role_id == enrollment.employee_role_id) &&
+      hbx_enrollment.terminated_on.blank? &&
+      matching_plan_details(enrollment, hbx_enrollment, plan_cache) &&
+      (!%w(coverage_terminated unverified void shopping coverage_canceled inactive).include?(hbx_enrollment.aasm_state))
+  end
+  if renewal_enrollments.any?
+    return "renewal"
+  else
+    return "initial"
+  end
+end
+
 renewed_sponsorships = find_renewed_sponsorships(start_on_date)
 
 f = File.open("policies_to_pull.txt","w")
@@ -44,8 +73,17 @@ renewed_sponsorships.each do |bs|
         :active].include?(ba.aasm_state)
   end
 
+  initial_enrollments = []
+
+  renewal_enrollments = []
+
   employer_enrollment_query = ::Queries::NamedEnrollmentQueries.find_simulated_renewal_enrollments(selected_application.sponsored_benefits, start_on_date)
   employer_enrollment_query.each do |enrollment_hbx_id|
+    enrollment = HbxEnrollment.by_hbx_id(enrollment_hbx_id).first
+    if initial_or_renewal(enrollment,plan_cache) == 'initial'
+      initial_enrollments << enrollment_hbx_id
+    elsif initial_or_renewal(enrollment,plan_cache) == 'renewal'
+      renewal_enrollments << enrollment_hbx_id
     f.puts(enrollment_hbx_id)
   end
 end
