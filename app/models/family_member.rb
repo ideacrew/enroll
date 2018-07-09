739ae54cc7eacfcce43f0b02d49b6e75186fecbc
sudhirreddysama <sudhirsama586@gmail.com>
2018-07-09 09:20:21 -0400

    refs# 23578 multiply benchmark with ehb

--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -90,7 +90,9 @@ class FamilyMember
     benefit_sponsorship = HbxProfile.current_hbx.benefit_sponsorship
     benefit_coverage_period = benefit_sponsorship.benefit_coverage_periods.detect {|bcp| bcp.contains?(TimeKeeper.datetime_of_record)}
     slcsp = benefit_coverage_period.second_lowest_cost_silver_plan
-    slcsp.premium_for(TimeKeeper.datetime_of_record, person.age_on(TimeKeeper.datetime_of_record))
+    ehb = benefit_coverage_period.second_lowest_cost_silver_plan.ehb
+    cost = slcsp.premium_for(TimeKeeper.datetime_of_record, person.age_on(TimeKeeper.datetime_of_record))
+    multiply_cost_with_ehb = cost * ehb
   end
 
   def broker=(new_broker)
