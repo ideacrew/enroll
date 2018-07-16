c333870a77818480f24c965eaa0be6df1f5c1618
Battula <vrbattula@outlook.com>
2018-07-16 14:20:08 -0400

    Refs #26047 Added condition for renewals to check only office location while force publishing BA.

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service.rb
@@ -64,7 +64,11 @@ module BenefitSponsors
 
     def force_submit_application_with_eligibility_errors
       unless business_policy_satisfied_for?(:submit_benefit_application)
-        if business_policy.fail_results.keys.include?(:employer_primary_office_location) || business_policy.fail_results.keys.include?(:benefit_application_fte_count)
+        if benefit_application.is_renewing?
+          if business_policy.fail_results.keys.include?(:employer_primary_office_location))
+            benefit_application.submit_for_review! if benefit_application.may_submit_for_review?
+          end
+        elsif business_policy.fail_results.keys.include?(:employer_primary_office_location) || business_policy.fail_results.keys.include?(:benefit_application_fte_count)
           benefit_application.submit_for_review! if benefit_application.may_submit_for_review?
         end
       end
