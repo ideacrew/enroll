9ba3306ccfe5463e38f37f5efee5d11c14220f0e
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-07-16 15:48:20 -0400

    fix date math

--- script/policies_for_simulated_renewals.rb
+++ script/policies_for_simulated_renewals.rb
@@ -44,12 +44,13 @@ def matching_plan_details(enrollment, other_hbx_enrollment, plan_cache)
 end
 
 def initial_or_renewal(enrollment,plan_cache,predecessor_id)
+  return "initial" if predecessor_id.blank?
   renewal_enrollments = enrollment.family.households.flat_map(&:hbx_enrollments).select{|hbx_enrollment| hbx_enrollment.sponsored_benefit_package_id == predecessor_id}
   reject_statuses = HbxEnrollment::CANCELED_STATUSES + HbxEnrollment::WAIVED_STATUSES + %w(unverified void)
   renewal_enrollments_no_cancels_waives = renewal_enrollments.reject{|ren| reject_statuses.include?(ren.aasm_state.to_s)}
-  renewal_enrollments_no_terms = renewal_enrollments_no_cancels_waives.reject{|ren| %w(coverage_terminated).include?(ren.aasm_state.to_s) && 
+  renewal_enrollments_no_terms = renewal_enrollments_no_cancels_waives.reject{|ren| %w(coverage_terminated coverage_termination_pending).include?(ren.aasm_state.to_s) && 
                                                                                     ren.terminated_on.present? && 
-                                                                                    ren.terminated_on < enrollment.effective_on}
+                                                                                    ren.terminated_on < (enrollment.effective_on - 1.day})
   renewal_plans = renewal_enrollments_no_terms.map(&:plan).compact.map(&:renewal_plan_id)
   if renewal_enrollments_no_terms.present? && renewal_plans.include?(enrollment.plan_id)
     return "renewal"
