4e896f806b8600c84ebbd70205404d4ad13a052e
saimekala07 <saimekala07@gmail.com>
2018-07-24 14:42:19 -0400

    refs #27044 TYPO

--- script/fix_benefit_application_for_mpy.rb
+++ script/fix_benefit_application_for_mpy.rb
@@ -2,9 +2,8 @@ BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(:"benefit_applica
   benefit_sponsorship.census_employees.each do |census|
     census.benefit_group_assignments.each do |benefit_group_assignment|
       if benefit_group_assignment.benefit_application.present?
-          benefit_group_assignment.update(is_active: false) if benefit_group_assignment.benefit_application.is_renewing? && benefit_group_assignment.is_active == true
-          benefit_group_assignment.update(is_active: true) if benefit_group_assignment.benefit_application.active? && benefit_group_assignment.is_active == false
-        end
+        benefit_group_assignment.update(is_active: false) if benefit_group_assignment.benefit_application.is_renewing? && benefit_group_assignment.is_active == true
+        benefit_group_assignment.update(is_active: true) if benefit_group_assignment.benefit_application.active? && benefit_group_assignment.is_active == false
       end
     end
   end
