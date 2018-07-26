478c7645dd5015cd57e7361e5a0731c41204057b
saikumar9 <saikumarkotagiri9@gmail.com>
2018-07-26 10:19:35 -0400

    Refactored rake to activate/deactivate employer link for broker

--- lib/tasks/activate_or_deactivate_employer_link_for_broker.rake
+++ lib/tasks/activate_or_deactivate_employer_link_for_broker.rake
@@ -1,8 +1,8 @@
 require File.join(Rails.root, "app", "data_migrations", "activate_or_deactivate_employer_link_for_broker")
 # This rake task is to enable or disable employer link for a broker
-# RAILS_ENV=production bundle exec rake migrations:activate_or_deactivate_employer_link_for_broker activate_fein=332623443 deactivate_fein=222222222
+# RAILS_ENV=production bundle exec rake migrations:activate_or_deactivate_employer_link_for_broker plan_design_org_id="sdjb43743vhg34"
 
 namespace :migrations do
   desc "enabling employer link for a broker"
-  ActivateOrDeactivateEmpoyerLinkForBroker.define_task :activate_or_deactivate_employer_link_for_broker => :environment
+  ActivateOrDeactivateEmployerLinkForBroker.define_task :activate_or_deactivate_employer_link_for_broker => :environment
 end
--- spec/data_migrations/activate_or_deactivate_employer_link_for_broker_spec.rb
+++ spec/data_migrations/activate_or_deactivate_employer_link_for_broker_spec.rb
@@ -4,7 +4,7 @@ require File.join(Rails.root, "app", "data_migrations", "activate_or_deactivate_
 describe BuildShopEnrollment do
 
   let(:given_task_name) { "activate_or_deactivate_employer_link_for_broker" }
-  subject { ActivateOrDeactivateEmpoyerLinkForBroker.new(given_task_name, double(:current_scope => nil)) }
+  subject { ActivateOrDeactivateEmployerLinkForBroker.new(given_task_name, double(:current_scope => nil)) }
 
   let!(:rating_area)                    { FactoryGirl.create_default :benefit_markets_locations_rating_area }
   let!(:service_area)                   { FactoryGirl.create_default :benefit_markets_locations_service_area }
@@ -24,22 +24,6 @@ describe BuildShopEnrollment do
                                             sic_code: employer_profile.sic_code
                                           )
                                         }
-  let!(:organization2)                   { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
-  let!(:employer_profile2)               { organization2.employer_profile }
-  let!(:broker_organization2)            { FactoryGirl.build(:benefit_sponsors_organizations_general_organization, site: site)}
-  let!(:broker_agency_profile2)         { FactoryGirl.create(:benefit_sponsors_organizations_broker_agency_profile, organization: broker_organization2, market_kind: 'shop', legal_name: 'Legal Name1') }
-
-  let(:inactive_plan_design_organization) {
-                                            SponsoredBenefits::Organizations::PlanDesignOrganization.new(
-                                              owner_profile_id: broker_agency_profile2._id,
-                                              sponsor_profile_id: employer_profile2._id,
-                                              office_locations: employer_profile2.office_locations,
-                                              fein: organization2.fein,
-                                              legal_name: organization2.legal_name,
-                                              has_active_broker_relationship: true,
-                                              sic_code: employer_profile2.sic_code
-                                            )
-                                          }
 
   describe "given a task name", dbclean: :after_each do
     it "has the given task name" do
@@ -52,54 +36,26 @@ describe BuildShopEnrollment do
     context "when both valid employer_profile feins is passed" do
       before :each do
         active_plan_design_organization.save!
-        inactive_plan_design_organization.save!
-        allow(ENV).to receive(:[]).with("activate_fein").and_return(organization.fein)
-        allow(ENV).to receive(:[]).with("deactivate_fein").and_return(organization2.fein)
-        subject.migrate
+        allow(ENV).to receive(:[]).with("plan_design_org_id").and_return(active_plan_design_organization.id)
       end
 
       it "should successfully update plan_design_organization1" do
-        active_plan_design_organization.reload
-        expect(active_plan_design_organization.has_active_broker_relationship).to eq true
-      end
-
-      it "should successfully update plan_design_organization2" do
-        inactive_plan_design_organization.reload
-        expect(inactive_plan_design_organization.has_active_broker_relationship).to eq false
-      end
-    end
-
-    context "when only one of the feins are valid" do
-      before :each do
-        active_plan_design_organization.save!
-        allow(ENV).to receive(:[]).with("activate_fein").and_return(organization.fein)
-        allow(ENV).to receive(:[]).with("deactivate_fein").and_return(broker_organization2.fein)
+        expect(active_plan_design_organization.has_active_broker_relationship).to eq false
         subject.migrate
-      end
-
-      it "should successfully update plan_design_organization1" do
         active_plan_design_organization.reload
         expect(active_plan_design_organization.has_active_broker_relationship).to eq true
-      end
-
-      it "should not update plan_design_organization2" do
-        expect(inactive_plan_design_organization.has_active_broker_relationship).to eq true
       end
     end
 
     context "when both invalid feins is passed" do
       before :each do
-        allow(ENV).to receive(:[]).with("activate_fein").and_return(broker_organization.fein)
-        allow(ENV).to receive(:[]).with("deactivate_fein").and_return(broker_organization2.fein)
-        subject.migrate
+        allow(ENV).to receive(:[]).with("plan_design_org_id").and_return(broker_organization.id)
       end
 
       it "should exit as there is no plan_design_organization for the given fein" do
         expect(active_plan_design_organization.has_active_broker_relationship).to eq false
-      end
-
-      it "should exit as there is no plan_design_organization for the given fein" do
-        expect(inactive_plan_design_organization.has_active_broker_relationship).to eq true
+        subject.migrate
+        expect(active_plan_design_organization.has_active_broker_relationship).to eq false
       end
     end
   end
