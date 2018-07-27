394167cf7d4096bdadafa8442e9e84237901dcaa
202chaz <chaz.haskins@gmail.com>
2018-07-27 11:03:22 -0400

    form updates

--- config/routes.rb
+++ config/routes.rb
@@ -42,9 +42,9 @@ Rails.application.routes.draw do
     post "/security_question_responses/replace", controller: "users/security_question_responses", action: 'replace'
 
     member do
-      get :reset_password, :lockable, :confirm_lock, :login_history, :edit
-      put :confirm_reset_password, :update
-      post :unlock, :change_password, :change_username, :change_email
+      get :reset_password, :lockable, :confirm_lock, :login_history, :change_username, :change_email, :check_for_existing_username_or_email, :edit
+      put :confirm_reset_password, :confirm_change_username, :confirm_change_email, :update
+      post :unlock, :change_password
     end
   end
 
