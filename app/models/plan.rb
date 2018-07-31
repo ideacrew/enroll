28d6ce32134154ad240f51c75e7169453b8a919a
Jaya Chandra Garapati <jayachandragarapati@ip-192-168-254-5.ec2.internal>
2018-07-31 16:10:54 -0400

    Ref #26819 code Refactor plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -465,9 +465,9 @@ class Plan
       when 'dental'
         Plan.individual_dental_by_active_year(active_year).with_premium_tables
       when 'health'
-        shopping_family_member_ids = tax_household.household.latest_created_hbx_enrollment.hbx_enrollment_members.map(&:applicant_id)
+        shopping_family_member_ids = tax_household.household.latest_created_hbx_enrollment.hbx_enrollment_members.map(&:applicant_id) rescue nil
         csr_kind = tax_household.latest_eligibility_determination.csr_eligibility_kind rescue nil
-        csr_kind = tax_household.tax_household_members.where(:applicant_id.in =>  shopping_family_member_ids).map(&:is_ia_eligible).include?(false) ? "csr_100" : csr_kind
+        csr_kind = tax_household.tax_household_members.where(:applicant_id.in =>  shopping_family_member_ids).map(&:is_ia_eligible).include?(false) ? "csr_100" : csr_kind rescue nil
         if csr_kind.present?
           Plan.individual_health_by_active_year_and_csr_kind_with_catastrophic(active_year, csr_kind).with_premium_tables
         else
