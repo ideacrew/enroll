ba060ae12fa48d751ece00972936d66bd760f1ed
venkatbhattiprolu <vbhat847@gmail.com>
2018-08-01 16:46:21 -0400

    refs#27033 mod to the delete condition

--- app/data_migrations/remove_invalid_coverage_household_member.rb
+++ app/data_migrations/remove_invalid_coverage_household_member.rb
@@ -9,11 +9,9 @@ class RemoveInvalidCoverageHouseholdMember < MongoidMigrationTask
       action = ENV['action'].to_s
       ch = person.first.primary_family.active_household.coverage_households.where(:is_immediate_family => true).first
       if action == "remove_invalid_chms"
-        ch.coverage_household_members.delete_if do |chm| 
-          (chm.family_member_id.present? && !chm.family.family_members.map(&:id).compact.flatten.map(&:to_s).include?(chm.family_member_id.to_s)) || chm.family_member.blank?
-          puts "Removed invalid coverage household members." unless Rails.env.test?
-        end
-      else 
+        ch.coverage_household_members.delete_if { |chm| ((chm.family_member_id.present? && !chm.family.family_members.map(&:id).compact.flatten.map(&:to_s).include?(chm.family_member_id.to_s)) || chm.family_member.blank?) }
+        puts "Removed invalid coverage household members." unless Rails.env.test?
+      else
         coverage_household_member = ch.coverage_household_members.where(family_member_id: family_member_id).first
         chm = ch.remove_coverage_household_member(coverage_household_member_id, family_member_id) if coverage_household_member.present?
         puts "removed invalid coverage household member: #{coverage_household_member_id}" unless Rails.env.test?
