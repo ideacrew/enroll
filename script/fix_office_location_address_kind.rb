07cdae952f8ea07340c5b0ddf526efa9c098fa1e
saimekala07 <saimekala07@gmail.com>
2018-08-02 15:37:19 -0400

    refs #25819 Adding script to fix primary office location address kind

---
+++ script/fix_office_location_address_kind.rb
@@ -0,0 +1,8 @@
+ BenefitSponsors::Organizations::Organization.all.employer_profiles.each do |organization|
+   organization.employer_profile.office_locations.each do |office_location|
+     if office_location.is_primary
+       BenefitSponsors::Organizations::AcaShopCcaEmployerProfile.skip_callback(:update, :after, :notify_observers)
+       office_location.address.update_attributes(kind: 'primary')
+     end
+   end
+ end
