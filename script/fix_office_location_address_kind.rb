1961fc31cb47b3dcd878a0d3a542c2969b66d56d
saimekala07 <saimekala07@gmail.com>
2018-08-02 16:17:19 -0400

    refs #25819 Adding more checks

--- script/fix_office_location_address_kind.rb
+++ script/fix_office_location_address_kind.rb
@@ -1,6 +1,6 @@
  BenefitSponsors::Organizations::Organization.all.employer_profiles.each do |organization|
    organization.employer_profile.office_locations.each do |office_location|
-     if office_location.is_primary
+     if office_location.is_primary && office_location.address.kind == "work"
        BenefitSponsors::Organizations::AcaShopCcaEmployerProfile.skip_callback(:update, :after, :notify_observers)
        office_location.address.update_attributes(kind: 'primary')
      end
