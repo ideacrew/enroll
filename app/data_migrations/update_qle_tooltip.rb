d1fd96404a6dd9e16fe651c0aa15531bfd449dc7
saip7795 <saip7795@gmail.com>
2018-08-02 12:46:51 -0400

    Ref25415: Updated the rake task to accept variables and the text as well

--- app/data_migrations/update_qle_tooltip.rb
+++ app/data_migrations/update_qle_tooltip.rb
@@ -2,10 +2,14 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateQleTooltip< MongoidMigrationTask
 
   def migrate
-    qles = QualifyingLifeEventKind.where(title: 'Entered into a legal domestic partnership').first
+  	qle_title = ENV['title']
+    qles = QualifyingLifeEventKind.where(title: qle_title).first
     if qles.present?
-      qles.update_attributes(tool_tip: 'Entering a domestic partnership as permitted or recognized by the Massachusetts')
-      puts "updated qle tooltip successfully."
+    	tooltip_text= ENV['text']
+      qles.update_attributes(tool_tip: tooltip_text)
+      puts "updated qle tooltip successfully." unless Rails.env.test?
+    else
+    	puts "Qle tooltip not found." unless Rails.env.test?
     end
   end
 end
