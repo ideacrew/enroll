c1d9151b55999f8bd628c330fc856563b285745e
saikumar9 <saikumarkotagiri9@gmail.com>
2018-08-06 12:49:05 -0400

    broker migration

---
+++ app/data_migrations/migrate_brokers_as_exempt_organizations.rb
@@ -0,0 +1,35 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+require 'csv'
+
+class MigrateBrokersAsExemptOrganizations < MongoidMigrationTask
+  def migrate
+    field_names  = %w(
+                      Organization_hbx_id
+                      Organization_legal_name
+                      Organization_type
+                     )
+    Dir.mkdir("hbx_report") unless File.exists?("hbx_report")
+    file_name = "#{Rails.root}/hbx_report/broker_general_organizations_report_#{TimeKeeper.datetime_of_record.strftime("%m_%d_%Y_%H_%M_%S")}.csv"
+    CSV.open(file_name, "w", force_quotes: true) do |csv|
+      csv << field_names
+      organizations = ::BenefitSponsors::Organizations::Organization.all.broker_agency_profiles.where(_type: "BenefitSponsors::Organizations::GeneralOrganization")
+      organizations.each do |organization|
+        csv << [
+                organization.hbx_id,
+                organization.legal_name,
+                organization._type
+               ]
+        begin
+          if organization.profiles.count == 1
+            organization.update_attributes!(_type: "BenefitSponsors::Organizations::ExemptOrganization")
+            puts "Successfully migrated broker organization as exempt organization, organization_legal_name: #{organization.legal_name}, organization_hbx_id: #{organization.hbx_id}"
+          else
+            puts "Cannot migrate the broker organization because this organization has more than two profiles, organization_legal_name: #{organization.legal_name}, organization_hbx_id: #{organization.hbx_id}"
+          end
+        rescue => e
+          puts "Failed to migrate organization: #{organization.legal_name}, organization_hbx_id: #{organization.hbx_id} because of the error: #{e.backtrace}"
+        end
+      end
+    end
+  end
+end
