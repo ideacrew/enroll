567f884a56463c84ab845f2b21cd1cf89cf2259d
Prem Shaganti <prem.shaganti@dc.gov>
2018-08-06 14:57:32 -0400

    ref:address multiple officelocations issue

--- app/javascript/benefit_sponsors/controllers/office_locations_controller.js
+++ app/javascript/benefit_sponsors/controllers/office_locations_controller.js
@@ -10,24 +10,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "officeLocations", "officeLocation" ]
+  static targets = [ "officeLocations", "officeLocation" ];
 
   addLocation() {
     //clone new location node, unhide remove button, modify name attribute
     var newLocation = document.importNode(this.officeLocationTarget, true)
     var totalLocations = document.importNode(this.officeLocationsTarget, true)
     // totalLocationsCount includes currently loaded OL form too
-    var totalLocationsCount = totalLocations.querySelectorAll('.ol_title').length
+    var totalLocationsCount = totalLocations.querySelectorAll('.ol_title').length;
     newLocation.querySelectorAll('.js-remove').forEach(function(element) {
       element.remove()
-    })
+    });
 
     newLocation.querySelectorAll('.ol_title').forEach(function(element) {
       element.innerHTML = "Office Location"
-    })
+    });
 
     newLocation.querySelectorAll('input').forEach(function(input) {
-      var name = input.getAttribute('name').replace('[0]', `[${totalLocationsCount}]`)
+      var name = input.getAttribute('name').replace('[0]', `[${totalLocationsCount}]`);
       input.setAttribute('name', name)
       input.value = ''
     })
@@ -35,7 +35,7 @@ export default class extends Controller {
     newLocation.querySelector('input[placeholder="ZIP"]').setAttribute('data-action', "")
 
     newLocation.querySelectorAll('select').forEach(function(input) {
-      var name = input.getAttribute('name').replace('[0]', `[${totalLocationsCount}]`)
+      var name = input.getAttribute('name').replace('[0]', `[${totalLocationsCount}]`);
       input.setAttribute('name', name)
 
       if (input.value != "phone main" && input.id != "kindSelect") {
@@ -56,6 +56,6 @@ export default class extends Controller {
     event.target.closest('.js-office-location').querySelectorAll('input[id="delete_location"]').forEach(function(input) {
       input.setAttribute('value', true)
     })
-    event.target.closest('.js-office-location').hide();
+    $(event.target).closest('.js-office-location').hide();
   }
 }
