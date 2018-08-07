a16a693dcf5aa403a0fb8cbd2f74c003bd13636f
Tony Schaffert <antonio.schaffert@gmail.com>
2018-08-07 12:31:00 -0400

    custom node config for vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const customConfig = require('./custom')
+
+// Merge custom config
+environment.config.merge(customConfig)
 
 environment.loaders.append('vue', vue)
 module.exports = environment
