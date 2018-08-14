8a51ffa653d1dd47c890751217c9c4df8da390d2
sudhirreddysama <sudhirsama586@gmail.com>
2018-08-14 10:46:51 -0400

    refs# 25400 Pay Now Kaiser-Store Transaction ID in EA DB

--- app/models/family.rb
+++ app/models/family.rb
@@ -52,6 +52,7 @@ class Family
   embeds_many :broker_agency_accounts, class_name: "BenefitSponsors::Accounts::BrokerAgencyAccount"
   embeds_many :general_agency_accounts
   embeds_many :documents, as: :documentable
+  has_many :payment_transactions
 
   after_initialize :build_household
   before_save :clear_blank_fields
