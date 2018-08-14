dc4f4d86c279e5c5018f8ace87f4bb61d07c203d
saikumar9 <saikumarkotagiri9@gmail.com>
2018-08-14 14:58:08 -0400

    WIP - create eligibility, additional PDC types
    cherry pick for new model

--- spec/controllers/exchanges/hbx_profiles_controller_spec.rb
+++ spec/controllers/exchanges/hbx_profiles_controller_spec.rb
@@ -648,9 +648,8 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :around_each do
       expect(response).to render_template('new_eligibility')
     end
 
-    #TODO: fix me when create eligibility feature is merged and working.
     context 'when can_add_pdc permission is not given' do
-      xit 'should not render the new_eligibility partial' do
+      it 'should not render the new_eligibility partial' do
         sign_in(user)
         get :new_eligibility, params: params, xhr: true, format: :js
 
