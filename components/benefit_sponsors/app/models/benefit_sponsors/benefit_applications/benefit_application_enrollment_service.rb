c555d5a93f2e5f79c56f0c0ced8be0e0062c80c4
saip7795 <saip7795@gmail.com>
2018-08-15 15:29:05 -0400

    Ref26438: Code change to make sure the enrollments are terminated on correct date

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service.rb
@@ -166,12 +166,10 @@ module BenefitSponsors
     def terminate(end_on, termination_date)
       if business_policy_satisfied_for?(:terminate_benefit)
         if benefit_application.may_terminate_enrollment?
-          benefit_application.terminate_enrollment!
-          if benefit_application.terminated?
             updated_dates = benefit_application.effective_period.min.to_date..end_on
             benefit_application.update_attributes!(:effective_period => updated_dates, :terminated_on => termination_date)
+            benefit_application.terminate_enrollment!
           end
-        end
       else
         [false, benefit_application, business_policy.fail_results]
       end
