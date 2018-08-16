ab137b2445cae34e322054575c0f2d1fdf5f3a72
vsonnathi <sonnathivinil@gmail.com>
2018-08-16 11:41:07 -0400

    united.

--- app/data_migrations/load_issuer_profiles.rb
+++ app/data_migrations/load_issuer_profiles.rb
@@ -4,17 +4,17 @@ class LoadIssuerProfiles < MongoidMigrationTask
 
   def migrate
     issuer_profile_data = [
-      {fein: "043373331", legal_name: "BMC HealthNet Plan", abbreviation: "BMCHP", hbx_carrier_id: 20003, issuer_hios_ids: "82569", shop_health: true },
-      {fein: "237442369", legal_name: "Fallon Health", abbreviation: "FCHP", hbx_carrier_id: 20005, issuer_hios_ids: "88806", shop_health: true },
-      {fein: "042864973", legal_name: "Health New England", abbreviation: "HNE", hbx_carrier_id: 20007, issuer_hios_ids: "34484", shop_health: true },
-      {fein: "041045815", legal_name: "Blue Cross Blue Shield MA", abbreviation: "BCBS", hbx_carrier_id: 20002, issuer_hios_ids: "42690", shop_health: true },
-      {fein: "042452600", legal_name: "Harvard Pilgrim Health Care", abbreviation: "HPHC", hbx_carrier_id: 20008, issuer_hios_ids: "36046", shop_health: true },
-      {fein: "234547586", legal_name: "Neighborhood Health Plan", abbreviation: "NHP", hbx_carrier_id: 20010, issuer_hios_ids: "41304", shop_health: true },
-      {fein: "800721489", legal_name: "Tufts Health Direct", abbreviation: "THPD", hbx_carrier_id: 20011, issuer_hios_ids: "59763", shop_health: true },
+      {fein: "043373331", issuer_hios_ids: "82569", legal_name: "BMC HealthNet Plan", abbreviation: "BMCHP", hbx_carrier_id: 20003, shop_health: true },
+      {fein: "237442369", issuer_hios_ids: "88806", legal_name: "Fallon Health", abbreviation: "FCHP", hbx_carrier_id: 20005, shop_health: true },
+      {fein: "042864973", issuer_hios_ids: "34484", legal_name: "Health New England", abbreviation: "HNE", hbx_carrier_id: 20007, shop_health: true },
+      {fein: "041045815", issuer_hios_ids: "42690", legal_name: "Blue Cross Blue Shield MA", abbreviation: "BCBS", hbx_carrier_id: 20002, shop_health: true },
+      {fein: "042452600", issuer_hios_ids: "36046", legal_name: "Harvard Pilgrim Health Care", abbreviation: "HPHC", hbx_carrier_id: 20008, shop_health: true },
+      {fein: "234547586", issuer_hios_ids: "41304", legal_name: "Neighborhood Health Plan", abbreviation: "NHP", hbx_carrier_id: 20010, shop_health: true },
+      {fein: "800721489", issuer_hios_ids: "59763", legal_name: "Tufts Health Direct", abbreviation: "THPD", hbx_carrier_id: 20011, shop_health: true },
 
       {fein: "362739571", legal_name: "UnitedHealthcare", abbreviation: "UHIC", hbx_carrier_id: 20014, issuer_hios_ids: "31779", shop_health: true },
-      # {fein: "050513223", legal_name: "Altus", abbreviation: "ALT", hbx_carrier_id: 20001, issuer_hios_ids: "18076", shop_dental: true },
-      # {fein: "046143185", legal_name: "Delta Dental", abbreviation: "DDA", hbx_carrier_id: 20004, issuer_hios_ids: "80538,11821", shop_dental: true }
+      # {fein: "050513223", issuer_hios_ids: "18076", legal_name: "Altus", abbreviation: "ALT", hbx_carrier_id: 20001, shop_dental: true },
+      # {fein: "046143185", issuer_hios_ids: "80538,11821", legal_name: "Delta Dental", abbreviation: "DDA", hbx_carrier_id: 20004, shop_dental: true }
     ]
 
     site_key = "cca"
