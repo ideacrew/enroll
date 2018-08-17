86e2c9d0e27378b57a354a201374301e6526e977
saikumar9 <saikumarkotagiri9@gmail.com>
2018-08-17 11:36:30 -0400

    tests
    41557 - cherry pick for new model

--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -276,3 +276,39 @@ describe "financial assistance eligibiltiy for a family", type: :model, dbclean:
   end
 
 end
+
+describe Household, "for creating a new taxhousehold using create eligibility", type: :model, dbclean: :after_each do
+  let!(:person100) {FactoryGirl.create(:person)}
+  let!(:family100) {FactoryGirl.create(:family, :with_primary_family_member, person: person100)}
+  let(:household100) {family100.active_household}
+  let!(:hbx_profile100) {FactoryGirl.create(:hbx_profile)}
+  let(:current_date) {TimeKeeper.date_of_record}
+  let(:params) do
+    {
+      "person_id" => person100.id.to_s,
+      "family_actions_id" => "family_actions_#{family100.id}",
+      "max_aptc" => '100.00',
+      "csr" => '94',
+      "effective_date" => "#{current_date.year}-#{current_date.month}-#{current_date.day}",
+      "family_members" => {family100.primary_applicant.person.hbx_id => {'pdc_type' => 'is_ia_eligible', 'reason' => '7hvgds'}}
+    }
+  end
+
+  context "create_new_tax_household" do
+    before :each do
+      household100.create_new_tax_household(params)
+    end
+
+    it "should create new tax_household instance" do
+      expect(household100.tax_households).not_to be []
+    end
+
+    it "should create new tax_household_member instance" do
+      expect(household100.tax_households[0].tax_household_members).not_to be []
+    end
+
+    it "should create new eligibility_determination instance" do
+      expect(household100.tax_households[0].eligibility_determinations).not_to be []
+    end
+  end
+end
