5784cdd0ba385c0ecd0cadb0c3a8eb5fe4d5f2ee
saip7795 <saip7795@gmail.com>
2018-08-21 13:57:53 -0400

    Ref28798: modfying the existing rake task to activate bga for all the ee's of a particular er

--- app/data_migrations/activate_benefit_group_assignment.rb
+++ app/data_migrations/activate_benefit_group_assignment.rb
@@ -2,6 +2,19 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class ActivateBenefitGroupAssignment < MongoidMigrationTask
   def migrate
+    action = ENV['action'].to_s
+    case action
+      when "update_benefit_group_assignment_for_er"
+        update_benefit_group_assignment_for_er
+      when "update_benefit_group_assignment_for_ce"
+        update_benefit_group_assignment_for_ce
+      else
+        puts"The Action defined is not performed in the rake task" unless Rails.env.test?
+    end
+  end
+
+
+  def update_benefit_group_assignment_for_ce
     begin
       census_employee = CensusEmployee.by_ssn(ENV['ce_ssn']).first
       if census_employee.nil?
@@ -13,6 +26,20 @@ class ActivateBenefitGroupAssignment < MongoidMigrationTask
     rescue => e
       e.message
     end
+  end
 
+  
+  def update_benefit_group_assignment_for_er  
+    begin
+      benefit_package_id = ENV['benefit_package_id']      
+      benefit_package = ::BenefitSponsors::BenefitPackages::BenefitPackage.find(benefit_package_id)
+      if benefit_package.nil?
+        puts "No census employee was found with given ssn"
+      else
+        benefit_package.activate_benefit_group_assignments
+      end
+    rescue => e
+      e.message
+    end
   end
 end
--- spec/data_migrations/activate_benefit_group_assignment_spec.rb
+++ spec/data_migrations/activate_benefit_group_assignment_spec.rb
@@ -12,7 +12,7 @@ describe ActivateBenefitGroupAssignment do
     end
   end
 
-  describe "activate benefit group assignment" do
+  describe "activate benefit group assignment for particular census employee" do
 
     # let!(:benefit_group1)     { FactoryGirl.create(:benefit_group, plan_year: plan_year)}
     # let!(:benefit_group2)     { FactoryGirl.create(:benefit_group, plan_year: plan_year)}
@@ -25,20 +25,111 @@ describe ActivateBenefitGroupAssignment do
     before(:each) do
       allow(ENV).to receive(:[]).with("ce_ssn").and_return(census_employee.ssn)
       allow(ENV).to receive(:[]).with("bga_id").and_return(benefit_group_assignment1.id)
+      allow(ENV).to receive(:[]).with("action").and_return("update_benefit_group_assignment_for_ce")
     end
 
-    context "activate_benefit_group_assignment", dbclean: :after_each do
-      it "should activate_related_benefit_group_assignment" do
+    context "activate_benefit_group_assignmentfor_particular_census_employee", dbclean: :after_each do
+      it "should activate_related_benefit_group_assignment_for_particular_census_employee" do
         expect(benefit_group_assignment1.is_active).to eq false
         subject.migrate
         census_employee.reload
         expect(census_employee.benefit_group_assignments.where(id:benefit_group_assignment1.id).first.is_active).to eq true
       end
-      it "should_not activate_unrelated_benefit_group_assignment" do
+      it "should_not activate_unrelated_benefit_group_assignment_for_particular_census_employee" do
         expect(benefit_group_assignment2.is_active).to eq false
         subject.migrate
         census_employee.reload
         expect(census_employee.benefit_group_assignments.where(id:benefit_group_assignment2.id).first.is_active).to eq false
+      end
+    end
+  end
+
+    describe "activate benefit group assignment for all the census employees for an employer" do
+    let(:employer_organization)  { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+    let(:site)  { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+    let(:current_effective_date)  { TimeKeeper.date_of_record }
+    let!(:benefit_market_catalog) { create(:benefit_markets_benefit_market_catalog, :with_product_packages,
+                                            benefit_market: benefit_market,
+                                            title: "SHOP Benefits for #{current_effective_date.year}",
+                                            application_period: (current_effective_date.beginning_of_year..current_effective_date.end_of_year))
+                                          }
+    let(:benefit_market)      { site.benefit_markets.first }
+    let!(:product_package) { benefit_market_catalog.product_packages.first }
+    let!(:rating_area)   { FactoryGirl.create_default :benefit_markets_locations_rating_area }
+    let!(:service_area)  { FactoryGirl.create_default :benefit_markets_locations_service_area }
+    let(:organization) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+    let!(:employer_attestation)     { BenefitSponsors::Documents::EmployerAttestation.new(aasm_state: "approved") }
+    let(:benefit_sponsorship) do
+      FactoryGirl.create(
+        :benefit_sponsors_benefit_sponsorship,
+        :with_rating_area,
+        :with_service_areas,
+        supplied_rating_area: rating_area,
+        service_area_list: [service_area],
+        organization: organization,
+        profile_id: organization.profiles.first.id,
+        benefit_market: site.benefit_markets[0],
+        employer_attestation: employer_attestation)
+    end
+
+    let(:start_on)  { current_effective_date.prev_month }
+    let(:effective_period)  { start_on..start_on.next_year.prev_day }
+    let!(:benefit_application) {
+      application = FactoryGirl.create(:benefit_sponsors_benefit_application, :with_benefit_sponsor_catalog, benefit_sponsorship: benefit_sponsorship, effective_period: effective_period, aasm_state: :canceled)
+      application.benefit_sponsor_catalog.save!
+      application
+    }
+    let!(:benefit_application_1) {
+      application = FactoryGirl.create(:benefit_sponsors_benefit_application, :with_benefit_sponsor_catalog, benefit_sponsorship: benefit_sponsorship, effective_period: effective_period, aasm_state: :enrolling)
+      application.benefit_sponsor_catalog.save!
+      application
+    }
+
+    let!(:benefit_package) { FactoryGirl.create(:benefit_sponsors_benefit_packages_benefit_package, benefit_application: benefit_application, product_package: product_package) }
+    let!(:benefit_package_1) { FactoryGirl.create(:benefit_sponsors_benefit_packages_benefit_package, benefit_application: benefit_application_1, product_package: product_package) }
+    let(:benefit_group_assignment) {FactoryGirl.build(:benefit_sponsors_benefit_group_assignment, benefit_group: benefit_package)}
+    let(:benefit_group_assignment_1) {FactoryGirl.build(:benefit_sponsors_benefit_group_assignment, benefit_group: benefit_package_1, is_active: "false")}
+    let(:employee_role) { FactoryGirl.create(:benefit_sponsors_employee_role, person: person, employer_profile: benefit_sponsorship.profile, census_employee_id: census_employee.id) }
+    let(:census_employee) { FactoryGirl.create(:benefit_sponsors_census_employee,
+      employer_profile: benefit_sponsorship.profile,
+      benefit_sponsorship: benefit_sponsorship,
+      benefit_group_assignments: [benefit_group_assignment,benefit_group_assignment_1]
+    )}
+    let(:person) { FactoryGirl.create(:person) }
+    let!(:family) { FactoryGirl.create(:family, :with_primary_family_member, person: person)}
+
+    let(:employee_role_1) { FactoryGirl.create(:benefit_sponsors_employee_role, person: person_1, employer_profile: benefit_sponsorship.profile, census_employee_id: census_employee_1.id) }
+    let(:census_employee_1) { FactoryGirl.create(:benefit_sponsors_census_employee,
+      employer_profile: benefit_sponsorship.profile,
+      benefit_sponsorship: benefit_sponsorship,
+      benefit_group_assignments: [benefit_group_assignment,benefit_group_assignment_1]
+    )}
+    let(:person_1) { FactoryGirl.create(:person) }
+    let!(:family_1) { FactoryGirl.create(:family, :with_primary_family_member, person: person_1)}
+
+
+    before(:each) do
+      allow(ENV).to receive(:[]).with("benefit_package_id").and_return(benefit_package_1.id.to_s)
+      allow(ENV).to receive(:[]).with("action").and_return("update_benefit_group_assignment_for_er")
+    end
+
+    context "activate_benefit_group_assignment for all the employees", dbclean: :after_each do
+      it "should activate_related_benefit_group_assignment_for_all_the_census_employees" do
+        expect(benefit_group_assignment_1.is_active).to eq false
+        subject.migrate
+        census_employee.reload
+        census_employee_1.reload
+        expect(census_employee.benefit_group_assignments.where(id:benefit_group_assignment_1.id).first.is_active).to eq true
+        expect(census_employee_1.benefit_group_assignments.where(id:benefit_group_assignment_1.id).first.is_active).to eq true
+      end
+      it "should_not activate_unrelated_benefit_group_assignment_for_all_the_census_employees" do
+        census_employee.benefit_group_assignments.where(id:benefit_group_assignment.id).first.update_attributes!(is_active:"false")
+        census_employee_1.benefit_group_assignments.where(id:benefit_group_assignment.id).first.update_attributes!(is_active:"false")
+        subject.migrate
+        census_employee.reload
+        census_employee_1.reload
+        expect(census_employee.benefit_group_assignments.where(id:benefit_group_assignment.id).first.is_active).to eq false
+        expect(census_employee_1.benefit_group_assignments.where(id:benefit_group_assignment.id).first.is_active).to eq false
       end
     end
   end
