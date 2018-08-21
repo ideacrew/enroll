c938665ea87a49620931cc0c6a0ecc8b895ab02b
saipraveen18 <saipraveen.gudimetla@gmail.com>
2018-08-21 15:50:01 -0400

    refs #25390 show pay now button only for intial kaiser enrollment

--- app/helpers/insured/plan_shopping/receipt_helper.rb
+++ app/helpers/insured/plan_shopping/receipt_helper.rb
@@ -1,19 +1,23 @@
 module Insured::PlanShopping::ReceiptHelper
   def show_pay_now?
-    carrier_with_payment_option? && individual? && initial_enrollment?
+    (carrier_with_payment_option? && individual? && (!has_any_previous_kaiser_enrollments? || has_break_in_coverage_enrollments?))
   end
 
   def carrier_with_payment_option?
-    @enrollment.plan.carrier_profile.legal_name == "Kaiser"
+    @enrollment.plan.carrier_profile.legal_name == 'Kaiser'
   end
 
   def individual?
-    @enrollment.kind == 'individual'
+    @enrollment.kind == ('individual' || 'coverall')
   end
 
-  def initial_enrollment?
-    #transaction_id created_at effective year
-    # can be added when backend part will be done.
-    true
+  def has_any_previous_kaiser_enrollments?
+    all_kaiser_enrollments = @enrollment.family.enrollments.select { |enr| enr.plan.carrier_profile.legal_name == 'Kaiser' && enr.effective_on.year == @enrollment.effective_on.year }
+    enrollments = all_kaiser_enrollments - @enrollment.to_a
+    enrollments.present? ? true : false
+  end
+
+  def has_break_in_coverage_enrollments?
+    @enrollment.family.enrollments.terminated.any? { |enr| enr.plan.carrier_profile.legal_name == 'Kaiser' &&  enr.terminated_on.year == @enrollment.effective_on.year && (@enrollment.effective_on - enr.terminated_on) > 1 }
   end
 end
