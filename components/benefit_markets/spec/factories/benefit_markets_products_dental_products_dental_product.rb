0b4279850fce07eaec4e549aad0472a9db807824
raghuram <raghuramg83@gmail.com>
2018-08-22 16:58:52 -0400

    Updated product package, product factories and benefit_market shared context

--- components/benefit_markets/spec/factories/benefit_markets_products_dental_products_dental_product.rb
+++ components/benefit_markets/spec/factories/benefit_markets_products_dental_products_dental_product.rb
@@ -18,6 +18,22 @@ FactoryGirl.define do
     
     # association :service_area, factory: :benefit_markets_locations_service_area, strategy: :create
 
+    trait :with_renewal_product do
+      transient do
+        renewal_service_area nil
+      end
+
+      before(:create) do |product, evaluator|
+        renewal_product = create(:benefit_markets_products_dental_products_dental_product,
+          application_period: (product.application_period.min.next_year..product.application_period.max.next_year),
+          product_package_kinds: product.product_package_kinds,
+          service_area: evaluator.renewal_service_area,
+          metal_level_kind: product.metal_level_kind)
+
+        product.renewal_product_id = renewal_product.id
+      end
+    end
+
     after(:build) do |product, evaluator|
       product.premium_tables << build_list(:benefit_markets_products_premium_table, 1, effective_period: product.application_period)
     end
--- components/benefit_markets/spec/factories/benefit_markets_products_product_packages.rb
+++ components/benefit_markets/spec/factories/benefit_markets_products_product_packages.rb
@@ -27,18 +27,24 @@ FactoryGirl.define do
 
       case product_package.product_kind
       when :health
-        product_package.products = create_list(:benefit_markets_products_health_products_health_product,
-          evaluator.number_of_products,
-          application_period: product_package.application_period,
-          product_package_kinds: [ product_package.package_kind ],
-          service_area: service_area,
-          metal_level_kind: :gold)
+        product_package.products = BenefitMarkets::Products::HealthProducts::HealthProduct.by_product_package(product_package).to_a
+        if product_package.products.blank?
+          product_package.products = create_list(:benefit_markets_products_health_products_health_product,
+            evaluator.number_of_products,
+            application_period: product_package.application_period,
+            product_package_kinds: [ product_package.package_kind ],
+            service_area: service_area,
+            metal_level_kind: :gold)
+        end
       when :dental
-        product_package.products = create_list(:benefit_markets_products_dental_products_dental_product,
-          evaluator.number_of_products,
-          application_period: product_package.application_period,
-          product_package_kinds: [ product_package.package_kind ],
-          service_area: service_area)
+        product_package.products = BenefitMarkets::Products::DentalProducts::DentalProduct.by_product_package(product_package).to_a
+        if product_package.products.blank?
+          product_package.products = create_list(:benefit_markets_products_dental_products_dental_product,
+            evaluator.number_of_products,
+            application_period: product_package.application_period,
+            product_package_kinds: [ product_package.package_kind ],
+            service_area: service_area)
+        end
       end
     end
   end
