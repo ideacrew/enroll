839161f04976ad44a914507233d46785dd5335b4
darisipudi <abhinavdarisipudi@ip-192-168-254-5.ec2.internal>
2018-08-28 14:27:58 -0400

    28143: updating rake

--- app/data_migrations/update_family_members_index.rb
+++ app/data_migrations/update_family_members_index.rb
@@ -14,16 +14,19 @@ class UpdateFamilyMembersIndex < MongoidMigrationTask
     end
   end
 
+  def primary_person
+    Person.where(hbx_id: ENV['primary_hbx']).first if Person.present?
+  end
 
   def update_family_member_index
     begin
-      primary_person = Person.where(hbx_id: ENV['primary_hbx']).first
       dependent_person = Person.where(hbx_id: ENV['dependent_hbx']).first
       if primary_person.present? && dependent_person.present?
         family_members = primary_person.families.first.family_members
         family_members.where(id: ENV['dependent_family_id']).first.unset(:person_id)
         family_members.where(id: ENV['primary_family_id']).first.update_attributes(person_id: primary_person.id, is_primary_applicant: true)
         family_members.where(id: ENV['dependent_family_id']).first.update_attributes(person_id: dependent_person.id, is_primary_applicant: false)
+        puts "family_member_index_updated"
       else
         raise "some error person with hbx_id:#{ENV['primary_hbx']} and hbx_id:#{ENV['dependent_hbx']} not found"
       end
@@ -33,13 +36,13 @@ class UpdateFamilyMembersIndex < MongoidMigrationTask
 
   def update_family_member_id
     begin
-      primary_person = Person.where(hbx_id: ENV['primary_hbx']).first
       coverage_household = primary_person.primary_family.active_household.coverage_households.where(:is_immediate_family => true).first
       old_family_id= ENV['old_family_id'].to_s
       correct_family_id = ENV['correct_family_id'].to_s
       if primary_person.present? 
         coverage_household_member = coverage_household.coverage_household_members.where(family_member_id: old_family_id).first if old_family_id.present?
         coverage_household_member.update_attributes!(family_member_id: correct_family_id)
+        puts "family_member_id_updated"
       end
     rescue => e
       puts "error: #{e.backtrace}" unless Rails.env.test?
