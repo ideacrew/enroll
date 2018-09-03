2d21618c0b819aeeea5e1c2ca3a0f31f4f61e11a
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2018-09-03 12:41:17 -0400

    Refs #29847 #29846 redirect payment request to EA instead of non existing yet kaiser payment redirection URI. build temporary testing view.

--- app/controllers/payment_transactions_controller.rb
+++ app/controllers/payment_transactions_controller.rb
@@ -6,9 +6,9 @@ class PaymentTransactionsController < ApplicationController
   def generate_saml_response
     hbx_enrollment = ::HbxEnrollment.by_hbx_id(params[:enrollment_id]).first
     payment = PaymentTransaction.build_payment_instance(hbx_enrollment)
-    saml_generator = OneLogin::RubySaml::SamlGenerator.new(payment.payment_transaction_id,hbx_enrollment)
+    saml_generator = OneLogin::RubySaml::SamlGenerator.new(payment.payment_transaction_id, hbx_enrollment)
     response_doc = saml_generator.build_saml_response
     @saml_response = saml_generator.encode_saml_response(response_doc)
-    render json: {response: @saml_response}
+    render json: {"SAMLResponse": @saml_response}
   end
 end
