18fa5b4827ecc255f4187929e2587607001c7645
harshaellanki <h_vardhan@yahoo.com>
2018-09-11 11:15:00 -0400

    ref:19070 Notice to renewal EEs that ER is terminated from SHOP

--- app/models/plan_year.rb
+++ app/models/plan_year.rb
@@ -861,7 +861,7 @@ class PlanYear
       transitions from: :renewing_enrolled,   to: :active,              :guard  => :is_event_date_valid?
       transitions from: :renewing_published,  to: :renewing_enrolling,  :guard  => :is_event_date_valid?
       transitions from: :renewing_enrolling,  to: :renewing_enrolled,   :guards => [:is_open_enrollment_closed?, :is_enrollment_valid?]
-      transitions from: :renewing_enrolling,  to: :renewing_application_ineligible, :guard => :is_open_enrollment_closed?, :after => [:renewal_employer_ineligibility_notice, :zero_employees_on_roster]
+      transitions from: :renewing_enrolling,  to: :renewing_application_ineligible, :guard => :is_open_enrollment_closed?, :after => [:renewal_employer_ineligibility_notice, :zero_employees_on_roster, :renewal_employer_ineligibility_notice_to_employee]
 
       transitions from: :enrolling, to: :enrolling  # prevents error when plan year is already enrolling
     end
@@ -1213,6 +1213,17 @@ class PlanYear
       self.employer_profile.trigger_notices("renewal_employer_ineligibility_notice", "acapi_trigger" =>  true)
     rescue Exception => e
       Rails.logger.error { "Unable to deliver employer renewal ineligiblity denial notice for #{self.employer_profile.organization.legal_name} due to #{e}" }
+    end
+  end
+
+  def renewal_employer_ineligibility_notice_to_employee
+     return true if benefit_groups.any?{|bg| bg.is_congress?}
+      self.employer_profile.census_employees.non_terminated.each do |ce|
+       begin
+         ShopNoticesNotifierJob.perform_later(ce.id.to_s, "renewal_employer_ineligibility_notice_to_employee", "acapi_trigger" =>  true)
+       rescue Exception => e
+          Rails.logger.error { "Unable to deliver employer renewal ineligiblity denial notice for #{self.employer_profile.organization.legal_name} due to #{e}" }
+      end
     end
   end
 
