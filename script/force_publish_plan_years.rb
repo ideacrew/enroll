75e789c61fc8953dabb647ca46f13a495dc4a1ef
Zachary Landes <zach.g.landes@gmail.com>
2018-09-12 11:42:04 -0400

    updating force publish script to output to rails logger

--- script/force_publish_plan_years.rb
+++ script/force_publish_plan_years.rb
@@ -1,3 +1,5 @@
+@logger = Logger.new("#{Rails.root}/log/force_publish_begin_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log")
+
 Organization.where({
   :'employer_profile.plan_years' =>
   { :$elemMatch => {
@@ -8,7 +10,8 @@ Organization.where({
   py = org.employer_profile.renewing_plan_year
   if py.may_force_publish? && py.is_application_valid?
     org.employer_profile.renewing_plan_year.force_publish!
+    @logger.debug "Employer #{org.fein} has been force published"
   else
-    puts "Organization:#{org.fein} plan_year_errors: #{py.errors} & application_warnings: #{py.application_eligibility_warnings}"
+    @logger.debug "Employer #{org.fein} contains plan_year_errors: #{py.errors} & application_warnings: #{py.application_eligibility_warnings}"
   end 
 end
