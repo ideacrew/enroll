cb523f4cd905d9967fe5c7d52966d640f3fb2a02
Jayanth <jayanth.rordev@gmail.com>
2018-09-13 16:57:03 -0400

    refs#27403 Fixed indentation

--- app/notices/shop_employee_notices/termination_of_employers_health_coverage.rb
+++ app/notices/shop_employee_notices/termination_of_employers_health_coverage.rb
@@ -1,5 +1,5 @@
 class ShopEmployeeNotices::TerminationOfEmployersHealthCoverage < ShopEmployeeNotice
-   def deliver
+  def deliver
     build
     append_data
     generate_pdf_notice
@@ -8,12 +8,13 @@ class ShopEmployeeNotices::TerminationOfEmployersHealthCoverage < ShopEmployeeNo
     upload_and_send_secure_message
     send_generic_notice_alert
   end
-   def append_data
+   
+  def append_data
     active_plan_year = census_employee.employer_profile.plan_years.where(:aasm_state.in => PlanYear::INITIAL_ELIGIBLE_STATE).first
     notice.plan_year = PdfTemplates::PlanYear.new({
                       :start_on => active_plan_year.start_on
       })
-     ben_sponsorship = HbxProfile.current_hbx.benefit_sponsorship
+    ben_sponsorship = HbxProfile.current_hbx.benefit_sponsorship
     current_open_enrollment_coverage = ben_sponsorship.benefit_coverage_periods.where(open_enrollment_end_on: Settings.aca.individual_market.open_enrollment.end_on ).first
     bc_period = current_open_enrollment_coverage.open_enrollment_end_on >= TimeKeeper.date_of_record ? current_open_enrollment_coverage : ben_sponsorship.renewal_benefit_coverage_period
      notice.enrollment = PdfTemplates::Enrollment.new({
