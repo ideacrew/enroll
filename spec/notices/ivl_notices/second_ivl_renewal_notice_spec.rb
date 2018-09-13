c6de742664578632407189881378a4ddd563853f
saikumar9 <saikumarkotagiri9@gmail.com>
2018-09-13 15:52:24 -0400

    fix for failing specs

--- spec/notices/ivl_notices/second_ivl_renewal_notice_spec.rb
+++ spec/notices/ivl_notices/second_ivl_renewal_notice_spec.rb
@@ -31,10 +31,13 @@ RSpec.describe IvlNotices::SecondIvlRenewalNotice, :dbclean => :after_each do
   }}
   let!(:hbx_profile) { FactoryGirl.create(:hbx_profile, :open_enrollment_coverage_period) }
 
+  before :each do
+    allow(person).to receive("primary_family").and_return(family)
+    allow(person.consumer_role).to receive_message_chain("person.primary_family").and_return(false)
+    allow(person.consumer_role).to receive_message_chain("person.families.first.primary_applicant.person").and_return(person)
+  end
+
   describe "New" do
-    before do
-      allow(person.consumer_role).to receive_message_chain("person.families.first.primary_applicant.person").and_return(person)
-    end
     context "valid params" do
       it "should initialze" do
         expect{IvlNotices::SecondIvlRenewalNotice.new(person.consumer_role, valid_parmas)}.not_to raise_error
@@ -53,8 +56,6 @@ RSpec.describe IvlNotices::SecondIvlRenewalNotice, :dbclean => :after_each do
 
   describe "#build" do
     before do
-      allow(person).to receive("primary_family").and_return(family)
-      allow(person.consumer_role).to receive_message_chain("person.families.first.primary_applicant.person").and_return(person)
       @proj_eligibility_notice = IvlNotices::SecondIvlRenewalNotice.new(person.consumer_role, valid_parmas)
       @proj_eligibility_notice.build
     end
@@ -75,8 +76,6 @@ RSpec.describe IvlNotices::SecondIvlRenewalNotice, :dbclean => :after_each do
 
   describe "#append_open_enrollment_data" do
     before do
-      allow(person).to receive("primary_family").and_return(family)
-      allow(person.consumer_role).to receive_message_chain("person.families.first.primary_applicant.person").and_return(person)
       @proj_eligibility_notice = IvlNotices::SecondIvlRenewalNotice.new(person.consumer_role, valid_parmas)
       @proj_eligibility_notice.build
     end
@@ -92,8 +91,6 @@ RSpec.describe IvlNotices::SecondIvlRenewalNotice, :dbclean => :after_each do
 
   describe "#generate_pdf_notice" do
     before do
-      allow(person).to receive("primary_family").and_return(family)
-      allow(person.consumer_role).to receive_message_chain("person.families.first.primary_applicant.person").and_return(person)
       @proj_eligibility_notice = IvlNotices::SecondIvlRenewalNotice.new(person.consumer_role, valid_parmas)
     end
 
@@ -109,5 +106,4 @@ RSpec.describe IvlNotices::SecondIvlRenewalNotice, :dbclean => :after_each do
       expect(File.exist?(file.path)).to be true
     end
   end
-
 end
