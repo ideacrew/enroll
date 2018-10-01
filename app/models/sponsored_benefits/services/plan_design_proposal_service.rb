484b734600733ed387c89cf364412892fba3de10
nisanth <nisanth.yaganti@gmail.com>
2018-10-01 16:55:23 -0400

    saving/updating health & dental benfits through service in bqt

--- app/models/sponsored_benefits/services/plan_design_proposal_service.rb
+++ app/models/sponsored_benefits/services/plan_design_proposal_service.rb
@@ -28,17 +28,57 @@ module SponsoredBenefits
       end
 
       def ensure_dental_benefits
-        if benefit_group.build_dental_relationship_benefits.empty?
+        if benefit_group.dental_relationship_benefits.empty?
           benefit_group.build_dental_relationship_benefits 
         end
         benefit_group
       end
 
       def save_health_benefits(attrs={})
-        benefit_group(attrs)
+        benefit_group = benefit_group(attrs)
+        update_benefits(attrs) if benefit_group.persisted?
+        benefit_group.elected_plans = benefit_group.elected_plans_by_option_kind
+
+        if benefit_group.sole_source?
+          benefit_group.build_relationship_benefits
+          benefit_group.estimate_composite_rates
+        else
+          benefit_group.build_composite_tier_contributions
+        end
+        benefit_group.set_bounding_cost_plans
       end
 
-      def save_dental_benfits(attrs={})
+      def save_dental_benefits(attrs={})
+        update_benefits(attrs)
+        benefit_group.elected_dental_plans = benefit_group.elected_dental_plans_by_option_kind
+        benefit_group.set_bounding_cost_dental_plans
+      end
+
+      def update_benefits(attrs={})
+        self.send("reset_#{kind}_benefits")
+        benefit_group.title = "Benefit Group Created for: #{plan_design_organization.legal_name} by #{plan_design_organization.broker_agency_profile.legal_name}"
+        if is_dental_benefits?
+          benefit_group.update_attributes({
+            dental_plan_option_kind: attrs[:plan_option_kind],
+            dental_reference_plan_id: attrs[:reference_plan_id],
+            dental_relationship_benefits_attributes: attrs[:relationship_benefits_attributes]
+          })
+        else
+          benefit_group.update_attributes(attrs)
+        end
+      end
+
+      def reset_health_benefits
+        benefit_group.composite_tier_contributions.destroy_all
+        benefit_group.relationship_benefits.destroy_all
+      end
+
+      def reset_dental_benefits
+        benefit_group.dental_relationship_benefits.destroy_all
+      end
+
+      def plan_design_organization
+        proposal.plan_design_organization
       end
 
       def benefit_group(attrs={})
@@ -50,6 +90,10 @@ module SponsoredBenefits
         return @application if defined? @application
         sponsorship = proposal.profile.benefit_sponsorships.first
         @application = sponsorship.benefit_applications.first
+      end
+
+      def is_dental_benefits?
+        kind == "dental"
       end
     end
   end
