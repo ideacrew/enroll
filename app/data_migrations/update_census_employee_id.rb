f602d237ba4827fce985773654c4732b1a377911
darisipudi <abhinavdarisipudi@ip-192-168-254-10.ec2.internal>
2018-10-01 16:54:17 -0400

    31338 - spec for update census employee id

--- app/data_migrations/update_census_employee_id.rb
+++ app/data_migrations/update_census_employee_id.rb
@@ -1,19 +1,15 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateCensusEmployeeId< MongoidMigrationTask
   def migrate
-  	begin
       id = ENV["employee_role_id"].to_s
-      census_employee_id = ENV['census_employee_id']
+      input_id = ENV['input_id']
       employee_role = EmployeeRole.find(id)
        
       if employee_role.nil?
-      	puts "Employee Role nil" unless Rails.env.test?
+        puts "Employee Role nil" unless Rails.env.test?
       end
-
-      employee_role.update_attributes(census_employee_id: census_employee_id)
+      
+      employee_role.update_attributes(census_employee_id: input_id)
       puts 'Updated census empoyee id' unless Rails.env.test? 
-    rescue Exception => e
-     puts e.message
-    end  
   end
 end
