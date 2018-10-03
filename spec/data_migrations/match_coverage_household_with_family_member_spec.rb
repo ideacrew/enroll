82495b296feebb23cc7afa7258c84678745e676e
GouthamRedd <gouti1234@gmail.com>
2018-10-03 17:24:24 -0400

    refactor the rake, match_coverage_household_with_family_member

--- spec/data_migrations/match_coverage_household_with_family_member_spec.rb
+++ spec/data_migrations/match_coverage_household_with_family_member_spec.rb
@@ -2,9 +2,20 @@ require "rails_helper"
 require File.join(Rails.root, "app", "data_migrations", "match_coverage_household_with_family_member")
 
 describe MatchCoverageHouseholdWithFamilyMember, dbclean: :after_each do
+  before do
+    DatabaseCleaner.clean
+  end
 
   let(:given_task_name) { "match_coverage_household_with_family_member" }
   subject { MatchCoverageHouseholdWithFamilyMember.new(given_task_name, double(:current_scope => nil)) }
+  let!(:family11) { FactoryGirl.create(:family, :with_primary_family_member_and_dependent)}
+  let!(:person11) { family11.primary_applicant.person }
+
+  before :each do
+    person11.update_attributes!(hbx_id: "1009988")
+    @immediate_coverage_household = family11.active_household.immediate_family_coverage_household
+    @extended_coverage_household = family11.active_household.extended_family_coverage_household
+  end
 
   describe "given a task name" do
     it "has the given task name" do
@@ -14,33 +25,29 @@ describe MatchCoverageHouseholdWithFamilyMember, dbclean: :after_each do
 
   describe "matching coverage household with family members", dbclean: :after_each do
 
-    let(:person) { FactoryGirl.create(:person) }
-    let(:family) { FactoryGirl.create(:family, :with_primary_family_member_and_dependent, person: person)}
-    let(:coverage_household) { family.latest_household.coverage_households.first }
-
-    context 'When family member has empty coverage household members' do
-      before do
-        allow(ENV).to receive(:[]).with('hbx_id').and_return person.hbx_id
-        coverage_household.coverage_household_members.last.destroy
+    context "for a case where hbx_id is wrong" do
+      before :each do
+        allow(ENV).to receive(:[]).with('hbx_id').and_return "1009987"
       end
-      it "should add a coverage household members for familiy members" do
-        expect(coverage_household.coverage_household_members.size).to eq(2)
-        subject.migrate
-        coverage_household.reload
-        expect(coverage_household.coverage_household_members.size).to eq(3)
+
+      it "should throw an error" do
+        expect {subject.migrate}.to raise_error(RuntimeError, "More/No people found with the given hbx_id: 1009987")
       end
     end
 
-    context 'When family member has unnecessary  coverage household family members' do
-      before do
-        allow(ENV).to receive(:[]).with('hbx_id').and_return person.hbx_id
-        coverage_household.coverage_household_members.create(family_member_id: 'hffh5as76d57a')
+    context "for a case where hbx_id is correct" do
+      before :each do
+        person11.person_relationships.each { |pr| pr.update_attributes!(kind: "parent") }
+        allow(ENV).to receive(:[]).with('hbx_id').and_return person11.hbx_id
       end
-      it "should remove the coverage household members that is not related to family members" do
-        expect(coverage_household.coverage_household_members.size).to eq(4)
+
+      it "should sort coverage_household_members as per relationships" do
+        expect(@immediate_coverage_household.coverage_household_members.count).to eq 3
+        expect(@extended_coverage_household.coverage_household_members.count).to eq 0
         subject.migrate
-        coverage_household.reload
-        expect(coverage_household.coverage_household_members.size).to eq(3)
+        family11.active_household.coverage_households.map(&:reload)
+        expect(@immediate_coverage_household.coverage_household_members.count).to eq 1
+        expect(@extended_coverage_household.coverage_household_members.count).to eq 2
       end
     end
   end
