6a21de06ac5e627a2ba43765e4c95fef8324b7e6
darisipudi <abhinavdarisipudi@ip-192-168-254-2.ec2.internal>
2018-10-05 15:04:50 -0400

    31338 - typo

--- app/data_migrations/update_census_employee_id.rb
+++ app/data_migrations/update_census_employee_id.rb
@@ -2,7 +2,7 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateCensusEmployeeId< MongoidMigrationTask
   def migrate
       id = ENV["employee_role_id"].to_s
-      input_id = ENV['input_id']
+      input_id = ENV['census_employee_id']
       employee_role = EmployeeRole.find(id)
        
       if employee_role.nil?
