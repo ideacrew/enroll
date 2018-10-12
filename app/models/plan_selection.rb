af6b9b1920ed29766c3ccb3e6a938a2e81f3a1a6
Battula <vrbattula@outlook.com>
2018-10-12 11:36:11 -0400

    refs #21598 Fix failing cucumbers.

--- app/models/plan_selection.rb
+++ app/models/plan_selection.rb
@@ -44,9 +44,11 @@ class PlanSelection
       hbx_enrollment.move_to_contingent!
     else
       hbx_enrollment.select_coverage!(qle: qle)
-      plan_year = hbx_enrollment.benefit_group.plan_year
-      if plan_year.termination_pending?
-        plan_year.terminate_employee_enrollments(plan_year.end_on, transmit_xml:true)
+      if market_kind == 'shop'
+        plan_year = hbx_enrollment.benefit_group.plan_year
+        if plan_year.termination_pending?
+          plan_year.terminate_employee_enrollments(plan_year.end_on, transmit_xml:true)
+        end
       end
     end
   end
