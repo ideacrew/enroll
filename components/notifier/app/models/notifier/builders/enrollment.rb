7eecc91511b3985ba3c23f1060ef6986e715cdc3
Battula <vrbattula@outlook.com>
2018-10-22 15:10:47 -0400

    refs #27580 Include dental enrollment.

--- components/notifier/app/models/notifier/builders/enrollment.rb
+++ components/notifier/app/models/notifier/builders/enrollment.rb
@@ -5,15 +5,19 @@ module Notifier
       return @enrollment if defined? @enrollment
       if payload['event_object_kind'].constantize == HbxEnrollment
         @enrollment = HbxEnrollment.find(payload['event_object_id'])
-      elsif ["employee_notice_for_employee_terminated_from_roster", "initial_employee_plan_selection_confirmation"].include?(event_name)
+      elsif event_matched?
         @enrollment = health_enrollment
       end
     end
 
     def dental_enrollment
-      if event_name == "employee_notice_for_employee_terminated_from_roster"
+      if event_matched?
         enrollments.by_coverage_kind("dental").first
       end
+    end
+
+    def event_matched?
+      ["employee_notice_for_employee_terminated_from_roster", "initial_employee_plan_selection_confirmation"].include?(event_name)
     end
 
     def enrollments
