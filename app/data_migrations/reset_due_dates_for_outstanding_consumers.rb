18424d2bc0ac94798acb10697ce5ac129a604db7
venkatbhattiprolu <vbhat847@gmail.com>
2018-10-24 13:46:00 -0400

    refs#36711 updating rake to pick if dep is outstanding

--- app/data_migrations/reset_due_dates_for_outstanding_consumers.rb
+++ app/data_migrations/reset_due_dates_for_outstanding_consumers.rb
@@ -2,11 +2,11 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class ResetDueDatesForOutstandingConsumers < MongoidMigrationTask
   def migrate
-    Person.all_consumer_roles.where(:"consumer_role.aasm_state" => "verification_outstanding").each do |person|
+    family = Family.by_enrollment_individual_market.where(:"households.hbx_enrollments"=>{"$elemMatch"=>
+      {:aasm_state => "coverage_selected", :effective_on => { :"$gte" => TimeKeeper.date_of_record.beginning_of_year,
+        :"$lte" =>  TimeKeeper.date_of_record.end_of_year }}}).each do |family|
       begin
-        family = person.primary_family
-        next unless family
-        family.active_household.hbx_enrollments.individual_market.enrolled.each do |hbx_enrollment|
+        family.active_household.hbx_enrollments.individual_market.where(aasm_state: "coverage_selected").each do |hbx_enrollment|
           if hbx_enrollment.is_any_member_outstanding? && hbx_enrollment.may_move_to_contingent?
             family.set_due_date_on_verification_types
             hbx_enrollment.move_to_contingent!
@@ -19,4 +19,3 @@ class ResetDueDatesForOutstandingConsumers < MongoidMigrationTask
     end
   end
 end
-
