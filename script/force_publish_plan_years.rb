00a864d527d4abcee800de341bddce1410d25510
Nikhil S <nikhilks219@gmail.com>
2018-10-24 12:31:59 -0400

    updating benefit package and saving census employee

--- script/force_publish_plan_years.rb
+++ script/force_publish_plan_years.rb
@@ -1,5 +1,3 @@
-
-
 class ForcePublishPlanYears
   
   def initialize(publish_date, current_date)  
@@ -7,6 +5,16 @@ class ForcePublishPlanYears
     @current_date = current_date
   end
   
+  def renewing_draft_plan_years
+    Organization.where({
+      :'employer_profile.plan_years' =>
+      { :$elemMatch => {
+        :start_on => @publish_date,
+        :aasm_state => 'renewing_draft'
+        }}
+    })
+  end
+
   def call 
     revert_plan_years #revert plan years that are in renewing_published to renewing_draft and set back their OE start dates    
     assign_packages #assign benefit packages to census employeess missing them    
@@ -34,52 +42,30 @@ class ForcePublishPlanYears
   def assign_packages
     CSV.open("#{Rails.root}/unnassigned_packages_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.csv", "w") do |csv|
       csv << ["Org", "Employee"]
-      Organization.where({
-        :'employer_profile.plan_years' =>
-        { :$elemMatch => {
-          :start_on => @publish_date,
-          :aasm_state => 'renewing_draft'
-          }}
-          }).each do |org|
+      renewing_draft_plan_years.each do |org|
         py = org.employer_profile.renewing_plan_year
         if py.application_errors.present?
-          org.employer_profile.census_employees.each do |ce|
-            if unassigned(ce)
-              data = [org.fein, ce.full_name]   
-              csv << data 
-            end
-          end
+          save_ce(org, py, csv)
         end
       end
     end 
   end
-  def unassigned(census_employee)
-    if py = census_employee.employer_profile.plan_years.renewing.first
-      if census_employee.benefit_group_assignments.where(:benefit_group_id.in => py.benefit_groups.map(&:id)).blank?
-         census_employee.try(:add_renew_benefit_group_assignment, py.benefit_groups.first)
-         if census_employee.benefit_group_assignments.where(:benefit_group_id.in => py.benefit_groups.map(&:id)).present?
-          return false
-        else 
-          return true
-        end
-      else 
-        return true
+
+  def save_ce(org, py, csv)
+    org.employer_profile.census_employees.each do |ce|
+      if ce.benefit_group_assignments.where(:benefit_group_id.in => py.benefit_groups.map(&:id)).blank?
+        data = [org.fein, ce.full_name]  
+        csv << data 
+        ce.try(:save!)
       end
     end
   end
-  
 
   def force_publish
-    Organization.where({
-      :'employer_profile.plan_years' =>
-        { :$elemMatch => {
-          :start_on => @publish_date,
-          :aasm_state => 'renewing_draft'
-          }}
-      }).each do |org|
-        py = org.employer_profile.renewing_plan_year
-          org.employer_profile.renewing_plan_year.force_publish! if py.may_force_publish? && py.is_application_valid?
-      end
+    renewing_draft_plan_years.each do |org|
+      py = org.employer_profile.renewing_plan_year
+      org.employer_profile.renewing_plan_year.force_publish! if py.may_force_publish? && py.is_application_valid?
+    end
   end
 
   def clean_up
@@ -103,24 +89,15 @@ class ForcePublishPlanYears
       end
     end
   end
-
-
-
   
   def set_back_oe_date
-    Organization.where({
-      :'employer_profile.plan_years' =>
-      { :$elemMatch => {
-        :start_on => @publish_date,
-        :aasm_state => 'renewing_draft'
-        }}
-        }).each do |org|
-          py = org.employer_profile.plan_years.last
-          if py.open_enrollment_start_on > @current_date
-            py.update_attributes!(open_enrollment_start_on: @current_date)
-            py.save!
-          end
+    renewing_draft_plan_years.each do |org|
+      py = org.employer_profile.plan_years.last
+      if py.open_enrollment_start_on > @current_date
+        py.update_attributes!(open_enrollment_start_on: @current_date)
+        py.save!
       end
+    end
   end
 end
 
