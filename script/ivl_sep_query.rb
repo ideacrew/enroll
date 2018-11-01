dc4c7834020884b936ec61837bda51762b9f5442
Raghuram <raghuramg83@gmail.com>
2018-11-01 18:41:54 -0400

    Refs #37377 Refactored IVL sep query and added specs

--- script/ivl_sep_query.rb
+++ script/ivl_sep_query.rb
@@ -16,76 +16,93 @@ class IvlEnrollmentsPublisher
   end
 end
 
-enrollment_kinds = %w(employer_sponsored employer_sponsored_cobra)
-active_statuses = %w(coverage_selected auto_renewing unverified renewing_coverage_selected auto_renewing_contingent renewing_contingent_selected)
 
-purchases = Family.collection.aggregate([
-  {"$match" => {
-    "households.hbx_enrollments.workflow_state_transitions" => {
-      "$elemMatch" => {
-        "to_state" => {"$in" => active_statuses},
-        "transition_at" => {
-           "$gte" => start_time,
-           "$lt" => end_time
-        }
-      }
-    }
-  }},
-  {"$unwind" => "$households"},
-  {"$unwind" => "$households.hbx_enrollments"},
-  {"$match" => {
-    "households.hbx_enrollments.workflow_state_transitions" => {
-      "$elemMatch" => {
-        "to_state" => {"$in" => active_statuses},
-        "transition_at" => {
-           "$gte" => start_time,
-           "$lt" => end_time
-        }
-      }
-    },
-    "households.hbx_enrollments.kind" => {"$nin" => enrollment_kinds}
-  }},
-  {"$group" => {
-    "_id" => "$households.hbx_enrollments.hbx_id",
-    "enrollment_state" => {"$last" => "$households.hbx_enrollments.aasm_state"}
-  }},
-  {"$project" => {
-    "_id" => 1,
-    "enrollment_state" => "$enrollment_state"
-  }}
-])
+class IvlEnrollmentsQuery
 
-terms = Family.collection.aggregate([
-  {"$match" => {
-    "households.hbx_enrollments.workflow_state_transitions" => {
-      "$elemMatch" => {
-        "to_state" => {"$in" => ["coverage_terminated","coverage_canceled"]},
-        "transition_at" => {
-           "$gte" => start_time,
-           "$lt" => end_time
-        }
-      }
-    }
-  }},
-  {"$unwind" => "$households"},
-  {"$unwind" => "$households.hbx_enrollments"},
-  {"$match" => {
-    "households.hbx_enrollments.workflow_state_transitions" => {
-      "$elemMatch" => {
-        "to_state" => {"$in" => ["coverage_terminated","coverage_canceled"]},
-        "transition_at" => {
-           "$gte" => start_time,
-           "$lt" => end_time
-        }
-      }
-    },
-    "households.hbx_enrollments.kind" => {"$nin" => enrollment_kinds}
-  }},
-  {"$group" => {"_id" => "$households.hbx_enrollments.hbx_id"}}
-])
+  attr_reader :start_time, :end_time
+  
+  ENROLLMENT_KINDS = %w(employer_sponsored employer_sponsored_cobra)
+  ACTIVE_STATUSES = %w(coverage_selected auto_renewing unverified renewing_coverage_selected auto_renewing_contingent renewing_contingent_selected)
 
-puts purchases.count
-puts terms.count
+  def initialize(start_time, end_time)
+    @start_time = start_time
+    @end_time = end_time
+  end
+
+  def purchases
+    Family.collection.aggregate([
+      {"$match" => {
+        "households.hbx_enrollments.workflow_state_transitions" => {
+          "$elemMatch" => {
+            "to_state" => {"$in" => ACTIVE_STATUSES},
+            "transition_at" => {
+               "$gte" => start_time,
+               "$lt" => end_time
+            }
+          }
+        }
+      }},
+      {"$unwind" => "$households"},
+      {"$unwind" => "$households.hbx_enrollments"},
+      {"$match" => {
+        "households.hbx_enrollments.workflow_state_transitions" => {
+          "$elemMatch" => {
+            "to_state" => {"$in" => ACTIVE_STATUSES},
+            "transition_at" => {
+               "$gte" => start_time,
+               "$lt" => end_time
+            }
+          }
+        },
+        "households.hbx_enrollments.kind" => {"$nin" => ENROLLMENT_KINDS}
+      }},
+      {"$group" => {
+        "_id" => "$households.hbx_enrollments.hbx_id",
+        "enrollment_state" => {"$last" => "$households.hbx_enrollments.aasm_state"}
+      }},
+      {"$project" => {
+        "_id" => 1,
+        "enrollment_state" => "$enrollment_state"
+      }}
+    ])
+  end
+
+  def terminations
+    Family.collection.aggregate([
+      {"$match" => {
+        "households.hbx_enrollments.workflow_state_transitions" => {
+          "$elemMatch" => {
+            "to_state" => {"$in" => ["coverage_terminated","coverage_canceled"]},
+            "transition_at" => {
+               "$gte" => start_time,
+               "$lt" => end_time
+            }
+          }
+        }
+      }},
+      {"$unwind" => "$households"},
+      {"$unwind" => "$households.hbx_enrollments"},
+      {"$match" => {
+        "households.hbx_enrollments.workflow_state_transitions" => {
+          "$elemMatch" => {
+            "to_state" => {"$in" => ["coverage_terminated","coverage_canceled"]},
+            "transition_at" => {
+               "$gte" => start_time,
+               "$lt" => end_time
+            }
+          }
+        },
+        "households.hbx_enrollments.kind" => {"$nin" => ENROLLMENT_KINDS}
+      }},
+      {"$group" => {"_id" => "$households.hbx_enrollments.hbx_id"}}
+    ])
+  end
+end
+
+enrollment_query = IvlEnrollmentsQuery.new(start_time, end_time)
+
+purchases = enrollment_query.purchases
+terms = enrollment_query.terminations
 
 purchase_event = "acapi.info.events.hbx_enrollment.coverage_selected"
 purchases.each do |rec|
