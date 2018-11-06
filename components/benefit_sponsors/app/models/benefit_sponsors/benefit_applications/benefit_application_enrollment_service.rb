0724588378baa6855d7e4e19dd084c14007991b2
saimekala07 <saimekala07@gmail.com>
2018-11-06 16:47:00 -0500

    refs #37023 #37008 #36983 #36995 Adding comment

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service.rb
@@ -142,6 +142,7 @@ module BenefitSponsors
     def cancel(notify_trading_partner)
       if business_policy_satisfied_for?(:cancel_benefit)
         if benefit_application.may_cancel?
+          # update end of canceled to start date here...
           benefit_application.cancel!(notify_trading_partner)
         else
           raise StandardError, "Benefit cancel state transition failed"
