c5a967c5bf8619562d16556f6789a3e5e4fde67b
nisanth <nisanth.yaganti@gmail.com>
2018-11-09 10:39:59 -0500

    adding site sign in url to email bosy & settings.yml

--- app/helpers/config/site_helper.rb
+++ app/helpers/config/site_helper.rb
@@ -176,6 +176,10 @@ module Config::SiteHelper
     Settings.site.invoice_bill_url
   end
 
+  def site_user_sign_in_url
+    Settings.site.user_sign_in_url
+  end
+
   def mail_address
     Settings.site.mail_address
   end
