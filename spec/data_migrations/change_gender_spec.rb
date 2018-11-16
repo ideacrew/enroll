87375c15e6dd4e932dc4680b8fbfcb5584ad66b9
Ji Wang <traceyw@vt.edu>
2018-11-16 09:21:16 -0500

    add negative case spec

--- spec/data_migrations/change_gender_spec.rb
+++ spec/data_migrations/change_gender_spec.rb
@@ -34,9 +34,6 @@ describe ChangeGender, dbclean: :after_each do
 
   describe "changing gender for a person without employee role ", dbclean: :after_each do
     let(:person) { FactoryGirl.create(:person, gender: "male") }
-    # let(:employer_profile) { FactoryGirl.create(:employer_profile)}
-    # let(:census_employee) { FactoryGirl.create(:census_employee, employer_profile: employer_profile, gender: "male") }
-
     before(:each) do
       allow(ENV).to receive(:[]).with("hbx_id").and_return(person.hbx_id)
       allow(ENV).to receive(:[]).with("ce_id").and_return("")
@@ -47,6 +44,19 @@ describe ChangeGender, dbclean: :after_each do
       subject.migrate
       person.reload
       expect(person.gender).to eq "female"
+    end
+  end
+  describe "do not change gender for an invalid person input", dbclean: :after_each do
+    let(:person) { FactoryGirl.create(:person, gender: "male") }
+    before(:each) do
+      allow(ENV).to receive(:[]).with("hbx_id").and_return("")
+      allow(ENV).to receive(:[]).with("ce_id").and_return("")
+      allow(ENV).to receive(:[]).with("gender").and_return("female")
+    end
+    it "should change the gender" do
+      subject.migrate
+      person.reload
+      expect(person.gender).to eq "male"
     end
   end
 end
