64c157571aff104a7f33496ab8c25f1ba6d8c3a4
saikumar9 <saikumarkotagiri9@gmail.com>
2018-11-19 15:08:40 -0500

    Refs #37447, code refactor

--- app/models/factories/employer_enroll_factory.rb
+++ app/models/factories/employer_enroll_factory.rb
@@ -38,36 +38,10 @@ module Factories
 
     def end
       @logger.debug "Processing #{employer_profile.legal_name}"
-      result = @employer_profile.plan_years.any?{ |plan_year| plan_year.benefit_groups.any?{|bg| bg.is_congress?}}
-      @logger.debug "Is congresstional employer? #{result}"
       expiring_plan_years = @employer_profile.plan_years.published_or_renewing_published.where(:"end_on".lt => (@date || TimeKeeper.date_of_record))
       expiring_plan_years.no_timeout.each do |expiring_plan_year|
         begin
-          bg_ids = expiring_plan_year.benefit_groups.collect(&:_id).uniq
-          query = { :benefit_group_id.in => bg_ids }
-          families = Family.where("households.hbx_enrollments" => {:$elemMatch => query})
-
-          families.no_timeout.each do |family|
-            begin
-              @logger.info "Processing family with family_id: #{family.id.to_s}"
-              family.active_household.hbx_enrollments.enrolled_and_renewing.where(query).each do |enrollment|
-                begin
-                  @logger.info "Processing enrollment with hbx_id: #{enrollment.hbx_id}"
-                  enrollment.expire_coverage! if enrollment.may_expire_coverage?
-                  if !enrollment.benefit_group_assignment_id.blank?
-                    assignment = enrollment.benefit_group_assignment
-                    assignment.expire_coverage! if assignment.may_expire_coverage?
-                    assignment.update_attributes(is_active: false) if assignment.is_active?
-                  end
-                rescue Exception => e
-                  @logger.debug "Exception #{e.backtrace} occured for enrollment with hbx_id #{enrollment.hbx_id}"
-                end
-              end
-            rescue => e
-              @logger.debug "Cannot process enrollments for family with id: #{family.id}, primary_person_hbx_id: #{family.primary_person.hbx_id}, Error: #{e.backtrace}"
-            end
-          end
-          expiring_plan_year.expire! if expiring_plan_year.may_expire?
+          expire_enrollments_and_bgas(expiring_plan_year)
         rescue => e
           @logger.debug "Cannot process plan_year with id: #{expiring_plan_year.id.to_s}"
         end
@@ -121,6 +95,32 @@ module Factories
           end
         end
       end
+    end
+
+    def expire_enrollments_and_bgas(expiring_plan_year)
+      bg_ids = expiring_plan_year.benefit_groups.collect(&:_id).uniq
+      query = { :benefit_group_id.in => bg_ids }
+      families = Family.where("households.hbx_enrollments" => {:$elemMatch => query})
+
+      families.no_timeout.each do |family|
+        begin
+          family.active_household.hbx_enrollments.enrolled_and_renewing.where(query).each do |enrollment|
+            begin
+              enrollment.expire_coverage! if enrollment.may_expire_coverage?
+              if !enrollment.benefit_group_assignment_id.blank?
+                assignment = enrollment.benefit_group_assignment
+                assignment.expire_coverage! if assignment.may_expire_coverage?
+                assignment.update_attributes(is_active: false) if assignment.is_active?
+              end
+            rescue Exception => e
+              @logger.debug "Exception #{e.backtrace} occured for enrollment with hbx_id #{enrollment.hbx_id}"
+            end
+          end
+        rescue => e
+          @logger.debug "Cannot process enrollments for family with id: #{family.id}, primary_person_hbx_id: #{family.primary_person.hbx_id}, Error: #{e.backtrace}"
+        end
+      end
+      expiring_plan_year.expire! if expiring_plan_year.may_expire?
     end
 
     def enrollment_statuses
