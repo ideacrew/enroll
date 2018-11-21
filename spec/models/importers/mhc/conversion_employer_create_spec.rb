1299d51d1e7aeb2adfe6a6fa49828da89308a458
Prem Shaganti <prem.shaganti@dc.gov>
2018-11-21 10:31:34 -0500

    ref:EmployerStaffRoleObserver instance for person model shouldn't be invoked for old model conversion employer creation

--- spec/models/importers/mhc/conversion_employer_create_spec.rb
+++ spec/models/importers/mhc/conversion_employer_create_spec.rb
@@ -38,7 +38,7 @@ describe ::Importers::Mhc::ConversionEmployerCreate, dbclean: :after_each do
 
   context "provided with employer date" do
     before :each do
-      # BenefitSponsors::Observers::EmployerStaffRoleObserver.any_instance.stub(:contact_changed?).and_return(true)
+      BenefitSponsors::Observers::EmployerStaffRoleObserver.any_instance.stub(:contact_changed?).and_return(true)
       @employer_profile = EmployerProfile.find_by_fein(fein)
     end
 
