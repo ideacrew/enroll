9b35053cbdd258c739cabfd4bd50eacf35c3c7be
sudhirreddysama <sudhirsama586@gmail.com>
2018-11-26 14:21:12 -0500

    negative scenario added in spec

--- spec/data_migrations/update_sep_market_kind_spec.rb
+++ spec/data_migrations/update_sep_market_kind_spec.rb
@@ -16,10 +16,11 @@ describe UpdateSepMarketKind, dbclean: :after_each do
     let!(:person100)  { FactoryGirl.create(:person, :with_consumer_role, :with_employee_role) }
     let!(:primary_applicant) { double }
     let!(:family100)  { FactoryGirl.create(:family, :with_primary_family_member, person: person100 ) }
-     let(:special_enrollment_period) {FactoryGirl.build(:special_enrollment_period,family:family100,market_kind: "ivl")}
-    let!(:add_special_enrollemt_period) {family100.special_enrollment_periods = [special_enrollment_period]
+    let(:special_enrollment_period) {FactoryGirl.build(:special_enrollment_period,family:family100,qualifying_life_event_kind_id: qualifying_life_event_kind101.id, market_kind: "ivl")}
+    let!(:add_special_enrollment_period) {family100.special_enrollment_periods = [special_enrollment_period]
                                           family100.save
     }
+    let!(:qualifying_life_event_kind101)  { FactoryGirl.create(:qualifying_life_event_kind, market_kind: "shop") }
 
     context 'update sep market kind to shop', dbclean: :after_each  do
       
@@ -37,10 +38,11 @@ describe UpdateSepMarketKind, dbclean: :after_each do
     context 'not update sep market kind to ivl', dbclean: :after_each  do
       
       before(:each) do
+        special_enrollment_period.qualifying_life_event_kind.update_attributes(market_kind: "ivl")
         allow(person100).to receive(:has_active_employee_role?).and_return(true)
       end
 
-      it "should not update market kind to ivl" do
+      it "should remain market kind to ivl" do
         subject.migrate
         expect(special_enrollment_period.market_kind).to eq "ivl"
       end
