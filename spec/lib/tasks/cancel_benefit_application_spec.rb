170cd17d0423ef7df90ece6c70f5ba2e75c2c7b7
Battula <vrbattula@outlook.com>
2018-11-26 17:06:42 -0500

    Fixed cancel_benefit_application_spec

--- spec/lib/tasks/cancel_benefit_application_spec.rb
+++ spec/lib/tasks/cancel_benefit_application_spec.rb
@@ -4,28 +4,28 @@ require 'rake'
 describe 'cancel employer benefit application & enrollments', :dbclean => :around_each do
   describe 'migrations:cancel_benefit_application' do
 
-    let!(:site)  { FactoryGirl.create(:benefit_sponsors_site, :with_owner_exempt_organization, :with_benefit_market, :with_benefit_market_catalog_and_product_packages, :cca) }
-    let!(:benefit_market) { site.benefit_markets.first }
-    let!(:benefit_market_catalog)  { benefit_market.benefit_market_catalogs.first }
-    let!(:employer_attestation)     { BenefitSponsors::Documents::EmployerAttestation.new(aasm_state: "approved") }
-    let(:organization) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+    let(:site)                { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+    let(:benefit_market)      { site.benefit_markets.first }
+    let(:current_effective_date)  { TimeKeeper.date_of_record }
+    let!(:benefit_market_catalog) { create(:benefit_markets_benefit_market_catalog, :with_product_packages,
+                                            benefit_market: benefit_market,
+                                            title: "SHOP Benefits for #{current_effective_date.year}",
+                                            application_period: (current_effective_date.beginning_of_year..current_effective_date.end_of_year))
+                                          }
+    let(:organization)        { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+    let(:employer_profile)    { organization.employer_profile }
+    let(:benefit_sponsorship) { employer_profile.add_benefit_sponsorship }
     let(:employee_role)     { FactoryGirl.create(:employee_role)}
-    let(:benefit_sponsorship) { FactoryGirl.create(:benefit_sponsors_benefit_sponsorship, organization: organization, profile_id: organization.profiles.first.id, benefit_market: site.benefit_markets[0], employer_attestation: employer_attestation) }
     let!(:benefit_application) { FactoryGirl.create(:benefit_sponsors_benefit_application, :with_benefit_sponsor_catalog, benefit_sponsorship: benefit_sponsorship, aasm_state: :enrollment_open) }
     let!(:product_package_kind) { :single_issuer }
     let!(:product_package) { benefit_market_catalog.product_packages.where(package_kind: product_package_kind).first }
     let!(:benefit_package) { FactoryGirl.create(:benefit_sponsors_benefit_packages_benefit_package, benefit_application: benefit_application, product_package: product_package) }
     let(:family) { FactoryGirl.build(:family, :with_primary_family_member)}
     let(:census_employee) { FactoryGirl.create(:census_employee, employer_profile: organization.employer_profile, employee_role_id: employee_role.id) }
-    let!(:termination_date){TimeKeeper.date_of_record.strftime('%m/%d/%Y')}
     let(:enrollment)     { FactoryGirl.build(:hbx_enrollment, household: family.active_household, sponsored_benefit_package_id: benefit_package.id, employee_role_id: employee_role.id)}
     let!(:fein)          {organization.fein}
-    let!(:py_end_on){TimeKeeper.date_of_record.end_of_month.strftime('%m/%d/%Y')}
 
     before do
-      benefit_sponsorships = organization.benefit_sponsorships.select { |bs| bs.id != benefit_sponsorship.id}
-      benefit_sponsorships.first.delete
-      organization.reload
       enrollment.update_attributes(aasm_state:'coverage_selected')
       employee_role.update_attributes(census_employee_id: census_employee.id)
     end
