83671ea93f0236d4ef31e0cce520676c23268f51
Ji Wang <traceyw@vt.edu>
2018-11-27 16:29:16 -0500

    update rake task to correct due date affected by notice

---
+++ app/data_migrations/correct_due_date_affected_by_FEL_notice.rb
@@ -0,0 +1,20 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class CorrectDueDateAffectedByFELNotice < MongoidMigrationTask
+  def migrate
+    begin
+      family = Family.where(min_verification_due_date:Date.new(2019,2,10))
+      family.each do |fam|
+        messages = fam.primary_person.inbox.messages.where(subject:"Reminder - You Must Submit Documents by the Deadline to Keep Your Insurance")
+         if messages.present? 
+            if messages.last.created_at > Date.new(2018,11,17)
+              fam.update_attributes(min_verification_due_date: Date.new(2019,3,21))
+              puts "updated due date for family with primary person #{fam.primary_person.hbx_id}" unless Rails.env.test?
+            end
+         end
+      end
+    rescue => e
+      e.message
+    end
+  end
+end
