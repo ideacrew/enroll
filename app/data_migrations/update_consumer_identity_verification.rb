9804d5dbdf51759417cec53b32bb33fbda3bb905
venkatbhattiprolu <vbhat847@gmail.com>
2018-12-03 14:11:04 -0500

    refs#37626 mod to the rake

--- app/data_migrations/update_consumer_identity_verification.rb
+++ app/data_migrations/update_consumer_identity_verification.rb
@@ -4,11 +4,13 @@ class UpdateConsumerIdentityVerification < MongoidMigrationTask
   def migrate
     begin
       hbx_ids = "#{ENV['hbx_id']}".split(',').uniq
-      hbx_id = hbx_ids.each do |hbx_id|
-        person = Person.where(hbx_id).first if person.present?
-        if person.consumer_role.identity_validation == "pending"
-          person.consumer_role.update_attributes!(bookmark_url:"/insured/interactive_identity_verifications/service_unavailable")
-          puts "Updated bookmark URL on consumer"
+      hbx_ids.each do |hbx_id|
+        person = Person.where(hbx_id: hbx_id).first 
+        unless person.present?
+          puts "No person record is found with #{hbx_id}"
+        else
+          person.consumer_role.update_attributes!(bookmark_url:"/insured/interactive_identity_verifications/service_unavailable") if person.consumer_role.identity_validation == "pending"
+          puts "Updated bookmark URL on consumer" unless Rails.env.test?
         end
       end
     rescue => e
