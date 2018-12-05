e1ba7be1a920b336f55134b6c02aa76730cc4769
harshaellanki <h_vardhan@yahoo.com>
2018-12-05 12:52:58 -0500

    added specs for plan filters

--- spec/views/insured/plan_shoppings/plan_filters.html.erb_spec.rb
+++ spec/views/insured/plan_shoppings/plan_filters.html.erb_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "insured/_plan_filters.html.erb" do
   let(:benefit_group){ double("BenefitGroup") }
-  let(:hbx_enrollment) { FactoryGirl.build_stubbed(:hbx_enrollment) }
+  let(:hbx_enrollment) { FactoryGirl.build_stubbed(:hbx_enrollment, effective_on: TimeKeeper.date_of_record.next_month.beginning_of_month) }
   context "without consumer_role" do
     let(:person) {double(is_consumer_role_active?: false)}
     before :each do
@@ -115,10 +115,6 @@ RSpec.describe "insured/_plan_filters.html.erb" do
 
     end
 
-    it 'should display find your doctor link' do
-      expect(rendered).to_not have_selector('a', text: /estimate your costs/i)
-    end
-
     it "should display metal level filters if plan_option_kind is single_carrier" do
       allow(benefit_group).to receive(:plan_option_kind).and_return("single_carrier")
       render :template => "insured/plan_shoppings/_plan_filters.html.erb"
@@ -150,7 +146,6 @@ RSpec.describe "insured/_plan_filters.html.erb" do
 
   context "with consumer_role and tax_household" do
     let(:person) {double(is_consumer_role_active?: true)}
-
 
     before :each do
       assign(:hbx_enrollment, hbx_enrollment)
@@ -191,6 +186,16 @@ RSpec.describe "insured/_plan_filters.html.erb" do
     it "should have selected aptc pct amount" do
       expect(rendered).to match /85/
       expect(rendered).to have_selector("input#elected_aptc[value='280.50']")
+    end
+
+    it 'should display find your doctor link' do
+      expect(rendered).to_not have_selector('a', text: /estimate your cost/i)
+    end
+
+    it 'should display estimate your cost link' do
+      assign(:coverage_kind, "dental")
+      render :template => "insured/plan_shoppings/_plan_filters.html.erb"
+      expect(rendered).to have_selector('a', text: /estimate your cost/i)
     end
   end
 
