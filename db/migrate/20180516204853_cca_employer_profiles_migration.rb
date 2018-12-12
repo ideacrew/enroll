385861519b402d371f4c500b5798c4aad711d966
saimekala07 <saimekala07@gmail.com>
2018-12-12 12:13:24 -0500

    updating employer migartion for test run

--- db/migrate/20180516204853_cca_employer_profiles_migration.rb
+++ db/migrate/20180516204853_cca_employer_profiles_migration.rb
@@ -1,34 +1,9 @@
 class CcaEmployerProfilesMigration < Mongoid::Migration
   def self.up
-
-    if Settings.site.key.to_s == "cca"
-      site_key = "cca"
-
-      Dir.mkdir("hbx_report") unless File.exists?("hbx_report")
-      file_name = "#{Rails.root}/hbx_report/employer_profiles_migration_status_#{TimeKeeper.datetime_of_record.strftime("%m_%d_%Y_%H_%M_%S")}.csv"
-      field_names = %w( organization_hbx_id legal_name benefit_sponsor_organization_id status)
-
-      logger = Logger.new("#{Rails.root}/log/employer_profiles_migration_data.log") unless Rails.env.test?
-      logger.info "Script Start - #{TimeKeeper.datetime_of_record}" unless Rails.env.test?
-
-      CSV.open(file_name, 'w') do |csv|
-        csv << field_names
-
-        #build and create GeneralOrganization and its profiles
-        status = create_profile(site_key, csv, logger)
-
-        if status
-          puts "" unless Rails.env.test?
-          puts "Check employer_profiles_migration_data logs & employer_profiles_migration_status csv for additional information." unless Rails.env.test?
-        else
-          puts "" unless Rails.env.test?
-          puts "Script execution failed" unless Rails.env.test?
-        end
-      end
-      logger.info "End of the script" unless Rails.env.test?
-    else
-      say "Skipping for non-CCA site"
-    end
+    logger = Logger.new("#{Rails.root}/log/employer_profiles_migration_data.log") unless Rails.env.test?
+    logger.info "Script Start - #{TimeKeeper.datetime_of_record}" unless Rails.env.test?
+    create_profile(logger)
+    logger.info "End of the script" unless Rails.env.test?
   end
 
   def self.down
@@ -36,14 +11,8 @@ class CcaEmployerProfilesMigration < Mongoid::Migration
 
   private
 
-  def self.create_profile(site_key, csv, logger)
+  def self.create_profile(logger)
 
-    #find or build site
-    sites = find_site(site_key)
-    return false unless sites.present?
-    site = sites.first
-
-    #get main app organizations for migration
     old_organizations = Organization.unscoped.exists(:employer_profile => true)
 
     #counters
@@ -91,64 +60,11 @@ class CcaEmployerProfilesMigration < Mongoid::Migration
             link_existing_census_employees_to_new_profile(census_employees_with_old_id)
 
             print '.' unless Rails.env.test?
-            csv << [old_org.hbx_id, old_org.legal_name, new_organization.id, "Migration Success"]
             success = success + 1
-          else
-            # handle MPY's census employee who are in production
-            new_org_benefit_sponsorship = existing_new_organizations.first.active_benefit_sponsorship
-            profile = existing_new_organizations.first.employer_profile
-            @old_profile = old_org.employer_profile
-            old_org_census_employees = find_census_employees
-
-            old_org_census_employees.each do |old_census|
-              census_employee_found = new_org_benefit_sponsorship.census_employees.unscoped.by_ssn(old_census.ssn).first if new_org_benefit_sponsorship.census_employees.present?
-              if census_employee_found.present?
-                if old_census.benefit_group_assignments.present?
-                  CensusEmployee.skip_callback(:save, :after, :assign_benefit_packages)
-                  CensusEmployee.skip_callback(:save, :after, :assign_default_benefit_package)
-                  CensusEmployee.skip_callback(:save, :after, :construct_employee_role)
-                  CensusEmployee.skip_callback(:update, :after, :update_hbx_enrollment_effective_on_by_hired_on)
-                  census_employee_found.benefit_group_assignments << old_census.benefit_group_assignments
-                  census_employee_found.save(:validate => false)
-                  old_census.update(benefit_group_assignments: [])
-                end
-              else
-                old_census.employee_role.update_attributes(benefit_sponsors_employer_profile_id: profile.id) if old_census.employee_role && old_census.employee_role.benefit_sponsors_employer_profile_id.blank?
-                old_census.benefit_sponsors_employer_profile_id = profile.id if old_census.benefit_sponsors_employer_profile_id.blank?
-                old_census.benefit_sponsorship = new_org_benefit_sponsorship
-                CensusEmployee.skip_callback(:save, :after, :assign_default_benefit_package)
-                CensusEmployee.skip_callback(:save, :after, :assign_benefit_packages)
-                CensusEmployee.skip_callback(:save, :after, :construct_employee_role)
-                CensusEmployee.skip_callback(:update, :after, :update_hbx_enrollment_effective_on_by_hired_on)
-                old_census.save(:validate => false)
-              end
-            end
-
-            # handle MPY's staff role
-            old_org_staff_roles = find_staff_roles
-            new_org_staff_roles = profile.staff_roles
-            old_org_staff_roles.each do |old_staff|
-              staff_found = new_org_staff_roles.select{|role| role.first_name == old_staff.first_name}.first if new_org_staff_roles.present?
-              unless staff_found.present?
-                staff_role = old_staff.employer_staff_roles.where(employer_profile_id: @old_profile.id).first
-                EmployerStaffRole.skip_callback(:update, :after, :notify_observers)
-                staff_role.update_attributes(benefit_sponsor_employer_profile_id: profile.id)
-              end
-            end
-
-          #  handles MPY employer document who are in production
-            build_employer_attestation(profile) if @old_profile.employer_attestation.present?
-            build_documents(old_org, profile)
-            build_inbox_messages(profile)
-            profile.organization.save
-            print 'E' unless Rails.env.test?
-            existing_organization = existing_organization + 1
-            csv << [old_org.hbx_id, old_org.legal_name, existing_new_organizations.first.id, "Already Migrated to new model, handled MPY employer scenario"]
           end
         rescue Exception => e
           failed = failed + 1
           print 'F' unless Rails.env.test?
-          csv << [old_org.hbx_id, old_org.legal_name, "0", "Migration Failed"]
           logger.error "Migration Failed for Organization HBX_ID: #{old_org.hbx_id},
           validation_errors:
           organization - #{new_organization.errors.messages}
@@ -180,7 +96,6 @@ class CcaEmployerProfilesMigration < Mongoid::Migration
       new_profile.contact_method = :paper_only
     end
 
-    build_employer_attestation(new_profile) if @old_profile.employer_attestation.present?
     build_documents(old_org, new_profile)
     build_inbox_messages(new_profile)
     build_office_locations(old_org, new_profile)
@@ -240,10 +155,10 @@ class CcaEmployerProfilesMigration < Mongoid::Migration
         old_emp_docs = @old_profile.documents.where(identifier: doc.identifier)
         old_org_docs = @old_profile.organization.documents.where(identifier: doc.identifier)
         old_document_id = if old_emp_docs.present?
-          old_emp_docs.first.id.to_s
-        elsif old_org_docs.present?
-          old_org_docs.first.id.to_s
-        end
+                            old_emp_docs.first.id.to_s
+                          elsif old_org_docs.present?
+                            old_org_docs.first.id.to_s
+                          end
         msg.body.gsub!(old_document_id, doc.id.to_s) if (doc.id.to_s.present? && old_document_id.present?)
       end
 
@@ -273,7 +188,7 @@ class CcaEmployerProfilesMigration < Mongoid::Migration
 
   def self.find_staff_roles
     Person.where(:employer_staff_roles => {
-        '$elemMatch' => {employer_profile_id: @old_profile.id}})
+                     '$elemMatch' => {employer_profile_id: @old_profile.id}})
   end
 
   def self.link_existing_staff_roles_to_new_profile(person_records_with_old_staff_roles)
