7d4d5b12e1ffacb31005ba139d90e2b1ec3a0f61
Raghuram <raghuramg83@gmail.com>
2018-12-12 22:11:06 -0500

    Refs #38529 updates to rake task

--- app/data_migrations/extending_open_enrollment_end_date_for_employers.rb
+++ app/data_migrations/extending_open_enrollment_end_date_for_employers.rb
@@ -3,7 +3,7 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class ExtendingOpenEnrollmentEndDateForEmployers < MongoidMigrationTask
 
   def migrate
-    feins = ENV['feins'].to_s.split(",") if ENV['feins'].present?
+    feins = ENV['feins'].to_s.split(",").map(&:strip) if ENV['feins'].present?
 
     plan_year_start_on = Date.strptime((ENV['py_start_on']).to_s, "%m/%d/%Y")
     new_open_enrollment_end_on_date = Date.strptime((ENV['new_oe_end_date']).to_s, "%m/%d/%Y")
@@ -16,7 +16,7 @@ class ExtendingOpenEnrollmentEndDateForEmployers < MongoidMigrationTask
 
     count = 0
     organizations.each do |org|
-      org.employer_profile.plan_years.where(:aasm_state.in => PlanYear::RENEWING).first.update_attribute(:open_enrollment_end_on, new_open_enrollment_end_on_date)
+      org.employer_profile.plan_years.where(:start_on => plan_year_start_on, :aasm_state.in => PlanYear::RENEWING).first.update_attribute(:open_enrollment_end_on, new_open_enrollment_end_on_date)
       puts "Changing Open Enrollment End On date for #{org.legal_name}" unless Rails.env.test?
       count = count+1
     end
