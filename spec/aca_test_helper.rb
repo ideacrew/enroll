82b265ed8b4e6712bb5b0eecfeb0c87ff3bcd797
Raghuram <raghuramg83@gmail.com>
2018-12-18 14:26:06 -0500

    Added spec fixes for landing page view errors

--- spec/aca_test_helper.rb
+++ spec/aca_test_helper.rb
@@ -6,4 +6,8 @@ module AcaTestHelper
   def self.aca_state_abbreviation
     @aca_state_abbreviation ||= Settings.aca.state_abbreviation
   end
+
+  def individual_market_is_enabled?
+    @individual_market_is_enabled ||= Settings.aca.market_kinds.include?("individual")
+  end
 end
