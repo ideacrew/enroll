42f502a61453a2f781d5b2cfa2be7dcebc2c8ccd
saimekala07 <saimekala07@gmail.com>
2018-12-20 17:51:51 -0500

    refs #38344 Adding migration for pricing and contribution models

--- db/migrate/20181213164439_create_dc_pricing_and_contribution_models.rb
+++ db/migrate/20181213164439_create_dc_pricing_and_contribution_models.rb
@@ -1,7 +1,19 @@
 class CreateDcPricingAndContributionModels < Mongoid::Migration
   def self.up
+    if Settings.site.key.to_s == "dc"
+      require File.expand_path(File.join(Rails.root, "db/seedfiles/cca/pricing_and_contribution_models_seed"))
+      say_with_time("Load DC Pricing Models") do
+        load_cca_pricing_models_seed
+      end
+      say_with_time("Load DC Contribution Models") do
+        load_cca_contribution_models_seed
+      end
+    else
+      say("Skipping migration for non-DC site")
+    end
   end
 
   def self.down
+    raise "Migration is not reversible."
   end
 end
