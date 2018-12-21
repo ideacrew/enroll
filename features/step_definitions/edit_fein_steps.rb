5fe98d689325620c284fc02187a643fc52e34ab9
Mahesh Kurma <maheshruby405@gmail.com>
2018-12-21 16:46:08 -0500

    refs #38455 modify cucumber feature

--- features/step_definitions/edit_fein_steps.rb
+++ features/step_definitions/edit_fein_steps.rb
@@ -1,36 +1,14 @@
-And(/^the user will see (.*?) | (.*?) window$/) do |text, window|
-  if text == "submit"
-    page.find_button('submit')
-  elsif text == "editable"
-    expect(page).to have_content('Change FEIN')
-    org_fein = initial_application.benefit_sponsorship.organization.fein
-    expect(page).to have_content(org_fein)
-  else
-    expect(page).to have_content(text)
-  end
-end
 
-And(/^the user clicks Change FEIN$/) do
+When(/^the user clicks Change FEIN link in the Actions dropdown for that Employer$/) do
+  find('.dropdown.pull-right', text: 'Actions').click
   click_link('Change FEIN')
 end
 
-And(/^the Change FEIN window will expand$/) do
-  page.has_xpath?("//*[@id='change_fein']")
-end
-
-When(/^the user clicks Cancel X button$/) do
-  find('.btn', text: 'Cancel').trigger('click')
-end
-
-Then(/^the Change FEIN window will collapse$/) do
-  page.has_xpath?("//*[@id='change_fein']").blank?
-end
-
 And(/^an FEIN with less than nine digits is entered$/) do
-  find('#organizations_general_organization_new_fein').set("4657")
+  find('#organizations_general_organization_new_fein').set("89-423")
 end
 
-When(/^the user clicks submit button$/) do
+And(/^the user clicks submit button$/) do
   page.find_button('submit').trigger('click')
 end
 
@@ -38,7 +16,7 @@ Then(/^an warning message will be presented as FEIN must be at least nine digits
   expect(page).to have_content('FEIN must be at least 9 digits')
 end
 
-Given(/^an FEIN with nine digits matches an existing Employer Profile FEIN$/) do
+And(/^an FEIN with nine digits matches an existing Employer Profile FEIN$/) do
   find('#organizations_general_organization_new_fein').set(second_employer[:fein])
 end
 
