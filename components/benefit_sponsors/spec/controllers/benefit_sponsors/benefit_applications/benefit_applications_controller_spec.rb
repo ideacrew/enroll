d9286636b7b5407b0511fc1bb117bf9d63fae175
vineel <sonnathivinil@gmail.com>
2019-01-02 15:33:41 -0500

    fix specs

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/benefit_applications/benefit_applications_controller_spec.rb
@@ -198,14 +198,14 @@ module BenefitSponsors
 
       before { sign_in user }
 
-      it "should return true if rates are available" do
+      it "should return false when it is not in late rate scenario" do
         xhr :get, :late_rates_check, :start_on_date => effective_period_start_on, benefit_sponsorship_id: "123"
-        expect(response.body).to eq "true"
+        expect(response.body).to eq "false"
       end
 
-      it "should return false if rates are not available" do
+      it "should return true during late rates scenario" do
         xhr :get, :late_rates_check, :start_on_date => effective_period_start_on-1.year, benefit_sponsorship_id: "123"
-        expect(response.body).to eq "false"
+        expect(response.body).to eq "true"
       end
     end
 
