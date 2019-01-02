af3f7b7c56ae4e7b207fc837c024861e7b95cf84
vineel <sonnathivinil@gmail.com>
2019-01-02 12:43:17 -0500

    fix spec

--- components/benefit_markets/spec/models/benefit_markets/forms/product_form_spec.rb
+++ components/benefit_markets/spec/models/benefit_markets/forms/product_form_spec.rb
@@ -4,26 +4,33 @@ module BenefitMarkets
   RSpec.describe Forms::ProductForm do
     describe '::for_new' do
       let(:product) { create :benefit_markets_products_product }
+      let(:future_product) { create :benefit_markets_products_product }
       let(:date) { TimeKeeper.date_of_record }
-      let(:future_year) { (date+1.year).year }
+      let(:future_date) { (date+1.year) }
       let!(:product) { FactoryGirl.create(:benefit_markets_products_health_products_health_product) }
 
-      subject { BenefitMarkets::Forms::ProductForm.for_new(date) }
+      context "#with current date" do
 
-      it 'should instantiate a new Product Form' do
-        expect(subject).to be_an_instance_of(BenefitMarkets::Forms::ProductForm)
+        subject { BenefitMarkets::Forms::ProductForm.for_new(date) }
+
+        it 'should instantiate a new Product Form' do
+          expect(subject).to be_an_instance_of(BenefitMarkets::Forms::ProductForm)
+        end
+
+        it 'should return false if rates are present' do
+          expect(subject.fetch_results.is_late_rate).to eq false
+        end
       end
 
-      it 'should return false if rates are present' do
-        expect(subject.fetch_results.is_late_rate).to eq false
-      end
+      context "#with future date" do
+        subject { BenefitMarkets::Forms::ProductForm.for_new(future_date) }
 
-      it 'should return true if rates are not present' do
-        product.application_period = Date.new(future_year, 1, 1)..Date.new(future_year, 12, 31)
-        pt = product.premium_tables.first
-        pt.effective_period = product.application_period
-        product.save
-        expect(subject.fetch_results.is_late_rate).to eq true
+        it 'should return true if rates are not present' do
+          future_product.application_period = Date.new(future_date.year, 1, 1)..Date.new(future_date.year, 12, 31)
+          future_product.premium_tables = nil
+          future_product.save
+          expect(subject.fetch_results.is_late_rate).to eq true
+        end
       end
     end
   end
