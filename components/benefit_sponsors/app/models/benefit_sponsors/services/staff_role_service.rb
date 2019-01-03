3d53cc8c0ee18749d4194cc3f39ccb2799def661
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-01-03 10:03:16 -0500

    refs #38786 added staff role form

--- components/benefit_sponsors/app/models/benefit_sponsors/services/staff_role_service.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/services/staff_role_service.rb
@@ -2,21 +2,33 @@ module BenefitSponsors
   module Services
     class StaffRoleService
 
-      attr_accessor :profile_id
+      attr_accessor :profile_id, :profile_type, :person
 
       def initialize(attrs={})
         @attrs = attrs
         @profile_id = attrs[:profile_id]
+        @profile_type = attrs[:profile_type]
       end
 
       def find_profile(form)
         organization = BenefitSponsors::Organizations::Organization.where("profiles._id" => BSON::ObjectId.from_string(form[:profile_id])).first
-        organization.employer_profile if organization.present?
+
+        if form.profile_type == "broker_agency_staff"
+          organization.broker_agency_profile if organization.present?
+        else
+          organization.employer_profile if organization.present?
+        end
+
       end
 
       def add_profile_representative!(form)
         profile = find_profile(form)
-        Person.add_employer_staff_role(form[:first_name], form[:last_name], form[:dob], form[:email] , profile)
+        if form.profile_type == "broker_agency_staff"
+          match_or_create_person(form)
+          persist_broker_agency_staff_role!(profile)
+        else
+          Person.add_employer_staff_role(form[:first_name], form[:last_name], form[:dob], form[:email] , profile)
+        end
       end
 
       def deactivate_profile_representative!(form)
@@ -38,6 +50,76 @@ module BenefitSponsors
           return false, 'Please contact HBX Admin to report this error'
         end
 
+      end
+
+      def match_or_create_person(form)
+        matched_people = get_matched_people(form)
+
+        if matched_people.count > 1
+          errors.add(:staff_role, "too many people match the criteria provided for your identity.  Please contact HBX.")
+          return false
+        end
+        if matched_people.count == 1
+          mp = matched_people.first
+          self.person = mp
+        else
+          self.person = build_person(form)
+        end
+
+        #add_person_contact_info
+        person.save!
+
+      end
+
+      def get_matched_people(form)
+        if self.profile_type == "broker_agency_staff"
+          Person.where(
+              first_name: regex_for(form[:first_name]),
+              last_name: regex_for(form[:last_name]),
+              dob: self[:dob]
+          )
+        else
+          Person.where(
+              first_name: regex_for(form[:first_name]),
+              last_name: regex_for(form[:last_name])
+          )
+        end
+      end
+
+      def persist_broker_agency_staff_role!(profile)
+        exisiting_brokers_with_same_profile =  person.broker_agency_staff_roles.select{|role| role if role.benefit_sponsors_broker_agency_profile_id == profile.id }
+        if exisiting_brokers_with_same_profile.present?
+
+          return false,  "You are already associated with the Broker Agency"
+        else
+          person.broker_agency_staff_roles << ::BrokerAgencyStaffRole.new({
+                                                                              broker_agency_profile: profile
+                                                                          })
+          person.save!
+          return true, person
+        end
+      end
+
+
+      def add_person_contact_info
+        if is_broker_profile?
+          person.add_work_email(email)
+        elsif is_employer_profile?
+          person.contact_info(email, area_code, number, extension) if email
+        end
+      end
+
+      def build_person(form)
+        Person.new({
+                       :first_name => form[:first_name].strip,
+                       :last_name => form[:last_name].strip,
+                       :dob => form[:dob]
+                   })
+      end
+
+      def regex_for(str)
+        clean_string = ::Regexp.escape(str.strip)
+        /^#{clean_string}$/i
       end
 
     end
