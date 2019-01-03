7ac2f95dad51bee7a453ab33fdd6d932e4dc7b34
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-01-03 16:08:36 -0500

    refs #38786 added specs and translations

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
@@ -3,8 +3,6 @@ module BenefitSponsors
     module BrokerAgencies
       class BrokerAgencyStaffRolesController < ::BenefitSponsors::ApplicationController
 
-        include Pundit
-
         def new
           @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_new
           respond_to do |format|
@@ -15,45 +13,17 @@ module BenefitSponsors
 
         def create
           @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_create(staff_params)
-          #authorize @staff
           begin
             @status , @result = @staff.save
             unless @status
-              flash[:error] = ('Role was not added because '  + @result)
+              flash[:error] = (' Broker Staff Role was not added because '  + @result)
             else
-              flash[:notice] = "Role added sucessfully"
+              flash[:notice] = "Broker Staff Role added sucessfully"
             end
           rescue Exception => e
             flash[:error] = e.message
           end
-          redirect_to edit_profiles_registration_path(id: staff_params[:profile_id])
-        end
-
-        #new person registered with existing organization is pending for staff role approval
-        #below action is triggered from employer to approve for staff role
-        def approve
-          @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_approve(staff_params)
-          authorize @staff
-          @status , @result = @staff.approve
-          unless @status
-            flash[:error] = 'Please contact HBX Admin to report this error'
-          else
-            flash[:notice] = 'Role is approved'
-          end
-          redirect_to edit_profiles_registration_path(id: staff_params[:profile_id])
-        end
-
-        # For this person find an employer_staff_role that match this employer_profile_id and mark the role inactive
-        def destroy
-          @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_destroy(staff_params)
-          authorize @staff
-          @status , @result = @staff.destroy!
-          unless @status
-            flash[:error] = ('Role was not deactivated because '  + @result)
-          else
-            flash[:notice] = 'Staff role was deleted'
-          end
-          redirect_to edit_profiles_registration_path(id: staff_params[:profile_id])
+          redirect_to new_profiles_registration_path(profile_type: "broker_agency")
         end
 
         def search_broker_agency
