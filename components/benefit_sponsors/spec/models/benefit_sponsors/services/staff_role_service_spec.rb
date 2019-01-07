0500b2de0fa922ac26f8232c55e7bfc0c4bb9eb4
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-01-07 16:17:46 -0500

    refs #38786 added specs

--- components/benefit_sponsors/spec/models/benefit_sponsors/services/staff_role_service_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/services/staff_role_service_spec.rb
@@ -14,9 +14,12 @@ module BenefitSponsors
 
 
     # let!(:employer_profile) {benefit_sponsor.employer_profile}
-    let!(:active_employer_staff_role) {FactoryBot.build(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: employer_profile.id)}
-    let!(:person) { FactoryBot.create(:person, employer_staff_roles:[active_employer_staff_role]) }
-    let(:user) { FactoryBot.create(:user, :person => person)}
+    let!(:active_employer_staff_role) {FactoryGirl.build(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: employer_profile.id)}
+    let!(:person) { FactoryGirl.create(:person, employer_staff_roles:[active_employer_staff_role]) }
+    let!(:broker_agency_staff_role) {FactoryGirl.build(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id)}
+    let!(:active_employer_staff_role) {FactoryGirl.build(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: employer_profile.id)}
+    let!(:broker_person) { FactoryGirl.create(:person) }
+    let(:user) { FactoryGirl.create(:user, :person => person)}
 
     describe ".find_profile" do
 
@@ -70,6 +73,40 @@ module BenefitSponsors
 
         it 'should add staff role for profile' do
           expect(subject.add_profile_representative!(add_staff_role_form)).to eq [true, new_person]
+        end
+      end
+
+      context "adding broker staff role for new person" do
+          let!(:new_person) { FactoryGirl.create(:person) }
+          let(:staff_role_form) { BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.new(
+              profile_id: broker_agency_profile.id,
+              profile_type: "broker_agency_staff",
+              first_name: new_person.first_name,
+              last_name: new_person.last_name,
+              dob: new_person.dob.to_s,
+              email: "steve@gmail.com")
+          }
+
+          it 'should add broker staff role for broker agency profile' do
+            expect(subject.add_profile_representative!(staff_role_form)).to eq [true, new_person]
+          end
+      end
+
+      context "adding person to broker agency in which he is already a staff" do
+        before do
+          person.broker_agency_staff_roles << broker_agency_staff_role
+        end
+        let(:staff_role_form) { BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.new(
+            profile_id: broker_agency_profile.id,
+            profile_type: "broker_agency_staff",
+            first_name: person.first_name,
+            last_name: person.last_name,
+            dob: person.dob.to_s,
+            email: "steve@gmail.com")
+        }
+
+        it 'should not add broker staff role' do
+          expect(subject.add_profile_representative!(staff_role_form)).to eq [false, "you are already associated with this Broker Agency"]
         end
       end
     end
@@ -147,6 +184,55 @@ module BenefitSponsors
         end
       end
 
+    end
+
+    describe ".match_or_create_person", dbclean: :after_each do
+      context 'when more than 1 person matched' do
+        before :each do
+          2.times { FactoryGirl.create(:person, first_name: "steve", last_name: "smith", dob: "10/10/1974") }
+        end
+
+        let(:staff_role_form) { BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.new(
+            profile_id: broker_agency_profile.id,
+            profile_type: "broker_agency_staff",
+            first_name: "steve",
+            last_name: "smith",
+            dob: "10/10/1974")
+        }
+        it "should raise an exception" do
+          expect(subject.match_or_create_person(staff_role_form)).to eq [false, "too many people match the criteria provided for your identity.  Please contact HBX."]
+        end
+      end
+
+      context 'when no person matched' do
+        let(:staff_role_form) { BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.new(
+            profile_id: broker_agency_profile.id,
+            profile_type: "broker_agency_staff",
+            first_name: "steve",
+            last_name: "smith",
+            dob: "10/10/1974",
+            email: "steve@gmail.com")
+        }
+        it "should build a new person" do
+          expect(subject.match_or_create_person(staff_role_form)).to eq true
+          expect(subject.person.first_name).to eq "steve"
+        end
+      end
+
+      context 'when only one person matched' do
+        let(:staff_role_form) { BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.new(
+            profile_id: broker_agency_profile.id,
+            profile_type: "broker_agency_staff",
+            first_name: person.first_name,
+            last_name: person.last_name,
+            dob: person.dob,
+            email: "steve@gmail.com")
+        }
+        it "form should assign person to exisiting person" do
+          expect(subject.match_or_create_person(staff_role_form)).to eq true
+          expect(subject.person.first_name).to eq person.first_name
+        end
+      end
     end
   end
 end
