0a48dd31b090c08f39f49405829fbc4b5523a35e
vineel <sonnathivinil@gmail.com>
2019-01-07 11:12:19 -0500

    Revert "Refs #39025"
    This reverts commit 3aabd7499a05ba540d0d9fde2e676746b8971bf3.

--- app/data_migrations/change_fein.rb
+++ app/data_migrations/change_fein.rb
@@ -8,11 +8,9 @@ class ChangeFein< MongoidMigrationTask
     right_fein = ENV['new_fein']
 
     correct_org = Organization.where(fein: wrong_fein).first
-    new_model_correct_exempt_org = BenefitSponsors::Organizations::ExemptOrganization.where("fein" => wrong_fein).first
-
     deprecated_org = Organization.where(fein: right_fein).first
 
-    if correct_org.nil? || new_model_correct_exempt_org.nil?
+    if correct_org.nil?
       puts "No organization was found by the given fein: #{wrong_fein}" unless Rails.env.test?
     else
       if deprecated_org.present?
@@ -21,11 +19,8 @@ class ChangeFein< MongoidMigrationTask
         deprecated_org.set(fein: wrong_fein)
         puts "Swapped fein from #{wrong_fein} to #{right_fein}" unless Rails.env.test?
       else
-        new_model_correct_exempt_org.update_attributes(fein: right_fein)
-        puts "Changed fein to #{new_model_correct_exempt_org.fein} in new model" unless Rails.env.test?
-
         correct_org.update_attributes(fein: right_fein)
-        puts "Changed fein to #{correct_org.fein} in old model" unless Rails.env.test
+        puts "Changed fein to #{right_fein}" unless Rails.env.test?
       end
     end
   end
--- spec/data_migrations/change_fein_spec.rb
+++ spec/data_migrations/change_fein_spec.rb
@@ -3,8 +3,6 @@ require File.join(Rails.root, "app", "data_migrations", "change_fein")
 
 describe ChangeFein, dbclean: :after_each do
   let(:given_task_name) { "change_fein" }
-  let(:old_fein) { "123456789" }
-  let(:new_fein) { "987654321" }
   subject { ChangeFein.new(given_task_name, double(:current_scope => nil)) }
 
   describe "given a task name" do
@@ -14,26 +12,19 @@ describe ChangeFein, dbclean: :after_each do
   end
 
   describe "changing organization's fein" do
-    let(:organization) { FactoryGirl.create(:organization, fein: old_fein)}
-
-
-    let(:site)                  { build(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
-    let(:issuer_profile)  { FactoryGirl.create :benefit_sponsors_organizations_issuer_profile, assigned_site: site}
-    let(:new_model_exempt_organization) { FactoryGirl.create(:benefit_sponsors_organizations_exempt_organization, profiles: [issuer_profile], fein: organization.fein)}
+    let(:organization) { FactoryGirl.create(:organization)}
 
     before(:each) do
-      allow(ENV).to receive(:[]).with("old_fein").and_return(old_fein)
-      allow(ENV).to receive(:[]).with("new_fein").and_return(new_fein)
+      allow(ENV).to receive(:[]).with("old_fein").and_return(organization.fein)
+      allow(ENV).to receive(:[]).with("new_fein").and_return("987654321")
     end
 
-    it "should update from old fein to new fein" do
-      new_model_exempt_organization
+    it "should change effective on date" do
+      fein=organization.fein
+      expect(organization.fein).to eq fein
       subject.migrate
       organization.reload
-      expect(organization.fein).to eq new_fein
-      new_model_exempt_organization.reload
-      expect(new_model_exempt_organization.fein).to eq new_fein
+      expect(organization.fein).to eq "987654321"
     end
-
   end
 end
