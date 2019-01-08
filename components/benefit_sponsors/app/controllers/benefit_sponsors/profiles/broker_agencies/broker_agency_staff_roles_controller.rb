966a13211c8c49f70f9101ae4d6d63c7a84e3506
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-01-08 14:18:35 -0500

    refs #39061, #39064 broker staff alert messages

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
@@ -13,17 +13,15 @@ module BenefitSponsors
 
         def create
           @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_create(staff_params)
-          begin
             @status , @result = @staff.save
-            unless @status
-              flash[:error] = (' Broker Staff Role was not added because '  + @result)
-            else
-              flash[:notice] = "Broker Staff Role added"
-            end
-          rescue Exception => e
-            flash[:error] = e.message
+          unless @status
+            @messages = (' Broker Staff Role was not added because '  + @result)
+          else
+            @messages = "Broker Staff Role added sucessfully"
           end
-          redirect_to new_profiles_registration_path(profile_type: "broker_agency")
+          respond_to do |format|
+            format.js
+          end
         end
 
         def search_broker_agency
