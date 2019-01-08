ec63816b279a75078937ef71d9449a93b52488f7
Josh <karperj@umd.edu>
2019-01-08 13:54:41 -0500

    refs#38256 added support for multiple broker links in my portal & fixed portal header

--- app/helpers/portal_header_helper.rb
+++ app/helpers/portal_header_helper.rb
@@ -7,15 +7,12 @@ module PortalHeaderHelper
       "<a class='portal'>#{l10n("welcome.index.welcome_to_site_header")}</a>".html_safe
     elsif current_user.try(:has_hbx_staff_role?)
       link_to "#{image_tag 'icons/icon-exchange-admin.png'} &nbsp; I'm an Admin".html_safe, main_app.exchanges_hbx_profiles_root_path, class: "portal"
-    elsif current_user.person.try(:broker_role)
-      if current_user.person.broker_role.broker_agency_profile.is_a? BrokerAgencyProfile
-        link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, main_app.broker_agencies_profile_path(id: current_user.person.broker_role.broker_agency_profile_id), class: "portal"
-      else
-        link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, benefit_sponsors.profiles_broker_agencies_broker_agency_profile_path(id: current_user.person.broker_role.benefit_sponsors_broker_agency_profile_id), class: "portal"
-      end
     elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
       link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Trained Expert".html_safe, home_exchanges_agents_path, class: "portal"
-    elsif current_user.person && current_user.person.active_employee_roles.any?
+    elsif current_user.try(:has_broker_agency_staff_role?) && controller == "broker_agency_profiles"
+      #This covers both brokers and broker agency staff since brokers have this role as well
+      link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, get_broker_profile_path, class: "portal"
+    elsif current_user.person && current_user.person.active_employee_roles.any? && controller_path.include?("insured")
       link_to "#{image_tag exchange_icon_path('icon-individual.png')} &nbsp; I'm an #{controller=='employer_profiles'? 'Employer': 'Employee'}".html_safe, main_app.family_account_path, class: "portal"
     elsif (controller_path.include?("insured") && current_user.try(:has_consumer_role?))
       if current_user.identity_verified_date.present?
@@ -23,9 +20,7 @@ module PortalHeaderHelper
       else
         "<a class='portal'>#{image_tag 'icons/icon-family.png'} &nbsp; Individual and Family</a>".html_safe
       end
-    elsif current_user.try(:has_broker_agency_staff_role?)
-      link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, get_broker_profile_path, class: "portal"
-    elsif current_user.try(:has_employer_staff_role?) # FIX THIS & check specs
+    elsif current_user.try(:has_employer_staff_role?) && controller == 'employer_profiles' 
       link_to "#{image_tag 'icons/icon-business-owner.png'} &nbsp; I'm an Employer".html_safe, benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab=>'home'), class: "portal"
     elsif current_user.has_general_agency_staff_role?
       link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a General Agency".html_safe, benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id), class: "portal"
@@ -35,16 +30,13 @@ module PortalHeaderHelper
   end
 
   def get_broker_profile_path
-
-    broker_agency_profile = current_user.person.broker_role.broker_agency_profile
-
     #if class is from benefit sponsor
-    klass_name = broker_agency_profile.class.to_s.demodulize.constantize
-
-    if broker_agency_profile.is_a? BrokerAgencyProfile
-      main_app.broker_agencies_profile_path(id: current_user.person.broker_role.broker_agency_profile_id)
+    klass_name = @broker_agency_profile.class.to_s.demodulize.constantize
+    
+    if @broker_agency_profile.is_a? BrokerAgencyProfile
+      main_app.broker_agencies_profile_path(id: @broker_agency_profile.id)
     elsif klass_name == BrokerAgencyProfile
-      benefit_sponsors.profiles_broker_agencies_broker_agency_profile_path(id: current_user.person.broker_role.benefit_sponsors_broker_agency_profile_id)
+      benefit_sponsors.profiles_broker_agencies_broker_agency_profile_path(id: @broker_agency_profile.id)
     end
   end
 
