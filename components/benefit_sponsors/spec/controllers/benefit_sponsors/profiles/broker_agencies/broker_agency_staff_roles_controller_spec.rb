ac5a02c87961d1ba1b309a739bfae3123cbf1329
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-01-08 14:18:35 -0500

    refs #39061, #39064 broker staff alert messages

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller_spec.rb
@@ -9,11 +9,11 @@ module BenefitSponsors
     let(:organization_with_hbx_profile)  { site.owner_organization }
     let!(:organization)                  { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site) }
     let!(:broker_agency_profile1) { organization.broker_agency_profile }
-    let!(:broker_role1) { FactoryGirl.create(:broker_role, aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile1.id, person: new_person_for_staff) }
 
     let(:bap_id) { organization.broker_agency_profile.id }
     let!(:new_person_for_staff) { FactoryGirl.create(:person) }
     let!(:new_person_for_staff1) { FactoryGirl.create(:person) }
+    let!(:broker_role1) { FactoryGirl.create(:broker_role, aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile1.id, person: new_person_for_staff) }
     let!(:broker_agency_staff_role) { FactoryGirl.create(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: bap_id, person: new_person_for_staff1 ) }
     let(:staff_class) { BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm }
 
@@ -49,24 +49,23 @@ module BenefitSponsors
         }
 
         before :each do
-          post :create, staff_params
+          xhr :post, :create, staff_params
         end
 
         it "should initialize staff" do
           expect(assigns(:staff).class).to eq staff_class
         end
 
-        it "should redirect" do
-          expect(response).to have_http_status(:redirect)
+        it "should render js template" do
+          expect(response.content_type).to eq Mime::JS
         end
 
-        it "should redirect to edit page of benefit_sponsor" do
-          expect(response).to redirect_to(new_profiles_registration_path(profile_type: "broker_agency"))
-          expect(response.location.include?("new")).to eq true
+        it "should be a success" do
+          expect(response).to have_http_status(:success)
         end
 
-        it "should get an notice" do
-          expect(flash[:notice]).to match /Broker Staff Role added/
+        it "should get alert message" do
+          expect(assigns(:messages)).to match /Broker Staff Role added/
         end
       end
 
@@ -81,20 +80,16 @@ module BenefitSponsors
         }
 
         before :each do
-          post :create, staff_params
+          xhr :post, :create, staff_params
         end
 
-        it "should redirect" do
-          expect(response).to have_http_status(:redirect)
+        it "should render js template" do
+          expect(response.content_type).to eq Mime::JS
         end
 
-        it "should redirect to edit page of benefit_sponsor" do
-          expect(response).to redirect_to(new_profiles_registration_path(profile_type: "broker_agency"))
-          expect(response.location.include?("new")).to eq true
-        end
 
-        it "should get an notice" do
-          expect(flash[:error]).to match /Broker Staff Role was not added because you are already associated with this Broker Agency/
+        it "should get alert message" do
+          expect(assigns(:messages)).to match /Broker Staff Role was not added because you are already associated with this Broker Agency/
         end
       end
 
@@ -109,20 +104,15 @@ module BenefitSponsors
         }
 
         before :each do
-          post :create, staff_params
+          xhr :post, :create, staff_params
         end
 
-        it "should redirect" do
-          expect(response).to have_http_status(:redirect)
+        it "should render js template" do
+          expect(response.content_type).to eq Mime::JS
         end
 
-        it "should redirect to edit page of benefit_sponsor" do
-          expect(response).to redirect_to(new_profiles_registration_path(profile_type: "broker_agency"))
-          expect(response.location.include?("new")).to eq true
-        end
-
-        it "should get an notice" do
-          expect(flash[:notice]).to match /Broker Staff Role added/
+        it "should get alert message" do
+          expect(assigns(:messages)).to match /Broker Staff Role added/
         end
       end
     end
