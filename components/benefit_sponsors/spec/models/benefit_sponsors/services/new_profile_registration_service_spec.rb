7c5fe59db40cb3e749148a1e55658415bde3562b
lokesh reddy <lokelokesh2012@gmail.com>
2019-01-10 10:31:33 -0500

    moved inbox controller to new model

--- components/benefit_sponsors/spec/models/benefit_sponsors/services/new_profile_registration_service_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/services/new_profile_registration_service_spec.rb
@@ -105,5 +105,15 @@ module BenefitSponsors
         end
       end
     end
+
+    describe "has_general_agency_staff_role_for_profile?" do
+      context "check for general agency staff role" do
+        let(:general_agency_person) { FactoryGirl.create(:person, emails:[FactoryGirl.build(:email, kind:'work')],employer_staff_roles:[active_employer_staff_role]) }
+        let(:general_agency_user) { FactoryGirl.create(:user, :person => general_agency_person)}
+        it "has general_agency_staff_role" do
+          expect(subject.new.has_general_agency_staff_role_for_profile?(general_agency_user,general_agency_profile)).to eq false
+        end
+      end
+    end
   end
 end
