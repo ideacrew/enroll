9570ddfbf544d9e3a247344beca5d1a360484d56
harsha chalamala <chvreddy1015@gmail.com>
2019-01-10 15:07:54 -0500

    Revert "Merge remote-tracking branch 'origin/performance-37813' into stage"
    This reverts commit 531d33b41ca8ebfb2bd5355139ae622b53df4405, reversing
    changes made to 8421d0fb9b3ce601b2619efaa49bdda746b65f8f.

--- app/models/mongorder.rb
+++ app/models/mongorder.rb
@@ -5,11 +5,12 @@ module Mongorder
     return base_fields unless s_field
     s_order ||= "asc"
     search_dir = (s_order.downcase == "asc") ? 1 : -1
-    fields = base_fields.reject { |bf| bf.first.downcase.to_s == s_field.downcase.to_s }
+    fields = base_fields.reject { |bf| bf.first.downcase.to_s == s_field.downcase.to_s } 
     fields.unshift([s_field.to_s, search_dir])
   end
 
-  def search(search_string, s_field = nil, s_order = nil, disable_sort = false)
+  def search(search_string, s_field = nil, s_order = nil)
+    search_order_expr = build_search_order_expression(s_field, s_order)
     search_scope = if search_string
                      self.where(
                        self.search_hash(search_string)
@@ -17,11 +18,7 @@ module Mongorder
                    else
                      self
                    end
-    if !disable_sort
-      search_order_expr = build_search_order_expression(s_field, s_order)
-      search_scope.order_by(search_order_expr)
-    end
-    return search_scope
+    search_scope.order_by(search_order_expr)
   end
 
 end
