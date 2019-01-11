a817758a67b4125fc4c6836e97f7b8a588c5a471
lokesh reddy <lokelokesh2012@gmail.com>
2019-01-11 15:20:25 -0500

    adding specs to general agency profiles controller & fixing pundit policies

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/general_agencies/general_agency_profiles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/general_agencies/general_agency_profiles_controller.rb
@@ -10,57 +10,31 @@ module BenefitSponsors
 
         rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-        # before_action :set_current_person, only: [:staff_index]
-        # before_action :check_and_download_commission_statement, only: [:download_commission_statement, :show_commission_statement]
         before_action :find_general_agency_profile, only: [:employers, :family_index]
         before_action :find_general_agency_staff, only: [:edit_staff, :update_staff]
 
         layout 'single_column'
 
-        # EMPLOYER_DT_COLUMN_TO_FIELD_MAP = {
-        #   "2"     => "legal_name",
-        #   "4"     => "employer_profile.aasm_state",
-        #   "5"     => "employer_profile.plan_years.start_on"
-        # }
-
-        # def index
-        #   #TODO authorize
-        #   # authorize self
-        #   @general_agency_profiles = BenefitSponsors::Organizations::Organization.general_agency_profiles.map(&:general_agency_profile)
-        # end
-
         def show
-          authorize self, :redirect_signup?
+          authorize self
           set_flash_by_announcement
           @general_agency_profile = ::BenefitSponsors::Organizations::GeneralAgencyProfile.find(params[:id])
           @provider = current_user.person
         end
 
         def employers
+          authorize self
           @datatable = Effective::Datatables::BenefitSponsorsGeneralAgencyDataTable.new({id: params[:id]})
         end
 
-        def staffs
-          authorize self, :redirect_signup?
+        def staffs # re-check this action & uncomment spec
+          authorize self
           @general_agency_profile = ::BenefitSponsors::Organizations::GeneralAgencyProfile.find(params[:format])
           @staffs = @general_agency_profile.general_agency_staff_roles
         end
 
-        # def staff_index
-        #   # authorize self
-        #   @q = params.permit(:q)[:q]
-        #   @staff = eligible_brokers
-        #   @page_alphabets = page_alphabets(@staff, "last_name")
-        #   page_no = cur_page_no(@page_alphabets.first)
-        #   if @q.nil?
-        #     @staff = @staff.where(last_name: /^#{page_no}/i)
-        #   else
-        #     @staff = @staff.where(last_name: /^#{@q}/i)
-        #   end
-        # end
-
         def edit_staff
-          authorize self, :redirect_signup?
+          authorize self
           respond_to do |format|
             format.js
             format.html
@@ -68,7 +42,7 @@ module BenefitSponsors
         end
 
         def update_staff
-          authorize self, :redirect_signup?
+          authorize self
           if params['approve']
             @staff.approve!
             flash[:notice] = "Staff approved successfully."
@@ -87,27 +61,6 @@ module BenefitSponsors
         def redirect_to_show(general_agency_profile_id)
           redirect_to benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: general_agency_profile_id)
         end
-
-        # TODO need to refactor for cases around SHOP broker agencies
-        # def family_datatable
-        #   authorize self
-        #   find_broker_agency_profile(BSON::ObjectId.from_string(params.permit(:id)[:id]))
-        #   dt_query = extract_datatable_parameters
-        #
-        #   query = BenefitSponsors::Queries::BrokerFamiliesQuery.new(dt_query.search_string, @broker_agency_profile.id)
-        #   @total_records = query.total_count
-        #   @records_filtered = query.filtered_count
-        #   @families = query.filtered_scope.skip(dt_query.skip).limit(dt_query.take).to_a
-        #   primary_member_ids = @families.map do |fam|
-        #     fam.primary_family_member.person_id
-        #   end
-        #   @primary_member_cache = {}
-        #   Person.where(:_id => { "$in" => primary_member_ids }).each do |pers|
-        #     @primary_member_cache[pers.id] = pers
-        #   end
-        #
-        #   @draw = dt_query.draw
-        # end
 
         def families
           authorize self
@@ -179,9 +132,8 @@ module BenefitSponsors
         end
 
         def user_not_authorized(exception)
-          if exception.query == :redirect_signup?
-            redirect_to main_app.new_user_registration_path
-          elsif current_user.has_general_agency_staff_role?
+          return redirect_to main_app.new_user_registration_path unless current_user
+          if current_user.has_general_agency_staff_role?
             redirect_to profiles_general_agencies_general_agency_profile_path(:id => current_user.person.general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id)
           else
             redirect_to benefit_sponsors.new_profiles_registration_path(:profile_type => :general_agency)
