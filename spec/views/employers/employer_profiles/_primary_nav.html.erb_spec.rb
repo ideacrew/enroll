c4dd0b7bf7d94870f57ef234e578505487fb8c9e
lokesh reddy <lokelokesh2012@gmail.com>
2019-01-15 14:37:48 -0500

    added ga staff role factory to primary_nav spec

--- spec/views/employers/employer_profiles/_primary_nav.html.erb_spec.rb
+++ spec/views/employers/employer_profiles/_primary_nav.html.erb_spec.rb
@@ -117,11 +117,12 @@ RSpec.describe "employers/employer_profiles/_primary_nav AS GeneralAgency" do
   let(:person) { FactoryBot.create(:person,:with_ssn, :first_name=>'fred', :last_name=>'flintstone'  )}
   let(:current_user) { FactoryBot.create(:user, :roles => ['general_agency_staff'], :person => person) }
   before :each do
-    general_agency = FactoryBot.create :general_agency, legal_name: 'Zooxy', general_agency_traits: :with_staff
-    staff = general_agency.general_agency_profile.general_agency_staff_roles.last
+    # general_agency = FactoryBot.create :general_agency, legal_name: 'Zooxy', general_agency_traits: :with_staff
+    # staff = general_agency.general_agency_profile.general_agency_staff_roles.last
+    staff = FactoryBot.create(:general_agency_staff_role, person: person)
     staff.person.emails.last.update(kind: 'work')
     user = FactoryBot.create(:user, :roles => ['general_agency_staff'], :person => staff.person)
-    
+
     @employer_profile = employer_profile
     sign_in user
     allow(view).to receive(:policy_helper).and_return(double("EmployerProfilePolicy", updateable?: true, list_enrollments?: true))
