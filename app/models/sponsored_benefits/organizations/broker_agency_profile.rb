3cecab3f68b61d42a96d923d9c31c26a4bf4aab2
nisanth <nisanth.yaganti@gmail.com>
2019-01-22 13:04:42 -0500

    configuring sponsored benefits engine in dc & creating/updating plan design organizations on broker hire & fire

--- app/models/sponsored_benefits/organizations/broker_agency_profile.rb
+++ app/models/sponsored_benefits/organizations/broker_agency_profile.rb
@@ -15,6 +15,18 @@ module SponsoredBenefits
           SponsoredBenefits::Organizations::PlanDesignOrganization.find_by_owner(id).first
         end
 
+        def cca_profile_office_locations(profile)
+          profile.office_locations if Settings.aca.state_abbreviation == "MA" # Bring in aca helper
+        end
+
+        def dc_profile_office_locations(profile)
+          profile.organization.office_locations if Settings.aca.state_abbreviation == "DC" # Bring in aca helper
+        end
+
+        def office_locations(profile)
+          cca_profile_office_locations(profile) || dc_profile_office_locations(profile)
+        end
+
         def find_or_initialize_broker_profile(profile)
           organization = SponsoredBenefits::Organizations::Organization.find_or_initialize_by(fein: profile.fein)
           unless organization.persisted?
@@ -22,35 +34,41 @@ module SponsoredBenefits
               hbx_id: profile.hbx_id,
               legal_name: profile.legal_name,
               dba: profile.dba,
-              office_locations: profile.office_locations,
-              broker_agency_profile: SponsoredBenefits::Organizations::BrokerAgencyProfile.new # Prospect
+              office_locations: office_locations(profile),
+              broker_agency_profile: self.new
             })
           end
           organization
         end
 
-        def assign_employer(broker_agency:, employer:, office_locations:)
+        def init_plan_design_organization(broker_agency, employer)
           broker_profile = find_or_initialize_broker_profile(broker_agency).broker_agency_profile
+          plan_design_organization = broker_profile.plan_design_organizations.new({
+            owner_profile_id: broker_agency._id,
+            sponsor_profile_id: employer._id,
+            office_locations: office_locations(employer),
+            fein: employer.fein,
+            legal_name: employer.legal_name,
+            has_active_broker_relationship: true
+          })
 
+          plan_design_organization.assign_attributes({
+            sic_code: employer.sic_code
+          }) if Settings.aca.state_abbreviation == "MA" # Bring in aca helper
+
+          broker_profile.save!
+        end
+
+        def assign_employer(broker_agency:, employer:)
           plan_design_organization = SponsoredBenefits::Organizations::PlanDesignOrganization.find_by_owner_and_sponsor(broker_agency.id, employer.id)
 
           if plan_design_organization
             plan_design_organization.update_attributes!({
               has_active_broker_relationship: true,
-              office_locations: office_locations,
+              office_locations: office_locations(employer),
             })
           else
-            broker_profile.plan_design_organizations.new({
-              owner_profile_id: broker_agency._id,
-              sponsor_profile_id: employer._id,
-              office_locations: office_locations,
-              fein: employer.fein,
-              legal_name: employer.legal_name,
-              has_active_broker_relationship: true,
-              sic_code: employer.sic_code,
-            })
-
-            broker_profile.save!
+            init_plan_design_organization(broker_agency, employer)
           end
         end
 
