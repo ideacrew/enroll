42d78a91b6161a23efdc8a6c258976f2aec9d884
Zachary Landes <zach.g.landes@gmail.com>
2019-01-23 11:00:28 -0500

    adding reports to force publish rake

--- app/data_migrations/force_publish_plan_years.rb
+++ app/data_migrations/force_publish_plan_years.rb
@@ -6,17 +6,31 @@ class ForcePublishPlanYears < MongoidMigrationTask
     def migrate
       start_on_date = Date.strptime(ENV['start_on_date'].to_s, "%m/%d/%Y")
       current_date = Date.strptime(ENV['current_date'].to_s, "%m/%d/%Y")
-      puts 'Reverting plan years...' unless Rails.env.test?
-      revert_plan_years(start_on_date, current_date) # setting renewing published plan years back to renewing draft
-      puts 'Assigning packages...' unless Rails.env.test?
-      assign_packages(start_on_date, current_date) #assign benefit packages to census employeess missing them    
-      puts'Setting back oe dates...' unless Rails.env.test?
-      set_back_oe_date(start_on_date, current_date) #set back oe dates for renewing draft employers with OE dates greater than current date 
-      puts 'Force Publishing...' unless Rails.env.test?
-      force_publish(start_on_date, current_date) # force publish
-      puts 'Generating error CSV of ERs with PYs not in Renewing Enrolling for some reason... ' unless Rails.env.test?
-      clean_up(start_on_date, current_date)#create error csv file with ERs that did not transition correctly
-      completion_check(start_on_date, current_date)  unless Rails.env.test?
+      if ENV['only_assign_packages'] == "true"
+        puts 'Assigning packages and creating unassigned packages csv...'
+        assign_packages(start_on_date, current_date)
+      elsif ENV['reports_only'] == "true"
+        puts 'Creating detail and non-detail report...'
+        detail_report(start_on_date)
+        non_detail_report(start_on_date)
+      elsif ENV['detail_report_only'] == "true"
+        puts 'Creating detail report...'
+        detail_report(start_on_date)
+      elsif ENV['query_count_only'] == "true"
+        puts "Enrollment count is #{query_enrollment_count(start_on_date, current_date)}"
+      else
+        puts 'Reverting plan years...' unless Rails.env.test?
+        revert_plan_years(start_on_date, current_date) # setting renewing published plan years back to renewing draft
+        puts 'Assigning packages...' unless Rails.env.test?
+        assign_packages(start_on_date, current_date) #assign benefit packages to census employeess missing them    
+        puts'Setting back oe dates...' unless Rails.env.test?
+        set_back_oe_date(start_on_date, current_date) #set back oe dates for renewing draft employers with OE dates greater than current date 
+        puts 'Force Publishing...' unless Rails.env.test?
+        force_publish(start_on_date, current_date) # force publish
+        puts 'Generating error CSV of ERs with PYs not in Renewing Enrolling for some reason... ' unless Rails.env.test?
+        clean_up(start_on_date, current_date)#create error csv file with ERs that did not transition correctly
+        completion_check(start_on_date, current_date)  unless Rails.env.test?
+      end
     end
   
     def plan_years_in_aasm_state(aasm_states, start_on_date)
@@ -126,6 +140,191 @@ class ForcePublishPlanYears < MongoidMigrationTask
         end
       end
     end
+
+    def non_detail_report(start_on_date)
+      orgs = Organization.where(:"employer_profile.plan_years" => {:$elemMatch => {:start_on => start_on_date, :aasm_state.in => PlanYear::RENEWING}})
+
+      CSV.open("#{Rails.root}/monthly_renewal_employer_enrollment_report_#{start_on_date.strftime('%m_%d')}.csv", "w") do |csv|
+        csv << [
+          "Employer Legal Name",
+          "Employer FEIN",
+          "Renewal State",
+          "#{start_on_date.prev_year.year} Active Enrollments",
+          "#{start_on_date.prev_year.year} Passive Renewal Enrollments"
+        ]
+
+        orgs.each do |organization|
+
+          puts "Processing #{organization.legal_name}"
+
+          employer_profile = organization.employer_profile
+
+          data = [
+            employer_profile.legal_name,
+            employer_profile.fein,
+            employer_profile.renewing_plan_year.aasm_state.camelcase
+          ]
+          next if employer_profile.active_plan_year.blank?
+
+          active_bg_ids = employer_profile.active_plan_year.benefit_groups.pluck(:id)
+          families = Family.where(:"households.hbx_enrollments" => {:$elemMatch => {:benefit_group_id.in => active_bg_ids, :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES}})
+
+          if employer_profile.renewing_plan_year.present?
+            if employer_profile.renewing_plan_year.renewing_enrolling? || employer_profile.renewing_plan_year.renewing_enrolled?
+              renewal_bg_ids = employer_profile.renewing_plan_year.benefit_groups.pluck(:id)
+            end
+          end
+
+          active_enrollment_count = 0
+          renewal_enrollment_count = 0
+
+          families.each do |family|
+
+            enrollments = family.active_household.hbx_enrollments.where({
+              :benefit_group_id.in => active_bg_ids,
+              :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES
+            })
+
+            %w(health dental).each do |coverage_kind|
+              if enrollments.where(:coverage_kind => coverage_kind).present?
+                active_enrollment_count += 1
+              end
+            end
+
+            if renewal_bg_ids.present?
+              renewal_enrollments = family.active_household.hbx_enrollments.where({
+                :benefit_group_id.in => renewal_bg_ids,
+                :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES + ['auto_renewing']
+              })
+
+              %w(health dental).each do |coverage_kind|
+                if renewal_enrollments.where(:coverage_kind => coverage_kind).present?
+                  renewal_enrollment_count += 1
+                end
+              end
+            end
+          end
+
+          data += [active_enrollment_count, renewal_enrollment_count]
+          csv << data
+        end
+      end
+    end 
+
+    def detail_report(start_on_date)
+      orgs = Organization.where(:"employer_profile.plan_years" => {:$elemMatch => {:start_on => start_on_date, :aasm_state.in => PlanYear::RENEWING}})
+  
+      CSV.open("#{Rails.root}/detail_report_#{start_on_date.strftime('%m_%d')}.csv", "w") do |csv|
+      csv << [
+        "Employer Legal Name",
+        "Employer FEIN",
+        "Employer HBX ID",
+        "Renewal State",
+        "First name",
+        "Last Name",
+        "Roster status",
+        "Hbx ID",
+        "#{start_on_date.prev_year.year} enrollment", 
+        "#{start_on_date.prev_year.year} plan", 
+        "#{start_on_date.prev_year.year} effective_date",
+        "#{start_on_date.prev_year.year} enrollment kind",
+        "#{start_on_date.prev_year.year} status",
+        "#{start_on_date.year} enrollment", 
+        "#{start_on_date.year} plan", 
+        "#{start_on_date.year} effective_date",
+        "#{start_on_date.year} enrollment kind",
+        "#{start_on_date.year} status"
+      ]
+      orgs.each do |organization|
+
+        puts "Processing #{organization.legal_name}"
+
+        employer_profile = organization.employer_profile
+        next if employer_profile.active_plan_year.blank?
+        active_bg_ids = employer_profile.active_plan_year.benefit_groups.pluck(:id)
+        families = Family.where(:"households.hbx_enrollments" => {:$elemMatch => {:benefit_group_id.in => active_bg_ids, :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES +  HbxEnrollment::WAIVED_STATUSES}})
+
+        if employer_profile.renewing_plan_year.present?
+          if employer_profile.renewing_plan_year.renewing_enrolling? || employer_profile.renewing_plan_year.renewing_enrolled?
+            renewal_bg_ids = employer_profile.renewing_plan_year.benefit_groups.pluck(:id)
+          end
+        end
+
+          puts "found #{families.count} families"
+          families.each do |family|
+            begin
+              enrollments = family.active_household.hbx_enrollments.where({
+                :benefit_group_id.in => active_bg_ids,
+                :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES  +  HbxEnrollment::WAIVED_STATUSES
+              })
+
+              employee_role = enrollments.last.employee_role 
+              if employee_role.present?
+                employee = employee_role.census_employee
+              end
+
+              if employee.blank?
+                  puts "#{family.id}-----#{family.primary_applicant}" if family.primary_applicant.nil?
+                  person = family.primary_applicant.person
+                  
+                  role = person.employee_roles.detect{|role| role.employer_profile_id.to_s == employer_profile.id.to_s} 
+                  employee = role.census_employee
+              end
+
+              if renewal_bg_ids.present?
+                renewal_enrollments = family.active_household.hbx_enrollments.where({
+                  :benefit_group_id.in => renewal_bg_ids,
+                  :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES + ['auto_renewing']   +  HbxEnrollment::WAIVED_STATUSES
+                  })
+              end
+
+              employer_employee_data = [
+                employer_profile.legal_name,
+                employer_profile.fein,
+                employer_profile.hbx_id,
+                employer_profile.renewing_plan_year.aasm_state.camelcase
+              ]
+
+              if employee.present?
+                employer_employee_data += [employee.first_name, employee.last_name, employee.aasm_state.humanize, employee_role.try(:person).try(:hbx_id)] 
+              else
+                employer_employee_data += [nil, nil, nil, nil]
+              end
+
+
+              %w(health dental).each do |coverage_kind|
+                next if enrollments.where(:coverage_kind => coverage_kind).blank?
+
+                data = employer_employee_data
+                data += enrollment_details_by_coverage_kind(enrollments, coverage_kind)
+                if renewal_bg_ids.present?
+                  data += enrollment_details_by_coverage_kind(renewal_enrollments, coverage_kind)
+                end
+
+                csv << data
+              end
+            rescue Exception => e 
+              puts "Failed: #{family.id} #{e.to_s}"
+              next
+            end
+          end
+        end
+      end
+    end
+
+
+    def enrollment_details_by_coverage_kind(enrollments, coverage_kind)
+      enrollment = enrollments.where(:coverage_kind => coverage_kind).sort_by(&:submitted_at).last
+      return [] if enrollment.blank?
+      [
+        enrollment.hbx_id,
+        enrollment.try(:plan).try(:hios_id),
+        enrollment.effective_on.strftime("%m/%d/%Y"),
+        enrollment.coverage_kind,
+        enrollment.aasm_state.humanize
+      ]
+    end
+
    end
   
   
