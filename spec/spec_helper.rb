1625808312840b9b21d1e0ecad9a53fbc9ee96f2
nisanth <nisanth.yaganti@gmail.com>
2019-01-24 11:48:33 -0500

    removing duplicate factory declaration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,8 +20,6 @@ require 'database_cleaner'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'factory_girl_rails'
-FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
-FactoryGirl.find_definitions
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
