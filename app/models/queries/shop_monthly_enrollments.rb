f7fc4d98475aae6f4b593e0afc0ea963154e867e
saimekala07 <saimekala07@gmail.com>
2019-01-24 12:10:23 -0500

    refs #39113 updating quiet period query to include new hire enrollments that purchased during OE and effective date gretaer than plan year start date.

--- app/models/queries/shop_monthly_enrollments.rb
+++ app/models/queries/shop_monthly_enrollments.rb
@@ -19,11 +19,21 @@ module Queries
     end
 
     def query_families_with_quiet_period_enrollments
+      # include new hire enrollment that purchased in open enrollment with effective_on date greater benefit application start date,
+      # and quiet period enrollments.
       add({
         "$match" => {
-          "$and" => [
-            "households.hbx_enrollments.sponsored_benefit_id" => { "$in" => collect_benefit_group_ids },
-            "households.hbx_enrollments.workflow_state_transitions" => { "$elemMatch" => quiet_period_expression }
+          "$or" => [
+            {"$and" =>[
+              "households.hbx_enrollments.sponsored_benefit_id" => { "$in" => collect_benefit_group_ids },
+              "households.hbx_enrollments.workflow_state_transitions" => { "$elemMatch" => quiet_period_expression }
+            ]},
+            {"$and" =>[
+              "households.hbx_enrollments.aasm_state" => {"$in" => @enrollment_statuses},
+              "households.hbx_enrollments.effective_on" => {"$gt" => @effective_on},
+              "households.hbx_enrollments.sponsored_benefit_id" => { "$in" => collect_benefit_group_ids },
+              "households.hbx_enrollments.submitted_at" => {"$lt" => quiet_period.begin}
+            ]}
           ]
         }
       })
@@ -59,7 +69,8 @@ module Queries
       add({
         "$match" => {
           "$or" => [
-            quiet_period_coverage_expression
+            quiet_period_coverage_expression,
+            new_hire_enrollment_expression
           ]
         }
       })
@@ -118,6 +129,15 @@ module Queries
         "households.hbx_enrollments.workflow_state_transitions" => { 
           "$elemMatch" => quiet_period_expression 
         }
+      }
+    end
+
+    def new_hire_enrollment_expression
+      {
+          "households.hbx_enrollments.effective_on" => {"$gt" => @effective_on},
+          "households.hbx_enrollments.sponsored_benefit_id" => { "$in" => collect_benefit_group_ids },
+          "households.hbx_enrollments.kind" => "employer_sponsored",
+          "households.hbx_enrollments.submitted_at" => {"$lt" => quiet_period.begin}
       }
     end
 
