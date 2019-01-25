a696880d975d8a55ba5a5c2fb96cf51b15c15c8a
saikumar9 <saikumarkotagiri9@gmail.com>
2019-01-25 10:33:21 -0500

    Refs #38728 spec fix

--- spec/controllers/exchanges/hbx_profiles_controller_spec.rb
+++ spec/controllers/exchanges/hbx_profiles_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require File.join(Rails.root, "components/benefit_sponsors/spec/support/benefit_sponsors_product_spec_helpers")
 
 RSpec.describe Exchanges::HbxProfilesController, dbclean: :after_each do
 
@@ -716,10 +717,37 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :after_each do
   end
 
   describe "benefit application creation" do
-    let!(:user)           { FactoryGirl.create(:user) }
-    let!(:person)         { FactoryGirl.create(:person, user: user) }
-    let!(:permission)     { FactoryGirl.create(:permission, :super_admin) }
-    let!(:hbx_staff_role) { FactoryGirl.create(:hbx_staff_role, person: person, permission_id: permission.id) }
+    let!(:user)                { FactoryGirl.create(:user) }
+    let!(:person)              { FactoryGirl.create(:person, user: user) }
+    let!(:permission)          { FactoryGirl.create(:permission, :super_admin) }
+    let!(:hbx_staff_role)      { FactoryGirl.create(:hbx_staff_role, person: person, permission_id: permission.id) }
+    let!(:rating_area)         { FactoryGirl.create_default :benefit_markets_locations_rating_area }
+    let!(:service_area)        { FactoryGirl.create_default :benefit_markets_locations_service_area }
+    let!(:site)                { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+    let!(:benefit_market)      { site.benefit_markets.first }
+    let!(:organization)        { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
+    let!(:employer_profile)    { organization.employer_profile }
+    let!(:benefit_sponsorship) { bs = employer_profile.add_benefit_sponsorship
+                                bs.save!
+                                bs
+                               }
+    let(:effective_period)     { (TimeKeeper.date_of_record + 3.months)..(TimeKeeper.date_of_record + 1.year + 3.months - 1.day) }
+    let!(:current_benefit_market_catalog) do
+      BenefitSponsors::ProductSpecHelpers.construct_cca_benefit_market_catalog_with_renewal_catalog(site, benefit_market, effective_period)
+      benefit_market.benefit_market_catalogs.where(
+        "application_period.min" => effective_period.min.to_s
+      ).first
+    end
+
+    let!(:valid_params)   {
+      { admin_datatable_action: true,
+        benefit_sponsorship_id: benefit_sponsorship.id.to_s,
+        start_on: effective_period.min,
+        end_on: effective_period.max,
+        open_enrollment_start_on: TimeKeeper.date_of_record + 2.months,
+        open_enrollment_end_on: TimeKeeper.date_of_record + 2.months + 20.day
+      }
+    }
 
     before :each do
       sign_in(user)
@@ -741,7 +769,7 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :after_each do
 
     context '.create_benefit_application' do
       before :each do
-        xhr :post, :create_benefit_application
+        xhr :post, :create_benefit_application, valid_params
       end
 
       it 'should respond with success status' do
