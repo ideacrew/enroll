fee8e130e0c6aaabea0e13c36bb2fc76f2da85b1
saimekala07 <saimekala07@gmail.com>
2019-01-25 13:53:43 -0500

    refs #39560 updated to run monthly employer batch run to end of month.

--- components/benefit_sponsors/app/models/benefit_sponsors/scheduled_events/aca_shop_scheduled_events.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/scheduled_events/aca_shop_scheduled_events.rb
@@ -120,7 +120,7 @@ module BenefitSponsors
 
       def auto_transmit_monthly_benefit_sponsors
         if aca_shop_market_transmit_scheduled_employers
-          if (new_date.prev_day.mday + 1) == aca_shop_market_employer_transmission_day_of_month
+          if (new_date.prev_day.mday + 1) >= aca_shop_market_employer_transmission_day_of_month
             transmit_scheduled_benefit_sponsors(new_date)
           end
         end
@@ -128,18 +128,17 @@ module BenefitSponsors
 
       def transmit_scheduled_benefit_sponsors(new_date, feins=[])
         start_on = new_date.next_month.beginning_of_month
+        transition_at = (new_date.prev_day.mday + 1) > aca_shop_market_employer_transmission_day_of_month ? new_date : nil
         benefit_sponsors = BenefitSponsors::BenefitSponsorships::BenefitSponsorship
         benefit_sponsors = benefit_sponsors.find_by_feins(feins) if feins.any?
-        
-        benefit_sponsors.may_transmit_renewal_enrollment?(start_on).each do |benefit_sponsorship|
+        benefit_sponsors.may_transmit_renewal_enrollment?(start_on, transition_at).each do |benefit_sponsorship|
           begin
             execute_sponsor_event(benefit_sponsorship, :transmit_renewal_eligible_event) if benefit_sponsorship.is_renewal_transmission_eligible?
             execute_sponsor_event(benefit_sponsorship, :transmit_renewal_carrier_drop_event) if benefit_sponsorship.is_renewal_carrier_drop?
           rescue Exception => e 
           end
         end
-
-        benefit_sponsors.may_transmit_initial_enrollment?(start_on).each do |benefit_sponsorship|
+        benefit_sponsors.may_transmit_initial_enrollment?(start_on, transition_at).each do |benefit_sponsorship|
           begin
             execute_sponsor_event(benefit_sponsorship, :transmit_initial_eligible_event)
           rescue Exception => e 
