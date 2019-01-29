5587a266e72d5a35a412518bba3c37c462c8b172
RyanEddyIC <ryan.eddy@ideacrew.com>
2019-01-29 11:25:40 -0500

    Add ability to handle multiple family_member_ids at once

--- app/data_migrations/remove_dependent.rb
+++ app/data_migrations/remove_dependent.rb
@@ -3,20 +3,29 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class RemoveDependent < MongoidMigrationTask
   def migrate
     begin
-      id = ENV["family_member_id"].to_s
-      family_member = FamilyMember.find(id)
-      if family_member.nil?
-        puts "No family member found" unless Rails.env.test?
-      else
-        active_household = family_member.family.active_household
+      ids = ENV["family_member_id"].to_s.split(",")
+
+      family_members = []
+      ids.each do |id|
+        fm = FamilyMember.find(id)
+        if fm.present?
+          family_members.push(fm)
+        else
+          puts "No family member found with id: '#{id}'"
+        end
+      end
+
+      family_members.each do |fm|
+        active_household = fm.family.active_household
         coverage_household = active_household.coverage_households.where(:is_immediate_family => true).first
         enrollments = active_household.hbx_enrollments.my_enrolled_plans.where(:"aasm_state".ne => "coverage_canceled")
-        if (coverage_household.coverage_household_members.map(&:family_member_id).map(&:to_s) & [id]).present?|| (enrollments.map(&:hbx_enrollment_members).flatten.uniq.map(&:applicant_id).map(&:to_s) & [id]).present?
-          puts "you cannot remove this family member. This member may have Coverage Household Member records or Enrollments" unless Rails.env.test?
+
+        if (coverage_household.coverage_household_members.map(&:family_member_id).map(&:to_s) & [fm.id]).present?|| (enrollments.map(&:hbx_enrollment_members).flatten.uniq.map(&:applicant_id).map(&:to_s) & [fm.id]).present?
+          puts "You cannot remove family member '#{fm.id}'. This member may have Coverage Household Member records or Enrollments." unless Rails.env.test?
           return
         else
-          family_member.destroy!
-          puts "remove duplicate dependent with family member id: #{family_member.id}" unless Rails.env.test?
+          fm.destroy!
+          puts "Removed dependent with family member id: '#{fm.id}'" unless Rails.env.test?
         end
       end
     rescue Exception => e
@@ -24,5 +33,3 @@ class RemoveDependent < MongoidMigrationTask
     end
   end
 end
-
-
--- lib/tasks/migrations/remove_dependent.rake
+++ lib/tasks/migrations/remove_dependent.rake
@@ -1,6 +1,7 @@
 require File.join(Rails.root, "app", "data_migrations", "remove_dependent")
 # This rake task is to remove a family member
-# RAILS_ENV=production bundle exec rake migrations:remove_dependent family_member_id=12345
+# RAILS_ENV=production bundle exec rake migrations:remove_dependent family_member_id='5c3de1h4d'
+# RAILS_ENV=production bundle exec rake migrations:remove_dependent family_member_id='5c3de1h4d,9j8d6h4ks8,4c9d8f7g3h'
 
 namespace :migrations do
   desc "remove dependent"
