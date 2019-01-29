2c2d732f6f1cdeb494a2710d936b98496dd9a2c2
saimekala07 <saimekala07@gmail.com>
2019-01-29 15:19:36 -0500

    refs #39642 updating spec

--- spec/data_migrations/components/fix_benefit_sponsorship_state_spec.rb
+++ spec/data_migrations/components/fix_benefit_sponsorship_state_spec.rb
@@ -18,25 +18,31 @@ describe FixBenefitSponsorshipState, dbclean: :after_each do
     include_context "setup benefit market with market catalogs and product packages"
     include_context "setup initial benefit application"
 
-        let(:current_effective_date) { TimeKeeper.date_of_record.beginning_of_month }
-        let(:effective_on) { current_effective_date }
-        let(:aasm_state) { :active }
-        let(:benefit_sponsorship_state) { :applicant }
+    context "should update benefit sponsorship, when sponsorship has active benefit application" do
 
-    let(:employer_organization)   { FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
-    let(:employer_profile)        { employer_organization.employer_profile }
-    let(:site)                    { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
-    let!(:employer_attestation)   { FactoryGirl.build(:employer_attestation, aasm_state:'unsubmitted') }
-    let!(:benefit_sponsorship)   { employer_profile.add_benefit_sponsorship }
+      let(:aasm_state) { :active }
+      let(:benefit_sponsorship_state) { :applicant }
 
-
-    it "should update benefit sponsorship state from applicant to active state" do
-      expect(benefit_sponsorship.aasm_state).to eq :applicant
-      subject.migrate
-      benefit_sponsorship.reload
-      expect(benefit_sponsorship.aasm_state).to eq :active
+      it "should update benefit sponsorship state from applicant to active state" do
+        expect(benefit_sponsorship.aasm_state).to eq :applicant
+        subject.migrate
+        benefit_sponsorship.reload
+        expect(benefit_sponsorship.aasm_state).to eq :active
+      end
     end
 
+    context "should not update benefit sponsorship, when sponsorship has no active benefit application" do
+
+      let(:aasm_state) { :enrollment_eligible }
+      let(:benefit_sponsorship_state) { :applicant }
+
+      it "should not update benefit sponsorship" do
+        expect(benefit_sponsorship.aasm_state).to eq :applicant
+        subject.migrate
+        benefit_sponsorship.reload
+        expect(benefit_sponsorship.aasm_state).to eq :applicant
+      end
+    end
 
   end
 end
