eb141b92fe860a869587a622f0ccaf0da6c96c63
saimekala07 <saimekala07@gmail.com>
2019-01-29 15:14:27 -0500

    Refs #39642 Adding rake to update benefit sponsorship state to active across DB, for sponsorship that has active benefit application.

---
+++ app/data_migrations/components/fix_benefit_sponsorship_state.rb
@@ -0,0 +1,14 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class FixBenefitSponsorshipState < MongoidMigrationTask
+  def migrate
+    BenefitSponsors::BenefitSponsorships::BenefitSponsorship.all.each do |benefit_sponsorship|
+      if benefit_sponsorship.benefit_applications.any?{|b| b.active?} && benefit_sponsorship.aasm_state != :active
+        before_update = benefit_sponsorship.aasm_state
+        benefit_sponsorship.aasm_state = :active
+        benefit_sponsorship.save
+        puts "Benefit Sponsorship state updated from #{before_update} to #{benefit_sponsorship.aasm_state}" unless Rails.env.test?
+      end
+    end
+  end
+end
