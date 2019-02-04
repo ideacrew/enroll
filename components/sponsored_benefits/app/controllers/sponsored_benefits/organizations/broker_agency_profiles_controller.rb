c0b7784f977d95a4d34405743ba0ccb5770af433
nisanth <nisanth.yaganti@gmail.com>
2019-02-04 17:37:47 -0500

    refactoring employers action & datatable in BQT

--- components/sponsored_benefits/app/controllers/sponsored_benefits/organizations/broker_agency_profiles_controller.rb
+++ components/sponsored_benefits/app/controllers/sponsored_benefits/organizations/broker_agency_profiles_controller.rb
@@ -2,30 +2,23 @@ require_dependency "sponsored_benefits/application_controller"
 
 module SponsoredBenefits
   class Organizations::BrokerAgencyProfilesController < ApplicationController
-    include Config::AcaConcern
     include DataTablesAdapter
-
-    before_action :find_broker_agency_profile, only: [:employers]
+    before_action :find_profile, only: [:employers]
 
     def employers
-      @broker_role = current_user.person.broker_role || nil
-      @general_agency_profiles = GeneralAgencyProfile.all_by_broker_role(@broker_role, approved_only: true) if general_agency_is_enabled? and @broker_role
-      if general_agency_is_enabled?
-        @datatable = ::Effective::Datatables::BrokerAgencyEmployerDatatable.new(profile_id: @broker_agency_profile._id, general_agency_is_enabled: "true")
-      else
-        @datatable = ::Effective::Datatables::BrokerAgencyEmployerDatatable.new(profile_id: @broker_agency_profile._id)
-      end
-      @broker_role = current_user.person.broker_role || nil
-      puts "@broker_role #{@broker_role.inspect}"
-      @general_agency_profiles = GeneralAgencyProfile.all_by_broker_role(@broker_role, approved_only: true) if @broker_role
+      # This should be index action in plan design organizations controller
+      @datatable = ::Effective::Datatables::PlanDesignOrganizationDatatable.new(profile_id: @profile._id, is_general_agency?: is_profile_general_agency?)
     end
 
   private
 
-    def find_broker_agency_profile
-      @broker_agency_profile = ::BrokerAgencyProfile.find(params[:id])
-      @broker_agency_profile ||= BenefitSponsors::Organizations::BrokerAgencyProfile.find(params[:id])
-      @id = @broker_agency_profile.id
+    def find_profile
+      @profile = ::BrokerAgencyProfile.find(params[:id]) || ::GeneralAgencyProfile.find(params[:id])
+      @profile ||= BenefitSponsors::Organizations::Profile.find(params[:id])
+    end
+
+    def is_profile_general_agency?
+      @profile.class.to_s == "GeneralAgencyProfile"
     end
   end
 end
