2c2731d2994253da20a02ac57e406710a3101f07
Prem Shaganti <prem.shaganti@dc.gov>
2019-02-13 15:16:43 -0500

    ref: address rspec warning initializers

--- components/sponsored_benefits/spec/controllers/sponsored_benefits/organizations/plan_design_organizations_controller_spec.rb
+++ components/sponsored_benefits/spec/controllers/sponsored_benefits/organizations/plan_design_organizations_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 require "#{SponsoredBenefits::Engine.root}/spec/shared_contexts/sponsored_benefits"
 
-USER_ROLES = [:with_hbx_staff_role, :with_broker_role]
+module UserRoles
+  USER_ROLES = [:with_hbx_staff_role, :with_broker_role] unless const_defined?(:USER_ROLES)
+end
 
 module SponsoredBenefits
+  include UserRoles
+
   RSpec.describe Organizations::PlanDesignOrganizationsController, type: :controller, dbclean: :around_each  do
     include_context "set up broker agency profile for BQT, by using configuration settings"
 
