06444e68e12e90fde3da2348f9b7dbc0e6655e4e
Prem Shaganti <prem.shaganti@dc.gov>
2019-02-13 13:22:31 -0500

    ref: address failing specs under sponsored benefits engine

--- spec/controllers/sponsored_benefits/organizations/plan_design_proposals/plans_controller_spec.rb
+++ spec/controllers/sponsored_benefits/organizations/plan_design_proposals/plans_controller_spec.rb
@@ -4,24 +4,31 @@ require "#{SponsoredBenefits::Engine.root}/spec/shared_contexts/sponsored_benefi
 module SponsoredBenefits
   RSpec.describe Organizations::PlanDesignProposals::PlansController, type: :controller, dbclean: :around_each  do
     routes { SponsoredBenefits::Engine.routes }
+
     include_context "set up broker agency profile for BQT, by using configuration settings"
 
-    let!(:person) { FactoryGirl.create(:person, :with_broker_role) }
+    let(:plans) { FactoryGirl.create_list(:plan, 5, :with_premium_tables, market: 'shop')}
+
+    let(:person) { FactoryGirl.create(:person, :with_broker_role).tap do |person|
+      person.broker_role.update_attributes(broker_agency_profile_id: broker_agency_profile.id.to_s)
+    end }
+
     let!(:user_with_broker_role) { FactoryGirl.create(:user, person: person) }
 
-    describe "GET #index" do #todo
-      it "returns a success response" do
-        plan_design_census_employee
-        benefit_application
-        person.broker_role.update_attributes(broker_agency_profile_id: plan_design_organization.owner_profile_id)
+    describe "GET #index" do
+      let(:carrier_profile_id) {plans.first.carrier_profile.id.to_s}
+
+      before :each do
         sign_in user_with_broker_role
-        get :index, plan_design_organization_id: plan_design_organization.id, selected_carrier_level: "single_carrier"
+        get :index, plan_design_organization_id: plan_design_organization.id, selected_carrier_level: "single_carrier", carrier_id: carrier_profile_id, active_year: TimeKeeper.date_of_record.year, format: :js
+      end
+
+      it "returns a success response" do
         expect(response).to have_http_status(:success)
       end
 
       it "should set plans instance variable" do
-
-
+        expect(assigns(:plans)).to eq ([plans.first])
       end
     end
   end
