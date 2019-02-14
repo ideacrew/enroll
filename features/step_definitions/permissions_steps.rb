256169526a680066542ca6ba937777f006ac12bb
harsha chalamala <chvreddy1015@gmail.com>
2019-02-14 14:36:42 -0500

    removed ambiguos step definitions

--- features/step_definitions/permissions_steps.rb
+++ features/step_definitions/permissions_steps.rb
@@ -1,5 +1,4 @@
 When(/^Hbx Admin clicks on Employers link$/) do
-<<<<<<< HEAD
 	click_link 'Employers'
 end
 
@@ -18,19 +17,7 @@ end
 When(/^Hbx Admin should not see an Force Publish button$/) do
 	expect(page).not_to have_content("Force Publish")
 end
-=======
-  click_link 'Employers'
-end
-
-Then(/^Hbx Admin should see an Force Publish button$/) do
-  expect(page).to have_content("Force Publish")
-end
-
-Then(/^Hbx Admin should not see an Force Publish button$/) do
-  expect(page).not_to have_content("Force Publish")
-end
 
 And(/^system date is between submission deadline & application effective date$/) do
   allow(TimeKeeper).to receive(:date_of_record).and_return((@custom_plan_year.start_on - 3.days))
 end
->>>>>>> origin/usecase_39450
