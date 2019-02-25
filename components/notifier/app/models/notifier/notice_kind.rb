dc4bd283d307086a937a17f25f01ead97d4cec62
raghuram <raghuramg83@gmail.com>
2019-02-25 11:12:58 -0500

    Added IVL conditions for notice builders, added settings for IVL

--- components/notifier/app/models/notifier/notice_kind.rb
+++ components/notifier/app/models/notifier/notice_kind.rb
@@ -86,11 +86,14 @@ module Notifier
       if finder_mapping.nil?
         raise ArgumentError.new("BOGUS EVENT...could n't find resoure mapping for event #{event_name}.")
       end
+
       @payload = payload
       @resource = finder_mapping.mapped_class.send(finder_mapping.search_method, payload[finder_mapping.identifier_key.to_s])
+
       if @resource.blank?
         raise ArgumentError.new("Bad Payload...could n't find resoure with #{payload[finder_mapping.identifier_key.to_s]}.")
       end
+
       generate_pdf_notice
       upload_and_send_secure_message
       send_generic_notice_alert
@@ -145,6 +148,14 @@ module Notifier
         from_state: aasm.from_state,
         to_state: aasm.to_state
         )
+    end
+
+    def individual_market?
+      market_kind == :aca_individual
+    end
+
+    def shop_market?
+      market_kind.blank? || market_kind == :aca_shop
     end
 
     # def self.markdown
