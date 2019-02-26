43e91cd8b63fcd2464a22a7173b06ea7041c29f3
krbhavith <krbhavith@gmail.com>
2019-02-26 14:05:59 -0500

    refs #38233_38234 spec update for group advance termination notice

--- spec/models/model_events/group_termination_confirmation_notice_spec.rb
+++ spec/models/model_events/group_termination_confirmation_notice_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'ModelEvents::GroupNonPaymentTerminationNotice', dbclean: :around_each do
+describe 'ModelEvents::GroupTerminationConfirmationNotice', dbclean: :around_each do
   let(:person) { FactoryGirl.create(:person, :with_family) }
   let(:family)  { person.primary_family }
   let!(:employer_profile) { FactoryGirl.create(:employer_profile) }
@@ -50,11 +50,35 @@ describe 'ModelEvents::GroupNonPaymentTerminationNotice', dbclean: :around_each 
         subject.plan_year_update(model_event)
       end
     end
+
+    context "when group terminated for voluntary reason" do
+      subject { Observers::NoticeObserver.new }
+
+       let(:model_event) { ModelEvents::ModelEvent.new(:group_termination_confirmation_notice, model_instance, {}) }
+
+       it "should trigger notice event" do
+        allow(model_instance).to receive(:termination_kind).and_return(nil)
+
+        expect(subject.notifier).to receive(:notify) do |event_name, payload|
+          expect(event_name).to eq "acapi.info.events.employer.group_advance_termination_confirmation"
+          expect(payload[:employer_id]).to eq employer_profile.hbx_id.to_s
+          expect(payload[:event_object_kind]).to eq 'PlanYear'
+          expect(payload[:event_object_id]).to eq model_instance.id.to_s
+        end
+
+        expect(subject.notifier).to receive(:notify) do |event_name, payload|
+          expect(event_name).to eq "acapi.info.events.employee.notify_employee_of_group_advance_termination"
+          expect(payload[:event_object_kind]).to eq 'PlanYear'
+          expect(payload[:event_object_id]).to eq model_instance.id.to_s
+        end
+        subject.plan_year_update(model_event)
+      end
+    end
   end
 
   describe "NoticeBuilder" do
 
-    context "when notice event notify_employer_of_group_non_payment_termination is received" do
+    context "when notice for employer is received" do
 
       let(:data_elements) {
         [
@@ -103,7 +127,7 @@ describe 'ModelEvents::GroupNonPaymentTerminationNotice', dbclean: :around_each 
       end
     end
 
-    context "when notice event notify_employee_of_group_non_payment_termination is received" do
+    context "when notice for employee is received" do
       let(:data_elements) {
         [
           "employee_profile.notice_date",
