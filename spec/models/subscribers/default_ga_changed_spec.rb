49c872a4ed85925e6fb067f588a159c5c5cb95f5
nisanth <nisanth.yaganti@gmail.com>
2019-02-28 16:34:32 -0500

    handling ga hired, fired notices & secure messages

--- spec/models/subscribers/default_ga_changed_spec.rb
+++ spec/models/subscribers/default_ga_changed_spec.rb
@@ -22,13 +22,13 @@ describe Subscribers::DefaultGaChanged do
       let(:pdo) {plan_design_organization_with_assigned_ga}
 
       it "should not do clear without pre_default_ga_id" do
-        expect(subject).not_to receive(:send_general_agency_assign_msg)
+        expect(subject.service).not_to receive(:send_message)
         subject.call(nil, nil, nil, nil, message.except("pre_default_ga_id"))
       end
 
       it "should do clear with pre_default_ga_id" do
         pdo
-        expect(subject).to receive(:send_general_agency_assign_msg)
+        expect(subject.service).to receive(:send_message)
         subject.call(nil, nil, nil, nil, message)
         pdo.reload
         expect(pdo.active_general_agency_account).to eq nil
@@ -49,7 +49,7 @@ describe Subscribers::DefaultGaChanged do
       it "should do change when employer_profile does not have active general_agency_profile" do
         pdo.general_agency_accounts.delete_all
         expect(pdo.active_general_agency_account).to eq nil
-        expect(subject).to receive(:send_general_agency_assign_msg)
+        expect(subject.service).to receive(:send_message)
         subject.call(nil, nil, nil, nil, message)
         pdo.reload
         expect(pdo.active_general_agency_account.general_agency_profile).to eq new_ga
@@ -57,7 +57,7 @@ describe Subscribers::DefaultGaChanged do
 
       it "should do not change when employer_profile have active general_agency_profile" do
         expect(pdo_with_ga.active_general_agency_account).not_to eq nil
-        expect(subject).not_to receive(:send_general_agency_assign_msg)
+        expect(subject.service).not_to receive(:send_message)
         subject.call(nil, nil, nil, nil, message)
         pdo_with_ga.reload
         expect(pdo_with_ga.active_general_agency_account.general_agency_profile).not_to eq new_ga
