6d3a52ac5968452427f8611ac943185267704dce
JCG <jaya.data23@gmail.com>
2019-02-28 17:27:54 -0500

    Refs:39595 Reverting checkmark code for download notices

--- components/notifier/app/controllers/notifier/notice_kinds_controller.rb
+++ components/notifier/app/controllers/notifier/notice_kinds_controller.rb
@@ -71,9 +71,8 @@ module Notifier
     end
 
     def download_notices
-      notices = Notifier::NoticeKind.where(:id.in => params['ids'].split(","))
-
-      send_data notices.to_csv,
+      #notices = Notifier::NoticeKind.where(:id.in => params['ids'].split(","))
+      send_data Notifier::NoticeKind.to_csv,
         :filename => "notices_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv",
         :disposition => 'attachment',
         :type => 'text/csv'
