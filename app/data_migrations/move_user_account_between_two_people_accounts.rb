88c4b094dd454a5121a3beec744eb19f8de33421
saikumar9 <saikumarkotagiri9@gmail.com>
2019-03-04 15:09:45 -0500

    Refs #40145 merge EE and ER accounts

--- app/data_migrations/move_user_account_between_two_people_accounts.rb
+++ app/data_migrations/move_user_account_between_two_people_accounts.rb
@@ -1,4 +1,5 @@
-require File.join(Rails.root, "lib/mongoid_migration_task")
+require File.join(Rails.root, 'lib/mongoid_migration_task')
+
 class MoveUserAccountBetweenTwoPeopleAccounts < MongoidMigrationTask
   def migrate
     trigger_single_table_inheritance_auto_load_of_child = Document
@@ -6,21 +7,24 @@ class MoveUserAccountBetweenTwoPeopleAccounts < MongoidMigrationTask
     hbx_id_2=ENV['hbx_id_2']
     person1 = Person.where(hbx_id:hbx_id_1).first
     person2 = Person.where(hbx_id:hbx_id_2).first
-    if person1.nil?
-      puts "No person found with hbx_id #{hbx_id_1}" unless Rails.env.test?
-    elsif person2.nil?
-      puts "No person found with hbx_id #{hbx_id_1}" unless Rails.env.test?
-    else
-      user=person1.user
-      if user.nil?
-        puts "person with hbx_id  #{hbx_id_1} has no user" unless Rails.env.test?
+
+    begin
+      if person1.nil?
+        puts "No person found with hbx_id #{hbx_id_1}" unless Rails.env.test?
+      elsif person2.nil?
+        puts "No person found with hbx_id #{hbx_id_1}" unless Rails.env.test?
       else
-        user_id = person1.user_id
-        person1.unset(:user_id)
-        person2.set(user_id: user_id)
-        puts "move the user account from  #{hbx_id_1} to #{hbx_id_2}" unless Rails.env.test?
+        if person1.user.nil?
+          puts "person with hbx_id #{hbx_id_1} has no user" unless Rails.env.test?
+        else
+          user_id = person1.user_id
+          person1.unset(:user_id)
+          person2.set(user_id: user_id)
+          puts "move the user account from  #{hbx_id_1} to #{hbx_id_2}" unless Rails.env.test?
+        end
       end
+    rescue Exception => e
+      puts "Error occured during the process, #{e.message}" unless Rails.env.test?
     end
   end
 end
-
