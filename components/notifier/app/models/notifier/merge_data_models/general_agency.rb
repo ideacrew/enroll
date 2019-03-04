eb1988ade6a9cd36f741e89e07cd721854c551e0
Battula <vrbattula@outlook.com>
2019-03-04 15:40:58 -0500

    refs #39988 Fix failing spec.

--- components/notifier/app/models/notifier/merge_data_models/general_agency.rb
+++ components/notifier/app/models/notifier/merge_data_models/general_agency.rb
@@ -11,8 +11,8 @@ module Notifier
     attribute :email, String
     attribute :broker, MergeDataModels::Broker
     attribute :legal_name, String
-    attribute :assignment_date, Date
-    attribute :termination_date, Date
+    attribute :assignment_date, String
+    attribute :termination_date, String
     attribute :employer_name, String
     attribute :employer_poc_firstname, String
     attribute :employer_poc_lastname, String
