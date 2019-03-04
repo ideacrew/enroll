728d2ba7b1f2a6437b338e89ee0fb4d950b83b51
saikumar9 <saikumarkotagiri9@gmail.com>
2019-03-04 15:58:52 -0500

    Refs #40145 refactor

--- app/data_migrations/move_user_account_between_two_people_accounts.rb
+++ app/data_migrations/move_user_account_between_two_people_accounts.rb
@@ -3,8 +3,8 @@ require File.join(Rails.root, 'lib/mongoid_migration_task')
 class MoveUserAccountBetweenTwoPeopleAccounts < MongoidMigrationTask
   def migrate
     trigger_single_table_inheritance_auto_load_of_child = Document
-    hbx_id_1=ENV['hbx_id_1']
-    hbx_id_2=ENV['hbx_id_2']
+    hbx_id_1 = ENV['hbx_id_1']
+    hbx_id_2 = ENV['hbx_id_2']
     person1 = Person.where(hbx_id:hbx_id_1).first
     person2 = Person.where(hbx_id:hbx_id_2).first
 
