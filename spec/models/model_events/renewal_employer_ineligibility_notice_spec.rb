72632be8c7f4ac133645bf55e0e20abf21c5b80d
Jayanth <jayanth.rordev@gmail.com>
2019-03-04 10:18:45 -0500

    refs#40019 update specs with new modelevent

--- spec/models/model_events/renewal_employer_ineligibility_notice_spec.rb
+++ spec/models/model_events/renewal_employer_ineligibility_notice_spec.rb
@@ -12,14 +12,13 @@ describe 'ModelEvents::RenewalEmployerIneligibiltyNotice', dbclean: :around_each
   let!(:active_benefit_group) { FactoryGirl.create(:benefit_group, plan_year: plan_year, title: "Benefits #{plan_year.start_on.year}") }
   let!(:benefit_group_assignment)  { FactoryGirl.create(:benefit_group_assignment, benefit_group: active_benefit_group, census_employee: census_employee) }
   let!(:employee_role) { FactoryGirl.create(:employee_role, employer_profile: employer,census_employee_id: census_employee.id, person: person) }
-  let!(:model_instance) { FactoryGirl.create(:plan_year, employer_profile: employer, start_on: start_on + 1.year, :aasm_state => 'renewing_enrolling' ) }
+  let!(:model_instance) { FactoryGirl.create(:plan_year, employer_profile: employer, start_on: start_on + 1.year, :aasm_state => 'renewing_enrolling', open_enrollment_end_on:  TimeKeeper.date_of_record.prev_day) }
   let!(:renewal_benefit_group) { FactoryGirl.create(:benefit_group, plan_year: model_instance, title: "Benefits #{model_instance.start_on.year}") }
   let!(:date_mock_object) { (start_on - 2.months).end_of_month.prev_day }
   let!(:hbx_enrollment) { FactoryGirl.create(:hbx_enrollment, :with_enrollment_members,
     household: family.active_household,
     employee_role_id: employee_role.id,
     aasm_state: "shopping",
-    enrollment_kind: "special_enrollment",
     benefit_group_id: renewal_benefit_group.id,
     benefit_group_assignment_id: benefit_group_assignment.id )
   }
@@ -46,7 +45,7 @@ describe 'ModelEvents::RenewalEmployerIneligibiltyNotice', dbclean: :around_each
             expect(model_event).to have_attributes(:event_key => :application_denied, :klass_instance => model_instance, :options => {})
           end
         end
-        model_instance.close_open_enrollment!
+        model_instance.advance_date!
       end
     end
   end
