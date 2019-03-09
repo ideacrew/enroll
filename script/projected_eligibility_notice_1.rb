3016c45a9461fa34f6490ea9fdad8df0456de45b
JCG <jaya.data23@gmail.com>
2019-03-09 02:09:20 -0500

    Refs:40299 Typo

--- script/projected_eligibility_notice_1.rb
+++ script/projected_eligibility_notice_1.rb
@@ -26,41 +26,40 @@ CSV.open(file_name, "w", force_quotes: true) do |csv|
   notice_trigger = event_kind.notice_triggers.first
   @data_hash.each do |family_id, members|
     #begin
-      subscriber = members.detect{ |m| m["dependent"].casecmp('NO').zero? }
-      dependents = members.select{|m| m["dependent"].casecmp('YES').zero? }
-      primary_person = HbxEnrollment.by_hbx_id(members.first["applid"]).first.family.primary_person rescue nil
-      next if primary_person.nil?
-      next if subscriber.present? && subscriber["resident"] && subscriber["resident"].casecmp('NO').zero?
-      next if members.select{ |m| m["incarcerated"] && m["incarcerated"].casecmp('YES').zero? }.present?
-      next if members.any?{ |m| m["citizen_status"].blank? || (m["citizen_status"] == "non_native_not_lawfully_present_in_us") || (m["citizen_status"] == "not_lawfully_present_in_us")}
-      consumer_role = primary_person.consumer_role
-      if consumer_role.present?
-        begin
-          if ARGV.include?("send_via_notice_eng")
-            binding.pry
-            @notifier = Services::NoticeService.new
-            @notifier.deliver(
-              recipient: consumer_role,
-              event_object: consumer_role,
-              notice_event: 'projected_eligibility_notice_1',
-              notice_params: {
-                dependents: dependents.map(&:to_hash),
-                primary_member: subscriber.to_hash
-              }
-            )
-          end
-          csv << [
-            family_id,
-            primary_person.hbx_id,
-            primary_person.full_name
-          ]
-          puts "***************** Notice delivered to #{primary_person.hbx_id} *****************" unless Rails.env.test?
-        rescue StandardError => e
-          puts "Unable to deliver to #{primary_person.hbx_id} due to the following error #{e}" unless Rails.env.test?
+    subscriber = members.detect{ |m| m["dependent"].casecmp('NO').zero? }
+    dependents = members.select{|m| m["dependent"].casecmp('YES').zero? }
+    primary_person = HbxEnrollment.by_hbx_id(members.first["applid"]).first.family.primary_person rescue nil
+    next if primary_person.nil?
+    next if subscriber.present? && subscriber["resident"] && subscriber["resident"].casecmp('NO').zero?
+    next if members.select{ |m| m["incarcerated"] && m["incarcerated"].casecmp('YES').zero? }.present?
+    next if members.any?{ |m| m["citizen_status"].blank? || (m["citizen_status"] == "non_native_not_lawfully_present_in_us") || (m["citizen_status"] == "not_lawfully_present_in_us")}
+    consumer_role = primary_person.consumer_role
+    if consumer_role.present?
+      begin
+        if ARGV.include?("send_via_notice_eng")
+          @notifier = Services::NoticeService.new
+          @notifier.deliver(
+            recipient: consumer_role,
+            event_object: consumer_role,
+            notice_event: 'projected_eligibility_notice_1',
+            notice_params: {
+              dependents: dependents.map(&:to_hash),
+              primary_member: subscriber.to_hash
+            }
+          )
         end
-      else
-        puts "No consumer role for #{primary_person.hbx_id} -- #{e}" unless Rails.env.test?
+        csv << [
+          family_id,
+          primary_person.hbx_id,
+          primary_person.full_name
+        ]
+        puts "***************** Notice delivered to #{primary_person.hbx_id} *****************" unless Rails.env.test?
+      rescue StandardError => e
+        puts "Unable to deliver to #{primary_person.hbx_id} due to the following error #{e}" unless Rails.env.test?
       end
+    else
+      puts "No consumer role for #{primary_person.hbx_id} -- #{e}" unless Rails.env.test?
+    end
     # rescue StandardError => e
     #   puts "Unable to process family_id: #{family_id} due to the following error #{e}" unless Rails.env.test?
     # end
