9bcab966c9a45c15aaa6963e92d645209dfb19b0
krbhavith <krbhavith@gmail.com>
2019-03-12 12:49:11 -0400

    refs #39902 stubbed missing data in merge_data_models

--- components/notifier/app/models/notifier/merge_data_models/broker.rb
+++ components/notifier/app/models/notifier/merge_data_models/broker.rb
@@ -17,6 +17,10 @@ module Notifier
     def self.stubbed_object
       Notifier::MergeDataModels::Broker.new({
         primary_fullname: 'Count Olaf',
+        primary_first_name: 'Count',
+        primary_last_name: 'Olaf',
+        assignment_date: TimeKeeper.date_of_record.strftime('%m/%d/%Y'),
+        termination_date: TimeKeeper.date_of_record.strftime('%m/%d/%Y'),
         organization: 'Best Brokers LLC',
         phone: '703-303-1007',
         email: 'count.olaf@bestbrokers.llc',
