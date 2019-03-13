e3f4513a152b3252ad162614bc729d07c750d238
nisanth <nisanth.yaganti@gmail.com>
2019-03-13 17:56:31 -0400

    adding required dummy models/changes

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,6 +2,56 @@ class User
 
   include Mongoid::Document
   include Mongoid::Timestamps
+  include AuthorizationConcern
+  include PermissionsConcern
+
+  field :hints, type: Boolean, default: true
+  # for i18L
+  field :preferred_language, type: String, default: "en"
+
+  ## Enable Admin approval
+  ## Seed: https://github.com/plataformatec/devise/wiki/How-To%3a-Require-admin-to-activate-account-before-sign_in
+  field :approved, type: Boolean, default: true
+
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ##RIDP
+  field :identity_verified_date, type: Date
+  field :identity_final_decision_code, type: String
+  field :identity_final_decision_transaction_id, type: String
+  field :identity_response_code, type: String
+  field :identity_response_description_text, type: String
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+  field :idp_uuid, type: String
+
+  field :authentication_token
+  field :roles, :type => Array, :default => []
+
+  # Oracle Identity Manager ID
+  field :oim_id, type: String, default: ""
+
+  field :last_portal_visited, type: String
+  field :idp_verified, type: Boolean, default: false
+
+  belongs_to :profile, polymorphic: true
+
+  has_one :person
+  accepts_nested_attributes_for :person, :allow_destroy => true
 
   def self.generate_valid_password
   end
