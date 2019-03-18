c5f26930742fd2e6542423d23853c0220ce886d7
harsha280 <harsha.vskr@gmail.com>
2019-03-18 11:55:29 -0400

    Refs 36639 Added Spec for update verification type status

--- app/data_migrations/update_verification_type_status.rb
+++ app/data_migrations/update_verification_type_status.rb
@@ -2,12 +2,12 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateVerificationTypeStatus < MongoidMigrationTask
   def migrate
     begin
-      person = Person.where(id: ENV['hbx_id']).first
+      person = Person.where(hbx_id: ENV['hbx_id']).first
       if person.nil?
         puts "No person record found for given HBX ID" unless Rails.env.test?
       else
-        verification_type = person.verification_types.where(type_name: ENV['verification_type_name'] )
-        verification_type.pass_type if verification_type && verification_type.type_name == 'pending'
+        verification_type = person.verification_types.where(type_name: ENV['verification_type_name']).first
+        verification_type.pass_type if verification_type && verification_type.validation_status == 'pending'
         puts "Changed verification type validation status" unless Rails.env.test?
       end
     rescue StandardError => e
