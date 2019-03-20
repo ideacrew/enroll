5888bb0fb21e765e0a2aa68153dbbf0e99734ab6
Trey Evans <tevans@Just-Plain-Awful.local>
2019-03-20 12:05:48 -0400

    Add full spec runner.

---
+++ test_utilities/run_all_specs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+result=0
+
+base="$( dirname "${BASH_SOURCE[0]}" )/.."
+
+cd $base
+
+root=`pwd -P`
+
+for test_dir in `ls -1 $root/components/ | grep -v old_sponsored_benefits`; do
+  echo $root/components/$test_dir
+  cd $root/components/$test_dir
+  bundle install
+  bundle exec rspec --fail-fast
+  ((result+=$?))
+  if [ $result -ne 0 ]; then
+    echo "ENGINE FAILED"
+	  exit $result
+  fi
+done
+
+cd $root
+bundle exec rake spec:parallel[3]
