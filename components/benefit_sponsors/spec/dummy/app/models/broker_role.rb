80c7efe8a8c57e2bd4281492d4356e3b4760be79
raghuram <raghuramg83@gmail.com>
2019-03-20 17:53:36 -0400

    BenefitSponsors engine: application controller spec fixes

--- components/benefit_sponsors/spec/dummy/app/models/broker_role.rb
+++ components/benefit_sponsors/spec/dummy/app/models/broker_role.rb
@@ -1,6 +1,7 @@
 class BrokerRole
   include Mongoid::Document
   include Mongoid::Timestamps
+  include AASM
 
   PROVIDER_KINDS = %W[broker assister]
   BROKER_UPDATED_EVENT_NAME = "acapi.info.events.broker.updated"
@@ -76,6 +77,16 @@ class BrokerRole
 
   def email
     parent.emails.detect { |email| email.kind == "work" }
+  end
+
+  aasm do
+    state :applicant, initial: true
+    state :active
+    state :denied
+    state :decertified
+    state :broker_agency_pending
+    state :broker_agency_declined
+    state :broker_agency_terminated
   end
   
   class << self
