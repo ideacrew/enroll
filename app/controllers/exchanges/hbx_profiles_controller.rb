aae4c06aa55cc9a6b406e97e8f97b904f2e59a2e
Mahesh Kurma <maheshruby405@gmail.com>
2019-03-21 20:51:12 -0400

    refs #40246 12 added check to show warnings

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -440,7 +440,8 @@ def employer_poc
     @element_to_replace_id = params[:row_actions_id]
     @organization = Organization.find(@element_to_replace_id.split('_').last)
     @plan_year = @organization.renewing_or_draft_py
-    if @plan_year.may_force_publish? && (params[:publish_with_warnings] == 'true' || ((@plan_year.application_eligibility_warnings.blank?) && params[:publish_with_warnings] == 'false'))
+    @check_warnings = @plan_year.application_eligibility_warnings.blank? || @plan_year.application_errors.present?
+    if @plan_year.may_force_publish? && (params[:publish_with_warnings] == 'true' || (check_warnings && params[:publish_with_warnings] == 'false'))
       @plan_year.force_publish!
     end
     respond_to do |format|
