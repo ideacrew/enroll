0cc59b902c6bbcefb754889d2061b12f4039df10
saimekala07 <saimekala07@gmail.com>
2019-03-28 12:49:46 -0400

    refs #40670 Revert

--- spec/views/events/individuals/created.xml.haml_spec.rb
+++ spec/views/events/individuals/created.xml.haml_spec.rb
@@ -48,30 +48,5 @@ RSpec.describe "events/individuals/created.haml.erb", dbclean: :after_each do
     it "should have one office_location" do
       expect(@doc.xpath("//x:office_locations", "x" => "http://openhbx.org/api/terms/1.0").count).to eq 1
     end
-
-    context "when person has phone with kind = phone main" do
-
-      before :each do
-        person_broker.phones.where(kind: "work").first.update_attribute(:kind,'phone main')
-        render :template => "events/individuals/created", :locals => { :individual => person_broker}
-        @doc = Nokogiri::XML(rendered)
-      end
-
-      it "should return person phones kind" do
-        expect(person_broker.phones.map(&:kind)).to eq ['phone main','home','home']
-      end
-
-      it "should render template with phone has kind = work" do
-        expect(@doc.xpath("//x:person/x:phones/x:phone[1]/x:type", "x" => "http://openhbx.org/api/terms/1.0").text).to eq "urn:openhbx:terms:v1:phone_type#work"
-      end
-
-      it "should have phone" do
-        expect(@doc.xpath("//x:person/x:phones/x:phone[2]/x:type", "x" => "http://openhbx.org/api/terms/1.0").text).to eq "urn:openhbx:terms:v1:phone_type#home"
-      end
-
-      it "should have phone" do
-        expect(@doc.xpath("//x:person/x:phones/x:phone[3]/x:type", "x" => "http://openhbx.org/api/terms/1.0").text).to eq "urn:openhbx:terms:v1:phone_type#home"
-      end
-    end
   end
 end
