23e82154a9e8477112e21ba28ff0d0cc8145f428
saimekala07 <saimekala07@gmail.com>
2019-03-28 11:42:09 -0400

    Adding Spec

--- spec/views/events/individuals/created.xml.haml_spec.rb
+++ spec/views/events/individuals/created.xml.haml_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 require File.join(Rails.root, "spec", "support", "acapi_vocabulary_spec_helpers")
 
-RSpec.describe "events/individuals/created.haml.erb" do
+RSpec.describe "events/individuals/created.haml.erb", dbclean: :after_each do
+  include AcapiVocabularySpecHelpers
   (1..15).to_a.each do |rnd|
 
     describe "given a generated individual, round #{rnd}" do
@@ -22,5 +23,55 @@ RSpec.describe "events/individuals/created.haml.erb" do
       end
     end
 
+  end
+
+  describe "given a person has broker role", dbclean: :after_each do
+
+    let(:broker_agency_organization) { FactoryGirl.create(:benefit_sponsors_organizations_general_organization,:with_site,:with_broker_agency_profile)}
+    let!(:broker_agency_profile) { broker_agency_organization.broker_agency_profile }
+    let!(:person_broker) {FactoryGirl.create(:person,:with_work_email, :with_work_phone)}
+    let!(:broker) {FactoryGirl.create(:broker_role,aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, person: person_broker)}
+
+    before :each do
+      render :template => "events/individuals/created", :locals => { :individual => person_broker}
+      @doc = Nokogiri::XML(rendered)
+    end
+
+    it "should be schema valid" do
+      expect(validate_with_schema(@doc)).to eq []
+    end
+
+    it "should have one broker role" do
+      expect(@doc.xpath("//x:broker_roles/x:broker_role", "x" => "http://openhbx.org/api/terms/1.0").count).to eq 1
+    end
+
+    it "should have one office_location" do
+      expect(@doc.xpath("//x:office_locations", "x" => "http://openhbx.org/api/terms/1.0").count).to eq 1
+    end
+
+    context "when person has phone with kind = phone main" do
+
+      before :each do
+        person_broker.phones.where(kind: "work").first.update_attribute(:kind,'phone main')
+        render :template => "events/individuals/created", :locals => { :individual => person_broker}
+        @doc = Nokogiri::XML(rendered)
+      end
+
+      it "should return person phones kind" do
+        expect(person_broker.phones.map(&:kind)).to eq ['phone main','home','home']
+      end
+
+      it "should render template with phone has kind = work" do
+        expect(@doc.xpath("//x:person/x:phones/x:phone[1]/x:type", "x" => "http://openhbx.org/api/terms/1.0").text).to eq "urn:openhbx:terms:v1:phone_type#work"
+      end
+
+      it "should have phone" do
+        expect(@doc.xpath("//x:person/x:phones/x:phone[2]/x:type", "x" => "http://openhbx.org/api/terms/1.0").text).to eq "urn:openhbx:terms:v1:phone_type#home"
+      end
+
+      it "should have phone" do
+        expect(@doc.xpath("//x:person/x:phones/x:phone[3]/x:type", "x" => "http://openhbx.org/api/terms/1.0").text).to eq "urn:openhbx:terms:v1:phone_type#home"
+      end
+    end
   end
 end
