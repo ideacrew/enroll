bf813c85c175d3e09afbf8ba739c84b5c5dbcd75
Bill Transue <transue@gmail.com>
2019-03-28 10:51:33 -0400

    Update adding base errors to models

--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/sponsored_benefit.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/sponsored_benefit.rb
@@ -40,7 +40,7 @@ module BenefitSponsors
 
       def product_package_exists
         if product_package.blank? && source_kind == :benefit_sponsor_catalog
-          self.errors.add(:base => "Unable to find mappable product package")
+          self.errors.add(:base, "Unable to find mappable product package")
         end
       end
 
