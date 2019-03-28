569121a3616e4dd1a0541a90ef62f62717b07b6f
Bill Transue <transue@gmail.com>
2019-03-28 11:46:40 -0400

    Use environment variable ENROLL_REVIEW_ENVIRONMENT

--- spec/views/ui-components/v1/layouts/_header.slim_spec.rb
+++ spec/views/ui-components/v1/layouts/_header.slim_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe "layouts/_header.html.erb" do
   describe "Header Styles" do
     context "when environment variable PRODUCTION is set to true" do
       it "does NOT render pre_prod_nav_color CSS class" do
-        ClimateControl.modify PRODUCTION: "true" do
-          visit root_url
-          expect(page).to_not have_css("nav.pre_prod_nav_color")
-        end
+        visit root_url
+        expect(page).to_not have_css("nav.pre_prod_nav_color")
       end
     end
 
     context "when environment variable PRODUCTION is not set" do
       it "renders pre_prod_nav_color CSS class" do
-        visit root_url
-        expect(page).to have_css("nav.pre_prod_nav_color")
+        ClimateControl.modify ENROLL_REVIEW_ENVIRONMENT: "true" do
+          visit root_url
+          expect(page).to have_css("nav.pre_prod_nav_color")
+        end
       end
     end
   end
