45b8e2d69a4072f9d5d3c3512ec17b8de6ce7e10
nisanth <nisanth.yaganti@gmail.com>
2019-03-27 22:49:46 -0400

    loading plan design proposal edit as html

--- spec/controllers/sponsored_benefits/organizations/plan_design_proposals_controller_spec.rb
+++ spec/controllers/sponsored_benefits/organizations/plan_design_proposals_controller_spec.rb
@@ -126,17 +126,12 @@ module SponsoredBenefits
             xhr :post, :create, { plan_design_organization_id: plan_design_organization.to_param, forms_plan_design_proposal: valid_attributes }, valid_session
           }.to change { plan_design_organization.reload.plan_design_proposals.count }.by(1)
         end
-
-        it "redirects to the created benefit_application" do
-          xhr :post, :create, { plan_design_organization_id: plan_design_organization.to_param, forms_plan_design_proposal: valid_attributes }, valid_session
-          expect(response).to have_http_status(:redirect)
-        end
       end
 
       context "with invalid params" do
         it "returns a success response (i.e. to display the 'new' template)" do
           xhr :post, :create, { plan_design_organization_id: plan_design_organization.to_param, forms_plan_design_proposal: invalid_attributes}, valid_session
-          expect(response).to have_http_status(:redirect)
+          expect(response).to be_success
         end
       end
     end
