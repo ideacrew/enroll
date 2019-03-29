8a85440b065d72033f7132d7f06c6e1690ec95ff
JCG <jaya.data23@gmail.com>
2019-03-29 15:30:13 -0400

    Refs:40721 Refacored age and dependent_service naming convention with regards to IVL_PRE notice through Notice Engine

--- components/notifier/app/models/notifier/builders/consumer_role.rb
+++ components/notifier/app/models/notifier/builders/consumer_role.rb
@@ -63,7 +63,7 @@ module Notifier
             consumer_role.person.age_on(TimeKeeper.date_of_record)
           else
             (
-              Date.current.year - Date.parse(payload['notice_params']['primary_member']['dob']).year
+              Date.current.year - Date.strptime(payload['notice_params']['primary_member']['dob'],"%m-%d-%Y").year
             )
           end
       end
