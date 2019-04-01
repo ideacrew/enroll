bb86edc130d607e1dd9340ec1cd91527af388024
Zachary Landes <zach.g.landes@gmail.com>
2019-04-01 16:42:52 -0400

    passing dd_coverage_household_member_spec.rb

--- spec/data_migrations/add_coverage_household_member_spec.rb
+++ spec/data_migrations/add_coverage_household_member_spec.rb
@@ -25,20 +25,25 @@ describe AddCoverageHouseholdMember, dbclean: :after_each do
     end
 
     it "should add a household" do
-      family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members.each do |chm|
-        chm.delete
-        subject.migrate
-        family.households.first.reload
-        expect(family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members).not_to eq []
+        ClimateControl.modify family_member_id: family.family_members.first.id, hbx_id: person.hbx_id  do
+        family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members.each do |chm|
+          chm.delete
+          subject.migrate
+          family.households.first.reload
+          expect(family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members).not_to eq []
+        end
       end
     end
 
 
     it "should not add a household if already exists" do
-      size = family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members.size
-      subject.migrate
-      family.households.first.reload
-      expect(family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members.size).to eq size
+      ClimateControl.modify family_member_id: family.family_members.first.id, hbx_id: person.hbx_id do
+        size = family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members.size
+        subject.migrate
+        family.households.first.reload
+        expect(family.households.first.coverage_households.where(:is_immediate_family => true).first.coverage_household_members.size).to eq size
+      end
     end
   end
+
 end
