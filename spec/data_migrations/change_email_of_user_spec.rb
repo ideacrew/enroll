19001397080d10698c225088761c5cb7ea685d33
Jayanth <jayanth.rordev@gmail.com>
2019-04-02 16:34:25 -0400

    refs#ivl_new_model_rails_5 fix migration task

--- spec/data_migrations/change_email_of_user_spec.rb
+++ spec/data_migrations/change_email_of_user_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 require File.join(Rails.root, "app", "data_migrations", "change_email_of_user")
 describe ChangeEmailOfUser, dbclean: :after_each do
   let(:given_task_name) { "change_email_of_user" }
+  let(:user) { FactoryBot.create(:user) }
+
   subject { ChangeEmailOfUser.new(given_task_name, double(:current_scope => nil)) }
   describe "given a task name" do
     it "has the given task name" do
@@ -9,31 +11,30 @@ describe ChangeEmailOfUser, dbclean: :after_each do
     end
   end
   describe "change the email of a user" do
-    let(:user) { FactoryBot.create(:user) }
-    before(:each) do
-      allow(ENV).to receive(:[]).with("user_oimid").and_return(user.oim_id)
-      allow(ENV).to receive(:[]).with("new_email").and_return("newemail@gmail.com")
-    end
+
     it "should change the email of the user" do
-      email=user.email
-      expect(user.email).to eq email
-      subject.migrate
-      user.reload
-      expect(user.email).to eq "newemail@gmail.com"
+      ClimateControl.modify user_oimid: user.oim_id, new_email: "newemail@gmail.com" do 
+        email=user.email
+        expect(user.email).to eq email
+        subject.migrate
+        user.reload
+        expect(user.email).to eq "newemail@gmail.com"
+      end
     end
   end
   describe "not change the email if the user not found" do
-    let(:user) { FactoryBot.create(:user) }
-    before(:each) do
-      allow(ENV).to receive(:[]).with("user_oimid").and_return("")
-      allow(ENV).to receive(:[]).with("new_email").and_return("newemail@gmail.com")
-    end
     it "should change the email of the user" do
-      email=user.email
-      expect(user.email).to eq email
-      subject.migrate
-      user.reload
-      expect(user.email).to eq email
+      with_modified_env user_oimid: "", new_email: "newemail@gmail.com" do 
+        email=user.email
+        expect(user.email).to eq email
+        subject.migrate
+        user.reload
+        expect(user.email).to eq email
+      end
     end
+  end
+
+  def with_modified_env(options, &block)
+    ClimateControl.modify(options, &block)
   end
 end
