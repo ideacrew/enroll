5187a2bc0c014317ff6430f09bb1c99c8910e42e
Zachary Landes <zach.g.landes@gmail.com>
2019-04-02 17:24:44 -0400

    passing spec/data_migrations/remove_coverage_household_for_inactive_family_member_spec.rb

--- spec/data_migrations/remove_coverage_household_for_inactive_family_member_spec.rb
+++ spec/data_migrations/remove_coverage_household_for_inactive_family_member_spec.rb
@@ -15,27 +15,27 @@ describe RemoveCoverageHouseHoldMemberForInactiveFamilyMember, dbclean: :after_e
     let(:person) { FactoryBot.create(:person) }
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
     let(:family_member){ FactoryBot.create(:family_member,family: family)}
-    before do
-      allow(ENV).to receive(:[]).with('person_hbx_id').and_return person.hbx_id
-      allow(ENV).to receive(:[]).with('family_member_hbx_id').and_return family_member.hbx_id
-    end
 
     it "should remove an inactive family member to household" do
-      family.active_household.immediate_family_coverage_household.coverage_household_members.new(:is_subscriber => true, :family_member_id => "567678789")
-      family.active_household.immediate_family_coverage_household.save
-      size = family.active_household.immediate_family_coverage_household.coverage_household_members.size
-      subject.migrate
-      person.reload
-      family.reload
-      expect(family.active_household.immediate_family_coverage_household.coverage_household_members.size) == size-1
+      ClimateControl.modify person_hbx_id: person.hbx_id, family_member_hbx_id:family_member.hbx_id do 
+        family.active_household.immediate_family_coverage_household.coverage_household_members.new(:is_subscriber => true, :family_member_id => "567678789")
+        family.active_household.immediate_family_coverage_household.save
+        size = family.active_household.immediate_family_coverage_household.coverage_household_members.size
+        subject.migrate
+        person.reload
+        family.reload
+        expect(family.active_household.immediate_family_coverage_household.coverage_household_members.size) == size-1
+      end
     end
 
     it "should not remove any family member from coverage household member" do
-      size = family.active_household.immediate_family_coverage_household.coverage_household_members.size
-      subject.migrate
-      person.reload
-      family.reload
-      expect(family.active_household.immediate_family_coverage_household.coverage_household_members.size) == size
+      ClimateControl.modify person_hbx_id: person.hbx_id, family_member_hbx_id:family_member.hbx_id do 
+        size = family.active_household.immediate_family_coverage_household.coverage_household_members.size
+        subject.migrate
+        person.reload
+        family.reload
+        expect(family.active_household.immediate_family_coverage_household.coverage_household_members.size) == size
+      end
     end
   end
 end
