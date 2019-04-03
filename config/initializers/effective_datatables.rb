3bd788c77bf8b61efc8b6b848c74afe562d0e0f0
Prem Shaganti <prem.shaganti@dc.gov>
2019-04-03 13:31:51 -0400

    Ref: support for newer version of datatable

--- config/initializers/effective_datatables.rb
+++ config/initializers/effective_datatables.rb
@@ -2,67 +2,42 @@ EffectiveDatatables.setup do |config|
   # Authorization Method
   #
   # This method is called by all controller actions with the appropriate action and resource
-  # If the method returns false, an Effective::AccessDenied Error will be raised (see README.md for complete info)
+  # If it raises an exception or returns false, an Effective::AccessDenied Error will be raised
   #
-  # Use via Proc (and with CanCan):
-  # config.authorization_method = Proc.new { |controller, action, resource| authorize!(action, resource) }
+  # Use via Proc:
+  # Proc.new { |controller, action, resource| authorize!(action, resource) }       # CanCan
+  # Proc.new { |controller, action, resource| can?(action, resource) }             # CanCan with skip_authorization_check
+  # Proc.new { |controller, action, resource| authorize "#{action}?", resource }   # Pundit
+  # Proc.new { |controller, action, resource| current_user.is?(:admin) }           # Custom logic
   #
-  # Use via custom method:
+  # Use via Boolean:
+  # config.authorization_method = true  # Always authorized
+  # config.authorization_method = false # Always unauthorized
+  #
+  # Use via Method (probably in your application_controller.rb):
   # config.authorization_method = :my_authorization_method
-  #
-  # And then in your application_controller.rb:
-  #
-  # def my_authorization_method(action, resource)
-  #   current_user.is?(:admin)
+  # def my_authorization_method(resource, action)
+  #   true
   # end
-  #
-  # Or disable the check completely:
-  # config.authorization_method = false
+  config.authorization_method = Proc.new { |controller, action, resource| authorize!(action, resource) }
 
-  config.authorization_method = Proc.new { |controller, action, resource| true } # All users can see every screen
+  # Default number of entries shown per page
+  # Valid options are: 5, 10, 25, 50, 100, 250, 500, :all
+  config.default_length = 25
 
-  # Date & DateTime Format
-  # By default, format Date and DateTime values with the following
-  # config.date_format = '%Y-%m-%d'
-  # config.datetime_format = '%Y-%m-%d %H:%M'
+  # Default class used on the <table> tag
+  config.html_class = 'table table-hover'
 
-  # Boolean format
-  # If :yes_no, display as yes_no instead of true/false
-  # config.boolean_format = :yes_no
+  # Log search/sort information to the console
+  config.debug = true
 
-  # # Format integer columns with the following proc or function
-  # config.integer_format = :number_with_delimiter
+  # If a user has previously visited this page and is returning, use the cookie to restore last session
+  # Irregardless of this setting, effective_datatables still uses a cookie to function
+  config.save_state = true
 
-  # # Default number of entries shown per page
-  # # Valid options are: 10, 25, 50, 100, 250, 1000, :all
-  # config.default_entries = 10
-
-  # When using the actions_column DSL method, apply the following behavior
-  # Valid values for each action are:
-  # true (always include a link to this action)
-  # false (never include a link to this action)
-  # :authorize (include link only if authorized to show/edit/destroy the resource class)
-  # :authorize_each (include link only if authorized to show/edit/destroy the resource)
-  # Proc.new { |resource| can?(:edit, resource.parent) }
-  #
-  # When :authorize, a single check will be made via the above config.authorization_method
-  # to see if the current_user has permission to :show/:edit/:destroy the collection class, i.e. Post
-  # EffectiveDatatables.authorized? (:show || :edit || :destroy), Post
-  # This is a faster permission check, because it only checks the class once
-  #
-  # If you want to authorize each individual resource, use :authorize_each
-  # EffectiveDatatables.authorized? (:show || :edit || :destroy), Post.find(1)
-  #
-  # You can override these defaults on a per-table basis
-  # by calling `actions_column(show: false, edit: true, destroy: :authorize)`
-  # config.actions_column = {
-  #   show: :authorize,
-  #   edit: :authorize,
-  #   destroy: :authorize,
-  #   unarchive: :authorize
-  # }
-
-  # Which packages to load when using the charts DSL
-  # config.google_chart_packages = ['corechart', 'gauge', 'geochart']
+  # Configure the _effective_dt cookie.
+  config.cookie_max_size = 2000       # String size. Final byte size is about 1.5 times bigger, after rails signs it
+  config.cookie_domain = :all         # Should usually be :all
+  config.cookie_tld_length = nil      # Leave nil to autodetect, or set to probably 2
 
 end
