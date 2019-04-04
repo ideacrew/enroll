756f191acc1f57690ffb03e0d468cc334c4d124b
Jayanth <jayanth.rordev@gmail.com>
2019-04-04 10:17:12 -0400

    refs#ivl_new_model_rails_5 fixed migration spec

--- spec/data_migrations/change_username_spec.rb
+++ spec/data_migrations/change_username_spec.rb
@@ -1,57 +1,54 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "change_username")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'change_username')
 
 describe ChangeUsername, dbclean: :after_each do
   let(:given_task_name) { "change_username" }
   subject { ChangeUsername.new(given_task_name, double(:current_scope => nil)) }
-  describe "given a task name" do
-    it "has the given task name" do
+  let(:user) { FactoryBot.create(:user) }
+  let(:new_user) { FactoryBot.create(:user) }
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
-  describe "change the username of a user" do
-    let(:user) { FactoryBot.create(:user) }
-    before(:each) do
-      allow(ENV).to receive(:[]).with("old_user_oimid").and_return(user.oim_id)
-      allow(ENV).to receive(:[]).with("new_user_oimid").and_return("NewUsername")
-    end
-    it "should change the username of the user" do
-      username=user.oim_id
-      expect(user.oim_id).to eq username
-      subject.migrate
-      user.reload
-      expect(user.oim_id).to eq "NewUsername"
+  
+  describe 'change the username of a user' do
+    it 'should change the username of the user' do
+    with_modified_env old_user_oimid: user.oim_id, new_user_oimid: 'NewUsername' do 
+        username=user.oim_id
+        expect(user.oim_id).to eq username
+        subject.migrate
+        user.reload
+        expect(user.oim_id).to eq 'NewUsername'
+      end
     end
   end
   
-  describe "not change the username if the user not found" do
-    let(:user) { FactoryBot.create(:user) }
-    before(:each) do
-      allow(ENV).to receive(:[]).with("old_user_oimid").and_return("")
-      allow(ENV).to receive(:[]).with("new_user_oimid").and_return("NewUsername")
-    end
-    it "should not change the username of the user" do
-      username=user.oim_id
-      expect(user.oim_id).to eq username
-      subject.migrate
-      user.reload
-      expect(user.oim_id).to eq username
+  describe 'not change the username if the user not found' do
+    it 'should not change the username of the user' do
+      with_modified_env old_user_oimid: '', new_user_oimid: 'NewUsername' do 
+        username=user.oim_id
+        expect(user.oim_id).to eq username
+        subject.migrate
+        user.reload
+        expect(user.oim_id).to eq username
+      end
     end
   end
 
-  describe "if new user already present in Enroll System" do
-    let(:user) { FactoryBot.create(:user) }
-    let(:new_user) { FactoryBot.create(:user) }
-    before(:each) do
-      allow(ENV).to receive(:[]).with("old_user_oimid").and_return(user.oim_id)
-      allow(ENV).to receive(:[]).with("new_user_oimid").and_return(new_user.oim_id)
+  describe 'if new user already present in Enroll System' do
+    it 'should not change the new username of the user' do
+      with_modified_env old_user_oimid: user.oim_id, new_user_oimid: new_user.oim_id do 
+        username=user.oim_id
+        expect(user.oim_id).to eq username
+        subject.migrate
+        user.reload
+        expect(user.oim_id).not_to eq new_user.oim_id
+      end
     end
-    it "should not change the new username of the user" do
-      username=user.oim_id
-      expect(user.oim_id).to eq username
-      subject.migrate
-      user.reload
-      expect(user.oim_id).not_to eq new_user.oim_id
-    end
+  end
+
+  def with_modified_env(options, &block)
+    ClimateControl.modify(options, &block)
   end
 end
