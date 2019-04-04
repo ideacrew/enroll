c302560f904e0236a495b61eb3b3d612b33362c4
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 17:14:06 -0400

    passing  spec/migrations/cca_broker_agency_profiles_migration_spec.rb

--- spec/migrations/cca_broker_agency_profiles_migration_spec.rb
+++ spec/migrations/cca_broker_agency_profiles_migration_spec.rb
@@ -45,9 +45,9 @@ describe "CcaBrokerAgencyProfilesMigration" do
     end
 
     it "should match total migrated organizations with employer profiles" do
-      silence_stream(STDOUT) do
+      # silence_stream(STDOUT) do
         Mongoid::Migrator.run(:up, @migrations_paths, @test_version.to_i)
-      end
+      # end
 
       expect(@migrated_organizations.count).to eq @old_organizations.count
     end
