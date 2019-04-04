921841543bbd794df8a9a2a35474307f3ee892cc
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 15:06:39 -0400

    passing spec/data_migrations/update_employee_plan_selection_spec.rb

--- spec/data_migrations/update_employee_plan_selection_spec.rb
+++ spec/data_migrations/update_employee_plan_selection_spec.rb
@@ -14,22 +14,22 @@ describe UpdateEmployeePlanSelection, dbclean: :after_each do
     let(:family){FactoryBot.create(:family,:with_primary_family_member)}
     let!(:hbx_enrollment){FactoryBot.create(:hbx_enrollment, benefit_group:benefit_group,household:family.active_household)}
     let(:plan){FactoryBot.create(:plan,hios_id:"12345")}
-    before(:each) do
-      allow(ENV).to receive(:[]).with("fein").and_return(benefit_group.employer_profile.parent.fein)
-      allow(ENV).to receive(:[]).with("hios_id").and_return(plan.hios_id)
-      allow(ENV).to receive(:[]).with("active_year").and_return(plan.active_year)
 
-    end
     it "should not change the employee plan if the plan year is not published" do
+      ClimateControl.modify fein:benefit_group.employer_profile.parent.fein,hios_id:plan.hios_id,active_year:"#{plan.active_year}" do 
       subject.migrate
       family.reload
       expect(family.active_household.hbx_enrollments.first.plan.hios_id).not_to eq plan.hios_id
+      end
     end
     it "should change the employee plan selection" do
-      benefit_group.plan_year.update_attributes(aasm_state: "published")
-      subject.migrate
-      family.reload
-      expect(family.active_household.hbx_enrollments.first.plan.hios_id).to eq plan.hios_id
+      ClimateControl.modify fein:benefit_group.employer_profile.parent.fein,hios_id:plan.hios_id,active_year:"#{plan.active_year}" do 
+
+        benefit_group.plan_year.update_attributes(aasm_state: "published")
+        subject.migrate
+        family.reload
+        expect(family.active_household.hbx_enrollments.first.plan.hios_id).to eq plan.hios_id
+      end
     end
   end
 end
