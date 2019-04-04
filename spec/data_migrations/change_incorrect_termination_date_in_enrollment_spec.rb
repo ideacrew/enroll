f8fc253689ddd3683576231d64b3dab24cc9e36f
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 17:51:04 -0400

    passing spec/data_migrations/change_incorrect_termination_date_in_enrollment_spec.rb

--- spec/data_migrations/change_incorrect_termination_date_in_enrollment_spec.rb
+++ spec/data_migrations/change_incorrect_termination_date_in_enrollment_spec.rb
@@ -40,16 +40,18 @@ describe ChangeIncorrectTerminationDateInEnrollment do
     let(:date) { (TimeKeeper.date_of_record.next_month.beginning_of_month + 2.days).to_s }
 
     before do
-      allow(ENV).to receive(:[]).with('hbx_id').and_return enrollment_1.hbx_id
-      allow(ENV).to receive(:[]).with('termination_date').and_return date
-      family.active_household.hbx_enrollments << enrollment_1
-      subject.migrate
+      # allow(ENV).to receive(:[]).with('hbx_id').and_return enrollment_1.hbx_id
+      # allow(ENV).to receive(:[]).with('termination_date').and_return date
     end
-
+    
     it "should modify hbx_enrollment not in terminated state" do
-      enrollment = HbxEnrollment.by_hbx_id(enrollment_1.hbx_id).first
-      expect(enrollment.aasm_state).to eq "coverage_terminated"
-      expect(enrollment.terminated_on).to eq TimeKeeper.date_of_record.next_month.beginning_of_month + 2.days
+      ClimateControl.modify hbx_id:enrollment_1.hbx_id,termination_date: "#{date}" do 
+        family.active_household.hbx_enrollments << enrollment_1
+        subject.migrate
+        enrollment = HbxEnrollment.by_hbx_id(enrollment_1.hbx_id).first
+        expect(enrollment.aasm_state).to eq "coverage_terminated"
+        expect(enrollment.terminated_on).to eq TimeKeeper.date_of_record.next_month.beginning_of_month + 2.days
+      end
     end
 
   end
@@ -58,17 +60,20 @@ describe ChangeIncorrectTerminationDateInEnrollment do
     let(:date) { (TimeKeeper.date_of_record.next_month.beginning_of_month + 2.days).to_s }
 
     before do
-      allow(ENV).to receive(:[]).with('hbx_id').and_return enrollment_2.hbx_id
-      allow(ENV).to receive(:[]).with('termination_date').and_return date
-      family.active_household.hbx_enrollments << enrollment_2
-      subject.migrate
-      family.reload
+      # allow(ENV).to receive(:[]).with('hbx_id').and_return enrollment_2.hbx_id
+      # allow(ENV).to receive(:[]).with('termination_date').and_return date
     end
-
+    
     it "should modify hbx_enrollment in terminated state" do
+      ClimateControl.modify hbx_id:enrollment_2.hbx_id,termination_date: "#{date}" do 
+        family.active_household.hbx_enrollments << enrollment_2
+        subject.migrate
+        family.reload
+
       enrollment = HbxEnrollment.by_hbx_id(enrollment_2.hbx_id).first
       expect(enrollment.aasm_state).to eq "coverage_terminated"
       expect(enrollment.terminated_on).to eq TimeKeeper.date_of_record.next_month.beginning_of_month + 2.days
+      end
     end
 
   end
