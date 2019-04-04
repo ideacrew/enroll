01e5daf748c376d1331ad08acf0e61f8a7a5074b
Jayanth <jayanth.rordev@gmail.com>
2019-04-04 14:36:40 -0400

    refs#ivl_new_model_rails_5 fixed migration spec

--- spec/data_migrations/update_family_members_index_spec.rb
+++ spec/data_migrations/update_family_members_index_spec.rb
@@ -1,50 +1,46 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "update_family_members_index")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'update_family_members_index')
 
 describe UpdateFamilyMembersIndex do
 
-  let(:given_task_name) { "update_family_members_index" }
+  let(:given_task_name) { 'update_family_members_index' }
   subject { UpdateFamilyMembersIndex.new(given_task_name, double(:current_scope => nil)) }
-  describe "given a task name" do
-    it "has the given task name" do
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
-  describe "case with if both primary_person and dependent is not present" do
-    before :each do
-      allow(ENV).to receive(:[]).with("primary_hbx").and_return("")
-      allow(ENV).to receive(:[]).with("dependent_hbx").and_return("")
+
+  def with_modified_env(options, &block)
+    ClimateControl.modify(options, &block)
+  end
+
+  describe 'case with if both primary_person and dependent is not present' do
+    context 'case with if dependent not present' do
+      it 'found no family with given dependent_hbx' do
+        with_modified_env primary_hbx: '1111', dependent_hbx: '' do
+          expect{subject.migrate}.to raise_error("some error person with hbx_id:1111 and hbx_id: not found")
+        end
+      end
     end
 
-    context "case with if dependent not present" do
-      before :each do
-        allow(ENV).to receive(:[]).with("primary_hbx").and_return("1111")
-        allow(ENV).to receive(:[]).with("dependent_hbx").and_return("")
-      end
-      it "found no family with given dependent_hbx" do
-        expect{subject.migrate}.to raise_error("some error person with hbx_id:1111 and hbx_id: not found")
-      end
-    end
-
-    context "case with if primary_person not present" do
-      before :each do
-        allow(ENV).to receive(:[]).with("primary_hbx").and_return("")
-        allow(ENV).to receive(:[]).with("dependent_hbx").and_return("1112")
-      end
-      it "found no family with given primary_hbx" do
-        expect{subject.migrate}.to raise_error("some error person with hbx_id: and hbx_id:1112 not found")
+    context 'case with if primary_person not present' do
+      it 'found no family with given primary_hbx' do
+        with_modified_env primary_hbx: '', dependent_hbx: '1112' do
+          expect{subject.migrate}.to raise_error('some error person with hbx_id: and hbx_id:1112 not found')
+        end
       end
     end
   end
 
   describe "update family_members index", dbclean: :after_each do
 
-    let(:wife) { FactoryBot.create(:person, first_name: "wifey")}
-    let(:husband) { FactoryBot.create(:person, first_name: "hubby")}
+    let(:wife) { FactoryBot.create(:person, first_name: 'wifey')}
+    let(:husband) { FactoryBot.create(:person, first_name: 'hubby')}
     let(:family) { FactoryBot.build(:family) }
     let!(:husbands_family) do
-      husband.person_relationships << PersonRelationship.new(relative_id: husband.id, kind: "self")
-      husband.person_relationships << PersonRelationship.new(relative_id: wife.id, kind: "spouse")
+      husband.person_relationships << PersonRelationship.new(relative_id: husband.id, kind: 'self')
+      husband.person_relationships << PersonRelationship.new(relative_id: wife.id, kind: 'spouse')
       husband.save
 
       family.add_family_member(wife)
@@ -53,23 +49,17 @@ describe UpdateFamilyMembersIndex do
       family
     end
 
-    before(:each) do
-      allow(ENV).to receive(:[]).with("primary_hbx").and_return(husband.hbx_id)
-      allow(ENV).to receive(:[]).with("dependent_hbx").and_return(wife.hbx_id)
-      allow(ENV).to receive(:[]).with("primary_family_id").and_return(husbands_family.family_members.first.id)
-      allow(ENV).to receive(:[]).with("dependent_family_id").and_return(husbands_family.family_members.second.id)
-    end
-
-    it "should swap the index of family members" do
-      expect(husbands_family.family_members.first.is_primary_applicant?).to eq false
-      expect(husbands_family.family_members.second.is_primary_applicant?).to eq true
-
-      subject.migrate
-      husbands_family.reload
-      hus_fam_id = husbands_family.family_members.first.id
-      wife_fam_id = husbands_family.family_members.second.id
-      expect(husbands_family.family_members.where(id: hus_fam_id).first.is_primary_applicant?).to eq true
-      expect(husbands_family.family_members.where(id: wife_fam_id).first.is_primary_applicant?).to eq false
+    it 'should swap the index of family members' do
+      with_modified_env primary_hbx: husband.hbx_id, dependent_hbx: wife.hbx_id, primary_family_id: husbands_family.family_members.first.id, dependent_family_id: husbands_family.family_members.second.id do
+        expect(husbands_family.family_members.first.is_primary_applicant?).to eq false
+        expect(husbands_family.family_members.second.is_primary_applicant?).to eq true
+        subject.migrate
+        husbands_family.reload
+        hus_fam_id = husbands_family.family_members.first.id
+        wife_fam_id = husbands_family.family_members.second.id
+        expect(husbands_family.family_members.where(id: hus_fam_id).first.is_primary_applicant?).to eq true
+        expect(husbands_family.family_members.where(id: wife_fam_id).first.is_primary_applicant?).to eq false
+      end
     end
   end
 end
