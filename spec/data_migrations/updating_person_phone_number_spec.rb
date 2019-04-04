cfffc781178aab9ee00f892ac3faca63f8210345
Jayanth <jayanth.rordev@gmail.com>
2019-04-04 14:23:10 -0400

    refs#ivl_new_model_rails_5 fixed migration spec

--- spec/data_migrations/updating_person_phone_number_spec.rb
+++ spec/data_migrations/updating_person_phone_number_spec.rb
@@ -1,34 +1,27 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "updating_person_phone_number")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'updating_person_phone_number')
 
 describe UpdatingPersonPhoneNumber, dbclean: :after_each do
 
-  let(:given_task_name) { "update_broker_phone_kind" }
+  let(:given_task_name) { 'update_broker_phone_kind' }
   subject { UpdatingPersonPhoneNumber.new(given_task_name, double(:current_scope => nil)) }
 
-  describe "given a task name" do
-    it "has the given task name" do
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
 
-  describe "changing broker phone kind" do  
+  describe 'changing broker phone kind' do  
      let(:phone) {FactoryBot.build(:phone, kind:'work')}
      let(:person) { FactoryBot.create(:person,phones:[phone]) }
 
-
-    before(:each) do
-      allow(ENV).to receive(:[]).with("hbx_id").and_return(person.hbx_id)
-      allow(ENV).to receive(:[]).with("area_code").and_return('302')
-      allow(ENV).to receive(:[]).with("number").and_return('4667333')
-      allow(ENV).to receive(:[]).with("ext").and_return('')
-      allow(ENV).to receive(:[]).with("full_number").and_return('3014667333')
-    end
-
-    it "should change the employee contribution" do
-      subject.migrate
-      person.reload
-      expect(person.phones.where(kind:'work').first.full_phone_number).to eq '3014667333'
+    it 'should change the employee contribution' do
+      ClimateControl.modify hbx_id: person.hbx_id, area_code: '302', number: '4667333', ext: '', full_number: '3014667333' do
+        subject.migrate
+        person.reload
+        expect(person.phones.where(kind:'work').first.full_phone_number).to eq '3014667333'
+      end
     end
   end
 end
