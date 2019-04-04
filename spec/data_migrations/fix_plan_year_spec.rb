149c976495fa93b5f58fd281268e2e837aa90d49
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 12:49:27 -0400

    spec/data_migrations/fix_plan_year_spec.rb

--- spec/data_migrations/fix_plan_year_spec.rb
+++ spec/data_migrations/fix_plan_year_spec.rb
@@ -27,73 +27,87 @@ describe FixPlanYear, dbclean: :after_each do
     context "termianted plan year to active plan year" do
 
       before :each do
-        allow(ENV).to receive(:[]).with("fein").and_return organization.fein
-        allow(ENV).to receive(:[]).with("start_on").and_return plan_year.start_on
-        allow(ENV).to receive(:[]).with("end_on").and_return (plan_year.start_on+ 1.year) -1.day
-        allow(ENV).to receive(:[]).with("aasm_state").and_return 'active'
-        allow(ENV).to receive(:[]).with("terminated_on").and_return ''
-        allow(ENV).to receive(:[]).with("update_enrollments").and_return "true"
         plan_year.update_attributes(aasm_state:'terminated', terminated_on: term_date, end_on: term_date)
         hbx_enrollment.update_attributes(aasm_state:'coverage_terminated',terminated_on:term_date)
       end
 
       it "should make plan year & enrollments active " do
-        expect(plan_year.aasm_state).to eq 'terminated'   # before migration
-        expect(plan_year.terminated_on).to eq term_date
-        expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
-        subject.migrate
-        plan_year.reload
-        hbx_enrollment.reload
-        expect(plan_year.aasm_state).to eq 'active'  # after migration
-        expect(plan_year.terminated_on).to eq nil
-        expect(hbx_enrollment.aasm_state).to eq 'coverage_selected'
+        ClimateControl.modify fein:organization.fein, 
+        start_on:"#{plan_year.start_on}",
+        end_on:"#{(plan_year.start_on+ 1.year) -1.day}",
+        aasm_state: 'active',
+        terminated_on:'',
+        update_enrollments:'true' do
+
+          expect(plan_year.aasm_state).to eq 'terminated'   # before migration
+          expect(plan_year.terminated_on).to eq term_date
+          expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
+          subject.migrate
+          plan_year.reload
+          hbx_enrollment.reload
+          expect(plan_year.aasm_state).to eq 'active'  # after migration
+          expect(plan_year.terminated_on).to eq nil
+          expect(hbx_enrollment.aasm_state).to eq 'coverage_selected'
+        end
       end
     end
-
     context "termianted plan year to cancelled plan year" do
 
       before :each do
-        allow(ENV).to receive(:[]).with("fein").and_return organization.fein
-        allow(ENV).to receive(:[]).with("start_on").and_return plan_year.start_on
-        allow(ENV).to receive(:[]).with("end_on").and_return (plan_year.start_on-1.day) + 1.year
-        allow(ENV).to receive(:[]).with("aasm_state").and_return 'canceled'
-        allow(ENV).to receive(:[]).with("terminated_on").and_return ''
-        allow(ENV).to receive(:[]).with("update_enrollments").and_return "true"
+        # allow(ENV).to receive(:[]).with("fein").and_return organization.fein
+        # allow(ENV).to receive(:[]).with("start_on").and_return plan_year.start_on
+        # allow(ENV).to receive(:[]).with("end_on").and_return (plan_year.start_on-1.day) + 1.year
+        # allow(ENV).to receive(:[]).with("aasm_state").and_return 'canceled'
+        # allow(ENV).to receive(:[]).with("terminated_on").and_return ''
+        # allow(ENV).to receive(:[]).with("update_enrollments").and_return "true"
         plan_year.update_attributes(aasm_state:'terminated', terminated_on: term_date, end_on: term_date)
         hbx_enrollment.update_attributes(aasm_state:'coverage_terminated',terminated_on:term_date)
         hbx_enrollment2.update_attributes(aasm_state:'shopping',terminated_on:term_date)
       end
 
       it "should make plan year & enrollments active " do
-        expect(plan_year.aasm_state).to eq 'terminated'   # before migration
-        expect(plan_year.terminated_on).to eq term_date
-        expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
-        expect(hbx_enrollment2.aasm_state).to eq 'shopping'
-        subject.migrate
-        plan_year.reload
-        hbx_enrollment.reload
-        expect(plan_year.aasm_state).to eq 'canceled'  # after migration
-        expect(plan_year.terminated_on).to eq nil
-        expect(hbx_enrollment.aasm_state).to eq 'coverage_canceled'
-        expect(hbx_enrollment2.aasm_state).to eq 'shopping'
+        ClimateControl.modify fein:organization.fein, 
+          start_on:"#{plan_year.start_on}",
+          end_on:"#{((plan_year.start_on-1.day) + 1.year)}",
+          aasm_state: 'canceled',
+          terminated_on:'',
+          update_enrollments:'true' do
+            expect(plan_year.aasm_state).to eq 'terminated'   # before migration
+            expect(plan_year.terminated_on).to eq term_date
+            expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
+            expect(hbx_enrollment2.aasm_state).to eq 'shopping'
+            subject.migrate
+            plan_year.reload
+            hbx_enrollment.reload
+            expect(plan_year.aasm_state).to eq 'canceled'  # after migration
+            expect(plan_year.terminated_on).to eq nil
+            expect(hbx_enrollment.aasm_state).to eq 'coverage_canceled'
+            expect(hbx_enrollment2.aasm_state).to eq 'shopping'
+        end
       end
 
-
-      it "should not update enrollments " do
-        allow(ENV).to receive(:[]).with("update_enrollments").and_return "false"
-        expect(plan_year.aasm_state).to eq 'terminated'   # before migration
-        expect(plan_year.terminated_on).to eq term_date
-        expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
-        expect(hbx_enrollment2.aasm_state).to eq 'shopping'
-        subject.migrate
-        plan_year.reload
-        hbx_enrollment.reload
-        expect(plan_year.aasm_state).to eq 'canceled'  # after migration
-        expect(plan_year.terminated_on).to eq nil
-        expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
-        expect(hbx_enrollment2.aasm_state).to eq 'shopping'
+        it "should not update enrollments " do
+          ClimateControl.modify fein:organization.fein, 
+          start_on:"#{plan_year.start_on}",
+          end_on:"#{((plan_year.start_on-1.day) + 1.year)}",
+          aasm_state: 'canceled',
+          terminated_on:'',
+          update_enrollments:'false' do
+            expect(plan_year.aasm_state).to eq 'terminated'   # before migration
+            expect(plan_year.terminated_on).to eq term_date
+            expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
+            expect(hbx_enrollment2.aasm_state).to eq 'shopping'
+            subject.migrate
+            plan_year.reload
+            hbx_enrollment.reload
+            expect(plan_year.aasm_state).to eq 'canceled'  # after migration
+            expect(plan_year.terminated_on).to eq nil
+            expect(hbx_enrollment.aasm_state).to eq 'coverage_terminated'
+            expect(hbx_enrollment2.aasm_state).to eq 'shopping'
+        end
       end
     end
   end
 end
+
 
