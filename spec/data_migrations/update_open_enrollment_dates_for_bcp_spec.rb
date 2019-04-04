ac353074b35e47d0ca35e9142ddaf0a29274dab7
Jayanth <jayanth.rordev@gmail.com>
2019-04-04 11:44:42 -0400

    refs#ivl_new_model_rails_5 fixed migration spec

--- spec/data_migrations/update_open_enrollment_dates_for_bcp_spec.rb
+++ spec/data_migrations/update_open_enrollment_dates_for_bcp_spec.rb
@@ -1,18 +1,18 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "update_open_enrollment_dates_for_bcp")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'update_open_enrollment_dates_for_bcp')
 
 describe UpdateOpenEnrollmentDatesForBcp, dbclean: :after_each do
 
-  let(:given_task_name) { "update_open_enrollment_dates_for_bcp" }
+  let(:given_task_name) { 'update_open_enrollment_dates_for_bcp' }
   subject { UpdateOpenEnrollmentDatesForBcp.new(given_task_name, double(:current_scope => nil)) }
 
-  describe "given a task name" do
-    it "has the given task name" do
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
 
-  describe "update open enrollment dates for benefit coverage period" do
+  describe 'update open enrollment dates for benefit coverage period' do
     let!(:organization) { FactoryBot.create(:organization) }
     let!(:hbx_profile) { FactoryBot.create(:hbx_profile, :open_enrollment_coverage_period, :organization => organization) }
     let!(:benefit_sponsorship) { hbx_profile.benefit_sponsorship }
@@ -20,19 +20,14 @@ describe UpdateOpenEnrollmentDatesForBcp, dbclean: :after_each do
     let(:open_enrollment_start_on) { Date.new(TimeKeeper.date_of_record.year,11,1) }
     let(:open_enrollment_end_on) { Date.new(TimeKeeper.date_of_record.year,12,31) }
 
-    before(:each) do
-      allow(ENV).to receive(:[]).with("title").and_return(benefit_coverage_period.title)
-      allow(ENV).to receive(:[]).with("new_oe_start_date").and_return(open_enrollment_start_on)
-      allow(ENV).to receive(:[]).with("new_oe_end_date").and_return(open_enrollment_end_on)
-    end
-
-    context "to update open enrollment dates" do
-
-      it "should update open enrollment start_on and end_on dates" do
-        allow(HbxProfile).to receive(:current_hbx).and_return hbx_profile
-        subject.migrate
-        expect(benefit_coverage_period.reload.open_enrollment_start_on).to eq open_enrollment_start_on
-        expect(benefit_coverage_period.reload.open_enrollment_end_on).to eq open_enrollment_end_on
+    context 'to update open enrollment dates' do
+      it 'should update open enrollment start_on and end_on dates' do
+        ClimateControl.modify title: benefit_coverage_period.title, new_oe_start_date: open_enrollment_start_on.to_s, new_oe_end_date: open_enrollment_end_on.to_s do 
+          allow(HbxProfile).to receive(:current_hbx).and_return hbx_profile
+          subject.migrate
+          expect(benefit_coverage_period.reload.open_enrollment_start_on).to eq open_enrollment_start_on
+          expect(benefit_coverage_period.reload.open_enrollment_end_on).to eq open_enrollment_end_on
+        end
       end
     end
   end
