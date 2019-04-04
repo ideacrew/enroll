8a3d8cd900e54fc19519061a49f8c39de1e1208c
Jayanth <jayanth.rordev@gmail.com>
2019-04-04 11:50:41 -0400

    refs#ivl_new_model_rails_5 fixed migration spec

--- spec/data_migrations/update_invalid_benefit_group_assignments_for_employer_spec.rb
+++ spec/data_migrations/update_invalid_benefit_group_assignments_for_employer_spec.rb
@@ -1,18 +1,18 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "update_invalid_benefit_group_assignments_for_employer")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'update_invalid_benefit_group_assignments_for_employer')
 
 describe UpdateInvalidBenefitGroupAssignmentsForEmployer, dbclean: :after_each do
 
-  let(:given_task_name) { "update_invalid_benefit_group_assignments_for_employer" }
+  let(:given_task_name) { 'update_invalid_benefit_group_assignments_for_employer' }
   subject { UpdateInvalidBenefitGroupAssignmentsForEmployer.new(given_task_name, double(:current_scope => nil)) }
 
-  describe "given a task name" do
-    it "has the given task name" do
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
 
-  describe "changing plan year's state" do
+  describe 'changing plan year state"' do
 
     let!(:benefit_group) { FactoryBot.create(:benefit_group, plan_year: plan_year)}
     let(:plan_year) { FactoryBot.create(:plan_year) }
@@ -22,7 +22,6 @@ describe UpdateInvalidBenefitGroupAssignmentsForEmployer, dbclean: :after_each d
     let(:census_employee) { FactoryBot.create(:census_employee, employer_profile_id: plan_year.employer_profile.id)}
 
     before(:each) do
-      allow(ENV).to receive(:[]).with("fein").and_return(plan_year.employer_profile.parent.fein)
       benefit_group_assignments = [benefit_group_assignment]
       allow(CensusEmployee).to receive(:find).and_return(census_employee)
       allow(census_employee).to receive(:benefit_group_assignments).and_return benefit_group_assignments
@@ -31,20 +30,21 @@ describe UpdateInvalidBenefitGroupAssignmentsForEmployer, dbclean: :after_each d
 
     end
 
-    context "updating benefit group assignments", dbclean: :after_each do
+    context 'updating benefit group assignments', dbclean: :after_each do
 
-      it "should update the invalid benefit group assignments" do
-        allow(benefit_group_assignment).to receive(:valid?).and_return(false)
-        allow(benefit_group_assignment).to receive_message_chain(:hbx_enrollment, :update_attributes!).with(benefit_group_id: benefit_group.id)
-        allow(benefit_group_assignment).to receive_message_chain(:hbx_enrollment, :benefit_group_id ).and_return(true)
+      it 'should update the invalid benefit group assignments' do
+        ClimateControl.modify fein: plan_year.employer_profile.parent.fein do
+          allow(benefit_group_assignment).to receive(:valid?).and_return(false)
+          allow(benefit_group_assignment).to receive_message_chain(:hbx_enrollment, :update_attributes!).with(benefit_group_id: benefit_group.id)
+          allow(benefit_group_assignment).to receive_message_chain(:hbx_enrollment, :benefit_group_id ).and_return(true)
 
-        subject.migrate
-        census_employee.reload
-        census_employee.benefit_group_assignments.each do |bga|
-          expect(bga.benefit_group.id).to eq(bga.hbx_enrollment.benefit_group.id)
+          subject.migrate
+          census_employee.reload
+          census_employee.benefit_group_assignments.each do |bga|
+            expect(bga.benefit_group.id).to eq(bga.hbx_enrollment.benefit_group.id)
+          end
         end
       end
-
     end
   end
 end
