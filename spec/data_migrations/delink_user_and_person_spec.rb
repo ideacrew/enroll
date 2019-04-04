144c26c7d71b69066b0aa353c002ff0d213c7c40
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 17:07:12 -0400

    passing spec/data_migrations/delink_user_and_person_spec.rb

--- spec/data_migrations/delink_user_and_person_spec.rb
+++ spec/data_migrations/delink_user_and_person_spec.rb
@@ -10,20 +10,24 @@ describe DelinkUserAndPerson do
   subject { DelinkUserAndPerson.new(given_task_name, double(:current_scope => nil)) }
 
   before(:each) do
-    allow(ENV).to receive(:[]).with("person_hbx_id").and_return(person.hbx_id)
+    # allow(ENV).to receive(:[]).with("person_hbx_id").and_return(person.hbx_id)
   end
 
   it "should delink the person from user" do
-    expect(person.user).not_to eq nil
-    subject.migrate
-    person.reload
-    expect(person.user).to eq nil
+    ClimateControl.modify person_hbx_id: person.hbx_id do 
+      expect(person.user).not_to eq nil
+      subject.migrate
+      person.reload
+      expect(person.user).to eq nil
+    end
   end
   it "after delink the user should exists" do
-    id=user.id
-    expect(User.where(id:id)).not_to eq nil
-    subject.migrate
-    user.reload
-    expect(User.where(id:id)).not_to eq nil
+    ClimateControl.modify person_hbx_id: person.hbx_id do 
+      id=user.id
+      expect(User.where(id:id)).not_to eq nil
+      subject.migrate
+      user.reload
+      expect(User.where(id:id)).not_to eq nil
+    end
   end
 end
