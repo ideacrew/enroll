03de2883d00f7728b68ace1facf34a7d94e1e526
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 12:14:28 -0400

    passing spec/data_migrations/change_enrollment_member_applicant_id_spec.rb

--- spec/data_migrations/change_enrollment_member_applicant_id_spec.rb
+++ spec/data_migrations/change_enrollment_member_applicant_id_spec.rb
@@ -15,17 +15,16 @@ describe ChangeEnrollmentMemberApplicantId, dbclean: :after_each do
     let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment,terminated_on:Date.today,household: family.active_household)}
     let!(:hbx_enrollment_member1) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.first.id,is_subscriber: true, eligibility_date:Date.today )}
     let!(:hbx_enrollment_member2) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.last.id,is_subscriber: false, eligibility_date:Date.today )}
-    before do
-      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return hbx_enrollment.hbx_id
-      allow(ENV).to receive(:[]).with('enrollment_member_id').and_return ''
-      allow(ENV).to receive(:[]).with('family_member_id').and_return family_member.id
-    end
+
     it "should not change enrollment member applicant id if no enrollment found" do
-      applicant_id = hbx_enrollment_member1.applicant_id
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
-      subject.migrate
-      hbx_enrollment.reload
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+      ClimateControl.modify enrollment_hbx_id:hbx_enrollment.hbx_id,enrollment_member_id: '',family_member_id:family_member.id do 
+
+        applicant_id = hbx_enrollment_member1.applicant_id
+        expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+        subject.migrate
+        hbx_enrollment.reload
+        expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+      end
     end
   end
   describe "do not change enrollment member if no enrollment was found", dbclean: :after_each do
@@ -34,36 +33,33 @@ describe ChangeEnrollmentMemberApplicantId, dbclean: :after_each do
     let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment,terminated_on:Date.today,household: family.active_household)}
     let!(:hbx_enrollment_member1) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.first.id,is_subscriber: true, eligibility_date:Date.today )}
     let!(:hbx_enrollment_member2) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.last.id,is_subscriber: false, eligibility_date:Date.today )}
-    before do
-      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return ''
-      allow(ENV).to receive(:[]).with('enrollment_member_id').and_return hbx_enrollment_member1.id
-      allow(ENV).to receive(:[]).with('family_member_id').and_return family_member.id
-    end
+
     it "should not change enrollment member applicant id if no enrollment found" do
-      applicant_id = hbx_enrollment_member1.applicant_id
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
-      subject.migrate
-      hbx_enrollment.reload
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+        ClimateControl.modify enrollment_hbx_id:'' , enrollment_member_id:  hbx_enrollment_member1.id,family_member_id:family_member.id do 
+          applicant_id = hbx_enrollment_member1.applicant_id
+          expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+          subject.migrate
+          hbx_enrollment.reload
+          expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+      end
     end
   end
+
   describe "do not change enrollment member if no family member", dbclean: :after_each do
     let(:family) { FactoryBot.create(:family, :with_primary_family_member)}
     let(:family_member){FactoryBot.create(:family_member,family:family)}
     let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment,terminated_on:Date.today,household: family.active_household)}
     let!(:hbx_enrollment_member1) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.first.id,is_subscriber: true, eligibility_date:Date.today )}
     let!(:hbx_enrollment_member2) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.last.id,is_subscriber: false, eligibility_date:Date.today )}
-    before do
-      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return hbx_enrollment.hbx_id
-      allow(ENV).to receive(:[]).with('enrollment_member_id').and_return hbx_enrollment_member1.id
-      allow(ENV).to receive(:[]).with('family_member_id').and_return ''
-    end
+
     it "should not change enrollment member applicant id if no enrollment found" do
-      applicant_id = hbx_enrollment_member1.applicant_id
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
-      subject.migrate
-      hbx_enrollment.reload
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+      ClimateControl.modify enrollment_hbx_id:  hbx_enrollment.hbx_id , enrollment_member_id:  hbx_enrollment_member1.id,family_member_id:'' do 
+        applicant_id = hbx_enrollment_member1.applicant_id
+        expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+        subject.migrate
+        hbx_enrollment.reload
+        expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+      end
     end
   end
   describe "change enrollment member if all correct information provided", dbclean: :after_each do
@@ -72,19 +68,18 @@ describe ChangeEnrollmentMemberApplicantId, dbclean: :after_each do
     let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment,terminated_on:Date.today,household: family.active_household)}
     let!(:hbx_enrollment_member1) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.first.id,is_subscriber: true, eligibility_date:Date.today )}
     let!(:hbx_enrollment_member2) {FactoryBot.create(:hbx_enrollment_member,hbx_enrollment:hbx_enrollment,applicant_id: family.family_members.last.id,is_subscriber: false, eligibility_date:Date.today )}
-    before do
-      allow(ENV).to receive(:[]).with('enrollment_hbx_id').and_return hbx_enrollment.hbx_id
-      allow(ENV).to receive(:[]).with('enrollment_member_id').and_return hbx_enrollment_member1.id
-      allow(ENV).to receive(:[]).with('family_member_id').and_return family_member.id
-    end
+
     it "should change enrollment member applicant id if all information is provided" do
-      applicant_id = hbx_enrollment_member1.applicant_id
-      new_applicant_id = family_member.id
-      expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
-      subject.migrate
-      hbx_enrollment.reload
-      hbx_enrollment_member1.reload
-      expect(hbx_enrollment_member1.applicant_id).to eq new_applicant_id
+      ClimateControl.modify enrollment_hbx_id:  hbx_enrollment.hbx_id , enrollment_member_id:  hbx_enrollment_member1.id,family_member_id: family_member.id do 
+        applicant_id = hbx_enrollment_member1.applicant_id
+        new_applicant_id = family_member.id
+        expect(hbx_enrollment_member1.applicant_id).to eq applicant_id
+        subject.migrate
+        hbx_enrollment.reload
+        hbx_enrollment_member1.reload
+        expect(hbx_enrollment_member1.applicant_id).to eq new_applicant_id
+      end
     end
   end
 end
+
