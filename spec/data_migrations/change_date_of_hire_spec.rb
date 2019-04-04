9cbd87a946453c66b0f25c89107391f4e17cf2b2
Zachary Landes <zach.g.landes@gmail.com>
2019-04-04 16:38:53 -0400

    passing  spec/data_migrations/change_date_of_hire_spec.rb

--- spec/data_migrations/change_date_of_hire_spec.rb
+++ spec/data_migrations/change_date_of_hire_spec.rb
@@ -8,61 +8,58 @@ describe ChangeDateOfHire, dbclean: :after_each do
       expect(subject.name).to eql given_task_name
     end
   end
-  describe "Change doh for employee role" do
+  describe "Change doh for employee role", dbclean: :after_each do
     subject {ChangeDateOfHire.new("change_date_of_hire", double(:current_scope => nil)) }
-    let(:person_with_employee_role) { FactoryBot.create(:person, :with_employee_role) }
+    let(:person_with_employee_role) { FactoryBot.create(:person, :with_employee_role, :with_ssn) }
     let(:employer_profile_id){ person_with_employee_role.employee_roles.first.employer_profile_id }
     let(:date){ TimeKeeper.date_of_record - 5.days}
-    before :each do
-      allow(ENV).to receive(:[]).with('hbx_id').and_return "#{person_with_employee_role.hbx_id}"
-      allow(ENV).to receive(:[]).with('new_doh').and_return date
-      allow(ENV).to receive(:[]).with('employer_profile_id').and_return employer_profile_id
-      subject.migrate
-      person_with_employee_role.reload
-    end
+
     it "should change dot of ce not in employment termination state" do
-      expect(person_with_employee_role.employee_roles.first.hired_on).to eq date
+      ClimateControl.modify hbx_id: "#{person_with_employee_role.hbx_id}", new_doh:"#{date}", employer_profile_id: employer_profile_id do 
+
+        subject.migrate
+        person_with_employee_role.reload
+
+        expect(person_with_employee_role.employee_roles.first.hired_on).to eq date
+      end
     end
   end
 
   describe "Change doh for employee role for multiple employees" do
     subject {ChangeDateOfHire.new("change_date_of_hire", double(:current_scope => nil)) }
-    let(:person_with_employee_role1) { FactoryBot.create(:person, :with_employee_role) }
-    let(:person_with_employee_role2) { FactoryBot.create(:person, :with_employee_role) }
+    let(:person_with_employee_role1) { FactoryBot.create(:person, :with_employee_role, :with_ssn) }
+    let(:person_with_employee_role2) { FactoryBot.create(:person, :with_employee_role, :with_ssn) }
     let(:employer_profile_id){ person_with_employee_role1.employee_roles.first.employer_profile_id }
     let(:date){ TimeKeeper.date_of_record - 5.days}
-    before :each do
-      allow(ENV).to receive(:[]).with('hbx_id').and_return "#{person_with_employee_role1.hbx_id},#{person_with_employee_role2.hbx_id}"
-      allow(ENV).to receive(:[]).with('new_doh').and_return date
-      allow(ENV).to receive(:[]).with('employer_profile_id').and_return employer_profile_id
-      person_with_employee_role2.employee_roles.first.update_attribute("employer_profile_id",employer_profile_id)
-      subject.migrate
-      person_with_employee_role1.reload
-      person_with_employee_role2.reload
-    end
+
     it "should change dot of ce not in employment termination state" do
-      expect(person_with_employee_role1.employee_roles.first.hired_on).to eq date
-      expect(person_with_employee_role2.employee_roles.first.hired_on).to eq date
+      ClimateControl.modify hbx_id: "#{person_with_employee_role1.hbx_id},#{person_with_employee_role2.hbx_id}", new_doh:"#{date}", employer_profile_id: employer_profile_id do 
+        person_with_employee_role2.employee_roles.first.update_attribute("employer_profile_id",employer_profile_id)
+        subject.migrate
+        person_with_employee_role1.reload
+        person_with_employee_role2.reload
+
+        expect(person_with_employee_role1.employee_roles.first.hired_on).to eq date
+        expect(person_with_employee_role2.employee_roles.first.hired_on).to eq date
+      end
     end
   end
 
   describe "Doesnot Change doh for employee role with different employee profile id" do
     subject {ChangeDateOfHire.new("change_date_of_hire", double(:current_scope => nil)) }
-    let(:person_with_employee_role1) { FactoryBot.create(:person, :with_employee_role) }
-    let(:person_with_employee_role2) { FactoryBot.create(:person, :with_employee_role) }
+    let(:person_with_employee_role1) { FactoryBot.create(:person, :with_employee_role, :with_ssn) }
+    let(:person_with_employee_role2) { FactoryBot.create(:person, :with_employee_role, :with_ssn) }
     let(:employer_profile_id){ person_with_employee_role1.employee_roles.first.employer_profile_id }
     let(:date){ TimeKeeper.date_of_record - 5.days}
-    before :each do
-      allow(ENV).to receive(:[]).with('hbx_id').and_return "#{person_with_employee_role1.hbx_id},#{person_with_employee_role2.hbx_id}"
-      allow(ENV).to receive(:[]).with('new_doh').and_return date
-      allow(ENV).to receive(:[]).with('employer_profile_id').and_return employer_profile_id
-      subject.migrate
-      person_with_employee_role1.reload
-      person_with_employee_role2.reload
-    end
+
     it "should change dot of ce not in employment termination state" do
-      expect(person_with_employee_role1.employee_roles.first.hired_on).to eq date
-      expect(person_with_employee_role2.employee_roles.first.hired_on).to_not eq date
+      ClimateControl.modify hbx_id: "#{person_with_employee_role1.hbx_id},#{person_with_employee_role2.hbx_id}", new_doh:"#{date}", employer_profile_id: employer_profile_id do 
+        subject.migrate
+        person_with_employee_role1.reload
+        person_with_employee_role2.reload
+        expect(person_with_employee_role1.employee_roles.first.hired_on).to eq date
+        expect(person_with_employee_role2.employee_roles.first.hired_on).to_not eq date
+      end
     end
   end
 end
