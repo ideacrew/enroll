f111670c8b82373063674cd43d00a345c7906f97
Jayanth <jayanth.rordev@gmail.com>
2019-04-05 10:37:15 -0400

    refs#ivl_new_model_rails_5 fixed migration spec

--- spec/data_migrations/change_gender_spec.rb
+++ spec/data_migrations/change_gender_spec.rb
@@ -1,34 +1,30 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "change_gender")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'change_gender')
 
 describe ChangeGender, dbclean: :after_each do
 
-  let(:given_task_name) { "change_gender" }
+  let(:given_task_name) { 'change_gender' }
   subject { ChangeGender.new(given_task_name, double(:current_scope => nil)) }
 
-  describe "given a task name" do
-    it "has the given task name" do
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
 
-  describe "changing gender for an Employee", dbclean: :after_each do
-    let(:person) { FactoryBot.create(:person, gender: "male") }
+  describe 'changing gender for an Employee', dbclean: :after_each do
+    let(:person) { FactoryBot.create(:person, gender: 'male') }
     let(:employer_profile) { FactoryBot.create(:employer_profile)}
-    let(:census_employee) { FactoryBot.create(:census_employee, employer_profile: employer_profile, gender: "male") }
+    let(:census_employee) { FactoryBot.create(:census_employee, employer_profile: employer_profile, gender: 'male') }
 
-    before(:each) do
-      allow(ENV).to receive(:[]).with("hbx_id").and_return(person.hbx_id)
-      allow(ENV).to receive(:[]).with("id").and_return(census_employee.id)
-      allow(ENV).to receive(:[]).with("gender").and_return("female")
-    end
-
-    it "should change the gender" do
-      subject.migrate
-      census_employee.reload
-      person.reload
-      expect(census_employee.gender).to eq "female"
-      expect(person.gender).to eq "female"
+    it 'should change the gender' do
+      ClimateControl.modify hbx_id: person.hbx_id, id: census_employee.id, gender: 'female' do
+        subject.migrate
+        census_employee.reload
+        person.reload
+        expect(census_employee.gender).to eq 'female'
+        expect(person.gender).to eq 'female'
+      end
     end
   end
 end
