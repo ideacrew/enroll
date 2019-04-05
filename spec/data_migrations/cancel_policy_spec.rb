1636e8cffff3adec0da273cc0c7dc4356785daa9
Jayanth <jayanth.rordev@gmail.com>
2019-04-05 14:47:19 -0400

    refs#ivl_new_model_rails_5 fixed AUTO JUMP

--- spec/data_migrations/cancel_policy_spec.rb
+++ spec/data_migrations/cancel_policy_spec.rb
@@ -1,30 +1,28 @@
-require "rails_helper"
-require File.join(Rails.root, "app", "data_migrations", "cancel_policy")
+require 'rails_helper'
+require File.join(Rails.root, 'app', 'data_migrations', 'cancel_policy')
 
 describe CancelPolicy do
 
-  let(:given_task_name) { "cancel_policy" }
+  let(:given_task_name) { 'cancel_policy' }
   subject { CancelPolicy.new(given_task_name, double(:current_scope => nil)) }
 
-  describe "given a task name" do
-    it "has the given task name" do
+  describe 'given a task name' do
+    it 'has the given task name' do
       expect(subject.name).to eql given_task_name
     end
   end
 
-  describe "changing aasm state" do
+  describe 'changing aasm state' do
     
     let(:family) { FactoryBot.create(:family, :with_primary_family_member)}
     let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, household: family.active_household)}
 
-    before(:each) do
-      allow(ENV).to receive(:[]).with("hbx_id").and_return(hbx_enrollment.hbx_id)
-    end
-
-    it "should change aasm state to canceled" do
-      subject.migrate
-      hbx_enrollment.reload
-      expect(hbx_enrollment.aasm_state).to eq "coverage_canceled"
+    it 'should change aasm state to canceled' do
+      ClimateControl.modify hbx_id: hbx_enrollment.hbx_id do
+        subject.migrate
+        hbx_enrollment.reload
+        expect(hbx_enrollment.aasm_state).to eq 'coverage_canceled'
+      end
     end
   end
 end
