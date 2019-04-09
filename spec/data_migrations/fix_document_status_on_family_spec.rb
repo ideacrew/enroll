c83e1adf269a8905c306673d1f0dbc26d5cda174
Jayanth <jayanth.rordev@gmail.com>
2019-04-09 11:08:50 -0400

    rspec#ivl_new_model_rails_5 fix document status spec

--- spec/data_migrations/fix_document_status_on_family_spec.rb
+++ spec/data_migrations/fix_document_status_on_family_spec.rb
@@ -6,20 +6,25 @@ describe FixDocumentStatus, dbclean: :after_each do
   let(:person) { FactoryBot.create(:person, :with_consumer_role) }
   let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
   let(:family_person) {family.primary_applicant.person}
+  let(:vlp_document) {FactoryBot.build(:vlp_document, :identifier => 'identifier', :verification_type  => 'Citizenship')}
 
-  before(:each) do
-    allow(ENV).to receive(:[]).with("hbx_ids").and_return "#{family_person.hbx_id}"
+  around do |example|
+    ClimateControl.modify hbx_ids:[family_person.hbx_id].join(',') do
+      example.run
+      DatabaseCleaner.clean
+    end
   end
 
-  it "should update the document status on family" do
+  it 'should update the document status on family' do
+    family_person.consumer_role.vlp_documents << vlp_document
     family_person.consumer_role.is_state_resident = true
-    family_person.consumer_role.update_attributes(ssn_validation: "valid")
+    family_person.consumer_role.update_attributes(ssn_validation: 'valid')
     family_person.save!
     family.update_family_document_status!
-    expect(family.all_persons_vlp_documents_status).to eq("Fully Uploaded")
+    expect(family.all_persons_vlp_documents_status).to eq('Fully Uploaded')
     family_person.consumer_role.vlp_documents.first.destroy
     subject.migrate
     family.reload
-    expect(family.vlp_documents_status).to eq("None")
+    expect(family.vlp_documents_status).to eq('None')
   end
 end
