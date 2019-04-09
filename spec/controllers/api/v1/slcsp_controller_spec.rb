3746a954e8022bad35732c34833a018120075f04
Prem Shaganti <prem.shaganti@dc.gov>
2019-04-09 14:44:14 -0400

    ref: rails 5 support controller support on rendering xmls

--- spec/controllers/api/v1/slcsp_controller_spec.rb
+++ spec/controllers/api/v1/slcsp_controller_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
-describe Api::V1::SlcspController do
+describe Api::V1::SlcspController, :type => :controller do
 
   let(:request_xml) { File.read(Rails.root.join("spec", "test_data", "slcsp_payloads", "request.xml")) }
   let(:response_xml) { File.read(Rails.root.join("spec", "test_data", "slcsp_payloads", "response.xml")) }
   let(:plan) {double}
 
   context "valid request" do
+
     it 'returns https status 200' do
       allow(HappyMapper).to receive(:parse).with(anything).and_return(HappyMapper.parse(request_xml))
       allow_any_instance_of(Api::V1::SlcspController).to receive(:find_slcsp).with(anything).and_return(plan)
       allow_any_instance_of(ActionController::Rendering).to receive(:render).and_return(response_xml)
 
-      post :plan, {:format => "xml"}
-      expect(response.status).to eq(200)
+      post :plan, :format => :xml
+      expect(assigns(:plan)).to eq(plan)
+      assert_response :success
+      # in rails 5, controller not renders views
+      # when body is xml or json http_status_code will give wrong output
+      # expect(response.status).to eq(200)
     end
   end
 
