677237979d5237eda9d3e5a05997e5933307a982
Trey Evans <tevans@Just-Plain-Awful.local>
2019-04-10 12:41:07 -0400

    Why cache missing translations?

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,6 +1,23 @@
 require 'mongo_i18n'
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(MongoI18n.store), I18n.backend)
+
+module I18n
+  module Backend
+    module Cache
+      def _fetch(cache_key, &block)
+        result = I18n.cache_store.read(cache_key)
+        return result unless result.nil?
+        result = catch(:exception, &block)
+        unless result.is_a?(Proc) || result.is_a?(MissingTranslation)
+          I18n.cache_store.write(cache_key, result)
+        end
+        result
+      end
+    end
+  end
+end
+
 unless (ENV["NO_CACHE_TRANSLATIONS"] == "true")
-#  I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
-#  I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+  I18n::Backend::Chain.send(:include, I18n::Backend::Cache)
+  I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 end
