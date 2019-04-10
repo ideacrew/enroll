10038e5b82111444ff8f6c2d25782ad5c09fed4e
Prem Shaganti <prem.shaganti@dc.gov>
2019-04-10 15:35:43 -0400

    ref: update Devise gem with latest rails 5 and solve unintialized constant Error

--- components/benefit_markets/spec/rails_helper.rb
+++ components/benefit_markets/spec/rails_helper.rb
@@ -78,8 +78,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Mongoid::Matchers, type: :model
-  config.include Devise::TestHelpers, :type => :controller
-  config.include Devise::TestHelpers, :type => :view
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers, :type => :view
   config.include FactoryBot::Syntax::Methods
   config.include Capybara::DSL
 
