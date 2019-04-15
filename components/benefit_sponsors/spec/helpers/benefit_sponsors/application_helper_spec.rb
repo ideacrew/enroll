7e6c51b63aeeed523489041323ce1acd07d2eda5
Trey Evans <tevans@Just-Plain-Awful.local>
2019-04-15 14:41:43 -0400

    Fix spec requirement.

--- components/benefit_sponsors/spec/helpers/benefit_sponsors/application_helper_spec.rb
+++ components/benefit_sponsors/spec/helpers/benefit_sponsors/application_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe BenefitSponsors::ApplicationHelper, type: :helper, dbclean: :after_each do
   include BenefitSponsors::ApplicationHelper
