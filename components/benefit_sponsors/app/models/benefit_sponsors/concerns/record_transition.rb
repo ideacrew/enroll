0c1cd06ca6d8f99fed94d3b2e60e336e9beaf497
Raghuram Ganjala <raghuramg83@gmail.com>
2019-04-15 11:59:29 -0400

    deleted workflow state transitions files from applications, sponsorships

--- components/benefit_sponsors/app/models/benefit_sponsors/concerns/record_transition.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/concerns/record_transition.rb
@@ -4,14 +4,14 @@ module BenefitSponsors
 
     included do
       include AASM
-      embeds_many :workflow_state_transitions, as: :transitional
+      embeds_many :workflow_state_transitions, as: :transitional, class_name: "::WorkflowStateTransition"
       aasm do
         after_all_transitions :record_transition
       end
     end
 
     def record_transition
-      workflow_state_transitions << WorkflowStateTransition.new({
+      workflow_state_transitions << ::WorkflowStateTransition.new({
         from_state: aasm.from_state, 
         to_state: aasm.to_state, 
         event: aasm.current_event
