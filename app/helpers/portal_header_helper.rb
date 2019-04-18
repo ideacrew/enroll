aa1a17494518c93592066ec918891531f5688f8a
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-04-18 14:11:56 -0400

    bqt fix

--- app/helpers/portal_header_helper.rb
+++ app/helpers/portal_header_helper.rb
@@ -1,12 +1,13 @@
 module PortalHeaderHelper
   include L10nHelper
   def portal_display_name(controller)
+    profile_id = @broker_agency_profile.id if @broker_agency_profile
     if current_user.nil?
       "<a class='portal'>#{l10n("welcome.index.byline")}</a>".html_safe
     elsif current_user.try(:has_hbx_staff_role?)
       link_to "#{image_tag 'icons/icon-exchange-admin.png'} &nbsp; I'm an Admin".html_safe, exchanges_hbx_profiles_root_path, class: "portal"
     elsif current_user.person.try(:broker_role)
-      link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, broker_agencies_profile_path(id: @broker_agency_profile_id || session[:broker_agency_profile_id] || params[:id]), class: "portal"
+      link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, broker_agencies_profile_path(id: @broker_agency_profile_id || session[:broker_agency_profile_id] || profile_id || params[:id]), class: "portal"
     elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
       link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Trained Expert".html_safe, home_exchanges_agents_path, class: "portal"
     elsif current_user.person && current_user.person.active_employee_roles.any?
@@ -22,7 +23,7 @@ module PortalHeaderHelper
         "<a class='portal'>#{image_tag 'icons/icon-family.png'} &nbsp; Individual and Family</a>".html_safe
       end
     elsif controller_path.include?('broker_agencies') && current_user.try(:has_broker_agency_staff_role?)
-      link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, broker_agencies_profile_path(id: @broker_agency_profile_id || session[:broker_agency_profile_id] || params[:id]), class: "portal"
+      link_to "#{image_tag 'icons/icon-expert.png'} &nbsp; I'm a Broker".html_safe, broker_agencies_profile_path(id: @broker_agency_profile_id || session[:broker_agency_profile_id] || profile_id || params[:id]), class: "portal"
     elsif current_user.try(:has_employer_staff_role?)
       link_to "#{image_tag 'icons/icon-business-owner.png'} &nbsp; I'm an Employer".html_safe, employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.employer_profile_id, :tab=>'home'), class: "portal"
     elsif current_user.has_general_agency_staff_role?
