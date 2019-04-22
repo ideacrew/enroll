c61a1d3e4a875fcc028ac81e95e6a6b6e2e99340
zasman <zanesandin@gmail.com>
2019-04-22 17:54:36 -0400

    REFS - 41464: Fixes the pundit allow for displaying the button

--- spec/views/employers/employer_profiles/my_account/_census_employees.html.erb_spec.rb
+++ spec/views/employers/employer_profiles/my_account/_census_employees.html.erb_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe "employers/employer_profiles/my_account/_census_employees.html.er
   let(:user_with_hbx_staff_role) { FactoryGirl.create(:user, :with_family, :with_hbx_staff_role) }
   let(:user_with_employer_role) {FactoryGirl.create(:user, :with_family, :employer_staff) }
   let(:hbx_staff_permission) { FactoryGirl.create(:permission, :hbx_staff) }
+  let(:non_hbx_employer_profile_policy) { EmployerProfilePolicy.new(user_with_employer_role, employer_profile) }
+  let(:hbx_employer_profile_policy) { EmployerProfilePolicy.new(user_with_hbx_staff_role, user_with_hbx_staff_role) }
 
   before :each do
     allow(employer_profile).to receive(:census_employees).and_return [census_employee]
     assign(:employer_profile, employer_profile)
     assign(:available_employee_names, "employee_names")
     assign(:census_employees, [])
-    # This line was originally in the spec
-    allow(view).to receive(:policy_helper).and_return(double("EmployerProfilePolicy", updateable?: true))
     allow(view).to receive(:generate_checkbook_urls_employers_employer_profile_path).and_return('/')
     allow(view).to receive(:current_user).and_return(user_with_employer_role)
-
+    allow(view).to receive(:policy_helper).and_return(non_hbx_employer_profile_policy)
     render "employers/employer_profiles/my_account/census_employees"
   end
 
@@ -46,6 +46,7 @@ RSpec.describe "employers/employer_profiles/my_account/_census_employees.html.er
 
     it 'should display for HBX admin' do
       allow(view).to receive(:current_user).and_return(user_with_hbx_staff_role)
+      allow(view).to receive(:policy_helper).and_return(hbx_employer_profile_policy)
       user_with_hbx_staff_role.stub_chain('person.hbx_staff_role.permission').and_return(hbx_staff_permission)
       render "employers/employer_profiles/my_account/census_employees"
       expect(rendered).to match(/Terminate Employee Roster Enrollments/)
