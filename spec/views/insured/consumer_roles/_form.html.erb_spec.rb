132a7dcad57140eba46d328ba59e1e3250e35ba0
Vishal Kalletla <vishuk1201@gmail.com>
2019-05-02 13:14:54 -0400

    spec fix for consumer role form

--- spec/views/insured/consumer_roles/_form.html.erb_spec.rb
+++ spec/views/insured/consumer_roles/_form.html.erb_spec.rb
@@ -1,29 +1,6 @@
 require "rails_helper"
-
+include ActionView::Context
 RSpec.describe "insured/consumer_roles/_form.html.erb" do
-  let(:person) { Person.new }
-  let(:current_user) {FactoryBot.create(:user)}
-
-  #before do
-    #Translation.create(key: "en.required_field", value: "\"required field\"")
-  #end
-
-  before :each do
-    helper = Object.new.extend ActionView::Helpers::FormHelper
-    helper.extend ActionView::Context
-    helper.extend ActionDispatch::Routing::PolymorphicRoutes
-    helper.extend ActionView::Helpers::FormOptionsHelper
-    person.build_consumer_role if person.consumer_role.blank?
-    person.consumer_role.build_nested_models_for_person
-    mock_form = ActionView::Helpers::FormBuilder.new(:person, person, helper, {})
-    stub_template "shared/_consumer_fields.html.erb" => ''
-    sign_in current_user
-    allow(view).to receive(:policy_helper).and_return(double("FamilyPolicy", updateable?: true))
-    assign(:consumer_role, person.consumer_role)
-    assign(:person, person)
-    render partial: "insured/consumer_roles/form", locals: {f: mock_form}
-  end
-
   context "render insured consumer role form" do
     let(:person) { Person.new }
     let(:current_user) {FactoryBot.create(:user)}
@@ -62,7 +39,6 @@ RSpec.describe "insured/consumer_roles/_form.html.erb" do
       expect(rendered).to match /Even if you don’t want health coverage for yourself, providing your SSN can be helpful since it can speed up the application process. We use SSNs to check income and other information to see who’s eligible for help with health coverage costs./
     end
   end
-
 
   context "user has an associated e_case_id" do
     let(:person) { FactoryBot.create(:person) }
