fe1fd7343b21e9639affca0ed52138ac27196409
zasman <zanesandin@gmail.com>
2019-05-06 14:00:27 -0400

    Adds prepend true for protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
 
   ## Devise filters
   before_action :require_login, unless: :authentication_not_required?
