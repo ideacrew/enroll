6e1e0429266110c85b449a9877d6302703c33698
zasman <zanesandin@gmail.com>
2019-05-13 16:52:58 -0400

    REFS 40288 - Fixes requirement to call as an environmental variable rather than setting.
    Stuff
    
    Stuff
    
    Stuff
    
    Stuff
    
    Fixed

--- spec/views/layout/_header.html.erb_spec.rb
+++ spec/views/layout/_header.html.erb_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "layouts/_header.html.erb" do
   describe "Header Styles" do
-    context "settings set to deployed_to_production" do
+    context "review_environmental variable set" do
       before do
-        allow(Settings.site).to receive(:deployed_to_production).and_return(true)
-      end
-
-      it "does NOT render pre_prod_nav_color CSS class" do
-        visit root_url
-        expect(page).to_not have_css("nav.pre_prod_nav_color")
-      end
-    end
-
-    context "setting NOT set to deployed_to_production" do
-      before do
-        allow(Settings.site).to receive(:deployed_to_production).and_return(false)
+        ENV['ENROLL_REVIEW_ENVIRONMENT'] = 'true'
       end
 
       it "renders pre_prod_nav_color CSS class" do
         visit root_url
         expect(page).to have_css("nav.pre_prod_nav_color")
+      end
+    end
+
+    context "review_environmental variable NOT set" do
+      before do
+        ENV['ENROLL_REVIEW_ENVIRONMENT'] = 'false'
+      end
+
+      it "does not render pre_prod_nav_color CSS class" do
+        visit root_url
+        expect(page).to_not have_css("nav.pre_prod_nav_color")
       end
     end
   end
