2fc5a8d5e609e6a41ac365db269541498babc2f5
Raghuram Ganjala <raghuramg83@gmail.com>
2019-05-14 13:54:53 -0400

    Refs #41576 Added elected product choice field to dental sponsored benefit model

--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/dental_sponsored_benefit.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/dental_sponsored_benefit.rb
@@ -2,6 +2,7 @@ module BenefitSponsors
   module SponsoredBenefits
     class DentalSponsoredBenefit < SponsoredBenefit
 
+      field :elected_product_choices, type: Array  # used for choice model to store employer preferences
     end
   end
 end
