f0efe641fc1f149ca17cab62b6ee1092f84408bf
Zachary Landes <zach.g.landes@gmail.com>
2019-05-15 17:02:45 -0400

    adding non detailed report

--- app/data_migrations/ma_nfp_reports.rb
+++ app/data_migrations/ma_nfp_reports.rb
@@ -1,9 +1,16 @@
-require File.join(Rails.root, "lib/mongoid_migration_task")
+# require File.join(Rails.root, "lib/mongoid_migration_task")
 
-class MaNfpReports < MongoidMigrationTask
-  def migrate
-    file_name = "#{Rails.root}/ma_nfp_report.csv"
-    start_on = Date.strptime(ENV['start_on'],'%m/%d/%Y')
+class MaNfpReports
+
+  def initialize(start_on)
+    detailed_report(start_on)
+    assign_packages(start_on)
+    non_detailed_report(start_on)
+  end
+
+  def detailed_report(start_on)
+    file_name = "#{Rails.root}/ma_nfp_detailed_report.csv"
+    # start_on = Date.strptime(ENV['start_on'],'%m/%d/%Y')
     field_names  = %w(
               Employer_Legal_Name
               Employer_FEIN
@@ -27,7 +34,7 @@ class MaNfpReports < MongoidMigrationTask
               2019_status
             )
 
-    system("rm -rf #{Rails.root}/ma_nfp_report.csv")
+    system("rm -rf #{file_name}")
 
     CSV.open(file_name, "w", force_quotes: true) do |csv|
       csv << field_names
@@ -38,13 +45,11 @@ class MaNfpReports < MongoidMigrationTask
           bene_app_2018 = ben_spon.benefit_applications.where(:"effective_period.min".lt=>start_on, aasm_state: :active).first
           ben_spon.census_employees.active.each do |census|
             if census.employee_role.present?
-                family = census.employee_role.person.primary_family
+              family = census.employee_role.person.primary_family
               bene_app_2018 = ben_spon.benefit_applications.where(:"effective_period.min".lt =>start_on, aasm_state: :active).first
               packages_2018 = bene_app_2018.present? ? bene_app_2018.benefit_packages.map(&:id) : []
-
               package_ids = packages_2018 + bene_app.benefit_packages.map(&:id)
-
-              enrollment = family.active_household.hbx_enrollments.where(:sponsored_benefit_package_id.in=>package_ids, :aasm_state.nin=>["shopping","coverage_canceled","coverage_expired","coverage_terminated"])
+              enrollment = family.active_household.hbx_enrollments.where(:sponsored_benefit_package_id.in=>package_ids, :aasm_state.nin=>["shopping","coverage_canceled","coverage_expired"])
             end
             next unless enrollment
             ["health", "dental"].each do |kind|
@@ -73,13 +78,104 @@ class MaNfpReports < MongoidMigrationTask
                       enrollment_2019.try(:effective_on),
                       enrollment_2019.try(:aasm_state),
                       ]
-             end
+              end
             end
           end
         end
       end
     end
   end
+end  
+
+
+
+def assign_packages(start_on)
+  CSV.open("#{Rails.root}/unnassigned_packages_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.csv", "w") do |csv|
+    csv << ["Sponsor fein", "Sponsor legal_name", "Census_Employee", "ce id"]
+      BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(:'benefit_applications.effective_period.min'=>start_on).each do |ben_spon|
+        ben_spon.benefit_applications.each do |bene_app|
+          if bene_app.effective_period.min == start_on && bene_app.is_renewing?
+            bene_app_2018 = ben_spon.benefit_applications.where(:"effective_period.min".lt=>start_on, aasm_state: :active).first
+            ben_spon.census_employees.active.each do |census|
+              if census.employee_role.present?
+                if census.benefit_group_assignments.where(:benefit_package_id.in => bene_app.benefit_packages.map(&:id)).blank?
+                  unless census.aasm_state == "employment_terminated" || census.aasm_state == "rehired" ||  census.aasm_state == "cobra_terminated"
+                data = [ben_spon.fein, ben_spon.legal_name, census.full_name, census.id]  
+                csv << data
+                end
+              end
+            end
+            puts "#{ben_spon.fein} has errors #{ben_spon.errors}" if ben_spon.errors.present? unless Rails.env.test?
+          end
+        end
+      end
+    end
+    puts "Unnasigned packages file created" unless Rails.env.test?
+  end
 end
 
+def non_detailed_report(start_on)
+  file_name = "#{Rails.root}/ma_nfp_non_detailed_report.csv"
+  # start_on = Date.strptime(ENV['start_on'],'%m/%d/%Y')
+  field_names  = [
+    "Employer_Legal_Name",
+    "Employer_FEIN",
+    "Renewal State",
+    "#{start_on.prev_year.year} Active Enrollments",
+    "#{start_on.prev_year.year} Passive Renewal Enrollments"
+  ]
+  system("rm -rf #{file_name}")
 
+  CSV.open(file_name, "w", force_quotes: true) do |csv|
+    csv << field_names
+    BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(:'benefit_applications.effective_period.min'=>start_on).each do |ben_spon|
+      ben_spon.benefit_applications.each do |bene_app|
+        if bene_app.effective_period.min == start_on && bene_app.is_renewing?
+          data =  [ben_spon.legal_name, ben_spon.fein, ben_spon.renewal_benefit_application.aasm_state.to_s.camelcase]
+          next if ben_spon.renewal_benefit_application.blank?
+          active_bg_ids = ben_spon.current_benefit_application.benefit_packages.pluck(:id)
+          families = Family.where(:"households.hbx_enrollments" => {:$elemMatch => {:sponsored_benefit_package_id.in => active_bg_ids, :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES
+            }})
+          if ben_spon.renewal_benefit_application.present?
+            if ben_spon.renewal_benefit_application.aasm_state.in?([:enrollment_open, :enrollment_extended, :enrollment_closed])
+              renewal_bg_ids = ben_spon.renewal_benefit_application.benefit_packages.pluck(:id)
+            end
+          end
+
+          active_enrollment_count = 0
+          renewal_enrollment_count = 0
+          families.each do |family|
+                enrollments = family.active_household.hbx_enrollments.where({
+                  :sponsored_benefit_package_id.in => active_bg_ids,
+                  :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES
+                })
+                %w(health dental).each do |coverage_kind|
+                  if enrollments.where(:coverage_kind => coverage_kind).present?
+                    active_enrollment_count += 1
+                  end
+                end
+
+                if renewal_bg_ids.present?
+                  renewal_enrollments = family.active_household.hbx_enrollments.where({
+                    :sponsored_benefit_package_id.in => renewal_bg_ids,
+                    :aasm_state.in => HbxEnrollment::ENROLLED_STATUSES + ['auto_renewing']
+                  })
+
+                  %w(health dental).each do |coverage_kind|
+                    if renewal_enrollments.where(:coverage_kind => coverage_kind).present?
+                      renewal_enrollment_count += 1
+                    end
+                  end
+                end
+              end
+          data += [active_enrollment_count, renewal_enrollment_count]
+          csv << data
+        end
+      end
+    end
+  end
+end  
+
+start_on = Date.new(2019,6,1)
+reports = MaNfpReports.new(Date.new(2019,6,1))
+# reports.rub(start_on)
