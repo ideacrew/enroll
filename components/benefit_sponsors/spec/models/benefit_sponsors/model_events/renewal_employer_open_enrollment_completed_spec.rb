c3b6d15ece273b8ead11e7d593494649544c084d
Jayanth <jayanth.rordev@gmail.com>
2019-05-15 10:25:56 -0400

    refs#dc_new_model_base_notices2 fixed renewal OE completed notice for employee

--- components/benefit_sponsors/spec/models/benefit_sponsors/model_events/renewal_employer_open_enrollment_completed_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/model_events/renewal_employer_open_enrollment_completed_spec.rb
@@ -2,26 +2,41 @@
 
 require 'rails_helper'
 
+require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_market.rb"
+require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_application.rb"
+
 RSpec.describe 'BenefitSponsors::ModelEvents::RenewalEmployerOpenEnrollmentCompleted', dbclean: :after_each do
-  let(:model_event) { "employer_open_enrollment_completed" }
-  let(:notice_event) { "renewal_employer_open_enrollment_completed" }
-  let(:start_on) { TimeKeeper.date_of_record.next_month.beginning_of_month}
-  let!(:site) { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
-  let!(:organization)     { FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
-  let!(:employer_profile)    { organization.employer_profile }
-  let!(:benefit_sponsorship)    { employer_profile.add_benefit_sponsorship }
-  let!(:model_instance) do
-    FactoryBot.create(:benefit_sponsors_benefit_application,
-                      :with_benefit_package,
-                      benefit_sponsorship: benefit_sponsorship,
-                      aasm_state: 'enrollment_open',
-                      effective_period: start_on..(start_on + 1.year) - 1.day)
-  end
+  include_context "setup benefit market with market catalogs and product packages"
+  include_context "setup renewal application"
+
+  let(:renewal_state)            { :enrollment_open }
+  let(:notice_event) { "renewal_employee_enrollment_confirmation" }
+  let!(:person){ FactoryBot.create(:person, :with_family)}
+  let!(:family) {person.primary_family}
+  let!(:employee_role) { FactoryBot.create(:benefit_sponsors_employee_role, person: person, employer_profile: abc_profile, census_employee_id: census_employee.id)}
+  let!(:census_employee)  { FactoryBot.create(:benefit_sponsors_census_employee, benefit_sponsorship: benefit_sponsorship, employer_profile: abc_profile) }
+
+  let!(:model_instance) {renewal_application}
   let(:enrollment_policy) {instance_double("BenefitMarkets::BusinessRulesEngine::BusinessPolicy", success_results: "Success") }
-  let!(:date_mock_object) { double("Date", day: (model_instance.open_enrollment_period.max + 1.day).day)}
+  let!(:hbx_enrollment) do
+    hbx_enrollment = FactoryBot.create(:hbx_enrollment, :with_enrollment_members, :with_product,
+                                       household: family.active_household,
+                                       aasm_state: "renewing_coverage_selected",
+                                       effective_on: model_instance.start_on,
+                                       rating_area_id: model_instance.recorded_rating_area_id,
+                                       sponsored_benefit_id: model_instance.benefit_packages.first.health_sponsored_benefit.id,
+                                       sponsored_benefit_package_id: model_instance.benefit_packages.first.id,
+                                       benefit_sponsorship_id: model_instance.benefit_sponsorship.id,
+                                       benefit_group_assignment_id: census_employee.renewal_benefit_group_assignment.id,
+                                       employee_role_id: employee_role.id)
+    hbx_enrollment.benefit_sponsorship = benefit_sponsorship
+    hbx_enrollment.save!
+    hbx_enrollment
+  end
 
   before do
     allow(model_instance).to receive(:is_renewing?).and_return(true)
+    census_employee.update_attributes(employee_role_id: employee_role.id)
   end
 
   describe "ModelEvent" do
@@ -45,17 +60,25 @@ RSpec.describe 'BenefitSponsors::ModelEvents::RenewalEmployerOpenEnrollmentCompl
       it "should trigger notice event" do
         expect(subject.notifier).to receive(:notify) do |event_name, payload|
           expect(event_name).to eq "acapi.info.events.employer.renewal_employer_open_enrollment_completed"
-          expect(payload[:employer_id]).to eq employer_profile.hbx_id.to_s
+          expect(payload[:employer_id]).to eq abc_profile.hbx_id.to_s
           expect(payload[:event_object_kind]).to eq 'BenefitSponsors::BenefitApplications::BenefitApplication'
           expect(payload[:event_object_id]).to eq model_instance.id.to_s
         end
+
+        expect(subject.notifier).to receive(:notify) do |event_name, payload|
+          expect(event_name).to eq "acapi.info.events.employee.renewal_employee_enrollment_confirmation"
+          expect(payload[:employee_role_id]).to eq census_employee.employee_role_id.to_s
+          expect(payload[:event_object_kind]).to eq 'HbxEnrollment'
+          expect(payload[:event_object_id]).to eq hbx_enrollment.id.to_s
+        end
+
         allow_any_instance_of(BenefitMarkets::BusinessRulesEngine::BusinessPolicy).to receive(:is_satisfied?).with(model_instance).and_return true
         subject.process_application_events(model_instance, model_event)
       end
     end
   end
 
-  describe "NoticeBuilder" do
+  describe "NoticeBuilder for Employer" do
 
     let(:data_elements) do
       [
@@ -81,7 +104,7 @@ RSpec.describe 'BenefitSponsors::ModelEvents::RenewalEmployerOpenEnrollmentCompl
       subject { Notifier::NoticeKind.new(template: template, recipient: recipient) }
 
       before do
-        allow(subject).to receive(:resource).and_return(employer_profile)
+        allow(subject).to receive(:resource).and_return(abc_profile)
         allow(subject).to receive(:payload).and_return(payload)
       end
 
@@ -94,20 +117,71 @@ RSpec.describe 'BenefitSponsors::ModelEvents::RenewalEmployerOpenEnrollmentCompl
       end
 
       it "should return employer name" do
-        expect(merge_model.employer_name).to eq employer_profile.legal_name
+        expect(merge_model.employer_name).to eq abc_profile.legal_name
       end
 
       it "should return plan year start date" do
-        expect(merge_model.benefit_application.renewal_py_start_date).to eq model_instance.start_on.strftime('%m/%d/%Y')
+        expect(merge_model.benefit_application.renewal_py_start_date).to eq renewal_application.start_on.strftime('%m/%d/%Y')
       end
 
       it "should return publish due date" do
-        due_date = Date.new(model_instance.start_on.prev_month.year, model_instance.start_on.prev_month.month, Settings.aca.shop_market.renewal_application.publish_due_day_of_month).strftime('%m/%d/%Y')
+        due_date = Date.new(renewal_application.start_on.prev_month.year, renewal_application.start_on.prev_month.month, Settings.aca.shop_market.renewal_application.publish_due_day_of_month).strftime('%m/%d/%Y')
         expect(merge_model.benefit_application.renewal_py_submit_due_date).to eq due_date
       end
 
       it "should return false when there is no broker linked to employer" do
         expect(merge_model.broker_present?).to be_falsey
+      end
+    end
+  end
+
+  describe "NoticeBuilder for Employee" do
+
+    let(:data_elements) do
+      [
+        "employee_profile.notice_date",
+        "employee_profile.first_name",
+        "employee_profile.last_name",
+        "employee_profile.employer_name",
+        "employer_profile.notice_date"
+      ]
+    end
+    let(:merge_model) { subject.construct_notice_object }
+    let(:recipient) { "Notifier::MergeDataModels::EmployeeProfile" }
+    let(:template)  { Notifier::Template.new(data_elements: data_elements) }
+    let(:payload)   { {"event_object_kind" => "HbxEnrollment", "event_object_id" => hbx_enrollment.id }}
+
+    context "when notice event received" do
+      subject { Notifier::NoticeKind.new(template: template, recipient: recipient) }
+
+      before do
+        allow(subject).to receive(:resource).and_return(employee_role)
+        allow(subject).to receive(:payload).and_return(payload)
+        employee_role.update_attributes(census_employee_id: census_employee.id)
+      end
+
+      it "should return merge model" do
+        expect(merge_model).to be_a(recipient.constantize)
+      end
+
+      it "should return notice_date" do
+        expect(merge_model.notice_date).to eq TimeKeeper.date_of_record.strftime('%m/%d/%Y')
+      end
+
+      it "should return employee first_name" do
+        expect(merge_model.first_name).to eq employee_role.person.first_name
+      end
+
+      it "should return employee last_name" do
+        expect(merge_model.last_name).to eq employee_role.person.last_name
+      end
+
+      it "should return employer_name" do
+        expect(merge_model.employer_name).to eq abc_profile.legal_name
+      end
+
+      it "should return the date of the notice" do
+        expect(merge_model.notice_date).to eq TimeKeeper.date_of_record.strftime('%m/%d/%Y')
       end
     end
   end
