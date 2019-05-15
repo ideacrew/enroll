2095165351dbb124228033349ee1cd82dd1de0ff
Raghuram Ganjala <raghuramg83@gmail.com>
2019-05-15 02:09:11 -0400

    Refs #41576 fixed choice model checkboxes

--- components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/dental_sponsored_benefit.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/sponsored_benefits/dental_sponsored_benefit.rb
@@ -3,6 +3,17 @@ module BenefitSponsors
     class DentalSponsoredBenefit < SponsoredBenefit
 
       field :elected_product_choices, type: Array  # used for choice model to store employer preferences
+
+      def lookup_package_products(coverage_date)
+        if product_package_kind == :multi_product
+          package_products = product_package.products.by_service_areas(recorded_service_area_ids).select do |product|
+            elected_product_choices.include?(product.id.to_s)
+          end
+          BenefitMarkets::Products::Product.by_coverage_date(package_products, coverage_date)
+        else
+          super
+        end
+      end
     end
   end
 end
