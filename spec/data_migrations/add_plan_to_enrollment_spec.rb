47d64c36069fea10cc225c8169b3bc794f845c6e
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2019-05-15 09:28:05 -0400

    test commit uncommented

--- spec/data_migrations/add_plan_to_enrollment_spec.rb
+++ spec/data_migrations/add_plan_to_enrollment_spec.rb
@@ -12,26 +12,26 @@ describe AddPlantoEnrollment do
     end
   end
 
-  # describe "enrollment with no plan" do
-  #
-  #   let(:family) { FactoryBot.create(:family, :with_primary_family_member)}
-  #   let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, household: family.active_household)}
-  #   let(:plan) { FactoryBot.create(:plan, :with_premium_tables, active_year: TimeKeeper.date_of_record.year)}
-  #
-  #
-  #   before(:each) do
-  #     allow(ENV).to receive(:[]).with("enrollment_id").and_return(hbx_enrollment._id)
-  #     allow(ENV).to receive(:[]).with("plan_id").and_return(plan._id)
-  #     hbx_enrollment.update(plan_id:'', carrier_profile_id:'')
-  #   end
-  #
-  #   it "should add a plan to enrollment" do
-  #     expect(hbx_enrollment.plan_id).to eq nil
-  #     expect(hbx_enrollment.carrier_profile_id).to eq nil
-  #     subject.migrate
-  #     hbx_enrollment.reload
-  #     expect(hbx_enrollment.plan_id).to eq plan.id
-  #     expect(hbx_enrollment.carrier_profile_id).to eq plan.carrier_profile_id
-  #   end
-  # end
+  describe "enrollment with no plan" do
+
+    let(:family) { FactoryBot.create(:family, :with_primary_family_member)}
+    let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, household: family.active_household)}
+    let(:plan) { FactoryBot.create(:plan, :with_premium_tables, active_year: TimeKeeper.date_of_record.year)}
+
+
+    before(:each) do
+      allow(ENV).to receive(:[]).with("enrollment_id").and_return(hbx_enrollment._id)
+      allow(ENV).to receive(:[]).with("plan_id").and_return(plan._id)
+      hbx_enrollment.update(plan_id:'', carrier_profile_id:'')
+    end
+
+    it "should add a plan to enrollment" do
+      expect(hbx_enrollment.plan_id).to eq nil
+      expect(hbx_enrollment.carrier_profile_id).to eq nil
+      subject.migrate
+      hbx_enrollment.reload
+      expect(hbx_enrollment.plan_id).to eq plan.id
+      expect(hbx_enrollment.carrier_profile_id).to eq plan.carrier_profile_id
+    end
+  end
 end
