8f374c7149b9b23dc8f3eb3ece247db047ee00e6
Raghuram Ganjala <raghuramg83@gmail.com>
2019-05-24 10:27:08 -0400

    Refs #dc_new_model Update sprokets asset lookup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -430,14 +430,14 @@ module ApplicationHelper
     carrier_name = carrier_logo(plan)
     image_tag("logo/carrier/#{carrier_name.parameterize.underscore}.jpg", width: options[:width]) # Displays carrier logo (Delta Dental => delta_dental.jpg)
   end
-      
+
   def digest_logos
     carrier_logo_hash = Hash.new(carriers:{})
     carriers = ::BenefitSponsors::Organizations::Organization.issuer_profiles
     carriers.each do |car|
       if Rails.env == "production"
         image = "logo/carrier/#{car.legal_name.parameterize.underscore}.jpg"
-        digest_image = "/assets/#{Rails.application.assets.find_asset(image).digest_path}"
+        digest_image = "/assets/#{::Sprockets::Railtie.build_environment(Rails.application).find_asset(image).digest_path}"
         carrier_logo_hash[car.legal_name] = digest_image
       else
         image = "/assets/logo/carrier/#{car.legal_name.parameterize.underscore}.jpg"
@@ -653,7 +653,7 @@ module ApplicationHelper
   end
 
   def asset_data_base64(path)
-    asset = Rails.application.assets.find_asset(path)
+    asset = ::Sprockets::Railtie.build_environment(Rails.application).find_asset(path)
     throw "Could not find asset '#{path}'" if asset.nil?
     base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
     "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
