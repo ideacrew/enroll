c6a293594f9de7ea84c1a8307b04f9039be611fd
rabbittracey <traceyw@vt.edu>
2019-05-29 14:11:14 -0400

    fix rspec

--- app/policies/census_employee_policy.rb
+++ app/policies/census_employee_policy.rb
@@ -1,14 +1,14 @@
 class CensusEmployeePolicy < ApplicationPolicy
   #TODOJF JULY 1 review this
   def update?
-    if @user.has_role? :hbx_staff
+    if @user.has_hbx_staff_role?
       true
     else
-      can_change = if (@user.has_role?(:employer_staff) && !@user.has_role?(:broker))
+      can_change = if (@user.has_employer_staff_role? && !@user.has_broker_role?)
         @user.person.employer_staff_roles.map(&:employer_profile_id).map(&:to_s).include? @record.employer_profile_id.try(:to_s) rescue false
       elsif @user.has_role? :broker
         @record.employer_profile.try(:active_broker) == @user.person
-      elsif @user.has_role?(:general_agency_staff)
+      elsif @user.has_general_agency_staff_role?
         emp_ids = EmployerProfile.find_by_general_agency_profile(@user.person.general_agency_staff_roles.first.general_agency_profile).map(&:id)
         emp_ids.include? ( @record.employer_profile.id)
       else
@@ -19,17 +19,17 @@ class CensusEmployeePolicy < ApplicationPolicy
   end
 
   def show?
-    if @user.has_role? :hbx_staff
+    if @user.has_hbx_staff_role?
       true
     else
-      can_change = if (@user.has_role?(:employer_staff) && !@user.has_role?(:broker))
+      can_change = if (@user.has_employer_staff_role? && !@user.has_broker_role?)
         @user.person.employer_staff_roles.map(&:employer_profile_id).map(&:to_s).include? @record.employer_profile_id.try(:to_s) rescue false
-      elsif @user.has_role? :broker
+      elsif @user.has_broker_role?
         @record.employer_profile.try(:active_broker) == @user.person
       elsif @user.has_broker_agency_staff_role?
         profile_ids = @user.person.broker_agency_staff_roles.map(&:broker_agency_profile_id)
         profile_ids.include? (@record.employer_profile.broker_agency_profile.id)
-      elsif @user.has_role?(:general_agency_staff)
+      elsif @user.has_general_agency_staff_role?
         emp_ids = EmployerProfile.find_by_general_agency_profile(@user.person.general_agency_staff_roles.first.general_agency_profile).map(&:id)
         emp_ids.include? ( @record.employer_profile.id)
       else
