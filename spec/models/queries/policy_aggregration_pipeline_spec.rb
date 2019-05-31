f6427fc10a4141ea9201492e6901264edf14176d
Nikhil S <nikhilks219@gmail.com>
2019-05-31 10:39:07 -0400

    factories for aggregrate pipeline

--- spec/models/queries/policy_aggregration_pipeline_spec.rb
+++ spec/models/queries/policy_aggregration_pipeline_spec.rb
@@ -57,7 +57,34 @@ describe Queries::PolicyAggregationPipeline, "Policy Queries", dbclean: :after_e
   let!(:hbx_enrollment){FactoryBot.create(:hbx_enrollment, hbx_enrollment_members:[hbx_enrollment_member], household:family.active_household)}
 
   let(:family_member){FactoryBot.create(:family_member, family: family,is_primary_applicant: false, is_active: true, person: valid_employee_role.person)}
+  let!(:hbx_id_list) {[abc_organization.hbx_id]}
+  let!(:rating_area) { create_default(:benefit_markets_locations_rating_area) }
+  let(:market_inception) { TimeKeeper.date_of_record.year }
+  let!(:current_effective_date) { Date.new(TimeKeeper.date_of_record.last_year.year, TimeKeeper.date_of_record.month, 1) }
+  let(:aasm_state) { :active }
+  let!(:save_catalog){ benefit_market.benefit_market_catalogs.map(&:save)}
+  let(:business_policy) { instance_double("some_policy", success_results: "validated successfully")}
+  let!(:benefit_group_assignment) { FactoryBot.build(:benefit_group_assignment, start_on: current_benefit_package.start_on, benefit_group_id:nil, benefit_package: current_benefit_package, is_active:false)}
+  let(:product) { FactoryBot.create(:benefit_markets_products_health_products_health_product)}
+  let(:employee_role) { FactoryBot.build(:employee_role, benefit_sponsors_employer_profile_id:abc_profile.id)}
+  let(:census_employee) { FactoryBot.create(:census_employee, dob: TimeKeeper.date_of_record - 21.year, employer_profile_id: nil, benefit_sponsors_employer_profile_id: abc_profile.id, benefit_sponsorship: benefit_sponsorship, :benefit_group_assignments => [benefit_group_assignment],employee_role_id:employee_role.id) }
+  let(:person) {FactoryBot.create(:person, first_name: census_employee.first_name, last_name: census_employee.last_name, dob: TimeKeeper.date_of_record - 21.year, ssn:census_employee.ssn)}
+  let(:family) { FactoryBot.create(:family, :with_primary_family_member, person:person)}
+  let(:active_household) {family.active_household}
+  let(:sponsored_benefit) {current_benefit_package.sponsored_benefits.first}
+  let(:reference_product) {current_benefit_package.sponsored_benefits.first.reference_product}
+  let(:hbx_enrollment_member){ FactoryBot.build(:hbx_enrollment_member, is_subscriber:true, coverage_start_on: current_benefit_package.start_on, eligibility_date: current_benefit_package.start_on, applicant_id: family.family_members.first.id) }
+  let(:enrollment) { FactoryBot.create(:hbx_enrollment, hbx_enrollment_members:[hbx_enrollment_member],product: reference_product, sponsored_benefit_package_id: current_benefit_package.id, effective_on:initial_application.effective_period.min, household:family.active_household,benefit_group_assignment_id: benefit_group_assignment.id, employee_role_id:employee_role.id, benefit_sponsorship_id:benefit_sponsorship.id)}
+  let!(:issuer_profile)  { FactoryBot.create(:benefit_sponsors_organizations_issuer_profile) }
+  let!(:update_reference_product) {reference_product.update_attributes(issuer_profile_id:issuer_profile.id)}
 
+  before(:each) do
+    person = family.primary_applicant.person
+    person.employee_roles = [employee_role]
+    person.employee_roles.map(&:save)
+    active_household.hbx_enrollments =[enrollment]
+    active_household.save!
+  end
   context 'aggregation methods' do
 
     before do
@@ -88,9 +115,7 @@ describe Queries::PolicyAggregationPipeline, "Policy Queries", dbclean: :after_e
       expect(instance.pipeline.count).to be 5
       expect([instance.pipeline[4]]).to eq [{"$match"=> {"households.hbx_enrollments.sponsored_benefit_package_id"=>
                                                         {"$in"=>
-                                                        [benefit_group_ids[0],
-                                                        benefit_group_ids[1],
-                                                        benefit_group_ids[2]]}}}]
+                                                        [benefit_group_ids[0]]}}}]
     end
 
     it 'exclude_employers_by_hbx_ids' do
