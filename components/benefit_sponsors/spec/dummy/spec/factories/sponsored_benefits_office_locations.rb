04f452bb381b239ff23217d7bced9c9202cbbb67
harshaellanki <h_vardhan@yahoo.com>
2019-06-04 10:18:51 -0400

    moved some models&factories to dummy to run generalagency notices specs from engine

---
+++ components/benefit_sponsors/spec/dummy/spec/factories/sponsored_benefits_office_locations.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :sponsored_benefits_office_location, class: 'SponsoredBenefits::Organizations::OfficeLocation' do
+    is_primary false
+    address { FactoryBot.build(:sponsored_benefits_locations_address, kind: "branch") }
+    # phone   { FactoryBot.build(:sponsored_benefits_phone, kind: "work") }
+
+    trait :primary do
+      is_primary true
+      address { FactoryBot.build(:sponsored_benefits_locations_address, kind: "primary") }
+    end
+  end
+end
