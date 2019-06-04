8d8e9043452c2015a04fb27ebd88a8ea7c5fcc2a
saikumar9 <saikumarkotagiri9@gmail.com>
2019-06-04 09:31:35 -0400

    Refs #42288, Review page display of disability driver

--- db/seedfiles/translations/en/faa_translations.rb
+++ db/seedfiles/translations/en/faa_translations.rb
@@ -1,5 +1,7 @@
 # Assistance Translations
 ASSISTANCE_TRANSLATIONS = {
   "en.faa.curam_lookup" => "It looks like you've already completed an application for Medicaid and cost savings on DC Health Link. Please call DC Health Link at (855) 532-5465 to make updates to that application. If you keep going, we'll check to see if you qualify to enroll in a private health insurance plan on DC Health Link, but won't be able to tell you if you qualify for Medicaid or cost savings.",
-  "en.faa.acdes_lookup" => "It looks like you're already covered by Medicaid. Please call DC Health Link at (855) 532-5465 to make updates to your case. If you keep going, we'll check to see if you qualify to enroll in a private health insurance plan on DC Health Link, but won't be able to tell you if you qualify for Medicaid or cost savings."
+  "en.faa.acdes_lookup" => "It looks like you're already covered by Medicaid. Please call DC Health Link at (855) 532-5465 to make updates to your case. If you keep going, we'll check to see if you qualify to enroll in a private health insurance plan on DC Health Link, but won't be able to tell you if you qualify for Medicaid or cost savings.",
+
+  "en.faa.other_ques.disability_question" => "Does this person have a disability? *"
 }.freeze
