d798c4646070fe3c37a681af37b695dff413134a
Zachary Landes <zach.g.landes@gmail.com>
2019-06-07 10:29:06 -0400

    passing spec/data_migrations/update_waiver_reason_spec.rb

--- spec/data_migrations/update_waiver_reason_spec.rb
+++ spec/data_migrations/update_waiver_reason_spec.rb
@@ -14,7 +14,7 @@ describe UpdateWaiverReason, dbclean: :after_each do
 
   describe 'update waiver reason' do
     let(:family) { FactoryBot.create(:family, :with_primary_family_member)}
-    let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, household: family.active_household, waiver_reason: 'this is the reason')}
+    let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, family: family, household: family.active_household, waiver_reason: 'this is the reason')}
 
     it 'should update waiver reason' do
       ClimateControl.modify id: "#{hbx_enrollment.hbx_id}", waiver_reason: 'waiver_reason' do
