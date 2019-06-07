03e825f321e83a02f19862beec512fafdb9f818b
rabbittracey <traceyw@vt.edu>
2019-06-07 15:18:47 -0400

    update script"

---
+++ script/orphan_user_account_list.rb
@@ -0,0 +1,20 @@
+  require 'csv'
+  
+  field_names  = %w(
+                     user_name
+                     email  
+                     )
+  processed_count = 0
+  file_name = "#{Rails.root}/orphan_user_report_#{TimeKeeper.date_of_record.strftime("%m_%d_%Y")}.csv"
+  CSV.open(file_name, "w", force_quotes: true) do |csv|
+    csv << field_names
+    User.orphans.each do |user|
+        csv << [
+          user.oim_id,
+          user.email
+        ]
+      processed_count += 1
+    end
+    offset = offset + batch_size
+    puts "#{org_count} orphan user listed in #{file_name}" unless Rails.env.test?
+  end
