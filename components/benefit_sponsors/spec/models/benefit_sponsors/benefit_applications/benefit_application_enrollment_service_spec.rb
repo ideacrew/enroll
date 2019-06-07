a8c3ca0072f575440a37a48758313f18a234048c
zasman <zanesandin@gmail.com>
2019-06-07 15:09:29 -0400

    fixes benefit_application_enrollment_service_spec.rb spec

--- components/benefit_sponsors/spec/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/benefit_applications/benefit_application_enrollment_service_spec.rb
@@ -954,6 +954,7 @@ module BenefitSponsors
       let(:family) {person.primary_family}
 
       let!(:hbx_enrollment) {  FactoryBot.create(:hbx_enrollment, :with_enrollment_members, :with_product,
+                        family: family,
                         household: family.active_household,
                         aasm_state: "coverage_selected",
                         effective_on: initial_application.start_on,
@@ -966,6 +967,7 @@ module BenefitSponsors
       }
 
       let!(:dental_hbx_enrollment) {  FactoryBot.create(:hbx_enrollment, :with_enrollment_members, :with_product,
+                        family: family,
                         household: family.active_household,
                         aasm_state: "coverage_selected",
                         effective_on: initial_application.start_on,
