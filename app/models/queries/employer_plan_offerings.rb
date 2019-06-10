05ae23a3ca55ca6b01218968b6453694772044a1
nisanth <nisanth.yaganti@gmail.com>
2019-06-10 14:53:14 -0400

    fixing issues with loading dental plans

--- app/models/queries/employer_plan_offerings.rb
+++ app/models/queries/employer_plan_offerings.rb
@@ -11,7 +11,6 @@ module Queries
       end
     end
 
-    delegate :single_carrier_offered_health_plans, :metal_level_offered_health_plans, :single_option_offered_health_plans, :sole_source_offered_health_plans, :single_option_offered_dental_plans, :to => :strategy
-
+    delegate :single_carrier_offered_health_plans, :metal_level_offered_health_plans, :single_option_offered_health_plans, :sole_source_offered_health_plans, :single_option_offered_dental_plans, :custom_plan_option_offered_dental_plans, :single_carrier_offered_dental_plans, :dental_reference_plans_by_id, :to => :strategy
   end
 end
