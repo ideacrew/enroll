ac9fd9b041b91e7f9d197efd67871db63f7b9dd1
Nikhil S <nikhilks219@gmail.com>
2019-06-11 12:22:34 -0400

    fixed enrollment_details.html.erb_spec.rb

--- spec/views/employers/census_employees/enrollment_details.html.erb_spec.rb
+++ spec/views/employers/census_employees/enrollment_details.html.erb_spec.rb
@@ -6,7 +6,7 @@ describe "employers/census_employees/_enrollment_details.html.erb", dbclean: :af
   let(:family) { FactoryBot.create(:family, :with_primary_family_member)}
   let(:active_household) {family.active_household}
   let(:hbx_enrollment_member){ FactoryBot.build(:hbx_enrollment_member, is_subscriber:true, applicant_id: family.family_members.first.id, coverage_start_on: TimeKeeper.date_of_record, eligibility_date: TimeKeeper.date_of_record) }
-  let!(:enrollment) { FactoryBot.create(:hbx_enrollment,household: active_household, hbx_enrollment_members:[hbx_enrollment_member])}
+  let!(:enrollment) { FactoryBot.create(:hbx_enrollment, family: family, household: active_household, hbx_enrollment_members:[hbx_enrollment_member])}
   let(:member_enrollment) {BenefitSponsors::Enrollments::MemberEnrollment.new(member_id:hbx_enrollment_member.id,product_price:BigDecimal(100),sponsor_contribution:BigDecimal(100))}
   let(:decorated_hbx_enrollment) {double(member_enrollments:[member_enrollment], product_cost_total:'',sponsor_contribution_total:'')}
 
