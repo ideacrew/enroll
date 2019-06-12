9a36484880278a2e8b9bfbcf5b50673e889777ac
nisanth <nisanth.yaganti@gmail.com>
2019-06-12 17:44:58 -0400

    adding dummy & fixing specs

--- components/sponsored_benefits/spec/dummy/app/models/caches/plan_details.rb
+++ components/sponsored_benefits/spec/dummy/app/models/caches/plan_details.rb
@@ -3,5 +3,9 @@ module Caches
     def self.lookup_rate(plan_id, rate_schedule_date, effective_age)
       1
     end
+
+    def self.lookup_rate_with_area(plan_id, rate_schedule_date, effective_age, rating_area)
+      1
+    end
   end
 end
