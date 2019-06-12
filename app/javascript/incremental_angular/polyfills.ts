a522c1e748b4b763beca3cb7ff20ebd2121f3226
Trey Evans <tevans@Just-Plain-Awful.local>
2019-06-12 11:37:00 -0400

    Webpack compiles  again.  Webpacker still broken.

--- app/javascript/incremental_angular/polyfills.ts
+++ app/javascript/incremental_angular/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-import 'core-js/es7/reflect';
+import 'core-js/proposals/reflect-metadata';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
