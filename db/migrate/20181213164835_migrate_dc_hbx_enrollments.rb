ef5e5d4d81ae5f49e8699d6710271377e8c24f8d
saimekala07 <saimekala07@gmail.com>
2019-06-13 14:30:02 -0400

    Updating enrollment migration.

--- db/migrate/20181213164835_migrate_dc_hbx_enrollments.rb
+++ db/migrate/20181213164835_migrate_dc_hbx_enrollments.rb
@@ -17,7 +17,6 @@ class MigrateDcHbxEnrollments < Mongoid::Migration
 
   end
 
-
   private
 
   def self.update_hbx_enrollments
@@ -28,9 +27,13 @@ class MigrateDcHbxEnrollments < Mongoid::Migration
       BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(:'benefit_applications'.exists=>true).each do |bs|
         bs.benefit_applications.each do |ba|
           ba.benefit_groups.each do |bg|
+            plan_year = PlanYear.find(ba.id)
+            raise "Plan year not found" unless plan_year.present?
+            old_bg =  plan_year.benefit_groups.unscoped.select{|b| b.title == bg.title}
+            raise "Issue with benefit group" unless bg.present? || bg.count > 1
             bg_hash = {}
-            bg_hash[bg.id] = {'benefit_sponsorship_id' => bs.id,'health_sponsored_benefit_id' => bg.health_sponsored_benefit.id, "rating_area_id" => ba.recorded_rating_area_id}
-            bg_hash[bg.id].merge!({'dental_sponsored_benefit_id' => bg.dental_sponsored_benefit.id }) if bg.dental_sponsored_benefit.present?
+            bg_hash[old_bg.first.id] = {'benefit_package_id' => bg.id, 'benefit_sponsorship_id' => bs.id,'health_sponsored_benefit_id' => bg.health_sponsored_benefit.id, "rating_area_id" => ba.recorded_rating_area_id}
+            bg_hash[old_bg.first.id].merge!({'dental_sponsored_benefit_id' => bg.dental_sponsored_benefit.id }) if bg.dental_sponsored_benefit.present?
             @benefit_app_hash.merge!(bg_hash)
           end
         end
@@ -66,84 +69,85 @@ class MigrateDcHbxEnrollments < Mongoid::Migration
       end
     end
 
-    say_with_time("Time taken to shop enrollments") do
-
-      ce_count = CensusEmployee.all.exists(benefit_group_assignments: true).count
+    say_with_time("Time taken to migrate enrollments") do
+      f_count = Family.where(:'households.hbx_enrollments'.exists=>true).count
       offset_count = 0
       limit_count = 1000
+      ivl_plan_hash = @plan_to_product_hash[:aca_individual]
       fehb_plan_hash = @plan_to_product_hash[:fehb]
       shop_plan_hash = @plan_to_product_hash[:aca_shop]
 
-      while (offset_count <= ce_count) do
+      while (offset_count <= f_count) do
         puts "offset_count: #{offset_count}"
-        CensusEmployee.all.exists(benefit_group_assignments: true).limit(limit_count).offset(offset_count).each do |ce|
+        Family.where(:'households.hbx_enrollments'.exists=>true).limit(limit_count).offset(offset_count).each do |fam|
           begin
-            ce.benefit_group_assignments.each do |bga|
-              benefit_package_id = bga.benefit_package_id
-              get_hbx_enrollments(bga).each do |hbx|
+            fam.active_household.hbx_enrollments.each do |hbx|
+              next if (hbx.shopping? || hbx.inactive? ||hbx.renewing_waived?)
+              next if hbx.coverage_canceled? && hbx.plan_id.blank?
 
-                plan_hash = hbx.benefit_group.is_congress ? fehb_plan_hash : shop_plan_hash
+              if hbx.benefit_group_id.present?
+                plan_hash = shop_plan_hash
                 product_data = plan_hash[hbx.plan_id]
-                benefit_app_data = @benefit_app_hash[benefit_package_id]
+                benefit_app_data = @benefit_app_hash[hbx.benefit_group_id]
 
                 if benefit_app_data && product_data
                   hbx.update_attributes(
                       product_id: product_data['product_id'],
                       issuer_profile_id: product_data['carrier_profile_id'],
                       benefit_sponsorship_id: benefit_app_data['benefit_sponsorship_id'],
-                      sponsored_benefit_package_id: benefit_package_id,
+                      sponsored_benefit_package_id: benefit_app_data['benefit_package_id'],
                       sponsored_benefit_id: hbx.coverage_kind == "health" ? benefit_app_data['health_sponsored_benefit_id'] : benefit_app_data['dental_sponsored_benefit_id'],
                       rating_area_id: benefit_app_data["rating_area_id"]
                   )
                   print '.' unless Rails.env.test?
-                elsif !(hbx.shopping? ||hbx.coverage_canceled? || hbx.renewing_waived?)
+                else
                   print 'F' unless Rails.env.test?
-                  @logger.error "benefit application reference not found enrollment: #{hbx.hbx_id} -- #{hbx.employer_profile.legal_name}"
+                  @logger.error "benefit application reference not found enrollment: #{hbx.hbx_id} --- #{hbx.aasm_state} --- #{hbx.try(:employer_profile).try(:hbx_id)} --- #{hbx.try(:employer_profile).try(:legal_name)}"
+                end
+              else
+                product_data = ivl_plan_hash[hbx.plan_id]
+                if product_data
+                  hbx.update_attributes(
+                      product_id: product_data['product_id'],
+                      issuer_profile_id: product_data['carrier_profile_id']
+                  )
+                  print '.' unless Rails.env.test?
+                else
+                  print 'F' unless Rails.env.test?
+                  @logger.error "IVL enrollment reference not found hbx_id: #{hbx.hbx_id}---#{hbx.aasm_state}"
                 end
               end
             end
           rescue => e
             print 'F' unless Rails.env.test?
-            @logger.error "Update failed for Shop HBX Enrollment Census Employee id: #{ce.employer_profile.legal_name},
+            @logger.error "Update failed for enrollment family id: #{fam.id},
             #{e.message}" unless Rails.env.test?
           end
         end
         offset_count += limit_count
       end
-    end
 
-    say_with_time("Time taken to IVL enrollments") do
-      f_count = Family.where(:'households.hbx_enrollments'.exists=>true, :"households.hbx_enrollments.kind".nin=> ['employer_sponsored', 'employer_sponsored_cobra']).count
-      offset_count = 0
-      limit_count = 1000
-      ivl_plan_hash = @plan_to_product_hash[:aca_individual]
-      kind = %w(individual coverall unassisted_qhp insurance_assisted_qhp streamlined_medicaid emergency_medicaid hcr_chip)
-
-      while (offset_count <= f_count) do
-        puts "offset_count: #{offset_count}"
-        Family.where(:'households.hbx_enrollments'.exists=>true, :"households.hbx_enrollments.kind".in=> kind).limit(limit_count).offset(offset_count).each do |fam|
-          begin
-            fam.active_household.hbx_enrollments.each do |hbx|
-              next if hbx.is_shop?
-              product_data = ivl_plan_hash[hbx.plan_id]
+      # update congress product id
+      BenefitSponsors::Organizations::Organization.where(:"profiles._type" => /.*FehbEmployerProfile$/).each do |org|
+        org.active_benefit_sponsorship.census_employees.unscoped.each do |ce|
+          ce.benefit_group_assignments.each do |bga|
+            get_hbx_enrollments(bga).each do |hbx|
+              next if (hbx.shopping? || hbx.inactive? ||hbx.renewing_waived?)
+              next if hbx.coverage_canceled? && hbx.plan_id.blank?
+              product_data = fehb_plan_hash[hbx.plan_id]
               if product_data
                 hbx.update_attributes(
                     product_id: product_data['product_id'],
                     issuer_profile_id: product_data['carrier_profile_id']
                 )
                 print '.' unless Rails.env.test?
-              elsif !(hbx.shopping? ||hbx.coverage_canceled? || hbx.renewing_waived?)
+              else
                 print 'F' unless Rails.env.test?
-                @logger.error "IVL enrollment reference not found hbx_id: #{hbx.hbx_id}"
+                @logger.error "Plan id not found: #{hbx.hbx_id}---#{hbx.aasm_state}"
               end
             end
-          rescue => e
-            print 'F' unless Rails.env.test?
-            @logger.error "Update failed for IVL HBX Enrollment family id: #{fam.id},
-            #{e.message}" unless Rails.env.test?
           end
         end
-        offset_count += limit_count
       end
     end
     reset_hash
