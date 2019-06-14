3083930a2206ac856798ab54f9a8b23c4c0ad591
rabbittracey <traceyw@vt.edu>
2019-06-14 12:18:40 -0400

    fix rspec

--- app/data_migrations/change_hbx_id.rb
+++ app/data_migrations/change_hbx_id.rb
@@ -8,7 +8,7 @@ class ChangeHbxId< MongoidMigrationTask
       hbx_id = ENV['hbx_id']
       if ENV['new_hbx_id'].present? 
         new_hbx_id = ENV['new_hbx_id']
-      elsif action = "change_person_hbx"
+      elsif action == "change_person_hbx"
         new_hbx_id = HbxIdGenerator.generate_member_id
       else
         new_hbx_id = HbxIdGenerator.generate_organization_id
@@ -20,22 +20,35 @@ class ChangeHbxId< MongoidMigrationTask
       when "change_organization_hbx"
         change_organization_hbx(hbx_id,new_hbx_id)
       end
-      
-      def change_organization_hbx(hbx_id,new_hbx_id)
-        organization = Organization.where(hbx_id: hbx_id).first
-        if organization.nil?
-          puts "No organization was found by the given hbx_id: #{hbx_id}" unless Rails.env.test?
-        elsif !new_hbx_id.present?
-          puts "No new hbx id is provided" unless Rails.env.test?
-        else
-          organization.update_attributes(hbx_id: new_hbx_id)
-          puts "Change Hbx Id: #{hbx_id} to #{new_hbx_id} " unless Rails.env.test?
-        end
-      end
     rescue => e
       puts "#{e}"
     end
   end
+  
+  def change_person_hbx(hbx_id,new_hbx_id)
+    person = Person.where(hbx_id: hbx_id).first
+    if person.nil?
+      puts "No person was found by the given hbx_id: #{hbx_id}" unless Rails.env.test?
+    elsif !new_hbx_id.present?
+      puts "No new hbx id is provided" unless Rails.env.test?
+    else
+      person.update_attributes(hbx_id: new_hbx_id)
+      puts "Change Hbx Id: #{hbx_id} to #{new_hbx_id} " unless Rails.env.test?
+    end
+  end
+
+  def change_organization_hbx(hbx_id,new_hbx_id)
+    organization = Organization.where(hbx_id: hbx_id).first
+    if organization.nil?
+      puts "No organization was found by the given hbx_id: #{hbx_id}" unless Rails.env.test?
+    elsif !new_hbx_id.present?
+      puts "No new hbx id is provided" unless Rails.env.test?
+    else
+      organization.update_attributes(hbx_id: new_hbx_id)
+      puts "Change Hbx Id: #{hbx_id} to #{new_hbx_id} " unless Rails.env.test?
+    end
+  end
+
 end
 
 
