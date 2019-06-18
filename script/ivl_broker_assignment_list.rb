0cbc68b30330b35021be59ec2c5bd01b7e6614ff
rabbittracey <traceyw@vt.edu>
2019-06-18 14:43:47 -0400

    modify the rake to generate ivl broker list

--- script/ivl_broker_assignment_list.rb
+++ script/ivl_broker_assignment_list.rb
@@ -3,9 +3,10 @@
   batch_size = 500
   offset = 0
 
-  ppl_count = Person.all_consumer_roles.count
+  ppl_count = Family.exists("broker_agency_accounts" =>true).all.size
   field_names  = %w(
-             Full_Name
+             First_Name
+             Last_Name
              Person_Hbxid
              Broker_Name
              Broker_NPN
@@ -13,28 +14,41 @@
              Broker_Assignment_end
              )
 
-  processed_count = 0
+  @processed_count = 0
   file_name = "#{Rails.root}/ivl_broker_assignment_report_#{TimeKeeper.date_of_record.strftime("%m_%d_%Y")}.csv"
   date_range = Date.new(2016,1,1)..TimeKeeper.date_of_record
   CSV.open(file_name, "w", force_quotes: true) do |csv|
     csv << field_names
     while offset <= ppl_count
-      Person.all_consumer_roles.offset(offset).limit(batch_size).each do |person|
-        next if person.primary_family.nil? || person.primary_family.broker_agency_accounts.empty?
-        person.primary_family.broker_agency_accounts.each do |ba_account|
-          next if ba_account.nil? || ba_account.broker_agency_profile.nil? || ba_account.broker_agency_profile.primary_broker_role.nil?
-          # csv << [
-          #   person.full_name ,
-          #   person.hbx_id,
-          #   ba_account.broker_agency_profile.primary_broker_role.person.full_name,
-          #   ba_account.broker_agency_profile.primary_broker_role.npn,
-          #   ba_account.start_on,
-          #   ba_account.end_on
-          # ]
-          puts person.full_name if !person.full_name.nil?
-          puts person.id
-        end
-        processed_count += 1
+      Family.exists("broker_agency_accounts" =>true).all.offset(offset).limit(batch_size).each do |family|
+        next if family.primary_person.nil? 
+        primary_person = family.primary_person
+        history = family.broker_agency_accounts.map{|a| [a.broker_agency_profile.primary_broker_role.person.full_name,a.broker_agency_profile.primary_broker_role.npn,a.start_on,a.end_on]}
+
+        csv << [
+            primary_person.first_name,
+            primary_person.last_name,
+            primary_person.hbx_id,
+            history
+          ]
+
+
+        # family.broker_agency_accounts.each do |ba_account|
+        #   next if ba_account.nil? || ba_account.broker_agency_profile.nil? || ba_account.broker_agency_profile.primary_broker_role.nil?
+        #   
+        #   broker_role =  ba_account.broker_agency_profile.primary_broker_role
+        #   csv << [
+        #     primary_person.first_name,
+        #     primary_person.last_name,
+        #     primary_person.hbx_id,
+        #     broker_role.person.full_name,
+        #     broker_role.npn,
+        #     ba_account.start_on,
+        #     ba_account.end_on
+        #   ]
+        #   puts @processed_count if @processed_count % 100 ==0
+        # end
+        @processed_count += 1
       end
       offset = offset + batch_size
     end
