ddfefbaa6d387381b534a2980ac8268e1d6e72d5
utkarsh7989 <utkarsh7989@gmail.com>
2019-06-19 14:55:11 -0400

    update specs for invitation controller

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -35,22 +35,46 @@ RSpec.describe InvitationsController do
     end
 
     describe 'with a valid invitation for already existing user' do
-      let(:user) { FactoryBot.create(:user) }
-      let(:person) { FactoryBot.create(:person, user: user) }
-      let(:broker_agency_profile) { FactoryBot.create(:broker_agency_profile) }
-      let!(:broker_agency_staff_role) { FactoryBot.create(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, aasm_state: 'active', person: person)}
-      let(:invitation) { Invitation.new }
-      let(:params) { {id: invitation.id, person_id: person.id }}
+      context "broker invitation" do
+        let(:user) { FactoryBot.create(:user) }
+        let(:person) { FactoryBot.create(:person, user: user) }
+        let(:broker_agency_profile) { FactoryBot.create(:broker_agency_profile) }
+        let!(:broker_agency_staff_role) { FactoryBot.create(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, aasm_state: 'active', person: person)}
+        let(:invitation) { FactoryBot.create(:invitation, :broker_role, :source_id => broker_agency_profile.id)}
+        let(:params) { {id: invitation.id, person_id: person.id }}
 
-      before(:each) do
-        sign_in(user)
-        allow(Invitation).to receive(:find).with(invitation.id).and_return(invitation)
+        before(:each) do
+          sign_in(user)
+          allow(Invitation).to receive(:find).with(invitation.id).and_return(invitation)
+        end
+
+        it 'should redirect to sign in page if already person with user record is present' do
+          invitation.source_id = broker_agency_staff_role.id
+          get :claim, params: params
+          expect(response).to redirect_to(new_user_session_url(:invitation_id => params[:id]))
+        end
       end
 
-      it 'should redirect to sign in page if already person with user record is present' do
-        invitation.source_id = broker_agency_staff_role.id
-        get :claim, params: params
-        expect(response).to redirect_to(new_user_session_url(:invitation_id => params[:id]))
+      context "general agency invitation" do
+        let(:site) { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+        let!(:general_agency_organization) { FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_general_agency_profile, site: site) }
+        let(:general_agency_profile) { general_agency_organization.general_agency_profile }
+        let!(:general_agency_staff_role) { FactoryBot.create(:general_agency_staff_role, benefit_sponsors_general_agency_profile_id: general_agency_profile.id, aasm_state: 'active', person: ga_person)}
+        let(:ga_user) { FactoryBot.create(:user) }
+        let(:ga_person) { FactoryBot.create(:person, user: ga_user) }
+        let(:invitation) { FactoryBot.create(:invitation, :general_agency_staff_role, :source_id => general_agency_profile.id)}
+        let(:params) { {id: invitation.id, person_id: ga_person.id }}
+
+        before(:each) do
+          sign_in(user)
+          allow(Invitation).to receive(:find).with(invitation.id).and_return(invitation)
+        end
+
+        it 'should redirect to sign in page if already person with user record is present' do
+          invitation.source_id = general_agency_staff_role.id
+          get :claim, params: params
+          expect(response).to redirect_to(new_user_session_url(:invitation_id => params[:id]))
+        end
       end
     end
 
