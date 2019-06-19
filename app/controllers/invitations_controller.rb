95166c81140a37c94d0e5bd0099bdd1f43f641d2
utkarsh7989 <utkarsh7989@gmail.com>
2019-06-19 11:02:22 -0400

    ref:41638 updates invitation url for new ga staff

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -46,12 +46,22 @@ class InvitationsController < ApplicationController
     person = Person.find(params_hash[:person_id]) if params_hash[:person_id]
     if person&.user
       invitation = Invitation.find(params_hash[:id])
-      staff_role = BrokerAgencyStaffRole.find(invitation.source_id)
-      session[:portal] = benefit_sponsors.profiles_broker_agencies_broker_agency_profile_path(staff_role.benefit_sponsors_broker_agency_profile_id)
+      update_session_portal(invitation)
       redirect_to new_user_session_url(:invitation_id => params_hash[:id])
     elsif current_user.nil?
       session[:portal] = url_for(params_hash)
       redirect_to new_user_registration_url(:invitation_id => params_hash[:id])
     end
   end
+
+  def update_session_portal(invitation)
+    if invitation.role == "general_agency_staff_role"
+      staff_role = GeneralAgencyStaffRole.find(invitation.source_id)
+      session[:portal] = benefit_sponsors.profiles_general_agencies_general_agency_profile_path(staff_role.benefit_sponsors_general_agency_profile_id)
+    else
+      staff_role = BrokerAgencyStaffRole.find(invitation.source_id)
+      session[:portal] = benefit_sponsors.profiles_broker_agencies_broker_agency_profile_path(staff_role.benefit_sponsors_broker_agency_profile_id)
+    end
+  end
+
 end
