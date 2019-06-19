79001f36ff61026ed0edfb8df6ae45634c0b7907
Zachary Landes <zach.g.landes@gmail.com>
2019-06-19 13:14:48 -0400

    adding custom qle form
    updating custom qle answers
    
    answer type form not coming through
    
    qle
    
    name space correction
    
    updating qle creation form
    
    adding stimulus controller
    
    modified manage family UI
    
    Adds the navlinks to the new manage qle page but commented out for pundit debugging
    
    Improve QLE Form, add nesting with fields_for
    
    Adds panel style to new nanage qle page
    
    Adds a navbar to the manage qles, fixes the collection selects, and adds some more styling
    
    Removes unnecessary pundit files
    
    Cleans up the UI to make the form a little more clear
    
    Adds code to form and controller action to easily redirect to proper action
    
    sets up some more qles infrastructure
    
    Creates qles controller spec
    
    moving and renaming partials
    
    updating table to divs for new qle form
    
    cleaning up question form
    
    formatting qle form
    
    Commiting stuff
    
    updating qle controller to remove getelementbyid
    
    working on date response
    
    Adds qle forms service and other things
    
    Creates passing qle form spec
    
    Updates the controller spec

---
+++ app/models/custom_qle_question.rb
@@ -0,0 +1,11 @@
+class CustomQleQuestion
+  include Mongoid::Document
+
+  embedded_in :qualifying_life_event_kind 
+  embeds_one :custom_qle_answer
+
+  field :content, type: String
+  field :type, type: String
+
+  accepts_nested_attributes_for :custom_qle_answer
+end
