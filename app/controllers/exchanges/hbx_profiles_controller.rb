15c585dbb78cdc10c063419cb2d6f4c9f07996d8
Zachary Landes <zach.g.landes@gmail.com>
2019-06-20 14:36:09 -0400

    updating qle creation form

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -502,24 +502,26 @@ def employer_poc
     end
   end
 
-
-  # def custom_qle
-  #   @qle = QualifyingLifeEventKind.new
-  #   @question = CustomQleQuestion.new(qualifying_life_event_kind: @qle)
-  #   @answer ||=  @question.try(:custom_qle_answers).try(:new) || CustomQleAnswer.new
-  #   create_qle_question(params['custom_qle_question']['content'],@qle) if params['custom_qle_question'].present?
-  #   update_qle_answer if params['custom_qle_answer'].present?
-  # end
+  def custom_qle
+    @qle = QualifyingLifeEventKind.new
+    @question ||= CustomQleQuestion.new(qualifying_life_event_kind: @qle)
+    @answer ||= CustomQleAnswer.new(custom_qle_question: @question)
+    # create_qle_answer(params['custom_qle_question']['content']) if params['answer'].present?
+    # find_or_create_qle_question(params['custom_qle_question']['content'],@qle) if params['custom_qle_question'].present?
+  end
   
-  def create_qle_answer 
+  def create_qle_answer(params)
+    binding.pry
+    @question = CustomQleQuestion.where(content: params).first
     @answer = @question.custom_qle_answers.create(type: params['answer']['type'])
   end
 
   def update_qle_answer
   end
   
-  def create_qle_question(params, qle) 
-    @question = CustomQleQuestion.create(qualifying_life_event_kind: qle, content: params) 
+  def find_or_create_qle_question(params, qle) 
+    binding.pry
+    @question = CustomQleQuestion.find_or_create_by(content: params, qualifying_life_event_kind: qle)
   end
 
   def edit_dob_ssn
