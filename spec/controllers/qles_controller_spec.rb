880fccfe94c4e66756dc771e03b28da6b482b3d8
zasman <zanesandin@gmail.com>
2019-06-24 17:18:57 -0400

    Creates qles controller spec

---
+++ spec/controllers/qles_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe QlesController, :type => :controller do
+  describe "GET #new_manage_qle" do
+    it "successfully renders the new manage qle form" do
+
+    end
+  end
+
+  describe "POST #create_manage_qle" do
+    context "successfully redirects to" do
+      it "new" do
+      end
+
+      it "modify" do
+      end
+
+      it "deactivate" do
+
+      end
+    end
+  end
+end
