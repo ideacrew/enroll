8aa26874379f69ba9da09a27d8644668b047a98a
Kelley Jenkins <ckjenkins3@gmail.com>
2019-06-25 09:42:53 -0400

    Adds methods to check contact preference for employee and employer

--- components/benefit_sponsors/app/models/benefit_sponsors/organizations/profile.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/organizations/profile.rb
@@ -112,6 +112,14 @@ module BenefitSponsors
       def invoices
         documents.select{ |document| ["invoice", "initial_invoice"].include? document.subject }
       end
+      
+      def can_receive_paper_communication?
+        [:paper_only, :paper_and_electronic].include?(contact_method)
+      end
+      
+      def can_receive_electronic_communication?
+        [:electronic_only, :paper_and_electronic].include?(contact_method)
+      end
 
       class << self
         def find(id)
