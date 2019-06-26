0e1695a1fd33658ecd19daa0d94b2539462b35c9
Kelley Jenkins <ckjenkins3@gmail.com>
2019-06-26 10:25:13 -0400

    adds specs for changes notice_builder

--- components/notifier/spec/models/notifier/notice_builder_spec.rb
+++ components/notifier/spec/models/notifier/notice_builder_spec.rb
@@ -2,50 +2,67 @@ require 'rails_helper'
 
 module Notifier
   module NoticeBuilder
-    
-  RSpec.describe "delivery", dbclean: :around_each do
+    class DummyNoticeKind
+      attr_accessor :title, :event_name, :resource, :market_kind
+      include Notifier::NoticeBuilder
+
+      def initialize(params)
+        self.event_name = params[:event_name]
+        self.title = params[:title]
+        self.market_kind = params[:market_kind]
+      end
+    end
+
+    RSpec.describe NoticeBuilder, dbclean: :around_each do
       let!(:site)            { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, Settings.site.key) }
       let!(:model_instance)     { FactoryBot.build(:benefit_sponsors_organizations_general_organization, "with_aca_shop_#{Settings.site.key}_employer_profile".to_sym, site: site) }
       let(:employer_profile)    { model_instance.employer_profile }
       let(:event_name) {"acapi.info.events.employer.welcome_notice_to_employer"}
-      let(:subject) { Notifier::NoticeKind.new(event_name: event_name) }
-      before :each do 
-        allow(subject).to receive(:resource).and_return(employer_profile)
+      let(:resource) {model_instance.employer_profile }
+      let(:payload) do
+        {
+          "employer_id" => employer_profile.id.to_s,
+          "event_object_kind" => "BenefitSponsors::Organizations::AcaShop#{Settings.site.key.capitalize}EmployerProfile",
+          "event_object_id" => employer_profile.id
+        }
       end
-      
-    describe ".store_paper_notice" do
-      
-      it "should have contact method preferences" do
-        expect(subject.has_contact_method?).to be true
+      let(:subject) do
+        Notifier::NoticeBuilder::DummyNoticeKind.new(event_name: event_name, title: 'Test')
       end
-      
-      it 'should have a paper related contact method' do
-        expect(subject.resource.can_receive_paper_communication?).to be true
-      end
-      
-      it 'should save to AWS' do
-        subject.store_paper_notice
-        #failing because needs @resource is nil
-        # expect.....
-      end
-    end 
     
-    describe ".send_generic_notice_alert" do
+      describe ".store_paper_notice" do
+        let(:bucket_name) { 'paper-notices' }
+        let(:notice_filename_for_paper_notice) { "#{employer_profile.id}_Test.pdf" }
+        let(:doc_uri) { "urn:openhbx:terms:v1:file_storage:s3:bucket:#{bucket_name}#sample-key" }
+        let(:notice_path_for_paper_notice) { Rails.root.join("tmp", notice_filename_for_paper_notice) }
 
-      it 'should send generic notice alert if electornic preference' do
-        expect(subject.resource.can_receive_electronic_communication?).to be true
+        before do
+          allow(FileUtils).to receive(:cp)
+          allow(File).to receive(:delete)
+          allow(subject).to receive(:resource).and_return(resource)
+          allow(subject).to receive(:notice_path).and_return("notice_path")
+          allow(Aws::S3Storage).to receive(:save).and_return(doc_uri)
+          subject.store_paper_notice
+        end
+
+        it 'AWS Storage to save doc_uri' do
+          expect(Aws::S3Storage).to have_received(:save).with(notice_path_for_paper_notice, bucket_name, notice_filename_for_paper_notice)
+        end
       end
-      
-      it "should have contact method preferences" do
-        expect(subject.has_contact_method?).to be true
+
+      describe ".send_generic_notice_alert" do
+        let(:usermailer) {double "UserMailer"}
+
+        before do
+          allow(UserMailer).to receive(:generic_notice_alert).and_return(usermailer)
+          allow(usermailer).to receive(:deliver_now).and_return(true)
+          subject.send_generic_notice_alert
+        end
+
+        it "should receive send_generic_notice_alert" do
+          expect(UserMailer).to have_received(:generic_notice_alert)
+        end
       end
-      
-      it 'should mail notice' do
-        subject.send_generic_notice_alert 
-        #failing because needs @resource is nil
-        
-      end 
-    end 
-  end
+    end
   end
 end
