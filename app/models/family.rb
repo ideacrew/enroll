5dc26671514a1bb21d400d394c82f67037e8a225
Vishal Kalletla <vishuk1201@gmail.com>
2019-06-28 13:09:57 -0400

    41609: code refactor

--- app/models/family.rb
+++ app/models/family.rb
@@ -762,7 +762,7 @@ class Family
     # Instead log missing new broker profile scenario and avoid the hire of broker by family.
     if broker_role.benefit_sponsors_broker_agency_profile_id.present?
       broker_agency_profile_id = broker_role.benefit_sponsors_broker_agency_profile_id
-    else
+    elsif Rails.env.production?
       logger = Logger.new("#{Rails.root}/log/family_hire_broker_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log")
       logger.info "Unable to find benefit sponsor broker profile for broker role,
                     broker_role_id: #{broker_role_id},
@@ -772,8 +772,7 @@ class Family
 
     terminate_broker_agency if existing_agency
     start_on = Time.now
-    broker_agency_account =  BenefitSponsors::Accounts::BrokerAgencyAccount.new(benefit_sponsors_broker_agency_profile_id: broker_agency_profile_id, writing_agent_id: broker_role_id, start_on: start_on, is_active: true)
-    broker_agency_accounts.push(broker_agency_account)
+    broker_agency_accounts.new(benefit_sponsors_broker_agency_profile_id: broker_agency_profile_id, writing_agent_id: broker_role_id, start_on: start_on, is_active: true)
     self.save
   end
 
