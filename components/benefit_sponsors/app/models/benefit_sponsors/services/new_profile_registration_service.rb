9a23c2fe085938fb1596440eddbd8149cc2abffa
harshaellanki <h_vardhan@yahoo.com>
2019-06-29 16:51:48 -0400

    ref:42836/42860 validating open enrollment end date while publishing plan year

--- components/benefit_sponsors/app/models/benefit_sponsors/services/new_profile_registration_service.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/services/new_profile_registration_service.rb
@@ -63,7 +63,7 @@ module BenefitSponsors
       end
 
       def load_organization_form(form)
-        form.organization.entity_kind_options = BenefitSponsors::Organizations::Organization::ENTITY_KINDS
+        form.organization.entity_kind_options = BenefitSponsors::Organizations::Organization::DC_ENTITY_KINDS
         form
       end
 
