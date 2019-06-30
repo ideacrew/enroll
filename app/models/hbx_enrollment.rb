c32af2b4203096260fd5c913a0992cfbda6c5f10
Raghuram Ganjala <raghuramg83@gmail.com>
2019-06-30 11:28:31 -0400

    Refs #42883 Treat void state as canceled

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -28,8 +28,8 @@ class HbxEnrollment
 
   ENROLLED_STATUSES   = %w[coverage_selected transmitted_to_carrier coverage_enrolled coverage_termination_pending unverified].freeze
   SELECTED_AND_WAIVED = %w(coverage_selected inactive)
-  TERMINATED_STATUSES = %w(coverage_terminated unverified coverage_expired void)
-  CANCELED_STATUSES   = %w(coverage_canceled)
+  TERMINATED_STATUSES = %w[coverage_terminated unverified coverage_expired].freeze
+  CANCELED_STATUSES   = %w[coverage_canceled void].freeze # Void state enrollments are invalid enrollments. will be treated same as canceled.
   RENEWAL_STATUSES    = %w(auto_renewing renewing_coverage_selected renewing_transmitted_to_carrier renewing_coverage_enrolled
                               auto_renewing_contingent renewing_contingent_selected renewing_contingent_transmitted_to_carrier
                               renewing_contingent_enrolled
