6b44b4e1ffdb0fca6d24aae9f52ab4c3f7d13a6f
zasman <zanesandin@gmail.com>
2019-07-01 15:03:58 -0400

    Continued deactivation form progress

--- app/javascript/incremental_angular/app/admin/qle_kinds/qle_kind_deactivation_form.component.ts
+++ app/javascript/incremental_angular/app/admin/qle_kinds/qle_kind_deactivation_form.component.ts
@@ -12,6 +12,12 @@ export class QleKindDeactivationFormComponent {
   constructor(injector: Injector, private _elementRef : ElementRef) {
 
   }
+  public hasErrors(control : AbstractControl) : Boolean {
+    return ((control.touched || control.dirty) && !control.valid);
+  }
+  public errorClassFor(control : AbstractControl) : String {
+    return (this.hasErrors(control) ? " has-error" : "");
+  }
   ngOnInit() {
     var qleKindToDeactivateJson = (<HTMLElement>this._elementRef.nativeElement).getAttribute("data-qle-kind-to-deactivate");
     if (qleKindToDeactivateJson != null) {
