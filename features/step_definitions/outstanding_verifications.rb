771fa5f4a41fa058d2445f0b7c3149e12d2cde60
Vishal Kalletla <vishuk1201@gmail.com>
2019-07-01 16:56:02 -0400

    cucumber fix for outstanding_verifications

--- features/step_definitions/outstanding_verifications.rb
+++ features/step_definitions/outstanding_verifications.rb
@@ -3,20 +3,24 @@ Given(/^oustanding verfications users exists$/) do
   @person_name = person.full_name
   person.consumer_role.update_attributes!(aasm_state: "verification_outstanding")
   family = FactoryBot.create(:family, :with_primary_family_member, person: person)
-  FactoryBot.create(:hbx_enrollment, :with_enrollment_members,
-                    :household => family.active_household,
-                    :aasm_state => "coverage_selected",
-                    :is_any_enrollment_member_outstanding => true,
-                    :kind => "individual",
-                    :effective_on => TimeKeeper.date_of_record.beginning_of_year)
+  issuer_profile = FactoryBot.create(:benefit_sponsors_organizations_issuer_profile)
+  product = FactoryBot.create(:benefit_markets_products_health_products_health_product, benefit_market_kind: 'aca_individual', issuer_profile: issuer_profile)
+  enrollment = FactoryBot.create(:hbx_enrollment, :with_enrollment_members,
+                                 :household => family.active_household,
+                                 :aasm_state => 'coverage_selected',
+                                 :is_any_enrollment_member_outstanding => true,
+                                 :kind => 'individual',
+                                 :product => product,
+                                 :effective_on => TimeKeeper.date_of_record.beginning_of_year)
   FactoryBot.create(:hbx_enrollment_member, applicant_id: family.primary_applicant.id, eligibility_date: (TimeKeeper.date_of_record - 2.months), hbx_enrollment: enrollment)
   enrollment.save!
   Family.by_enrollment_individual_market.where(:'households.hbx_enrollments.is_any_enrollment_member_outstanding' => true)
 end
 
 When(/^Admin clicks Outstanding Verifications$/) do
-  page.find('.families.dropdown-toggle.interaction-click-control-families').click
-  page.find('.interaction-click-control-outstanding-verifications').trigger('click')
+  visit exchanges_hbx_profiles_root_path
+  find(:xpath, "//li[contains(., 'Families')]", :wait => 10).click
+  page.find('.interaction-click-control-outstanding-verifications').click
 end
 
 When(/^Admin clicks Families tab$/) do
