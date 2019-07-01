bcee0e0ba75248befe0ebfb4621d23a2044e3111
Kelley Jenkins <ckjenkins3@gmail.com>
2019-07-01 15:03:28 -0400

    adds functionality to update contact method for both ee and ivl when changed

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -205,6 +205,8 @@ class PeopleController < ApplicationController
           @person.consumer_role.check_for_critical_changes(@family, info_changed: @info_changed, no_dc_address: person_params["no_dc_address"], dc_status: @dc_status)
         end
         @person.consumer_role.update_attribute(:is_applying_coverage, person_params[:is_applying_coverage]) if @person.consumer_role.present?
+        # if dual role, this will update both ivl and ee
+        @person.active_employee_roles.each { |role| role.update_attributes(contact_method: person_params[:consumer_role_attributes][:contact_method]) } if @person.has_multiple_roles?
         format.html { redirect_to redirect_path, notice: 'Person was successfully updated.' }
         format.json { head :no_content }
       else
