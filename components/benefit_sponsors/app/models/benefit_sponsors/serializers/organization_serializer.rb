df93fa7e536355539642897a66a2a662a6c5d105
nisanth <nisanth.yaganti@gmail.com>
2019-07-02 14:13:44 -0400

    fixing ui issues for congressional employers

--- components/benefit_sponsors/app/models/benefit_sponsors/serializers/organization_serializer.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/serializers/organization_serializer.rb
@@ -2,11 +2,19 @@ module BenefitSponsors
   module Serializers
     class OrganizationSerializer < ActiveModel::Serializer
       attributes :legal_name, :dba, :entity_kind, :entity_kind_options
-      attribute :fein, if: :is_general_organization?
+      attribute :fein, if: :can_fein?
       attribute :entity_kind_options
+
+      def can_fein?
+        is_general_organization? || is_congress?
+      end
 
       def is_general_organization?
         object.is_a? BenefitSponsors::Organizations::GeneralOrganization
+      end
+
+      def is_congress?
+        object.is_a_fehb_profile?
       end
 
       def entity_kind_options
