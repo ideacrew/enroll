e457c4a8a94fabde79ff5fed669580c369e90498
Kelley Jenkins <ckjenkins3@gmail.com>
2019-07-02 14:54:59 -0400

    updates shop notice file names for AWS and report

--- components/notifier/spec/models/notifier/notice_builder_spec.rb
+++ components/notifier/spec/models/notifier/notice_builder_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 module Notifier
   module NoticeBuilder
     class DummyNoticeKind
-      attr_accessor :title, :event_name, :resource, :market_kind
+      attr_accessor :title, :event_name, :resource, :market_kind, :notice_number
       include Notifier::NoticeBuilder
 
       def initialize(params)
         self.event_name = params[:event_name]
         self.title = params[:title]
         self.market_kind = params[:market_kind]
+        self.notice_number = params[:notice_number]
       end
     end
 
@@ -21,18 +22,18 @@ module Notifier
       let(:resource) {model_instance.employer_profile }
       let(:payload) do
         {
-          "employer_id" => employer_profile.id.to_s,
+          "employer_id" => employer_profile.hbx_id.to_s,
           "event_object_kind" => "BenefitSponsors::Organizations::AcaShop#{Settings.site.key.capitalize}EmployerProfile",
           "event_object_id" => employer_profile.id
         }
       end
       let(:subject) do
-        Notifier::NoticeBuilder::DummyNoticeKind.new(event_name: event_name, title: 'Test')
+        Notifier::NoticeBuilder::DummyNoticeKind.new(event_name: event_name, title: 'Test', notice_number: "ABC_123")
       end
 
       describe ".store_paper_notice" do
         let(:bucket_name) { 'paper-notices' }
-        let(:notice_filename_for_paper_notice) { "#{employer_profile.organization.hbx_id}_Test.pdf" }
+        let(:notice_filename_for_paper_notice) { "#{employer_profile.organization.hbx_id}_#{subject.title}_#{subject.notice_number.gsub('_','')}_#{subject.notice_type}.pdf" }
         let(:doc_uri) { "urn:openhbx:terms:v1:file_storage:s3:bucket:#{bucket_name}#sample-key" }
         let(:notice_path_for_paper_notice) { Rails.root.join("tmp", notice_filename_for_paper_notice) }
 
@@ -41,6 +42,7 @@ module Notifier
           allow(File).to receive(:delete)
           allow(subject).to receive(:resource).and_return(resource)
           allow(subject).to receive(:notice_path).and_return("notice_path")
+          allow(subject).to receive(:notice_type).and_return("ER")
           allow(Aws::S3Storage).to receive(:save).and_return(doc_uri)
           subject.store_paper_notice
         end
