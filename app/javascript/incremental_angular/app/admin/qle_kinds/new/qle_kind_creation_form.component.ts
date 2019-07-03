59c494d2483ff4ac9eeac67c9db75172672ba8ac
Zachary Landes <zach.g.landes@gmail.com>
2019-07-03 15:19:13 -0400

    working on showing question title

--- app/javascript/incremental_angular/app/admin/qle_kinds/new/qle_kind_creation_form.component.ts
+++ app/javascript/incremental_angular/app/admin/qle_kinds/new/qle_kind_creation_form.component.ts
@@ -6,6 +6,7 @@ import { ErrorLocalizer } from '../../../error_localizer';
 import { ErrorMapper, ErrorResponse } from '../../../error_mapper';
 import { HttpResponse } from "@angular/common/http";
 import { QleKindQuestionFormComponent } from './qle_kind_question_form.component';
+import { __core_private_testing_placeholder__ } from '@angular/core/testing';
 
 
 @Component({
@@ -15,15 +16,21 @@ import { QleKindQuestionFormComponent } from './qle_kind_question_form.component
 export class QleKindCreationFormComponent {
   public qleKindToCreate : QleKindCreationResource | null = null;
   public creationFormGroup : FormGroup = new FormGroup({});
-
   public creationUri : string | "";
-  public showQuestion : boolean | false;
+  public showQuestionInput : boolean | true;
+  public showQuestionType : boolean | false;
+  public showQuestionTitle : boolean | false;
+  public showQuestionContainer : boolean | false;
+
+
+
   @ViewChild('headerRef') headerRef: ElementRef;
 
   constructor(
      injector: Injector,
      private _elementRef : ElementRef,
      private _creationForm: FormBuilder,
+
      @Inject("QleKindCreationService") private CreationService : QleKindCreationService,
      ) {
 
@@ -37,33 +44,42 @@ export class QleKindCreationFormComponent {
 
   ngOnInit() {
     var qleKindToCreateJson = (<HTMLElement>this._elementRef.nativeElement).getAttribute("data-qle-kind-create-url");
-        this.creationFormGroup.addControl(
-          "title",
-          new FormControl(""),
-        )
-        this.creationFormGroup.addControl(
-          "tool_tip",
-          new FormControl(""), 
-        )
-        this.creationFormGroup.addControl(
-          "action_kind",
-          new FormControl(""),  
-        )
-        this.creationFormGroup.addControl(
-          "reason",
-          new FormControl(""),  
-        )
-        this.creationFormGroup.addControl(
-          "market_kind",
-          new FormControl(""),  
-        )
-        this.creationFormGroup.addControl(
-          "is_self_attested",
-          new FormControl(""),  
-        )
-        questions: this._creationForm.array([
-          this.initQuestion(),
-      ]);
+        // this.creationFormGroup.addControl(
+        //   "title",
+        //   new FormControl(""),
+        // )
+        // this.creationFormGroup.addControl(
+        //   "tool_tip",
+        //   new FormControl(""), 
+        // )
+        // this.creationFormGroup.addControl(
+        //   "action_kind",
+        //   new FormControl(""),  
+        // )
+        // this.creationFormGroup.addControl(
+        //   "reason",
+        //   new FormControl(""),  
+        // )
+        // this.creationFormGroup.addControl(
+        //   "market_kind",
+        //   new FormControl(""),  
+        // )
+        // this.creationFormGroup.addControl(
+        //   "is_self_attested",
+        //   new FormControl(""),  
+        // )
+        this.creationFormGroup = this._creationForm.group({
+          title: ['', [Validators.required, Validators.minLength(1)]],
+          tool_tip: ['', [Validators.required, Validators.minLength(1)]],
+          action_kind: ['',[]],
+          reason: ['', [Validators.required, Validators.minLength(1)]],
+          market_kind: ['', [Validators.required, Validators.minLength(1)]],
+          is_self_attested: [''],
+          questions: this._creationForm.array([
+            this.initQuestion(),
+          ])
+        })
+      
     var submissionUriAttribute = (<HTMLElement>this._elementRef.nativeElement).getAttribute("data-qle-kind-creation-url");
       if (submissionUriAttribute != null) {
         this.creationUri = submissionUriAttribute;
@@ -72,38 +88,56 @@ export class QleKindCreationFormComponent {
 
   initQuestion(){
     return this._creationForm.group({
-      title: ['', Validators.required],
+      id: "",
+      questionTitle: ['', Validators.required],
+      questionType:[''],
     });
-  }
 
+  }
+  displayQuestionTitle(i:number){
+    var title =  this.creationFormGroup.value.questions[i].questionTitle;
+    console.log(title)
+  }
+  removeQuestionInput(){
+    return this.showQuestionInput = false
+  }
+  showQuestionTypes(){
+    this.removeQuestionInput()
+    return this.showQuestionType = true
+
+  }
   showQuestions(){
-    return this.showQuestion = true
+       this.showQuestionContainer = true
+    return this.showQuestionInput = true
+
   }
 
   addQuestion(){
     const control = <FormArray>this.creationFormGroup.controls['questions'];
     control.push(this.initQuestion());
+    console.log(control)
   }
 
   submitCreation() {
     var form = this;
     var errorMapper = new ErrorMapper();
+    
     if (this.creationFormGroup != null) {
       if (this.creationFormGroup.valid) {
-        var invocation = this.CreationService.submitCreation(this.creationUri, this.creationFormGroup.value);
-        invocation.subscribe(
-          function(data: HttpResponse<any>) {
-            var location_header = data.headers.get("Location");
-            if (location_header != null) {
-              window.location.href = location_header;
-            }
-          },
-          function(error) {
-            errorMapper.mapParentErrors(form.creationFormGroup, <ErrorResponse>(error.error.errors));
-            errorMapper.processErrors(form.creationFormGroup, <ErrorResponse>(error.error.errors));
-            form.headerRef.nativeElement.scrollIntoView();
-          }
-        )
+        // var invocation = this.CreationService.submitCreation(this.creationUri, this.creationFormGroup.value);
+        // invocation.subscribe(
+        //   function(data: HttpResponse<any>) {
+        //     var location_header = data.headers.get("Location");
+        //     if (location_header != null) {
+        //       window.location.href = location_header;
+        //     }
+        //   },
+        //   function(error) {
+        //     errorMapper.mapParentErrors(form.creationFormGroup, <ErrorResponse>(error.error.errors));
+        //     errorMapper.processErrors(form.creationFormGroup, <ErrorResponse>(error.error.errors));
+        //     form.headerRef.nativeElement.scrollIntoView();
+        //   }
+        // )
       }
     }
   }
