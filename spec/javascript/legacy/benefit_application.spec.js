441c921a1112c02a8d7272a306fdec6d5a8f5250
Trey Evans <tevans@Just-Plain-Awful.local>
2019-07-09 15:20:08 -0400

    Put in basic javascript jest test.
    This creates the basic javascript testing possible with jest.
    It can be used for our legacy javascript as we move it into webpacker.

---
+++ spec/javascript/legacy/benefit_application.spec.js
@@ -0,0 +1,6 @@
+import { calculateEmployeeCosts, calculateEmployerContributions } from 'legacy/benefit_application';
+
+test("exports the global methods", () => {
+  expect(calculateEmployeeCosts).not.toBe(null);
+  expect(calculateEmployerContributions).not.toBe(null);
+});
