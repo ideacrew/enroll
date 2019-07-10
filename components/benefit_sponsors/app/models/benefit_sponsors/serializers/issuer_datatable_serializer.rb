1257f718e92ac3639b5be716857d4e39997e2992
vineel <sonnathivinil@gmail.com>
2019-07-10 10:07:33 -0400

    Remove redundant code

--- components/benefit_sponsors/app/models/benefit_sponsors/serializers/issuer_datatable_serializer.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/serializers/issuer_datatable_serializer.rb
@@ -5,9 +5,7 @@ module BenefitSponsors
     class IssuerDatatableSerializer < ActiveModel::Serializer
       include FastJsonapi::ObjectSerializer
       extend ::ApplicationHelper
-      attributes :legal_name, :phone, :email, :working_hours
-
-      attribute(&:legal_name)
+      attributes :legal_name
 
       attribute :phone do
         'N/A'
