81caed330e08269faefcc8f4b40c5e59e97ed6b0
vineel <sonnathivinil@gmail.com>
2019-07-12 11:59:11 -0400

    products controller specs

--- spec/controllers/exchanges/issuers_controller_spec.rb
+++ spec/controllers/exchanges/issuers_controller_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Exchanges::IssuersController do
-  let!(:site)            { build(:benefit_sponsors_site, :with_owner_exempt_organization, :dc) }
-  let!(:issuer_profile)  { create_list(:benefit_sponsors_organizations_issuer_profile, 2, organization: site.owner_organization) }
-  let(:user)             { double("user", :has_hbx_staff_role? => true) }
+  let(:site)           { build(:benefit_sponsors_site, :with_owner_exempt_organization, :dc) }
+  let(:issuer_profile) { create_list(:benefit_sponsors_organizations_issuer_profile, 2, organization: site.owner_organization) }
+  let(:user)            { double("user", :has_hbx_staff_role? => true) }
 
   before(:each) { sign_in(user) }
 
