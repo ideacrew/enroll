b689e224bbfd6215dc5bf61b3ba6a76e94245709
zasman <zanesandin@gmail.com>
2019-07-17 14:06:54 -0400

    Adds update broker employer relationship script and a spec for it

---
+++ spec/script/update_broker_employer_relationship_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe "UpdateBrokerEmploymentRelationship" do
+  it "should invoke without errors" do
+    expect { system 'script/update_broker_employer_relationship.rb' }.to_not raise_error
+  end
+end
