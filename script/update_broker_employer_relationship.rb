aa496264df341db6d067f427c3b6cb2afa2e4a86
zasman <zanesandin@gmail.com>
2019-07-18 17:46:56 -0400

    Fixes rubocop issues

--- script/update_broker_employer_relationship.rb
+++ script/update_broker_employer_relationship.rb
@@ -2,7 +2,8 @@
 pdos = SponsoredBenefits::Organizations::PlanDesignOrganization.where(has_active_broker_relationship: false).all
 
 @count = 0
-feins = []
+# This was in the original PR but isn't used
+# feins = []
 pdos.each do |pdo|
   next unless Organization.where(:'employer_profile._id' => pdo.sponsor_profile_id, :'employer_profile.broker_agency_accounts' => {:$elemMatch => { is_active: true, broker_agency_profile_id: pdo.owner_profile_id }}).present?
     pdo.update_attributes(has_active_broker_relationship: true)
