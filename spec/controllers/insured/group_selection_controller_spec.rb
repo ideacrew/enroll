9e1b8ab7e2222a484a463e41e39284434b5fed2e
Josh <karperj@umd.edu>
2019-07-18 11:04:14 -0400

    refs#42808 improve spec coverage

--- spec/controllers/insured/group_selection_controller_spec.rb
+++ spec/controllers/insured/group_selection_controller_spec.rb
@@ -317,6 +317,40 @@ RSpec.describe Insured::GroupSelectionController, :type => :controller, dbclean:
         expect(response).to redirect_to(insured_plan_shopping_path(id: new_household.hbx_enrollments(true).first.id, change_plan: 'change', coverage_kind: 'health', market_kind: 'individual', enrollment_kind: 'sep'))
       end
 
+      context "IVL edit plan paths" do
+        #These paths should only be reached with an IVL enrollment, hence their location here.
+        before do
+          sign_in
+          allow(HbxEnrollment).to receive(:find).and_return(hbx_enrollment)
+        end
+
+        context "GET edit_plan" do
+          it "return http success and render" do
+            sign_in
+            allow(HbxEnrollment).to receive(:find).and_return(hbx_enrollment)
+            get :edit_plan, params: { person_id: person.id, family_id: person.primary_family.id, hbx_enrollment_id: hbx_enrollment.id }
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template(:edit_plan)
+          end
+        end
+
+        context "POST term_or_cancel" do
+          it "should cancel enrollment with no term date given" do
+            allow(hbx_enrollment).to receive(:effective_on).and_return(TimeKeeper.date_of_record + 10)
+            post :term_or_cancel, params: { hbx_enrollment_id: hbx_enrollment.id, term_or_cancel: 'cancel' }
+            expect(hbx_enrollment.aasm_state).to eq "coverage_canceled"
+            expect(response).to redirect_to(family_account_path)
+          end
+
+          it "should schedule terminate enrollment with term date given" do
+            allow(hbx_enrollment).to receive(:effective_on).and_return(TimeKeeper.date_of_record - 10)
+            post :term_or_cancel, params: { hbx_enrollment_id: hbx_enrollment.id, term_date: TimeKeeper.date_of_record + 1, term_or_cancel: 'terminate' }
+            expect(hbx_enrollment.aasm_state).to eq "coverage_termination_pending"
+            expect(response).to redirect_to(family_account_path)
+          end
+        end
+
+      end
     end
   end
 
