241e9eb946b4728f7def522fcd8530aaa1605303
rabbittracey <traceyw@vt.edu>
2019-07-23 10:25:27 -0400

    fix related rspec

--- spec/helpers/broker_agencies/profiles_helper_spec.rb
+++ spec/helpers/broker_agencies/profiles_helper_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe BrokerAgencies::ProfilesHelper, dbclean: :after_each, :type => :h
 
   let(:user) { FactoryGirl.create(:user) }
   let(:person) { FactoryGirl.create(:person, user: user) }
-  let(:person2) { FactoryGirl.create(:person) }
+  let(:broker_agency_profile) {FactoryGirl.create(:broker_agency_profile)}
+  let(:person2) { broker_agency_profile.primary_broker_role.person }
 
   describe 'disable_edit_broker_agency?' do
 
@@ -27,19 +28,12 @@ RSpec.describe BrokerAgencies::ProfilesHelper, dbclean: :after_each, :type => :h
 
   describe 'can_show_destroy?' do
 
-    it 'should return true if current user is logged in' do
-      allow(person).to receive(:broker_role).and_return true
-      expect(helper.can_show_destroy?(user, person)). to eq true
+    it 'should return true if person is the primary broker' do
+      expect(helper.can_show_destroy?(person2, broker_agency_profile)). to eq true
     end
 
-    it 'should return true if staff has broker role' do
-      allow(person).to receive(:broker_role).and_return true
-      expect(helper.can_show_destroy?(user, person)). to eq true
-    end
-
-    it 'should return false if staff has broker role' do
-      allow(person).to receive(:broker_role).and_return false
-      expect(helper.can_show_destroy?(user, person2)). to eq nil
+    it 'should return false if the person is not the primary broker' do
+      expect(helper.can_show_destroy?(person, broker_agency_profile)). to eq false
     end
 
   end
