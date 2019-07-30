0aab3d29fcccf2d2929a526ee2969def3a9882f9
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-07-30 13:17:45 -0400

    refs #43128 rubocop fixes

--- components/sponsored_benefits/spec/factories/sponsored_benefits_accounts_general_agency_accounts.rb
+++ components/sponsored_benefits/spec/factories/sponsored_benefits_accounts_general_agency_accounts.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 FactoryBot.define do
   factory :sponsored_benefits_accounts_general_agency_account, class: '::SponsoredBenefits::Accounts::GeneralAgencyAccount' do
     start_on { TimeKeeper.date_of_record - 10.days }
