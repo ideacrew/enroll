a38021c6a098f7b70ac9c7914e724da04c687f43
Trey Evans <tevans@Just-Plain-Awful.local>
2019-07-30 16:11:09 -0400

    Speed up enrollment report for shop.

--- app/reports/hbx_reports/shop_enrollment_report.rb
+++ app/reports/hbx_reports/shop_enrollment_report.rb
@@ -36,26 +36,44 @@ class ShopEnrollmentReport < MongoidMigrationTask
                     'Subscriber HBXID', 'Subscriber First Name','Subscriber Last Name', 'HIOS ID', 'Premium Subtotal', 
                     'ER Contribution', 'Applied APTC Amount', 'Total Responsible Amount', 'Family Size', 'Enrollment Reason', 'In Glue']
     file_name = "#{Rails.root}/hbx_report/shop_enrollment_report.csv"
+
+    rating_area_cache = get_rating_areas
     CSV.open(file_name, "w", force_quotes: true) do |csv|
       csv << shop_headers
-      enrollment_ids_final.each_slice(300) do |en_slice|
+      enrollment_ids_final.each_slice(400) do |en_slice|
         en_org_map = get_employer_profiles_for(en_slice)
         en_pers_map = get_people_for(en_slice)
+        f_member_person_map = get_family_member_map(en_slice)
         enrollment_map = Hash.new
+        product_map = get_products_for(en_slice)
         HbxEnrollment.where("hbx_id" => {"$in" => en_slice.to_a}).each do |en|
           enrollment_map[en.hbx_id] = en
         end
+        rel_map = get_relationship_map(enrollment_map.values,  f_member_person_map)
+        sb_map = get_sponsored_benefit_map(en_slice)
         en_slice.each do |id|
           begin
             hbx_enrollment = enrollment_map[id]
             next unless hbx_enrollment.is_shop?
+            sponsored_benefit = sb_map[hbx_enrollment.sponsored_benefit_id]
+            hbx_enrollment.sponsored_benefit = sponsored_benefit
+            hbx_enrollment.sponsored_benefit_package = sponsored_benefit.benefit_package
+            hbx_enrollment.benefit_sponsorship = sponsored_benefit.benefit_package.benefit_application.benefit_sponsorship
             employer_profile =  if en_org_map.has_key?(id)
                                    en_org_map[id]
                                 else
                                   hbx_enrollment.employer_profile
                                 end
             enrollment_reason = enrollment_kind(hbx_enrollment)
-            plan_year = hbx_enrollment.sponsored_benefit_package.benefit_application
+            product = product_map[id]
+            calculator = Enrollments::RandomAccessSponsoredEnrollmentCalculator.new(
+              hbx_enrollment,
+              f_member_person_map,
+              rel_map,
+              sb_map,
+              rating_area_cache
+            ).groups_for_products([product]).first.group_enrollment
+            plan_year = sponsored_benefit.benefit_package.benefit_application
             plan_year_start = plan_year.start_on.to_s
             if en_pers_map.has_key?(id)
               subscriber = en_pers_map[id]
@@ -64,12 +82,17 @@ class ShopEnrollmentReport < MongoidMigrationTask
               last_name = subscriber.last_name
             end
             in_glue = glue_list.include?(id) if glue_list.present?
-            csv << [employer_profile.hbx_id,employer_profile.fein,employer_profile.legal_name,plan_year_start,plan_year.aasm_state,
-                    plan_year.benefit_sponsorship.aasm_state,hbx_enrollment.hbx_id,hbx_enrollment.created_at,hbx_enrollment.effective_on,
-                    hbx_enrollment.terminated_on,hbx_enrollment.coverage_kind,hbx_enrollment.aasm_state,subscriber_hbx_id,first_name,
-                    last_name,hbx_enrollment.product.hios_id,hbx_enrollment.total_premium,hbx_enrollment.total_employer_contribution,
-                    hbx_enrollment.applied_aptc_amount,hbx_enrollment.total_employee_cost,hbx_enrollment.hbx_enrollment_members.size,
-                    enrollment_reason,in_glue]
+            csv << [
+              employer_profile.hbx_id, employer_profile.fein, employer_profile.legal_name,
+              plan_year_start, plan_year.aasm_state, plan_year.benefit_sponsorship.aasm_state,
+              hbx_enrollment.hbx_id, hbx_enrollment.created_at, hbx_enrollment.effective_on,
+              hbx_enrollment.terminated_on, hbx_enrollment.coverage_kind, hbx_enrollment.aasm_state,
+              subscriber_hbx_id,first_name,last_name,
+              product.hios_id,
+              calculator.total_premium, calculator.total_employer_contribution, hbx_enrollment.applied_aptc_amount, calculator.total_employee_cost,
+              hbx_enrollment.hbx_enrollment_members.size,
+              enrollment_reason,
+              in_glue]
           rescue StandardError => e
             @logger = Logger.new("#{Rails.root}/log/shop_enrollment_report_error.log")
             (@logger.error { "Could not add the hbx_enrollment's information on to the CSV for eg_id:#{id}, subscriber_hbx_id:#{subscriber_hbx_id}, #{e.inspect}" }) unless Rails.env.test?
@@ -77,6 +100,39 @@ class ShopEnrollmentReport < MongoidMigrationTask
         end
       end
     end
+  end
+
+  def get_rating_areas
+    ra_map = Hash.new
+    BenefitMarkets::Locations::RatingArea.all.each do |ra|
+      ra_map[ra.id] = ra
+    end
+    ra_map
+  end
+
+  def get_products_for(enrollment_hbx_ids)
+    en_p_id_map = Hash.new
+    product_map = Hash.new
+    product_ids = Array.new
+    HbxEnrollment.collection.aggregate([
+      {"$match" => {"hbx_id" => {"$in" => enrollment_hbx_ids.to_a}}},
+      {"$project" => {"product_id" => 1, "hbx_id" => 1}}
+    ]).each do |rec|
+      en_p_id_map[rec["hbx_id"]] = rec["product_id"]
+      product_ids << rec["product_id"]
+    end
+    product_id_map = Hash.new
+    BenefitMarkets::Products::Product.where(
+      "_id" => {"$in" => product_ids}
+    ).each do |product|
+      product_id_map[product.id] = product
+    end
+    en_p_id_map.each_pair do |k, v|
+      if product_id_map.has_key?(v)
+        product_map[k] = product_id_map[v]
+      end
+    end
+    product_map
   end
 
   def get_people_for(enrollment_hbx_ids)
@@ -113,6 +169,99 @@ class ShopEnrollmentReport < MongoidMigrationTask
       end
     end
     en_pers_map
+  end
+
+  def get_sponsored_benefit_map(enrollment_hbx_ids)
+    product_map = Hash.new
+    product_ids = Array.new
+    bsc_ids = Array.new
+    HbxEnrollment.collection.aggregate([
+      {"$match" => {"hbx_id" => {"$in" => enrollment_hbx_ids.to_a}}},
+      {"$project" => {"sponsored_benefit_id" => 1, "hbx_id" => 1}}
+    ]).each do |rec|
+      product_ids << rec["sponsored_benefit_id"]
+    end
+    BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(
+      "benefit_applications.benefit_packages.sponsored_benefits._id" => {"$in" => product_ids.compact}  
+    ).each do |bs|
+      bs.benefit_applications.each do |ba|
+        bsc_ids << ba.benefit_sponsor_catalog_id
+        ba.benefit_packages.flat_map(&:sponsored_benefits).each do |sb|
+          product_map[sb.id] = sb
+        end
+      end
+    end
+    bsc_id_map = Hash.new
+    BenefitMarkets::BenefitSponsorCatalog.where(
+      "_id" => {"$in" => bsc_ids.compact}
+    ).without("product_packages.products").each do |bsc|
+      bsc_id_map[bsc.id] = bsc
+    end
+    product_map.values.each do |sb|
+      ba = sb.benefit_package.benefit_application
+      bsc_id = ba.benefit_sponsor_catalog_id
+      if bsc_id_map.has_key?(bsc_id)
+        ba.benefit_sponsor_catalog = bsc_id_map[bsc_id]
+        bsc_id_map[bsc_id].benefit_application = ba
+      end
+    end
+    product_map
+  end
+
+  def get_family_member_map(enrollment_hbx_ids)
+    f_member_pid_map = Hash.new
+    f_member_pers_map = Hash.new
+    person_ids = Array.new
+    HbxEnrollment.collection.aggregate([
+      {"$match" => {"hbx_id" => {"$in" => enrollment_hbx_ids.to_a}}},
+      {"$project" => {"hbx_id" => 1, "family_id" => 1}},
+      {"$lookup" => {
+        "from" => "families",
+        "localField" => "family_id",
+        "foreignField" => "_id",
+        "as" => "family"
+      }},
+      {"$unwind" => "$family"},
+      {"$project" => {"hbx_id" => 1, "family_id" => 1, "family.family_members" => 1}},
+      {"$unwind" => "$family.family_members"},
+      {"$project" => {"person_id" => "$family.family_members.person_id", "family_member_id" => "$family.family_members._id"}}
+    ]).each do |rec|
+      f_member_pid_map[rec["family_member_id"]] = rec["person_id"]
+      person_ids << rec["person_id"]
+    end
+    pers_id_map = Hash.new
+    Person.where(
+      "_id" => {"$in" => person_ids}
+    ).each do |person|
+      pers_id_map[person.id] = person
+    end
+    f_member_pid_map.each_pair do |k, v|
+      if pers_id_map.has_key?(v)
+        f_member_pers_map[k] = pers_id_map[v]
+      end
+    end
+    f_member_pers_map
+  end
+
+  def get_relationship_map(enrollments, family_member_map)
+    rel_mapping = Hash.new
+    enrollments.each do |en|
+      subscriber = en.hbx_enrollment_members.detect(&:is_subscriber?)
+      if subscriber
+        if family_member_map.has_key?(subscriber.applicant_id)
+          sub_person = family_member_map[subscriber.applicant_id]
+          en.hbx_enrollment_members.each do |hem|
+            if !hem.is_subscriber?
+              if family_member_map.has_key?(hem.applicant_id)
+                hem_person = family_member_map[hem.applicant_id]
+                rel_mapping[[hem.applicant_id, subscriber.applicant_id]] = sub_person.find_relationship_with(hem_person)
+              end
+            end
+          end
+        end
+      end
+    end
+    rel_mapping
   end
 
   def get_employer_profiles_for(enrollment_hbx_ids)
