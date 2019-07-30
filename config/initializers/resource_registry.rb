7148cac563811d584aecead561800bdbbdbdad9d
Raghuram Ganjala <raghuramg83@gmail.com>
2019-07-30 11:38:50 -0400

    Updated resource registry initializer and boot

--- config/initializers/resource_registry.rb
+++ config/initializers/resource_registry.rb
@@ -2,54 +2,32 @@ ResourceRegistry.configure do
   {
     application: {
       config: {
-        name: "public",
-        default_namespace: "core",
+        name: "EnrollApp", # reference Rails.app.name
+        default_namespace: "options",
         root: '.',
         system_dir: "system",
         auto_register: []
       },
       load_paths: ['system']
-    },
-    resource_registry: {
-      config: {
-        name: "public",
-        default_namespace: "core",
-        root: '.',
-        system_dir: "system",
-        auto_register: []
-      },
-      load_paths: []
-    },
-    options: {
-      key: :tenants,
-      namespaces:[
-        {
-          key: :dchbx,
-          namespaces: [{
-            key: :persistence,
-            settings: [
-              { key: :store, default: 'file_store' },
-              { key: :serializer, default: 'yaml_serializer' },
-              { key: :container, default: 'config' }
-            ]
-          }]
-        }
-      ]
     }
+    # ,
     # options: {
-    #   tenants: [ :dchbx, :cca ],
-    #   dchbx: {
-    #     persistence:{
-    #       store: "file_store",
-    #       serializer: "yaml_serializer",
-    #       container: "config"
+    #   key: :tenants,
+    #   namespaces:[
+    #     {
+    #       key: :dchbx,
+    #       namespaces: [{
+    #         key: :persistence,
+    #         settings: [
+    #           { key: :store, default: 'file_store' },
+    #           { key: :serializer, default: 'yaml_serializer' },
+    #           { key: :container, default: 'config' }
+    #         ]
+    #       }]
     #     }
-    #   },
-    #   cca: {}
+    #   ]
     # }
   }
 end
 
-Kernel.const_set("Registry", ResourceRegistry::Registry)
-ResourceRegistry.const_set(:Inject, Registry.injector)
 require_relative Rails.root.join('system', 'boot')
--- system/boot.rb
+++ system/boot.rb
@@ -1,23 +1,10 @@
 module System
-  # require_relative "local/inject"
-
-  # require ResourceRegistry.services_path.join('load_registry').to_s
-  # path = Rails.root.join(Registry.config.system_dir, Registry["persistence.container"])
 
   path = Rails.root.join(Registry.config.system_dir, "config")
 
   Dir.glob(File.join(path, "*")).each do |file_path|
-    ResourceRegistry::Services::LoadRegistry.new.call(file_path)
+    ResourceRegistry::Services::LoadRegistryOptions.new.call(file_path)
   end
 
-  Registry.finalize!(freeze: true) # if defined? Rails && Rail.env == 'production'
-
-  # Repository.namespace(:options) do |container|
-  #   path  = container.config.root.join(container.config.system_dir, 'config')
-  #   obj   = LoadOptions.load_attr(path)
-  #   obj.to_hash.each_pair { |key, value| container.register("#{key}".to_sym, "#{value}") }
-  # end
-
-  # # EnrollContainer.finalize!(freeze: true) # if defined? Rails && Rail.env == 'production'
-  # # set constant
+  Registry.finalize!(freeze: true) #if defined? Rails && Rail.env == 'production'
 end
