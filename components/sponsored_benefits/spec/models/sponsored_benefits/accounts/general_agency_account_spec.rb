016b9cf2df0436bee33241c80c7e2c9cbc04e7ec
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-07-30 13:00:32 -0400

    refs #43128 display correct ga name on employer data table

---
+++ components/sponsored_benefits/spec/models/sponsored_benefits/accounts/general_agency_account_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'pry'
+
+module SponsoredBenefits
+  RSpec.describe Accounts::GeneralAgencyAccount, type: :model, dbclean: :after_each do
+
+    let(:general_agency) { FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_general_agency_profile, :with_site, legal_name: "NEW AGENCY PROFILE") }
+    let(:general_agency_profile) { general_agency.profiles.first }
+    let(:general_agency_account) { FactoryBot.build(:sponsored_benefits_accounts_general_agency_account, general_agency_profile: general_agency_profile, general_agency_profile_id: 'general_agency_profile_id' )}
+
+    context "#ga_name" do
+
+      it "should get legal name for general agency" do
+        expect(general_agency_account.ga_name).to eq general_agency_profile.legal_name
+      end
+    end
+  end
+end
