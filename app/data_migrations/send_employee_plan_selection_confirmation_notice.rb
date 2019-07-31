0e5bed6623d6eb3b78d62056f1de7c65a59cebfa
rabbittracey <traceyw@vt.edu>
2019-07-31 10:23:58 -0400

    modify the comment output

--- app/data_migrations/send_employee_plan_selection_confirmation_notice.rb
+++ app/data_migrations/send_employee_plan_selection_confirmation_notice.rb
@@ -9,15 +9,15 @@ class SendEmployeePlanSelectionConfirmationNotice < MongoidMigrationTask
 
   def trigger_plan_selection_confirmation_notice(census_employee)
     if !census_employee.employee_role.present?
-      puts "can not send notice to ce with id #{census_employee.id} due to no employee role"
+      puts "can not send notice to ce with id #{census_employee.id} due to no employee role" unless Rails.env.test?
     elsif !census_employee.active_benefit_group_assignment.present?
-      puts "can not send notice to ce with id #{census_employee.id} due to no active active_benefit_group_assignment"
+      puts "can not send notice to ce with id #{census_employee.id} due to no active active_benefit_group_assignment" unless Rails.env.test?
     elsif census_employee.active_benefit_group_assignment.hbx_enrollment.nil?
-      puts "can not send notice to ce with id #{census_employee.id} due to no enrollment purchased"
+      puts "can not send notice to ce with id #{census_employee.id} due to no enrollment purchased" unless Rails.env.test?
     else
       observer = Observers::NoticeObserver.new
       observer.deliver(recipient: census_employee.employee_role, event_object: census_employee.active_benefit_group_assignment.hbx_enrollment, notice_event: "initial_employee_plan_selection_confirmation")
-      puts "sent plan selection confirmation notice to ce with id #{census_employee.id}"
+      puts "sent plan selection confirmation notice to ce with id #{census_employee.id}" unless Rails.env.test?
     end
   end
 end
