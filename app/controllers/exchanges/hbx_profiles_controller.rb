8328f9d0b582158440c0ea56572256bdddef165b
Battula <vrbattula@outlook.com>
2019-07-31 06:06:29 -0400

    refs #43423 Use unset to remove ssn instead of update attributes.

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -607,7 +607,12 @@ def employer_poc
       @dont_allow_change = true
     else
       begin
-        @person.update_attributes!(dob: Date.strptime(params[:jq_datepicker_ignore_person][:dob], '%m/%d/%Y').to_date, encrypted_ssn: Person.encrypt_ssn(params[:person][:ssn]))
+        if params[:person][:ssn].present?
+          @person.update_attributes!(encrypted_ssn: Person.encrypt_ssn(params[:person][:ssn]))
+        else
+          @person.unset(:encrypted_ssn)
+        end
+        @person.update_attributes!(dob: Date.strptime(params[:jq_datepicker_ignore_person][:dob], '%m/%d/%Y').to_date)
         @person.consumer_role.check_for_critical_changes(@person.primary_family, info_changed: @info_changed, no_dc_address: "false", dc_status: @dc_status) if @person.consumer_role && @person.is_consumer_role_active?
         CensusEmployee.update_census_employee_records(@person, current_user)
       rescue Exception => e
