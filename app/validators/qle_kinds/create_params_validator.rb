7e4d8f133b9841bb46708960af6b1f2bb1412ccd
zasman <zanesandin@gmail.com>
2019-08-01 13:29:11 -0400

    Adds validators for qle kinds

---
+++ app/validators/qle_kinds/create_params_validator.rb
@@ -0,0 +1,15 @@
+module QleKinds
+  class CreateParamsValidator < ::BenefitSponsors::BaseParamValidator
+    define do
+      required(:title).filled(:str?)
+      required(:pre_event_sep_in_days).filled
+      required(:post_event_sep_in_days).filled
+      required(:effective_on_kinds).value(:array?, min_size?: 1)
+      required(:market_kind).value(:filled?, included_in?: QualifyingLifeEventKind::MARKET_KINDS)
+      required(:is_self_attested).filled(::Dry::Types["params.bool"])
+      optional(:action_kind).maybe(:str?)
+      optional(:tool_tip).maybe(:str?)
+      optional(:reason).maybe(:str?)
+    end
+  end
+end
