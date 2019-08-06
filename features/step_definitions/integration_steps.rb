a50416b04a53a3e0b77848546bd792fffe7d868c
zasman <zanesandin@gmail.com>
2019-08-06 13:34:46 -0400

    Adds new qle kind feature cucumber

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -626,8 +626,37 @@ And(/^.+user visits the new Qualifying Life Event Kind page$/) do
   visit(manage_exchanges_qles_path)
 end
 
-When(/^.+ fills out the new QLE Kind form for (.*?) event$/) do |new_event_name|
+And(/^.+ selects Create a Custom QLE and clicks submit$/) do
+  choose('qle_wizard_new_qle_selected_radio')
+  click_button('Submit')
+end
 
+When(/^.+ fills out the new QLE Kind form for (.*?) event$/) do |new_event_name|
+  fill_in('qle_kind_creation_form_title', with: new_event_name)
+  fill_in('qle_kind_creation_form_tool_tip', with: "Tool Tip")
+  # select('', from: 'qle_kind_creation_form_action_kind')
+  # select('', from: 'qle_kind_creation_form_reason')
+  # Visible to Customer
+  choose('qle_kind_edit_form_visible_to_customer', option: 'Yes')
+  # Note: not using capybara 'choose' here because the 'option' param
+  # is for value, but we're using ng-reflect-value
+  inputs = page.all('input')
+  shop_radio = inputs.detect { |input| input["ng-reflect-value"] == 'shop' }
+  shop_radio.click
+  # Self attested
+  is_self_attested_radio = inputs.detect do |input|
+    input[:id] == 'qle_kind_creation_form_is_self_attested' && input["ng-reflect-value"] == 'Yes'
+  end
+  is_self_attested_radio.click
+  fill_in('qle_kind_creation_form_pre_event_sep_eligibility', with: '10')
+  fill_in('qle_kind_creation_form_post_event_sep_eligibility', with: '10')
+  fill_in('qle_kind_creation_start_on', with: '10/10/2020')
+  fill_in('qle_kind_creation_end_on', with: '10/20/2030')
+end
+
+And(/^.+ clicks Create QLE Kind button$/) do
+  click_button('Create QLE Kind')
+  # TODO: Still not submitting for some reason
 end
 
 And(/^.+ clicks Confirm$/) do
