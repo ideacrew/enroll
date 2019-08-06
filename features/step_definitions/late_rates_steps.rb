fee29c585187c6377816cc07ff0d38f8380102bb
sri49 <sriharsha.poosa@gmail.com>
2019-08-06 17:29:42 -0400

    Refs #37177. WIP

--- features/step_definitions/late_rates_steps.rb
+++ features/step_definitions/late_rates_steps.rb
@@ -5,7 +5,6 @@ Given(/^employer ABC Widgets visits benefits page$/) do
 end
 
 And(/^Employer doesn't have benefit applications$/) do
-  expect(page).to have_content("#{Settings.site.late_rates_message}")
   expect(page).to have_no_content("Edit Plan Year")
 end
 
@@ -14,12 +13,30 @@ When(/^Employer clicks on Add PlanYear button$/) do
 end
 
 Then(/^Employer should see shell plan year page$/) do
-	#expect(page).to have_content("SAVE")
   expect(page).to have_content("Add Benefit Package")
-	# find(:xpath, '//*[@id="uic-wrapper"]/div[1]/div/div[2]/span').click
-	# find(:xpath, '//*[@id="uic-wrapper"]/div[1]/div').click
-	# expect(page).to have_content((TimeKeeper.date_of_record + 1.months).strftime("%m/%d/%Y"))
-	# find('ul', :text => "#{(TimeKeeper.date_of_record + 1.month).strftime('%m/%d/%Y')}").click
-	# wait_for_ajax(3, 2)
+end
+
+And(/^Employer clicks on all valid information$/) do
+  find(:xpath, '//*[@id="uic-wrapper"]/div[1]/div/div[2]/span').click
+  expect(page).to have_content((TimeKeeper.date_of_record.beginning_of_month + 2.months).strftime("%m/%d/%Y"))
+  find('li', :text => "#{(TimeKeeper.date_of_record.beginning_of_month + 2.months).strftime('%m/%d/%Y')}").click
+
+  find('#fteEmployee').click
+  fill_in 'benefit_application[fte_count]', :with => 10
+
+  find('#pteEmployee').click
+  fill_in 'benefit_application[pte_count]', :with => 3
+
+  find('#medSecPayers').click
+  fill_in 'benefit_application[msp_count]', :with => 3
+
+
+end
+
+When(/^Employer clicks on Continue Button$/) do
+  #expect(page).to have_content("#{Settings.site.late_rates_message}")
+  #click_link 'CONTINUE'
+  #find(:xpath, '//*[@id="benefitContinueBtn"]').click
+  #expect(page).to have_content("CONTINUE")
   find(".broker-registration", wait: 10).click
 end
