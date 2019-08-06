22942bbf151eaaeda600c7b5eab8ff3343dd0881
Zachary Landes <zach.g.landes@gmail.com>
2019-08-06 10:10:47 -0400

    joining action and reason kinds

--- app/services/admin/qle_kinds/update_service.rb
+++ app/services/admin/qle_kinds/update_service.rb
@@ -76,11 +76,10 @@ module Admin
           is_active: false,
           post_event_sep_in_days: request.post_event_sep_in_days,
           tool_tip: request.tool_tip,
-          reason: request.reason,
-          action_kind: request.action_kind,
+          reason: request.reason.split('').drop(3).join(''),
+          action_kind: request.action_kind.split('').drop(3).join(''),
           end_on: request.end_on,
           start_on: request.start_on
-
         )
         BenefitSponsors::Services::ServiceResponse.new(record)
       end
