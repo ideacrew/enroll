37d9877fac8f6e73d9aaa392f1ebf196b3672c2f
zasman <zanesandin@gmail.com>
2019-08-06 15:31:24 -0400

    Reorganizes some qualifying life event kind cucumbers and adds a test for edit

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -613,52 +613,6 @@ And(/^.+ sees the (.*) page and clicks Continue$/) do |which_page|
   continue.click
 end
 
-And(/^.+ clicks the Manage QLE link$/) do
-  click_link 'Manage QLEs'
-end
-
-Then(/^.+ should see the QLE Kind Wizard$/) do
-  expect(page.current_path).to eq(manage_exchanges_qles_path)
-  expect(page).to have_content("Manage Qualifying Life Events")
-end
-
-And(/^.+user visits the new Qualifying Life Event Kind page$/) do
-  visit(manage_exchanges_qles_path)
-end
-
-And(/^.+ selects Create a Custom QLE and clicks submit$/) do
-  choose('qle_wizard_new_qle_selected_radio')
-  click_button('Submit')
-end
-
-When(/^.+ fills out the new QLE Kind form for (.*?) event$/) do |new_event_name|
-  fill_in('qle_kind_creation_form_title', with: new_event_name)
-  fill_in('qle_kind_creation_form_tool_tip', with: "Tool Tip")
-  # select('', from: 'qle_kind_creation_form_action_kind')
-  # select('', from: 'qle_kind_creation_form_reason')
-  # Visible to Customer
-  choose('qle_kind_edit_form_visible_to_customer', option: 'Yes')
-  # Note: not using capybara 'choose' here because the 'option' param
-  # is for value, but we're using ng-reflect-value
-  inputs = page.all('input')
-  shop_radio = inputs.detect { |input| input["ng-reflect-value"] == 'shop' }
-  shop_radio.click
-  # Self attested
-  is_self_attested_radio = inputs.detect do |input|
-    input[:id] == 'qle_kind_creation_form_is_self_attested' && input["ng-reflect-value"] == 'Yes'
-  end
-  is_self_attested_radio.click
-  fill_in('qle_kind_creation_form_pre_event_sep_eligibility', with: '10')
-  fill_in('qle_kind_creation_form_post_event_sep_eligibility', with: '10')
-  fill_in('qle_kind_creation_start_on', with: '10/10/2020')
-  fill_in('qle_kind_creation_end_on', with: '10/20/2030')
-end
-
-And(/^.+ clicks Create QLE Kind button$/) do
-  click_button('Create QLE Kind')
-  # TODO: Still not submitting for some reason
-end
-
 And(/^.+ clicks Confirm$/) do
   find_link('Confirm', wait: 10)
   click_link 'Confirm'
