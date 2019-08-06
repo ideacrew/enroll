17cd480d30635441481fa2e1f8440dcda94ab3bd
zasman <zanesandin@gmail.com>
2019-08-06 10:06:25 -0400

    Adds cucumber for visiting qle manage page

--- features/support/worlds/permissions_world.rb
+++ features/support/worlds/permissions_world.rb
@@ -28,10 +28,59 @@ module PermissionsWorld
       view_admin_tabs: true, can_extend_open_enrollment: true, can_change_fein: true,  view_the_configuration_tab: true, can_submit_time_travel_request: false, can_add_custom_qle: true)
   end
 
+
+  def build_test_roles
+    User.where(email: /themanda.*dc.gov/).delete_all
+    Person.where(last_name: /^amanda\d+$/).delete_all
+    a = 10000000
+    u1 = User.create( email: 'themanda.staff@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u2 = User.create( email: 'themanda.readonly@dc.gov', password: 'P@55word', password_confirmation: 'P@55word',  oim_id: "ex#{rand(5999999)+a}")
+    u3 = User.create( email: 'themanda.csr_supervisor@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u4 = User.create( email: 'themanda.csr_tier1@dc.gov', password: 'P@55word', password_confirmation: 'P@55word',  oim_id: "ex#{rand(5999999)+a}")
+    u5 = User.create( email: 'themanda.csr_tier2@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u6 = User.create( email: 'developer@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u7 = User.create( email: 'themanda.csr_tier3@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+    u8 = User.create( email: 'themanda.super_admin@dc.gov', password: 'P@55word', password_confirmation: 'P@55word', oim_id: "ex#{rand(5999999)+a}")
+
+    hbx_profile_id = FactoryBot.create(:hbx_profile).id
+    p1 = Person.create( first_name: 'staff', last_name: "amanda#{rand(1000000)}", user: u1)
+    p2 = Person.create( first_name: 'read_only', last_name: "amanda#{rand(1000000)}", user: u2)
+    p3 = Person.create( first_name: 'supervisor', last_name: "amanda#{rand(1000000)}", user: u3)
+    p4 = Person.create( first_name: 'tier1', last_name: "amanda#{rand(1000000)}", user: u4)
+    p5 = Person.create( first_name: 'tier2', last_name: "amanda#{rand(1000000)}", user: u5)
+    p6 = Person.create( first_name: 'developer', last_name: "developer#{rand(1000000)}", user: u6)
+    p7 = Person.create( first_name: 'tier3', last_name: "amanda#{rand(1000000)}", user: u7)
+    p8 = Person.create( first_name: 'super_admin', last_name: "amanda#{rand(1000000)}", user: u8)
+
+    HbxStaffRole.create!( person: p1, permission_id: Permission.hbx_staff.id, subrole: 'hbx_staff', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p2, permission_id: Permission.hbx_read_only.id, subrole: 'hbx_read_only', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p3, permission_id: Permission.hbx_csr_supervisor.id, subrole: 'hbx_csr_supervisor', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p4, permission_id: Permission.hbx_csr_tier1.id, subrole: 'hbx_csr_tier1', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p5, permission_id: Permission.hbx_csr_tier2.id, subrole: 'hbx_csr_tier2', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p6, permission_id: Permission.developer.id, subrole: 'developer', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p7, permission_id: Permission.hbx_tier3.id, subrole: 'hbx_tier3', hbx_profile_id: hbx_profile_id)
+    HbxStaffRole.create!( person: p8, permission_id: Permission.super_admin.id, subrole: 'super_admin', hbx_profile_id: hbx_profile_id)
+  end
+
   def hbx_admin_can_update_ssn
+    Permission.hbx_staff.update_attributes!(can_update_ssn: true)
     Permission.super_admin.update_attributes!(can_update_ssn: true)
     Permission.hbx_tier3.update_attributes!(can_update_ssn: true)
-    Permission.hbx_staff.update_attributes!(can_update_ssn: true)
+  end
+
+  def hbx_admin_can_access_user_account_tab
+    Permission.hbx_staff.update_attributes!(can_access_user_account_tab: true)
+    Permission.super_admin.update_attributes!(can_access_user_account_tab: true)
+    Permission.hbx_tier3.update_attributes!(can_access_user_account_tab: true)
+  end
+
+  def hbx_admin_csr_view_personal_info_page
+    Permission.hbx_staff.update_attributes!(view_personal_info_page: true)
+    Permission.super_admin.update_attributes!(view_personal_info_page: true)
+    Permission.hbx_tier3.update_attributes!(view_personal_info_page: true)
+    Permission.hbx_csr_supervisor.update_attributes!(view_personal_info_page: true)
+    Permission.hbx_csr_tier2.update_attributes!(view_personal_info_page: true)
+    Permission.hbx_csr_tier1.update_attributes!(view_personal_info_page: true)
   end
 
   def hbx_admin_can_complete_resident_application
@@ -39,6 +88,7 @@ module PermissionsWorld
     Permission.super_admin.update_attributes!(can_complete_resident_application: true)
     Permission.hbx_tier3.update_attributes!(can_complete_resident_application: true)
   end
+
   def hbx_admin_can_add_sep
     Permission.hbx_staff.update_attributes!(can_add_sep: true)
     Permission.super_admin.update_attributes!(can_add_sep: true)
@@ -47,6 +97,14 @@ module PermissionsWorld
 
   def hbx_admin_can_lock_unlock
     Permission.hbx_staff.update_attributes(can_lock_unlock: true)
+    Permission.super_admin.update_attributes(can_lock_unlock: true)
+    Permission.hbx_tier3.update_attributes(can_lock_unlock: true)
+  end
+
+  def hbx_admin_can_add_pdc
+    Permission.hbx_staff.update_attributes!(can_add_pdc: true)
+    Permission.super_admin.update_attributes!(can_add_pdc: true)
+    Permission.hbx_tier3.update_attributes!(can_add_pdc: true)
   end
 
   def hbx_admin_can_view_username_and_email
@@ -61,6 +119,8 @@ module PermissionsWorld
 
   def hbx_admin_can_reset_password
     Permission.hbx_staff.update_attributes(can_reset_password: true)
+    Permission.super_admin.update_attributes(can_reset_password: true)
+    Permission.hbx_tier3.update_attributes(can_reset_password: true)
   end
 
   def hbx_admin_can_change_fein
@@ -73,7 +133,13 @@ module PermissionsWorld
     Permission.hbx_tier3.update_attributes(can_force_publish: true)
   end
 
+  def hbx_admin_can_modify_plan_year
+    Permission.super_admin.update_attributes(can_modify_plan_year: true)
+    Permission.hbx_tier3.update_attributes(can_modify_plan_year: true)
+  end
+
   def hbx_admin_can_extend_open_enrollment
+    Permission.super_admin.update_attributes!(can_extend_open_enrollment: true)
     Permission.hbx_tier3.update_attributes(can_extend_open_enrollment: true)
   end
 
@@ -82,14 +148,93 @@ module PermissionsWorld
     Permission.hbx_tier3.update_attributes(can_create_benefit_application: true)
   end
 
-  def hbx_admin_can_access_user_account_tab
-    Permission.hbx_staff.update_attributes!(can_access_user_account_tab: true)
-    Permission.super_admin.update_attributes!(can_access_user_account_tab: true)
-    Permission.hbx_tier3.update_attributes!(can_access_user_account_tab: true)
+  def grant_super_admin_access
+    raise "User Email Argument expected!!"if ENV['user_email'].blank?
+    user_emails = ENV['user_email'].split(',')
+    hbx_organization = BenefitSponsors::Organizations::Organization.hbx_profiles.first
+    users = User.where(:email.in => user_emails)
+    users.each do |user|
+      HbxStaffRole.create!( person: user.person, permission_id: Permission.super_admin.id, subrole: 'super_admin', hbx_profile_id: HbxProfile.current_hbx.id, benefit_sponsor_hbx_profile_id: hbx_organization.hbx_profile.id)
+    end
+  end
+
+  def grant_hbx_tier3_access
+    raise "User Email Argument expected!!"if ENV['user_email'].blank?
+    user_emails = ENV['user_email'].split(',')
+    hbx_organization = BenefitSponsors::Organizations::Organization.hbx_profiles.first
+    users = User.where(:email.in => user_emails)
+    users.each do |user|
+      HbxStaffRole.create!( person: user.person, permission_id: Permission.hbx_tier3.id, subrole: 'hbx_tier3', hbx_profile_id: HbxProfile.current_hbx.id, benefit_sponsor_hbx_profile_id: hbx_organization.hbx_profile.id)
+    end
+  end
+
+  def hbx_admin_can_view_application_types
+    Permission.hbx_staff.update_attributes!(can_view_application_types: true)
+    Permission.super_admin.update_attributes!(can_view_application_types: true)
+    Permission.hbx_tier3.update_attributes!(can_view_application_types: true)
+  end
+
+  def hbx_admin_can_access_new_consumer_application_sub_tab
+    Permission.hbx_staff.update_attributes!(can_access_new_consumer_application_sub_tab: true)
+    Permission.super_admin.update_attributes!(can_access_new_consumer_application_sub_tab: true)
+    Permission.hbx_tier3.update_attributes!(can_access_new_consumer_application_sub_tab: true)
+    Permission.hbx_csr_supervisor.update_attributes!(can_access_new_consumer_application_sub_tab: true)
+    Permission.hbx_csr_tier1.update_attributes!(can_access_new_consumer_application_sub_tab: true)
+    Permission.hbx_csr_tier2.update_attributes!(can_access_new_consumer_application_sub_tab: true)
+  end
+
+  def hbx_admin_can_access_identity_verification_sub_tab
+    Permission.hbx_staff.update_attributes!(can_access_identity_verification_sub_tab: true)
+    Permission.super_admin.update_attributes!(can_access_identity_verification_sub_tab: true)
+    Permission.hbx_tier3.update_attributes!(can_access_identity_verification_sub_tab: true)
+    Permission.hbx_csr_supervisor.update_attributes!(can_access_identity_verification_sub_tab: true)
+    Permission.hbx_csr_tier1.update_attributes!(can_access_identity_verification_sub_tab: true)
+    Permission.hbx_csr_tier2.update_attributes!(can_access_identity_verification_sub_tab: true)
+  end
+
+  def hbx_admin_can_access_outstanding_verification_sub_tab
+    Permission.hbx_staff.update_attributes!(can_access_outstanding_verification_sub_tab: true)
+    Permission.super_admin.update_attributes!(can_access_outstanding_verification_sub_tab: true)
+    Permission.hbx_tier3.update_attributes!(can_access_outstanding_verification_sub_tab: true)
+  end
+
+  def hbx_admin_can_access_accept_reject_identity_documents
+    Permission.hbx_staff.update_attributes!(can_access_accept_reject_identity_documents: true)
+    Permission.super_admin.update_attributes!(can_access_accept_reject_identity_documents: true)
+    Permission.hbx_tier3.update_attributes!(can_access_accept_reject_identity_documents: true)
+  end
+
+  def hbx_admin_can_access_accept_reject_paper_application_documents
+    Permission.hbx_staff.update_attributes!(can_access_accept_reject_paper_application_documents: true)
+    Permission.super_admin.update_attributes!(can_access_accept_reject_paper_application_documents: true)
+    Permission.hbx_tier3.update_attributes!(can_access_accept_reject_paper_application_documents: true)
+    Permission.hbx_csr_supervisor.update_attributes!(can_access_accept_reject_paper_application_documents: true)
+    Permission.hbx_csr_tier1.update_attributes!(can_access_accept_reject_paper_application_documents: true)
+    Permission.hbx_csr_tier2.update_attributes!(can_access_accept_reject_paper_application_documents: true)
+  end
+
+  def hbx_admin_can_transition_family_members
+   Permission.hbx_staff.update_attributes!(can_transition_family_members: true)
+   Permission.super_admin.update_attributes!(can_transition_family_members: true)
+   Permission.hbx_tier3.update_attributes!(can_transition_family_members: true)
+  end
+
+  def hbx_admin_can_delete_identity_application_documents
+    Permission.hbx_staff.update_attributes!(can_delete_identity_application_documents: true)
+    Permission.super_admin.update_attributes!(can_delete_identity_application_documents: true)
+    Permission.hbx_tier3.update_attributes!(can_delete_identity_application_documents: true)
+  end
+
+  def hbx_admin_can_access_pay_now
+    Permission.hbx_staff.update_attributes!(can_access_pay_now: true)
+    Permission.hbx_csr_supervisor.update_attributes!(can_access_pay_now: true)
+    Permission.hbx_csr_tier1.update_attributes!(can_access_pay_now: true)
+    Permission.hbx_csr_tier2.update_attributes!(can_access_pay_now: true)
   end
 
   def make_all_permissions
     define_permissions
+    build_test_roles
     hbx_admin_can_update_ssn
     hbx_admin_can_complete_resident_application
     hbx_admin_can_add_sep
@@ -101,6 +246,8 @@ module PermissionsWorld
     hbx_admin_can_extend_open_enrollment
     hbx_admin_can_create_benefit_application
     hbx_admin_can_access_user_account_tab
+    hbx_admin_can_transition_family_members
+    hbx_admin_can_access_pay_now
   end
 end
 
