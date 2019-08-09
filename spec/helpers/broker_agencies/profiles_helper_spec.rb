d29433a605b8048b651658194d44cd7f0ec2ec8b
harsha chalamala <chvreddy1015@gmail.com>
2019-08-09 14:27:50 -0400

    Revert "Merge remote-tracking branch 'origin/ticket_43195' into new_test_branch"
    This reverts commit 57f2d19af15ea70b010fd863aefa4ce484d1addb, reversing
    changes made to 0717483031fa0e6d18ad3a95ebdc0fd139cd701e.

--- spec/helpers/broker_agencies/profiles_helper_spec.rb
+++ spec/helpers/broker_agencies/profiles_helper_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe BrokerAgencies::ProfilesHelper, dbclean: :after_each, :type => :h
 
   let(:user) { FactoryGirl.create(:user) }
   let(:person) { FactoryGirl.create(:person, user: user) }
-  let(:broker_agency_profile) {FactoryGirl.create(:broker_agency_profile)}
-  let(:person2) { broker_agency_profile.primary_broker_role.person }
+  let(:person2) { FactoryGirl.create(:person) }
 
   describe 'disable_edit_broker_agency?' do
 
@@ -28,12 +27,19 @@ RSpec.describe BrokerAgencies::ProfilesHelper, dbclean: :after_each, :type => :h
 
   describe 'can_show_destroy?' do
 
-    it 'should return true if person is the primary broker' do
-      expect(helper.can_show_destroy?(person2, broker_agency_profile)). to eq true
+    it 'should return true if current user is logged in' do
+      allow(person).to receive(:broker_role).and_return true
+      expect(helper.can_show_destroy?(user, person)). to eq true
     end
 
-    it 'should return false if the person is not the primary broker' do
-      expect(helper.can_show_destroy?(person, broker_agency_profile)). to eq false
+    it 'should return true if staff has broker role' do
+      allow(person).to receive(:broker_role).and_return true
+      expect(helper.can_show_destroy?(user, person)). to eq true
+    end
+
+    it 'should return false if staff has broker role' do
+      allow(person).to receive(:broker_role).and_return false
+      expect(helper.can_show_destroy?(user, person2)). to eq nil
     end
 
   end
