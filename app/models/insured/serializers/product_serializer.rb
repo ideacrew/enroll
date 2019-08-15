6c352d771da49bdff421659368122ba3f1d9e49d
RyanEddyIC <ryan.eddy@ideacrew.com>
2019-08-15 11:58:09 -0400

    Update attributes for health product

--- app/models/insured/serializers/product_serializer.rb
+++ app/models/insured/serializers/product_serializer.rb
@@ -3,7 +3,7 @@
 module Insured
   module Serializers
     class ProductSerializer < ::ActiveModel::Serializer
-      attributes :id, :application_period, :name, :hios_id, :issuer_profile_id, :metal_level, :kind
+      attributes :id, :application_period, :title, :hios_id, :issuer_profile_id, :metal_level_kind, :kind
 
       # provide defaults(if any needed) that were not set on Model
       def attributes(*args)
