bb45d179943a58daec0bc76519079d66c24a545e
RyanEddyIC <ryan.eddy@ideacrew.com>
2019-08-15 11:58:53 -0400

    Self term or cancel form spec

---
+++ spec/models/insured/forms/self_term_or_cancel_form_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+module Insured
+  RSpec.describe Forms::SelfTermOrCancelForm, type: :model, dbclean: :after_each do
+
+    subject { Insured::Forms::SelfTermOrCancelForm.new }
+
+    describe "model attributes" do
+      it {
+        [:carrier_logo, :covered_members, :current_premium, :enrollment, :is_under_ivl_oe, :market_kind, :product, :qle_kind_id, :sep_id, :should_term_or_cancel, :term_date].each do |key|
+          expect(subject.attributes.has_key?(key)).to be_truthy
+        end
+      }
+    end
+
+    describe "validate Form" do
+
+      let(:valid_params) {
+        {
+          :current_premium => "100",
+          :market_kind => "kind"
+        }
+      }
+
+      let(:invalid_params) {
+        {
+          :current_premium => nil,
+          :market_kind => nil
+        }
+      }
+
+      context "with invalid params" do
+
+        let(:build_self_term_or_cancel_form) { Insured::Forms::SelfTermOrCancelForm.new(invalid_params)}
+
+        it "should return false" do
+          expect(build_self_term_or_cancel_form.valid?).to be_falsey
+        end
+      end
+
+      context "with valid params" do
+
+        let(:build_self_term_or_cancel_form) { Insured::Forms::SelfTermOrCancelForm.new(valid_params)}
+
+        it "should return true" do
+          expect(build_self_term_or_cancel_form.valid?).to be_truthy
+        end
+      end
+    end
+
+    describe "#for_view" do
+      let (:family) { FactoryBot.create(:family, :with_primary_family_member) }
+      let (:sep) { FactoryBot.create(:special_enrollment_period, family: family) }
+      let (:sbc_document) { FactoryBot.build(:document, subject: "SBC", identifier: "urn:openhbx#123") }
+      let (:product) { FactoryBot.create(:benefit_markets_products_health_products_health_product, title: "AAA", issuer_profile_id: "ab1233", sbc_document: sbc_document) }
+      let (:enrollment) { FactoryBot.create(:hbx_enrollment, :individual_unassisted, family: family, product: product) }
+
+      it "should create a valid form for the view" do
+        family.special_enrollment_periods << sep
+        attrs = {enrollment_id: enrollment.id.to_s, family_id: family.id}
+        form = Insured::Forms::SelfTermOrCancelForm.for_view(attrs)
+        expect(Insured::Forms::SelfTermOrCancelForm.self_term_or_cancel_service(attrs)).to be_instance_of(Insured::Services::SelfTermOrCancelService)
+        expect(form.enrollment).not_to be nil
+        expect(form.product).not_to be nil
+        expect(form.should_term_or_cancel).not_to be nil
+        expect(form.current_premium).not_to be nil
+      end
+    end
+
+    describe "#for_post" do
+      let (:family) { FactoryBot.create(:family, :with_primary_family_member) }
+      let (:sep) {FactoryBot.create(:special_enrollment_period, family: family) }
+      let (:sbc_document) { FactoryBot.build(:document, subject: "SBC", identifier: "urn:openhbx#124") }
+      let (:product) { FactoryBot.create(:benefit_markets_products_health_products_health_product, title: "AAA", issuer_profile_id: "ab1233", sbc_document: sbc_document) }
+      let (:enrollment_to_cancel) { FactoryBot.create(:hbx_enrollment, :individual_unassisted, family: family, product: product, effective_on: DateTime.now + 1.month) }
+      let (:enrollment_to_term) { FactoryBot.create(:hbx_enrollment, :individual_unassisted, family: family, product: product, effective_on: DateTime.now - 1.month) }
+
+      it "should cancel an enrollment if it is not yet effective" do
+        attrs = {enrollment_id: enrollment_to_cancel.id, term_date: TimeKeeper.date_of_record.to_s}
+        Insured::Forms::SelfTermOrCancelForm.for_post(attrs)
+        enrollment_to_cancel.reload
+        expect(enrollment_to_cancel.aasm_state).to eq 'coverage_canceled'
+      end
+
+      it "should terminate an enrollment if it is already effective" do
+        attrs = {enrollment_id: enrollment_to_term.id, term_date: (TimeKeeper.date_of_record + 1.month).to_s}
+        Insured::Forms::SelfTermOrCancelForm.for_post(attrs)
+        enrollment_to_term.reload
+        expect(enrollment_to_term.aasm_state).to eq 'coverage_termination_pending'
+      end
+    end
+
+  end
+end
