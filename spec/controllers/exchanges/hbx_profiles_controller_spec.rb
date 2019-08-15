10890efe18fa7da5abb53d3526a0137389f173cc
Nikhil S <nikhilks219@gmail.com>
2019-08-15 17:29:48 -0400

    43305: Not allowing change enrollment end date functionality for particular enrollments

--- spec/controllers/exchanges/hbx_profiles_controller_spec.rb
+++ spec/controllers/exchanges/hbx_profiles_controller_spec.rb
@@ -845,15 +845,59 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :around_each do
   describe "POST view_enrollment_to_update_end_date", :dbclean => :around_each do
     let(:user) { FactoryBot.create(:user, roles: ["hbx_staff"]) }
     let!(:person) { FactoryBot.create(:person)}
-    let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
+    let!(:family) { FactoryBot.create(:family, :with_primary_family_member_and_dependent, person: person)}
+    let(:primary) { family.primary_family_member }
+    let(:dependents) { family.dependents }
     let!(:household) { FactoryBot.create(:household, family: family) }
-    let!(:enrollment) {
+    let(:effective_on) {TimeKeeper.date_of_record.beginning_of_year - 1.year}
+    let(:product) { FactoryBot.create(:benefit_markets_products_health_products_health_product, benefit_market_kind: :aca_individual, kind: :health, csr_variant_id: '01') }
+    let(:hbx_en_member1) { FactoryBot.build(:hbx_enrollment_member,
+                                              eligibility_date: effective_on,
+                                              coverage_start_on: effective_on,
+                                              applicant_id: dependents.first.id) }
+    let(:hbx_en_member2) { FactoryBot.build(:hbx_enrollment_member,
+                                          eligibility_date: effective_on + 2.months,
+                                          coverage_start_on: effective_on + 2.months,
+                                          applicant_id: dependents.first.id) }
+    let(:hbx_en_member3) { FactoryBot.build(:hbx_enrollment_member,
+                                      eligibility_date: effective_on + 6.months,
+                                      coverage_start_on: effective_on + 6.months,
+                                      applicant_id: dependents.last.id) }
+    let!(:enrollment1) {
       FactoryBot.create(:hbx_enrollment,
                          family: family,
+                         product: product,
                          household: family.active_household,
                          coverage_kind: "health",
-                         effective_on: TimeKeeper.date_of_record.last_month.beginning_of_month,
-                         aasm_state: 'coverage_termination_pending'
+                         effective_on: effective_on,
+                         terminated_on: effective_on.next_month.end_of_month,
+                         kind: 'individual',
+                         hbx_enrollment_members: [hbx_en_member1],
+                         aasm_state: 'coverage_terminated'
+      )}
+    let!(:enrollment2) {
+      FactoryBot.create(:hbx_enrollment,
+                         family: family,
+                         product: product,
+                         kind: 'individual',
+                         household: family.active_household,
+                         coverage_kind: "health",
+                         hbx_enrollment_members: [hbx_en_member2],
+                         effective_on: effective_on + 2.months,
+                         terminated_on: (effective_on + 5.months).end_of_month,
+                         aasm_state: 'coverage_terminated'
+      )}
+    let!(:enrollment3) {
+      FactoryBot.create(:hbx_enrollment,
+                         family: family,
+                         product: product,
+                         household: family.active_household,
+                         coverage_kind: "health",
+                         effective_on: effective_on + 6.months,
+                         terminated_on: effective_on.end_of_year,
+                         kind: 'individual',
+                         hbx_enrollment_members: [hbx_en_member3],
+                         aasm_state: 'coverage_terminated'
       )}
 
     before :each do
@@ -865,6 +909,12 @@ RSpec.describe Exchanges::HbxProfilesController, dbclean: :around_each do
       post :view_enrollment_to_update_end_date, params: {person_id: person.id.to_s, family_actions_id: family.id}, format: :js, xhr: true
       expect(response).to have_http_status(:success)
       expect(response).to render_template("view_enrollment_to_update_end_date")
+    end
+
+    it "should get duplicate enrollment id's" do
+      post :view_enrollment_to_update_end_date, params: {person_id: person.id.to_s, family_actions_id: family.id}, format: :js, xhr: true
+      expect(assigns(:dup_enr_ids).include?(enrollment1.id.to_s)).to eq true
+      expect(assigns(:dup_enr_ids).include?(enrollment3.id.to_s)).to eq false
     end
   end
 
