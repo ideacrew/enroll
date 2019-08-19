4d2336a82bf6b26ddd723e23f4856b71ccabf322
Vishal Kalletla <vishuk1201@gmail.com>
2019-08-19 16:05:53 -0400

    43738: additional cucumber scenarios

--- features/hbx_admin/step_definitions/create_plan_year.rb
+++ features/hbx_admin/step_definitions/create_plan_year.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+Given(/^initial employer (.*) has (.*) benefit application with terminated on (.*)$/) do |name, aasm, expected_start_event|
+  step "initial employer #{name} has #{aasm} benefit application"
+
+  unless expected_start_event == 'draft_py_effective_on'
+    profile = @organization[name].employer_profile
+
+    # Considering the below dates based on draft PY creation date, which is 3rd option from the dropdown of create PY UI
+    two_month_advanced_date = TimeKeeper.date_of_record + 2.months
+    one_month_advanced_date = TimeKeeper.date_of_record + 1.month
+
+    benefit_application = profile.benefit_sponsorships.first.benefit_applications.where(aasm_state: :termination_pending).first
+    start_on = TimeKeeper.date_of_record - 4.months
+    end_on = expected_start_event == 'draft_py_date_lt_term_on' ? two_month_advanced_date : one_month_advanced_date
+    benefit_application.update_attributes(effective_period: start_on..end_on)
+  end
+end
+
 And(/^Hbx Admin should see the list of employer accounts and an Action button$/) do
   within('.effective-datatable') do
     expect(page).to have_css('.dropdown-toggle', count: 2)
@@ -11,7 +28,7 @@ Given('the user has clicked the Create Plan Year button') do
 end
 
 Given('the user has a valid input for all required fields') do
-  find('#baStartDate > option:nth-child(2)').click
+  find('#baStartDate > option:nth-child(3)').click
   find('#fteCount').fill_in :with => '20'
   find('label',:text => 'Open Enrollment End Date').click
 end
@@ -20,8 +37,8 @@ When(/the admin clicks (.*)$/) do |btn|
   find('.btn', :text => btn).click
 end
 
-Then('the user will see a success message') do
-  expect(page).to have_content('Successfully created a draft plan year')
+Then(/the user will see a (.*) message/) do |message|
+  expect(page).to have_content(message)
 end
 
 Then('the draft application will be created') do
@@ -35,11 +52,11 @@ end
 Then('the user will see a pop up modal with "Confirm" or "Cancel" action') do
   expect(page).to have_content('Confirm Create Plan Year')
 end
+#
+# Then('the existing application will remain in Publish Pending') do
+#   expect(page).to have_content('Plan Year (Publish Pending)')
+# end
 
-Then('the existing application will remain in Publish Pending') do
-  expect(page).to have_content('Plan Year (Publish Pending)')
-end
-
-Then('the new plan year will NOT be created.') do
+Then('the draft application will NOT be created') do
   expect(page).to_not have_content('Plan Year (Draft)')
 end
