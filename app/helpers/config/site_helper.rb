764483ee859f12e2882cb0956900704055f7fdac
Raghuram Ganjala <raghuramg83@gmail.com>
2019-08-20 19:46:16 -0400

    Upgraded resource registry gem, updated configuration & setting yaml files

--- app/helpers/config/site_helper.rb
+++ app/helpers/config/site_helper.rb
@@ -57,15 +57,15 @@ module Config::SiteHelper
   end
 
   def site_copyright_period_start
-    Registry['tenants.dchbx.enroll.site.copyright_period_start']
+    Registry['enterprise.dchbx.primary.production.copyright_period_start']
   end
 
   def site_help_url
-    Registry['tenants.dchbx.enroll.site.help_url']
+    Registry['enterprise.dchbx.primary.production.help_url']
   end
 
   def site_business_resource_center_url
-    Registry['tenants.dchbx.enroll.site.business_resource_center_url']
+    Registry['enterprise.dchbx.primary.production.business_resource_center_url']
   end
 
   def link_to_site_business_resource_center
@@ -73,15 +73,15 @@ module Config::SiteHelper
   end
 
   def site_nondiscrimination_notice_url
-    Registry['tenants.dchbx.enroll.site.nondiscrimination_notice_url']
+    Registry['enterprise.dchbx.primary.production.nondiscrimination_notice_url']
   end
 
   def site_policies_url
-    Registry['tenants.dchbx.enroll.site.policies_url']
+    Registry['enterprise.dchbx.primary.production.policies_url']
   end
 
   def site_faqs_url
-    Registry['tenants.dchbx.enroll.site.faqs_url']
+    Registry['enterprise.dchbx.primary.production.faqs_url']
   end
 
   def site_short_name
--- system/boot.rb
+++
@@ -1,7 +0,0 @@
-module System
-
-  path = Rails.root.join(Registry.config.system_dir, "config")
-  ResourceRegistry.load_options!(path)
-  
-  Registry.finalize!(freeze: true) #if defined? Rails && Rail.env == 'production'
-end
