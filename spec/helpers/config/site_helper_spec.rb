07c0df314175a7caaad643c34b087c6cff5ffd68
Raghuram Ganjala <raghuramg83@gmail.com>
2019-08-21 13:51:52 -0400

    Updated site helper spec

--- spec/helpers/config/site_helper_spec.rb
+++ spec/helpers/config/site_helper_spec.rb
@@ -2,11 +2,7 @@ require "rails_helper"
 
 RSpec.describe Config::SiteHelper, :type => :helper, dbclean: :after_each do
 
-  describe "Registry settings" do
-    let(:employee_role) {FactoryBot.create(:employee_role, person: person)}
-    let(:person) {FactoryBot.create(:person, :with_ssn)}
-    let(:primary_family) {FactoryBot.create(:family, :with_primary_family_member, person: person)}
-
+  describe "Site settings" do
 
     context '.site_copyright_period_start' do 
 
@@ -19,14 +15,14 @@ RSpec.describe Config::SiteHelper, :type => :helper, dbclean: :after_each do
 
       it 'should return help url' do
         expect(helper.site_copyright_period_start).to be_kind_of(String)
-      end 
+      end
     end
 
     context '.site_business_resource_center_url' do
 
       it 'should return business resource center url' do 
         expect(helper.site_business_resource_center_url).to be_kind_of(String)
-      end 
+      end
     end
 
     context '.site_nondiscrimination_notice_url' do
@@ -40,14 +36,14 @@ RSpec.describe Config::SiteHelper, :type => :helper, dbclean: :after_each do
 
       it 'should return site policies url' do
         expect(helper.site_policies_url).to be_kind_of(String)
-      end 
+      end
     end
 
     context '.site_faqs_url' do
 
       it 'should return site faqs url' do
         expect(helper.site_faqs_url).to be_kind_of(String)
-      end 
+      end
     end
   end
 end
