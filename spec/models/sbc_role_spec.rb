6e8f9b6d052e44999156662b93de3296ddcf9a10
vineel <sonnathivinil@gmail.com>
2019-08-26 13:08:28 -0400

    refactor

--- spec/models/sbc_role_spec.rb
+++ spec/models/sbc_role_spec.rb
@@ -6,15 +6,19 @@ describe SbcRole, dbclean: :after_each do
   let!(:sbc_user)                { FactoryBot.create(:user) }
   let!(:person)                  { FactoryBot.create(:person, :with_sbc_role, user: sbc_user) }
 
-  it "should return find by id" do
-    expect(SbcRole.find(person.sbc_role.id)).to eq person.sbc_role
+  context "#class methods" do
+    it "should return find by id" do
+      expect(SbcRole.find(person.sbc_role.id)).to eq person.sbc_role
+    end
+
+    it "should return all users with sbc_roles" do
+      expect(SbcRole.all).to eq [sbc_user&.person&.sbc_role]
+    end
   end
 
-  it "should return all users with sbc_roles" do
-    expect(SbcRole.all).to eq [sbc_user&.person&.sbc_role]
-  end
-
-  it "should return parent" do
-    expect(person.sbc_role.parent).to eq person
+  context "#instance_methods" do
+    it "should return parent" do
+      expect(person.sbc_role.parent).to eq person
+    end
   end
 end
