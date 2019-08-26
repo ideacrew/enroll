8384a953b602e9746a56afb704a43c4ba960b7e4
vineel <sonnathivinil@gmail.com>
2019-08-26 13:07:15 -0400

    sbc_role specs

---
+++ spec/models/sbc_role_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe SbcRole, dbclean: :after_each do
+  let!(:sbc_user)                { FactoryBot.create(:user) }
+  let!(:person)                  { FactoryBot.create(:person, :with_sbc_role, user: sbc_user) }
+
+  it "should return find by id" do
+    expect(SbcRole.find(person.sbc_role.id)).to eq person.sbc_role
+  end
+
+  it "should return all users with sbc_roles" do
+    expect(SbcRole.all).to eq [sbc_user&.person&.sbc_role]
+  end
+
+  it "should return parent" do
+    expect(person.sbc_role.parent).to eq person
+  end
+end
