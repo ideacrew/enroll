c53470c044e8d859d6e36d174e40c043b4b5a3f4
RyanEddyIC <ryan.eddy@ideacrew.com>
2019-08-27 11:12:49 -0400

    Move logic to factory and serializers

--- app/models/insured/serializers/enrollment_serializer.rb
+++ app/models/insured/serializers/enrollment_serializer.rb
@@ -5,11 +5,22 @@ module Insured
     class EnrollmentSerializer < ::ActiveModel::Serializer
       attributes :id, :hbx_id, :effective_on
       attribute :covered_members_first_names
+      attribute :current_premium
       attribute :should_term_or_cancel_ivl
 
       def covered_members_first_names
         object.hbx_enrollment_members.inject([]) do |names, member|
           names << member.person.first_name
+        end
+      end
+
+      def current_premium
+        if object.is_shop?
+          object.total_employee_cost
+        elsif object.kind == 'coverall'
+          object.total_premium
+        else
+          object.total_premium > object.applied_aptc_amount.to_f ? object.total_premium - object.applied_aptc_amount.to_f : 0
         end
       end
 
