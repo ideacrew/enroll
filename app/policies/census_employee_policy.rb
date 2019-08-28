a6a67a80fc03e8a4f4f72726a8e32b039d9fac67
rabbittracey <traceyw@vt.edu>
2019-08-28 14:36:38 -0400

    need to fix the rspec

--- app/policies/census_employee_policy.rb
+++ app/policies/census_employee_policy.rb
@@ -5,7 +5,7 @@ class CensusEmployeePolicy < ApplicationPolicy
     return true if @user.has_role?(:hbx_staff)
     can_change =
       if @user.has_role?(:employer_staff) && !@user.has_role?(:broker)
-        @user.person.employer_staff_roles.map(&:employer_profile_id).map(&:to_s).include? @record.employer_profile_id.to_s
+        @user.person.employer_staff_roles.map(&:benefit_sponsor_employer_profile_id).map(&:to_s).include? @record.benefit_sponsor_employer_profile_id.to_s
       elsif @user.has_role?(:broker)
         @record.employer_profile.try(:active_broker) == @user.person
       elsif @user.has_role?(:general_agency_staff)
@@ -22,7 +22,7 @@ class CensusEmployeePolicy < ApplicationPolicy
   def show?
     return true if @user.has_role?(:hbx_staff) || @user.has_hbx_staff_role?
     return true if (@user.has_role?(:broker) || @user.has_broker_role?) && @record.employer_profile.try(:active_broker) == @user.person
-    return true if (@user.has_role?(:employer_staff) || @user.has_employer_staff_role?) && @user.person.employer_staff_roles.map(&:employer_profile_id).map(&:to_s).include?(@record.employer_profile_id.to_s)
+    return true if (@user.has_role?(:employer_staff) || @user.has_employer_staff_role?) && @user.person.employer_staff_roles.map(&:benefit_sponsor_employer_profile_id).map(&:to_s).include?(@record.benefit_sponsor_employer_profile_id.to_s)
     return false if !@user.has_role?(:general_agency_staff) || !@user.has_general_agency_staff_role?
     ga_id = @user.person.general_agency_staff_roles.last.general_agency_profile.id
     employer_id = @record.employer_profile.id
