1139d67ce3d86022f0abe3dc993818798183173a
vineel <sonnathivinil@gmail.com>
2019-08-29 09:24:29 -0400

    Revert "plan display mapping"
    This reverts commit f4e66031bb6dcda216760fdb2d4d90f1fdf33a52.

--- app/models/service_visit.rb
+++
@@ -1,8 +0,0 @@
-class ServiceVisit
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
-  field :copay_in_network, type: String
-  field :co_insurance_in_network, type: String
-  field :in_network_result, type: String
-end
