be3f537f0e07b05cc43b60f6c3b6ceb399439f85
Matt Williams <m.a.dubya@gmail.com>
2019-09-05 11:32:02 -0400

    ensure a consistent asset digest/hash

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -34,3 +34,19 @@ webpack_environment.plugins.push(
 )
 
 module.exports = webpack_environment;
+
+const merge = require('webpack-merge')
+const sassLoader = environment.loaders.get("sass");
+const cssLoader = environment.loaders.get("css");
+
+sassLoader.use.map(function(loader) {
+  if (loader.loader === "css-loader") {
+    loader.options = merge(loader.options, { sourceMap: false });
+  }
+});
+
+cssLoader.use.map(function(loader) {
+  if (loader.loader === "css-loader") {
+    loader.options = merge(loader.options, { sourceMap: false });
+  }
+});
