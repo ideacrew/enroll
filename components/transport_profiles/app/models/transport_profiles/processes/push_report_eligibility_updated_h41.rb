e0907e09d15cea22f427d6cffdc7f4e07dd9af88
Zachary Landes <zach.g.landes@gmail.com>
2019-09-10 10:52:00 -0400

    adding push_report_eligiblity_updated_h41s spec

--- components/transport_profiles/app/models/transport_profiles/processes/push_report_eligibility_updated_h41.rb
+++ components/transport_profiles/app/models/transport_profiles/processes/push_report_eligibility_updated_h41.rb
@@ -4,13 +4,14 @@ module TransportProfiles
 
       def initialize(source_uri, gateway, destination_file_name: d_file_name, source_credentials: s_credentials)
         super("Distribute eligibility updated H41", gateway)
-
-        add_step(TransportProfiles::Steps::RouteTo.new(:report_eligibility_updated_h41s, source_uri, gateway, destination_file_name: destination_file_name, source_credentials: source_credentials))
+        add_step(TransportProfiles::Steps::RouteTo.new(:cms_h41_uploads_archive, source_uri, gateway, destination_file_name: destination_file_name, source_credentials: source_credentials))
+        add_step(TransportProfiles::Steps::RouteTo.new(:cms_h41_uploads, source_uri, gateway, destination_file_name: destination_file_name, source_credentials: source_credentials))
       end
 
       def self.used_endpoints
         [
-          :report_eligibility_updated_h41s
+          :cms_h41_uploads_archive,
+          :cms_h41_uploads
         ]
       end
     end
