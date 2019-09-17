7b61e6cb0ffab18319b4052cc650e78e771df479
Bill Transue <transue@gmail.com>
2019-09-17 12:29:29 -0400

    Add bin/webpack to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,6 +54,7 @@ namespace :assets do
           execute("cd #{release_path} && rm -rf node_modules && rm -f package-lock.json")
           execute("cd #{release_path} && nvm use 10 && yarn install")
           execute :rake, "assets:clobber"
+          execute("cd #{release_path} && ./bin/webpack")
           execute("cd #{release_path} && nvm use 10 && RAILS_ENV=production NODE_ENV=production bundle exec rake assets:precompile")
         end
       end
