0287712f50d51b1b3c6847489e82adf11a8108b1
utkarsh7989 <utkarsh7989@gmail.com>
2019-09-19 11:31:44 -0400

    WIP ref:42681 translations for in network cost

--- app/models/products/services/copay_after_deductible_service.rb
+++ app/models/products/services/copay_after_deductible_service.rb
@@ -15,12 +15,12 @@ module Products
         if record.co_insurance_in_network_tier_1.include?("Not Applicable")
           "You must meet the deductible first, then #{number} per prescription"
         elsif record.co_insurance_in_network_tier_1.include?("No Charge after deductible")
-          if record.qhp_cost_share_variance.medical_and_drug_deductible?
+          if record.qhp_cost_share_variance.medical_and_drug_deductible? #ticket_42681
             "You must meet the deductible first, then #{number} per prescription"
-          elsif record.qhp_cost_share_variance.separarate_drug_deductible? && DRUG_DEDUCTIBLE_OPTIONS.include?(record.visit_type)
-            "You must meet the separate drug deductible first, then #{number} per prescription"
-          elsif record.qhp_cost_share_variance.separarate_medical_deductible?
-            "You must meet the deductible first, then #{number} per visit."
+          elsif record.qhp_cost_share_variance.separarate_drug_deductible? && DRUG_DEDUCTIBLE_OPTIONS.include?(record.visit_type) #ticket_42681
+            "You must meet the separate drug deductible first, then #{number} per prescription."
+          else #ticket_42681
+            "You must meet the deductible first, then #{number} per visit"
           end
         elsif record.co_insurance_in_network_tier_1.gsub("%","").to_i == 100 #ticket_42680
           "You must meet the deductible first, then #{number} per visit"
@@ -29,15 +29,19 @@ module Products
 
       def out_network_process
         number, _string = record.copay_out_of_network.split(/\ (?=[\w])/)
-        if record.co_insurance_out_of_network.gsub("%","").to_i == 100 #ticket_42680
+        if record.co_insurance_out_of_network.gsub("%","").to_i == 100 #ticket_42681
           "You must meet the out-of-network deductible first, then #{number} per visit"
-        #WIP
-        # elsif NO_CHARGE.include?(record.co_insurance_out_of_network)
-        #     NO_CHARGE
-        # elsif record.co_insurance_out_of_network.gsub("%","").to_i.zero?
-        #   NO_CHARGE
-        # elsif record.co_insurance_out_of_network.gsub("%","").to_i == 100
-        #   "Not covered. You are responsible for the full cost"
+        elsif record.co_insurance_out_of_network.include?("No Charge after deductible")
+          #WIP
+          # if NO_OUT_OF_NETWORK_DEDUCTIBLE.include?(record.visit_type) && EXPECTED_SERVICES.include?(record.visit_type) #ticket_42681
+          #   "You must meet the deductible first, then #{number} per visit."
+          # elsif NO_OUT_OF_NETWORK_DEDUCTIBLE.include?(record.visit_type) && DEVICES.include?(record.visit_type) #ticket_42681
+          #   "You must meet the deductible first, then #{number} per device."
+          if DEVICES.include?(record.visit_type) #ticket_42681
+            "You must meet the out-of-network deductible first, then #{number} per device."
+          else #ticket_42681
+            "You must meet the out-of-network deductible first, then #{number} per visit"
+          end
         end
       end
     end
