e1ecde8ad98455547482b9b8855a425fcc8546a7
utkarsh7989 <utkarsh7989@gmail.com>
2019-09-23 17:09:32 -0400

    ref:42690 param Copay per Stay with deductible/Not Applicable Coinsurance

--- app/models/products/services/copay_per_stay_service.rb
+++ app/models/products/services/copay_per_stay_service.rb
@@ -14,6 +14,8 @@ module Products
         number, _string = record.copay_in_network_tier_1.split(/\ (?=[\w])/)
         if record.copay_in_network_tier_1.include?("after deductible") #ticket_42688
           "You must meet the deductible first, then #{number} per stay." if record.co_insurance_in_network_tier_1.include?("Not Applicable") || record.co_insurance_in_network_tier_1.delete("%").to_i == 100 #tickets 42694, 42688
+        elsif record.copay_in_network_tier_1.include?("with deductible") #ticket_42688
+          "You pay #{number} copay per stay. You are also responsible for any remaining allowed charges, which will accrue towards your yearly deductible." if record.co_insurance_in_network_tier_1.include?("Not Applicable") #tickets_42690
         elsif record.co_insurance_in_network_tier_1.include?("Not Applicable") #ticket_42686
           "#{number} copay per stay"
         end
@@ -21,9 +23,11 @@ module Products
 
       def out_network_process
         number, _string = record.copay_out_of_network.split(/\ (?=[\w])/)
-        return unless record.copay_out_of_network.include?("after deductible")
-
-        "You must first meet the out-of-network deductible, then #{number} per stay." if record.co_insurance_out_of_network.include?("Not Applicable") || record.co_insurance_out_of_network.delete("%").to_i == 100 #tickets 42694, 42688
+        if record.copay_out_of_network.include?("after deductible")
+          "You must first meet the out-of-network deductible, then #{number} per stay." if record.co_insurance_out_of_network.include?("Not Applicable") || record.co_insurance_out_of_network.delete("%").to_i == 100 #tickets 42694, 42688
+        elsif record.copay_out_of_network.include?("with deductible")
+          "You pay #{number} copay per stay. You are also responsible for any remaining allowed charges, which will accrue towards your yearly out-of-network deductible." if record.co_insurance_out_of_network.include?("Not Applicable") #tickets_42690
+        end
       end
 
     end
--- spec/models/products/services/copay_per_stay_service_spec.rb
+++ spec/models/products/services/copay_per_stay_service_spec.rb
@@ -36,6 +36,17 @@ describe Products::Services::CopayPerStayService do
         expect(Products::Services::CopayPerStayService.new(service_visit).in_network_process).to eq "You must meet the deductible first, then #{amount} per stay."
       end
     end
+
+    context "$[PARAM] Copay per Stay with deductible/Not Applicable Coinsurance" do
+      let(:service_visit) do
+        build(:products_qhp_service_visit, copay_in_network_tier_1: "#{amount} Copay per Stay with deductible", co_insurance_in_network_tier_1: not_applicable)
+      end
+      let(:result) {"You pay #{amount} copay per stay. You are also responsible for any remaining allowed charges, which will accrue towards your yearly deductible."}
+
+      it "should return translated result" do
+        expect(Products::Services::CopayPerStayService.new(service_visit).in_network_process).to eq result
+      end
+    end
   end
 
   context "Out of Network Costs" do
@@ -56,6 +67,17 @@ describe Products::Services::CopayPerStayService do
 
       it "should return translated result" do
         expect(Products::Services::CopayPerStayService.new(service_visit).out_network_process).to eq "You must first meet the out-of-network deductible, then #{amount} per stay."
+      end
+    end
+
+    context "$[PARAM] Copay per Stay with deductible/Not Applicable Coinsurance" do
+      let(:service_visit) do
+        build(:products_qhp_service_visit, copay_out_of_network: "#{amount} Copay per Stay with deductible", co_insurance_out_of_network: not_applicable)
+      end
+      let(:result) {"You pay #{amount} copay per stay. You are also responsible for any remaining allowed charges, which will accrue towards your yearly out-of-network deductible."}
+
+      it "should return translated result" do
+        expect(Products::Services::CopayPerStayService.new(service_visit).out_network_process).to eq result
       end
     end
   end
