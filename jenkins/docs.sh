adc9ab0c6ee540054b57067c0579554a5e3a5594
Trey Evans <tevans@ip-192-168-254-7.ec2.internal>
2019-09-30 13:26:18 -0400

    Add angular coverage documentation.

--- jenkins/docs.sh
+++ jenkins/docs.sh
@@ -5,13 +5,14 @@ base="$( dirname "${BASH_SOURCE[0]}" )/.."
 cd $base
 root=`pwd -P`
 
-rm -Rf ./doc ./rubocop ./angular_documentation rdocs.zip
+rm -Rf ./doc ./rubocop ./angular_documentation ./angular_coverage rdocs.zip
 
 bundle exec yardoc
 bundle exec rubocop -f h -o rubocop/index.html
 
 nvm use 10
 yarn install
+yarn run ng test --code-coverage
 yarn run angular-docs
 
-zip -r rdocs.zip doc rubocop angular_documentation
+zip -r rdocs.zip doc rubocop angular_documentation angular_coverage
