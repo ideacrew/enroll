585ffbecd532ec006d4af2fa725c5bb6495bf18b
Prem Shaganti <prem.shaganti@dc.gov>
2019-10-03 14:17:36 -0400

    ref:43737 Filter the renewal CSR product based given input CSV sheet

--- spec/shared_contexts/enrollment.rb
+++ spec/shared_contexts/enrollment.rb
@@ -46,7 +46,11 @@ RSpec.shared_context "setup families enrollments", :shared_context => :metadata 
                       renewal_product: renewal_individual_health_product)
   end
 
-  let!(:active_csr_87_product) {FactoryBot.create(:active_csr_87_product, renewal_product: renewal_csr_87_product)}
+  let!(:active_csr_87_product) do
+    FactoryBot.create(:active_csr_87_product, renewal_product: renewal_csr_87_product).tap do |product|
+      product.hios_base_id = product.hios_id.split("-").first
+    end
+  end
 
   let!(:renewal_individual_health_product) do
     FactoryBot.create(:benefit_markets_products_health_products_health_product,
@@ -55,7 +59,11 @@ RSpec.shared_context "setup families enrollments", :shared_context => :metadata 
                       csr_variant_id: '01')
   end
 
-  let!(:renewal_csr_87_product) {FactoryBot.create(:renewal_csr_87_product)}
+  let!(:renewal_csr_87_product) do
+    FactoryBot.create(:renewal_csr_87_product).tap do |product|
+      product.hios_base_id = product.hios_id.split("-").first
+    end
+  end
 
   before do
     renewal_individual_health_product.update_attributes!(hios_id: active_individual_health_product.hios_id,
