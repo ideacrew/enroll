4a5f1c77f4a7d19e3b59e8434f63e4b013c27e33
Trey Evans <tevans@Just-Plain-Awful.local>
2019-10-28 15:20:01 -0400

    Custom event polyfill.

--- app/javascript/incremental_angular/polyfills.ts
+++ app/javascript/incremental_angular/polyfills.ts
@@ -38,7 +38,8 @@ import 'core-js/es7/string';
 
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-(window as any).__Zone_enable_cross_context_check = true;
+
+import 'custom-event-polyfill';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -77,6 +78,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import './zone-flags';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
