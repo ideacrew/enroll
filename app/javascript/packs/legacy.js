3f0ae5e84a80b806e5c02755de1bde5bb818dd3b
Trey Evans <tevans@Just-Plain-Awful.local>
2019-10-28 20:19:39 -0400

    Use exported module to namespace to page.

--- app/javascript/packs/legacy.js
+++ app/javascript/packs/legacy.js
@@ -1,7 +1,10 @@
 import 'core-js/es6/array';
 import '../polyfills/append';
+import '../polyfills/foreach';
 import { InitGroupSelection } from "../legacy/group_selection.js";
 import * as legacyBenefitApplications from "../legacy/benefit_application.js";
+import { MetalLevelSelect } from "../legacy/metal_level_select";
 InitGroupSelection();
 global.calculateEmployerContributions = legacyBenefitApplications.calculateEmployerContributions;
 global.calculateEmployeeCosts = legacyBenefitApplications.calculateEmployeeCosts;
+global.MetalLevelSelect = MetalLevelSelect;
