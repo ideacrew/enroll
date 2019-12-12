76b16d4466b04f774b9747934ace6505b2e6a23d
saipraveen18 <saipraveen.gudimetla@gmail.com>
2019-12-12 16:36:34 -0500

    refs #77854 add rake to delete incorrect invoices

--- app/data_migrations/delete_invoice_with_fein.rb
+++ app/data_migrations/delete_invoice_with_fein.rb
@@ -1,22 +1,36 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class DeleteInvoiceWithFein < MongoidMigrationTask
-  
-  def self.get_orgs(feins)
+
+  def get_orgs(feins)
     feins = feins.split(",")
-    Organization.where("fein" => {"$in" => feins}).entries
+    BenefitSponsors::Organizations::Organization.where("fein" => {"$in" => feins}).entries
   end
 
-  def self.migrate(feins)
+  def migrate
+    feins = ENV['feins']
     orgs = get_orgs(feins)
+
+    puts 'No organizations are present' if orgs.blank?
+
+    delete_initial_employer_invoice_notice(orgs)
     delete_invoice(orgs)
   end
 
-  def self.delete_invoice(orgs)
-    current_date =TimeKeeper.date_of_record
+  def delete_invoice(orgs)
+    current_date = TimeKeeper.date_of_record
     orgs.each do |org|
-      invoice = org.invoices.detect{|invoice| invoice.date.try(:month) ==current_date.month && invoice.date.try(:year) ==  current_date.year  }
-      invoice.destroy if invoice
+      invoice = org.employer_profile.documents.detect{|invoice| invoice.date.try(:month) == current_date.month && invoice.date.try(:year) == current_date.year  }
+      invoice&.destroy
+    end
+  end
+
+  def delete_initial_employer_invoice_notice(orgs)
+    orgs.each do |org|
+      documents = org.employer_profile.documents.select{|document| document.title == "Your_Invoice_Is_Now_Available_In_Your_Dc_Health_Link_Account" }
+      next if documents.blank?
+
+      documents.map(&:destroy)
     end
   end
 end
