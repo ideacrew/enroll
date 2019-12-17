a1df48f01ae1d1f92060345c8540af2d983e518c
harsha chalamala <chvreddy1015@gmail.com>
2019-12-17 15:32:14 -0500

    misplaced run all specs file

--- test_utilities/run_all_specs.sh
+++ test_utilities/run_all_specs.sh
@@ -12,8 +12,13 @@ for test_dir in `ls -1 $root/components/ | grep -v old_sponsored_benefits`; do
   echo $root/components/$test_dir
   cd $root/components/$test_dir
   bundle install
-  bundle exec rspec 
+  bundle exec rspec --fail-fast
+  ((result+=$?))
+  if [ $result -ne 0 ]; then
+    echo "ENGINE FAILED"
+    exit $result
+  fi
 done
 
-cd $root
-bundle exec rake parallel:spec[4]
+cd $root  cd $root
+bundle exec rake parallel:spec[3]
