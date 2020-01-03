7d5933fc61d35a3530bb9c6a41f16d1bd5c7a21a
saikumar9 <saikumarkotagiri9@gmail.com>
2020-01-03 10:58:19 -0500

    Refs #80540, refactor for responsible party enrollments

--- script/final_catastrophic_notice.rb
+++ script/final_catastrophic_notice.rb
@@ -35,47 +35,62 @@ notice_trigger = event_kind.notice_triggers.first
 CSV.open(file_2, "w", force_quotes: true) do |csv|
   csv << field_names
   @data_hash.each do |person_hbx_id , enrollments|
-    #To verify the aasm state
-    enrollment_aasm_states = []
-    enrollments.each do |enrollment|
-      enrollment_aasm_states << enrollment["State"]
-    end
-    enrollment_aasm_states.uniq!
+    begin
+      grouped_families = enrollments.inject({}) do |fam_hash, enrollment|
+                           ea_enrollment = HbxEnrollment.by_hbx_id(enrollment["Enrollment_Group_ID"]).first
+                           family_id = ea_enrollment.family.id.to_s
+                           if fam_hash[family_id].nil?
+                             fam_hash[family_id] = [enrollment]
+                           else
+                             fam_hash[family_id] << enrollment
+                           end
+                           fam_hash
+                         end
 
-    #To verify the market kind
-    enrollment_market_kinds = []
-    enrollments.each do |enrollment|
-      ea_enrollment = HbxEnrollment.by_hbx_id(enrollment["Enrollment_Group_ID"]).first
-      enrollment_market_kinds << !ea_enrollment.is_shop? if ea_enrollment.present?
-    end
+      grouped_families.each do |family_id, enrollments_by_family|
+        #To verify the market kind
+        enrollment_market_kinds = enrollments_by_family.inject([]) do |market_kinds, enrollment|
+                                    ea_enrollment = HbxEnrollment.by_hbx_id(enrollment["Enrollment_Group_ID"]).first
+                                    @family = ea_enrollment.family
+                                    market_kinds << !ea_enrollment.is_shop? if ea_enrollment.present?
+                                    market_kinds
+                                  end
 
-    #Reject if aasm_state is CANCELED and also reject if market kind is SHOP
-    if (enrollment_aasm_states.count > 1 || ( enrollment_aasm_states.count == 1 && enrollment_aasm_states.first != "canceled")) && !(enrollment_market_kinds.include? false)
-      person = Person.where(:hbx_id => person_hbx_id).first
-      consumer_role = person.consumer_role if person.present?
-      if person.present? && consumer_role.present?
-        begin
-          builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
-                        template: notice_trigger.notice_template,
-                        subject: event_kind.title,
-                        event_name: 'final_catastrophic_plan',
-                        mpi_indicator: notice_trigger.mpi_indicator
-                        }.merge(notice_trigger.notice_trigger_element_group.notice_peferences)
-                        )
-          builder.deliver
+        #To verify the aasm state
+        enrollment_aasm_states = enrollments_by_family.inject({}) do |states, enrollment|
+                                   states << enrollment["State"]
+                                 end.uniq!
 
-          csv << [
-            person.hbx_id,
-            person.first_name,
-            person.last_name
-          ]
-          puts "CAT Notice is sent to primary person with hbx_id: #{person.hbx_id}" unless Rails.env.test?
-        rescue Exception => e
-          puts "Unable to deliver to #{person.hbx_id} for the following error #{e.backtrace}" unless Rails.env.test?
+        #Reject if aasm_state is CANCELED and also reject if market kind is SHOP
+        if (enrollment_aasm_states.count > 1 || ( enrollment_aasm_states.count == 1 && enrollment_aasm_states.first != "canceled")) && !(enrollment_market_kinds.include? false)
+          person = @family.primary_person
+          consumer_role = person.consumer_role if person.present?
+          if person.present? && consumer_role.present?
+            begin
+              builder = notice_trigger.notice_builder.camelize.constantize.new(consumer_role, {
+                          template: notice_trigger.notice_template,
+                          subject: event_kind.title,
+                          event_name: 'final_catastrophic_plan',
+                          mpi_indicator: notice_trigger.mpi_indicator
+                        }.merge(notice_trigger.notice_trigger_element_group.notice_peferences))
+              builder.deliver
+
+              csv << [
+                person.hbx_id,
+                person.first_name,
+                person.last_name
+              ]
+              puts "CAT Notice is sent to primary person with hbx_id: #{person.hbx_id}" unless Rails.env.test?
+            rescue Exception => e
+              puts "Unable to deliver to #{person.hbx_id} for the following error #{e.backtrace}" unless Rails.env.test?
+            end
+          else
+            puts "No Person or No consumer role exists for hbx_id: #{person_hbx_id}" unless Rails.env.test?
+          end
         end
-      else
-        puts "No Person or No consumer role exists for hbx_id: #{person_hbx_id}" unless Rails.env.test?
       end
+    rescue => error
+      puts error.backtrace unless Rails.env.test?
     end
   end
 end
