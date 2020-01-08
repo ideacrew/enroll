0e2e2421abd8f9595568c1ba867bdb17e317b8ee
Chaz Haskins <chaz.haskins@gmail.com>
2020-01-08 10:46:16 -0500

    Added new cucumbers

--- features/support/worlds/hbx_admin.rb
+++ features/support/worlds/hbx_admin.rb
@@ -2,7 +2,7 @@ module HbxAdminWorld
   def hbx_admin(*traits)
     p_staff=Permission.create(name: 'hbx_staff', modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
       send_broker_agency_message: true, approve_broker: true, approve_ga: true,
-      modify_admin_tabs: true, view_admin_tabs: true)
+      modify_admin_tabs: true, view_admin_tabs: true, can_view_username_and_email: true, can_reset_password: true, can_lock_unlock: true)
     attributes = traits.extract_options!
     @hbx_admin ||= FactoryGirl.create :user, *traits, attributes
     hbx_profile = FactoryGirl.create :hbx_profile
