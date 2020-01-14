007fa6f738e6a750fc287b2e6fecd5a76e63c607
saipraveen18 <saipraveen.gudimetla@gmail.com>
2020-01-14 15:17:50 -0500

    #28697 refactor mongo query

--- app/controllers/employers/employer_profiles_controller.rb
+++ app/controllers/employers/employer_profiles_controller.rb
@@ -193,7 +193,7 @@ class Employers::EmployerProfilesController < Employers::EmployersController
 
   def export_census_employees
     respond_to do |format|
-      format.csv { send_data @employer_profile.get_census_data, filename: "#{@employer_profile.legal_name.parameterize.underscore}_census_employees_#{TimeKeeper.date_of_record}.csv" }
+      format.csv { send_data @employer_profile.get_census_employees, filename: "#{@employer_profile.legal_name.parameterize.underscore}_census_employees_#{TimeKeeper.date_of_record}.csv" }
     end
   end
 
