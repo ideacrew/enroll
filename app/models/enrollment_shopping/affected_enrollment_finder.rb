61f6c1999e6092fe3fec3669294dbfa51866708a
Raghuram Ganjala <raghuramg83@gmail.com>
2020-01-15 14:25:48 -0500

    compare ids instead of instances

--- app/models/enrollment_shopping/affected_enrollment_finder.rb
+++ app/models/enrollment_shopping/affected_enrollment_finder.rb
@@ -7,7 +7,7 @@ module EnrollmentShopping
       family = enrollment.family
 
       family.active_household.hbx_enrollments.show_enrollments_sans_canceled.by_coverage_kind(enrollment.coverage_kind).select do |hbx_enrollment|
-        (hbx_enrollment.sponsored_benefit_package.benefit_application == enrollment.sponsored_benefit_package.benefit_application) && 
+        (hbx_enrollment.sponsored_benefit_package.benefit_application.id == enrollment.sponsored_benefit_package.benefit_application.id) && 
         (hbx_enrollment.active_during?(enrollment.effective_on) || hbx_enrollment.effective_on > enrollment.effective_on) && 
         (hbx_enrollment.id != enrollment.id)
       end
