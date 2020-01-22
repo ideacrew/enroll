0b271091b4f6d6e66be091b085b881649bc316b0
saikumar9 <saikumarkotagiri9@gmail.com>
2020-01-22 09:48:33 -0500

    Refs #71268, Fixed code to look for document with subject I-571 (Refugee Travel Document) for I571 document

--- spec/views/events/lawful_presence/vlp_verification_request.xml.haml_spec.rb
+++ spec/views/events/lawful_presence/vlp_verification_request.xml.haml_spec.rb
@@ -52,5 +52,40 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
         expect(rendered).not_to include('<document_I551_receipt_number>receipt_numbr</document_I551_receipt_number>')
       end
     end
+
+    context 'document type: I571' do
+      let!(:i571_vlp_document) do
+        vlp_doc = FactoryBot.build(:vlp_document,
+                                   subject: subject,
+                                   receipt_number: 'receipt_numbr')
+        individual.consumer_role.vlp_documents << vlp_doc
+        individual.consumer_role.vlp_documents.map(&:save!)
+        individual.consumer_role.vlp_documents.last
+      end
+
+      before do
+        render :template => 'events/lawful_presence/vlp_verification_request.xml', :locals => { :individual => individual, :coverage_start_date => Time.zone.today }
+      end
+
+      context 'invalid case' do
+        let(:subject) { 'I-571 (Refugee Travel Document)' }
+
+        it 'should be schema valid' do
+          expect(validate_with_schema(Nokogiri::XML(rendered))).to be_empty
+        end
+
+        it 'should set value true for element has_document_I571' do
+          expect(rendered).to include('<has_document_I571>true</has_document_I571>')
+        end
+      end
+
+      context 'invalid case' do
+        let(:subject) { 'I-551 (Permanent Resident Card)' }
+
+        it 'should not set value true for element has_document_I571' do
+          expect(rendered).not_to include('<has_document_I571>true</has_document_I571>')
+        end
+      end
+    end
   end
 end
