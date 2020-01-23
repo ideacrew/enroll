8fd1c094734b740e4fc060dee87f5910409da6b0
saikumar9 <saikumarkotagiri9@gmail.com>
2020-01-23 15:46:11 -0500

    Refs #71269, Replaced receipt_number with card_number to send missing info for document I-766 Employment Authorization Card

--- spec/views/events/lawful_presence/vlp_verification_request.xml.haml_spec.rb
+++ spec/views/events/lawful_presence/vlp_verification_request.xml.haml_spec.rb
@@ -16,7 +16,7 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
         end
 
         before :each do
-          render :template => 'events//lawful_presence/vlp_verification_request.xml', :locals => { :individual => individual, :coverage_start_date => Time.zone.today }
+          render template: 'events/lawful_presence/vlp_verification_request.xml', locals: { individual: individual, coverage_start_date: Time.zone.today }
         end
 
         it 'should be schema valid' do
@@ -37,7 +37,7 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
       end
 
       before do
-        render :template => 'events/lawful_presence/vlp_verification_request.xml', :locals => { :individual => individual, :coverage_start_date => Time.zone.today }
+        render template: 'events/lawful_presence/vlp_verification_request.xml', locals: { individual: individual, coverage_start_date: Time.zone.today }
       end
 
       it 'should be schema valid' do
@@ -64,7 +64,7 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
       end
 
       before do
-        render :template => 'events/lawful_presence/vlp_verification_request.xml', :locals => { :individual => individual, :coverage_start_date => Time.zone.today }
+        render template: 'events/lawful_presence/vlp_verification_request.xml', locals: { individual: individual, coverage_start_date: Time.zone.today }
       end
 
       context 'invalid case' do
@@ -85,6 +85,35 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
         it 'should not set value true for element has_document_I571' do
           expect(rendered).not_to include('<has_document_I571>true</has_document_I571>')
         end
+      end
+    end
+
+    context 'document type: I766' do
+      let!(:i766_vlp_document) do
+        vlp_doc = FactoryBot.build(:vlp_document,
+                                   subject: 'I-766 (Employment Authorization Card)',
+                                   card_number: 'card_number00',
+                                   receipt_number: 'receipt_numbr',
+                                   expiration_date: TimeKeeper.date_of_record.end_of_year)
+        individual.consumer_role.vlp_documents << vlp_doc
+        individual.consumer_role.vlp_documents.map(&:save!)
+        individual.consumer_role.vlp_documents.last
+      end
+
+      before do
+        render template: 'events/lawful_presence/vlp_verification_request.xml', locals: { individual: individual, coverage_start_date: Time.zone.today }
+      end
+
+      it 'should be schema valid' do
+        expect(validate_with_schema(Nokogiri::XML(rendered))).to be_empty
+      end
+
+      it 'should set value for element receipt_number' do
+        expect(rendered).to include('<receipt_number>card_number00</receipt_number>')
+      end
+
+      it 'should not set the value of receipt_number to the element receipt_number' do
+        expect(rendered).not_to include('<receipt_number>receipt_numbr</receipt_number>')
       end
     end
   end
