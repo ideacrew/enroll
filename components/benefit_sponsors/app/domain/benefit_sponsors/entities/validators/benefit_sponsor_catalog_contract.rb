5fdd5df1740b07ede2979a719af3b88c9a67e304
Raghuram Ganjala <raghuramg83@gmail.com>
2020-02-03 17:23:36 -0500

    Updated operations for benefit sponsor catalog

--- components/benefit_sponsors/app/domain/benefit_sponsors/entities/validators/benefit_sponsor_catalog_contract.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/validators/benefit_sponsor_catalog_contract.rb
@@ -3,8 +3,38 @@
 module BenefitSponsors
   module Entities
     module Validators
-      class BenefitSponsorCatalogContract
+      class BenefitSponsorCatalogContract < ApplicationContract
 
+        params do
+          required(:effective_date).filled(:date)
+          required(:effective_period).value(Type::Duration)
+          required(:open_enrollment_period).value(Type::Duration)
+          required(:probation_period_kinds).array(:symbol)
+          optional(:benefit_application).maybe(:hash)
+          required(:product_packages).array(:hash)
+          required(:service_areas).array(:hash)
+        end
+
+        rule(:benefit_application) do
+          if key? && value
+            result = BenefitApplicationContract.call(value)
+            key.failure(text: "invalid benefit application", error: result.errors.to_h) if result && result.failure?
+          end
+        end
+
+        rule(:product_packages).each do
+          if key? && value 
+            result = ProductPackageContract.call(value)
+            key.failure(text: "invalid product package", error: result.errors.to_h) if result && result.failure?
+          end
+        end
+
+        rule(:service_areas).each do
+          if key? && value
+            result = ServiceAreaContract.call(value)
+            key.failure(text: "invalid service area", error: result.errors.to_h) if result && result.failure?
+          end
+        end
       end
     end
   end
