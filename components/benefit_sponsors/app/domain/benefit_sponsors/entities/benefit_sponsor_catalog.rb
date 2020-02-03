bc300297213f215651888a32d08d3f5569c35766
Raghuram Ganjala <raghuramg83@gmail.com>
2020-02-03 16:09:53 -0500

    Added domain folder for benefit sponsors

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/benefit_sponsor_catalog.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module BenefitSponsors
+  module Entities
+    class BenefitSponsorCatalog < Dry::Struct
+      transform_keys(&:to_sym)
+
+      attribute :effective_date,          Types::Strict::Date
+      attribute :effective_period,        Types::Duration
+      attribute :open_enrollment_period,  Types::Duration
+      attribute :probation_period_kinds,  Types::Strict::Array
+      attribute :benefit_application_id,  Types::Strict::String
+      attribute :product_packages,        Types::Strict::Array
+
+    end
+  end
+end
