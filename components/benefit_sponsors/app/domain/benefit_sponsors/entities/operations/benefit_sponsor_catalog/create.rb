5b6824ad840bec7be7a729d3ca2fd28da22b5763
Raghuram Ganjala <raghuramg83@gmail.com>
2020-02-03 17:23:36 -0500

    Updated operations for benefit sponsor catalog

--- components/benefit_sponsors/app/domain/benefit_sponsors/entities/operations/benefit_sponsor_catalog/create.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/operations/benefit_sponsor_catalog/create.rb
@@ -10,21 +10,27 @@ module BenefitSponsors
 
         class Create
 
-          attr_reader :benefit_sponsorship, :effective_date
+          # attr_reader :benefit_sponsorship, :effective_date
 
-          def call(benefit_sponsorship, effective_date)
-            @benefit_sponsorship = benefit_sponsorship
-            @effective_date = effective_date
+          # @params 
+          # def call(benefit_sponsorship, effective_date)
+          def call(params)
+            # @effective_date = params[:effective_date]
+
+            values = yield validate(params[:benefit_sponsorship])
+            benefit_sponsor_catalog = yield build(values[:benefit_sponsorship], params[:effective_date])
 
             # entity_attributes       = yield extract_attributes(sponosor_eligibility, contribution_model)
             # result                  = yield validate(entity_attributes)
-            sponsor_catalog = yield build(attributes)
+            sponsor_catalog = yield build(params[:benefit_sponsorship], )
             sponsor_catalog = yield validate(sponsor_catalog)
             sponsor_catalog = yield assign_default_contribution_model(sponsor_catalog)
             sponsor_catalog = yield create(sponsor_catalog)
 
             Success(sponsor_catalog)
           end
+          
+          private
 
           # def extract_attributes(sponosor_eligibility, contribution_model)
           #   attrs = {
@@ -33,21 +39,9 @@ module BenefitSponsors
           #     exception_granted: sponsor_eligibility[:flexibile_contribution_model_enabled],
           #     contribution_model: contribution_model.attributes.slice(:_id, :contribution_type, :effective_period, :eligibility_policies)
           #   }
-
           #   Success(attrs)
           # end
 
-          # def validate(input)
-          #   contract = BenefitMarkets::Entities::Validators::SponsorContributionModelCriteriaContract.new
-          #   Success(contract.call(input))
-          # end
-
-          def build
-            benefit_market = benefit_sponsorship.benefit_market
-            benefit_sponsor_catalog = benefit_market.benefit_sponsor_catalog_for(benefit_sponsorship.recorded_service_areas, effective_date)
-            
-            Success(benefit_sponsor_catalog)
-          end
           
           def assign_default_contribution_model(benefit_sponsor_catalog)
             enrollment_eligibility = benefit_sponsorship.enrollment_eligibility_for(effective_date)
@@ -63,7 +57,27 @@ module BenefitSponsors
             Success(contribution_model_criteria)
           end
 
-          private
+          def validate(params)
+            contract = BenefitMarkets::Entities::Validators::BenefitSponsorshipContract.new
+            
+            Success(contract.call(params))
+          end
+          # benefit_sponsorship
+          # effective_date
+          # benefit_market
+
+          def get_benefit_market_catalog(values)
+            benefit_market.benefit_market_catalog_for()
+          end
+
+          def build(values)
+            benefit_market = benefit_sponsorship.benefit_market
+            benefit_market.benefit_market_catalog_for(effective_date)
+
+            benefit_sponsor_catalog = benefit_market.benefit_sponsor_catalog_for(benefit_sponsorship.recorded_service_areas, effective_date)
+            
+            Success(benefit_sponsor_catalog)
+          end
 
           def business_policy_for(benefit_sponsor_catalog, enrollment_eligibility)
             eligibility_policy = benefit_sponsor_catalog.enrollment_eligibility_policy
