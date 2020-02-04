657d005feab425b7615fd813decffd9ee8f5fb39
Battula <vrbattula@outlook.com>
2020-02-04 16:48:31 -0500

    Added more entities and contracts.

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/benefit_packages/benefit_package.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module BenefitSponsors
+  module Entities
+    module BenefitPackages
+      class BenefitPackage < Dry::Struct
+        transform_keys(&:to_sym)
+
+        attribute :title, Types::Strict::String
+        attribute :description, Types::Strict::String
+        attribute :probation_period_kind, Types::Strict::Symbol
+        attribute :is_default, Types::Strict::Bool
+        attribute :is_active, Types::Strict::Bool
+        attribute :predecessor_id, Types::Strict::String
+
+        attribute :sponsored_benefits, SponsoredBenefits::SponsoredBenefit
+
+      end
+    end
+  end
+end
