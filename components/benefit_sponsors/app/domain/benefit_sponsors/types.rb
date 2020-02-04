6c18a4f373c67d17d62e172d4ce207c83c32659d
Raghuram Ganjala <raghuramg83@gmail.com>
2020-02-04 16:04:26 -0500

    Fixes wrong references for Duration type

--- components/benefit_sponsors/app/domain/benefit_sponsors/types.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/types.rb
@@ -2,24 +2,25 @@
 require 'uri'
 require 'cgi'
 require 'dry-types'
-module Types
-  include Dry.Types()
-  include Dry::Logic
+module BenefitSponsors
+  module Types
+    include Dry.Types()
+    include Dry::Logic
 
-  Uri                 = Types.Constructor(::URI) { |val| ::URI.parse(val) }
-  Url                 = Uri
+    Uri                 = Types.Constructor(::URI) { |val| ::URI.parse(val) }
+    Url                 = Uri
 
-  Email               = Coercible::String.constrained(format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
-  Emails              = Array.of(Email)
-  CallableDateTime    = Types::DateTime.default { DateTime.now }
-  Duration            = Types.Constructor(ActiveSupport::Duration) {|val| ActiveSupport::Duration.build(val) }
-  PositiveInteger     = Coercible::Integer.constrained(gteq: 0)
-  HashOrNil           = Types::Hash | Types::Nil
-  StringOrNil         = Types::String | Types::Nil
+    Email               = Coercible::String.constrained(format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+    Emails              = Array.of(Email)
+    CallableDateTime    = Types::DateTime.default { DateTime.now }
+    Duration            = Types.Constructor(ActiveSupport::Duration) {|val| ActiveSupport::Duration.build(val) }
+    PositiveInteger     = Coercible::Integer.constrained(gteq: 0)
+    HashOrNil           = Types::Hash | Types::Nil
+    StringOrNil         = Types::String | Types::Nil
 
-  RequiredSymbol  = Types::Strict::Symbol.constrained(min_size: 2)
-  RequiredString  = Types::Strict::String.constrained(min_size: 1)
-  StrippedString  = String.constructor(->(val){ String(val).strip })
+    RequiredSymbol  = Types::Strict::Symbol.constrained(min_size: 2)
+    RequiredString  = Types::Strict::String.constrained(min_size: 1)
+    StrippedString  = String.constructor(->(val){ String(val).strip })
 
+  end
 end
-
