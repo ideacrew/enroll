316ab2191b47c446b6962f4d2f7fda4127a2c9a3
Battula <vrbattula@outlook.com>
2020-02-04 13:40:21 -0500

    Moved entities into their respective namespaces.

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/products/product_package.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module BenefitSponsors
+  module Entities
+    module Products
+      class ProductPackage < Dry::Struct
+        transform_keys(&:to_sym)
+
+        attribute :application_period,           Types::Duration
+        attribute :benefit_kind,                 Types::Strict::Symbol
+        attribute :product_kind,                 Types::Strict::Symbol
+        attribute :package_kind,                 Types::Strict::Symbol
+        attribute :title,                        Types::Strict::String
+        attribute :description,                  Types::Strict::String
+
+        attribute :products,                     Types::Products::Product
+        attribute :contribution_model,           Types::ContributionModels::ContributionModel
+        attribute :assigned_contribution_model,  Types::ContributionModels::ContributionModel
+        attribute :contribution_models,          Types::Array.of(ContributionModels::ContributionModel)
+        attribute :pricing_model,                Types::Products::PricingModel
+
+      end
+    end
+  end
+end
