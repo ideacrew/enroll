01191f481c9ac8bf06e5e0492d9e203e6113e995
Raghuram Ganjala <raghuramg83@gmail.com>
2020-02-04 12:12:51 -0500

    Added product_package related contracts

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/validators/pricing_unit_contract.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module BenefitSponsors
+  module Entities
+    module Validators
+      class PricingUnitContract < ApplicationContract
+
+        params do
+          required(:name).filled(:string)
+          required(:display_name).filled(:string)
+          required(:order).filled(:integer)
+        end
+      end
+    end
+  end
+end
