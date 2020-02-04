df919f63e817a7fc0ad7453e1ae3d03f7f10bb1a
Battula <vrbattula@outlook.com>
2020-02-04 16:48:31 -0500

    Added more entities and contracts.

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/sponsored_benefits/pricing_determination_tier.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module BenefitSponsors
+  module Entities
+    module SponsoredBenefits
+      class PricingDeterminationTier < Dry::Struct
+        transform_keys(&:to_sym)
+
+        attribute :pricing_unit_id, Types::Strict::String
+        attribute :price, Types::Strict::Float
+
+      end
+    end
+  end
+end
