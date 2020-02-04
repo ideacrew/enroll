4074645db972e96ee6750d1d40a8ebf572cc6655
Battula <vrbattula@outlook.com>
2020-02-04 11:44:29 -0500

    Added entities

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/document.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module BenefitSponsors
+  module Entities
+    class Document < Dry::Struct
+      transform_keys(&:to_sym)
+
+      attribute :title,               Types::Strict::String
+      attribute :creator,             Types::Strict::String
+      attribute :subject,             Types::Strict::String
+      attribute :description,         Types::Strict::String
+      attribute :publisher,           Types::Strict::String
+      attribute :contributor,         Types::Strict::String
+      attribute :date,                Types::Strict::Duration
+      attribute :type,                Types::Strict::String
+      attribute :format,              Types::Strict::String
+      attribute :identifier,          Types::Strict::String
+      attribute :source,              Types::Strict::String
+      attribute :language,            Types::Strict::String
+      attribute :relation,            Types::Strict::String
+      attribute :coverage,            Types::Strict::String
+      attribute :rights,              Types::Strict::String
+      attribute :tags,                Types::Strict::Array
+      attribute :size,                Types::Strict::String
+    end
+  end
+end
