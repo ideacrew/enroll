3d82c5a2a9fcdc456dd3b82e440792979116cca3
harshaellanki <h_vardhan@yahoo.com>
2020-02-05 12:31:00 -0500

    WIP: Adding specs and moved SponsoredBenefits entities and validators to BenefitSponsors from BenefitMarkets

--- components/benefit_sponsors/app/domain/benefit_sponsors/entities/sponsored_benefits/sponsor_contribution.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/sponsored_benefits/sponsor_contribution.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-module BenefitMarkets
+module BenefitSponsors
   module Entities
     module SponsoredBenefits
       class SponsorContribution < Dry::Struct
         transform_keys(&:to_sym)
 
-        attribute :contribution_levels, Types::Array.of(SponsoredBenefits::ContributionLevels)
+        attribute :contribution_levels, Types::Array.of(SponsoredBenefits::ContributionLevel)
       end
     end
   end
