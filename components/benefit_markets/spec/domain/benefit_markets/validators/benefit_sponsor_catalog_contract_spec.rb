0789984a50242a88836a0e896048e15a7523b151
harshaellanki <h_vardhan@yahoo.com>
2020-02-06 15:05:53 -0500

    moved contracts to the entity structure

--- components/benefit_markets/spec/domain/benefit_markets/validators/benefit_sponsor_catalog_contract_spec.rb
+++ components/benefit_markets/spec/domain/benefit_markets/validators/benefit_sponsor_catalog_contract_spec.rb
@@ -16,13 +16,34 @@ RSpec.describe BenefitMarkets::Validators::BenefitSponsorCatalogContract do
   let(:missing_params)          { {effective_date: effective_date, effective_period: effective_period, open_enrollment_period: open_enrollment_period, service_areas: service_areas} }
   let(:all_params)              { {} }
 
-  let(:error_message) { { :probation_period_kinds => ["is missing"], :benefit_application => ["is missing"], :product_packages => ["is missing"] } }
+  let(:error_message) { { :probation_period_kinds => ["is missing"], :product_packages => ["is missing"] } }
 
 
   context "Given invalid required parameters" do
-    # context "sending with missing parameters should fail validation with :errors" do
-    #   it { expect(subject.call(missing_params).failure?).to be_truthy }
-    #   it { expect(subject.call(missing_params).errors.to_h).to eq error_message }
-    # end
+    context "sending with missing parameters should fail validation with :errors" do
+      it { expect(subject.call(missing_params).failure?).to be_truthy }
+      it { expect(subject.call(missing_params).errors.to_h).to eq error_message }
+    end
+  end
+
+  context "Given valid required parameters" do
+
+    let(:required_params)     { missing_params.merge({probation_period_kinds: probation_period_kinds, product_packages: product_packages }) }
+
+    context "with a required only" do
+      it "should pass validation" do
+        expect(subject.call(required_params).success?).to be_truthy
+        expect(subject.call(required_params).to_h).to eq required_params
+      end
+    end
+
+    context "with all params" do
+      let(:all_params)          { required_params.merge({benefit_application: benefit_application}) }
+
+      it "should pass validation" do
+        expect(subject.call(all_params).success?).to be_truthy
+        expect(subject.call(all_params).to_h).to eq all_params
+      end
+    end
   end
 end
