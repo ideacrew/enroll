c71fc702da952c2f64c0120f422d6f80e04353d9
harshaellanki <h_vardhan@yahoo.com>
2020-02-07 16:47:01 -0500

    wip: adding specs for contracts

--- components/benefit_markets/app/domain/benefit_markets/validators/member_relationship_contract.rb
+++ components/benefit_markets/app/domain/benefit_markets/validators/member_relationship_contract.rb
@@ -7,9 +7,9 @@ module BenefitMarkets
       params do
         required(:relationship_name).filled(:symbol)
         required(:relationship_kinds).array(:hash)
-        optional(:age_threshold).value(:integer)
-        optional(:age_comparison).value(:symbol)
-        optional(:disability_qualifier).value(:bool)
+        optional(:age_threshold).maybe(:integer)
+        optional(:age_comparison).maybe(:symbol)
+        optional(:disability_qualifier).maybe(:bool)
       end
     end
   end
