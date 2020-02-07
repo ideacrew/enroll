1f72ce07b34552ef99d3892206c7956231f900fd
Battula <vrbattula@outlook.com>
2020-02-06 22:09:38 -0500

    Fix CreateBenefitSponsorCatalog operation.

--- components/benefit_markets/app/domain/benefit_markets/operations/create_benefit_sponsor_catalog.rb
+++ components/benefit_markets/app/domain/benefit_markets/operations/create_benefit_sponsor_catalog.rb
@@ -22,6 +22,11 @@ module BenefitMarkets
         sponsor_catalog       = yield create_sponsor_catalog(sponsor_catalog_attrs.values.data)
 
 
+        values                = yield validate(params)
+        sponsor_catalog_hash  = yield construct_sponsor_catalog(values)
+        sponsor_catalog_attrs = yield validate_sponsor_catalog(sponsor_catalog_hash)
+        sponsor_catalog       = yield create_sponsor_catalog(sponsor_catalog_attrs.values.data)
+        
         Success(sponsor_catalog)
       end
 
@@ -33,9 +38,10 @@ module BenefitMarkets
         Success(params)
       end
 
-      def create_sponsor_catalog(values)
-        benefit_market_catalog = values[:benefit_sponsor_catalog]
-        enrollment_dates = enrollment_dates_for(values)
+      def construct_sponsor_catalog(values)
+        benefit_market_catalog = values[:benefit_market_catalog]
+
+        enrollment_dates = enrollment_dates_for(values)[:enrollment_dates]
 
         sponsor_catalog_hash = {
           effective_date: values[:effective_date],
@@ -50,49 +56,29 @@ module BenefitMarkets
         Success(sponsor_catalog_hash)
       end
 
-      def filter_product_packages(service_areas, product_package)
-        product_package_hash = product_package.except(:products)
-        product_package_hash[:products] = BenefitMarkets::Operations::BenefitMarketCatalog::ScopeProductsByServiceArea.new.call({
-          effective_date: values[:effective_date], 
-          market_kind: values[:market_kind], 
-          package_kind: product_package[:package_kind]
-        })
-
-      #   {
-      #   title: market_product_package.title,
-      #   description: market_product_package.description,
-      #   product_kind: market_product_package.product_kind,
-      #   benefit_kind: market_product_package.benefit_kind, 
-      #   package_kind: market_product_package.package_kind
-      # )
-
-      # product_package.application_period = benefit_sponsor_catalog.effective_period
-      # product_package.contribution_model = market_product_package.contribution_model.create_copy_for_embedding
-      # product_package.pricing_model = market_product_package.pricing_model.create_copy_for_embedding
-      # product_package.products = market_product_package.load_embedded_products(benefit_sponsor_catalog.service_areas, @effective_date)
-      # product_package
-
-        # }
-        benefit_market_catalog[:product_packages].collect{}
-      end
-
       def build_product_packages(values)
         values[:benefit_market_catalog][:product_packages].collect do |product_package|
           BenefitMarkets::Operations::Products::CreateProductPackage.new.call(
             values.merge({product_package: product_package, application_period: @enrollment_dates[:effective_period]})
-          )
+          ).success
         end
       end
 
-      def validate(params)
-        contract = BenefitMarkets::Entities::Validators::BenefitSponsorshipContract.new
+      def validate_sponsor_catalog(catalog_hash)
+        contract = BenefitMarkets::Validators::BenefitSponsorCatalogContract.new
 
-        Success(contract.call(params))
+        Success(contract.call(catalog_hash))
+      end
+
+      def create_sponsor_catalog(attrs)
+        catalog_entity = BenefitMarkets::Entities::BenefitSponsorCatalog.new(attrs)
+
+        Success(catalog_entity)
       end
 
       def enrollment_dates_for(values)
         @enrollment_dates if defined? @enrollment_dates
-        @enrollment_dates = BenefitMarkets::Operations::BenefitMarketCatalog::GetEnrollmentDates.new.call(effective_date: values[:effective_date], market_kind: values[:market_kind])
+        @enrollment_dates = BenefitMarkets::Operations::BenefitMarketCatalog::GetEnrollmentDates.new.call(effective_date: values[:effective_date], market_kind: values[:market_kind]).success
       end
     end
   end
