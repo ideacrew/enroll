f6a242527be16d8329c75e236e2dfc6c8bc589fe
Battula <vrbattula@outlook.com>
2020-02-10 13:58:45 -0500

    Assign products to benefit sponsor catalog entity.

--- components/benefit_markets/app/domain/benefit_markets/entities/products/product.rb
+++ components/benefit_markets/app/domain/benefit_markets/entities/products/product.rb
@@ -7,25 +7,25 @@ module BenefitMarkets
         transform_keys(&:to_sym)
 
         attribute :benefit_market_kind,   Types::Strict::Symbol
-        attribute :application_period,    Types::DateRange
-        attribute :hbx_id,                Types::Strict::String
+        attribute :application_period,    Types::CustomRange
+        attribute :hbx_id,                Types::String.optional.meta(omittable: true)
         attribute :title,                 Types::Strict::String
-        attribute :description,           Types::Strict::String
-        attribute :issuer_profile_id,     Types::Strict::String
+        attribute :description,           Types::String.optional
+        attribute :issuer_profile_id,     Types::Bson
         attribute :product_package_kinds, Types::Strict::Array
         attribute :kind,                  Types::Strict::Symbol
-        attribute :premium_ages,          Types::Range
+        attribute :premium_ages,          Types::CustomRange
         attribute :provider_directory_url,      Types::Strict::String
         attribute :is_reference_plan_eligible,  Types::Strict::Bool
         attribute :deductible, Types::Strict::String
         attribute :family_deductible, Types::Strict::String
-        attribute :issuer_assigned_id, Types::Strict::String
-        attribute :service_area_id, Types::Strict::String
-        attribute :network_information, Types::Strict::String
+        attribute :issuer_assigned_id, Types::String.optional.meta(omittable: true)
+        attribute :service_area_id, Types::Bson
+        attribute :network_information, Types::String.optional.meta(omittable: true)
         attribute :nationwide, Types::Strict::Bool
         attribute :dc_in_network, Types::Strict::Bool
 
-        attribute :sbc_document,   Document
+        attribute :sbc_document,   Document.meta(omittable: true)
         attribute :premium_tables, Types::Array.of(PremiumTable)
       end
     end
