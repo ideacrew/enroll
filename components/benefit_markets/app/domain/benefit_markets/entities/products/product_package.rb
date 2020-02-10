65768718a1900df9ea799cd07d8055d2b5c85bc0
Battula <vrbattula@outlook.com>
2020-02-10 13:58:45 -0500

    Assign products to benefit sponsor catalog entity.

--- components/benefit_markets/app/domain/benefit_markets/entities/products/product_package.rb
+++ components/benefit_markets/app/domain/benefit_markets/entities/products/product_package.rb
@@ -6,14 +6,14 @@ module BenefitMarkets
       class ProductPackage < Dry::Struct
         transform_keys(&:to_sym)
 
-        attribute :application_period,           Types::DateRange
+        attribute :application_period,           Types::CustomRange
         attribute :benefit_kind,                 Types::Strict::Symbol
         attribute :product_kind,                 Types::Strict::Symbol
         attribute :package_kind,                 Types::Strict::Symbol
         attribute :title,                        Types::Strict::String
         attribute :description,                  Types::Strict::String
 
-        # attribute :products,                     Types::Array.of(BenefitMarkets::Entities::Products::Product)
+        attribute :products,                     Types::Array.of(BenefitMarkets::Entities::Products::Product)
         attribute :contribution_model,           BenefitMarkets::Entities::ContributionModels::ContributionModel
         attribute :assigned_contribution_model,  BenefitMarkets::Entities::ContributionModels::ContributionModel.meta(omittable: true)
         attribute :contribution_models,          Types::Array.of(BenefitMarkets::Entities::ContributionModels::ContributionModel)
