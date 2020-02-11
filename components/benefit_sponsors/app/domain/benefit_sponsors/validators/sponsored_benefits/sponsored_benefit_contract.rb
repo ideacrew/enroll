5776a7e13eef15262054556402d40772582f816e
harshaellanki <h_vardhan@yahoo.com>
2020-02-11 15:14:18 -0500

    added specs for validators in benefit_sponsors component

--- components/benefit_sponsors/app/domain/benefit_sponsors/validators/sponsored_benefits/sponsored_benefit_contract.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/validators/sponsored_benefits/sponsored_benefit_contract.rb
@@ -9,7 +9,6 @@ module BenefitSponsors
           required(:product_package_kind).filled(:symbol)
           required(:product_option_choice).filled(:string)
           required(:source_kind).filled(:symbol)
-
           required(:reference_product).filled(:hash)
           required(:sponsor_contribution).filled(:hash)
           required(:pricing_determinations).array(:hash)
@@ -18,21 +17,21 @@ module BenefitSponsors
 
         rule(:reference_product) do
           if key? && value
-            result = ::BenefitMarkets::Validators::ProductContract.call(value)
+            result = ::BenefitMarkets::Validators::Products::ProductContract.new.call(value)
             key.failure(text: "invalid reference product", error: result.errors.to_h) if result&.failure?
           end
         end
 
         rule(:sponsor_contribution) do
           if key? && value
-            result = SponsoredContributionContract.call(value)
+            result = SponsorContributionContract.new.call(value)
             key.failure(text: "invalid sponsor contribution", error: result.errors.to_h) if result&.failure?
           end
         end
 
         rule(:pricing_determinations).each do
           if key? && value
-            result = PricingDeterminationContract.call(value)
+            result = PricingDeterminationContract.new.call(value)
             key.failure(text: "invalid pricing determination tier", error: result.errors.to_h) if result&.failure?
           end
         end
