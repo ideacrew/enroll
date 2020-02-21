40f2a0d082ab25c5bc62c832f03187b7b619a934
username <user@email.com>
2020-02-21 12:59:51 -0800

    Fix permission. Allow hbx staff role

--- app/policies/angular_admin_application_policy.rb
+++ app/policies/angular_admin_application_policy.rb
@@ -1,6 +1,6 @@
 class AngularAdminApplicationPolicy < ApplicationPolicy
   def visit?
-    return false unless user.has_hbx_staff_role?
+    return true if user.has_hbx_staff_role?
     permission = user.person.hbx_staff_role.permission
     return false unless permission
     permission.approve_broker &&
@@ -11,6 +11,6 @@ class AngularAdminApplicationPolicy < ApplicationPolicy
       permission.can_access_user_account_tab &&
       permission.view_login_history &&
       permission.view_the_configuration_tab &&
-      permission.view_personal_info_page 
+      permission.view_personal_info_page
   end
 end
