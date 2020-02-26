dc6429686042ad6c6c260a888c404443a847e351
saikumar9 <saikumarkotagiri9@gmail.com>
2020-02-26 15:16:45 -0500

    Refs #85326, Case Management to be compliant with new V37 for edit DOB/SSN functionality

--- app/domain/operations/call_fed_hub.rb
+++ app/domain/operations/call_fed_hub.rb
@@ -9,41 +9,30 @@ module Operations
     send(:include, Dry::Monads[:result, :do])
 
     def call(person_id:, verification_type:)
-      person = yield fetch_person(person_id)
-      if verification_type == 'Immigration status'
-        latest_vlp_doc = yield fetch_latest_vlp_document(person)
-        yield validate_vlp_document(latest_vlp_doc)
-      end
+      person = if verification_type == 'Immigration status'
+                 yield verify_vlp_document(person_id)
+               else
+                 yield fetch_person(person_id)
+               end
+
       yield call_fed_hub(person, verification_type)
       assign_message(verification_type)
     end
 
     private
 
-    def fetch_person(person_id)
-      person = ::Person.find(person_id)
-      person ? Success(person) : Failure([:danger, 'Person not found'])
-    end
-
-    def fetch_latest_vlp_document(person)
-      vlp_doc = person.consumer_role.vlp_documents.asc(:created_at).last
-      vlp_doc ? Success(vlp_doc) : Failure([:danger, 'VLP Document not found'])
-    end
-
-    def validate_vlp_document(vlp_doc)
-      attributes = vlp_doc.attributes.merge({"expiration_date" => vlp_doc.expiration_date.to_s}).delete_if {|_k,v| v.blank?}
-      result = ::Validators::VlpV37Contract.new.call(attributes)
-      if result.failure?
-        errors = result.errors.to_h
-        message = if errors.values.flatten.include? "Invalid VLP Document type"
-                    "VLP document type is invalid: #{vlp_doc.subject}"
-                  else
-                    errors.keys.first.to_s.titlecase + ' ' + errors.values.flatten.first
-                  end
-        Failure([:danger, message])
+    def verify_vlp_document(person_id)
+      result = ::Operations::ValidateVlpDocument.new.call(person_id: person_id)
+      if result.success?
+        result
       else
-        Success(true)
+        Failure([:danger, result.failure])
       end
+    end
+
+    def fetch_person(person_id)
+      person = ::Person.where(_id: person_id).first
+      person ? Success(person) : Failure([:danger, 'Person not found'])
     end
 
     def call_fed_hub(person, verification_type)
