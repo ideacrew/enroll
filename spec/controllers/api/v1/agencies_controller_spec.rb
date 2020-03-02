747069c4370ced56b023b4d9d4f5280047143a23
Trey Evans <tevans@Just-Plain-Awful.local>
2020-03-02 16:21:26 -0500

    Add simplified primary query.

--- spec/controllers/api/v1/agencies_controller_spec.rb
+++ spec/controllers/api/v1/agencies_controller_spec.rb
@@ -12,4 +12,54 @@ RSpec.describe Api::V1::AgenciesController, :type => :controller, :dbclean => :a
       expect(response.status).to eq 200
     end
   end
+
+  describe "GET #primary_agency_staff, with no records" do
+    before :each do
+      sign_in(user)
+      get :primary_agency_staff
+    end
+
+    it "is successful" do
+      expect(response.status).to eq(200)
+    end
+
+    it "has and empty json response" do
+      expect(response.body).to eq("[]")
+    end
+  end
+
+  describe "GET #primary_agency_staff, with a broker" do
+    let(:broker_role) { FactoryBot.create(:broker_role, aasm_state: "active") }
+
+    before :each do
+      broker_role
+      sign_in(user)
+      get :primary_agency_staff
+    end
+
+    it "is successful" do
+      expect(response.status).to eq(200)
+    end
+
+    it "has no records" do
+      expect(response.body).not_to eq("[]")
+    end
+  end
+
+  describe "GET #primary_agency_staff, with a ga staff" do
+    let(:general_staff_role) {FactoryBot.create(:general_agency_staff_role, aasm_state: "active", is_primary: true)}
+    before :each do
+      general_staff_role
+      sign_in(user)
+      get :primary_agency_staff
+    end
+
+    it "is successful" do
+      expect(response.status).to eq(200)
+    end
+
+    it "has no records" do
+      expect(response.body).not_to eq("[]")
+    end
+  end
 end
