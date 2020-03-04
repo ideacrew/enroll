599b3f2abadcd681f56a45207d9562c3b91968d3
Nikhil S <nikhilks219@gmail.com>
2020-03-04 14:36:50 -0500

    69967: flickering cucumbers

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -195,7 +195,8 @@ Then(/click citizen no/) do
 end
 
 When(/click eligible immigration status yes/) do
-  find(:xpath, '//label[@for="person_eligible_immigration_status_true"]').click
+  find('label[for=person_eligible_immigration_status_true]', wait: 20).click
+  choose 'person_eligible_immigration_status_true', visible: false, allow_label_click: true
 end
 
 Then(/should find I-551 doc type/) do
