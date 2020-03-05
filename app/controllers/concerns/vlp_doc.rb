19be5bf9a0e992fb9423e47a788f348540d0e3ce
saikumar9 <saikumarkotagiri9@gmail.com>
2020-03-05 16:28:19 -0500

    Refs #85326, UI error messages display for Case Management for Admin Edit DOB/SSN and Call Hub functionalities

--- app/controllers/concerns/vlp_doc.rb
+++ app/controllers/concerns/vlp_doc.rb
@@ -17,10 +17,10 @@ module VlpDoc
       vlp_doc_params = params[source][:consumer_role][:vlp_documents_attributes]['0'].to_h.delete_if {|k,v| v.blank? }
       result = ::Validators::VlpV37Contract.new.call(vlp_doc_params)
       if result.failure? && source == 'person'
-        add_document_errors_to_consumer_role(consumer_role, [result.errors.to_h.keys.first.to_s.titlecase, result.errors.to_h.values.flatten.first])
+        add_document_errors_to_consumer_role(consumer_role, ['Please fill in your information for', result.errors.to_h.keys.first.to_s.titlecase])
         return false
       elsif result.failure? && source == 'dependent'
-        add_document_errors_to_dependent(dependent, [result.errors.to_h.keys.first.to_s.titlecase, result.errors.to_h.values.flatten.first])
+        add_document_errors_to_dependent(dependent, ['Please fill in your information for', result.errors.to_h.keys.first.to_s.titlecase])
         return false
       end
     end
