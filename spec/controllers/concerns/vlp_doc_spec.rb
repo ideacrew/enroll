43e279604c601fab2627a445ababab711363f29f
saikumar9 <saikumarkotagiri9@gmail.com>
2020-03-05 16:28:19 -0500

    Refs #85326, UI error messages display for Case Management for Admin Edit DOB/SSN and Call Hub functionalities

--- spec/controllers/concerns/vlp_doc_spec.rb
+++ spec/controllers/concerns/vlp_doc_spec.rb
@@ -125,7 +125,7 @@ describe FakesController do
         end
 
         it 'should add errors to the object if params are invalid' do
-          expect(person.errors.full_messages).to eq(['Description is required for VLP Document type: Other (With Alien Number)'])
+          expect(person.errors.full_messages).to eq(['Please fill in your information for Description'])
         end
       end
 
@@ -193,7 +193,7 @@ describe FakesController do
         end
 
         it 'should add errors to the object if params are invalid' do
-          expect(dependent.errors.full_messages).to eq(['Description is required for VLP Document type: Other (With Alien Number)'])
+          expect(dependent.errors.full_messages).to eq(['Please fill in your information for Description'])
         end
       end
 
