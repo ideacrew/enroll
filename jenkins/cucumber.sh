a9ed578786d376c2cb38a340cd90c6985ca2561b
Trey Evans <tevans@ip-192-168-254-13.ec2.internal>
2020-03-06 11:56:53 -0500

    Parallel Cucumber.

--- jenkins/cucumber.sh
+++ jenkins/cucumber.sh
@@ -22,20 +22,20 @@ yarn install
 bundle exec rails r -e test "DatabaseCleaner.clean"
 
 ### Run cucumber
-rm -fr tmp/cucumber.log tmp/cucumber_failures.log tmp/cucumber_failures_2.log tmp/cucumber_failures_3.log cucumber.summary cucumber2.summary cucumber3.summary
-if bundle exec cucumber --format summary --out cucumber.summary --format rerun --out tmp/cucumber_failures.log
+rm -fr tmp/cucumber.log tmp/cucumber_failures.log tmp/cucumber_failures2.log tmp/cucumber_failures3.log tmp/cucumber_failures4.log tmp/cucumber_failures_2.log tmp/cucumber_failures_3.log cucumber.summary cucumber2.summary cucumber3.summary cucumber4.summary cucumber_2.summary cucumber_3.summary
+if bundle exec parallel_test features --type cucumber -n 4
 then
   echo "Cucumber passed the first time!"
   exit 0
 else
   echo "Give cucumber one more try"
-  if bundle exec cucumber @tmp/cucumber_failures.log --format summary --out cucumber2.summary --format rerun --out tmp/cucumber_failures_2.log
+  if bundle exec cucumber @tmp/cucumber_failures.log @tmp/cucumber_failures2.log @tmp/cucumber_failures3.log @tmp/cucumber_failures4.log --format summary --out cucumber_2.summary --format rerun --out tmp/cucumber_failures_2.log
   then
     echo "Cucumber worked on retry"
     exit 0
   else
     echo "Give cucumber yet another try"
-    bundle exec cucumber @tmp/cucumber_failures_2.log --format summary --out cucumber3.summary --format rerun --out tmp/cucumber_failures_3.log
+    bundle exec cucumber @tmp/cucumber_failures_2.log --format summary --out cucumber_3.summary --format rerun --out tmp/cucumber_failures_3.log
   fi
 fi
 
