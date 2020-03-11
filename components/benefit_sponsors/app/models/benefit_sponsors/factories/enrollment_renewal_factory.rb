9a1cc605f5304dd5fb5e5cce80bc6982978e5f9b
Vishal Kalletla <vishuk1201@gmail.com>
2020-03-11 16:15:14 -0400

    86331: check presences of product for an enrollment

--- components/benefit_sponsors/app/models/benefit_sponsors/factories/enrollment_renewal_factory.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/factories/enrollment_renewal_factory.rb
@@ -54,7 +54,9 @@ module BenefitSponsors
       end
 
       def has_renewal_product?
-        renewal_product = @base_enrollment.product.renewal_product
+        base_product = @base_enrollment.product
+        return true unless base_product.present?
+        renewal_product = base_product.renewal_product
         @sponsored_benefit.products(@new_effective_on).include?(renewal_product)
       end
 
