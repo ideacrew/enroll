4af3a9a0a77dc5b05df9e5656a022adef2759a16
Vishal Kalletla <vishuk1201@gmail.com>
2020-03-12 13:22:16 -0400

    86331: checking waived status instead of product

--- components/benefit_sponsors/app/models/benefit_sponsors/factories/enrollment_renewal_factory.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/factories/enrollment_renewal_factory.rb
@@ -54,9 +54,9 @@ module BenefitSponsors
       end
 
       def has_renewal_product?
-        base_product = @base_enrollment.product
-        return true unless base_product.present?
-        renewal_product = base_product.renewal_product
+        return true if @base_enrollment.is_coverage_waived?
+
+        renewal_product = @base_enrollment.product.renewal_product
         @sponsored_benefit.products(@new_effective_on).include?(renewal_product)
       end
 
