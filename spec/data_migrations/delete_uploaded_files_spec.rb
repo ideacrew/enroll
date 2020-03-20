a4a618ec5536e2f3073664665de3fd677f8bff2c
saikumar9 <saikumarkotagiri9@gmail.com>
2020-03-20 10:42:08 -0400

    Refs #86297, refactor migration file for additional checks

--- spec/data_migrations/delete_uploaded_files_spec.rb
+++ spec/data_migrations/delete_uploaded_files_spec.rb
@@ -15,12 +15,13 @@ describe DeleteUploadedFiles, dbclean: :after_each do
 
   describe 'migration' do
     let!(:consumer_role)  { FactoryBot.create(:person, :with_consumer_role, :with_active_consumer_role).consumer_role }
+    let(:vlp_doc2) { FactoryBot.build(:vlp_document, :subject => 'Other (With I-94)', updated_at: (TimeKeeper.date_of_record + 1.day)) }
 
     context 'succesful migration' do
-      let(:vlp_doc) { FactoryBot.build(:vlp_document, subject: 'ajhssjah.png', verification_type: 'Citizenship') }
+      let(:vlp_doc1) { FactoryBot.build(:vlp_document, subject: 'ajhssjah.png', verification_type: 'Citizenship') }
 
-      before do
-        consumer_role.vlp_documents = [vlp_doc]
+      before :each do
+        consumer_role.vlp_documents = [vlp_doc1, vlp_doc2]
         consumer_role.save!
         subject.migrate
         @file = "#{Rails.root}/list_of_people_uploaded_files_deleted.csv"
@@ -32,7 +33,7 @@ describe DeleteUploadedFiles, dbclean: :after_each do
 
       it "should delete all vlp_documents(uploaded files) under consumer_role" do
         consumer_role.reload
-        expect(consumer_role.vlp_documents.count).to be_zero
+        expect(consumer_role.vlp_documents.count).to eq(1)
       end
 
       it 'should add data to the file' do
@@ -43,16 +44,29 @@ describe DeleteUploadedFiles, dbclean: :after_each do
 
     context 'for no valid vlp documents for deletion' do
       let(:vlp_doc1) { FactoryBot.build(:vlp_document, updated_at: TimeKeeper.date_of_record) }
-      let(:vlp_doc2) { FactoryBot.build(:vlp_document, :subject => 'Other (With I-94)', updated_at: (TimeKeeper.date_of_record + 1.day)) }
 
-      before :each do
-        consumer_role.vlp_documents = [vlp_doc1, vlp_doc2]
-        consumer_role.save!
-        subject.migrate
+      context 'with vlp documents' do
+        before do
+          consumer_role.vlp_documents = [vlp_doc1, vlp_doc2]
+          consumer_role.vlp_documents.each {|doc| doc.save! }
+          consumer_role.save!
+          subject.migrate
+        end
+
+        it 'should not delete any vlp_documents under consumer_role' do
+          expect(consumer_role.vlp_documents.count).to eq(2)
+        end
       end
 
-      it 'should not delete any vlp_documents under consumer_role' do
-        expect(consumer_role.vlp_documents.count).to eq(2)
+      context 'without any vlp documents' do
+        before do
+          consumer_role.vlp_documents = []
+          consumer_role.save!
+        end
+
+        it 'should do not raise any error' do
+          expect{subject.migrate}.not_to raise_error
+        end
       end
     end
   end
