8f33ed8401b34d807d1cac3b8ef7ef8ac4dce11a
raghuramg <raghuramg83@gmail.com>
2020-03-25 18:00:04 -0400

    Load custom types from benefit markets and benefit sponsors
    - Removed duplicate custom types
    - Removed custom types already defined in resource registry

--- components/benefit_markets/app/domain/benefit_markets/types.rb
+++ components/benefit_markets/app/domain/benefit_markets/types.rb
@@ -5,27 +5,12 @@ require 'cgi'
 require 'dry-types'
 
 Dry::Types.load_extensions(:maybe)
-module BenefitMarkets
-  module Types
-    include Dry.Types()
-    include Dry::Logic
 
-    Uri                 = Types.Constructor(::URI) { |val| ::URI.parse(val) }
-    Url                 = Uri
+module Types
+  send(:include, Dry.Types())
+  include Dry::Logic
 
-    Email               = Coercible::String.constrained(format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
-    Emails              = Array.of(Email)
-    CallableDateTime    = Types::DateTime.default { DateTime.now }
-    Duration            = Types.Constructor(ActiveSupport::Duration) {|val| ActiveSupport::Duration.build(val) }
-    PositiveInteger     = Coercible::Integer.constrained(gteq: 0)
-    HashOrNil           = Types::Hash | Types::Nil
-    CustomRange         = Types.Constructor(Types::Range) { |val| { 'min' => val[:min], 'max' => val[:max] } }
-    Bson                = Types.Constructor(BSON::ObjectId) { |val| BSON::ObjectId val }
-    StringOrNil         = Types::String | Types::Nil
-
-    RequiredSymbol  = Types::Strict::Symbol.constrained(min_size: 2)
-    RequiredString  = Types::Strict::String.constrained(min_size: 1)
-    StrippedString  = String.constructor(->(val){ String(val).strip })
-
-  end
+  # Emails              = Array.of(Types::Email)
+  PositiveInteger     = Coercible::Integer.constrained(gteq: 0)
+  Bson                = Types.Constructor(BSON::ObjectId) { |val| BSON::ObjectId val }
 end
