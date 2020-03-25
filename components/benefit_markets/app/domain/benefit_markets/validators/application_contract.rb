0f01c9ae8a5c483505efb6413c02156d1d526f55
raghuramg <raghuramg83@gmail.com>
2020-03-25 18:08:42 -0400

    Updated BenefitSponsorCatalog and ProductPackage contracts with required attributes
    - Made contribution models, products required under ProductPackage contract
    - Made product_packages required under BenefitSponsorCatalog contract
    - Created Application Contract and added rules to validate product_packages, contribution_models, products.
    - Modified product_packages, contribution_models, products rules to skip validation if respective entitiy passed

---
+++ components/benefit_markets/app/domain/benefit_markets/validators/application_contract.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+Dry::Validation.load_extensions(:monads)
+
+module BenefitMarkets
+  module Validators
+
+    # Configuration values and shared rules and macros for domain model validation contracts
+    class ApplicationContract < Dry::Validation::Contract
+
+
+      # @!macro ruleeach
+      #   Validates a nested array of $0 params
+      #   @!method rule(contribution_models)
+      rule(:product_packages).each do
+        if key? && value
+          if !value.is_a?(::BenefitMarkets::Entities::ProductPackage)
+            if value.is_a?(Hash)
+              result = BenefitMarkets::Validators::Products::ProductPackageContract.new.call(value)
+              key.failure(text: "invalid product package", error: result.errors.to_h) if result&.failure?
+            else
+              key.failure(text: "invalid product packages. expected a hash or product_package entity")
+            end
+          end
+        end
+      end
+
+      # @!macro ruleeach
+      #   Validates a nested array of $0 params
+      #   @!method rule(contribution_models)
+      rule(:contribution_models).each do
+        if key? && value
+          if !value.is_a?(::BenefitMarkets::Entities::ContributionModel)
+            if value.is_a?(Hash)
+              result = BenefitMarkets::Validators::ContributionModels::ContributionModelContract.new.call(value)
+              key.failure(text: "invalid contribution model", error: result.errors.to_h) if result&.failure?
+            else
+              key.failure(text: "invalid contribution models. expected a hash or contribution_model entity")
+            end
+          end
+        end
+      end
+
+      # @!macro ruleeach
+      #   Validates a nested array of $0 params
+      #   @!method rule(products)
+      rule(:products).each do
+        if key? && value
+          if !value.is_a?(::BenefitMarkets::Entities::Product)
+            if value.is_a?(Hash)
+              contract_class = "::BenefitMarkets::Validators::Products::#{value[:kind].to_s.camelize}ProductContract".constantize
+              result = contract_class.new.call(value)
+              key.failure(text: "invalid product", error: result.errors.to_h) if result&.failure?
+            else
+              key.failure(text: "invalid products. expected a hash or product entity")
+            end
+          end
+        end
+      end
+    end
+  end
+end
