4f3a61b3f3f0445c36490eb47a5eea6cb2405a7a
Trey Evans <tevans@ip-192-168-254-16.ec2.internal>
2020-04-10 10:44:41 -0400

    Bring permissions into line.

--- app/data_migrations/define_permissions.rb
+++ app/data_migrations/define_permissions.rb
@@ -10,7 +10,8 @@ class DefinePermissions < MigrationTask
       .update_attributes!(modify_family: true, modify_employer: true, revert_application: true, list_enrollments: true,
                           send_broker_agency_message: true, approve_broker: true, approve_ga: true, can_update_ssn: false, can_complete_resident_application: false,
                           can_add_sep: false, can_lock_unlock: true, can_view_username_and_email: false, can_reset_password: false, modify_admin_tabs: true,
-                          view_admin_tabs: true,  view_the_configuration_tab: true, can_submit_time_travel_request: false, view_agency_staff: true)
+                          view_admin_tabs: true,  view_the_configuration_tab: true, can_submit_time_travel_request: false,
+                          view_agency_staff: true, manage_agency_staff: true)
     Permission
       .find_or_initialize_by(name: 'hbx_read_only')
       .update_attributes!(modify_family: true, modify_employer: false, revert_application: false, list_enrollments: true,
@@ -42,7 +43,7 @@ class DefinePermissions < MigrationTask
                           send_broker_agency_message: true, approve_broker: true, approve_ga: true, can_update_ssn: false, can_complete_resident_application: false,
                           can_add_sep: false, can_lock_unlock: false, can_view_username_and_email: false, can_reset_password: false, modify_admin_tabs: true,
                           view_admin_tabs: true,  view_the_configuration_tab: true, can_submit_time_travel_request: false,
-                          view_agency_staff: true
+                          view_agency_staff: true, manage_agency_staff: true
                         )
     Permission
       .find_or_initialize_by(name: 'super_admin')
