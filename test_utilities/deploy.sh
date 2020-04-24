15ea588953f040f5bc1fff0a653c32ac1b0f11e9
David Plappert <david@davidplappert.com>
2020-04-24 07:41:09 -0500

    deploy cmd

--- test_utilities/deploy.sh
+++ test_utilities/deploy.sh
@@ -4,4 +4,4 @@ regex='refs/heads/(.*)'
 [[ "$BRANCH" =~ $regex ]]
 SIMPLE_BRANCH=${BASH_REMATCH[1]}
 
-`$DEPLOY_CMD "branch=${BRANCH}" --data-urlencode "status=${STATUS}"`
+bash -c "${DEPLOY_CMD} branch=${BRANCH}--data-urlencode status=${STATUS}"
