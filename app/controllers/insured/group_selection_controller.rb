0932635b31066a595a08394601c8d40137fb608b
saipraveen18 <saipraveen.gudimetla@gmail.com>
2020-04-27 14:24:49 -0400

    refs #87036 fix typo

--- app/controllers/insured/group_selection_controller.rb
+++ app/controllers/insured/group_selection_controller.rb
@@ -96,7 +96,7 @@ class Insured::GroupSelectionController < ApplicationController
       census_effective_on = @employee_role.census_employee.coverage_effective_on(hbx_enrollment.sponsored_benefit_package)
       if census_effective_on > hbx_enrollment.effective_on
         raise 'You are attempting to purchase coverage through Qualifying Life Event prior to your eligibility date.'\
-              ' Please contact your Employer for assistance. You are eligible for employer benefits from ' + newly_designated_effective_on.strftime('%m/%d/%Y')
+              ' Please contact your Employer for assistance. You are eligible for employer benefits from ' + census_effective_on.strftime('%m/%d/%Y')
       end
 
       if @adapter.is_waiving?(params)
