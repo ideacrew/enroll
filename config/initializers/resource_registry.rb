44b84dc5e613ecb1c865a1483a7fc61ca3c4cb07
saimekala07 <saimekala07@gmail.com>
2020-05-05 05:53:53 -0400

    refs #87520 configuring resource reistry settings

---
+++ config/initializers/resource_registry.rb
@@ -0,0 +1,7 @@
+EnrollRegistry = ResourceRegistry::Registry.new
+
+EnrollRegistry.configure do |config|
+  config.name       = :enroll
+  config.created_at = DateTime.now
+  config.load_path  = Rails.root.join('system', 'config', 'templates', 'features').to_s
+end
