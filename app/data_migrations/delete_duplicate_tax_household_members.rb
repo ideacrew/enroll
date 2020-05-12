cb843f9a3f5fdc6e9ddc8e833c442eb165c1c04a
RyanEddyIC <ryan.eddy@ideacrew.com>
2020-05-12 10:15:19 -0400

    Add p statements for server debugging

--- app/data_migrations/delete_duplicate_tax_household_members.rb
+++ app/data_migrations/delete_duplicate_tax_household_members.rb
@@ -11,20 +11,24 @@ class DeleteDuplicateTaxHouseholdMembers < MongoidMigrationTask
         return
       end
 
-      person_hbx_ids = []
+      member_ids = []
       person.primary_family.active_household.tax_households.each do |household|
-        household.tax_household.members.each do |member|
-          hbx_id = member.person.id
-          if !person_hbx_ids.empty?
-            if person_hbx_ids.include?(hbx_id)
-              member.destroy
-            else
-              person_hbx_ids << hbx_id
-            end
+        p "Checking tax household id " + household.id
+        p "Checking #{household.tax_household_members.count} members"
+        person_hbx_ids = []
+        member_count = 0
+        deletion_count = 0
+        household.tax_household_members.each do |member|
+          member_count += 1
+          hbx_id = member.person.hbx_id
+          if person_hbx_ids.include?(hbx_id)
+            member.destroy
+            deletion_count += 1
           else
             person_hbx_ids << hbx_id
           end
         end
+        p "Checked #{member_count} tax_household_members, deleted #{deletion_count}"
       end
 
     rescue StandardError => e
