df5c5c74fe0a8bdc397ad9d97a145e3d36b57c3f
saikumar9 <saikumarkotagiri9@gmail.com>
2020-06-02 12:58:22 -0400

    Refs #87679, allow csr_0 as CSR KIND and fix flickering cucumber

--- app/models/eligibility_determination.rb
+++ app/models/eligibility_determination.rb
@@ -6,7 +6,7 @@ class EligibilityDetermination
 
   embedded_in :tax_household
 
-  CSR_KINDS = %w(csr_100 csr_94 csr_87 csr_73)
+  CSR_KINDS = %w(csr_100 csr_94 csr_87 csr_73 csr_0)
 
   #   csr_0:   "02", # Native Americans
   #   limited: "03", # limited?
