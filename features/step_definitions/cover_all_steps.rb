be4a4da39705bd456b70cb30a46df270ff3ba633
Battula <vrbattula@outlook.com>
2020-06-08 20:55:53 -0400

    Debug cucumber failure on github actions

--- features/step_definitions/cover_all_steps.rb
+++ features/step_definitions/cover_all_steps.rb
@@ -60,6 +60,7 @@ Then(/^Hbx Admin should see text Household Info$/) do
 end
 
 Then(/^Hbx Admin should see text Special Enrollment Period$/) do
+  wait_for_ajax
   FactoryBot.create(:qualifying_life_event_kind, market_kind: "individual")
   expect(page).to have_content('Special Enrollment Period')
   expect(page).to have_content('TOP LIFE CHANGES')
