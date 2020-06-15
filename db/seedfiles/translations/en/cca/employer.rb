a9309c4bf30b6c979cf0522e8465525a51a3522c
Vishal Kalletla <vishuk1201@gmail.com>
2020-06-15 10:12:46 -0400

    87968: translations - benefits with plan

--- db/seedfiles/translations/en/cca/employer.rb
+++ db/seedfiles/translations/en/cca/employer.rb
@@ -1,16 +1,18 @@
 # MA Census Employee Translations
 EMPLOYER_TRANSLATIONS = {
-    :'en.employers.plan_years.reference_plan_details' => "Reference Plan Details",
-    :'en.employers.plan_years.plan_benefits' => "PLAN BENEFITS",
-    :'en.employers.plan_years.co_pay' => "CO-PAY",
-    :'en.employers.plan_years.coinsurance' => "COINSURANCE",
-    :'en.employers.plan_years.more_details' => "More Details",
-    :'en.employers.plan_years.benefits_without_plan' => "<p>It’s time to create a benefits package for your employees.</br><ul><li>To set up a plan, select ‘Add Plan Year’</li><li>If your broker already set up a plan quote for you to consider, select ‘Claim Quote’</li></ul></p>",
-    :'en.employers.employer_profiles.my_account.important_documents_needed' => "Important Documents Needed",
-    :'en.employers.employer_profiles.my_account.eligibility_verification_html' => "You will need to submit proof that your group is an eligible Massachusetts small business before you can complete the enrollment process.</br></br>Please review our list of documents that can be used as proof and upload PDF document(s) that meet the requirements.</br></br>After you’ve uploaded your documents, you can continue with choosing your benefit package. We will review your documents and let you know if we need any further documentation.",
-    :'en.employers.employer_profiles.welcome' => "Take the next step towards offering health and dental insurance. You can take these steps on your own or find a broker to help you through the process.",
-    :'en.employers.employer_profiles.welcome.submit_verification_documents' => "Submit verification documents",
-    :'en.employers.employer_profiles.welcome.submit_verification_documents.content' => "Before you can complete your enrollment, you’ll need to submit proof that your group is eligible to shop through Health Connector for Business. If you haven’t submitted your documentation yet, please review our list of required documents, then upload in your account.",
-    :'en.employers.employer_profiles.welcome.complete_your_employee_roster' => "Complete your Employee Roster and choose a plan",
-    :'en.employers.employer_profiles.welcome.complete_your_employee_roster.content_html' => "<ul><li>To complete your Employee Roster, select the Employees tab and follow the instructions for adding employees to your group.</li><li>To work on choosing your plan, select the Benefits tab to get started</li></ul>"
+  :'en.employers.plan_years.reference_plan_details' => "Reference Plan Details",
+  :'en.employers.plan_years.plan_benefits' => "PLAN BENEFITS",
+  :'en.employers.plan_years.co_pay' => "CO-PAY",
+  :'en.employers.plan_years.coinsurance' => "COINSURANCE",
+  :'en.employers.plan_years.more_details' => "More Details",
+  :'en.employers.plan_years.benefits_without_plan' => "<p>It’s time to create a benefits package for your employees.</br><ul><li>To set up a plan, select 'Add Plan Year'</li><li>If your broker already set up a plan quote for you to consider, select 'Claim Quote'</li></ul></p>",
+  :'en.employers.plan_years.benefits_with_plan' => "Carefully review your plan information and pricing below. To make changes to your benefits package, select 'Edit Plan Year' If you have employees that you haven’t added to your roster yet, select 'Employees' in the blue menu bar to edit and add employee information",
+  :'en.employers.plan_years.benefits_with_plan.important' => "<strong>IMPORTANT:</strong></br><ul><li>Your employees will need to take action after your plan year is published. We will send them an email, email letting them know once their open enrollment period has begun.</li><li>Your employees will need to sign into their account to waive or enroll in their benefits during open enrollment. If they do not take this step, they will not be enrolled in your plan.</li></ul>",
+  :'en.employers.employer_profiles.my_account.important_documents_needed' => "Important Documents Needed",
+  :'en.employers.employer_profiles.my_account.eligibility_verification_html' => "You will need to submit proof that your group is an eligible Massachusetts small business before you can complete the enrollment process.</br></br>Please review our list of documents that can be used as proof and upload PDF document(s) that meet the requirements.</br></br>After you’ve uploaded your documents, you can continue with choosing your benefit package. We will review your documents and let you know if we need any further documentation.",
+  :'en.employers.employer_profiles.welcome' => "Take the next step towards offering health and dental insurance. You can take these steps on your own or find a broker to help you through the process.",
+  :'en.employers.employer_profiles.welcome.submit_verification_documents' => "Submit verification documents",
+  :'en.employers.employer_profiles.welcome.submit_verification_documents.content' => "Before you can complete your enrollment, you’ll need to submit proof that your group is eligible to shop through Health Connector for Business. If you haven’t submitted your documentation yet, please review our list of required documents, then upload in your account.",
+  :'en.employers.employer_profiles.welcome.complete_your_employee_roster' => "Complete your Employee Roster and choose a plan",
+  :'en.employers.employer_profiles.welcome.complete_your_employee_roster.content_html' => "<ul><li>To complete your Employee Roster, select the Employees tab and follow the instructions for adding employees to your group.</li><li>To work on choosing your plan, select the Benefits tab to get started</li></ul>"
 }.freeze
