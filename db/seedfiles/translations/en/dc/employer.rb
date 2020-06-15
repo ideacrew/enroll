12b438a7d223f86c7819a6c025a2bdb2f6dfc986
Vishal Kalletla <vishuk1201@gmail.com>
2020-06-15 10:22:52 -0400

    87967,87868: translations - benefits with/without plan

--- db/seedfiles/translations/en/dc/employer.rb
+++ db/seedfiles/translations/en/dc/employer.rb
@@ -1,9 +1,13 @@
 EMPLOYER_TRANSLATIONS = {
-  "en.employers.plan_years.reference_plan_details" => "Reference Plan Details",
-  "en.employers.plan_years.plan_benefits" => "PLAN BENEFITS",
-  "en.employers.plan_years.co_pay" => "CO-PAY",
-  "en.employers.plan_years.coinsurance" => "COINSURANCE",
-  "en.employers.plan_years.more_details" => "More Details",
-  "en.employers.employer_profiles.my_account.important_documents_needed" => "Important Documents Needed",
-  "en.employers.employer_profiles.my_account.eligibility_documents_needed" => "After you complete your application, you will need to submit proof of your eligibility to shop as a small business through the Health Connector. You will need to submit this proof before you are able to shop for a plan. Please review <a href="'https://www.mahealthconnector.org/business/employers/employer-verification-checklist'" target="'_blank'">our list of documents</a> that can be used as proof and make sure that you have the right documentation available."
-}
+  :'en.employers.plan_years.reference_plan_details' => "Reference Plan Details",
+  :'en.employers.plan_years.plan_benefits' => "PLAN BENEFITS",
+  :'en.employers.plan_years.co_pay' => "CO-PAY",
+  :'en.employers.plan_years.coinsurance' => "COINSURANCE",
+  :'en.employers.plan_years.more_details' => "More Details",
+  :'en.employers.plan_years.benefits_without_plan' => "It's time to create a benefits package. Select 'Add Plan Year' to create your first one. Most employers only create one benefits package but you'll have the opportunity to create more than one if you want to offer different benefits to different
+      groups of employees.",
+  :'en.employers.plan_years.benefits_with_plan' => "Carefully review the plan information and pricing below. If you haven't added employees to your plan yet, select 'Employees' in the blue menu bar to the left and edit each of your employees to add them to your benefit package. If you want to create more than one benefit package and offer different benefits to different employees, select 'Edit Plan Year' and then select 'Add Benefit Group' at bottom of the page. When you're satisfied with your benefit package(s), select 'Publish Plan Year'.",
+  :'en.employers.plan_years.benefits_with_plan.important' => "<strong>IMPORTANT</strong></br><p> – Once you select 'Publish Plan Year', your employees will receive a notification and open enrollment will begin according to the timeline you selected for your plan year.</p>",
+  :'en.employers.employer_profiles.my_account.important_documents_needed' => "Important Documents Needed",
+  :'en.employers.employer_profiles.my_account.eligibility_documents_needed' => "After you complete your application, you will need to submit proof of your eligibility to shop as a small business through the Health Connector. You will need to submit this proof before you are able to shop for a plan. Please review <a href="'https://www.mahealthconnector.org/business/employers/employer-verification-checklist'" target="'_blank'">our list of documents</a> that can be used as proof and make sure that you have the right documentation available."
+}.freeze
