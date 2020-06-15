b0d944fafe2411f552daba000b212047f4f8e66f
Vishal Kalletla <vishuk1201@gmail.com>
2020-06-15 09:37:28 -0400

    87967: translations - benefits without plan

--- db/seedfiles/translations/en/cca/employer.rb
+++ db/seedfiles/translations/en/cca/employer.rb
@@ -5,6 +5,7 @@ EMPLOYER_TRANSLATIONS = {
     :'en.employers.plan_years.co_pay' => "CO-PAY",
     :'en.employers.plan_years.coinsurance' => "COINSURANCE",
     :'en.employers.plan_years.more_details' => "More Details",
+    :'en.employers.plan_years.benefits_without_plan' => "<p>It’s time to create a benefits package for your employees.</br><ul><li>To set up a plan, select ‘Add Plan Year’</li><li>If your broker already set up a plan quote for you to consider, select ‘Claim Quote’</li></ul></p>",
     :'en.employers.employer_profiles.my_account.important_documents_needed' => "Important Documents Needed",
     :'en.employers.employer_profiles.my_account.eligibility_verification_html' => "You will need to submit proof that your group is an eligible Massachusetts small business before you can complete the enrollment process.</br></br>Please review our list of documents that can be used as proof and upload PDF document(s) that meet the requirements.</br></br>After you’ve uploaded your documents, you can continue with choosing your benefit package. We will review your documents and let you know if we need any further documentation.",
     :'en.employers.employer_profiles.welcome' => "Take the next step towards offering health and dental insurance. You can take these steps on your own or find a broker to help you through the process.",
--- db/seedfiles/translations/en/dc/employer.rb
+++ db/seedfiles/translations/en/dc/employer.rb
@@ -1,9 +1,11 @@
 EMPLOYER_TRANSLATIONS = {
-  "en.employers.plan_years.reference_plan_details" => "Reference Plan Details",
-  "en.employers.plan_years.plan_benefits" => "PLAN BENEFITS",
-  "en.employers.plan_years.co_pay" => "CO-PAY",
-  "en.employers.plan_years.coinsurance" => "COINSURANCE",
-  "en.employers.plan_years.more_details" => "More Details",
-  "en.employers.employer_profiles.my_account.important_documents_needed" => "Important Documents Needed",
-  "en.employers.employer_profiles.my_account.eligibility_documents_needed" => "After you complete your application, you will need to submit proof of your eligibility to shop as a small business through the Health Connector. You will need to submit this proof before you are able to shop for a plan. Please review <a href="'https://www.mahealthconnector.org/business/employers/employer-verification-checklist'" target="'_blank'">our list of documents</a> that can be used as proof and make sure that you have the right documentation available."
-}
+  :'en.employers.plan_years.reference_plan_details' => "Reference Plan Details",
+  :'en.employers.plan_years.plan_benefits' => "PLAN BENEFITS",
+  :'en.employers.plan_years.co_pay' => "CO-PAY",
+  :'en.employers.plan_years.coinsurance' => "COINSURANCE",
+  :'en.employers.plan_years.more_details' => "More Details",
+  :'en.employers.plan_years.benefits_without_plan' => "It's time to create a benefits package. Select 'Add Plan Year' to create your first one. Most employers only create one benefits package but you'll have the opportunity to create more than one if you want to offer different benefits to different
+        groups of employees.",
+  :'en.employers.employer_profiles.my_account.important_documents_needed' => "Important Documents Needed",
+  :'en.employers.employer_profiles.my_account.eligibility_documents_needed' => "After you complete your application, you will need to submit proof of your eligibility to shop as a small business through the Health Connector. You will need to submit this proof before you are able to shop for a plan. Please review <a href="'https://www.mahealthconnector.org/business/employers/employer-verification-checklist'" target="'_blank'">our list of documents</a> that can be used as proof and make sure that you have the right documentation available."
+}.freeze
