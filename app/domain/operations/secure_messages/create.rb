4ca21d53f8c98ca02cd7d82232020385f59a1f1f
saipraveen18 <saipraveen.gudimetla@gmail.com>
2020-06-25 14:01:37 -0400

    refs #88182 creating the message object and storing it in the inbox of the resource

--- app/domain/operations/secure_messages/create.rb
+++ app/domain/operations/secure_messages/create.rb
@@ -13,8 +13,9 @@ module Operations
         payload = yield construct_message_payload(params[:message_params])
         validated_payload = yield validate_message_payload(payload)
         message_entity = yield create_message_entity(validated_payload)
+        resource = yield create(params[:resource], message_entity.to_h)
 
-        Success(message_entity)
+        Success(resource)
       end
 
       private
@@ -31,6 +32,12 @@ module Operations
       def create_message_entity(params)
         message = ::Entities::SecureMessages::Message.new(params)
         Success(message)
+      end
+
+      def create(resource, message_entity)
+        resource.inbox.messages << Message.new(message_entity)
+        resource.save!
+        Success(resource)
       end
 
     end
