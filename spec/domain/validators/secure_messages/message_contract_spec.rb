585a7d290cb5e73aa9d6d8f89fd95caaf9ee92c1
saipraveen18 <saipraveen.gudimetla@gmail.com>
2020-06-26 13:33:10 -0400

    refs #88182 add more specs

--- spec/domain/validators/secure_messages/message_contract_spec.rb
+++ spec/domain/validators/secure_messages/message_contract_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Validators::SecureMessages::MessageContract,  dbclean: :after_eac
   describe "message contract" do
 
     context "sending with missing values should return errors" do
-      let(:invalid_params)   { {subject: '', body: '' } }
-      let(:error_message)   { {:subject => ["Please enter subject"], :body => ["Please enter content"]} }
+      let(:invalid_params) { {subject: '', body: '' } }
+      let(:error_message) { {:subject => ["Please enter subject"], :body => ["Please enter content"]} }
 
       it 'should be a container-ready operation' do
         expect(subject.respond_to?(:call)).to be_truthy
@@ -25,17 +25,17 @@ RSpec.describe Validators::SecureMessages::MessageContract,  dbclean: :after_eac
 
     context "sending with missing keys should return errors" do
 
-      let(:invalid_params)   { {} }
-      let(:error_message)   { {:subject => ["is missing"], :body => ["is missing"]} }
+      let(:invalid_params) { {} }
+      let(:error_message) { {:subject => ["is missing"], :body => ["is missing"]} }
 
       it 'should throw errors' do
-        expect(subject.call(valid_params)).to eq error_message
+        expect(subject.call(invalid_params).errors.to_h).to eq error_message
       end
     end
 
     context "sending with all keys and values should not errors" do
 
-      let(:valid_params)   { {subject: 'text', body: 'text' } }
+      let(:valid_params) { {subject: 'text', body: 'text' } }
 
 
       it 'should return Dry::Validation::Result object' do
