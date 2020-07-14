cf18111933c8a61c29fa8049b13698e9fff12c11
saimekala07 <saimekala07@gmail.com>
2020-07-14 11:43:53 -0400

    refs # Display reasons in dropdown based on market

--- app/models/forms/qualifying_life_event_kind_form.rb
+++ app/models/forms/qualifying_life_event_kind_form.rb
@@ -19,11 +19,32 @@ module Forms
     attribute :market_kind, String
     attribute :effective_on_kinds, Array[String]
     attribute :ordinal_position, Integer
+    attribute :ivl_reasons, Array[String]
+    attribute :shop_reasons, Array[String]
+    attribute :fehb_reasons, Array[String]
+    attribute :other_reason, String
 
     attribute :id, String
 
     def self.for_new(params)
-      self.new(params)
+      form = self.new(params)
+      form[:ivl_reasons] = individual_market_reasons
+      form[:shop_reasons] = shop_market_reasons
+      form[:fehb_reasons] = fehb_market_reasons
+      form
+    end
+
+    def self.individual_market_reasons
+      binding.pry
+      QualifyingLifeEventKind.individual_market_events.map(&:reason).map(&:humanize)
+    end
+
+    def self.fehb_market_reasons
+      QualifyingLifeEventKind.fehb_market_events.map(&:reason).map(&:humanize)
+    end
+
+    def self.shop_market_reasons
+      QualifyingLifeEventKind.shop_market_events.map(&:reason).map(&:humanize)
     end
   end
 end
