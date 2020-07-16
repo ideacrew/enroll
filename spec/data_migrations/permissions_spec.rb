cc19b815e5454a959092daa8dec32224a340d20f
zasman <zanesandin@gmail.com>
2020-07-16 09:58:12 -0400

    REFS R4 - Fixes permission data spec

--- spec/data_migrations/permissions_spec.rb
+++ spec/data_migrations/permissions_spec.rb
@@ -14,7 +14,7 @@ describe DefinePermissions, dbclean: :after_each do
       subject.initial_hbx
     end
     it "creates permissions" do
-      expect(Permission.count).to eq(8)
+      expect(Permission.all.to_a.count).to eq(8)
       #expect(Person.first.hbx_staff_role.subrole).to eq 'hbx_staff'
       expect(Permission.all.map(&:name)).to match_array roles
     end
