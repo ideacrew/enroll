c2a94cd5ffdfc26636170cebd83eb7dc1ebd1956
saimekala07 <saimekala07@gmail.com>
2020-07-24 09:49:46 -0400

    Refs #ManageSEPTypes, update script for new QLE fields.

--- script/set_assm_state_on_qle.rb
+++ script/set_assm_state_on_qle.rb
@@ -1,4 +1,5 @@
 QualifyingLifeEventKind.where(is_active: true).each do |qle|
-  qle.update_attributes(aasm_state: :active)
+  visible = qle.is_self_attested ? true : false
+  qle.update_attributes(aasm_state: :active, is_visible: visible)
+  qle.workflow_state_transitions << WorkflowStateTransition.new(from_state: :draft, to_state: :active, comment: 'manually updating aasm state as part of Manage SEP type feature.')
 end
-
