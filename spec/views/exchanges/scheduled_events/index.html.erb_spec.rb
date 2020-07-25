8c76016f1797bd34dc839a167395df90d244d342
saikumar9 <saikumarkotagiri9@gmail.com>
2020-07-25 09:53:14 -0400

    Refs #ManageSEPTypes, failing spec fix

--- spec/views/exchanges/scheduled_events/index.html.erb_spec.rb
+++ spec/views/exchanges/scheduled_events/index.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "exchanges/scheduled_events/index.html.erb" do
   let(:scheduled_event) { FactoryBot.create(:scheduled_event) }
   before :each do
     assign(:scheduled_event, scheduled_event)
-    allow(view).to receive(:policy_helper).and_return(double("Policy", view_admin_tabs?: true, access_outstanding_verification_sub_tab?: true, access_identity_verification_sub_tab?: true, view_the_configuration_tab?: true, can_access_user_account_tab?: true, begin_resident_enrollment?: true, access_new_consumer_application_sub_tab?: true))
+    allow(view).to receive(:policy_helper).and_return(double("Policy", view_admin_tabs?: true, access_outstanding_verification_sub_tab?: true, access_identity_verification_sub_tab?: true, view_the_configuration_tab?: true, can_access_user_account_tab?: true, begin_resident_enrollment?: true, access_new_consumer_application_sub_tab?: true, can_manage_qles?: true))
     sign_in user
     @calendar_events = [FactoryBot.create(:scheduled_event)]
     @scheduled_events = [FactoryBot.create(:scheduled_event)]
