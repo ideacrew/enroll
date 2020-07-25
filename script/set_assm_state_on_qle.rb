c6748969773fe3b83963716e3ac90557eb721b0c
saimekala07 <saimekala07@gmail.com>
2020-07-25 11:47:26 -0400

    Refs #ManageSEPTypes, update QLE script to set start date.

--- script/set_assm_state_on_qle.rb
+++ script/set_assm_state_on_qle.rb
@@ -1,5 +1,6 @@
 QualifyingLifeEventKind.where(is_active: true).each do |qle|
   visible = qle.is_self_attested ? true : false
-  qle.update_attributes(aasm_state: :active, is_visible: visible)
+  start_date = qle.start_on ? qle.start_on : qle.created_at
+  qle.update_attributes(aasm_state: :active, is_visible: visible, start_on: start_date)
   qle.workflow_state_transitions << WorkflowStateTransition.new(from_state: :draft, to_state: :active, comment: 'manually updating aasm state as part of Manage SEP type feature.')
 end
