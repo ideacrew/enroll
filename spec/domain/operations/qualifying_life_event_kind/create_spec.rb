072524c186eda5ea94e59b9110f36abd54c5247e
saimekala07 <saimekala07@gmail.com>
2020-07-27 08:38:15 -0400

    Refs #ManageSEPTypes, Adding specs publish operation.

--- spec/domain/operations/qualifying_life_event_kind/create_spec.rb
+++ spec/domain/operations/qualifying_life_event_kind/create_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Operations::QualifyingLifeEventKind::Create, type: :model, dbclea
         { 'start_on': "#{TimeKeeper.date_of_record.year}-07-01",
           'end_on': "#{TimeKeeper.date_of_record.year}-08-01",
           'title': 'test title',
-          'tool_tip': 'jhsdjhs',
+          'tool_tip': 'test tooltip',
           'pre_event_sep_in_days': '10',
           'is_self_attested': 'true',
           'reason': 'lost_access_to_mec',
@@ -53,25 +53,67 @@ RSpec.describe Operations::QualifyingLifeEventKind::Create, type: :model, dbclea
       end
 
       context 'for publish' do
-        before :each do
-          qlek_create_params.merge!({'publish': 'Publish'})
-          @result = subject.call(qlek_create_params)
+        context 'for success case', :dbclean => :after_each do
+          before :each do
+            qlek_create_params.merge!({'publish': 'Publish'})
+            @result = subject.call(qlek_create_params)
+          end
+
+          it 'should return success' do
+            expect(@result).to be_a(Dry::Monads::Result::Success)
+          end
+
+          it 'should return a qlek object' do
+            expect(@result.success).to be_a(::QualifyingLifeEventKind)
+          end
+
+          it 'should create QualifyingLifeEventKind object' do
+            expect(::QualifyingLifeEventKind.all.count).to eq(1)
+          end
+
+          it 'should publish the newly created qlek object' do
+            expect(QualifyingLifeEventKind.all.first.active?).to be_truthy
+          end
         end
 
-        it 'should return success' do
-          expect(@result).to be_a(Dry::Monads::Result::Success)
-        end
+        context 'for failure case', :dbclean => :after_each do
+          let!(:q1) {FactoryBot.create(:qualifying_life_event_kind, market_kind: 'individual', reason: 'lost_access_to_mec', aasm_state: :active, is_active: true)}
+          let!(:q2) {FactoryBot.create(:qualifying_life_event_kind, title: 'test title', market_kind: 'individual', reason: 'lost_access_to_mec', aasm_state: :draft, is_active: false)}
 
-        it 'should return a qlek object' do
-          expect(@result.success).to be_a(::QualifyingLifeEventKind)
-        end
+          let(:qlek_publish_params) do
+            { 'start_on': "#{TimeKeeper.date_of_record.year}-07-01",
+              'end_on': "#{TimeKeeper.date_of_record.year}-08-01",
+              'title': 'test title',
+              'tool_tip': 'test tooltip',
+              'pre_event_sep_in_days': '10',
+              'is_self_attested': 'true',
+              'reason': q2.reason,
+              'post_event_sep_in_days': '88',
+              'market_kind': 'individual',
+              'effective_on_kinds': ['date_of_event'],
+              'coverage_effective_on': "#{TimeKeeper.date_of_record.year}-07-01",
+              'coverage_end_on': "#{TimeKeeper.date_of_record.year}-08-01",
+              'event_kind_label': 'event kind label',
+              'is_visible': true,
+              'id': q2.id.to_s,
+               'publish': 'Publish',
+              'date_options_available': true }
+          end
+          before do
+            @result = subject.call(qlek_create_params)
+          end
 
-        it 'should create QualifyingLifeEventKind object' do
-          expect(::QualifyingLifeEventKind.all.count).to eq(1)
-        end
+          it 'should return failure' do
+            expect(@result).to be_a(Dry::Monads::Result::Failure)
+          end
 
-        it 'should publish the newly created qlek object' do
-          expect(QualifyingLifeEventKind.all.first.active?).to be_truthy
+          it 'should return error message' do
+            expect(@result.failure[1]).to eq(['Active SEP type exists with same reason'])
+          end
+
+          it 'should not publish qlek objects' do
+            expect(::QualifyingLifeEventKind.where(title: 'test title').first.aasm_state).to eq :draft
+          end
         end
       end
     end
