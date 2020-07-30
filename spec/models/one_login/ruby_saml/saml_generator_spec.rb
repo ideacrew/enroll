e9dcfeb7ad092344e9ff73515c2ae2b00c1906c2
ymhari <hariym471@gmail.com>
2020-07-30 09:36:12 -0400

    ref: 37695 Adds product to enrollment for spec failure.

--- spec/models/one_login/ruby_saml/saml_generator_spec.rb
+++ spec/models/one_login/ruby_saml/saml_generator_spec.rb
@@ -4,7 +4,8 @@ module OneLogin
   RSpec.describe RubySaml::SamlGenerator do
     let(:transaction_id)   { '1234' }
     let!(:family) { FactoryBot.create(:family, :with_primary_family_member_and_dependent) }
-    let!(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, household: family.active_household, aasm_state: 'shopping', family: family) }
+    let(:product) {FactoryBot.create(:benefit_markets_products_health_products_health_product, benefit_market_kind: :aca_individual, kind: :health, csr_variant_id: '01')}
+    let!(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, household: family.active_household, aasm_state: 'shopping', family: family, product: product) }
     let!(:hbx_enrollment_member1) { FactoryBot.create(:hbx_enrollment_member, applicant_id: family.primary_applicant.id, is_subscriber: true, eligibility_date: (TimeKeeper.date_of_record - 10.days), hbx_enrollment: hbx_enrollment) }
     let!(:hbx_enrollment_member2) { FactoryBot.create(:hbx_enrollment_member, applicant_id: family.family_members[1].id, eligibility_date: (TimeKeeper.date_of_record - 10.days), hbx_enrollment: hbx_enrollment) }
     let(:saml_generator) { OneLogin::RubySaml::SamlGenerator.new(transaction_id,hbx_enrollment) }
