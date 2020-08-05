ce14b84b50218d22313080c853ac924e30fe7c42
nisanth <nisanth.yaganti@gmail.com>
2020-08-05 12:08:49 -0500

    updating rubo cop errors

--- app/data_migrations/set_minimum_contribution_factor_on_contribution_level.rb
+++ app/data_migrations/set_minimum_contribution_factor_on_contribution_level.rb
@@ -11,7 +11,7 @@ class SetMinimumContributionFactorOnContributionLevel < MongoidMigrationTask
 
     batch_size = 50
     offset = 0
-    while (offset <= benefit_sponsorships.count)
+    while offset <= benefit_sponsorships.count
       benefit_sponsorships.offset(offset).limit(batch_size).no_timeout.each do |benefit_sponsorship|
         begin
           benefit_application = benefit_sponsorship.benefit_applications.where(:"effective_period.min".gte => time).first
@@ -20,7 +20,7 @@ class SetMinimumContributionFactorOnContributionLevel < MongoidMigrationTask
             sponsored_benefit = benefit_package.health_sponsored_benefit
             sponsor_contribution = sponsored_benefit.sponsor_contribution
             sponsor_contribution.contribution_levels.each do |contribution_level|
-              if contribution_level.display_name.match(/Employee/i)
+              if contribution_level.display_name =~ /Employee/i
                 contribution_level.update_attributes!(min_contribution_factor: 0.5)
               else
                 contribution_level.update_attributes!(min_contribution_factor: 0.33)
@@ -33,7 +33,7 @@ class SetMinimumContributionFactorOnContributionLevel < MongoidMigrationTask
           p "Unable to save assigned_contribution_model for #{benefit_sponsorship.legal_name} due to #{e.inspect}"
         end
       end
-      offset = offset + batch_size
+      offset += batch_size
       puts "offset count - #{offset}" unless Rails.env.test?
     end
   end
