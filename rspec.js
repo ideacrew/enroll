eee41385a8ae65d637358721401e7f8fb6850d0d
Mark Goho <markgoho@gmail.com>
2020-08-19 18:11:20 -0400

    use 6 jobs for rspec

--- rspec.js
+++ rspec.js
@@ -3,45 +3,27 @@ const { promises: fs } = require("fs");
 async function getJson() {
   const response = await fs.readFile("./ci/small-report.json", "utf-8");
 
-  const { type, features } = JSON.parse(response);
+  const { version, examples, summary, summary_line } = JSON.parse(response);
 
-  const filteredFeatures = features.filter((feature) => {
-    const state = feature.properties.STATE;
+  const splitConfig = examples.reduce((totalConfig, example) => {
+    const filePath = example.file_path;
 
-    return (
-      state === "09" ||
-      state === "10" ||
-      state === "11" ||
-      state === "12" ||
-      state === "24" ||
-      state === "34" ||
-      state === "36" ||
-      state === "42" ||
-      state === "51" ||
-      state === "54"
-    );
-  });
+    if (totalConfig[filePath] !== undefined) {
+      const currentTotal = totalConfig[filePath].runTime;
 
-  const featuresWithFips = filteredFeatures.map((feature) => {
-    const { type, properties, geometry } = feature;
+      return {
+        ...totalConfig,
+        [filePath]: { currentTotal: currentTotal + example.run_time },
+      };
+    } else {
+      return {
+        ...totalConfig,
+        [filePath]: { currentTotal: example.run_time },
+      };
+    }
+  }, {});
 
-    const updatedProperties = {
-      ...properties,
-      fips: `${properties.STATE}${properties.COUNTY}`,
-    };
-
-    return {
-      type,
-      geometry,
-      properties: updatedProperties,
-    };
-  });
-
-  const geojson = { type, features: featuresWithFips };
-
-  const jsonList = JSON.stringify(geojson);
-
-  await fs.writeFile("./public/mtb-counties.geo.json", jsonList);
+  console.log(splitConfig);
 }
 
 getJson();
