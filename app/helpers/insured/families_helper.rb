5956d02b819dacbf9980d5af8070597e36910fd2
saimekala07 <saimekala07@gmail.com>
2020-08-22 15:28:35 -0400

    Refs #ManageSEPTypes, update condition

--- app/helpers/insured/families_helper.rb
+++ app/helpers/insured/families_helper.rb
@@ -107,7 +107,8 @@ module Insured::FamiliesHelper
       title: qle.title, id: qle.id.to_s, label: qle.event_kind_label,
       is_self_attested: qle.is_self_attested,
       current_date: TimeKeeper.date_of_record.strftime("%m/%d/%Y"),
-      qle_event_date_kind: qle.qle_event_date_kind.to_s
+      qle_event_date_kind: qle.qle_event_date_kind.to_s,
+      reason: qle.reason
     }
 
     if qle.tool_tip.present?
