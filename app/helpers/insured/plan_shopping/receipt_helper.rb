87ffa7a3d73f681480bb4b4274a433f4d1e711be
ymhari <hariym471@gmail.com>
2020-08-28 17:38:43 -0400

    ref: 89373 excludes shop enrollments for helper.

--- app/helpers/insured/plan_shopping/receipt_helper.rb
+++ app/helpers/insured/plan_shopping/receipt_helper.rb
@@ -16,7 +16,10 @@ module Insured
 
       def has_any_previous_kaiser_enrollments?
         carrier = EnrollRegistry[:pay_now_functionality].setting(:carriers).item
-        all_kaiser_enrollments = @enrollment.family.hbx_enrollments.to_a.select { |enr| !enr.product_id.nil? && enr.product.issuer_profile.legal_name == carrier && enr.effective_on.year == @enrollment.effective_on.year }
+        all_kaiser_enrollments = @enrollment.family.hbx_enrollments.to_a.select do |enr|
+          enr_carrier = enr.product.issuer_profile.legal_name
+          !enr.product_id.nil? && !["employer_sponsored", "employer_sponsored_cobra"].include?(enr.kind) && enr_carrier == carrier && enr.effective_on.year == @enrollment.effective_on.year
+        end
         enrollments = all_kaiser_enrollments - @enrollment.to_a
         enrollments.present? ? true : false
       end
