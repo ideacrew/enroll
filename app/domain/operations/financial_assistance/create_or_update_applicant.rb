690add1df20bdd72e092c7b753436ee9c2320acb
saikumar9 <saikumarkotagiri9@gmail.com>
2020-09-14 19:38:58 -0400

    WIP - Operations: CreateOrUpdateApplicant and DropApplicant within Enroll Application

--- app/domain/operations/financial_assistance/create_or_update_applicant.rb
+++ app/domain/operations/financial_assistance/create_or_update_applicant.rb
@@ -8,7 +8,38 @@ module Operations
     class CreateOrUpdateApplicant
       include Dry::Monads[:result, :do]
 
-      def call(params:)
+      # on an after hook on every update of a family_member
+        # Call ParseApplicant for the family member
+        # Input: family_member
+        # FAA Engine Call.
+        # Success(application_id)
+        # Success(family.active_family_members.collect {|family_member| family_member_attributes(family_member)})
+
+      def call(params)
+        values              = yield validate(params)
+        financial_applicant = yield parse_family_member(values)
+        result              = yield create_or_update_applicant(financial_application)
+
+        Success(result)
+      end
+
+      private
+
+      def validate(params)
+        Failure('Given family member is not a valid object') unless params[:family_member].is_a?(::FamilyMember)
+        Failure('Given family member does not have a matching person') unless params[:family_member].person.present?
+
+        Success(params)
+      end
+
+      def parse_family_member(values)
+        member_attrs_result = ::Operations::FinancialAssistance::ParseApplicant.new.call(values)
+        member_attrs_result.success? ? Success(member_attrs_result.success) : Failure(member_attrs_result.failure)
+      end
+
+      def create_or_update_applicant(financial_application)
+        result = ::FinancialAssistance::Operations::Applicant::CreateOrUpdate.new.call(financial_applicant: financial_applicant)
+        result.success? ? Success(result.success) : Failure(result.failure)
       end
     end
   end
--- app/domain/operations/financial_assistance/drop_applicant.rb
+++ app/domain/operations/financial_assistance/drop_applicant.rb
@@ -8,7 +8,39 @@ module Operations
     class DropApplicant
       include Dry::Monads[:result, :do]
 
+      # This is called when we soft delete
       def call(params:)
+        # Input: family_member
+        values              = yield validate(params)
+        financial_applicant = yield parse_family_member(values)
+        result              = yield delete_applicant(financial_application)
+
+        Success(result)
+      end
+
+      private
+
+      def validate(params)
+        Failure('Given family member is not a valid object') unless params[:family_member].is_a?(::FamilyMember)
+        Failure('Given family member is an active object') if params[:family_member].is_active
+        Failure('Given family member does not have a matching person') unless params[:family_member].person.present?
+
+        Success(params)
+
+        # check the class.
+        # check the field is_active is false.
+        # check if the object is persisted.
+      end
+
+      def parse_family_member(values)
+        member_attrs_result = ::Operations::FinancialAssistance::ParseApplicant.new.call(values)
+        member_attrs_result.success? ? Success(member_attrs_result.success) : Failure(member_attrs_result.failure)
+      end
+
+      def create_or_update_applicant(financial_application)
+        # FAA Engine Call.
+        result = ::FinancialAssistance::Operations::Applicant::Delete.new.call({financial_applicant: financial_applicant})
+        result.success? ? Success(result.success) : Failure(result.failure)
       end
     end
   end
