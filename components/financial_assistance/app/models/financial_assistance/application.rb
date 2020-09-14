e833e1d94dcaa278815cd5edcfb7a6da28d7b494
saikumar9 <saikumarkotagiri9@gmail.com>
2020-09-14 09:54:10 -0400

    Refs #174786575, import applicants

--- components/financial_assistance/app/models/financial_assistance/application.rb
+++ components/financial_assistance/app/models/financial_assistance/application.rb
@@ -659,7 +659,8 @@ module FinancialAssistance
         result_object = ::FinancialAssistance::Operations::Applicant::Build.new.call(params: member_attributes)
         next unless result_object.success?
         applicant_params = result_object.success.to_h
-        applicants.build(applicant_params)
+        applicant = applicants.where(person_hbx_id: applicant_params[:person_hbx_id]).first
+        applicant ? applicant.assign_attributes(applicant_params) : applicants.build(applicant_params)
       end
     end
 
