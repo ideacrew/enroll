a9d9f2cbd5e91fe7182d6d43a4bb90880f3bb401
Sai Praveen <saipraveen.gudimetla@gmail.com>
2020-09-14 18:08:01 -0400

    modify address contract and add specs

--- app/domain/validators/address_contract.rb
+++ app/domain/validators/address_contract.rb
@@ -4,14 +4,14 @@ module Validators
   class AddressContract < Dry::Validation::Contract
 
     params do
-      required(:kind).maybe(:string)
-      required(:address_1).maybe(:string)
+      required(:kind).filled(:string)
+      required(:address_1).filled(:string)
       optional(:address_2).maybe(:string)
       optional(:address_3).maybe(:string)
-      required(:city).maybe(:string)
+      required(:city).filled(:string)
       optional(:county).maybe(:string)
-      required(:state).maybe(:string)
-      required(:zip).maybe(:string)
+      required(:state).filled(:string)
+      required(:zip).filled(:string)
       optional(:country_name).maybe(:string)
     end
   end
