15d1e239a566715eeaf3870809dcdb44666cf8bb
Vishal Kalletla <vishuk1201@gmail.com>
2020-09-16 14:47:06 -0400

    ref 174842324: spec wip

--- components/financial_assistance/spec/domain/financial_assistance/operations/applicant/create_or_update_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applicant/create_or_update_spec.rb
@@ -4,68 +4,67 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe FinancialAssistance::Operations::Applicant::CreateOrUpdate, dbclean: :after_each do
-
   let(:family_id) { BSON::ObjectId.new }
-  let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family_id, aasm_state: 'draft') }
-  let!(:applicant) do
-    FactoryBot.create(:financial_assistance_applicant,
-                      :with_work_phone,
-                      :with_work_email,
-                      :with_home_address,
-                      application: application,
-                      ssn: '889984400',
-                      dob: (Date.today - 10.years),
-                      first_name: 'james',
-                      last_name: 'bond',
-                      is_primary_applicant: true)
-  end
-
-  let(:applicant_params) do
-    {:person_hbx_id=>"13cce9fe14b04209b2443330900108d8",
-     :ssn=>"889984400",
-     :dob=>(Date.today - 10.years).strftime("%d/%m/%Y"),
-     first_name: 'james_test',
-     last_name: 'bond',
-     gender: 'male',
-     :is_applying_coverage=>true,
-     :citizen_status=>"us_citizen",
-     :is_consumer_role=>true,
-     :same_with_primary=>false,
-     :indian_tribe_member=>false,
-     :is_incarcerated=>true,
-     :addresses => [{
-      "address_2"=>"#111",
-      "address_3"=>"",
-      "county"=>"Hampden",
-      "country_name"=>"",
-      "kind"=>"home",
-      "address_1"=>"1111 Awesome Street",
-      "city"=>"Washington",
-      "state"=>"DC",
-      "zip"=>"01001"}],
-    :phones=>[
-      {
-       "country_code"=>"",
-       "area_code"=>"202",
-       "number"=>"1111111",
-       "extension"=>"1",
-       "full_phone_number"=>"20211111111",
-       "kind"=>"home"}],
-    :emails=>[
-      {"kind"=>"home", "address"=>"example1@example.com"}],
-    :family_member_id=>BSON::ObjectId('5f60c648bb40ee0c3d288a83'),
-    :is_primary_applicant=>true,
-    :is_consent_applicant=>false,
-    :relationship=>"self"}
-  end
 
   describe 'when a draft application is present' do
-
-    before do
-      @result = subject.call(params: applicant_params, family_id: family_id)
-    end
-
     context "and the incoming payload and existing attributes are different" do
+      let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family_id, aasm_state: 'draft') }
+      let!(:applicant) do
+        FactoryBot.create(:financial_assistance_applicant,
+                          :with_work_phone,
+                          :with_work_email,
+                          :with_home_address,
+                          application: application,
+                          ssn: '889984400',
+                          dob: (Date.today - 10.years),
+                          first_name: 'james',
+                          last_name: 'bond',
+                          is_primary_applicant: true)
+      end
+
+      let(:applicant_params) do
+        {:person_hbx_id=>"13cce9fe14b04209b2443330900108d8",
+         :ssn=>"889984401",
+         :dob=>(Date.today - 1.years).strftime("%d/%m/%Y"),
+         first_name: "childfirst",
+         last_name: "childlast",
+         gender: "male",
+         :is_applying_coverage=>true,
+         :citizen_status=>"us_citizen",
+         :is_consumer_role=>true,
+         :same_with_primary=>false,
+         :indian_tribe_member=>false,
+         :is_incarcerated=>true,
+         :addresses => [{
+                            "address_2"=>"#111",
+                            "address_3"=>"",
+                            "county"=>"Hampden",
+                            "country_name"=>"",
+                            "kind"=>"home",
+                            "address_1"=>"1111 Awesome Street",
+                            "city"=>"Washington",
+                            "state"=>"DC",
+                            "zip"=>"01001"}],
+         :phones=>[
+             {
+                 "country_code"=>"",
+                 "area_code"=>"202",
+                 "number"=>"1111111",
+                 "extension"=>"1",
+                 "full_phone_number"=>"20211111111",
+                 "kind"=>"home"}],
+         :emails=>[
+             {"kind"=>"home", "address"=>"example1@example.com"}],
+         :family_member_id=>BSON::ObjectId('5f60c648bb40ee0c3d288a83'),
+         :is_primary_applicant=>true,
+         :is_consent_applicant=>false,
+         :relationship=>"child"}
+      end
+
+      before do
+        @result = subject.call(params: applicant_params, family_id: family_id)
+      end
+
       it 'should return a success object' do
         expect(@result).to be_a(Dry::Monads::Result::Success)
       end
@@ -75,9 +74,153 @@ RSpec.describe FinancialAssistance::Operations::Applicant::CreateOrUpdate, dbcle
       end
 
       it 'should create a applicant object' do
+        #relationship creation issue
         expect(application.reload.applicants.count).to eq(2)
       end
     end
+
+    context "and the incoming payload and existing attributes are same" do
+      let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family_id, aasm_state: 'draft') }
+      let!(:applicant) do
+        FactoryBot.create(:financial_assistance_applicant,
+                          :with_work_phone,
+                          :with_work_email,
+                          :with_home_address,
+                          application: application,
+                          ssn: '889984400',
+                          dob: (Date.today - 10.years),
+                          first_name: 'james',
+                          last_name: 'bond',
+                          :is_applying_coverage=>true,
+                          :citizen_status=>"us_citizen",
+                          :is_consumer_role=>true,
+                          :same_with_primary=>false,
+                          :indian_tribe_member=>false,
+                          :is_incarcerated=>true,
+                          :is_primary_applicant=>true,
+                          :is_consent_applicant=>false,
+                          :is_disabled => false,
+                          :family_member_id=>BSON::ObjectId('5f60c648bb40ee0c3d288a83'),
+                          :relationship=>"child")
+      end
+
+      let!(:applicant_params) {applicant.serializable_hash.deep_symbolize_keys.merge(:ssn => '889984400', :relationship => "self")}
+
+      before do
+        @result = subject.call(params: compare(applicant_params), family_id: family_id)
+      end
+
+      it 'should return a success object' do
+        expect(@result).to be_a(Dry::Monads::Result::Failure)
+      end
+
+      it 'should return applicant object' do
+        expect(@result.failure).to eq("No information is changed")
+      end
+
+      it 'should create a applicant object' do
+        expect(application.reload.applicants.count).to eq(1)
+      end
+    end
+
+    context "and the incoming payload and existing attributes are same but attributes order changed" do
+
+      let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family_id, aasm_state: 'draft') }
+      let!(:applicant) do
+        FactoryBot.create(:financial_assistance_applicant,
+                          :with_work_phone,
+                          :with_work_email,
+                          :with_home_address,
+                          application: application,
+                          ssn: '889984400',
+                          dob: (Date.today - 10.years),
+                          first_name: 'james',
+                          last_name: 'bond',
+                          :is_applying_coverage=>true,
+                          :citizen_status=>"us_citizen",
+                          :is_consumer_role=>true,
+                          :same_with_primary=>false,
+                          :indian_tribe_member=>false,
+                          :is_incarcerated=>true,
+                          :is_primary_applicant=>true,
+                          :is_consent_applicant=>false,
+                          :is_disabled => false,
+                          :family_member_id=>BSON::ObjectId('5f60c648bb40ee0c3d288a83'),
+                          :relationship=>"child")
+      end
+
+      let!(:applicant_params){{
+                               :addresses=>[
+                                   {:address_1=>"1112 Awesome Street", :address_2=>"#112", :address_3=>"",
+                                    :city=>"Washington", :country_name=>"", :county=>"Hampden", :kind=>"work", :state=>"DC", :zip=>"01001"},
+                                   {:address_1=>"1111 Awesome Street", :address_2=>"#111", :address_3=>"",
+                                   :city=>"Washington", :country_name=>"", :county=>"Hampden", :kind=>"home", :state=>"DC", :zip=>"01001"}],
+                               :alien_number=>nil,
+                               :card_number=>nil,
+                               :citizen_status=>"us_citizen",
+                               :citizenship_number=>nil,
+                               :country_of_citizenship=>nil,
+                               :dob=> (Date.today - 10.years).strftime("%d/%m/%Y"),
+                               :emails=>[{:address=>"example1@example.com", :kind=>"work"}],
+                               :encrypted_ssn=>"wFDFw1whehQ1Udku1/79DA==\n",
+                               :ethnicity=>nil,
+                               :expiration_date=>nil,
+                               :family_member_id=>BSON::ObjectId('5f60c648bb40ee0c3d288a83'),
+                               :first_name=>"james",
+                               :gender=>nil,
+                               :i94_number=>nil,
+                               :indian_tribe_member=>false,
+                               :is_active=>true,
+                               :is_applying_coverage=>true,
+                               :is_consent_applicant=>false,
+                               :is_consumer_role=>true,
+                               :is_incarcerated=>true,
+                               :is_primary_applicant=>true,
+                               :is_ssn_applied=>nil,
+                               :is_tobacco_user=>"unknown",
+                               :issuing_country=>nil,
+                               :last_name=>"bond",
+                               :middle_name=>nil,
+                               :name_pfx=>nil,
+                               :name_sfx=>nil,
+                               :naturalization_number=>nil,
+                               :no_ssn=>"0",
+                               :passport_number=>nil,
+                               :person_hbx_id=> applicant.person_hbx_id,
+                               :phones=>[{:area_code=>"202", :country_code=>"", :extension=>"1", :full_phone_number=>"", :kind=>"work", :number=>"1111111", :primary=>nil}],
+                               :race=>nil,
+                               :receipt_number=>nil,
+                               :same_with_primary=>false,
+                               :sevis_id=>nil,
+                               :tribal_id=>nil,
+                               :visa_number=>nil,
+                               :vlp_document_id=>nil,
+                               :vlp_subject=>nil,
+                               :relationship=>"self",
+                               :ssn=>"889984400"}}
+
+      before do
+        applicant.addresses << FactoryBot.build(:financial_assistance_address, kind: "work")
+        applicant.save!
+
+        @result = subject.call(params: compare(applicant_params), family_id: family_id)
+      end
+
+      it 'should return a success object' do
+
+        expect(@result).to be_a(Dry::Monads::Result::Failure)
+      end
+
+      it 'should return applicant object' do
+        expect(@result.failure).to eq("No information is changed")
+      end
+
+      it 'should create a applicant object' do
+        expect(application.reload.applicants.count).to eq(1)
+      end
+
+    end
+
 
     # context "and there's no difference between the incoming payload and existing attributes" do
     #   it "should not update nor persist the record" do
@@ -88,32 +231,51 @@ RSpec.describe FinancialAssistance::Operations::Applicant::CreateOrUpdate, dbcle
 
   end
 
-  describe 'when a draft application does not exist' do
-    before do
-      application.update_attributes!(aasm_state: 'submitted')
-      @result = subject.call(params: applicant_params, family_id: family_id)
-    end
+  # describe 'when a draft application does not exist' do
+  #   before do
+  #     application.update_attributes!(aasm_state: 'submitted')
+  #     @result = subject.call(params: applicant_params, family_id: family_id)
+  #   end
+  #
+  #   it 'should return a failure object' do
+  #     expect(@result).to be_a(Dry::Monads::Result::Failure)
+  #   end
+  #
+  #   it 'should return failure message' do
+  #     expect(@result.failure).to eq('Application Not Found')
+  #   end
+  # end
+  #
+  # describe 'invalid params' do
+  #   before do
+  #     @result = subject.call(params: {test: 'test'}, family_id: family_id)
+  #   end
+  #
+  #   it 'should return a failure object' do
+  #     expect(@result).to be_a(Dry::Monads::Result::Failure)
+  #   end
+  #
+  #   it 'should return errors for failed applicant contract validation' do
+  #     expect(@result.failure.errors.present?).to be_truthy
+  #   end
+  # end
+end
 
-    it 'should return a failure object' do
-      expect(@result).to be_a(Dry::Monads::Result::Failure)
-    end
-
-    it 'should return failure message' do
-      expect(@result.failure).to eq('Application Not Found')
-    end
+def compare(applicant_db_hash)
+  sanitized_applicant_hash = applicant_db_hash.inject({}) do |db_hash, element_hash|
+    db_hash[element_hash[0]] = if [:addresses, :emails, :phones].include?(element_hash[0])
+                                 fetch_array_of_attrs_for_embeded_objects(element_hash[1])
+                               else
+                                 element_hash[1]
+                               end
+    db_hash
   end
+end
 
-  describe 'invalid params' do
-    before do
-      @result = subject.call(params: {test: 'test'}, family_id: family_id)
-    end
-
-    it 'should return a failure object' do
-      expect(@result).to be_a(Dry::Monads::Result::Failure)
-    end
-
-    it 'should return errors for failed applicant contract validation' do
-      expect(@result.failure.errors.present?).to be_truthy
-    end
+def fetch_array_of_attrs_for_embeded_objects(data)
+  new_arr = []
+  data.each do |special_hash|
+    new_arr << special_hash.except(:_id, :created_at, :updated_at, :tracking_version)
   end
+  new_arr
 end
