7684239d7c1904469b6641ce007c7a4fb345b2ac
Vishal Kalletla <vishuk1201@gmail.com>
2020-09-23 10:01:24 -0400

    89455: rubocop fix

--- app/models/family.rb
+++ app/models/family.rb
@@ -233,7 +233,7 @@ class Family
                                                     :"households.hbx_enrollments" => {
                                                       :$elemMatch => {
                                                         :sponsored_benefit_package_id => {"$in" => benefit_application.benefit_packages.pluck(:_id) },
-                                                        :aasm_state => {"$nin" => %w(coverage_canceled shopping coverage_terminated) }
+                                                        :aasm_state => {'$nin' => %w[coverage_canceled shopping coverage_terminated] }
                                                       }
                                                   })}
 
