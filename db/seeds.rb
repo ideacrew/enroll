61e0054cb71fffb89e716c3e11c1d948e7272197
vineel <sonnathivinil@gmail.com>
2020-10-02 14:24:05 -0400

    Benefit market catalog and ivl benefit packages

--- db/seeds.rb
+++ db/seeds.rb
@@ -140,6 +140,7 @@ if (ENV["type"] != "fixtures") && missing_plan_dumps
   # system "bundle exec rake load:benefit_market_catalog[2018]"
   system "bundle exec rake load:dc_benefit_market_catalog[2019]"
   system "bundle exec rake load:dc_benefit_market_catalog[2020]"
+  system "bundle exec rake load:dc_benefit_market_catalog[2021]"
   puts "::: complete :::"
   puts "*"*80
 
@@ -213,6 +214,7 @@ puts "Loading IVL benefit packages."
 # Need to fix this rake to handle based on year for IVL
 system "bundle exec rake import:create_2019_ivl_packages"
 system "bundle exec rake import:create_2020_ivl_packages"
+system "bundle exec rake import:create_2021_ivl_packages"
 puts "::: complete :::"
 puts "*"*80
 
