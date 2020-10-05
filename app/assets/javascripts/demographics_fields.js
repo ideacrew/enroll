699af9982707945148cde11f972fbe3615bd49e2
Sai Praveen <saipraveen.gudimetla@gmail.com>
2020-10-05 09:32:06 -0400

    change tribe to 'Tribe'

--- app/assets/javascripts/demographics_fields.js
+++ app/assets/javascripts/demographics_fields.js
@@ -177,7 +177,7 @@ function validationForIndianTribeMember() {
       return true;
     }
     if (!$("input#indian_tribe_member_yes").is(':checked') && !$("input#indian_tribe_member_no").is(':checked')) {
-      alert("Please select the option for 'Are you a member of an American Indian or Alaska Native tribe?'");
+      alert("Please select the option for 'Are you a member of an American Indian or Alaska Native Tribe?'");
       e.preventDefault && e.preventDefault();
       return false;
     };
