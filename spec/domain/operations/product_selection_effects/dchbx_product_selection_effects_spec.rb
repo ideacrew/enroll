8ac8716623192e6ad2be5476016866d76d029f13
saimekala07 <saimekala07@gmail.com>
2020-10-14 10:03:19 -0400

    Fix robo cop errors

--- spec/domain/operations/product_selection_effects/dchbx_product_selection_effects_spec.rb
+++ spec/domain/operations/product_selection_effects/dchbx_product_selection_effects_spec.rb
@@ -14,17 +14,14 @@ describe Operations::ProductSelectionEffects::DchbxProductSelectionEffects, "whe
 
   let(:consumer_role) { FactoryBot.create(:consumer_role) }
   let(:hbx_profile) do
-    FactoryBot.create(
-        :hbx_profile,
-        :normal_ivl_open_enrollment,
-        coverage_year: coverage_year
-    )
+    FactoryBot.create(:hbx_profile,
+                      :normal_ivl_open_enrollment,
+                      coverage_year: coverage_year)
   end
   let(:benefit_package) { benefit_coverage_period.benefit_packages.first }
   let(:benefit_coverage_period) do
     hbx_profile.benefit_sponsorship.benefit_coverage_periods.detect do |bcp|
-      (bcp.start_on.year == coverage_year) &&
-          bcp.start_on > bcp.open_enrollment_start_on
+      (bcp.start_on.year == coverage_year) && bcp.start_on > bcp.open_enrollment_start_on
     end
   end
   let(:family) {FactoryBot.create(:family, :with_primary_family_member, person: consumer_role.person)}
@@ -32,13 +29,11 @@ describe Operations::ProductSelectionEffects::DchbxProductSelectionEffects, "whe
     BenefitMarkets::Products::Product.find(benefit_package.benefit_ids.first)
   end
   let(:ivl_enrollment) do
-    FactoryBot.create(
-        :hbx_enrollment,
-        :individual_unassisted,
-        household: family.active_household,
-        effective_on: Date.new(coverage_year, 11, 1),
-        family: family
-    )
+    FactoryBot.create(:hbx_enrollment,
+                      :individual_unassisted,
+                      household: family.active_household,
+                      effective_on: Date.new(coverage_year, 11, 1),
+                      family: family)
   end
 
   let(:product_selection) do
@@ -70,25 +65,20 @@ describe Operations::ProductSelectionEffects::DchbxProductSelectionEffects, "whe
 
   let(:consumer_role) { FactoryBot.create(:consumer_role) }
   let(:hbx_profile) do
-    FactoryBot.create(
-        :hbx_profile,
-        :normal_ivl_open_enrollment,
-        coverage_year: coverage_year
-    )
+    FactoryBot.create(:hbx_profile,
+                      :normal_ivl_open_enrollment,
+                      coverage_year: coverage_year)
   end
   let(:benefit_package) { benefit_coverage_period.benefit_packages.first }
   let(:benefit_coverage_period) do
     hbx_profile.benefit_sponsorship.benefit_coverage_periods.detect do |bcp|
-      (bcp.start_on.year == coverage_year) &&
-          bcp.start_on > bcp.open_enrollment_start_on
+      (bcp.start_on.year == coverage_year) && bcp.start_on > bcp.open_enrollment_start_on
     end
   end
   let(:family) do
-    FactoryBot.create(
-        :family,
-        :with_primary_family_member,
-        person: consumer_role.person
-    )
+    FactoryBot.create(:family,
+                      :with_primary_family_member,
+                      person: consumer_role.person)
   end
   let(:product) do
     BenefitMarkets::Products::Product.find(benefit_package.benefit_ids.first)
@@ -104,16 +94,14 @@ describe Operations::ProductSelectionEffects::DchbxProductSelectionEffects, "whe
     r_product
   end
   let(:ivl_enrollment) do
-    FactoryBot.create(
-        :hbx_enrollment,
-        :individual_unassisted,
-        :with_enrollment_members,
-        enrollment_members: family.family_members,
-        household: family.active_household,
-        effective_on: Date.new(coverage_year, 11, 1),
-        family: family,
-        product: product
-    )
+    FactoryBot.create(:hbx_enrollment,
+                      :individual_unassisted,
+                      :with_enrollment_members,
+                      enrollment_members: family.family_members,
+                      household: family.active_household,
+                      effective_on: Date.new(coverage_year, 11, 1),
+                      family: family,
+                      product: product)
   end
 
   let(:product_selection) do
