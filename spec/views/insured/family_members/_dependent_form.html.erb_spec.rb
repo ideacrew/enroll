b6f9c35b2a8ddf02c2da4e829059585c2cafba6a
Jayanth <jayanth.rordev@gmail.com>
2020-10-27 12:43:33 -0400

    refs#90552 Update age off checkbox for CADC Resident

--- spec/views/insured/family_members/_dependent_form.html.erb_spec.rb
+++ spec/views/insured/family_members/_dependent_form.html.erb_spec.rb
@@ -87,8 +87,8 @@ describe "insured/family_members/_dependent_form.html.erb" do
     end
 
     it "should have age off exclusion field for consumer role only" do
-      expect(rendered).to match "Ageoff Exclusion"
-      expect(rendered).to have_field('age_off_excluded', checked: false)
+      expect(rendered).not_to match "Ageoff Exclusion"
+      expect(rendered).not_to have_field('age_off_excluded', checked: false)
     end
   end
 
