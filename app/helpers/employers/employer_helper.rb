ad6581d4f3fce65c1716f5dd4a1d4223c069e454
Nikhil S <nikhilks219@gmail.com>
2020-11-12 13:28:37 -0500

    90744: PY Reinstate - UI

--- app/helpers/employers/employer_helper.rb
+++ app/helpers/employers/employer_helper.rb
@@ -288,19 +288,19 @@ module Employers::EmployerHelper
     return [links, content]
   end
 
-  def is_rehired(ce)
-    (ce.coverage_terminated_on.present?  && (ce.is_eligible? || ce.employee_role_linked?))
+  def is_rehired(census_employee)
+    (census_employee.coverage_terminated_on.present? && (census_employee.is_eligible? || census_employee.employee_role_linked?))
   end
 
-  def is_terminated(ce)
-    (ce.coverage_terminated_on.present? && !(ce.is_eligible? || ce.employee_role_linked?))
+  def is_terminated(census_employee)
+    (census_employee.coverage_terminated_on.present? && !(census_employee.is_eligible? || census_employee.employee_role_linked?))
   end
 
   def selected_benefit_plan(plan)
     case plan
-      when :single_issuer then 'One Carrier'
-      when :metal_level then 'One Level'
-      when :single_product then 'A Single Plan'
+    when :single_issuer then 'One Carrier'
+    when :metal_level then 'One Level'
+    when :single_product then 'A Single Plan'
     end
   end
 
@@ -310,5 +310,11 @@ module Employers::EmployerHelper
 
   def display_referred_by_field_for_employer?
     Settings.aca.employer_registration_has_referred_by_field
+  end
+
+  def display_reinstate_benefit_application?(application)
+    return false unless [:terminated, :termination_pending, :retroactive_cancel].include?(application.aasm_state)
+    current_month = TimeKeeper.date_of_record.beginning_of_month
+    ((current_month - 11.months)..current_month).cover?(application.effective_period.min.to_date)
   end
 end
--- features/employers/benefit_applications/reinstate/admin_enable_reinstate_benefit_application_permission.feature
+++ features/employers/benefit_applications/reinstate/admin_enable_reinstate_benefit_application_permission.feature
@@ -3,9 +3,9 @@ Feature: As an admin user I should have the ability to <action> reinstate button
   Scenario Outline: Setup site, employer, and benefit application
     Given the Reinstate feature configuration is <feature_switch>
     And a CCA site exists with a benefit market
-    And benefit market catalog exists for terminated initial employer with health benefits
+    And benefit market catalog exists for <aasm_state> initial employer with health benefits
     And there is an employer ABC Widgets
-    And initial employer ABC Widgets has terminated benefit application
+    And initial employer ABC Widgets has <aasm_state> benefit application
     And that a user with a HBX staff role with Super Admin subrole exists and is logged in
     And the user is on the Employer Index of the Admin Dashboard
     When the user clicks Action for that Employer
@@ -16,6 +16,10 @@ Feature: As an admin user I should have the ability to <action> reinstate button
     And user logs out
 
   Examples:
-    | feature_switch | action  |
-    |    enabled     | see     |
-    |    disabled    | not see |
+    | feature_switch |    aasm_state       | action  |
+    |    enabled     |    terminated       | see     |
+    |    enabled     | termination_pending | see     |
+    # |    enabled     | retroactive_cancel  | see     |
+    |    enabled     |      active         | not see |
+    |    disabled    |      active         | not see |
+    |    disabled    |    terminated       | not see |
