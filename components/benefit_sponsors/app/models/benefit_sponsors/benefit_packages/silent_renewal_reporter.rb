c5afe2352e2f5e12b775e7ce3af332a55a797c7c
Trey Evans <tevans@justplainawful.lan>
2020-11-12 11:02:11 -0500

    Fix rubocop.

--- components/benefit_sponsors/app/models/benefit_sponsors/benefit_packages/silent_renewal_reporter.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/benefit_packages/silent_renewal_reporter.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
   module BenefitPackages
     class SilentRenewalReporter
-      def report_renewal_failure(_census_employee, _benefit_package, _failure)
-      end
+      def report_renewal_failure(_census_employee, _benefit_package, _failure); end
     end
   end
 end
