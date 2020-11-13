f92e7414f683e5e6b220ce64d0f1184ca7916ae0
Nikhil S <nikhilks219@gmail.com>
2020-11-13 18:21:22 -0500

    90749: PY Reinstate - Transmission Option

--- features/employers/benefit_applications/admin_reinstate_benefit_application_steps.rb
+++ features/employers/benefit_applications/admin_reinstate_benefit_application_steps.rb
@@ -11,3 +11,11 @@ end
 Then(/^the user will (.*) Reinstate button$/) do |action|
   action == 'see' ? (page.has_css?('Reinstate') == true) : (page.has_css?('Reinstate') == false)
 end
+
+When("Admin clicks on Reinstate button") do
+  find('li', :text => 'Reinstate').click
+end
+
+Then("Admin will see transmit to carrier checkbox") do
+  expect(page).to have_content('Transmit to Carrier')
+end
