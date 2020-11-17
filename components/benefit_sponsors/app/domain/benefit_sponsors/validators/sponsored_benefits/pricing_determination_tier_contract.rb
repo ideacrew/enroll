dc9a1bd548b544a358d6544742fd7b563cde366a
saikumar9 <saikumarkotagiri9@gmail.com>
2020-11-17 09:04:08 -0500

    Refs #90852, rubocop fixes

--- components/benefit_sponsors/app/domain/benefit_sponsors/validators/sponsored_benefits/pricing_determination_tier_contract.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/validators/sponsored_benefits/pricing_determination_tier_contract.rb
@@ -2,14 +2,14 @@
 
 module BenefitSponsors
   module Validators
-  	module SponsoredBenefits
-	    class PricingDeterminationTierContract < Dry::Validation::Contract
+    module SponsoredBenefits
+      class PricingDeterminationTierContract < Dry::Validation::Contract
 
-	      params do
-	        required(:pricing_unit_id).filled(Types::Bson)
-	        required(:price).filled(:float)
-	      end
-	    end
-	  end
+        params do
+          required(:pricing_unit_id).filled(Types::Bson)
+          required(:price).filled(:float)
+        end
+      end
+    end
   end
 end
