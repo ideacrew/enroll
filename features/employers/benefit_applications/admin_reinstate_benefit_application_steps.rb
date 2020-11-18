6a1d795f91a2939d2a3e1dc45b3ea914381470bf
Nikhil S <nikhilks219@gmail.com>
2020-11-18 18:44:37 -0500

    90749: reinstate start date display

--- features/employers/benefit_applications/admin_reinstate_benefit_application_steps.rb
+++ features/employers/benefit_applications/admin_reinstate_benefit_application_steps.rb
@@ -20,8 +20,15 @@ Then("Admin will see transmit to carrier checkbox") do
   expect(page).to have_content('Transmit to Carrier')
 end
 
-Then("Admin will fill Reinstate date") do
-  fill_in "Select Term Date", with: TimeKeeper.date_of_record.beginning_of_month.to_s
+Then(/^Admin will see Reinstate Start Date for (.*) benefit application$/) do |aasm_state|
+  ben_app = ::BenefitSponsors::Organizations::Organization.find_by(legal_name: /ABC Widgets/).active_benefit_sponsorship.benefit_applications.first
+  expect(page.all('tr').detect { |tr| tr[:id] == ben_app.id.to_s }.present?).to eq true
+  if ['terminated','termination_pending'].include?(aasm_state)
+    value = page.all('input').detect { |input| input[:reinstate_start_date] == ben_app.end_on.next_day.to_date.to_s}
+    expect(value.present?).to eq true
+  else
+    expect(page).to have_content(ben_app.start_on.to_date.to_s)
+  end
 end
 
 When("Admin clicks on Submit button") do
