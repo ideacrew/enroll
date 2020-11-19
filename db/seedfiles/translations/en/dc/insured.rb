a4aa10f9fa4600baa29070dda3f61095a5427246
RyanEddyIC <ryan.eddy@ideacrew.com>
2020-11-19 14:00:42 -0500

    DC to ME changes

--- db/seedfiles/translations/en/dc/insured.rb
+++ db/seedfiles/translations/en/dc/insured.rb
@@ -308,8 +308,8 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.family_info' => "Family Info",
   :'en.account_setup' => "Account Setup",
   :'en.family_information' => "Family Information",
-  :'en.insured.homeless_dc' => "I'm a homeless DC resident.",
-  :'en.insured.temp_outside_dc' => "I'm living outside of DC and plan to return, or am moving to DC but don’t have a DC address yet.",
+  :'en.insured.homeless_dc' => "I'm a homeless ME resident.",
+  :'en.insured.temp_outside_dc' => "I'm living outside of ME and plan to return, or am moving to ME but don’t have an ME address yet.",
   :'en.insured.family_members.index.continue_to_get_insurance' => "If you need to get health insurance coverage for other members of your family, select 'Add New Person'. When you’re finished, select CONTINUE.",
   :'en.insured.family_members.index.add_member_info_html' => "<span class='bold'>Here's your chance to add family members to your plan.</span><br>Add family members by clicking the 'Add Member' button. If you don't need to add more members, or when you are done adding members, click continue.",
   :'en.month' => "Month",
@@ -436,4 +436,3 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.insured.plan_shoppings.thankyou.you_must_complete_steps_to_enroll' => "You must complete these steps to enroll.",
   :'en.insured.plan_shoppings.thankyou.additional_administration_msg' => "* Your employer may charge an additional administration fee for your COBRA/Continuation coverage. If you have any questions, please direct them to the Employer."
 }.freeze
-
