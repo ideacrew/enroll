6193b635b6cef764662461adac2969486c6019ac
saikumar9 <saikumarkotagiri9@gmail.com>
2020-11-19 15:29:27 -0500

    Refs #90809, Operations Clone and Build for HbxEnrollment with tests, code refactor for Operation Reinstate to make use of Build and Clone operations

--- app/domain/operations/hbx_enrollments/reinstate.rb
+++ app/domain/operations/hbx_enrollments/reinstate.rb
@@ -7,8 +7,8 @@ module Operations
     # is a new hbx_enrollment. The effective_period of the newly
     # created hbx_enrollment depends on the aasm_state of the input
     # hbx_enrollment. The aasm_state of the newly created application
-    # will be shopping. Currently, this operation does not account for
-    # APTC related values yet for IVL cases.
+    # will be shopping. Currently, this operation supports SHOP
+    # enrollments only does not account for IVL cases.
     class Reinstate
       include Dry::Monads[:result, :do]
 
@@ -16,11 +16,10 @@ module Operations
       # @return [ HbxEnrollment ] hbx_enrollment
       def call(params)
         values           = yield validate(params)
-        @effective_on    = yield fetch_effective_on(values)
-        _filtered_values = yield filter(values)
-        new_enr          = yield reinstate_hbx_enrollment
+        new_enr          = yield new_hbx_enrollment(values)
+        hbx_enrollment   = yield reinstate_hbx_enrollment(new_enr)
 
-        Success(new_enr)
+        Success(hbx_enrollment)
       end
 
       private
@@ -28,21 +27,17 @@ module Operations
       def validate(params)
         return Failure('Missing Key.') unless params.key?(:hbx_enrollment)
         return Failure('Not a valid HbxEnrollment object.') unless params[:hbx_enrollment].is_a?(HbxEnrollment)
+        valid_states_for_reinstatement = ['coverage_terminated', 'coverage_termination_pending', 'coverage_canceled']
+        return Failure("Given HbxEnrollment is not in any of the #{valid_states_for_reinstatement} states.") unless valid_states_for_reinstatement.include?(params[:hbx_enrollment].aasm_state)
+        init_instance_variables(params)
+        return Failure("HbxEnrollment cannot be reinstated. Employer Sponsored Benefits no longer offers the plan/product: #{@reinstate_plan.name}.") unless can_be_reinstated?
 
         Success(params)
       end
 
-      def filter(values)
-        valid_states_for_reinstatement = ['coverage_terminated', 'coverage_termination_pending', 'coverage_canceled']
-        return Failure("Given HbxEnrollment is not in any of the #{valid_states_for_reinstatement} states.") unless valid_states_for_reinstatement.include?(values[:hbx_enrollment].aasm_state)
-        init_instance_variables(values)
-        return Failure("HbxEnrollment cannot be reinstated. Employer Sponsored Benefits no longer offers the plan/product: #{@reinstate_plan.name}.") unless can_be_reinstated?
-
-        Success(values)
-      end
-
       def init_instance_variables(values)
         @current_enr = values[:hbx_enrollment]
+        @effective_on = fetch_effective_on(values)
         return unless @current_enr.is_shop?
 
         benefit_application = @current_enr.sponsored_benefit_package.benefit_application
@@ -66,49 +61,41 @@ module Operations
         @current_enr = params[:hbx_enrollment]
         case @current_enr.aasm_state
         when 'coverage_terminated'
-          Success(@current_enr.terminated_on.next_day)
+          @current_enr.terminated_on.next_day
         when 'coverage_termination_pending'
-          Success(@current_enr.terminated_on.next_day)
+          @current_enr.terminated_on.next_day
         when 'coverage_canceled'
-          Success(@current_enr.effective_on)
-        else
-          Failure("Cannot determine effective_on because of the aasm_state: #{@current_enr.aasm_state}")
+          @current_enr.effective_on
         end
       end
 
-      def reinstate_hbx_enrollment
-        new_enr = init_hbx_enrollment
-        @current_enr.hbx_enrollment_members.each do |member|
-          init_hbx_enrollment_member(member, new_enr)
+      def new_hbx_enrollment(values)
+        clone_result = Clone.new.call({hbx_enrollment: values[:hbx_enrollment], effective_on: @effective_on, options: additional_attrs})
+        return clone_result if clone_result.failure?
+        new_enrollment = clone_result.success
+        update_member_coverage_dates(new_enrollment.hbx_enrollment_members)
+        new_enrollment.save!
+        Success(new_enrollment)
+      end
+
+      def update_member_coverage_dates(members)
+        members.each do |member|
+          member.eligibility_date = @effective_on
+          member.coverage_start_on = member_coverage_start_date(member)
         end
-        new_enr.save!
-        Success(new_enr)
       end
 
-      def init_hbx_enrollment
-        ba_params = @current_enr.attributes.deep_symbolize_keys.slice(:coverage_kind, :enrollment_kind, :kind, :predecessor_enrollment_id)
-        ba_params.merge!({aasm_state: 'shopping', effective_on: @effective_on})
-        ba_params.merge!(additional_attrs)
-        new_enr = HbxEnrollment.new
-        new_enr.assign_attributes(ba_params)
-        new_enr.family = @current_enr.family
-        new_enr.household = @current_enr.family.active_household
-        new_enr
-      end
-
-      def init_hbx_enrollment_member(current_member, new_enr)
-        member_params = current_member.attributes.deep_symbolize_keys.slice(:applicant_id, :is_subscriber)
-        member_params.merge!({coverage_start_on: member_coverage_start_date(current_member), eligibility_date: @effective_on})
-        new_member = new_enr.hbx_enrollment_members.new
-        new_member.assign_attributes(member_params)
-      end
-
-      def member_coverage_start_date(current_member)
+      def member_coverage_start_date(enrollment_member)
         if @current_enr.is_shop? && @can_reinstate_under_renewal_py
           @effective_on
         else
-          current_member.coverage_start_on || @current_enr.effective_on || @effective_on
+          enrollment_member.coverage_start_on || @current_enr.effective_on || @effective_on
         end
+      end
+
+      def reinstate_hbx_enrollment(new_enrollment)
+        # TODO: State transitions as needed.
+        Success(new_enrollment)
       end
 
       def fetch_renewal_bga
