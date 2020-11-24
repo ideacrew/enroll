06cca50b1859f9a1350437ce39fae8c616af6f64
saikumar9 <saikumarkotagiri9@gmail.com>
2020-11-24 15:36:30 -0500

    Refs #90963, fix failing spec

--- components/benefit_markets/spec/domain/benefit_markets/operations/benefit_sponsor_catalogs/clone_spec.rb
+++ components/benefit_markets/spec/domain/benefit_markets/operations/benefit_sponsor_catalogs/clone_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require "#{BenefitSponsors::Engine.root}/spec/shared_contexts/benefit_market.rb"
 
 RSpec.describe BenefitMarkets::Operations::BenefitSponsorCatalogs::Clone, dbclean: :after_each do
   before do
