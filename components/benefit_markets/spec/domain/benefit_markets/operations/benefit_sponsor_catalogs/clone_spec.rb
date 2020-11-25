3771b4dce8cca5b451ef0dcb2a2548c89e6f356c
saikumar9 <saikumarkotagiri9@gmail.com>
2020-11-25 09:16:26 -0500

    Refs #90963, Refactor spec to fix failures.

--- components/benefit_markets/spec/domain/benefit_markets/operations/benefit_sponsor_catalogs/clone_spec.rb
+++ components/benefit_markets/spec/domain/benefit_markets/operations/benefit_sponsor_catalogs/clone_spec.rb
@@ -7,7 +7,67 @@ RSpec.describe BenefitMarkets::Operations::BenefitSponsorCatalogs::Clone, dbclea
     DatabaseCleaner.clean
   end
 
-  let!(:benefit_sponsor_catalog) { FactoryBot.create(:benefit_markets_benefit_sponsor_catalog) }
+  let(:effective_date) { Date.new(Date.today.year,6,1) }
+  let(:member_relationships) do
+    [BenefitMarkets::ContributionModels::MemberRelationship.new(relationship_name: 'employee', relationship_kinds: ['self']),
+     BenefitMarkets::ContributionModels::MemberRelationship.new(relationship_name: 'spouse', relationship_kinds: ['spouse']),
+     BenefitMarkets::ContributionModels::MemberRelationship.new(relationship_name: 'domestic_partner', relationship_kinds: ['life_partner', 'domestic_partner']),
+     BenefitMarkets::ContributionModels::MemberRelationship.new(relationship_name: 'dependent', relationship_kinds: ['child', 'adopted_child', 'foster_child', 'stepchild', 'ward'])]
+  end
+  let(:contribution_units) do
+    [build(:benefit_markets_contribution_models_fixed_percent_contribution_unit,
+           :with_member_relationship_maps,
+           name: 'employee',
+           display_name: 'Employee',
+           order: 0,
+           default_contribution_factor: 0.0,
+           minimum_contribution_factor: 0.0,
+           member_relationship_operator: :==),
+     build(:benefit_markets_contribution_models_fixed_percent_contribution_unit,
+           :with_member_relationship_maps,
+           name: 'spouse',
+           display_name: 'Spouse',
+           order: 1,
+           default_contribution_factor: 0.0,
+           minimum_contribution_factor: 0.0),
+     build(:benefit_markets_contribution_models_fixed_percent_contribution_unit,
+           :with_member_relationship_maps,
+           name: 'domestic_partner',
+           display_name: 'Domestic Partner',
+           order: 2,
+           default_contribution_factor: 0.0,
+           minimum_contribution_factor: 0.0),
+     build(:benefit_markets_contribution_models_fixed_percent_contribution_unit,
+           :with_member_relationship_maps,
+           name: 'dependent',
+           display_name: 'Child Under 26',
+           order: 3,
+           default_contribution_factor: 0.0,
+           minimum_contribution_factor: 0.0)]
+  end
+  let(:contribution_model) do
+    cm_params = {sponsor_contribution_kind: '::BenefitSponsors::SponsoredBenefits::FixedPercentSponsorContribution',
+                 contribution_calculator_kind: '::BenefitSponsors::ContributionCalculators::SimpleShopReferencePlanContributionCalculator',
+                 title: "#{Settings.site.key.to_s.upcase} Shop Simple List Bill Contribution Model",
+                 key: :zero_percent_sponsor_fixed_percent_contribution_model,
+                 many_simultaneous_contribution_units: true,
+                 contribution_units: contribution_units,
+                 member_relationships: member_relationships}
+    BenefitMarkets::ContributionModels::ContributionModel.new(cm_params)
+  end
+  let(:product_package) do
+    FactoryBot.build(:benefit_markets_products_product_package, contribution_model: contribution_model, contribution_models: [contribution_model])
+  end
+  let!(:benefit_sponsor_catalog) do
+    BenefitMarkets::BenefitSponsorCatalog.new(effective_date: effective_date,
+                                              effective_period: effective_date..(effective_date + 1.year - 1.day),
+                                              open_enrollment_period: (effective_date - 1.month)..(effective_date - 1.month + 9.days),
+                                              probation_period_kinds: [:first_of_month, :first_of_month_after_30_days, :first_of_month_after_60_days],
+                                              product_packages: [product_package],
+                                              service_areas: [FactoryBot.build(:benefit_markets_locations_service_area)],
+                                              sponsor_market_policy: FactoryBot.build(:benefit_markets_market_policies_sponsor_market_policy),
+                                              member_market_policy: FactoryBot.build(:benefit_markets_market_policies_member_market_policy))
+  end
 
   context 'success' do
     before do
