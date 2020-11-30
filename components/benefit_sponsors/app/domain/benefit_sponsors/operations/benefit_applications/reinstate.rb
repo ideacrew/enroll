322daabc977ae6a7b8bbc3d07a14cd23d50584ec
saikumar9 <saikumarkotagiri9@gmail.com>
2020-11-30 10:42:49 -0500

    Refs #90757, BenefitApplications can be reinstated only if the 12 months coverage is not completed.

--- components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_applications/reinstate.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_applications/reinstate.rb
@@ -32,30 +32,43 @@ module BenefitSponsors
 
         def validate(params)
           return Failure('Missing Key.') unless params.key?(:benefit_application)
-          return Failure('Not a valid Benefit Application object.') unless params[:benefit_application].is_a?(BenefitSponsors::BenefitApplications::BenefitApplication)
+          @current_ba = params[:benefit_application]
+          return Failure('Not a valid Benefit Application object.') unless @current_ba.is_a?(BenefitSponsors::BenefitApplications::BenefitApplication)
           valid_states_for_reinstatement = [:terminated, :termination_pending, :canceled]
-          return Failure("Given BenefitApplication is not in any of the #{valid_states_for_reinstatement} states.") unless valid_states_for_reinstatement.include?(params[:benefit_application].aasm_state)
-          return Failure('Overlapping BenefitApplication exists for this Employer.') if overlapping_ba_exists?(params)
+          return Failure("Given BenefitApplication is not in any of the #{valid_states_for_reinstatement} states.") unless valid_states_for_reinstatement.include?(@current_ba.aasm_state)
+          return Failure("Given BenefitApplication's effective starting date is not in the past 12 months.") unless initial_ba_within_valid_timeframe?
+          return Failure('Overlapping BenefitApplication exists for this Employer.') if overlapping_ba_exists?
 
           Success(params)
         end
 
-        def overlapping_ba_exists?(params)
-          @effective_period = effective_period_range(params)
-          current_ba = params[:benefit_application]
-          valid_bas = current_ba.benefit_sponsorship.benefit_applications.non_canceled.where(:id.ne => current_ba.id)
+        def initial_ba_within_valid_timeframe?
+          application = parent_ba_by_reinstate_id(@current_ba)
+          start_of_month = TimeKeeper.date_of_record.beginning_of_month
+          timeframe_months = EnrollRegistry[:reinstate_timeframe].setting(:timeframe_months).item
+          # Example: In the month of November(2020), I should be able to reinstate the BAs that have min date on or after 12/1/2019.
+          ((start_of_month.next_month - timeframe_months.months)..start_of_month.end_of_month).cover?(application.effective_period.min)
+        end
+
+        def parent_ba_by_reinstate_id(benefit_application)
+          reinstated_id = benefit_application.reinstated_id
+          reinstated_id.nil? ? benefit_application : parent_ba_by_reinstate_id(benefit_application.parent_reinstate_application)
+        end
+
+        def overlapping_ba_exists?
+          @effective_period = effective_period_range
+          valid_bas = @current_ba.benefit_sponsorship.benefit_applications.non_canceled.where(:id.ne => @current_ba.id)
           valid_bas.any?{|ba| ba.effective_period.cover?(@effective_period.min) || ba.effective_period.min >= @effective_period.min}
         end
 
-        def effective_period_range(params)
-          current_ba = params[:benefit_application]
-          case current_ba.aasm_state
+        def effective_period_range
+          case @current_ba.aasm_state
           when :terminated
-            (current_ba.terminated_on + 1.day)..current_ba.effective_period.max
+            (@current_ba.terminated_on + 1.day)..@current_ba.effective_period.max
           when :termination_pending
-            (current_ba.terminated_on + 1.day)..current_ba.effective_period.max
+            (@current_ba.terminated_on + 1.day)..@current_ba.effective_period.max
           when :canceled
-            current_ba.effective_period
+            @current_ba.effective_period
           end
         end
 
