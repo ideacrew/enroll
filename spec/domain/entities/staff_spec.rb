7fae9bddca8fe9d95c5626adf3a6e03c227f47dd
nisanth <nisanth.yaganti@gmail.com>
2020-12-01 14:49:51 -0500

    adding in test cases for staff contract and entity

---
+++ spec/domain/entities/staff_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe ::Entities::Staff, dbclean: :after_each do
+  before :all do
+    DatabaseCleaner.clean
+  end
+
+  describe 'initialize instance' do
+    let(:params) do
+      { 
+        first_name: 'jhon',
+        last_name: 'doe' 
+      }
+    end
+
+    context 'with valid params' do
+      it 'should initialize staff instance' do
+        expect(::Entities::Staff.new(params)).to be_a ::Entities::Staff
+      end
+    end
+
+    context 'with valid optional params' do
+      it 'should initialize with optional email & number' do
+        params.merge!({email: nil, area_code: nil, number: nil})
+        expect(::Entities::Staff.new(params)).to be_a ::Entities::Staff
+      end
+    end
+
+    context 'with invalid params' do
+      it 'should raise error without any params' do
+        expect { subject }.to raise_error(Dry::Struct::Error, /:first_name is missing in Hash input/)
+      end
+
+      it 'should raise error with nil dob' do
+        params.merge!({dob: nil})
+        expect { ::Entities::Staff.new(params) }.to raise_error(Dry::Struct::Error, /has invalid type for :dob/)
+      end
+    end
+  end
+end
---
+++ spec/domain/validators/staff_contract_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Validators::StaffContract, type: :model, dbclean: :after_each do
+
+  before :all do
+    DatabaseCleaner.clean
+  end
+
+  let(:params) do
+    {
+      first_name: 'test', last_name: 'test',
+    }
+  end
+
+  context 'success case' do
+
+    it 'should return success' do
+      result = subject.call(params)
+      expect(result.success?).to be_truthy
+    end
+  end
+
+  context 'failure case' do
+
+    context 'without required keys' do
+
+      it 'should return failure' do
+        result = subject.call(params.except(:first_name))
+        expect(result.failure?).to be_truthy
+      end
+    end
+
+    context 'with dob key as nil value' do
+      it 'should return failure' do
+        result = subject.call(params.merge!({dob: nil}))
+        expect(result.failure?).to be_truthy
+      end
+    end
+  end
+end
