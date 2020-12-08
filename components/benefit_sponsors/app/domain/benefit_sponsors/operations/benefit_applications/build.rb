a6b97cc061a1adc266a1effd6bfbb46787e8d28c
saikumar9 <saikumarkotagiri9@gmail.com>
2020-12-08 16:11:48 -0500

    Refs #90852, Code refactor to aviod using _type attribute in Contracts and Entites

--- components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_applications/build.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_applications/build.rb
@@ -28,7 +28,24 @@ module BenefitSponsors
         end
 
         def initialize_entity(values)
-          Success(::BenefitSponsors::Entities::BenefitApplication.new(values.to_h))
+          ba_params = values.to_h
+          ba_params[:benefit_packages].each do |bp_params|
+            sb_entities = bp_params[:sponsored_benefits].inject([]) do |sbs_array, sb_params|
+              sbs_array << init_sponored_benefit(sb_params)
+            end
+            bp_params[:sponsored_benefits] = sb_entities
+          end
+
+          Success(::BenefitSponsors::Entities::BenefitApplication.new(ba_params))
+        end
+
+        def init_sponored_benefit(sb_params)
+          entity_class = if sb_params[:product_kind].present?
+                           "::BenefitSponsors::Entities::#{sb_params[:product_kind].to_s.camelize}SponsoredBenefit".constantize
+                         else
+                           ::BenefitSponsors::Entities::SponsoredBenefit
+                         end
+          entity_class.new(sb_params)
         end
       end
     end
