641807fbb5396880a8b1aabf101dc551b39e80ae
saimekala07 <saimekala07@gmail.com>
2020-12-22 15:18:11 -0500

    refs #90941 Adding hbx_enrollment & benefit group assigment operation to benefit sponsor dummy folder.

---
+++ components/benefit_sponsors/spec/dummy/app/domain/operations/benefit_group_assignments/build.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Operations
+  module BenefitGroupAssignments
+    # This class initializes a benefit_group_assignment entity after
+    # validating the incoming benefit_group_assignment params.
+    class Build
+      include Dry::Monads[:result, :do]
+
+      # @param [ Hash ] benefit_group_assignment attributes
+      # @return [ ::Entities::BenefitGroupAssignments::BenefitGroupAssignment ] benefit_group_assignment
+      def call(params)
+        values = yield validate(params)
+        entity = yield initialize_entity(values)
+
+        Success(entity)
+      end
+
+      private
+
+      def validate(params)
+        contract_result = ::Validators::BenefitGroupAssignments::BenefitGroupAssignmentContract.new.call(params)
+        contract_result.success? ? Success(contract_result.to_h) : Failure(contract_result.errors)
+      end
+
+      def initialize_entity(values)
+        Success(::Entities::BenefitGroupAssignments::BenefitGroupAssignment.new(values.to_h))
+      end
+    end
+  end
+end
