6eb96dd6ecf053673d7de325aac06ca3dbe1307d
zasman <zanesandin@gmail.com>
2020-12-22 14:52:01 -0500

    REFS Brakeman - Rubocop fixes.

--- app/controllers/insured/group_selection_controller.rb
+++ app/controllers/insured/group_selection_controller.rb
@@ -106,7 +106,7 @@ class Insured::GroupSelectionController < ApplicationController
     end
 
 
-    if (@adapter.keep_existing_plan?(permitted_group_selection_params) && @adapter.previous_hbx_enrollment.present?)
+    if @adapter.keep_existing_plan?(permitted_group_selection_params) && @adapter.previous_hbx_enrollment.present?
       sep = @hbx_enrollment.earlier_effective_sep_by_market_kind
 
       if sep.present?
