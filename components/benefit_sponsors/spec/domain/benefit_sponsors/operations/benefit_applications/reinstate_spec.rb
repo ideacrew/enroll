7518cc010b7421606b423a9a7685444f8880a001
saimekala07 <saimekala07@gmail.com>
2020-12-22 15:21:41 -0500

    refs #90941 Adding specs to operation.

--- components/benefit_sponsors/spec/domain/benefit_sponsors/operations/benefit_applications/reinstate_spec.rb
+++ components/benefit_sponsors/spec/domain/benefit_sponsors/operations/benefit_applications/reinstate_spec.rb
@@ -34,19 +34,45 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
   let(:current_effective_date) {TimeKeeper.date_of_record.beginning_of_year}
 
   include_context 'setup initial benefit application'
+  let(:person) { FactoryBot.create(:person, :with_employee_role, :with_family) }
+  let(:family) { person.primary_family }
+  let!(:census_employee) do
+    ce = FactoryBot.create(:census_employee, :with_active_assignment, benefit_sponsorship: benefit_sponsorship, employer_profile: benefit_sponsorship.profile, benefit_group: current_benefit_package)
+    ce.update_attributes!(employee_role_id: person.employee_roles.first.id)
+    person.employee_roles.first.update_attributes(census_employee_id: ce.id)
+    ce
+  end
+  let!(:enrollment) do
+    FactoryBot.create(:hbx_enrollment, :with_enrollment_members,
+                      household: family.latest_household,
+                      coverage_kind: 'health',
+                      family: family,
+                      aasm_state: 'coverage_selected',
+                      effective_on: current_effective_date,
+                      kind: 'employer_sponsored',
+                      benefit_sponsorship_id: benefit_sponsorship.id,
+                      sponsored_benefit_package_id: current_benefit_package.id,
+                      sponsored_benefit_id: current_benefit_package.sponsored_benefits[0].id,
+                      employee_role_id: census_employee.employee_role.id,
+                      product: current_benefit_package.sponsored_benefits[0].reference_product,
+                      rating_area_id: BSON::ObjectId.new,
+                      benefit_group_assignment_id: census_employee.active_benefit_group_assignment.id)
+  end
+
 
   context 'with valid benefit_application' do
     let(:current_year) {current_effective_date.year}
     let(:end_of_the_year) {Date.new(current_year, 12, 31)}
 
     before do
+      load_cache
       setup_contribution_models(benefit_sponsor_catalog)
       allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(current_year, 10, 1))
       initial_application.benefit_packages.each do |bp|
         bp.sponsored_benefits.each do |spon_benefit|
           spon_benefit.update_attributes!(_type: 'BenefitSponsors::SponsoredBenefits::HealthSponsoredBenefit')
           create_pd(spon_benefit)
-          update_contribution_levels(spon_benefit)
+          update_contribution_levels(spon_benefit) if initial_application.employer_profile.is_a_fehb_profile?
         end
       end
     end
@@ -54,12 +80,13 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
     context 'success' do
       context 'reinstate terminated benefit application' do
         before do
-          initial_application.terminate_enrollment!
           period = initial_application.effective_period.min..(initial_application.effective_period.min.next_month.next_month.next_month.prev_day)
-          initial_application.update_attributes!(termination_reason: 'Testing', terminated_on: (TimeKeeper.date_of_record - 1.month).end_of_month, effective_period: period)
-          @new_ba = subject.call({benefit_application: initial_application}).success
+          initial_application.update_attributes!(termination_reason: 'nonpayment', terminated_on: (TimeKeeper.date_of_record - 1.month).end_of_month, effective_period: period)
+          initial_application.terminate_enrollment!
+          @new_ba = subject.call({params: {benefit_application: initial_application}}).success
           @first_wfst = @new_ba.workflow_state_transitions.first
           @second_wfst = @new_ba.workflow_state_transitions.second
+          census_employee.reload
         end
 
         it 'should return a success with a BenefitApplication' do
@@ -78,6 +105,17 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
           expect(@new_ba.reinstated_id).to eq(initial_application.id)
         end
 
+        it 'should reinstate benefit group assignment' do
+          reinstated_bga = census_employee.benefit_group_assignments.where(:benefit_package_id.in => @new_ba.benefit_packages.map(&:id)).first
+          expect(reinstated_bga.start_on).to eq(@new_ba.start_on)
+        end
+
+        it 'should reinstate enrollment' do
+          reinstated_enrollment = HbxEnrollment.where(:sponsored_benefit_package_id.in => @new_ba.benefit_packages.map(&:id)).first
+          expect(reinstated_enrollment.effective_on).to eq(@new_ba.start_on)
+          expect(reinstated_enrollment.predecessor_enrollment_id).to eq(enrollment.id)
+        end
+
         it 'should create new benefit_sponsor_catalog' do
           expect(@new_ba.benefit_sponsor_catalog_id).not_to eq(initial_application.benefit_sponsor_catalog_id)
           expect(@new_ba.benefit_sponsor_catalog.benefit_application).to eq(@new_ba)
@@ -87,6 +125,12 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
         it 'should create a renewal draft for the reinstated benefit_application' do
           expect(@new_ba.benefit_sponsorship.benefit_applications.count).to eq(3)
           expect(@new_ba.benefit_sponsorship.benefit_applications.last.aasm_state).to eq(:draft)
+        end
+
+        it 'should renew benefit group assignment for renewing draft application' do
+          renewing_app = benefit_sponsorship.renewal_benefit_application
+          renewing_bga = census_employee.benefit_group_assignments.where(:benefit_package_id.in => renewing_app.benefit_packages.map(&:id)).first
+          expect(renewing_bga.start_on).to eq renewing_app.start_on
         end
 
         context 'workflow_state_transitions' do
@@ -104,10 +148,51 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
         end
       end
 
-      context 'reinstate canceled benefit application' do
+      context 'reinstate retro active canceled benefit application' do
         before do
           initial_application.cancel!
-          @new_ba = subject.call({benefit_application: initial_application}).success
+          @new_ba = subject.call({params: {benefit_application: initial_application}}).success
+          @first_wfst = @new_ba.workflow_state_transitions.first
+          @second_wfst = @new_ba.workflow_state_transitions.second
+        end
+
+        it 'should return a BenefitApplication with aasm_state active' do
+          expect(@new_ba.aasm_state).to eq(:active)
+        end
+
+        it 'should return a BenefitApplication with matching effective_period' do
+          expect(@new_ba.effective_period).to eq(initial_application.effective_period)
+        end
+
+        it 'should populate reinstated_id' do
+          expect(@new_ba.reinstated_id).to eq(initial_application.id)
+        end
+
+        it 'should create new benefit_sponsor_catalog' do
+          expect(@new_ba.benefit_sponsor_catalog_id).not_to eq(initial_application.benefit_sponsor_catalog_id)
+        end
+
+        context 'workflow_state_transitions' do
+          it 'should record transition to_state' do
+            expect(@first_wfst.to_state).to eq('reinstated')
+          end
+
+          it 'should record transition from_state' do
+            expect(@second_wfst.from_state).to eq('reinstated')
+          end
+
+          it 'should record transition to_state' do
+            expect(@second_wfst.to_state).to eq('active')
+          end
+        end
+      end
+
+      context 'reinstate active to canceled state benefit application' do
+        before do
+          initial_application.aasm_state = :canceled
+          initial_application.workflow_state_transitions.new(from_state: :active, to_state: :canceled)
+          initial_application.save
+          @new_ba = subject.call({params: {benefit_application: initial_application}}).success
           @first_wfst = @new_ba.workflow_state_transitions.first
           @second_wfst = @new_ba.workflow_state_transitions.second
         end
@@ -148,7 +233,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
           initial_application.schedule_enrollment_termination!
           period = initial_application.effective_period.min..(initial_application.effective_period.min.next_month.next_month.next_month.prev_day)
           initial_application.update_attributes!(termination_reason: 'Testing, future termination', terminated_on: (TimeKeeper.date_of_record - 1.month).end_of_month, effective_period: period)
-          @new_ba = subject.call({benefit_application: initial_application}).success
+          @new_ba = subject.call({params: {benefit_application: initial_application}}).success
         end
 
         it 'should return a success with a BenefitApplication' do
@@ -193,7 +278,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
           initial_application.terminate_enrollment!
           period = initial_application.effective_period.min..(initial_application.effective_period.min.next_month.next_month.next_month.prev_day)
           initial_application.update_attributes!(termination_reason: 'Testing', terminated_on: (TimeKeeper.date_of_record - 1.month).end_of_month, effective_period: period)
-          @result = subject.call({benefit_application: initial_application})
+          @result = subject.call({params: {benefit_application: initial_application}})
         end
 
         it 'should return failure with a message' do
@@ -212,7 +297,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
           initial_application.terminate_enrollment!
           period = initial_application.effective_period.min..(initial_application.effective_period.min.next_month.next_month.next_month.prev_day)
           initial_application.update_attributes!(termination_reason: 'Testing', terminated_on: (TimeKeeper.date_of_record - 1.month).end_of_month, effective_period: period)
-          @result = subject.call({benefit_application: initial_application})
+          @result = subject.call({params: {benefit_application: initial_application}})
         end
 
         it 'should return failure with a message' do
@@ -226,7 +311,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
         min = TimeKeeper.date_of_record.prev_year.beginning_of_month
         initial_application.update_attributes!(aasm_state: :terminated, effective_period: min..(min.next_year.prev_day))
         initial_application.benefit_sponsor_catalog.update_attributes!(effective_period: min..(min.next_year.prev_day))
-        @result = subject.call({benefit_application: initial_application})
+        @result = subject.call({params: {benefit_application: initial_application}})
       end
 
       it 'should return a failure with message' do
@@ -238,7 +323,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
   context 'failure' do
     context 'no params' do
       before do
-        @result = subject.call({})
+        @result = subject.call({params: {}})
       end
 
       it 'should return a failure with a message' do
@@ -248,7 +333,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
 
     context 'invalid params' do
       before do
-        @result = subject.call({benefit_application: 'benefit_application'})
+        @result = subject.call({params: {benefit_application: 'benefit_application'}})
       end
 
       it 'should return a failure with a message' do
@@ -258,7 +343,7 @@ RSpec.describe BenefitSponsors::Operations::BenefitApplications::Reinstate, dbcl
 
     context "invalid benefit_application's aasm state" do
       before do
-        @result = subject.call({benefit_application: initial_application})
+        @result = subject.call({params: {benefit_application: initial_application}})
       end
 
       it 'should return a failure with a message' do
@@ -294,4 +379,9 @@ def setup_contribution_models(benefit_sponsor_catalog)
       pp.save!
     end
   end
+end
+
+def load_cache
+  ::BenefitMarkets::Products::ProductRateCache.initialize_rate_cache!
+  ::BenefitMarkets::Products::ProductFactorCache.initialize_factor_cache!
 end
