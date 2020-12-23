558099b74c06fd3c9445c1321337702e7b3eb838
zasman <zanesandin@gmail.com>
2020-12-23 14:24:09 -0500

    REFS Brakeman - Fixes some stuff in employee roles.

--- app/controllers/insured/employee_roles_controller.rb
+++ app/controllers/insured/employee_roles_controller.rb
@@ -22,13 +22,9 @@ class Insured::EmployeeRolesController < ApplicationController
 
   def match
     @no_save_button = true
-    @person_params = params.require(:person).permit(person_parameters_list).merge(
-      {
-        :user_id => current_user.id,
-        :no_ssn => params.dig(:person, :no_ssn),
-        :dob => params.dig(:jq_datepicker_ignore_person, :dob).to_date
-      }
-    )
+    @person_params = params.require(:person).permit(person_parameters_list).merge({:user_id => current_user.id})
+    @person_params.merge(no_ssn: params.dig(:person, :no_ssn)) if params.dig(:person, :no_ssn)
+    @person_params.merge(:dob => params.dig(:jq_datepicker_ignore_person, :dob).to_date) if params.dig(:jq_datepicker_ignore_person, :dob)
     @employee_candidate = ::Forms::EmployeeCandidate.new(@person_params)
     @person = @employee_candidate
     if @employee_candidate.valid?
