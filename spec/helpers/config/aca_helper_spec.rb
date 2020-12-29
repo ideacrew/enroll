9805ae9dea567bcd52f9344915a7ca6d264e6ace
Sai Praveen <saipraveen.gudimetla@gmail.com>
2020-12-29 15:32:06 -0500

    fix rubocop

--- spec/helpers/config/aca_helper_spec.rb
+++ spec/helpers/config/aca_helper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe Config::AcaHelper, :type => :helper, dbclean: :after_each do
@@ -5,7 +7,7 @@ RSpec.describe Config::AcaHelper, :type => :helper, dbclean: :after_each do
   describe "Aca settings" do
 
     context '.family_contribution_percent_minimum_for_application_start_on' do
-      
+
       it 'should return family contribution percent for inital employers in flex period' do
         expect(helper.family_contribution_percent_minimum_for_application_start_on(Date.new(2020,5,1), false)).to eq 0
       end
@@ -22,9 +24,9 @@ RSpec.describe Config::AcaHelper, :type => :helper, dbclean: :after_each do
         expect(helper.family_contribution_percent_minimum_for_application_start_on(Date.new(2020, 5, 1), true)).to eq 0
       end
     end
-    
+
     context '.employer_contribution_percent_minimum_for_application_start_on' do
-      
+
       it 'should return employer contribution percent for inital employers in flex period' do
         expect(helper.employer_contribution_percent_minimum_for_application_start_on(Date.new(2020,5,1), false)).to eq 0
       end
