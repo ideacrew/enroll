2f0e8cc418f130a03320d083a29449d7900721d3
saikumar9 <saikumarkotagiri9@gmail.com>
2021-01-06 14:22:05 -0500

    Refs #76580, fix the rspec failures for a couple of services.

--- spec/models/services/available_eligibility_service_spec.rb
+++ spec/models/services/available_eligibility_service_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe Services::AvailableEligibilityService, type: :model, :dbclean => 
   context 'initialize' do
     it { expect(described_class.respond_to?(:new)).to eq true }
 
-    it { expect(described_class.new('enrollment_id')).to be_a Services::AvailableEligibilityService }
+    it { expect(described_class.new('enrollment_id', TimeKeeper.date_of_record)).to be_a Services::AvailableEligibilityService }
 
     it { expect{described_class.new}.to raise_error(ArgumentError) }
   end
 
   context 'available_eligibility' do
-    it { expect(described_class.new('enrollment_id').respond_to?(:available_eligibility)).to eq true }
+    it { expect(described_class.new('enrollment_id', TimeKeeper.date_of_record).respond_to?(:available_eligibility)).to eq true }
 
     it 'should raise error for bad enrollment id' do
-      service_instance = described_class.new('enrollment_id')
+      service_instance = described_class.new('enrollment_id', TimeKeeper.date_of_record)
       error_message = 'Cannot find a valid enrollment with given enrollment id'
       expect{service_instance.available_eligibility}.to raise_error(RuntimeError, error_message)
     end
