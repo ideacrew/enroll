67983406f32dd93c38d09689ce46e51f3e12322f
saimekala07 <saimekala07@gmail.com>
2021-01-11 18:07:05 -0500

    refs # rebase

--- components/benefit_sponsors/app/domain/benefit_sponsors/validators/sponsored_benefits/health_sponsored_benefit_contract.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/validators/sponsored_benefits/health_sponsored_benefit_contract.rb
@@ -6,7 +6,6 @@ module BenefitSponsors
       # This class checks and validates the incoming params
       # that are required to build a new health sponsored benefit object
       # if any of the checks or rules fail it returns a failure
-      
       class HealthSponsoredBenefitContract < SponsoredBenefitContract
 
         params do
