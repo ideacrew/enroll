a9f0ae1f9b898a6f9eb15b9ccedb73404c8ed253
zasman <zanesandin@gmail.com>
2021-01-12 14:07:42 -0500

    REFS 90342 - Fixes duplicate workflow transitions.

--- app/models/consumer_role.rb
+++ app/models/consumer_role.rb
@@ -1036,6 +1036,7 @@ class ConsumerRole
   end
 
   def pass_lawful_presence(*args)
+    return if lawful_presence_authorized?
     lawful_presence_determination.authorize!(*args)
     verification_types.reject{|type| VerificationType::NON_CITIZEN_IMMIGRATION_TYPES.include? type.type_name }.each{ |type| type.pass_type }
   end
