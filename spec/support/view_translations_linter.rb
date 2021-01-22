dd1d20d12ac3e5db70963e187dda8676e453c61f
zasman <zanesandin@gmail.com>
2021-01-22 12:34:33 -0500

    REFS Maintenance - Finalizes custom class for linting views.

---
+++ spec/support/view_translations_linter.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# The following class is can be used in RSpecs, Rake Tasks, etc. to lint different view files for untranslated strings.
+# Currently supports linting for untranslated strings WITHIN ERB tags and OUTSIDE of ERB tags
+# Class is initialized with three params:
+# Read View Files - an array of files which have been "read" and stringified such as:
+# [File.read("view_filename"), "File.read("view_filename_2")]
+# Suggested query is to use a list of changed files from Git as an arguement such as:
+# `git diff --name-only origin/master HEAD | grep .html.erb`.strip.split("\n")
+# Whitelisted Translation Strings - an array of whitelisted strings to ignore, best kept in YML
+# Filter Type - Denoting which query you'd like to lint the file for. Currently supported are
+# 'in_erb' and 'outside_erb'
+
+class ViewTranslationsLinter
+  attr_accessor :filter_type, :read_view_files, :whitelisted_translation_strings, :non_whitelisted_substrings
+
+  def initialize(read_view_files = nil, whitelisted_translation_strings = nil, filter_type = nil)
+    @read_view_files = read_view_files
+    @whitelisted_translation_strings = whitelisted_translation_strings
+    @filter_type = filter_type
+    @non_whitelisted_substrings = []
+  end
+
+  def all_translations_present?
+    return true if read_view_files.blank?
+    read_view_files.each do |read_file|
+      return false unless no_potential_untranslated_strings?(read_file)
+    end
+    true
+  end
+
+  def no_potential_untranslated_strings?(read_file)
+
+    potential_substrings(read_file).each do |substring|
+      if whitelisted_translation_strings.detect { |whitelisted_substring| substring.downcase.include?(whitelisted_substring.downcase) }.blank?
+        non_whitelisted_substrings << substring
+      end
+    end
+    return true if non_whitelisted_substrings.blank?
+    raise(untranslated_warning_message(non_whitelisted_substrings))
+  end
+
+  def potential_substrings(read_file)
+    case filter_type
+    # when 'in_slim'
+    # when 'outside_slim'
+    when 'in_erb'
+      # The following method will return all code between ERB tags.
+      # Since this will return *all* code between ERB tags,
+      # Suggested strings to whitelist:
+      # HTML Elements and helper methods not containing text, such as "render"
+      # Methods for dates or currency amounts, such as "hired_on", or "current_year"
+      # Record Identifiers such as, "model_name_id"
+      potential_substrings = read_file.scan(/<%=(.*)%>/).flatten
+    when 'outside_erb'
+      # The following filter will take the full read HTML.erb file and:
+      # Return a string without all HTML/ERB Tags
+      # Turn the string into an array, separated at each new line
+      # Remove any blank strings from the array
+      # Remove any white space from left and right of array elements
+      # Remove white space to the left and right of sentences
+      # Cut the array down to only the uniq elements
+      # Remove any array elements which are all special characters (I.E. "!", ":")
+      # removes leading and ending extra whitespace, and blank strings, returning an array like:
+      # ["list all attributes", "success"]
+      #  Further reading: https://stackoverflow.com/a/54405741/5331859
+      potential_substrings = ActionView::Base.full_sanitizer.sanitize(read_file).split("\n").reject!(&:blank?).map(&:strip).uniq.reject! do |substring|
+        substring.split("").all? { |char| ('a'..'z').to_a.exclude?(char.downcase) }
+      end
+    end
+    potential_substrings
+  end
+
+  def untranslated_warning_message(non_whitelisted_substrings)
+    "The following are potentially untranslated substrings. #{non_whitelisted_substrings.join(', ')}. "\
+    "Please modify your ERB and place them in translation helper tags "\
+    "with a coorelating translation or whitelist them."
+  end
+end
--- spec/views/translations/branch_files_translations_check_spec.rb
+++ spec/views/translations/branch_files_translations_check_spec.rb
@@ -1,38 +1,45 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require Rails.root.to_s + "/spec/support/erb_translations_linter.rb"
+require Rails.root.to_s + "/spec/support/view_translations_linter.rb"
 
 # This will check the git diff for all .html.erb files and match all non HTML/ERB tag substrings
 # against all the translations in the database to assure translations are added to the file.
 
 RSpec.describe "Branch Files Translations Spec" do
-  # Returns an array of all available translations with special characters removed, downcased
+  # Returns any changed ERB files froom current branch
   let(:filename_list) { `git diff --name-only origin/master HEAD | grep .html.erb`.strip.split("\n") }
-  # Will read all ERB files and and return an array of all of them as strings
-  let(:read_files) { filename_list.map { |view_filename| File.read("#{Rails.root}/#{view_filename}") } }
+  # Will read all ERB filesfrom branch  and and return an array of all of them as strings
+  let(:read_view_files) { filename_list.map { |view_filename| File.read("#{Rails.root}/#{view_filename}") } }
   # YML file containing whitelisted keys.
   # Note: Do NOT whitelist strings without lead dev approval
   let(:whitelisted_strings_hash) { YAML.load_file(Rails.root.to_s + "/spec/support/fixtures/whitelisted_translation_strings.yml").with_indifferent_access }
-  let(:whitelisted_translation_strings_from_erb_tags) do
+  let(:whitelisted_translation_strings_in_erb_tags) do
     all_whitelisted_strings = []
-    keys = whitelisted_strings_hash[:whitelisted_translation_strings_from_erb_tags].keys
+    keys = whitelisted_strings_hash[:whitelisted_translation_strings_in_erb_tags].keys
     keys.each do |key|
-      all_whitelisted_strings << whitelisted_strings_hash[:whitelisted_translation_strings_from_erb_tags][key]
+      all_whitelisted_strings << whitelisted_strings_hash[:whitelisted_translation_strings_in_erb_tags][key]
     end
     all_whitelisted_strings.flatten
   end
-  let(:whitelisted_non_erb_tag_strings) do
-    whitelisted_strings_hash[:whitelisted_non_erb_tag_strings][:string_list] + whitelisted_strings_hash[:whitelisted_non_erb_tag_strings][:non_text_method_calls]
+
+  let(:whitelisted_translation_strings_outside_erb_tags) do
+    all_whitelisted_strings = []
+    keys = whitelisted_strings_hash[:whitelisted_translation_strings_outside_erb_tags].keys
+    keys.each do |key|
+      all_whitelisted_strings << whitelisted_strings_hash[:whitelisted_translation_strings_outside_erb_tags][key]
+    end
+    all_whitelisted_strings.flatten
   end
-  let(:translations_linter) { ErbTranslationsLinter.new(read_files, whitelisted_translation_strings_from_erb_tags, whitelisted_non_erb_tag_strings) }
+
+  let(:translations_linter_in_erb) { ViewTranslationsLinter.new(read_view_files, whitelisted_translation_strings_in_erb_tags, 'in_erb') }
+  let(:translations_linter_outside_erb) { ViewTranslationsLinter.new(read_view_files, whitelisted_translation_strings_outside_erb_tags, 'outside_erb') }
 
   it "should not contain any ERB tags containing untranslated strings" do
-    expect(translations_linter.all_translations_in_erb_tags?).to eq(true)
+    expect(translations_linter_in_erb.all_translations_present?).to eq(true)
   end
 
   it "should not contain any untranslated substrings outside of HTML tags" do
-    expect(translations_linter.all_translations_outside_erb_tags?).to eq(true)
+    expect(translations_linter_outside_erb.all_translations_present?).to eq(true)
   end
 end
-
