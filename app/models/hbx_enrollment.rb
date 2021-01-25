edaf36e8983be7ca57cf3523af5a0012d9e55c03
saimekala07 <saimekala07@gmail.com>
2021-01-25 14:13:43 -0500

    refs #92179 IVL:cancel renewal coverage if exists on canceling expired coverage

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -1834,7 +1834,7 @@ class HbxEnrollment
                          :transmitted_to_carrier, :coverage_renewed, :unverified,
                          :coverage_enrolled, :renewing_waived, :inactive, :coverage_reinstated],
                   to: :coverage_canceled, after: :propogate_cancel
-      transitions from: :coverage_expired, to: :coverage_canceled, :guard => :is_ivl_by_kind?
+      transitions from: :coverage_expired, to: :coverage_canceled, :guard => :is_ivl_by_kind?, after: :propogate_cancel
     end
 
     event :cancel_for_non_payment, :after => :record_transition do
@@ -1843,7 +1843,7 @@ class HbxEnrollment
                          :transmitted_to_carrier, :coverage_renewed, :unverified,
                          :coverage_enrolled, :renewing_waived, :inactive],
                   to: :coverage_canceled, after: :propogate_cancel
-      transitions from: :coverage_expired, to: :coverage_canceled, :guard => :is_ivl_by_kind?
+      transitions from: :coverage_expired, to: :coverage_canceled, :guard => :is_ivl_by_kind?, after: :propogate_cancel
     end
 
     event :terminate_coverage, :after => :record_transition do
