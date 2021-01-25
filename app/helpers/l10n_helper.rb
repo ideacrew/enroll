6d0a6745ff84fee0b4849e34ed7779b0ee6d405b
zasman <zanesandin@gmail.com>
2021-01-25 11:34:35 -0500

    REFS Maintenance - Changes I18n to t.

--- app/helpers/l10n_helper.rb
+++ app/helpers/l10n_helper.rb
@@ -1,5 +1,5 @@
 module L10nHelper
   def l10n(translation_key, interpolated_keys={})
-    I18n.t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, ''))).html_safe
+    t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
   end
 end
--- components/benefit_sponsors/app/helpers/benefit_sponsors/l10n_helper.rb
+++ components/benefit_sponsors/app/helpers/benefit_sponsors/l10n_helper.rb
@@ -1,7 +1,7 @@
 module BenefitSponsors
   module L10nHelper
     def l10n(translation_key, interpolated_keys={})
-      I18n.t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, ''))).html_safe
+      t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
     end
   end
 end
