8b00efef5f0b200fae0fa4d1cfe404e932163955
Jayanth <jayanth.rordev@gmail.com>
2021-01-29 18:09:30 -0500

    refs#paynow_176573485 Refactored Payment transactions controller

--- app/controllers/payment_transactions_controller.rb
+++ app/controllers/payment_transactions_controller.rb
@@ -6,12 +6,12 @@ class PaymentTransactionsController < ApplicationController
   def generate_saml_response
     connect_test = HTTParty.post(SamlInformation.kp_pay_now_url) #checks to see if EA can connect to carrier payment portal.
     status = connect_test.code if Rails.env.production?
-    hbx_enrollment = ::HbxEnrollment.by_hbx_id(params[:enrollment_id]).first
-    payment = PaymentTransaction.build_payment_instance(hbx_enrollment)
-    saml_generator = OneLogin::RubySaml::SamlGenerator.new(payment.payment_transaction_id, hbx_enrollment)
-    response_doc = saml_generator.build_saml_response
-    @saml_response = saml_generator.encode_saml_response(response_doc)
-    render json: {"SAMLResponse": @saml_response, status: status, error: nil}
+    result = Operations::GenerateSamlResponse.new.call({enrollment_id: params[:enrollment_id]})
+    if result.success?
+      render json: {"SAMLResponse": result, status: status, error: nil}
+    else
+      render json: {error: result.failure}
+    end
   rescue StandardError => e
     render json: {error: e}
   end
