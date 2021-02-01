71cd44f72ed4c69fec45c9b29a6506b4d24c3fc8
zasman <zanesandin@gmail.com>
2021-02-01 16:23:30 -0500

    REFS Maintenance - Adds test file.
    REFS Maintenance - Quick Fix for linter.
    
    REFS Maintenance - Fixes up some stuff.

--- spec/support/view_translations_linter.rb
+++ spec/support/view_translations_linter.rb
@@ -30,15 +30,18 @@ class ViewTranslationsLinter
 
   def unapproved_strings_in_view(stringified_view)
     non_approved_substrings = []
+    return unless potential_substrings(stringified_view).present?
     potential_substrings(stringified_view).each do |substring|
       # Use match
-      non_approved_substring = approved_translation_strings.detect { |approved_substring| approved_substring.match(substring.downcase) }
-      non_approved_substrings << substring if non_approved_substring.blank?
+      non_approved_substring_match = approved_translation_strings.detect { |approved_string| substring.match(approved_string) }
+      non_approved_substrings << substring if non_approved_substring_match.blank?
     end
     non_approved_substrings
   end
 
   # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Lint/SafeNavigationChain
+  # rubocop:disable Metrics/PerceivedComplexity
   def potential_substrings(stringified_view)
     potential_substrings = []
     case filter_type
@@ -53,10 +56,8 @@ class ViewTranslationsLinter
       # Methods for dates or currency amounts, such as "hired_on", or "current_year"
       # Record Identifiers such as, "model_name_id"
       potential_substrings_between_erb_tags = stringified_view.scan(/<%=(.*)%>/).flatten
-      # REmove special characters
-      potential_substrings_no_special_characters = potential_substrings_between_erb_tags.map { |substring| substring.gsub!(/[^0-9a-z ]/i, '') }
       # Remove leading and ending whitespace and downcase
-      potential_substrings = potential_substrings_no_special_characters.map(&:strip).map(&:downcase)
+      potential_substrings = potential_substrings_between_erb_tags&.map(&:strip)&.map(&:downcase)
     when 'outside_erb'
       # The following filter will take the full read HTML.erb file and:
       # Return a string without all HTML/ERB Tags
@@ -72,16 +73,18 @@ class ViewTranslationsLinter
       # ["list all attributes", "success"]
       #  Further reading: https://stackoverflow.com/a/54405741/5331859
       potential_substrings_no_html_tags = ActionView::Base.full_sanitizer.sanitize(stringified_view).split("\n+")
-      potential_substrings_words_only = potential_substrings_no_html_tags.reject!(&:blank?).map(&:strip).uniq.select { |element| element.length > 1 }.map do |string|
+      potential_substrings_words_only = potential_substrings_no_html_tags&.reject!(&:blank?).map(&:strip)&.uniq.select { |element| element.length > 1 }.map do |string|
         # Remove special characters from strings. This will also act to remove any single special character strings hanging around like "-"
         string.gsub!(/[^0-9a-z ]/i, '')
       end
       # Remove any blank strings (after the gsub removed special characters) and Downcase to simplify adding to the allow list
-      potential_substrings = potential_substrings_words_only.reject!(&:blank?).map(&:downcase)
+      potential_substrings = potential_substrings_words_only&.reject!(&:blank?)&.map(&:downcase)
     end
     potential_substrings
   end
   # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Lint/SafeNavigationChain
+  # rubocop:enable Metrics/PerceivedComplexity
 
   def untranslated_warning_message(views_with_errors)
     views_with_errors.each do |filename, unapproved_substrings|
