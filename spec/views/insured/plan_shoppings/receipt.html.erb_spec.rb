67c8f4a10c559a2f2d812d6cd9b3628898171ebe
utkarsh7989 <utkarsh7989@gmail.com>
2021-02-11 17:02:41 -0500

    removes irrelevant kaiser pay message

--- spec/views/insured/plan_shoppings/receipt.html.erb_spec.rb
+++ spec/views/insured/plan_shoppings/receipt.html.erb_spec.rb
@@ -129,7 +129,6 @@ RSpec.describe "insured/plan_shoppings/receipt.html.erb" do
     it "doesn't have Pay Now messaging" do
       render file: "insured/plan_shoppings/receipt.en.html.erb"
       expect(rendered).to_not have_content(/Select PAY NOW to make your first premium payment online/)
-      expect(rendered).to_not have_content(/You only have the option to PAY NOW while you’re on this page/)
       expect(rendered).to_not have_content(/Select PAY NOW to make your first premium payment directly to Kaiser Permanente/)
     end
   end
@@ -141,16 +140,10 @@ RSpec.describe "insured/plan_shoppings/receipt.html.erb" do
       sign_in(user)
       allow(view).to receive(:show_pay_now?).and_return true
     end
+
     it "should have a Pay now button" do
       render file: "insured/plan_shoppings/receipt.en.html.erb"
       expect(rendered).to have_selector('button', text: /Pay Now/)
-    end
-
-    it "have Pay Now messaging" do
-      render file: "insured/plan_shoppings/receipt.en.html.erb"
-      expect(rendered).to have_selector('strong', text: /You only have the option to PAY NOW while you’re on this page./)
-      expect(rendered).to have_text(/You only have the option to PAY NOW while you’re on this page/)
-      expect(rendered).to have_text(/If you leave this page without selecting PAY NOW,/)
     end
   end
 end
