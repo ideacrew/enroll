004a3f0714b415a499cc9da5172f2225b30856b3
Jayanth <jayanth.rordev@gmail.com>
2021-02-17 15:50:08 -0500

    refs#paynow_bug_fixes Added new changes to view file & helper methods

--- app/helpers/insured/plan_shopping/pay_now_helper.rb
+++ app/helpers/insured/plan_shopping/pay_now_helper.rb
@@ -7,14 +7,14 @@ module Insured
       def show_pay_now?(source, hbx_enrollment)
         return false unless EnrollRegistry[:pay_now_functionality].feature.is_enabled
         if source == "Plan Shopping"
-          pay_now_validation?(hbx_enrollment) && !pay_now_button_timed_out?(hbx_enrollment) ? true : false
+          can_pay_now?(hbx_enrollment) && !pay_now_button_timed_out?(hbx_enrollment) ? true : false
         else
-          pay_now_validation?(hbx_enrollment) && past_effective_on?(hbx_enrollment) ? true : false
+          can_pay_now?(hbx_enrollment) && past_effective_on?(hbx_enrollment) ? true : false
         end
       end
 
-      def pay_now_validation?(hbx_enrollment)
-        return true if carrier_with_payment_option?(hbx_enrollment) && individual?(hbx_enrollment) && (has_break_in_coverage_enrollments?(hbx_enrollment) || !has_any_previous_kaiser_enrollments?(hbx_enrollment))
+      def can_pay_now?(hbx_enrollment)
+        return true if carrier_with_payment_option?(hbx_enrollment) && individual?(hbx_enrollment) && (has_break_in_coverage_enrollments?(hbx_enrollment) || !has_any_previous_enrollments?(hbx_enrollment))
       end
 
       def carrier_with_payment_option?(hbx_enrollment)
@@ -25,7 +25,7 @@ module Insured
         hbx_enrollment.kind.in?(['individual', 'coverall'])
       end
 
-      def has_any_previous_kaiser_enrollments?(hbx_enrollment)
+      def has_any_previous_enrollments?(hbx_enrollment)
         carrier = EnrollRegistry[:pay_now_functionality].setting(:carriers).item
         all_kaiser_enrollments = hbx_enrollment.family.hbx_enrollments.where(:aasm_state.nin => ["inactive", "shopping", "coverage_canceled"]).select do |enr|
           next if enr.product.blank? || enr.subscriber.blank? || enr.is_shop?
