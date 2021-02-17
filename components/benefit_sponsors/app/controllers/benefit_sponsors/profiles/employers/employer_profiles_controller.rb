edbd4d239366cb439db200acc283e1e0620998c0
nisanth <nisanth.yaganti@gmail.com>
2021-02-17 11:37:48 -0500

    adding employer poc to tabs.

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller.rb
@@ -84,6 +84,7 @@ module BenefitSponsors
           authorize User, :add_roles?
           @person_id = params[:person_id]
           @agency = BenefitSponsors::Operations::Employers::New.new.call({person_id: params[:person_id], profile_type: params[:profile_type], regitration_params: registration_params}).value!
+          @staff_member = BenefitSponsors::Operations::Employers::Forms::NewEmployerStaff.new.call({id: params[:person_id]}).value!
           respond_to do |format|
             format.html
           end
