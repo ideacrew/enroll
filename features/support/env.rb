d1387c26d71abd24800e697021868adedbf3562e
zasman <zanesandin@gmail.com>
2021-02-17 15:20:40 -0500

    REFS Maintenance - Changes truncation to deletion due to upgrade.

--- features/support/env.rb
+++ features/support/env.rb
@@ -44,7 +44,7 @@ ActionController::Base.allow_rescue = false
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
   load Rails.root + "db/seedfiles/english_translations_seed.rb"
-  DatabaseCleaner.strategy = :truncation, {:except => %w[translations]}
+  DatabaseCleaner.strategy = :deletion, {:except => %w[translations]}
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
