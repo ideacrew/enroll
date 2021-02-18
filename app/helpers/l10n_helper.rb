285c81259c70391879b739cf27a981495f899fde
zasman <zanesandin@gmail.com>
2021-02-18 10:04:18 -0500

    REFS Maintenance - Fixes exception for missing translations that arent strings.

--- app/helpers/l10n_helper.rb
+++ app/helpers/l10n_helper.rb
@@ -1,5 +1,7 @@
 module L10nHelper
   def l10n(translation_key, interpolated_keys={})
+    Rails.logger.error {"#L10nHelper passed non string key: #{translation_key.inspect}"} unless translation_key.is_a?(String)
+    return "Translation Missing" unless translation_key.is_a?(String)
     t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
   end
 end
---
+++ spec/helpers/l10n_helper_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe L10nHelper, :type => :helper do
+  # All translations are configured to load before every rspec
+  it "should translate existing translations" do
+    expect(helper.l10n('date')).to eq("Date")
+  end
+
+  it "should handle non existent translations gracefully" do
+    expect(helper.l10n('pizza')).to eq("Pizza")
+  end
+
+  it "should handle non string translation keys gracefully" do
+    expect(helper.l10n({:formats=>{:default=>"%m/%d/%Y"}})).to eq("")
+
+  end
+end
