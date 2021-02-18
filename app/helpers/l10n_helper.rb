7f52ddd38954f4b619b8ba727c777a3e6669098d
zasman <zanesandin@gmail.com>
2021-02-18 10:20:09 -0500

    REFS Maintenance - Rubocop errors.

--- app/helpers/l10n_helper.rb
+++ app/helpers/l10n_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module L10nHelper
-  def l10n(translation_key, interpolated_keys={})
+  def l10n(translation_key, interpolated_keys = {})
     Rails.logger.error {"#L10nHelper passed non string key: #{translation_key.inspect}"} unless translation_key.is_a?(String)
     return "Translation Missing" unless translation_key.is_a?(String)
     t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
