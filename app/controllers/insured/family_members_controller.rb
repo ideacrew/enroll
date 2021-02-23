54b2c57ba9ec14cbff0e426ff93eadadfbab8029
Hithesh Kolla <hitheshkolla@ip-192-168-254-15.ec2.internal>
2021-02-23 16:38:56 -0500

    Spec fix

--- app/controllers/insured/family_members_controller.rb
+++ app/controllers/insured/family_members_controller.rb
@@ -108,10 +108,12 @@ class Insured::FamilyMembersController < ApplicationController
 
   def destroy
     @dependent.destroy!
-    active_family_members_count = @family.active_family_members.count
-    immediate_household_members_count = @family.active_household.immediate_family_coverage_household.coverage_household_members.count
-    extended_family_members_count = @family.active_household.extended_family_coverage_household.coverage_household_members.count
-    Rails.logger.info("In FamilyMembersController create action #{params}, #{@family.inspect}") unless active_family_members_count == immediate_household_members_count + extended_family_members_count
+    if @family.present?
+      active_family_members_count = @family.active_family_members&.count
+      immediate_household_members_count = @family.active_household.immediate_family_coverage_household.coverage_household_members.count
+      extended_family_members_count = @family.active_household.extended_family_coverage_household.coverage_household_members.count
+      Rails.logger.info("In FamilyMembersController create action #{params}, #{@family.inspect}") unless active_family_members_count == immediate_household_members_count + extended_family_members_count
+    end
     respond_to do |format|
       format.html { render 'index' }
       format.js { render 'destroyed' }
