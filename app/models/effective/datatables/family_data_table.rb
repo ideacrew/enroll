b6451a20c74d2852eece21f59b78ab5f531f9b78
nisanth <nisanth.yaganti@gmail.com>
2021-02-23 14:29:53 -0500

    add/show roles func to families table

--- app/models/effective/datatables/family_data_table.rb
+++ app/models/effective/datatables/family_data_table.rb
@@ -32,6 +32,7 @@ module Effective
            ['Reinstate', view_terminated_hbx_enrollments_exchanges_hbx_profiles_path(family: row.id, person_id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"), reinstate_enrollment_type(row, pundit_allow(Family, :can_update_ssn?))],
            [("<div class='" + pundit_class(Family, :can_update_ssn?) + "'> Edit DOB / SSN </div>").html_safe, edit_dob_ssn_path(id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"), 'ajax'],
            ['View Username and Email', get_user_info_exchanges_hbx_profiles_path(person_id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"), (individual_market_is_enabled? && pundit_allow(Family, :can_view_username_and_email?)) ? 'ajax' : 'disabled'],
+           ['Show/Add Roles', main_app.show_roles_person_path(id: row.primary_applicant.person.id), add_roles_link_type(pundit_allow(HbxProfile, :can_add_role?))],
            ['Collapse Form', hide_form_exchanges_hbx_profiles_path(family_id: row.id, person_id: row.primary_applicant.person.id, family_actions_id: "family_actions_#{row.id.to_s}"),'ajax']
            ]
 
@@ -84,6 +85,12 @@ module Effective
 
       def add_sep_link_type(allow)
         allow ? 'ajax' : 'disabled'
+      end
+
+      def add_roles_link_type(allow)
+        feature_enabled = ::EnrollRegistry.feature_enabled?(:manage_account_functionality)
+        policy_accepted_and_allow = feature_enabled && allow
+        policy_accepted_and_allow ? 'static' : 'hide'
       end
 
       def active_admin_dt_enrollments(row)
