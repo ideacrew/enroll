b588d22b64ffb2f103205c841d5bf449484762a7
ymhari <hariym471@gmail.com>
2021-02-25 14:25:44 -0500

    ref: 92707 Adds cucumbers.

--- features/financial_assistance/step_definitions/financial_assistance_steps.rb
+++ features/financial_assistance/step_definitions/financial_assistance_steps.rb
@@ -40,6 +40,7 @@ end
 
 Then(/^they should see a new finanical assistance application$/) do
   expect(page.current_url).to match("/applications/.*/edit")
+  expect(page).to have_content("If you need to add other members to your health insurance, select 'Add New Person'. When you’re finished, select CONTINUE.")
 end
 
 Then(/^they should see each of their dependents listed$/) do
