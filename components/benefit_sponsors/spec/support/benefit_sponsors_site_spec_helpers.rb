0e58d449f13e096e1cad9faee7bf25f01867cf0d
vineel <sonnathivinil@gmail.com>
2021-02-25 12:19:52 -0500

    Add ME helper

--- components/benefit_sponsors/spec/support/benefit_sponsors_site_spec_helpers.rb
+++ components/benefit_sponsors/spec/support/benefit_sponsors_site_spec_helpers.rb
@@ -1,10 +1,11 @@
 require File.join(File.dirname(__FILE__), "client_site_spec_helpers/dc")
 require File.join(File.dirname(__FILE__), "client_site_spec_helpers/cca")
+require File.join(File.dirname(__FILE__), "client_site_spec_helpers/me")
 
 module BenefitSponsors
   class SiteSpecHelpers
     const_name = Settings.site.key.upcase
     mod = const_get("BenefitSponsors::ClientSiteSpecHelpers::#{const_name}")
     extend mod
-end
+  end
 end
