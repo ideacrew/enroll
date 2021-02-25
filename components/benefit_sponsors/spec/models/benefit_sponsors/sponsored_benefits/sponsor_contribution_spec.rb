19ecaa43f81228469a66315aeac1b533b39918be
Mark Goho <markgoho@gmail.com>
2021-02-25 15:13:04 -0500

    adjust how benefit sponsors specs are run

--- components/benefit_sponsors/spec/models/benefit_sponsors/sponsored_benefits/sponsor_contribution_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-require 'rails_helper'
-RSpec.describe BenefitSponsors::SponsoredBenefits::SponsorContribution, type: :model, :dbclean => :after_each do
-  it "should exists as a class" do
-    expect(BenefitSponsors::SponsoredBenefits::SponsorContribution.new).to be_truthy
-  end
-end
--- components/benefit_sponsors/spec/models/benefit_sponsors/sponsored_benefits/sponsored_benefit_builder_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe BenefitSponsors::SponsoredBenefits::SponsoredBenefitBuilder, type: :model, :dbclean => :after_each do
-  it "should exist as a class" do
-    expect(BenefitSponsors::SponsoredBenefits::SponsoredBenefitBuilder.new).to be_truthy
-  end
-end
