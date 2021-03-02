296694e96ad3229e4e98457fd3765c7045137eb4
Bill Transue <transue@gmail.com>
2021-03-02 10:03:24 -0500

    Update branch for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db, %w{nginx@172.30.1.38}
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
-set :branch, 'me_enroll'
+set :branch, 'me_demo'
 #server '.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
