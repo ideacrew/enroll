91a8192734e74481d065ebe8f129768f092334f9
Jayanth Reddy Chinthareddy <jayreddy@Jayanths-MacBook-Pro.local>
2021-03-03 15:51:23 -0500

    ref#92821 fixed rubocop errors

--- features/financial_assistance/step_definitions/family_info_steps.rb
+++ features/financial_assistance/step_definitions/family_info_steps.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-And /^consumer clicks on pencil symbol next to primary person$/ do
+And(/^consumer clicks on pencil symbol next to primary person$/) do
   page.all('.fa-pencil-alt').first.click
 end
 
-Then /^consumer should see today date and clicks continue$/ do
-  page.find("#applicant_ssn")[:disabled] == "true"
-  page.find("input[name='jq_datepicker_ignore_applicant[dob]']")[:disabled] == "true"
+Then(/^consumer should see today date and clicks continue$/) do
+  expect(page.find("#applicant_ssn")[:disabled]).to eq "true"
+  expect(page.find("input[name='jq_datepicker_ignore_applicant[dob]'")[:disabled]).to eq "true"
 end
