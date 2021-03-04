c319c4746a5c00c12674721d645cb6607fc9282e
harshaellanki <h_vardhan@yahoo.com>
2021-03-04 09:21:55 -0500

    checking current user is present

--- components/sponsored_benefits/app/controllers/sponsored_benefits/application_controller.rb
+++ components/sponsored_benefits/app/controllers/sponsored_benefits/application_controller.rb
@@ -11,10 +11,10 @@ module SponsoredBenefits
 
       def set_broker_agency_profile_from_user
         current_uri = request.env['PATH_INFO']
-        if current_person.broker_role.present?
+        if current_person&.broker_role.present?
           @broker_agency_profile = ::BrokerAgencyProfile.find(current_person.broker_role.broker_agency_profile_id) # Deprecate this
           @broker_agency_profile ||= BenefitSponsors::Organizations::Profile.find(current_person.broker_role.benefit_sponsors_broker_agency_profile_id)
-        elsif active_user.has_hbx_staff_role? && params[:plan_design_organization_id].present?
+        elsif active_user&.has_hbx_staff_role? && params[:plan_design_organization_id].present?
            @broker_agency_profile = SponsoredBenefits::Organizations::PlanDesignOrganization.find(params[:plan_design_organization_id]).broker_agency_profile
         elsif params[:plan_design_proposal_id].present?
           org = SponsoredBenefits::Organizations::PlanDesignProposal.find(params[:plan_design_proposal_id]).plan_design_organization
@@ -35,7 +35,7 @@ module SponsoredBenefits
       end
 
       def current_person
-        current_user.person
+        current_user&.person
       end
 
       def active_user
