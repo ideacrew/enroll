c3484f7246ae697033ad026fa12c863fd1267694
GouthamRedd <gouti1234@gmail.com>
2021-03-09 10:49:27 -0500

    EE Plan shopping POM Libraries

---
+++ features/support/object_model_pages/shop/employee/ee_confirm_your_plan_selection.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class EmployeeConfirmYourPlanSelection
+
+  def self.previous_link
+    '.interaction-click-control-previous'
+  end
+
+  def self.save_and_exit_link
+    '.interaction-click-control-save---exit'
+  end
+
+  def self.waive_coverage
+    '.interaction-click-control-waive-coverage'
+  end
+
+  def self.confirm_btn
+    '#btn-continue'
+  end
+end
---
+++ features/support/object_model_pages/shop/employee/ee_enrollment_submitted.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class EmployeeEnrollmentSubmitted
+
+  def self.print_btn
+    '#btnPrint'
+  end
+
+  def self.continue_btn
+    '.interaction-click-control-continue'
+  end
+end
