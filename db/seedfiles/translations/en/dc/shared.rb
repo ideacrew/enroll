cf1a3669f8cc6b26895513a23f97620440d229dd
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-03-10 16:17:29 -0500

    REFS maintenance - adds translations to existing broker form and home address

---
+++ db/seedfiles/translations/en/dc/shared.rb
@@ -0,0 +1,8 @@
+SHARED_TRANSLATIONS = {
+    "en.address_line_1" => "Address Line 1",
+    "en.address_line_2" => "Address Line 2",
+    "en.city" => "City",
+    "en.zip" => "Zip",
+    "en.new_address_shared" => "NEW ADDRESS",
+    "en.home_address_shared" => "Home Address"
+}
