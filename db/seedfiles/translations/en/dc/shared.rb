e0bf500e64f961b321b01a728cdca4b66238cf43
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-03-10 16:38:55 -0500

    fixing capitalization to match previous commits

--- db/seedfiles/translations/en/dc/shared.rb
+++ db/seedfiles/translations/en/dc/shared.rb
@@ -1,8 +1,8 @@
 SHARED_TRANSLATIONS = {
-    "en.address_line_1" => "Address Line 1",
-    "en.address_line_2" => "Address Line 2",
-    "en.city" => "City",
-    "en.zip" => "Zip",
+    "en.address_line_1" => "ADDRESS LINE 1",
+    "en.address_line_2" => "ADDRESS LINE 2",
+    "en.city" => "CITY",
+    "en.zip" => "ZIP",
     "en.new_address_shared" => "NEW ADDRESS",
     "en.home_address_shared" => "Home Address"
 }
