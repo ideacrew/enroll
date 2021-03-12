40ca94e961e59d53cd4bab1a9a413e361fe79a5a
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-03-12 08:15:58 -0500

    translations for security_questions views

--- db/seedfiles/translations/en/dc/exchange.rb
+++ db/seedfiles/translations/en/dc/exchange.rb
@@ -40,5 +40,13 @@ EXCHANGE_TRANSLATIONS = {
   "en.exchange.manage_sep_types.fehb" => "Congress",
   "en.exchange.manage_sep_types.sep_type_list" => "List SEP Types",
   "en.exchange.manage_sep_types.titles" => "SEPs",
-  "en.exchange.manage_sep_types.sort_description" => "Drag and drop to change the order that the SEPs appear to customer or admin. This list includes all SEPs. SEPs with Admin Only visibility will show on this list and on the Admin SEP menu and carousel, but will not show on the customer SEP carousel."
+  "en.exchange.manage_sep_types.sort_description" => "Drag and drop to change the order that the SEPs appear to customer or admin. This list includes all SEPs. SEPs with Admin Only visibility will show on this list and on the Admin SEP menu and carousel, but will not show on the customer SEP carousel.",
+  "en.exchange.security_questions.error_cant_save" => "prohibited this question from being saved:",
+  "en.exchange.securty_questions.edit_question" => "Edit Question",
+  "en.exchange.securty_questions.back" => "Back",
+  "en.exchange.employer_applications.title" => "Title",
+  "en.exchange.employer_applications.created" => "Created",
+  "en.exchange.employer_applications.actions" => "Actions",
+  "en.exchange.employer_applications.new_question" => "New Question",
+  "en.exchange.employer_applications.security_questions" => "Security Questions"
 }
