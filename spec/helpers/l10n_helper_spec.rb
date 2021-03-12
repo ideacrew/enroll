893f50570ecee2769f63ebfb5ac0d364616f2139
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-03-12 09:48:10 -0500

    REFS maintenance - fixed broken translation for insured

---
+++ spec/helpers/l10n_helper_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+RSpec.describe L10nHelper, :type => :helper do
+  describe "#l10n" do
+    it "Should titelize nonexistent translations" do
+      expect(helper.l10n("test_translation_key")).to eq "Test Translation Key"
+    end
+  end
+end
