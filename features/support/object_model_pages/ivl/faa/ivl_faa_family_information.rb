677ae41a043e96ec0cc10466a75917f838d82b74
Ann Caballero <ann.caballero@ideacrew.com>
2021-03-18 14:57:18 -0400

    Rubocop fixes

--- features/support/object_model_pages/ivl/faa/ivl_faa_family_information.rb
+++ features/support/object_model_pages/ivl/faa/ivl_faa_family_information.rb
@@ -3,11 +3,11 @@
 #insured/family_members?consumer_role_id
 class IvlFaaFamilyInformation
 
-    def self.add_new_person
-      '#household_info_add_member'
-    end
+  def self.add_new_person
+    '#household_info_add_member'
+  end
 
-    def self.continue_btn
-      '#btn-continue'
-    end
+  def self.continue_btn
+    '#btn-continue'
+  end
 end
