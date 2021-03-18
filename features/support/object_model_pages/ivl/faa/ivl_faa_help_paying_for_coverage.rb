3f2c772d623154b9573e50344931529bc23e0922
Ann Caballero <ann.caballero@ideacrew.com>
2021-03-18 14:07:52 -0400

    new trunk yearly agg branch

---
+++ features/support/object_model_pages/ivl/faa/ivl_faa_help_paying_for_coverage.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+#insured/consumer_role/help_paying_coverage
+class IvlFaaHelpPayingForCoverage
+
+    def self.yes_radiobtn
+      'label[for="radio1"] span'
+    end
+
+    def self.no_radiobtn
+      'label[for="radio2"] span'
+    end
+  
+    def self.continue_btn
+      '#btn-continue'
+    end
+  
+    def self.your_application_for_premium_reductions_text
+      'Your Application for Premium Reductions'
+    end
+end
