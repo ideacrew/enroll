7e7b76b5318af18eca23345d8e3fbf64479f4813
Ann Caballero <ann.caballero@ideacrew.com>
2021-03-18 14:57:18 -0400

    Rubocop fixes

--- features/support/object_model_pages/ivl/faa/ivl_faa_help_paying_for_coverage.rb
+++ features/support/object_model_pages/ivl/faa/ivl_faa_help_paying_for_coverage.rb
@@ -3,19 +3,19 @@
 #insured/consumer_role/help_paying_coverage
 class IvlFaaHelpPayingForCoverage
 
-    def self.yes_radiobtn
-      'label[for="radio1"] span'
-    end
+  def self.yes_radiobtn
+    'label[for="radio1"] span'
+  end
 
-    def self.no_radiobtn
-      'label[for="radio2"] span'
-    end
-  
-    def self.continue_btn
-      '#btn-continue'
-    end
-  
-    def self.your_application_for_premium_reductions_text
-      'Your Application for Premium Reductions'
-    end
+  def self.no_radiobtn
+    'label[for="radio2"] span'
+  end
+
+  def self.continue_btn
+    '#btn-continue'
+  end
+
+  def self.your_application_for_premium_reductions_text
+    'Your Application for Premium Reductions'
+  end
 end
