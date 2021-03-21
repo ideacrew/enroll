039d8d43f119e152b95361a0981e7ea69a007135
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-03-21 13:02:32 -0400

    Update payload information for UI question

--- features/step_definitions/household_info_continue_button_steps.rb
+++ features/step_definitions/household_info_continue_button_steps.rb
@@ -122,6 +122,7 @@ And(/^primary applicant completes application and marks they are required to fil
   find("#has_self_employment_income_false").click
   find(:xpath, '//*[@id="btn-continue"]').click
 
+  find('#has_unemployment_income_false').click if FinancialAssistanceRegistry[:unemployment_income].enabled?
   find("#has_other_income_false").click
   find(:xpath, '//*[@id="btn-continue"]').click
   find("#has_deductions_false").click
