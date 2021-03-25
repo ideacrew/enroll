83d488f36c1253dcb50e915c2b9bea9f4fb8134d
Jayanth Reddy Chinthareddy <jayreddy@Jayanths-MacBook-Pro.local>
2021-03-25 13:22:22 -0400

    refs#93028_support Removed change as part of rebase

--- components/financial_assistance/app/models/financial_assistance/applicant.rb
+++ components/financial_assistance/app/models/financial_assistance/applicant.rb
@@ -675,8 +675,6 @@ module FinancialAssistance
 
       questions_array << is_former_foster_care  if foster_age_satisfied? && is_applying_coverage
       questions_array << is_post_partum_period  unless is_pregnant
-      questions_array << has_unemployment_income if FinancialAssistanceRegistry[:unemployment_income].enabled?
-      questions_array << is_physically_disabled
       questions_array << pregnancy_due_on << children_expected_count if is_pregnant
       questions_array << pregnancy_end_on << is_enrolled_on_medicaid if is_post_partum_period
       questions_array << is_student
