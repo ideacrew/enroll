696c8f328de269a65b9b690923c00128a8ef796f
Tony Schaffert <tony.schaffert@gmail.com>
2021-03-27 15:19:47 -0400

    Fix Rubocop yet again

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
@@ -17,5 +19,5 @@
 
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-    inflect.acronym 'SSN'
+  inflect.acronym 'SSN'
 end
