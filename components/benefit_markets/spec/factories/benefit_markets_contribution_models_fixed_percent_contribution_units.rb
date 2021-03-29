4a7d570756174027921883c5ab7d7855861f3485
Zane Sandin <zanesandin@gmail.com>
2021-03-29 15:43:31 -0400

    REFS Maintenance - Reverts unnecssarily changed files.

--- components/benefit_markets/spec/factories/benefit_markets_contribution_models_fixed_percent_contribution_units.rb
+++ components/benefit_markets/spec/factories/benefit_markets_contribution_models_fixed_percent_contribution_units.rb
@@ -1,28 +1,20 @@
 FactoryBot.define do
   factory :benefit_markets_contribution_models_fixed_percent_contribution_unit, class: 'BenefitMarkets::ContributionModels::FixedPercentContributionUnit' do
 
-    after(:build) do |contribution_unit|
-      member_relationship_operator = (contribution_unit.name == "employee") ? :== : :>=
-
-      contribution_unit.member_relationship_maps = [
-        build(:benefit_markets_contribution_models_member_relationship_map,
-              relationship_name: contribution_unit.name.to_sym,
-              operator: member_relationship_operator,
-              contribution_unit: contribution_unit,
-              count: 1)
-      ]
+    transient do
+      member_relationship_operator { :>= }
     end
 
-    # trait :with_member_relationship_maps do
-    #   member_relationship_maps {
-    #   	[
-    #   		build(:benefit_markets_contribution_models_member_relationship_map,
-    #         relationship_name: name,
-    #         operator: member_relationship_operator,
-    #         contribution_unit: self,
-    #         count: 1)
-    #   	]
-    #   }
-    # end
+    trait :with_member_relationship_maps do
+      member_relationship_maps {
+      	[ 
+      		build(:benefit_markets_contribution_models_member_relationship_map,
+            relationship_name: name,
+            operator: member_relationship_operator,
+            contribution_unit: self,
+            count: 1)
+      	]
+      }
+    end
   end
 end
