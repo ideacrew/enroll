46768c990dd8f23be5565de202bb07c754dd39ea
Zane Sandin <zanesandin@gmail.com>
2021-03-30 16:06:20 -0400

    REFS Maintenance - Adds proper system config to engine.

--- components/benefit_markets/spec/factories/benefit_markets_locations_rating_areas.rb
+++ components/benefit_markets/spec/factories/benefit_markets_locations_rating_areas.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :benefit_markets_locations_rating_area, class: 'BenefitMarkets::Locations::RatingArea' do
 
     active_year { TimeKeeper.date_of_record.year }
-    exchange_provided_code { "R-#{EnrollRegistry[:enroll_app].setting(:state_abbreviation).item}001" }
+    exchange_provided_code { "R-#{::EnrollRegistry[:enroll_app].setting(:state_abbreviation).item}001" }
     # These should never occur at the same time
-    covered_states { [EnrollRegistry[:enroll_app].setting(:state_abbreviation).item] }
+    covered_states { [::EnrollRegistry[:enroll_app].setting(:state_abbreviation).item] }
     county_zip_ids do
-      [create(:benefit_markets_locations_county_zip, county_name: EnrollRegistry[:enroll_app].setting(:contact_center_county).item, zip: EnrollRegistry[:enroll_app].setting(:contact_center_zip_code).item, state: Settings.aca.state_abbreviation).id]
+      [create(:benefit_markets_locations_county_zip, county_name: ::EnrollRegistry[:enroll_app].setting(:contact_center_county).item, zip: ::EnrollRegistry[:enroll_app].setting(:contact_center_zip_code).item, state: Settings.aca.state_abbreviation).id]
     end
   end
 end
