5ae1379b96114259e1f1470378e4de29d075ee1b
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-03-30 14:48:05 -0400

    Remove FA relationship factory

--- components/financial_assistance/spec/factories/financial_assistance_relationships.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :financial_assistance_relationship, class: "::FinancialAssistance::Relationship" do
-    kind { 'spouse' }
-    applicant_id {}
-    relative_id {}
-  end
-end
