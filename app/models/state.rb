d1ba29062ff1dd778d5a910367048fc4c4e0927d
Zane Sandin <zanesandin@gmail.com>
2021-03-30 12:31:14 -0400

    REFS Maintenance - Fixes cucumbeers.

--- app/models/state.rb
+++ app/models/state.rb
@@ -27,7 +27,7 @@ class State
     ["Louisiana", "LA"],
     ["Maine", "ME"],
     ["Maryland", "MD"],
-    ["Connecticut", "ME"],
+    ["Connecticut", "CT"],
     ["Michigan", "MI"],
     ["Minnesota", "MN"],
     ["Mississippi", "MS"],
