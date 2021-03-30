9d62a7d01966cf15c457407d0881c107187c76b4
Sai Praveen <saipraveen.gudimetla@gmail.com>
2021-03-30 18:28:58 -0400

    refs #93509 enable start new application button in all cases and show modal pop upon clicking if faa application exists

--- components/financial_assistance/db/seedfiles/translations/en/faa_translations.rb
+++ components/financial_assistance/db/seedfiles/translations/en/faa_translations.rb
@@ -4,6 +4,8 @@
 
 module FaaTranslations
   ASSISTANCE_TRANSLATIONS = {
+    "en.faa.start_new_application" => 'Start New Application',
+    "en.faa.cancel" => 'Cancel',
     "en.faa.curam_lookup" => "It looks like you've already completed an application for Medicaid and cost savings on DC Health Link. Please call DC Health Link at (855) 532-5465 to make updates to that application. If you keep going, we'll check to see if you qualify to enroll in a private health insurance plan on DC Health Link, but won't be able to tell you if you qualify for Medicaid or cost savings.",
     "en.faa.acdes_lookup" => "It looks like you're already covered by Medicaid. Please call DC Health Link at (855) 532-5465 to make updates to your case. If you keep going, we'll check to see if you qualify to enroll in a private health insurance plan on DC Health Link, but won't be able to tell you if you qualify for Medicaid or cost savings.",
 
@@ -13,7 +15,8 @@ module FaaTranslations
     "en.faa.medicaid_question" => "Do you want us to submit this application to the <state-abbreviation-placeholder> Department of Human Services (DHS) to do a full review of your application for Medicaid eligibility?",
     'en.faa.edit.delete_applicant' => 'Are you sure you want to remove this applicant?',
     'en.faa.edit.remove_warning' => 'This cannot be undone.',
-    'en.faa.incomes.job_income_note' => "Note: For job income this person currently receives, do not enter an end date into the ‘To’ field. Only enter an end date if the job income ended."
+    'en.faa.incomes.job_income_note' => "Note: For job income this person currently receives, do not enter an end date into the ‘To’ field. Only enter an end date if the job income ended.",
+    'en.faa.cost_savings.start_new_application' => 'Are you sure you want to start a brand new application?',
   }.freeze
 
   ELIGIBILITY_TRANSLATIONS = {
