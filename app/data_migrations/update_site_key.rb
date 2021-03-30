d9aca520493db2fd9312b3b1a16b560a48aced6b
Hithesh Kolla <hitheshkolla@hitheshkolla.myfiosgateway.com>
2021-03-29 21:44:51 -0400

    migration update

--- app/data_migrations/update_site_key.rb
+++ app/data_migrations/update_site_key.rb
@@ -1,9 +1,12 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
-
 class UpdateSiteKey < MongoidMigrationTask
   def migrate
-    site_key = ENV['new_site_key'].to_sym
-    BenefitSponsors::Site.all.first.update!(site_key: site_key)
+    site_key = ENV['new_site_key']&.to_sym
+    if site_key.present?
+      BenefitSponsors::Site.all.first.update!(site_key: site_key)
+    else
+      puts "No site key provided" unless Rails.env.test?
+    end
   rescue Exception => e
     puts e.message
   end
