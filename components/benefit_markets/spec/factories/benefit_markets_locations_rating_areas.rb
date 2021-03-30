8cd3d3dfb2579368d5e839ff8fc0cc13c890c5e4
Zane Sandin <zanesandin@gmail.com>
2021-03-30 15:41:01 -0400

    REFS Maintenance - FIxes rubocop issues.

--- components/benefit_markets/spec/factories/benefit_markets_locations_rating_areas.rb
+++ components/benefit_markets/spec/factories/benefit_markets_locations_rating_areas.rb
@@ -5,6 +5,8 @@ FactoryBot.define do
     exchange_provided_code { "R-#{EnrollRegistry[:enroll_app].setting(:state_abbreviation).item}001" }
     # These should never occur at the same time
     covered_states { [EnrollRegistry[:enroll_app].setting(:state_abbreviation).item] }
-    county_zip_ids { [create(:benefit_markets_locations_county_zip, county_name: EnrollRegistry[:enroll_app].setting(:contact_center_county).item, zip: EnrollRegistry[:enroll_app].setting(:contact_center_zip_code).item, state: Settings.aca.state_abbreviation).id] }
+    county_zip_ids do
+      [create(:benefit_markets_locations_county_zip, county_name: EnrollRegistry[:enroll_app].setting(:contact_center_county).item, zip: EnrollRegistry[:enroll_app].setting(:contact_center_zip_code).item, state: Settings.aca.state_abbreviation).id]
+    end
   end
 end
