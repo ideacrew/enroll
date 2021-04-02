d7efe0b0d204d4e61f1e62412410ec34e604830b
utkarsh7989 <utkarsh7989@gmail.com>
2021-04-02 03:05:26 -0400

    adds hide home address info javascript to dependents forms

--- app/assets/javascripts/employee_dependent.js
+++ app/assets/javascripts/employee_dependent.js
@@ -13,12 +13,14 @@ $(document).on('change', '#dependent_same_with_primary', function(){
   var target = $(this).parents('#dependent-address').find('#dependent-home-address-area');
   if ($(this).is(':checked')) {
     $(target).hide();
+    $(this).parents('#dependent-address').find('#home-info').hide();
     $("#dependent_addresses_0_address_1, #dependent_addresses_0_zip, #dependent_addresses_0_city").each(function() {
       $(this).prop('required', false);
     })
     $(target).find("#address_info .address_required").removeAttr('required');
   } else {
     $(target).show();
+    $(this).parents('#dependent-address').find('#home-info').show();
     $("#dependent_addresses_0_address_1, #dependent_addresses_0_zip, #dependent_addresses_0_city").each(function() {
       $(this).prop('required', true);
     })
