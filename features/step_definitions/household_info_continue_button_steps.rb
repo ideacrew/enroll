751b745a927e0bbc80a21bd1443cf71af34a6dbf
Jayanth Reddy Chinthareddy <jayreddy@Jayanths-MacBook-Pro.local>
2021-04-02 16:05:08 -0400

    refs#89736 Fix cucumbers where immigration status is nil

--- features/step_definitions/household_info_continue_button_steps.rb
+++ features/step_definitions/household_info_continue_button_steps.rb
@@ -106,7 +106,7 @@ When(/^all applicants are in Info Completed state$/) do
     find("#has_daily_living_no", wait: 10).click
     find("#need_help_paying_bills_no", wait: 10).click
     find("#radio_physically_disabled_no", wait: 10).click
-    choose('is_veteran_or_active_military_no')
+    # choose('is_veteran_or_active_military_no')
     find('[name=commit]', wait: 10).click
   end
 end
@@ -138,7 +138,7 @@ And(/^primary applicant completes application and marks they are required to fil
   find("#has_daily_living_no").click
   find("#need_help_paying_bills_no").click
   find("#radio_physically_disabled_no").click
-  choose('is_veteran_or_active_military_no')
+  # choose('is_veteran_or_active_military_no')
   find('[name=commit]').click
 end
 
