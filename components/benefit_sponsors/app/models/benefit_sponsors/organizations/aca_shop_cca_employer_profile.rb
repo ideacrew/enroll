88931d210c33d74e24ac3ec50feaba77a59ee784
Zane Sandin <zanesandin@gmail.com>
2021-04-05 14:20:46 -0400

    REFS Maintenance - Fixes some rubocop issues.

--- components/benefit_sponsors/app/models/benefit_sponsors/organizations/aca_shop_cca_employer_profile.rb
+++ components/benefit_sponsors/app/models/benefit_sponsors/organizations/aca_shop_cca_employer_profile.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
   module Organizations
     class AcaShopCcaEmployerProfile < BenefitSponsors::Organizations::Profile
@@ -9,17 +11,39 @@ module BenefitSponsors
       field :referred_by,         type: String
       field :referred_reason,     type: String
 
-      # TODO use SIC code validation
+      # TODO: use SIC code validation
       validates_presence_of :sic_code
 
-      embeds_one  :employer_attestation, class_name: '::EmployerAttestation'
+      embeds_one :employer_attestation, class_name: '::EmployerAttestation'
 
       add_observer BenefitSponsors::Observers::EmployerProfileObserver.new, [:update]
       add_observer BenefitSponsors::Observers::NoticeObserver.new, [:process_employer_profile_events]
 
       after_update :notify_observers
 
-      REFERRED_KINDS = ['Radio', 'Sign on bus, subway, gas station, etc.', 'Online advertisement, such as on Google or Pandora', 'Billboard', 'Video on a website', 'Social media, such as Facebook', 'Online search (for example, searching through Google for places to get insurance)', 'Insurance broker', 'Health insurance company/carrier', 'Hospital or community health center', 'Health insurance Assister or Navigator', 'State or Government Agency (Main Streets or Small Business Administration) ', 'Employer Association', 'Chamber of Commerce', 'Friend or family member', 'Health Connector sponsored event', 'New England Benefits Association (NEBA)', 'Greater Boston Chamber of Commerce', 'Television', 'Newspaper', 'Other']
+      REFERRED_KINDS = [
+        'Radio',
+        'Sign on bus, subway, gas station, etc.',
+        'Online advertisement, such as on Google or Pandora',
+        'Billboard',
+        'Video on a website',
+        'Social media, such as Facebook',
+        'Online search (for example, searching through Google for places to get insurance)',
+        'Insurance broker',
+        'Health insurance company/carrier',
+        'Hospital or community health center',
+        'Health insurance Assister or Navigator',
+        'State or Government Agency (Main Streets or Small Business Administration)',
+        'Employer Association',
+        'Chamber of Commerce',
+        'Friend or family member',
+        'Access Health sponsored event',
+        'New England Benefits Association (NEBA)',
+        'Greater Boston Chamber of Commerce',
+        'Television',
+        'Newspaper',
+        'Other'
+      ].freeze
 
       # TODO: Temporary fix until we move employer_attestation to benefit_sponsorship
       def is_attestation_eligible?
