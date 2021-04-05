6cd75d089c91e24bdc90c6dd35fbe8a6e5358108
Zane Sandin <zanesandin@gmail.com>
2021-04-05 13:59:10 -0400

    REFS Maintenance - Removes unnecessary method duplications.

--- app/models/concerns/config/site_model_concern.rb
+++ app/models/concerns/config/site_model_concern.rb
@@ -39,21 +39,5 @@ module Config::SiteModelConcern
     def is_shop_and_individual_market_enabled?
       EnrollRegistry.feature_enabled?(:aca_shop_market) && EnrollRegistry.feature_enabled?(:aca_individual_market)
     end
-
-    def is_shop_market_enabled?
-      EnrollRegistry.feature_enabled?(:aca_shop_market)
-    end
-
-    def is_fehb_market_enabled?
-      EnrollRegistry.feature_enabled?(:fehb_market)
-    end
-
-    def is_shop_or_fehb_market_enabled?
-      EnrollRegistry.feature_enabled?(:fehb_market) || EnrollRegistry.feature_enabled?(:aca_shop_market)
-    end
-
-    def is_individual_market_enabled?
-      EnrollRegistry.feature_enabled?(:aca_individual_market)
-    end
   end
 end
