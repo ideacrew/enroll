445474a58ec7438829c3b9f9a99b1dc2e93013c8
Zane Sandin <zanesandin@gmail.com>
2021-04-05 16:41:42 -0400

    REFS Maintenance - Fixes innappropriate spec.

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/registrations_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/registrations_controller_spec.rb
@@ -10,7 +10,7 @@ module BenefitSponsors
     let!(:rating_area) { create_default(:benefit_markets_locations_rating_area) }
     let(:agency_class) { BenefitSponsors::Organizations::OrganizationForms::RegistrationForm }
     # let!(:site)  { FactoryBot.create(:benefit_sponsors_site, :with_owner_exempt_organization, :dc, :with_benefit_market) }
-    let!(:site) { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :dc) }
+    let!(:site) { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, site_key: ::EnrollRegistry[:enroll_app].settings(:site_key).item) }
     let(:person) { FactoryBot.create(:person) }
     let(:edit_user) { FactoryBot.create(:user, :person => person)}
     let(:user) { FactoryBot.create(:user) }
