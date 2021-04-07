cd1350fa4240bda3009328bfffb7c700e25d60a7
Zane Sandin <zanesandin@gmail.com>
2021-04-07 15:26:50 -0400

    REFS maintenance - removes unused method.

--- components/sponsored_benefits/app/models/sponsored_benefits/services/general_agency_manager.rb
+++ components/sponsored_benefits/app/models/sponsored_benefits/services/general_agency_manager.rb
@@ -140,10 +140,6 @@ module SponsoredBenefits
         msg_box.save
       end
 
-      def dc?
-        Settings.aca.state_abbreviation != "MA"
-      end
-
       protected
 
       # rubocop:disable Metrics/ParameterLists
