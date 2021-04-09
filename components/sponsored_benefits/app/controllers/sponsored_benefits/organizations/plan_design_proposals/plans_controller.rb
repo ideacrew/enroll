8802e9c1d446b6dfb9cbbf7e10efdd13f852c0bc
Zane Sandin <zanesandin@gmail.com>
2021-04-09 13:38:48 -0400

    REFS Maintenance - Refactors plans controller.

--- components/sponsored_benefits/app/controllers/sponsored_benefits/organizations/plan_design_proposals/plans_controller.rb
+++ components/sponsored_benefits/app/controllers/sponsored_benefits/organizations/plan_design_proposals/plans_controller.rb
@@ -17,11 +17,11 @@ module SponsoredBenefits
         @plans = @plans.select{|a| a.premium_tables.present?}
         @search_options = ::Plan.search_options(@plans)
         @search_option_titles = {
-                'plan_type': (Settings.aca.state_abbreviation == 'DC') ? 'Plan Type' : 'HMO / PPO',
-                'plan_hsa': 'HSA - Compatible',
-                'metal_level': 'Metal Level',
-                'plan_deductible': 'Individual deductible (in network)'
-              }
+          'plan_type': (Settings.aca.state_abbreviation == 'DC') ? 'Plan Type' : 'HMO / PPO',
+          'plan_hsa': 'HSA - Compatible',
+          'metal_level': 'Metal Level',
+          'plan_deductible': 'Individual deductible (in network)'
+        }
       end
 
       def dental_reference_plans
@@ -30,35 +30,36 @@ module SponsoredBenefits
         @plans = @plans.select{|a| a.premium_tables.present?}
         @search_options = ::Plan.search_options(@plans)
         @search_option_titles = {
-            'plan_type': (Settings.aca.state_abbreviation == 'DC') ? 'Plan Type' : 'HMO / PPO',
-            'plan_hsa': 'HSA - Compatible',
-            'metal_level': 'Metal Level',
-            'plan_deductible': 'Individual deductible (in network)'
+          'plan_type': EnrollRegistry.feature_enabled?(:hmo_ppo_plan_type) ? 'HMO / PPO' : 'Plan Type',
+          'plan_hsa': 'HSA - Compatible',
+          'metal_level': 'Metal Level',
+          'plan_deductible': 'Individual deductible (in network)'
         }
       end
 
       private
-        helper_method :selected_carrier_level, :plan_design_organization, :carrier_profile, :carriers_cache, :kind
 
-        def selected_carrier_level
-          @selected_carrier_level ||= params[:selected_carrier_level]
-        end
+      helper_method :selected_carrier_level, :plan_design_organization, :carrier_profile, :carriers_cache, :kind
 
-        def plan_design_organization
-          @plan_design_organization ||= PlanDesignOrganization.find(params[:plan_design_organization_id])
-        end
+      def selected_carrier_level
+        @selected_carrier_level ||= params[:selected_carrier_level]
+      end
 
-        def carrier_profile
-          @carrier_profile ||= ::CarrierProfile.find(params[:carrier_id])
-        end
+      def plan_design_organization
+        @plan_design_organization ||= PlanDesignOrganization.find(params[:plan_design_organization_id])
+      end
 
-        def carriers_cache
-          @carriers_cache ||= ::CarrierProfile.all.inject({}){|carrier_hash, carrier_profile| carrier_hash[carrier_profile.id] = carrier_profile.legal_name; carrier_hash;}
-        end
+      def carrier_profile
+        @carrier_profile ||= ::CarrierProfile.find(params[:carrier_id])
+      end
 
-        def kind
-          params[:kind]
-        end
+      def carriers_cache
+        @carriers_cache ||= ::CarrierProfile.all.inject({}){|carrier_hash, carrier_profile| carrier_hash[carrier_profile.id] = carrier_profile.legal_name; carrier_hash; }
+      end
+
+      def kind
+        params[:kind]
+      end
     end
   end
 end
