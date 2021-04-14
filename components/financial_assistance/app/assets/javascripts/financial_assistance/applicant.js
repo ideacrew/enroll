754a89e46906d75d9dcf65b701cab326f9d6e685
utkarsh7989 <utkarsh7989@gmail.com>
2021-04-14 16:33:53 -0400

    WIP faa applicant dependent address changes

--- components/financial_assistance/app/assets/javascripts/financial_assistance/applicant.js
+++ components/financial_assistance/app/assets/javascripts/financial_assistance/applicant.js
@@ -2,9 +2,11 @@ $(document).on('change', '#applicant_same_with_primary', function(){
   var target = $(this).parents('#applicant-address').find('#applicant-home-address-area');
   if ($(this).is(':checked')) {
     $(target).hide();
+    $(this).parents('#applicant-address').find('#home-info').hide();
     $(target).find("#address_info .address_required").removeAttr('required');
   } else {
     $(target).show();
+    $(this).parents('#applicant-address').find('#home-info').show();
     if (!$(target).find("#applicant_no_dc_address").is(':checked')){
       $(target).find("#address_info .address_required").attr('required', true);
     };
