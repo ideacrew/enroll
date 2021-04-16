292f60a5e4603264cf7e813ac3417035cb728d96
utkarsh7989 <utkarsh7989@gmail.com>
2021-04-16 12:06:16 -0400

    adds faa translations
    Address Validation with Quadrants

--- app/data_migrations/golden_seed_helper.rb
+++ app/data_migrations/golden_seed_helper.rb
@@ -277,10 +277,10 @@ module GoldenSeedHelper
     end
     address = Address.new(
       kind: "primary",
-      address_1: address_1,
-      city: city,
-      state: state,
-      zip: zip
+      address_1: "60#{counter_number} #{('a'..'z').to_a.sample} #{['Street NE', 'Ave NE', 'Drive NE'].sample}",
+      city: EnrollRegistry[:enroll_app].setting(:contact_center_zip_code).item,
+      state: EnrollRegistry[:enroll_app].setting(:state_abbreviation).item,
+      zip: EnrollRegistry[:enroll_app].setting(:contact_center_zip_code).item
     )
     area_code = primary_phone.present? ? primary_phone.area_code : FFaker::PhoneNumber.area_code
     phone_number = primary_phone.present? ? primary_phone.number : generate_unique_phone_number
