bdd18937e4408aff7b9f0df882e1922d666e8f0a
Zane Sandin <zanesandin@gmail.com>
2021-04-21 09:51:54 -0400

    REFS Maintenance - Removes part of task that stores old config.

--- lib/store_registry_config.rb
+++ lib/store_registry_config.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 # Stores the current client configuration in config/client_config/#{state_abbreviation}/system
-# If there is a current client configuration there, it will store that configuration first in
-# config/client_config/#{state_abbreviation}/todays_date_config
 class StoreRegistryConfig < MongoidMigrationTask
   def current_config_folder
     "#{Rails.root}/system"
@@ -10,17 +8,6 @@ class StoreRegistryConfig < MongoidMigrationTask
 
   def target_config_folder
     "#{Rails.root}/config/client_config/#{@current_configured_state_abbreviation}"
-  end
-
-  def files_present_in_target_storage?
-    Dir.glob("#{@target_config_folder}/system/**/*").present?
-  end
-
-  def copy_last_set_of_config_files
-    return unless files_present_in_target_storage?
-    old_config_storage_folder = "#{TimeKeeper.date_of_record.to_s.gsub('/', '_')}_#{@current_configured_state_abbreviation}_config"
-    puts("Set of config files for #{@current_configured_state_abbreviation} present. Making copy of those in #{@target_config_folder}/#{old_config_storage_folder}")
-    `cd #{target_config_folder}; mkdir #{old_config_storage_folder}; cd; cp -r #{@target_config_folder}/system #{@target_config_folder}/#{old_config_storage_folder}`
   end
 
   def current_configured_state_abbreviation
@@ -36,7 +23,6 @@ class StoreRegistryConfig < MongoidMigrationTask
     @current_configured_state_abbreviation = current_configured_state_abbreviation
     @target_config_folder = target_config_folder
     puts("Initializing current configuration store. Current storage in #{@current_config_folder} will be stored in  #{@target_config_folder}.")
-    copy_last_set_of_config_files
     copy_current_system_configuration_to_storage
     puts("Storing current client configuration complete.")
   end
--- lib/tasks/store_registry_config.rake
+++ lib/tasks/store_registry_config.rake
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 # Stores the current client configuration in config/client_config/#{state_abbreviation}/system
-# If there is a current client configuration there, it will store that configuration first in
-# config/client_config/#{state_abbreviation}/todays_date_config# For Ex: RAILS_ENV=production bundle exec rake migrations:store_current_configuration
 
 require "#{Rails.root}/lib/store_registry_config.rb"
 
