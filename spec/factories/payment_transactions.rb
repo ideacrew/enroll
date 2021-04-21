1ae339bdec1fcde96fa0eb2f3776a62bfae2a804
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-04-21 12:03:17 -0400

    Add csv report generating task for paynow click tracking

---
+++ spec/factories/payment_transactions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :payment_transaction do
+    family
+    enrollment_id { '123456789' }
+    submitted_at { DateTime.new(2021,2,1) }
+    source { 'plan_shopping' }
+  end
+end
