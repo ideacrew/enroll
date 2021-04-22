32425242b06dd98b3a7040aa1ff1770a233fff8c
Zane Sandin <zanesandin@gmail.com>
2021-04-22 10:26:00 -0400

    REFS maintenance - adds rake task for all stored clients.
    REFS Maintenance - upates DC config.
    
    Fixes github client swap and changese namespace in client configuration.
    
    REFS maintenance - fixes yaml syntax.
    
    REFS maintenance - adds proper variable.
    
    REFS mainteneance - small tweak to kick off suite.
    
    REFS maintenance - renames some stuff.
    
    REFS maintenance - removes improper me storage.
    
    REFS maintenance - Fixes storage.
    
    REFS maintenance - Fixes me stored config.
    
    REFS maintenance - Adds a deploy thing.
    
    REFS maintenance - adds ignored thingie.
    
    REFS maintenance -a dds nil.
    
    REFS maintenance -f ixes deploy.
    
    REFS maintenance - FIxes deploy thingie.
    
    REFS Maintenance - Fixing stored resource site config .
    
    REFS maintenance - adds extra puts message.
    
    REFS maintenance - fixes namespace.
    
    REFS Maintenance - Adds inspect of environmental variable.
    
    REFS maintenance - stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,6 +56,10 @@ namespace :assets do
           execute :rake, "assets:clobber"
           execute("cd #{release_path} && nvm use 10 && RAILS_ENV=production NODE_ENV=production bundle exec rake assets:precompile")
           execute :rake, "seed:translations[db/seedfiles/english_translations_seed.rb]"
+          puts("Switching to #{env['client']} configuration.") unless ENV['client'].nil?
+          execute :rake, "configuration:client_configuration_toggler client='#{ENV['client']}'" unless ENV['client'].nil?
+          puts("No client configuration present, using current committed configuration.") if ENV['client'].nil?
+          puts("Here is the environmental configuration for client: #{ENV.inspect}")
         end
       end
     end
