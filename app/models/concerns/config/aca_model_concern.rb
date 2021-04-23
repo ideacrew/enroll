e2c0eb0be3108cedd56672fa084689f303a70a07
Zane Sandin <zanesandin@gmail.com>
2021-04-23 15:51:48 -0400

    REFS Maintenance - Adds enroll registry temporarily.

--- app/models/concerns/config/aca_model_concern.rb
+++ app/models/concerns/config/aca_model_concern.rb
@@ -2,6 +2,16 @@
 
 # rubocop:disable Style/ClassVars
 
+# TODO: WHy do I need to define this in this file?
+EnrollRegistry = ResourceRegistry::Registry.new
+
+EnrollRegistry.configure do |config|
+  config.name       = :enroll
+  config.created_at = DateTime.now
+  config.load_path  = Rails.root.join('system', 'config', 'templates', 'features').to_s
+end
+
+
 module Config::AcaModelConcern
   extend ActiveSupport::Concern
 
