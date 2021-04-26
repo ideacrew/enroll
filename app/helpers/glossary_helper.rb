f2749219ec3044e980a97ed09ff395b24e3289fe
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-04-26 11:08:57 -0400

    testing translations

--- app/helpers/glossary_helper.rb
+++ app/helpers/glossary_helper.rb
@@ -2,7 +2,8 @@
 
 module GlossaryHelper
   def support_texts(key)
-    @support_texts ||= {}
-    @support_texts[key] ||= ERB.new(YAML.load_file("app/views/shared/support_text_household.yml")[key]).result
+    l10n("support_texts.#{key}")
+    # @support_texts ||= {}
+    # @support_texts[key] ||= ERB.new(YAML.load_file("app/views/shared/support_text_household.yml")[key]).result
   end
 end
