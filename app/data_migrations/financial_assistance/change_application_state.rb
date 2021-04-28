d22d2b8531ae97861c3e3d438386e91a2aa069f9
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-04-28 12:37:56 -0400

    Fix rubocop issues

--- app/data_migrations/financial_assistance/change_application_state.rb
+++ app/data_migrations/financial_assistance/change_application_state.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
+# Change aasm_state of financial assistance application
 class ChangeApplicationState < MongoidMigrationTask
   def migrate
-    applications = get_application
+    applications = find_applications
     action = ENV['action'].to_s
 
     case action
-      when "cancel"
-        cancel_application(applications)
-      when "terminate"
-        terminate_application(applications)
+    when "cancel"
+      cancel_application(applications)
+    when "terminate"
+      terminate_application(applications)
     end
   end
 
-  def get_application
-    hbx_ids = "#{ENV['hbx_id']}".split(',').uniq
+  def find_applications
+    hbx_ids = ENV['hbx_id'].to_s.split(',').uniq
     hbx_ids.inject([]) do |enrollments, hbx_id|
       application = FinancialAssistance::Application.by_hbx_id(hbx_id.to_s)
-      if application.size != 1
-        raise "Found no (OR) more than 1 enrollments with the #{hbx_id}" unless Rails.env.test?
-      end
+      raise "Found no (OR) more than 1 enrollments with the #{hbx_id}" if application.size != 1 && !Rails.env.test?
       enrollments << application.first
     end
   end
@@ -29,7 +30,6 @@ class ChangeApplicationState < MongoidMigrationTask
       application.update_attributes!(aasm_state: "terminated")
       puts "application with hbx_id: #{application.hbx_id} terminated" unless Rails.env.test?
     end
-
   end
 
   def cancel_application(applications)
