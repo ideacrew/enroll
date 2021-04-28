8324a97374514c24a8e4678aad3d0d8c0ccaa3b7
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-04-28 13:20:34 -0400

    Update undefined cucumber step

---
+++ features/support/object_model_pages/financial_assistance/cost_savings_application_page.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+#dchbx.org
+class CostSavingsApplicationPage
+
+  def self.start_new_application_btn
+    '.interaction-click-control-start-new-application'
+  end
+end
