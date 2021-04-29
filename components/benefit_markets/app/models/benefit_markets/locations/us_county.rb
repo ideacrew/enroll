aaeef7415d6443daacccb3f4e3779981fda51e97
Sai Praveen <saipraveen.gudimetla@gmail.com>
2021-04-29 15:54:01 -0400

    load us counties and sent county in enrollment xml

---
+++ components/benefit_markets/app/models/benefit_markets/locations/us_county.rb
@@ -0,0 +1,16 @@
+module BenefitMarkets
+  module Locations
+    class UsCounty
+      include Mongoid::Document
+      include Mongoid::Timestamps
+
+      field :state_postal_code, type: String
+      field :county_fips_code, type: String
+      field :county_name, type: String
+
+      index({county_fips_code:  1}, {unique: true})
+      index({state_postal_code: 1, county_name: 1})
+      index({state_postal_code: 1})
+    end
+  end
+end
