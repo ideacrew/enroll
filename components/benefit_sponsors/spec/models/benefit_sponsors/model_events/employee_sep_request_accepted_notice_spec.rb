c65cc58c4d1f5100959cf4ce5dd004479aa72227
Zane Sandin <zanesandin@gmail.com>
2021-05-03 12:42:52 -0400

    REFS maintenance - fixes stub in benefit sponsors.

--- components/benefit_sponsors/spec/models/benefit_sponsors/model_events/employee_sep_request_accepted_notice_spec.rb
+++ components/benefit_sponsors/spec/models/benefit_sponsors/model_events/employee_sep_request_accepted_notice_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe 'BenefitSponsors::ModelEvents::EmployeeSepRequestAccepted', :dbcl
         allow(model_instance).to receive(:family).and_return(family)
         allow(family).to receive(:primary_applicant).and_return(fm)
         allow(fm).to receive(:person).and_return(person)
-        EnrollRegistry[:aca_shop_market].feature.stub(:is_enabled).and_return(true)
+        allow(EnrollRegistry).to receive(:feature_enabled?).with(:aca_shop_market).and_return(true)
       end
 
       it "should trigger notice event" do
@@ -67,7 +67,7 @@ RSpec.describe 'BenefitSponsors::ModelEvents::EmployeeSepRequestAccepted', :dbcl
         allow(family).to receive(:primary_applicant).and_return(fm)
         allow(fm).to receive(:person).and_return(person)
         allow(person).to receive(:has_multiple_active_employers?).and_return(true)
-        EnrollRegistry[:aca_shop_market].feature.stub(:is_enabled).and_return(true)
+        allow(EnrollRegistry).to receive(:feature_enabled?).with(:aca_shop_market).and_return(true)
       end
 
       it "should trigger notice event" do
