c656cc92c15c6ff483f39b0e9d5757fcdd74c175
Tony Schaffert <tony.schaffert@gmail.com>
2021-05-04 15:25:05 -0400

    more spec update

--- spec/controllers/insured/consumer_roles_controller_spec.rb
+++ spec/controllers/insured/consumer_roles_controller_spec.rb
@@ -220,6 +220,22 @@ RSpec.describe Insured::ConsumerRolesController, dbclean: :after_each, :type => 
 
   end
 
+<<<<<<< HEAD
+=======
+    context "PUT update" do
+      let(:addresses_attributes) do
+        {"0" => {"kind" => "home", "address_1" => "address1_a NE", "address_2" => "", "city" => "city1", "state" => "DC", "zip" => "22211", "id" => person.addresses[0].id.to_s},
+         "1" => {"kind" => "mailing", "address_1" => "address1_b NE", "address_2" => "", "city" => "city1", "state" => "DC", "zip" => "22211", "id" => person.addresses[1].id.to_s} }
+      end
+      let(:consumer_role_attributes) {consumer_role.attributes.to_hash }
+      let(:person_params) do
+        {"family" => {"application_type" => "Phone"}, "dob" => "1985-10-01", "first_name" => "martin","gender" => "male","last_name" => "york","middle_name" => "","name_sfx" => "","ssn" => "468389102","user_id" => "xyz",
+         us_citizen: "true", naturalized_citizen: "true"}
+      end
+      let(:person){ FactoryBot.create(:person, :with_family) }
+      let(:census_employee){FactoryBot.build(:census_employee)}
+      let(:employee_role){FactoryBot.build(:employee_role, :census_employee => census_employee)}
+>>>>>>> 20104f19f1 (more spec update)
 
   context "POST create with failed construct_employee_role", dbclean: :after_each do
     let(:person_params) do
@@ -312,8 +328,37 @@ RSpec.describe Insured::ConsumerRolesController, dbclean: :after_each, :type => 
         put :update, params: { person: person_params, id: "test" }
       end
 
+<<<<<<< HEAD
       it "should not empty the person's addresses on update" do
         expect(person.addresses).not_to eq []
+=======
+      context 'Address attributes' do
+        let(:valid_addresses_attributes) do
+          {"0" => {"kind" => "home", "address_1" => "address1_a NE", "address_2" => "", "city" => "city1", "state" => "DC", "zip" => "22211"},
+           "1" => {"kind" => "mailing", "address_1" => "address1_b NE", "address_2" => "", "city" => "city1", "state" => "DC", "zip" => "22211" } }
+        end
+        let(:invalid_addresses_attributes) do
+          {"0" => {"kind" => "home", "address_1" => "address1_a NE", "address_2" => "", "city" => "city1", "state" => "DC", "zip" => "222"},
+           "1" => {"kind" => "mailing", "address_1" => "test NE", "address_2" => "", "city" => "test", "state" => "DC", "zip" => "223"} }
+        end
+
+        it "should not update existing person with invalid addresses" do
+          person_params[:addresses_attributes] = invalid_addresses_attributes
+          allow(controller).to receive(:update_vlp_documents).and_return(true)
+          put :update, params: { person: person_params, id: "test" }
+          expect(response).to have_http_status(:success)
+          expect(response).to render_template(:edit)
+          expect(person.errors.full_messages).to include 'Home address: zip should be in the form: 12345 or 12345-1234'
+        end
+
+        it "should update existing person with valid addresses" do
+          person_params[:phones_attributes] = valid_addresses_attributes
+          allow(controller).to receive(:update_vlp_documents).and_return(true)
+          put :update, params: { person: person_params, id: "test" }
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(ridp_agreement_insured_consumer_role_index_path)
+        end
+>>>>>>> 20104f19f1 (more spec update)
       end
 
       it "should update addresses" do
