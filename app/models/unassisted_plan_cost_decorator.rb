524ebfb60ded51f7961c3b9fa7807d1226577a0e
Trey Evans <lewis.r.evans@gmail.com>
2021-05-05 09:47:34 -0400

    Cache rating address.

--- app/models/unassisted_plan_cost_decorator.rb
+++ app/models/unassisted_plan_cost_decorator.rb
@@ -41,14 +41,16 @@ class UnassistedPlanCostDecorator < SimpleDelegator
   end
 
   def rating_area
-    used_address = hbx_enrollment.consumer_role.rating_address
-    rating_area = ::BenefitMarkets::Locations::RatingArea.rating_area_for(used_address, during: schedule_date)
-    #  .where(active_year: __getobj__.active_year).detect{|a| a.county_zip_ids.include?(county_id)}
-    # rating_area.exchange_provided_code.present? ? rating_area.exchange_provided_code : __getobj__.premium_tables.first.rating_area.exchange_provided_code
-    if rating_area
-      rating_area.exchange_provided_code
-    else
-      __getobj__.premium_tables.first.rating_area.exchange_provided_code
+    @rating_area_code ||= begin
+      used_address = hbx_enrollment.consumer_role.rating_address
+      rating_area = ::BenefitMarkets::Locations::RatingArea.rating_area_for(used_address, during: schedule_date)
+      #  .where(active_year: __getobj__.active_year).detect{|a| a.county_zip_ids.include?(county_id)}
+      # rating_area.exchange_provided_code.present? ? rating_area.exchange_provided_code : __getobj__.premium_tables.first.rating_area.exchange_provided_code
+      if rating_area
+        rating_area.exchange_provided_code
+      else
+        __getobj__.premium_tables.first.rating_area.exchange_provided_code
+      end
     end
   end
 
