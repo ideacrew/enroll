5a41d5304efa418d8ee46fce4ec18b1064bdc336
nisanth <nisanth.yaganti@gmail.com>
2021-05-06 15:05:43 -0400

    rubocop fix & resetting GH specs to fail fast

--- app/models/person.rb
+++ app/models/person.rb
@@ -87,7 +87,7 @@ class Person
   field :tribal_id, type: String
 
   field :is_tobacco_user, type: String, default: "unknown"
-  field :tobacco_use, type: Boolean # todo - use above field
+  field :tobacco_use, type: Boolean # TODO: use above field
   field :language_code, type: String
 
   field :no_dc_address, type: Boolean, default: false
