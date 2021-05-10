266f5c6b32d73f3e7d678dae6ed5fc06c6ee8011
Zane Sandin <zanesandin@gmail.com>
2021-05-10 12:38:02 -0400

    REFS Maintenance - Adds golden seed ivl to accomodate extra stuff.

--- spec/data_migrations/golden_seed_spec.rb
+++ spec/data_migrations/golden_seed_spec.rb
@@ -16,38 +16,74 @@ describe "Golden Seed Rake Tasks", dbclean: :after_each do
         expect(subject.name).to eql given_task_name
       end
     end
+    context "with csv input" do
+      describe "requirements" do
+        before :each do
+          subject.migrate
+        end
 
-    describe "requirements" do
-      before :each do
-        subject.migrate
+        it "will not create new hbx profile and benefit sponsorship if they are already present" do
+          expect(HbxProfile.all.count).to eq(1)
+          subject.migrate
+          expect(HbxProfile.all.count).to eq(1)
+          expect(HbxProfile.all.map(&:benefit_sponsorship).count).to eq(1)
+        end
+
+        it "should create at least one IVL health product if none exist" do
+          products = BenefitMarkets::Products::Product.all.select { |product| product.benefit_market_kind.to_sym == :aca_individual }
+          expect(products.count).to_not be(0)
+        end
+
+        it "should create fully matched consumer records" do
+          consumer_roles = Person.all.select { |person| person.consumer_role.present? }
+          expect(consumer_roles.count).to_not be(0)
+        end
+
+        it "should create users for consumers" do
+          consumer_roles = Person.all.select { |person| person.consumer_role.present? }
+          consumer_person_ids = consumer_roles.map(&:_id)
+          consumer_users = User.all.select { |user| consumer_person_ids.include?(user.person_id) }
+          expect(consumer_users.length).to be > 1
+        end
+
+        it "should create active enrollments" do
+          expect(HbxEnrollment.enrolled.count).to be > 1
+        end
       end
+    end
+    context "without csv input" do
+      describe "requirements" do
+        before :each do
+          allow(subject).to receive(:ivl_testbed_scenario_csv).and_return(nil)
+          subject.migrate
+        end
+        it "will not create new hbx profile and benefit sponsorship if they are already present" do
+          expect(HbxProfile.all.count).to eq(1)
+          subject.migrate
+          expect(HbxProfile.all.count).to eq(1)
+          expect(HbxProfile.all.map(&:benefit_sponsorship).count).to eq(1)
+        end
 
-      it "will not create new hbx profile and benefit sponsorship if they are already present" do
-        expect(HbxProfile.all.count).to eq(1)
-        subject.migrate
-        expect(HbxProfile.all.count).to eq(1)
-        expect(HbxProfile.all.map(&:benefit_sponsorship).count).to eq(1)
-      end
+        it "should create at least one IVL health product if none exist" do
+          products = BenefitMarkets::Products::Product.all.select { |product| product.benefit_market_kind.to_sym == :aca_individual }
+          expect(products.count).to_not be(0)
+        end
 
-      it "should create at least one IVL health product if none exist" do
-        products = BenefitMarkets::Products::Product.all.select { |product| product.benefit_market_kind.to_sym == :aca_individual }
-        expect(products.count).to_not be(0)
-      end
+        it "should create fully matched consumer records" do
+          consumer_roles = Person.all.select { |person| person.consumer_role.present? }
+          expect(consumer_roles.count).to_not be(0)
+        end
 
-      it "should create fully matched consumer records" do
-        consumer_roles = Person.all.select { |person| person.consumer_role.present? }
-        expect(consumer_roles.count).to_not be(0)
-      end
+        it "should create users for consumers" do
+          consumer_roles = Person.all.select { |person| person.consumer_role.present? }
+          consumer_person_ids = consumer_roles.map(&:_id)
+          consumer_users = User.all.select { |user| consumer_person_ids.include?(user.person_id) }
+          expect(consumer_users.length).to be > 1
+        end
 
-      it "should create users for consumers" do
-        consumer_roles = Person.all.select { |person| person.consumer_role.present? }
-        consumer_person_ids = consumer_roles.map(&:_id)
-        consumer_users = User.all.select { |user| consumer_person_ids.include?(user.person_id) }
-        expect(consumer_users.length).to be > 1
-      end
-
-      it "should create active enrollments" do
-        expect(HbxEnrollment.enrolled.count).to be > 1
+        it "should create active enrollments" do
+          expect(HbxEnrollment.enrolled.count).to be > 1
+        end
       end
     end
   end
