5835718516e21b5d10bdb5cb0406ce7fcb628eb7
ymhari <hariym471@gmail.com>
2021-05-13 11:56:52 -0400

    Updates as per request.

--- components/magi_medicaid/app/domain/magi_medicaid/types.rb
+++ components/magi_medicaid/app/domain/magi_medicaid/types.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
-require 'uri'
-require 'cgi'
 require 'dry-types'
 
 Dry::Types.load_extensions(:maybe)
 
-# dry types
-module Types
-  send(:include, Dry.Types())
-  include Dry::Logic
+# MagiMedicaid engine
+module MagiMedicaid
+  # dry types
+  module Types
+    send(:include, Dry.Types())
+    include Dry::Logic
 
-  Bson = Types.Constructor(BSON::ObjectId) { |val| BSON::ObjectId val }
+    Bson = Types.Constructor(BSON::ObjectId) { |val| BSON::ObjectId val }
+  end
 end
--- components/magi_medicaid/lib/magi_medicaid.rb
+++ components/magi_medicaid/lib/magi_medicaid.rb
@@ -3,13 +3,11 @@
 require "magi_medicaid/engine"
 require 'mongoid'
 require 'aasm'
-require 'config'
 require 'dry-types'
 require 'dry-validation'
 # require 'dry-struct'
 require 'dry-monads'
 require 'font-awesome-rails'
-require 'haml-rails'
 require 'money-rails'
 # require Iap::Engine.root.join('app/domain/magi_medicaid/types')
 
