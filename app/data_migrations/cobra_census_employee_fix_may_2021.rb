9d7f051ed67dc97b7b9864cc4b0039e031dfab92
Zane Sandin <zanesandin@gmail.com>
2021-05-14 17:18:48 -0400

    REFS maintenance - fixes rubocop stuff.

--- app/data_migrations/cobra_census_employee_fix_may_2021.rb
+++ app/data_migrations/cobra_census_employee_fix_may_2021.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
+# A class to fix missing cobra enrollments
 class FixPlanYear < CobraCensusEmployeeFixMay2021
   def migrate
     ivl_csv = File.read(ENV['census_employee_csv_file_location'])
@@ -7,7 +10,7 @@ class FixPlanYear < CobraCensusEmployeeFixMay2021
       employee_data = employee_data.to_h.with_indifferent_access
       person = Person.where(hbx_id: employee_data['employee_hbx_id']).first
       puts("No person record found for #{employee_data['employee_hbx_id']}") if person.blank?
-      employee_role = person.employee_roles.detect { |employee_role| employee_role.employer_profile.hbx_id == employee_data[:employer_hbx_id] }
+      employee_role = person.employee_roles.detect { |er| er.employer_profile.hbx_id == employee_data[:employer_hbx_id] }
       puts("No employee role record found for #{employee_data['employee_hbx_id']}") if employee_role.blank?
       census_employee = employee_role.census_employee
       puts("Census employee found for #{employee_data['employee_hbx_id']}, beginning cobra enrollment.")
