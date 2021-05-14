4e2ce32a1482360af4d86d8efdafc0cc51a8473d
harshaellanki <h_vardhan@yahoo.com>
2021-05-14 19:28:10 -0400

    updated rake

--- app/data_migrations/cobra_census_employee_fix_may_2021.rb
+++ app/data_migrations/cobra_census_employee_fix_may_2021.rb
@@ -3,19 +3,29 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
 # A class to fix missing cobra enrollments
-class FixPlanYear < CobraCensusEmployeeFixMay2021
+class CobraCensusEmployeeFixMay2021 < MongoidMigrationTask
   def migrate
-    ivl_csv = File.read(ENV['census_employee_csv_file_location'])
-    CSV.parse(ivl_csv, :headers => true).each do |employee_data|
-      employee_data = employee_data.to_h.with_indifferent_access
+    file_name = ENV['file_name'].to_s
+    CSV.foreach("#{Rails.root}/#{file_name}", headers: true) do |row|
+      employee_data = row.to_h.with_indifferent_access
       person = Person.where(hbx_id: employee_data['employee_hbx_id']).first
-      puts("No person record found for #{employee_data['employee_hbx_id']}") if person.blank?
-      employee_role = person.employee_roles.detect { |er| er.employer_profile.hbx_id == employee_data[:employer_hbx_id] }
-      puts("No employee role record found for #{employee_data['employee_hbx_id']}") if employee_role.blank?
-      census_employee = employee_role.census_employee
-      puts("Census employee found for #{employee_data['employee_hbx_id']}, beginning cobra enrollment.")
-      cobra_date = employee_data[:cobra_start_date]&.to_date
-      census_employee.update_for_cobra(cobra_date, nil)
+      if person.present?
+        employee_role = person.employee_roles.detect { |er| er.employer_profile.hbx_id == employee_data[:employer_hbx_id] }
+        if employee_role.present?
+          census_employee = employee_role.census_employee
+          cobra_date = employee_data[:cobra_start_date]&.to_date
+          begin
+            census_employee.update_for_cobra(cobra_date, nil)
+            puts("Sucessfully created Cobra enrollment for person with hbx_id: #{employee_data['employee_hbx_id']}")
+          rescue StandardError => e
+            e.message
+          end
+        else
+          puts("No employee role record found for #{employee_data['employee_hbx_id']}")
+        end
+      else
+        puts("No person record found for #{employee_data['employee_hbx_id']}")
+      end
     end
   end
 end
