aed88a5d6b364e06185bfc0e1651c40f9433bab1
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-05-14 17:29:28 -0400

    pasing json to ajax function for redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,9 +61,7 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js   { render text: "window.location.assign('#{root_path}');" }
-      format.json { render json: root_path, status: :redirect, location: root_path }
-      #format.json { render json: "$.get('#{root_path}');" }
-      #format.json { render json: { :token_expired => 'root_path' } }
+      format.json { render json: { :token_expired => 'root_path' }.to_json }
     end
   end
 
