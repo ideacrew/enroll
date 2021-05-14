9754de84e245c96c66a92604efd6258b64429076
Zane Sandin <zanesandin@gmail.com>
2021-05-14 16:51:41 -0400

    REFS Maintennace - Adds rake task for cobra census employee enrollment fix.

---
+++ app/data_migrations/cobra_census_employee_fix_may_2021.rb
@@ -0,0 +1,18 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class FixPlanYear < CobraCensusEmployeeFixMay2021
+  def migrate
+    ivl_csv = File.read(ENV['census_employee_csv_file_location'])
+    CSV.parse(ivl_csv, :headers => true).each do |employee_data|
+      employee_data = employee_data.to_h.with_indifferent_access
+      person = Person.where(hbx_id: employee_data['employee_hbx_id']).first
+      puts("No person record found for #{employee_data['employee_hbx_id']}") if person.blank?
+      employee_role = person.employee_roles.detect { |employee_role| employee_role.employer_profile.hbx_id == employee_data[:employer_hbx_id] }
+      puts("No employee role record found for #{employee_data['employee_hbx_id']}") if employee_role.blank?
+      census_employee = employee_role.census_employee
+      puts("Census employee found for #{employee_data['employee_hbx_id']}, beginning cobra enrollment.")
+      cobra_date = employee_data[:cobra_start_date]&.to_date
+      census_employee.update_for_cobra(cobra_date, nil)
+    end
+  end
+end
