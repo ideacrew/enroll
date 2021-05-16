d89960ab01e855cf2fecfcaed46a368eddae76ac
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-05-16 15:12:56 -0400

    Make blind, daily help, bill help, and disability questions optional

--- features/financial_assistance/step_definitions/other_questions_page_steps.rb
+++ features/financial_assistance/step_definitions/other_questions_page_steps.rb
@@ -141,6 +141,18 @@ And(/^the user fills out the rest of the other questions form and submits it$/) 
   find('[name=commit]').click
 end
 
+And(/^the user fills out the required other questions and submits it$/) do
+  choose('is_ssn_applied_no')
+  choose('is_pregnant_no')
+  choose('is_post_partum_period_no')
+  choose('is_former_foster_care_no')
+  choose('is_student_no')
+  choose('is_veteran_or_active_military_no')
+  choose("is_resident_post_092296_no")
+  choose("medicaid_pregnancy_no") if page.all("#medicaid_pregnancy_no").present?
+  find('[name=commit]').click
+end
+
 And(/^the user fills out the rest of form with medicaid during pregnancy as yes and submits it$/) do
   choose('is_ssn_applied_no')
   find("#has_daily_living_no").click
