bc7070efcdabcb1b983df640758b26d6147f6702
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-05-25 15:26:41 -0400

    REFS Maintenance - redirecting to root_url on json & 401 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,7 +61,7 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js   { render text: "window.location.assign('#{root_path}');" }
-      format.json { render json: { :token_expired => 'root_path' }.to_json }
+      format.json { render json: { :token_expired => root_url }, status: :unauthorized }
     end
   end
 
