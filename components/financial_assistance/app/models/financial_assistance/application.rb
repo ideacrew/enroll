f58fa04f2d1a00fbeb61d0b67d613d59b9bb195d
Zane Sandin <zanesandin@gmail.com>
2021-05-27 14:56:53 -0400

    REFS maintenance - reverts unncessary change.

--- components/financial_assistance/app/models/financial_assistance/application.rb
+++ components/financial_assistance/app/models/financial_assistance/application.rb
@@ -922,6 +922,7 @@ module FinancialAssistance
 
     def is_application_valid?
       application_attributes_validity = self.valid?(:submission) ? true : false
+
       if relationships_complete?
         relationships_validity = true
       else
