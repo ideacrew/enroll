2c830d3040fa302a2c2c52eaf3395252c5c681fe
jayreddy519 <jayanth.rordev@gmail.com>
2021-05-27 09:34:40 -0400

    Updates new changes to save date

--- components/financial_assistance/app/controllers/financial_assistance/applicants_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applicants_controller.rb
@@ -43,6 +43,7 @@ module FinancialAssistance
 
     def update
       if params[:financial_assistance_applicant].present?
+        format_date_params params[:financial_assistance_applicant]
         @applicant.update_attributes!(permit_params(params[:financial_assistance_applicant]))
         head :ok, content_type: "text/html"
       else
@@ -157,6 +158,9 @@ module FinancialAssistance
       model_params["pregnancy_due_on"] = Date.strptime(model_params["pregnancy_due_on"].to_s, "%m/%d/%Y") if model_params["pregnancy_due_on"].present?
       model_params["pregnancy_end_on"] = Date.strptime(model_params["pregnancy_end_on"].to_s, "%m/%d/%Y") if model_params["pregnancy_end_on"].present?
       model_params["student_status_end_on"] = Date.strptime(model_params["student_status_end_on"].to_s, "%m/%d/%Y") if model_params["student_status_end_on"].present?
+
+      model_params["person_coverage_end_on"] = Date.strptime(model_params["person_coverage_end_on"].to_s, "%m/%d/%Y") if model_params["person_coverage_end_on"].present?
+      model_params["medicaid_cubcare_due_on"] = Date.strptime(model_params["medicaid_cubcare_due_on"].to_s, "%m/%d/%Y") if model_params["medicaid_cubcare_due_on"].present?
     end
 
     def build_error_messages(model)
