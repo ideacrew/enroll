b3f13f9fbf77416f084ed013dce18175b13dd0e8
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-05-27 10:17:07 -0400

    committing the supporting files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   require 'resque/server'
 
   require 'sidekiq/web'
