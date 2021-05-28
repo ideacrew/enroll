81a4bafa802e7d2dcf16cae4aa17e9bcfed5de42
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-05-28 09:05:49 -0400

    fake spec added to see on swagger ui

--- spec/requests/applications_spec.rb
+++ spec/requests/applications_spec.rb
@@ -1,4 +1,26 @@
 require 'swagger_helper'
 
 RSpec.describe 'applications', type: :request do
+
+  path '/api/v1/applications' do
+    
+    get 'edit an application' do
+      produces 'application/json'
+      parameter name: :id, in: path, type: string 
+
+        response '200' 'edit made' do
+          schema type: :object,
+            properties: {
+              id: { type: :string },
+              title: { type: :string },
+              content: { type: :string }
+            },
+            required: [ 'id', 'title', 'content' ]
+
+          let(:id) { Applications.edit(title: 'foo', content: 'bar').id }
+          run_test!
+        end
+      end
+    end
+  end
 end
