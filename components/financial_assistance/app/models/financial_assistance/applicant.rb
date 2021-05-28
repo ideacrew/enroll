694fad73ca49791b9779d5e992f7ef2001f8447b
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-05-28 17:42:21 -0400

    Rubocop fix

--- components/financial_assistance/app/models/financial_assistance/applicant.rb
+++ components/financial_assistance/app/models/financial_assistance/applicant.rb
@@ -747,9 +747,7 @@ module FinancialAssistance
         return deductions.present? if has_deductions
         deductions.blank?
       when :health_coverage
-        if EnrollRegistry[:indian_health_service_question].feature.is_enabled
-          return false if indian_tribe_member && health_service_through_referral.nil?
-        end
+        return false if indian_tribe_member && health_service_through_referral.nil? && EnrollRegistry[:indian_health_service_question].feature.is_enabled
         return false if has_enrolled_health_coverage.nil? || has_eligible_health_coverage.nil?
         return benefits.enrolled.present? && benefits.eligible.present? && benefits.all? {|benefit| benefit.valid? :submission} if has_enrolled_health_coverage && has_eligible_health_coverage
         return benefits.enrolled.present? && benefits.enrolled.all? {|benefit| benefit.valid? :submission} && benefits.eligible.blank? if has_enrolled_health_coverage && !has_eligible_health_coverage
