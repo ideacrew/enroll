1f7a9d6f764f35cd7f1a5e06eee60dbf39a7950b
Battula <vrbattula@outlook.com>
2021-05-28 16:21:21 -0400

    WIP: Adds operations to determine SLCSP

---
+++ app/domain/operations/products/age_lookup.rb
@@ -0,0 +1,26 @@
+module Operations
+  module Products
+    # This class is to load county zip combinations.
+    class AgeLookup
+      include Dry::Transaction::Operation
+
+      def call(age)
+        # TODO: read the lowest and highest ages from Settings/DB.
+        # These values are not constant for all the exchanges - need to update
+        lowest_age_for_premium_lookup = 14
+        highest_age_for_premium_lookup = 64
+
+        output =
+          if age <= lowest_age_for_premium_lookup
+            lowest_age_for_premium_lookup
+          elsif age >= highest_age_for_premium_lookup
+            highest_age_for_premium_lookup
+          else
+            age
+          end
+
+        Success(output)
+      end
+    end
+  end
+end
