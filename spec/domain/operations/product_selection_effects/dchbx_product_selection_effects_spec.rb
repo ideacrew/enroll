49a4a38719a220c37a4035716a70bf187fce177f
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-01 14:42:31 -0400

    refs proj200 ivl feature - allow admin to report sep based on sep reported date

--- spec/domain/operations/product_selection_effects/dchbx_product_selection_effects_spec.rb
+++ spec/domain/operations/product_selection_effects/dchbx_product_selection_effects_spec.rb
@@ -318,7 +318,7 @@ RSpec.describe Operations::ProductSelectionEffects::DchbxProductSelectionEffects
     allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(current_year, 11, 1))
   end
 
-  include_context 'family with one member and one enrollment and one renewal enrollment'
+  include_context 'family with one member and one enrollment'
 
   before do
     enrollment.update_attributes(effective_on: Date.new(current_year - 3))
@@ -327,7 +327,7 @@ RSpec.describe Operations::ProductSelectionEffects::DchbxProductSelectionEffects
   end
 
   it 'should not create a renewal enrollment' do
-    expect(family.hbx_enrollments.count).to eq(2)
+    expect(family.hbx_enrollments.count).to eq(1)
   end
 end
 
