8824c2834a25e0e293339714d58684c0de2fc4bf
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-06-02 12:59:52 -0400

    Change location of fa medicaid chip config

--- features/support/worlds/resource_registry_world.rb
+++ features/support/worlds/resource_registry_world.rb
@@ -22,6 +22,17 @@ module ResourceRegistryWorld
   def add_shop_markets_to_sep_types
     EnrollRegistry[:sep_types].settings(:market_kind).meta.stub(:enum).and_return([{individual: "Individual"}, {shop: "SHOP"}, {fehb: "Congress"}])
   end
+
+  def enable_fa_feature(feature_name)
+    return if fa_feature_enabled?(feature_name)
+
+    feature_dsl = FinancialAssistanceRegistry[feature_name]
+    feature_dsl.feature.stub(:is_enabled).and_return(true)
+  end
+
+  def fa_feature_enabled?(feature_name)
+    FinancialAssistanceRegistry.feature_enabled?(feature_name)
+  end
 end
 
 World(ResourceRegistryWorld)
