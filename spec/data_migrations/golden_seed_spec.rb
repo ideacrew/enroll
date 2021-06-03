7f1872d115e890bbdae0d0301d9619a1bdf4050f
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-03 15:51:28 -0400

    fix more specs

--- spec/data_migrations/golden_seed_spec.rb
+++ spec/data_migrations/golden_seed_spec.rb
@@ -9,10 +9,6 @@ require File.join(Rails.root, "app", "data_migrations", "load_issuer_profiles")
 
 describe "Golden Seed Rake Tasks", dbclean: :after_all do
 
-  before do
-    DatabaseCleaner.clean
-  end
-
   describe "Generate Consumers and Families for Individual Market" do
     let(:given_task_name) { "golden_seed_individual" }
     subject { GoldenSeedIndividual.new(given_task_name, double(:current_scope => nil)) }
