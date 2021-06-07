a2e36da9d298512c8bad4f90a4efcf5b83f377f5
Battula <vrbattula@outlook.com>
2021-06-07 15:36:10 -0400

    Reuses FetchSilverProductPremiums operations

--- app/domain/operations/products/fetch_slcsp_premium_for_tax_household_member.rb
+++ app/domain/operations/products/fetch_slcsp_premium_for_tax_household_member.rb
@@ -29,29 +29,20 @@ module Operations
       end
 
       def fetch_product_premiums(products, tax_household_member, effective_date, rating_area_id)
-        family_member = tax_household_member.family_member
-        age = family_member.age_on(effective_date)
-        # age = ::Operations::AgeLookup.new.call(age).success if false && age_rated # TODO - Get age_rated through settings
-        cost_product_hash =
-          products.inject([]) do |result, product|
-            premium_table = product.premium_tables.where(
-              {
-                :rating_area_id => rating_area_id,
-                :'effective_period.min'.lte => effective_date,
-                :'effective_period.max'.gte => effective_date
-              }
-            ).first
+        family_member_id = tax_household_member.family_member_id
 
-            tuple = premium_table.premium_tuples.where(age: age).first
-            result << { cost: tuple.cost, product_id: product.id } if tuple.present?
-            result
-          end
+        product_hash = Operations::Products::FetchSilverProductPremiums.new.call(
+          {
+            products: products,
+            family: tax_household_member.family,
+            family_member_id: family_member_id,
+            effective_date: effective_date,
+            rating_area_id: rating_area_id
+          }
+        )
 
-        sorted_product_hash = cost_product_hash.sort_by { |tuple_hash| tuple_hash[:cost] }
-        result = sorted_product_hash[1] || sorted_product_hash[0]
-
-        if result
-          Success(result)
+        if product_hash.success? && product_hash.success[family_member_id.to_s]
+          Success(product_hash.success[family_member_id.to_s])
         else
           Failure("Unable to determine SLCSP premium for tax_household_member: #{tax_household_member.id} for effective_date: #{effective_date} and rating_area: #{rating_area_id}")
         end
