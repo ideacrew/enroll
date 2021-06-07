c783b8d8ad355edc702b1b1d971b1ac2be32a7ab
Battula <vrbattula@outlook.com>
2021-06-07 15:36:10 -0400

    Reuses FetchSilverProductPremiums operations

--- app/domain/operations/products/fetch_silver_product_premiums.rb
+++ app/domain/operations/products/fetch_silver_product_premiums.rb
@@ -10,9 +10,11 @@ module Operations
       # @param [RatingArea] rating_area_id
       # @param [Family] family
       # @param [Product] products
+      # @param [FamilyMember] family_member_id Passing in family_member_id will only return premiums for that particular member 
       def call(params)
         values            = yield validate(params)
-        product_premiums  = yield fetch_product_premiums(values[:products], values[:family], values[:effective_date], values[:rating_area_id].to_s)
+        family_members    = yield fetch_family_members(values[:family], values[:family_member_id])
+        product_premiums  = yield fetch_product_premiums(values[:products], family_members, values[:effective_date], values[:rating_area_id].to_s)
 
         Success(product_premiums)
       end
@@ -28,22 +30,34 @@ module Operations
         Success(params)
       end
 
-      def fetch_product_premiums(products, family, effective_date, rating_area_id)
-        member_premiums = family.family_members.active.inject({}) do |member_result, family_member|
+      def fetch_family_members(family, family_member_id)
+        family_members = family.family_members.active
+        family_members = family.family_members.where(id: BSON::ObjectId(family_member_id)) if family_member_id
+
+        if family_members.present?
+          Success(family_members)
+        else
+          Failure("Unable to find family members for the given family: #{family.id} & family_member_id: #{family_member_id}")
+        end
+      end
+
+      def fetch_product_premiums(products, family_members, effective_date, rating_area_id)
+        member_premiums = family_members.inject({}) do |member_result, family_member|
           age = family_member.age_on(effective_date)
           age = ::Operations::AgeLookup.new.call(age).success if false && age_rated # Todo - Get age_rated through settings
+          cost_product_hash =
+            products.inject([]) do |result, product|
+              premium_table = product.premium_tables.where({
+                                                             :rating_area_id => rating_area_id,
+                                                             :'effective_period.min'.lte => effective_date,
+                                                             :'effective_period.max'.gte => effective_date
+                                                           }).first
 
-          member_result[family_member.id.to_s] = products.inject([]) do |result, product|
-            premium_table = product.premium_tables.where({
-                                                           :rating_area_id => rating_area_id,
-                                                           :'effective_period.min'.lte => effective_date,
-                                                           :'effective_period.max'.gte => effective_date
-                                                         }).first
-
-            tuple = premium_table.premium_tuples.where(age: age).first
-            result << { cost: tuple.cost, product_id: product.id } if tuple.present?
-            result
-          end.sort_by {|tuple_hash| tuple_hash[:cost]}
+              tuple = premium_table.premium_tuples.where(age: age).first
+              result << { cost: tuple.cost, product_id: product.id } if tuple.present?
+              result
+            end.sort_by {|tuple_hash| tuple_hash[:cost]}
+          member_result[family_member.id.to_s] = cost_product_hash[1] || cost_product_hash[0]
 
           member_result
         end
