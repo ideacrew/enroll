b1c19dcdf77326ee7d9734daa8be3405891b23ad
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-06-10 11:02:17 -0400

    Immigration status checkbox feature added

--- app/assets/javascripts/demographics_fields.js
+++ app/assets/javascripts/demographics_fields.js
@@ -87,7 +87,7 @@ function applyListenersFor(target) {
 
   $("input[name='" + target + "[eligible_immigration_status]']").change(function() {
     var selected_doc_type = $('#immigration_doc_type').val();
-    if ($(this).val() == 'true') {
+    if ($(this).val() == 'true' && this.checked) {
       $('#vlp_documents_container').show();
       $('#naturalization_doc_type_select').hide();
       $('#immigration_doc_type_select').show();
