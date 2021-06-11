6fdc88e2c65425cf75d8224e8f88aaba10f4e048
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-11 13:09:41 -0400

    fix rspec and cucumber

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -135,6 +135,14 @@ Then(/^.+ should not see error message (.*)$/) do |text|
 end
 
 And(/(.*) selects eligible immigration status$/) do |text|
+  class VlpDocument
+    VLP_DOCUMENT_KINDS = [ "I-327 (Reentry Permit)", "I-551 (Permanent Resident Card)", "I-571 (Refugee Travel Document)", "I-766 (Employment Authorization Card)",
+                           "Certificate of Citizenship","Naturalization Certificate","Machine Readable Immigrant Visa (with Temporary I-551 Language)", "Temporary I-551 Stamp (on passport or I-94)", "I-94 (Arrival/Departure Record)",
+                           "I-94 (Arrival/Departure Record) in Unexpired Foreign Passport", "Unexpired Foreign Passport",
+                           "I-20 (Certificate of Eligibility for Nonimmigrant (F-1) Student Status)", "DS2019 (Certificate of Eligibility for Exchange Visitor (J-1) Status)",
+                           "Other (With Alien Number)", "Other (With I-94 Number)" ]
+  end
+
   if text == "Dependent"
     find(:xpath, '//label[@for="dependent_us_citizen_false"]').click
     find(:xpath, '//label[@for="dependent_eligible_immigration_status_true"]').click
