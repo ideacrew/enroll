cf574107384ef0d306c38db1df67f1033b36e145
nisanth <nisanth.yaganti@gmail.com>
2021-06-11 15:18:34 -0400

    updating / adding operations to give response to match aca_entities

--- spec/domain/operations/products/fetch_slcsp_spec.rb
+++ spec/domain/operations/products/fetch_slcsp_spec.rb
@@ -23,58 +23,54 @@ RSpec.describe ::Operations::Products::FetchSlcsp, dbclean: :after_each do
   describe 'valid params' do
 
     let(:person) { FactoryBot.create(:person, :with_consumer_role) }
-    let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
-    let(:family_member_id) { family.family_members.first.id.to_s }
-    let(:rating_address) { person.consumer_role.rating_address }
 
-    let(:effective_date) { TimeKeeper.date_of_record }
     let(:params) do
       {
-        family: family,
-        effective_date: effective_date
+        member_silver_product_premiums: member_silver_product_premiums
       }
     end
 
-    let(:silver_product_premiums) do
+    let(:member_silver_product_premiums) do
       {
-        family_member_id => [
-          { :cost => 200.0, :product_id => BSON::ObjectId.new },
-          { :cost => 300.0, :product_id => BSON::ObjectId.new },
-          { :cost => 400.0, :product_id => BSON::ObjectId.new }
-        ]
+        [person.hbx_id] => {
+          :health_only => {
+            person.hbx_id => [
+              {
+                :monthly_premium => 200.0,
+                :member_identifier => BSON::ObjectId.new,
+                :product_id => BSON::ObjectId.new
+              },
+              {
+                :monthly_premium => 300.0,
+                :member_identifier => BSON::ObjectId.new,
+                :product_id => BSON::ObjectId.new
+              },
+              {
+                :monthly_premium => 400.0,
+                :member_identifier => BSON::ObjectId.new,
+                :product_id => BSON::ObjectId.new
+              }
+            ]
+          }
+        }
       }
     end
-
-    let!(:list_products) { FactoryBot.create_list(:benefit_markets_products_health_products_health_product, 5, :silver) }
-
-    let(:products) { ::BenefitMarkets::Products::Product.all }
-    let(:products_payload) do
-      {
-        rating_area_id: BSON::ObjectId.new,
-        products: products
-      }
-    end
-
-    before :each do
-      allow(Operations::Products::FetchSilverProducts).to receive(:new).and_return double(call: ::Dry::Monads::Result::Success.new(products_payload))
-      allow(Operations::Products::FetchSilverProductPremiums).to receive(:new).and_return double(call: ::Dry::Monads::Result::Success.new(silver_product_premiums))
-    end
-
 
     it 'should return success' do
       result = subject.call(params)
       expect(result.success?).to eq true
     end
 
-    it 'should return an array of slcsp for the given family' do
+    it 'should return an array of health_only_slcsp_premiums for the given family' do
       value = subject.call(params).value!
       expect(value.is_a?(Hash)).to eq true
-      expect(value[[rating_address.id.to_s]].keys.include?(:health_only)).to eq true
+      expect(value[person.hbx_id].keys.include?(:health_only_slcsp_premiums)).to eq true
     end
 
-    it 'should return premium & product id of second lowest plan' do
-      values = subject.call(params).value![[rating_address.id.to_s]][:health_only][family_member_id]
-      expect(values[:cost]).to eq 300.0
+    it 'should return details of second lowest plan' do
+      values = subject.call(params).value![person.hbx_id][:health_only_slcsp_premiums][0]
+      expect(values[:monthly_premium]).to eq 300.0
+      expect(values[:member_identifier]).not_to eq nil
       expect(values[:product_id]).not_to eq nil
     end
   end
