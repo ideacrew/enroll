66bb9b7c56e713074b441336dcd0ee58a50b88a4
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-11 11:46:58 -0400

    Load vlp document kinds based on settings

--- app/models/vlp_document.rb
+++ app/models/vlp_document.rb
@@ -29,29 +29,7 @@ class VlpDocument < Document
   ]
 
   #list of the documents user can provide to verify Immigration status
-  VLP_DOCUMENT_KINDS = [
-    "I-327 (Reentry Permit)",
-    "I-551 (Permanent Resident Card)",
-    "I-571 (Refugee Travel Document)",
-    "I-766 (Employment Authorization Card)"
-  ]
-  if EnrollRegistry[:enroll_app].setting(:state_abbreviation).item != "ME"
-    VLP_DOCUMENT_KINDS += [
-        "Certificate of Citizenship",
-        "Naturalization Certificate"
-    ]
-  end
-  VLP_DOCUMENT_KINDS += [
-    "Machine Readable Immigrant Visa (with Temporary I-551 Language)",
-    "Temporary I-551 Stamp (on passport or I-94)",
-    "I-94 (Arrival/Departure Record)",
-    "I-94 (Arrival/Departure Record) in Unexpired Foreign Passport",
-    "Unexpired Foreign Passport",
-    "I-20 (Certificate of Eligibility for Nonimmigrant (F-1) Student Status)",
-    "DS2019 (Certificate of Eligibility for Exchange Visitor (J-1) Status)",
-    "Other (With Alien Number)",
-    "Other (With I-94 Number)"
-  ]
+  VLP_DOCUMENT_KINDS = EnrollRegistry[:vlp_documents].setting(:vlp_document_kind_options).item
 
     VLP_DOCUMENTS_VERIF_STATUS = ['not submitted', 'downloaded', 'verified', 'rejected']
 
