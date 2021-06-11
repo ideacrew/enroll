c450c6e580bcb2aeaeeddd3ebf77fdae5a1b54b1
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-11 13:09:41 -0400

    fix rspec and cucumber

--- spec/models/consumer_role_spec.rb
+++ spec/models/consumer_role_spec.rb
@@ -1230,6 +1230,14 @@ describe 'verification_types' do
   end
 end
 
+class VlpDocument
+  VLP_DOCUMENT_KINDS = [ "I-327 (Reentry Permit)", "I-551 (Permanent Resident Card)", "I-571 (Refugee Travel Document)", "I-766 (Employment Authorization Card)",
+                         "Certificate of Citizenship","Naturalization Certificate","Machine Readable Immigrant Visa (with Temporary I-551 Language)", "Temporary I-551 Stamp (on passport or I-94)", "I-94 (Arrival/Departure Record)",
+                         "I-94 (Arrival/Departure Record) in Unexpired Foreign Passport", "Unexpired Foreign Passport",
+                         "I-20 (Certificate of Eligibility for Nonimmigrant (F-1) Student Status)", "DS2019 (Certificate of Eligibility for Exchange Visitor (J-1) Status)",
+                         "Other (With Alien Number)", "Other (With I-94 Number)" ]
+end
+
 describe 'vlp documents' do
   context 'i551' do
     let!(:consumer_role) { FactoryBot.create(:consumer_role, vlp_documents: [vlp_doc]) }
