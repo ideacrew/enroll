560b29fd47a3f9b42fe6880a3a55b838c9a6a081
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-11 11:46:58 -0400

    Load vlp document kinds based on settings

--- components/financial_assistance/app/models/financial_assistance/applicant.rb
+++ components/financial_assistance/app/models/financial_assistance/applicant.rb
@@ -57,29 +57,7 @@ module FinancialAssistance
     DRIVER_QUESTION_ATTRIBUTES.freeze
 
     #list of the documents user can provide to verify Immigration status
-    VLP_DOCUMENT_KINDS = [
-        "I-327 (Reentry Permit)",
-        "I-551 (Permanent Resident Card)",
-        "I-571 (Refugee Travel Document)",
-        "I-766 (Employment Authorization Card)"
-    ]
-    if EnrollRegistry[:enroll_app].setting(:state_abbreviation).item != "ME"
-      VLP_DOCUMENT_KINDS += [
-          "Certificate of Citizenship",
-          "Naturalization Certificate"
-      ]
-    end
-    VLP_DOCUMENT_KINDS += [
-        "Machine Readable Immigrant Visa (with Temporary I-551 Language)",
-        "Temporary I-551 Stamp (on passport or I-94)",
-        "I-94 (Arrival/Departure Record)",
-        "I-94 (Arrival/Departure Record) in Unexpired Foreign Passport",
-        "Unexpired Foreign Passport",
-        "I-20 (Certificate of Eligibility for Nonimmigrant (F-1) Student Status)",
-        "DS2019 (Certificate of Eligibility for Exchange Visitor (J-1) Status)",
-        "Other (With Alien Number)",
-        "Other (With I-94 Number)"
-    ].freeze
+    VLP_DOCUMENT_KINDS = EnrollRegistry[:vlp_documents].setting(:vlp_document_kind_options).item
 
     CITIZEN_KINDS = {
       us_citizen: "US citizen",
