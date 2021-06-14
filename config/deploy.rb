4b169f2336db16b18590b5ed33a13d95b231416e
Zane Sandin <zanesandin@gmail.com>
2021-06-14 11:54:18 -0400

    REFS Maintenance - Fixes deploy rb to set the variable more precisely.

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,10 +56,10 @@ namespace :assets do
           execute :rake, "assets:clobber"
           execute("cd #{release_path} && nvm use 10 && RAILS_ENV=production NODE_ENV=production bundle exec rake assets:precompile")
           execute :rake, "seed:translations[db/seedfiles/english_translations_seed.rb]"
-          client_variabe = ENV['CLIENT'].downcase || ENV['client'].downcase
-          puts("Switching to #{client_variabe} configuration.") unless client_variabe.nil?
-          execute :rake, "configuration:client_configuration_toggler client='#{client_variabe}'" unless client_variabe.nil?
-          puts("No client configuration present, using current committed configuration.") if client_variabe.nil?
+          client_variable = ENV['CLIENT'].downcase || ENV['client'].downcase || EnrollRegistry[:enroll_app]&.setting(:site_key)&.item.to_s
+          puts("Switching to #{client_variable} configuration.") unless client_variable.blank?
+          execute :rake, "configuration:client_configuration_toggler client='#{client_variable}'" unless client_variable.blank?
+          puts("No client configuration present, using current committed configuration.") if client_variable.blank?
         end
       end
     end
