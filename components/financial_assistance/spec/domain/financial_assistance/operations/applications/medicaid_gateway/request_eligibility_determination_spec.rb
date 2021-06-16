4cada049455b70e4a75bf321bcca734ef8609bc2
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-16 12:45:02 -0400

    request_eligibility_determination spec fix

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/medicaid_gateway/request_eligibility_determination_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/medicaid_gateway/request_eligibility_determination_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe ::FinancialAssistance::Operations::Applications::MedicaidGateway::RequestEligibilityDetermination, dbclean: :after_each do
+  include Dry::Monads[:result, :do]
+
   let!(:person) { FactoryBot.create(:person, hbx_id: "732020")}
   let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
   let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family.id, aasm_state: 'submitted', hbx_id: "830293", effective_date: DateTime.new(2021,1,1,4,5,6)) }
@@ -38,6 +40,14 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::MedicaidGateway:
   end
 
   let!(:eligibility_determination) { FactoryBot.create(:financial_assistance_eligibility_determination, application: application) }
+  let(:event) { Success(double) }
+  let(:obj)  { FinancialAssistance::Operations::Applications::MedicaidGateway::PublishApplication.new }
+
+  before do
+    allow(FinancialAssistance::Operations::Applications::MedicaidGateway::PublishApplication).to receive(:new).and_return(obj)
+    allow(obj).to receive(:build_event).and_return(event)
+    allow(event.success).to receive(:publish).and_return(true)
+  end
 
   context 'success' do
     context 'with valid application' do
