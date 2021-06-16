2c8752e8efd58476cda2f4be7ef8f37649e0aaa9
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-06-16 11:16:12 -0400

    Refs #DeterminationSubscriber, replace logger.debug with logger.info.

--- app/event_source/subscribers/determination_subscriber.rb
+++ app/event_source/subscribers/determination_subscriber.rb
@@ -7,41 +7,42 @@ module Subscribers
     include ::EventSource::Subscriber[amqp: 'magi_medicaid.mitc.eligibilities']
 
     subscribe(:on_magi_medicaid_mitc_eligibilities) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_magi_medicaid_mitc_eligibilities with #{delivery_info}"
+      logger.info "invoked on_magi_medicaid_mitc_eligibilities with #{delivery_info}"
       persist(response)
     end
 
-    subscribe(:on_determined_aptc_eligible) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_determined_aptc_eligible with #{delivery_info}"
-      persist(response)
-    end
+    # subscribe(:on_determined_aptc_eligible) do |delivery_info, _metadata, response|
+    #   logger.debug "invoked on_determined_aptc_eligible with #{delivery_info}"
+    #   persist(response)
+    # end
 
-    subscribe(:on_determined_medicaid_chip_eligible) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_determined_medicaid_chip_eligible with #{delivery_info}"
-      persist(response)
-    end
+    # subscribe(:on_determined_medicaid_chip_eligible) do |delivery_info, _metadata, response|
+    #   logger.debug "invoked on_determined_medicaid_chip_eligible with #{delivery_info}"
+    #   persist(response)
+    # end
 
-    subscribe(:on_determined_totally_ineligible) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_determined_totally_ineligible with #{delivery_info}"
-      persist(response)
-    end
+    # subscribe(:on_determined_totally_ineligible) do |delivery_info, _metadata, response|
+    #   logger.debug "invoked on_determined_totally_ineligible with #{delivery_info}"
+    #   persist(response)
+    # end
 
-    subscribe(:on_determined_magi_medicaid_eligible) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_determined_magi_medicaid_eligible with #{delivery_info}"
-      persist(response)
-    end
+    # subscribe(:on_determined_magi_medicaid_eligible) do |delivery_info, _metadata, response|
+    #   logger.debug "invoked on_determined_magi_medicaid_eligible with #{delivery_info}"
+    #   persist(response)
+    # end
 
-    subscribe(:on_determined_uqhp_eligible) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_determined_uqhp_eligible with #{delivery_info}"
-      persist(response)
-    end
+    # subscribe(:on_determined_uqhp_eligible) do |delivery_info, _metadata, response|
+    #   logger.debug "invoked on_determined_uqhp_eligible with #{delivery_info}"
+    #   persist(response)
+    # end
 
-    subscribe(:on_determined_mixed_determination) do |delivery_info, _metadata, response|
-      logger.debug "invoked on_determined_mixed_determination with #{delivery_info}"
-      persist(response)
-    end
+    # subscribe(:on_determined_mixed_determination) do |delivery_info, _metadata, response|
+    #   logger.debug "invoked on_determined_mixed_determination with #{delivery_info}"
+    #   persist(response)
+    # end
 
     def self.persist(response)
+      logger.info "inside persist of DeterminationSubscriber: #{response}"
       payload = JSON.parse(response, :symbolize_names => true)
       result = FinancialAssistance::Operations::Applications::MedicaidGateway::AddEligibilityDetermination.new.call(payload)
 
@@ -52,10 +53,10 @@ module Subscribers
                 end
 
       # TODO: log message
-      puts "enroll_determination_subscriber_message: #{message}"
+      logger.info "enroll_determination_subscriber_message: #{message}"
     rescue StandardError => e
       # TODO: log error message
-      puts "enroll_determination_subscriber_error: #{e.backtrace}"
+      logger.info "enroll_determination_subscriber_error: #{e.backtrace}"
     end
   end
 end
