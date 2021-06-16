a26bf7a8893e57a966ca97617be5c693073c0ce0
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-16 17:35:48 -0400

    update main_app gemfile.lock and remove stimulus_reflex.rb - no longer needed

--- config/initializers/stimulus_reflex.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# This file is added to resolve gemfile and rspec requirement, revisit or remove this initializer when no longer needed
-StimulusReflex.configure do |config|
-  config.on_failed_sanity_checks = :warn
-end
