42580fcefbe2dcb8f3d2ac86de9a52a3092cf984
Zane Sandin <zanesandin@gmail.com>
2021-06-17 16:43:04 -0400

    REFS maintenance - adds todo note.

--- app/models/concerns/golden_seed_worker_concern.rb
+++ app/models/concerns/golden_seed_worker_concern.rb
@@ -84,6 +84,7 @@ module GoldenSeedWorkerConcern
       unique_row_notes += ", Health Program 1: #{target_row_data[:person_attributes]['health_program_1']}, " if target_row_data[:person_attributes]['health_program_1'].present?
       unique_row_notes += "APTC Amount: #{target_row_data[:person_attributes]['aptc_amount']}, " if target_row_data[:person_attributes]['aptc_amount'].present?
       unique_row_notes += "CSR: #{target_row_data[:person_attributes]['csr']}" if target_row_data[:person_attributes]['csr'].present?
+      # TODO: For somoe reason case notes from the sheet QA gave me isn't being detected and idk why.
       unique_row_notes += (target_row_data[:person_attributes]['case_notes']).to_s if target_row_data[:person_attributes]['case_notes'].present?
       target_row.update_attributes(
         unique_row_identifier: target_row_data[:person_attributes]["case_name"],
