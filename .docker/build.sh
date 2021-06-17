3d776f9133928e5d5609ee9555de63f5386e827d
Matt Williams <m.a.dubya@gmail.com>
2021-06-16 21:39:35 -0400

    mitc env variables

--- .docker/build.sh
+++ .docker/build.sh
@@ -28,7 +28,13 @@ docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.0.1' \
              --build-arg ENROLL_DB_PORT="27017" \
              --build-arg ENROLL_DB_NAME="enroll_production" \
              --build-arg REDIS_HOST_ENROLL="localhost" \
-             --build-arg RABBITMQ_URL="amqp://guest:guest@host.docker.internal:5672" \
+             --build-arg RABBITMQ_URL="amqp://rabbitmq:5672" \
+	     --build-arg RABBITMQ_HOST="amqp://rabbitmq" \
+	     --build-arg RABBITMQ_PORT="5672" \
+	     --build-arg RABBITMQ_VHOST="event_source" \
+	     --build-arg MITC_HOST="http://mitc" \
+	     --build-arg MITC_PORT="3001" \
+	     --build-arg MITC_URL="http://mitc:3001" \
              --build-arg SECRET_KEY_BASE="c8d2b9b204fbac78081a88a2c29b28cfeb82e6ccd3664b3948b813463b5917b315dbbd3040e8dffcb5b68df427099db0ce03e59e2432dfe5d272923b00755b82" \
              -f .docker/production/Dockerfile --target app -t $2:$1 .
 docker push $2:$1
