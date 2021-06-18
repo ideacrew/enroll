e23e2b619a186bc67870050e95956f8334578325
Zane Sandin <zanesandin@gmail.com>
2021-06-18 10:34:20 -0400

    REFS maintenance - tweaks the seed worker spec.

--- spec/workers/seed_worker_spec.rb
+++ spec/workers/seed_worker_spec.rb
@@ -5,9 +5,9 @@ require 'rails_helper'
 describe SeedWorker, :dbclean => :after_each do
   describe "#perform" do
     let(:file_location) do
-      filename = "#{Rails.root}/ivl_testbed_scenarios_*.csv"
-      ivl_testbed_templates = Dir.glob(filename)
-      ivl_testbed_templates.first
+      "#{Rails.root}/ivl_testbed_scenarios_2021.csv"
+      # ivl_testbed_templates = Dir.glob(filename)
+      # ivl_testbed_templates.first
     end
     let(:user) { FactoryBot.create(:user) }
 
@@ -15,23 +15,21 @@ describe SeedWorker, :dbclean => :after_each do
       @seed = Seeds::Seed.new(
         user: user,
         filename: file_location, # Get filename
+        csv_template: "individual_market_seed",
         aasm_state: 'draft'
       )
       # TODO: need to figure out how to save the file
-      if file_location.present?
-        CSV.foreach(file_location, headers: true) do |row|
-          # To avoid nil values
-          row_data = row.to_h.reject { |key, _value| key.blank? }.transform_values { |v| v.blank? ? "" : v }.with_indifferent_access
-          @seed.rows.build(data: row_data)
-        end
-        @seed.save!
+      CSV.foreach(file_location, headers: true) do |row|
+        # To avoid nil values
+        row_data = row.to_h.reject { |key, _value| key.blank? }.transform_values { |v| v.blank? ? "" : v }.with_indifferent_access
+        @seed.rows.build(data: row_data)
       end
+      @seed.save!
     end
 
     it "should be able to seed the database and infer data like primary families/persons" do
-      return if file_location.blank?
       # To speed up only do a few of them
-      @seed.rows.to_a[6..@seed.rows.count].each(&:destroy)
+      @seed.rows.to_a[12..@seed.rows.count].each(&:destroy)
       SeedWorker.new.perform(@seed.id)
       expect(FinancialAssistance::Application.all.count).to be > 0 if EnrollRegistry.feature_enabled?(:financial_assistance)
       expect(Family.all.count).to be > 0
