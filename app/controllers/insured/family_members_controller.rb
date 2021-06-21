7dc0d43befde4d2a4099c65c6a56b404eeece0b4
Zane Sandin <zanesandin@gmail.com>
2021-06-21 13:10:17 -0400

    REFS maintenance - removes vestigal method that was refactored into model.

--- app/controllers/insured/family_members_controller.rb
+++ app/controllers/insured/family_members_controller.rb
@@ -251,28 +251,6 @@ class Insured::FamilyMembersController < ApplicationController
 
   private
 
-  def verify_unique_dependent
-    @dependent = ::Forms::FamilyMember.new(params[:dependent])
-    # Don't use SSN in case there is no SSN
-    potential_duplicate = @family&.family_members&.select do |family_member|
-      family_member&.first_name == @dependent.first_name &&
-        family_member&.last_name == @dependent.last_name &&
-        family_member&.dob == @dependent.dob
-    end
-    # Disabling rubocop because it seems like a guard clause changes behavior
-    # rubocop:disable Style/GuardClause
-    if potential_duplicate.present?
-      # Manage Family Page Redirect
-      alert_message = l10n(
-        'insured.family_members.duplicate_error_message',
-        action: "add",
-        contact_center_phone_number: EnrollRegistry[:enroll_app].settings(:contact_center_short_number).item
-      )
-      redirect_to insured_family_members_path, alert: alert_message
-    end
-    # rubocop:enable Style/GuardClause
-  end
-
   def dependent_person_params
     params.permit(:dependent => {})
   end
