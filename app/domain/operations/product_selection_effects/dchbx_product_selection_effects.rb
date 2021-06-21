812fc0259107a7cbb097b72e5127f7626d9b5606
saipraveen18 <saipravee.gudimetla@gmail.com>
2021-06-21 11:14:28 -0500

    proj200 ivl feature - fix latest sep issue

--- app/domain/operations/product_selection_effects/dchbx_product_selection_effects.rb
+++ app/domain/operations/product_selection_effects/dchbx_product_selection_effects.rb
@@ -57,7 +57,7 @@ module Operations
       end
 
       def renew_prior_py_ivl_enrollments(enrollment)
-        sep = enrollment.family.latest_ivl_sep
+        sep = enrollment.family.latest_created_sep
         return nil unless sep&.coverage_renewal_flag
         return nil if enrollment.is_shop?
         @benefit_coverage_periods = fetch_bcp_gt_enr_effective_year(enrollment)
