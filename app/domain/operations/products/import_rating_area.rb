08b07b7fa8e29e87bbde5e61d604d55088e69ad3
nisanth <nisanth.yaganti@gmail.com>
2021-06-22 12:47:49 -0400

    updating rating areas setting to county & fixing issue with county zip records

--- app/domain/operations/products/import_rating_area.rb
+++ app/domain/operations/products/import_rating_area.rb
@@ -87,8 +87,8 @@ module Operations
           rating_area = ::BenefitMarkets::Locations::RatingArea.where({active_year: year, exchange_provided_code: rating_area_id }).first
 
           if rating_area.present?
-            rating_area.county_zip_ids += location_ids.flatten
-            rating_area.county_zip_ids.uniq!
+            rating_area.county_zip_ids += location_ids
+            rating_area.county_zip_ids.flatten.uniq!
             rating_area.save!
           else
             ::BenefitMarkets::Locations::RatingArea.new(
