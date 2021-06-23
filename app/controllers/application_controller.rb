e3cb6474429ab956ab63a7496b0f7756865f0c66
Zane Sandin <zanesandin@gmail.com>
2021-06-23 09:20:37 -0400

    REFS manitenance - sets review variable puts output and fixes typo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,7 +97,7 @@ class ApplicationController < ActionController::Base
   private
 
   def nonprod_environment?
-    redirect_to root_path, :flash => { :error => "Unable to run seeds on prod envirornment." } unless ENV['ENROLL_REVIEW_ENVIRONMENT'] == 'true' || !Rails.env.production?
+    redirect_to root_path, :flash => { :error => "Unable to run seeds on prod environment." } unless (ENV['ENROLL_REVIEW_ENVIRONMENT'] == 'true' || !Rails.env.production?)
   end
 
     def strong_params
