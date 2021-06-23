9f21838abd226d5b9aab27d85cab231475f655db
Battula <vrbattula@outlook.com>
2021-06-23 12:42:49 -0400

    Updates DocumentMetaDataSubscriber to ack/nack response

--- app/event_source/subscribers/document_meta_data_subscriber.rb
+++ app/event_source/subscribers/document_meta_data_subscriber.rb
@@ -7,20 +7,23 @@ module Subscribers
     include ::EventSource::Subscriber[amqp: 'polypress.document_builder']
 
     subscribe(:on_document_created) do |delivery_info, _metadata, response|
-      logger.info "invoked on_document_created with delivery_info: #{delivery_info}, response: #{response}"
+      logger.info "Enroll: invoked on_document_created with delivery_info: #{delivery_info}, response: #{response}"
 
       payload = JSON.parse(response, :symbolize_names => true)
-      family = Family.where(hbx_assigned_id: payload[:resource_id]).first
+      family = Family.where(hbx_assigned_id: payload[:subjects][0][:id]).first
 
       result = Operations::Documents::Create.new.call(resource: family&.primary_person, document_params: payload, doc_identifier: payload[:id])
 
       if result.success?
-        logger.info "enroll_document_meta_data_subscriber_info Result: #{result.success} for payload: #{payload}"
+        ack(delivery_info.delivery_tag)
+        logger.info "Enroll: enroll_document_meta_data_subscriber_info Result: #{result.success} for payload: #{payload}"
       else
-        logger.error "enroll_document_meta_data_subscriber_error: #{result.failure} for payload: #{payload}"
+        nack(delivery_info.delivery_tag)
+        logger.error "Enroll: enroll_document_meta_data_subscriber_error: #{result.failure.errors} for payload: #{payload}"
       end
     rescue StandardError => e
-      logger.error "enroll_document_meta_data_subscriber_error: #{e.backtrace}"
+      nack(delivery_info.delivery_tag)
+      logger.error "Enroll: enroll_document_meta_data_subscriber_error: #{e.backtrace} for payload: #{payload}"
     end
   end
 end
