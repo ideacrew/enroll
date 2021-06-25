c4cde6bd97321be8ebefcbf3f1d4777ece58373a
Jay Reddy <jayanth.rordev@gmail.com>
2021-06-25 12:19:15 -0400

    ui updates

--- components/financial_assistance/app/models/financial_assistance/applicant.rb
+++ components/financial_assistance/app/models/financial_assistance/applicant.rb
@@ -736,6 +736,7 @@ module FinancialAssistance
       when :other_income
         if FinancialAssistanceRegistry[:unemployment_income].enabled?
           return false if has_unemployment_income.nil?
+          return incomes.unemployment.first.save if incomes.unemployment.count == 1 && has_unemployment_income
           return incomes.unemployment.present? if has_unemployment_income
         end
         return false if has_other_income.nil?
