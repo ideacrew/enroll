2e87a78ecc8cbefb9e3ed0481d16858e8cc41bab
Zane Sandin <zanesandin@gmail.com>
2021-06-29 12:47:27 -0400

    REFS maintenance - fixes rubocop stuff.

--- features/hbx_admin/step_definitions/ui_config_steps.rb
+++ features/hbx_admin/step_definitions/ui_config_steps.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 Given(/issuers feature is enabled?/) do
-    enable_feature(:issuers_tab)
+  enable_feature(:issuers_tab)
 end
 
 Given(/issuers feature is disabled?/) do
-    disable_feature(:issuers_tab)
+  disable_feature(:issuers_tab)
 end
 
 Then(/^they should see the Issuers tab$/) do
-    expect(page).to have_content("Issuers")
-  end
+  expect(page).to have_content("Issuers")
+end
 
-  Then(/^they should not see the Issuers tab$/) do
-    expect(page).to_not have_content("Issuers")
-  end
+Then(/^they should not see the Issuers tab$/) do
+  expect(page).to_not have_content("Issuers")
+end
