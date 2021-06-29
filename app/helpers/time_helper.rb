1792895ba8ab8a84099a7a56f5963160c7142c7c
saipraveen18 <saipraveen.gudimetla@gmail.com>
2021-06-29 10:03:37 -0400

    proj200 shop feature - refactor code

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -22,17 +22,6 @@ module TimeHelper
     TimeKeeper.date_of_record.between?(set_date_min_to_effective_on(enrollment), set_date_max_to_plan_end_of_year(enrollment)) ? TimeKeeper.date_of_record : set_date_max_to_plan_end_of_year(enrollment)
   end
 
-  # def sep_optional_date family, min_or_max, market_kind=nil
-  #   person = family.primary_applicant.person
-  #   has_dual_roles         = person.has_consumer_role? && person.has_active_employee_role?
-  #   has_only_employee_role = person.has_active_employee_role? && !person.has_consumer_role?
-  #
-  #   return nil unless has_only_employee_role || (has_dual_roles && ['shop', 'fehb'].include?(market_kind))
-  #
-  #   active_plan_years = person.active_employee_roles.map(&:employer_profile).map(&:benefit_applications).map(&:published_or_renewing_published).flatten
-  #   min_or_max == 'min' ? active_plan_years.map(&:start_on).min : active_plan_years.map(&:end_on).max
-  # end
-
   def sep_optional_date(family, min_or_max, market_kind = nil, effective_date = nil)
     person = family.primary_applicant.person
     return if effective_date.nil?
