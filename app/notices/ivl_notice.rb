854010145c56b1694bd1258c0e7f38cb4dbeecbf
Zane Sandin <zanesandin@gmail.com>
2021-06-29 13:50:37 -0400

    REFS maintenance -a dds exception for pdfs.

--- app/notices/ivl_notice.rb
+++ app/notices/ivl_notice.rb
@@ -125,11 +125,19 @@ class IvlNotice < Notice
   end
 
   def attach_blank_page(template_path = nil)
-    path = template_path.nil? ? notice_path : template_path
-    blank_page = Rails.root.join('lib/pdf_templates', 'blank.pdf')
-    page_count = Prawn::Document.new(:template => path).page_count
-    if (page_count % 2) == 1
-      join_pdfs_with_path([path, blank_page], path)
+    begin
+      path = template_path.nil? ? notice_path : template_path
+      blank_page = Rails.root.join('lib/pdf_templates', 'blank.pdf')
+      page_count = Prawn::Document.new(:template => path).page_count
+      if (page_count % 2) == 1
+        join_pdfs_with_path([path, blank_page], path)
+      end
+    rescue => e
+      message = "Error Attaching IVL Notice PDF with Prawn: #{e.message}; "
+      message = message + "PDF Path: #{path}, "
+      message = message + "stacktrace: #{e.backtrace}"
+      log(message, {:severity => "error"})
+      raise e
     end
   end
 
