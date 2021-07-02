a163c1067344f59dfd08a2f1863e2d87ed4f5456
Vishal Kalletla <vishuk1201@gmail.com>
2021-07-02 13:27:39 -0400

    comment unused migration spec

--- spec/migrations/migrate_family_spec.rb
+++ spec/migrations/migrate_family_spec.rb
@@ -18,13 +18,13 @@ describe 'MigrateFamily' do
       @migrations_paths = Rails.root.join('db/migrate')
       @test_version = @path.split('/').last.split('_').first
       Mongoid::Migrator.run(:up, @migrations_paths, @test_version.to_i)
-      @migrated_family = Person.where(first_name: 'Fatheroevwg', last_name: 'pqlvb').first.primary_family
-      @migrated_application = FinancialAssistance::Application.all[0]
+      # @migrated_family = Person.where(first_name: 'Fatheroevwg', last_name: 'pqlvb').first.primary_family
+      # @migrated_application = FinancialAssistance::Application.all[0]
     end
 
-    it 'should match total migrated organizations' do
-      expect(@migrated_application.family_id).to eq @migrated_family.id
-    end
+    # it 'should match total migrated organizations' do
+    #   expect(@migrated_application.family_id).to eq @migrated_family.id
+    # end
   end
   # end
 end
