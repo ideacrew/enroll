4ea3027a42729647cb13ec9a7e973ecb8ae85278
harshaellanki <h_vardhan@yahoo.com>
2021-07-04 12:16:17 -0400

    wip: ridp primary request initial commit

--- app/domain/operations/transformers/person_to/cv3_person.rb
+++ app/domain/operations/transformers/person_to/cv3_person.rb
@@ -10,12 +10,13 @@ module Operations
     module PersonTo
       # Person params to be transformed.
       class Cv3Person
-
+        # constructs cv3 payload for medicaid gateway.
         include Dry::Monads[:result, :do]
         include Acapi::Notifiers
         include Rails.application.routes.url_helpers
         require 'securerandom'
-
+        # @param [ Hash ] params Applicant Attributes
+        # @return [ BenefitMarkets::Entities::Applicant ] applicant Applicant
         def call(person)
           request_payload = yield construct_payload(person)
 
