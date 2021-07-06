fe65016851c79c320f130bb55c9b038156a8f113
Jacob Kagon <jacob@kagon.net>
2021-07-06 12:22:01 -0400

    finish adding is_enabled feature to the inbox tab

--- features/hbx_admin/step_definitions/ui_config_steps.rb
+++ features/hbx_admin/step_definitions/ui_config_steps.rb
@@ -15,3 +15,21 @@ end
 Then(/^they should not see the Issuers tab$/) do
   expect(page).to_not have_content("Issuers")
 end
+
+Given(/inbox feature is enabled?/) do
+  enable_feature(:inbox_tab)
+end
+
+Given(/inbox feature is disabled?/) do
+  disable_feature(:inbox_tab)
+end
+
+Then(/^they should see the Inbox tab$/) do
+  expect(page).to have_content("Inbox")
+end
+
+Then(/^they should not see the Inbox tab$/) do
+  expect(page).to_not have_content("Inbox")
+end
+
+
