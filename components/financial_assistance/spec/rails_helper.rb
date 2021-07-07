2e1869b5f836d86a29c764073daff26175b7884c
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-07-07 14:25:14 -0400

    model_event back to model in rails_helper

--- components/financial_assistance/spec/rails_helper.rb
+++ components/financial_assistance/spec/rails_helper.rb
@@ -73,7 +73,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 #  config.include ModelMatcherHelpers, :type => :model
-  config.include Mongoid::Matchers, type: :model_event
+  config.include Mongoid::Matchers, type: :model
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.include Devise::Test::IntegrationHelpers, :type => :request
   config.include Devise::Test::ControllerHelpers, :type => :view
