f24ba43a9551109372f488af4033d999bc18eeeb
Dan Thomas <dan@ideacrew.com>
2021-07-07 10:20:13 -0400

    Add entity, contract and operation to support RIDP response polling

---
+++ app/domain/operations/find_ridp_eligibility_response.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Operations
+  # Checks database for RIDP eligibility responses
+  class FindRidpEligibilityResponse
+    send(:include, Dry::Monads[:result, :do])
+    send(:include, Dry::Monads[:try])
+
+    # @param [String] member_id
+    def call(params)
+      values = yield validate(params)
+      eligibility_result = yield find_eligibility_result(values)
+      ridp_response = yield build_ridp_response(eligibility_result)
+
+      Success(ridp_response)
+    end
+
+    private
+
+    def validate(params)
+      Validators::RidpEligibilityResponseContract.new.call(params)
+    end
+
+    def find_eligibility_response(values)
+      response =
+        RidpEligibilityResponseModel.where(
+          '$and' => [
+            { :primary_member_hbx_id.eq => values[:primary_member_hbx_id] },
+            { :deleted_at.eq => nil }
+          ]
+        )
+
+      if response.present?
+        response.write_attribute(:deleted_at, DateTime.now)
+        Success(response)
+      else
+        unless response.present?
+          Failure[:ridp_eligibility_response_not_found, values: values]
+        end
+      end
+    end
+
+    def build_ridp_response(eligibility_result)
+      entity = eligibility_result.to_serializeable_hash
+      entity ? Success(entity) : Failure(entity)
+    end
+  end
+end
