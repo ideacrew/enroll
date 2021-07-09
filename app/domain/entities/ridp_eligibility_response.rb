b2f8b4e4bedf7b1e71745eff5bd5571a0b1a764b
harshaellanki <h_vardhan@yahoo.com>
2021-07-09 08:46:35 -0400

    typos and rubocop fixes

--- app/domain/entities/ridp_eligibility_response.rb
+++ app/domain/entities/ridp_eligibility_response.rb
@@ -2,14 +2,17 @@
 
 module Entities
   class RidpEligibilityResponse < Dry::Struct
-    attribute :primary_member_hbx_id, Types::String.meta(omittable: true)
+
+    attribute :primary_member_hbx_id, Types::String.meta(omittable: false)
     attribute :event_kind, Types::RidpEventKinds.meta(omittable: true)
+
     attribute :ridp_eligibility do
-      attribute :delivery_info, Types::String.optional
-      attribute :metadata, Types::String.optional
-      attribute :event, Types::String.optional
+      attribute :delivery_info, Types::String.optional.meta(omittable: true)
+      attribute :metadata, Types::String.optional.meta(omittable: true)
+      attribute :event, Types::String.optional.meta(omittable: true)
     end
-    attribute :created_at, Types::DateTime.meta(omittable: true)
-    attribute :deleted_at, Types::DateTime.optional
+
+    attribute :created_at, Types::DateTime.optional.meta(omittable: true)
+    attribute :deleted_at, Types::DateTime.optional.meta(omittable: true)
   end
 end
