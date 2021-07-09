b522d60fe09fdd2af509deb7b07d9bb8e4acf5af
Zane Sandin <zanesandin@gmail.com>
2021-07-09 12:52:58 -0400

    REFS maintenance - makes income omittable.

--- components/financial_assistance/app/domain/financial_assistance/entities/applicant.rb
+++ components/financial_assistance/app/domain/financial_assistance/entities/applicant.rb
@@ -57,7 +57,7 @@ module FinancialAssistance
       attribute :addresses, Types::Array.of(FinancialAssistance::Entities::Address)
       attribute :emails, Types::Array.of(FinancialAssistance::Entities::Email)
       attribute :phones, Types::Array.of(FinancialAssistance::Entities::Phone)
-      attribute :incomes, Types::Array.of(FinancialAssistance::Entities::Income)
+      attribute :incomes, Types::Array.of(FinancialAssistance::Entities::Income).meta(omittable: true)
     end
   end
 end
