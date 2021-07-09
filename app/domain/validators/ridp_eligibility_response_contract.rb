3505f89e22ffeb3ad40137755ece0ec924120127
harshaellanki <h_vardhan@yahoo.com>
2021-07-09 08:46:35 -0400

    typos and rubocop fixes

--- app/domain/validators/ridp_eligibility_response_contract.rb
+++ app/domain/validators/ridp_eligibility_response_contract.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 module Validators
+  # Ridp eligibility response validator
   class RidpEligibilitiyResponseContract < Dry::Validation::Contract
     params do
       required(:primary_member_hbx_id).filled(:string)
       optional(:event_kind).maybe(Types::RidpEventKinds)
-      optional(:ridp_eligibility).hash do
+
+      optional(:ridp_eligibility).schema do
         optional(:delivery_info).maybe(:string)
         optional(:metadata).maybe(:string)
         optional(:event).maybe(:string)
       end
+
       optional(:created_at).maybe(:date_time)
       optional(:deleted_at).maybe(:date_time)
     end
