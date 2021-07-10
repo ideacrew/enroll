049f41b513d37b5e6336bc3f18339932bace9912
Battula <vrbattula@outlook.com>
2021-07-09 21:26:42 -0400

    Fixes issues with SEP data in the payload

--- app/domain/operations/notices/ivl_enr_notice_trigger.rb
+++ app/domain/operations/notices/ivl_enr_notice_trigger.rb
@@ -77,7 +77,7 @@ module Operations
           product = enr.product
           issuer = product.issuer_profile
           consumer_role = enr.consumer_role
-          {
+          enrollment_hash = {
             effective_on: enr.effective_on,
             aasm_state: enr.aasm_state,
             market_place_kind: enr.kind,
@@ -85,15 +85,75 @@ module Operations
             product_kind: enr.coverage_kind,
             total_premium: enr.total_premium,
             hbx_enrollment_members: enrollment_member_hash(enr),
-            product_reference: {hios_id: product.hios_id, name: product.title, active_year: product.active_year, is_dental_only: product.dental?, metal_level: product.metal_level, benefit_market_kind: product.benefit_market_kind.to_s,
-                                csr_variant_id: product.csr_variant_id, is_csr: product.is_csr?, family_deductible: product.family_deductible,
-                                individual_deductible: product.deductible, product_kind: product.product_kind.to_s, issuer_profile_reference: {hbx_id: issuer.hbx_id, name: issuer.legal_name, abbrev: issuer.abbrev}},
-            issuer_profile_reference: {hbx_id: issuer.hbx_id, name: issuer.legal_name, abbrev: issuer.abbrev, phone: issuer.office_locations.where(is_primary: true).first.phone.full_phone_number},
-            consumer_role_reference: {is_active: consumer_role.is_active, is_applying_coverage: consumer_role.is_applying_coverage, is_applicant: consumer_role.is_applicant, is_state_resident: consumer_role.is_state_resident,
-                                      lawful_presence_determination: {}, citizen_status: consumer_role.citizen_status},
+            product_reference: product_reference(product, issuer),
+            issuer_profile_reference: issuer_profile_reference(issuer),
+            consumer_role_reference: consumer_role_reference(consumer_role),
             is_receiving_assistance: (enr.applied_aptc_amount > 0 || (product.is_csr? ? true : false))
           }
+          enrollment_hash.merge!(special_enrollment_period_reference: special_enrollment_period_reference(enr)) if enr.is_special_enrollment?
+          enrollment_hash
         end
+      end
+
+      def qualifying_life_event_kind_reference(qle)
+        qle_hash = {
+          start_on: qle.start_on,
+          title: qle.title,
+          reason: qle.reason,
+          market_kind: qle.market_kind
+        }
+        qle_hash.merge!(end_on: qle.end_on) if qle.end_on
+        qle_hash
+      end
+
+      def special_enrollment_period_reference(enrollment)
+        sep = enrollment.family.latest_active_sep
+        qle = sep.qualifying_life_event_kind
+        {
+          qualifying_life_event_kind_reference: qualifying_life_event_kind_reference(qle),
+          qle_on: sep.qle_on,
+          start_on: sep.start_on,
+          end_on: sep.end_on,
+          effective_on: sep.effective_on,
+          submitted_at: sep.submitted_at
+        }
+      end
+
+      def consumer_role_reference(consumer_role)
+        {
+          is_active: consumer_role.is_active,
+          is_applying_coverage: consumer_role.is_applying_coverage,
+          is_applicant: consumer_role.is_applicant,
+          is_state_resident: consumer_role.is_state_resident || false,
+          lawful_presence_determination: {},
+          citizen_status: consumer_role.citizen_status
+        }
+      end
+
+      def issuer_profile_reference(issuer)
+        {
+          hbx_id: issuer.hbx_id,
+          name: issuer.legal_name,
+          abbrev: issuer.abbrev,
+          phone: issuer.office_locations.where(is_primary: true).first&.phone&.full_phone_number
+        }
+      end
+
+      def product_reference(product, issuer)
+        {
+          hios_id: product.hios_id,
+          name: product.title,
+          active_year: product.active_year,
+          is_dental_only: product.dental?,
+          metal_level: product.metal_level,
+          benefit_market_kind: product.benefit_market_kind.to_s,
+          csr_variant_id: product.csr_variant_id,
+          is_csr: product.is_csr?,
+          family_deductible: product.family_deductible,
+          individual_deductible: product.deductible,
+          product_kind: product.product_kind.to_s,
+          issuer_profile_reference: { hbx_id: issuer.hbx_id, name: issuer.legal_name, abbrev: issuer.abbrev }
+        }
       end
 
       def enrollment_member_hash(enrollment)
