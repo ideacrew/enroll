7095cac14b0a8a9d4f60442ea2eca51eca1ed615
utkarsh7989 <utkarsh7989@gmail.com>
2021-07-11 16:25:34 -0400

    greys out home address on selecting homeless

--- components/financial_assistance/app/assets/javascripts/financial_assistance/applicant.js
+++ components/financial_assistance/app/assets/javascripts/financial_assistance/applicant.js
@@ -12,3 +12,31 @@ $(document).on('change', '#applicant_same_with_primary', function(){
     };
   }
 });
+
+$(document).on('change', "#applicant_is_homeless", function(){
+// $('#dependent_is_homeless').change(function() {
+  if ($(this).prop('checked')){
+    $('span:contains("Add Mailing Address")').text('Remove Mailing Address');
+    $('.row-form-wrapper.mailing-div').show();
+    $("#applicant_addresses_attributes_1_zip, #applicant_addresses_attributes_1_address_1, #applicant_addresses_attributes_1_city").each(function() {
+      $(this).prop('required', true);
+    })
+    $("#applicant_addresses_attributes_0_zip, #applicant_addresses_attributes_0_address_1, #applicant_addresses_attributes_0_address_2, #applicant_addresses_attributes_0_city, #applicant_addresses_attributes_0_county").each(function() {
+      $(this).attr('disabled', true);
+    })
+    $("#address_info .home-div .selectric-wrapper").hide();
+  } else {
+    $('span:contains("Remove Mailing Address")').text('Add Mailing Address');
+    $('.mailing-div').hide();
+    $(".mailing-div input[type='text']").val("");
+    $(".mailing-div #state_id_mailing").val("")
+    $('.mailing-div .label-floatlabel').hide();
+    $("#applicant_addresses_attributes_1_zip, #applicant_addresses_attributes_1_address_1, #applicant_addresses_attributes_1_city").each(function() {
+      $(this).prop('required', false);
+    })
+    $("#applicant_addresses_attributes_0_zip, #applicant_addresses_attributes_0_address_1, #applicant_addresses_attributes_0_address_2, #applicant_addresses_attributes_0_city, #applicant_addresses_attributes_0_county").each(function() {
+      $(this).attr('disabled', false);
+    })
+    $("#address_info .home-div .selectric-wrapper").show();
+  }
+});
