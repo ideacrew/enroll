fb544bc25dce2505b859775bd54e1dd73578a000
harshaellanki <h_vardhan@yahoo.com>
2021-07-12 13:38:17 -0400

    added missing type

--- app/domain/entities/types.rb
+++ app/domain/entities/types.rb
@@ -4,5 +4,7 @@ module Entities
   # Contains base types and type helpers for type constraints.
   module Types
     include Dry.Types()
+
+    RidpEventKinds = Types::Coercible::String.enum('primary', 'secondary')
   end
 end
