8b189b7ef8b793b262e0426d77dec1cc027e0a7a
harshaellanki <h_vardhan@yahoo.com>
2021-07-13 16:56:51 -0400

    fixed spec and added extra logger

--- app/domain/validators/ridp_eligibility_response_contract.rb
+++ app/domain/validators/ridp_eligibility_response_contract.rb
@@ -5,11 +5,11 @@ module Validators
   class RidpEligibilityResponseContract < ::Dry::Validation::Contract
     params do
       required(:primary_member_hbx_id).filled(:string)
-      optional(:event_kind).maybe(Types::RidpEventKinds)
+      required(:event_kind).maybe(Types::RidpEventKinds)
 
       optional(:ridp_eligibility).schema do
-        optional(:delivery_info).maybe(:string)
-        optional(:metadata).maybe(:string)
+        optional(:delivery_info).maybe(:hash)
+        optional(:metadata).maybe(:hash)
         optional(:event).maybe(:hash)
       end
 
--- spec/domain/validators/ridp_eligibility_response_contract_spec.rb
+++ spec/domain/validators/ridp_eligibility_response_contract_spec.rb
@@ -5,23 +5,22 @@ require 'rails_helper'
 #spec for RidpEligibilityResponseContract
 module Validators
   RSpec.describe Validators::RidpEligibilityResponseContract do
-    let(:primary_member_hhx_id) { '2345667789' }
+    let(:primary_member_hbx_id) { '2345667789' }
     let(:event_kind) { 'primary' }
-    let(:delivery_info) { '2345667789' }
-    let(:metadata) { '2345667789' }
-    let(:event) { '2345667789' }
-    let(:created_at) { Time.now }
+    let(:delivery_info) { {delivery_info: '2345667789'} }
+    let(:metadata) { {metadata: '2345667789'} }
+    let(:event) { {event: '2345667789'} }
+    let(:created_at) { DateTime.now }
     let(:deleted_at) { nil }
 
     let(:ridp_eligibility) do
       { delivery_info: delivery_info, metadata: metadata, event: event }
     end
 
-    let(:required_params) { { primary_member_hbx_id: primary_member_hbx_id } }
+    let(:required_params) { { event_kind: event_kind, primary_member_hbx_id: primary_member_hbx_id } }
 
     let(:optional_params) do
       {
-        event_kind: event_kind,
         ridp_eligibility: ridp_eligibility,
         created_at: created_at,
         deleted_at: deleted_at
@@ -32,27 +31,34 @@ module Validators
 
     context 'Invalid parameters' do
       context 'No required or optional parameters' do
-        result = describe_class.new.call({})
-        expect(result.failure?).to be_truthy
+        it 'should fail' do
+          result = described_class.new.call({})
+          expect(result.failure?).to be_truthy
+        end
       end
 
-      contecxt 'Optional parameters only' do
-        result = describe_class.new.call(optional_params)
-        expect(result.failure?).to be_truthy
+      context 'Optional parameters only' do
+        it 'should fail' do
+          result = described_class.new.call(optional_params)
+          expect(result.failure?).to be_truthy
+        end
       end
     end
+
     context 'Valid parameters' do
       context 'Required params only' do
-        result = describe_class.new.call(required_params)
-        expect(result.success?).to be_truthy
-        expect(result.value.to_h).to eq required_params
+        it 'should validate' do
+          result = described_class.new.call(required_params)
+          expect(result.success?).to be_truthy
+          expect(result.to_h).to eq required_params
+        end
       end
 
       context 'Required and optional (All) params' do
         it 'should validate' do
-          result = describe_class.new.call(all_params)
+          result = described_class.new.call(all_params)
           expect(result.success?).to be_truthy
-          expect(result.value.to_h).to eq all_params
+          expect(result.to_h).to eq all_params
         end
       end
     end
