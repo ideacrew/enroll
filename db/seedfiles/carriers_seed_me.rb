2522c02d27a8edc2ec4281bb3e47d3c23b300636
vineel <sonnathivinil@gmail.com>
2021-07-16 11:40:18 -0400

    For plan crosswalk

--- db/seedfiles/carriers_seed_me.rb
+++ db/seedfiles/carriers_seed_me.rb
@@ -8,19 +8,19 @@ hbx_office = OfficeLocation.new(
   )
 
 org = Organization.new(fein: "453416923", legal_name: "Community Health Options", office_locations: [hbx_office])
-cp = org.create_carrier_profile(id: "53e67210eb899a4603000045", abbrev: "CHO", hbx_carrier_id: 30_001, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['33653'])
+cp = org.create_carrier_profile(id: "60491e4834ccb831ee000009", abbrev: "CHO", hbx_carrier_id: 30_001, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['33653'])
 
 org = Organization.new(fein: "042452600", legal_name: "Harvard Pilgrim Health Care", office_locations: [hbx_office])
-cp = org.create_carrier_profile(id: "53e67210eb899a4603000029", abbrev: "HPHC", hbx_carrier_id: 30_002, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['96667'])
+cp = org.create_carrier_profile(id: "60491e4834ccb831ee000007", abbrev: "HPHC", hbx_carrier_id: 30_002, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['96667'])
 
 org = Organization.new(fein: "470397286", legal_name: "Renaissance Dental", office_locations: [hbx_office])
-cp = org.create_carrier_profile(id: "53e67210eb899a4603000061", abbrev: "RNSD", hbx_carrier_id: 30_003, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['76302'])
+cp = org.create_carrier_profile(id: "60491e4834ccb831ee000005", abbrev: "RNSD", hbx_carrier_id: 30_003, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['76302'])
 
 org = Organization.new(fein: "311705652", legal_name: "Anthem Blue Cross and Blue Shield", office_locations: [hbx_office])
-cp = org.create_carrier_profile(id: "53e67210eb899a4603000037", abbrev: "ANTHM", hbx_carrier_id: 30_004, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['48396'])
+cp = org.create_carrier_profile(id: "60491e4834ccb831ee000003", abbrev: "ANTHM", hbx_carrier_id: 30_004, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['48396'])
 
 org = Organization.new(fein: "010286541", legal_name: "Northeast Delta Dental", office_locations: [hbx_office])
-cp = org.create_carrier_profile(id: "53e67210eb899a4603000073", abbrev: "NEDD", hbx_carrier_id: 30_005, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['50165'])
+cp = org.create_carrier_profile(id: "60491e4834ccb831ee000001", abbrev: "NEDD", hbx_carrier_id: 30_005, ivl_health: true, ivl_dental: true, shop_health: false, shop_dental: false, issuer_hios_ids: ['50165'])
 
 puts "::: Generated ME Carriers :::"
 puts "*"*80
