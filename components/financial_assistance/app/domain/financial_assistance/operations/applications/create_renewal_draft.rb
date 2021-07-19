c4b5f8847c2edd35dd6ff719d7a4cebc115a1072
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-07-19 16:20:39 -0400

    Refs #FaaRenewals: wip - operation CreateRenewalDraft with tests

---
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/create_renewal_draft.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module FinancialAssistance
+  module Operations
+    module Applications
+      # This Operation creates a new renewal_draft application from a given FinancialAssistance::Application,
+      # if the given application is eligible.
+      # Operation receives a persisted FinancialAssistance::Application object
+      class CreateRenewalDraft
+        include Dry::Monads[:result, :do]
+
+        # @param [Hash] opts The options to generate renewal_draft application
+        # @option opts [::FinancialAssistance::Application] :application
+        # @return [Dry::Monads::Result]
+        def call(application)
+          # verify if application is in determined aasm state.
+          determined_application = yield check_application_state(application)
+          renewal_draft_app      = yield create_renewal_draft_application(determined_application)
+
+          Success(renewal_draft_app)
+        end
+
+        private
+
+        def check_application_state(application)
+          return Failure("Given input: #{application} is not a valid FinancialAssistance::Application.") unless application.is_a?(::FinancialAssistance::Application)
+          return Failure("Cannot generate renewal_draft for given application with aasm_state #{application.aasm_state}. Application must be in determined state.") unless application.determined?
+          Success(application)
+        end
+
+        def create_renewal_draft_application(application)
+          service = ::FinancialAssistance::Services::ApplicationService.new(application_id: application.id)
+          draft_app = service.copy!
+          # Using update attributes instead of calling aasm event becuase 'renewal_draft' is the first state for a renewal application.
+          draft_app.update_attributes!(aasm_state: 'renewal_draft')
+          Success(draft_app)
+        rescue StandardError => e
+          # Log error with backtrace
+          Failure("Could not generate renewal draft for given application with hbx_id: #{application.hbx_id}, error: #{e.message}")
+        end
+      end
+    end
+  end
+end
---
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/create_renewal_draft_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ::FinancialAssistance::Operations::Applications::CreateRenewalDraft, dbclean: :after_each do
+  before :all do
+    DatabaseCleaner.clean
+  end
+
+  let!(:application) do
+    FactoryBot.create(:financial_assistance_application, hbx_id: '111000222')
+  end
+
+  let!(:applicant) do
+    FactoryBot.create(:financial_assistance_applicant,
+                      person_hbx_id: '100095',
+                      is_primary_applicant: true,
+                      first_name: 'Gerald',
+                      last_name: 'Rivers',
+                      dob: Date.new(Date.today.year - 22, Date.today.month, Date.today.day),
+                      application: application)
+  end
+
+  context 'success' do
+    before do
+      @result = subject.call(application)
+      @renewal_draft_app = @result.success
+    end
+
+    it 'should return success' do
+      expect(@result).to be_success
+    end
+
+    it 'should return application' do
+      expect(@renewal_draft_app).to be_a(::FinancialAssistance::Application)
+    end
+
+    it 'should return application with renewal_draft' do
+      expect(@renewal_draft_app.renewal_draft?).to be_truthy
+    end
+  end
+
+  context 'failure' do
+    context 'invalid input data' do
+      before do
+        @result = subject.call('test')
+      end
+
+      it 'should return failure with error message' do
+        expect(@result.failure).to eq("Given input: test is not a valid FinancialAssistance::Application.")
+      end
+    end
+
+    context 'for an application which is not in determined state' do
+      shared_examples_for 'non determined state application' do |app_state|
+        before do
+          application.update_attributes!(aasm_state: app_state)
+          @result = subject.call(application)
+        end
+
+        it 'should return failure' do
+          expect(@result).to be_failure
+        end
+
+        it "should return failure with error messasge for application with aasm_state: #{app_state}" do
+          expect(@result.failure).to eq("Cannot generate renewal_draft for given application with aasm_state #{application.aasm_state}. Application must be in determined state.")
+        end
+      end
+
+      context 'failure because of application aasm_state' do
+        it_behaves_like 'non determined state application', 'draft'
+        it_behaves_like 'non determined state application', 'renewal_draft'
+        it_behaves_like 'non determined state application', 'submitted'
+        it_behaves_like 'non determined state application', 'determination_response_error'
+      end
+    end
+  end
+end
