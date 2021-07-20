72090b4cdd5173e6078e1397b19e2fff326fa407
Vishal Kalletla <vishuk1201@gmail.com>
2021-07-20 11:27:15 -0400

    ref# migrations: fix no_ssn and race for person

--- db/migrate/20210512153640_migrate_family.rb
+++ db/migrate/20210512153640_migrate_family.rb
@@ -254,11 +254,13 @@ class MigrateFamily < Mongoid::Migration
         last_name: person_hash['person_name']['last_name'],
         full_name: person_hash['person_name']['full_name'],
         ssn: person_hash['person_demographics']['ssn'],
-        no_ssn: "0", # person_hash['person_demographics']['no_ssn'], update in aca entities contracts to receive as string
+        no_ssn: person_hash['person_demographics']['no_ssn'], #update in aca entities contracts to receive as string
         gender: person_hash['person_demographics']['gender'],
         dob: person_hash['person_demographics']['dob'],
         date_of_death: person_hash['person_demographics']['date_of_death'],
         dob_check: person_hash['person_demographics']['dob_check'],
+        race: person_hash['race'],
+        ethnicity: [person_hash['race']],
         is_incarcerated: person_hash['person_demographics']['is_incarcerated'],
         tribal_id: person_hash['person_demographics']['tribal_id'],
         language_code: person_hash['person_demographics']['language_code'],
