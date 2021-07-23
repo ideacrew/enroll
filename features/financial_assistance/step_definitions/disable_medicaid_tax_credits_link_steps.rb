ee63cea226dafb6aa5da5d0c70827ce35bce1490
Jacob Kagon <jacob@kagon.net>
2021-07-23 17:22:00 -0400

    done with medicaid_tax_credits_link feature just need more specs

---
+++ features/financial_assistance/step_definitions/disable_medicaid_tax_credits_link_steps.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+Given(/medicaid tax credits link feature is enabled?/) do
+  enable_feature(:medicaid_tax_credits_link)
+end
+
+Given(/medicaid tax credits link feature is disabled?/) do
+  disable_feature(:medicaid_tax_credits_link)
+end
+
+Then(/^they should see the Medicaid and Tax Credits Link tile$/) do
+  expect(page).to have_content("Medicaid & Tax Credits")
+end
+
+Then(/^they should not see the Medicaid and Tax Credits Link tile$/) do
+  expect(page).to_not have_content("Medicaid & Tax Credits")
+end
+
+
+
+
+
+
