b6cd6638771bb0a06872b4d829478bfd3236f628
Jacob Kagon <jacob@kagon.net>
2021-07-26 11:46:55 -0400

    done with third spec

--- features/financial_assistance/step_definitions/disable_medicaid_tax_credits_link_steps.rb
+++ features/financial_assistance/step_definitions/disable_medicaid_tax_credits_link_steps.rb
@@ -8,6 +8,14 @@ Given(/medicaid tax credits link feature is disabled?/) do
   disable_feature(:medicaid_tax_credits_link)
 end
 
+And(/^the consumer navigates to the (.*?) page$/) do |page|
+  expect(page).to have_content(page)
+end
+
+And(/^the consumer clicks the (.*?) link$/) do |link|
+  expect(link).to have_content(link)
+end
+
 Then(/^they should see the Medicaid and Tax Credits Link tile$/) do
   expect(page).to have_content("Medicaid & Tax Credits")
 end
@@ -15,6 +23,9 @@ end
 Then(/^they should not see the Medicaid and Tax Credits Link tile$/) do
   expect(page).to_not have_content("Medicaid & Tax Credits")
 end
+
+
+
 
 
 
