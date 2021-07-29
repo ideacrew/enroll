410e5974691b88b12ced05a8c29db751b7cdad96
Zane Sandin <zanesandin@gmail.com>
2021-07-29 13:48:33 -0400

    REFS maintenance - fixes flaky pay now steps.

--- features/step_definitions/pay_now_steps.rb
+++ features/step_definitions/pay_now_steps.rb
@@ -180,8 +180,8 @@ And(/^creates a consumer with SEP$/) do
   fill_in "person_addresses_attributes_0_address_2", with: "apt 1005"
   # May have to refactor this for the specific state
   fill_in "person_addresses_attributes_0_city", with: "Washington"
-  find(:xpath, "//span[@class='label'][contains(., 'SELECT STATE')]", wait: 10).click
-  find(:xpath, "//li[contains(., '#{Settings.contact_center.state}')]").click
+  find_all(IvlPersonalInformation.select_state_dropdown).first.click
+  find(:xpath, "//li[contains(., '#{EnrollRegistry[:enroll_app].setting(:state_abbreviation).item}')]").click
   fill_in "person[addresses_attributes][0][zip]", with: Settings.contact_center.zip_code
   find('.btn', text: 'CONTINUE', wait: 10).click
   visit '/insured/consumer_role/upload_ridp_document'
