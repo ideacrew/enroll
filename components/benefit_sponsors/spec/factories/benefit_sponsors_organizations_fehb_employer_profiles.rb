aa58665e30c7601fa0d3ddd403f7d80af92b4c3a
Zane Sandin <zanesandin@gmail.com>
2021-07-29 11:37:59 -0400

    REFS maintenance - fixes a spec.

--- components/benefit_sponsors/spec/factories/benefit_sponsors_organizations_fehb_employer_profiles.rb
+++ components/benefit_sponsors/spec/factories/benefit_sponsors_organizations_fehb_employer_profiles.rb
@@ -12,7 +12,7 @@ FactoryBot.define do
     end
 
     after(:build) do |profile, evaluator|
-      profile.office_locations << build_list(:benefit_sponsors_locations_office_location, evaluator.office_locations_count, :with_massachusetts_address)
+      profile.office_locations << build_list(:benefit_sponsors_locations_office_location, evaluator.office_locations_count, "with_#{EnrollRegistry[:enroll_app].setting(:site_key).item.downcase}_address".to_sym)
     end
 
     trait :with_benefit_sponsorship do
