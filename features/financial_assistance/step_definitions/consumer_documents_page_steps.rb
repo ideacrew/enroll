ba2d45b54b3c15bc0cd77756a6d77ab7446d1374
Jacob Kagon <jacob@kagon.net>
2021-08-02 17:42:00 -0400

    seventh rebase after merging

---
+++ features/financial_assistance/step_definitions/consumer_documents_page_steps.rb
@@ -0,0 +1,30 @@
+When(/^a consumer visits the home page$/) do
+    visit "/families/home?tab=home"
+  end
+
+  And(/^the consumer clicks the Documents Link$/) do
+    visit "/insured/families/verification?tab=verification"
+  end
+
+  Given(/medicaid tax credits link is enabled?/) do
+    enable_feature(:medicaid_tax_credits_link)
+  end
+  
+  Given(/medicaid tax credits link is disabled?/) do
+    disable_feature(:medicaid_tax_credits_link)
+  end
+  
+  Then(/^they should see the medicaid and tax credits link tile$/) do
+    expect(page).to have_content("Medicaid & Tax Credits")
+  end
+  
+  Then(/^they should not see the medicaid and tax credits link tile$/) do
+    expect(page).to_not have_content("Medicaid & Tax Credits")
+  end
+  
+  
+  
+  
+  
+  
+  
