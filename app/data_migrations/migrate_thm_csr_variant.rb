8e768bf3c570317b4369f3ced3c076e4a8e03e44
Jay Reddy <jayanth.rordev@gmail.com>
2021-08-06 13:41:12 -0400

    updated rake task

--- app/data_migrations/migrate_thm_csr_variant.rb
+++ app/data_migrations/migrate_thm_csr_variant.rb
@@ -10,34 +10,52 @@ class MigrateThmCsrVariant < MongoidMigrationTask
   def migrate
     @logger = Logger.new("#{Rails.root}/log/migrate_thm_csr_variant.log") unless Rails.env.test?
     Rails.logger.info "Script Start - #{TimeKeeper.datetime_of_record}" unless Rails.env.test?
-    families = Family.all
-    batch_size = 500
-    offset = 0
-    while offset <= families.count
-      families.offset(offset).limit(batch_size).no_timeout.each do |family|
-        if family.nil? || family.active_household.nil? || family.active_household.latest_active_tax_household.nil?
-          puts "No primary_family or active househod or latest_active_household exists for person with the given hbx_id #{family.id}" unless Rails.env.test?
-          next
-        end
-        active_household = family.active_household
-        latest_tax_household = active_household.latest_active_tax_household_with_year(TimeKeeper.date_of_record.year)
-        if latest_tax_household.present? && latest_tax_household.latest_eligibility_determination.present?
-          ed = latest_tax_household.latest_eligibility_determination
-          Rails.logger.info "No Eligibility determinaton found for the family of - #{family.primary_applicant.person.full_name}" if ed.nil?
-          csr_percent = ed.csr_percent_as_integer
-          thhm = latest_tax_household.tax_household_members.where(is_ia_eligible: true)
-          Rails.logger.info "Tax household members not present for given tax household" if thhm.nil?
-          thhm&.each do |thm|
-            thm.update(csr_percent_as_integer: csr_percent)
-            Rails.logger.info "Updated csr variant for family of - #{family.primary_applicant.person.full_name}" unless Rails.env.test?
+
+    field_names = %w[Person_HBX_Id Family_ID Tax_Household_ID TH_Member_ID CSR_Percent_as_Integer]
+
+    report_file_name = "#{Rails.root}/updated_csr_list_report_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv"
+    logger_file_name = "#{Rails.root}/existing_csr_list_before_update_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv"
+
+    CSV.open(logger_file_name, 'w', force_quotes: true) do |logger_csv|
+      logger_csv << field_names
+      CSV.open(report_file_name, 'w', force_quotes: true) do |report_csv|
+        report_csv << field_names
+        families = Family.all_tax_households
+        batch_size = 500
+        offset = 0
+        while offset <= families.count
+          families.offset(offset).limit(batch_size).no_timeout.each do |family|
+            if family.nil? || family.active_household.nil? || family.active_household.tax_households.nil?
+              puts "No primary_family or active househod or latest_active_household exists for person with the given hbx_id #{family.id}" unless Rails.env.test?
+              next
+            end
+
+            family.active_household.tax_households.each do |tax_household|
+              ed = tax_household.latest_eligibility_determination # check two determinations created at determined
+              Rails.logger.info "No Eligibility determinaton found for the family of - #{family.primary_applicant.person.full_name}" if ed.nil?
+              csr_percent = ed.csr_percent_as_integer
+
+              thhms = tax_household.tax_household_members.where(is_ia_eligible: true)
+              Rails.logger.info "Tax household members not present for given tax household of - #{family.primary_applicant.person.full_name}" if thhms.nil?
+
+              thhms&.each do |thm|
+                Rails.logger.info "Csr Variant Before Update for - #{thm.id} - is - #{thm.csr_percent_as_integer}" unless Rails.env.test?
+                logger_csv << [family&.primary_person&.hbx_id.to_s, family.id.to_s, tax_household.id.to_s, thm.id.to_s, thm.csr_percent_as_integer]
+                thm.update_attributes!(csr_percent_as_integer: csr_percent)
+                report_csv << [family&.primary_person&.hbx_id.to_s, family.id.to_s, tax_household.id.to_s, thm.id.to_s, thm.csr_percent_as_integer]
+                Rails.logger.info "Updated csr variant for family for - #{thm.applicant_id} - is - #{thm.csr_percent_as_integer}" unless Rails.env.test?
+              end
+            end
+            puts "Update csr variant for family of #{family.primary_applicant.person.full_name}" unless Rails.env.test?
+            Rails.logger.info "End of the script" unless Rails.env.test?
           end
+          offset += batch_size
+          puts "offset count - #{offset}"
         end
-        puts "Update csr variant for family of #{family.primary_applicant.person.full_name}" unless Rails.env.test?
-        Rails.logger.info "End of the script" unless Rails.env.test?
       end
-      offset += batch_size
-      puts "offset count - #{offset}"
     end
+  rescue StandardError => e
+    puts "Errored processing person with hbx_id: #{family&.primary_applicant&.person&.hbx_id}, error: #{e.message}" unless Rails.env.test?
   end
 end
 # rubocop:enable Metrics/AbcSize
