85df88fb5dd78e104eee6b8e9aaeafc93e631842
Jacob Kagon <jacob@kagon.net>
2021-08-08 15:11:54 -0400

    rubocop correction

--- features/step_definitions/footer_steps.rb
+++ features/step_definitions/footer_steps.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 Given(/email address feature is enabled?/) do
-    enable_feature(:contact_email_feature)
-  end
-  
-  Given(/email address feature is disabled?/) do
-    disable_feature(:contact_email_feature)
-  end
+  enable_feature(:contact_email_feature)
+end
 
-  And(/the user visits the HBX home page?/) do
-   visit '/'
-   page.execute_script "window.scrollBy(0,10000)"
-  end 
-  
-  Then(/^they should see the contact email address$/) do
-    expect(page).to have_content("info@dchealthlink.com")
-  end
+Given(/email address feature is disabled?/) do
+  disable_feature(:contact_email_feature)
+end
 
-  Then(/^they should not see the contact email address$/) do
-    expect(page).to_not have_content("info@dchealthlink.com")
-  end
+And(/the user visits the HBX home page?/) do
+  visit '/'
+  page.execute_script "window.scrollBy(0,10000)"
+end
+
+Then(/^they should see the contact email address$/) do
+  expect(page).to have_content("info@dchealthlink.com")
+end
+
+Then(/^they should not see the contact email address$/) do
+  expect(page).to_not have_content("info@dchealthlink.com")
+end
