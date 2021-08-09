47eddaec00e10ea198e263f41dd2afc1db0e506b
Jay Reddy <jayanth.rordev@gmail.com>
2021-08-09 14:16:12 -0400

    update rake task

--- app/data_migrations/migrate_thm_csr_variant.rb
+++ app/data_migrations/migrate_thm_csr_variant.rb
@@ -1,34 +1,63 @@
 # frozen_string_literal: true
 
-require File.join(Rails.root, "lib/mongoid_migration_task")
-# New rake task to update csr variant on tax household member
+require File.join(Rails.root, 'lib/mongoid_migration_task')
+# new rake task to update csr list
 class MigrateThmCsrVariant < MongoidMigrationTask
 
-  def migrate
-    logger_field_names = %w[Family_ID Backtrace]
-    logger_file_name = "#{Rails.root}/updated_csr_variant_logger_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv"
-
-    families = Family.all_tax_households
-    batch_size = 500
-    processed_count = 0
-
-    FileUtils.touch(logger_file_name) unless File.exist?(logger_file_name)
-    CSV.open(logger_file_name, 'w', force_quotes: true) do |logger_csv|
-      logger_csv << logger_field_names
-      families.limit(batch_size).no_timeout.each do |family|
-        family.active_household.tax_households.each do |tax_household|
-          csr_percent = tax_household.latest_eligibility_determination.csr_percent_as_integer
-          thhms = tax_household.tax_household_members.where(is_ia_eligible: true)
-          thhms&.each do |thm|
-            thm.update_attributes!(csr_percent_as_integer: csr_percent)
-            puts "Updated csr variant for family for - #{thm.applicant_id} - is - #{thm.csr_percent_as_integer}"
-            processed_count += 1
-          end
+  def process_families(families, logger, offset_count)
+    families.no_timeout.limit(5_000).offset(offset_count).inject([]) do |_dummy, family|
+      person = family.primary_person
+      next unless person.present?
+      family.active_household.tax_households.where(:"eligibility_determinations.csr_percent_as_integer".ne => nil).each do |thh|
+        csr_percent_as_integer = thh.eligibility_determinations.last.csr_percent_as_integer
+        thh.tax_household_members.where(:is_ia_eligible => true).each do |thhm|
+          thhm.update_attributes!(csr_percent_as_integer: csr_percent_as_integer)
         end
-      rescue StandardError => e
-        logger_csv << [family.id, e.backtrace[0..5].join('\n')]
+      end
+    rescue StandardError => e
+      logger.info "error processing family with primary person hbx_id: #{person.hbx_id}, error: #{e.message}" unless Rails.env.test?
+    end
+  end
+
+  def unprocessed_families(families, current_year)
+    total_family_count = 0
+    families.no_timeout.each do |family|
+      tax_households = if current_year
+                         family.active_household.tax_households.tax_household_with_year(2021)
+                       else
+                         family.active_household.tax_households
+                       end
+      tax_households.each do |thh|
+        thh.tax_household_members.where(:is_ia_eligible => true).each do |thhm|
+          total_family_count += 1 if thhm.csr_percent_as_integer.nil?
+        end
       end
     end
-    puts("Total records updated are #{processed_count}.")
+    total_family_count
+  end
+
+  def migrate
+    logger = Logger.new("#{Rails.root}/log/thhm_csr_update_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log")
+    start_time = DateTime.current
+    logger.info "IndividualthhmCsr start_time: #{start_time}" unless Rails.env.test?
+    families = Family.all_tax_households
+    latest_year_families = unprocessed_families(families, true)
+    families_count = unprocessed_families(families, false)
+    total_count = families.count
+    logger.info "Total number of families to be processed #{total_count}"
+    familes_per_iteration = 5_000.0
+    number_of_iterations = (total_count / familes_per_iteration).ceil
+    counter = 0
+
+    while counter < number_of_iterations
+      offset_count = familes_per_iteration * counter
+      process_families(families, logger, offset_count)
+      counter += 1
+    end
+    end_time = DateTime.current
+
+    logger.info "Unprocessed families List: #{families_count}" unless Rails.env.test?
+    logger.info "Unprocessed families List from current year: #{latest_year_families}" unless Rails.env.test?
+    logger.info "IndividualthhmCsr end_time: #{end_time}, total_time_taken_in_minutes: #{((end_time - start_time) * 24 * 60).to_i}" unless Rails.env.test?
   end
 end
