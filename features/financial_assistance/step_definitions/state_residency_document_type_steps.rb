a9587c31015e891564e683b33f0da918352f7633
Jacob Kagon <jacob@kagon.net>
2021-08-10 14:46:43 -0400

    add cucumber spec to state_residency_document_type

---
+++ features/financial_assistance/step_definitions/state_residency_document_type_steps.rb
@@ -0,0 +1,13 @@
+
+
+When(/^the consumer visits the Documents page$/) do 
+  visit('/insured/families/verification?tab=verification')
+end
+
+Then(/^they should see the state residency tile$/) do
+    expect(page).to have_content(EnrollRegistry[:enroll_app].setting(:state_residency).item)
+end
+
+Then(/^they should not see the state residency tile$/) do
+    expect(page).to_not have_content(EnrollRegistry[:enroll_app].setting(:state_residency).item)
+end
