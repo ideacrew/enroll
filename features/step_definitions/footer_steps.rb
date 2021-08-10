66cddffacebeab5a1234569bcc05f1e5b91ee51c
Jacob Kagon <jacob@kagon.net>
2021-08-10 11:10:35 -0400

    abstract out email address in cucumber

--- features/step_definitions/footer_steps.rb
+++ features/step_definitions/footer_steps.rb
@@ -6,9 +6,9 @@ And(/the user visits the HBX home page?/) do
 end
 
 Then(/^they should see the contact email address$/) do
-  expect(page).to have_content("info@dchealthlink.com")
+  expect(page).to have_content(EnrollRegistry[:enroll_app].setting(:mail_address).item)
 end
 
 Then(/^they should not see the contact email address$/) do
-  expect(page).to_not have_content("info@dchealthlink.com")
+  expect(page).to_not have_content(EnrollRegistry[:enroll_app].setting(:mail_address).item)
 end
