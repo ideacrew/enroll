7c73fed5d21e49b084d44a69fe1d4901de908d6c
nisanth <nisanth.yaganti@gmail.com>
2021-08-11 17:08:22 -0400

    validating tobacco use question answered or not

--- app/models/forms/family_member.rb
+++ app/models/forms/family_member.rb
@@ -70,6 +70,10 @@ module Forms
         self.errors.add(:tribal_id, "is required when native american / alaska native is selected") if !EnrollRegistry[:indian_alaskan_tribe_details].enabled? && !tribal_id.present? && @indian_tribe_member
       end
 
+      if ::EnrollRegistry.feature_enabled?(:tobacco_cost) && is_tobacco_user.blank?
+        self.errors.add(:base, "Tobacco User needs to be answered")
+      end
+
       return unless (@is_resident_role.to_s == "true" || @is_consumer_role.to_s == "true") && is_applying_coverage.to_s == "true" && @is_incarcerated.nil?
       self.errors.add(:base, "Incarceration status is required")
     end
