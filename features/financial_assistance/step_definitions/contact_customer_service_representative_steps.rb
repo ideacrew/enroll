d60c1677d71e1233e1f02a0070e63f9fd5772593
Jacob Kagon <jacob@kagon.net>
2021-08-11 15:20:25 -0400

    add spec and conditionals to disable contact customer service rep

---
+++ features/financial_assistance/step_definitions/contact_customer_service_representative_steps.rb
@@ -0,0 +1,14 @@
+When(/the consumer clicks the Get Help Signing Up Button?/) do
+    find('.interaction-click-control-get-help-signing-up').click
+end
+
+Then(/they should see the Contact Customer Support and Certified Applicant Counselor links?/) do
+    page.should have_content('Help from a Customer Service Representative')
+    page.should have_content('Help from a Certified Applicant Counselor')
+end
+
+Then(/they should not see the Contact Customer Support and Certified Applicant Counselor links?/) do
+  page.should_not have_content('Help from a Customer Service Representative')
+  page.should_not have_content('Help from a Certified Applicant Counselor')
+end
+
