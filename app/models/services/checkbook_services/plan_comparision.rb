3edb0dfd2dfdea61d3f0fcee6fd2c9ee61a1ccc4
Battula <vrbattula@outlook.com>
2021-08-12 15:09:41 -0400

    Fixes rubocop error

--- app/models/services/checkbook_services/plan_comparision.rb
+++ app/models/services/checkbook_services/plan_comparision.rb
@@ -187,9 +187,9 @@ module Services
         today = @hbx_enrollment.effective_on
         if EnrollRegistry[:indian_alaskan_tribe_details].enabled?
           person = @hbx_enrollment.consumer_role.person
-          tribal_details = person.tribal_state.present? && person.tribal_name.present?
+          _tribal_details = person.tribal_state.present? && person.tribal_name.present?
         else
-          tribal_details = @hbx_enrollment.consumer_role.person.tribal_id.present?
+          _tribal_details = @hbx_enrollment.consumer_role.person.tribal_id.present?
         end
         @hbx_enrollment.hbx_enrollment_members.each do |member|
           age = member.family_member.person.age_on(today)
