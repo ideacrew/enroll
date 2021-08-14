d28d8bc418297917e91171e2e82d58688ebf5957
Battula <vrbattula@outlook.com>
2021-08-14 15:05:43 -0400

    Fixes typo & removes unnecessary fields

--- app/models/services/checkbook_services/plan_comparision.rb
+++ app/models/services/checkbook_services/plan_comparision.rb
@@ -100,15 +100,13 @@ module Services
 
         {
           "county": address&.county,
-          "zipcode": county&.zip,
+          "zipcode": address&.zip,
           "remote_access_key": Rails.application.config.checkbook_services_remote_access_key,
           "reference_id": Rails.application.config.checkbook_services_reference_id,
           "enrollment_year": enrollment_year,
           "family": consumer_build_family,
           "aptc": elected_aptc.to_s,
           "csr": csr_value,
-          "environment": checkbook_env_key,
-          "from": EnrollRegistry[:enroll_app].setting(:site_key).item.to_s,
           "enrollmentId": @hbx_enrollment.id.to_s # Host Name will be static as Checkbook suports static URL's and hostname should be changed before going to production.
         }
       end
@@ -215,11 +213,6 @@ module Services
           family << {'dob': dependent.family_member.person.dob.strftime("%Y-%m-%d") ,'relationship': dependent.primary_relationship}
         end
         family
-      end
-
-      def checkbook_env_key
-        config = Rails.application.config
-        config.respond_to?(:checkbook_services_environment_key) ? config.checkbook_services_environment_key : nil
       end
     end
   end
