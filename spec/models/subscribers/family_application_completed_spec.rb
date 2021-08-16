9f57aba1a7503a4e4fc8ee5ad20e4a395cfc4858
Jay Reddy <jayanth.rordev@gmail.com>
2021-08-16 12:59:30 -0400

    New commit to update curam import

--- spec/models/subscribers/family_application_completed_spec.rb
+++ spec/models/subscribers/family_application_completed_spec.rb
@@ -502,7 +502,9 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
         end
 
         it "has 4 tax household members with primary person as primary tax household member" do
+          expect(tax_household_db.eligibility_determinations.first.csr_percent_as_integer).to eq 0
           expect(tax_household_db.tax_household_members.length).to eq 4
+          expect(tax_household_db.tax_household_members.first.csr_percent_as_integer).to eq 0
           expect(tax_household_db.tax_household_members.map(&:is_primary_applicant?)).to eq [true,false,false,false]
           expect(tax_household_db.tax_household_members.select(&:is_primary_applicant?).first.family_member).to eq person.primary_family.primary_family_member
         end
@@ -545,6 +547,7 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
           end
 
           it "should build a new household with 3 coverage household members and 3 taxhousehold members" do
+            # person.primary_family.active_household
             # updated_tax_household = tax_household_db.household.latest_active_tax_household
             # expect(tax_household_db.tax_household_members.length).to eq 3
             # expect(tax_household_db.tax_household_members.map(&:is_primary_applicant?)).to eq [true,false,false]
