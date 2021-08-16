f218b566744151d5e56eabd534b60051d70a4b36
Jacob Kagon <jacob@kagon.net>
2021-08-16 16:35:56 -0400

    rubocop fix plus spec and update resource registry features

--- features/financial_assistance/step_definitions/state_residency_document_type_steps.rb
+++ features/financial_assistance/step_definitions/state_residency_document_type_steps.rb
@@ -1,13 +1,13 @@
+#frozen_string_literal: true
 
-
-When(/^the consumer visits the Documents page$/) do 
+When(/^the consumer visits the Documents page$/) do
   visit('/insured/families/verification?tab=verification')
 end
 
 Then(/^they should see the state residency tile$/) do
-    expect(page).to have_content(EnrollRegistry[:enroll_app].setting(:state_residency).item)
+  expect(page).to have_content(EnrollRegistry[:enroll_app].setting(:state_residency).item)
 end
 
 Then(/^they should not see the state residency tile$/) do
-    expect(page).to_not have_content(EnrollRegistry[:enroll_app].setting(:state_residency).item)
+  expect(page).to_not have_content(EnrollRegistry[:enroll_app].setting(:state_residency).item)
 end
