c772d7d4891884d1c02a628f01f40071762c81cd
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-08-17 14:48:18 -0400

    Update event listener to tie to document instead of set after ready

--- app/assets/javascripts/families.js
+++ app/assets/javascripts/families.js
@@ -1,28 +1,26 @@
 
-$(document).ready(function() {
-  $(".interaction-click-control-leave-dc-health-link").on('click', function (e) {
-    if ($(this).parent('form').attr('method') == 'post') {
-      e.preventDefault();
-      $.ajax({
-          type: "GET",
-          url: "/payment_transactions/generate_saml_response",
-          data: {enrollment_id: $(this).val(), source: $("#source").val()},
-          success: function (response) {
-            if (response["error"] != null){
-              alert("We're sorry, but something went wrong. You can try again, or pay once you receive your invoice.")}
-            else if (response["status"] == 404){
-              alert("We're sorry, but something went wrong. You can try again, or pay once you receive your invoice.")}
-            else{
-              $(this).siblings('#sp').val(response["SAMLResponse"]);
-              $(this).parent('form').submit();
-            }
-          },
-          error: function (response) {
-              // error handling
+$(document).on('click', ".interaction-click-control-leave-dc-health-link", function(e) {
+  if ($(this).parent('form').attr('method') == 'post') {
+    e.preventDefault();
+    $.ajax({
+        type: "GET",
+        url: "/payment_transactions/generate_saml_response",
+        data: {enrollment_id: $(this).val(), source: $("#source").val()},
+        success: function (response) {
+          if (response["error"] != null){
+            alert("We're sorry, but something went wrong. You can try again, or pay once you receive your invoice.")}
+          else if (response["status"] == 404){
+            alert("We're sorry, but something went wrong. You can try again, or pay once you receive your invoice.")}
+          else{
+            $(this).siblings('#sp').val(response["SAMLResponse"]);
+            $(this).parent('form').submit();
           }
-      });
-    }
-  });
+        },
+        error: function (response) {
+            // error handling
+        }
+    });
+  }
 });
 
 function enableTransition() {
