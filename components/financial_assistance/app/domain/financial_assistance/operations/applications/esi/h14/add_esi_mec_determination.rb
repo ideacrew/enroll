566c10585dea22c1fea52191528eef844bdc4c3a
saipraveen18 <saipraveen.gudimetla@gmail.com>
2021-08-20 17:55:40 -0400

    Fix rubocop and gemfile conflicts

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/esi/h14/add_esi_mec_determination.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/esi/h14/add_esi_mec_determination.rb
@@ -55,9 +55,7 @@ module FinancialAssistance
 
               applicant_esi_evidence = applicant.evidences.by_name(:esi_mec).first
               applicant_esi_evidence.update_attributes(eligibility_status: response_esi_evidence.eligibility_status)
-              if response_esi_evidence.eligibility_results.present?
-                applicant_esi_evidence.eligibility_results << FinancialAssistance::EligibilityResult.new(response_esi_evidence.eligibility_results.first.to_h)
-              end
+              applicant_esi_evidence.eligibility_results << FinancialAssistance::EligibilityResult.new(response_esi_evidence.eligibility_results.first.to_h) if response_esi_evidence.eligibility_results.present?
               applicant.save!
 
               Success(applicant)
