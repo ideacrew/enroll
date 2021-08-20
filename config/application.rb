7c13a3ab530a9ff6c8763118b35d49cf8d696b3d
Jacob Kagon <jacob@kagon.net>
2021-08-20 17:22:12 -0400

    stored new font config

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,8 @@ module Enroll
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/info"
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff2|ttf)$/
 
     I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
     config.i18n.fallbacks = {'am' => 'en', 'es' => 'en', 'ja' => 'en'}
