326ae35268d19b3b55f39edf0906999e82f45764
Jay Reddy <jayanth.rordev@gmail.com>
2021-08-21 17:02:23 -0400

    rubocop fix

--- spec/views/insured/families/_enrollment.html.erb_spec.rb
+++ spec/views/insured/families/_enrollment.html.erb_spec.rb
@@ -138,7 +138,6 @@ RSpec.describe "insured/families/_enrollment.html.erb" do
           expect(rendered).to have_content('Individual & Family')
           expect(rendered).to have_selector('strong', text: HbxProfile::ShortName.to_s)
           expect(rendered).to have_content(/#{hbx_enrollment.hbx_id}/)
-          
           expect(rendered).to have_content('Actions')
           expect(rendered).to have_content('Make changes to my coverage')
         end
