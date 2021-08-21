232ee6c4ea4d77b9248b7b63643390f0b9269914
saipraveen18 <saipraveen.gudimetla@gmail.com>
2021-08-21 09:26:44 -0400

    Fix spec

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/non_esi/non_esi_mec_request_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/non_esi/non_esi_mec_request_spec.rb
@@ -2,46 +2,89 @@
 
 require 'rails_helper'
 
-RSpec.describe ::FinancialAssistance::Operations::Applications::Fdsh::NonEsi::H31::NonEsiMecRequest, dbclean: :after_each do
+RSpec.describe ::FinancialAssistance::Operations::Applications::NonEsi::H31::NonEsiMecRequest, dbclean: :after_each do
   include Dry::Monads[:result, :do]
 
-  let!(:person) { FactoryBot.create(:person, hbx_id: "732020") }
-  let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person) }
-  let!(:application) do
-    FactoryBot.create(:financial_assistance_application,
-                      family_id: family.id,
-                      aasm_state: 'submitted',
-                      hbx_id: "830293",
-                      assistance_year: TimeKeeper.date_of_record.year,
-                      effective_date: DateTime.new(2021,1,1,4,5,6))
-  end
+  let!(:person) { FactoryBot.create(:person, hbx_id: "732020")}
+  let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
+  let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family.id, aasm_state: 'submitted', hbx_id: "830293", effective_date: DateTime.new(2021,1,1,4,5,6)) }
   let!(:applicant) do
-    applicant = FactoryBot.create(:applicant,
-                                  first_name: person.first_name,
-                                  last_name: person.last_name,
-                                  dob: person.dob,
-                                  gender: person.gender,
-                                  ssn: person.ssn,
-                                  application: application,
-                                  person_hbx_id: person.hbx_id,
-                                  ethnicity: [],
-                                  is_primary_applicant: true,
-                                  is_self_attested_blind: false,
-                                  is_applying_coverage: true,
-                                  is_required_to_file_taxes: true,
-                                  is_pregnant: false,
-                                  has_job_income: false,
-                                  has_self_employment_income: false,
-                                  has_unemployment_income: false,
-                                  has_other_income: false,
-                                  has_deductions: false,
-                                  has_enrolled_health_coverage: false,
-                                  has_eligible_health_coverage: false,
-                                  has_eligible_medicaid_cubcare: false,
-                                  is_claimed_as_tax_dependent: false,
-                                  is_incarcerated: false,
-                                  is_post_partum_period: false)
-    applicant
+    FactoryBot.create(:applicant,
+                      first_name: person.first_name,
+                      last_name: person.last_name,
+                      dob: person.dob,
+                      gender: person.gender,
+                      ssn: person.ssn,
+                      application: application,
+                      eligibility_determination_id: eligibility_determination.id,
+                      citizen_status: 'us_citizen',
+                      person_hbx_id: person.hbx_id,
+                      ethnicity: [],
+                      is_primary_applicant: true,
+                      is_self_attested_blind: false,
+                      is_applying_coverage: true,
+                      is_required_to_file_taxes: true,
+                      is_pregnant: false,
+                      has_job_income: false,
+                      has_self_employment_income: false,
+                      has_unemployment_income: false,
+                      has_other_income: false,
+                      has_deductions: false,
+                      has_enrolled_health_coverage: false,
+                      has_eligible_health_coverage: false,
+                      has_eligible_medicaid_cubcare: false,
+                      is_claimed_as_tax_dependent: false,
+                      is_incarcerated: false,
+                      is_student: false,
+                      is_former_foster_care: false,
+                      is_post_partum_period: false)
+  end
+  let!(:create_home_address) do
+    add = ::FinancialAssistance::Locations::Address.new({
+                                                          kind: 'home',
+                                                          address_1: '3 Awesome Street',
+                                                          address_2: '#300',
+                                                          city: 'Washington',
+                                                          state: 'DC',
+                                                          zip: '20001'
+                                                        })
+    applicant.addresses << add
+    applicant.save!
+  end
+
+  let!(:eligibility_determination) { FactoryBot.create(:financial_assistance_eligibility_determination, application: application) }
+  let(:event) { Success(double) }
+  let(:obj)  { ::FinancialAssistance::Operations::Applications::NonEsi::H31::PublishNonEsiMecRequest.new }
+
+  let(:premiums_hash) do
+    {
+      [person.hbx_id] => {:health_only => {person.hbx_id => [{:cost => 200.0, :member_identifier => person.hbx_id, :monthly_premium => 200.0}]}}
+    }
+  end
+
+  let(:slcsp_info) do
+    {
+      person.hbx_id => {:health_only_slcsp_premiums => {:cost => 200.0, :member_identifier => person.hbx_id, :monthly_premium => 200.0}}
+    }
+  end
+
+  let(:lcsp_info) do
+    {
+      person.hbx_id => {:health_only_lcsp_premiums => {:cost => 100.0, :member_identifier => person.hbx_id, :monthly_premium => 100.0}}
+    }
+  end
+
+  let(:fetch_double) { double(:new => double(call: double(:value! => premiums_hash)))}
+  let(:fetch_slcsp_double) { double(:new => double(call: double(:value! => slcsp_info)))}
+  let(:fetch_lcsp_double) { double(:new => double(call: double(:value! => lcsp_info)))}
+
+  before do
+    allow(::FinancialAssistance::Operations::Applications::NonEsi::H31::PublishNonEsiMecRequest).to receive(:new).and_return(obj)
+    allow(obj).to receive(:build_event).and_return(event)
+    allow(event.success).to receive(:publish).and_return(true)
+    stub_const('::Operations::Products::Fetch', fetch_double)
+    stub_const('::Operations::Products::FetchSlcsp', fetch_slcsp_double)
+    stub_const('::Operations::Products::FetchLcsp', fetch_lcsp_double)
   end
 
   context 'success' do
