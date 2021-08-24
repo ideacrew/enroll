6f6151462a62eabccb257411d5f1d65525de8b13
Kristin Merbach <kristin@kristinmerbach.com>
2021-08-24 12:56:42 -0400

    inbound transfers

---
+++ components/financial_assistance/spec/domain/financial_assistance/operations/transfers/medicaid_gateway/account_transfer_in_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'aca_entities/serializers/xml/medicaid/atp'
+require 'aca_entities/atp/transformers/cv/family'
+
+RSpec.describe ::FinancialAssistance::Operations::Transfers::MedicaidGateway::AccountTransferIn, dbclean: :after_each do
+  include Dry::Monads[:result, :do]
+
+  let(:xml) do{ File.read(::FinancialAssistance::Engine.root.join('spec', 'shared_examples', 'medicaid_gateway', 'Simple_Test_Case_E_New.xml')) }
+
+  let(:record) { ::AcaEntities::Serializers::Xml::Medicaid::Atp::AccountTransferRequest.parse(xml) }
+
+  let(:transformed) { ::AcaEntities::Atp::Transformers::Cv::Family.transform(record.to_hash(identifier: true)) }
+
+
+  context 'success' do
+    context 'with valid application' do
+      before do
+        @result = subject.call(transformed)
+      end
+
+      it 'should return success' do
+        expect(@result).to be_success
+      end
+
+      it 'should return success with message' do
+        expect(@result.success).to eq('Successfully transferred in account')
+      end
+    end
+  end
+end
