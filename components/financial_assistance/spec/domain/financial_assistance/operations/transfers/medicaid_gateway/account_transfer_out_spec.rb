609a090dd9fcbb5d9385f5feaa422e2b33d98180
Kristin Merbach <kristin@kristinmerbach.com>
2021-08-24 13:00:49 -0400

    atp transfers out of enroll

---
+++ components/financial_assistance/spec/domain/financial_assistance/operations/transfers/medicaid_gateway/account_transfer_out_spec.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ::FinancialAssistance::Operations::Transfers::MedicaidGateway::AccountTransferOut, dbclean: :after_each do
+  include Dry::Monads[:result, :do]
+
+  let!(:person) { FactoryBot.create(:person, hbx_id: "732020")}
+  let!(:person2) { FactoryBot.create(:person, hbx_id: "732021") }
+  let!(:person3) { FactoryBot.create(:person, hbx_id: "732022") }
+  let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
+  let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family.id, aasm_state: 'submitted', hbx_id: "830293", effective_date: DateTime.new(2021,1,1,4,5,6)) }
+  let!(:applicant) do
+    applicant = FactoryBot.create(:applicant,
+                                  first_name: person.first_name,
+                                  last_name: person.last_name,
+                                  dob: person.dob,
+                                  gender: person.gender,
+                                  ssn: person.ssn,
+                                  application: application,
+                                  ethnicity: [],
+                                  is_primary_applicant: true,
+                                  person_hbx_id: person.hbx_id,
+                                  is_self_attested_blind: false,
+                                  is_applying_coverage: true,
+                                  is_required_to_file_taxes: true,
+                                  is_filing_as_head_of_household: true,
+                                  is_pregnant: false,
+                                  has_job_income: false,
+                                  has_self_employment_income: false,
+                                  has_unemployment_income: false,
+                                  has_other_income: false,
+                                  has_deductions: false,
+                                  is_self_attested_disabled: true,
+                                  is_physically_disabled: false,
+                                  citizen_status: 'us_citizen',
+                                  has_enrolled_health_coverage: false,
+                                  has_eligible_health_coverage: false,
+                                  has_eligible_medicaid_cubcare: false,
+                                  is_claimed_as_tax_dependent: false,
+                                  is_incarcerated: false,
+                                  net_annual_income: 10_078.90,
+                                  is_post_partum_period: false)
+    applicant
+  end
+
+  let!(:eligibility_determination) { FactoryBot.create(:financial_assistance_eligibility_determination, application: application) }
+
+  # let!(:products) { FactoryBot.create_list(:benefit_markets_products_health_products_health_product, 5, :silver) }
+
+  let(:premiums_hash) do
+    {
+      [person.hbx_id] => {:health_only => {person.hbx_id => [{:cost => 200.0, :member_identifier => person.hbx_id, :monthly_premium => 200.0}]}},
+      [person2.hbx_id] => {:health_only => {person2.hbx_id => [{:cost => 200.0, :member_identifier => person2.hbx_id, :monthly_premium => 200.0}]}},
+      [person3.hbx_id] => {:health_only => {person3.hbx_id => [{:cost => 200.0, :member_identifier => person3.hbx_id, :monthly_premium => 200.0}]}}
+    }
+  end
+
+  let(:slcsp_info) do
+    {
+      person.hbx_id => {:health_only_slcsp_premiums => {:cost => 200.0, :member_identifier => person.hbx_id, :monthly_premium => 200.0}},
+      person2.hbx_id => {:health_only_slcsp_premiums => {:cost => 200.0, :member_identifier => person2.hbx_id, :monthly_premium => 200.0}},
+      person3.hbx_id => {:health_only_slcsp_premiums => {:cost => 200.0, :member_identifier => person3.hbx_id, :monthly_premium => 200.0}}
+    }
+  end
+
+  let(:lcsp_info) do
+    {
+      person.hbx_id => {:health_only_lcsp_premiums => {:cost => 100.0, :member_identifier => person.hbx_id, :monthly_premium => 100.0}},
+      person2.hbx_id => {:health_only_lcsp_premiums => {:cost => 100.0, :member_identifier => person2.hbx_id, :monthly_premium => 100.0}},
+      person3.hbx_id => {:health_only_lcsp_premiums => {:cost => 100.0, :member_identifier => person3.hbx_id, :monthly_premium => 100.0}}
+    }
+  end
+
+  let(:fetch_double) { double(:new => double(call: double(:value! => premiums_hash)))}
+  let(:fetch_slcsp_double) { double(:new => double(call: double(:value! => slcsp_info)))}
+  let(:fetch_lcsp_double) { double(:new => double(call: double(:value! => lcsp_info)))}
+
+  before do
+    stub_const('::Operations::Products::Fetch', fetch_double)
+    stub_const('::Operations::Products::FetchSlcsp', fetch_slcsp_double)
+    stub_const('::Operations::Products::FetchLcsp', fetch_lcsp_double)
+  end
+
+
+  context 'success' do
+    context 'with valid application' do
+      before do
+        @result = subject.call({application_id: application})
+      end
+
+      it 'should return success' do
+        expect(@result).to be_success
+      end
+
+      it 'should return success with message' do
+        expect(@result.success).to eq('Successfully published the payload to medicaid_gateway to be transferred out to ACES')
+      end
+    end
+  end
+
+  context 'failure' do
+    context 'invalid application id' do
+      before do
+        @result = subject.call({application_id: 'application_id'})
+      end
+
+      it 'should return a failure with error message' do
+        expect(@result.failure).to eq('Unable to find Application with ID application_id.')
+      end
+    end
+
+    context 'invalid application aasm_state' do
+      before do
+        application.update_attributes!(aasm_state: 'draft')
+        @result = subject.call({application_id: application.id})
+      end
+
+      it 'should return a failure with error message' do
+        expect(@result.failure).to eq('Application is in draft state. Please submit application.')
+      end
+    end
+  end
+end
