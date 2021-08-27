4503209276ef65ae7683390109018fea0b6a6a06
Jacob Kagon <jacob@kagon.net>
2021-08-27 13:40:12 -0400

    add asset config back

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module Enroll
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/info"
     I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
     config.i18n.fallbacks = {'am' => 'en', 'es' => 'en', 'ja' => 'en'}
     config.paths.add "app/api", glob: "**/*.rb"
