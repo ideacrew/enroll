be3d66639ccfb07e847d42d4bc32aa92f0124349
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-08-31 08:31:59 -0400

    Update csr kind to new thh csr method

---
+++ spec/script/update_eligibility_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe "UpdateEligibility", :dbclean => :after_each do
+  let(:hbx_profile) {FactoryBot.create(:hbx_profile)}
+  include_context 'setup one tax household with one ia member'
+
+  it "should invoke without errors" do
+    expect { system 'script/update_eligibility.rb' }.to_not raise_error
+  end
+end
