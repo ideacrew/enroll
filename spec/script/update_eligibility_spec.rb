a357364d53dede72c1555e1b0db14502efe2a4bf
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-08-31 08:51:45 -0400

    Fix rspec

--- spec/script/update_eligibility_spec.rb
+++ spec/script/update_eligibility_spec.rb
@@ -4,7 +4,6 @@ require "rails_helper"
 
 describe "UpdateEligibility", :dbclean => :after_each do
   let(:hbx_profile) {FactoryBot.create(:hbx_profile)}
-  include_context 'setup one tax household with one ia member'
 
   it "should invoke without errors" do
     expect { system 'script/update_eligibility.rb' }.to_not raise_error
