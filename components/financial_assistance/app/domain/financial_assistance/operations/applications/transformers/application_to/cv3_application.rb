5d3bd4f8d32c3a82bbc94be39a4d0df0ebab23dc
saipraveen18 <saipraveen.gudimetla@gmail.com>
2021-09-08 11:41:44 -0400

    comment out for demo purpose

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/transformers/application_to/cv3_application.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/transformers/application_to/cv3_application.rb
@@ -684,7 +684,8 @@ module FinancialAssistance
               application.eligibility_determinations.inject([]) do |result, ed|
                 result << {hbx_id: ed.hbx_assigned_id.to_s,
                            max_aptc: ed.max_aptc,
-                           is_insurance_assistance_eligible: ed.is_eligibility_determined,
+                           #need to remove this comment
+                           # is_insurance_assistance_eligible: ed.is_eligibility_determined,
                            annual_tax_household_income: ed.aptc_csr_annual_household_income,
                            tax_household_members: get_thh_member(ed, application)}
               end
