8aab7be0595686f7dc148722eb57d591d7748fea
Jacob Kagon <jacob@kagon.net>
2021-09-09 11:48:30 -0400

    fix registrations_controller_spec

--- features/hbx_admin/step_definitions/manage_sep_types_steps.rb
+++ features/hbx_admin/step_definitions/manage_sep_types_steps.rb
@@ -960,7 +960,7 @@ When("I click on Shop For Plans banner button") do
 end
 
 When("I click Shop for new plan button on CHH page") do
-  find(".interaction-click-control-shop-for-new-plan").click
+  find(".interaction-click-control-shop-for-new-plan", wait: 5).click
 end
 
 Then(/^.+ should see Termination on kinds dropdown$/) do
