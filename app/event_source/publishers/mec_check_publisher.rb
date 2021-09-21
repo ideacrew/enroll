c3cedfbfa2fd99533efcf074627f613e69794bed
mdkaraman <mdkaramanov@gmail.com>
2021-09-21 09:46:28 -0400

    rubocop fixes

--- app/event_source/publishers/mec_check_publisher.rb
+++ app/event_source/publishers/mec_check_publisher.rb
@@ -4,7 +4,7 @@ module Publishers
   # Publisher will send application payload to medicaid gateway for MEC check
   class MecCheckPublisher
     include ::EventSource::Publisher[amqp: 'enroll.iap.mec_check']
-  
+
     register_event 'mec_check_requested'
   end
 end
--- app/event_source/subscribers/mec_check_subscriber.rb
+++ app/event_source/subscribers/mec_check_subscriber.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
 module Subscribers
-    # Subscriber will receive response payload from medicaid gateway and perform validation along with persisting the payload
-    class MecCheckSubscriber
-      include EventSource::Logging
-      include ::EventSource::Subscriber[amqp: 'magi_medicaid.mec_check.enroll']
-  
-      # event_source branch: release_0.5.2
-      subscribe(:on_magi_medicaid_mec_check_enroll) do |delivery_info, _metadata, response|
-        logger.info "MecCheckSubscriber: invoked on_magi_medicaid_mec_check_enroll with delivery_info: #{delivery_info}, response: #{response}"
-        payload = JSON.parse(response, :symbolize_names => true)
+  # Subscriber will receive response payload from medicaid gateway and perform validation along with persisting the payload
+  class MecCheckSubscriber
+    include EventSource::Logging
+    include ::EventSource::Subscriber[amqp: 'magi_medicaid.mec_check.enroll']
 
-        result = FinancialAssistance::Operations::Applications::MedicaidGateway::AddMecCheck.new.call(payload)
+    # event_source branch: release_0.5.2
+    subscribe(:on_magi_medicaid_mec_check_enroll) do |delivery_info, _metadata, response|
+      logger.info "MecCheckSubscriber: invoked on_magi_medicaid_mec_check_enroll with delivery_info: #{delivery_info}, response: #{response}"
+      payload = JSON.parse(response, :symbolize_names => true)
 
-        if result.success?
-          ack(delivery_info.delivery_tag)
-          logger.info "MecCheckSubscriber: acked with success: #{result.success}"
-        else
-          errors = result.failure.errors.to_h
-          nack(delivery_info.delivery_tag)
-          logger.info "MecCheckSubscriber: nacked with failure, errors: #{errors}"
-        end
-      rescue StandardError => e
+      result = FinancialAssistance::Operations::Applications::MedicaidGateway::AddMecCheck.new.call(payload)
+
+      if result.success?
+        ack(delivery_info.delivery_tag)
+        logger.info "MecCheckSubscriber: acked with success: #{result.success}"
+      else
+        errors = result.failure.errors.to_h
         nack(delivery_info.delivery_tag)
-        logger.info "MecCheckSubscriber: error: #{e.backtrace}"
+        logger.info "MecCheckSubscriber: nacked with failure, errors: #{errors}"
       end
+    rescue StandardError => e
+      nack(delivery_info.delivery_tag)
+      logger.info "MecCheckSubscriber: error: #{e.backtrace}"
     end
   end
-  
+end
