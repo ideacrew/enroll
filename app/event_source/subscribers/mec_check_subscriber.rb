b77b1b8c25833e60d9bf8b6292cf8ccef2d936e0
mdkaraman <mdkaramanov@gmail.com>
2021-09-22 18:09:50 -0400

    remove .enroll from event source mec check subscriber

--- app/event_source/subscribers/mec_check_subscriber.rb
+++ app/event_source/subscribers/mec_check_subscriber.rb
@@ -4,10 +4,10 @@ module Subscribers
   # Subscriber will receive response payload from medicaid gateway and perform validation along with persisting the payload
   class MecCheckSubscriber
     include EventSource::Logging
-    include ::EventSource::Subscriber[amqp: 'magi_medicaid.mec_check.enroll']
+    include ::EventSource::Subscriber[amqp: 'magi_medicaid.mec_check']
 
     # event_source branch: release_0.5.2
-    subscribe(:on_magi_medicaid_mec_check_enroll) do |delivery_info, _metadata, response|
+    subscribe(:on_magi_medicaid_mec_check) do |delivery_info, _metadata, response|
       logger.info "MecCheckSubscriber: invoked on_magi_medicaid_mec_check_enroll with delivery_info: #{delivery_info}, response: #{response}"
       payload = JSON.parse(response, :symbolize_names => true)
 
