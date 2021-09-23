2f0b6e96c4f9c4d6df45f33f176010427c6055ba
Mark Goho <markgoho@gmail.com>
2021-09-22 20:37:55 -0400

    run new rspec report

--- ci/scripts/rspec-split.ts
+++ ci/scripts/rspec-split.ts
@@ -1,25 +1,30 @@
 import { promises as fs } from 'fs';
-import { runtimeDictionary } from './concat-rspec-reports';
 
 import {
   FileWithRuntime,
   FileWithRuntimeDictionary,
   FileGroup,
+  RspecExample,
 } from './models';
-import { createFilesWithRuntime, splitFilesIntoGroups } from './util';
+import {
+  createFileDictionary,
+  createFilesWithRuntime,
+  splitFilesIntoGroups,
+} from './util';
 import { runtimeDetails } from './util/numberOfGroups';
+
+const REPORT_PATH = './ci/rspec/local-rspec-report.json';
+const SPLIT_CONFIG_PATH = './ci/rspec-split-config.json';
 
 async function createSplitConfig(): Promise<void> {
   // Read cli arguments
-  const [splitConfigPath] = process.argv.slice(2);
 
-  if (splitConfigPath === undefined) {
-    console.error('Missing cli arguments');
-    process.exit(1);
-  }
+  const rspecExamples = await fs.readFile(REPORT_PATH, 'utf-8');
+  const examples: RspecExample[] = JSON.parse(rspecExamples);
 
   // Create a dictionary of
-  const filesByRuntime: FileWithRuntimeDictionary = await runtimeDictionary();
+  const filesByRuntime: FileWithRuntimeDictionary =
+    createFileDictionary(examples);
 
   const arrayOfSlowFiles: FileWithRuntime[] =
     createFilesWithRuntime(filesByRuntime);
@@ -32,7 +37,7 @@ async function createSplitConfig(): Promise<void> {
   );
 
   try {
-    await fs.writeFile(`./${splitConfigPath}`, JSON.stringify(splitConfig));
+    await fs.writeFile(SPLIT_CONFIG_PATH, JSON.stringify(splitConfig));
   } catch (e) {
     console.error(e);
   }
