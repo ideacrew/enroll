0d0bd589ec8cb365d5287ee53d9d291796770c21
Zane Sandin <zanesandin@gmail.com>
2021-09-24 15:45:33 -0400

    REFS maintenance - adds person concern to benefit sponsors dummy.

---
+++ components/benefit_sponsors/spec/dummy/app/models/concerns/crm_gateway/person_concern.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module CrmGateway
+  # Methods related to publishing data to CRM gateway
+  module PersonConcern
+    extend ActiveSupport::Concern
+
+    included do
+      after_save :trigger_primary_subscriber_publish
+    end
+
+    def trigger_primary_subscriber_publish
+      return unless EnrollRegistry.feature_enabled?(:crm_publish_primary_subscriber)
+      return unless has_active_consumer_role?
+      return unless primary_family.present? && self == primary_family.primary_person
+      puts("Triggering CRM primary subscriber update publish for person with mongo id #{self.id}")
+      ::Operations::People::SugarCrm::PublishPrimarySubscriber.new.call(self)
+    end
+  end
+end
