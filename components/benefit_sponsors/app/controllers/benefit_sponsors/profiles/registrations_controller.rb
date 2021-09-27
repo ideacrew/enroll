cf921df90724e75a731986133e410abf96930245
Jacob Kagon <jacob@kagon.net>
2021-09-27 13:54:43 -0400

    add new ME broker registrations confirmation page

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/registrations_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/registrations_controller.rb
@@ -30,18 +30,24 @@ module BenefitSponsors
         begin
           saved, result_url = @agency.save
           result_url = self.send(result_url)
-          if saved
-            if is_employer_profile?
+          if saved && is_employer_profile?
               person = current_person
               create_sso_account(current_user, current_person, 15, "employer") do
               end
-            elsif is_general_agency_profile? || redirect_to_requirements_after_confirmation?
-              flash[:notice] = "Your registration has been submitted. A response will be sent to the email address you provided once your application is reviewed."
-            end
-            redirect_to result_url unless redirect_to_requirements_after_confirmation?
-            # The "confirmation" page is a special view the broker will be redirected to informing them of the necessary requirements to become a broker on the site
-            # It is not enabled by default
-            render 'confirmation', :layout => 'single_column' if redirect_to_requirements_after_confirmation?
+          elsif saved && is_general_agency_profile? || redirect_to_requirements_after_confirmation?
+            flash[:notice] = "Your registration has been submitted. A response will be sent to the email address you provided once your application is reviewed."
+          end
+          template_filename = if EnrollRegistry.feature_enabled?(:redirect_to_requirements_page_after_confirmation)
+                      "confirmation"
+                     else
+                      "broker_agencies/broker_roles/extended_confirmation.html.erb"
+                    end
+          # Change me file to extended_confirrmation
+          if is_broker_profile?
+            render template_filename, :layout => 'single_column'
+            return
+          else
+            redirect_to result_url
             return
           end
         rescue Exception => e
@@ -50,6 +56,7 @@ module BenefitSponsors
         params[:profile_type] = profile_type
         render default_template, :flash => { :error => @agency.errors.full_messages }
       end
+          
 
       def edit
         @agency = BenefitSponsors::Organizations::OrganizationForms::RegistrationForm.for_edit(profile_id: params[:id])
