d944819ca7702b22c3bd93375e5de6c617475b87
Jay Reddy <jayanth.rordev@gmail.com>
2021-09-27 10:00:26 -0400

    update csr eligibility kind references to pick kind from individual level

--- app/models/factories/ivl_eligibility_factory.rb
+++ app/models/factories/ivl_eligibility_factory.rb
@@ -222,8 +222,9 @@ module Factories
 
     def fetch_csr
       return {:csr => 'csr_0'} if (shopping_tax_members.count != shopping_member_ids.count) || any_aptc_ineligible?
+      # picks csr_kind based on individual level
 
-      csr_kinds = tax_households.map(&:current_csr_eligibility_kind)
+      csr_kinds = tax_households.first.eligibile_csr_kind(shopping_member_ids)
       {:csr => prioritized_csr(csr_kinds)}
     end
   end
