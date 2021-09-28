4a3b85d246418e7e55e51b5e8efcba857a39c1f1
Zane Sandin <zanesandin@gmail.com>
2021-09-28 17:02:00 -0400

    REFS Mainteannce - Fixes missing text and spec.

--- db/seedfiles/translations/en/dc/user_mailer.rb
+++ db/seedfiles/translations/en/dc/user_mailer.rb
@@ -1,4 +1,4 @@
 
 USER_MAILER_TRANSLATIONS = {
-  "en.user_mailer.generic_consumer.bottom_text" => "%{site_short_name} is strongly committed to maintaining the privacy of your personal information and the security of our computer systems. With respect to the collection, use and disclosure of personal information, %{site_short_name} makes every effort to ensure compliance with applicable Federal law."
+  "en.user_mailer.generic_consumer.bottom_text" => "<p>Questions? Call %{contact_center_name} at %{contact_center_phone_number}, or go online to <a href=%{site_home_business_url}>%{site_short_name}<a/></p><p>%{site_short_name} is strongly committed to maintaining the privacy of your personal information and the security of our computer systems. With respect to the collection, use and disclosure of personal information, %{site_short_name} makes every effort to ensure compliance with applicable Federal law.</p>"
 }
