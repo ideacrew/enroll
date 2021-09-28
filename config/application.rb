ddee0f6adbd49ae49b74cd2875b7742cb6ef7b86
Jay Reddy <jayanth.rordev@gmail.com>
2021-09-28 14:03:05 -0400

    fix linter issue

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 require "action_cable/engine"
 require "sprockets/railtie" # Uncomment this line for Rails 3.1+
-require "config/initializers"
+require "config"
 
 # Configure fallbacks for mongoid errors:
 require "i18n/backend/fallbacks"
