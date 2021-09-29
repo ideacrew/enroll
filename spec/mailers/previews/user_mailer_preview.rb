96ba379bbeed1f2c49f5623b8d03addd8dbb4f4a
Zane Sandin <zanesandin@gmail.com>
2021-09-29 10:24:48 -0400

    REFS Maintenance - Fixes rubocop stuff.

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 # Class for testing emails in browser
+# Visit http://localhost:3000/rails/mailers/
+# And you'll see an index of emails
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
     UserMailer.welcome(User.first)
   end
 
-  def generic_user_welcome
+  def generic_consumer
     UserMailer.generic_consumer_welcome(Person.first.first_name, Person.first.hbx_id, "genericemail@exmaple.com")
   end
 
@@ -21,7 +23,36 @@ class UserMailerPreview < ActionMailer::Preview
       source_kind: "broker_role",
       role: "broker_role"
     )
-
     UserMailer.broker_invitation_email(email, person_name, invitation)
+  end
+
+  def invitation_email
+    user = User.first
+    email = user.email
+    person_name = user.person.full_name
+    invitation = Invitation.create!(
+      invitation_email: email,
+      source_id: user.id,
+      invitation_email_type: "employer_staff_role",
+      source_kind: "employer_staff_role",
+      role: "employer_staff_role"
+    )
+    UserMailer.invitation_email(email, person_name, invitation)
+  end
+
+  def message_to_broker
+    user = User.all.to_a.detect { |user_record| user_record.person.present? }
+    # TODO: need to make an actual broker role for this
+    #broker = user.person
+    broker = BrokerRole.new(person: Person.all.detect { |person| person.emails.present? })
+    person = user.person
+    UserMailer.message_to_broker(person, broker, params)
+  end
+
+  def plan_shopping_completed
+    enrollment = HbxEnrollment.all.detect { |hbx| hbx.product.present? }
+    user = User.first
+    plan_decorator = UnassistedPlanCostDecorator.new(enrollment.product, enrollment)
+    UserMailer.plan_shopping_completed(user, enrollment, plan_decorator)
   end
 end
