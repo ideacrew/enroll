4d291c558035cf567888987edc147c725f694400
Zane Sandin <zanesandin@gmail.com>
2021-09-30 10:44:15 -0400

    REFS maintenance - fixes maine translations.

---
+++ db/seedfiles/translations/en/me/inbox.rb
@@ -0,0 +1,5 @@
+INBOX_TRANSLATIONS = {
+  "en.inbox.create_inbox_broker_message" => "<p>%{site_short_name} is ready to help the people of %{state_name} get quality, affordable coverage that meets their needs and budget. Take a moment to explore your account features.If you have questions, we’re here to help.</p> <p>The %{site_short_name} Team</p> <p>%{contact_center_name}</p>",
+  "en.inbox.create_inbox_normal_user_message" => "<p>%{site_short_name} is ready to help the people of %{state_name} get quality, affordable coverage that meets their needs and budget. Take a moment to explore your account features. Remember, there’s limited time to sign up for a plan. If you have questions, we’re here to help.</p> <p>The %{site_short_name} Team</p> <p>%{contact_center_name}</p>"
+
+}
