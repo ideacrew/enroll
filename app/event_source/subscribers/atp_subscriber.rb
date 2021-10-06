f2dc36c345a3bb31e038d1dbd63882daf3bac097
Kristin Merbach <kristin@kristinmerbach.com>
2021-10-06 11:00:02 -0400

    response work

--- app/event_source/subscribers/atp_subscriber.rb
+++ app/event_source/subscribers/atp_subscriber.rb
@@ -12,12 +12,16 @@ module Subscribers
       logger.info "AtpSubscriber: invoked on_magi_medicaid_atp_enroll with delivery_info: #{delivery_info}, response: #{response}"
       payload = JSON.parse(response, :symbolize_names => true)
       result = FinancialAssistance::Operations::Transfers::MedicaidGateway::AccountTransferIn.new.call(payload)
+      puts "sucessful ingestion: #{result.success?}"
       transfer_details = {}
-      transfer_details[:transfer_id] = payload[:family][:magi_medicaid_applications][0][:transfer_id] || payload
-
+      puts "\n\n\n"
+      puts payload["family"]["magi_medicaid_applications"][0]["transfer_id"]
+      transfer_details[:transfer_id] = payload["family"]["magi_medicaid_applications"][0]["transfer_id"] || payload
+      
       if result.success?
         transfer_response = FinancialAssistance::Operations::Transfers::MedicaidGateway::AccountTransferResponse.new.call(payload)
-        transfer_details = transfer_details.merge(transfer_response.value!) if transfer_response.success?
+        puts transfer_response
+        transfer_details = transfer_response.success? ? transfer_details.merge(transfer_response.value!) : transfer_details.merge({result: "Failed to generate response message"})
         ack(delivery_info.delivery_tag)
         logger.info "AtpSubscriber: acked with success: #{result.success}"
       else
@@ -26,9 +30,11 @@ module Subscribers
         nack(delivery_info.delivery_tag)
         logger.info "AtpSubscriber: nacked with failure, errors: #{errors}"
       end
-
-      FinancialAssistance::Operations::Transfers::MedicaidGateway::PublishTransferResponse.new.call(transfer_details)
-
+      puts "\n\n\n\n\n"
+      puts transfer_details
+      puts "\n\n\n\n\n"
+      return_call = FinancialAssistance::Operations::Transfers::MedicaidGateway::PublishTransferResponse.new.call(transfer_details)
+      puts "return_call: #{return_call}"
     rescue StandardError => e
       nack(delivery_info.delivery_tag)
       logger.info "AtpSubscriber: error: #{e.backtrace}"
