fd54ccd3f353adebdad314d3dcf223f7264bec3d
Battula <vrbattula@outlook.com>
2021-10-12 22:09:36 -0400

    Updates operation to include family payload for QHP cases

--- app/domain/operations/notices/ivl_oe_reverification_trigger.rb
+++ app/domain/operations/notices/ivl_oe_reverification_trigger.rb
@@ -46,7 +46,6 @@ module Operations
         financial_application = fetch_application(family)
 
         return Success('qhp_eligible_on_reverification') if financial_application.nil?
-        return Success('expired_consent_on_reverification') unless financial_application.determined?
 
         applicants = financial_application.applicants
 
@@ -63,14 +62,89 @@ module Operations
       end
       # rubocop:enable Metrics/CyclomaticComplexity
 
+      def build_addresses(person)
+        address = person.mailing_address
+        [
+          {
+            :kind => address.kind,
+            :address_1 => address.address_1,
+            :address_2 => address.address_2,
+            :address_3 => address.address_3,
+            :state => address.state,
+            :city => address.city,
+            :zip => address.zip
+          }
+        ]
+      end
+
+      def update_and_build_verification_types(person)
+        person.consumer_role.types_include_to_notices.collect do |verification_type|
+          {
+            type_name: verification_type.type_name,
+            validation_status: verification_type.validation_status,
+            due_date: verification_type.due_date
+          }
+        end
+      end
+
+      def build_family_member_hash(family)
+        family.family_members.collect do |fm|
+          person = fm.person
+          outstanding_verification_types = person.consumer_role.types_include_to_notices
+          member_hash = {
+            is_primary_applicant: fm.is_primary_applicant,
+            person: {
+              hbx_id: person.hbx_id,
+              person_name: { first_name: person.first_name, last_name: person.last_name },
+              person_demographics: { gender: person.gender, dob: person.dob, is_incarcerated: person.is_incarcerated || false },
+              person_health: { is_tobacco_user: person.is_tobacco_user },
+              is_active: person.is_active,
+              is_disabled: person.is_disabled,
+              addresses: build_addresses(person)
+            }
+          }
+          member_hash[:person].merge!(verification_types: update_and_build_verification_types(person)) if outstanding_verification_types.present?
+          member_hash
+        end
+      end
+
+      def build_household_hash(family)
+        family.households.collect do |household|
+          {
+            start_date: household.effective_starting_on,
+            is_active: household.is_active,
+            coverage_households: household.coverage_households.collect { |ch| {is_immediate_family: ch.is_immediate_family, coverage_household_members: ch.coverage_household_members.collect {|chm| {is_subscriber: chm.is_subscriber}}} }
+          }
+        end
+      end
+
+      def documents_needed?(family)
+        family.family_members.any? { |member| member.person.consumer_role.types_include_to_notices.present? }
+      end
+
+      def build_family_payload(family)
+        payload = { family_members: build_family_member_hash(family), households: build_household_hash(family), hbx_id: family.hbx_assigned_id.to_s, documents_needed: documents_needed?(family) }
+        family_contract = AcaEntities::Contracts::Families::FamilyContract.new.call(payload)
+        return Failure("invalid payload for family id: #{family.id}") unless family_contract.success?
+
+        family_entity = AcaEntities::Families::Family.new(family_contract.to_h)
+
+        Success(family_entity)
+      end
+
       def build_payload(family)
         financial_application = fetch_application(family)
 
-        entity = ::AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(
-          JSON.parse(financial_application.eligibility_response_payload,:symbolize_name => true)
-        )
+        entity =
+          if financial_application.present? && financial_application.eligibility_response_payload.present?
+            ::AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(
+              JSON.parse(financial_application.eligibility_response_payload, :symbolize_name => true)
+            )
+          else
+            build_family_payload(family)
+          end
 
-        if entity.success?
+        if entity&.success?
           Success(entity.success.to_h)
         else
           Failure("Error parsing the payload for the given family id: #{family.id}")
