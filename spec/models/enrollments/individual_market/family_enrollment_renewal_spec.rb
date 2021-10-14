28863def7968a181a8a4320ef8c5269b20beb3b0
ymhari <hariym471@gmail.com>
2021-10-14 11:14:46 -0400

    ref: 96729 Fixes renewal script to fetch csr variant for 02 and 03 variants.

--- spec/models/enrollments/individual_market/family_enrollment_renewal_spec.rb
+++ spec/models/enrollments/individual_market/family_enrollment_renewal_spec.rb
@@ -354,6 +354,8 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
         let!(:current_product) { FactoryBot.create(:active_ivl_silver_health_product, hios_id: "11111111122302-04", hios_base_id: "11111111122302", csr_variant_id: "04", renewal_product_id: renewal_product.id) }
         let!(:csr_product) { FactoryBot.create(:renewal_ivl_silver_health_product, hios_id: "11111111122302-05", hios_base_id: "11111111122302", csr_variant_id: "05") }
         let!(:csr_01_product) { FactoryBot.create(:active_ivl_silver_health_product, hios_id: "11111111122302-01", hios_base_id: "11111111122302", csr_variant_id: "01") }
+        let!(:csr_02_product) { FactoryBot.create(:active_ivl_silver_health_product, hios_id: "11111111122302-02", hios_base_id: "11111111122302", csr_variant_id: "02") }
+        let!(:csr_03_product) { FactoryBot.create(:active_ivl_silver_health_product, hios_id: "11111111122302-03", hios_base_id: "11111111122302", csr_variant_id: "03") }
 
         context "and have different CSR amount for renewal product year" do
           let(:aptc_values) {{ csr_amt: "87" }}
@@ -386,6 +388,28 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
 
           it "should be renewed into same CSR variant product" do
             expect(subject.assisted_renewal_product).to eq renewal_product.id
+          end
+        end
+
+        context "when eligible CSR variant is 02 and 03 for renewal product year" do
+          before do
+            enrollment.product.update_attributes(metal_level_kind: 'gold')
+          end
+
+          context 'when eligible csr is 100' do
+            let(:aptc_values) {{ csr_amt: "100" }}
+
+            it "should be renewed into eligible CSR variant product" do
+              expect(subject.assisted_renewal_product).to eq csr_02_product.id
+            end
+          end
+
+          context 'when eligible csr is limited' do
+            let(:aptc_values) {{ csr_amt: "limited" }}
+
+            it "should be renewed into eligible CSR variant product" do
+              expect(subject.assisted_renewal_product).to eq csr_03_product.id
+            end
           end
         end
       end
