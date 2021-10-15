f9cd859479a0cc8a19a6cbfcefc8ff35b7286d4d
Bill Transue <transue@gmail.com>
2021-10-15 11:13:46 -0400

    Fix rubocop

--- app/controllers/concerns/vlp_doc.rb
+++ app/controllers/concerns/vlp_doc.rb
@@ -20,11 +20,11 @@ module VlpDoc
       result = ::Validators::VlpV37Contract.new.call(vlp_doc_params)
       if result.failure? && source == 'person'
         invalid_key = result.errors.to_h.keys.first
-        add_document_errors_to_consumer_role(consumer_role, ['Please fill in your information for', invalid_field(invalid_key).to_s.titlecase + '.'])
+        add_document_errors_to_consumer_role(consumer_role, ['Please fill in your information for', "#{invalid_field(invalid_key).to_s.titlecase}."])
         return false
       elsif result.failure? && source == 'dependent'
         invalid_key = result.errors.to_h.keys.first
-        add_document_errors_to_dependent(dependent, ['Please fill in your information for', invalid_field(invalid_key).to_s.titlecase + '.'])
+        add_document_errors_to_dependent(dependent, ['Please fill in your information for', "#{invalid_field(invalid_key).to_s.titlecase}."])
         return false
       end
     end
