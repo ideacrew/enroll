f64dbdfb1dfaf336394888b9999e2efe359812b9
Mark Goho <markgoho@gmail.com>
2021-10-20 17:39:36 -0400

    re-enable spec

--- spec/data_migrations/update_bulk_broker_emails_spec.rb
+++ spec/data_migrations/update_bulk_broker_emails_spec.rb
@@ -12,24 +12,24 @@ describe "Update Broker Emails", dbclean: :after_all do
     end
   end
 
-  # describe "spec for bulk broker email updates" do
-  #   # These values come from test CSV
-  #   let!(:broker_role) { FactoryBot.create(:broker_role, aasm_state: "active", person: person, npn: "1111") }
-  #   let!(:person) { FactoryBot.create(:person, first_name: "Jimmy", last_name: "Stephens") }
-  #   let(:original_broker_email_address) { "fakeemail1@fake.com" }
-  #   let!(:original_email) { person.emails.create(kind: "work", address: original_broker_email_address) }
-  #   let(:new_broker_email_address) {"fakeemail9@fake.com"}
-  #   before do
-  #     allow_any_instance_of(UpdateBulkBrokerEmails).to receive(:broker_csv).and_return(
-  #       File.read("#{Rails.root}/spec/support/update_bulk_broker_emails_2021_10_5.csv")
-  #     )
-  #     Rails.application.load_tasks
-  #     Rake::Task['migrations:update_bulk_broker_emails'].invoke
-  #   end
+  describe "spec for bulk broker email updates" do
+    # These values come from test CSV
+    let!(:broker_role) { FactoryBot.create(:broker_role, aasm_state: "active", person: person, npn: "1111") }
+    let!(:person) { FactoryBot.create(:person, first_name: "Jimmy", last_name: "Stephens") }
+    let(:original_broker_email_address) { "fakeemail1@fake.com" }
+    let!(:original_email) { person.emails.create(kind: "work", address: original_broker_email_address) }
+    let(:new_broker_email_address) {"fakeemail9@fake.com"}
+    before do
+      allow_any_instance_of(UpdateBulkBrokerEmails).to receive(:broker_csv).and_return(
+        File.read("#{Rails.root}/spec/support/update_bulk_broker_emails_2021_10_5.csv")
+      )
+      Rails.application.load_tasks
+      Rake::Task['migrations:update_bulk_broker_emails'].invoke
+    end
 
-  #   it "should update the broker email" do
-  #     person.reload
-  #     expect(person.emails.where(address: new_broker_email_address).present?).to eq(true)
-  #   end
-  # end
+    it "should update the broker email" do
+      person.reload
+      expect(person.emails.where(address: new_broker_email_address).present?).to eq(true)
+    end
+  end
 end
