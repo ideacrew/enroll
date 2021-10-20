508ec01ca5231e7266abe9538ffa39e5ea67dc27
Mark Goho <markgoho@gmail.com>
2021-10-20 11:01:35 -0400

    update rspec-report workflow (#1170)

--- ci/scripts/rspec-split.ts
+++ ci/scripts/rspec-split.ts
@@ -1,38 +1,47 @@
 import { promises as fs } from 'fs';
-import { runtimeDictionary } from './concat-rspec-reports';
 
 import {
   FileWithRuntime,
   FileWithRuntimeDictionary,
   FileGroup,
+  RspecExample,
 } from './models';
-import { createFilesWithRuntime, splitFilesIntoGroups } from './util';
-import { runtimeDetails } from './util/numberOfGroups';
+
+import {
+  createFileDictionary,
+  createFilesWithRuntime,
+  splitFilesIntoGroups,
+} from './util';
+
+const REPORT_PATH = './ci/rspec/local-rspec-report.json';
+const SPLIT_CONFIG_PATH = './ci/rspec-split-config.json';
 
 async function createSplitConfig(): Promise<void> {
   // Read cli arguments
-  const [splitConfigPath] = process.argv.slice(2);
+  const rspecExamples = await fs.readFile(REPORT_PATH, 'utf-8');
+  const examples: RspecExample[] = JSON.parse(rspecExamples);
 
-  if (splitConfigPath === undefined) {
-    console.error('Missing cli arguments');
-    process.exit(1);
-  }
+  const [manualGroupCountInput] = process.argv.slice(2);
+
+  const groupCount: number | undefined =
+    manualGroupCountInput !== undefined
+      ? parseInt(manualGroupCountInput, 10)
+      : undefined;
 
   // Create a dictionary of
-  const filesByRuntime: FileWithRuntimeDictionary = await runtimeDictionary();
+  const filesByRuntime: FileWithRuntimeDictionary =
+    createFileDictionary(examples);
 
   const arrayOfSlowFiles: FileWithRuntime[] =
     createFilesWithRuntime(filesByRuntime);
 
-  const { suggestedGroupCount } = runtimeDetails(arrayOfSlowFiles);
-
   const splitConfig: FileGroup[] = splitFilesIntoGroups(
     arrayOfSlowFiles,
-    suggestedGroupCount
+    groupCount
   );
 
   try {
-    await fs.writeFile(`./${splitConfigPath}`, JSON.stringify(splitConfig));
+    await fs.writeFile(SPLIT_CONFIG_PATH, JSON.stringify(splitConfig));
   } catch (e) {
     console.error(e);
   }
