595c93aa252a841e6cbc8ecaf6cae30c33a55c30
Zane <zanesandin@gmail.com>
2021-11-02 18:57:35 -0400

    Execute family update if family member count has changed. (#1275)

--- app/models/concerns/crm_gateway/family_concern.rb
+++ app/models/concerns/crm_gateway/family_concern.rb
@@ -20,6 +20,8 @@ module CrmGateway
         previous_changes_attributes = family_member.person.previous_changes.reject { |key, _value| CRITICAL_CRM_ATTRIBUTES.exclude?(key) }
         relevant_previous_changes_array << previous_changes_attributes.merge!(hbx_id: family_member.person.hbx_id) unless previous_changes_attributes.blank?
       end
+      # Record family member count to update on family members
+      relevant_previous_changes_array << {family_member_count: self.family_members.count}
       self.relevant_previous_changes = relevant_previous_changes_array
     end
 
@@ -43,6 +45,8 @@ module CrmGateway
       return unless EnrollRegistry.feature_enabled?(:crm_update_family_save)
       # Only publish on critical changes
       current_previous_changes = self.relevant_previous_changes
+      previous_family_member_count = self.relevant_previous_changes.detect { |element| element.key?(:family_member_count) }[:family_member_count]
+      new_family_member_count = self.family_members.count
       new_previous_changes = []
       self.family_members.map do |member|
         critical_changes = member.person.previous_changes.reject { |key, _value| CRITICAL_CRM_ATTRIBUTES.exclude?(key) }
@@ -50,7 +54,10 @@ module CrmGateway
         next unless critical_changes.present?
         member_previous_changes_hash = current_previous_changes.detect { |attributes_hash| attributes_hash['hbx_id'] == member.person.hbx_id }
         new_previous_changes << critical_changes if member_previous_changes_hash != critical_changes
+        # Record family member count to update on family members
       end
+      # Consider it a change if family member count has changed
+      new_previous_changes << new_family_member_count if new_family_member_count != previous_family_member_count
       return nil if new_previous_changes.blank?
       puts("Triggering CRM family update publish for family with mongo id #{self.id}")
       result = ::Operations::Families::SugarCrm::PublishFamily.new.call(self)
