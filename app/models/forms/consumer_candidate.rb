5e911268a7adc4d28293ca6c5bd3774b4f5ac783
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-11-19 15:30:03 -0500

    [finishes #180375158] replace '.count?' w/ thorough check

--- app/models/forms/consumer_candidate.rb
+++ app/models/forms/consumer_candidate.rb
@@ -51,7 +51,7 @@ module Forms
                                                                     :first_name => first_name,
                                                                     :ssn => ssn})
 
-      return nil if records.count == 0
+      return nil if records.nil? || records.none?
 
       if (match_criteria == :dob_present && ssn.present? && records.first.employer_staff_roles?) ||
          (match_criteria == :dob_present && ssn.blank?) ||
