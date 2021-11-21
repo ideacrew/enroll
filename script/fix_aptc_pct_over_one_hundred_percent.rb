1845f369a74a8702140c345608b1436db096b249
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2021-11-21 08:19:25 -0500

    fix to aptc script breaking on run (#1351)
    Co-authored-by: Mark Goho <markgoho@gmail.com>

--- script/fix_aptc_pct_over_one_hundred_percent.rb
+++ script/fix_aptc_pct_over_one_hundred_percent.rb
@@ -14,6 +14,5 @@ CSV.open(report_name, "w", force_quotes: true) do |csv|
   affected_enrollments.each do |enrollment|
     enrollment.update_attributes!(elected_aptc_pct: 1.0)
     csv << [enrollment.hbx_id, enrollment.elected_aptc_pct, "Enrollment with hbx id #{enrollment.hbx_id} APTC % updated from #{enrollment.elected_aptc_pct} to 1.0"]
-    ran += 1
   end
 end
