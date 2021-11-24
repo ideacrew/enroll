c264f085e2bd08bf7f046ef528c2cf27672ce741
RyanEddyIC <ryan.eddy@ideacrew.com>
2021-11-24 11:56:58 -0500

    Add person hbx_id to csv output

--- script/fix_aptc_pct_over_one_hundred_percent.rb
+++ script/fix_aptc_pct_over_one_hundred_percent.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 report_name = "#{Rails.root}/enrollment_aptc_pct_list_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv"
 field_names = %w(
+        primary_hbx_id
         enrollment_hbx_id
         faulty_aptc_pct
         message
@@ -12,7 +13,9 @@ CSV.open(report_name, "w", force_quotes: true) do |csv|
 
   affected_enrollments = HbxEnrollment.with_aptc.where(:created_at => {"$gte" => Date.new(2021,3,1)}).where(:elected_aptc_pct => {"$gt" => 1.0})
   affected_enrollments.each do |enrollment|
+    affected_aptc = enrollment.elected_aptc_pct
+    primary_hbx_id = enrollment.family.primary_family_member.person.hbx_id
     enrollment.update_attributes!(elected_aptc_pct: 1.0)
-    csv << [enrollment.hbx_id, enrollment.elected_aptc_pct, "Enrollment with hbx id #{enrollment.hbx_id} APTC % updated from #{enrollment.elected_aptc_pct} to 1.0"]
+    csv << [primary_hbx_id, enrollment.hbx_id, enrollment.elected_aptc_pct, "Enrollment with primary family member hbx_id #{primary_hbx_id} APTC % updated from #{affected_aptc} to 1.0"]
   end
 end
