94889f64e11ff753421a84b600d4097562796242
Harshavardhan Reddy Chalamala <chvreddy1015@gmail.com>
2021-11-26 11:01:03 -0500

    code fix #180412223

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -378,7 +378,9 @@ class ApplicationController < ActionController::Base
 
     def set_bookmark_url(url=nil)
       set_current_person
-      return if current_user.has_hbx_staff_role?
+      return if current_user.has_hbx_staff_role? ||
+        current_user.has_broker_agency_staff_role? ||
+        current_user.has_broker_role?
       bookmark_url = url || request.original_url
       if /employee/.match(bookmark_url)
         role = @person.try(:employee_roles).try(:last)
@@ -390,7 +392,9 @@ class ApplicationController < ActionController::Base
 
     def set_employee_bookmark_url(url=nil)
       set_current_person
-      return if current_user.has_hbx_staff_role?
+      return if current_user.has_hbx_staff_role? ||
+        current_user.has_broker_agency_staff_role? ||
+        current_user.has_broker_role?
       role = @person.try(:employee_roles).try(:last)
       bookmark_url = url || request.original_url
       save_bookmark role, bookmark_url
@@ -399,7 +403,9 @@ class ApplicationController < ActionController::Base
 
     def set_consumer_bookmark_url(url=nil)
       set_current_person
-      return if current_user.has_hbx_staff_role?
+      return if current_user.has_hbx_staff_role? ||
+        current_user.has_broker_agency_staff_role? ||
+        current_user.has_broker_role?
       role = @person.try(:consumer_role)
       bookmark_url = url || request.original_url
       save_bookmark role, bookmark_url
@@ -408,7 +414,9 @@ class ApplicationController < ActionController::Base
 
     def set_resident_bookmark_url(url=nil)
       set_current_person
-      return if current_user.has_hbx_staff_role?
+      return if current_user.has_hbx_staff_role? ||
+        current_user.has_broker_agency_staff_role? ||
+        current_user.has_broker_role?
       role = @person.try(:resident_role)
       bookmark_url = url || request.original_url
       save_bookmark role, bookmark_url
@@ -417,7 +425,10 @@ class ApplicationController < ActionController::Base
 
     def save_faa_bookmark(url)
       current_person = get_current_person
-      return if current_person.consumer_role.blank? || current_user.has_hbx_staff_role?
+      return if current_person.consumer_role.blank? ||
+        current_user.has_hbx_staff_role? ||
+        current_user.has_broker_agency_staff_role? ||
+        current_user.has_broker_role?
       current_person.consumer_role.update_attribute(:bookmark_url, url) if current_person.consumer_role.identity_verified?
     end
 
