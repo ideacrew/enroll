9db74ccd8f89f3718d4a9476ff7d124d39b9e045
harshaellanki <h_vardhan@yahoo.com>
2021-11-29 16:08:46 -0500

    moved javascript to view file

--- app/assets/javascripts/consumer_role.js
+++ app/assets/javascripts/consumer_role.js
@@ -9,19 +9,3 @@ $(document).on('change', "#person_no_dc_address, #dependent_no_dc_address, #no_d
     $(this).parents('#address_info').find('.address_required').attr('required', true);
   };
 });
-
-$(document).on('ready', function(){
-    if(`EnrollRegistry.feature_enabled?(:ridp_h139) && Rails.env.production? && !(ENV['ENROLL_REVIEW_ENVIRONMENT'] == 'true')`) {
-      var path = "/insured/fdsh_ridp_verifications/new";
-    } else {
-      var path = "/insured/interactive_identity_verifications/new";
-    }
-    $(document).on("click", ".interaction-choice-control-value-agreement-agree", function(){
-        $(".interaction-click-control-continue").attr("href", path);
-        $('.aut_cons_text').parents('.row-form-wrapper').addClass('hide');
-    });
-    $(document).on("click", ".interaction-choice-control-value-agreement-disagree", function(){
-        $(".interaction-click-control-continue").attr("href", "/insured/consumer_role/upload_ridp_document");
-        $('.aut_cons_text').parents('.row-form-wrapper').removeClass('hide');
-    });
-});
