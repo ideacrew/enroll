334e19e3edb79fbfbb588aef716dd4efb0f57fb0
Zane Sandin <zanesandin@gmail.com>
2021-12-07 10:33:42 -0500

    Refactor resource registry to read from config folders and move less files.
    Remove root level system folder.
    
    Fixes some stuff.
    
    stuff
    
    stuff
    
    stuff
    
    stuff

--- spec/lib/custom_linters/client_config/client_config_storage_enforcer_spec.rb
+++ spec/lib/custom_linters/client_config/client_config_storage_enforcer_spec.rb
@@ -14,14 +14,6 @@ describe "Client Config Storage Enforcement" do
   let(:current_committed_config_stored_ymls) do
     Dir.glob("#{committed_config_folder}/system/**/*.yml")
   end
-  let(:current_committed_config_ymls) do
-    Dir.glob("#{Rails.root}/system/**/*.yml")
-  end
-  let(:warning_message) do
-    "Currently committed configuration does not match stored configuration." \
-    " Please run bundle exec rake configuration:store_current_configuration" \
-    " to store current configuration"
-  end
 
   let(:non_stored_client_locations) do
     Dir.glob("#{Rails.root}/config/client_config/**").reject { |folder| folder == committed_config_folder }
@@ -33,16 +25,6 @@ describe "Client Config Storage Enforcement" do
 
   let(:non_stored_client_system_folders) do
     non_stored_client_locations.select { |folder| "#{folder}/system" }
-  end
-
-  it "should not show any file differences between committed client and stored client" do
-    current_committed_config_ymls.each do |full_filepath|
-      filepath_without_root = full_filepath.sub("#{Rails.root}/", '')
-      stored_filepath = "#{committed_config_folder}/#{filepath_without_root}"
-      result = FileUtils.identical?(full_filepath, stored_filepath)
-      raise("#{warning_message}. Unstored changed file is #{full_filepath}") unless result == true
-      expect(result).to eq(true)
-    end
   end
 
   it "should not show any different keys between current config and OTHER client stored configs" do
