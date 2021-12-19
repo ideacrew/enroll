497a04e7dff29b5e6ceb55ac75b0c79c45ef08cd
Mark Goho <markgoho@gmail.com>
2021-12-19 14:51:07 -0500

    add pre-build script

--- .docker/build_gha.sh
+++ .docker/build_gha.sh
@@ -1,28 +1,3 @@
-# setup docker image config
-#cp Gemfile Gemfile.tmp
-#cp Gemfile.lock Gemfile.lock.tmp
-cp config/cable.yml config/cable.yml.tmp
-cp config/mongoid.yml config/mongoid.yml.tmp
-cp config/saml.yml config/saml.yml.tmp
-#cp config/symmetric-encryption.yml config/symmetric-encryption.yml.tmp
-cp config/environments/production.rb config/environments/production.rb.tmp
-cp config/initializers/devise.rb config/initializers/devise.rb.tmp
-cp config/initializers/redis.rb config/initializers/redis.rb.tmp
-cp app/models/saml_information.rb app/models/saml_information.rb.tmp
-
-#cp .docker/config/Gemfile .
-#cp .docker/config/Gemfile.lock .
-cp .docker/config/puma.rb config/
-cp .docker/config/cable.yml config/
-cp .docker/config/mongoid.yml config/
-cp .docker/config/saml.yml config/
-cp .docker/config/saml_information.rb app/models/
-#cp .docker/config/symmetric-encryption.yml config/
-cp .docker/config/production.rb config/environments/
-cp .docker/config/devise.rb config/initializers/
-cp .docker/config/redis.rb config/initializers/
-cp .docker/config/sidekiq.rb config/initializers/
-
 docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.0.1' \
              --build-arg NODE_MAJOR='12' \
              --build-arg YARN_VERSION='1.22.4' \
@@ -45,17 +20,5 @@ docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.0.1' \
 	     --build-arg CLIENT="me" \
              --build-arg SECRET_KEY_BASE="c8d2b9b204fbac78081a88a2c29b28cfeb82e6ccd3664b3948b813463b5917b315dbbd3040e8dffcb5b68df427099db0ce03e59e2432dfe5d272923b00755b82" \
              -f .docker/production/Dockerfile --target app -t $2:$1 --network="host" .
-# docker push $2:$1
 
-#mv Gemfile.tmp Gemfile
-#mv Gemfile.lock.tmp Gemfile.lock
-mv config/cable.yml.tmp config/cable.yml
-mv config/mongoid.yml.tmp config/mongoid.yml
-mv config/saml.yml.tmp config/saml.yml
-mv app/models/saml_information.rb.tmp  app/models/saml_information.rb
-#mv config/symmetric-encryption.yml.tmp config/symmetric-encryption.yml
-mv config/environments/production.rb.tmp config/environments/production.rb
-mv config/initializers/devise.rb.tmp config/initializers/devise.rb
-mv config/initializers/redis.rb.tmp config/initializers/redis.rb
-rm config/puma.rb
-rm config/initializers/sidekiq.rb
+
