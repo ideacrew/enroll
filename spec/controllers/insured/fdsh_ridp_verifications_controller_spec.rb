04a0af9430035821e30fe870e748419c4db41999
Eidarus Omar <eidarus.omar@ideacrew.com>
2021-12-21 10:28:07 -0500

    rubo

--- spec/controllers/insured/fdsh_ridp_verifications_controller_spec.rb
+++ spec/controllers/insured/fdsh_ridp_verifications_controller_spec.rb
@@ -1,3 +1,5 @@
+#frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Insured::FdshRidpVerificationsController do
@@ -7,8 +9,8 @@ describe Insured::FdshRidpVerificationsController do
     let!(:primary_event) { ::Fdsh::Ridp::EligibilityResponseModel.create(event_kind: 'primary', primary_member_hbx_id: person.hbx_id) }
 
     it 'finds a primary response' do
-        controller.instance_variable_set(:@person, person)
-        expect(controller.find_response('primary')).to eq(primary_event)
+      controller.instance_variable_set(:@person, person)
+      expect(controller.find_response('primary')).to eq(primary_event)
     end
   end
 
