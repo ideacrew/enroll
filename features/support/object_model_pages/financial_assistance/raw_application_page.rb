0ba9b37b1ef2e171add74849b0382d97beb2af06
Jay Reddy <jayanth.rordev@gmail.com>
2021-12-22 14:33:27 -0500

    Add new page object model

---
+++ features/support/object_model_pages/financial_assistance/raw_application_page.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Financial Assistance Raw/Full application form
+module FinancialAssistance
+  # For Full Application page
+  class RawApplicationPage
+
+    def self.claimed_as_dependent_text
+      'This person will be claimed as a dependent by'
+    end
+  end
+end
