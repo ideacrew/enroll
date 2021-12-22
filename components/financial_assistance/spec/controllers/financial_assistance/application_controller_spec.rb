415f6d9dbf391ec6018bf1bde01296fc89756aff
Jacob Kagon <jacob@kagon.net>
2021-12-22 10:45:47 -0500

    fix application_controller_spec

--- components/financial_assistance/spec/controllers/financial_assistance/application_controller_spec.rb
+++ components/financial_assistance/spec/controllers/financial_assistance/application_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe FinancialAssistance::ApplicationController, dbclean: :after_each, type: :controller do
   let(:person) { FactoryBot.create(:person, :with_consumer_role)}
-  let!(:user) { FactoryBot.create(:user, :person => person) }
+  let(:user) { FactoryBot.create(:user, :person => person) }
   let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person) }
   let(:family_id) { family.id}
   let(:application) { FactoryBot.create(:application, family_id: family_id, effective_date: TimeKeeper.date_of_record) }
@@ -12,16 +12,19 @@ RSpec.describe FinancialAssistance::ApplicationController, dbclean: :after_each,
 
   describe "#find_application" do
     context "hbx_admin" do
-      let(:admin_user) { FactoryBot.create(:user, person: admin_person) }
-      let(:admin_person) { FactoryBot.create(:person, :with_hbx_staff_role) }
+      let!(:admin_person) { FactoryBot.create(:person) }
+      let!(:admin_user) { FactoryBot.create(:user, person: admin_person) }
 
       before do
+        allow(admin_user).to receive(:try).with(:person).and_return(admin_person)
+        allow(admin_person).to receive(:hbx_staff_role).and_return(true)
         sign_in(admin_user)
+        allow(controller).to receive(:current_user).and_return(admin_user)
         allow(controller).to receive(:params).and_return(application_id: application.id)
-        allow(controller).to receive(:session).and_return(person_id: person.id)
       end
 
       it "finds the correct application" do
+        admin_user
         expect(controller.send(:find_application)).to eq(application)
       end
     end
