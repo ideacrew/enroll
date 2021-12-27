861fcccb41be073ec91096ec5b814a18b9eb5d78
Bill Transue <transue@gmail.com>
2021-12-27 12:12:17 -0500

    Fix for income fix (#1514)
    * Fix for income fix
    
    * add spec for nil income
    
    Co-authored-by: Eidarus Omar <eidarus.omar@ideacrew.com>

--- components/financial_assistance/spec/models/financial_assistance/applicant_spec.rb
+++ components/financial_assistance/spec/models/financial_assistance/applicant_spec.rb
@@ -515,6 +515,22 @@ RSpec.describe ::FinancialAssistance::Applicant, type: :model, dbclean: :after_e
         it 'should validate applicant as complete' do
           expect(applicant.applicant_validation_complete?).to eq true
         end
+
+        context 'has nil income' do
+          before do
+            applicant.update_attributes!({has_job_income: nil,
+                                          has_self_employment_income: nil,
+                                          has_other_income: nil,
+                                          has_unemployment_income: nil})
+            applicant.incomes << income
+            applicant.incomes.first.update_attributes(amount: nil)
+          end
+
+          it "shouldn't validate applicant as complete" do
+            expect(applicant.applicant_validation_complete?).to eq false
+          end
+        end
+
       end
 
       context 'has_medicare_cubcare_eligible feature enabled' do
