ea7a1b170c91d4ff27283536261ffec00c9ee1c7
Bill Transue <transue@gmail.com>
2021-12-30 19:11:07 -0500

    Fix 0.0 income validation

--- components/financial_assistance/spec/models/financial_assistance/applicant_spec.rb
+++ components/financial_assistance/spec/models/financial_assistance/applicant_spec.rb
@@ -531,6 +531,24 @@ RSpec.describe ::FinancialAssistance::Applicant, type: :model, dbclean: :after_e
           end
         end
 
+        context 'has 0.00 income' do
+          before do
+            allow(FinancialAssistanceRegistry).to receive(:feature_enabled?).with(:has_medicare_cubcare_eligible).and_return(false)
+            allow(applicant).to receive(:medicare_eligible_qns).and_return(true)
+            allow(applicant).to receive(:valid?).and_return(true)
+            applicant.update_attributes!({has_job_income: nil,
+                                          has_self_employment_income: nil,
+                                          has_other_income: nil,
+                                          has_unemployment_income: nil})
+            applicant.incomes << income
+            applicant.incomes.first.update_attributes(amount: Money.new('0.00'))
+          end
+
+          it "validates $0 incomes" do
+            expect(applicant.applicant_validation_complete?).to eq true
+          end
+        end
+
       end
 
       context 'has_medicare_cubcare_eligible feature enabled' do
