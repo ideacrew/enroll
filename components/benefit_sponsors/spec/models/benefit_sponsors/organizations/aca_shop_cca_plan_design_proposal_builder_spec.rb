6024e6c4617f65be60158522e56896711959e1d9
Mark Goho <markgoho@gmail.com>
2022-01-02 14:22:37 -0500

    run all rspecs

--- components/benefit_sponsors/spec/models/benefit_sponsors/organizations/aca_shop_cca_plan_design_proposal_builder_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module BenefitSponsors
-  RSpec.describe Organizations::AcaShopCcaPlanDesignProposalBuilder, type: :model, :dbclean => :after_each do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
