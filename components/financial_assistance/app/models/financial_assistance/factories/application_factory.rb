4ac933f7e628d9df56346c10d06e5a4b7290733d
Sai Mekala <saimekala07@gmail.com>
2022-01-03 10:44:51 -0500

    show updated data in Copy New Application Feature (#1534)
    * ME_180767570: Fix copy new IAP application feature to pick updated family member info.
    
    Pick latest draft application when family has two draft iap application.
    Adding specs.
    
    * Fix Operation
    
    * Fixing rubocop
    
    * Uncomment method
    
    * Add spec for relationship updates

--- components/financial_assistance/app/models/financial_assistance/factories/application_factory.rb
+++ components/financial_assistance/app/models/financial_assistance/factories/application_factory.rb
@@ -83,6 +83,9 @@ module FinancialAssistance
       end
 
       def detect_applicant_changes(family_members)
+        # Update: family members with the applicants
+        update_existing_applicants(family_members)
+
         # ADDS: family members that are not applicants
         add_members = family_members.reduce([]) do |ids, family_member|
           ids << family_member[:family_member_id] unless @new_application.applicants.where(family_member_id: family_member[:family_member_id]).present?
@@ -108,6 +111,19 @@ module FinancialAssistance
             @family_members_changed = true
           end
           # Have to update relationship separately because the applicant should already be persisted before doing this.
+          if member_params[:relationship].present? && member_params[:relationship] != 'self'
+            applicant.relationship = member_params[:relationship]
+            applicant.save!
+          end
+        end
+      end
+
+      def update_existing_applicants(family_members)
+        family_members.each do |member_params|
+          applicant_params = member_params.except(:relationship)
+          applicant = @new_application.applicants.where(person_hbx_id: applicant_params.to_h[:person_hbx_id]).first
+          next unless applicant.present?
+          applicant.update_attributes!(applicant_params)
           if member_params[:relationship].present? && member_params[:relationship] != 'self'
             applicant.relationship = member_params[:relationship]
             applicant.save!
