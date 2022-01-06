3bfd3a1a4b722317359ab3791cea74769d177ee3
Jay Reddy <jayanth.rordev@gmail.com>
2022-01-06 13:44:37 -0500

    cuke fix

--- features/insured/step_definitions/individual_curam_document_steps.rb
+++ features/insured/step_definitions/individual_curam_document_steps.rb
@@ -4,7 +4,7 @@ When(/^the user navigates to the DOCUMENTS tab$/) do
 end
 
 Then(/^a button will be visible to the user labeled MEDICAID & TAX CREDITS$/) do
-  expect(page).to have_content('Medicaid & Tax Credits')
+  expect(page).to have_content(AdminHomepage.medicaid_button_text)
 end
 
 When(/^MEDICAID & TAX CREDITS button is visible to the user$/) do
