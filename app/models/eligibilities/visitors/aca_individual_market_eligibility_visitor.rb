7a6f82dbe061d0671dcf6a0cf89d714df6c307a2
Raghuram Ganjala <raghuramg83@gmail.com>
2022-01-06 13:58:31 -0500

    Added IVL outstanding verification report and fixed bug in ivl eligibility visitor

--- app/models/eligibilities/visitors/aca_individual_market_eligibility_visitor.rb
+++ app/models/eligibilities/visitors/aca_individual_market_eligibility_visitor.rb
@@ -12,7 +12,7 @@ module Eligibilities
       end
 
       def visit(verification_type)
-        return unless verification_type.type_name == evidence_item.key.camelcase
+        return unless verification_type.type_name.downcase == evidence_item.titleize.downcase
 
         evidence_state_attributes = {
           status: status_for(verification_type),
