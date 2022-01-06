69628d6fe3f05f7dc9f21c889335c35c4e0d5ac4
Jay Reddy <jayanth.rordev@gmail.com>
2022-01-06 13:20:22 -0500

    cuke fix

--- features/insured/step_definitions/individual_curam_document_steps.rb
+++ features/insured/step_definitions/individual_curam_document_steps.rb
@@ -8,19 +8,19 @@ Then(/^a button will be visible to the user labeled MEDICAID & TAX CREDITS$/) do
 end
 
 When(/^MEDICAID & TAX CREDITS button is visible to the user$/) do
-  find_link('Medicaid & Tax Credits').visible?
+  find(IvlDocumentsPage.medicare_and_tax_credit_btn).visible?
 end
 
 Then(/^there will be text to the left of the MEDICAID & TAX CREDITS button$/) do
-  expect(page).to have_content('If you applied for Medicaid and tax credit savings, view additional documents')
+  expect(page).to have_content(l10n("insured.families.apply_for_medicaid_widget"))
 end
 
 Then(/^there will be messages text to the left of the MEDICAID & TAX CREDITS button$/) do
-  expect(page).to have_content('If you applied for Medicaid and tax credit savings, view additional messages')
+  expect(page).to have_content(l10n("insured.families.apply_for_medicaid_widget"))
 end
 
 When(/^the user clicks on the MEDICAID & TAX CREDITS button$/) do
-  switch_to_window { find('.btn', text: 'Medicaid & Tax Credits').click }
+  switch_to_window { find('.btn', text: l10n("insured.families.medicaid_and_tax_credits")).click }
 end
 
 Then(/^EA sets a flag in IAM to direct the consumer to the curam\/ drupal login$/) do
