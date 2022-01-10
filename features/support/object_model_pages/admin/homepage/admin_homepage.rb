d77a0aea4d4eb923a89520c954490731773378ea
Jay Reddy <jayanth.rordev@gmail.com>
2022-01-10 09:19:17 -0500

    little tweaks

--- features/support/object_model_pages/admin/homepage/admin_homepage.rb
+++ features/support/object_model_pages/admin/homepage/admin_homepage.rb
@@ -110,8 +110,4 @@ class AdminHomepage
   def self.medicaid_banner_text
     '[data-cuke="medicaid-banner-text"]'
   end
-
-  def self.medicaid_button_text
-    '[data-cuke="medicaid-button-text"]'
-  end
 end
