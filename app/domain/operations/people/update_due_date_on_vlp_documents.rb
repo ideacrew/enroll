49dcab192e407316469028b184657bdc81ca183a
Battula <vrbattula@outlook.com>
2022-01-11 11:43:54 -0500

    Trigger callbacks on Person after updating due date

--- app/domain/operations/people/update_due_date_on_vlp_documents.rb
+++ app/domain/operations/people/update_due_date_on_vlp_documents.rb
@@ -33,9 +33,12 @@ module Operations
 
       def update_due_date(family, due_date)
         family.contingent_enrolled_active_family_members.each do |family_member|
-          family_member.person.verification_types.active.each do |verification_type|
-            verification_type.update_attributes!(due_date: due_date, due_date_type: 'notice') if can_update_due_date?(verification_type)
-          end
+          updated_verification_dates =
+            family_member.person.verification_types.active.all? do |verification_type|
+              verification_type.update_attributes!(due_date: due_date, due_date_type: 'notice') if can_update_due_date?(verification_type)
+            end
+
+          family_member.person.save! if updated_verification_dates
         end
         Success(true)
       end
