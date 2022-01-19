5e20ab9b69b36ead9e2f8cddacc6eaed8db03c89
Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>
2022-01-19 16:58:20 -0500

    add conditional for applicant aasm state and active enrollments (#1581)
    * check for if coverage household is in the applicant state and has enrollments
    
    * fix coverage household spec
    
    Co-authored-by: Bill Transue <transue@gmail.com>

--- app/models/coverage_household.rb
+++ app/models/coverage_household.rb
@@ -164,9 +164,7 @@ class CoverageHousehold
 
   def evaluate_individual_market_eligiblity
     eligibility_ruleset = ::RuleSet::CoverageHousehold::IndividualMarketVerification.new(self)
-    if eligibility_ruleset.applicable?
-      self.send(eligibility_ruleset.determine_next_state)
-    end
+    self.send(eligibility_ruleset.determine_next_state) if eligibility_ruleset.applicable? && self.aasm_state != "applicant"
   end
 
   def active_individual_enrollments
