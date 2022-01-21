a42f721e29abe7ecc6d22746fb188d85f9047700
Mark Goho <markgoho@gmail.com>
2022-01-21 14:23:33 -0500

    simplify build args

--- .docker/check-encryption.sh
+++ .docker/check-encryption.sh
@@ -1,4 +1,4 @@
-if echo 'input = {value: "Hello world"}; AcaEntities::Operations::Encryption::Encrypt.new.call(input)' | rails c | grep -q 'Success("'; then
+if echo 'input = {value: "Hello world"}; AcaEntities::Operations::Encryption::Encrypt.new.call(input)' | bundle exec rails c | grep -q 'Success("'; then
     echo "Encryption works"
     exit 0
 else
