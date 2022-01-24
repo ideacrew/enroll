f8ae972be7a3c44e56ddfe3b15810fe333e86ae7
Harshavardhan Ellanki <h_vardhan@yahoo.com>
2022-01-24 12:32:57 -0500

    update income evidence with request results (#1585)

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/ifsv/h9t/ifsv_eligibility_determination_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/ifsv/h9t/ifsv_eligibility_determination_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 require "#{FinancialAssistance::Engine.root}/spec/shared_examples/ifsv/test_ifsv_eligibility_response"
 
 RSpec.describe ::FinancialAssistance::Operations::Applications::Ifsv::H9t::IfsvEligibilityDetermination, dbclean: :after_each do
+  include_context 'FDSH IFSV sample response'
+
   before :all do
     DatabaseCleaner.clean
   end
@@ -24,17 +26,27 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Ifsv::H9t::IfsvE
                       eligibility_determination_id: ed.id,
                       person_hbx_id: '1629165429385938',
                       is_primary_applicant: true,
-                      first_name: 'esi',
+                      first_name: 'Income',
                       last_name: 'evidence',
-                      ssn: "518124854",
+                      ssn: "111111111",
                       dob: Date.new(1988, 11, 11),
+                      application: application)
+  end
+
+  let!(:applicant2) do
+    FactoryBot.create(:financial_assistance_applicant,
+                      eligibility_determination_id: ed.id,
+                      person_hbx_id: '1629165429385939',
+                      is_primary_applicant: true,
+                      first_name: 'Non Income',
+                      last_name: 'evidence',
+                      ssn: "222222222",
+                      dob: Date.new(1989, 11, 11),
                       application: application)
   end
 
   context 'success' do
     context 'FTI Ifsv eligible response' do
-      include_context 'FDSH IFSV sample response'
-
       before do
         @applicant = application.applicants.first
         @result = subject.call(payload: response_payload)
@@ -54,19 +66,26 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Ifsv::H9t::IfsvE
         expect(income_evidence.verification_outstanding).to be_falsey
         expect(income_evidence.due_on).to be_blank
         expect(income_evidence.is_satisfied).to eq true
+        expect(income_evidence.request_results.present?).to eq true
         expect(@result.success).to eq('Successfully updated Applicant with evidence')
       end
     end
 
-    context 'FTI Ifsv ineligible response' do
-      include_context 'FDSH IFSV sample response'
+    context "applicant without evidence" do
+      it 'should log an error if no income evidence present for an applicant' do
+        log_message = "Income Evidence Not Found for applicant with person_hbx_id: 1629165429385939 in application with hbx_id: 200000126"
+        expect(Rails.logger).to receive(:error).at_least(:once).with(log_message)
+        subject.call(payload: response_payload)
+      end
+    end
 
+    context 'FTI Ifsv ineligible response' do
       before do
         @applicant = application.applicants.first
         @result = subject.call(payload: response_payload_2)
 
-        @application = ::FinancialAssistance::Application.by_hbx_id(response_payload[:hbx_id]).first.reload
-        @app_entity = ::AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(response_payload).success
+        @application = ::FinancialAssistance::Application.by_hbx_id(response_payload_2[:hbx_id]).first.reload
+        @app_entity = ::AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(response_payload_2).success
       end
 
       it 'should return success' do
@@ -79,6 +98,7 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Ifsv::H9t::IfsvE
         expect(income_evidence.outstanding?).to be_truthy
         expect(income_evidence.verification_outstanding).to be_truthy
         expect(income_evidence.is_satisfied).to eq false
+        expect(income_evidence.request_results.present?).to eq true
         expect(@result.success).to eq('Successfully updated Applicant with evidence')
       end
     end
