5305f8089e7ab37da9215896dc54cb0283640853
Harshavardhan Ellanki <h_vardhan@yahoo.com>
2022-02-09 09:44:11 -0500

    update only response applicant based on the identifier in payload (#1647)
    * Update only response applicant based on the identifier in payload
    
    * dependent service spec fix

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/rrv/medicare/add_rrv_medicare_determination.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/rrv/medicare/add_rrv_medicare_determination.rb
@@ -20,7 +20,7 @@ module FinancialAssistance
             def call(params)
               application_entity = yield initialize_application_entity(params[:payload])
               application = yield find_application(application_entity)
-              result = yield update_applicant(application_entity, application)
+              result = yield update_applicant(application_entity, application, params[:applicant_identifier])
 
               Success(result)
             end
@@ -36,18 +36,15 @@ module FinancialAssistance
               application.present? ? Success(application) : Failure("Could not find application with given hbx_id: #{application_entity.hbx_id}")
             end
 
-            def update_applicant(response_app_entity, application)
-              response_app_entity.applicants.each do |response_applicant_entity|
-                applicant = find_matching_applicant(application, response_applicant_entity)
-                update_applicant_verifications(applicant, response_applicant_entity)
-              end
-              Success('Successfully updated Applicant with evidences and verifications')
-            end
+            def update_applicant(response_app_entity, application, applicant_identifier)
+              response_applicant = response_app_entity.applicants.detect {|applicant| applicant.person_hbx_id == applicant_identifier}
+              applicant = application.applicants.where(person_hbx_id: applicant_identifier).first
 
-            def find_matching_applicant(application, res_applicant_entity)
-              application.applicants.detect do |applicant|
-                applicant.person_hbx_id == res_applicant_entity.person_hbx_id
-              end
+              return Failure("applicant not found with #{applicant_identifier} for rrv Medicare") unless applicant
+              return Failure("applicant not found in response with #{applicant_identifier} for rrv Medicare") unless response_applicant
+
+              update_applicant_verifications(applicant, response_applicant)
+              Success('Successfully updated Applicant with evidences and verifications')
             end
 
             def update_applicant_verifications(applicant, response_applicant_entity)
@@ -56,10 +53,9 @@ module FinancialAssistance
 
               if applicant_non_esi_evidence.present?
                 if response_non_esi_evidence.aasm_state == 'outstanding'
-                  applicant_non_esi_evidence.move_to_outstanding!
-                  applicant_non_esi_evidence.update!(verification_outstanding: true)
+                  applicant.set_evidence_outstanding(applicant_non_esi_evidence)
                 else
-                  applicant_non_esi_evidence.update!(is_satisfied: true)
+                  applicant.set_evidence_verified(applicant_non_esi_evidence)
                 end
 
                 if response_non_esi_evidence.request_results.present?
