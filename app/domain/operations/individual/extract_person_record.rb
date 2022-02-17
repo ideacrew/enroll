bc610d05375ac71fdad727d3306810d0933991fe
ymhari <hariym471@gmail.com>
2022-02-16 19:16:00 -0500

    181147353 Operation refactor and helper.

--- app/domain/operations/individual/extract_person_record.rb
+++ app/domain/operations/individual/extract_person_record.rb
@@ -5,15 +5,17 @@ require 'dry/monads/do'
 
 module Operations
   module Individual
-    class ExtractPersonRecord
+    class FetchExistingCoverages
       send(:include, Dry::Monads[:result, :do])
 
-      def call(enrollment_id:)
-        enrollment           =  yield fetch_enrollment(enrollment_id)
+      def call(params)
+        enrollment           =  yield fetch_enrollment(params[:enrollment_id])
         enrollment_members   =  yield fetch_enrollment_members(enrollment)
 
-        family_ids           =  yield fetch_family_ids(enrollment_members)
-        prior_enrollments    =  yield prior_enrollments(family_ids)
+        family_ids           =  yield fetch_all_family_ids(enrollment_members)
+        prior_enrollments    =  yield prior_enrollments(family_ids, enrollment)
+
+        Success(prior_enrollments)
       end
 
       private
@@ -28,30 +30,23 @@ module Operations
 
       def fetch_enrollment_members(enrollment)
         members = enrollment.hbx_enrollment_members
-        if members&.present?
-          Success(members)
-        else
-          Failure('Hbx Enrollment members not found')
-        end
+        members.count > 0 ? Success(members) : Failure('Enrollment does not include dependents')
       end
 
-      def fetch_family_ids(enrollment_members)
+      def fetch_all_family_ids(enrollment_members)
         family_ids = []
-        enrollment_members.each do |hem|
-          person = hem.person
-          family_ids << Family.find_all_by_person(person).pluck(:id)
+        enrollment_members.each do |enr_member|
+          person = enr_member.person
+          family_ids << Family.find_all_by_person(person).pluck(:id).to_s
         end
-        family_ids.uniq
+        family_ids.count > 0 ? Success(family_ids.uniq) : Failure('No families found for members')
       end
 
-      def prior_enrollments(family_ids)
-        enrollments_list = []
-        family_ids.each do |fam_id|
-          enrollments = HbxEnrollment.where({
-                                        :family_id => fam_id.to_s
-                                      }).enrolled_and_renewing
-       end
-       enrollments_list
+      def prior_enrollments(family_ids, enrollment)
+        all_enrollments = HbxEnrollment.where({:family_id.in => family_ids, coverage_kind: enrollment.coverage_kind, market_kind: enrollment.market_kind}).enrolled_and_renewing
+        existing_enrollments = all_enrollments - enrollment
+
+        Success(existing_enrollments)
       end
     end
   end
