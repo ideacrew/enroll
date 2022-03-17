c2ac93a92be4ec2e6dc3885ca44d7935aef1f025
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2022-03-17 11:30:13 -0400

    fix reinstatement state after drop issue and fail if aptc exists issue (#1693)
    * fix drop failing if past enrollment has aptc
    
    * update reinstatement state after drop

--- spec/domain/operations/hbx_enrollments/drop_enrollment_members_spec.rb
+++ spec/domain/operations/hbx_enrollments/drop_enrollment_members_spec.rb
@@ -56,19 +56,58 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
     end
 
     context 'when members are selected for drop', dbclean: :around_each do
-      before do
-        @dropped_members = subject.call({hbx_enrollment: enrollment,
-                                         options: {"termination_date_#{enrollment.id}" => TimeKeeper.date_of_record.to_s,
-                                                   "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}}).success
+      context 'when previous enrollment has 0 applied aptc' do
+        before do
+          @dropped_members = subject.call({hbx_enrollment: enrollment,
+                                           options: {"termination_date_#{enrollment.id}" => TimeKeeper.date_of_record.to_s,
+                                                     "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}}).success
+        end
+
+        it 'should return dropped member info' do
+          expect(@dropped_members.first[:hbx_id]).to eq hbx_enrollment_member3.id.to_s
+          expect(@dropped_members.first[:terminated_on]).to eq TimeKeeper.date_of_record
+        end
+
+        it 'should terminate previously existing enrollment' do
+          expect(enrollment.aasm_state).to eq 'coverage_terminated'
+        end
+
+        it 'should select coverage for the reinstatement' do
+          expect(family.hbx_enrollments.last.aasm_state).to eq 'coverage_selected'
+        end
       end
 
-      it 'should return dropped member info' do
-        expect(@dropped_members.first[:hbx_id]).to eq hbx_enrollment_member3.id.to_s
-        expect(@dropped_members.first[:terminated_on]).to eq TimeKeeper.date_of_record
+      context 'when previous enrollment has applied aptc' do
+        before do
+          enrollment.update_attributes!(applied_aptc_amount: 100)
+          FactoryBot.create(:tax_household, household: family.active_household, effective_starting_on: enrollment.effective_on)
+          @dropped_members = subject.call({hbx_enrollment: enrollment,
+                                           options: {"termination_date_#{enrollment.id}" => TimeKeeper.date_of_record.to_s,
+                                                     "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}}).success
+        end
+
+        it 'should return dropped member info' do
+          expect(@dropped_members.first[:hbx_id]).to eq hbx_enrollment_member3.id.to_s
+          expect(@dropped_members.first[:terminated_on]).to eq TimeKeeper.date_of_record
+        end
       end
 
-      it 'should terminate previously existing enrollment' do
-        expect(enrollment.aasm_state).to eq 'coverage_terminated'
+      context 'when termination date is in the past' do
+        before do
+          FactoryBot.create(:tax_household, household: family.active_household, effective_starting_on: enrollment.effective_on)
+          @dropped_members = subject.call({hbx_enrollment: enrollment,
+                                           options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record - 30.days).to_s,
+                                                     "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}}).success
+        end
+
+        it 'should return dropped member info' do
+          expect(@dropped_members.first[:hbx_id]).to eq hbx_enrollment_member3.id.to_s
+          expect(@dropped_members.first[:terminated_on]).to eq TimeKeeper.date_of_record - 30.days
+        end
+
+        it 'should begin coverage for the reinstatement' do
+          expect(family.hbx_enrollments.last.aasm_state).to eq 'coverage_enrolled'
+        end
       end
     end
 
