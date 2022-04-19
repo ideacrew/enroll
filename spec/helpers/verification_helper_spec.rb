e98f32149408851d39c4cf6c971b09da7e96cfa6
Nisanth Yaganti <yagantinisanth@gmail.com>
2022-04-19 11:03:02 -0400

    Identity Documents - Consumer Application Rejected Status (#1756)
    * adding rejected status to evidence (#1742)
    
    * Added Rejected status for legacy verifications (#1740)
    
    * 181783314: wip, rejected status for legacy verification types
    
    * remove redundant else
    
    * 181783314 - fix failing spec
    
    * 181783314 - tests
    
    * 181783314 - code refactor to update verification type.
    
    * 181783314 - update tests
    
    * 181783314 - update spec for VerificationType
    
    * Display action needed for rejected status for VerificationType & Evidence (#1745)
    
    * 181791800 - display action needed for rejected status for VerificationType & Evidence.
    * 181791800 - minor rspec refactor
    
    * Reports people with any verification_type or evidence in rejected status (#1750)
    
    * 181792077 - report: people with any verification_type or evidence in rejected status
    
    * 181792077: adds missing translations.
    
    * 181792077: change CSV generation location.
    
    * fix failing spec
    
    * 181792077 - adds first_name and phone_number columns for the report
    
    * setting family status as outstanding when evidence is rejected (#1754)
    
    Co-authored-by: Harshavardhan Ellanki <h_vardhan@yahoo.com>
    
    * adding rejected status to identity verifications
    
    Co-authored-by: Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
    Co-authored-by: Harshavardhan Ellanki <h_vardhan@yahoo.com>

--- spec/helpers/verification_helper_spec.rb
+++ spec/helpers/verification_helper_spec.rb
@@ -24,14 +24,15 @@ RSpec.describe VerificationHelper, :type => :helper do
     let(:types) { ['Identity', 'Application'] }
     shared_examples_for 'ridp type status' do |current_state, ridp_type, uploaded_doc, status|
       before do
-        uploaded_doc ? person.consumer_role.ridp_documents << FactoryBot.build(:ridp_document, :ridp_verification_type => ridp_type) : person.consumer_role.ridp_documents = []
-        if current_state == "valid"
-          person.consumer_role.identity_validation = "valid"
-          person.consumer_role.application_validation = "valid"
-        else
-          person.consumer_role.identity_validation = "outstanding"
-          person.consumer_role.application_validation = "outstanding"
-        end
+        consumer_role = person.consumer_role
+        uploaded_doc ? consumer_role.ridp_documents << FactoryBot.build(:ridp_document, :ridp_verification_type => ridp_type) : consumer_role.ridp_documents = []
+        is_rejected = current_state == 'rejected'
+        consumer_role.assign_attributes(
+          identity_validation: current_state,
+          application_validation: current_state,
+          identity_rejected: is_rejected,
+          application_rejected: is_rejected
+        )
       end
       it "returns #{status} status for #{ridp_type} #{uploaded_doc ? 'with uploaded doc' : 'without uploaded docs'}" do
         expect(helper.ridp_type_status(ridp_type, person)).to eq status
@@ -41,9 +42,13 @@ RSpec.describe VerificationHelper, :type => :helper do
       it_behaves_like 'ridp type status', 'outstanding', 'Identity', false, 'outstanding'
       it_behaves_like 'ridp type status', 'valid', 'Identity', false, 'valid'
       it_behaves_like 'ridp type status', 'outstanding', 'Identity', true, 'in review'
+      it_behaves_like 'ridp type status', 'rejected', 'Identity', true, 'rejected'
+      it_behaves_like 'ridp type status', 'rejected', 'Identity', false, 'rejected'
       it_behaves_like 'ridp type status', 'outstanding', 'Application', false, 'outstanding'
       it_behaves_like 'ridp type status', 'valid', 'Application', false, 'valid'
       it_behaves_like 'ridp type status', 'outstanding', 'Application', true, 'in review'
+      it_behaves_like 'ridp type status', 'rejected', 'Application', true, 'rejected'
+      it_behaves_like 'ridp type status', 'rejected', 'Application', false, 'rejected'
     end
   end
 
