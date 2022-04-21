1c24ce3426f38c2512d990135dc40ee20fe3d07d
ymhari <hariym471@gmail.com>
2022-04-21 15:56:01 -0400

    Updates for subscriber drop and edi transactions.

--- app/domain/operations/hbx_enrollments/drop_enrollment_members.rb
+++ app/domain/operations/hbx_enrollments/drop_enrollment_members.rb
@@ -6,6 +6,8 @@ module Operations
     class DropEnrollmentMembers
       include Dry::Monads[:result, :do]
 
+      attr_reader :new_effective_date, :termination_date, :base_enrollment, :new_enrollment
+
       # @param [ HbxEnrollment ] hbx_enrollment
       # @return [ Array ] dropped_members
       def call(params)
@@ -21,42 +23,45 @@ module Operations
         return Failure('Member drop feature is turned off.') unless EnrollRegistry.feature_enabled?(:drop_enrollment_members)
         return Failure('Missing HbxEnrollment Key.') unless params.key?(:hbx_enrollment)
         return Failure('Not a valid HbxEnrollment object.') unless params[:hbx_enrollment].is_a?(HbxEnrollment)
-        return Failure('Enrollment need be in an active state to drop dependent') unless params[:hbx_enrollment].is_admin_terminate_eligible?
-        return Failure('Not an ivl enrollment.') unless params[:hbx_enrollment].is_ivl_by_kind? # Added per the request, also need further development if to be used for shop
+        @base_enrollment = params[:hbx_enrollment]
+        return Failure('Enrollment need be in an active state to drop dependent') unless base_enrollment.is_admin_terminate_eligible?
+        return Failure('Not an ivl enrollment.') unless base_enrollment.is_ivl_by_kind? # Added per the request, also need further development if to be used for shop
         return Failure('No members selected to drop.') if params[:options].select{|string| string.include?("terminate_member")}.empty?
         return Failure('No termination date given.') if params[:options].select{|string| string.include?("termination_date")}.empty?
-        return Failure('Termination date cannot be outside of the current calender year.') unless Date.strptime(params[:options]["termination_date_#{params[:hbx_enrollment].id}"], "%m/%d/%Y").year == TimeKeeper.date_of_record.year
+        @termination_date = Date.strptime(params[:options]["termination_date_#{params[:hbx_enrollment].id}"], "%m/%d/%Y")
+        @new_effective_date = (termination_date > base_enrollment.effective_on) ? termination_date + 1.day : base_enrollment.effective_on
+        return Failure('Select termination date that would result member drop in present calender year.') unless new_effective_date.year == termination_date.year
+        return Failure('Termination date cannot be outside of the current calender year.') unless termination_date.year == TimeKeeper.date_of_record.year
 
         Success(params)
       end
 
       def drop_enrollment_members(params)
-        termination_date = Date.strptime(params[:options]["termination_date_#{params[:hbx_enrollment].id}"], "%m/%d/%Y")
-        dropped_enr_members = params[:options].select{|string| string.include?("terminate_member")}.values
-        all_enr_members = params[:hbx_enrollment].hbx_enrollment_members
-        non_eligible_members = all_enr_members.select{ |member| dropped_enr_members.include?(member.id.to_s) }
-        new_effective_date = (termination_date > params[:hbx_enrollment].effective_on) ? termination_date + 1.day : params[:hbx_enrollment].effective_on
-        return Failure('Select termination date that would result member drop in present calender year.') unless new_effective_date.year == termination_date.year
+        custom_params_result = custom_params
+        return custom_params_result if custom_params_result.failure?
 
-        custom_params = custom_params(params[:hbx_enrollment], new_effective_date)
-        clone_enrollment = Operations::HbxEnrollments::Clone.new.call({hbx_enrollment: params[:hbx_enrollment], effective_on: new_effective_date, options: custom_params})
+        clone_enrollment = Operations::HbxEnrollments::Clone.new.call({hbx_enrollment: base_enrollment, effective_on: new_effective_date, options: custom_params_result.value!})
         return clone_enrollment if clone_enrollment.failure?
 
-        new_enrollment = clone_enrollment.value!
-        new_enrollment.hbx_enrollment_members.delete_if {|mem| non_eligible_members.pluck(:applicant_id).include?(mem.applicant_id)}
-        set_product_id(params[:hbx_enrollment], new_enrollment)
+        @new_enrollment = clone_enrollment.value!
+        dropped_enr_members = params[:options].select{|string| string.include?("terminate_member")}.values
+        drop_selected_members(dropped_enr_members)
+        set_product_result = set_product_id
+        return set_product_result if set_product_result.failure?
+
         new_enrollment.check_for_subscriber
-        new_enrollment.generate_hbx_signature
+        updates_for_subscriber_drop unless check_subscriber_drop
         new_enrollment.select_coverage! if new_enrollment.may_select_coverage?
-        update_member_effective_dates(new_enrollment)
+        update_member_effective_dates
         return Failure('Failed to save dropped member enrollment') unless new_enrollment.persisted?
 
-        get_household_applied_aptc(new_enrollment, new_effective_date) if params[:hbx_enrollment].applied_aptc_amount > 0
-        terminate_base_enrollment(params[:hbx_enrollment], new_enrollment, termination_date)
+        update_household_applied_aptc if base_enrollment.applied_aptc_amount > 0
+        terminate_base_enrollment
+        notify_trading_partner(params)
 
         dropped_member_info = []
         dropped_enr_members.each do |member_id|
-          member = params[:hbx_enrollment].hbx_enrollment_members.where(id: member_id).first
+          member = base_enrollment.hbx_enrollment_members.where(id: member_id).first
           dropped_member_info << {hbx_id: member.hbx_id,
                                   full_name: member.person.full_name,
                                   terminated_on: termination_date}
@@ -65,13 +70,38 @@ module Operations
         Success(dropped_member_info)
       end
 
-      def update_member_effective_dates(new_enrollment)
+      def drop_selected_members(dropped_enr_members)
+        all_enr_members = base_enrollment.hbx_enrollment_members
+        non_eligible_members = all_enr_members.select{ |member| dropped_enr_members.include?(member.id.to_s) }
+        new_enrollment.hbx_enrollment_members.delete_if {|mem| non_eligible_members.pluck(:applicant_id).include?(mem.applicant_id)}
+      end
+
+      def notify_trading_partner(params)
+        transmit_drop = params.key?("transmit_hbx_#{base_enrollment.id}") ? true : false
+        base_enrollment.notify_enrollment_cancel_or_termination_event(transmit_drop)
+        new_enrollment.notify_of_coverage_start(transmit_drop)
+      end
+
+      def check_subscriber_drop
+        new_enrollment.subscriber.hbx_id == base_enrollment.subscriber.hbx_id
+      end
+
+      def updates_for_subscriber_drop
+        new_enrollment.generate_hbx_signature
+        new_enrollment.consumer_role_id = new_enrollment.subscriber.person.consumer_role.id
+      end
+
+      def update_member_effective_dates
         new_enrollment.hbx_enrollment_members.each do |member|
-          member.update_attributes(eligibility_date: new_enrollment.effective_on, coverage_start_on: new_enrollment.effective_on)
+          if check_subscriber_drop
+            member.update_attributes(eligibility_date: new_enrollment.effective_on)
+          else
+            member.update_attributes(eligibility_date: new_enrollment.effective_on, coverage_start_on: new_enrollment.effective_on)
+          end
         end
       end
 
-      def terminate_base_enrollment(base_enrollment, new_enrollment, termination_date)
+      def terminate_base_enrollment
         if base_enrollment.may_terminate_coverage? && (new_enrollment.effective_on > base_enrollment.effective_on)
           base_enrollment.terminate_coverage!
           base_enrollment.update_attributes!(terminated_on: termination_date)
@@ -80,19 +110,25 @@ module Operations
         end
       end
 
-      def custom_params(base_enrollment, new_effective_date)
-        { predecessor_enrollment_id: base_enrollment.id,
-          rating_area_id: ::BenefitMarkets::Locations::RatingArea.rating_area_for(base_enrollment.consumer_role.rating_address, during: new_effective_date)&.id }
+      def custom_params
+        rating_area = ::BenefitMarkets::Locations::RatingArea.rating_area_for(base_enrollment.consumer_role.rating_address, during: new_effective_date)
+        return Failure('Rating area could be found.') unless rating_area.present?
+        Success({ predecessor_enrollment_id: base_enrollment.id,
+                  rating_area_id: rating_area&.id })
       end
 
-      def set_product_id(base_enrollment, new_enrollment)
+      def set_product_id
         tax_household = base_enrollment.family.active_household.latest_active_thh_with_year(new_enrollment.effective_on.year)
-        return unless tax_household.present?
+        return Success() unless tax_household.present?
 
         products = products_for_csr_variant(base_enrollment, new_enrollment, tax_household)
         return Failure('Could not find product for new enrollment with present csr kind.') unless products.count >= 1
 
         new_enrollment.product_id = products.last.id
+        service_area_check = ::Operations::Products::ProductOfferedInServiceArea.new.call({enrollment: new_enrollment})
+        return Failure('Product is NOT offered in service area.') if service_area_check.failure?
+
+        Success()
       end
 
       def products_for_csr_variant(base_enrollment, new_enrollment, tax_household)
@@ -101,12 +137,12 @@ module Operations
         ::BenefitMarkets::Products::HealthProducts::HealthProduct.by_year(new_enrollment.effective_on.year).where({:hios_id => "#{base_enrollment.product.hios_base_id}-#{csr_variant}"})
       end
 
-      def get_household_applied_aptc(hbx_enrollment, effective_date)
-        tax_household = hbx_enrollment.family.active_household.latest_tax_household_with_year(hbx_enrollment.effective_on.year)
+      def update_household_applied_aptc
+        tax_household = new_enrollment.family.active_household.latest_tax_household_with_year(new_enrollment.effective_on.year)
         return unless tax_household
 
-        applied_aptc = tax_household.monthly_max_aptc(hbx_enrollment, effective_date)
-        ::Insured::Factories::SelfServiceFactory.update_enrollment_for_apcts(hbx_enrollment, applied_aptc)
+        applied_aptc = tax_household.monthly_max_aptc(new_enrollment, new_effective_date)
+        ::Insured::Factories::SelfServiceFactory.update_enrollment_for_apcts(new_enrollment, applied_aptc)
       end
     end
   end
--- spec/domain/operations/hbx_enrollments/drop_enrollment_members_spec.rb
+++ spec/domain/operations/hbx_enrollments/drop_enrollment_members_spec.rb
@@ -62,6 +62,85 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
       EnrollRegistry[:drop_enrollment_members].feature.stub(:is_enabled).and_return(true)
     end
 
+    context 'invalid params', dbclean: :around_each do
+      context 'when feature is turned off' do
+        before do
+          EnrollRegistry[:drop_enrollment_members].feature.stub(:is_enabled).and_return(false)
+          @result = subject.call({hbx_enrollment: enrollment,
+                                  options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                            "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+        end
+
+        it 'should return a failure' do
+          expect(@result.failure).to eq "Member drop feature is turned off."
+        end
+      end
+
+      context 'when sending invalid params' do
+        it 'should return a failure when hbx_enrollment key is not present' do
+          result = subject.call({options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+          expect(result.failure).to eq "Missing HbxEnrollment Key."
+        end
+
+        it 'should return a failure when hbx_enrollment key is not hbx enrollment object' do
+          result = subject.call({hbx_enrollment: double,
+                                 options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+          expect(result.failure).to eq "Not a valid HbxEnrollment object."
+        end
+
+        it 'should return a failure when hbx_enrollment is not terminatable' do
+          allow(enrollment).to receive(:is_admin_terminate_eligible?).and_return(false)
+          result = subject.call({hbx_enrollment: enrollment,
+                                 options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+
+          expect(result.failure).to eq "Enrollment need be in an active state to drop dependent"
+        end
+
+        it 'should return a failure when hbx_enrollment is not an ivl' do
+          allow(enrollment).to receive(:is_ivl_by_kind?).and_return(false)
+          result = subject.call({hbx_enrollment: enrollment,
+                                 options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+
+          expect(result.failure).to eq "Not an ivl enrollment."
+        end
+
+        it 'should return a failure when hbx_enrollment is not an ivl' do
+          result = subject.call({hbx_enrollment: enrollment,
+                                 options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s}})
+
+          expect(result.failure).to eq "No members selected to drop."
+        end
+
+        it 'should return a failure when hbx_enrollment is not an ivl' do
+          result = subject.call({hbx_enrollment: enrollment,
+                                 options: {"terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+
+          expect(result.failure).to eq "No termination date given."
+        end
+
+        it 'should return a failure when hbx_enrollment is not an ivl' do
+          result = subject.call({hbx_enrollment: enrollment,
+                                 options: {"termination_date_#{enrollment.id}" => enrollment.effective_on.end_of_year.to_s,
+                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+
+          expect(result.failure).to eq "Select termination date that would result member drop in present calender year."
+        end
+
+        it 'should return a failure when hbx_enrollment is not an ivl' do
+          result = subject.call({hbx_enrollment: enrollment,
+                                 options: {"termination_date_#{enrollment.id}" => enrollment.effective_on.next_year.to_s,
+                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+
+          expect(result.failure).to eq "Termination date cannot be outside of the current calender year."
+        end
+
+      end
+    end
+
     context 'when members are selected for drop', dbclean: :around_each do
       context 'when previous enrollment has 0 applied aptc' do
         before do
@@ -82,6 +161,20 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
         it 'should select coverage for the reinstatement' do
           expect(family.hbx_enrollments.where(:id.ne => enrollment.id).last.aasm_state).to eq 'coverage_selected'
         end
+
+        context 'when address not in rating area' do
+          before :each do
+            enrollment.update_attributes(aasm_state: 'coverage_selected')
+            allow(::BenefitMarkets::Locations::RatingArea).to receive(:rating_area_for).with(enrollment.consumer_role.rating_address, during: (TimeKeeper.date_of_record + 2.day)).and_return(nil)
+          end
+
+          it 'should return failure' do
+            result = subject.call({hbx_enrollment: enrollment,
+                                   options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                             "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+            expect(result.failure).to eq 'Rating area could be found.'
+          end
+        end
       end
 
       context 'when termination date is equal to base enrollment effective date' do
@@ -98,12 +191,12 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
 
         it 'member should have coverage start on as new enrollment effective on' do
           new_enrollment = family.hbx_enrollments.where(:id.ne => enrollment.id).last
-          expect(new_enrollment.hbx_enrollment_members.pluck(:eligibility_date, :coverage_start_on).flatten.uniq).to eq [new_enrollment.effective_on]
+          expect(new_enrollment.hbx_enrollment_members.pluck(:eligibility_date, :coverage_start_on).flatten.uniq).to eq [new_enrollment.effective_on, enrollment.hbx_enrollment_members.first.eligibility_date]
         end
       end
 
       context 'when previous enrollment has applied aptc', dbclean: :around_each do
-        before do
+        let(:enrollment_2) do
           enrollment_2 = FactoryBot.create(:hbx_enrollment,
                                            :with_product,
                                            :individual_assisted,
@@ -120,15 +213,19 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
                                            benefit_sponsorship_id: initial_application.benefit_sponsorship.id,
                                            kind: "individual",
                                            consumer_role_id: consumer_role.id)
+        end
+
+        let(:product) { FactoryBot.create(:benefit_markets_products_health_products_health_product, metal_level_kind: :silver, benefit_market_kind: :aca_individual) }
+        let(:tax_household) { FactoryBot.create(:tax_household, household: family.active_household, effective_starting_on: enrollment_2.effective_on, effective_ending_on: nil) }
+
+
+        before do
           enrollment_2.benefit_sponsorship = benefit_sponsorship
           enrollment_2.save!
-
-          @product = FactoryBot.create(:benefit_markets_products_health_products_health_product, metal_level_kind: :silver, benefit_market_kind: :aca_individual)
-          @product.premium_tables.first.update_attributes!(rating_area: ::BenefitMarkets::Locations::RatingArea.where('active_year' => TimeKeeper.date_of_record.year).first)
+          product.premium_tables.first.update_attributes!(rating_area: ::BenefitMarkets::Locations::RatingArea.where('active_year' => TimeKeeper.date_of_record.year).first)
           BenefitMarkets::Products::ProductRateCache.initialize_rate_cache!
+          enrollment_2.update_attributes!(product: product, consumer_role: person.consumer_role)
 
-          enrollment_2.update_attributes!(product: @product, consumer_role: person.consumer_role)
-          tax_household = FactoryBot.create(:tax_household, household: family.active_household, effective_starting_on: enrollment_2.effective_on, effective_ending_on: nil)
           family.family_members.each do |member|
             FactoryBot.create(:tax_household_member, tax_household: tax_household, applicant_id: member.id)
           end
@@ -149,6 +246,48 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
           expect(reinstatement.elected_aptc_pct).to_not eq 0.0 #Since there will be a change to coverage_start on member level to new effective date.
           expect(reinstatement.applied_aptc_amount).to_not eq 0
           expect(reinstatement.aggregate_aptc_amount).to_not eq 0
+        end
+
+        it 'family should have new enrollment with same product id' do
+          reinstatement = family.hbx_enrollments.where(:id.ne => enrollment.id).last
+          expect(reinstatement.product.id).to eq enrollment_2.product.id
+        end
+
+        context 'when csr kind is different from base enrollment', dbclean: :around_each do
+          before :each do
+            enrollment_2.update_attributes(aasm_state: 'coverage_selected')
+            allow(tax_household).to receive(:eligibile_csr_kind).with(enrollment_2.hbx_enrollment_members.map(&:applicant_id) - [hbx_enrollment_member3.applicant_id]).and_return('csr_100')
+          end
+
+          it 'should return failure with new csr variant and product is not available.' do
+            result = subject.call({hbx_enrollment: enrollment_2, options: {"termination_date_#{enrollment_2.id}" => TimeKeeper.date_of_record.to_s,
+                                                                           "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+            expect(result.failure).to eq "Could not find product for new enrollment with present csr kind."
+          end
+
+          context 'when the new csr product is available' do
+            let(:product_1) {product.dup}
+
+            before :each do
+              enrollment_2.update_attributes(aasm_state: 'coverage_selected')
+              product_1.update_attributes(hios_id: "#{product.hios_base_id}-02", csr_variant_id: "02")
+            end
+
+            it 'enrollment with new product' do
+              subject.call({hbx_enrollment: enrollment_2, options: {"termination_date_#{enrollment_2.id}" => TimeKeeper.date_of_record.to_s,
+                                                                    "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+
+              reinstatement = family.hbx_enrollments.where(:id.ne => enrollment_2.id).last
+              expect(reinstatement.product.id).to eq product_1.id
+            end
+
+            it 'when product is not in service area' do
+              product_1.unset(:service_area_id)
+              result = subject.call({hbx_enrollment: enrollment_2, options: {"termination_date_#{enrollment_2.id}" => TimeKeeper.date_of_record.to_s,
+                                                                             "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}})
+              expect(result.failure).to eq "Product is NOT offered in service area."
+            end
+          end
         end
       end
 
@@ -171,6 +310,37 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
       end
     end
 
+    context 'when subscriber is being dropped' do
+      before do
+        subject.call({hbx_enrollment: enrollment, options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
+                                                            "terminate_member_#{hbx_enrollment_member3.id}" => enrollment.subscriber.id.to_s}})
+        @new_enrollment = family.hbx_enrollments.where(:id.ne => enrollment.id).last
+      end
+
+      it 'should have new subscriber' do
+        expect(@new_enrollment.subscriber).not_to eq nil
+        expect(@new_enrollment.subscriber).not_to eq enrollment.subscriber
+      end
+
+      it 'should have new hbx signature' do
+        expect(@new_enrollment.enrollment_signature).not_to eq enrollment.enrollment_signature
+      end
+
+      it 'should have new consumer role id' do
+        expect(@new_enrollment.consumer_role_id).not_to eq nil
+        expect(@new_enrollment.consumer_role_id).not_to eq enrollment.consumer_role_id
+      end
+
+      it 'should have new consumer role id' do
+        expect(@new_enrollment.consumer_role_id).not_to eq nil
+        expect(@new_enrollment.consumer_role_id).not_to eq enrollment.consumer_role_id
+      end
+
+      it 'all the enr members should have same coverage and eligibility date as new effective date' do
+        expect(@new_enrollment.hbx_enrollment_members.pluck(:eligibility_date, :coverage_start_on).flatten.uniq).to eq [@new_enrollment.effective_on]
+      end
+    end
+
     context 'when members are NOT selected for drop', dbclean: :around_each do
       before do
         @dropped_members = subject.call({hbx_enrollment: enrollment,
@@ -185,6 +355,5 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
         expect(enrollment.aasm_state).to eq 'coverage_selected'
       end
     end
-
   end
 end
