d56e56f7b1e401ea2b610f0657b63b438a30a4df
Jay Reddy <jayanth.rordev@gmail.com>
2022-05-18 14:22:31 -0400

    dental fix for dependent drop (#1817)

--- spec/domain/operations/hbx_enrollments/drop_enrollment_members_spec.rb
+++ spec/domain/operations/hbx_enrollments/drop_enrollment_members_spec.rb
@@ -147,6 +147,7 @@ RSpec.describe Operations::HbxEnrollments::DropEnrollmentMembers, :type => :mode
           @dropped_members = subject.call({hbx_enrollment: enrollment,
                                            options: {"termination_date_#{enrollment.id}" => (TimeKeeper.date_of_record + 1.day).to_s,
                                                      "terminate_member_#{hbx_enrollment_member3.id}" => hbx_enrollment_member3.id.to_s}}).success
+          allow(enrollment).to receive(:is_health_enrollment?).and_return(true)
         end
 
         it 'should return dropped member info' do
