7fb5fad7edd0961dfa5fd14a7521d6f812cb211b
mdkaraman <mdkaramanov@gmail.com>
2022-05-26 19:26:13 -0400

    helper spec

---
+++ components/financial_assistance/spec/helpers/financial_assistance/county_helper_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ::FinancialAssistance::CountyHelper, :type => :helper, dbclean: :after_each do
+
+  let(:zipcode_mappings) { ::FinancialAssistance::CountyHelper::ZIPCODE_MAPPINGS }
+
+  context 'zipcode mappings hash' do
+    it 'should exist and not be empty' do
+      expect(zipcode_mappings.present?).to be_truthy
+    end
+  end
+
+  context 'zipcode_to_geocode' do
+    context 'zipcode is not present in hash' do
+      it 'should return nil' do
+        zip = "XXXXX"
+        expect(zipcode_to_geocode(zip)).to eq nil
+      end
+    end
+
+    context 'zipcode is present in hash' do
+      it 'should return a string' do
+        zip = zipcode_mappings.keys.first
+        expect(zipcode_to_geocode(zip)).to be_instance_of(String)
+      end
+    end
+  end
+
+  context 'city_to_county' do
+    context 'city is not present in hash' do
+      it 'should return nil' do
+        city = "XXXXX"
+        expect(city_to_county(city)).to eq nil
+      end
+    end
+
+    context 'zipcode is present in hash' do
+      it 'should return a string' do
+        city = zipcode_mappings.values.first[:city]
+        expect(city_to_county(city)).to be_instance_of(String)
+      end
+    end
+
+    context 'five_digit_format' do
+      context 'given 9-digit zip' do
+        it 'should return first five digits' do
+          zip = '12345-1234'
+          expect(five_digit_format(zip)).to eq('12345')
+        end
+      end
+    end
+  end
+end
