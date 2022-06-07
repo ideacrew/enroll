27d9161ae9417b0c232171177c635c6087c40744
Nisanth Yaganti <yagantinisanth@gmail.com>
2022-06-07 17:56:06 -0400

    adding create event to lawful presence determination (#1854)
    * adding create event to lawful presence determination
    
    * Revert "adding create event to lawful presence determination"
    
    This reverts commit d3e200999483d9a48e6620a1e473fb6b3ee58c44.
    
    * making hub call after consumer role creation
    
    * using rails changes object
    
    * typo
    
    * using symbols
    
    * adding additional loggers
    
    Co-authored-by: Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>

--- app/models/lawful_presence_determination.rb
+++ app/models/lawful_presence_determination.rb
@@ -126,7 +126,7 @@ class LawfulPresenceDetermination
   end
 
   def publish_updated_event
-    attrs = { consumer_role_id: ivl_role.id }.merge!(self.changed_attributes)
+    attrs = { consumer_role_id: ivl_role.id }.merge!(self.changes)
     event = event('events.individual.consumer_roles.lawful_presence_determinations.updated', attributes: attrs)
     event.success.publish if event.success?
   rescue StandardError => e
