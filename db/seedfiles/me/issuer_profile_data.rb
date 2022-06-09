58af7c75ddbec3bce7a4928dcc35c43ba6bc80eb
vineel <sonnathivinil@gmail.com>
2022-06-09 16:11:11 -0400

    182420972: Taro Health onboarding

--- db/seedfiles/me/issuer_profile_data.rb
+++ db/seedfiles/me/issuer_profile_data.rb
@@ -2,5 +2,6 @@ ISSUER_PROFILE_DATA = [
   {fein: "453416923", issuer_hios_ids: ["33653"], legal_name: "Community Health Options", abbreviation: "CHO", hbx_carrier_id: 30_001, ivl_health: true },
   {fein: "042452600", issuer_hios_ids: ["96667"], legal_name: "Harvard Pilgrim Health Care", abbreviation: "HPHC", hbx_carrier_id: 30_002, ivl_health: true },
   {fein: "311705652", issuer_hios_ids: ["48396"], legal_name: "Anthem Blue Cross and Blue Shield", abbreviation: "ANTHM", hbx_carrier_id: 30_004, ivl_health: true },
-  {fein: "010286541", issuer_hios_ids: ["50165"], legal_name: "Northeast Delta Dental", abbreviation: "NEDD", hbx_carrier_id: 30_005, ivl_dental: true }
+  {fein: "010286541", issuer_hios_ids: ["50165"], legal_name: "Northeast Delta Dental", abbreviation: "NEDD", hbx_carrier_id: 30_005, ivl_dental: true },
+  {fein: "873357382", issuer_hios_ids: ["54879"], legal_name: "Taro Health Plan of Maine, Inc.", abbreviation: "TARO", hbx_carrier_id: 30_006, ivl_health: true }
 ]
