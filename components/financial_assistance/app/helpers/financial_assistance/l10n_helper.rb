49314c7669ad3d556ab25a86529af360a1c10c05
Kristin Merbach <kristin@kristinmerbach.com>
2022-06-30 09:28:56 -0700

    add new divorce agreement copy and replace non-translated copy with translations

--- components/financial_assistance/app/helpers/financial_assistance/l10n_helper.rb
+++ components/financial_assistance/app/helpers/financial_assistance/l10n_helper.rb
@@ -5,5 +5,9 @@ module FinancialAssistance
     def l10n(translation_key, interpolated_keys = {})
       t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
     end
+
+    def l10n_sentence(translation_key, interpolated_keys = {})
+      t(translation_key, interpolated_keys.merge(default: translation_key)).html_safe
+    end
   end
 end
