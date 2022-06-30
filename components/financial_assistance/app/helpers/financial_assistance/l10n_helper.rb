865af97bc930c7f1882569fdcce89cb110c4fbec
Kristin Merbach <kristin@kristinmerbach.com>
2022-06-30 13:00:13 -0700

    checking to see if this gets flagged

--- components/financial_assistance/app/helpers/financial_assistance/l10n_helper.rb
+++ components/financial_assistance/app/helpers/financial_assistance/l10n_helper.rb
@@ -3,7 +3,11 @@
 module FinancialAssistance
   module L10nHelper
     def l10n(translation_key, interpolated_keys = {})
-      t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
+      ActionController::Base.helpers.sanitize(t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)))
+    end
+
+    def l10n_sentence(translation_key, interpolated_keys = {})
+      ActionController::Base.helpers.sanitize(t(translation_key, interpolated_keys.merge(default: translation_key)))
     end
   end
 end
