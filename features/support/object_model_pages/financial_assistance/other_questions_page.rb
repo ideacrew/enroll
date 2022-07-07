f0dbd84c90fa941eaa3cdb1974ed8fd013c39767
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2022-07-07 21:31:19 +0000

    rubocop fix

--- features/support/object_model_pages/financial_assistance/other_questions_page.rb
+++ features/support/object_model_pages/financial_assistance/other_questions_page.rb
@@ -2,13 +2,11 @@
 
 # Financial Assistance Other Questions form
 module FinancialAssistance
-    # For Other Questions page
-    class OtherQuestionsPage
-  
-      def self.no_ssn_dropdown
-        '[data-cuke="no-ssn-dropdown"]'
-      end
+  # For Other Questions page
+  class OtherQuestionsPage
 
+    def self.no_ssn_dropdown
+      '[data-cuke="no-ssn-dropdown"]'
     end
   end
-  
+end
