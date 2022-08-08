85673690849f47aa0e7e93610f94229b51464064
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2022-08-08 16:03:53 +0000

    add terminated enrollment to spec

--- spec/controllers/insured/consumer_roles_controller_spec.rb
+++ spec/controllers/insured/consumer_roles_controller_spec.rb
@@ -426,6 +426,15 @@ RSpec.describe Insured::ConsumerRolesController, dbclean: :after_each, :type => 
       end
 
       it "should not show shop coverage if no enrollments exist" do
+        FactoryBot.create(:hbx_enrollment,
+                          family: family,
+                          product: product,
+                          household: family.active_household,
+                          coverage_kind: "health",
+                          effective_on: effective_on,
+                          kind: 'employer_sponsored',
+                          hbx_enrollment_members: [hbx_en_member1, hbx_en_member2],
+                          aasm_state: 'coverage_terminated')
         put :update, params: { person: person_params, id: "test" }
         expect(assigns['shop_coverage_result']).to eq false
       end
