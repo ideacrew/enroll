677b1b83d4209c7b87fd80b9d58368ff6a2e1683
Mark Goho <markgoho@gmail.com>
2022-08-10 20:03:58 +0000

    update gems in ui_helper engine

--- components/ui_helpers/lib/ui_helpers/workflow_helper.rb
+++ components/ui_helpers/lib/ui_helpers/workflow_helper.rb
@@ -87,14 +87,16 @@ module UIHelpers
     end
 
     # TODO: All these Settings calls will have to be refactored for platformization
-    def translation_placeholder_text(text)
+    def translation_placeholder_text(value)
+      text = value.dup.to_s
+
       text.gsub! '<board_of_elections_address-placeholder>', Settings.contact_center.board_of_elections_address
       text.gsub! '<board_of_elections_email-placeholder>', Settings.contact_center.board_of_elections_email
       text.gsub! '<board_of_elections_entity-placeholder>', Settings.contact_center.board_of_elections_entity
       text.gsub! '<board_of_elections_phone_number-placeholder>', Settings.contact_center.board_of_elections_phone_number
       text.gsub! '<contact-center-phone_number-placeholder>', Settings.contact_center.phone_number
       text.gsub! '<medicaid-question-translation-placeholder>', state_abbreviation_text(l10n("faa.medicaid_question"))
-      text.gsub! '<short-name-placeholder>', EnrollRegistry[:enroll_app].setting(:short_name).item
+      text.gsub! '<short-name-placeholder>', ::EnrollRegistry[:enroll_app].setting(:short_name).item
       text.gsub! '<state-abbreviation-placeholder>', aca_state_abbreviation
       text.gsub! '<reviewed-information>', l10n('insured.review_information')
 
