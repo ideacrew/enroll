824b5e8a029dbd52285205e64896c8909c31509c
Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>
2022-08-11 14:17:42 -0400

    enhance ivl enrollment tile (#1993)
    * feat: add osse amount to enrollment tile
    
    * feat: fix erb tag
    
    * feat: add enrollment view spec

--- db/seedfiles/translations/en/me/insured.rb
+++ db/seedfiles/translations/en/me/insured.rb
@@ -193,6 +193,7 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.insured.families.future_enrollment_termination_date' => "Future enrollment termination date",
   :'en.market_type' => "Market Type",
   :'en.APTC_amount' => "APTC amount",
+  :"en.osse_amount" => "OSSE amount",
   :'en.covered' => "Covered",
   :'en.insured.plan_contact_info' => "Plan Contact Info",
   :'en.make_changes' => "Make Changes",
