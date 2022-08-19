20f8709a74256c1b978024bb8fa2ab9d9de98232
Battula <vrbattula@outlook.com>
2022-08-19 11:08:10 -0400

    fix rubocop isuses and adds dummy eligibility models in benefit sponsors engine

--- components/benefit_sponsors/spec/dummy/app/models/eligibilities/osse/subject.rb
+++ components/benefit_sponsors/spec/dummy/app/models/eligibilities/osse/subject.rb
@@ -2,22 +2,23 @@
 
 module Eligibilities
   module Osse
-    # Value model
-    class Value
+    # Subject model
+    class Subject
       include Mongoid::Document
       include Mongoid::Timestamps
       # include ::EventSource::Command
       # include Dry::Monads[:result, :do, :try]
-      # include GlobalID::Identification
+      include GlobalID::Identification
       # include Eligibilities::Eventable
 
       embedded_in :grant, class_name: "::Eligibilities::Osse::Grant"
 
       field :title, type: String
       field :description, type: String
-      field :key, type: Symbol
+      field :key, type: String
+      field :klass, type: String
 
-      validates_presence_of :key
+      validates_presence_of :key, :klass
     end
   end
 end
