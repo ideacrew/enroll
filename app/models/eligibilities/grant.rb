1cad18afbceb27cb9d8fcddc52ab91377c566e5d
HArsHA Ellanki <h_vardhan@yahoo.com>
2022-08-23 01:53:44 -0400

    rubocop fix

--- app/models/eligibilities/grant.rb
+++ app/models/eligibilities/grant.rb
@@ -5,12 +5,12 @@ module Eligibilities
     include Mongoid::Document
     include Mongoid::Timestamps
 
-    field :title, type: String 
-    field :title, type: String 
-    field :key, type: String 
+    field :title, type: String
+    field :title, type: String
+    field :key, type: String
     field :value, type: Float
-    field :start_on, type: Date 
-    field :end_on, type: Date 
+    field :start_on, type: Date
+    field :end_on, type: Date
 
     embedded_in :determination, class_name: "::Eligibilities::Determination"
     embedded_in :eligibility_state, class_name: "::Eligibilities::EligibilityState"
