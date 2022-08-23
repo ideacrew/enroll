069d55d833d20910e2e572a739b5035250862a22
HArsHA Ellanki <h_vardhan@yahoo.com>
2022-08-23 01:48:04 -0400

    added grant model and associations

--- app/models/eligibilities/determination.rb
+++ app/models/eligibilities/determination.rb
@@ -7,12 +7,28 @@ module Eligibilities
 
     embedded_in :determinable, polymorphic: true
     embeds_many :subjects, class_name: "::Eligibilities::Subject", cascade_callbacks: true
+    embeds_many :grants, class_name: "::Eligibilities::Grant", cascade_callbacks: true
 
     field :effective_date, type: Date
     field :outstanding_verification_status, type: String
     field :outstanding_verification_earliest_due_date, type: Date
     field :outstanding_verification_document_status, type: String
 
-    accepts_nested_attributes_for :subjects
+    accepts_nested_attributes_for :subjects, :grants
   end
 end
+
+    # [
+    #   {
+    #     key: 'aptc_grant',
+    #     value: 500.00,
+    #     members: [ "Subject_gid1", 'Subject_gid2']
+    #     start_on: date
+    #   },
+    #   {
+    #     key: 'aptc_grant',
+    #     value: 300.00,
+    #     members: [ "Subject_gid3", 'Subject_gid4']
+    #     end_on: date
+    #    }
+    # ]
---
+++ app/models/eligibilities/grant.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Eligibilities
+  class Grant
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :title, type: String 
+    field :title, type: String 
+    field :key, type: String 
+    field :value, type: Float
+    field :start_on, type: Date 
+    field :end_on, type: Date 
+
+    embedded_in :determination, class_name: "::Eligibilities::Determination"
+    embedded_in :eligibility_state, class_name: "::Eligibilities::EligibilityState"
+
+  end
+end
