2c327a73623e03b1e14311b92faad0c57a9d61e5
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2022-09-07 11:20:20 -0400

    183095228: migrate household's thhs to TaxHouseholdGroup and its embedded docs.

--- app/models/tax_household_group.rb
+++ app/models/tax_household_group.rb
@@ -4,6 +4,8 @@ class TaxHouseholdGroup
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  SOURCE_KINDS = %w[Curam Admin Renewals Faa Ffe].freeze
+
   embedded_in :family
 
   field :source, type: String
@@ -11,15 +13,32 @@ class TaxHouseholdGroup
   field :start_on, type: Date
   field :end_on, type: Date
   field :assistance_year, type: Integer
-  field :aasm_state, type: String
+  field :determined_on, type: Date
 
+  field :hbx_id, type: String
+
+  validates :source,
+            allow_blank: false,
+            inclusion: { in: SOURCE_KINDS,
+                         message: "%{value} is not a valid source kind" }
 
   embeds_many :tax_households
   embedded_in :family
 
+  before_save :generate_hbx_id
+
   index({application_id:  1})
+  index({ effective_ending_on:  1 })
+  index({ assistance_year:  1 })
 
   # Scopes
-  scope :by_year, ->(year) { where(start_on: (Date.new(year)..Date.new(year).end_of_year)) }
-  scope :active, ->{ where(end_on: nil) }
+  scope :by_year,   ->(year) { where(assistance_year: year) }
+  scope :active,    ->{ where(end_on: nil) }
+  scope :inactive,  ->{ where(:end_on.ne => nil) }
+
+  private
+
+  def generate_hbx_id
+    write_attribute(:hbx_assigned_id, HbxIdGenerator.generate_tax_household_group_id) if hbx_id.blank?
+  end
 end
