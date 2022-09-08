2316744249e4a2813b1f9af0ee06372be3605b5a
Jacob Kagon <jacob@kagon.net>
2022-09-08 14:12:18 -0700

    rubocop fix

--- app/helpers/views/view_helper.rb
+++ app/helpers/views/view_helper.rb
@@ -1,12 +1,17 @@
-module Views::ViewHelper
+# frozen_string_literal: true
 
-  def display_standard_plan(plan)
-    if plan.is_standard_plan
-      l10n("yes")
-    elsif plan.is_standard_plan == false
-      l10n("no")
-    else
-      "N/A"
+
+module Views
+  # view helpers
+  module ViewHelper
+    def display_standard_plan(plan)
+      if plan.is_standard_plan
+        l10n("yes")
+      elsif plan.is_standard_plan == false
+        l10n("no")
+      else
+        "N/A"
+      end
     end
   end
 end
