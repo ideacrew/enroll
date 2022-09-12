921c9aca16ed58bcffb0ca266d929ab2b804094d
Marco Ornelas <marco.ornelas@ideacrew.com>
2022-09-12 15:38:29 -0400

    initial commit of the breakup operations

---
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/pvc/create_pvc_request.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+require 'aca_entities/magi_medicaid/libraries/iap_library'
+
+module FinancialAssistance
+  module Operations
+    module Applications
+      module Rrv
+        # operation to manually trigger rrv events.
+        # It will take families as input and find the determined application, add evidences and publish the group of applications
+        class CreateRrvRequest
+          include Dry::Monads[:result, :do]
+          include EventSource::Command
+          include EventSource::Logging
+
+          def call(params)
+            values = yield validate(params)
+            applications = yield collect_applications_from_families(values)
+            event = yield build_event(applications)
+            result = yield publish(event)
+
+            Success(result)
+          end
+
+          private
+
+          def validate(params)
+            errors = []
+            errors << 'families ref missing' unless params[:families]
+            errors << 'assistance_year ref missing' unless params[:assistance_year]
+
+            errors.empty? ? Success(params) : Failure(errors)
+          end
+
+          def fetch_application(family, assistance_year)
+            ::FinancialAssistance::Application.where(assistance_year: assistance_year,
+                                                     aasm_state: 'determined',
+                                                     family_id: family.id).max_by(&:created_at)
+          end
+
+          def transform_and_construct(family, assistance_year)
+            application = fetch_application(family, assistance_year)
+            payload = FinancialAssistance::Operations::Applications::Transformers::ApplicationTo::Cv3Application.new.call(application)
+            AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(payload.value!).value!
+          end
+
+          def is_aptc_or_csr_eligible?(application)
+            eligibility = application.eligibility_determinations.max_by(&:determined_at)
+            eligibility.present? && (eligibility.is_aptc_eligible? || eligibility.is_csr_eligible?)
+          end
+
+          def collect_applications_from_families(params)
+            applications_with_evidences = []
+            count = 0
+            rrv_logger = Logger.new("#{Rails.root}/log/rrv_logger_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log")
+
+            params[:families].no_timeout.each do |family|
+              determined_application = fetch_application(family, params[:assistance_year])
+              next unless determined_application.present? && is_aptc_or_csr_eligible?(determined_application)
+
+              determined_application.create_rrv_evidences
+              cv3_application = transform_and_construct(family, params[:assistance_year])
+              applications_with_evidences << cv3_application.to_h
+              count += 1
+              rrv_logger.info("********************************* processed #{count}*********************************") if count % 100 == 0
+            rescue StandardError => e
+              rrv_logger.info("failed to process for person with hbx_id #{family.primary_person.hbx_id} due to #{e.inspect}")
+            end
+
+            applications_with_evidences.present? ? Success(applications_with_evidences) : Failure("No Applications for given families")
+          end
+
+          def build_event(payload)
+            event('events.iap.applications.magi_medicaid_application_renewal_assistance_eligible', attributes: { applications: payload })
+          end
+
+          def publish(event)
+            event.publish
+
+            Success("Successfully published the rrv payload")
+          end
+        end
+      end
+    end
+  end
+end
---
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/pvc/submit_pvc_set.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+require 'aca_entities/magi_medicaid/libraries/iap_library'
+
+module FinancialAssistance
+  module Operations
+    module Applications
+      module Rrv
+        # operation to manually trigger pvc events.
+        # It will take families as input and find the determined application, add evidences and publish the group of applications
+        class SubmitPvcSet
+          include Dry::Monads[:result, :do]
+          include EventSource::Command
+          include EventSource::Logging
+
+          PVC_CSR_LIST = ["02", "04", "05", "06"].freeze
+  
+          # @param [Int] applications_per_event
+          # @param [Int] assistance_year
+          # @param [Array] csr_list
+          # @return [ Success ] Job successfully completed
+          def call(params)
+            values = yield validate(params)
+            families = find_families(values)
+            submit(params, families)
+
+            Success('Successfully Submitted RRV Set')
+          end
+
+          private
+
+          def validate(params)
+            errors = []
+            errors << 'applications_per_event ref missing' unless params[:applications_per_event]
+            errors << 'assistance_year ref missing' unless params[:assistance_year]
+            params[:csr_list] = PVC_CSR_LIST if params[:csr_list].blank?
+
+            errors.empty? ? Success(params) : Failure(errors)
+          end
+
+          def find_families(params)
+            # family_ids = FinancialAssistance::Application.where(aasm_state: "determined", assistance_year: params[:assistance_year]).distinct(:family_id)
+            Family.periodic_verifiable_for_assistance_year(params[:assistance_year], params[:csr_list])
+          end
+
+          def submit(params, families)
+            skip = params[:skip] || 0
+            applications_per_event = params[:applications_per_event]
+
+            while skip < families.count
+              criteria = families.skip(skip).limit(applications_per_event)
+              publish({families: criteria.pluck(:id), assistance_year: params[:assistance_year]})
+              puts "Total number of records processed #{skip + criteria.pluck(:id).length}"
+              skip += applications_per_event
+
+              break if params[:max_applications].present? && params[:max_applications] > skip
+            end
+          end
+
+          def build_event(payload)
+            event('events.iap.applications.request_family_pvc_determination', attributes: payload)
+          end
+
+          def publish(payload)
+            event = build_event(payload)
+            event.success.publish
+
+            Success("Successfully published the pvc payload")
+          end
+        end
+      end
+    end
+  end
+end
