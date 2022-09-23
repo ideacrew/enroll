9879131ee92e3d782f2d03ba08b44e4bda835a92
HArsHA Ellanki <h_vardhan@yahoo.com>
2022-09-23 16:06:59 -0400

    added feature switch to check enrollment while triggering hub calls on person update

--- spec/domain/operations/individual/determine_verifications_spec.rb
+++ spec/domain/operations/individual/determine_verifications_spec.rb
@@ -39,19 +39,69 @@ RSpec.describe Operations::Individual::DetermineVerifications, dbclean: :after_e
     end
   end
 
-  context 'when person and consumer role exists' do
+  describe 'when person and consumer role exists' do
     let(:person) { FactoryBot.create(:person, :with_consumer_role) }
     let(:consumer_role) { person.consumer_role }
 
-    it 'returns success' do
-      expect(subject.success?).to eq true
-      expect(subject.success).to eq "Successfully triggered Hub Calls for ConsumerRole with person_hbx_id: #{person.hbx_id}"
+    context "when trigger_verifications_before_enrollment_purchase is enabled" do
+      before do
+        EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(true)
+      end
+
+      it 'returns success' do
+        expect(subject.success?).to eq true
+        expect(subject.success).to eq "Successfully triggered Hub Calls for ConsumerRole with person_hbx_id: #{person.hbx_id}"
+      end
+
+      it 'updates consumer_role state to verification_outstanding' do
+        expect(consumer_role.aasm_state).to eq 'unverified'
+        subject
+        expect(consumer_role.reload.aasm_state).to eq 'verification_outstanding'
+      end
     end
 
-    it 'updates consumer_role state to verification_outstanding' do
-      expect(consumer_role.aasm_state).to eq 'unverified'
-      subject
-      expect(consumer_role.reload.aasm_state).to eq 'verification_outstanding'
+    context "when trigger_verifications_before_enrollment_purchase is disabled" do
+      before do
+        EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(false)
+      end
+
+      context "if person don't have an active enrollment" do
+        it 'returns success' do
+          expect(subject.success?).to eq false
+          expect(subject.failure).to eq "ConsumerRole with person_hbx_id: #{person.hbx_id} is not enrolled to trigger hub calls"
+        end
+
+        it 'updates consumer_role state to verification_outstanding' do
+          expect(consumer_role.aasm_state).to eq 'unverified'
+          subject
+          expect(consumer_role.reload.aasm_state).to eq 'unverified'
+        end
+      end
+
+      context "if person has an active enrollment" do
+        let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
+        let!(:enrollment) do
+          FactoryBot.create(
+            :hbx_enrollment,
+            :with_enrollment_members,
+            :individual_assisted,
+            family: family,
+            consumer_role_id: consumer_role.id,
+            enrollment_members: family.family_members
+          )
+        end
+
+        it 'returns success' do
+          expect(subject.success?).to eq true
+          expect(subject.success).to eq "Successfully triggered Hub Calls for ConsumerRole with person_hbx_id: #{person.hbx_id}"
+        end
+
+        it 'updates consumer_role state to verification_outstanding' do
+          expect(consumer_role.aasm_state).to eq 'unverified'
+          subject
+          expect(consumer_role.reload.aasm_state).to eq 'verification_outstanding'
+        end
+      end
     end
   end
 end
