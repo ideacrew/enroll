4313036e3a4d8a794036b6c8c170cf180db91781
mdkaraman <mdkaramanov@gmail.com>
2022-10-03 11:17:52 -0400

    update medicaid agency contact

--- components/financial_assistance/db/seedfiles/translations/en/dc/financial_assistance.rb
+++ components/financial_assistance/db/seedfiles/translations/en/dc/financial_assistance.rb
@@ -51,7 +51,7 @@ FINANCIAL_ASSISTANCE_TRANSLATIONS = {
   "en.faa.eligible_for_medicaid" => "These people <span>likely qualify</span> for ",
   "en.faa.eligible_for_medicaid2" => "&#58", # use html character code for colon so l10n helper doesn't break
   "en.faa.dhs_decision" => "<span>NOTE: The Department of Human Services (DHS) will make a final decision on whether those listed qualify for <span class='run-glossary'>Medicaid</span>.</span>",
-  "en.faa.dhs_contact" => "They will send you a letter, and may ask you to provide documents. If you haven’t heard from DHS within 45 days, you may want to ask for an update by calling DC Health link at ",
+  "en.faa.dhs_contact" => "They will send you a letter, and may ask you to provide documents. If you haven’t heard from DHS within 45 days, you may want to ask for an update by calling District Direct at ",
   "en.faa.primary_caretaker_question_text" => "Is this person the main person taking care of any children age 18 or younger? *",
   "en.faa.premium_reductions_1" => "These people <strong>qualify for lower monthly premiums. The monthly premium reduction is %{reduction_amount} per month </strong>.",
   "en.faa.premium_reductions_2" => "per month.</span> This means they won't have to pay full price for health insurance.",
