e9caf52c1f82cce4b5ab8c001fb380fe8a9202e9
Sarah Austin <scaustin34@gmail.com>
2022-10-13 15:06:55 -0400

    remove puts

--- spec/views/insured/families/_insurance_fields.html.erb_spec.rb
+++ spec/views/insured/families/_insurance_fields.html.erb_spec.rb
@@ -5,9 +5,8 @@ RSpec.describe "insured/families/_insurance_fields.html.erb" do
 
         it "should select yes by default" do
             EnrollRegistry[:default_is_your_health_coverage_ending_no].feature.stub(:is_enabled).and_call_original
-            p EnrollRegistry[:default_is_your_health_coverage_ending_no]
             render partial: "insured/families/insurance_fields"
-            p rendered
+
             expect(rendered).to have_selector('input#reason_accept[checked=checked]')
         end
 
