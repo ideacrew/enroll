0dcc16dd37b201c9749b56611cfd06c31cfbbae1
Kristin Merbach <kristin@kristinmerbach.com>
2022-10-14 17:31:17 -0700

    add new copy if ff set and not under open enrollment

--- components/financial_assistance/db/seedfiles/translations/en/me/financial_assistance.rb
+++ components/financial_assistance/db/seedfiles/translations/en/me/financial_assistance.rb
@@ -181,6 +181,7 @@ FINANCIAL_ASSISTANCE_TRANSLATIONS = {
   "en.faa.cost_savings_nav" => "Applications",
   "en.faa.cost_savings_applications" => "Applications",
   "en.faa.cost_savings_applications_desc" => "If you started or completed an application for financial assistance, it will be listed below. To update an application, find your current application and select 'Copy to new application'. If the status says it's a draft, that means you haven't submitted that application yet.",
+  "en.faa.cost_savings_applications_oe_desc" => "Application updates will not apply to %{next_year} enrollments. If you need to make changes to your application for %{next_year}, please log back in after Open Enrollment begins on %{oe_start}.",
   "en.faa.start_new_application" => "Start New Application",
   "en.faa.start_new_application_modal_body" => "You’re about to start a new application for cost savings. If you’ve got an application already, go to the most recent application and use ‘Actions’ to copy or update that application.",
   "en.faa.cancel" => "Cancel",
