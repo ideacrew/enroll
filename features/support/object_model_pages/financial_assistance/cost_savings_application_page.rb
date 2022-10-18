0fd8bed05d49b0da8ddbf195617786d62607f6ff
Kristin Merbach <kristin@kristinmerbach.com>
2022-10-17 17:18:45 -0700

    add cucumber coverage

--- features/support/object_model_pages/financial_assistance/cost_savings_application_page.rb
+++ features/support/object_model_pages/financial_assistance/cost_savings_application_page.rb
@@ -17,6 +17,18 @@ class CostSavingsApplicationPage
 
   def self.index_with_filter
     "[data-cuke='index_with_filter']"
+
+  def self.employer_sponsored_insurance_benefit_checkbox
+    "[data-cuke='employer_sponsored_insurance_benefit_checkbox']"
+  end
+
+  def self.esi_benefit
+    "[data-cuke='esi_benefit']"
+  end
+
+  def self.non_esi_benefit
+    "[data-cuke='non_esi_benefit']"
+
   end
 
   def self.meets_mvs_and_affordable
