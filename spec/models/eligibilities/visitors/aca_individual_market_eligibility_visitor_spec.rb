ebbf13cedcabf255cbd73aff8e4090da87895c74
Joseph Ogunbodede <57965400+j1joey@users.noreply.github.com>
2022-10-20 13:36:58 -0400

    Ran Rubocop

--- spec/models/eligibilities/visitors/aca_individual_market_eligibility_visitor_spec.rb
+++ spec/models/eligibilities/visitors/aca_individual_market_eligibility_visitor_spec.rb
@@ -126,7 +126,7 @@ RSpec.describe ::Eligibilities::Visitors::AcaIndividualMarketEligibilityVisitor,
       subject.call
       expect(subject.evidence.key?(evidence_item.key.to_sym)).to be_truthy
       evidence = subject.evidence[evidence_item.key.to_sym]
-      expect(evidence[:status]).to eq 'outstanding' 
+      expect(evidence[:status]).to eq 'outstanding'
       expect(evidence[:is_satisfied]).to be_falsey
       expect(evidence[:verification_outstanding]).to be_truthy
       expect(evidence[:due_on]).to eq due_on
@@ -135,7 +135,7 @@ RSpec.describe ::Eligibilities::Visitors::AcaIndividualMarketEligibilityVisitor,
 
   it_behaves_like 'outstanding verification type', 'outstanding'
   it_behaves_like 'outstanding verification type', 'rejected'
-  
+
   shared_examples_for 'outstanding verification type' do |status|
     let(:evidence_item) { eligibility_item.evidence_items.detect{|evidence_item| evidence_item.key == 'social_security_number'} }
 
@@ -146,7 +146,7 @@ RSpec.describe ::Eligibilities::Visitors::AcaIndividualMarketEligibilityVisitor,
       end
     end
 
-    it 'should set verification review true when verification status is #{status}' do
+    it "should set verification review true when verification status is #{status}" do
       subject.call
       expect(subject.evidence.key?(evidence_item.key.to_sym)).to be_truthy
       evidence = subject.evidence[evidence_item.key.to_sym]
