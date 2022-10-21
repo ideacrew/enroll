b077d7e1ab513f47985b7e291ea2b463286275ad
HArsHA Ellanki <h_vardhan@yahoo.com>
2022-10-20 23:19:53 -0400

    publish rrv events asynchronously

--- app/data_migrations/trigger_rrv_by_year.rb
+++ app/data_migrations/trigger_rrv_by_year.rb
@@ -9,18 +9,15 @@ class TriggerRrvByYear < MongoidMigrationTask
     skip = (ENV['skip'] || 0).to_i
     limit = (ENV['limit'] || 5000).to_i
 
-    family_ids = FinancialAssistance::Application.where(aasm_state: "determined", assistance_year: assistance_year, :"applicants.is_ia_eligible" => true).distinct(:family_id)
-    all_families = Family.where(:_id.in => family_ids)
+    max_applications = FinancialAssistance::Application.where(:aasm_state => "determined",
+                                                              :assistance_year => assistance_year,
+                                                              :"applicants.is_ia_eligible" => true).distinct(:family_id)
 
-    if all_families.count > 0
-      while skip < all_families.count
-        criteria = all_families.skip(skip).limit(limit)
-        FinancialAssistance::Operations::TestApplications::Rrv::CreateRrvRequest.new.call({families: criteria, assistance_year: assistance_year})
-        puts "Total number of records processed #{skip + criteria.pluck(:id).length}"
-        skip += limit
-      end
-    else
-      puts "No Determined applications with ia_eligible applicants in the year #{assistance_year}"
-    end
+    FinancialAssistance::Operations::Applications::Rrv::SubmitRrvSet.new.call({
+                                                                                assistance_year: assistance_year,
+                                                                                applications_per_event: limit,
+                                                                                skip: skip,
+                                                                                max_applications: max_applications.count + 100
+                                                                              })
   end
 end
