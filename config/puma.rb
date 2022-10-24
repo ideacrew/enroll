79923901a0c94dfa5ab04d04acb8deebb51adfb0
ipublic <dan@ideacrew.com>
2022-10-24 17:53:38 -0400

    initial settings

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 4)
+min_threads_count = Integer(ENV['RAILS_MIN_THREADS'] || 5)
+max_threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 10)
+threads min_threads_count, max_threads_count
+
+preload_app!
+
+port        ENV['PORT']     || 3000
+environment ENV['RAILS_ENV'] || 'development'
+
+on_worker_boot do
+  Mongoid::Clients.clients.each do |name, client|
+    client.close
+    client.reconnect
+  end
+end
+
+before_fork do
+  Mongoid.disconnect_clients
+end
