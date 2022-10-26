3f7bb53cbd6f6d2c07e2096f7ff4224c4d4bb453
mdkaraman <mdkaramanov@gmail.com>
2022-10-26 12:47:40 -0400

    enrollment history view spec clean up and refactor

--- spec/views/insured/families/enrollment_history.html.erb_spec.rb
+++ spec/views/insured/families/enrollment_history.html.erb_spec.rb
@@ -1,126 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe "insured/families/enrollment_history.html.erb" do
-#   let(:person_employee) { FactoryBot.create(:person, :with_employee_role, :with_family) }
   let(:person) { FactoryBot.create(:person, :with_family) }
   let(:person_hbx) { FactoryBot.create(:person, :with_hbx_staff_role, :with_family) }
   let(:family) { FactoryBot.create(:family, :with_primary_family_member) }
   let(:user_with_hbx_staff_role) { FactoryBot.create(:user, :with_family, :with_hbx_staff_role) }
   let(:user_with_consumer_role) { FactoryBot.create(:user, :with_family, :with_consumer_role) }
-#   let(:user_with_employer_role) { FactoryBot.create(:user, :with_family, :employer_staff) }
-#   let(:hbx_staff_permission) { FactoryBot.create(:permission, :hbx_staff) }
-#   let(:qle_first_of_month) { FactoryBot.create(:qualifying_life_event_kind, :effective_on_first_of_month, ) }
-#   let(:sep){
-#     sep = family.special_enrollment_periods.new
-#     sep.effective_on_kind = 'first_of_month'
-#     sep.qualifying_life_event_kind= qle_first_of_month
-#     sep.qualifying_life_event_kind_id = qle_first_of_month.id
-#     sep.qle_on= Date.new(TimeKeeper.date_of_record.year, 04, 14)
-#     sep.admin_flag = true
-#     sep
-#   }
-
-#   let(:hbx_enrollment) { HbxEnrollment.new(created_at: TimeKeeper.date_of_record, effective_on: TimeKeeper.date_of_record) }
   let(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, family: family) }
-  let(:term_hbx_enrollment) { HbxEnrollment.new(created_at: TimeKeeper.date_of_record, effective_on: TimeKeeper.date_of_record, aasm_state: "coverage_terminated") }
+  let(:canceled_hbx_enrollment) { FactoryBot.create(:hbx_enrollment, family: family, aasm_state: 'coverage_canceled') }
 
   before :each do
-    stub_template "insured/families/_right_column.html.erb" => ''
-    stub_template "insured/families/_qle_detail.html.erb" => ''
     stub_template "insured/families/_enrollment.html.erb" => ''
-    stub_template "insured/families/_navigation.html.erb" => ''
-    stub_template "insured/families/_shop_for_plans_widget.html.erb" => ''
-    stub_template "insured/families/_apply_for_medicaid_widget.html.erb" => ''
-    stub_template "insured/plan_shoppings/_help_with_plan.html.erb" => ''
-    # assign(:person, person_employee)
-    # assign(:person, person)
-    # allow(view).to receive(:current_user).and_return(user_with_employer_role)
-    # allow(view).to receive(:current_user).and_return(user_with_consumer_role)
-    # allow(view).to receive(:policy_helper).and_return(double("FamilyPolicy", can_view_entire_family_enrollment_history?: true))
-    # assign(:family, family)
-    EnrollRegistry[:medicaid_tax_credits_link].feature.stub(:is_enabled).and_return(true)
+    assign(:person, person)
   end
-
-#   it "should display the title" do
-#     render file: "insured/families/home.html.erb"
-#     allow(family).to receive(:active_seps).and_return(false)
-#     expect(rendered).to have_selector('h1', text: "My #{EnrollRegistry[:enroll_app].setting(:short_name).item}")
-#   end
-
-#   it "should have plan-summary area" do
-#     render file: "insured/families/home.html.erb"
-#     allow(family).to receive(:active_seps).and_return(false)
-#     expect(rendered).to have_selector('div#plan-summary')
-#   end
-
-#   it "should display 'existing SEP - Eligible to enroll' partial if there is an active admin SEP" do
-#     render file: "insured/families/home.html.erb"
-#     assign(:active_sep, sep)
-#     render file: "insured/families/home.html.erb"
-#     expect(rendered).to have_selector('div#qle-details-for-existing-sep')
-#   end
-
-#   it "should not display 'existing SEP - Eligible to enroll' partial if there is an active admin SEP" do
-#     sep.qualifying_life_event_kind.update_attributes!(is_active: false)
-#     render file: "insured/families/home.html.erb"
-#     assign(:active_sep, sep)
-#     render file: "insured/families/home.html.erb"
-#     expect(rendered).to_not have_selector('div#qle-details-for-existing-sep')
-#   end
-
-#   it "should not display 'existing SEP - Eligible to enroll' partial if there is no active admin SEP" do
-#     assign(:active_sep, [])
-#     render file: "insured/families/home.html.erb"
-#     expect(rendered).to_not have_selector('div#qle-details-for-existing-sep')
-#   end
-
-#   it "should not display 'sep message' partial if there is no active qle kind" do
-#     sep.qualifying_life_event_kind.update_attributes!(is_active: false)
-#     assign(:active_sep, sep)
-#     render file: "insured/families/home.html.erb"
-#     expect(rendered).to_not have_selector('div#sep_message')
-#   end
-
-#   it "should display 'sep message' partial if there is an active qle kind" do
-#     assign(:active_sep, sep)
-#     render file: "insured/families/home.html.erb"
-#     expect(rendered).to have_selector('div#sep_message')
-#   end
 
   context 'user is HBX admin' do
     before do
-      assign(:person, person_hbx)
       allow(view).to receive(:current_user).and_return(user_with_hbx_staff_role)
-      assign(:family, family)
-      assign(:all_hbx_enrollments_for_admin, [hbx_enrollment, term_hbx_enrollment])
-    end
-
-    it "should display select box to show all enrollments" do
       allow(view).to receive(:policy_helper).and_return(
         double("FamilyPolicy", can_view_entire_family_enrollment_history?: true)
       )
-      render file: "insured/families/enrollment_history.html.erb"
-      binding.irb
-      expect(rendered).to include("Display All Enrollments?")
     end
+
+    context 'user has enrollments' do
+      before do
+        assign(:all_hbx_enrollments_for_admin, [hbx_enrollment, canceled_hbx_enrollment])
+      end
+
+      it "should display select box to show all enrollments" do
+        render
+        expect(rendered).to match /display-all-enrollments-qs/
+      end
+    end
+
+    context 'user DOES NOT have enrollments' do
+        before do
+          assign(:all_hbx_enrollments_for_admin, [])
+        end
+  
+        it "should NOT display select box to show all enrollments" do
+          render
+          expect(rendered).to_not match /display-all-enrollments-qs/
+        end
+      end
   end
 
   context 'user is NOT HBX admin' do
     before do
-      assign(:person, person)
       allow(view).to receive(:current_user).and_return(user_with_consumer_role)
       assign(:hbx_enrollments, [hbx_enrollment])
-      # assign(:all_qle_events, [qle_first_of_month])
-      assign(:family, family)
-    end
-
-    it "should NOT display select box to show all enrollments" do
-    #   assign(:person, person)
       allow(view).to receive(:policy_helper).and_return(
         double("FamilyPolicy", can_view_entire_family_enrollment_history?: nil)
       )
-      render file: "insured/families/enrollment_history.html.erb"
-      expect(rendered).to_not include("Display All Enrollments?")
+    end
+
+    it "should NOT display select box to show all enrollments" do
+      render
+      expect(rendered).to_not match /display-all-enrollments-qs/
     end
   end
 end
