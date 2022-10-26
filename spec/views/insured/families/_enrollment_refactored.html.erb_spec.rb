0b23a949dd85af8fc5c2b15bc0a622d08a01769e
Sarah Austin <scaustin34@gmail.com>
2022-10-26 17:12:22 -0400

    case fix for expect have text

--- spec/views/insured/families/_enrollment_refactored.html.erb_spec.rb
+++ spec/views/insured/families/_enrollment_refactored.html.erb_spec.rb
@@ -299,7 +299,7 @@ RSpec.describe "insured/families/_enrollment_refactored.html.erb" do
       end
 
       it 'displays future_enrollment_termination_date when enrollment is in coverage_termination_pending state' do
-        expect(rendered).to match(/Future Enrollment Termination Date/)
+        expect(rendered).to have_text(/Future enrollment termination date/)
       end
 
       it 'displays terminated_on when coverage_termination_pending and not future_enrollment_termination_date' do
