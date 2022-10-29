8961d01856bc7f1175aa79cce30bd562d433c569
Raghuram <raghuramg83@gmail.com>
2022-10-29 15:42:42 -0400

    Added mongo configurations for puma

--- .docker/config/puma.rb
+++ .docker/config/puma.rb
@@ -10,6 +10,8 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+preload_app!
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT", 3000)
@@ -17,6 +19,17 @@ port        ENV.fetch("PORT", 3000)
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV", "development")
+
+on_worker_boot do
+  Mongoid::Clients.clients.each do |_name, client|
+    client.close
+    client.reconnect
+  end
+end
+
+before_fork do
+  Mongoid.disconnect_clients
+end
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
