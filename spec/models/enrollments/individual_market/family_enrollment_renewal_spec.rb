e9b2a69d94d9a597401cdfc03db601c325a3ec7c
Nisanth Yaganti <yagantinisanth@gmail.com>
2022-10-31 00:45:59 -0400

    family enrollment renewal spec fix

--- spec/models/enrollments/individual_market/family_enrollment_renewal_spec.rb
+++ spec/models/enrollments/individual_market/family_enrollment_renewal_spec.rb
@@ -360,6 +360,7 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
 
       context 'when mthh enabled' do
         before do
+          allow(UnassistedPlanCostDecorator).to receive(:new).and_return(double(total_ehb_premium: 1390, total_premium: 1390))
           EnrollRegistry[:temporary_configuration_enable_multi_tax_household_feature].feature.stub(:is_enabled).and_return(true)
         end
 
@@ -593,7 +594,6 @@ if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
           enrollment.product = dental_product
           enrollment.save!
           EnrollRegistry[:temporary_configuration_enable_multi_tax_household_feature].feature.stub(:is_enabled).and_return(true)
-          allow(UnassistedPlanCostDecorator).to receive(:new).and_return(double(total_ehb_premium: 1390, total_premium: 1390))
         end
 
         context 'unassisted renewal' do
