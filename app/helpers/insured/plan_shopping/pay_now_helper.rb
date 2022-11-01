d55c075cc926a94f7b5d68c7ee9ce7ab0aa9fc8c
Jay Reddy <jayanth.rordev@gmail.com>
2022-11-01 16:22:35 -0400

    taro Pay Now updates (#2331)
    * Taro Pay now Updates
    
    * latest updates
    
    * remove unused updates
    
    * latest updates
    
    * revert changes related to env change
    
    * latest updates
    
    Co-authored-by: Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>

--- app/helpers/insured/plan_shopping/pay_now_helper.rb
+++ app/helpers/insured/plan_shopping/pay_now_helper.rb
@@ -14,7 +14,8 @@ module Insured
         "Harvard Pilgrim Health Care" => "https://www.harvardpilgrim.org/public/home",
         "Anthem Blue Cross and Blue Shield" => "https://www.anthem.com/contact-us/maine",
         "Northeast Delta Dental" => "https://www.nedelta.com/Home",
-        "Taro Health Plan of Maine, Inc." => "https://uatthi.onlineinsight.com/ebpp/eapp/paynow"
+        "Taro Health Plan of Maine, Inc." => EnrollRegistry['taro_health_pay_now'].setting(:taro_health_home_page_url).item
+
       }.freeze
 
       # rubocop:disable Metrics/CyclomaticComplexity
