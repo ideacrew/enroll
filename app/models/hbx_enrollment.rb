6288479a097d9531ae4c450cb64ee3f21427ca21
Raghu Ram <raghuramg83@gmail.com>
2022-11-07 11:21:57 -0500

    fix for renewal cancellation issue when plan shopped under active plan year (#2348)
    Refs#100564 Fix for renewal cancellation issue when plan shopped under current plan year
    
    Co-authored-by: Varun <vrbattula@outlook.com>
    Co-authored-by: Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>

--- app/models/hbx_enrollment.rb
+++ app/models/hbx_enrollment.rb
@@ -1018,7 +1018,7 @@ class HbxEnrollment
 
   def terminate_coverage_with(termination_date)
     # IVL enrollments go automatically to coverage_terminated
-    if termination_date.to_date >= TimeKeeper.date_of_record && is_shop?
+    if is_shop? && termination_date.to_date >= TimeKeeper.date_of_record
       schedule_coverage_termination!(termination_date) if may_schedule_coverage_termination?
     else
       if may_terminate_coverage?
@@ -1101,8 +1101,11 @@ class HbxEnrollment
     return unless successor_application&.is_submitted?
     return unless enrollment_benefit_application.successors.include?(successor_application)
 
-    passive_renewals_under(successor_application).each{|en| en.cancel_coverage! if en.may_cancel_coverage? }
-    renew_benefit(successor_benefit_package) if active_renewals_under(successor_application).blank? && successor_application.coverage_renewable? && non_inactive_transition? && non_terminated_enrollment?
+    passive_renewals = passive_renewals_under(successor_application).to_a
+    passive_renewals.select!(&:renewing_waived?) if dummy_inactive_transition? # cancel only renewal waivers for waived enrollment updates
+    passive_renewals.each {|en| en.cancel_coverage! if en.may_cancel_coverage? }
+
+    renew_benefit(successor_benefit_package) if active_renewals_under(successor_application).blank? && successor_application.coverage_renewable? && !dummy_inactive_transition? && non_terminated_enrollment?
   end
 
   def reinstated_app
@@ -1153,8 +1156,8 @@ class HbxEnrollment
                                    :effective_on.gte => benefit_application.start_on })
   end
 
-  def non_inactive_transition?
-    !(aasm.from_state == :inactive && aasm.to_state == :inactive)
+  def dummy_inactive_transition?
+    (aasm.from_state == :inactive && aasm.to_state == :inactive)
   end
 
   def non_terminated_enrollment?
@@ -2780,7 +2783,6 @@ class HbxEnrollment
 
   def update_osse_childcare_subsidy
     effective_year = sponsored_benefit_package.start_on.year
-
     return if coverage_kind.to_s == 'dental'
     return unless employee_role&.osse_eligible?(effective_on)
     return unless shop_osse_eligibility_is_enabled?(effective_year)
