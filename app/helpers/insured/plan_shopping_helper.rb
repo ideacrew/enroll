6af06a972c859298b9f59e251182ad0416e28436
Kristin Merbach <kristin@kristinmerbach.com>
2022-11-09 14:52:20 -0800

    rubocop

--- app/helpers/insured/plan_shopping_helper.rb
+++ app/helpers/insured/plan_shopping_helper.rb
@@ -1,23 +1,28 @@
-module Insured::PlanShoppingHelper
-  include L10nHelper
+# frozen_string_literal: true
 
-  def is_determined_and_not_csr_0?(entity, enrollment)
-    return false unless entity && enrollment
-    if EnrollRegistry.feature_enabled?(:temporary_configuration_enable_multi_tax_household_feature)
-      enrolled_family_member_ids = enrollment.hbx_enrollment_members.map(&:applicant_id)
-      csr_op = ::Operations::PremiumCredits::FindCsrValue.new.call({
-                                                                     family: enrollment.family,
-                                                                     year: enrollment.effective_on.year,
-                                                                     family_member_ids: enrolled_family_member_ids
-                                                                   })
+module Insured
+  # view helper methods for planning shopping page
+  module PlanShoppingHelper
+    include L10nHelper
 
-      return false unless csr_op.success?
+    def is_determined_and_not_csr_0?(entity, enrollment)
+      return false unless entity && enrollment
+      if EnrollRegistry.feature_enabled?(:temporary_configuration_enable_multi_tax_household_feature)
+        enrolled_family_member_ids = enrollment.hbx_enrollment_members.map(&:applicant_id)
+        csr_op = ::Operations::PremiumCredits::FindCsrValue.new.call({
+                                                                       family: enrollment.family,
+                                                                       year: enrollment.effective_on.year,
+                                                                       family_member_ids: enrolled_family_member_ids
+                                                                     })
 
-      valid_csr_eligibility_kind = csr_op.value!
-    else
-      valid_csr_eligibility_kind = entity.valid_csr_kind(enrollment)
+        return false unless csr_op.success?
+
+        valid_csr_eligibility_kind = csr_op.value!
+      else
+        valid_csr_eligibility_kind = entity.valid_csr_kind(enrollment)
+      end
+
+      (EligibilityDetermination::CSR_KINDS.include? valid_csr_eligibility_kind.to_s) && (valid_csr_eligibility_kind.to_s != 'csr_0')
     end
-
-    (EligibilityDetermination::CSR_KINDS.include? valid_csr_eligibility_kind.to_s) && (valid_csr_eligibility_kind.to_s != 'csr_0')
   end
 end
