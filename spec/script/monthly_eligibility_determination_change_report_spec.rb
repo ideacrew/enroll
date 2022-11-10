c43ce33f27345cd0a73667fb30ab4df919140a58
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2022-11-10 20:19:04 +0000

    add monthly version of iap application report

---
+++ spec/script/monthly_eligibility_determination_change_report_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require File.join(Rails.root, 'spec/shared_contexts/ivl_eligibility')
+
+describe 'monthly_eligibility_determination_change_report' do
+  before do
+    DatabaseCleaner.clean
+  end
+  include_context 'setup two tax households with one ia member each'
+
+  before :each do
+    EnrollRegistry[:temporary_configuration_enable_multi_tax_household_feature].feature.stub(:is_enabled).and_return(false)
+    tax_household.update_attributes!(created_at: DateTime.now - 1.month)
+    invoke_monthly_eligibility_change_script
+    @file_content = CSV.read("#{Rails.root}/monthly_eligibility_report_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv")
+  end
+
+  it 'should add data to the file' do
+    expect(@file_content.size).to be 2
+  end
+
+  it 'should match with the first name' do
+    expect(@file_content[1][0]).to eq(person.first_name)
+  end
+
+  it 'should match with the last name' do
+    expect(@file_content[1][1]).to eq(person.last_name)
+  end
+
+  it 'should match with the hbx id' do
+    expect(@file_content[1][2]).to eq(person.hbx_id)
+  end
+
+  it 'should match with the max aptc' do
+    expect(@file_content[1][5]).to eq format('%.2f', eligibilty_determination.max_aptc.to_f)
+  end
+
+  it 'should match with the csr percent as integer' do
+    expect(@file_content[1][7]).to eq(eligibilty_determination.csr_percent_as_integer.to_s)
+  end
+
+  it 'should return Curam as the source Curam' do
+    expect(@file_content[1][10]).to eq('Curam')
+  end
+
+  it 'should display FPL amount' do
+    expect(@file_content.first[15]).to eq("Current_FPL_Amount")
+  end
+
+  after :each do
+    FileUtils.rm_rf("#{Rails.root}/monthly_eligibility_report_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv")
+    FileUtils.rm_rf("#{Rails.root}/monthly_eligibility_report_logger_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv")
+  end
+end
+
+def invoke_monthly_eligibility_change_script
+  eligibility_change_script = File.join(Rails.root, "script/monthly_eligibility_determination_change_report.rb")
+  load eligibility_change_script
+end
