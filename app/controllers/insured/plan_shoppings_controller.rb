cdc0945875c5ed80ee68ce4efd9f94c74626ced9
Kristin Merbach <kristin@kristinmerbach.com>
2022-11-14 14:47:38 -0800

    only call once

--- app/controllers/insured/plan_shoppings_controller.rb
+++ app/controllers/insured/plan_shoppings_controller.rb
@@ -350,7 +350,7 @@ class Insured::PlanShoppingsController < ApplicationController
     if @tax_household.present? || @aptc_grants.present?
       entity = @tax_household || @aptc_grants
       @csr_available = is_eligibility_determined_and_not_csr_0?(entity)
-      if is_eligibility_determined_and_not_csr_0?(entity)
+      if @csr_available
         sort_for_csr(@plans)
       else
         sort_by_standard_plans(@plans)
