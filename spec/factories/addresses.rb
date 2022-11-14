46b5f4d4e24af1c42ac4f56f6249a4d74bf5bab4
RTH_RoR <rohith.rubyrails@gmail.com>
2022-11-14 11:27:35 -0500

    revert change on factories/addresses.rb

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     kind { 'home' }
     sequence(:address_1, 1111) { |n| "#{n} Awesome Street NE" }
     sequence(:address_2, 111) { |n| "##{n}" }
-    city { 'Washington dc' }
+    city { 'Washington' }
     state { Settings.aca.state_abbreviation }
     zip { '01001' }
     county { 'Hampden' }
