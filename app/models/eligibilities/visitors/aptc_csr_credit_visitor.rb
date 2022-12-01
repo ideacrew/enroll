0c5c4459afcc220dcfd7f27b8b9cf562f5e537dc
Haridhar Yamjala <hariym471@gmail.com>
2022-12-01 17:36:28 -0500

    extends app range for ov

--- app/models/eligibilities/visitors/aptc_csr_credit_visitor.rb
+++ app/models/eligibilities/visitors/aptc_csr_credit_visitor.rb
@@ -41,7 +41,7 @@ module Eligibilities
           :family_id => subject.family.id,
           :aasm_state => 'determined',
           :effective_date.gte => DateTime.new(year_begin.year, year_begin.month, year_begin.day) - 6.months,
-          :effective_date.lte => DateTime.new(year_end.year, year_end.month, year_end.day)
+          :effective_date.lte => DateTime.new(year_end.year, year_end.month, year_end.day) + 3.months
         ).last
       end
 
