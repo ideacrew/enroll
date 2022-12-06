17e1198f326d65d337037532063198fa373aef31
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2022-12-06 17:54:14 -0500

    Migration to update TaxHouseholdEnrollments (#2430)
    * Migration to update TaxHouseholdEnrollments
    
    * Update tax_household_member_enrollment_member with updated EnrMember Id

--- app/domain/operations/premium_credits/find_aptc_with_tax_households.rb
+++ app/domain/operations/premium_credits/find_aptc_with_tax_households.rb
@@ -115,12 +115,12 @@ module Operations
           next if member_info.blank?
 
           th_member_enr_member = th_enrollment.tax_household_members_enrollment_members.find_or_create_by(
-            hbx_enrollment_member_id: hbx_enrollment_member_id&.to_s,
-            tax_household_member_id: tax_household_member_id&.to_s
+            family_member_id: family_member_id
           )
 
           th_member_enr_member.update!(
-            family_member_id: family_member_id,
+            hbx_enrollment_member_id: hbx_enrollment_member_id&.to_s,
+            tax_household_member_id: tax_household_member_id&.to_s,
             age_on_effective_date: member_info.age_on_effective_date,
             relationship_with_primary: member_info.relationship_with_primary,
             date_of_birth: member_info.date_of_birth
