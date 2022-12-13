355356b6b0e522ac221babee2f7d2b93a8dba704
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2022-12-13 14:37:28 -0500

    Make changes to script to run on specific enrollments.

--- script/list_of_tax_household_enrollments.rb
+++ script/list_of_tax_household_enrollments.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 # This script generates a CSV report with information about Enrollments with TaxHouseholdEnrollments
-# rails runner script/list_of_tax_household_enrollments.rb -e production
+# To run this for all 2022 enrollments
+# bundle exec rails runner script/list_of_tax_household_enrollments.rb
+
+# To run this on specific enrollments
+# bundle exec rails runner script/list_of_tax_household_enrollments.rb enr_hbx_ids='127633, 1987222, 3746543'
 
 def process_enrollments(enrollments, logger)
   file_name = "#{Rails.root}/list_of_enrollments_with_thh_enr_info.csv"
@@ -48,13 +52,24 @@ def process_enrollments(enrollments, logger)
   end
 end
 
+def find_enrollments
+  show_enrollments_sans_canceled = %w[coverage_selected transmitted_to_carrier coverage_enrolled coverage_termination_pending unverified coverage_reinstated auto_renewing renewing_coverage_selected renewing_transmitted_to_carrier
+                                      renewing_coverage_enrolled auto_renewing_contingent renewing_contingent_selected renewing_contingent_transmitted_to_carrier renewing_contingent_enrolled coverage_terminated coverage_expired
+                                      inactive renewing_waived]
+
+  hbx_ids = ENV['enr_hbx_ids'].to_s.split(',').map(&:squish!)
+
+  if hbx_ids.present?
+    HbxEnrollment.by_year(2022).by_coverage_kind('health').where(:aasm_state.in => show_enrollments_sans_canceled, :hbx_id.in => hbx_ids)
+  else
+    HbxEnrollment.by_year(2022).by_coverage_kind('health').where(:aasm_state.in => show_enrollments_sans_canceled)
+  end
+end
+
 start_time = DateTime.current
 logger = Logger.new("#{Rails.root}/migration_validation_log_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log")
 logger.info "Migration Report start_time: #{start_time}"
-show_enrollments_sans_canceled = %w[coverage_selected transmitted_to_carrier coverage_enrolled coverage_termination_pending unverified coverage_reinstated auto_renewing renewing_coverage_selected renewing_transmitted_to_carrier
-                                    renewing_coverage_enrolled auto_renewing_contingent renewing_contingent_selected renewing_contingent_transmitted_to_carrier renewing_contingent_enrolled coverage_terminated coverage_expired
-                                    inactive renewing_waived]
-enrollments = HbxEnrollment.by_year(2022).by_coverage_kind('health').where(:aasm_state.in => show_enrollments_sans_canceled)
+enrollments = find_enrollments
 process_enrollments(enrollments, logger)
 end_time = DateTime.current
 logger.info "Migration Report end_time: #{end_time}, total_time_taken_in_minutes: #{((end_time - start_time) * 24 * 60).to_i}"
