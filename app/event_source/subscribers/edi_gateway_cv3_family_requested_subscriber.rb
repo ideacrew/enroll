6921c32c123322519166b010edba7586ff62bc91
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-01-06 10:16:21 -0500

    log messages to specific event file

--- app/event_source/subscribers/edi_gateway_cv3_family_requested_subscriber.rb
+++ app/event_source/subscribers/edi_gateway_cv3_family_requested_subscriber.rb
@@ -6,16 +6,25 @@ module Subscribers
     include ::EventSource::Subscriber[amqp: 'edi_gateway.families.cv3_family']
 
     subscribe(:on_requested) do |delivery_info, _metadata, response|
-      logger.info "Subscribers::EdiGatewayCv3FamilyRequestedSubscriber: invoked on_requested with response: #{response.inspect}"
+      subscriber_logger = subscriber_logger_for(:on_requested)
+      subscriber_logger.info "Subscribers::EdiGatewayCv3FamilyRequestedSubscriber: invoked on_requested with response: #{response.inspect}"
       payload = JSON.parse(response, symbolize_names: true)
       result = Operations::EdiGateway::PublishCv3Family.new.call(payload)
 
-      logger.info "Subscribers::EdiGatewayCv3FamilyRequestedSubscriber => #{payload} -- success: #{result.success?} -- output: #{result}" unless Rails.env.test?
+      subscriber_logger.info "Subscribers::EdiGatewayCv3FamilyRequestedSubscriber => #{payload} -- success: #{result.success?} -- output: #{result}" unless Rails.env.test?
 
       ack(delivery_info.delivery_tag)
     rescue StandardError, SystemStackError => e
-      logger.info "Subscribers::EdiGatewayCv3FamilyRequestedSubscriber:: errored & acked. Backtrace: #{e.backtrace}"
+      subscriber_logger.info "Subscribers::EdiGatewayCv3FamilyRequestedSubscriber:: errored & acked. Backtrace: #{e.backtrace}"
       ack(delivery_info.delivery_tag)
+    end
+
+    private
+
+    def subscriber_logger_for(event)
+      Logger.new(
+        "#{Rails.root}/log/#{event}_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log"
+      )
     end
   end
 end
