a1326f7f2729bc006f1f361da643865fe744de69
Jacob Kagon <jacob@kagon.net>
2023-01-15 21:34:22 -0500

    fix syntax error

--- features/financial_assistance/step_definitions/work_flow_steps.rb
+++ features/financial_assistance/step_definitions/work_flow_steps.rb
@@ -150,6 +150,8 @@ end
 
 Then(/^the oe application warning will not display$/) do
   expect(page.has_css?(CostSavingsApplicationPage.oe_application_warning_display)).to eq false
+end
+
 Then(/^the coverage update reminder warning will display$/) do
   expect(page.has_css?(CostSavingsApplicationPage.coverage_update_reminder_display)).to eq true
 end
