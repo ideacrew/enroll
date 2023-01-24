5b4fbc3fc62b5024e7d7b3450a3fb833705baf09
Marco Ornelas <marco.ornelas@ideacrew.com>
2023-01-24 18:39:36 -0500

    rubocop fixes

--- app/domain/operations/slcsp_calculation.rb
+++ app/domain/operations/slcsp_calculation.rb
@@ -22,57 +22,57 @@ module Operations
     def process(params)
       puts params
       result = {}
-      for i in 1..12 do
+      (1..12).each do |i|
         month_key = Date::MONTHNAMES[i][0..2].downcase.to_sym
         params[:members].each do |member|
-            return unless member[:coverage][month_key]
-            value=calculate(member, params[:taxYear], params[:state], i, month_key)
-            result[month_key] = value.value!
+          next unless member[:coverage][month_key]
+          value = calculate(member, params[:taxYear], params[:state], i, month_key)
+          result[month_key] = value.value!
         end
       end
       Success(result)
     end
 
     def calculate(member, assistance_year, state, month, month_key)
-        payload = build_operation_payload(member, assistance_year, state, month, month_key)
-        result = Operations::BenchmarkProducts::IdentifySlcspWithPediatricDentalCosts.new.call(payload)
-        Failure("Unable to calculate SLCSP for #{member[:name]}") unless result.success?
-        Success(result.value![:household_group_benchmark_ehb_premium])
+      payload = build_operation_payload(member, assistance_year, state, month, month_key)
+      result = Operations::BenchmarkProducts::IdentifySlcspWithPediatricDentalCosts.new.call(payload)
+      Failure("Unable to calculate SLCSP for #{member[:name]}") unless result.success?
+      Success(result.value![:household_group_benchmark_ehb_premium])
     end
 
-    def build_operation_payload(member, assistance_year, state, month, month_key)
-        residence_data=resolve_residence(member[:residences], month_key)
+    def build_operation_payload(member, assistance_year, _state, month, month_key)
+      residence_data = resolve_residence(member[:residences], month_key)
         # puts member.dig("dob","day").class
-        dob = Date.new(member[:dob][:year], member[:dob][:month], member[:dob][:day])        
-        {
-            rating_address: {
-              county: residence_data[:county][:name],
-              zip: residence_data[:county][:fips],
-              state: residence_data[:county][:state]
-            },
-            effective_date: Date.new(assistance_year, month, 1),
-            households: [
-              {
-                household_id: generate_id,
-                members: [
-                  {
-                    relationship_with_primary: 'self',
-                    date_of_birth: dob
-                  }
-                ]
-              }
-            ]
-          }
+      dob = Date.new(member[:dob][:year], member[:dob][:month], member[:dob][:day])
+      {
+        rating_address: {
+          county: residence_data[:county][:name],
+          zip: residence_data[:county][:fips],
+          state: residence_data[:county][:state]
+        },
+        effective_date: Date.new(assistance_year, month, 1),
+        households: [
+            {
+              household_id: generate_id,
+              members: [
+                {
+                  relationship_with_primary: 'self',
+                  date_of_birth: dob
+                }
+              ]
+            }
+          ]
+      }
     end
 
     def resolve_residence(residences, month_key)
-        residences.each do |residence|
-            return residence if residence[:months][month_key]
-        end
+      residences.each do |residence|
+        return residence if residence[:months][month_key]
+      end
     end
 
     def generate_id
-        SecureRandom.uuid.gsub("-","")[1..10]   
+      SecureRandom.uuid.gsub("-","")[1..10]
     end
 
     def validate(params)
