b83c323cf702d26a93b0e8fa11c4b6389e43a95c
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-01-24 18:49:47 -0500

    pass in exclusion list as an argument

--- spec/domain/operations/edi_gateway/trigger_bulk_tax1095a_notices_spec.rb
+++ spec/domain/operations/edi_gateway/trigger_bulk_tax1095a_notices_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ::Operations::EdiGateway::TriggerBulkTax1095aNotices do
 
   describe 'with invalid params' do
     it "should fail to publish" do
-      result = described_class.new.call({tax_year: Date.today.year,tax_form_type: ""})
+      result = described_class.new.call({tax_year: Date.today.year,tax_form_type: "", exclusion_list: []})
       expect(result.success?).to be_falsey
       expect(result.failure).to eq("Valid tax form type is not present")
     end
@@ -14,7 +14,7 @@ RSpec.describe ::Operations::EdiGateway::TriggerBulkTax1095aNotices do
 
   describe 'with valid params' do
     it "return success" do
-      result = described_class.new.call({tax_year: Date.today.year,tax_form_type: "IVL_TAX"})
+      result = described_class.new.call({tax_year: Date.today.year,tax_form_type: "IVL_TAX", exclusion_list: []})
       expect(result.success?).to be_truthy
     end
   end
