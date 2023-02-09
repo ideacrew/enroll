af642e39f7af71ba417431c539f6e6980a91b6bf
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2023-02-09 15:33:32 -0500

    add event, publisher and operation for families found_by and find_by (#2576)
    * update aca_entities revision to latest
    
    * add event, publisher and operation for families found_by and find_by
    
    * use error instead of info to log errors

---
+++ app/event_source/subscribers/families/find_by_requested_subscriber.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Subscribers
+  module Families
+    # Subscriber will receive a request(from edi_gateway) to find family
+    class FindByRequestedSubscriber
+      include ::EventSource::Subscriber[amqp: 'edi_gateway.families']
+
+      subscribe(:on_find_by_requested) do |delivery_info, _properties, response|
+        logger.info "on_find_by_requested response: #{response}"
+        subscriber_logger = subscriber_logger_for(:on_families_find_by_requested)
+        response = JSON.parse(response, symbolize_names: true)
+        logger.info "on_find_by_requested response: #{response}"
+        subscriber_logger.info "on_find_by_requested response: #{response}"
+
+        process_find_by_requested_event(subscriber_logger, response) unless Rails.env.test?
+
+        ack(delivery_info.delivery_tag)
+      rescue StandardError, SystemStackError => e
+        logger.error "on_find_by_requested error: #{e} backtrace: #{e.backtrace}; acked (nacked)"
+        ack(delivery_info.delivery_tag)
+      end
+
+      private
+
+      def error_messages(result)
+        if result.failure.is_a?(Dry::Validation::Result)
+          result.failure.errors.to_h
+        else
+          result.failure
+        end
+      end
+
+      def process_find_by_requested_event(subscriber_logger, response)
+        subscriber_logger.info "process_find_by_requested_event: ------- start"
+        result = ::Operations::Families::FindBy.new.call(response)
+
+        if result.success?
+          message = result.success
+          subscriber_logger.info "on_find_by_requested acked #{message.is_a?(Hash) ? message[:event] : message}"
+        else
+          subscriber_logger.info "process_find_by_requested_event: failure: #{error_messages(result)}"
+        end
+        subscriber_logger.info "process_find_by_requested_event: ------- end"
+      rescue StandardError => e
+        subscriber_logger.error "process_find_by_requested_event: error: #{e} backtrace: #{e.backtrace}"
+        subscriber_logger.error "process_find_by_requested_event: ------- end"
+      end
+
+      def subscriber_logger_for(event)
+        Logger.new("#{Rails.root}/log/#{event}_#{Date.today.in_time_zone('Eastern Time (US & Canada)').strftime('%Y_%m_%d')}.log")
+      end
+    end
+  end
+end
---
+++ spec/domain/operations/families/find_by_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ::Operations::Families::FindBy, dbclean: :after_each do
+  subject { described_class.new.call(input_params) }
+
+  let(:input_params) { { person_hbx_id: person_hbx_id, year: year } }
+
+  describe '#call' do
+    let(:person) { FactoryBot.create(:person, :with_consumer_role) }
+    let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person) }
+
+    context 'with valid input params' do
+      let(:person_hbx_id) { family.primary_person.hbx_id }
+      let(:year) { TimeKeeper.date_of_record.year }
+
+      it 'returns a success with a message' do
+        expect(
+          subject.success
+        ).to eq('Successfully published event: events.families.found_by')
+      end
+    end
+
+    context 'with invalid input params' do
+      context 'invalid person hbx_id' do
+        let(:person_hbx_id) { 'sdcb982c2e83' }
+        let(:year) { TimeKeeper.date_of_record.year }
+
+        it 'returns a failure with a message' do
+          expect(
+            subject.failure
+          ).to eq("Unable to find person with hbx_id: #{person_hbx_id}")
+        end
+      end
+
+      context 'person without any family' do
+        let(:person_hbx_id) { person.hbx_id }
+        let(:year) { TimeKeeper.date_of_record.year }
+
+        it 'returns a failure with a message' do
+          expect(
+            subject.failure
+          ).to eq("No primary family exists for person with hbx_id: #{person_hbx_id}")
+        end
+      end
+    end
+  end
+end
