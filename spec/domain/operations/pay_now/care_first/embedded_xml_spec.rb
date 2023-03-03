54eb6eeddda39cdf7871ef137782305d1475a485
Esme Rubinstein <erubinst@oberlin.edu>
2023-03-03 15:03:44 -0500

    starting on specs

---
+++ spec/domain/operations/pay_now/care_first/embedded_xml_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Operations::PayNow::CareFirst::EmbeddedXml do
+    let!(:person) {FactoryBot.create(:person, :with_consumer_role, :with_active_consumer_role)}
+    let!(:family) {FactoryBot.create(:family, :with_primary_family_member, person: person)}
+    let(:product) {FactoryBot.create(:benefit_markets_products_health_products_health_product, benefit_market_kind: :aca_individual, kind: :health, csr_variant_id: '01')}
+    let!(:hbx_enrollment) { FactoryBot.create(:hbx_enrollment, family: family, household: family.active_household, aasm_state: 'shopping', product: product) }
+
+    describe 'with invalid params' do
+        it "fails with invalid enrollment " do
+          result = described_class.new.call("000000")
+          expect(result.success?).to be_falsey
+          expect(result.failure).to eq("unable to transform hbx enrollment 000000")
+        end
+    end
+
+end
