f7935ab34a0d210a9c8914f4764d42bdb32cd519
Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
2023-03-08 10:58:14 -0500

    creating new enroll operation to handle cf pay now embedded xml (#2611)
    * add carefirst embed xml feature flag
    
    * pass pay now key to saml generator
    
    * add custom xml insertion to saml generator
    
    * spec coverage and custom CF operation outline
    
    * rubocop fixes
    
    * call super in saml generator initialize
    
    * creating structure for embedded xml operation
    
    * refactor and update class name
    
    * rubocop fix
    
    * formatting xml operation
    
    * refactor saml generator operation
    
    * updating error handling for new operation
    
    * fixing specs
    
    * starting on specs
    
    * rubocop fixes
    
    * adding initial specs but more will need to be done once aca entities in place
    
    * removing print statemnt
    
    * adding commented specs for when aca entities piece in place
    
    * fixing rubocop issues
    
    * returning string for xml so that we can test payload
    
    * rubocop issue fixed
    
    * adding individual check to enrollment receipt as well
    
    * reverting commit meant for other PR
    
    * adding enclosing key in payload
    
    * fix rubocop issues
    
    * refactoring transform members
    
    * fixing naming to match
    
    ---------
    
    Signed-off-by: Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
    Co-authored-by: mdkaraman <mdkaramanov@gmail.com>

---
+++ spec/domain/operations/pay_now/care_first/embedded_xml_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require File.join(Rails.root, "spec/shared_contexts/benchmark_products")
+
+RSpec.describe Operations::PayNow::CareFirst::EmbeddedXml do
+  include_context "family with 2 family members with county_zip, rating_area & service_area"
+  include_context "3 dental products with different rating_methods, different child_only_offerings and 3 health products"
+  let(:enr_product) do
+    product = BenefitMarkets::Products::DentalProducts::DentalProduct.by_year(TimeKeeper.date_of_record.year).detect(&:family_based_rating?)
+    product.update_attributes!(dental_level: nil)
+    product
+  end
+
+  let(:submitted_at) { 2.months.ago }
+  let(:enrollment_kind) { "open_enrollment" }
+  let!(:enrollment) do
+    FactoryBot.create(:hbx_enrollment,
+                      :individual_unassisted,
+                      effective_on: TimeKeeper.date_of_record.beginning_of_month,
+                      terminated_on: TimeKeeper.date_of_record.end_of_month,
+                      family: family,
+                      product_id: enr_product.id,
+                      rating_area_id: rating_area.id,
+                      enrollment_kind: enrollment_kind,
+                      coverage_kind: "dental",
+                      submitted_at: submitted_at,
+                      consumer_role_id: family.primary_person.consumer_role.id,
+                      enrollment_members: family.family_members)
+  end
+  context "valid payload is created" do
+    # add piece related to calling aca entities function
+    it "should return successful xml" do
+      expect(described_class.new.call(enrollment)).to be_success
+    end
+  end
+  # for after acaentities piece gets included
+
+  #   context "external xml function fails" do
+  #     before do
+  #         allow(AcaEntitiesOperation).to receive_message_chain('new.call').and_return(Dry::Monads::Result::Failure.new("unable to create xml"))
+
+  #     end
+  #     it "should return failure" do
+  #         expect(described_class.new.call(enrollment)).to be_a(Dry::Monads::Result::Failure)
+  #     end
+  #   end
+end
