86b49f219fea1661255f5781f3d13a3df7274915
Vishal Kalletla <vishuk1201@gmail.com>
2023-03-19 09:23:16 -0400

    add specs

--- spec/models/hbx_enrollment_spec_3.rb
+++ spec/models/hbx_enrollment_spec_3.rb
@@ -49,6 +49,164 @@ describe HbxEnrollment,"reinstate and change end date", type: :model, :dbclean =
     end
   end
 
+  context "reinstate aptc enrollment" do
+    before :all do
+      TimeKeeper.set_date_of_record_unprotected!(Date.new((Date.today.year - 1), 6, 1))
+    end
+    let!(:site_key) { EnrollRegistry[:enroll_app].setting(:site_key).item.upcase }
+    let!(:hbx_profile) {FactoryBot.create(:hbx_profile, :open_enrollment_coverage_period)}
+    let!(:family)        { FactoryBot.create(:family, :with_primary_family_member_and_dependent, person: person) }
+    let!(:person)       { FactoryBot.create(:person, :with_consumer_role) }
+    let!(:address) { family.primary_person.rating_address }
+    let!(:effective_date) { TimeKeeper.date_of_record.beginning_of_year }
+    let!(:application_period) { effective_date.beginning_of_year..effective_date.end_of_year }
+    let!(:rating_area) do
+      ::BenefitMarkets::Locations::RatingArea.rating_area_for(address, during: effective_date) || FactoryBot.create_default(:benefit_markets_locations_rating_area, active_year: effective_date.year)
+    end
+    let!(:service_area) do
+      ::BenefitMarkets::Locations::ServiceArea.service_areas_for(address, during: effective_date).first || FactoryBot.create_default(:benefit_markets_locations_service_area, active_year: effective_date.year)
+    end
+    let!(:renewal_rating_area) do
+      ::BenefitMarkets::Locations::RatingArea.rating_area_for(address, during: renewal_calender_date) || FactoryBot.create_default(:benefit_markets_locations_rating_area, active_year: renewal_calender_date.year)
+    end
+    let!(:renewal_service_area) do
+      ::BenefitMarkets::Locations::ServiceArea.service_areas_for(address, during: renewal_calender_date).first || FactoryBot.create_default(:benefit_markets_locations_service_area, active_year: renewal_calender_date.year)
+    end
+
+    let!(:product) do
+      prod =
+        FactoryBot.create(
+          :benefit_markets_products_health_products_health_product,
+          :with_issuer_profile,
+          :silver,
+          benefit_market_kind: :aca_individual,
+          kind: :health,
+          application_period: application_period,
+          service_area: service_area,
+          csr_variant_id: '01',
+          renewal_product_id: renewal_individual_health_product.id
+        )
+      prod.premium_tables = [premium_table]
+      prod.save
+      prod
+    end
+    let!(:premium_table)        { build(:benefit_markets_products_premium_table, effective_period: application_period, rating_area: rating_area) }
+    let!(:renewal_calender_date) { TimeKeeper.date_of_record.beginning_of_year.next_year }
+    let!(:renewal_application_period) { renewal_calender_date.beginning_of_year..renewal_calender_date.end_of_year }
+    let!(:renewal_individual_health_product) do
+      prod =
+        FactoryBot.create(
+          :benefit_markets_products_health_products_health_product,
+          :with_issuer_profile,
+          :silver,
+          benefit_market_kind: :aca_individual,
+          kind: :health,
+          service_area: renewal_service_area,
+          csr_variant_id: '01',
+          application_period: renewal_application_period
+        )
+      prod.premium_tables = [renewal_individual_premium_table]
+      prod.save
+      prod
+    end
+
+    let!(:tax_household_group) do
+      family.tax_household_groups.create!(
+        assistance_year: TimeKeeper.date_of_record.year,
+        source: 'Admin',
+        start_on: TimeKeeper.date_of_record.beginning_of_year,
+        tax_households: [
+          FactoryBot.build(:tax_household, household: family.active_household)
+        ]
+      )
+    end
+
+    let!(:tax_household) do
+      tax_household_group.tax_households.first
+    end
+
+    let!(:eligibility_determination) do
+      determination = family.create_eligibility_determination(effective_date: TimeKeeper.date_of_record.beginning_of_year)
+      determination.grants.create(
+        key: "AdvancePremiumAdjustmentGrant",
+        value: yearly_expected_contribution,
+        start_on: TimeKeeper.date_of_record.beginning_of_year,
+        end_on: TimeKeeper.date_of_record.end_of_year,
+        assistance_year: TimeKeeper.date_of_record.year,
+        member_ids: family.family_members.map(&:id).map(&:to_s),
+        tax_household_id: tax_household.id
+      )
+
+      determination
+    end
+
+    let!(:aptc_grant) { eligibility_determination.grants.first }
+    let!(:yearly_expected_contribution) { 125.00 * 12 }
+
+    let!(:slcsp_info) do
+      OpenStruct.new(
+        households: [OpenStruct.new(
+          household_id: aptc_grant.tax_household_id,
+          household_benchmark_ehb_premium: benchmark_premium,
+          members: family.family_members.collect do |fm|
+            OpenStruct.new(
+              family_member_id: fm.id.to_s,
+              relationship_with_primary: fm.primary_relationship,
+              date_of_birth: fm.dob,
+              age_on_effective_date: fm.age_on(TimeKeeper.date_of_record)
+            )
+          end
+        )]
+      )
+    end
+
+    let!(:primary_bp) { 500.00 }
+    let!(:dependent_bp) { 600.00 }
+    let!(:benchmark_premium) { primary_bp }
+
+    let!(:renewal_individual_premium_table) { build(:benefit_markets_products_premium_table, effective_period: renewal_application_period, rating_area: renewal_rating_area) }
+    let!(:enrollment) {FactoryBot.create(:hbx_enrollment, :individual_assisted, family: family, product: product, consumer_role_id: person.consumer_role.id, rating_area_id: rating_area.id)}
+
+    before :each do
+      allow(::Operations::BenchmarkProducts::IdentifySlcspWithPediatricDentalCosts).to receive(:new).and_return(
+        double('IdentifySlcspWithPediatricDentalCosts',
+               call: double(:value! => slcsp_info, :success? => true))
+      )
+      effective_on = hbx_profile.benefit_sponsorship.current_benefit_period.start_on
+      tax_household10 = FactoryBot.create(:tax_household, household: family.active_household, effective_ending_on: nil, effective_starting_on: hbx_profile.benefit_sponsorship.current_benefit_period.start_on)
+      eligibility_determination = FactoryBot.create(:eligibility_determination, tax_household: tax_household10, max_aptc: 2000, determined_on: hbx_profile.benefit_sponsorship.current_benefit_period.start_on)
+      tax_household10.tax_household_members.create(applicant_id: family.primary_applicant.id, is_subscriber: true, is_ia_eligible: true)
+      tax_household10.tax_household_members.create(applicant_id: family.family_members[1].id, is_ia_eligible: true)
+      @product = product
+      service_area = ::BenefitMarkets::Locations::ServiceArea.service_areas_for(address, during: effective_on.year).first || FactoryBot.create_default(:benefit_markets_locations_service_area, active_year: effective_on.year)
+      @product.update_attributes(ehb: 0.9844, application_period: Date.new(effective_on.year, 1, 1)..Date.new(effective_on.year, 1, 1).end_of_year, service_area_id: service_area.id)
+      premium_table = @product.premium_tables.first
+      premium_table.update_attributes(effective_period: Date.new(effective_on.year, 1, 1)..Date.new(effective_on.year, 1, 1).end_of_year)
+      @product.save!
+      enrollment.update_attributes(product: @product, effective_on: Date.new(effective_on.year, 1, 1), aasm_state: "coverage_terminated", terminated_on: Date.new(effective_on.year,5,31))
+      hbx_profile.benefit_sponsorship.benefit_coverage_periods.each {|bcp| bcp.update_attributes!(slcsp_id: @product.id)}
+      allow(::BenefitMarkets::Products::ProductRateCache).to receive(:lookup_rate).with(@product, effective_on, person.age_on(Date.today), "R-#{site_key}001", 'N').and_return(679.8)
+      cr1 = FactoryBot.build(:consumer_role, :contact_method => "Paper Only")
+      family.family_members[1].person.consumer_role = cr1
+      cr2 = FactoryBot.build(:consumer_role, :contact_method => "Paper Only")
+      family.family_members[2].person.consumer_role = cr2
+
+      family.save!
+
+      EnrollRegistry[:temporary_configuration_enable_multi_tax_household_feature].feature.stub(:is_enabled).and_return(true)
+      allow(UnassistedPlanCostDecorator).to receive(:new).and_return(double(total_ehb_premium: 1500, total_premium: 1600))
+    end
+
+    it "should create tax household enrollment" do
+      reinstate_enrollment = enrollment.reinstate
+      expect(TaxHouseholdEnrollment.where(enrollment_id: reinstate_enrollment.id).present?).to be_truthy
+    end
+
+    after do
+      TimeKeeper.set_date_of_record_unprotected!(Date.today)
+    end
+  end
+
   context "Terminated enrollment re-instatement" do
     let(:current_date) { Date.new(TimeKeeper.date_of_record.year, 6, 1) }
     let(:effective_on_date)         { Date.new(TimeKeeper.date_of_record.year, 3, 1) }
