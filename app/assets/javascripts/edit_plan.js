a72844b4abef183a42a28110ccabccf00bc5c460
Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>
2023-03-21 14:42:05 -0400

    change aptc tool premium correction (#2658)
    * new premium calculation for aptc edge case
    
    * fix cross site scripting

--- app/assets/javascripts/edit_plan.js
+++ app/assets/javascripts/edit_plan.js
@@ -139,14 +139,16 @@ $(document).on("ready ajax:success", function() {
     // Update the view to reflect the total cash to be applied
     $('#aptc_applied_total').val("$" + aptc_total_cash_amount_to_apply);
 
+    // Show dollar amount of Tax Credit value
+    var new_premium = (total_premium - aptc_total_cash_amount_to_apply);
+    $('#new-premium').text(toFixedTrunc(new_premium.toFixed(8))).html();
+
     if (mthh_enabled && (max_aptc * (percent / 100) > max_aptc_available)) {
       $('#applied_pct_1').val((max_aptc_available / max_aptc).toFixed(2));
       $('#aptc_applied_pct_1_percent').val((max_aptc_available / max_aptc).toFixed(2) * 100 + '%');
       $('#aptc_applied_total').val("$" + max_aptc_available);
+      $('#new-premium').text(toFixedTrunc((total_premium - max_aptc_available).toFixed(8))).html();
     }
-    // Show dollar amount of Tax Credit value
-    var new_premium = (total_premium - aptc_total_cash_amount_to_apply);
-    $('#new-premium').html(toFixedTrunc(new_premium.toFixed(8)));
   }
 
 });
