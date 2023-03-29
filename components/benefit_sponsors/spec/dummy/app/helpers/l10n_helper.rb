cc01893a482dedd1d647d22aaced8ce75666c6ec
RTH_RoR <103944218+Rohithone@users.noreply.github.com>
2023-03-29 15:34:10 -0400

    revert upload worker code (#2694)
    * revert upload worker code
    
    * rubocop offenses

--- components/benefit_sponsors/spec/dummy/app/helpers/l10n_helper.rb
+++ components/benefit_sponsors/spec/dummy/app/helpers/l10n_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module L10nHelper
-  include ActionView::Helpers::TranslationHelper
   def l10n(translation_key, interpolated_keys = {})
     t(translation_key, interpolated_keys.merge(default: translation_key.gsub(/\W+/, '').titleize)).html_safe
   end
