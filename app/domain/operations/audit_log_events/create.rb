5959e52b8adcabe46e9c16501f9c8e70e815324b
Raghuram <raghuramg83@gmail.com>
2023-04-13 08:15:21 -0400

    added create operation to persist audit log event

---
+++ app/domain/operations/audit_log_events/create.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module Operations
+  module AuditLogEvents
+    # Persist Audit Log Event
+    class Create
+      send(:include, Dry::Monads[:result, :do])
+
+      # @param [Hash] opts Options to persist audit log event
+      # @option opts [<GlobalID>] :subject_gid required
+      # @option opts [<String>] :correlation_id required
+      # @option opts [<String>] :event_category required
+      # @option opts [<String>] :trigger required
+      # @option opts [<String>] :response required
+      # @option opts [<DateTime>] :event_time required
+      # @return [Dry::Monad] result
+      def call(params)
+        event_params = yield construct_params(params)
+        values = yield build(event_params)
+        audit_event = yield create(values)
+
+        Success(audit_event)
+      end
+
+      private
+
+      def construct_params(params)
+        Success({
+           subject_gid: params[:subject_gid],
+           correlation_id: params[:correlation_id],
+           event_category: params[:event_category],
+           trigger: params[:trigger],
+           response: params[:response],
+           event_time: params[:event_time]
+        })
+      end
+
+      def build(params)
+        result = Operations::AuditLogs::Build.new.call(params)
+        result.success? ? Success(result.to_h) : Failure(result.failure.errors)
+      end
+
+      def create(values)
+        log_event = AuditLogEvent.new(values)
+
+        if log_event.save
+          Success(log_event)
+        else
+          Failure(log_event)
+        end
+      end
+    end
+  end
+end
---
+++ spec/domain/operations/audit_log_events/create_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ::Operations::AuditLogEvents::Create,
+               type: :model,
+               dbclean: :after_each do
+
+  it 'should be a container-ready operation' do
+    expect(subject.respond_to?(:call)).to be_truthy
+  end
+
+  context 'when required attributes passed' do
+    it 'should persist audit event' do
+    end
+  end
+
+  context 'when required attributes not passed' do
+    it 'should fail to persist audit event' do
+    end
+  end
+end
