650d4156c3d7bf84c508dca231cd1f66d0439ffa
Vishal Kalletla <vishuk1201@gmail.com>
2023-05-01 14:27:06 -0400

    cucumber fix

--- features/financial_assistance/step_definitions/income_adjustments_page_steps.rb
+++ features/financial_assistance/step_definitions/income_adjustments_page_steps.rb
@@ -97,6 +97,7 @@ Then(/^the health_savings_account have glossary content$/) do
 end
 
 Then(/^the alimony_paid does not have glossary link$/) do
+  sleep 1
   expect(page.has_css?(IvlIapIncomeAdjustmentsPage.alimony_paid)).to be_truthy
   expect(page.has_css?(IvlIapIncomeAdjustmentsPage.alimony_paid_glossary_link)).to be_falsy
 end
