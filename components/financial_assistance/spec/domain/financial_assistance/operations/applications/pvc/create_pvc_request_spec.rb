57603143d95377e1b2e5766f8291a8037fbdb5a9
HArsHA Ellanki <h_vardhan@yahoo.com>
2023-05-09 09:34:35 -0400

    fix pvc arguments mismatch

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/pvc/create_pvc_request_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/pvc/create_pvc_request_spec.rb
@@ -110,8 +110,6 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Pvc::CreatePvcRe
     stub_const('::Operations::Products::FetchSlcsp', fetch_slcsp_double)
     stub_const('::Operations::Products::FetchLcsp', fetch_lcsp_double)
     allow(FinancialAssistance::Operations::Applications::Pvc::CreatePvcRequest).to receive(:new).and_return(obj)
-    allow(obj).to receive(:build_event).and_return(event)
-    allow(event.success).to receive(:publish).and_return(true)
     allow(premiums_double).to receive(:failure?).and_return(false)
     allow(slcsp_double).to receive(:failure?).and_return(false)
     allow(lcsp_double).to receive(:failure?).and_return(false)
