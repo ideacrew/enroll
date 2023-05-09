57307f8fbfd57f2b05ab004bcfd26cbe8caab66b
Vishal Kalletla <vishuk1201@gmail.com>
2023-05-09 12:51:48 -0400

    use BuildAndPublish operation to publish events

--- app/workers/address_worker.rb
+++ app/workers/address_worker.rb
@@ -2,10 +2,22 @@
 
 # Class to build address worker to perform later
 class AddressWorker
+  include EventSource::Command
   include Sidekiq::Worker
   sidekiq_options retry: false
 
   def perform(params)
-    Operations::People::Addresses::Compare.new.call(params)
+    result = Operations::People::Addresses::Compare.new.call(params)
+
+    return if result.failure?
+    event_payload = build_event_payload(result.success)
+    ::Operations::Events::BuildAndPublish.new.call(event_payload)
+  end
+
+  def build_event_payload(payload)
+    headers = { correlation_id: payload["person_hbx_id"] }
+    event_key = payload["is_primary"] ? "primary_member_address_relocated" :  "member_address_relocated"
+
+    {event_name: "events.families.family_members.#{event_key}", attributes: { payload: payload.to_h }, headers: headers}
   end
 end
