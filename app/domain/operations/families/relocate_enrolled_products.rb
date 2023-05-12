5ebf298a6bb7ea4d60760e126c6e2aa379ad0a5c
Vishal Kalletla <vishuk1201@gmail.com>
2023-05-12 08:57:27 -0400

    add operation to check for service and rating area change

---
+++ app/domain/operations/families/relocate_enrolled_products.rb
@@ -0,0 +1,166 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module Operations
+  module Families
+    class RelocateEnrolledProducts
+      send(:include, Dry::Monads[:result, :do])
+
+      EVENT_OUTCOME_MAPPING = {:service_area_changed => "product_service_area_relocated",
+                               :rating_area_changed => "premium_rating_area_relocated",
+                               :no_change => "product_unavailable_relocated"}.freeze
+
+      #{"change_set":{"old_set":{"state":"MT"},"new_set":{"state":"MA"}},:person_hbx_id:"750684","primary_family_id":"635d3c61fcbfd93005ee29dd","is_primary":true}}
+      def call(params)
+        valid_params = yield validate(params)
+        person = yield find_person(valid_params)
+        families = yield find_families(person)
+        all_enrollments = yield fetch_all_enrollments_by_families(families.map(&:id))
+        filtered_enrollments = yield filter_enrollments(all_enrollments, valid_params[:primary_family_id])
+        enrollments_hash = yield  check_service_area_change_for_enrollments(filtered_enrollments, valid_params[:address_set])
+        enrollments_hash = yield  check_rating_area_change_for_enrollments(filtered_enrollments, enrollments_hash, valid_params[:address_set][:modified_address])
+        find_event_outcome(enrollments_hash)
+        action_on_enrollment(enrollments_hash)
+        build_and_publish_enrollment_event(enrollments_hash)
+      end
+
+      private
+
+      def validate(params)
+        return Failure("Change_set is missing") unless params[:change_set].present?
+        return Failure("Person_hbx_id is missing") unless params[:person_hbx_id].present?
+        return Failure("primary_family_id is missing") unless params[:primary_family_id].present?
+        return Failure("address_set is missing") unless params[:address_set].present?
+
+        Success(params)
+      end
+
+      def find_person(valid_params)
+        Operations::People::Find.new.call({person_hbx_id: valid_params[:person_hbx_id]})
+      end
+
+      def find_families(person)
+        families = person.families
+        return Failure("No families found for a given person") if families.blank?
+
+        Success(families)
+      end
+
+      def fetch_all_enrollments_by_families(family_ids)
+        all_enrollments = HbxEnrollment.where({:family_id.in => family_ids, :aasm_state => {"$in" => HbxEnrollment::ENROLLED_STATUSES }})
+        Success(all_enrollments)
+      end
+
+      def filter_enrollments(all_enrollments, primary_family_id)
+        all_enrollments = all_enrollments.where(kind: "individual") if true
+        all_enrollments = if false
+                            all_enrollments
+                          elsif primary_family_id.present?
+                            all_enrollments.where(family_id: primary_family_id)
+                          end
+
+        return Failure("No enrollments found for a given criteria") if all_enrollments.blank?
+
+        Success(all_enrollments)
+      end
+
+      def check_service_area_change_for_enrollments(all_enrollments, address_set)
+        hash = all_enrollments.each_with_object({}) do |enrollment, result|
+          new_service_areas = fetch_service_area(address_set[:modified_address], enrollment)
+          old_service_areas = fetch_service_area(address_set[:original_address], enrollment)
+          result[enrollment.hbx_id] = {is_service_area_changed: old_service_areas != new_service_areas, product_offered_in_new_service_area: is_the_enrolled_product_available_in_new_service_area?(enrollment, new_service_areas)}
+        end
+        Success(hash)
+      end
+
+      def is_the_enrolled_product_available_in_new_service_area?(enrollment, new_service_areas)
+        new_service_areas.include?(enrollment.product.service_area_id)
+      end
+
+      def fetch_service_area(rating_address, enrollment)
+        ::BenefitMarkets::Locations::ServiceArea.service_areas_for(
+          Address.new(rating_address),
+          during: enrollment.effective_on
+        ).map(&:id)
+      end
+
+      def check_rating_area_change_for_enrollments(all_enrollments, enrollments_hash, modified_address)
+        all_enrollments.each do |enrollment|
+          enrollments_hash[enrollment.hbx_id].merge!({enrollment_valid_in_new_rating_area:  is_the_enrollment_valid_in_new_rating_area?(enrollment, modified_address)})
+        end
+
+        Success(enrollments_hash)
+      end
+
+      def is_the_enrollment_valid_in_new_rating_area?(enrollment, rating_address)
+        enrollment_rating_area = enrollment.rating_area.exchange_provided_code
+        address_rating_area = ::BenefitMarkets::Locations::RatingArea.rating_area_for(
+          Address.new(rating_address),
+          during: enrollment.effective_on
+        )&.exchange_provided_code
+
+        (enrollment_rating_area == address_rating_area)
+      end
+
+      def find_event_outcome(enrollments_hash)
+        enrollments_hash.each do |k,v|
+          result = if v[:is_service_area_changed] == true
+                     if v[:product_offered_in_new_service_area] == true
+                       if v[:enrollment_valid_in_new_rating_area] == true
+                         'rating_area_changed'
+                       else
+                         'service_area_changed'
+                       end
+                     elsif v[:enrollment_valid_in_new_rating_area] == true
+                       'rating_area_changed'
+                     else
+                       'service_area_changed'
+                     end
+                   elsif v[:enrollment_valid_in_new_rating_area] == true
+                     'rating_area_changed'
+                   else
+                     'no_change'
+                   end
+          enrollments_hash[k].merge!({ event_outcome: result })
+        end
+        enrollments_hash
+      end
+
+      def action_on_enrollment(enrollments_hash)
+        enrollments_hash.each do |k,v|
+          case v[:event_outcome]
+          when "service_area_changed"
+            enrollments_hash[k].merge!({expected_enrollment_action: "Terminate Enrollment Effective End of the Month"})
+          when "rating_area_changed"
+            enrollments_hash[k].merge!({expected_enrollment_action: "Generate Rerated Enrollment with same product ID "})
+          else
+            enrollments_hash[k].merge!({expected_enrollment_action: "No Action Required"})
+          end
+        end
+      end
+
+      def build_and_publish_enrollment_event(enrollments_hash)
+        enrollments_hash.each do |k,v|
+          event_key = EVENT_OUTCOME_MAPPING[v[:event_outcome].to_sym]
+          event_payload = build_event_payload(enrollments_hash[k].merge!(enrollment_hbx_id: k), event_key)
+          publish_event(event_payload)
+        end
+        Success(true)
+      end
+
+      # loop enrollments_hash and publish event if service area is changed
+      def publish_event(payload)
+        ::Operations::Events::BuildAndPublish.new.call(payload)
+      end
+
+      def build_event_payload(payload, event_key)
+        headers = { correlation_id: payload[:enrollment_hbx_id] }
+
+        {event_name: "events.families.family_members.primary_family_member.#{event_key}", attributes:  payload.to_h, headers: headers}
+      end
+    end
+  end
+end
+
---
+++ app/event_source/events/families/family_members/primary_family_member/premium_rating_area_relocated.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Events
+  module Families
+    module FamilyMembers
+      module PrimaryFamilyMember
+        # This class will register event 'member_address_updated'
+        class PremiumRatingAreaRelocated < EventSource::Event
+          publisher_path 'publishers.families.family_members.primary_family_member.service_and_rating_area_relocated_publisher'
+
+        end
+      end
+    end
+  end
+end
+
