f593821917e136e59295f983dc2e8051faa48cbf
HArsHA Ellanki <h_vardhan@yahoo.com>
2023-05-19 10:40:45 -0400

    cop fix

--- components/benefit_markets/app/models/benefit_markets/products/health_products/health_product.rb
+++ components/benefit_markets/app/models/benefit_markets/products/health_products/health_product.rb
@@ -32,7 +32,7 @@ module BenefitMarkets
       field :ehb,                         type: Float,    default: 0.0
       field :is_standard_plan,            type: Boolean,  default: false
       field :is_hc4cc_plan,               type: Boolean,  default: false
- 
+
       field :rx_formulary_url,            type: String
       field :hsa_eligibility,             type: Boolean,  default: false
       field :network_information,         type: String
