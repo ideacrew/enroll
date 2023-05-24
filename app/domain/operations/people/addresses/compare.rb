7717da8f13815969be330bd7992388ba0e781ae6
vishal kalletla <vishuk1201@gmail.com>
2023-05-24 12:06:57 -0500

    setup operations and events for address, rating and service area changes (#2812)
    * setup operation to compare address changes and publish event
    
    * add events and publisher
    
    * rubocop fix
    
    * add worker for address update
    
    * fix typo
    
    * more code
    
    * add spec
    
    * add feature switch
    
    * add more spec
    
    * rename events, add worker specs and update specs
    
    * use BuildAndPublish operation to publish events
    
    * more code
    
    * add loggers
    
    * update payload builder
    
    * add more code
    
    * fix typo
    
    * update payload
    
    * fix typo
    
    * add comments
    
    * update aca_entities reference
    
    * rubocop fix
    
    * update aca_entities ref
    
    * add subscriber to consume address relocated events (#2787)
    
    * add subscriber to consume address relocated events
    
    * add operation to check for service and rating area change
    
    * add spec
    
    * add interactor to simplfy operation class
    
    * remove subscriber
    
    * add interactors specs
    
    * update interactor name
    
    * rename method
    
    * fix specs
    
    * fix find_event_outcome_spec
    
    * refactor subscriber, rename class names and update specs
    
    * add subscriber to consume service and rating area change events (#2795)
    
    * add subscriber to consume address relocated events
    
    * add operation to check for service and rating area change
    
    * add spec
    
    * add interactor to simplfy operation class
    
    * remove subscriber
    
    * add subscriber to consume service and rating area change events
    
    * add interactors specs
    
    * update interactor name
    
    * setup operation to relocate enrollment
    
    * fix typo
    
    * pass hbx id instead of object
    
    * more code
    
    * add spec and code refactor
    
    * call RelocateEnrollment operation from subscriber
    
    * fix spec
    
    * rename method
    
    * update spec
    
    * fix specs
    
    * fix find_event_outcome_spec
    
    * refactor subscriber, introduced interactors for effective date calculation/aptc updated and spec updates
    
    * refactor subscriber, rename class names and update specs
    
    * refactor code for expected result and for future development
    
    * cancel enrollment if the effective date is in the future
    
    * rubocop fix
    
    * fix failing spec

---
+++ app/domain/operations/people/addresses/compare.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+module Operations
+  module People
+    module Addresses
+      # Class to compare address changes and build payload
+      class Compare
+        include Dry::Monads[:result, :do]
+
+        # @param [Hash] params
+        # @return [Dry::Monads::Result]
+        #  Success => {address_id: address_id,
+        #   address_set: {original_address: original_set, modified_address: modified_set},
+        #   change_set: {old_set: old_set, new_set: new_set}, person_hbx_id: person_hbx_id,
+        #   primary_family_id: primary_family_id, is_primary: is_primary}
+        def call(params)
+          address_id, person_hbx_id = yield validate(params)
+          person = yield find_person(person_hbx_id)
+          address = yield find_address(address_id, person)
+          change_set = yield build_change_set(address)
+          address_versions = yield summarized_changes(address, change_set)
+          payload = yield build_payload(address_id, address_versions, change_set, person)
+
+          Success(payload)
+        end
+
+        private
+
+        def validate(params)
+          return Failure("Missing address id ") unless params["address_id"].present?
+          return Failure("Missing person hbx id ") unless params["person_hbx_id"].present?
+          Success([params["address_id"], params["person_hbx_id"]])
+        end
+
+        def find_person(person_hbx_id)
+          Operations::People::Find.new.call({person_hbx_id: person_hbx_id})
+        end
+
+        def find_address(address_id, person)
+          address = person.addresses.where(id: address_id).first
+          return Success(address) if address
+          Failure("No address found for the given id")
+        end
+
+        def build_change_set(address)
+          change_set = {}
+          history_track = address.history_tracks.last
+          Failure("change set payload is only applicable for action :update") unless history_track.action.to_sym == :update
+          Failure("No address changes present") unless history_track.original.present? && history_track.modified.present?
+
+          change_set.merge!(old_set: history_track.original, new_set: history_track.modified)
+          Success(change_set)
+        end
+
+        def summarized_changes(address, change_set)
+          original_set = {}
+          modified_set = {}
+          address.attributes.slice("address_1", "address_2", "address_3", "county", "kind", "city", "state", "zip").each_key do |field|
+            if change_set[:new_set][field].present? && change_set[:old_set][field].present?
+              original_set[field] = change_set[:old_set][field]
+              modified_set[field] = change_set[:new_set][field]
+            else
+              original_set[field] = address.send(field)
+              modified_set[field] = address.send(field)
+            end
+          end
+          Success({original_address: original_set, modified_address: modified_set})
+        end
+
+        def build_payload(address_id, address_versions, change_set, person)
+          payload = {}
+
+          primary_family = person.primary_family
+          payload.merge!(address_id: address_id, address_set: address_versions, change_set: change_set, person_hbx_id: person.hbx_id, primary_family_id: primary_family&.id, is_primary: primary_family.present?)
+          Success(payload)
+        end
+      end
+    end
+  end
+end
+
