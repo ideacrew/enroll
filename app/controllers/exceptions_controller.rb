22eecf9f98c3654dc76e920bed7ce1e9c3f20840
Marco Ornelas <marco.ornelas@ideacrew.com>
2023-05-24 21:32:04 -0400

    removed an embarrasing double render, removed unecesary routes for the exceptions controller

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -2,8 +2,6 @@
 
 # Handles custom exceptions if enabled, otherwise just loads files from the public folder
 class ExceptionsController < ApplicationController
-  before_action :recover_exception_code
-
   ## Devise filters
   skip_before_action :require_login, unless: :authentication_not_required?
   skip_before_action :authenticate_user_from_token!
@@ -14,15 +12,20 @@ class ExceptionsController < ApplicationController
   # for current_user
   skip_before_action :set_current_user
   def show
-    render 'show', status: @exception_code if EnrollRegistry.feature_enabled?(:custom_exceptions_controller)
-
-    render file: "public/#{@exception_code}.html", status: @exception_code, layout: false
+    status_code = recover_exception_code || 500
+    if EnrollRegistry.feature_enabled?(:custom_exceptions_controller)
+      render 'show', status: status_code
+    else
+      render file: "public/#{status_code}.html", status: status_code, layout: false
+    end
   end
 
   private
 
+  # recover the exception code from the request
+  # @return [Integer] the exception code
   def recover_exception_code
     exception = request.env["action_dispatch.exception"]
-    @exception_code = ActionDispatch::ExceptionWrapper.new(request.env, exception).status_code
+    ActionDispatch::ExceptionWrapper.new(request.env, exception).status_code if exception
   end
 end
