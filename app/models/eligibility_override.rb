0e07383d3088920c07f239dcf76c55b906dd8102
mdkaraman <mdkaramanov@gmail.com>
2023-06-08 17:57:21 -0400

    add eligibility overrides

---
+++ app/models/eligibility_override.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Represents an eligibility override for a member
+class EligibilityDetermination
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    embedded_in :member_determination
+
+    # The override rule that was applied.
+    field :override_rule, type: Symbol
+
+    # Whether or not the override was applied.
+    field :override_applied, type: Boolean
+
+end
--- app/models/member_determination.rb
+++ app/models/member_determination.rb
@@ -10,7 +10,7 @@ class MemberDetermination
   field :kind, type: String
 
   # Whether or not the member is eligible for the kind of determination.
-  field :is_eligible, type: Boolean
+  field :criteria_met, type: Boolean
 
   # The reasons the member qualifies for the determination.
   field :determination_reasons, type: Array
