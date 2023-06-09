00cbe6207cfe90b82ad111e79a9ef413f4a5af23
mdkaraman <mdkaramanov@gmail.com>
2023-06-09 16:12:17 -0400

    spec fixes

--- app/models/eligibility_override.rb
+++ app/models/eligibility_override.rb
@@ -2,15 +2,15 @@
 
 # Represents an eligibility override for a member
 class EligibilityDetermination
-    include Mongoid::Document
-    include Mongoid::Timestamps
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-    embedded_in :member_determination
+  embedded_in :member_determination
 
     # The override rule that was applied.
-    field :override_rule, type: Symbol
+  field :override_rule, type: Symbol
 
     # Whether or not the override was applied.
-    field :override_applied, type: Boolean
+  field :override_applied, type: Boolean
 
 end
--- components/financial_assistance/app/models/financial_assistance/eligibility_override.rb
+++ components/financial_assistance/app/models/financial_assistance/eligibility_override.rb
@@ -2,17 +2,17 @@
 
 module FinancialAssistance
     # Represents an eligibility override for a member
-    class EligibilityOverride
-      include Mongoid::Document
-      include Mongoid::Timestamps
+  class EligibilityOverride
+    include Mongoid::Document
+    include Mongoid::Timestamps
 
-        embedded_in :member_determination, class_name: '::FinancialAssistance::MemberDetermination'
+    embedded_in :member_determination, class_name: '::FinancialAssistance::MemberDetermination'
 
-        # The override rule that was applied.
-        field :override_rule, type: Symbol
+      # The override rule that was applied.
+    field :override_rule, type: Symbol
 
-        # Whether or not the override was applied.
-        field :override_applied, type: Boolean
+      # Whether or not the override was applied.
+    field :override_applied, type: Boolean
 
-    end
+  end
 end
