8e17a42b86c03f1b5ba2c35ea22cc3d176769022
Marco Ornelas <marco.ornelas@ideacrew.com>
2023-06-12 16:55:19 -0400

    changing the testing methodology

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -733,6 +733,53 @@ describe "Enabled/Disabled IVL market" do
     end
   end
 
+  describe 'registration_recaptcha_enabled?' do
+    it 'should return true if recaptcha is enabled if view is benefit_sponsor and ff is enabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_sponsor_recaptcha).and_return(true)
+      expect(helper.registration_recaptcha_enabled?('benefit_sponsor')).to eq true
+    end
+
+    it 'should return false if recaptcha is enabled if view is benefit_sponsor and ff is disabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_sponsor_recaptcha).and_return(false)
+      expect(helper.registration_recaptcha_enabled?('benefit_sponsor')).to eq false
+    end
+
+    it 'should return true if recaptcha is enabled if view is user_account and ff is enabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_user_account_recaptcha).and_return(true)
+      expect(helper.registration_recaptcha_enabled?('user_account')).to eq true
+    end
+
+    it 'should return false if recaptcha is enabled if view is user_account and ff is disabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_user_account_recaptcha).and_return(false)
+      expect(helper.registration_recaptcha_enabled?('user_account')).to eq false
+    end
+
+    it 'should return true if recaptcha is enabled if view is general_agency and ff is enabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_ga_recaptcha).and_return(true)
+      expect(helper.registration_recaptcha_enabled?('general_agency')).to eq true
+    end
+
+    it 'should return false if recaptcha is enabled if view is general_agency and ff is disabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_ga_recaptcha).and_return(false)
+      expect(helper.registration_recaptcha_enabled?('general_agency')).to eq false
+    end
+
+    it 'should return true if recaptcha is enabled if view is broker_agency and ff is enabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_broker_recaptcha).and_return(true)
+      expect(helper.registration_recaptcha_enabled?('broker_agency')).to eq true
+    end
+
+    it 'should return false if recaptcha is enabled if view is broker_agency and ff is disabled' do
+      allow(EnrollRegistry).to receive(:feature_enabled?).with(:registration_broker_recaptcha).and_return(false)
+      expect(helper.registration_recaptcha_enabled?('broker_agency')).to eq false
+    end
+
+    it 'should return false by default' do
+      expect(helper.registration_recaptcha_enabled?('abc')).to eq false
+    end
+
+  end
+
   describe "#previous_year" do
     it "should return past year" do
       expect(helper.previous_year).to eq (TimeKeeper.date_of_record.year - 1)
@@ -835,7 +882,7 @@ describe "Enabled/Disabled IVL market" do
     it_behaves_like 'float_fix', (2.76 + 2.43), 5.19
     it_behaves_like 'float_fix', (0.57 * 100), 57
   end
-
+    
   describe 'round_down_float_two_decimals' do
 
     shared_examples_for 'rounding float number' do |input, output|
