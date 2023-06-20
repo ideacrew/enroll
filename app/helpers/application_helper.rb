19e4041098984f85ec63fb3f06bd658035a5d965
Nisanth Yaganti <yagantinisanth@gmail.com>
2023-06-20 11:23:31 -0400

    adding new partial for announcements to isolate (#2852)
    * adding new partial for announcements to isolate
    
    * spec fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -342,16 +342,29 @@ module ApplicationHelper
   def render_flash
     rendered = []
     flash.each do |type, messages|
-      next if messages.respond_to?(:include?) && messages&.include?("nil is not a symbol nor a string")
+      next if messages.blank? || (messages.respond_to?(:include?) && messages.include?("nil is not a symbol nor a string"))
+
       if messages.respond_to?(:each)
         messages.each do |m|
-          rendered << render(:partial => 'layouts/flash', :locals => {:type => type, :message => m}) unless m.blank?
+          rendered << get_flash(type, m) if m.present?
         end
       else
-        rendered << render(:partial => 'layouts/flash', :locals => {:type => type, :message => messages}) unless messages.blank?
+        rendered << get_flash(type, messages)
       end
     end
     rendered.join.html_safe
+  end
+
+  def get_flash(type, msg)
+    if is_announcement?(msg)
+      render(:partial => 'layouts/announcement_flash', :locals => {:type => type, :message => msg[:announcement]})
+    else
+      render(:partial => 'layouts/flash', :locals => {:type => type, :message => msg})
+    end
+  end
+
+  def is_announcement?(item)
+    item.respond_to?(:keys) && item[:is_announcement]
   end
 
   def dd_value(val)
