e94ee8ef6b53d8a42f83a6f3b562bee33f9e0b40
rahulgudim <118293805+rahulgudim@users.noreply.github.com>
2023-07-05 15:51:40 -0500

    Fix the ability to read non related inbox (#2892)
    * Fix bulk notices upload, upgrade Stimulus
    
    * Fix the ability to read non related inbox
    
    * fix few more authorizations
    
    * refactor specs
    
    * rubocop fix

--- components/benefit_sponsors/app/policies/benefit_sponsors/person_policy.rb
+++ components/benefit_sponsors/app/policies/benefit_sponsors/person_policy.rb
@@ -5,7 +5,8 @@ module BenefitSponsors
   class PersonPolicy < ApplicationPolicy
     def can_read_inbox?
       return true if user.person.hbx_staff_role
-      true if user.person.broker_role || record.broker_role
+      return true if (user.person&.broker_role || record.broker_role) && (user.person.id == record.id)
+      false
     end
   end
 end
