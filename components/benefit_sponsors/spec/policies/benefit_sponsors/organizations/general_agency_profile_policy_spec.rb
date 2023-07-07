abbe59071aa32c9a6437f708ba03fc5a6f07d90d
rahul <rahul.gudimetla@ideacrew.com>
2023-07-07 15:49:46 -0500

    Fix cases where General agency cannot view others profile

--- components/benefit_sponsors/spec/policies/benefit_sponsors/organizations/general_agency_profile_policy_spec.rb
+++ components/benefit_sponsors/spec/policies/benefit_sponsors/organizations/general_agency_profile_policy_spec.rb
@@ -6,6 +6,8 @@ module BenefitSponsors
   RSpec.describe Organizations::GeneralAgencyProfilePolicy, dbclean: :after_each  do
     let!(:user) { FactoryBot.create(:user) }
     let!(:person) {FactoryBot.create(:person, user: user)}
+    let(:person_with_ga_staff_role) { FactoryBot.create(:person, :with_general_agency_staff_role) }
+    let(:user_with_ga_staff_role) { FactoryBot.create(:user, person: person_with_ga_staff_role, roles: ["general_agency_staff"]) }
 
     context 'access to general agency profile' do
       it 'returns true if admin user and has hbx staff role' do
@@ -13,19 +15,35 @@ module BenefitSponsors
         FactoryBot.create(:person, user: user_with_hbx_staff_role)
         policy = BenefitSponsors::Organizations::GeneralAgencyProfilePolicy.new(user_with_hbx_staff_role, nil)
         expect(policy.can_read_inbox?).to be true
+        expect(policy.show?).to be true
       end
 
       it 'returns false if user has no valid role' do
         policy = BenefitSponsors::Organizations::GeneralAgencyProfilePolicy.new(user, nil)
         expect(policy.can_read_inbox?).to be false
+        expect(policy.show?).to be false
+      end
+    end
+
+    context 'general agency with staff roles' do
+      before do
+        allow(user_with_ga_staff_role).to receive(:has_general_agency_staff_role?).and_return true
       end
 
-      it 'returns true if general agency has staff role' do
-        person_with_ga_staff_role = FactoryBot.create(:person, :with_general_agency_staff_role)
-        user_with_ga_staff_role = FactoryBot.create(:user, person: person_with_ga_staff_role, roles: ["general_agency_staff"])
-        policy = BenefitSponsors::Organizations::GeneralAgencyProfilePolicy.new(user_with_ga_staff_role, nil)
+      it 'returns true if user has general agency staff role' do
+        general_agency_profile = GeneralAgencyProfile.last
+        policy = BenefitSponsors::Organizations::GeneralAgencyProfilePolicy.new(user_with_ga_staff_role, general_agency_profile)
 
         expect(policy.can_read_inbox?).to be true
+        expect(policy.show?).to be true
+      end
+
+      it 'returns false if user has no general agency staff role' do
+        some_random_profile = FactoryBot.create(:person, :with_general_agency_staff_role)
+        policy = BenefitSponsors::Organizations::GeneralAgencyProfilePolicy.new(user_with_ga_staff_role, some_random_profile)
+
+        expect(policy.can_read_inbox?).to be false
+        expect(policy.show?).to be false
       end
     end
   end
