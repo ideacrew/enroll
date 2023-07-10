3fe72d32dd73e7a265b24023528fdb33718ae172
utkarsh7989 <utkarsh7989@gmail.com>
2023-07-10 10:25:28 -0400

    selecting bga that covers the given date

--- app/models/benefit_group_assignment.rb
+++ app/models/benefit_group_assignment.rb
@@ -102,8 +102,7 @@ class BenefitGroupAssignment
       elsif assignments_with_no_end_on.present?
         filter_assignments_with_no_end_on(assignments_with_no_end_on, date)
       else
-        bg_assignment = perspective_assignments_with_end_on.detect{ |assignment| assignment.start_on && (assignment.start_on..assignment.end_on).cover?(date) }
-        bg_assignment || perspective_assignments_with_end_on.last
+        perspective_assignments_with_end_on.detect{ |assignment| assignment.start_on && (assignment.start_on..assignment.end_on).cover?(date) }
       end
     end
 
