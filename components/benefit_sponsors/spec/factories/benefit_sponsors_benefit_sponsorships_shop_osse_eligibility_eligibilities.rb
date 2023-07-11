c35345de67828dc911a380d6712151cdc6079a4f
raghuram <raghuramg83@gmail.com>
2023-07-11 12:01:41 -0400

    add factories and specs for new eligibility models

---
+++ components/benefit_sponsors/spec/factories/benefit_sponsors_benefit_sponsorships_shop_osse_eligibility_eligibilities.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :benefit_sponsors_benefit_sponsorship_osse_eligibility,
+          class:
+            'BenefitSponsors::BenefitSponsorships::ShopOsseEligibility::Eligibility' do
+    title { 'Contribution Subsidy' }
+    description { 'Osse Contribution Subsidy' }
+
+    transient do
+      from_state { :draft }
+      to_state { :eligible }
+      is_eligible { true }
+      effective_on { TimeKeeper.date_of_record.beginning_of_month }
+    end
+
+    trait :with_state_history do
+      after :build do |eligibility, evaluator|
+        eligibility.state_histories << FactoryBot.build(
+          :eligible_state_history,
+          from_state: evaluator.from_state,
+          to_state: evaluator.to_state,
+          is_eligible: evaluator.is_eligible,
+          effective_on: evaluator.effective_on
+        )
+      end
+    end
+  end
+end
