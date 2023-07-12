85dd4705f983be84b58db96986048a5c4d34b0cc
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2023-07-12 16:43:28 -0400

    aon 4.2 - minor code change

--- components/benefit_sponsors/app/models/effective/datatables/benefit_sponsors_employer_datatable.rb
+++ components/benefit_sponsors/app/models/effective/datatables/benefit_sponsors_employer_datatable.rb
@@ -284,7 +284,7 @@ module Effective
       end
 
       def authorized?(current_user, _controller, _action, _resource)
-        (current_user.present? && current_user.has_hbx_staff_role?) ? true : false
+        current_user.present? && current_user.hbx_staff_role?
       end
     end
   end
