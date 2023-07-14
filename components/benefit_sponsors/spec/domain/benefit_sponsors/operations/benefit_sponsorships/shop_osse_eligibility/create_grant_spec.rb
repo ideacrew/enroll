c80e673db18d9f4457641cb5cabdacce4440ddc6
raghuram <raghuramg83@gmail.com>
2023-07-14 10:30:41 -0400

    Add domain operations for osse eligibility

---
+++ components/benefit_sponsors/spec/domain/benefit_sponsors/operations/benefit_sponsorships/shop_osse_eligibility/create_grant_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BenefitSponsors::Operations::BenefitSponsorships::ShopOsseEligibility::CreateGrant, type: :model, dbclean: :after_each do
+
+  let(:key) { :contribution_grant }
+  let(:title) { 'childcare subsidy contribution' }
+  let(:value) { {title: title, key: key} }
+  let(:description) { 'childcare subsidy contribution grant' }
+  let(:history_params) do
+    {
+      effective_on: Date.today,
+      is_eligible: true,
+      from_state: 'draft',
+      to_state: 'eligible',
+      transition_at: DateTime.now
+    }
+  end
+
+  let(:state_histories) do
+    [AcaEntities::Eligible::StateHistory.new(history_params)]
+  end
+
+  let(:required_params) do
+    { key: key, title: title, value: value, state_histories: state_histories }
+  end
+
+  let(:optional_params) { { description: description } }
+
+  let(:all_params) { required_params.merge(optional_params) }
+
+  it 'should be a container-ready operation' do
+    expect(subject.respond_to?(:call)).to be_truthy
+  end
+
+  context 'when required attributes passed' do
+
+    it 'should be success' do
+      result = subject.call(all_params)
+      expect(result.success?).to be_truthy
+    end
+
+    it 'should create evidence' do
+      result = subject.call(all_params)
+      expect(result.success).to be_a(AcaEntities::BenefitSponsors::BenefitSponsorships::ShopOsseEligibility::Grant)
+    end
+  end
+
+  context 'when required attributes not passed' do
+    it 'should fail with validation error' do
+      result = subject.call(required_params.except(:title))
+      expect(result.failure?).to be_truthy
+      expect(result.failure.error?(:title)).to be_truthy
+    end
+  end
+end
