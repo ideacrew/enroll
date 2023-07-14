ff7931a17bcea409a9a1dfd076d883ecbb0cee4d
raghuram <raghuramg83@gmail.com>
2023-07-14 10:30:41 -0400

    Add domain operations for osse eligibility

---
+++ components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_sponsorships/shop_osse_eligibility/create_admin_attested_evidence.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module BenefitSponsors
+  module Operations
+    module BenefitSponsorships
+      module ShopOsseEligibility
+        class CreateAdminAttestedEvidence
+          send(:include, Dry::Monads[:result, :do])
+
+          # @param [ Hash ] params Product Attributes
+          # @return [ BenefitMarkets::Entities::Product ] product Product
+          def call(params)
+            values = yield validate(params)
+            product = yield create(values)
+
+            Success(product)
+          end
+
+          private
+
+          def validate(params)
+            result =
+              AcaEntities::BenefitSponsors::BenefitSponsorships::ShopOsseEligibility::AdminAttestedEvidenceContract.new.call(
+                params
+              )
+
+            if result.success?
+              Success(result.to_h)
+            else
+              result
+            end
+          end
+
+          def create(values)
+            evidence_entity = AcaEntities::BenefitSponsors::BenefitSponsorships::ShopOsseEligibility::AdminAttestedEvidence.new(values)
+
+            Success(evidence_entity)
+          end
+        end
+      end
+    end
+  end
+end
