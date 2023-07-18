e29ca9fa2ffe53732ba0d65e87d1ec842b7ea009
raghuram <raghuramg83@gmail.com>
2023-07-18 08:48:12 -0400

    rubocop fixes

--- components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_sponsorships/shop_osse_eligibility/create_eligibility.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/operations/benefit_sponsorships/shop_osse_eligibility/create_eligibility.rb
@@ -7,6 +7,7 @@ module BenefitSponsors
   module Operations
     module BenefitSponsorships
       module ShopOsseEligibility
+        # Operation to support eligibility creation
         class CreateEligibility
           send(:include, Dry::Monads[:result, :do])
 
