d96117608651a4de26ee7ede3641d52359ad84b7
RTH_RoR <rohith.rubyrails@gmail.com>
2023-07-19 10:42:35 -0400

    sql injection fix

--- app/data_migrations/employee_renewals_export.rb
+++ app/data_migrations/employee_renewals_export.rb
@@ -12,11 +12,13 @@ class EmployeeRenewalsExport < MongoidMigrationTask
   end
 
   def benefit_applications_in_aasm_state(aasm_states, start_on_date)
-    BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where("benefit_applications.effective_period.min = :start_on_date
-  AND benefit_applications.predecessor_id IS NOT NULL
-  AND benefit_applications.aasm_state IN (:aasm_states)",
-                                                                   start_on_date: start_on_date,
-                                                                   aasm_states: aasm_states)
+    BenefitSponsors::BenefitSponsorships::BenefitSponsorship.where(
+      "benefit_applications.effective_period.min = ? AND
+  benefit_applications.predecessor_id IS NOT NULL AND
+  benefit_applications.aasm_state IN (?)",
+      start_on_date,
+      aasm_states
+    )
   end
 
   def detailed_report_field_names(start_on_date)
