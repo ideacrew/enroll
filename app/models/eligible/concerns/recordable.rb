4f6381b11e1ae32f951892030fabd2b8adfc70c9
raghuram <raghuramg83@gmail.com>
2023-07-19 13:25:52 -0400

    updates for eligible module concerns

---
+++ app/models/eligible/concerns/recordable.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Eligible
+  module Concerns
+    # Mixin to record state transitions
+    module Recordable
+
+      def record_transition(_params)
+
+        entity = Operations::Eligible::CreateStateHistory.new.call({
+                                                                     effective_on: Date.today,
+                                                                     is_eligible: ELIGIBLE_STATES.include?(aasm.to_state),
+                                                                     from_state: aasm.from_state,
+                                                                     to_state: aasm.to_state,
+                                                                     event: aasm.current_event,
+                                                                     transition_at: DateTime.now
+                                                                   })
+
+        if entity.success?
+          self.state_histories.build(entity.success.to_h)
+        # else
+        #   # failed...revert state transition
+        end
+      end
+    end
+  end
+end
