b54d2b59ad64a00e37106dff2d99cd1b0db7b7f5
raghuram <raghuramg83@gmail.com>
2023-07-31 12:48:44 -0400

    remove references to concerns

---
+++ app/models/ivl_osse_eligibilities/admin_attested_evidence.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module IvlOsseEligibility
+  # Admin attested Evidence model for ivl osse eligibility
+  class AdminAttestedEvidence < ::Eligible::Evidence
+  end
+end
---
+++ app/models/ivl_osse_eligibilities/ivl_osse_grant.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module IvlOsseEligibility
+  # Eligibility model for ivl osse
+  class IvlOsseGrant < ::Eligible::Grant
+  end
+end
