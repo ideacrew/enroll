45f19a14307f8f6c511cd1de8d23c20e0513a2d0
raghuram <raghuramg83@gmail.com>
2023-08-01 15:51:04 -0400

    update benefit sponsors dummy models

--- components/benefit_sponsors/spec/dummy/app/models/eligible/evidence.rb
+++ components/benefit_sponsors/spec/dummy/app/models/eligible/evidence.rb
@@ -7,6 +7,7 @@ module Eligible
     include Mongoid::Timestamps
 
     STATUSES = %i[initial approved denied].freeze
+    ELIGIBLE_STATUSES = %i[approved].freeze
 
     field :key, type: Symbol
     field :title, type: String
@@ -17,7 +18,7 @@ module Eligible
     field :evidence_ref, type: String
 
     embeds_many :state_histories,
-                class_name: '::Eligible::StateHistory',
+                class_name: "::Eligible::StateHistory",
                 cascade_callbacks: true,
                 as: :status_trackable
 
@@ -32,6 +33,32 @@ module Eligible
 
     def latest_state_history
       state_histories.latest_history
+    end
+
+    def is_eligible_on?(date)
+      eligible_periods.any? do |period|
+        if period[:end_on].present?
+          (period[:start_on]..period[:end_on]).cover?(date)
+        else
+          period[:start_on] <= date
+        end
+      end
+    end
+
+    def eligible_periods
+      eligible_periods = []
+      date_range = {}
+      state_histories.non_initial.each do |state_history|
+        date_range[:start_on] ||= state_history.effective_on if state_history.to_state == :approved
+
+        next unless date_range.present? && state_history.to_state == :denied
+        date_range[:end_on] = state_history.effective_on.prev_day
+        eligible_periods << date_range
+        date_range = {}
+      end
+
+      eligible_periods << date_range unless date_range.empty?
+      eligible_periods
     end
   end
 end
