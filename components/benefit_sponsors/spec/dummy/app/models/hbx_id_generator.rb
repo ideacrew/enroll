9793b9858a88836205dccfe54c8f1f4a956ca15d
Yosef Serkez <36456900+4till2@users.noreply.github.com>
2023-08-02 05:48:41 -1000

    more robust generator

--- components/benefit_sponsors/spec/dummy/app/models/hbx_id_generator.rb
+++ components/benefit_sponsors/spec/dummy/app/models/hbx_id_generator.rb
@@ -15,9 +15,15 @@ class HbxIdGenerator
     random_uuid
   end
 
+  # Random UUID generator combining a timestamp and random number to generate a unique semi-random 15 digit number.
+  # The length of the ID matters because a float with more than 15 digits is represented using scientific notation.
+  # Keep the id length less than 16 digits to avoid this issue.
   def self.random_uuid
-    timestamp = Time.now.strftime("%m%d%H%M%S%L")
-    random_number = SecureRandom.random_number(50)
-    "#{timestamp}#{random_number}".to_i
+    # 11 digit timestamp for uniqueness
+    timestamp = Time.now.strftime("%m%d%H%M%S")
+    # Max 4 digit random number
+    random_number = SecureRandom.random_number(9999)
+    # 15 digit unique number padded with 0
+    "#{timestamp}#{random_number.to_s.rjust(4, '0')}".to_i
   end
 end
