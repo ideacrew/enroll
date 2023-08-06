7149b1185f2b0ef2ac723652fc4a30cd1e5fc2af
HArsHA Ellanki <h_vardhan@yahoo.com>
2023-08-06 13:44:19 -0400

    added code to sponsored benefits engine dummy

---
+++ components/sponsored_benefits/spec/dummy/app/models/eligible/value.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Eligible
+  # Value model
+  class Value
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :title, type: String
+    field :description, type: String
+    field :key, type: Symbol
+    field :item, type: String
+
+    validates_presence_of :title, :key
+  end
+end
