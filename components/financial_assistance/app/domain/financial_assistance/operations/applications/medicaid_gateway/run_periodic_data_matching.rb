9d52fa25704f89ca4f7409a043c1cb9809952154
rahul <rahul.gudimetla@ideacrew.com>
2023-08-07 18:11:29 -0500

    refactor filter criteria

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/medicaid_gateway/run_periodic_data_matching.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/medicaid_gateway/run_periodic_data_matching.rb
@@ -35,39 +35,39 @@ module FinancialAssistance
           end
 
           def fetch_application(family, assistance_year)
-            ::FinancialAssistance::Application.where(assistance_year: assistance_year,
-                                                     aasm_state: 'determined',
-                                                     family_id: family.id).max_by(&:created_at)
+            applications = ::FinancialAssistance::Application.where(
+              family_id: family.id,
+              assistance_year: assistance_year,
+              aasm_state: 'determined',
+              'applicants.is_ia_eligible' => true
+            )
+            applications.max_by(&:created_at)
           end
 
           def filter_and_call_mec_service(params)
+            @logger = Logger.new("#{Rails.root}/log/run_periodic_data_matching_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log")
             batch_size = params[:batch_size] || 1000
-            determined_family_ids = FinancialAssistance::Application.where(:aasm_state => "determined",
-                                                                           :assistance_year => params[:assistance_year],
-                                                                           :"applicants.is_ia_eligible" => true).distinct(:family_id)
+            families = Family.where(:_id.in => HbxEnrollment.by_health.enrolled_and_renewal.distinct(:family_id))
+            total_families_count = families.count
 
-            Rails.logger.info "MedicaidGateway::RunPeriodicDataMatching Total families determined - #{determined_family_ids.count}"
-            not_outstanding_families = Family.where(:_id.in => determined_family_ids, :'eligibility_determination.outstanding_verification_status'.nin => %w[outstanding not_enrolled])
-            Rails.logger.info "MedicaidGateway::RunPeriodicDataMatching Total number of not outstanding families - #{not_outstanding_families.count}"
+            @logger.info "MedicaidGateway::RunPeriodicDataMatching Total families to run - #{total_families_count}"
             total_applications_ran = 0
-            (0..not_outstanding_families.count).step(batch_size) do |offset|
-              batch = not_outstanding_families.skip(offset).limit(batch_size)
+            (0..total_families_count).step(batch_size) do |offset|
+              batch = families.skip(offset).limit(batch_size)
               batch.each do |family|
+                enrollments = family.hbx_enrollments.by_health.enrolled_and_renewal
                 # Process each record in the batch
-
-                next unless family.hbx_enrollments.enrolled_and_renewal.any? {|enrollment| enrollment.applied_aptc_amount > 0 || %w[02 03 04 05 06].include?(enrollment.product.csr_variant_id) }
+                next unless enrollments.any? {|enrollment| enrollment.applied_aptc_amount.cents.positive? || %w[02 03 04 05 06].include?(enrollment.product.csr_variant_id) }
                 determined_application = fetch_application(family, params[:assistance_year])
-                next unless determined_application.present? && is_aptc_or_csr_eligible?(determined_application)
+                next unless determined_application.present?
                 total_applications_ran += 1
                 ::FinancialAssistance::Operations::Applications::MedicaidGateway::RequestMecChecks.new.call(application_id: determined_application.id) unless params[:skip_mec_call]
               end
             end
-            Rails.logger.info "MedicaidGateway::RunPeriodicDataMatching Total applications ran periodic data matching - #{total_applications_ran}"
+            @logger.info "MedicaidGateway::RunPeriodicDataMatching Completed periodic data matching for #{total_applications_ran} applications"
             Success({ total_applications_ran: total_applications_ran })
-          end
-
-          def is_aptc_or_csr_eligible?(application)
-            application.aptc_applicants.present?
+          rescue StandardError => e
+            @logger.error "Error: message: #{e.message}, backtrace: #{e.backtrace}"
           end
 
         end
--- components/financial_assistance/spec/dummy/app/models/hbx_enrollment.rb
+++ components/financial_assistance/spec/dummy/app/models/hbx_enrollment.rb
@@ -86,6 +86,7 @@ class HbxEnrollment
   scope :enrolled,             ->{ where(:aasm_state.in => ENROLLED_STATUSES) }
   scope :current_year,         ->{ where(:effective_on.gte => TimeKeeper.date_of_record.beginning_of_year, :effective_on.lte => TimeKeeper.date_of_record.end_of_year) }
   scope :individual_market,   ->{ where(:kind.nin => ["employer_sponsored", "employer_sponsored_cobra"]) }
+  scope :by_health,           ->{where(coverage_kind: "health").order(effective_on: :desc)}
 
   def product=(new_product)
     if new_product.blank?
