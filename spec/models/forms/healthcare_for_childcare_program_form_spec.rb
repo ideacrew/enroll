4c1d300d75f50fece986e81766dbee6f4a18ace1
raghuram <raghuramg83@gmail.com>
2023-08-08 18:17:21 -0400

    updated eligibility methods under specs

--- spec/models/forms/healthcare_for_childcare_program_form_spec.rb
+++ spec/models/forms/healthcare_for_childcare_program_form_spec.rb
@@ -48,7 +48,7 @@ describe Forms::HealthcareForChildcareProgramForm do
         subject.load_eligibility(primary)
         expect(subject.osse_eligibility).to be_truthy
         osse_eligibility = subject.role.eligibilities.first
-        expect(osse_eligibility.start_on).to eq(TimeKeeper.date_of_record.beginning_of_year)
+        expect(osse_eligibility.published_on).to eq(TimeKeeper.date_of_record.beginning_of_year)
         expect(osse_eligibility.evidences.first.is_satisfied).to be_truthy
       end
     end
@@ -77,7 +77,7 @@ describe Forms::HealthcareForChildcareProgramForm do
         subject.load_eligibility(primary)
         expect(subject.osse_eligibility).to be_truthy
         osse_eligibility = subject.role.eligibilities.first
-        expect(osse_eligibility.start_on).to eq(TimeKeeper.date_of_record.beginning_of_year)
+        expect(osse_eligibility.published_on).to eq(TimeKeeper.date_of_record.beginning_of_year)
         expect(osse_eligibility.evidences.first.is_satisfied).to be_truthy
       end
     end
