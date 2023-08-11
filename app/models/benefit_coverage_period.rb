0252fe232554615787c529b7cda5890a9b9edc28
Raghu Ram <raghuramg83@gmail.com>
2023-08-11 08:58:35 -0400

    rubocop fixes

--- app/models/benefit_coverage_period.rb
+++ app/models/benefit_coverage_period.rb
@@ -253,7 +253,8 @@ class BenefitCoveragePeriod
       organizations = Organization.where("hbx_profile.benefit_sponsorship.benefit_coverage_periods._id" => BSON::ObjectId.from_string(id))
       return unless organizations.present?
       hbx_profile = organizations.first.hbx_profile
-      hbx_profile&.benefit_sponsorship&.benefit_coverage_periods.find(BSON::ObjectId.from_string(id))
+      benefit_sponsorship = hbx_profile&.benefit_sponsorship
+      benefit_sponsorship&.benefit_coverage_periods&.find(BSON::ObjectId.from_string(id))
     end
 
     # The HBX benefit coverage period instance that includes this date within its start and end dates
