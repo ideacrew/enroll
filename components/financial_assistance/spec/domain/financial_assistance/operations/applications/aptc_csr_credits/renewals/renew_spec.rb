c98a3066e248c308e94de918391a3b1147a1513f
Yosef Serkez <36456900+4till2@users.noreply.github.com>
2023-08-11 08:20:55 -1000

    test failure reason message

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/aptc_csr_credits/renewals/renew_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/aptc_csr_credits/renewals/renew_spec.rb
@@ -303,6 +303,7 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::AptcCsrCreditEli
         it 'should return failure' do
           expect(@result).to be_failure
           expect(FinancialAssistance::Application.where(family_id: application.family.id).last.aasm_state).to eq 'applicants_update_required'
+          expect(@result.failure).to match(/because: \(missing_relationships\)/)
         end
       end
     end
