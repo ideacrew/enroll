9fcd9120eb2fca786b93abb2148275971e629217
Harshavardhan Ellanki <h_vardhan@yahoo.com>
2023-08-11 16:45:40 -0400

    new models and operations for new eligibility and shop/ivl osse eligibility (#3012)
    * new models and operations for new eligibility and shop/ivl osse eligibility
    
    Co-authored-by: raghuram  <raghuramg83@gmail.com>
    
    * updated aca_entities gem
    
    * spec fixes
    
    * codesweep fix
    
    * another try to fix codesweep concern
    
    * avoiding safe_constantize
    
    * updated dummy code
    
    ---------
    
    Co-authored-by: raghuram <raghuramg83@gmail.com>
    Co-authored-by: Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>

--- spec/factories/eligible_state_histories.rb
+++ spec/factories/eligible_state_histories.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
   factory :eligible_state_history, class: 'Eligible::StateHistory' do
     effective_on { TimeKeeper.date_of_record.beginning_of_month }
     is_eligible { true }
-    from_state { 'draft' }
-    to_state { 'eligible' }
+    from_state { :draft }
+    to_state { :eligible }
     transition_at { TimeKeeper.date_of_record }
-    event { 'mark_eligible' }
+    event { :mark_eligible }
     reason { 'met minimum criteria' }
     comment { 'consumer provided proper documentation' }
   end
