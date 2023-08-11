e24bef45640d202d71ac858fc33ae7219be3d902
Raghu Ram <raghuramg83@gmail.com>
2023-08-11 11:22:40 -0400

    add eligible models to benefit markets spec/dummy

---
+++ components/benefit_markets/spec/dummy/app/models/eligible/eligibility.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+
+module Eligible
+  # Eligibility model
+  class Eligibility
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    STATUSES = %i[initial published expired].freeze
+
+    field :key, type: Symbol
+    field :title, type: String
+    field :description, type: String
+    field :current_state, type: Symbol, default: :initial
+
+    embeds_many :evidences,
+                class_name: "::Eligible::Evidence",
+                cascade_callbacks: true
+    embeds_many :grants,
+                class_name: "::Eligible::Grant",
+                cascade_callbacks: true
+
+    embeds_many :state_histories,
+                class_name: "::Eligible::StateHistory",
+                cascade_callbacks: true,
+                as: :status_trackable
+
+    validates_presence_of :title
+
+    delegate :effective_on,
+             :is_eligible,
+             to: :latest_state_history,
+             allow_nil: false
+
+    scope :by_key, ->(key) { where(key: key.to_sym) }
+    scope :effectuated, -> { where(:current_state.ne => :initial) }
+
+    def latest_state_history
+      state_histories.max_by(&:created_at)
+    end
+
+    def eligibility_period_cover?(date)
+      if current_state == :initial
+        (effective_on..effective_on.end_of_year).cover?(date)
+      else
+        return false unless published_on
+        (published_on..expired_on).cover?(date)
+      end
+    end
+
+    def published_on
+      publish_history =
+        state_histories.by_state(:published).min_by(&:created_at)
+      publish_history&.effective_on
+    end
+
+    #default expired_on will be last day of callender year of the eligibility
+    #eligibility can't span across multiple years
+    #once eligibility is expired, it can never be moved back to published state
+    def expired_on
+      expiration_history =
+        state_histories.by_state(:expired).min_by(&:created_at)
+      expiration_history&.effective_on&.prev_day || published_on&.end_of_year
+    end
+
+    def is_eligible_on?(date)
+      evidences.all? { |evidence| evidence.is_eligible_on?(date) }
+    end
+
+    def grant_for(grant_key)
+      grants
+        .by_key(grant_key)
+        .detect { |grant| grant.value&.item.to_s == "true" }
+    end
+
+    class << self
+      ResourceReference = Struct.new(:class_name, :optional, :meta)
+
+      def resource_ref_dir
+        @resource_ref_dir ||= Concurrent::Map.new
+      end
+
+      def register(resource_kind, name, options)
+        resource_set = resource_kind.to_s.pluralize
+        resource_ref_dir[resource_set.to_sym] ||= {}
+        resource_ref_dir[resource_set.to_sym][name] = ResourceReference.new(
+          options[:class_name],
+          options[:optional],
+          options[:meta]
+        )
+      end
+
+      def grant(name, **options)
+        register(:grant, name, options)
+      end
+
+      def evidence(name, **options)
+        register(:evidence, name, options)
+      end
+
+      def evidences_resource_for(key)
+        resource_ref_dir[:evidences]&.dig(key)&.class_name ||
+          "Eligible::Evidence"
+      end
+
+      def grants_resource_for(key)
+        resource_ref_dir[:grants]&.dig(key)&.class_name || "Eligible::Grant"
+      end
+
+      def create_objects(collection, type)
+        collection.map do |item|
+          resource_name = send("#{type}_resource_for", item.key)
+          resource_name.constantize.new(item.to_h)
+        end
+      end
+    end
+  end
+end
---
+++ components/benefit_markets/spec/dummy/app/models/eligible/evidence.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module Eligible
+  # Evidence model
+  class Evidence
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    embedded_in :eligibility, class_name: "::Eligible::Eligibility"
+
+    STATUSES = %i[initial approved denied].freeze
+    ELIGIBLE_STATUSES = %i[approved].freeze
+
+    field :key, type: Symbol
+    field :title, type: String
+    field :description, type: String
+    field :is_satisfied, type: Boolean, default: false
+    field :current_state, type: Symbol, default: :initial
+    field :subject_ref, type: String
+    field :evidence_ref, type: String
+
+    embeds_many :state_histories,
+                class_name: "::Eligible::StateHistory",
+                cascade_callbacks: true,
+                as: :status_trackable
+
+    validates_presence_of :title, :key, :is_satisfied
+
+    delegate :effective_on,
+             :is_eligible,
+             to: :latest_state_history,
+             allow_nil: false
+
+    scope :by_key, ->(key) { where(key: key.to_sym) }
+
+    def latest_state_history
+      state_histories.max_by(&:created_at)
+    end
+
+    def is_eligible_on?(date)
+      eligible_periods.any? do |period|
+        if period[:end_on].present?
+          (period[:start_on]..period[:end_on]).cover?(date)
+        else
+          (period[:start_on]..period[:start_on].end_of_year).cover?(date)
+        end
+      end
+    end
+
+    # coverage period of the eligibility will start from the first day of the calendar year
+    # in which the eligibility was approved
+    # coverage period end on the last day of the calendar year in which the eligibility was denied
+    def eligible_periods
+      eligible_periods = []
+      date_range = {}
+      state_histories.non_initial.each do |state_history|
+        date_range[:start_on] ||= state_history.effective_on if state_history.to_state == :approved
+
+        next unless date_range.present? && state_history.to_state == :denied
+        date_range[:end_on] = state_history.effective_on.prev_day
+        eligible_periods << date_range
+        date_range = {}
+      end
+
+      eligible_periods << date_range unless date_range.empty?
+      eligible_periods
+    end
+  end
+end
