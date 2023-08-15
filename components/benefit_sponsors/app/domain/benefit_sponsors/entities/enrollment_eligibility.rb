447f45d1e0289b92f88c834e3903ecba948e0553
Raghu Ram <raghuramg83@gmail.com>
2023-08-15 19:11:40 -0400

    nil error fix

--- components/benefit_sponsors/app/domain/benefit_sponsors/entities/enrollment_eligibility.rb
+++ components/benefit_sponsors/app/domain/benefit_sponsors/entities/enrollment_eligibility.rb
@@ -5,20 +5,28 @@ module BenefitSponsors
     class EnrollmentEligibility < Dry::Struct
       transform_keys(&:to_sym)
 
-      attribute :market_kind,                     Types::Strict::Symbol
-      attribute :benefit_sponsorship_id,          Types::Bson
-      attribute :effective_date,                  Types::Strict::Date
-      attribute :benefit_application_kind,        Types::Strict::Symbol
-      attribute :service_areas,                   Types::Array.of(BenefitMarkets::Entities::ServiceArea)
-      attribute :osse_min_employer_contribution,  Types::Strict::Bool
-      attribute :sponsor_eligibilities,           Types::Array.optional.meta(omittable: true)
+      attribute :market_kind, Types::Strict::Symbol
+      attribute :benefit_sponsorship_id, Types::Bson
+      attribute :effective_date, Types::Strict::Date
+      attribute :benefit_application_kind, Types::Strict::Symbol
+      attribute :service_areas,
+                Types::Array.of(BenefitMarkets::Entities::ServiceArea)
+      attribute :osse_min_employer_contribution, Types::Strict::Bool
+      attribute :sponsor_eligibilities,
+                Types::Array.optional.meta(omittable: true)
 
       def metal_level_products_restricted?
-        sponsor_eligibilities.any?{|e| e.grant_for(:employer_metal_level_products).present?}
+        return false unless sponsor_eligibilities
+        sponsor_eligibilities.any? do |e|
+          e.grant_for(:employer_metal_level_products).present?
+        end
       end
 
       def employer_contribution_minimum_relaxed?
-        sponsor_eligibilities.any?{|e| e.grant_for(:all_contribution_levels_min_met).present?}
+        return false unless sponsor_eligibilities
+        sponsor_eligibilities.any? do |e|
+          e.grant_for(:all_contribution_levels_min_met).present?
+        end
       end
     end
   end
