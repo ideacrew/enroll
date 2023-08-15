d8bbb0b3f82b760148206db36635a4b68435620e
Yosef Serkez <36456900+4till2@users.noreply.github.com>
2023-08-15 12:09:36 -0400

    fix when back_to_account_all_shop is disabled

--- spec/views/shared/_sep_progress.html.erb_spec.rb
+++ spec/views/shared/_sep_progress.html.erb_spec.rb
@@ -1,44 +1,58 @@
 # frozen_string_literal: true
 
 # NOTE: The current tests provide a baseline coverage for the shared/_sep_progress.html.erb partial.
-# As the logic in the view evolves, it's crucial to expand upon these tests to ensure comprehensive coverage.
+# As the logic in the view is complex, it's crucial to expand upon these tests to ensure comprehensive coverage.
 # These tests are not exhaustive and should be considered a starting point. As features or functionalities
 # are added to or modified in the view, corresponding tests should be updated or added accordingly to
 # maintain a robust test suite.
 require 'rails_helper'
 
 describe "shared/_sep_progress.html.erb" do
+  # Define the steps to test.
+  tested_steps = %w[0 7].freeze
+
   let(:person_user) { FactoryBot.create(:person) }
   let(:current_user) { FactoryBot.create(:user, person: person_user) }
 
-  before do
-    sign_in current_user
-  end
+  before { sign_in current_user }
 
-  def validate_continue_button
+  # Helper to assert the presence of the continue button.
+  def assert_continue_button_present
     expect(rendered).to have_selector('#btn-continue', count: 1)
   end
 
-  def validate_link_visibility(should_be_visible)
-    %w[previous_step help_sign_up save_and_exit].each do |link_name|
+  # Helper to assert the visibility of specific links.
+  def assert_links_visibility(should_be_visible, skip_links: [])
+    links = %w[previous_step help_sign_up save_and_exit] - skip_links
+
+    links.each do |link_name|
       action = should_be_visible ? :to : :not_to
       expect(rendered).send(action, have_selector('a', text: l10n(link_name)))
     end
   end
 
-  # Currently testing for steps 0 and 7, where 7 is the last step. You can add more steps
-  # to the array if you want to test for more steps and update the test accordingly (e.g. the validate_link_visibility parameter)
-  %w[0 7].each do |step|
-    context "on step #{step}" do
-      before { render 'shared/sep_progress', step: step }
+  # Mocks the feature enabled state, defaulting all to false.
+  def mock_feature_state(feature, state)
+    allow(EnrollRegistry).to receive(:feature_enabled?).and_return(false)
+    allow(EnrollRegistry).to receive(:feature_enabled?).with(feature).and_return(state)
+  end
 
+  # Tests for specific steps. Extend tested_steps to test additional ones.
+  tested_steps.each do |step|
+    context "on step #{step}" do
       [true, false].each do |feature_state|
         context "with back_to_account_all_shop feature #{feature_state ? 'enabled' : 'disabled'}" do
-          before { allow(EnrollRegistry).to receive(:feature_enabled?).with(:back_to_account_all_shop).and_return(feature_state) }
+          before do
+            mock_feature_state(:back_to_account_all_shop, feature_state)
+            render 'shared/sep_progress', step: step
+          end
 
           it "validates button and link visibility" do
-            validate_continue_button
-            validate_link_visibility(step == '0')
+            assert_continue_button_present
+            #  # Help sign up is inconsistent when back_to_account_all_shop is disabled.
+            skip_links = feature_state ? [] : ['help_sign_up']
+            # Validate links based on the current step and feature state.
+            assert_links_visibility(step == '0', skip_links: skip_links)
           end
         end
       end
