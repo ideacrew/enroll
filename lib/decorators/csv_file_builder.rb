7e67e9ee25f16675d9aaa30543a5e4541550d0dc
rahul <rahul.gudimetla@ideacrew.com>
2023-08-18 11:40:37 -0500

    periodic dev testing updates to opertion

--- lib/decorators/csv_file_builder.rb
+++ lib/decorators/csv_file_builder.rb
@@ -3,17 +3,23 @@
 module Decorators
   # CSV file builder to build report
   class CSVFileBuilder
-    def initialize(filename, headers)
-      @output = CSV.open(filename, "w")
-
-      append_data(headers)
+    def initialize(file_path, headers, logger)
+      @file_path = file_path
+      @logger = logger
+      # creates the file and add headers
+      CSV.open(file_path, "w") do |csv|
+        csv << headers
+      end
     rescue StandardError => e
       @logger.error "Error: Failed in initialize CSVFileBuilder #{e.message}, backtrace: #{e.backtrace}"
     end
 
-    def append_data(data)
-      @output.add_row(data)
-      @output.flush
+    def append_data(rows)
+      CSV.open(@file_path, 'a') do |csv|
+        rows.each do |row|
+          csv << row
+        end
+      end
     rescue StandardError => e
       @logger.error "Error: Failed in append_data CSVFileBuilder #{e.message}, backtrace: #{e.backtrace}"
     end
