0a020ba6146f01b74b9a6016f2344a318b07bb21
TristanB17 <tristan0bambauer@gmail.com>
2023-08-21 11:21:04 -0600

    Relating family_members thru nuclear_family trait in Family Factory

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/auto_extend_income_evidence_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/auto_extend_income_evidence_spec.rb
@@ -158,9 +158,8 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::AutoExtendIncome
     # Maybe add 1 member with complete verified income evidence?
 
     let!(:person) { FactoryBot.create(:person, :with_consumer_role, hbx_id: '100095') }
-    let!(:family) { FactoryBot.create(:family, :with_primary_family_member_and_spouse_and_child, person: person) }
+    let!(:family) { FactoryBot.create(:family, :with_nuclear_family, person: person) }
     let!(:address) { FactoryBot.build(:financial_assistance_address) }
-    # let!(:family) { FactoryBot.create(:family, :with_primary_family_member_and_dependent, person: person) }
     let(:applicant_1_due_date) { TimeKeeper.date_of_record + 10.days }
     let(:applicant_2_due_date) { TimeKeeper.date_of_record }
     let(:applicant_3_due_date) { TimeKeeper.date_of_record + 30.days }
@@ -242,6 +241,7 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::AutoExtendIncome
                                                       update_reason: 'Auto extended due date',
                                                       updated_by: 'system')
 
+      binding.irb
       application.ensure_relationship_with_primary(applicant_2, 'spouse')
       application.ensure_relationship_with_primary(applicant_3, 'child')
       application.update_or_build_relationship(applicant_2, applicant_3, 'parent')
