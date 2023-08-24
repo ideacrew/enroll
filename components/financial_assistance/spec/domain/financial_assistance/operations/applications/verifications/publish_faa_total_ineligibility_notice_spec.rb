f55ffe76f4e2cbb00bac07202c926bed27f5d876
RyanEddyIC <ryan.eddy@ideacrew.com>
2023-08-24 15:40:28 -0400

    rubocop fix

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/verifications/publish_faa_total_ineligibility_notice_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/verifications/publish_faa_total_ineligibility_notice_spec.rb
@@ -10,7 +10,14 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::P
 
   let!(:person) { FactoryBot.create(:person, :with_ssn, hbx_id: "732020")}
   let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
-  let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family.id, aasm_state: 'determined', hbx_id: "830293", effective_date: TimeKeeper.date_of_record.beginning_of_year, eligibility_response_payload: response_payload.to_json) }
+  let!(:application) do
+    FactoryBot.create(:financial_assistance_application,
+                      family_id: family.id,
+                      aasm_state: 'determined',
+                      hbx_id: "830293",
+                      effective_date: TimeKeeper.date_of_record.beginning_of_year,
+                      eligibility_response_payload: response_payload.to_json)
+  end
   let!(:applicant) do
     FactoryBot.create(:applicant,
                       first_name: person.first_name,
