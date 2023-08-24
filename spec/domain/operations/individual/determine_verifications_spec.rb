8ae80923f5e4485ba65adba302caffb833461e1b
Vishal Kalletla <vishuk1201@gmail.com>
2023-08-24 00:44:06 -0400

    add spec

--- spec/domain/operations/individual/determine_verifications_spec.rb
+++ spec/domain/operations/individual/determine_verifications_spec.rb
@@ -4,91 +4,49 @@ require "rails_helper"
 
 RSpec.describe Operations::Individual::DetermineVerifications, dbclean: :after_each do
 
-  subject do
-    described_class.new.call(id: consumer_role_id)
-  end
-
-  let(:consumer_role_id) { consumer_role.id }
-
-  context 'when consumer role id is nil' do
-    let(:consumer_role_id) { nil }
-
-    it 'returns failure' do
-      expect(subject.failure?).to eq true
-    end
-  end
-
-  context 'when person is nil' do
-    let(:consumer_role) { ConsumerRole.new }
-
-    it 'returns failure' do
-      expect(subject.failure?).to eq true
-    end
-  end
-
-  context 'when person is not applying for coverage' do
-    let(:person) { FactoryBot.create(:person, :with_consumer_role) }
-    let(:consumer_role) do
-      role = person.consumer_role
-      role.update_attributes(is_applying_coverage: false)
-      role
+  describe '#call' do
+    subject do
+      described_class.new.call(id: consumer_role_id)
     end
 
-    it 'returns failure' do
-      expect(subject.failure?).to eq true
-    end
-  end
+    let(:consumer_role_id) { consumer_role.id }
 
-  describe 'when person and consumer role exists' do
-    let(:person) { FactoryBot.create(:person, :with_consumer_role) }
-    let(:consumer_role) { person.consumer_role }
+    context 'when consumer role id is nil' do
+      let(:consumer_role_id) { nil }
 
-    context "when trigger_verifications_before_enrollment_purchase is enabled" do
-      before do
-        EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(true)
-      end
-
-      it 'returns success' do
-        expect(subject.success?).to eq true
-        expect(subject.success).to eq "Successfully triggered Hub Calls for ConsumerRole with person_hbx_id: #{person.hbx_id}"
-      end
-
-      it 'updates consumer_role state to verification_outstanding' do
-        expect(consumer_role.aasm_state).to eq 'unverified'
-        subject
-        expect(consumer_role.reload.aasm_state).to eq 'verification_outstanding'
+      it 'returns failure' do
+        expect(subject.failure?).to eq true
       end
     end
 
-    context "when trigger_verifications_before_enrollment_purchase is disabled" do
-      before do
-        EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(false)
+    context 'when person is nil' do
+      let(:consumer_role) { ConsumerRole.new }
+
+      it 'returns failure' do
+        expect(subject.failure?).to eq true
+      end
+    end
+
+    context 'when person is not applying for coverage' do
+      let(:person) { FactoryBot.create(:person, :with_consumer_role) }
+      let(:consumer_role) do
+        role = person.consumer_role
+        role.update_attributes(is_applying_coverage: false)
+        role
       end
 
-      context "if person don't have an active enrollment" do
-        it 'returns success' do
-          expect(subject.success?).to eq false
-          expect(subject.failure).to eq "ConsumerRole with person_hbx_id: #{person.hbx_id} is not enrolled to trigger hub calls"
-        end
-
-        it 'updates consumer_role state to verification_outstanding' do
-          expect(consumer_role.aasm_state).to eq 'unverified'
-          subject
-          expect(consumer_role.reload.aasm_state).to eq 'unverified'
-        end
+      it 'returns failure' do
+        expect(subject.failure?).to eq true
       end
+    end
 
-      context "if person has an active enrollment" do
-        let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
-        let!(:enrollment) do
-          FactoryBot.create(
-            :hbx_enrollment,
-            :with_enrollment_members,
-            :individual_assisted,
-            family: family,
-            consumer_role_id: consumer_role.id,
-            enrollment_members: family.family_members
-          )
+    describe 'when person and consumer role exists' do
+      let(:person) { FactoryBot.create(:person, :with_consumer_role) }
+      let(:consumer_role) { person.consumer_role }
+
+      context "when trigger_verifications_before_enrollment_purchase is enabled" do
+        before do
+          EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(true)
         end
 
         it 'returns success' do
@@ -100,6 +58,135 @@ RSpec.describe Operations::Individual::DetermineVerifications, dbclean: :after_e
           expect(consumer_role.aasm_state).to eq 'unverified'
           subject
           expect(consumer_role.reload.aasm_state).to eq 'verification_outstanding'
+        end
+      end
+
+      context "when trigger_verifications_before_enrollment_purchase is disabled" do
+        before do
+          EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(false)
+        end
+
+        context "if person don't have an active enrollment" do
+          it 'returns success' do
+            expect(subject.success?).to eq false
+            expect(subject.failure).to eq "ConsumerRole with person_hbx_id: #{person.hbx_id} is not enrolled to trigger hub calls"
+          end
+
+          it 'updates consumer_role state to verification_outstanding' do
+            expect(consumer_role.aasm_state).to eq 'unverified'
+            subject
+            expect(consumer_role.reload.aasm_state).to eq 'unverified'
+          end
+        end
+
+        context "if person has an active enrollment" do
+          let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
+          let!(:enrollment) do
+            FactoryBot.create(
+              :hbx_enrollment,
+              :with_enrollment_members,
+              :individual_assisted,
+              family: family,
+              consumer_role_id: consumer_role.id,
+              enrollment_members: family.family_members
+            )
+          end
+
+          it 'returns success' do
+            expect(subject.success?).to eq true
+            expect(subject.success).to eq "Successfully triggered Hub Calls for ConsumerRole with person_hbx_id: #{person.hbx_id}"
+          end
+
+          it 'updates consumer_role state to verification_outstanding' do
+            expect(consumer_role.aasm_state).to eq 'unverified'
+            subject
+            expect(consumer_role.reload.aasm_state).to eq 'verification_outstanding'
+          end
+        end
+      end
+    end
+  end
+
+  describe 'create type_history_elements for triggered hub_calls' do
+
+    context '#valid ssn' do
+      before do
+        EnrollRegistry[:trigger_verifications_before_enrollment_purchase].feature.stub(:is_enabled).and_return(true)
+        ConsumerRole.skip_callback(:update, :after, :publish_updated_event)
+        consumer_role = FactoryBot.create(:consumer_role)
+        ConsumerRole.set_callback(:update, :after, :publish_updated_event)
+        allow(ConsumerRole).to receive(:find).and_return(consumer_role)
+        @result = described_class.new.call(id: consumer_role.id)
+        @types = consumer_role.verification_types
+      end
+
+      it 'should record history for requested hub calls' do
+        expect(@types.ssn_type.first.type_history_elements.count).to eq 1
+        expect(@types.citizenship_type.first.type_history_elements.count).to eq 1
+      end
+
+      it 'should set verification_type to pending' do
+        expect(@types.ssn_type.first.validation_status).to eq 'pending'
+        expect(@types.citizenship_type.first.validation_status).to eq 'pending'
+      end
+    end
+
+    context '#invalid ssn ' do
+      context 'when validate_and_record_publish_errors feature is enabled' do
+        before do
+          validator = instance_double(Operations::Fdsh::EncryptedSsnValidator)
+          allow(EnrollRegistry).to receive(:feature_enabled?).and_return(false)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:ssa_h3).and_return(true)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:vlp_h92).and_return(true)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:validate_and_record_publish_errors).and_return(true)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:trigger_verifications_before_enrollment_purchase).and_return(true)
+          allow(validator).to receive(:call).and_return(Dry::Monads::Failure('Invalid SSN'))
+          allow(Operations::Fdsh::EncryptedSsnValidator).to receive(:new).and_return(validator)
+          ConsumerRole.skip_callback(:update, :after, :publish_updated_event)
+          consumer_role = FactoryBot.create(:consumer_role, ssn: '999001234')
+          ConsumerRole.set_callback(:update, :after, :publish_updated_event)
+          allow(ConsumerRole).to receive(:find).and_return(consumer_role)
+          @result = described_class.new.call(id: consumer_role.id)
+          @types = consumer_role.verification_types
+        end
+
+        it 'should record history for requested hub calls' do
+          expect(@types.ssn_type.first.type_history_elements.count).to eq 2
+          expect(@types.citizenship_type.first.type_history_elements.count).to eq 1
+        end
+
+        it 'should set verification_type to pending' do
+          expect(@types.ssn_type.first.validation_status).to eq 'negative_response_received'
+          expect(@types.citizenship_type.first.validation_status).to eq 'pending'
+        end
+      end
+
+      context 'when validate_and_record_publish_errors feature is disabled' do
+        before do
+          validator = instance_double(Operations::Fdsh::EncryptedSsnValidator)
+          allow(EnrollRegistry).to receive(:feature_enabled?).and_return(false)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:ssa_h3).and_return(true)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:vlp_h92).and_return(true)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:validate_and_record_publish_errors).and_return(false)
+          allow(EnrollRegistry).to receive(:feature_enabled?).with(:trigger_verifications_before_enrollment_purchase).and_return(true)
+          allow(validator).to receive(:call).and_return(Dry::Monads::Failure('Invalid SSN'))
+          allow(Operations::Fdsh::EncryptedSsnValidator).to receive(:new).and_return(validator)
+          ConsumerRole.skip_callback(:update, :after, :publish_updated_event)
+          consumer_role = FactoryBot.create(:consumer_role, ssn: '999001234')
+          ConsumerRole.set_callback(:update, :after, :publish_updated_event)
+          allow(ConsumerRole).to receive(:find).and_return(consumer_role)
+          @result = described_class.new.call(id: consumer_role.id)
+          @types = consumer_role.verification_types
+        end
+
+        it 'should record history for requested hub calls' do
+          expect(@types.ssn_type.first.type_history_elements.count).to eq 1
+          expect(@types.citizenship_type.first.type_history_elements.count).to eq 1
+        end
+
+        it 'should set verification_type to pending' do
+          expect(@types.ssn_type.first.validation_status).to eq 'pending'
+          expect(@types.citizenship_type.first.validation_status).to eq 'pending'
         end
       end
     end
