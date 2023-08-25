d83d9e39ff3b9136b2c9687cd81ff3089290723c
Joseph Ogunbodede <57965400+j1joey@users.noreply.github.com>
2023-08-25 12:52:20 -0400

    fix float decimal logic

--- app/helpers/float_helper.rb
+++ app/helpers/float_helper.rb
@@ -6,6 +6,9 @@ module FloatHelper
   end
 
   def round_down_float_two_decimals(float_number)
+    if float_number == nil then 
+      float_number = 0.0
+    end
     BigDecimal((float_number).to_s).round(8).round(2, BigDecimal::ROUND_DOWN).to_f
   end
 end
