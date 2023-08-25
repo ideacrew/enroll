ab67c4de6842c90ce4acf037150669abac4daeb6
Harshavardhan Ellanki <h_vardhan@yahoo.com>
2023-08-25 16:22:59 -0400

    publishers and subscribers to create/renew osse eligibilities (#3075)
    * publishers and subscribers to create/renew osse eligibilities
    
    * updated ivl osse renewal

---
+++ app/event_source/subscribers/eligible/eligibility_subscriber.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+module Subscribers
+  module Eligible
+    # Subscriber will receive Enterprise requests like date change
+    class EligibilitySubscriber
+      include ::EventSource::Subscriber[amqp: 'enroll.eligible.eligibility.events']
+
+      subscribe(:on_create_default_eligibility) do |delivery_info, _metadata, response|
+        logger_name = "on_create_default_eligibility_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}"
+        subscriber_logger = Logger.new("#{Rails.root}/log/#{logger_name}.log")
+        subscriber_logger.info '-' * 100 unless Rails.env.test?
+        
+        payload = JSON.parse(response, symbolize_names: true)
+        subscriber_logger.info "EligibilitySubscriber, payload: #{payload}"
+
+        subject = GlobalID::Locator.locate(payload[:subject_gid])
+        eligibility_date = TimeKeeper.date_of_record
+        effective_date = payload[:effective_date].to_date
+        evidence_key = payload[:evidence_key].to_sym
+
+        eligibility_key = eligibility_key_for(subject, eligibility_date)
+        eligibility = subject.eligibility_for(eligibility_key, eligibility_date)
+
+        unless eligibility
+          result = eligibility_operation_for(subject).new.call(
+            {
+              subject: subject.to_global_id,
+              evidence_key: evidence_key,
+              evidence_value: "false",
+              effective_date: effective_date
+            }
+          )
+        end
+        ack(delivery_info.delivery_tag)
+      rescue StandardError, SystemStackError => e
+        subscriber_logger.info "EligibilitySubscriber, payload: #{payload}, error message: #{e.message}, backtrace: #{e.backtrace}"
+        ack(delivery_info.delivery_tag)
+      end
+
+      subscribe(:on_renew_eligibility) do |delivery_info, _metadata, response|
+        logger_name = "on_renew_eligibility_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}"
+        subscriber_logger = Logger.new("#{Rails.root}/log/#{logger_name}.log")
+        subscriber_logger.info '-' * 100 unless Rails.env.test?
+
+        payload = JSON.parse(response, symbolize_names: true)
+        subscriber_logger.info "EligibilitySubscriber#on_enroll_enterprise_events, response: #{payload}"
+
+        subject = GlobalID::Locator.locate(payload[:subject_gid])
+        eligibility_date = TimeKeeper.date_of_record
+        effective_date = payload[:effective_date].to_date
+        evidence_key = payload[:evidence_key].to_sym
+        eligibility_key = eligibility_key_for(subject, eligibility_date)
+
+        eligibility = subject.eligibility_for(eligibility_key, eligibility_date)
+        osse_eligibility = eligibility.blank? ? false : eligibility.is_eligible_on?(eligibility_date)
+
+        renew_eligibility_key = eligibility_key_for(subject, effective_date)
+        renewal_eligibility = subject.eligibility_for(renew_eligibility_key, effective_date)
+
+        unless renewal_eligibility
+          result = eligibility_operation_for(subject).new.call(
+            {
+              subject: subject.to_global_id,
+              evidence_key: evidence_key,
+              evidence_value: osse_eligibility.to_s,
+              effective_date: effective_date
+            }
+          )
+        end
+
+        ack(delivery_info.delivery_tag)
+      rescue StandardError, SystemStackError => e
+        subscriber_logger.info "EligibilitySubscriber#on_enroll_enterprise_events, payload: #{payload}, error message: #{e.message}, backtrace: #{e.backtrace}"
+        ack(delivery_info.delivery_tag)
+      end
+
+      private
+
+      def eligibility_operation_for(subject)
+        case subject.class.to_s
+        when "ConsumerRole" || "ResidentRole"
+          ::Operations::IvlOsseEligibilities::CreateIvlOsseEligibility
+        when "BenefitSponsors::BenefitSponsorships::BenefitSponsorship"
+          ::BenefitSponsors::Operations::BenefitSponsorships::ShopOsseEligibilities::CreateShopOsseEligibility
+        end
+      end
+
+      def eligibility_key_for(subject, eligibility_date)
+        case subject.class.to_s
+        when "ConsumerRole" || "ResidentRole"
+          "aca_ivl_osse_eligibility_#{eligibility_date.year}".to_sym
+        when "BenefitSponsors::BenefitSponsorships::BenefitSponsorship"
+          "aca_shop_osse_eligibility_#{eligibility_date.year}".to_sym
+        end
+      end
+    end
+  end
+end
