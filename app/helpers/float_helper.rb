08bd17864434ee260b7b025769ea23827b840bee
Joseph Ogunbodede <57965400+j1joey@users.noreply.github.com>
2023-08-29 10:50:15 -0400

    rubocop

--- app/helpers/float_helper.rb
+++ app/helpers/float_helper.rb
@@ -2,13 +2,11 @@
 
 module FloatHelper
   def float_fix(float_number)
-    BigDecimal((float_number).to_s).round(8).to_f
+    BigDecimal(float_number.to_s).round(8).to_f
   end
 
   def round_down_float_two_decimals(float_number)
-    if float_number == nil then 
-      float_number = 0.0
-    end
-    BigDecimal((float_number).to_s).round(8).round(2, BigDecimal::ROUND_DOWN).to_f
+    float_number = 0.0 if float_number.nil?
+    BigDecimal(float_number.to_s).round(8).round(2, BigDecimal::ROUND_DOWN).to_f
   end
 end
