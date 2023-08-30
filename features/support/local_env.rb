6ee5a64367418233a5dea02186d30f72cc604fe2
Marco (Polo) Ornelas <marco.ornelas@ideacrew.com>
2023-08-30 16:30:07 -0400

    re-enable cucumber

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -1,2 +1,7 @@
 # Enable Factory_bot
 World(FactoryBot::Syntax::Methods)
+if ENV["COVERAGE"]
+  require 'simplecov'
+  SimpleCov.command_name "cukes_#{Process.pid.to_s}_#{ENV['TEST_ENV_NUMBER'] || '1'}"
+  SimpleCov.start 'rails'
+end
