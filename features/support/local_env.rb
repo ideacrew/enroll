88b7c97db22640964f318c3b7fff5e13c4a089a4
Marco (Polo) Ornelas <marco.ornelas@ideacrew.com>
2023-08-30 16:34:34 -0400

    rubocop

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -2,6 +2,6 @@
 World(FactoryBot::Syntax::Methods)
 if ENV["COVERAGE"]
   require 'simplecov'
-  SimpleCov.command_name "cukes_#{Process.pid.to_s}_#{ENV['TEST_ENV_NUMBER'] || '1'}"
+  SimpleCov.command_name "cukes_#{Process.pid}_#{ENV['TEST_ENV_NUMBER'] || '1'}"
   SimpleCov.start 'rails'
 end
