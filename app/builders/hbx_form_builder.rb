cfde7aa96b7a8cf58cd462447635bd198f7d148f
Adam Runner <adam.runner@ideacrew.com>
2023-09-01 14:28:21 -0700

    code style changes for hbx_form_builder

--- app/builders/hbx_form_builder.rb
+++ app/builders/hbx_form_builder.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HbxFormBuilder < ActionView::Helpers::FormBuilder
   include ::L10nHelper
 
@@ -7,11 +9,11 @@ class HbxFormBuilder < ActionView::Helpers::FormBuilder
     class_list  = options.delete(:class)    || ""
     class_list += " #{'required' if is_required} floatlabel form-control mask-ssn"
     shared_options = {
-        class: class_list.split(/\s+/).uniq.join(" "),
-        maxlength: 11,
-        required: is_required,
-        placeholder: "#{l10n("social_security").to_s.upcase} #{'*' if is_required}",
-        readonly: is_readonly
+      class: class_list.split(/\s+/).uniq.join(" "),
+      maxlength: 11,
+      required: is_required,
+      placeholder: "#{l10n('social_security').to_s.upcase} #{'*' if is_required}",
+      readonly: is_readonly
     }
     if EnrollRegistry.feature_enabled?(:ssn_ui_validation)
       @template.text_field(
@@ -20,7 +22,7 @@ class HbxFormBuilder < ActionView::Helpers::FormBuilder
         options.merge(
           pattern: "(?!666|000|9\\d{2})\\d{3}[\\- ]{0,1}(?!00)\\d{2}[\\- ]{0,1}(?!0{4})\\d{4}",
           oninvalid: "this.setCustomValidity('Invalid Social Security number.')",
-          oninput: "this.setCustomValidity('')",
+          oninput: "this.setCustomValidity('')"
         ).merge(
           shared_options
         )
