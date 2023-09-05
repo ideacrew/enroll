f1362a7d8e7327576d45154a52efe8be04897323
Yosef Serkez <36456900+4till2@users.noreply.github.com>
2023-09-05 11:32:24 -0400

    temp fix request all resource limitation

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/aptc_csr_credit_eligibilities/renewals/request_all.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/aptc_csr_credit_eligibilities/renewals/request_all.rb
@@ -29,7 +29,7 @@ module FinancialAssistance
             # @return [Array] family_ids
             def renewal_eligible_family_ids(renewal_year)
               family_ids = ::HbxEnrollment.individual_market.enrolled.current_year.distinct(:family_id)
-              eligible_family_ids = ::FinancialAssistance::Application.by_year(renewal_year.pred).determined.where(:family_id.in => family_ids).group_by(&:family_id).select { |_family_id, group| group.max_by(&:created_at).eligible_for_renewal? }.keys
+              eligible_family_ids = ::FinancialAssistance::Application.by_year(renewal_year.pred).determined.where(:family_id.in => family_ids).distinct(:family_id)#.group_by(&:family_id).select { |_family_id, group| group.max_by(&:created_at).eligible_for_renewal? }.keys
               Success(eligible_family_ids)
             rescue StandardError => e
               Failure("Failed to find renewal eligible family_ids, error: #{e}")
