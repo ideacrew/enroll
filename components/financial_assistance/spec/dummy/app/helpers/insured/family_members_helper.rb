dd26231b9f99aef5cfbd0054ea3a1b78c0091f20
Adam Runner <adam.runner@ideacrew.com>
2023-09-08 16:16:50 -0700

    rubocop fixes for family members helper

--- components/financial_assistance/spec/dummy/app/helpers/insured/family_members_helper.rb
+++ components/financial_assistance/spec/dummy/app/helpers/insured/family_members_helper.rb
@@ -4,7 +4,6 @@
 module Insured
   # copy of app/helpers/insured/family_members_helper.rb
   module FamilyMembersHelper
-
     def employee_dependent_form_id(model)
       if model.try(:persisted?)
         "add_member_list_#{model.id}"
@@ -21,15 +20,17 @@ module Insured
       end
     end
 
+    # rubocop:disable Lint/EmptyRescueClause
     def get_address_from_dependent(dependent)
-      if dependent.class == FamilyMember
+      if dependent.instance_of?(FamilyMember)
         dependent.person.addresses
       else
         dependent.family_member.person.addresses
       end
-    rescue StandardError => e
+    rescue StandardError
       []
     end
+    # rubocop:enable Lint/EmptyRescueClause
 
     def is_applying_coverage_value_dependent(dependent)
       first_checked = true
@@ -38,7 +39,7 @@ module Insured
         first_checked = dependent.family_member.person.consumer_role.is_applying_coverage
         second_checked = !dependent.family_member.person.consumer_role.is_applying_coverage
       end
-      first_checked, second_checked
+      [first_checked, second_checked]
     end
   end
 end
