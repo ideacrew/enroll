eb2ca11082899430bb95b9fec8d268d41f50629c
Adam Runner <adam.runner@ideacrew.com>
2023-09-08 16:03:26 -0700

    rubocop fixes for permission_helper

--- components/financial_assistance/spec/dummy/app/helpers/permission_helper.rb
+++ components/financial_assistance/spec/dummy/app/helpers/permission_helper.rb
@@ -1,27 +1,29 @@
-# Use this in views.   Needed to make rspec work.
+# frozen_string_literal: true
+
+# Use the following line in your view specs:
 # allow(view).to receive(:policy_helper).and_return(double("PersonPolicy", updateable?: true))
 # https://github.com/elabs/pundit/issues/339
 # https://www.relishapp.com/rspec/rspec-rails/v/3-0/docs/view-specs/view-spec#passing-view-spec-that-stubs-a-helper-method
 module PermissionHelper
-  def policy_helper pundit_object
-    policy(pundit_object) 
+  def policy_helper(pundit_object)
+    policy(pundit_object)
   end
 
-  def pundit_span pundit_object, pundit_method
+  def pundit_span(pundit_object, pundit_method)
     return ' <span class="blocking" >' unless pundit_object
 
     result = policy_helper(pundit_object).send(pundit_method) ? '<span class="no-op">' : ' <span class="blocking" >'
     raw result
   end
 
-  def pundit_class pundit_object, pundit_method
+  def pundit_class(pundit_object, pundit_method)
     return '  blocking ' unless pundit_object
 
     result = policy_helper(pundit_object).send(pundit_method) ? ' no-op ' : '  blocking '
     raw result
   end
 
-  def pundit_allow pundit_object, pundit_method
-    result = policy_helper(pundit_object).send(pundit_method)
+  def pundit_allow(pundit_object, pundit_method)
+    policy_helper(pundit_object).send(pundit_method)
   end
 end
