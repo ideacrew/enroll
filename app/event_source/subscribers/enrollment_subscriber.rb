ddc3efb21c850a58ac07f2f9dbf839c926be425e
Yosef Serkez <36456900+4till2@users.noreply.github.com>
2023-09-14 10:02:44 -0400

    fix fetch application

--- app/event_source/subscribers/enrollment_subscriber.rb
+++ app/event_source/subscribers/enrollment_subscriber.rb
@@ -103,7 +103,7 @@ module Subscribers
 
       if HbxEnrollment::ENROLLED_AND_RENEWAL_STATUSES.include?(enrollment.aasm_state)
         family.fail_negative_and_pending_verifications
-        application = fetch_application(enrollment)
+        application = fetch_application(enrollment, family)
         subscriber_logger.info "EnrollmentSubscriber, redetermine_family_eligibility for enrollment #{enrollment.hbx_id} with the application #{application&.hbx_id}"
         application&.enrolled_with(enrollment)
       end
@@ -114,7 +114,7 @@ module Subscribers
 
     private
 
-    def fetch_application(enrollment)
+    def fetch_application(enrollment, family)
       application = if EnrollRegistry.feature_enabled?(:temporary_configuration_enable_multi_tax_household_feature)
                       thhe = TaxHouseholdEnrollment.where(enrollment_id: enrollment.id).first
                       application_hbx_id = thhe&.tax_household&.tax_household_group&.application_id
