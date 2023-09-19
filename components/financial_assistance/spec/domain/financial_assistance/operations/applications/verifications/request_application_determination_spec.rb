35009e3c3255f458acd90b84e6065dfcf7e6aad4
TristanB17 <tristan0bambauer@gmail.com>
2023-09-19 16:09:02 -0600

    Reuses existing eligibility components

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/verifications/request_application_determination_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/verifications/request_application_determination_spec.rb
@@ -6,14 +6,14 @@ require "#{FinancialAssistance::Engine.root}/spec/dummy/app/domain/operations/in
 RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::RequestApplicationDetermination, dbclean: :after_each do
   include Dry::Monads[:result, :do]
 
-  let!(:person) { FactoryBot.create(:person, :with_ssn, hbx_id: "732020") }
+  let!(:person_1) { FactoryBot.create(:person, :with_ssn, hbx_id: "732020") }
   let!(:person_2) { FactoryBot.create(:person, :with_ssn, hbx_id: "732021") }
   let!(:person_3) { FactoryBot.create(:person, :with_ssn, hbx_id: "732022") }
-  let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person)}
+  let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person_1)}
   let!(:application) { FactoryBot.create(:financial_assistance_application, family_id: family.id, aasm_state: 'determined', hbx_id: "830293", effective_date: TimeKeeper.date_of_record.beginning_of_year) }
   let!(:eligibility_determination) { FactoryBot.create(:financial_assistance_eligibility_determination, application: application) }
 
-  let!(:applicant) do
+  let!(:applicant_1) do
     FactoryBot.build(:financial_assistance_applicant,
                      :with_student_information,
                      :with_home_address,
@@ -24,10 +24,10 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::R
                      is_primary_applicant: true,
                      ssn: '889984400',
                      dob: Date.new(1994,11,17),
-                     first_name: person.first_name,
-                     last_name: person.last_name,
-                     gender: person.gender,
-                     person_hbx_id: person.hbx_id,
+                     first_name: person_1.first_name,
+                     last_name: person_1.last_name,
+                     gender: person_1.gender,
+                     person_hbx_id: person_1.hbx_id,
                      eligibility_determination_id: eligibility_determination.id)
   end
 
@@ -78,25 +78,25 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::R
                                                           state: 'DC',
                                                           zip: '20001'
                                                         })
-    applicant.addresses << add
-    applicant.save!
+    applicant_1.addresses << add
+    applicant_1.save!
   end
 
   let(:premiums_hash) do
     {
-      [person.hbx_id] => {:health_only => {person.hbx_id => [{:cost => 200.0, :member_identifier => person.hbx_id, :monthly_premium => 200.0}]}}
+      [person_1.hbx_id] => {:health_only => {person_1.hbx_id => [{:cost => 200.0, :member_identifier => person_1.hbx_id, :monthly_premium => 200.0}]}}
     }
   end
 
   let(:slcsp_info) do
     {
-      person.hbx_id => {:health_only_slcsp_premiums => {:cost => 200.0, :member_identifier => person.hbx_id, :monthly_premium => 200.0}}
+      person_1.hbx_id => {:health_only_slcsp_premiums => {:cost => 200.0, :member_identifier => person_1.hbx_id, :monthly_premium => 200.0}}
     }
   end
 
   let(:lcsp_info) do
     {
-      person.hbx_id => {:health_only_lcsp_premiums => {:cost => 100.0, :member_identifier => person.hbx_id, :monthly_premium => 100.0}}
+      person_1.hbx_id => {:health_only_lcsp_premiums => {:cost => 100.0, :member_identifier => person_1.hbx_id, :monthly_premium => 100.0}}
     }
   end
 
@@ -123,13 +123,13 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::R
     allow(hbx_profile).to receive(:benefit_sponsorship).and_return benefit_sponsorship
     allow(benefit_sponsorship).to receive(:current_benefit_period).and_return(benefit_coverage_period)
 
-    applicant.esi_evidence.update(aasm_state: 'pending')
+    applicant_1.esi_evidence.update(aasm_state: 'pending')
     applicant_2.esi_evidence.update(aasm_state: 'pending')
     applicant_3.esi_evidence.update(aasm_state: 'pending')
-    applicant.non_esi_evidence.update(aasm_state: 'pending')
+    applicant_1.non_esi_evidence.update(aasm_state: 'pending')
     applicant_2.non_esi_evidence.update(aasm_state: 'pending')
     applicant_3.non_esi_evidence.update(aasm_state: 'pending')
-    applicant.local_mec_evidence.update(aasm_state: 'pending')
+    applicant_1.local_mec_evidence.update(aasm_state: 'pending')
     applicant_2.local_mec_evidence.update(aasm_state: 'pending')
     applicant_3.local_mec_evidence.update(aasm_state: 'pending')
   end
@@ -163,7 +163,7 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::R
     context 'with an invalid applicant' do
       context 'on mec evidence' do
         before do
-          applicant.update(ssn: '000348745')
+          applicant_1.update(ssn: '000348745')
           @result = subject.call(application)
         end
 
@@ -172,27 +172,27 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::R
         end
 
         it "should update invalid applicants' verification history" do
-          evidence = applicant.esi_evidence
+          evidence = applicant_1.esi_evidence
           expect(evidence.verification_histories.length).to eq 1
           expect(evidence.verification_histories.first.update_reason).to include "Invalid SSN"
         end
 
         it "should NOT modify valid applicants' verification histories" do
-          evidence2 = applicant_2.esi_evidence
-          evidence3 = applicant_3.esi_evidence
-          expect(evidence2.verification_histories.length).to eq 0
-          expect(evidence3.verification_histories.length).to eq 0
+          evidence_2 = applicant_2.esi_evidence
+          evidence_3 = applicant_3.esi_evidence
+          expect(evidence_2.verification_histories.length).to eq 0
+          expect(evidence_3.verification_histories.length).to eq 0
         end
 
         it 'should update the invalid applicants aasm_states to attested' do
-          evidence = applicant.esi_evidence
+          evidence = applicant_1.esi_evidence
           expect(evidence.aasm_state).to eq "attested"
         end
       end
 
       context 'on income evidence' do
         before do
-          applicant.build_income_evidence(
+          applicant_1.build_income_evidence(
             key: :income,
             title: 'Income',
             aasm_state: :pending,
@@ -200,37 +200,37 @@ RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::R
             verification_outstanding: true,
             is_satisfied: false
           )
-          applicant.update(ssn: '000348745')
+          applicant_1.update(ssn: '000348745')
           @result = subject.call(application)
         end
 
-        it 'should fail' do
-          expect(@result).to_not be_success
+        it 'should succeed' do
+          expect(@result).to be_success
         end
 
         it "should update all applicants' verification histories" do
-          evidence1 = applicant.income_evidence
-          evidence2 = applicant_2.income_evidence
-          evidence3 = applicant_3.income_evidence
-          expect(evidence1.verification_histories.length).to eq 1
-          expect(evidence2.verification_histories.length).to eq 1
-          expect(evidence3.verification_histories.length).to eq 1
+          evidence_1 = applicant_1.income_evidence
+          evidence_2 = applicant_2.income_evidence
+          evidence_3 = applicant_3.income_evidence
+          expect(evidence_1.verification_histories.length).to eq 1
+          expect(evidence_2.verification_histories.length).to eq 1
+          expect(evidence_3.verification_histories.length).to eq 1
         end
 
         it 'should update the aasm_states to negative_response_received' do
-          evidence1 = applicant.income_evidence
-          evidence2 = applicant_2.income_evidence
-          evidence3 = applicant_3.income_evidence
-          expect(evidence1.aasm_state).to eq "negative_response_received"
-          expect(evidence2.aasm_state).to eq "negative_response_received"
-          expect(evidence3.aasm_state).to eq "negative_response_received"
+          evidence_1 = applicant_1.income_evidence
+          evidence_2 = applicant_2.income_evidence
+          evidence_3 = applicant_3.income_evidence
+          expect(evidence_1.aasm_state).to eq "negative_response_received"
+          expect(evidence_2.aasm_state).to eq "negative_response_received"
+          expect(evidence_3.aasm_state).to eq "negative_response_received"
         end
       end
     end
 
     context 'with ALL invalid applicants' do
       before do
-        applicant.update(ssn: '000348745')
+        applicant_1.update(ssn: '000348745')
         applicant_2.update(ssn: '000348746')
         applicant_3.update(ssn: '000348747')
         @result = subject.call(application)
