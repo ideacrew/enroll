c4970795089a8116523e4cb32f134b678d020bc8
Trey Evans <lewis.r.evans@gmail.com>
2023-09-28 12:53:43 -0400

    Correcting missing invocations of VLP services at the wrong time.

--- app/controllers/insured/family_members_controller.rb
+++ app/controllers/insured/family_members_controller.rb
@@ -4,6 +4,7 @@ class Insured::FamilyMembersController < ApplicationController
   include VlpDoc
   include ApplicationHelper
   include ::L10nHelper
+  include EventSource::Command
 
   before_action :dependent_person_params, only: [:create, :update]
   before_action :set_current_person
@@ -115,6 +116,8 @@ class Insured::FamilyMembersController < ApplicationController
       extended_family_members_count = household.extended_family_coverage_household.coverage_household_members.count
       Rails.logger.info("In FamilyMembersController create action #{params}, #{@family.inspect}") unless active_family_members_count == immediate_household_members_count + extended_family_members_count
       @created = true
+      consumer_role = @dependent.family_member.try(:person).try(:consumer_role)
+      fire_consumer_roles_create(consumer_role) if consumer_role
       respond_to do |format|
         format.html { render 'show' }
         format.js { render 'show' }
@@ -178,6 +181,7 @@ class Insured::FamilyMembersController < ApplicationController
       return
     end
     consumer_role = @dependent.family_member.try(:person).try(:consumer_role)
+    original_applying_for_coverage = consumer_role.is_applying_coverage
     if @address_errors.blank? && @dependent.update_attributes(dependent_person_params[:dependent]) && update_vlp_documents(consumer_role, 'dependent', @dependent)
       active_family_members_count = @family.active_family_members.count
       household = @family.active_household
@@ -191,6 +195,7 @@ class Insured::FamilyMembersController < ApplicationController
           params[:dependent][:is_applying_coverage]
         )
       end
+      fire_consumer_roles_update(consumer_role, original_applying_for_coverage)
       respond_to do |format|
         format.html { render 'show' }
         format.js { render 'show' }
@@ -256,6 +261,35 @@ class Insured::FamilyMembersController < ApplicationController
       format.html
       format.js
     end
+  end
+
+  # These methods are a fix to make sure that any associated events are
+  # fired correctly.
+  # It's placed here because:
+  #   1. Embedded documents can frequently change the state of consumer
+  #      roles, but those writes/changes can happen after the consumer
+  #      role itself is updated.
+  #   2. If we place the event broadcast into the embedded document itself,
+  #      the result is a tremendous amount of thrashing when this update
+  #      should actually be considered atomic.
+  #   3. We have no desire right now to impact the behaviour of primary family
+  #      members during the entry process.
+  #
+  # All of this logic should be refactored into the correct single operation
+  # when this controller is corrected into the clean code pattern.
+
+  def fire_consumer_roles_create(consumer_role)
+    event = event('events.individual.consumer_roles.created', attributes: { gid: consumer_role.to_global_id.uri })
+    event.success.publish if event.success?
+  rescue StandardError => e
+    Rails.logger.error { "Couldn't generate consumer role create event due to #{e.backtrace}" }
+  end
+
+  def fire_consumer_roles_update(consumer_role, original_applying_for_coverage)
+    event = event('events.individual.consumer_roles.updated', attributes: { gid: consumer_role.to_global_id.uri, previous: {is_applying_coverage: original_applying_for_coverage} })
+    event.success.publish if event.success?
+  rescue StandardError => e
+    Rails.logger.error { "Couldn't generate consumer role updated event due to #{e.backtrace}" }
   end
 
   private
