db85a74b1dca68a58711bdead268c412137b63a2
rahul <rahul.gudimetla@ideacrew.com>
2023-10-06 09:41:24 -0500

    fix specs

--- spec/factories/benefit_group_assignments.rb
+++ spec/factories/benefit_group_assignments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :benefit_group_assignment do
     benefit_group
-    start_on { benefit_group.plan_year.start_on }
+    start_on { benefit_group.benefit_application.start_on }
 
     trait :coverage_selected do
       hbx_enrollment
--- spec/factories/benefit_sponsors_benefit_group_assignments.rb
+++ spec/factories/benefit_sponsors_benefit_group_assignments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :benefit_sponsors_benefit_group_assignment, class: "BenefitGroupAssignment" do
     association :benefit_group, factory: :benefit_sponsors_benefit_packages_benefit_package, strategy: :build
-    start_on { benefit_group.plan_year.start_on }
+    start_on { benefit_group.benefit_application.start_on }
     trait :coverage_selected do
       hbx_enrollment { FactoryBot.create(:hbx_enrollment, :with_primary_family, :with_enrollment_members) }
     end
