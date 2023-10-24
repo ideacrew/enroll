b42b80e5d25a67f6c9a46d4e777c8ee2a05bc6dd
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-10-24 12:12:38 -0400

    rename method name

--- app/controllers/insured/plan_shoppings_controller.rb
+++ app/controllers/insured/plan_shoppings_controller.rb
@@ -603,22 +603,20 @@ class Insured::PlanShoppingsController < ApplicationController
   def set_elected_aptc_by_params(elected_aptc)
     return if session[:elected_aptc].to_d == elected_aptc.to_d
     aptc = if EnrollRegistry.feature_enabled?(:temporary_configuration_enable_multi_tax_household_feature)
-             calcuate_aptc(elected_aptc)
+             recalcuate_elected_aptc(elected_aptc)
            else
              elected_aptc.to_f
            end
     session[:elected_aptc] = aptc
   end
 
-  def calcuate_aptc(elected_aptc)
-    if session[:max_aptc].to_f.zero?
-      0.0
-    else
-      default_aptc_percentage = EnrollRegistry[:enroll_app].setting(:default_aptc_percentage).item
-      percentage = [elected_aptc.to_f / session[:max_aptc], default_aptc_percentage.to_f / 100].min
-      max_aptc = ::Operations::PremiumCredits::FindAptc.new.call({ hbx_enrollment: @enrollment, effective_on: @enrollment.effective_on }).value!.to_f
-      percentage * max_aptc
-    end
+  def recalcuate_elected_aptc(elected_aptc)
+    return 0.0 if session[:max_aptc].to_f.zero?
+
+    default_aptc_percentage = EnrollRegistry[:enroll_app].setting(:default_aptc_percentage).item
+    percentage = [elected_aptc.to_f / session[:max_aptc], default_aptc_percentage.to_f / 100].min
+    max_aptc = ::Operations::PremiumCredits::FindAptc.new.call({ hbx_enrollment: @enrollment, effective_on: @enrollment.effective_on }).value!.to_f
+    percentage * max_aptc
   end
 
   def validate_rating_address
