7c6ea3ae414d099eb5683604b4f50d8025be4fb0
Nikhil S <nikhilks219@gmail.com>
2023-11-13 11:31:38 -0500

    handling error message and rendering it properly (#3213)
    * handling error message and rendering it properly
    
    * adding translation for flash message
    
    ---------
    
    Co-authored-by: Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>

--- db/seedfiles/translations/en/cca/employer.rb
+++ db/seedfiles/translations/en/cca/employer.rb
@@ -24,5 +24,7 @@ EMPLOYER_TRANSLATIONS = {
   :'en.employers.employer_profiles.welcome.submit_verification_documents' => "Submit verification documents",
   :'en.employers.employer_profiles.welcome.submit_verification_documents.content' => "Before you can complete your enrollment, you’ll need to submit proof that your group is eligible to shop through Health Connector for Business. If you haven’t submitted your documentation yet, please review our list of required documents, then <a href='%{documents}'>upload in your account</a>.",
   :'en.employers.employer_profiles.welcome.complete_your_employee_roster' => "Complete your Employee Roster and choose a plan",
-  :'en.employers.employer_profiles.welcome.complete_your_employee_roster.content_html' => "To complete your Employee Roster, select the Employees tab and follow the instructions for adding employees to your group.</br></br>To work on choosing your plan, select the Benefits tab to get started."
+  :'en.employers.employer_profiles.welcome.complete_your_employee_roster.content_html' => "To complete your Employee Roster, select the Employees tab and follow the instructions for adding employees to your group.</br></br>To work on choosing your plan, select the Benefits tab to get started.",
+  :'en.employers.employer_profiles.ce_bulk_upload_success_message' => "File uploaded in s3, will process it on the background",
+  :'en.employers.employer_profiles.ce_bulk_upload_error_message' => "There was an error on the file upload in S3, please try again"
 }.freeze
