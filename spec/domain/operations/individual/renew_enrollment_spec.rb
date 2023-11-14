4dc44e1666f6f848006bd89863aa82bc07c42a7e
Michael Karamanov <mdkaramanov@gmail.com>
2023-11-14 14:55:50 -0500

    prevent applying aptc on catastrophic renewal (#3322)
    * prevent applying aptc on catastrophic renewal
    
    * add tax household in spec set up

--- spec/domain/operations/individual/renew_enrollment_spec.rb
+++ spec/domain/operations/individual/renew_enrollment_spec.rb
@@ -278,6 +278,20 @@ RSpec.describe Operations::Individual::RenewEnrollment, type: :model, dbclean: :
       end
     end
 
+    context 'enrollment product is catastrophic' do
+      before do
+        tax_household.update_attributes!(effective_starting_on: next_year_date.beginning_of_year)
+        tax_household.tax_household_members.first.update_attributes!(applicant_id: family_member.id)
+        product.update_attributes!(metal_level_kind: 'catastrophic')
+        @result = subject.call(hbx_enrollment: enrollment, effective_on: effective_on)
+      end
+
+      it 'should not apply aptc values to the renewal enrollment' do
+        expect(@result.success.applied_aptc_amount).to be_zero
+        expect(@result.success.elected_aptc_pct).to be_zero
+      end
+    end
+
     context 'with an expired enrollment by aasm state' do
       before :each do
         enrollment.expire_coverage!
