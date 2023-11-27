4ea5079b7dcdad77b90469b90eb862662a2b91fb
Raghu Ram <raghuramg83@gmail.com>
2023-11-27 08:01:29 -0500

    convert audit event log model to resource specific event logs

---
+++ app/models/concerns/event_log.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+# Event Log concern for storing events
+module EventLog
+  extend ActiveSupport::Concern
+  include GlobalID::Identification
+
+  included do
+    SEVERITY = %i[debug info notice warn error critical alert emerg].freeze
+    LOG_LEVELS = %i[debug info warn error fatal unknown].freeze
+    EVENT_CATEGORIES = %i[osse_eligibility password_change].freeze
+
+    field :subject_gid, type: String
+    field :correlation_id, type: String
+    field :event_category, type: Symbol
+    field :session_id, type: String # belongs_to :session or embed document?
+
+    # convert this to association
+    # belongs_to :account, class_name: "User", optional: false
+    field :account_id, type: String # TODO: we should seed system account
+    field :host_id, type: String
+
+    field :trigger, type: String
+    field :response, type: String
+    field :log_level, type: Symbol
+    field :severity, type: Symbol
+    field :event_time, type: DateTime
+    field :tags, type: Array
+
+    index({ subject_gid: 1 })
+    index({ event_category: 1 })
+    index({ session_id: 1 })
+    index({ account_id: 1 })
+    index({ host_id: 1 })
+    index({ trigger: 1 })
+    index({ severity: 1 })
+    index({ log_level: 1 })
+    index({ event_time: 1 })
+
+    scope :by_subject, ->(subject_id) { where(subject_gid: /#{subject_id}/i) }
+    scope :by_event_category, ->(category) { where(event_category: category) }
+    scope :by_session, ->(session_id) { where(session_id: session_id) }
+    scope :by_account, ->(account_id) { where(account_id: account_id) }
+    scope :by_log_level, ->(log_level) { where(log_level: log_level) }
+    scope :by_severity, ->(severity) { where(severity: severity) }
+    scope :by_host, ->(host_id) { where(host_id: host_id) }
+    scope :by_trigger, ->(trigger) { where(trigger: trigger) }
+    scope :events_during,
+          lambda { |time_period|
+            where(
+              :event_time.gte => time_period.min,
+              :event_time.lte => time_period.max
+            )
+          }
+  end
+
+  class_methods do
+    def store(data)
+      mutex = Mutex.new
+
+      mutex.synchronize do
+        set_collection_name(collection_name(data))
+        record = self.new(data)
+        record.save
+      end
+    end
+
+    def find(query)
+      mutex = Mutex.new
+
+      mutex.synchronize do
+        set_collection_name(collection_name(query))
+        where(query)
+      end
+    end
+
+    def set_collection_name(collection_name)
+      store_in collection: collection_name
+    end
+
+    def collection_name(options)
+      raise "subject_gid must be passed" unless options[:subject_gid]
+      subject = GlobalID::Locator.locate(options[:subject_gid])
+      subject.class.collection_name.to_s + "_audit_log_events"
+    end
+  end
+end
--- spec/factories/person_event_logs.rb
+++ spec/factories/person_event_logs.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :audit_log_event, class: 'AuditLogEvent' do
+  factory :person_event_log, class: "EventLogs::PersonEventLog" do
     sequence(:subject_gid) do |n|
       "gid://enroll/FamilyMember/6156ad4c0319b0018#{n}"
     end
@@ -10,8 +10,8 @@ FactoryBot.define do
     sequence(:account_id) { |n| "d156ad4c031#{n}" }
     host_id { :enroll }
     event_category { :osse_eligibility }
-    trigger { 'determine_eligibility' }
-    response { 'success' }
+    trigger { "determine_eligibility" }
+    response { "success" }
     log_level { :debug }
     severity { :debug }
     sequence(:event_time) { |n| 2.days.ago + n.minutes }
