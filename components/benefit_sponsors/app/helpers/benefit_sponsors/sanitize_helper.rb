8fe15702d107ee497ebf53ede129ed20a1be9884
HArsHA Ellanki <h_vardhan@yahoo.com>
2023-11-30 10:35:06 -0500

    spec fix

--- components/benefit_sponsors/app/helpers/benefit_sponsors/sanitize_helper.rb
+++ components/benefit_sponsors/app/helpers/benefit_sponsors/sanitize_helper.rb
@@ -4,6 +4,8 @@ module BenefitSponsors
   # helper to sanite the value
   module SanitizeHelper
     def sanitize(value)
+      return value unless value.is_a?(String)
+
       ActionController::Base.helpers.sanitize(value)
     end
   end
