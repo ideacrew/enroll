618cc0dcd055427a05005574df125baaa4936884
HArsHA Ellanki <h_vardhan@yahoo.com>
2023-11-29 19:26:55 -0500

    rubocop fix

--- components/benefit_sponsors/app/helpers/benefit_sponsors/sanitize_helper.rb
+++ components/benefit_sponsors/app/helpers/benefit_sponsors/sanitize_helper.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
+  # helper to sanite the value
   module SanitizeHelper
     def sanitize(value)
       ActionController::Base.helpers.sanitize(value)
--- components/benefit_sponsors/spec/helpers/benefit_sponsors/sanitize_helper_spec.rb
+++ components/benefit_sponsors/spec/helpers/benefit_sponsors/sanitize_helper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe BenefitSponsors::SanitizeHelper, type: :helper, dbclean: :after_each do
