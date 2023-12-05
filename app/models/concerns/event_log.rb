404954940cc9dd0a0cb3417b5e42c374668971bc
Raghu Ram <raghuramg83@gmail.com>
2023-12-05 10:18:38 -0500

    removed unused fields from event log concern

--- app/models/concerns/event_log.rb
+++ app/models/concerns/event_log.rb
@@ -6,21 +6,17 @@ module EventLog
   include GlobalID::Identification
 
   included do
-    SEVERITY = %i[debug info notice warn error critical alert emerg].freeze
-    LOG_LEVELS = %i[debug info warn error fatal unknown].freeze
     EVENT_CATEGORIES = %i[osse_eligibility password_change].freeze
 
     belongs_to :account, class_name: "User"
     embeds_one :session_detail, class_name: "EventLog::SessionDetail", as: :sessionable
 
     field :subject_gid, type: String
-    # field :correlation_id, type: String
     field :event_category, type: Symbol
+    field :correlation_id, type: String
     field :host_id, type: String
     field :trigger, type: String
-    field :response, type: String
-    field :log_level, type: Symbol
-    field :severity, type: Symbol
+    field :message_id, type: String
     field :event_time, type: DateTime
     field :tags, type: Array
 
@@ -29,15 +25,11 @@ module EventLog
     index({ account_id: 1 })
     index({ host_id: 1 })
     index({ trigger: 1 })
-    index({ severity: 1 })
-    index({ log_level: 1 })
     index({ event_time: 1 })
 
     scope :by_subject, ->(subject_id) { where(subject_gid: /#{subject_id}/i) }
     scope :by_event_category, ->(category) { where(event_category: category) }
     scope :by_account, ->(account_id) { where(account_id: account_id) }
-    scope :by_log_level, ->(log_level) { where(log_level: log_level) }
-    scope :by_severity, ->(severity) { where(severity: severity) }
     scope :by_host, ->(host_id) { where(host_id: host_id) }
     scope :by_trigger, ->(trigger) { where(trigger: trigger) }
     scope :events_during,
@@ -49,30 +41,3 @@ module EventLog
           }
   end
 end
-
-
-
-Event DSL
-----------
-
-:session_id
-:subject 
-
-
-:subject_gid, type: String
-:correlation_id, type: String
-:event_category, type: Symbol
-:session_id, type: String # belongs_to :session or embed document?
-
-# convert this to association
-# belongs_to :account, class_name: "User", optional: false
-:account_id, type: String # TODO: we should seed system account
-:host_id, type: String
-
-:trigger, type: String
-:response, type: String
-:log_level, type: Symbol
-:severity, type: Symbol
-:event_time, type: DateTime
-:tags, type: Array
-
