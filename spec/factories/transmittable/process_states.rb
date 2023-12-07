137571513740a6fa50363c96498315d23602e733
TristanB17 <tristan0bambauer@gmail.com>
2023-12-07 12:08:09 -0700

    reenable local client switch

--- spec/factories/transmittable/process_states.rb
+++ spec/factories/transmittable/process_states.rb
@@ -2,9 +2,12 @@
 
 FactoryBot.define do
   factory :transmittable_process_state, class: "::Transmittable::ProcessState" do
-    event {"initial"}
     state_key {:initial}
     started_at {DateTime.now}
     message {"initial"}
+  end
+
+  before(:create) do |instance|
+    instance.event = "initial"
   end
 end
