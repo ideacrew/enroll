3f563f33c0a86cc8438caea2594b586ff8544d39
mdkaraman <mdkaramanov@gmail.com>
2023-12-08 15:58:46 -0500

    add async layer to expire coverages process

--- app/domain/operations/hbx_enrollments/expire.rb
+++ app/domain/operations/hbx_enrollments/expire.rb
@@ -12,7 +12,6 @@ module Operations
       def call(params)
         enrollment_hbx_id = yield validate(params)
         hbx_enrollment    = yield find_enrollment(enrollment_hbx_id)
-        _valid_expiration = yield validate_expiration(hbx_enrollment)
         result            = yield expire_enrollment(hbx_enrollment)
 
         Success(result)
@@ -22,7 +21,6 @@ module Operations
 
       def validate(params)
         return Failure('Missing enrollment_hbx_id') unless params.key?(:enrollment_hbx_id)
-
         Success(params[:enrollment_hbx_id])
       end
 
@@ -30,22 +28,14 @@ module Operations
         Operations::HbxEnrollments::Find.new.call({hbx_id: enrollment_hbx_id})
       end
 
-      def validate_expiration(enrollment)
-        failures = []
-        failures << "#{enrollment.kind} is not a valid IVL enrollment kind" unless enrollment.market_name == 'Individual'
-        failures << "enrollment does not meet the expiration criteria" unless enrollment.may_expire_coverage?
-        if failures.empty?
-          Success(enrollment)
-        else
-          Failure("Unable to expire enrollment hbx id #{enrollment.hbx_id} - #{failures.join(', ')}")
-        end
-      end
-
       def expire_enrollment(enrollment)
-        result = enrollment.expire_coverage!
-        return Failure("Failed to expire enrollment hbx id #{enrollment.hbx_id}.") unless result
+        return Failure("Failed to expire enrollment hbx id #{enrollment.hbx_id} - #{enrollment.kind} is not a valid IVL enrollment kind") unless enrollment.is_ivl_by_kind?
+
+        enrollment.expire_coverage!
 
         Success("Successfully expired enrollment hbx id #{enrollment.hbx_id}")
+      rescue StandardError => e
+        Failure("Failed to expire enrollment hbx id #{enrollment.hbx_id} - #{e.message}")
       end
     end
   end
--- app/event_source/subscribers/individual/enrollments/expire_coverages_subscriber.rb
+++ app/event_source/subscribers/individual/enrollments/expire_coverages_subscriber.rb
@@ -9,10 +9,25 @@ module Subscribers
 
         subscribe(:on_request) do |delivery_info, _metadata, response|
           @logger = subscriber_logger_for(:on_enroll_individual_enrollments_expire_coverages_request)
+          payload = JSON.parse(response).deep_symbolize_keys
+
+          @logger.info "ExpireCoveragesSubscriber on_request, response: #{payload}"
+          result = Operations::HbxEnrollments::ExpirationHandler.new.call(payload)
+
+          result.success? ? @logger.info(result.value!) : @logger.error(result.failure)
+          ack(delivery_info.delivery_tag)
+        rescue StandardError, SystemStackError => e
+          @logger.error "ExpireCoveragesSubscriber on_request, response: #{response}, error message: #{e.message}, backtrace: #{e.backtrace}"
+          @logger.error "ExpireCoveragesSubscriber on_request, ack: #{response}"
+          ack(delivery_info.delivery_tag)
+        end
+
+        subscribe(:on_expire) do |delivery_info, _metadata, response|
+          @logger = subscriber_logger_for(:on_enroll_individual_enrollments_expire_coverages_expire)
           payload = JSON.parse(response, symbolize_names: true)
           enrollment_hbx_id = payload[:enrollment_hbx_id]
 
-          @logger.info "ExpireCoveragesSubscriber, response: #{payload}"
+          @logger.info "ExpireCoveragesSubscriber on_expire, response: #{payload}"
           @logger.info "------------ Processing enrollment: #{enrollment_hbx_id}, index_id: #{payload[:index_id]} ------------"
           result = Operations::HbxEnrollments::Expire.new.call(payload)
           @logger.info "Processed enrollment: #{enrollment_hbx_id}"
@@ -20,8 +35,8 @@ module Subscribers
           result.success? ? @logger.info(result.value!) : @logger.error(result.failure)
           ack(delivery_info.delivery_tag)
         rescue StandardError, SystemStackError => e
-          @logger.error "ExpireCoveragesSubscriber, payload: #{payload}, error message: #{e.message}, backtrace: #{e.backtrace}"
-          @logger.error "ExpireCoveragesSubscriber, ack: #{payload}"
+          @logger.error "ExpireCoveragesSubscriber on_expire, response: #{response}, error message: #{e.message}, backtrace: #{e.backtrace}"
+          @logger.error "ExpireCoveragesSubscriber on_expire, ack: #{response}"
           ack(delivery_info.delivery_tag)
         end
 
