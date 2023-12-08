7d8299c4ae9ffe72163131b895308b70f08e84e8
Raghu Ram <raghuramg83@gmail.com>
2023-12-08 15:13:12 -0500

    added login_session_id and removed person_id

--- app/models/event_logs/session_detail.rb
+++ app/models/event_logs/session_detail.rb
@@ -8,7 +8,7 @@ module EventLogs
     embedded_in :sessionable, polymorphic: true
 
     field :session_id, type: String
+    field :login_session_id, type: String
     field :portal, type: Symbol
-    field :person_id, type: String
   end
 end
