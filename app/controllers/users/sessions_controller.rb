e5a5002d65a9d536c46beb8618c50a4f5ce7beb0
RyanEddyIC <ryan.eddy@ideacrew.com>
2023-12-08 13:08:23 -0500

    refactor Devise SessionsController#create to use super

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -14,14 +14,12 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-    sign_in(resource_name, resource)
-    set_login_token
-    yield resource if block_given?
-    location = after_sign_in_path_for(resource)
-    flash[:warning] = current_user.get_announcements_by_roles_and_portal(location) if current_user.present?
-    respond_with resource, location: location
+    super do
+      flash.delete(:notice) unless is_flashing_format?
+      set_login_token
+      location = after_sign_in_path_for(resource)
+      flash[:warning] = current_user.get_announcements_by_roles_and_portal(location) if current_user.present?
+    end
   end
 
   def destroy
