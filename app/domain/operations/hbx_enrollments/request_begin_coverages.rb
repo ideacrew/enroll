74c403f472afa85b4f48e4e77621408cd5969cc0
mdkaraman <mdkaramanov@gmail.com>
2023-12-11 17:33:40 -0500

    rubocop

--- app/domain/operations/hbx_enrollments/request_begin_coverages.rb
+++ app/domain/operations/hbx_enrollments/request_begin_coverages.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 module Operations
-    module HbxEnrollments
-      # Publish event to request coverage initiation of all IVL renewal enrollments for the new year
-      class RequestBeginCoverages
-        include EventSource::Command
-        include Dry::Monads[:result, :do]
+  module HbxEnrollments
+    # Publish event to request coverage initiation of all IVL renewal enrollments for the new year
+    class RequestBeginCoverages
+      include EventSource::Command
+      include Dry::Monads[:result, :do]
 
-      # @param [Hash] opts The options to request initiation of all IVL renewal enrollments for the new year
-      # @option opts [Hash] :params
-      # @return [Dry::Monads::Result]
+    # @param [Hash] opts The options to request initiation of all IVL renewal enrollments for the new year
+    # @option opts [Hash] :params
+    # @return [Dry::Monads::Result]
       def call(_params)
         logger         = yield initialize_logger
         query_criteria = yield fetch_query_critiria
@@ -34,18 +34,18 @@ module Operations
       end
 
       def end_on
-        @start_on ||= HbxProfile.current_hbx.benefit_sponsorship.current_benefit_coverage_period.end_on
+        @end_on ||= HbxProfile.current_hbx.benefit_sponsorship.current_benefit_coverage_period.end_on
       end
 
-      # TODO: Remove hard coded aaasm_state and kind values.
-      #       Define constants for them in HbxEnrollment class and use them.
+    # TODO: Remove hard coded aaasm_state and kind values.
+    #       Define constants for them in HbxEnrollment class and use them.
       def fetch_query_critiria
         Success(
-            {
-                "effective_on": { "$gte": start_on, "$lt": end_on },
-                "kind": { "$in": ["individual", "coverall"] },
-                "aasm_state": { "$in": ["auto_renewing", "renewing_coverage_selected"] }
-              }
+          {
+            "effective_on": { "$gte": start_on, "$lt": end_on },
+            "kind": { "$in": ["individual", "coverall"] },
+            "aasm_state": { "$in": ["auto_renewing", "renewing_coverage_selected"] }
+          }
         )
       end
 
@@ -64,6 +64,6 @@ module Operations
         logger.info(msg)
         Success(msg)
       end
-      end
     end
+  end
 end
--- spec/domain/operations/hbx_enrollments/request_begin_coverages_spec.rb
+++ spec/domain/operations/hbx_enrollments/request_begin_coverages_spec.rb
@@ -5,14 +5,22 @@ require 'rails_helper'
 RSpec.describe ::Operations::HbxEnrollments::RequestBeginCoverages, dbclean: :after_each do
 
   let(:family)      { FactoryBot.create(:family, :with_primary_family_member) }
-  let!(:enrollment) { FactoryBot.create(:hbx_enrollment, :individual_unassisted, family: family, aasm_state: "auto_renewing") }
+  let(:enrollment) { FactoryBot.create(:hbx_enrollment, :individual_unassisted, family: family, aasm_state: "auto_renewing") }
+  let(:start_on) { Date.today.beginning_of_year }
+  let(:end_on) { Date.today.end_of_year }
+  let(:event_name) { "events.individual.enrollments.begin_coverages.request" }
+
+  before do
+    allow(HbxProfile).to receive_message_chain(:current_hbx, :benefit_sponsorship, :current_benefit_coverage_period, :start_on).and_return(start_on)
+    allow(HbxProfile).to receive_message_chain(:current_hbx, :benefit_sponsorship, :current_benefit_coverage_period, :end_on).and_return(end_on)
+  end
 
   describe 'success' do
     let(:result) { described_class.new.call({}) }
 
     it 'succeeds with message' do
       expect(result.success?).to be_truthy
-      expect(result.value!).to eq("Successfully published event: #{} to request beginning coverage for all IVL renewal enrollments effective on #{enrollment.start_on}.")
+      expect(result.value!).to eq("Successfully published event: #{event_name} to request beginning coverage for all IVL renewal enrollments effective on #{start_on}.")
     end
   end
 end
