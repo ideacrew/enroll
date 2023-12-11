df29198a4901b4c9676ad4afb1a4c2c82686e589
Raghu Ram <raghuramg83@gmail.com>
2023-12-11 08:25:45 -0500

    removed event_log_message_enabled flag from event

--- app/event_source/events/event_log/created.rb
+++ app/event_source/events/event_log/created.rb
@@ -5,7 +5,6 @@ module Events
       # This class will register created event
     class Created < EventSource::Event
       publisher_path 'publishers.event_log_publisher'
-      event_log_message_enabled true
 
     end
   end
