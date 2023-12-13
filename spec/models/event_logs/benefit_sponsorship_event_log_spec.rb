b27bc4af5ebd5668930e46a99c64f9b2d646aa17
Raghu Ram <raghuramg83@gmail.com>
2023-12-13 15:54:32 -0500

    add persistance models for event logs (#3357)
    * add persistance models for event logs
    
    * removed unused fields from event log concern
    
    * spec fixes
    
    * fixed rubocop errors
    
    * fix for rubocop error
    
    * added login_session_id and removed person_id
    
    ---------
    
    Co-authored-by: Harshavardhan Ellanki <h_vardhan@yahoo.com>

---
+++ spec/models/event_logs/benefit_sponsorship_event_log_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EventLogs::BenefitSponsorshipEventLog, type: :model, dbclean: :around_each do
+  before { DatabaseCleaner.clean }
+
+  describe "benefit sponsorship event log" do
+    context "when sponsorship passed as subject" do
+
+      let(:benefit_sponsorship)  { FactoryBot.create(:benefit_sponsors_benefit_sponsorship, :with_full_package) }
+      let(:user) { FactoryBot.create(:user, identity_verified_date: nil) }
+
+      context ".save" do
+        let(:params) do
+          {
+            account_id: user.id,
+            subject_gid: benefit_sponsorship.to_global_id,
+            correlation_id: "a156ad4c031",
+            host_id: :enroll,
+            event_category: :osse_eligibility,
+            trigger: "determine_eligibility",
+            event_time: DateTime.new
+          }
+        end
+
+        it "should persist event log" do
+          described_class.create(params)
+
+          expect(described_class.count).to eq 1
+        end
+
+        it "should find events from collection" do
+          expect(
+            described_class.where(
+              params.slice(:account_id, :event_category)
+            ).first
+          ).to eq described_class.first
+        end
+      end
+    end
+  end
+end
