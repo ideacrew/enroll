15f7e602e6d6524318f7daf8b539d559cc7c4eeb
Ryan Eddy <44847768+RyanEddyIC@users.noreply.github.com>
2023-12-13 10:26:17 -0500

    force log out for admins with concurrent login sessions (#3364)
    * force log out for admins with concurrent login sessions
    
    * remove flash keep from application controller
    
    * Add check for User class to prevent spec Doubles breaking
    
    * fix rubocop
    
    * Add multi-session Cucumber config and steps
    
    * update devise sessionscontroller#new to use super
    
    * add feature flag prevent_concurrent_sessions
    
    * add feature flagged cucumbers
    
    * fix rspecs
    
    * refactor Devise SessionsController#create to use super

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
 
   ## Devise filters
+  before_action :check_concurrent_sessions
   before_action :require_login, unless: :authentication_not_required?
   before_action :authenticate_user_from_token!
   before_action :authenticate_me!
@@ -105,6 +106,20 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def check_concurrent_sessions
+    return unless EnrollRegistry.feature_enabled?(:prevent_concurrent_sessions) && concurrent_sessions? && current_user.has_hbx_staff_role?
+    flash[:error] = l10n('devise.sessions.signed_out_concurrent_session')
+    sign_out current_user
+  end
+
+  def concurrent_sessions?
+    # If the session token differs from the token stored in the db
+    # a new login for this user is detected.
+    # Checking for User class prevents spec breaking for Doubles.
+    # Currently only enabled for admin users.
+    current_user.instance_of?(User) && (session[:login_token] != current_user&.current_login_token)
+  end
 
   def redirect_if_prod
     redirect_to root_path, :flash => { :error => "Unable to run seeds on prod environment." } unless ENV['ENROLL_REVIEW_ENVIRONMENT'] == 'true' || !Rails.env.production?
