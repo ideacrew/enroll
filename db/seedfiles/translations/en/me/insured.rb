aa5c59da3b0e3fa8c0e7b02a497d7dbd58d312c7
rahulgudim <118293805+rahulgudim@users.noreply.github.com>
2023-12-18 15:02:56 -0600

    clarify valid verification document types to user (#3383)
    * clarify valid verification document types to user
    
    * move text to translations
    
    * add translation to spec

--- db/seedfiles/translations/en/me/insured.rb
+++ db/seedfiles/translations/en/me/insured.rb
@@ -581,5 +581,11 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.eligible' => "Eligible",
   :'en.ineligible' => "Ineligible",
   :'en.your_tax_credit' => "Your Tax Credit",
-  :'en.your_tax_credit_for_hc4cc' => "Your Tax Credit: HC4CC eligible households must use at least 85 percent of the credit."
+  :'en.your_tax_credit_for_hc4cc' => "Your Tax Credit: HC4CC eligible households must use at least 85 percent of the credit.",
+  :'en.person_match_error_message' => "%{first_name} %{last_name} is already affiliated with another account.",
+  :'en.upload_documents' => "Upload Documents",
+  :'en.insured.verification_doc_file_error' => "Unable to upload file. Acceptable file types - PDF, JPEG, PNG, and GIF and ensure it does not exceed 100 MB.",
+  :'en.insured.verification_acceptable_file_types_text' => "Acceptable file types:",
+  :'en.insured.verification_acceptable_file_types' => "PDF, JPEG, PNG, and GIF",
+  :'en.insured.verification_max_file_size' => "File should not exceed 100 MB"
 }.freeze
