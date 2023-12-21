cc5775be9e107969dceeba77f5d3db1218ff8d40
RyanEddyIC <ryan.eddy@ideacrew.com>
2023-12-21 11:21:25 -0500

    component spec fix again

--- components/financial_assistance/spec/dummy/app/models/user.rb
+++ components/financial_assistance/spec/dummy/app/models/user.rb
@@ -23,7 +23,7 @@ class User
   validates_uniqueness_of :email,:case_sensitive => false
   validates_presence_of     :password, if: :password_required?
   validates_confirmation_of :password, if: :password_required?
-  validates_length_of       :password, within: User.configured_password_length, allow_blank: true
+  validates_length_of       :password, within: configured_password_length, allow_blank: true
   validates_format_of :email, with: Devise.email_regexp, allow_blank: true, :message => "(optional) is invalid"
 
   def oim_id_rules
@@ -157,7 +157,7 @@ class User
     end
   end
 
-  def self.configured_password_length
+  def configured_password_length
     default_min, default_max = EnrollRegistry[:enroll_app].setting(:default_password_length_range).item.split("..").map(&:to_i)
     return Range.new(default_min, default_max) unless EnrollRegistry.feature_enabled?(:strong_password_length)
     Devise.password_length
