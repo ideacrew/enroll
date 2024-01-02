f2cd62e424070aa76d92d33f01efb6abb028af5c
Jacob Kagon <jacob@kagon.net>
2024-01-02 09:57:12 -0800

    add scenario for leap year

--- components/financial_assistance/spec/domain/financial_assistance/operations/applicant/calculate_and_persist_net_annual_income_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applicant/calculate_and_persist_net_annual_income_spec.rb
@@ -128,12 +128,22 @@ RSpec.describe FinancialAssistance::Operations::Applicant::CalculateAndPersistNe
         applicant.incomes << income
       end
 
-      it "should calculate net_annual_income correctly" do
+      it "should calculate net_annual_income correctly in non-leap year" do
+        unless Date.gregorian_leap?(application.assistance_year)
+          result = subject.call(params)
+          expect(result.success).to eq applicant
+          expect(applicant.net_annual_income.to_f.ceil).to eq 7_003
+        end
+      end
+
+    it "should calculate net_annual_income correctly in a leap year" do
+      if Date.gregorian_leap?(application.assistance_year)
         result = subject.call(params)
         expect(result.success).to eq applicant
-        expect(applicant.net_annual_income.to_f.ceil).to eq 7_003
+        expect(applicant.net_annual_income.to_f.ceil).to eq 7_017
       end
     end
+  end
 
     context 'Income and deductions have start date in future year and end date in future year and frequency kind is daily' do
       let(:params) do
