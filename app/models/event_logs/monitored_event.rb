494b86a606d9e316a8ebb42f5d3d7d100860bd71
rahulgudim <118293805+rahulgudim@users.noreply.github.com>
2024-01-04 14:40:34 -0600

    add audit log ui for shop and ivl (#3438)
    * add persistance models for event logs
    
    * removed unused fields from event log concern
    
    * spec fixes
    
    * fixed rubocop errors
    
    * fix for rubocop error
    
    * added login_session_id and removed person_id
    
    * add operations to store event logs
    
    * updated event log store/create operation steps
    
    * add specs for event log store/create operations
    
    * updated gemfile references for aca entities
    
    * allow access for session details
    
    * update gemfile and lock file for aca_entities
    
    * rubocop updates
    
    * updates for event log create operation
    
    * rubocop fixes
    
    * added spec to test session concern
    
    * add monitored event model
    
    * publish eligibility event upon receiving osse eligibility
    
    * update resource specific domain or model class fetch logic
    
    * add feature flag for event logging
    
    * add feature check for store operation
    
    * updated operations to build params from headers
    
    * added trackable event class to build and publish event log compatiable event
    
    * update event log operations to pass hbx id
    
    * persist event_name under event log
    
    * spec fixes
    
    * add global id to eligibility
    
    * updates for ivl osse eligibility operation
    
    * convert trackable event as an operation
    
    * removed market_kind, trigger from event log
    
    * added events for SHOP and IVL osse eligibilities
    
    * added publishers for ivl and shop osse events
    
    * updated event log operations and models as per new naming convention
    
    * rubocop fixes
    
    * add benefit_sponsors dummy operations/models for event logs
    
    * code fixes for monitored event fields
    
    * WIP: Audit Log UI
    
    * add more files
    
    * add Ajax calls
    
    * add few more js files
    
    * audit Ui changes
    
    * mock data
    
    * refactor code to move to main app
    
    * fix employer navigation
    
    * updated gemfile and gemfile.lock
    
    * added env variable for event logging
    
    * spec fixes for event log operations
    
    * Add disabled properties to buton elements
    
    * Add main.css
    
    * Add Audit log feature in IVL flow
    
    * Update export file
    
    * updated event source gem sha
    
    * Order by desc and css changes
    
    * rubocop fixes
    
    * update event_source, aca_entities to trunk
    
    * auto build feature branch on new changes
    
    * update dummy user model for benefit sponsors
    
    * rubocop fix
    
    * more rubocop fixes
    
    * add event log concern to benefit sponsors spec dummy folder
    
    * Add tests for controller and model
    
    * Add feature specs
    
    * enable audit log env
    
    * fix hbx_id
    
    * fix initial ajax request
    
    * fix translations
    
    * remove cucumber tests for now
    
    * fix event log yml
    
    * fix rubocop errors
    
    * sanitize params
    
    * move to translations
    
    * fix rubocop
    
    ---------
    
    Signed-off-by: Raghu Ram <raghuramg83@gmail.com>
    Co-authored-by: Raghu Ram <raghuramg83@gmail.com>
    Co-authored-by: Harshavardhan Ellanki <h_vardhan@yahoo.com>
    Co-authored-by: kvootla <vootla.krishna@gmail.com>

--- app/models/event_logs/monitored_event.rb
+++ app/models/event_logs/monitored_event.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module EventLogs
+  # Model for Event Logs
   class MonitoredEvent
     include Mongoid::Document
     include Mongoid::Timestamps
@@ -20,5 +21,36 @@ module EventLogs
     index({ event_category: 1 })
     index({ event_time: 1 })
     index({ login_session_id: 1 })
+
+    def self.get_category_options(subject_hbx_id = nil)
+      if subject_hbx_id.present?
+        where(subject_hbx_id: subject_hbx_id).pluck(:event_category).uniq
+      else
+        pluck(:event_category).uniq
+      end
+    end
+
+    def self.fetch_event_logs(params)
+      query = {}
+      %w[subject_hbx_id event_category].each do |param|
+        query[param.to_sym] = params[param.to_sym] if params[param.to_sym].present?
+      end
+
+      query["$or"] = [{ account_hbx_id: params[:account] }, { account_username: params[:account] }] if params[:account].present?
+
+      if params[:event_start_date].present?
+        start_date = params[:event_start_date].to_date
+        query[:event_time] ||= {}
+        query[:event_time].merge!({ "$gte" => start_date.beginning_of_day })
+      end
+
+      if params[:event_end_date].present?
+        end_date = params[:event_end_date].to_date
+        query[:event_time] ||= {}
+        query[:event_time].merge!({ "$lte" => end_date.end_of_day })
+      end
+      query.present? ? EventLogs::MonitoredEvent.where(query).order(:event_time.desc) : EventLogs::MonitoredEvent.all.order(:event_time.desc)
+    end
+
   end
 end
