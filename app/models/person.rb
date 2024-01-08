ec67b001723b2bf4bbb36143fc05fdc03b1e54b4
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-01-08 09:48:57 -0500

    creates broker agency staff role if person matches during broker registration (#3437)
    * creates broker agency staff role if person matches during broker registration
    
    * update dummy model Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1316,6 +1316,24 @@ class Person
     end
   end
 
+  # Creates a new Broker Agency Staff Role with given input params.
+  #
+  # @note This method may raise an exception if the Broker Agency Staff Role is not created successfully.
+  #
+  # @param [Hash] basr_params.
+  #   The acceptable keys: :aasm_state, :benefit_sponsors_broker_agency_profile_id, :reason
+  # @return [Boolean] true if the Broker Agency Staff Role is created successfully.
+  def create_broker_agency_staff_role(basr_params)
+    basr = broker_agency_staff_roles.build(
+      {
+        aasm_state: basr_params[:aasm_state],
+        benefit_sponsors_broker_agency_profile_id: basr_params[:benefit_sponsors_broker_agency_profile_id]
+      }
+    )
+    save!
+    basr
+  end
+
   private
 
   def assign(collection, association)
