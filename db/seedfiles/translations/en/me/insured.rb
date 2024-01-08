32b3f02efecd1177e9ad445a8ccaf9cd089e3630
Alec Turnbull <alecturnbull81@gmail.com>
2024-01-08 10:29:22 -0600

    level aa contrast faa waiting page (#3447)
    * level aa contrast faa waiting page
    
    * trailing whitespace
    
    * improve older tests

--- db/seedfiles/translations/en/me/insured.rb
+++ db/seedfiles/translations/en/me/insured.rb
@@ -590,5 +590,6 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.insured.verification_doc_file_error' => "Unable to upload file. Acceptable file types - PDF, JPEG, PNG, and GIF and ensure it does not exceed 100 MB.",
   :'en.insured.verification_acceptable_file_types_text' => "Acceptable file types:",
   :'en.insured.verification_acceptable_file_types' => "PDF, JPEG, PNG, and GIF",
-  :'en.insured.verification_max_file_size' => "File should not exceed 100 MB"
+  :'en.insured.verification_max_file_size' => "File should not exceed 100 MB",
+  :'en.waiting_for_eligibility' => "Waiting for your eligibility results..."
 }.freeze
