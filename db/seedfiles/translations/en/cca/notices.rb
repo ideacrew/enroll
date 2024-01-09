2f01daab254e9574192742b89203f72b89fe64ce
Michael Karamanov <michael.karamanov@ideacrew.com>
2024-01-09 14:57:43 -0500

    translations refactor of ivl cap notice

---
+++ db/seedfiles/translations/en/cca/notices.rb
@@ -0,0 +1,11 @@
+# CCA Notices Translations
+NOTICES_TRANSLATIONS = {
+  :'en.notices.shared.dear_person' => "Dear %{first_name}:",
+  :'en.notices.shared.questions_or_concerns' => "If you have questions or concerns, we're here to help.",
+  :'en.notices.shared.the_site_short_name_team' => "The %{site_short_name} Team",
+  :'en.notices.ivl_cap.title' => "TAX INFORMATION FOR YOUR CATASTROPHIC HEALTH PLAN",
+  :'en.notices.ivl_cap.you_are_receiving_this_letter' => "You are receiving this letter because you were enrolled in a catastrophic health plan through %{site_short_name} in %{previous_year}. This plan meets the individual responsibility requirement to have health coverage.",
+  :'en.notices.ivl_cap.federal_law_required' => "",
+  :'en.notices.ivl_cap.you_will_not_receive_any_tax_forms' => "You will not receive any tax forms from %{site_short_name} for your enrollment in a catastrophic health plan because these plans do not qualify for an advance premium tax credit.  Only those enrolled in plans that qualify for an advance premium tax credit receive a tax form from %{site_short_name}.",
+  :'en.notices.ivl_cap.you_may_receive_a_tax_form' => "You may receive a tax form from your health insurance company.  Health insurance companies are encouraged (but not required) to provide a tax form for those enrolled in catastrophic coverage.  Contact your insurance company directly for more information on this tax form.",
+}.freeze
