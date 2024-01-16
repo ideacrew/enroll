a7f5d1801746ed2a91ad5bfedb2f63d7eeabe0f5
TristanB17 <tristan0bambauer@gmail.com>
2024-01-16 12:39:05 -0700

    get value of result prior to passing to PublishCloseCaseRequest

--- app/event_source/subscribers/fdsh_gateway/vlp_verifications_subscriber.rb
+++ app/event_source/subscribers/fdsh_gateway/vlp_verifications_subscriber.rb
@@ -3,12 +3,12 @@
 module Subscribers
   module FdshGateway
   # Subscriber will receive response payload from FDSH gateway
-    class VlpverificationsSubscriber
+    class VlpVerificationsSubscriber
       include EventSource::Logging
       include ::EventSource::Subscriber[amqp: 'fdsh.eligibilities.vlp']
 
       subscribe(:on_initial_verification_complete) do |delivery_info, metadata, response|
-        logger.info "Vlp::VlpverificationsSubscriber: invoked on_initial_verification_complete with delivery_info: #{delivery_info.inspect}, response: #{response.inspect}"
+        logger.info "Vlp::VlpVerificationsSubscriber: invoked on_initial_verification_complete with delivery_info: #{delivery_info.inspect}, response: #{response.inspect}"
         payload = JSON.parse(response, :symbolize_names => true)
 
         verification_payload = { person_hbx_id: metadata.correlation_id, metadata: metadata, response: payload }
@@ -16,17 +16,17 @@ module Subscribers
         result = Operations::Fdsh::Vlp::H92::InitialResponseProcessor.new.call(verification_payload)
 
         if result.success?
-          logger.info "Vlp::VlpverificationsSubscriber: on_initial_verification_complete acked with success: #{result.success}"
+          logger.info "Vlp::VlpVerificationsSubscriber: on_initial_verification_complete acked with success: #{result.success}"
           correlation_id = metadata.correlation_id
-          Operations::Fdsh::Vlp::Rx142::CloseCase::PublishCloseCaseRequest.new.call(result, correlation_id) if EnrollRegistry.feature_enabled?(:send_close_case_request)
+          Operations::Fdsh::Vlp::Rx142::CloseCase::PublishCloseCaseRequest.new.call(result.value!, correlation_id) if EnrollRegistry.feature_enabled?(:send_close_case_request)
         else
           errors = result.failure&.errors&.to_h
-          logger.info "Vlp::VlpverificationsSubscriber: on_initial_verification_complete acked with failure, errors: #{errors}"
+          logger.info "Vlp::VlpVerificationsSubscriber: on_initial_verification_complete acked with failure, errors: #{errors}"
         end
         ack(delivery_info.delivery_tag)
       rescue StandardError => e
         ack(delivery_info.delivery_tag)
-        logger.error "Vlp::VlpverificationsSubscriber: on_initial_verification_complete error_message: #{e.message}, backtrace: #{e.backtrace}"
+        logger.error "Vlp::VlpVerificationsSubscriber: on_initial_verification_complete error_message: #{e.message}, backtrace: #{e.backtrace}"
       end
     end
   end
