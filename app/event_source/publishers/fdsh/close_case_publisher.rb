ccf93e703f3b3c67eebd989b939b923d2c766d33
TristanB17 <tristan0bambauer@gmail.com>
2024-01-17 23:35:24 -0700

    readd fdsh layer module to publisher

--- app/event_source/publishers/fdsh/close_case_publisher.rb
+++ app/event_source/publishers/fdsh/close_case_publisher.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module Publishers
-  # Publisher will send a close case request to cms
-  class CloseCasePublisher
-    include ::EventSource::Publisher[amqp: 'fdsh.close_case_requests']
+  module Fdsh
+    # Publisher will send a close case request to cms
+    class CloseCasePublisher
+      include ::EventSource::Publisher[amqp: 'fdsh.close_case_requests']
 
-    register_event 'close_case_requested'
+      register_event 'close_case_requested'
+    end
   end
 end
