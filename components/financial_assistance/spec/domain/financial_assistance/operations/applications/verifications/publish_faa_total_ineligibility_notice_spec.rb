7cc9d6e0db09d89debbf2e783f3792c05caee95f
Marco (Polo) Ornelas <marco.ornelas@ideacrew.com>
2024-01-22 09:36:01 -0500

    trying to fix includes on rspec

--- components/financial_assistance/spec/domain/financial_assistance/operations/applications/verifications/publish_faa_total_ineligibility_notice_spec.rb
+++ components/financial_assistance/spec/domain/financial_assistance/operations/applications/verifications/publish_faa_total_ineligibility_notice_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require "#{FinancialAssistance::Engine.root}/spec/shared_examples/medicaid_gateway/test_case_d_response"
 
 RSpec.describe ::FinancialAssistance::Operations::Applications::Verifications::PublishFaaTotalIneligibilityNotice, dbclean: :after_each do
   include Dry::Monads[:result, :do]
