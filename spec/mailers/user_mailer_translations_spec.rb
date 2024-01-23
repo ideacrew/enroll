439f2e29dccffdf62e06310244f5b97c28ff7551
Trey Evans <lewis.r.evans@gmail.com>
2024-01-23 09:24:45 -0500

    CR-95: Fix 'Login' email text for brokers and broker staff.

--- spec/mailers/user_mailer_translations_spec.rb
+++ spec/mailers/user_mailer_translations_spec.rb
@@ -3,16 +3,32 @@
 require 'rails_helper'
 
 describe "translations for: en.user_mailer" do
-  describe "the user_mailer.broker_or_broker_staff_linked_notification_email.full_text translation" do
-    let(:key) { "user_mailer.broker_or_broker_staff_linked_notification_email.full_text" }
+  describe "the user_mailer.broker_linked_notification_email.full_text translation" do
+    let(:key) { "user_mailer.broker_linked_notification_email.full_text" }
 
     it "exists" do
       expect(I18n.exists?(key)).to be_truthy
     end
   end
 
-  describe "the user_mailer.broker_or_broker_staff_linked_notification_email.subject translation" do
-    let(:key) { "user_mailer.broker_or_broker_staff_linked_notification_email.subject" }
+  describe "the user_mailer.broker_linked_notification_email.subject translation" do
+    let(:key) { "user_mailer.broker_linked_notification_email.subject" }
+
+    it "exists" do
+      expect(I18n.exists?(key)).to be_truthy
+    end
+  end
+
+  describe "the user_mailer.broker_staff_linked_notification_email.full_text translation" do
+    let(:key) { "user_mailer.broker_staff_linked_notification_email.full_text" }
+
+    it "exists" do
+      expect(I18n.exists?(key)).to be_truthy
+    end
+  end
+
+  describe "the user_mailer.broker_staff_linked_notification_email.subject translation" do
+    let(:key) { "user_mailer.broker_staff_linked_notification_email.subject" }
 
     it "exists" do
       expect(I18n.exists?(key)).to be_truthy
