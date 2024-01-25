c153d16dfc55421bf71573043a5e0cbe8682bd5a
Jay Reddy <jayanth.rordev@gmail.com>
2024-01-25 15:00:39 -0500

    latest updates

--- app/controllers/insured/consumer_roles_controller.rb
+++ app/controllers/insured/consumer_roles_controller.rb
@@ -177,7 +177,7 @@ class Insured::ConsumerRolesController < ApplicationController
           end
         end
       end
-    rescue Exception => e
+    rescue Exception
       text = 'There may be an existing Person record (or) An issue with the Person record'
       flash[:warning] = text if @person.errors.present?
       redirect_to search_insured_consumer_role_index_path
