00d4b101c031e3055712cf6c09d10943d961b331
HArsHA Ellanki <h_vardhan@yahoo.com>
2024-01-30 11:50:34 -0500

    removing jwt token from URL

--- app/javascript/legacy/external_applications.js
+++ app/javascript/legacy/external_applications.js
@@ -4,24 +4,12 @@ function setupExternalRedirectedListener() {
   console.log(changeElement);
 
   if (changeElement != null) {
-    var jwtVal = changeElement.getAttribute("data-redirect-jwt");
     var redirUrl = changeElement.getAttribute("data-redirect-url");
+    var jwtVal = changeElement.getAttribute("data-redirect-jwt");
 
     if (redirUrl != null) {
       window.localStorage.setItem("jwt", jwtVal);
-      var headers = new Headers();
-      headers.append('Authorization', 'Bearer ' + jwtVal);
-      
-      fetch(redirUrl, {
-        method: 'GET',
-        headers: headers
-      })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+      window.location.assign(redirUrl);
     }
   }
 }
