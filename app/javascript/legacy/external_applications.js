3e847cc8baf0b143832e9761182f734bc6f53822
HArsHA Ellanki <h_vardhan@yahoo.com>
2024-01-30 14:47:42 -0500

    removed console logs

--- app/javascript/legacy/external_applications.js
+++ app/javascript/legacy/external_applications.js
@@ -1,8 +1,5 @@
 function setupExternalRedirectedListener() {
-  console.log('setupExternalRedirectedListener');
   var changeElement = document.getElementById("external_app_redirection_data");
-  console.log(changeElement);
-
   if (changeElement != null) {
     var redirUrl = changeElement.getAttribute("data-redirect-url");
     var jwtVal = changeElement.getAttribute("data-redirect-jwt");
