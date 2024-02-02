bf108b574be0e1ad63470b83e44588d13efdcd2b
TristanB17 <tristan0bambauer@gmail.com>
2024-02-02 08:49:22 -0700

    add comment to script

--- script/sanitize_plan_info.rb
+++ script/sanitize_plan_info.rb
@@ -1,3 +1,7 @@
+# This script is for the demo environment: it does two things
+# It changes all mentions of "Maine" to "State" in the titles of all instances of BenefitMarkets::Products::Product
+# It can be run with bundle exec rails r script/sanitize_plan_info.rb
+
 plans = BenefitMarkets::Products::Product.where(title: /maine/i)
 
 plans.each do |plan|
