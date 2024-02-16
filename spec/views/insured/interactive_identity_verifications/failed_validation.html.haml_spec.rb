f9f2ebfc57a4d750f48e1b1b38e8c83179345345
Alec Turnbull <alecturnbull81@gmail.com>
2024-02-16 15:42:39 -0600

    fix rspec

--- spec/views/insured/interactive_identity_verifications/failed_validation.html.haml_spec.rb
+++ spec/views/insured/interactive_identity_verifications/failed_validation.html.haml_spec.rb
@@ -12,6 +12,9 @@ describe "insured/interactive_identity_verifications/failed_validation" do
     allow(consumer_role).to receive(:person).and_return person
     allow(person).to receive(:consumer_role).and_return consumer_role
     sign_in current_user
+    allow(view).to receive(:policy_helper).and_return(double('ConsumerRolePolicy', accessible?: true))
+    allow(view).to receive(:pundit_allow).with(consumer_role, :accessible?).and_return(true)
+    allow(view).to receive(:pundit_class).and_return('no-op')
     allow(view).to receive(:policy_helper).and_return(double('FamilyPolicy', updateable?: true))
     allow(view).to receive(:pundit_allow).with(HbxProfile, :can_access_accept_reject_identity_documents?).and_return(true)
     allow(view).to receive(:pundit_allow).with(HbxProfile, :can_delete_identity_application_documents?).and_return(true)
