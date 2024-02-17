fc9a7830658b70ef33b4adc76e1e5dab09d7acd3
Raghu Ram <raghuramg83@gmail.com>
2024-02-17 15:21:15 -0500

    add person policy spec

--- spec/policies/person_policy_spec.rb
+++ spec/policies/person_policy_spec.rb
@@ -8,7 +8,6 @@ describe PersonPolicy do
   let(:hbx_profile) {FactoryBot.create(:hbx_profile)}
   Permission.all.delete
 
-
   context 'hbx_staff_role subroles' do
     it 'hbx_staff' do
       allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_staff))
@@ -39,6 +38,40 @@ describe PersonPolicy do
       allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :developer))
       expect(policy.updateable?).to be false
     end
+  end
 
+  context 'permissions' do
+    subject { described_class }
+
+    permissions :can_access_identity_verifications? do
+
+      let(:person_A){FactoryBot.create(:person)}
+      let!(:user_A){FactoryBot.create(:user, person: person_A)}
+
+      let(:person_B){ FactoryBot.create(:person) }
+      let!(:user_B){ FactoryBot.create(:user, person: person_B) }
+
+      context 'logged in current user not matched with passed record' do
+        context 'when user is not an admin' do
+          it "denies access if current person not matched with passed record" do
+            expect(subject).not_to permit(user_A, person_B)
+          end
+        end
+
+        context 'when user is an admin' do
+          let!(:hbx_staff_role) { FactoryBot.create(:hbx_staff_role, person: person_A)}
+
+          it "grants access if the current user is an admin" do
+            expect(subject).to permit(user_A, person_B)
+          end
+        end
+      end
+
+      context 'logged in person matched with passed record' do
+        it "grants access" do
+          expect(subject).to permit(user_A, person_A)
+        end
+      end
+    end
   end
 end
