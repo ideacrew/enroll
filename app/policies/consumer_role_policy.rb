20f3b878f53cab69b4190d109733fdaa3965c556
Alec Turnbull <alecturnbull81@gmail.com>
2024-02-18 12:35:45 -0600

    confirmed record has access to identity

--- app/policies/consumer_role_policy.rb
+++ app/policies/consumer_role_policy.rb
@@ -54,30 +54,10 @@ class ConsumerRolePolicy < ApplicationPolicy
   # If either are true, then the user has access beyond the RIDP page.
   def ridp_verified?
     user_person = @user&.person
+
     # NOTE: brokers and consumers both require consumer identity to be verified beyond ridp page
-    # TODO:
-    # 1. Need to confirm how this behaves when user is logged in as broker
-    #    If @user returns the broker, then we need to find a way to check the consumer's identity.
-    #    Presumably, consumer role id or consumer's person id is available somewhere in the request.
-    #    If @user is returning the consumer somehow, then the condition below is sufficient.
-    # 2. Confirm how this behaves when user is logged in as hbx admin
-
-    # Leaving commented code below, but it may need to be removed when above is confirmed
-    # broker_staff_roles = user_person.active_broker_staff_roles
-    # broker_role = user_person.broker_role
-    # if broker_role.present? || broker_staff_roles.any?
-    #   can_broker_modify_consumer?(broker_role, broker_staff_roles)
-    # end
-    user_person&.hbx_staff_role&.permission&.modify_family || user_person&.consumer_role&.identity_verified?
-  end
-
-  def can_broker_modify_consumer?(broker, broker_staff)
-    ivl_broker_account = @record&.person.primary_family.active_broker_agency_account
-    return false unless ivl_broker_account.present?
-    return true if broker.present? && ivl_broker_account.benefit_sponsors_broker_agency_profile_id == broker.benefit_sponsors_broker_agency_profile_id
-    staff_account = broker_staff.detect{|staff_role| staff_role.benefit_sponsors_broker_agency_profile_id == ivl_broker_account.benefit_sponsors_broker_agency_profile_id} if broker_staff.present?
-    return false unless staff_account
-    return true if ivl_broker_account.benefit_sponsors_broker_agency_profile_id == staff_account.benefit_sponsors_broker_agency_profile_id
+    # the second condition covers both cases
+    user_person&.hbx_staff_role&.permission&.modify_family || @record&.person&.consumer_role&.identity_verified?
   end
 
   def update?
