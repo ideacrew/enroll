61256ef9240f7bf57550b48d9bc8f8d383411b7d
TristanB17 <tristan0bambauer@gmail.com>
2024-02-27 13:14:12 -0700

    allow data- bootstrap attrs in HTML sanitization

--- lib/html_scrubber_util.rb
+++ lib/html_scrubber_util.rb
@@ -10,7 +10,11 @@ module HtmlScrubberUtil
     ActionController::Base.helpers.sanitize(
       value,
       tags: Loofah::HTML5::SafeList::ACCEPTABLE_ELEMENTS,
-      attributes: Loofah::HTML5::SafeList::ACCEPTABLE_ATTRIBUTES.dup.add("style")
+      attributes: Loofah::HTML5::SafeList::ACCEPTABLE_ATTRIBUTES.dup
+        .add("style")
+        .add("data-toggle")
+        .add("data-slide-to")
+        .add("data-target")
     ).html_safe
   end
 end
