de112e80b515e8b4471c844b7774e46643c4d2c0
TristanB17 <tristan0bambauer@gmail.com>
2024-02-26 21:02:43 -0700

    add fallback in case parent is null

--- app/assets/javascripts/announcement.js
+++ app/assets/javascripts/announcement.js
@@ -1,6 +1,11 @@
-$(document).on('click', 'div.alert-warning a.close', function(){
-  var content = $(this).parent('.alert-warning').text();
-  if (content != undefined && content != ""){
+$(document).on('click', 'div.alert-warning a.close', function() {
+  var content = $(this).parents('.alert-warning').text();
+
+  // The previous^ way to get the modal content returns null for some of the flash notices
+  // If it exists, do nothing, but if it is null or an empty string, try getting text from the direct parent
+  content ? null : content = $(this).parent().text();
+
+  if (content != undefined && content != "") {
     content = content.trim().replace(/^×/, '').trim();
     $.ajax({
       type: "GET",
