2b19a39c02087d23a219725dcbc07361922e6c3c
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-03-06 16:54:24 -0500

    adds YARD documentation for the methods

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -6,10 +6,22 @@ class ApplicationPolicy
     @record = record
   end
 
+  # Checks if the primary person of the given family is an individual market primary family member.
+  # A primary person is considered an individual market primary family member if they have either a consumer role or a resident role,
+  # and the user is the same as the user of the primary person.
+  #
+  # @param family [Family] The family to check.
+  # @return [Boolean] Returns true if the primary person of the family is an individual market primary family member, false otherwise.
   def individual_market_primary_family_member?(family)
     (family.primary_person.consumer_role.present? || family.primary_person.resident_role.present?) && user == family.primary_person.user
   end
 
+  # Checks if the user is associated with an active broker for the given family.
+  # A user is considered associated with an active broker if the broker is not blank, is active,
+  # and matches the broker associated with the active broker agency account of the family.
+  #
+  # @param family [Family] The family to check.
+  # @return [Boolean] Returns true if the user is associated with an active broker for the family, false otherwise.
   def active_associated_family_broker?(family)
     broker = user.person.broker_role
     return false if broker.blank? || !broker.active?
@@ -21,27 +33,51 @@ class ApplicationPolicy
       broker_agency_account.writing_agent_id == broker.id
   end
 
+  # Checks if the primary person of the given family has their identity verified.
+  # For Employee Role and Resident Role, RIDP verification is not required.
+  # Also, roles should not be blocked because of RIDP Verification check.
+  #
+  # @param family [Family] The family to check.
+  # @return [Boolean] Returns true if the primary person of the family has their identity verified or if the consumer role is blank, false otherwise.
   def ridp_verified_primary_person?(family)
     consumer_role = family.primary_person.consumer_role
-    # For Employee Role and Resident Role we don't need to check for RIDP verification.
-    # Also, we should not block the roles because of RIDP Verification check.
     return true if consumer_role.blank?
 
     consumer_role.identity_verified?
   end
 
+  # Checks if the user is an hbx staff admin.
+  # A user is considered an hbx staff admin if they have the permission to modify a family.
+  #
+  # @return [Boolean, nil] Returns true if the user is an hbx staff admin,
+  # false if they are not, or nil if the user or their permissions are not defined.
   def hbx_staff_admin?
     user.person.hbx_staff_role&.permission&.modify_family
   end
 
+  # Checks if the user is an individual market admin.
+  # A user is considered an individual market admin if they are an hbx staff admin.
+  #
+  # @return [Boolean, nil] Returns true if the user is an individual market admin,
+  # false if they are not, or nil if the user or their permissions are not defined.
   def individual_market_admin?
     hbx_staff_admin?
   end
 
+  # Checks if the user is a shop market admin.
+  # A user is considered a shop market admin if they are an hbx staff admin.
+  #
+  # @return [Boolean, nil] Returns true if the user is a shop market admin,
+  # false if they are not, or nil if the user or their permissions are not defined.
   def shop_market_admin?
     hbx_staff_admin?
   end
 
+  # Checks if the user is a fehb market admin.
+  # A user is considered a fehb market admin if they are an hbx staff admin.
+  #
+  # @return [Boolean, nil] Returns true if the user is a fehb market admin,
+  # false if they are not, or nil if the user or their permissions are not defined.
   def fehb_market_admin?
     hbx_staff_admin?
   end
