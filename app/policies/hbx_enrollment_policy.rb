e04594123b3e99b59f73cc43644df9274cb625d9
HArsHA Ellanki <h_vardhan@yahoo.com>
2024-03-11 11:19:50 -0400

    updated initialize methods in individual policies

--- app/policies/hbx_enrollment_policy.rb
+++ app/policies/hbx_enrollment_policy.rb
@@ -2,6 +2,10 @@
 
 # pundit Policies to access HbxEnrollment
 class HbxEnrollmentPolicy < ApplicationPolicy
+  def initialize
+    super
+    @family ||= record.family
+  end
 
   def checkout?
     create?
@@ -45,57 +49,32 @@ class HbxEnrollmentPolicy < ApplicationPolicy
 
   private
 
-  # Returns the family associated with the current enrollment.
-  #
-  # @return [Family] The family associated with the current enrollment.
-  def enrollment_family
-    @enrollment_family ||= record.family
-  end
-
-  def can_create_ivl_enrollment?
-    return true if individual_market_primary_family_member?(enrollment_family)
-    return true if individual_market_admin?
-    return true if active_associated_individual_market_family_broker?(enrollment_family)
-
-    false
-  end
-
-  def can_create_resident_role_enrollment?
-    return true if coverall_market_primary_family_member?(enrollment_family)
-    return true if coverall_market_admin?
-    return true if active_associated_coverall_market_family_broker?(enrollment_family)
-
-    false
-  end
-
-  def can_create_shop_enrollment?
-    return true if shop_market_primary_family_member?(enrollment_family)
-    return true if shop_market_admin?
-    return true if active_associated_shop_market_family_broker?(enrollment_family)
-    return true if active_associated_shop_market_general_agency?(enrollment_family)
-
-    false
-  end
-
-  def can_create_fehb_enrollment?
-    return true if fehb_market_primary_family_member?(enrollment_family)
-    return true if fehb_market_admin?
-    return true if active_associated_fehb_market_family_broker?(enrollment_family)
-    return true if active_associated_fehb_market_general_agency?(enrollment_family)
-
-    false
-  end
+  # # Returns the family associated with the current enrollment.
+  # #
+  # # @return [Family] The family associated with the current enrollment.
+  # def enrollment_family
+  #   @enrollment_family ||= record.family
+  # end
 
   def create?
-    case record.kind
-    when HbxEnrollment::INDIVIDUAL_KIND
-      can_create_ivl_enrollment?
-    when HbxEnrollment::COVERALL_KIND
-      can_create_resident_role_enrollment?
-    when *HbxEnrollment::GROUP_KINDS
-      can_create_shop_enrollment? || can_create_fehb_enrollment?
-    else
-      false
-    end
+    return true if individual_market_primary_family_member?
+    return true if individual_market_admin?
+    return true if active_associated_individual_market_family_broker?
+
+    return true if shop_market_primary_family_member?
+    return true if shop_market_admin?
+    return true if active_associated_shop_market_family_broker?
+    return true if active_associated_shop_market_general_agency?
+
+    return true if fehb_market_primary_family_member?
+    return true if fehb_market_admin?
+    return true if active_associated_fehb_market_family_broker?
+    return true if active_associated_fehb_market_general_agency?
+
+    return true if coverall_market_primary_family_member?
+    return true if coverall_market_admin?
+    return true if active_associated_coverall_market_family_broker?
+
+    false
   end
 end
