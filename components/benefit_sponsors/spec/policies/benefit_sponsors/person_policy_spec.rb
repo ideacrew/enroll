4c8ecb6331c3d86f6d57efecb3b1b7279b05b627
Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>
2024-03-13 19:51:05 -0400

    allow broker staff to delete broker messages (#3657)
    * allow broker staff to delete broker messages
    
    * cleanup person_policy
    
    * add permission check for admin
    
    * spec fix

--- components/benefit_sponsors/spec/policies/benefit_sponsors/person_policy_spec.rb
+++ components/benefit_sponsors/spec/policies/benefit_sponsors/person_policy_spec.rb
@@ -7,11 +7,13 @@ module BenefitSponsors
     context "checks authorization of person role" do
       let!(:site) { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
       let(:organization_with_hbx_profile)  { site.owner_organization }
+      let!(:super_admin_permission) { FactoryBot.create(:permission, :super_admin) }
 
       it "returns true if user is a HBX admin" do
         user_with_hbx_staff_role = FactoryBot.create(:user, :with_hbx_staff_role)
         FactoryBot.create(:person, user: user_with_hbx_staff_role)
         user_with_hbx_staff_role.person.build_hbx_staff_role(hbx_profile_id: organization_with_hbx_profile.hbx_profile.id)
+        user_with_hbx_staff_role.person.hbx_staff_role.permission_id = super_admin_permission.id
         user_with_hbx_staff_role.person.hbx_staff_role.save!
         policy = BenefitSponsors::PersonPolicy.new(user_with_hbx_staff_role, nil)
         expect(policy.can_read_inbox?).to be true
