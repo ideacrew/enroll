83c52e2167713c8c46d0dc66ba3465b80404a695
jayreddy519 <jayanth.rordev@gmail.com>
2024-03-13 12:34:13 -0400

    Broker Staff Pundit Policy - Person Update

--- spec/policies/person_policy_spec.rb
+++ spec/policies/person_policy_spec.rb
@@ -2,80 +2,75 @@
 
 require "rails_helper"
 
-describe PersonPolicy do
+RSpec.describe PersonPolicy, type: :policy do
   context 'with hbx_staff_role' do
-    let(:person){FactoryBot.create(:person, user: user)}
-    let(:user){FactoryBot.create(:user)}
-    let(:hbx_staff_role) { FactoryBot.create(:hbx_staff_role, person: person)}
-    let(:policy){PersonPolicy.new(user,person)}
+    let(:record_person) {FactoryBot.create(:person)}
+    let(:admin_person){FactoryBot.create(:person)}
+    let(:admin_user){FactoryBot.create(:user, person: admin_person)}
+    let(:hbx_staff_role) { FactoryBot.create(:hbx_staff_role, person: admin_person)}
     let(:hbx_profile) {FactoryBot.create(:hbx_profile)}
     Permission.all.delete
 
     context 'allowed to modify? for hbx_staff_role subroles' do
-      it 'hbx_staff' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_staff))
+      let(:policy){PersonPolicy.new(admin_user, record_person)}
+
+      it 'hbx_staff with modify family permission' do
+        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_staff, modify_family: true))
         expect(policy.can_update?).to be true
       end
 
-      it 'hbx_read_only' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_read_only))
-        expect(policy.updateable?).to be true
+      it 'hbx_staff without modify family permission' do
+        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_staff, modify_family: false))
+        expect(policy.can_update?).to be false
       end
 
-      it 'hbx_csr_supervisor' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_supervisor))
-        expect(policy.updateable?).to be true
+      it 'hbx_read_only with modify family permission' do
+        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_read_only, modify_family: true))
+        expect(policy.can_update?).to be true
       end
 
-      it 'hbx_csr_tier2' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_tier2))
-        expect(policy.updateable?).to be true
-      end
-
-      it 'csr_tier1' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_tier1))
-        expect(policy.updateable?).to be true
-      end
-
-      it 'developer' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :developer))
-        expect(policy.updateable?).to be false
+      it 'hbx_read_only without modify family permission' do
+        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_read_only, modify_family: false))
+        expect(policy.can_update?).to be false
       end
     end
 
-    context 'hbx_staff_role subroles' do
-      it 'hbx_staff' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_staff))
-        expect(policy.updateable?).to be true
-      end
+    # updateable? method does not have a proper authorization logic, it is always returning true except for :developer permission
+    # context 'hbx_staff_role subroles' do
+    #   it 'hbx_staff' do
+    #     allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_staff))
+    #     expect(policy.updateable?).to be true
+    #   end
 
-      it 'hbx_read_only' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_read_only))
-        expect(policy.updateable?).to be true
-      end
+    #   it 'hbx_read_only' do
+    #     allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_read_only))
+    #     expect(policy.updateable?).to be true
+    #   end
 
-      it 'hbx_csr_supervisor' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_supervisor))
-        expect(policy.updateable?).to be true
-      end
+    #   it 'hbx_csr_supervisor' do
+    #     allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_supervisor))
+    #     expect(policy.updateable?).to be true
+    #   end
 
-      it 'hbx_csr_tier2' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_tier2))
-        expect(policy.updateable?).to be true
-      end
+    #   it 'hbx_csr_tier2' do
+    #     allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_tier2))
+    #     expect(policy.updateable?).to be true
+    #   end
 
-      it 'csr_tier1' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_tier1))
-        expect(policy.updateable?).to be true
-      end
+    #   it 'csr_tier1' do
+    #     allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :hbx_csr_tier1))
+    #     expect(policy.updateable?).to be true
+    #   end
 
-      it 'developer' do
-        allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :developer))
-        expect(policy.updateable?).to be false
-      end
-    end
+    #   it 'developer' do
+    #     allow(hbx_staff_role).to receive(:permission).and_return(FactoryBot.create(:permission, :developer))
+    #     expect(policy.updateable?).to be false
+    #   end
+    # end
+
 
     context 'permissions' do
+      let(:policy){PersonPolicy.new(admin_user, record_person)}
       subject { described_class }
 
       permissions :can_access_identity_verifications? do
@@ -112,31 +107,12 @@ describe PersonPolicy do
   end
 
   context 'with broker role' do
-    Permission.all.delete
-
-    let(:consumer_role) do
-      FactoryBot.create(:consumer_role)
-    end
-
-    let(:person) do
-      pers = consumer_role.person
-      pers.user = user
-      pers.save!
-      pers
-    end
-
     let(:broker_agency_profile) do
       FactoryBot.create(:benefit_sponsors_organizations_broker_agency_profile)
     end
-
-    let(:user) do
-      FactoryBot.create(:user)
-    end
-
-    let(:existing_broker_staff_role) do
-      person.broker_agency_staff_roles.first
-    end
-
+    let(:broker_agency_staff_role) { FactoryBot.create(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, aasm_state: 'active')}
+    let(:broker_user) {FactoryBot.create(:user, :person => broker_agency_staff_role.person, roles: ['broker_role'])}
+    let(:broker_person) { broker_agency_staff_role.person }
     let(:broker_role) do
       role = BrokerRole.new(
         :broker_agency_profile => broker_agency_profile,
@@ -144,16 +120,34 @@ describe PersonPolicy do
         :npn => "123456789",
         :provider_kind => "broker"
       )
-      person.broker_role = role
-      person.save!
-      person.broker_role
+      broker_person.broker_role = role
+      broker_person.save!
+      broker_person.broker_role
     end
 
-    let(:policy){PersonPolicy.new(user,person)}
+    let(:person) { FactoryBot.create(:person, :with_consumer_role) }
+    let(:user) { FactoryBot.create(:user, person: person) }
+    let!(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person) }
 
-    it 'broker should be able to update' do
-      expect(policy.can_update?).to be true
+    let(:policy){PersonPolicy.new(broker_user, person)}
+
+    context 'authorized broker' do
+      before(:each) do
+        family.broker_agency_accounts << BenefitSponsors::Accounts::BrokerAgencyAccount.new(benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id,
+                                                                                            start_on: Time.now,
+                                                                                            is_active: true)
+        family.reload
+      end
+
+      it 'broker should be able to update' do
+        expect(policy.can_update?).to be true
+      end
     end
 
+    context 'unauthorized broker' do
+      it 'broker should not be able to update' do
+        expect(policy.can_update?).to be false
+      end
+    end
   end
 end
