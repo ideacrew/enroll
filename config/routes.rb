287ed09d3d8b4d6315785154be9a071b75c63aab
vishal kalletla <vishuk1201@gmail.com>
2024-03-13 13:20:20 -0400

    remove unused office location controller routes and related code (#3635)
    * remove unused routes
    
    * remove legacy route
    
    * remove officelocation controllers that are not in use
    
    * comment unused office location partial

--- config/routes.rb
+++ config/routes.rb
@@ -675,7 +675,6 @@ Rails.application.routes.draw do
       get :claim
     end
   end
-  resources :office_locations, only: [:new]
 
   get "document/download/:bucket/:key" => "documents#download", as: :document_download
   get "document/authorized_download/:model/:model_id/:relation/:relation_id" => "documents#authorized_download", as: :authorized_document_download
