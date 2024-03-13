c3329028643cd49a9fa527475ad8b96d58468146
Jacob Kagon <jacob@kagon.net>
2024-03-13 16:36:31 -0400

    allow broker staff to delete broker messages

--- components/benefit_sponsors/app/policies/benefit_sponsors/person_policy.rb
+++ components/benefit_sponsors/app/policies/benefit_sponsors/person_policy.rb
@@ -3,10 +3,56 @@
 module BenefitSponsors
   # Policy for person
   class PersonPolicy < ApplicationPolicy
+    ACCESSABLE_ROLES = %w[hbx_staff_role broker_role active_broker_staff_roles].freeze
+
     def can_read_inbox?
-      return true if user.person.hbx_staff_role
-      return true if (user.person&.broker_role || record.broker_role) && (user.person.id == record.id)
+      return true if can_hbx_staff_modify?
+      return true if can_broker_modify?
       false
+    end
+
+    def can_active_broker_staff_role_modify?
+      person = user&.person
+      return false unless person.active_broker_staff_roles.present?
+      person.active_broker_staff_roles.any? do |broker_staff_role|
+        record&.broker_role&.benefit_sponsors_broker_agency_profile_id == broker_staff_role.benefit_sponsors_broker_agency_profile_id
+      end
+    end
+
+    def can_broker_modify?
+      (has_active_broker_role? || has_active_broker_agency_staff_role?) && matches_broker_agency_profile?(record&.broker_role&.benefit_sponsors_broker_agency_profile_id)
+    end
+
+    def matches_broker_agency_profile?(broker_agency_profile_id)
+      Array(role).any? do |role|
+        role.benefit_sponsors_broker_agency_profile_id == broker_agency_profile_id
+      end
+    end
+
+    def can_hbx_staff_modify?
+      role.is_a?(HbxStaffRole)
+    end
+
+    def has_active_broker_role?
+      role.is_a?(::BrokerRole)
+    end
+
+    def has_active_broker_agency_staff_role?
+      role.any? { |role| role.is_a?(::BrokerAgencyStaffRole) }
+    end
+
+    def role
+      @role ||= find_role
+    end
+
+    def find_role
+      person = user&.person
+      return nil unless person
+      ACCESSABLE_ROLES.detect do |role|
+        return person.send(role) if person.respond_to?(role) && person.send(role)
+      end
+
+      nil
     end
   end
 end
--- components/benefit_sponsors/spec/controllers/benefit_sponsors/inboxes/messages_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/inboxes/messages_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 module BenefitSponsors
@@ -16,7 +18,7 @@ module BenefitSponsors
 
     let(:broker_organization) {FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site)}
     let(:broker_person) { FactoryBot.create(:person, :with_broker_role) }
-    let(:broker_user) { FactoryBot.create(:user, person: broker_person ) }
+    let(:broker_user) { FactoryBot.create(:user, person: broker_person) }
 
     describe "GET show / DELETE destroy" do
       context "redirect show message if user not signed in" do
@@ -67,7 +69,7 @@ module BenefitSponsors
           end
 
           it "should get a notice" do
-            expect(flash[:notice]).to match /Successfully deleted inbox message./
+            expect(flash[:notice]).to match(/Successfully deleted inbox message./)
           end
         end
 
@@ -105,10 +107,52 @@ module BenefitSponsors
 
           it "should get a notice" do
 
-            expect(flash[:notice]).to match /Successfully deleted inbox message./
+            expect(flash[:notice]).to match(/Successfully deleted inbox message./)
           end
         end
       end
+
+      context "for user with broker agency staff role" do
+        let(:broker_staff_person) { FactoryBot.create(:person) }
+        let(:broker_staff_user) { FactoryBot.create(:user, person: broker_staff_person) }
+
+        before do
+          broker_staff_person.broker_agency_staff_roles.create(aasm_state: :active, benefit_sponsors_broker_agency_profile_id: broker_organization.broker_agency_profile.id)
+          broker_person.broker_role.update_attributes!(benefit_sponsors_broker_agency_profile_id: broker_organization.broker_agency_profile.id)
+          @broker_inbox = broker_person.build_inbox
+          @broker_inbox.save!
+          welcome_subject = "Welcome to #{EnrollRegistry[:enroll_app].setting(:short_name).item}"
+          welcome_body = "#{EnrollRegistry[:enroll_app].setting(:short_name).item} is the #{Settings.aca.state_name}'s on-line marketplace to shop, compare, and select health insurance that meets your health needs and budgets."
+          @broker_inbox.messages.create(subject: welcome_subject, body: welcome_body)
+          sign_in broker_staff_user
+        end
+
+        context "show message" do
+          before do
+            get :show, params: {id: broker_person.id, message_id: @broker_inbox.messages.first.id}
+          end
+
+          it "should render show template" do
+            expect(response).to render_template("show")
+          end
+
+          it "should return http success" do
+            expect(response).to have_http_status(:success)
+          end
+        end
+
+        context "delete message" do
+          before do
+            delete :destroy, params: {id: broker_person.id, message_id: @broker_inbox.messages.first.id}, format: :js
+          end
+
+          it "should get a notice" do
+
+            expect(flash[:notice]).to match(/Successfully deleted inbox message./)
+          end
+        end
+      end
+
 
       context "for broker agency profile - from Admin login" do
         before do
@@ -127,7 +171,7 @@ module BenefitSponsors
 
         context "show message" do
           before do
-            get :show, params:{id: broker_person.id, message_id: @broker_inbox.messages.first.id}
+            get :show, params: {id: broker_person.id, message_id: @broker_inbox.messages.first.id}
           end
 
           it "should render show template" do
@@ -141,11 +185,11 @@ module BenefitSponsors
 
         context "delete message" do
           before do
-            delete :destroy, params:{id: broker_person.id, message_id: @broker_inbox.messages.first.id}, format: :js
+            delete :destroy, params: {id: broker_person.id, message_id: @broker_inbox.messages.first.id}, format: :js
           end
 
           it "should get a notice" do
-            expect(flash[:notice]).to match /Successfully deleted inbox message./
+            expect(flash[:notice]).to match(/Successfully deleted inbox message./)
           end
         end
       end
