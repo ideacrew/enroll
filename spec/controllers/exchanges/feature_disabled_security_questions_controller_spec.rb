929b173a435caf92b8513d163d111e8689a18bb9
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-03-14 16:38:55 -0400

    blocks access to the security questions endpoints if feature is disabled (#3668)
    * blocks access to the endpoints if feature is disabled
    
    * add frozen_string_literal to the top

---
+++ spec/controllers/exchanges/feature_disabled_security_questions_controller_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Exchanges::SecurityQuestionsController, dbclean: :after_each do
+  let(:person) { FactoryBot.create(:person) }
+  let(:user) { FactoryBot.create(:user, person: person) }
+
+  before do
+    allow(Settings.aca).to receive(:security_questions).and_return(false)
+    sign_in(user)
+  end
+
+  describe 'GET #index' do
+    context 'security questions feature is disabled' do
+      it 'redirects to root with flash message' do
+        get :index
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq(l10n('exchanges.security_questions.denied_access_message'))
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context 'security questions feature is disabled' do
+      it 'redirects to root with flash message' do
+        get :new
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq(l10n('exchanges.security_questions.denied_access_message'))
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'security questions feature is disabled' do
+      it 'redirects to root with flash message' do
+        post :create, params: {}
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq(l10n('exchanges.security_questions.denied_access_message'))
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    context 'security questions feature is disabled' do
+      it 'redirects to root with flash message' do
+        get :edit, params: { id: 'random_id' }
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq(l10n('exchanges.security_questions.denied_access_message'))
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'security questions feature is disabled' do
+      it 'redirects to root with flash message' do
+        patch :update, params: { id: 'random_id' }
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq(l10n('exchanges.security_questions.denied_access_message'))
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'security questions feature is disabled' do
+      it 'redirects to root with flash message' do
+        delete :destroy, params: { id: 'random_id' }
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq(l10n('exchanges.security_questions.denied_access_message'))
+      end
+    end
+  end
+end
