0516df4824a2643839e754c15aae7a81dddad982
Nisanth Yaganti <yagantinisanth@gmail.com>
2024-03-14 10:36:23 -0400

    correcting syntax on json req and policy update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,8 +80,8 @@ class ApplicationController < ActionController::Base
     policy_name = exception.policy.class.to_s.underscore
 
     flash[:error] = "Access not allowed for #{policy_name}.#{exception.query}, (Pundit policy)"
-      respond_to do |format|
-      format.json { render nothing: true, status: :forbidden }
+    respond_to do |format|
+      format.json { head :forbidden }
       format.html { redirect_to(request.referrer || main_app.root_path)}
       format.js { head :forbidden }
     end
