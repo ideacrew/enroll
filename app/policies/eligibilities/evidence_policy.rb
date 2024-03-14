b462dac9c8a3ff012dbe43ceeb8c9788b70675c8
vkghub <vishuk1201@gmail.com>
2024-03-14 13:12:29 -0400

    authorize hbx staff to access/modify documents of identity not verified consumers

--- app/policies/eligibilities/evidence_policy.rb
+++ app/policies/eligibilities/evidence_policy.rb
@@ -35,16 +35,15 @@ module Eligibilities
     #
     # @note The user is the one who is trying to perform the action. The record_user is the user who owns the record. The record is an instance of Eligibilities::Evidence.
     def allowed_to_modify?
-      return false unless individual_market_role_identity_verified?
-      return true if (current_user == associated_user)
+      return true if current_user == associated_user && individual_market_role_identity_verified?
       return false unless role.present?
       return can_hbx_staff_modify? if role.is_a?(HbxStaffRole)
-      return can_broker_modify? if (has_active_broker_role? || has_active_broker_agency_staff_role?)
+      return can_broker_modify? if has_active_broker_role? || has_active_broker_agency_staff_role?
       false
     end
 
     def individual_market_role_identity_verified?
-      return true if (associated_person.resident_role || associated_person.consumer_role&.identity_verified?)
+      return true if associated_person.resident_role || associated_person.consumer_role&.identity_verified?
       false
     end
 
@@ -71,6 +70,7 @@ module Eligibilities
     end
 
     def matches_individual_market_broker_account?
+      return false unless individual_market_role_identity_verified?
       return false unless associated_family.active_broker_agency_account.present?
       matches_broker_agency_profile?(associated_family.active_broker_agency_account.benefit_sponsors_broker_agency_profile_id)
     end
