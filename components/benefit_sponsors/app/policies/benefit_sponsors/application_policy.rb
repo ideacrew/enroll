bf6632867f85f016dccbbd3a1808971cd5c2c2fc
TristanB17 <tristan0bambauer@gmail.com>
2024-03-14 14:19:56 -0600

    update for rubocop

--- components/benefit_sponsors/app/policies/benefit_sponsors/application_policy.rb
+++ components/benefit_sponsors/app/policies/benefit_sponsors/application_policy.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
+  # Policy used for auth in the BenefitSponsors app when running rspec from a GHA
+  # For the time being there is no way to pass the GHAs without pulling from the BenefitSponsors
+  # All "building blocks" have been select from existing methods in the main app ApplicationPolicy for ease of transition
   class ApplicationPolicy
+
     attr_reader :user, :record
 
     def initialize(user, record)
@@ -22,9 +28,15 @@ module BenefitSponsors
     def account_holder_person
       return @account_holder_person if defined? @account_holder_person
 
-      @account_holder_person = account_holder.person
+      @account_holder_person = account_holder&.person
     end
 
+    def permission
+      return @permission if defined? @permission
+  
+      @permission = hbx_role&.permission
+    end
+  
     def hbx_role
       return @hbx_role if defined? @hbx_role
   
--- components/benefit_sponsors/app/policies/benefit_sponsors/organizations/organization_forms/staff_role_form_policy.rb
+++ components/benefit_sponsors/app/policies/benefit_sponsors/organizations/organization_forms/staff_role_form_policy.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
   module Organizations
     module OrganizationForms
+      # this policy does the basic authentication for all StaffRoleForms
       class StaffRoleFormPolicy < ApplicationPolicy
 
         attr_reader :service
@@ -8,7 +11,7 @@ module BenefitSponsors
         def initialize(user, record)
           super
           @service = BenefitSponsors::Services::StaffRoleService.new(
-              profile_id: record.profile_id
+            profile_id: record.profile_id
           )
         end
 
@@ -41,10 +44,12 @@ module BenefitSponsors
 
         def can_edit?
           reg_service = Services::NewProfileRegistrationService.new(profile_id: record.profile_id)
-          if profile.is_a?(BrokerAgencyProfile)
+          case
+
+          when profile.is_a?(BrokerAgencyProfile)
             # BrokerAgencyProfilePolicy has a different level of access than EmployerProfile and GeneralAgencyProfile
             BenefitSponsors::Organizations::BrokerAgencyProfilePolicy.new(account_holder, profile).access_to_broker_agency_profile?
-          elsif profile.is_a?(GeneralAgencyProfile)
+          when profile.is_a?(GeneralAgencyProfile)
             return true if admin?
             return false if user.person.general_agency_primary_staff.blank?
             return true if Person.staff_for_ga(profile).include?(user.person)
@@ -57,19 +62,20 @@ module BenefitSponsors
 
         private
 
+        # Keep form-specific methods in StaffRoleFormPolicy, afterwards, move others to respective profile policies
         def current_user_is_form_subject?
           # because this form is used by multiple Profile Types, we want to ignore the other types
           return false unless profile.is_a?(BrokerAgencyProfile)
 
           # person_id on the record is stored as a string
-          account_holder&.person.id.to_str == record&.person_id
+          account_holder&.person&.id&.to_str == record&.person_id
         end
 
         def primary_broker_is_form_subject?
           return false unless profile.is_a?(BrokerAgencyProfile)
 
           # person_id on the record is stored as a string
-          profile&.primary_broker_role&.person.id.to_str == record&.person_id
+          profile&.primary_broker_role&.person&.id&.to_str == record&.person_id
         end
       end
     end
