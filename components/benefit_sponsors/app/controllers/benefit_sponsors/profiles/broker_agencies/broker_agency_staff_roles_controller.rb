78775d57400312af109f9c1940812d32fad8cf99
TristanB17 <tristan0bambauer@gmail.com>
2024-03-14 17:19:55 -0600

    remove irrelevant comment

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
@@ -80,7 +80,6 @@ module BenefitSponsors
 
         # NOTE: this will probably be consolidated with a similarily named method in BrokerAgencyProfilesController
         def find_broker_agency_profile
-          # the #new action is missing profile_id from broker_staff_params, hence this conditional
           profile_id = BSON::ObjectId(params[:profile_id])
 
           organizations = BenefitSponsors::Organizations::Organization.where(:"profiles._id" => profile_id)
