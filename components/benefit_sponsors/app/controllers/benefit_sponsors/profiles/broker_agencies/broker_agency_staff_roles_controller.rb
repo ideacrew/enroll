6a7806a9589cbe08eeea829b073006594cc6978f
Tristan <33517218+TristanB17@users.noreply.github.com>
2024-03-15 18:55:20 -0600

    add pundit policies for broker agency profiles staff roles controller (#3659)
    * remove primary_broker
    
    * change permission migration files
    
    * commit before merge new trunk policy
    
    * initial push
    
    * rearrange policy methods to make future transition easier
    
    * remove comments and readd references to search_broker_agency
    
    * replaced match with eq in spec
    
    * make adjustments for rubocop
    
    * disable search_broker_agency due to schema issue
    
    * inherit from main_app ApplicationPolicy
    
    * restore previous BenefitSponsors::ApplicationPolicy
    
    * restore previous BenefitSponsors::ApplicationPolicy w/git
    
    * fix permission on broker_staff_roles_controller
    
    * remove auth for agency agnostic new staff role
    
    * additional fixes for rubocop
    
    * remove unnecessary stubs in spec
    
    * remove unnecessary methods
    
    * tweak comment
    
    * remove unnecessary views, utilize StaffFormPolicy instead of BrokerAgencyProfilePolicy
    
    * remove unused policies
    
    * update for rubocop
    
    * update for rubocop 2
    
    * built out spec file for broker agency staff role forms
    
    * remove irrelevant comment
    
    * remove trailing whitespace
    
    * add specificity to StaffRoleFormPolicy inherited class
    
    * improves broker staff controller post tests
    
    * use new app policy methods in broker agency profile policy
    
    * rearrange permission sequence in spec
    
    * clarify comments

--- components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
+++ components/benefit_sponsors/app/controllers/benefit_sponsors/profiles/broker_agencies/broker_agency_staff_roles_controller.rb
@@ -1,31 +1,38 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
   module Profiles
     module BrokerAgencies
+      # controller that manages adding, approving and removing of staff agency roles to a broker agency profile
       class BrokerAgencyStaffRolesController < ::BenefitSponsors::ApplicationController
 
         def new
+          profile = find_broker_agency_profile
+          authorize profile
+
           @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_new
           set_ie_flash_by_announcement
 
           respond_to do |format|
-            format.html { render 'new', layout: false} if params[:profile_type]
-            format.js  { render 'new'}
+            # previously rendered an HTML form which was unauthorized --> only JS responses should be allowed from this endpoint
+            format.js  { render 'new' }
           end
         end
 
         def create
           @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_create(broker_staff_params)
+
           begin
             @status,@result = @staff.save
             unless @staff.is_broker_registration_page
               flash[:notice] = "Role added successfully" if @status
-              flash[:error] = "Role was not added because " + @result unless @status
+              flash[:error] = "Role was not added because #{@result}" unless @status
             end
           rescue Exception => e
-            flash[:error] = "Role was not added because " + e.message
+            flash[:error] = "Role was not added because #{e.message}"
           end
           respond_to do |format|
-            format.html  { redirect_to profiles_broker_agencies_broker_agency_profile_path(id: params[:profile_id])}
+            format.html  { redirect_to profiles_broker_agencies_broker_agency_profile_path(id: params[:profile_id]) }
             format.js
           end
         end
@@ -38,10 +45,10 @@ module BenefitSponsors
             if @status
               flash[:notice] = "Role approved successfully"
             else
-              flash[:error] = "Role was not approved because " + @result
+              flash[:error] = "Role was not approved because #{@result}"
             end
           rescue Exception => e
-            flash[:error] = "Role was not approved because " + e.message
+            flash[:error] = "Role was not approved because #{e.message}"
           end
           redirect_to profiles_broker_agencies_broker_agency_profile_path(id: params[:profile_id])
         end
@@ -54,14 +61,16 @@ module BenefitSponsors
             if @status
               flash[:notice] = "Role removed successfully"
             else
-              flash[:error] = "Role was not removed because " + @result
+              flash[:error] = "Role was not removed because #{@result}"
             end
           rescue Exception => e
-            flash[:error] = "Role was not removed because " + e.message
+            flash[:error] = "Role was not removed because #{e.message}"
           end
           redirect_to profiles_broker_agencies_broker_agency_profile_path(id: params[:profile_id])
         end
 
+        # This endpoint should remain unauthorized: it is used by BenefitSponsors::Profiles::RegistrationsController to search for Broker Agencies
+        # By design, users do not have to be logged in to use this endpoint
         def search_broker_agency
           @staff = BenefitSponsors::Organizations::OrganizationForms::StaffRoleForm.for_broker_agency_search(broker_staff_params)
           @broker_agency_profiles = @staff.broker_agency_search
@@ -69,10 +78,17 @@ module BenefitSponsors
 
         private
 
+        def find_broker_agency_profile
+          profile_id = BSON::ObjectId(params[:profile_id])
+
+          organizations = BenefitSponsors::Organizations::Organization.where(:"profiles._id" => profile_id)
+          organizations&.first&.broker_agency_profile
+        end
+
         def broker_staff_params
           params[:staff].presence || params[:staff] = {}
           params[:staff].merge!({profile_id: params["staff"]["profile_id"] || params["profile_id"] || params["id"], person_id: params["person_id"], profile_type: params[:profile_type] || "broker_agency_staff",
-                                  filter_criteria: params.permit(:q), is_broker_registration_page: params[:broker_registration_page] || params["staff"]["is_broker_registration_page"]})
+                                 filter_criteria: params.permit(:q), is_broker_registration_page: params[:broker_registration_page] || params["staff"]["is_broker_registration_page"]})
           params[:staff].permit!
         end
       end
--- components/benefit_sponsors/app/policies/benefit_sponsors/application_policy.rb
+++ components/benefit_sponsors/app/policies/benefit_sponsors/application_policy.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 module BenefitSponsors
+  # Policy used for auth in the BenefitSponsors app when running rspec from a GHA
+  # For the time being there is no way to pass the GHAs without pulling from the BenefitSponsors
+  # All "building blocks" have been select from existing methods in the main app ApplicationPolicy for ease of transition
   class ApplicationPolicy
 
     attr_reader :user, :record
@@ -8,7 +13,34 @@ module BenefitSponsors
       @record = record
     end
 
+    # Returns the user who is the account holder.
+    #
+    # @return [User] The user who is the account holder.
+    def account_holder
+      user
+    end
 
-    # Include Policy Scope
+    # Returns the person who is the account holder.
+    # The method uses memoization to store the result of the first call to it and then return that result on subsequent calls,
+    # instead of calling `account_holder.person` each time.
+    #
+    # @return [Person] The person who is the account holder.
+    def account_holder_person
+      return @account_holder_person if defined? @account_holder_person
+
+      @account_holder_person = account_holder&.person
+    end
+
+    def permission
+      return @permission if defined? @permission
+
+      @permission = hbx_role&.permission
+    end
+
+    def hbx_role
+      return @hbx_role if defined? @hbx_role
+
+      @hbx_role = account_holder_person&.hbx_staff_role
+    end
   end
 end
