d271e7f18f8dcf8f243cb6a4bb550c480d3925ef
Sarah Austin <scaustin34@gmail.com>
2024-03-22 16:31:33 -0400

    bs4 footer

--- db/seedfiles/translations/en/cca/layout.rb
+++ db/seedfiles/translations/en/cca/layout.rb
@@ -3,5 +3,8 @@ LAYOUT_TRANSLATIONS = {
   "en.layouts.application_brand.call_customer_service" => "Call Customer Service",
   "en.layouts.application_brand.help" => "Help",
   "en.layouts.application_brand.logout" => "Logout",
-  "en.layouts.application_brand.my_id" => "ID"
+  "en.layouts.application_brand.my_id" => "ID",
+  "en.layout.footer.copyright" => "© %{year} %{site_name}",
+  "en.layout.footer.all_rights" => "All Rights Reserved.",
+  "en.layout.footer.get_help" => "Get Help",
 }
