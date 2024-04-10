772334450f2c1466d82d4b8bf4f22b72f6b76499
HARIDHAR YAMJALA <hariym471@gmail.com>
2024-04-10 15:25:19 -0400

    adding verification history to pvc failure case

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/pvc/create_pvc_request.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/pvc/create_pvc_request.rb
@@ -20,10 +20,10 @@ module FinancialAssistance
             values = yield validate(params)
             application = yield fetch_application(values)
             _evidences = yield create_non_esi_evidences(application)
-            cv3_application = yield transform_application(application)
+            cv3_application = yield transform_and_validate_application(application)
             event = yield build_event(cv3_application)
             publish(event)
-            create_evidence_history(application)
+            create_success_history(application)
 
             Success("Successfully published the pvc payload for family with hbx_id #{params[:family_hbx_id]}")
           end
@@ -56,14 +56,79 @@ module FinancialAssistance
             end
 
             Success(true)
+
+          rescue StandardError => e
+            pvc_logger.error("Failed to create non_esi_evidences for application with hbx_id #{application.hbx_id} due to #{e.inspect}")
+            Failure("Failed to create non_esi_evidences for application with hbx_id #{application.hbx_id}")
           end
 
-          def transform_application(application)
-            payload = ::FinancialAssistance::Operations::Applications::Transformers::ApplicationTo::Cv3Application.new.call(application)
-            AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(payload.value!)
+          def transform_and_validate_application(application)
+            payload_entity = Operations::Fdsh::BuildAndValidateApplicationPayload.new.call(application)
+            record_application_failure(application, payload_entity.failure.messages) if payload_entity.failure?
+
+            if EnrollRegistry.feature_enabled?(:validate_and_record_publish_application_errors) && payload_entity.success?
+              all_applicants_valid = validate_applicants(payload_entity, application)
+              return all_applicants_valid.any?(&:last) ? payload_entity : Failure("Failed to transform application with hbx_id #{application.hbx_id} due to all applicants being invalid")
+            end
+
+            payload_entity
           rescue StandardError => e
             pvc_logger.error("Failed to transform application with hbx_id #{application.hbx_id} due to #{e.inspect}")
+            record_application_failure(application, "transformation failure") if payload_entity.failure?
             Failure("Failed to transform application with hbx_id #{application.hbx_id}")
+          end
+
+          def validate_applicants(payload_entity, application)
+            eligible_applicants = application.active_applicants.select(&:non_esi_evidence)
+            applicants_entity = payload_entity.value!.applicants
+
+            eligible_applicants.map do |eligible_applicant|
+              applicant_entity = applicants_entity.detect { |appl_entity| eligible_applicant.person_hbx_id == appl_entity.person_hbx_id }
+              result = Operations::Fdsh::PayloadEligibility::CheckApplicantEligibilityRules.new.call(applicant_entity, :non_esi_mec)
+
+              if result.success?
+                [applicant_entity.person_hbx_id, true]
+              else
+                record_applicant_failure(eligible_applicant.non_esi_evidence, result)
+                [applicant_entity.person_hbx_id, false]
+              end
+            end
+          end
+
+          def record_applicant_failure(evidence, result)
+            add_verification_history(evidence, 'PVC_Submission_Failed', "PVC - Periodic verifications submission failed due to #{result.failure}", 'system')
+            update_evidence_to_default_state(evidence)
+          end
+
+          def record_application_failure(application, error_messages)
+            create_evidence_history(application, 'PVC_Submission_Failed', "PVC - Periodic verifications submission failed due to #{error_messages}", 'system')
+            update_evidence_state_for_all_applicants(application)
+          end
+
+          def add_verification_history(evidence, action, update_reason, update_by)
+            evidence.add_verification_history(action, update_reason, update_by) if evidence.present?
+          end
+
+          def update_evidence_state_for_all_applicants(application)
+            application.active_applicants.each do |applicant|
+              update_evidence_to_default_state(applicant.non_esi_evidence)
+            end
+          end
+
+          def create_evidence_history(application, action, update_reason, update_by)
+            application.active_applicants.each do |applicant|
+              evidence = applicant.non_esi_evidence
+              next unless evidence.present?
+              add_verification_history(evidence, action, update_reason, update_by)
+            end
+          end
+
+          def update_evidence_to_default_state(evidence)
+            evidence&.determine_mec_evidence_aasm_status
+          end
+
+          def add_verification_history(evidence, action, update_reason, update_by)
+            evidence.add_verification_history(action, update_reason, update_by)
           end
 
           def build_event(cv3_application)
@@ -75,12 +140,8 @@ module FinancialAssistance
             Success("Successfully published the pvc payload")
           end
 
-          def create_evidence_history(application)
-            application.active_applicants.each do |applicant|
-              evidence = applicant.non_esi_evidence
-              evidence&.add_verification_history('PVC_Submitted', 'PVC - Renewal verifications submitted', 'system')
-            end
-
+          def create_success_history(application)
+            create_evidence_history(application, 'PVC_Submitted', 'PVC - Renewal verifications submitted', 'system')
             application.save
           end
 
