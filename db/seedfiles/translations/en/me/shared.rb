e3b4d54aed1d2011655b26407f222a1a7ea45340
Kristin Merbach <kristin@kristinmerbach.com>
2024-04-22 09:37:00 -0700

    authority & consent ridp page

--- db/seedfiles/translations/en/me/shared.rb
+++ db/seedfiles/translations/en/me/shared.rb
@@ -74,6 +74,7 @@ SHARED_TRANSLATIONS = {
     "en.previous_step" => "Previous Step",
     "en.save_and_exit" => "Log Out",
     "en.help_sign_up" => "Help Me Sign Up",
+    "en.get_help_sign_up" => "Get Help Signing Up",
     # event log
     "en.event_log.eligibility" => "Eligibility",
     "en.event_log.outcome" => "Outcome",
