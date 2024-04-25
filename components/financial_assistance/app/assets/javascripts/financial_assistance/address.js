8945ae2b8785d4e70ac7fbd36446280a4b7f666c
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-04-25 15:45:57 -0400

    fixes show and hide applicant mailing related buttons

--- components/financial_assistance/app/assets/javascripts/financial_assistance/address.js
+++ components/financial_assistance/app/assets/javascripts/financial_assistance/address.js
@@ -3,25 +3,25 @@
 //       1. Use ids or classes to hide/show the buttons.
 //       2. Use ids or classes to hide/show the mailing address fields.
 function hideOrShowApplicantMailingAddress() {
-  var spanElement = document.querySelector('#address_info + div.applicant-mailing-address');
+  var mailingDivElement = document.querySelector('div.applicant-mailing-address:not(.dn)');
 
-  if (spanElement) {
-    spanElement.addEventListener('click', function() {
+  if (mailingDivElement) {
+    mailingDivElement.addEventListener('click', function() {
       var mailingDivs = document.querySelectorAll('.row-form-wrapper.mailing-div');
       var inputFields = document.querySelectorAll("#applicant_addresses_attributes_1_zip, #applicant_addresses_attributes_1_address_1, #applicant_addresses_attributes_1_city, #dependent_addresses_1_address_1, #dependent_addresses_1_zip, #dependent_addresses_1_city");
       var labelFloatlabels = document.querySelectorAll('.mailing-div .label-floatlabel');
       var destroyElement1 = document.querySelector("#applicant_addresses_attributes_1__destroy");
 
-      if (spanElement.textContent.trim() === "Add Mailing Address") {
-        spanElement.textContent = 'Remove Mailing Address';
+      if (mailingDivElement.textContent.trim() === "Add Mailing Address") {
+        mailingDivElement.textContent = 'Remove Mailing Address';
         mailingDivs.forEach(function(div) {
           div.style.display = 'block';
         });
         inputFields.forEach(function(input) {
           input.required = true;
         });
-      } else if (spanElement.textContent.trim() === "Remove Mailing Address") {
-        spanElement.textContent = 'Add Mailing Address';
+      } else if (mailingDivElement.textContent.trim() === "Remove Mailing Address") {
+        mailingDivElement.textContent = 'Add Mailing Address';
         document.querySelectorAll('.mailing-div').forEach(function(div) {
           div.style.display = 'none';
         });
