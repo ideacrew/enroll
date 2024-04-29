c3dd06840c9ce5caf319a483bc598a0cf8835cc3
Kristin Merbach <kristin@kristinmerbach.com>
2024-04-29 15:10:55 -0700

    add new progress layout

--- app/controllers/insured/fdsh_ridp_verifications_controller.rb
+++ app/controllers/insured/fdsh_ridp_verifications_controller.rb
@@ -3,7 +3,7 @@
 module Insured
   # controller for fdsh ridp H139 feature
   class FdshRidpVerificationsController < ApplicationController
-    layout 'bootstrap_4' if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
+    layout 'progress' if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
 
     before_action :set_bs4_layout if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
     before_action :set_current_person
