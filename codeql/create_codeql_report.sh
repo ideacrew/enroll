6c36483afb4de7fd93c0c4db13bf892ec914d09a
Trey Evans <lewis.r.evans@gmail.com>
2024-05-01 13:43:41 -0400

    Only block what we currently fail.

--- codeql/create_codeql_report.sh
+++ codeql/create_codeql_report.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-codeql database analyze --format=sarif-latest --output=codeql.json --sarif-add-snippets -- enroll/ruby \
-  codeql/ruby-queries:queries/security/cwe-094/CodeInjection.ql
+codeql database analyze --format=sarif-latest --output=codeql.json --sarif-add-snippets -- enroll/ruby
