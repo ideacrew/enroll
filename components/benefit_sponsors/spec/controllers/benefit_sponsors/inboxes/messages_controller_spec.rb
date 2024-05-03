192420b848417742507cfeb57cbef3ec4fa04957
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-05-03 17:19:21 -0400

    allows inbox show access to active broker agency staff (#3796)

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/inboxes/messages_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/inboxes/messages_controller_spec.rb
@@ -111,23 +111,88 @@ module BenefitSponsors
       end
 
       context "for broker agency profile - from Admin login" do
+        let(:person) { FactoryBot.create(:person, :with_consumer_role, :with_active_consumer_role) }
+        let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person) }
+        let(:user_of_family) { FactoryBot.create(:user, person: person) }
+        let(:hbx_profile) do
+          FactoryBot.create(
+            :hbx_profile,
+            :normal_ivl_open_enrollment,
+            us_state_abbreviation: EnrollRegistry[:enroll_app].setting(:state_abbreviation).item,
+            cms_id: "#{EnrollRegistry[:enroll_app].setting(:state_abbreviation).item.upcase}0"
+          )
+        end
+        let(:hbx_staff_person) { FactoryBot.create(:person) }
+        let(:hbx_staff_role) do
+          hbx_staff_person.create_hbx_staff_role(
+            permission_id: permission.id,
+            subrole: permission.name,
+            hbx_profile: hbx_profile
+          )
+        end
+        let(:hbx_admin_user) do
+          FactoryBot.create(:user, person: hbx_staff_person)
+          hbx_staff_role.person.user
+        end
+
+        let(:market_kind) { :both }
+        let(:broker_person) { FactoryBot.create(:person) }
+        let(:broker_role) { FactoryBot.create(:broker_role, person: broker_person) }
+        let(:broker_user) { FactoryBot.create(:user, person: broker_person) }
+        let(:message) { FactoryBot.create(:message, :inbox_folder, inbox: broker_person.inbox) }
+
+        let(:site) do
+          FactoryBot.create(
+            :benefit_sponsors_site,
+            :with_benefit_market,
+            :as_hbx_profile,
+            site_key: ::EnrollRegistry[:enroll_app].settings(:site_key).item
+          )
+        end
+
+        let(:broker_agency_organization) { FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site) }
+        let(:broker_agency_profile) { broker_agency_organization.broker_agency_profile }
+        let(:broker_agency_id) { broker_agency_profile.id }
+
+        let(:broker_agency_account) do
+          family.broker_agency_accounts.create!(
+            benefit_sponsors_broker_agency_profile_id: broker_agency_id,
+            writing_agent_id: broker_role.id,
+            is_active: baa_active,
+            start_on: TimeKeeper.date_of_record
+          )
+        end
+
+        let(:broker_staff_person) { FactoryBot.create(:person) }
+
+        let(:broker_staff_state) { 'active' }
+
+        let(:broker_staff) do
+          FactoryBot.create(
+            :broker_agency_staff_role,
+            person: broker_staff_person,
+            aasm_state: broker_staff_state,
+            benefit_sponsors_broker_agency_profile_id: broker_agency_id
+          )
+        end
+        let(:broker_staff_user) { FactoryBot.create(:user, person: broker_staff_person) }
+
+        let(:permission) { FactoryBot.create(:permission, :super_admin) }
+
         before do
-          user_with_hbx_staff_role = FactoryBot.create(:user, :with_hbx_staff_role)
-          FactoryBot.create(:person, user: user_with_hbx_staff_role)
-          user_with_hbx_staff_role.person.build_hbx_staff_role(hbx_profile_id: broker_organization.broker_agency_profile.id)
-          user_with_hbx_staff_role.person.hbx_staff_role.save!
-          broker_person.broker_role.update_attributes!(benefit_sponsors_broker_agency_profile_id: broker_organization.broker_agency_profile.id)
-          @broker_inbox = broker_person.build_inbox
-          @broker_inbox.save!
-          welcome_subject = "Welcome to #{EnrollRegistry[:enroll_app].setting(:short_name).item}"
-          welcome_body = "#{EnrollRegistry[:enroll_app].setting(:short_name).item} is the #{Settings.aca.state_name}'s on-line marketplace to shop, compare, and select health insurance that meets your health needs and budgets."
-          broker_message = @broker_inbox.messages.create(subject: welcome_subject, body: welcome_body)
-          sign_in user_with_hbx_staff_role
+          broker_role.update_attributes!(benefit_sponsors_broker_agency_profile_id: broker_agency_id)
+          broker_person.create_broker_agency_staff_role(
+            benefit_sponsors_broker_agency_profile_id: broker_role.benefit_sponsors_broker_agency_profile_id
+          )
+          broker_agency_profile.update_attributes!(primary_broker_role_id: broker_role.id, market_kind: market_kind)
+          broker_role.approve!
+          broker_staff
+          sign_in broker_staff_user
         end
 
         context "show message" do
           before do
-            get :show, params:{id: broker_person.id, message_id: @broker_inbox.messages.first.id}
+            get :show, params: { id: broker_role.person.id, message_id: message.id }
           end
 
           it "should render show template" do
