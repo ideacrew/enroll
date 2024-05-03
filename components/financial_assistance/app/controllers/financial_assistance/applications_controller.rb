b1727ec655a0e2d0b4b5e100940d71b8992d9b86
Kristin Merbach <kristin@kristinmerbach.com>
2024-05-03 11:06:00 -0700

    don't touch FAA yet

--- components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
@@ -22,7 +22,6 @@ module FinancialAssistance
     before_action :set_cache_headers, only: [:index, :relationships, :review_and_submit, :index_with_filter]
 
     layout "financial_assistance_nav", only: %i[edit step review_and_submit eligibility_response_error application_publish_error]
-    layout "bs4_financial_assistance_nav", only: %i[application_checklist edit]
 
     # We should ONLY be getting applications that are associated with PrimaryFamily of Current Person.
     # DO NOT include applications from other families.
