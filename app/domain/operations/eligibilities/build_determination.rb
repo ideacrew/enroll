395085c11f92deeb8c76dee123bfee0eec4fd684
Jacob Kagon <jacob@kagon.net>
2024-05-06 11:24:46 -0400

    fix formatting

--- app/domain/operations/eligibilities/build_determination.rb
+++ app/domain/operations/eligibilities/build_determination.rb
@@ -183,11 +183,11 @@ module Operations
 
       def build_eligibility_states(subject, eligibility_items, values)
         eligibility_items
-         .collect do |eligibility_item|
+          .collect do |eligibility_item|
             unless values[:eligibility_items_requested].blank? ||
-                  values[:eligibility_items_requested]&.key?(
-                    eligibility_item.key.to_sym
-                 )
+                    values[:eligibility_items_requested]&.key?(
+                      eligibility_item.key.to_sym
+                  )
               next
             end
 
