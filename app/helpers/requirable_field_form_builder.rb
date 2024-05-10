021319c9252db620ba1ee0f6f600242f669384b5
Charlie Parker <charlie.parker@ideacrew.com>
2024-05-10 16:44:16 -0400

    add builder to use for DRYing creation of required text fields with asterisks

---
+++ app/helpers/requirable_field_form_builder.rb
@@ -0,0 +1,5 @@
+class RequirableFieldFormBuilder < ActionView::Helpers::FormBuilder
+  def text_field(attribute, placeholder, required: false, **options)
+    super(attribute, options.merge(placeholder: "#{placeholder}#{required ? " *" : ""}", required: required))
+  end
+end
