96b6a2e72488a83f4b18dc0f554b35dabcbe1bf9
Charlie Parker <charlie.parker@ideacrew.com>
2024-05-12 23:11:43 -0400

    fixe merge order so that params override options

--- app/helpers/requirable_form_builder.rb
+++ app/helpers/requirable_form_builder.rb
@@ -1,10 +1,10 @@
 class RequirableFormBuilder < ActionView::Helpers::FormBuilder
   def requirable_text_field(attribute, label:, placeholder: label, required: false, **options)
-    requirable_label(attribute, label, required) + text_field(attribute, { placeholder: placeholder, required: required }.merge(options))
+    requirable_label(attribute, label, required) + text_field(attribute, options.merge(placeholder: placeholder, required: required))
   end
 
   def requirable_select(attribute, label:, choices:, options: {}, required: false, **html_options)
-    requirable_label(attribute, label, required) + select(attribute, choices, options, { required: required }.merge(html_options))
+    requirable_label(attribute, label, required) + select(attribute, choices, options, html_options.merge(required: required))
   end
 
   private
