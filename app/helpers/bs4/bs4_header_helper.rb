7eeb8a8e62ced3b7de3e4a0f5813c2aa994f8107
Charlie Parker <charlie.parker@ideacrew.com>
2024-05-15 10:34:12 -0400

    remove repeated header text (#3839)
    add translations for user roles, remove fallback on role check in portal helper, use new translation keys in portal helper

--- app/helpers/bs4/bs4_header_helper.rb
+++ app/helpers/bs4/bs4_header_helper.rb
@@ -11,48 +11,46 @@ module Bs4
       if current_user.nil?
         nil
       elsif current_user.try(:has_hbx_staff_role?)
-        link_to(l10n("Admin"), main_app.exchanges_hbx_profiles_root_path)
+        link_to(l10n("layout.header.role.admin"), main_app.exchanges_hbx_profiles_root_path)
       elsif display_i_am_broker_for_consumer?(current_user.person) && controller_path.exclude?('general_agencies')
-        link_to(l10n("Broker"), get_broker_profile_path)
+        link_to(l10n("layout.header.role.broker"), get_broker_profile_path)
       elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
-        link_to(l10n("Trained Expert"), main_app.home_exchanges_agents_path)
+        link_to(l10n("layout.header.role.trained_expert"), main_app.home_exchanges_agents_path)
       elsif current_user.person&.active_employee_roles&.any?
         if controller_path.include?('broker_agencies')
-          link_to(l10n("Broker"), get_broker_profile_path)
+          link_to(l10n("layout.header.role.broker"), get_broker_profile_path)
         elsif controller_path.include?('general_agencies')
-          link_to(l10n("General Agency"), benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
+          link_to(l10n("layout.header.role.general_agency"), benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
         elsif controller == 'employer_profiles' || controller_path.include?('employers')
           #current user has both broker_agency staff role and employee role but not employer_staff_roles
           if current_user.person.active_employer_staff_roles.present?
             employer_profile_path = benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home')
-            link_to(l10n("Employer"), employer_profile_path)
+            link_to(l10n("layout.header.role.employer"), employer_profile_path)
           elsif current_user.try(:has_broker_agency_staff_role?)
-            link_to(l10n("Broker"), get_broker_profile_path)
+            link_to(l10n("layout.header.role.broker"), get_broker_profile_path)
           end
         else
-          link_to(l10n("Employee"), main_app.family_account_path)
+          link_to(l10n("layout.header.role.employee"), main_app.family_account_path)
         end
       elsif (controller_path.include?("insured") && current_user.try(:has_consumer_role?)) ||
             (EnrollRegistry.feature_enabled?(:financial_assistance) && controller_path.include?("financial_assistance") && current_user.try(:has_consumer_role?))
         if current_user.identity_verified_date.present?
-          link_to(l10n("Individual and Family"), main_app.family_account_path)
+          link_to(l10n("layout.header.role.individual_and_family"), main_app.family_account_path)
         else
-          link_to(l10n("Individual and Family"), 'javascript:;')
+          link_to(l10n("layout.header.role.individual_and_family"), 'javascript:;')
         end
       # rubocop:disable Lint/DuplicateBranch
       elsif current_user.try(:has_broker_agency_staff_role?) && controller_path.exclude?('general_agencies') && controller_path.exclude?('employers')
-        link_to(l10n("Broker"), get_broker_profile_path)
+        link_to(l10n("layout.header.role.broker"), get_broker_profile_path)
       # rubocop:enable Lint/DuplicateBranch
       elsif current_user.try(:has_general_agency_staff_role?)
         if current_user.try(:has_employer_staff_role?) && controller_path.include?('employers')
-          link_to(l10n("Employer"), benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
+          link_to(l10n("layout.header.role.employer"), benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
         else
-          link_to(l10n("General Agency"), benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.active_general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
+          link_to(l10n("layout.header.role.general_agency"), benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.active_general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
         end
       elsif current_user.try(:has_employer_staff_role?)
-        link_to(l10n("Employer"), benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
-      else
-        link_to(l10n('welcome.index.byline', welcome_text: EnrollRegistry[:enroll_app].setting(:header_message).item.to_s).html_safe, 'javascript:;')
+        link_to(l10n("layout.header.role.employer"), benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
       end
     end
 
