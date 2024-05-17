1180a86415c9e3142a6810185f8c7d8d2986715d
Kristin Merbach <kristin@kristinmerbach.com>
2024-05-17 14:18:18 -0700

    more spec fixes

--- spec/views/insured/consumer_roles/search.html.erb_spec.rb
+++ spec/views/insured/consumer_roles/search.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "insured/consumer_roles/search.html.haml" do
+RSpec.describe "insured/consumer_roles/search.html.erb" do
 
   let(:person) { FactoryBot.create(:person) }
   let(:current_user) { FactoryBot.create(:user, person: person) }
@@ -9,15 +9,11 @@ RSpec.describe "insured/consumer_roles/search.html.haml" do
     sign_in current_user
     assign(:person, Forms::ConsumerCandidate.new)
     allow(view).to receive(:policy_helper).and_return(double("FamilyPolicy", updateable?: true))
-    render template: "insured/consumer_roles/search.html.haml"
+    render template: "insured/consumer_roles/search.html.erb"
   end
 
   it "should display the title" do
     expect(rendered).to have_selector('h2', text: 'Personal Information')
-  end
-
-  it "should have memo to indicate required fields" do
-    expect(rendered).to have_selector('p.memo', text: '* = required field')
   end
 
   it "should have the content enter your information" do
