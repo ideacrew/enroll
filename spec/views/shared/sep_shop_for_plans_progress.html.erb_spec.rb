4fb81f9d5a75e08a01dbec78b2451ea3b7e98cfc
Kristin Merbach <kristin@kristinmerbach.com>
2024-05-20 09:04:54 -0700

    rubocop fixes

--- spec/views/shared/sep_shop_for_plans_progress.html.erb_spec.rb
+++ spec/views/shared/sep_shop_for_plans_progress.html.erb_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe "shared/_sep_shop_for_plans_progress.html.haml" do
@@ -16,7 +18,7 @@ describe "shared/_sep_shop_for_plans_progress.html.haml" do
     end
 
     it "should have 33% complete" do
-      expect(rendered).to match /33% Complete/
+      expect(rendered).to match(/33% Complete/)
     end
   end
 
@@ -31,7 +33,7 @@ describe "shared/_sep_shop_for_plans_progress.html.haml" do
     end
 
     it "should have 66% complete" do
-      expect(rendered).to match /66% Complete/
+      expect(rendered).to match(/66% Complete/)
     end
 
     it "should have li option for plan selection" do
@@ -60,7 +62,7 @@ describe "shared/_sep_shop_for_plans_progress.html.haml" do
     end
 
     it "should have previous option" do
-      expect(rendered).to match /PREVIOUS/i
+      expect(rendered).to match(/PREVIOUS/i)
     end
   end
 
