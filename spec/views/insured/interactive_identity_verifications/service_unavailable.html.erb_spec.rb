a170727927d570d87d9d9ae31f9fee283ef4ac81
Kristin Merbach <kristin@kristinmerbach.com>
2024-05-20 09:04:54 -0700

    rubocop fixes

--- spec/views/insured/interactive_identity_verifications/service_unavailable.html.erb_spec.rb
+++ spec/views/insured/interactive_identity_verifications/service_unavailable.html.erb_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 describe "insured/interactive_identity_verifications/service_unavailable" do
--- spec/views/shared/shop_for_plans_progress.html.erb_spec.rb
+++ spec/views/shared/shop_for_plans_progress.html.erb_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe "shared/_shop_for_plans_progress.html.erb" do
@@ -16,7 +18,7 @@ describe "shared/_shop_for_plans_progress.html.erb" do
     end
 
     it "should have 33% complete" do
-      expect(rendered).to match /33%/
+      expect(rendered).to match(/33%/)
     end
   end
 
@@ -32,7 +34,7 @@ describe "shared/_shop_for_plans_progress.html.erb" do
     end
 
     it "should have 66% complete" do
-      expect(rendered).to match /66%/
+      expect(rendered).to match(/66%/)
     end
 
     it "should have li option for plan selection" do
@@ -44,7 +46,7 @@ describe "shared/_shop_for_plans_progress.html.erb" do
     end
 
     it "should have previous option" do
-      expect(rendered).to match /PREVIOUS/i
+      expect(rendered).to match(/PREVIOUS/i)
     end
   end
 
@@ -63,7 +65,7 @@ describe "shared/_shop_for_plans_progress.html.erb" do
       expect(rendered).to have_selector('#btn-continue.disabled')
     end
 
-     it "should have confirm button styling not disabled for shop " do
+    it "should have confirm button styling not disabled for shop " do
       allow(enrollment).to receive(:is_shop?).and_return(true)
       render 'shared/shop_for_plans_progress', step: '2'
       expect(rendered).not_to have_selector('#btn-continue.disabled')
