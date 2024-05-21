116b8f60c1a18a8de26e28a33342bc85d433e323
Charlie Parker <charlie.parker@ideacrew.com>
2024-05-21 10:34:56 -0400

    fix label for verified state (#3865)
    update new verification helper to return the string matching legacy for valid case

--- db/seedfiles/translations/en/cca/insured.rb
+++ db/seedfiles/translations/en/cca/insured.rb
@@ -102,7 +102,7 @@ How to find the SEVIS ID: On the DS-2019, the number is on the top right hand si
   :'en.application_type' => "Application Type",
   :'en.confirm_selection' => "Confirm",
   :'en.ridp_status.in_review' => "In Review",
-  :'en.ridp_status.valid' => "Valid",
+  :'en.ridp_status.verified' => "Verified",
   :'en.ridp_status.outstanding' => "Outstanding",
   :'en.suffix' => "Suffix",
   :'en.insured.families.get_help_sign_up' => "Get Help Signing Up",
--- db/seedfiles/translations/en/me/insured.rb
+++ db/seedfiles/translations/en/me/insured.rb
@@ -125,7 +125,7 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.application_type' => "Application Type",
   :'en.confirm_selection' => "Confirm",
   :'en.ridp_status.in_review' => "In Review",
-  :'en.ridp_status.valid' => "Valid",
+  :'en.ridp_status.verified' => "Verified",
   :'en.ridp_status.outstanding' => "Outstanding",
   :'en.insured.employee_roles.enroll_as_an_employee' => "Enroll as an employee of %{employer_name}",
   :'en.new_message' => "New Message",
