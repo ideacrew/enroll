908cb94e0e8eb61bc993128dd74fd07aaf51b213
vkghub <vishuk1201@gmail.com>
2024-05-24 09:58:19 -0400

    rubocop fix

--- app/event_source/publishers/people/person_alive_status/data_migration_requested_publisher.rb
+++ app/event_source/publishers/people/person_alive_status/data_migration_requested_publisher.rb
@@ -5,9 +5,9 @@ module Publishers
     module PersonAliveStatus
           # Publisher will send request to EA to create renewal drafts
       class DataMigrationRequestedPublisher
-          include ::EventSource::Publisher[amqp: 'enroll.people.person_alive_status.data_migration']
+        include ::EventSource::Publisher[amqp: 'enroll.people.person_alive_status.data_migration']
 
-          register_event 'requested'
+        register_event 'requested'
       end
     end
   end
