e4738e27d6e084e8463aee723b0e19005cf5cfb2
Charlie Parker <charlie.parker@ideacrew.com>
2024-05-28 13:36:20 -0400

    use existing continue strings as substring in dob error

--- app/models/forms/consumer_candidate.rb
+++ app/models/forms/consumer_candidate.rb
@@ -160,8 +160,8 @@ module Forms
       return unless (self.dob_check == "false" || self.dob_check.blank?) && self.dob.present?
 
       if ::TimeKeeper.date_of_record.year - self.dob.year < 18
-        message_key = EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "insured.match_person.dob_valid_error_2" : "insured.match_person.dob_valid_error"
-        errors.add(:base, l10n(message_key), :level => :warning)
+        continue_substring_key = EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "continue_next" : "continue"
+        errors.add(:base, l10n("insured.match_person.dob_valid_error", continue_substring: l10n(continue_substring_key).downcase), :level => :warning)
         self.dob_check = true
       else
         self.dob_check = false
