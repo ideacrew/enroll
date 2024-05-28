aa71e06b7d2434e8045990f410b5ad11d24f3320
quisbertdiana <82827623+quisbertdiana@users.noreply.github.com>
2024-05-28 09:29:17 -0400

    update individual steps

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -67,7 +67,7 @@ Then(/^I can see the error message (.*?)$/) do |message|
 end
 
 And(/the user sees Your Information page$/) do
-  expect(page).to have_content(l10n('your_information'))
+  expect(page).to have_content YourInformation.your_information_text
   find(YourInformation.continue_btn).click
 end
 
@@ -78,6 +78,10 @@ When(/the user registers as an individual$/) do
   fill_in IvlPersonalInformation.ssn, :with => "262-61-3061"
   find(IvlPersonalInformation.male_radiobtn).click
   screenshot("register")
+  find(IvlPersonalInformation.continue_btn).click
+end
+
+When(/the individual clicks continue on the personal information page$/) do
   find(IvlPersonalInformation.continue_btn).click
 end
 
@@ -387,8 +391,7 @@ end
 Then(/^.+ agrees to the privacy agreeement/) do
   wait_for_ajax
   expect(page).to have_content IvlAuthorizationAndConsent.authorization_and_consent_text
-  find(IvlAuthorizationAndConsent.continue_btn).click
-  sleep 2
+  find_all(IvlAuthorizationAndConsent.continue_btn)[0].click
 end
 
 When(/^Individual clicks on Individual and Family link/) do
@@ -751,7 +754,7 @@ Then(/Individual asks for help$/) do
 end
 
 And(/^.+ clicks? on the Continue button of the Account Setup page$/i) do
-  find(IvlPersonalInformation.continue_btn).click
+  find(IvlPersonalInformation.continue_btn_2).click
 end
 
 Then(/^.+ sees the Verify Identity Consent page/)  do
