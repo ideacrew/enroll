d6d7dbdd977427edff0c8a2ffbe76ffa52fa91f7
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2024-06-03 10:52:28 -0700

    starting to remove other income step files (#3907)
    * starting to remove step files
    
    * continued to remove step pattern

--- components/financial_assistance/app/controllers/financial_assistance/incomes_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/incomes_controller.rb
@@ -2,11 +2,9 @@
 
 module FinancialAssistance
   class IncomesController < FinancialAssistance::ApplicationController
-    include ::UIHelpers::WorkflowController
     include NavigationHelper
 
     before_action :find_application_and_applicant
-    before_action :load_support_texts, only: [:index, :other]
     before_action :set_cache_headers, only: [:index, :other]
 
     layout "financial_assistance_nav", only: [:index, :other, :new, :step]
@@ -25,10 +23,7 @@ module FinancialAssistance
 
     def new
       authorize @applicant, :new?
-      @model = @applicant.incomes.build
-      load_steps
-      current_step
-      render 'workflow/step'
+      render 'other'
     end
 
     def edit
@@ -40,35 +35,9 @@ module FinancialAssistance
     end
 
     def step
-      authorize @model, :step?
-      save_faa_bookmark(request.original_url.gsub(%r{/step.*}, "/step/#{@current_step.to_i}"))
-      set_admin_bookmark_url
-      flash[:error] = nil
-      model_name = @model.class.to_s.split('::').last.downcase
-      model_params = params[model_name]
-      format_date_params model_params if model_params.present?
-
-      @model.assign_attributes(permit_params(model_params)) if model_params.present?
-      update_employer_contact(@model, params) if @model.income_type == job_income_type
-      if params.key?(model_name)
-        if @model.save(context: "step_#{@current_step.to_i}".to_sym)
-          @current_step = @current_step.next_step if @current_step.next_step.present?
-          if params.key? :last_step
-            @model.update_attributes!(workflow: { current_step: 1 })
-            flash[:notice] = "Income Added - (#{@model.kind})"
-            redirect_to application_applicant_incomes_path(@application, @applicant)
-          else
-            @model.update_attributes!(workflow: { current_step: @current_step.to_i })
-            render 'workflow/step'
-          end
-        else
-          @model.workflow = { current_step: @current_step.to_i }
-          flash[:error] = build_error_messages(@model)
-          render 'workflow/step'
-        end
-      else
-        render 'workflow/step'
-      end
+      @income = @applicant.incomes.find params[:id]
+      authorize @income, :step?
+      redirect_to action: :other
     end
 
     def create
