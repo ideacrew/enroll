14129645900981b426b4c4d18a3528a8c2a9f166
jayreddy519 <jayanth.rordev@gmail.com>
2024-06-05 16:55:52 -0400

    latest changes

--- app/helpers/verification_helper.rb
+++ app/helpers/verification_helper.rb
@@ -252,7 +252,7 @@ module VerificationHelper
   end
 
   def build_admin_actions_list(v_type, f_member)
-    if f_member.consumer_role.aasm_state == 'unverified' || !VerificationType::ADMIN_CALL_HUB_VERIFICATION_TYPES.include?(v_type.type_name)
+    if f_member.consumer_role.aasm_state == 'unverified' || ['Alive Status', 'American Indian Status'].include?(v_type.type_name)
       ::VlpDocument::ADMIN_VERIFICATION_ACTIONS.reject{ |el| el == 'Call HUB' }
     elsif verification_type_status(v_type, f_member) == 'outstanding'
       ::VlpDocument::ADMIN_VERIFICATION_ACTIONS.reject{|el| el == "Reject" }
