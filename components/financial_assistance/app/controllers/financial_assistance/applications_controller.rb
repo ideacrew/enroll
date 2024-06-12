a7bd45a6066c177ab49a489c8ad5bf302a78bed7
Charlie Parker <charlie.parker@ideacrew.com>
2024-06-12 09:39:44 -0400

    fix for review and submit layout
    mc

--- components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
@@ -390,7 +390,9 @@ module FinancialAssistance
         EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "financial_assistance_progress" : "financial_assistance_nav"
       when "submit_your_application"
         EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "financial_assistance_progress" : "financial_assistance_nav"
-      when "application_year_selection", "application_checklist", "review_and_submit"
+      when "review_and_submit"
+        EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "financial_assistance_progress" : "financial_assistance_nav"
+      when "application_year_selection", "application_checklist"
         EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "financial_assistance_progress" : "financial_assistance"
       when "eligibility_results"
         if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
