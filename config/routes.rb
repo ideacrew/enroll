b65ffe325420c6e161e1f3286eb16282d96035ca
Hanna <hanna.kalesnikava@ideacrew.com>
2024-06-13 08:43:51 -0700

    Remove action post for verify_dob_change

--- config/routes.rb
+++ config/routes.rb
@@ -595,7 +595,7 @@ Rails.application.routes.draw do
 
   match "hbx_profiles/edit_dob_ssn" => "exchanges/hbx_profiles#edit_dob_ssn", as: :edit_dob_ssn, via: [:get, :post]
   match "hbx_profiles/update_dob_ssn" => "exchanges/hbx_profiles#update_dob_ssn", as: :update_dob_ssn, via: [:post], defaults: { format: 'js' }
-  match "hbx_profiles/verify_dob_change" => "exchanges/hbx_profiles#verify_dob_change", as: :verify_dob_change, via: [:get], defaults: { format: 'js' }
+  match "hbx_profiles/verify_dob_change" => "exchanges/hbx_profiles#verify_dob_change", as: :verify_dob_change, via: [:post], defaults: { format: 'js' }
   match "hbx_profiles/create_eligibility" => "exchanges/hbx_profiles#create_eligibility", as: :create_eligibility, via: [:post], defaults: { format: 'js' }
   match "hbx_profiles/process_eligibility" => "exchanges/hbx_profiles#process_eligibility", as: :process_eligibility, via: [:post], defaults: { format: 'js' }
 
