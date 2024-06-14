5a2556f01418a65b24ca8a2234dbc495b2548998
Charlie Parker <charlie.parker@ideacrew.com>
2024-06-14 15:55:21 -0400

    bs4 fa other options (#3926)
    * initial markup transfer
    
    * margin updates, move student questions into student block, translation cleanup
    
    * todos
    
    * add helper to handle subject substring for bs4 and legacy
    
    * refactor helper to default to legacy behavior, add helper method to view to use true argument for bs4 section
    
    * add missing helper call
    
    * fix date validation - use date_field helper, add row-form-wrapper for dependent field toggling, allow date formatting on questions save
    
    * add red required asterisk
    
    * fix selectric bug for pregnant, fix wrong id for paying bills
    
    * modal work
    
    mc
    
    * add close button
    
    * remove date debug comments
    
    * remove newline
    
    * removes fa style, updates required class per new 106 reqs
    
    * remove todos
    
    * reuse enable method
    
    * remove unneeded def
    
    * remove unneeded def
    
    * remove comments
    
    * add missing field, move asterisk for primary caregiver string out of translation
    
    * whitespace fix, add labels to legends to fix dangling form labels
    
    * add more missing labels
    
    * cleanup

--- components/financial_assistance/app/helpers/financial_assistance/application_helper.rb
+++ components/financial_assistance/app/helpers/financial_assistance/application_helper.rb
@@ -404,5 +404,9 @@ module FinancialAssistance
       review_step[:link] = financial_assistance.review_and_submit_application_path(application) if application.present? && application.ready_for_attestation? && application.is_draft?
       links << review_step
     end
+
+    def other_questions_prompt(key, use_applicant_name = false)
+      l10n("faa.other_ques.#{key}", subject: use_applicant_name ? @applicant.first_name.capitalize : l10n("faa.other_ques.this_person"))
+    end
   end
 end
