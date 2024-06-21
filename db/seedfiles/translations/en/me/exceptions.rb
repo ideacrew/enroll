20b263d193908b9bca6ce9327661203627b53898
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2024-06-21 11:28:24 -0700

    start moving exception page to bs4 (#3931)
    exception page to bs4

--- db/seedfiles/translations/en/me/exceptions.rb
+++ db/seedfiles/translations/en/me/exceptions.rb
@@ -1,4 +1,4 @@
 EXCEPTIONS_TRANSLATIONS = {
-  "en.exceptions.show" => "<h1>We weren't able to process your request.</h1><br> <p><u>Please call the %{contact_center_name} at %{contact_center_phone_number} (TTY: %{contact_center_tty_number})</u></p> <br><a href=%{login_url} class='btn btn-default'>My Account</a>",
+  "en.exceptions.show" => "<h1>We weren't able to process your request.</h1><br> <p><u>Please call the %{contact_center_name} at %{contact_center_phone_number} (TTY: %{contact_center_tty_number})</u></p> <br><a href=%{login_url} class='btn btn-default'>Go to My Account</a>",
   "en.exceptions.valid_birthdate" => "Please provide a valid birthdate."
 }
