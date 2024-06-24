8c1d965fbce1c9ab9011ca338cf3adbb9fb31122
vkghub <vishuk1201@gmail.com>
2024-06-24 14:38:31 -0400

    fix the export script

--- script/export_pvc_determined_families.rb
+++ script/export_pvc_determined_families.rb
@@ -5,22 +5,24 @@
 # To run this for specific year
 # bundle exec rails runner script/export_pvc_families.rb assistance_year='2024'
 
-assistance_year = ENV['assistance_year'].to_i
+assistance_year = ENV['assistance_year'] || TimeKeeper.date_of_record.year
 csr_list = [02, 04, 05, 06].freeze
 
 family_ids = Family.with_applied_aptc_or_csr_active_enrollments(csr_list).distinct(:_id)
 
 p "found #{family_ids.count} families"  unless Rails.env.test?
 
-CSV.open("export_pvc_determined_families_#{TimeKeeper.date_of_record.strftime('%m_%d_%Y')}.csv", "w") do |csv|
+CSV.open("export_pvc_determined_families_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}_#{Time.now.in_time_zone('Eastern Time (US & Canada)').strftime('%H:%M:%S')}.csv", "w") do |csv|
   csv << [
       "Primary Person Hbx ID",
       "Applicant Person Hbx ID",
       "Most recent Determined #{assistance_year} Application Hbx ID",
       "Is SSN Present",
       "Non ESI Evidence Title",
+      "Workflow Transition At",
       "Workflow Transition From State",
       "Workflow Transition To State",
+      "Verification History Date of Action",
       "Verification History Action",
       "Verification History Update Reason",
       "Verification History Updated By",
@@ -28,9 +30,14 @@ CSV.open("export_pvc_determined_families_#{TimeKeeper.date_of_record.strftime('%
       "PVC Determination"
       ]
 
-  counter = 0
+  families_counter = 0
+  applicants_counter = 0
+
+  puts "Processing families" unless Rails.env.test?
 
   family_ids.each do |family_id|
+    families_counter += 1
+    puts "Processing family_id: #{family_id} and index at #{families_counter}" unless Rails.env.test?
     family = Family.where(:_id => family_id).first
     primary_person = family.primary_person
 
@@ -47,11 +54,10 @@ CSV.open("export_pvc_determined_families_#{TimeKeeper.date_of_record.strftime('%
       next if non_esi_evidence.blank?
       request_result = non_esi_evidence.request_results.max_by(&:date_of_action)
       pvc_determination = request_result&.result || 'no pvc result found'
-      workflow_transition = non_esi_evidence.workflow_state_transitions.max_by(&:created_at)
-
+      workflow_transition = non_esi_evidence.workflow_state_transitions.max_by(&:transition_at)
       verification_history = non_esi_evidence.verification_histories.max_by(&:date_of_action)
 
-      counter += 1
+      applicants_counter += 1
 
       csv << [
           primary_person.hbx_id,
@@ -59,16 +65,18 @@ CSV.open("export_pvc_determined_families_#{TimeKeeper.date_of_record.strftime('%
           determined_application.hbx_id,
           applicant.encrypted_ssn.present?,
           non_esi_evidence.title,
+          workflow_transition&.transition_at,
           workflow_transition&.from_state,
           workflow_transition&.to_state,
+          verification_history&.date_of_action,
           verification_history&.action,
           verification_history&.update_reason,
           verification_history&.updated_by,
-          request_result&.created_at,
+          request_result&.date_of_action,
           pvc_determination
       ]
     end
   end
-
-  p "processed #{counter} families"  unless Rails.env.test?
+  p "processed #{applicants_counter} applicants"  unless Rails.env.test?
+  p "processed #{families_counter} families"  unless Rails.env.test?
 end
