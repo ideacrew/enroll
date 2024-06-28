8cf8a50769fd4bcb1d6b2ea4091852e0ecf2ed04
Charlie Parker <charlie.parker@ideacrew.com>
2024-06-28 07:59:39 -0400

    update exception structure to avoid full underline by breaking out substrings

--- db/seedfiles/translations/en/dc/exceptions.rb
+++ db/seedfiles/translations/en/dc/exceptions.rb
@@ -1,4 +1,6 @@
 EXCEPTIONS_TRANSLATIONS = {
-  "en.exceptions.show" => "<h3>We weren't able to process your request.</h3><br> Please call the %{contact_center_name} at %{contact_center_phone_number} (TTY: %{contact_center_tty_number}) <br><a href=%{site_home_business_url} class='btn btn-primary'>My Account</a>",
   "en.exceptions.valid_birthdate" => "Please provide a valid birthdate."
+  "en.exceptions.unable_to_process" => "We weren't able to process your request.",
+  "en.exceptions.please_call" => "Please call the %{contact_center_name} at",
+  "en.exceptions.go_to_my_account" => "My Account",
 }
--- db/seedfiles/translations/en/me/exceptions.rb
+++ db/seedfiles/translations/en/me/exceptions.rb
@@ -1,4 +1,6 @@
 EXCEPTIONS_TRANSLATIONS = {
-  "en.exceptions.show" => "<h1>We weren't able to process your request.</h1><br> <p><u>Please call the %{contact_center_name} at %{contact_center_phone_number} (TTY: %{contact_center_tty_number})</u></p> <br><a href=%{login_url} class='btn btn-default'>Go to My Account</a>",
   "en.exceptions.valid_birthdate" => "Please provide a valid birthdate."
+  "en.exceptions.unable_to_process" => "We weren't able to process your request.",
+  "en.exceptions.please_call" => "Please call the %{contact_center_name} at",
+  "en.exceptions.go_to_my_account" => "Go to My Account",
 }
