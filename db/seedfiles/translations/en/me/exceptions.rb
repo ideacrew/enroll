dd6ded90029f7237da475dc5fc80043c152bb314
Charlie Parker <charlie.parker@ideacrew.com>
2024-06-28 08:04:17 -0400

    fix broken translation file

--- db/seedfiles/translations/en/me/exceptions.rb
+++ db/seedfiles/translations/en/me/exceptions.rb
@@ -1,5 +1,5 @@
 EXCEPTIONS_TRANSLATIONS = {
-  "en.exceptions.valid_birthdate" => "Please provide a valid birthdate."
+  "en.exceptions.valid_birthdate" => "Please provide a valid birthdate.",
   "en.exceptions.unable_to_process" => "We weren't able to process your request.",
   "en.exceptions.please_call" => "Please call the %{contact_center_name} at",
   "en.exceptions.go_to_my_account" => "Go to My Account",
