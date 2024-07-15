1e1b10a9d25df7d81ee703759583a4363ad55a93
Brad Bodine <bbodine1@gmail.com>
2024-07-15 15:04:58 -0500

    fix selectric reactivating

--- app/assets/javascripts/ui-components/sidebars/plan_filters.js
+++ app/assets/javascripts/ui-components/sidebars/plan_filters.js
@@ -101,19 +101,6 @@ function clearAll() {
     inputs[i].checked = false;
     inputs[i].value = '';
   }
-  // Load only on plan_shopping page
-  if (window.location.pathname.split('/')[2] == 'plan_shoppings') {
-    // Select options -- WISH LIST GET RID OF SELECTRIC TO REMOVE JQUERY RELIANCE --
-    $('#filter-sidebar select.plan-carrier-selection-filter')
-      .prop('selectedIndex', 0)
-      .selectric('refresh');
-    $('#filter-sidebar select.plan-hsa-eligibility-selection-filter')
-      .prop('selectedIndex', 0)
-      .selectric('refresh');
-    $('#filter-sidebar select.plan-osse-eligibility-selection-filter')
-      .prop('selectedIndex', 0)
-      .selectric('refresh');
-  }
 
   // Clear stored values
   filterParams.selectedMetalLevels = [];
