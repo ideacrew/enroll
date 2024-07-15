5f667d779653171f491d5bdb6931e453cdd41b5e
Kristin Merbach <kristin@kristinmerbach.com>
2024-07-15 16:37:41 -0700

    health coverage fixes

--- components/financial_assistance/app/assets/javascripts/financial_assistance/benefit.js
+++ components/financial_assistance/app/assets/javascripts/financial_assistance/benefit.js
@@ -39,8 +39,12 @@ function startEditing(parentContainer) {
 
 // re-enable and raise the opacity of the form post editing
 function stopEditing() {
+  $('#nav-buttons a').removeClass('disabled');
+  $('.driver-question, .instruction-row, .add_new_benefit_kind', '.benefit').removeClass('disabled');
   $('.disabled a').removeAttr('tabindex');
+  $('.disabled a').removeClass('disabled');
   $('.disabled input:not(input[type=submit]), .disabled a').removeAttr('disabled');
+  $('.driver-question input:not(input[type=submit]), .disabled a').removeAttr('disabled');
   $('.driver-question, .instruction-row, .disabled a, .benefits-list, #nav-buttons a, .benefit, .add_new_benefit_kind').removeClass('disabled');
 };
 
