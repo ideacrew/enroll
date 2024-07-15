c587097489f926da319e62d5fcaf7cf044964cd3
Kristin Merbach <kristin@kristinmerbach.com>
2024-07-15 10:03:20 -0700

    started calculator plus lots of cleanup

--- components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
@@ -109,7 +109,11 @@ module FinancialAssistance
         @application.assign_attributes(permit_params(params[:application]))
 
         if @application.save
-          redirect_to submit_and_publish_application_redirect_path[:path], flash: submit_and_publish_application_redirect_path[:flash]
+          if submit_and_publish_application_redirect_path[:flash].present?
+            redirect_to submit_and_publish_application_redirect_path[:path], flash: submit_and_publish_application_redirect_path[:flash]
+          else
+            redirect_to submit_and_publish_application_redirect_path[:path]
+          end
         else
           @application.save!(validate: false)
           flash[:error] = build_error_messages(@application).join(", ")
