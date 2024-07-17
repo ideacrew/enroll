8aa37e3263ad537f7e45befe085995867672d791
jayreddy519 <jayanth.rordev@gmail.com>
2024-07-17 14:16:14 -0400

    latest updates

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -1410,7 +1410,7 @@ end
 
 Then(/the continue button has a data disabled attribute$/) do
   continue_button = find('a.btn.btn-lg.btn-primary.btn-block', visible: false)['data-disable-with']
-  expect(continue_button).to eql('continue')
+  expect(continue_button).to eql('Continue')
 end
 
 And(/^the Continue button is visible on Account Setup page/i) do
