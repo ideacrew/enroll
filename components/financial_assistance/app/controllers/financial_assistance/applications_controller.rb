82399131d75b6e393798636ef6241ffe01b1f86a
Kristin Merbach <kristin@kristinmerbach.com>
2024-07-18 21:06:22 -0700

    faa error pages to bs4

--- components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
@@ -6,7 +6,7 @@ module FinancialAssistance
     before_action :set_current_person
     before_action :set_family
     before_action :find_application, :except => [:index, :index_with_filter, :new, :review, :raw_application]
-    before_action :enable_bs4_layout, only: [:application_year_selection, :application_checklist, :edit, :eligibility_results, :review_and_submit, :submit_your_application, :wait_for_eligibility_response, :preferences] if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
+    before_action :enable_bs4_layout, only: [:application_year_selection, :application_checklist, :edit, :eligibility_results, :review_and_submit, :submit_your_application, :wait_for_eligibility_response, :preferences, :application_publish_error, :eligibility_response_error] if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
 
     around_action :cache_current_hbx, :only => [:index_with_filter]
 
@@ -282,7 +282,7 @@ module FinancialAssistance
       authorize @application, :application_publish_error?
       save_faa_bookmark(request.original_url)
       set_admin_bookmark_url
-
+      @override_flash = true if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
       respond_to :html
     end
 
