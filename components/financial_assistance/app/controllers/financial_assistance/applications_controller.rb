dd28d298c2893df27555a4ff41fd26f207acbe2e
Charlie Parker <charlie.parker@ideacrew.com>
2024-07-22 14:40:22 -0400

    bs4 home applications (#4032)
    * initial UI work - add flag switch in controller, refactor view for new mockup, add nav for FA
    
    mc
    
    * margin and capitalization cleanup, add breakpoint support for id/badge table column
    
    * fix indentation
    
    * add current badge
    
    mc
    
    * add modal and remove inline events from legacy
    
    mc
    
    * separate out legacy script and html
    
    mc
    
    * dropdown work
    
    mc
    
    * add actions column handling for sub md breakpoints
    
    * hide disabled actions
    
    * add bottom padding for badge
    
    * move badge div into if
    
    * pad top of badge and actions instead of bottom of id and badge
    
    * indentation fix
    
    * remove dropdown repositioning
    
    * allow table horizontal scrolling
    
    mc
    
    * add index for bs4
    
    * add cuke for filter page
    
    * fix button alignment
    
    * dropdown options refactor, add no options logic
    
    * dropdown cleanup
    
    * translation work, add no actions styling
    
    mc
    
    * move dropdown logic into dedicated helper
    
    * further dropdown refactor
    
    * remove debug
    
    * add comment to statics
    
    * rename args
    
    * comment update
    
    * remove redundant translation
    
    * add fallback for submission
    
    * use bootstrap class for table scrolling
    
    mc
    
    * refactor of attrs
    
    * oe warning refactor
    
    * table and nav fixes from manage family
    
    mc
    
    * use class for no actions
    
    * update partial for class
    
    * remove button font size
    
    * move details string to below applications section
    
    * restore missing title
    
    * update class name for clarity
    
    * fix wave empty header
    
    * fix tribe fields

--- components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
@@ -6,7 +6,7 @@ module FinancialAssistance
     before_action :set_current_person
     before_action :set_family
     before_action :find_application, :except => [:index, :index_with_filter, :new, :review, :raw_application]
-    before_action :enable_bs4_layout, only: [:application_year_selection, :application_checklist, :edit, :eligibility_results, :review_and_submit, :review, :submit_your_application, :wait_for_eligibility_response, :preferences, :application_publish_error, :eligibility_response_error] if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
+    before_action :enable_bs4_layout, only: [:application_year_selection, :application_checklist, :edit, :eligibility_results, :review_and_submit, :review, :submit_your_application, :wait_for_eligibility_response, :preferences, :application_publish_error, :eligibility_response_error, :index, :index_with_filter] if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
 
     around_action :cache_current_hbx, :only => [:index_with_filter]
 
@@ -385,7 +385,7 @@ module FinancialAssistance
       case action_name
       when "edit", "submit_your_application", "preferences", "review_and_submit", "review", "step", "eligibility_response_error", "application_publish_error"
         EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "financial_assistance_progress" : "financial_assistance_nav"
-      when "application_year_selection", "application_checklist"
+      when "application_year_selection", "application_checklist", "index", "index_with_filter"
         EnrollRegistry.feature_enabled?(:bs4_consumer_flow) ? "financial_assistance_progress" : "financial_assistance"
       when "eligibility_results"
         if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
