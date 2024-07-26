2fb6ddc845c2c33eeb5ec7abfcc9b3e5075ebb8b
Diana Quisbert <diana.quisbert@ideacrew.com>
2024-07-26 12:39:08 -0400

    update selector

--- features/step_definitions/integration_steps.rb
+++ features/step_definitions/integration_steps.rb
@@ -761,8 +761,7 @@ Then(/^.+ should see ([^"]*) dependents*$/) do |n|
 end
 
 When(/^.+ clicks? Add Member$/) do
-  find('interaction-click-control-add-new-person').click
-  #find(EmployeeFamilyInformation.add_new_person).click
+  find(EmployeeFamilyInformation.add_new_person).click
 end
 
 Then(/^.+ should see the new dependent form$/) do
