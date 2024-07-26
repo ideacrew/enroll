71899da7b905b38108efbb70b97efae27e98bf31
Charlie Parker <charlie.parker@ideacrew.com>
2024-07-26 15:11:58 -0400

    fix edit and submit for personal

--- app/assets/javascripts/employee_dependent.js
+++ app/assets/javascripts/employee_dependent.js
@@ -12,15 +12,15 @@ $(document).ready(function() {
 $(document).on('change', '#dependent_same_with_primary', function(){
   var target = $(this).parents('#dependent-address').find('#dependent-home-address-area');
   if ($(this).is(':checked')) {
-    $(target).hide();
-    $(this).parents('#dependent-address').find('#home-info').hide();
+    $(target).addClass('hidden');
+    $(this).parents('#dependent-address').find('#home-info').addClass('hidden');
     $("#dependent_addresses_0_address_1, #dependent_addresses_0_zip, #dependent_addresses_0_city").each(function() {
       $(this).prop('required', false);
     })
     $(target).find("#address_info .address_required").removeAttr('required');
   } else {
-    $(target).show();
-    $(this).parents('#dependent-address').find('#home-info').show();
+    $(target).removeClass('hidden');
+    $(this).parents('#dependent-address').find('#home-info').removeClass('hidden');
     $("#dependent_addresses_0_address_1, #dependent_addresses_0_zip, #dependent_addresses_0_city").each(function() {
       $(this).prop('required', true);
     })
