358337b5a77ed9ef1f060c5741a01be98a3c5959
Kristin Merbach <kristin@kristinmerbach.com>
2024-08-01 13:23:21 -0700

    help me sign up appscan issue

--- app/assets/javascripts/help_me_sign_up.js
+++ app/assets/javascripts/help_me_sign_up.js
@@ -25,23 +25,8 @@ $.ajax({
     broker_status = JSON.parse(response)
     var status = broker_status['status']
     var broker = broker_status['broker']
-    var broker_id = broker_status['broker_id']
-    if (broker_id && broker_id != '') {
-      $('#consumer_brokers_widget', "#inbox_provider_form", "#active_broker_tab button").addClass('hide')
-      $('#consumer_brokers_widget').html(broker)
-      $("#inbox_provider_form").addClass('hide')
-      $("#active_broker_tab button").addClass('hide')
-      $("#broker_index_view").addClass('hide')
-      $(".warning-body").html(status)
-      $('#active_broker_tab .alert').removeClass('alert-warning').addClass('alert-success')
-      $('#active_broker_tab .icon').removeClass('warning-icon').addClass('success-icon')
-      $("#broker_show_" + broker_id).removeClass('hide')
-      $('#consumer_brokers_widget').removeClass('hide')
-      $('.go-to-expert').removeClass('hide')
-    } else {
-      $('#consumer_brokers_widget').html(broker)
-      $('#help_index_status').html(status).removeClass('hide')
-    }
+    $('#help_index_status').html(status).removeClass('hide')
+    $('#consumer_brokers_widget').html(broker)
   });
 })
 
@@ -60,4 +45,25 @@ $(document).on('click', '#back_to_help', function(){
   $('#help_status').html('')
   $('#help_sign_up_title').removeClass('hide');
   $('#help_from_expert_title').addClass('hide');
+})
+
+$(document).on('click', '.select-broker', function(){
+  $.ajax({
+    type: 'GET',
+    data: {assister: this.getAttribute('data-assister'), broker: this.getAttribute('data-broker'),
+            person: $('#help_requestor').html(), email: $('#help_requestor_email').html(),
+            first_name: $('#person_first_name').val(), last_name: $('#person_last_name').val(),
+            ssn: $('#person_ssn').val(), dob: $('#jq_datepicker_ignore_person_dob').val()
+          },
+    url: '/exchanges/hbx_profiles/request_help.html?my_expert=true',
+  }).done(function(response) {
+    broker_status = JSON.parse(response);
+    var status = broker_status['status']
+    $('#inbox_provider_form').hide();
+    $('#active_broker_tab .alert').removeClass('alert-warning').addClass('alert-success')
+    $('#active_broker_tab .icon').removeClass('warning-icon').addClass('success-icon')
+    $('#active_broker_tab .warning-body').text(status)
+    $('a.go-to-expert, button').removeClass('hidden').removeClass('hide');
+    $("#active_broker_tab button").addClass('hide')
+  });
 })
