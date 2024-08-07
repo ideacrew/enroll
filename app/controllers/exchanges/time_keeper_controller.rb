4b4eba162bcb2840d160b0e40377cc690adf6786
vkghub <vishuk1201@gmail.com>
2024-08-07 17:45:24 -0400

    setup time jump feature

---
+++ app/controllers/exchanges/time_keeper_controller.rb
@@ -0,0 +1,18 @@
+module Exchanges
+class TimeKeeperController < ApplicationController
+  include EventSource::Command
+
+  def hop_to_date
+    binding.irb
+    authorize HbxProfile, :modify_admin_tabs? # Rename this to something more meaningful for this action
+    binding.irb
+    date = Date.parse( params[:forms_time_keeper][:date_of_record])
+    binding.irb
+    TimeKeeper.instance.set_date_of_record(date)
+    flash[:notice] = "Time Hop to " + TimeKeeper.date_of_record.strftime("%m/%d/%Y")
+    
+    redirect_to configuration_exchanges_hbx_profiles_path
+  end
+
+end
+end
