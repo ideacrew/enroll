8325d57ce23fc15ecf0ef1e9776014bdd5373557
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2024-08-07 08:09:40 -0700

    header and footer cleanup (#4176)
    * header and footer cleanup
    
    * fix rubocop
    
    * revise based on PR comments
    
    * more header cleanup
    
    * include missing file
    
    * don't display text til logged in, don't link unless portal available
    
    * push file change missed

--- app/helpers/bs4/bs4_header_helper.rb
+++ app/helpers/bs4/bs4_header_helper.rb
@@ -32,12 +32,11 @@ module Bs4
         else
           link_to(l10n("layout.header.role.employee"), main_app.family_account_path)
         end
-      elsif (controller_path.include?("insured") && current_user.try(:has_consumer_role?)) ||
-            (EnrollRegistry.feature_enabled?(:financial_assistance) && controller_path.include?("financial_assistance") && current_user.try(:has_consumer_role?))
+      elsif current_user.try(:has_consumer_role?)
         if current_user.identity_verified_date.present?
           link_to(l10n("layout.header.role.individual_and_family"), main_app.family_account_path)
         else
-          link_to(l10n("layout.header.role.individual_and_family"), 'javascript:;')
+          l10n("layout.header.role.individual_and_family")
         end
       # rubocop:disable Lint/DuplicateBranch
       elsif current_user.try(:has_broker_agency_staff_role?) && controller_path.exclude?('general_agencies') && controller_path.exclude?('employers')
@@ -51,6 +50,49 @@ module Bs4
         end
       elsif current_user.try(:has_employer_staff_role?)
         link_to(l10n("layout.header.role.employer"), benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
+      end
+    end
+
+    def my_portal_link(controller) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      ltext = l10n("layout.header.portal")
+      if current_user.nil?
+        nil
+      elsif current_user.try(:has_hbx_staff_role?)
+        link_to(ltext, main_app.exchanges_hbx_profiles_root_path)
+      elsif display_i_am_broker_for_consumer?(current_user.person) && controller_path.exclude?('general_agencies')
+        link_to(ltext, get_broker_profile_path)
+      elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
+        link_to(ltext, main_app.home_exchanges_agents_path)
+      elsif current_user.person&.active_employee_roles&.any?
+        if controller_path.include?('broker_agencies')
+          link_to(ltext, get_broker_profile_path)
+        elsif controller_path.include?('general_agencies')
+          link_to(ltext, benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
+        elsif controller == 'employer_profiles' || controller_path.include?('employers')
+          #current user has both broker_agency staff role and employee role but not employer_staff_roles
+          if current_user.person.active_employer_staff_roles.present?
+            employer_profile_path = benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home')
+            link_to(ltext, employer_profile_path)
+          elsif current_user.try(:has_broker_agency_staff_role?)
+            link_to(ltext, get_broker_profile_path)
+          end
+        else
+          link_to(ltext, main_app.family_account_path)
+        end
+      elsif current_user.try(:has_consumer_role?)
+        link_to(ltext, main_app.family_account_path) if current_user.identity_verified_date.present?
+      # rubocop:disable Lint/DuplicateBranch
+      elsif current_user.try(:has_broker_agency_staff_role?) && controller_path.exclude?('general_agencies') && controller_path.exclude?('employers')
+        link_to(ltext, get_broker_profile_path)
+      # rubocop:enable Lint/DuplicateBranch
+      elsif current_user.try(:has_general_agency_staff_role?)
+        if current_user.try(:has_employer_staff_role?) && controller_path.include?('employers')
+          link_to(ltext, benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
+        else
+          link_to(ltext, benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.active_general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
+        end
+      elsif current_user.try(:has_employer_staff_role?)
+        link_to(ltext, benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
       end
     end
 
