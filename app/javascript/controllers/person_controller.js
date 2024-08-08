e32df19b3cd478c032fbb4ca9406af71216153d0
TristanB17 <tristan0bambauer@gmail.com>
2024-08-08 03:56:19 -0600

    complete initial pass on new ssn field reqs

--- app/javascript/controllers/person_controller.js
+++ app/javascript/controllers/person_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "stimulus"
 import axios from 'axios'
 
 export default class extends Controller {
-
   showSsn(event) {
     const target = $(event.target);
     const personId = target.data('id');
