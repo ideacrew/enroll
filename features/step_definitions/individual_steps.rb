3ce4e96891a1759b762d04b66506af46525e51ec
RyanEddyIC <ryan.eddy@ideacrew.com>
2024-08-09 14:44:51 -0400

    add conditionals for step reuse in other tests

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -639,7 +639,8 @@ And(/.+ selects a plan on plan shopping page/) do
   wait_for_ajax(3, 2)
   expect(page).to have_content "Choose Plan"
   sleep 5
-  unless find(EmployeePersonalInformation.continue_btn).disabled?
+  continue_btn = find_all(EmployeePersonalInformation.continue_btn)
+  if continue_btn.any? && !continue_btn.first&.disabled?
     find(EmployeePersonalInformation.continue_btn).click
     wait_for_ajax(3, 2)
   end
