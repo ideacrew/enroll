a0323b721e3c2ee366aed0e24f66168591b22817
Charlie Parker <charlie.parker@ideacrew.com>
2024-08-14 14:58:44 -0400

    allow submit to not be listened

--- app/assets/javascripts/time_jump.js
+++ app/assets/javascripts/time_jump.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('submit').addEventListener('click', function() {
-    const dateValue = document.getElementById('hop_to_date_date_of_record').value;
+  if ($('#submit').length) {
+    document.getElementById('submit').addEventListener('click', function() {
+      const dateValue = document.getElementById('hop_to_date_date_of_record').value;
 
-    // Validate the date value
-    if (isNaN(Date.parse(dateValue))) {
-      alert('Invalid date format');
-      return;
-    }
+      // Validate the date value
+      if (isNaN(Date.parse(dateValue))) {
+        alert('Invalid date format');
+        return;
+      }
 
-    // Parse the date value
-    const date = new Date(dateValue);
-    const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Add leading zero
-    const day = ('0' + date.getDate()).slice(-2); // Add leading zero
-    const formattedDate = `${year}-${month}-${day}`;
+      // Parse the date value
+      const date = new Date(dateValue);
+      const year = date.getFullYear();
+      const month = ('0' + (date.getMonth() + 1)).slice(-2); // Add leading zero
+      const day = ('0' + date.getDate()).slice(-2); // Add leading zero
+      const formattedDate = `${year}-${month}-${day}`;
 
-    document.getElementById('selectedDate').textContent = formattedDate;
-    document.getElementById('hiddenDateField').value = formattedDate;
-  });
+      document.getElementById('selectedDate').textContent = formattedDate;
+      document.getElementById('hiddenDateField').value = formattedDate;
+    });
+  }
 });
