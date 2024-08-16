e888c7653e71f6b7e72658f49d9db7aa59fb3e1e
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-08-16 12:34:49 -0400

    adds script to trigger bulk cv validation (#4320)
    * adds script to trigger bulk cv validation
    
    * add error logging for failed family processing
    
    * logs the error message
    
    * fixes a typo

--- app/domain/operations/private/families/bulk_cv_validation/request.rb
+++ app/domain/operations/private/families/bulk_cv_validation/request.rb
@@ -61,11 +61,15 @@ module Operations
               csv << ['Family HBX ID', 'Family Updated At', 'Job ID', 'Publish Result']
 
               families.each do |family|
+                logger.info "***** Processing family hbx_assigned_id: #{family.hbx_assigned_id}"
                 csv << build_and_publish_event(family, job_id, logger)
+                logger.info "***** Processed family hbx_assigned_id: #{family.hbx_assigned_id}"
+              rescue StandardError => e
+                logger.error "***** Failed to process family hbx_assigned_id: #{family.hbx_assigned_id}, error: #{e.message}"
               end
             end
 
-            msg = "Events triggered for all the families. CSV file named #{file_name} is generated with the results."
+            msg = "----- Events triggered for all the families. CSV file named #{file_name} is generated with the results. -----"
             logger.info msg
             Success(msg)
           end
