fe135c0e84e3c3957dd0f908a55e67e9828a2588
Brad Bodine <bbodine1@gmail.com>
2024-08-19 09:19:58 -0500

    update "show all enrollments" button (#4332)
    * format js file
    
    * update "Show All Enrollments" button on enrollment history page - no toggle

--- app/javascript/src/enrollment.js
+++ app/javascript/src/enrollment.js
@@ -1,33 +1,35 @@
 // For when families home page is refreshed when user on it
-document.addEventListener("DOMContentLoaded", function () {
-  const initiallyHiddenEnrollmentPanels = document.getElementsByClassName("initially_hidden_enrollment");
-  const enrollmentToggleCheckbox = document.getElementById("display_all_enrollments");
-  const enrollmentToggleButton = document.getElementById("display_all_enrollments_btn");
+document.addEventListener('DOMContentLoaded', function () {
+  const initiallyHiddenEnrollmentPanels = document.getElementsByClassName(
+    'initially_hidden_enrollment'
+  );
+  const enrollmentToggleCheckbox = document.getElementById(
+    'display_all_enrollments'
+  );
+  const enrollmentToggleButton = document.getElementById(
+    'display_all_enrollments_btn'
+  );
 
   enrollmentToggleButton.addEventListener('click', toggleDisplayEnrollments);
   enrollmentToggleCheckbox.addEventListener('click', toggleDisplayEnrollments);
 
   function toggleDisplayEnrollments(event) {
-    if (event.target.type == "submit") {
-      enrollmentToggleCheckbox.checked = !enrollmentToggleCheckbox.checked;
+    for (const panel of initiallyHiddenEnrollmentPanels) {
+      panel.classList.remove('hidden');
     }
 
-    if (enrollmentToggleCheckbox.checked) {
-      for (let i = 0; i < initiallyHiddenEnrollmentPanels.length; i++) {
-        initiallyHiddenEnrollmentPanels[i].classList.remove("hidden");
-        enrollmentToggleButton.innerText = l10n("");
-      }
-    } else {
-      for (let i = 0; i < initiallyHiddenEnrollmentPanels.length; i++) {
-        initiallyHiddenEnrollmentPanels[i].classList.add("hidden");
-        enrollmentToggleButton.innerText = "Show All Enrollments";
-      }
-    }
-  };
+    enrollmentToggleButton.remove();
+  }
 
   // For when family home page loaded through clicking off of the families index page
-  if (enrollmentToggleCheckbox != null || enrollmentToggleCheckbox != undefined) {
-    enrollmentToggleCheckbox.addEventListener('click', toggleDisplayEnrollments);
+  if (
+    enrollmentToggleCheckbox != null ||
+    enrollmentToggleCheckbox != undefined
+  ) {
+    enrollmentToggleCheckbox.addEventListener(
+      'click',
+      toggleDisplayEnrollments
+    );
     enrollmentToggleButton.addEventListener('click', toggleDisplayEnrollments);
-  };
-})
+  }
+});
