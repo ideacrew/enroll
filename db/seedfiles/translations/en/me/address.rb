7e06c8f5dd4e9223b4181b834502e3384a0c9285
TristanB17 <tristan0bambauer@gmail.com>
2024-08-20 01:28:43 -0600

    add missing translations

--- db/seedfiles/translations/en/me/address.rb
+++ db/seedfiles/translations/en/me/address.rb
@@ -4,5 +4,16 @@ ADDRESS_TRANSLATIONS = {
     :'en.address.address_2_text' => 'ADDRESS LINE 2 (APT, UNIT, ETC.)',
     :'en.address.city_text' => 'City *',
     :'en.address.select_state_text' => 'Select State *',
-    :'en.address.zip_text' => 'ZIP *'
+    :'en.address.zip_text' => 'ZIP *',
+    :'en.address.address_1_error' => 'Please provide a valid address.',
+    :'en.address.address_1_placeholder' => '123 Main St',
+    :'en.address.city_error' => 'Please provide a valid city.',
+    :'en.address.state_error' => 'Please select a state.',
+    :'en.address.zip_code_error' => 'Please provide a valid zipcode.',
+    :'en.address.address_kind_error' => 'Please select an address kind.',
+    :'en.address.address_kind_select' => 'Select Address Kind',
+    :'en.phone_number.error' => 'Please provide a valid phone number.',
+    :'en.phone_number.extension' => 'Phone Number Extension',
+    :'en.address.agency.office_location' => 'Office Location',
+    :'en.address.agency.primary_office_location' => 'Primary Office Location'
 }.freeze
