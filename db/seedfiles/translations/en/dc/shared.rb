5795bf94c79c03610f1392deef9372734e162fb9
Tristan <33517218+TristanB17@users.noreply.github.com>
2024-08-21 11:17:41 -0600

    bootstrap 4 broker form updates (#4337)
    * registration page
    
    * more broker setup
    
    * set new feature flag
    
    * more work on brokers
    
    * agency profile pages
    
    * continue broker work
    
    * dont have secondary modal for dependent table (#4273)
    
    * don't open secondary modal when in comparison modal
    
    * forgot to commit a file
    
    * rubocop
    
    * fix spec by undoing rubocop suggestions
    
    * aptc calculator adjust amount applied (#4286)
    
    adjust the max amount of aptc to account for if the aptc is higher than the premium
    
    * aptc calc results should have round values (#4288)
    
    * fix rounding issue
    
    * fix rounding
    
    * use default cancel translation sting (#4290)
    
    * add method for determining bootstrap
    
    * consolidate with broker_setup
    
    * rewrite slim as html
    
    * remove method
    
    * fix render error
    
    * initial pass at broker forms
    
    * readd auth to update broker
    
    * revert minor changes
    
    * render correct layout when bs4 not active
    
    * update spec adhere to new validations
    
    * make rubocop requested changes
    
    * troubleshoot translation files
    
    * add missing comma
    
    * add minor fixes
    
    * revert changes to messages.js
    
    * make changes for cucumber
    
    * update features for bs4 updates
    
    * update features for bs4 updates 2
    
    * do more cucumber tinkering
    
    * remove ssn changes
    
    * ssn not taking
    
    * readd ssn to table
    
    * troubleshoot cucumbers
    
    * fix cucumber
    
    * fix possible rubocop issue
    
    * remove redundant return
    
    * reformat rubocop issues
    
    * remove consumer_flow from employer_profile
    
    * fix broken cucumber
    
    * remove trailing whitespace
    
    * disable bs4_consumer_flow for employer profile
    
    * fix cucumber but really this time
    
    * fix issue with slim to erb conversion
    
    * remove unnecessary code
    
    * reenable removal of address from edit broker
    
    * add more unique translations
    
    * add missing translations
    
    * remove unused change
    
    * remove console.log, prevent default on addLocation call
    
    ---------
    
    Co-authored-by: Kristin Merbach <kristin@kristinmerbach.com>
    Co-authored-by: kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>

--- db/seedfiles/translations/en/dc/shared.rb
+++ db/seedfiles/translations/en/dc/shared.rb
@@ -1,7 +1,7 @@
 SHARED_TRANSLATIONS = {
     "en.address_line_1" => "ADDRESS LINE 1",
     "en.address_line_2" => "ADDRESS LINE 2",
-    "en.city" => "CITY",
+    "en.city" => "City",
     "en.zip" => "ZIP",
     "en.new_address_shared" => "NEW ADDRESS",
     "en.home_address_shared" => "Home Address",
