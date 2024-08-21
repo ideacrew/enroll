19deb41906ffb6ff26d7ad15c0836dc6ef010229
Tristan <33517218+TristanB17@users.noreply.github.com>
2024-08-21 11:17:41 -0600

    bootstrap 4 broker form updates (#4337)
    * registration page
    
    * more broker setup
    
    * set new feature flag
    
    * more work on brokers
    
    * agency profile pages
    
    * continue broker work
    
    * dont have secondary modal for dependent table (#4273)
    
    * don't open secondary modal when in comparison modal
    
    * forgot to commit a file
    
    * rubocop
    
    * fix spec by undoing rubocop suggestions
    
    * aptc calculator adjust amount applied (#4286)
    
    adjust the max amount of aptc to account for if the aptc is higher than the premium
    
    * aptc calc results should have round values (#4288)
    
    * fix rounding issue
    
    * fix rounding
    
    * use default cancel translation sting (#4290)
    
    * add method for determining bootstrap
    
    * consolidate with broker_setup
    
    * rewrite slim as html
    
    * remove method
    
    * fix render error
    
    * initial pass at broker forms
    
    * readd auth to update broker
    
    * revert minor changes
    
    * render correct layout when bs4 not active
    
    * update spec adhere to new validations
    
    * make rubocop requested changes
    
    * troubleshoot translation files
    
    * add missing comma
    
    * add minor fixes
    
    * revert changes to messages.js
    
    * make changes for cucumber
    
    * update features for bs4 updates
    
    * update features for bs4 updates 2
    
    * do more cucumber tinkering
    
    * remove ssn changes
    
    * ssn not taking
    
    * readd ssn to table
    
    * troubleshoot cucumbers
    
    * fix cucumber
    
    * fix possible rubocop issue
    
    * remove redundant return
    
    * reformat rubocop issues
    
    * remove consumer_flow from employer_profile
    
    * fix broken cucumber
    
    * remove trailing whitespace
    
    * disable bs4_consumer_flow for employer profile
    
    * fix cucumber but really this time
    
    * fix issue with slim to erb conversion
    
    * remove unnecessary code
    
    * reenable removal of address from edit broker
    
    * add more unique translations
    
    * add missing translations
    
    * remove unused change
    
    * remove console.log, prevent default on addLocation call
    
    ---------
    
    Co-authored-by: Kristin Merbach <kristin@kristinmerbach.com>
    Co-authored-by: kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>

--- app/javascript/benefit_sponsors/controllers/office_locations_controller.js
+++ app/javascript/benefit_sponsors/controllers/office_locations_controller.js
@@ -10,6 +10,7 @@
 import { Controller } from "stimulus"
 
 let alreadyConnected = false;
+let bs4 = document?.documentElement?.dataset?.bs4;
 
 export default class extends Controller {
   static targets = [ "officeLocations", "officeLocation" ];
@@ -17,10 +18,12 @@ export default class extends Controller {
   connect() {
     if (this.element.getAttribute('data-controller-connected')) {
       alreadyConnected = true;
-    } else {
+    } else { 
       this.element.setAttribute('data-controller-connected', 'true');
-      document.getElementById('kindSelect')[0].remove();
-      document.getElementById('agency_organization_profile_attributes_office_locations_attributes_0_phone_attributes_kind')[0].remove();
+      if (bs4 != "true") {
+        document.getElementById('kindSelect')[0].remove();
+        document.getElementById('agency_organization_profile_attributes_office_locations_attributes_0_phone_attributes_kind')[0].remove();
+      }
     }
     this.officeLocationTargets.forEach(target => {
       target.classList.add("existing-location");
@@ -28,6 +31,8 @@ export default class extends Controller {
   }
 
   addLocation(event) {
+    event.preventDefault(); // call is made from a tag w/no href
+    if (bs4 == "true") event.stopImmediatePropagation();
     const locations = document.querySelectorAll('.js-office-location');
     const lastLocation = locations[locations.length-1];
     let emptyLocation = true;
@@ -40,16 +45,17 @@ export default class extends Controller {
     if (!emptyLocation) {
       event.preventDefault();
       //clone new location node, unhide remove button, modify name attribute
-      var newLocation = document.importNode(this.officeLocationTarget, true)
-      var totalLocations = document.importNode(this.officeLocationsTarget, true)
+      var newLocation = document.importNode(this.officeLocationTarget, true);
+      var totalLocations = document.importNode(this.officeLocationsTarget, true);
       // totalLocationsCount includes currently loaded OL form too
       var totalLocationsCount = totalLocations.querySelectorAll('.ol_title').length;
       delete newLocation.dataset.target;
       newLocation.querySelectorAll('.js-remove').forEach(function(element) {
-        element.remove()
+        element.remove();
       });
 
       newLocation.classList.add('new-location');
+      newLocation.classList.remove('existing-location');
 
       newLocation.querySelectorAll('.ol_title').forEach(function(element) {
         element.innerHTML = "Office Location"
@@ -57,28 +63,39 @@ export default class extends Controller {
 
       newLocation.querySelectorAll('input').forEach(function(input) {
         var name = input.getAttribute('name').replace('[0]', `[${totalLocationsCount}]`);
-        input.setAttribute('name', name)
-        input.value = ''
+        input.setAttribute('name', name);
+
+        if (bs4 == "true" && input.id == "phoneType") {
+          input.value = "work";
+        } else {
+          input.value = '';
+        }
       })
 
-      newLocation.querySelector('input[placeholder="ZIP"]').setAttribute('data-action', "");
-
-      newLocation.querySelector(".phone_number7").addEventListener('input', (event) => {
-        event.target.value = this.phoneMask(event.target.value);
-      });
+      if (bs4 == "true") {
+        newLocation.querySelector('a.remove_fields').classList.remove('hidden');
+        newLocation.querySelector('input[placeholder="00000"]').setAttribute('data-action', "");
+        newLocation.querySelector(".phone_number").addEventListener('input', (event) => {
+          event.target.value = this.fullPhoneMask(event.target.value);
+        });
+      } else {
+        newLocation.querySelector('input[placeholder="ZIP"]').setAttribute('data-action', "");
+        newLocation.querySelector(".phone_number7").addEventListener('input', (event) => {
+          event.target.value = this.phoneMask(event.target.value);
+        });
+      }
 
       newLocation.querySelectorAll('select').forEach(function(input) {
         var name = input.getAttribute('name').replace('[0]', `[${totalLocationsCount}]`);
-        input.setAttribute('name', name)
+        input.setAttribute('name', name);
 
         if (input.value != "work" && input.id != "kindSelect") {
-          input.value = ''
+          input.value = '';
         }
 
         if (input.id == "kindSelect") {
           input[0].remove();
         }
-
       })
 
       this.officeLocationsTarget.appendChild(newLocation);
@@ -92,7 +109,15 @@ export default class extends Controller {
       event.target.closest('.js-office-location').querySelectorAll('input[id="delete_location"]').forEach(function(input) {
         input.setAttribute('value', true)
       })
-      $(event.target).closest('.js-office-location').hide();
+      
+      // if .new-location is present, remove it
+      // else hide the .existing-location and mark it for deletion
+      let location = event.target.closest('.js-office-location');
+      if (bs4 == "true" && location.classList.contains('new-location')) {
+        location.remove();
+      } else {
+        $(event.target).closest('.js-office-location').hide();
+      }
     }
   }
 
@@ -100,5 +125,11 @@ export default class extends Controller {
     return phone.replace(/\D/g, '')
       .replace(/(\d{3})(\d{1,4})/, '$1-$2')
       .replace(/(-\d{4})\d+?$/, '$1');
+  }
+
+  fullPhoneMask(phone) {
+    let masked = phone.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    let value = !masked[2] ? masked[1] : '(' + masked[1] + ') ' + masked[2] + (masked[3] ? '-' + masked[3] : '');
+    return value;
   }
 }
