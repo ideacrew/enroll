ecd3dadefffb1a47701c667e3db13584d3b88882
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-08-26 17:51:07 -0400

    fixes missing trait issue

--- spec/domain/operations/reports/families/cv_validation_jobs/latest_report_spec.rb
+++ spec/domain/operations/reports/families/cv_validation_jobs/latest_report_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Operations::Reports::Families::CvValidationJobs::LatestReport do
     context 'when the jobs exist' do
       let(:job_id) { 'aksjh7d7gds7sg00000' }
       let(:create_jobs) do
-        FactoryBot.create_list(:cv_validation_job, 3, :success, job_id: job_id)
+        FactoryBot.create_list(:cv_validation_job, 3, job_id: job_id)
       end
 
       let(:csv_file_name) { Dir.glob("#{Rails.root}/latest_cv_validation_job_report_#{job_id}_*.csv").first }
