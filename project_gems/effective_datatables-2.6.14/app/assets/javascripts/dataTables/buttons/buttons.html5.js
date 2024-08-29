213323b114d43e8244bdab3f8d5e01214d351015
Kristin Merbach <kristin@kristinmerbach.com>
2024-08-29 15:35:51 -0700

    only use the first title element

--- project_gems/effective_datatables-2.6.14/app/assets/javascripts/dataTables/buttons/buttons.html5.js
+++ project_gems/effective_datatables-2.6.14/app/assets/javascripts/dataTables/buttons/buttons.html5.js
@@ -339,7 +339,7 @@ var _filename = function ( config, incExtension )
 	}
 
 	if ( filename.indexOf( '*' ) !== -1 ) {
-		filename = $.trim( filename.replace( '*', $('title').text() ) );
+		filename = $.trim( filename.replace( '*', $('title').first().text() ) );
 	}
 
 	// Strip characters which the OS will object to
@@ -380,7 +380,7 @@ var _title = function ( config )
 	}
 
 	return title.indexOf( '*' ) !== -1 ?
-		title.replace( '*', $('title').text() || 'Exported data' ) :
+		title.replace( '*', $('title').first().text() || 'Exported data' ) :
 		title;
 };
 
