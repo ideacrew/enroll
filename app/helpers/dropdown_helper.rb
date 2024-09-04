a45ceac84ae83b508366b22ceb80b873fcecdc33
Charlie Parker <charlie.parker@ideacrew.com>
2024-09-04 14:14:18 -0400

    add new dropdown types to helper, update dropdown construction in family data table to map to new types, filter out disableds, and use correct dropdown locals, add new aptc type for legacy and bs4

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -4,10 +4,10 @@
 module DropdownHelper
   def application_dropdowns(application)
     option_args = [
-      (['faa.applications.actions.update', edit_application_path(application), :default] if application.is_draft? || (application.imported? && current_user.has_hbx_staff_role?)),
-      (['faa.applications.actions.copy', copy_application_path(application), :default] unless do_not_allow_copy?(application, current_user)),
-      (['faa.applications.actions.view_eligibility', eligibility_results_application_path(application), :default] if application.is_determined? || application.is_terminated?),
-      (['faa.applications.actions.review', review_application_path(application), :default] if application.is_reviewable?)
+      ([l10n('faa.applications.actions.update'), edit_application_path(application), :default] if application.is_draft? || (application.imported? && current_user.has_hbx_staff_role?)),
+      ([l10n('faa.applications.actions.copy'), copy_application_path(application), :default] unless do_not_allow_copy?(application, current_user)),
+      ([l10n('faa.applications.actions.view_eligibility'), eligibility_results_application_path(application), :default] if application.is_determined? || application.is_terminated?),
+      ([l10n('faa.applications.actions.review'), review_application_path(application), :default] if application.is_reviewable?)
     ]
     option_args = add_hbx_only_dropdowns(application, option_args)
     construct_options(option_args)
@@ -17,19 +17,25 @@ module DropdownHelper
 
   # dropdown type link attributes
   DEFAULT = {data: {turbolinks: false}}.freeze
+  REMOTE = {remote: true}.freeze
+  REMOTE_EDIT_APTC_CSR = {class: "edit-aptc-csr-enabled", remote: true}.freeze
 
   def construct_options(options_args)
     options_args.compact.map { |option_args| construct_option(*option_args) }
   end
 
-  def construct_option(title_key, link, option_type)
-    {title: l10n(title_key), link: link, attributes: attribute_hash(option_type)}
+  def construct_option(title, link, option_type)
+    {title: title, link: link, attributes: attribute_hash(option_type)}
   end
 
   def attribute_hash(option_type)
     case option_type
     when :default
       ::DropdownHelper::DEFAULT.dup
+    when :remote
+      ::DropdownHelper::REMOTE.dup
+    when :remote_edit_aptc_csr
+      ::DropdownHelper::REMOTE_EDIT_APTC_CSR.dup
     end
   end
 
