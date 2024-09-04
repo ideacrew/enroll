35457ed0aa04ebb7cec3617b6f42ba297e09eda8
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2024-09-04 08:54:55 -0700

    getting tiles even closer, including dental (#4476)

--- db/seedfiles/translations/en/me/insured.rb
+++ db/seedfiles/translations/en/me/insured.rb
@@ -320,7 +320,7 @@ The I-94 number is also called the admissions number. It is an 11 character sequ
   :'en.hired_on' => "Hired On",
   :'en.plan_end' => "Plan End",
   :'en.enrollment_status' => "Enrollment Status",
-  :'en.enrollment_id' => "ID",
+  :'en.enrollment_id' => "Plan ID",
   :'en.coverage_waived' => "Coverage Waived",
   :'en.waiver_reason' => "Waiver Reason",
   :'en.insured.families.future_enrollment_termination_date' => "Future Enrollment Termination Date",
