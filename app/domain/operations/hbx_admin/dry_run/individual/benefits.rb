1ce7d88825d2db0217451e22a4f9c1008e7b90e6
vkghub <vishuk1201@gmail.com>
2024-09-05 15:58:56 -0400

    fetch more dry run values

--- app/domain/operations/hbx_admin/dry_run/individual/benefits.rb
+++ app/domain/operations/hbx_admin/dry_run/individual/benefits.rb
@@ -12,7 +12,7 @@ module Operations
           def call
             renewal_bcp, coverage_years = yield fetch_renewal_data
             products = yield fetch_products_for_year(coverage_years.first)
-            result = yield map_benefit_data(renewal_bcp, products)
+            result = yield map_benefit_data(renewal_bcp, products, coverage_years.first)
 
             Success([result, coverage_years])
           end
@@ -43,7 +43,10 @@ module Operations
             Success(products)
           end
 
-          def map_benefit_data(renewal_bcp, products)
+          def map_benefit_data(renewal_bcp, products, renewal_coverage_year)
+            create_renewals_on_date_change_feature = FinancialAssistanceRegistry[:create_renewals_on_date_change]
+            atleast_one_silver_plan_donot_cover_pediatric_dental_cost_feature = EnrollRegistry[:atleast_one_silver_plan_donot_cover_pediatric_dental_cost]
+binding.irb
             result = {
               l10n('admin_actions.dry_run.benefits.renewal_bcp_present') => renewal_bcp.present?,
               l10n('admin_actions.dry_run.benefits.renewal_bcp_oe_start_date') => renewal_bcp&.open_enrollment_start_on,
@@ -52,7 +55,12 @@ module Operations
               l10n('admin_actions.dry_run.benefits.renewal_bcp_end_date') => renewal_bcp&.end_on,
               l10n('admin_actions.dry_run.benefits.renewal_slcsp_id_present') => renewal_bcp&.slcsp_id.present?,
               l10n('admin_actions.dry_run.benefits.number_of_benefit_packages') => renewal_bcp&.benefit_packages&.count || 0,
-              l10n('admin_actions.dry_run.benefits.number_of_renewal_products') => products.count
+              l10n('admin_actions.dry_run.benefits.number_of_renewal_products') => products.count,
+              l10n('admin_actions.dry_run.benefits.create_renewals_on_date_change_feature_enabled') => create_renewals_on_date_change_feature.enabled?,
+              l10n('admin_actions.dry_run.benefits.renewals_creation_month') => create_renewals_on_date_change_feature&.settings(:renewals_creation_month)&.item,
+              l10n('admin_actions.dry_run.benefits.renewals_creation_day') => create_renewals_on_date_change_feature&.settings(:renewals_creation_day)&.item,
+              l10n('admin_actions.dry_run.benefits.atleast_one_silver_plan_donot_cover_pediatric_dental_cost_feature_enabled') => atleast_one_silver_plan_donot_cover_pediatric_dental_cost_feature.enabled?,
+              l10n('admin_actions.dry_run.benefits.slcsapd_feature_enabled_for_year', renewal_coverage_year: renewal_coverage_year) => atleast_one_silver_plan_donot_cover_pediatric_dental_cost_feature&.settings(renewal_coverage_year.to_s.to_sym)&.item
             }
 
             Success(result)
