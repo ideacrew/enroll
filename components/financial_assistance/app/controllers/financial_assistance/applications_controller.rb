50e4bca821272af3d80c2b11b7e8db6aaa78f2d4
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2024-09-05 20:52:55 -0700

    make sure application is found for raw_application and review_application (#4482)

--- components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
+++ components/financial_assistance/app/controllers/financial_assistance/applications_controller.rb
@@ -3,9 +3,10 @@
 module FinancialAssistance
   # IAP application controller
   class ApplicationsController < FinancialAssistance::ApplicationController
+
     before_action :set_current_person
     before_action :set_family
-    before_action :find_application, :except => [:index, :index_with_filter, :new, :review, :raw_application]
+    before_action :find_application, :except => [:index, :index_with_filter, :new]
     if EnrollRegistry.feature_enabled?(:bs4_consumer_flow)
       before_action :enable_bs4_layout, only: [:application_year_selection, :application_checklist, :edit, :eligibility_results, :review_and_submit, :review,
                                                :submit_your_application, :wait_for_eligibility_response, :preferences, :application_publish_error, :eligibility_response_error, :index, :index_with_filter]
