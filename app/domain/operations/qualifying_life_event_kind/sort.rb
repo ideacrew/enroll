063e7b8c80102c9b05f128c0b798074d95314017
Kristin Merbach <kristin@kristinmerbach.com>
2024-09-09 15:18:56 -0700

    started sep

--- app/domain/operations/qualifying_life_event_kind/sort.rb
+++ app/domain/operations/qualifying_life_event_kind/sort.rb
@@ -16,9 +16,13 @@ module Operations
 
       def persist_data(params)
         sort_data = params['sort_data']
+
+        # default for now
+        commonality_threshold = 10
         sort_data.each do |sort|
           qleks = ::QualifyingLifeEventKind.where(market_kind: params['market_kind'], id: sort['id'])
-          qleks.update(ordinal_position: sort['position'])
+          is_common = sort['position'].to_i <= commonality_threshold
+          qleks.update(ordinal_position: sort['position'], is_common: is_common)
         end
 
         Success('Successfully sorted Qualifying Life Event Kind objects')
