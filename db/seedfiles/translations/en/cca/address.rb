b369d3985a4eb40520b5ee5c545c13365a737dbc
Charlie Parker <charlie.parker@ideacrew.com>
2024-09-10 10:02:27 -0400

    remove unneeded translations, use correct text for address 1 label and placeholder

--- db/seedfiles/translations/en/cca/address.rb
+++ db/seedfiles/translations/en/cca/address.rb
@@ -1,12 +1,9 @@
 # Address Translations
 ADDRESS_TRANSLATIONS = {
-    :'en.address.address_1_text' => 'ADDRESS LINE 1 (NUMBER, STREET, QUADRANT) *',
-    :'en.address.address_2_text' => 'ADDRESS LINE 2 (APT, UNIT, ETC.)',
     :'en.address.city_text' => 'City *',
     :'en.address.select_state_text' => 'Select State *',
     :'en.address.zip_text' => 'ZIP *',
     :'en.address.address_1_error' => 'Please provide a valid address.',
-    :'en.address.address_1_placeholder' => '123 Main St',
     :'en.address.city_error' => 'Please provide a valid city.',
     :'en.address.state_error' => 'Please select a state.',
     :'en.address.zip_code_error' => 'Please provide a valid zipcode.',
--- db/seedfiles/translations/en/dc/address.rb
+++ db/seedfiles/translations/en/dc/address.rb
@@ -6,7 +6,6 @@ ADDRESS_TRANSLATIONS = {
     :'en.address.select_state_text' => 'Select State *',
     :'en.address.zip_text' => 'ZIP *',
     :'en.address.address_1_error' => 'Please provide a valid address.',
-    :'en.address.address_1_placeholder' => '123 Main St',
     :'en.address.city_error' => 'Please provide a valid city.',
     :'en.address.state_error' => 'Please select a state.',
     :'en.address.zip_code_error' => 'Please provide a valid zipcode.',
