cc39dcfd77bdabb1ef48df6d14dc3b5b98dd2660
Nisanth Yaganti <yagantinisanth@gmail.com>
2024-09-11 12:00:04 -0400

    Revert "validating county/zip combination on address (#4483)"
    This reverts commit 47dd62174a6457e18018aee3590714ce19f4e48a.

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -150,20 +150,8 @@ describe Address, "with proper validations" do
 
       context 'when county present' do
 
-        context 'with invalid county/zip combination' do
-          it 'returns false' do
-            expect(address.valid?).to eq false
-          end
-        end
-
-        context 'with valid county/zip' do
-          let(:county_zip) { FactoryBot.create(:benefit_markets_locations_county_zip) }
-          let(:county) { county_zip.county_name }
-          let(:zip) { county_zip.zip }
-
-          it 'returns true' do
-            expect(address.valid?).to eq true
-          end
+        it 'returns true' do
+          expect(address.valid?).to eq true
         end
       end
 
