85d2b78a78c1f07b931cf5ea0b522b29735acdaf
Charlie Parker <charlie.parker@ideacrew.com>
2024-09-12 08:03:37 -0400

    default threshold for persist order

--- app/domain/operations/qualifying_life_event_kind/update_list.rb
+++ app/domain/operations/qualifying_life_event_kind/update_list.rb
@@ -16,33 +16,37 @@ module Operations
       private
 
       def persist_data(input_params)
-        market_kind = input_params[:params][:market_kind]
+        @market_kind = input_params[:params][:market_kind]
         if input_params.dig(:params, :commonality_threshold).present?
-          persist_threshold(market_kind, input_params[:params][:commonality_threshold].to_i)
+          persist_threshold(input_params[:params][:commonality_threshold].to_i)
         elsif input_params.dig(:params, :sort_data).present?
-          persist_order(market_kind, input_params[:params][:sort_data])
+          persist_order(input_params[:params][:sort_data])
         else
           Failure('Invalid parameters')
         end
       end
 
-      def persist_threshold(market_kind, threshold)
-        qleks = ::QualifyingLifeEventKind.where(market_kind: market_kind)
+      def persist_threshold(threshold)
+        qleks = ::QualifyingLifeEventKind.where(market_kind: @market_kind)
         return Failure("Invalid threshold") unless threshold >= 0 && threshold <= qleks.count
 
         qleks.each_with_index do |qlek, index|
           qlek.update(is_common: index < threshold)
         end
+
         Success('Successfully clamped Qualifying Life Event Kind objects on the commonality threshold')
       rescue StandardError => e
         Failure(e.message)
       end
 
-      def persist_order(market_kind, sort_data)
-        # TODO: need rake to default threshold
-        sort_data.each do |sort|
-          qleks = ::QualifyingLifeEventKind.where(market_kind: market_kind, id: sort['id'])
-          qleks.update(ordinal_position: sort['position'])
+      def persist_order(sort_data)
+        qleks = ::QualifyingLifeEventKind.by_market_kind(@market_kind)
+        threshold = qleks.where(is_common: true).count
+        threshold = 10 if qleks.all? { |qlek| qlek.is_common.nil? }
+
+        qleks.each do |qlek|
+          sorted_index = sort_data.find { |sort| sort['id'] == qlek.id }['position']
+          qleks.update(ordinal_position: sorted_index, is_common: sorted_index < threshold)
         end
 
         Success('Successfully sorted Qualifying Life Event Kind objects')
