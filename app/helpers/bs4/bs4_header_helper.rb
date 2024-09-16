8dd36cf36af65599606b0ed121ae32889da09f6b
Kristin Merbach <kristin@kristinmerbach.com>
2024-09-16 07:31:06 -0700

    remove my portal link for single role users

--- app/helpers/bs4/bs4_header_helper.rb
+++ app/helpers/bs4/bs4_header_helper.rb
@@ -3,7 +3,7 @@
 # Here is a comment
 module Bs4
   # this is used with the new bs4 header
-  module Bs4HeaderHelper # rubocop:disable Metrics/ModuleLength
+  module Bs4HeaderHelper
     include L10nHelper
     include ApplicationHelper
 
@@ -53,50 +53,10 @@ module Bs4
       end
     end
 
-    def my_portal_link(controller) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-      ltext = l10n("layout.header.portal")
-      if current_user.nil?
-        nil
-      elsif current_user.try(:has_hbx_staff_role?)
-        link_to(ltext, main_app.exchanges_hbx_profiles_root_path)
-      elsif user_has_multiple_roles?
-        roles = all_non_admin_roles
-        render partial: 'ui-components/bs4/v1/navs/multi_role_my_portal_links', locals: roles
-      elsif display_i_am_broker_for_consumer?(current_user.person) && controller_path.exclude?('general_agencies')
-        link_to(ltext, get_broker_profile_path)
-      elsif current_user.try(:person).try(:csr_role) || current_user.try(:person).try(:assister_role)
-        link_to(ltext, main_app.home_exchanges_agents_path)
-      elsif current_user.person&.active_employee_roles&.any?
-        if controller_path.include?('broker_agencies')
-          link_to(ltext, get_broker_profile_path)
-        elsif controller_path.include?('general_agencies')
-          link_to(ltext, benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
-        elsif controller == 'employer_profiles' || controller_path.include?('employers')
-          #current user has both broker_agency staff role and employee role but not employer_staff_roles
-          if current_user.person.active_employer_staff_roles.present?
-            employer_profile_path = benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home')
-            link_to(ltext, employer_profile_path)
-          elsif current_user.try(:has_broker_agency_staff_role?)
-            link_to(ltext, get_broker_profile_path)
-          end
-        else
-          link_to(ltext, main_app.family_account_path)
-        end
-      elsif current_user.try(:has_consumer_role?)
-        link_to(ltext, main_app.family_account_path) if current_user.try(:has_consumer_role?).try(:identity_validation) == "valid"
-      # rubocop:disable Lint/DuplicateBranch
-      elsif current_user.try(:has_broker_agency_staff_role?) && controller_path.exclude?('general_agencies') && controller_path.exclude?('employers')
-        link_to(ltext, get_broker_profile_path)
-      # rubocop:enable Lint/DuplicateBranch
-      elsif current_user.try(:has_general_agency_staff_role?)
-        if current_user.try(:has_employer_staff_role?) && controller_path.include?('employers')
-          link_to(ltext, benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
-        else
-          link_to(ltext, benefit_sponsors.profiles_general_agencies_general_agency_profile_path(id: current_user.person.active_general_agency_staff_roles.first.benefit_sponsors_general_agency_profile_id))
-        end
-      elsif current_user.try(:has_employer_staff_role?)
-        link_to(ltext, benefit_sponsors.profiles_employers_employer_profile_path(id: current_user.person.active_employer_staff_roles.first.benefit_sponsor_employer_profile_id, :tab => 'home'))
-      end
+    def my_portal_link(_controller)
+      return nil unless user_has_multiple_roles?
+      roles = all_non_admin_roles
+      render partial: 'ui-components/bs4/v1/navs/multi_role_my_portal_links', locals: roles
     end
 
     def get_broker_profile_path # rubocop:disable Naming/AccessorMethodName
