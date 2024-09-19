31b5ed9f03b31cf58a01e6d39a077c62467f0021
Charlie Parker <charlie.parker@ideacrew.com>
2024-09-19 08:59:24 -0400

    support user accounts

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -25,7 +25,8 @@ class Exchanges::HbxProfilesController < ApplicationController
       :edit_dob_ssn,
       :get_user_info,
       :hide_form,
-      :outstanding_verification_dt
+      :outstanding_verification_dt,
+      :user_account_index
     ]
   end
   before_action :permitted_params_family_index_dt, only: [:family_index_dt]
