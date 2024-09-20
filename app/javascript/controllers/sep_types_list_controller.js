6fd94f07ff69d2e66b417bc6154de29ce1485181
Charlie Parker <charlie.parker@ideacrew.com>
2024-09-20 15:33:32 -0400

    fix default option (#4545)
    * default model attribute to false, update common scope to include nils to support existing data, remove field from create/edit SEP
    
    * support legacy UI with threshold functionality, refactor stimulus controller to consolidate response handling
    
    * more refactoring of banners
    
    * translation fix for rare button
    
    * comment fixes
    
    * remove redundant splat

--- app/javascript/controllers/sep_types_list_controller.js
+++ app/javascript/controllers/sep_types_list_controller.js
@@ -46,11 +46,12 @@ class UpdateListManager {
   }
 
   /**
-   * Perform a PATCH request to update the QLE list.
+   * Perform a PATCH request to update the QLE list and shows the response banner.
    * @param {Object} body The request body containing the list update data.
-   * @returns {Promise} The fetch request promise.
+   * @param {String} bannerDescription The message to use in the legacy response banner subheader.
+   * @returns {Bool} The success status of the request.
    */
-  async updateList(body) {
+  async updateList(body, bannerDescription) {
     body.market_kind = this.marketKind;
 		const response = await fetch('update_list', {
       method: 'PATCH',
@@ -60,19 +61,50 @@ class UpdateListManager {
         'X-CSRF-Token': Rails.csrfToken()
       }
     });
-    return await response.json();
+
+    let data = await response.json();
+    let isSuccess = data["status"] === "success";
+    this.showBanner(isSuccess, data["message"], bannerDescription);
+
+    return isSuccess;
   }
 
   /**
    * Show or hide the respective response banner.
    * @param {boolean} isSuccess The success status of the request.
+   * @param {string} bannerTitle The message to use in the legacy response banner header.
+   * @param {string} bannerDescription The message to use in the legacy response banner subheader.
    */
-  showBanner(isSuccess) {
-    const successBanner = $('#success-flash');
-    const errorBanner = $('#error-flash');
-    successBanner.toggleClass('hidden', !isSuccess)
-    errorBanner.toggleClass('hidden', isSuccess)
-    return isSuccess ? successBanner : errorBanner;
+  showBanner(isSuccess, bannerTitle, bannerDescription) {
+    if (bs4) {
+      const successBanner = $('#success-flash');
+      const errorBanner = $('#error-flash');
+      successBanner.toggleClass('hidden', !isSuccess)
+      errorBanner.toggleClass('hidden', isSuccess)
+      var flashDiv = isSuccess ? successBanner : errorBanner;
+    } else {
+      var flashDiv = $("#sort_notification_msg");
+      var flashHeader = flashDiv.find(".toast-header");
+      flashDiv.show();
+      
+      flashDiv.removeClass("success").removeClass("error");
+      flashDiv.toggleClass("success", isSuccess).toggleClass("error", !isSuccess);
+      flashHeader.removeClass("success").removeClass("error");
+      flashHeader.toggleClass("success", isSuccess).toggleClass("error", !isSuccess);
+      
+      flashHeader.find("strong").text(bannerTitle);
+     
+      var flashBody = flashDiv.find(".toast-body");
+      if (bannerDescription) {
+        flashBody.removeClass("hidden").text(bannerDescription);
+      } else {
+        flashBody.addClass("hidden");
+      }
+    }
+
+    setTimeout(function() {
+      flashDiv.addClass('hidden');
+    }, 3500);
   }
 }
 
@@ -107,35 +139,14 @@ class UpdateOrderManager extends UpdateListManager {
       return;
     }
 
-    // map the card elements to an array of card ids and their ordinal positions
-    let enumeratedCards = cards.entries();
+    let enumeratedCards = [...cards.entries()];
     for (const [index, card] of enumeratedCards) {
       card.dataset.index = index;
       card.dataset.ordinal_position = index + 1;
     }
-    let sortData = [...enumeratedCards].map((entry) => { return { id: entry[1].dataset.id, position: entry[0] + 1 } });
-    super.updateList({sort_data: sortData})
-      .then(data => {
-        let isSuccess = data['status'] === "success";
-        if (bs4) {
-          var flashDiv = super.showBanner(isSuccess);
-        } else {
-          var flashDiv = $("#sort_notification_msg");
-          var flashHeader = flashDiv.find(".toast-header");
-          flashDiv.show();
-          flashDiv.toggleClass("success", isSuccess);
-          flashDiv.removeClass("error", !isSuccess);
-          flashHeader.toggleClass("success", isSuccess);
-          flashHeader.removeClass("error", !isSuccess);
-          
-          flashHeader.find("strong").text(data['message']);
-          flashDiv.find(".toast-body").text(event.item.textContent);
-        }
 
-        setTimeout(function() {
-          flashDiv.addClass('hidden');
-        }, 3500);
-		})
+    let sortData = enumeratedCards.map((entry) => { return { id: entry[1].dataset.id, position: entry[0] + 1 } });
+    super.updateList({sort_data: sortData}, event.item.textContent);
 	}
 }
 
@@ -156,11 +167,9 @@ class UpdateThresholdManager extends UpdateListManager {
     let threshold = this.thresholdInputTarget.value;
 
     super.updateList({commonality_threshold: threshold})
-      .then(data => {
-        let isSuccess = data['status'] === 'success';
+      .then(isSuccess => {
         if (isSuccess) {
           this.thresholdInputTarget.dataset.initialValue = threshold;
-          super.showBanner(true);
 
           // move the threshold marker to the new threshold index or hide it if out of bounds
           let thresholdMarker = this.qleList.find('#threshold-marker').removeClass('hidden');
@@ -172,7 +181,6 @@ class UpdateThresholdManager extends UpdateListManager {
           }
         } else {
           this.thresholdInputTarget.value = this.thresholdInputTarget.dataset.initialValue;
-          super.showBanner(false);
         }
       })
   }
