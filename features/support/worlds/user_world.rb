1eda9b7f1e4c3df2a1ff988da92af991ba12779b
Kristin Merbach <kristin@kristinmerbach.com>
2024-09-20 09:39:36 -0700

    started more cucumbers

--- features/support/worlds/user_world.rb
+++ features/support/worlds/user_world.rb
@@ -1,8 +1,11 @@
 module UserWorld
   def employee(employer = nil)
+  def employee(employer = nil)
     if @employee
       @employee
     else
+      employer_staff_role = FactoryBot.create(:benefit_sponsor_employer_staff_role, aasm_state: 'is_active', benefit_sponsor_employer_profile_id: employer.profiles.first.id)
+      person = FactoryBot.create(:person, employer_staff_roles: [employer_staff_role])
       employer_staff_role = FactoryBot.create(:benefit_sponsor_employer_staff_role, aasm_state: 'is_active', benefit_sponsor_employer_profile_id: employer.profiles.first.id)
       person = FactoryBot.create(:person, employer_staff_roles: [employer_staff_role])
       @employee = FactoryBot.create(:user, :person => person)
@@ -11,9 +14,11 @@ module UserWorld
 
   def employer_staff
     @employer_staff || @employer_staff = FactoryBot.create(:user, :employer_staff, email: "employer@example.com")
+    @employer_staff || @employer_staff = FactoryBot.create(:user, :employer_staff, email: "employer@example.com")
   end
 
   def employee_role
+    @employee_role || @employee_role = FactoryBot.create(:user, :employee, email: "employee@example.com")
     @employee_role || @employee_role = FactoryBot.create(:user, :employee, email: "employee@example.com")
   end
 
@@ -45,14 +50,14 @@ module UserWorld
       hbx_profile_id = FactoryBot.create(:hbx_profile).id
       person = FactoryBot.create(:person)
       raise "No subrole was provided" if subrole.blank?
-      if Permission.where(name: subrole).present? # rubocop:disable Style/GuardClause
+      if Permission.where(name: subrole).present?
         permission = Permission.where(name: subrole).first
         permission.update_attributes(can_modify_plan_year: true)
         permission_id = permission.id
       else
         raise "No permission was found for subrole #{subrole}"
       end
-      HbxStaffRole.create!(person: person, permission_id: permission_id, subrole: subrole, hbx_profile_id: hbx_profile_id)
+      hbx_staff_role = HbxStaffRole.create!(person: person, permission_id: permission_id, subrole: subrole, hbx_profile_id: hbx_profile_id)
       @admin = FactoryBot.create(:user,:with_hbx_staff_role, :person => person)
     end
   end
@@ -76,7 +81,23 @@ Given(/^that a user with a (.*?) role(?: with (.*?) subrole)? exists and (.*?) l
          when "Employer"
            employee(employer)
          when "Broker"
+  user = case type
+         when "Employer"
+           employee(employer)
+         when "Broker"
     # in features/step_definitions/broker_employee_quote_steps.rb BrokerWorld module
+           broker(email: "broker@example.com")
+         when "HBX staff"
+           admin(subrole)
+         when 'Employer Role'
+           employer_staff
+         when 'Employee Role'
+           employee_role
+         when 'HBX CSR Tier1'
+           admin('hbx_csr_tier1')
+         else
+           users_by_role(type)
+         end
            broker(email: "broker@example.com")
          when "HBX staff"
            admin(subrole)
@@ -94,6 +115,10 @@ Given(/^that a user with a (.*?) role(?: with (.*?) subrole)? exists and (.*?) l
     login_as(user, :scope => :user)
   when 'is not'
     nil
+  when 'is'
+    login_as(user, :scope => :user)
+  when 'is not'
+    nil
   end
 end
 
@@ -105,6 +130,16 @@ end
 
 And(/^user with (.*?) role is (.*?)$/) do |type, locked_status|
   case type
+  when "Employer"
+    user = employee(employer)
+  when "Broker"
+    user = broker
+  when "HBX staff"
+    user = admin(subrole)
+  when 'Employer Role'
+    user = employer_staff
+  when 'Employee Role'
+    user = employee_role
   when "Employer"
     user = employee(employer)
   when "Broker"
@@ -152,23 +187,23 @@ When(/^the user clicks Action for that Employer$/) do
 end
 
 Then(/^the user will see the Extend Open Enrollment button$/) do
-  expect(page).to have_css('.btn.btn-xs', text: 'Extend Open Enrollment')
+  expect(page).to have_css('.action-item', text: 'Extend Open Enrollment')
 end
 
 And(/^the user clicks the Plan Years button$/) do
-  find('.btn.btn-xs', text: 'Plan Years', wait: 5).click
+  find('.action_item', text: 'Plan Years', wait: 5).click
 end
 
 Then(/^the user will see the Plan Years button$/) do
-  find('.btn.btn-xs', text: 'Plan Years', wait: 5).click
+  find('.action_item', text: 'Plan Years', wait: 5).click
 end
 
 Then(/^the user will not see the Extend Open Enrollment button$/) do
-  expect(page).to_not have_css('.btn.btn-xs', text: 'Extend Open Enrollment')
+  expect(page).to_not have_css('.action_item', text: 'Extend Open Enrollment')
 end
 
 When(/^the user clicks Extend Open Enrollment$/) do
-  find('.btn.btn-xs', text: 'Extend Open Enrollment').click
+  find('.action_item', text: 'Extend Open Enrollment').click
 end
 
 When(/^the user clicks Edit Open Enrollment$/) do
@@ -181,6 +216,7 @@ end
 
 Then(/^the user enters a new open enrollment end date$/) do
   input = find('input.hasDatepicker')
+  input.set(Date.today + 1.week)
   input.set(Date.today + 1.week)
 end
 
@@ -226,6 +262,7 @@ When(/^the user clicks the X icon on the Create Plan Year form$/) do
 end
 
 Then(/^the Effective End Date will populate with a date equal to one year minus (\d+) day from the Effective Start Date$/) do |_arg1|
+Then(/^the Effective End Date will populate with a date equal to one year minus (\d+) day from the Effective Start Date$/) do |_arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -235,17 +272,17 @@ end
 
 Then(/^the user will( not)? see the Change FEIN button$/) do |visible|
   if visible.blank?
-    expect(page).to have_css('.btn.btn-xs', text: 'Change FEIN')
+    expect(page).to have_css('.action_button', text: 'Change FEIN')
   else
-    expect(page).to_not have_css('.btn.btn-xs', text: 'Change FEIN')
+    expect(page).to_not have_css('.action_button', text: 'Change FEIN')
   end
 end
 
 Then(/^the user will( not)? see the Force Publish button$/) do |visible|
   if visible.blank?
-    expect(page).to have_css('.btn.btn-xs', text: 'Force Publish')
+    expect(page).to have_css('.action_button', text: 'Force Publish')
   else
-    expect(page).to_not have_css('.btn.btn-xs', text: 'Force Publish')
+    expect(page).to_not have_css('.action_button', text: 'Force Publish')
   end
 end
 
