fd4fed4f823e95985f84327cf3393c9253787c5b
Kristin Merbach <kristin@kristinmerbach.com>
2024-09-30 08:38:48 -0700

    resource registry page

--- db/seedfiles/translations/en/me/admin_actions.rb
+++ db/seedfiles/translations/en/me/admin_actions.rb
@@ -1,5 +1,11 @@
 # Translations for Admin actions.
 ADMIN_ACTIONS_TRANSLATIONS = {
+  'en.admin.rr.heading' => 'Resource Registry Settings',
+  'en.admin.rr.note' => 'Note: The following features are enabled in the Resource Registry for web pods some configs may vary for other pods.',
+  'en.admin.rr.item' => 'Item',
+  'en.admin.applicants' => "Applicants",
+  'en.admin.applicant' => "Applicant",
+  'en.admin.accepted' => "Accepted",
   'en.admin_actions.bulk_upload.new.description_text' => 'Use this tool to send the same message or notice to a specific set of users. Copy and paste the FEINs or HBX IDs for the users who should get the message in their secure inbox. Remember, each FEIN or HBX ID must be separated by a comma.',
   'en.admin_actions.bulk_upload.new.description_sub_text' => 'The message and any attachments will be the same for everyone on the recipient list – they cannot be personalized. You can attach files in PDF, JPEG, or PNG formats.',
   'en.admin_actions.drop_enrollment_members' => 'Disenroll Member(s)',
