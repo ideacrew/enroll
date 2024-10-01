d33c16b167a69faff12af809b524ae5176a351de
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2024-10-01 09:32:39 -0400

    Refactor applicant_qnc_code method to simplify boolean checks and remove unnecessary present? usage

--- components/financial_assistance/app/domain/financial_assistance/operations/applications/transformers/application_to/cv3_application.rb
+++ components/financial_assistance/app/domain/financial_assistance/operations/applications/transformers/application_to/cv3_application.rb
@@ -134,9 +134,12 @@ module FinancialAssistance
 
             def applicant_qnc_code(applicant)
               return true if use_defaults_for_qnc_and_five_year_bar_data?
-              return applicant.qualified_non_citizen if applicant.qualified_non_citizen.present?
 
-              applicant.eligible_immigration_status ? true : false
+              # Check qualified_non_citizen directly, no need for .present? as it's a boolean
+              return applicant.qualified_non_citizen unless applicant.qualified_non_citizen.nil?
+
+              # Return true if eligible_immigration_status is truthy, otherwise false
+              applicant.eligible_immigration_status.present?
             end
 
             def applicant_five_year_bar_met(applicant)
