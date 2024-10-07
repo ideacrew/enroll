6f739a92dd4ffed43273778d58d9c16347656611
Charlie Parker <charlie.parker@ideacrew.com>
2024-10-07 13:01:38 -0400

    add instance_for_action spec

--- components/financial_assistance/spec/models/financial_assistance/services/summary_service_spec.rb
+++ components/financial_assistance/spec/models/financial_assistance/services/summary_service_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe ::FinancialAssistance::Services::SummaryService do
   let!(:application) { FactoryBot.create(:application, family_id: BSON::ObjectId.new, assistance_year: Date.today.year) }
-  let(:applicant) do
+  let!(:applicant) do
     FactoryBot.create(:applicant, application: application,
                                   ssn: '243108282',
                                   dob: Date.new(1984, 3, 8),
@@ -14,8 +14,65 @@ describe ::FinancialAssistance::Services::SummaryService do
                                   is_primary_applicant: true)
   end
   let(:cfl_service) { ::FinancialAssistance::Services::ConditionalFieldsLookupService.new }
-  subject { ::FinancialAssistance::Services::SummaryService.new(is_concise: false, can_edit: false, cfl_service: cfl_service, application: application, applicants: application.active_applicants) }
+
+  describe('.instance_for_action') do
+    context 'when action is raw_application' do
+      let(:action_name) { 'raw_application' }
+      subject { described_class.instance_for_action(action_name, cfl_service, application, application.active_applicants) }
+
+      it 'should return instance of SummaryService with applicant_summaries of type AdminApplicantSummary' do
+        expect(subject.instance_variable_get(:@applicant_summaries).first).to be_a(FinancialAssistance::Services::SummaryService::Summary::ApplicantSummary::ApplicantSummary::AdminApplicantSummary)
+      end
+
+      it 'should return instance of SummaryService that is not editable' do
+        expect(subject.can_edit_incomes).to be_falsey
+
+        applicant_summary = subject.instance_variable_get(:@applicant_summaries).first
+        edit_links = applicant_summary.hash[:subsections].pluck(:edit_link).compact
+        expect(edit_links.length).to be_empty
+      end
+    end
+
+    context 'when action is review_and_submit' do
+      let(:action_name) { 'review_and_submit' }
+      subject { described_class.instance_for_action(action_name, cfl_service, application, application.active_applicants) }
+
+      it 'should return instance of SummaryService with applicant_summaries of type ConsumerApplicantSummary' do
+        expect(subject.instance_variable_get(:@applicant_summaries).first).to be_a(FinancialAssistance::Services::SummaryService::Summary::ApplicantSummary::ApplicantSummary::ConsumerApplicantSummary)
+      end
+
+      it 'should return instance of SummaryService that is editable' do
+        expect(subject.can_edit_incomes).to be_truthy
+
+        applicant_summary = subject.instance_variable_get(:@applicant_summaries).first
+        edit_links = applicant_summary.hash[:subsections].pluck(:edit_link).compact
+        binding.irb
+        expect(edit_links.length).to be > 0
+      end
+    end
+
+
+    context 'when action is review' do
+      let(:action_name) { 'review' }
+      subject { described_class.instance_for_action(action_name, cfl_service, application, application.active_applicants) }
+
+      it 'should return instance of SummaryService with applicant_summaries of type ConsumerApplicantSummary' do
+        expect(subject.instance_variable_get(:@applicant_summaries).first).to be_a(FinancialAssistance::Services::SummaryService::Summary::ApplicantSummary::ApplicantSummary::ConsumerApplicantSummary)
+      end
+
+      it 'should return instance of SummaryService that is not editable' do
+        expect(subject.can_edit_incomes).to be_falsey
+
+        applicant_summary = subject.instance_variable_get(:@applicant_summaries).first
+        edit_links = applicant_summary.hash[:subsections].pluck(:edit_link).compact
+        expect(edit_links.length).to be_empty
+      end
+    end
+  end
+
   describe 'applicant_summaries' do
+    subject { ::FinancialAssistance::Services::SummaryService.new(is_concise: false, can_edit: false, cfl_service: cfl_service, application: application, applicants: application.active_applicants) }
+    
     context "when benefits are present" do
       let!(:benefit) do
         ben = FactoryBot.build(:financial_assistance_benefit, employer_name: 'Test Employer', insurance_kind: 'employer_sponsored_insurance')
@@ -28,6 +85,7 @@ describe ::FinancialAssistance::Services::SummaryService do
       end
 
       it "should return esi hash" do
+        binding.irb
         esi_benefit_hash = subject.sections.first[:subsections][4][:rows][1][:coverages].first.first
         expect(esi_benefit_hash[:employer_name][:value]).to eq "Test Employer"
         expect(esi_benefit_hash[:employer_address_line_1][:value]).to eq "address_1"
