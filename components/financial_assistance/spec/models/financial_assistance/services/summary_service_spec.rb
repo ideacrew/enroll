95e65c5fec7ecc87f0c8bfaf999ef6aaa2ef8821
Charlie Parker <charlie.parker@ideacrew.com>
2024-10-07 14:24:46 -0400

    spec fixes

--- components/financial_assistance/spec/models/financial_assistance/services/summary_service_spec.rb
+++ components/financial_assistance/spec/models/financial_assistance/services/summary_service_spec.rb
@@ -59,26 +59,43 @@ describe ::FinancialAssistance::Services::SummaryService do
     end
   end
 
-  describe 'applicant_summaries' do
-    subject { ::FinancialAssistance::Services::SummaryService.new(is_concise: false, can_edit: false, cfl_service: cfl_service, application: application, applicants: application.active_applicants) }
-    
-    context "when benefits are present" do
-      let!(:benefit) do
-        ben = FactoryBot.build(:financial_assistance_benefit, employer_name: 'Test Employer', insurance_kind: 'employer_sponsored_insurance')
-        ben.build_employer_address(kind: 'home', address_1: 'address_1', city: 'Dummy City', state: 'DC', zip: '20001')
-        ben.build_employer_phone(kind: 'home', country_code: '001', area_code: '123', number: '4567890', primary: true)
-        applicant.benefits << ben
-        applicant.update_attributes(has_eligible_health_coverage: true)
-        applicant.save!
-        ben
-      end
+  describe '#sections' do
+    describe('when is_concise is false') do
+      subject { ::FinancialAssistance::Services::SummaryService.new(is_concise: false, can_edit: false, cfl_service: cfl_service, application: application, applicants: application.active_applicants).sections.first }
 
-      it "should return esi hash" do
-        binding.irb
-        esi_benefit_hash = subject.sections.first[:subsections][4][:rows][1][:coverages].first.first
-        expect(esi_benefit_hash[:employer_name][:value]).to eq "Test Employer"
-        expect(esi_benefit_hash[:employer_address_line_1][:value]).to eq "address_1"
-        expect(esi_benefit_hash[:city][:value]).to eq "Dummy City"
+      describe('health coverage section') do
+        let(:section) { subject[:subsections][4][:rows] }
+
+        context "when benefits are present" do
+          let!(:benefit) do
+            ben = FactoryBot.build(:financial_assistance_benefit, employer_name: 'Test Employer', insurance_kind: 'employer_sponsored_insurance')
+            ben.build_employer_address(kind: 'home', address_1: 'address_1', city: 'Dummy City', state: 'DC', zip: '20001')
+            ben.build_employer_phone(kind: 'home', country_code: '001', area_code: '123', number: '4567890', primary: true)
+            applicant.benefits << ben
+            applicant.update_attributes(has_eligible_health_coverage: true)
+            applicant.save!
+            ben
+          end
+
+          it "should return esi hash" do
+            esi_benefit_hash = section[1][:coverages].first.first
+            expect(esi_benefit_hash[:employer_name][:value]).to eq "Test Employer"
+            expect(esi_benefit_hash[:employer_address_line_1][:value]).to eq "address_1"
+            expect(esi_benefit_hash[:city][:value]).to eq "Dummy City"
+          end
+        end
+
+        context "when benefits are not present" do
+          before do
+            applicant.update_attributes(has_eligible_health_coverage: false, has_enrolled_health_coverage: false)
+            applicant.save!
+          end
+
+          it "should not return esi hash" do
+            expect(section[0][:coverages]).to be_nil
+            expect(section[1][:coverages]).to be_nil
+          end
+        end
       end
     end
   end
