ae8c8b2244129282d057d47155a9eba11b6c3f2c
Trey Evans <lewis.r.evans@gmail.com>
2024-10-09 16:50:25 -0400

    Fix other issues with inline styles.

--- config/initializers/rails_csp_nonce.rb
+++ config/initializers/rails_csp_nonce.rb
@@ -14,21 +14,28 @@ module EnrollMiddleware
       request = ActionDispatch::Request.new env
       stat, headers, body = @app.call(env)
       nonce = request.content_security_policy_nonce
-      content_type_header = headers['Content-Type']
-      content_type = Mime::Type.lookup(content_type_header)
+      resp = ActionDispatch::Response.new(stat, headers, body)
+      content_type = Mime::Type.lookup(resp.content_type || Mime[:html])
       return [stat, headers, body] unless content_type&.html?
 
       doc = nil
-      if body.is_a?(ActionDispatch::Response::RackBody)
-        doc = Loofah.document(body.body)
-      else
-        doc = Loofah.document(body)
+      begin
+        if body.is_a?(ActionDispatch::Response::RackBody)
+          doc = Loofah.document(body.body)
+        else
+          doc = Loofah.document(body)
+        end
+      rescue
+        return [stat, headers, body]
       end
       doc.xpath("//script").each do |node|
         nonce_node(node, nonce, :script, request)
       end
-      doc.xpath("//style").each do |node|
-        nonce_node(node, nonce, :style, request)
+      #doc.xpath("//style").each do |node|
+      #  nonce_node(node, nonce, :style, request)
+      #end
+      doc.xpath("//*[@onclick]").each do |node|
+        request.logger.tagged("CSPTagMissingNonce").warn("Detected inline onclick event handler: #{node["onclick"]}")
       end
       response = ActionDispatch::Response.new(stat, headers, doc.to_s)
       response.to_a
@@ -43,7 +50,7 @@ module EnrollMiddleware
 end
 
 Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-Rails.application.config.content_security_policy_nonce_directives = %w(script-src style-src)
+Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
 
 Rails.application.config.content_security_policy do |policy|
   csp_proto = Rails.env.production? ? :https : :http
@@ -52,8 +59,9 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, csp_proto, :data, "*.gstatic.com",  "*.fontawesome.com"
   policy.img_src     :self, csp_proto, :data, "*.google-analytics.com", "*.gstatic.com", "*.googletagmanager.com"
   policy.script_src  :self, csp_proto, "https://tagmanager.google.com", "https://www.googletagmanager.com", "https://apps.usw2.pure.cloud", "*.fontawesome.com", "*.google-analytics.com"
-  policy.style_src   :self, csp_proto, "unsafe-inline", "https://tagmanager.google.com", "https://www.googletagmanager.com", "https://fonts.googleapis.com", "*.fontawesome.com"
+  policy.style_src   :self, csp_proto, "'unsafe-inline'", "https://tagmanager.google.com", "https://www.googletagmanager.com", "https://fonts.googleapis.com", "*.fontawesome.com"
   policy.connect_src :self, csp_proto, "https://api.usw2.pure.cloud", "wss://streaming.usw2.pure.cloud"
+  policy.media_src   :self, csp_proto, :data
 end
 
 Rails.application.config.middleware.insert_after Browser::Middleware, EnrollMiddleware::RailsContentNonceRewrite
