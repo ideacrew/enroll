87f97bfced992929cf7e5eefdd2a0a91d664bf92
TristanB17 <tristan0bambauer@gmail.com>
2024-10-10 09:32:23 -0600

    remove methods from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,66 +9,6 @@ class UsersController < ApplicationController
   before_action :set_user, except: [:confirm_lock, :unsupported_browser]
   before_action :enable_bs4_layout, only: [:login_history, :change_username_and_email, :confirm_change_username_and_email] if EnrollRegistry.feature_enabled?(:bs4_admin_flow)
 
-  # Confirms the lock action for a user.
-  #
-  # @return [void]
-  # @response_to JS
-  def confirm_lock
-    authorize HbxProfile, :confirm_lock?
-    @user_id  = params[:user_action_id]
-
-    respond_to do |format|
-      format.js { render 'confirm_lock' }
-    end
-  end
-
-  # Locks a user.
-  #
-  # @return [void]
-  # @response_to JS
-  def lockable
-    authorize HbxProfile, :lockable?
-    @user.lock!
-    flash[:notice] = "User #{user.email} is successfully #{user.lockable_notice}."
-
-    respond_to do |format|
-      format.js { render 'lockable' }
-    end
-  end
-
-  # Resets a user's password.
-  #
-  # @return [void]
-  # @response_to JS
-  def reset_password
-    authorize HbxProfile, :reset_password?
-
-    respond_to do |format|
-      format.js { render 'reset_password' }
-    end
-  end
-
-  # Confirms the reset password action for a user.
-  #
-  # @return [void]
-  # @response_to JS
-  def confirm_reset_password
-    authorize HbxProfile, :confirm_reset_password?
-    @error = nil
-    validate_email if params[:user].present?
-
-    respond_to do |format|
-      format.js do
-        if @error.nil?
-          User.send_reset_password_instructions(email: @user.email)
-          redirect_to user_account_index_exchanges_hbx_profiles_url, notice: "Reset password instruction sent to user email."
-        else
-          render 'reset_password'
-        end
-      end
-    end
-  end
-
   # Changes a user's username and email.
   #
   # @return [void]
@@ -94,7 +34,6 @@ class UsersController < ApplicationController
     if @email_taken.present? || @username_taken.present?
       @matches = true
     else
-      @error_params = { email: @user.email, oim_id: @user.oim_id }
       @user.oim_id = params[:new_oim_id] if params[:new_oim_id] != params[:current_oim_id]
       @user.email = params[:new_email] if params[:new_email] && (params[:new_email] != params[:current_email])
       begin
@@ -105,11 +44,8 @@ class UsersController < ApplicationController
       end
     end
     respond_to do |format|
-      if @errors
-        format.js { render "change_username_and_email"}
-      else
-        format.js { render "username_email_result"}
-      end
+      format.js { render "change_username_and_email"} if @errors
+      format.js { render "username_email_result" }
     end
   end
 
