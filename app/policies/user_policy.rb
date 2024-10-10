d1976ac133d24aa591bab9ca21cb6b5eabc0b344
TristanB17 <tristan0bambauer@gmail.com>
2024-10-10 13:31:30 -0600

    fix issue with user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -6,14 +6,14 @@ class UserPolicy < ApplicationPolicy
   end
 
   def lockable?
-    return false unless role == user&.person && user&.person&.hbx_staff_role
-    return false unless role.permission
+    role = user&.person && user&.person&.hbx_staff_role
+    return false unless role.present? && role.permission
     role.permission.can_lock_unlock
   end
 
   def reset_password?
-    return false unless role == user&.person && user&.person&.hbx_staff_role
-    return false unless role.permission
+    role = user&.person && user&.person&.hbx_staff_role
+    return false unless role.present? && role.permission
     role.permission.can_reset_password
   end
 
