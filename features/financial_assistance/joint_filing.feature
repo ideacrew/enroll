b5e4133c74af14cd225eb16401eff101e3eb711b
Jacob Kagon <jacob@kagon.net>
2024-10-10 12:01:48 -0400

    cuke fix

--- features/financial_assistance/joint_filing.feature
+++ features/financial_assistance/joint_filing.feature
@@ -1,14 +1,15 @@
-Feature: Tax Info Page with bs4 enabled
+Feature: Joint Filing Taxes
 
   Background: User can edit tax info page for a household member
-    Given bs4_consumer_flow feature is enabled
-    Given the contrast level aa feature is enabled
+    Given bs4_consumer_flow feature is disable
     Given the FAA feature configuration is enabled
     Given a consumer, with a family, exists
-    Given that the user is on the FAA Household Info page with bs4 enabled
+    Given that the user is on the FAA Household Info page
   
   Scenario: Filing Jointly is Selected
     Given the user clicks add Income and Coverage Information
+    And the user is on the tax info page
     And the user selects they are filing jointly
+    And the user lands on the Job Incomes Page
     And the user navigates back to the tax info page
     Then the user will see that the is filing jointly question is true
--- features/financial_assistance/step_definitions/tax_info_steps.rb
+++ features/financial_assistance/step_definitions/tax_info_steps.rb
@@ -25,24 +25,13 @@ Given(/^that the user is on the FAA Household Info page$/) do
   find('a.interaction-click-control-continue').click
 end
 
-Given(/^that the user is on the FAA Household Info page with bs4 enabled$/) do
-  login_as consumer, scope: :user
-  step "the consumer is RIDP verified"
-  hbx_profile = FactoryBot.create(:hbx_profile, :open_enrollment_coverage_period)
-  hbx_profile.benefit_sponsorship.benefit_coverage_periods.each do |bcp|
-    ivl_product = FactoryBot.create(:benefit_markets_products_health_products_health_product, :ivl_product, application_period: (bcp.start_on..bcp.end_on))
-    bcp.update_attributes!(slcsp_id: ivl_product.id)
-  end
-
-  visit help_paying_coverage_insured_consumer_role_index_path
-  find(".interaction-choice-control-value-radio1").click
-  find("#continue_button").click
-  find("#btn-continue").click
-  find_all(".interaction-click-control-begin-application")[0].click
-end
 
 And(/^the user clicks add Income and Coverage Information$/) do
   find_all(".interaction-click-control-add-income---coverage-info")[0].click
+end
+
+And(/^the user is on the tax info page$/) do
+  find(".interaction-choice-control-value-is-required-to-file-taxes-yes")
 end
 
 And(/^the user selects they are filing jointly$/) do
@@ -50,6 +39,10 @@ And(/^the user selects they are filing jointly$/) do
   find("#is_joint_tax_filing_yes").click
   find("#is_claimed_as_tax_dependent_no").click
   find("#btn-continue").click
+end
+
+And(/^the user lands on the Job Incomes Page$/) do
+  find(".incomes")
 end
 
 And(/^the user navigates back to the tax info page$/) do
