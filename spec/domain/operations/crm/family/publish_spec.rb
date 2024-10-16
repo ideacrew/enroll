1ac6f35f9f00467df7160763b7108946e50c90df
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2024-10-16 13:57:06 -0400

    sends timestamps in string format to include nanoseconds (#4661)
    sends timestamp in string format to also pass nano seconds for better comparison

--- spec/domain/operations/crm/family/publish_spec.rb
+++ spec/domain/operations/crm/family/publish_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Operations::Crm::Family::Publish do
+  include TimeHelper
+
   before :all do
     DatabaseCleaner.clean
   end
@@ -20,8 +22,8 @@ RSpec.describe Operations::Crm::Family::Publish do
 
       it 'publishes the event successfully' do
         result = subject.call(hbx_id: hbx_id)
-        expect(result.success).to eq(
-          "Successfully published event: events.families.created_or_updated for family with primary person hbx_id: #{hbx_id}"
+        expect(result.success).to match(
+          "Successfully published event: events.families.created_or_updated for family with primary person hbx_id: #{hbx_id} with headers: "
         )
       end
     end
