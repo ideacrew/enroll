6e23827dbb9ab40400bee60aa348c78a7d243fe2
sri49 <sriharsha.poosa@gmail.com>
2024-10-17 16:18:32 -0400

    Revert "report to compare renewal products for enrollments (#4626)"
    This reverts commit 2f638d034d0b5f617f8762af05cb91bd5de34fe1.

--- script/enrollment_renewal_product_report.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Triggers the EnrollmentRenewalProductReport operation
-# current_year=2024 bundle exec rails runner script/product_cross_walk_enrollment_report.rb
-Operations::Reports::EnrollmentRenewalProductReport.new.call({current_year: current_year.to_i})
