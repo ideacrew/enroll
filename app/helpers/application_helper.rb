d692f29646eb6aad81faebf341d81b908fca891f
jayreddy519 <jayanth.rordev@gmail.com>
2024-10-23 14:07:15 -0400

    latest updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -617,7 +617,7 @@ module ApplicationHelper
   def display_relationship(relationship)
     relationship_mapping = {
       'domestic_partners_child' => "Domestic partner's child",
-      'parents_domestic_partner' => "Parent's domestic partner",
+      'parents_domestic_partner' => "Parent's domestic partner"
     }
 
     relationship_mapping[relationship].try(:humanize) || relationship.try(:humanize)
--- components/financial_assistance/app/helpers/financial_assistance/application_helper.rb
+++ components/financial_assistance/app/helpers/financial_assistance/application_helper.rb
@@ -213,7 +213,7 @@ module FinancialAssistance
 
       relationship_mapping = {
         'domestic_partners_child' => "Domestic partner's child",
-        'parents_domestic_partner' => "Parent's domestic partner",
+        'parents_domestic_partner' => "Parent's domestic partner"
       }
 
       relationship_mapping[relationship].try(:humanize) || relationship.try(:humanize)
